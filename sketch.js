a3b2d26233e1d04f36c5c864b70faa4e67e77841
Tom White <tom@sixdozen.com>
2017-03-06 17:26:08 +1300

    Creative Coding 2: Custom Pixel starter code

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let sourceImg=null;
+let maskImg=null;
+let renderCounter=0;
+
+function preload() {
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
+}
+
+function setup () {
+  let main_canvas = createCanvas(1080, 1920);
+  main_canvas.parent('canvasContainer');
+
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  sourceImg.loadPixels();
+  maskImg.loadPixels();
+}
+
+function draw () {
+  for(let i=0;i<100;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    let pointSize = 100;
+    let halfSize = 50;
+    fill(pix);
+    if(mask[0] > 128) {
+      ellipse(x, y, pointSize, pointSize);
+    }
+    else {
+      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    }
+  }
+  renderCounter = renderCounter + 1;
+  if(renderCounter > 10) {
+    console.log("Done!")
+    noLoop();
+    // saveBlocksImages();
+  }
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
