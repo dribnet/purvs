76f7ab9238c8d949b495150342cb64f4d980c33c
Isobel Dowell <isobeldowell@gmail.com>
2017-03-10 13:40:23 +1300

    hghg

--- clock.js
+++ clock.js
@@ -2,10 +2,45 @@
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
 function draw_clock(hour, minute, second, millis, alarm) {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+ point(240, 60);
+
+line(20, 50, 420, 110);
+
+triangle(347, 54, 392, 9, 392, 66);
+
+quad(158, 55, 199, 14, 392, 66, 351, 107);
+
+rect(50, 50, 80, 80)
+
+ellipse(50, 50, 80, 80);
+
+arc(90, 60, 80, 80, 0, HALF_PI);
+
+arc(90, 60, 80, 80, 0, radians(90));
+
+strokeWeight(8);
+
+fill(102);
+
+stroke(200);
+
+noFill();
+
+noStroke();
+
+fill(255, 0, 0);
+
+fill(255, 0, 0, 160);
+
+beginShape();
+vertex(180, 82);
+vertex(207, 36);
+vertex(214, 63);
+vertex(407, 11);
+vertex(412, 30);
+vertex(219, 82);
+vertex(226, 109);
+endShape(CLOSE);
+
+
 }
