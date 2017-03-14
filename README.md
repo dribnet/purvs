eeb1c2ad2f0aa0a81e18f900e464ee2986b7ec1f
Patrick Lynch <patrickjhlynch@gmail.com>
2017-03-14 15:06:01 +1300

    Updated with initial working clock design
    I based this design on the windows 95 idea I had last week. I have updated the readme, preview and thumbnail files.

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 ## PS1 MDDN 242 2017
 
-### Spiral Clock
+### Windows 95 Clock Prototype
 
-I based this off the 4th Maeda clock. I chose cyan and pink, two of my
-favourite colours to have the clock switch between. I used variables for
-the R, G, B and opacity values and used a for loop to iterate through these,
-the seconds, the translation and the rotation of the text. In future I wish
-I could have used a different font (one more similar to the original) to
-better capture the feel of the Maeda clock.
+I based this design on the initial idea I had that I outlined in PhotoShop last week. It was somewhat difficult to separate the minutes into two bars. For now I don’t have a working alarm but as it said in the assignment text I made the alarm status appear on the top bar to show it in alarm mode, and as the date is not passed to the clock function I can no longer place the date up there. I plan to have buttons that react to being clicked, a window that can be dragged around and an intuitive way to set an alarm in the future.
 
--- clock.js
+++ clock.js
@@ -3,28 +3,112 @@
  */ 
 function draw_clock(hour, minute, second, millis, alarm) {
 	
-	angleMode(DEGREES);
-  	background(0); // light gray background
-  	translate(320, 170);
-  	fill(0,255,0);
-  	textAlign(CENTER);
-  	textSize(70);
-  	rotate(-175.5);
-  	var red = 20;
-  	var green = 255;
-  	var blue = 200;
-  	var opacity = 10;
-  	for(var i = 15; i <=36; i++)
-  	{
-  		rotate(8);
-  		fill(red, green, blue, opacity);
-  		text("2:08:" + i + "PM", 0, 0);
-  		translate(-20, 15);
-  		red += 12;
-  		green -= 7;
-  		blue -= 3;
-  		opacity +=10;
-  	}
-  	fill(255);
-  	text("2:08:" + 37 + "PM", 0, 0);
+  background(0, 128, 128); // win95 bg
+  var am = 1; //no booleans in p5.js so using 1 = true, 0 = false
+  textSize(18);
+  
+  //Grey box
+  fill(192);
+  noStroke();
+  rect(230,75,500,350);
+  
+  //Top blue bar
+  fill(0,0,128);
+  rect(232,77,496,25);
+  fill(255);
+  text("Alarm set for 7:30am", 238, 95);
+
+  fill(0);
+  text("Loading time...", 350, 140);
+  
+  //X
+  fill(192);
+  rect(706, 80, 19, 19);
+  
+  fill(0);
+  text("X", 710, 96);
+  textSize(15);
+
+  if (hour > 12)
+  {
+    am = 0; //It is PM, not AM
+  }
+  else
+  {
+    am = 1; //It is AM, not PM
+  }
+
+  /******HOUR BAR*****/
+  fill(0,0,128);
+  for (var h = 1; h <= hour; h++)
+  {
+    if (hour > 12)
+    {
+      hour = hour-12; //Don't want 24 hour time
+    } 
+    rect(328+h*22, 155, 18, 30);
+
+  }
+  fill(255);
+  text(hour, 333+hour*22, 175);
+
+  /******MINUTE BAR ONE*****/
+  fill(0,0,128);
+  var minute1 = floor(minute/10);
+  for (var m1 = 0; m1 <= minute1; m1++)
+  {
+
+    rect(350+m1*22, 195, 18, 30);
+
+  }
+  fill(255);
+  text(minute1, 333+m1*22, 215);
+
+  /******MINUTE BAR 2*****/
+  fill(0,0,128);
+  var minute2 = minute - floor(minute/10)*10;
+  for (var m2 = 0; m2 <= minute2; m2++)
+  {
+
+    rect(350+m2*22, 235, 18, 30);
+
+  }
+  fill(255);
+  text(minute2, 333+m2*22, 255);
+
+  /******SECONDS*****/
+  var secondWidth = map(second + (millis/1000.0), 0, 60, 0, 330);
+
+  fill(0,0,128);
+  rect(350, 275, secondWidth, 30);
+
+  fill(0);
+  text(second + "/60 Seconds", 350, 325);
+
+  /******BORDERS FOR BARS*****/
+  noFill();
+  stroke(0);
+  for (var i = 153; i <= 273; i += 40)
+  {
+    rect(348, i, 333, 33);
+
+  }
+
+  /*****AM/PM BUTTONS*****/
+  rect(365, 360, 100, 30);
+  rect(495, 360, 100, 30);
+
+  noStroke();
+  fill(0);
+  if (am == 0)
+  {
+    fill(110);
+  }
+  text("am", 405, 380);
+  fill(0);
+  if (am == 1)
+  {
+    fill(110);
+  }
+  text("pm", 535, 380);
 }
