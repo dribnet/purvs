07f3d9610ac6271aa375a3a03875419255c49e63
Patrick Lynch <patrickjhlynch@gmail.com>
2017-03-17 14:54:24 +1300

    Updated clock with alarm
    I made several small cosmetic changes as well as the the usability of my code, and made windows pop up when the alarm goes off.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## PS1 MDDN 242 2017
 
-### Windows 95 Clock Prototype
+### Windows 95 Clock With Alarm
 
-I based this design on the initial idea I had that I outlined in PhotoShop last week. It was somewhat difficult to separate the minutes into two bars. For now I don’t have a working alarm but as it said in the assignment text I made the alarm status appear on the top bar to show it in alarm mode, and as the date is not passed to the clock function I can no longer place the date up there. I plan to have buttons that react to being clicked, a window that can be dragged around and an intuitive way to set an alarm in the future.
+I updated my clock to have an alarm, and made other changes such as having a method to make buttons and the windows. The X in the top right corner when pressed now also results in a pop up window. I also fixed the justification of numbers in the loading bars when they are double-digit. I updated the text at the top of the main window to reflect the status of the alarm. When the alarm goes of the display is filled with “Alarm!” windows.
 
--- clock.js
+++ clock.js
@@ -1,56 +1,77 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
- */ 
+ */
+
+/*****TODO*****/
+//Make more things functions
+//Make buttons respond to being clicked
+//Alarm function
+//Make everything more windows-y (microsoft sans serif font, window borders)
+
+var clicked = 0;
+
 function draw_clock(hour, minute, second, millis, alarm) {
 	
   background(0, 128, 128); // win95 bg
   var am = 1; //no booleans in p5.js so using 1 = true, 0 = false
-  textSize(18);
-  
-  //Grey box
-  fill(192);
-  noStroke();
-  rect(230,75,500,350);
-  
-  //Top blue bar
-  fill(0,0,128);
-  rect(232,77,496,25);
-  fill(255);
-  text("Alarm set for 7:30am", 238, 95);
+  var pm = 0;
+
+  textSize(15);
+  if (alarm < 0)
+  {
+    drawBox(230, 75, 500, 350, "Alarm not set");
+  }
+  else if (alarm == 0)
+  {
+    drawBox(230, 75, 500, 350, "Alarm!");
+  }
+  else if (alarm > 0)
+  {
+    drawBox(230, 75, 500, 350, "Alarm in " + floor(alarm) + " seconds");
+  }
 
   fill(0);
   text("Loading time...", 350, 140);
-  
-  //X
-  fill(192);
-  rect(706, 80, 19, 19);
-  
-  fill(0);
-  text("X", 710, 96);
-  textSize(15);
 
-  if (hour > 12)
+  //Converting from 24 hour time
+  if (hour >= 12)
   {
     am = 0; //It is PM, not AM
+    pm = 1;
   }
   else
   {
     am = 1; //It is AM, not PM
+    pm = 0;
+  }
+
+  if (hour == 0)
+  {
+    hour = 12; //Makes it 12AM instead of 0AM
+  }
+
+  if (hour > 12)
+  {
+    hour = hour-12; //Don't want 24 hour time
   }
 
   /******HOUR BAR*****/
   fill(0,0,128);
   for (var h = 1; h <= hour; h++)
   {
-    if (hour > 12)
-    {
-      hour = hour-12; //Don't want 24 hour time
-    } 
     rect(328+h*22, 155, 18, 30);
 
   }
   fill(255);
-  text(hour, 333+hour*22, 175);
+
+  if (hour < 10)
+  {
+    text(hour, 333+hour*22, 175);
+  }
+  else
+  {
+    text(hour, 329+hour*22, 175); //makes the text further left if it is double digit
+  }
 
   /******MINUTE BAR ONE*****/
   fill(0,0,128);
@@ -95,20 +116,104 @@ function draw_clock(hour, minute, second, millis, alarm) {
   }
 
   /*****AM/PM BUTTONS*****/
-  rect(365, 360, 100, 30);
-  rect(495, 360, 100, 30);
 
+  button("am", 365, 360, am);
+  button("pm", 495, 360, pm);
+
+  /*****USER PRESSES THE X*****/
+  if (clicked == 1)
+  {
+    drawBox(300,125,360,150, "Error");
+
+    fill(0);
+    text("Cannot exit time.", 420, 190);
+
+    button("OK", 425, 220, 1);
+  }
+
+  if (alarm == 0)
+  {
+    for (var x = 0; x <=5; x++)
+    {
+      for (var y = 0; y <= 15; y++)
+      {
+        drawBox(x*200, y*35-10, 200, 50, "Alarm!");
+      }
+    }
+  }
+}
+
+function button(string, x, y, active)
+{
+  noFill();
+  stroke(0);
+  if (active == 0)
+  {
+    stroke(110)
+  }
+  rect(x, y, 100, 30);
   noStroke();
   fill(0);
-  if (am == 0)
+  if (active == 0)
   {
-    fill(110);
+    fill(110)
   }
-  text("am", 405, 380);
-  fill(0);
-  if (am == 1)
-  {
-    fill(110);
-  }
-  text("pm", 535, 380);
+  textAlign(CENTER);
+  text(string, x+50, y+20);
+  textAlign(LEFT);
 }
+
+/******USER CLICKS X*****/
+function mouseReleased()
+{
+  if (mouseX >= 706 && mouseX <= 725 && mouseY >= 80 && mouseY <= 99)
+  {
+    clicked = 1;
+  }
+  else if (clicked == 1)
+  {
+    clicked = 0;
+  }
+}
+
+/******BOX*****/
+function drawBox(x, y, width, height, barText)
+{
+  //Grey box
+  fill(192);
+  noStroke();
+  rect(x,y,width,height);
+  
+  //Top blue bar
+  fill(0,0,128);
+  rect(x+2,y+2,width-4,25);
+  fill(255);
+  text(barText, x+8, y+20);
+
+  //X
+  fill(192);
+  rect(x+width-24, y+5, 19, 19);
+  
+  textSize(18);
+  fill(0);
+  text("X", x+width-20, y+21);
+  textSize(15);
+
+  noFill();
+  stroke(255);
+  rect(x-1,y-1,width+1,height+1);
+  noStroke();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
