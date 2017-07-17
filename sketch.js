e0148d26bb234b9496eb6f7bc58c6d683aa86748
Hannah Dockerty <dockerhann@myvuw.ac.nz>
2017-07-17 16:47:15 +1200

    rapunzel hair & flowers

--- sketch.js
+++ sketch.js
@@ -13,12 +13,13 @@ function setup () {
 }
 
 // global variables for colors
-var bg_color = "#fff";
+var bg_color = "#cecece";
 var face_color1 = "#ffc2a9";
-var face_color2 = "#ed926b";
-var face_color3 = "#a45338";
+var face_color2 = "#cc8f72";
 var eye_color1 = "#737141";
 var eye_color2 = "#000";
+var hair_color1 = "#fccd85";
+
 var stroke_color = "#000";
 
 function draw () {
@@ -28,15 +29,41 @@ function draw () {
   //noStroke();
 
   // move to position1, rotate, draw "head" ellipse
-  push();
-  translate(960/4, 200);
-  fill(face_color1);
+
+
     noStroke();
   // ellipse(0, 0, 380, 400);
   // triangle(-125, 150, 0, 260, 125, 150);
+fill(hair_color1);
+push();
+translate(960/4, 220);
+ellipse(100, 110, 280, 280*1.1);
+
+fill(0, 0, 0, 20);
+
+for (var i = 220; i < 260; i += 5){
+  ellipse(100, 110, i, i*1.1); 
+}
+
+    
+fill(face_color2);
+    beginShape();
+   curveVertex(160, 255);
+     curveVertex(70, 220);
+    curveVertex(50, 255);
+    curveVertex(30, 275);
+    curveVertex(15, 285);
+    curveVertex(205, 285);
+    curveVertex(180, 275);
+    curveVertex(160, 255);
+    curveVertex(135, 220);
+    curveVertex(50, 255);
+      endShape();
+
+  fill(face_color1);
+  push();
 
     beginShape();
-
        curveVertex(20, 30);
     curveVertex(100, 0);
     curveVertex(180, 30);
@@ -49,10 +76,9 @@ function draw () {
     curveVertex(100, 0);
     curveVertex(180, 30);
     endShape();
-
+pop();
   // stroke color
 
-  
 
   // draw two eyes
 fill(eye_color2);
@@ -74,7 +100,7 @@ translate(-2, -5);
     endShape();
     pop();
 
-   fill(bg_color);
+   fill("#fff");
    stroke(eye_color2);
     beginShape();
     curveVertex(25, 125);
@@ -117,7 +143,7 @@ translate(2, -5);
     curveVertex(25, 95);
     endShape();
     pop();
-     fill(bg_color);
+     fill("#fff");
    stroke(eye_color2);
     beginShape();
     curveVertex(25, 125);
@@ -139,30 +165,72 @@ noStroke();
     ellipse(43, 113, 20, 20);
     pop();
 
-  // draw nose
-  noStroke();
-  fill(face_color2);
-  beginShape();
-   curveVertex(85, 170);
-    curveVertex(102, 145);
-    curveVertex(120, 170);
-    curveVertex(100, 175);
-    curveVertex(85, 170);
-    curveVertex(102, 145);
-    curveVertex(120, 170);
-    endShape();
+  // // draw nose
+  // noStroke();
+  // fill(face_color2);
+  // beginShape();
+  //  curveVertex(85, 170);
+  //   curveVertex(102, 145);
+  //   curveVertex(120, 170);
+  //   curveVertex(100, 175);
+  //   curveVertex(85, 170);
+  //   curveVertex(102, 145);
+  //   curveVertex(120, 170);
+  //   endShape();
 
-  fill(face_color3);
-  ellipse(95, 172, 12, 8);
-  ellipse(110, 172, 12, 8);
+  // fill(face_color3);
+  // ellipse(95, 172, 12, 8);
+  // ellipse(110, 172, 12, 8);
 
-  // mouth-hole with background color
-  fill(bg_color);
-
-  pop();
+  // // mouth-hole with background color
+  // fill(bg_color);
 
 
+
+
+//draw flowers
+push();
+translate(-10, 50);
+fill ("#fff");
+ellipse(0, 0, 40, 40);
+fill("#ffae00");
+ellipse(0, 0, 15, 15);
+
+
+fill("#d16f97")
+translate(-10, 30);
+ellipse(0, 0, 20, 20);
+translate(0, 30);
+ellipse(10, -10, 15, 15);
+translate(-5, 30);
+ellipse(10, -10, 10, 10);
+
+fill("#ffe2de")
+translate(-15, -50);
+ellipse(0, 0, 10, 10);
+translate(5, -15);
+ellipse(0, 0, 13, 13);
+ellipse(10, -5, 13, 13);
+
+translate(30, 150);
+ellipse(10, 0, 10, 10);
+ellipse(20, 5, 13, 13);
+
+fill("#fff");
+ellipse (30, 5, 10, 10)
+ellipse (10, -15, 15, 15)
+
+fill("#d16f97")
+ellipse (0, -25, 18, 18)
+
+
+pop();
+
+
+pop();
 }
+
+
 
 function keyTyped() {
   if (key == '!') {
