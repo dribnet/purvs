f4f942be392b52b89994b0acf79d285ef4e8e911
Zach Thompson Commit <the_schwa@hotmail.com>
2017-07-17 15:35:48 +1200

    Changed to Miffy and Hello Kitty

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup () {
 // global variables for colors
 var bg_color = "#c6bdab";
 var fg_color1 = "#5b412a";
-var fg_color2 = "#7b611a";
+var fg_color2 = "#FFE158";
 var stroke_color = "#c78a5b";
 
 function draw () {
@@ -29,47 +29,61 @@ function draw () {
   push();
   translate(960/4, 500/2);
   rotate(4);
-  fill(fg_color1);
-  ellipse(0, 0, 300, 400);
+  fill(200);
+  ellipse (-60,-100, 50, 150);
+  ellipse (68,-100, 50, 150);
+  ellipse(0, 0, 200, 200);
+
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+
 
   // set fill back to foreground for eyeballs
-  fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  fill(0);
+  ellipse(-60, 30, 10, 10);
+  ellipse( 60, 30, 10, 10);
 
   // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  fill(0);
+  rectMode (CENTER);
+  rotate (80);
+  noStroke ()
+  rect (65,5, 2, 20, 50);
+  rotate (30);
+  rect (59,-29, 2, 20, 50);
+
   pop();
 
+  // Hello Kitty
   // move to position2, rotate, draw "head" ellipse
   push();
   translate(3*960/4, 500/2);
-  rotate(30);
-  fill(fg_color2);
+  fill(210);
   rectMode (CENTER);
-  rect(0, 0, 300, 400);
+  rotate (25);
+  rect (-80,-50, 100, 100, 7);
+  rotate(-50);
+  rect (80,-50, 100, 100, 7);
+  rotate (25);
+  ellipse(0, -25, 250, 200);
 
   // set fill to match background color
-  fill(bg_color);
-  // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
 
   // set fill back to foreground for eyeballs
-  fill(fg_color2);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  fill(0);
+  ellipse(-60, -50, 20, 25);
+  ellipse( 40, -50, 20, 25);
 
   // mouth-hole with background color
-  fill(bg_color);
-  triangle( 0, 70, 150, 20, 90, -90);
+  fill(fg_color2);
+  ellipse( -15, -20, 30, 20);
+
+  //Bow Tie
+  fill (150,0,0);
+  triangle (55,-110, 30, -150, 20, -90);
+  triangle (55,-110, 100, -120, 80, -70);
   pop();
 }
 
