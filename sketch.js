7def3912122d22f4c5189217cec30d376f686aa3
jack upton <jackupton2255@gmail.om>
2017-07-17 14:12:25 +1200

    janky monopoly man
    he is janky

--- sketch.js
+++ sketch.js
@@ -13,47 +13,25 @@ function setup () {
 }
 
 // global variables for colors
-var bg_color = "#c6bdab";
+var bg_color = "#6d81e5";
+var bags_face = "#eed5c1";
 var fg_color1 = "#5b412a";
 var fg_color2 = "#7b611a";
-var stroke_color = "#c78a5b";
+var stroke_color = "#000000";
+var white = "#FFFFFF";
 
 function draw () {
   // background color
   background(bg_color);
 
-  // stroke color
-  stroke(stroke_color)
-
-  // move to position1, rotate, draw "head" ellipse
-  push();
-  translate(960/4, 500/2);
-  rotate(4);
-  fill(fg_color1);
-  ellipse(0, 0, 300, 400);
-
-  // set fill to match background color
-  fill(bg_color);
-  // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
-
-  // set fill back to foreground for eyeballs
-  fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
-
-  // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
-  pop();
-
+  drawMonopoly();
+  
   // move to position2, rotate, draw "head" ellipse
   push();
   translate(3*960/4, 500/2);
   rotate(30);
   fill(fg_color2);
-  ellipse(0, 0, 300, 400);
+  ellipse(0, 0, 300, 100);
 
   // set fill to match background color
   fill(bg_color);
@@ -79,4 +57,48 @@ function keyTyped() {
   else if (key == '@') {
     saveBlocksImages(true);
   }
+}
+
+
+function drawMonopoly() {
+
+stroke(stroke_color)
+strokeWeight(2)
+
+
+
+  // move to position1, rotate, draw "head" ellipse
+  push();
+  translate(280, 250);
+  rotate(0);
+  fill(bags_face);
+  ellipse(0, 0, 300, 300);
+
+  //ear
+  ellipse(-170, -0, 50, 60);
+
+  //moustache 
+fill(white);
+  beginShape();
+  vertex(00,20);
+  bezierVertex(100,70,120,50,160,20);
+  bezierVertex(160,50,110,80,100,100);
+  bezierVertex(-100,70,-120,50,-160,20);
+
+  //vertex(00,40);
+  endShape(CLOSE);
+
+  // set fill back to foreground for eyeballs
+  fill(stroke_color);
+  ellipse(-60, -50, 25, 25);
+  ellipse( 50, -50, 25, 25);
+
+  // mouth-hole with background color
+  fill(stroke_color);
+  ellipse( 0, 70, 20, 20);
+  pop();
+
+
+
+
 }
