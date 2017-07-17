98675e81c697f539dc6d975d0298a4fbd6095b94
Christina Petris <christina.petris@outlook.com>
2017-07-17 13:31:52 +1200

    changed colours and face shape

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ function setup () {
 }
 
 // global variables for colors
-var bg_color = "#c6bdab";
-var fg_color1 = "#5b412a";
-var fg_color2 = "#7b611a";
-var stroke_color = "#c78a5b";
+var bg_color = "#FFF7F6";
+var fg_color1 = "#BDBEFF";
+var fg_color2 = "#C5EEFF";
+var stroke_color = "#2F2F40";
 
 function draw () {
   // background color
@@ -30,22 +30,22 @@ function draw () {
   translate(960/4, 500/2);
   rotate(4);
   fill(fg_color1);
-  ellipse(0, 0, 300, 400);
+  ellipse(0, 0, 300, 325);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -40, 50, 30);
+  ellipse( 50, -40, 50, 30);
 
   // set fill back to foreground for eyeballs
   fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  ellipse(-60, -40, 20, 20);
+  ellipse( 40, -40, 20, 20);
 
   // mouth-hole with background color
   fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  ellipse( 0, 70, 80, 35);
   pop();
 
   // move to position2, rotate, draw "head" ellipse
@@ -53,18 +53,18 @@ function draw () {
   translate(3*960/4, 500/2);
   rotate(30);
   fill(fg_color2);
-  ellipse(0, 0, 300, 400);
+  ellipse(0, 0, 300, 325);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -40, 50, 30);
+  ellipse( 50, -40, 50, 30);
 
   // set fill back to foreground for eyeballs
   fill(fg_color2);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  ellipse(-60, -40, 20, 20);
+  ellipse( 40, -40, 20, 20);
 
   // mouth-hole with background color
   fill(bg_color);
