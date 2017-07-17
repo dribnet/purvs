0b87c26bb502077ce645283d0004cdb093c8819a
Nathan Larson <nathnz@hotmail.co.nz>
2017-07-17 15:13:32 +1200

    Started work on spongebob
    Started on the nose, eyes and mouth

--- sketch.js
+++ sketch.js
@@ -14,9 +14,12 @@ function setup () {
 
 // global variables for colors
 var bg_color = "#ffffff";
-var fg_color1 = "#2a5d44";
-var fg_color2 = "#3a6d1c";
+var fg_color1 = "#fef46e";
+var fg_color2 = "#fef46e";
 var stroke_color = "#000000";
+
+var eye_color = "#43c6f2";
+var pupil_color = "#000000";
 
 function draw () {
   // background color
@@ -30,22 +33,33 @@ function draw () {
   translate(960/4, 500/2);
   rotate(4);
   fill(fg_color1);
-  ellipse(0, 0, 300, 400);
+
+  ellipse(0, 0, 300, 300);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -80, 80, 80);
+  ellipse( 50, -80, 80, 80);
 
   // set fill back to foreground for eyeballs
+  fill(eye_color);
+  ellipse(-50, -80, 30, 30);
+  ellipse( 50, -80, 30, 30);
+
+// set fill back to foreground for pupils
+  fill(pupil_color);
+  ellipse(-50, -80, 15, 15);
+  ellipse( 50, -80, 15, 15);
+
+// nose with skin color
   fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  ellipse(0, -30, 45, 45);
 
   // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  fill(fg_color1);
+  translate(-105, -100);
+  curve(150,-200, 200,100,0,100,50,-200);
   pop();
 
   // move to position2, rotate, draw "head" ellipse
