b7e956d5ed8b27236251907ace8c0361c7568237
Guest User <Guest@i-03-05-lab.mddn.vuw.ac.nz>
2017-07-17 13:51:09 +1200

    part1

--- sketch.js
+++ sketch.js
@@ -13,9 +13,9 @@ function setup () {
 }
 
 // global variables for colors
-var bg_color = "#c6bdab";
-var fg_color1 = "#5b412a";
-var fg_color2 = "#7b611a";
+var bg_color = "#3C428F";
+var fg_color1 = "#000000";
+var fg_color2 = "#FFFFFF";
 var stroke_color = "#c78a5b";
 
 function draw () {
@@ -24,19 +24,20 @@ function draw () {
 
   // stroke color
   stroke(stroke_color)
+  noStroke();
 
   // move to position1, rotate, draw "head" ellipse
   push();
   translate(960/4, 500/2);
   rotate(4);
   fill(fg_color1);
-  ellipse(0, 0, 300, 400);
+  ellipse(0, 0, 300, 350);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -80, 60, 20);
+  ellipse( 50, -80, 60, 20);
 
   // set fill back to foreground for eyeballs
   fill(fg_color1);
@@ -45,7 +46,9 @@ function draw () {
 
   // mouth-hole with background color
   fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  ellipse( 0, 70, 150, 100);
+  fill(fg_color1);
+  ellipse( 0, 90, 150, 80);
   pop();
 
   // move to position2, rotate, draw "head" ellipse
@@ -53,22 +56,28 @@ function draw () {
   translate(3*960/4, 500/2);
   rotate(30);
   fill(fg_color2);
-  ellipse(0, 0, 300, 400);
+  ellipse(0, 0, 300, 350);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -80, 60, 30);
+  ellipse( 50, -80, 60, 30);
+
+  fill(fg_color2);
+  ellipse(-50, -65, 60, 30);
+  ellipse( 50, -65, 60, 30);
 
   // set fill back to foreground for eyeballs
   fill(fg_color2);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  ellipse(-70, -85, 15, 15);
+  ellipse( 30, -85, 15, 15);
 
   // mouth-hole with background color
   fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  ellipse( 0, 70, 150, 100);
+  fill(fg_color2);
+  ellipse( 5, 55, 150, 90);
   pop();
 }
 
