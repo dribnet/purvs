65c403d92514cef462a5b8474a6d346693836c71
Shane Watters <shane.watters.nz@gmail.com>
2017-07-17 14:59:05 +1200

    Completing drawing of first character. The first character I have chosen to draw is the Japanese robot Doraemon. It was relatively easy however required the use of bezier curves, which are quite challenging to understand.

--- sketch.js
+++ sketch.js
@@ -22,31 +22,8 @@ function draw () {
   // background color
   background(bg_color);
 
-  // stroke color
-  stroke(stroke_color)
-
-  // move to position1, rotate, draw "head" ellipse
-  push();
-  translate(960/4, 500/2);
-  rotate(4);
-  fill(fg_color1);
-  ellipse(0, 0, 300, 400);
-
-  // set fill to match background color
-  fill(bg_color);
-  // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
-
-  // set fill back to foreground for eyeballs
-  fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
-
-  // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
-  pop();
+  
+  drawDoraemon();
 
   // move to position2, rotate, draw "head" ellipse
   push();
@@ -70,6 +47,8 @@ function draw () {
   fill(bg_color);
   ellipse( 0, 70, 150, 20);
   pop();
+
+  
 }
 
 function keyTyped() {
@@ -79,4 +58,60 @@ function keyTyped() {
   else if (key == '@') {
     saveBlocksImages(true);
   }
+}
+
+function drawDoraemon(){
+  // stroke color
+  stroke(0);
+  strokeWeight(2);
+
+  push();
+  translate(960/4, 500/2);
+  //head
+  fill(3, 166, 215);
+  ellipse(0,0, 300, 255);
+  //mouth
+  fill(255, 255, 255);
+  ellipse(0,25, 260, 205);
+  line(0,20, 0, -40);
+  fill(209, 4, 41);
+  arc(0, 20, 220, 180, 360, 180, OPEN);
+  fill(244, 83, 2);
+  arc(0, 90, 94, 50, 360, 180, OPEN);
+  fill(255);
+  bezier(0, 20, -20, 60, -190, 40, -115, -2);
+  bezier(0, 20, 20, 60, 190, 40, 115, -2);
+  fill(244, 83, 2);
+  bezier(-40, 104, -65, 80, -10, 70, 0, 90);
+  bezier(40, 104, 65, 80, 10, 70, 0, 90);
+  //left eye
+  fill(255, 255, 255);
+  ellipse(-25 ,-90, 50, 70);
+  fill(0, 0, 0);
+  ellipse(-18 ,-85, 15, 21);
+  fill(255, 255, 255);
+  ellipse(-15 ,-84, 7, 11);
+  //right eye
+  fill(255, 255, 255);
+  ellipse(25,-90, 50, 70);
+  fill(0, 0, 0);
+  ellipse(18 ,-85, 15, 21);
+  fill(255, 255, 255);
+  ellipse(15 ,-84, 7, 11);
+  //left whiskers
+  line(-20,-25, -80, -30);
+  line(-20,-15, -80, -15);
+  line(-20,-5, -80, -0);
+  //right whiskers
+  line(20,-25, 80, -30);
+  line(20,-15, 80, -15);
+  line(20,-5, 80, -0);
+  //nose
+  fill(209, 4, 41);
+  ellipse(0,-50, 40, 40);
+  fill(255, 255, 255);
+  strokeWeight(0);
+  ellipse(-4,-54, 15, 15);
+
+  pop();
 }
