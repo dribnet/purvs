dfb48c39307aeda147c2cfe4fc29904f14916c5d
ShakDesign <shakirajensen@hotmail.com>
2017-07-19 20:58:37 +1200

    updated version of faces

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## PS1 MDDN 342 2017
 
-Base of the faces of Spongebob and Squidward.
-
-Also update both images keeping their original sizes:
-preview.jpg (960x500) and thumbnail.png (230x120).
+Easily recognisable faces of spongebob and squidward.
--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ var fg_color1 = "#5b412a";
 var fg_color2 = "#7b611a";
 var fg_color3 = "#FFE839";
 var fg_color4 = "#8EEDE2";
+var nose_col = "#EDD835";
 var stroke_color = "#c78a5b";
 
 function draw () {
@@ -34,51 +35,110 @@ function draw () {
   fill(fg_color3);
   noStroke();
   rectMode(CENTER);
-  rect(0, 0, 300, 400);
+  rect(0, 0, 275, 300);
 
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -80, 70, 70);
+  ellipse( 50, -80, 70, 70);
 
 
   // set fill back to foreground for eyeballs
+  fill(fg_color4);
+  ellipse(-50, -80, 30, 30);
+  ellipse( 50, -80, 30, 30);
+
   fill("#000000");
   ellipse(-50, -80, 20, 20);
   ellipse( 50, -80, 20, 20);
 
-  // mouth-hole with background color
-  fill(bg_color);
+  //teeth
+  noStroke();
+  fill("#ffffff");
+  rect(20, 60, 30, 30)
+  rect(-20, 60, 30, 30)
+
+  //mouth
   rotate(180);
-  arc(0, 0, 100, 100, 180, HALF_PI);
+  fill(fg_color3);
+  stroke("#000000");
+  arc(0, 0, 200, 100, 180, HALF_PI);
+
+  //cheeks
+  noStroke();
+  fill("#EB99ED");
+  ellipse(-100, 0, 30, 30);
+  ellipse(100, 0, 30, 30);
+    
+  //nose
+  noFill();
+  stroke("#000000");
+  rotate(90);
+  arc(30, 0, 25, 125, 180, HALF_PI);
+
+  //spots
+  noStroke();
+  fill(nose_col);
+  ellipse(0, 30, 10, 10);
+  ellipse(-140, 100, 10, 10);
+  ellipse(-110, -100, 10, 10);
+  ellipse(-100, -30, 10, 10);
+  ellipse(100, 10, 10, 10);
+  ellipse(100, 120, 10, 10);
+  ellipse(-110, 30, 15, 15);
+  ellipse(110, -120, 15, 15);
+  ellipse(140, 100, 10, 10);
+  ellipse(-70, -50, 20, 20);
+  ellipse(-90, 110, 20, 20);
+  ellipse(-10, -10, 20, 20);
+  ellipse(130, -90, 30, 30);
+  ellipse(-130, -70, 30, 30);
   pop();
+
+
+
+
 
 
 
   // move to position2, rotate, draw "head" ellipse
   push();
   translate(3*960/4, 500/2);
-  rotate(-30);
   fill(fg_color4);
   noStroke()
-  ellipse(0, 0, 300, 400);
+  ellipse(0, -100, 300, 150);
+  rectMode(CENTER);
+  rect(0, 0, 100, 60);
+  ellipse(0, 50, 200, 75);
 
   // set fill to match background color
   fill(bg_color);
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-30, -80, 50, 70);
+  ellipse( 30, -80, 50, 70);
+
+  fill("#000000");
+  rect(0, -115, 120, 5);
 
   // set fill back to foreground for eyeballs
   fill("#000000");
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  ellipse(-30, -70, 20, 20);
+  ellipse( 30, -70, 20, 20);
+
+  //nose
+  push();
+  noFill();
+  stroke("#000000");
+  rotate(180);
+  arc(0, 50, 30, 125, 180, HALF_PI);
+  pop();
 
   // mouth-hole with background color
-  fill(bg_color);
-  ellipse(0, 70, 50, 50);
+  noFill();
+  stroke("#000000");
+  arc(0, 60, 110, 50, 180, HALF_PI);
   pop();
 }
 
