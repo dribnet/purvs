9ecf0827aa0c8fd1748e447e5945198c09619f80
Hannah Dockerty <dockerhann@myvuw.ac.nz>
2017-07-20 11:48:52 +1200

    ariel and rapunzel finishedish

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Face Sketches
 Sketch two recognisable faces using P5.js
 
 Face #1 is Rapunzel from Disney's tangled. I used a technique borrowed from Lewis Brewer, which basically involved plotting points on photoshop and using the co-ordinates to draw custom shapes. 
+
+Face #2 is Ariel from Disney's little mermaid. I decided to stick within the same general theme for the two faces, and although I used similar techniques for both, I created two fairly different variations on the original characters. 
--- sketch.js
+++ sketch.js
@@ -18,22 +18,19 @@ var face_color1 = "#ffc2a9";
 var face_color2 = "#cc8f72";
 var eye_color1 = "#737141";
 var eye_color2 = "#000";
+var eye_color3 = "#2f56a3";
 var hair_color1 = "#fccd85";
-
+var hair_color2 = "#ae1a18"
 var stroke_color = "#000";
 
 function draw () {
   // background color
   background(bg_color);
 
-  //noStroke();
-
-  // move to position1, rotate, draw "head" ellipse
-
+  //////start rapunzel////////
 
     noStroke();
-  // ellipse(0, 0, 380, 400);
-  // triangle(-125, 150, 0, 260, 125, 150);
+
 fill(hair_color1);
 push();
 translate(960/4, 220);
@@ -45,7 +42,7 @@ for (var i = 220; i < 260; i += 5){
   ellipse(100, 110, i, i*1.1); 
 }
 
-    
+    //draw neck
 fill(face_color2);
     beginShape();
    curveVertex(160, 255);
@@ -59,7 +56,7 @@ fill(face_color2);
     curveVertex(135, 220);
     curveVertex(50, 255);
       endShape();
-
+// draw face
   fill(face_color1);
   push();
 
@@ -165,28 +162,6 @@ noStroke();
     ellipse(43, 113, 20, 20);
     pop();
 
-  // // draw nose
-  // noStroke();
-  // fill(face_color2);
-  // beginShape();
-  //  curveVertex(85, 170);
-  //   curveVertex(102, 145);
-  //   curveVertex(120, 170);
-  //   curveVertex(100, 175);
-  //   curveVertex(85, 170);
-  //   curveVertex(102, 145);
-  //   curveVertex(120, 170);
-  //   endShape();
-
-  // fill(face_color3);
-  // ellipse(95, 172, 12, 8);
-  // ellipse(110, 172, 12, 8);
-
-  // // mouth-hole with background color
-  // fill(bg_color);
-
-
-
 
 //draw flowers
 push();
@@ -223,11 +198,163 @@ ellipse (10, -15, 15, 15)
 fill("#d16f97")
 ellipse (0, -25, 18, 18)
 
+pop();
+pop();
 
+/////start ariel////
+
+push();
+translate(900/1.5, 220);
+scale(0.4, 0.4);
+
+
+//draw hair
+fill(hair_color2);
+push();
+rotate(-20);
+ellipse(200, 500, 750, 1200);
+ellipse(100, 110, 700, 400);
 pop();
 
 
+// //draw neck
+// fill(face_color2);
+// beginShape();
+// curveVertex(400, 540);
+// curveVertex(100, 540);
+// curveVertex(115, 660);
+// curveVertex(105, 720);
+// curveVertex(50, 760);
+// curveVertex(490, 760);
+// curveVertex(445, 720);
+// curveVertex(405, 660);
+// curveVertex(400, 540);
+// curveVertex(100, 540);
+// curveVertex(115, 660);
+//endShape();
+
+//draw face
+fill(face_color1);
+ beginShape();
+     curveVertex(230, 60);
+    curveVertex(375, 0);
+    curveVertex(410, 115);
+    curveVertex(500, 180);
+    curveVertex(490, 260);
+    curveVertex(500, 400);
+    curveVertex(455, 490);
+    curveVertex(390, 590);
+    curveVertex(270, 660);
+    curveVertex(160, 590);
+    curveVertex(55, 490);
+    curveVertex(20, 400);
+     curveVertex(30, 260);
+    curveVertex(0, 180);
+    curveVertex(145, 65);
+    curveVertex(230, 60);
+    curveVertex(375, 0);
+    curveVertex(410, 115);
+    endShape();
+
+
+  //draw eyes
+fill(eye_color2);
+push();
+translate(-2, -5);
+  beginShape();
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+curveVertex(180, 270);
+curveVertex(200, 320);
+curveVertex(130, 340);
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+endShape();
 pop();
+
+  fill("#fff");
+  stroke(eye_color2);
+beginShape();
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+curveVertex(180, 270);
+curveVertex(200, 320);
+curveVertex(130, 340);
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+endShape();
+
+noStroke();
+fill(eye_color3);
+ellipse(140, 290, 80, 80);
+fill(eye_color2);
+ellipse(140, 290, 50, 50);
+fill('#fff');
+ellipse(120, 280, 10, 20);
+
+//lashes
+stroke('#000');
+strokeWeight(2);
+curve(55, 225, 90, 248, 60, 230, 100, 250)
+curve(40, 220, 80, 248, 45, 230, 100, 245)
+curve(25, 215, 70, 248, 35, 240, 90, 240)
+
+
+fill(eye_color2);
+push();
+translate(500, 0);
+scale(-1, 1);
+push();
+translate(-2, -5);
+  beginShape();
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+curveVertex(180, 270);
+curveVertex(200, 320);
+curveVertex(130, 340);
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+endShape();
+pop();
+
+  fill("#fff");
+  stroke(eye_color2);
+beginShape();
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+curveVertex(180, 270);
+curveVertex(200, 320);
+curveVertex(130, 340);
+curveVertex(65, 320);
+curveVertex(70, 255);
+curveVertex(130, 250);
+endShape();
+
+noStroke();
+fill(eye_color3);
+ellipse(140, 290, 80, 80);
+fill(eye_color2);
+ellipse(140, 290, 50, 50);
+fill('#fff');
+ellipse(120, 280, 10, 20);
+
+//lashes
+stroke('#000');
+strokeWeight(2);
+curve(55, 225, 90, 248, 60, 230, 100, 250)
+curve(40, 220, 80, 248, 45, 230, 100, 245)
+curve(25, 215, 70, 248, 35, 240, 90, 240)
+pop();
+
+
+
 }
 
 
