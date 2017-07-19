96d2a4b3be4aabde8981d3b7712e657638d394d1
Lara Hayward <haywardlbl@gmail.com>
2017-07-19 23:49:27 +1200

    Updated faces.
    Drew a lego face, which was pretty simple. The second face is Rick, which was more complicated.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## PS1 MDDN 342 2017
 
-Two faces with different colours.
+First face is a generic lego face. I wanted to start out with something simple
+to get used to coding again, so it uses only simple shapes along with an arc for the 
+smile.
+
+Second face is Rick from Rick and Morty. This time I wanted to do something more
+complicated. I used multiple curves and beginShape()-endShape() to create the hair,
+which was a bit of a challenge to get all the vertexes for, but I perservered. Overall,
+this was a great learning experience!
--- sketch.js
+++ sketch.js
@@ -14,9 +14,9 @@ function setup () {
 
 // global variables for colors
 var bg_color = "#ffffff";
-var fg_color1 = "#ff0000";
-var fg_color2 = "#0000ff";
-var stroke_color = "#ffffff";
+var fg_color1 = "#fee300";
+var fg_color2 = "#eee4d9";
+var stroke_color = "#000000";
 
 function draw () {
   // background color
@@ -24,51 +24,130 @@ function draw () {
 
   // stroke color
   stroke(stroke_color)
+  noStroke();
 
-  // move to position1, rotate, draw "head" ellipse
+  // move to position1, draw rectangles for head and top and bottom bits
   push();
   translate(960/4, 500/2);
-  rotate(4);
+  fill("#f0d700");
+  rect(-70, -150, 140, 50);
+  rect(-70, 100, 140, 50);
   fill(fg_color1);
-  ellipse(0, 0, 300, 400);
+  rect(-280/2, -225/2, 280, 225, 70);
 
-  // set fill to match background color
-  fill(bg_color);
+  // set fill to black color
+  fill("#000000");
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -10, 30, 30);
+  ellipse( 50, -10, 30, 30);
 
-  // set fill back to foreground for eyeballs
+  // draw arc for mouth
   fill(fg_color1);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
-
-  // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  stroke(stroke_color);
+  strokeWeight(7);
+  rotate(90);
+  arc(5, 0, 80, 80, -50, 50);
   pop();
+
+
 
   // move to position2, rotate, draw "head" ellipse
   push();
-  translate(3*960/4, 500/2);
-  rotate(30);
+  scale(0.75);
+  translate(3.8*960/4, 370);
+  stroke(stroke_color);
+  strokeWeight(2);
+
+  //hair
+  fill("#c7f1ff");
+  beginShape();
+  vertex(-70, 150);
+  vertex(-120, 160);
+  vertex(-110, 120);
+  vertex(-180, 110);
+  vertex(-150, 60);
+  vertex(-230, 30);
+  vertex(-150, -20);
+  vertex(-240, -70);
+  vertex(-160, -100);
+  vertex(-230, -230);
+  vertex(-100, -180);
+  vertex(-100, -340);
+  vertex(0, -220)
+  vertex(100, -340);
+  vertex(100, -180);
+  vertex(230, -230);
+  vertex(160, -100);
+  vertex(240, -70);
+  vertex(150, -20);
+  vertex(230, 30);
+  vertex(150, 60);
+  vertex(180, 110);
+  vertex(110, 120);
+  vertex(120, 160);
+  vertex(70, 150);
+  endShape();
+
+  //ears
   fill(fg_color2);
-  ellipse(0, 0, 300, 400);
+  ellipse(-120, 45, 50, 50);
+  ellipse(120, 45, 50, 50);
+
+
+  //face
+  rect(-225/2, -375/2, 225, 375, 300);
+
 
   // set fill to match background color
   fill(bg_color);
+
   // draw two eyes
-  ellipse(-50, -80, 50, 30);
-  ellipse( 50, -80, 50, 30);
+  ellipse(-50, -40, 80, 80);
+  ellipse( 50, -40, 80, 80);
 
   // set fill back to foreground for eyeballs
-  fill(fg_color2);
-  ellipse(-60, -80, 20, 20);
-  ellipse( 40, -80, 20, 20);
+  fill("#000000");
+  ellipse(-60, -45, 10, 10);
+  ellipse( 60, -40, 10, 10);
 
-  // mouth-hole with background color
-  fill(bg_color);
-  ellipse( 0, 70, 150, 20);
+  //monobrow
+  fill("#c7f1ff");
+  rect(-90, -110, 175, 20, 20);
+
+  //face details
+
+  //nose
+  noFill();
+  curve(15, -400, -12, -5, 12, -5, -15, -400);
+
+  //mouth
+  curve(-50, 150, -60, 100, 60, 100, 50, 150);
+
+  //mouth lines - rotating and translating a curve for it because it's easier
+  push();
+  translate(-65, 102);
+  rotate(75);
+  curve(10, -100, -14, 0, 14, 0, -10, -100);
+  pop();
+
+  push();
+  translate(65, 102);
+  rotate(360-75);
+  curve(10, -100, -14, 0, 14, 0, -10, -100);
+  pop();
+
+  //eyebags
+  push();
+  translate(-60, 10);
+  rotate(10);
+  curve(-40, -90, -30, 0, 30, 0, 40, -90);
+  pop();
+  push();
+  translate(60, 10);
+  rotate(-10);
+  curve(-40, -90, -30, 0, 30, 0, 40, -90);
+  pop();
+  
   pop();
 }
 
