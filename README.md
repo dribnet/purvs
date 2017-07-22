501e9a3c2b8f642838feacee474274a301baaa98
S-Williams42 <williasusa@myvuw.ac.nz>
2017-07-22 20:56:46 +1200

    began work on the first face

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ## PS1 MDDN 342 2017
 
 This README explains the three different paramaterized faces for part2. 
-At this stage I have been planning and creating the base code from which I will build my faces, which is why nothing exciting is happening in the images yet. I want to create three drastically different drawing styles. To help me keep everything clear and organised I am putting the faces and sliders into objects. They will keep track of their own dimensions, and be drawn onto their own graphics objects, which are then returned to the main sketch. Passing around all the slider values was getting silly, so I placed them inside an object. This object is passed to a face object, and not only returns the value required, but scales it as required as well.
-
-I want the first face to be very simple, exploring what can be done with mostly basic shapes. At first I thought a monster or a robot, but I wanted something a little more whimsical, and perhaps less intuitive; I have chosen a ragdoll. The main theme I want to play with within my paramaters is changing the mood and style of the face. To do this I intend to have the various parameters transition between a cute childish ragdoll, and a creepy, Coraline-inspired ragdoll.
-
+Sliders 1 and 4 are operational, but 4 only does something if the slider is right of center.
+At this stage I have been refining what works and how to do this cleanly, without confusing myself. For example at first I was going to do the eyes using x and y coordinates, but then that got really tricky when I wanted to draw the x shapes, so I backtracked and used translation, which meant I could also put the x drawing in a function, which means it is only written out once. I was also going to change values with a multipliable scaler, like in the example, but then I realised that with my sliders I have basicaly built my own scaling machine and all I need to do is plug in any two values, be they size, position, or anything, and the slider does the scaling directly. 
 For my second face I want to play with the idea of abstraction and art. What if the flowers blooming on her cheecks were actual flowers, and what if there were real stars in her eyes?
 
 I don't know yet what I am going to do for the third face.
--- RagDoll.js
+++ RagDoll.js
@@ -7,24 +7,78 @@ this.h = h;
 this.sliders = sliders;
 //the graphics object this image will be drawn to
 this.canvas = createGraphics(w,h);
+//variables for storing the calculated values from a slider
+this.scaler = 1;
 
 //returns the graphics object with the new face drawn on it
 this.drawFace = function(){
 	
 	this.canvas.clear();
-	this.canvas.background(0,0,0);
-
+	//this.canvas.background(0,0,0);
 	this.canvas.ellipseMode(CENTER);
-
 	this.canvas.push();
 	this.canvas.translate(w/4,h/4);
-	this.canvas.fill(this.sliders.scaleSliders(4,0,255,true),this.sliders.scaleSliders(2,0,255,true),this.sliders.scaleSliders(3,0,255,true));
-		var meep = this.sliders.scaleSliders(1,-100,100,false);
-	this.canvas.ellipse(meep,meep,w/3,w/3);
+	//eyes
+	this.drawEyes();
 	this.canvas.pop();
-	
-
-
 	return this.canvas;
+}
+
+this.drawEyes = function(){
+	//the measurements and positions of the eyes and eye embelishments
+	this.eyeX = 20;
+	this.eyeY = 0;
+	this.eyeSize = 20;
+	this.i = this.eyeSize/2;
+	this.eyeType = this.sliders.scaleSliders(1,0,100,true);
+
+		//normal face and eyes
+		if(this.eyeType<52){
+			this.canvas.fill(151, 102, 52);
+			this.canvas.ellipse(0,0,w/3,w/3);
+			this.canvas.fill(255);
+		}
+		//creepy face and eyes
+		else{
+this.canvas.fill(this.sliders.scaleSliders(4,50,200,true),this.sliders.scaleSliders(2,0,255,true),this.sliders.scaleSliders(3,0,255,true));
+	//face
+	this.canvas.ellipse(0,0,w/3,w/3);
+			this.canvas.fill(0);}
+		//draw the right eye circle and any embelishments
+		this.canvas.push();
+		this.canvas.translate(this.eyeX, this.eyeY);
+		this.canvas.ellipse(0,0,this.eyeSize, this.eyeSize);
+		//creepy x dissapeers later than the other eye
+		if (this.eyeType>=52 && this.eyeType < 90){
+		this.drawX(this.i,this.i);
+	}
+		this.canvas.pop();
+
+
+//draw the left eye circle and any embelishments
+		this.canvas.push();
+		this.canvas.translate(-this.eyeX, -this.eyeY);
+		this.canvas.ellipse(0,0,this.eyeSize, this.eyeSize);
+		//creepy x
+		if (this.eyeType>=52 && this.eyeType < 75){
+		this.drawX(this.i,this.i);
+	}
+		this.canvas.pop();
+
+		
+}
+
+//draws a white x with the required dimensions at the current 0,0 point of the canvas
+
+this.drawX = function (w,h){
+	this.xX = w/2;
+	this.xY = h/2;
+this.canvas.push();
+this.canvas.stroke(255);
+//line top right to bottom left
+this.canvas.line(-this.xX,-this.xY, this.xX,this.xY);
+//line bottom left to top right
+this.canvas.line(-this.xX,this.xY, this.xX,-this.xY);
+this.canvas.pop();
 }
 }
