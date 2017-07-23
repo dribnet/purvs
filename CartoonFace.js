1a7703db56d0e5128f1acbd7125d4c52c35c8110
S-Williams42 <williasusa@myvuw.ac.nz>
2017-07-24 01:43:00 +1200

    continued work on cartoon face and added line weights

--- CartoonFace.js
+++ CartoonFace.js
@@ -33,39 +33,86 @@ this.drawFace = function(){
 	this.c.noFill();
 	// rotation in degrees
 	this.c.angleMode(DEGREES);
-	this.c.ellipse(0,0,100,100);
+	
 
 
 	//draws the face
-	this.mouth(1);
-	this.nose(1);
+	this.drawWithWeight();
+	
 	this.c.pop();
 	return this.c;
 }
 
+this.drawWithWeight = function(){
+this.wNum = this.sliders.scaleSliders(1,1,4,true);
+	if(this.wNum == 1){
+this.c.push();
+	this.c.strokeWeight(1);
+	this.head();
+	this.c.strokeWeight(3);
+	this.mouth();
+	this.c.strokeWeight(2);
+	this.nose();
+	this.c.pop();
+
+}
+else if (this.wNum == 2){
+	this.c.push();
+	this.c.strokeWeight(4);
+	this.head();
+	this.c.strokeWeight(1);
+	this.mouth();
+	this.c.strokeWeight(2);
+	this.nose();
+	this.c.pop();
+}
+else if (this.wNum == 3){
+	this.c.push();
+	this.c.strokeWeight(1);
+	this.head();
+	this.c.strokeWeight(3);
+	this.mouth();
+	this.c.strokeWeight(5);
+	this.nose();
+	this.c.pop();
+}
+else if (this.wNum == 4){
+	
+}
+else if (this.wNum == 5){
+	
+}
+}
+
 //translates the canvas based on slider 2
 //draws one of the mouths based on the integer mNum
-this.mouth = function(mNum){
+
+this.mouth = function(){
+//works out which mouth to draw
+this.mNum = this.sliders.scaleSliders(4,1,6,true);
+
+
 	//positions the mouth
 this.c.push();
-this.c.translate(0,sliders.scaleSliders(1, 20, 30, true));
+this.c.translate(0,sliders.scaleSliders(1, 25, 30, true));
 this.c.rotate(this.sliders.scaleSliders(2,-30,30, true));
 
 //draws a mouth depending on the number it has
-if(mNum == 1){
+if(this.mNum == 1){
 this.lineMouth();
 }
-else if (mNum == 2){
-
+else if (this.mNum == 2){
+	this.sadMouth();
 }
-else if (mNum == 3){
+else if (this.mNum == 3){
+	this.curveMouth();
+}
+else if (this.mNum == 4){
+	this.grinMouth();
 	
 }
-else if (mNum == 4){
-	
-}
-else if (mNum == 5){
-	
+else if (this.mNum >= 5){
+	this.gapeMouth();
 }
 this.c.pop();
 }
@@ -73,35 +120,121 @@ this.c.pop();
 //draws a straight line mouth at position 0,0
 this.lineMouth = function(){
 	this.mX = this.sliders.scaleSliders(2,10,25,true);
+
 	this.c.line(-this.mX,0,this.mX,0);
 }
 
+this.curveMouth = function(){
+	this.mX = this.sliders.scaleSliders(2,10,20,true);
+	this.c.bezier(-this.mX-(this.mX/6), 0, -this.mX, 20, this.mX, 20, this.mX+(this.mX/6),0);
+}
+
+this.sadMouth = function(){
+	this.mX = this.sliders.scaleSliders(2,10,20,true);
+	this.c.bezier(-this.mX-(this.mX/6), 10, -this.mX, -10, this.mX, -10, this.mX+(this.mX/6),10);
+}
+
+this.grinMouth = function(){
+	this.mX = this.sliders.scaleSliders(2,10,25,true);
+	this.c.line(-this.mX,0,this.mX,0);
+	this.c.bezier(-this.mX-(this.mX/6), 0, -this.mX, 20, this.mX, 20, this.mX+(this.mX/6),0);
+}
+
+this.gapeMouth = function(){
+	this.mX = this.sliders.scaleSliders(2,10,40,true);
+	this.mY = this.sliders.scaleSliders(2,10,25,true);
+	this.c.ellipse(0,7, this.mX, this.mY);
+}
 
 
 
 //translates the canvas based on slider 2
 //draws one of the mouths based on the integer mNum
-this.nose = function(mNum){
-	//positions the mouth
+this.nose = function(){
+	//works out which nose
+	this.nNum = this.sliders.scaleSliders(5,1,6,true);
+	//positions the nose
 this.c.push();
-this.c.translate(0,0);
-this.c.rotate(0);
+this.c.translate(0,5);
+this.c.rotate(this.sliders.scaleSliders(1,-20,20,true));
 
-//draws a mouth depending on the number it has
-if(mNum == 1){
-
-}
-else if (mNum == 2){
+//draws a nose depending on the number it has
+if(this.nNum == 1){
+	this.bulbNose();
 
 }
-else if (mNum == 3){
+else if (this.nNum == 2){
+	
+	//circle nose
+	this.nX = this.sliders.scaleSliders(2,20,10,true);
+	this.c.ellipse(0,0, this.nX, this.nX);
+}
+else if (this.nNum == 3){
+	this.pointyNose();
+}
+else if (this.nNum == 4){
 	
 }
-else if (mNum == 4){
+else if (this.nNum == 5){
 	
 }
-else if (mNum == 5){
-	
+this.c.pop();
+}
+
+this.bulbNose = function(){
+	this.c.beginShape();
+	this.nX = this.sliders.scaleSliders(2,14,19,true);
+	this.c.curveVertex(-10,10);
+	this.c.curveVertex(0,5);
+	this.c.curveVertex(11,10);
+	this.c.curveVertex(this.nX,0);
+	this.c.curveVertex(11,-10);
+	this.c.curveVertex(0,-5);
+	this.c.curveVertex(-10,-10);
+
+
+	this.c.endShape();
+}
+
+this.pointyNose =function(){
+	//height of nose
+	this.nY = this.sliders.scaleSliders(2,10,4,true);
+	//length of nose
+	this.nX = this.sliders.scaleSliders(2,15,25,true);
+	this.c.beginShape();
+	this.c.vertex(0,-this.nY);
+	//this tip of the nose
+	this.c.vertex(this.nX, 0);
+	this.c.vertex(0, this.nY);
+	this.c.endShape();
+}
+
+this.head = function(){
+	//works out which head to draw
+	this.hNum = this.sliders.scaleSliders(3,1,6,true);
+	this.c.push();
+	this.c.rectMode(CENTER);
+
+if(this.hNum == 1){
+	//square head
+	this.c.rect(0,0,115,115);
+
+}
+else if (this.hNum == 2){
+	//oval head
+	this.c.ellipse(0,0,100,150);
+
+}
+else if (this.hNum == 3){
+	//circular head
+	this.c.ellipse(0,0,115,115);
+}
+else if (this.hNum == 4){
+	//rectangle head
+	this.c.rect(0,0,100,150);
+}
+else if (this.hNum == 5){
+	//no head
 }
 this.c.pop();
 }
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## PS1 MDDN 342 2017
 
 This README explains the three different paramaterized faces for part2. 
-I have started work on the cartoon face. At this stage I am not sure how I will keep the lines within the face shape or from overlapping, or if I will even choose to do that, because it might actually work really well and create a nice doodle-like style.
-At this point I have borrowed a lot of my setup code from the rag doll object and tweaked it a little. I have an example working of how the sliders and functions will work which is why I am uploading
+continued work on the cartoon face with some face shapes, noses, mouths, and line weights all responding to sliders
