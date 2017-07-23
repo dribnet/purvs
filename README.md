288f3ca2d99c1eab56ef0b0277557f20a4c11460
ElectricPowerHouse <acidplaystf2@gmail.com>
2017-07-23 20:52:07 +1200

    Added translation for monster head, and updated readme

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # PS1 MDDN 342 2017
 
-This README explains the three different paramaterized faces for part2.
-
-In my case, in each of my faces I use 3 sliders. For the first face I used my previous solution but added controls for the tilt, mouth openess, and number of eyes.
-
-In the second version I was mostly thinking about eyes and hair. For the eyes, there is a true/false variable which determines if the face is blinking as well as a continuous variable for shifting the eye left and right. The hair ranges from stubble to bushy.
-
-In the third face I tried out a darker robot idea. This robot is always staring right at you and might have a wide or narrow head. His middle eye is a lazy eye.
+This parametric alien face uses height, width and eyes as parameters to
+play around with.
+Use the slider on the left-top hand of the sketch to play around with
+parameters.
--- sketch.js
+++ sketch.js
@@ -40,11 +40,13 @@ var bg_color1 = [242, 58, 107];
 var bg_color2 = [47, 59, 64];
 var bg_color3 = [70, 70, 120];
 
-var faceColor = [243,100,242];
 var white = [255,255,255];
 var lightGray = [220,220,220];
+/*
+var faceColor = [243,100,242];
 var noseColor = [133,133,238];
 var mouthColor = [97,97,235];
+*/
 
 var fg_color1 = [151, 102, 52];
 var fg_color2 = [56, 91, 194];
@@ -57,7 +59,25 @@ var stroke_color3 = [50, 50, 50];
 var colorHair = [20, 20, 0];
 
 
-function drawMainFace(faceWidth,faceHeight,eyeNum){
+
+function drawMainFace(mX,mY,faceWidth,faceHeight,eyeNum,hornSize,blueColor,monsterHeight){
+
+  push();
+
+  var monsterWidth = monsterHeight*1.92;
+
+  translate(mX,mY);
+
+  // global variables for colors
+  var bg_color1 = [242, 58, 107+blueColor];
+  var bg_color2 = [47, 59, 64+blueColor];
+  var bg_color3 = [70, 70, 120+blueColor];
+
+  var faceColor = [243,100,180+blueColor];
+  var white = [255,255,255];
+  var lightGray = [220,220,220];
+  var noseColor = [133,133,238+blueColor];
+  var mouthColor = [97,97,235+blueColor];
 
   //faceWidth = 5;
 
@@ -65,34 +85,34 @@ fill(faceColor);
 //face
 beginShape();
 //lefttop
-vertex(width*0.3-(faceWidth),height*0.35-(faceHeight));
+vertex(monsterWidth*0.3-(faceWidth),monsterHeight*0.35-(faceHeight));
 //top
-vertex(width*0.5,height*0.1-(faceHeight));
+vertex(monsterWidth*0.5,monsterHeight*0.1-(faceHeight));
 //righttop
-vertex(width*0.7+(faceWidth),height*0.35-(faceHeight));
+vertex(monsterWidth*0.7+(faceWidth),monsterHeight*0.35-(faceHeight));
 //rightbottom
-vertex(width*0.75+(faceWidth),height*0.7+(faceHeight));
+vertex(monsterWidth*0.75+(faceWidth),monsterHeight*0.7+(faceHeight));
 //bottom
-vertex(width*0.5,height*0.9+(faceHeight));
+vertex(monsterWidth*0.5,monsterHeight*0.9+(faceHeight));
 //leftbottom
-vertex(width*0.25-(faceWidth),height*0.7+(faceHeight));
+vertex(monsterWidth*0.25-(faceWidth),monsterHeight*0.7+(faceHeight));
 
 endShape();
 
-var eyeSize = 43;
+var eyeSize = monsterWidth*0.066;
 //draw eyes
 //first 2
-drawEye(width*0.35-faceWidth,height*0.4-faceHeight,eyeSize);
-drawEye(width*0.65+faceWidth,height*0.4-faceHeight,eyeSize);
+drawEye(monsterWidth*0.35-faceWidth,monsterHeight*0.4-faceHeight,eyeSize);
+drawEye(monsterWidth*0.65+faceWidth,monsterHeight*0.4-faceHeight,eyeSize);
 //second 2
 if(eyeNum>=2){
-drawEye(width*0.42-(faceWidth*0.5),height*0.35-(faceHeight),eyeSize);
-drawEye(width*0.58+(faceWidth*0.5),height*0.35-(faceHeight),eyeSize);
+drawEye(monsterWidth*0.42-(faceWidth*0.5),monsterHeight*0.35-(faceHeight),eyeSize);
+drawEye(monsterWidth*0.58+(faceWidth*0.5),monsterHeight*0.35-(faceHeight),eyeSize);
 }
 //third 2
 if(eyeNum>=3){
-drawEye(width*0.42-(faceWidth*0.6),height*0.45-(faceHeight),eyeSize);
-drawEye(width*0.58+(faceWidth*0.6),height*0.45-(faceHeight),eyeSize);
+drawEye(monsterWidth*0.42-(faceWidth*0.6),monsterHeight*0.45-(faceHeight),eyeSize);
+drawEye(monsterWidth*0.58+(faceWidth*0.6),monsterHeight*0.45-(faceHeight),eyeSize);
 }
 
 
@@ -101,19 +121,19 @@ drawEye(width*0.58+(faceWidth*0.6),height*0.45-(faceHeight),eyeSize);
 fill(white);
 beginShape();
 //1
-vertex(width*0.65+(faceWidth*0.8),height*0.3-(faceHeight));
+vertex(monsterWidth*0.65+(faceWidth*0.8),monsterHeight*0.3-(faceHeight));
 //2
-vertex(width*0.8+(faceWidth),height*0.25-(faceHeight));
+vertex(monsterWidth*0.8+(faceWidth)+hornSize,monsterHeight*0.25-(faceHeight));
 //3
-vertex(width*0.82+(faceWidth),height*0.08-(faceHeight));
+vertex(monsterWidth*0.82+(faceWidth)+hornSize,monsterHeight*0.08-(faceHeight));
 //4
-vertex(width*0.78+(faceWidth),height*0.02-(faceHeight));
+vertex(monsterWidth*0.78+(faceWidth)+hornSize,monsterHeight*0.02-(faceHeight));
 //5
-vertex(width*0.77+(faceWidth),height*0.08-(faceHeight));
+vertex(monsterWidth*0.77+(faceWidth)+hornSize,monsterHeight*0.08-(faceHeight));
 //6
-vertex(width*0.74+(faceWidth),height*0.15-(faceHeight));
+vertex(monsterWidth*0.74+(faceWidth)+hornSize,monsterHeight*0.15-(faceHeight));
 //7
-vertex(width*0.58+(faceWidth*0.4),height*0.2-(faceHeight));
+vertex(monsterWidth*0.58+(faceWidth*0.4),monsterHeight*0.2-(faceHeight));
 endShape();
 
 
@@ -121,27 +141,27 @@ endShape();
 fill(white);
 beginShape();
 //1
-vertex(width*0.35-(faceWidth*0.8),height*0.3-(faceHeight));
+vertex(monsterWidth*0.35-(faceWidth*0.8),monsterHeight*0.3-(faceHeight));
 //2
-vertex(width*0.2-(faceWidth),height*0.25-(faceHeight));
+vertex(monsterWidth*0.2-(faceWidth)-hornSize,monsterHeight*0.25-(faceHeight));
 //3
-vertex(width*0.18-(faceWidth),height*0.08-(faceHeight));
+vertex(monsterWidth*0.18-(faceWidth)-hornSize,monsterHeight*0.08-(faceHeight));
 //4
-vertex(width*0.22-(faceWidth),height*0.02-(faceHeight));
+vertex(monsterWidth*0.22-(faceWidth)-hornSize,monsterHeight*0.02-(faceHeight));
 //5
-vertex(width*0.23-(faceWidth),height*0.08-(faceHeight));
+vertex(monsterWidth*0.23-(faceWidth)-hornSize,monsterHeight*0.08-(faceHeight));
 //6
-vertex(width*0.26-(faceWidth),height*0.15-(faceHeight));
+vertex(monsterWidth*0.26-(faceWidth)-hornSize,monsterHeight*0.15-(faceHeight));
 //7
-vertex(width*0.42-(faceWidth*0.4),height*0.2-(faceHeight));
+vertex(monsterWidth*0.42-(faceWidth*0.4),monsterHeight*0.2-(faceHeight));
 endShape();
 
 print(width,height);
 //draw nose
-var noseWidth = width*0.16+faceWidth;
-var noseHeight = width*0.03;
-var noseX = width*0.5;
-var noseY = height*0.57-faceHeight;
+var noseWidth =monsterWidth*0.16+faceWidth;
+var noseHeight = monsterWidth*0.03;
+var noseX = monsterWidth*0.5;
+var noseY = monsterHeight*0.57-faceHeight;
 fill(noseColor);
 
 rectMode(CENTER);
@@ -149,10 +169,10 @@ ellipse(noseX,noseY,noseWidth,noseHeight/2);
 ellipse(noseX,noseY+noseHeight/4,noseWidth/4,noseHeight);
 
 //draw mouth
-var mouthWidth = (width*0.23)+(faceWidth*2);
-var mouthHeight = (height*0.1)+(faceHeight);
-var mouthX = width*0.5;
-var mouthY = (height*0.75)+(faceHeight/2);
+var mouthWidth = (monsterWidth*0.23)+(faceWidth*2);
+var mouthHeight = (monsterHeight*0.1)+(faceHeight);
+var mouthX = monsterWidth*0.5;
+var mouthY = (monsterHeight*0.75)+(faceHeight/2);
 
 fill(mouthColor);
 ellipse(mouthX,mouthY,mouthWidth,mouthHeight);
@@ -160,8 +180,8 @@ ellipse(mouthX,mouthY,mouthWidth,mouthHeight);
 //tooth 1
 fill(white);
 rectMode(CORNER);
-var toothWidth = width*0.01;
-var toothDiff = width*0.015;
+var toothWidth = monsterWidth*0.01;
+var toothDiff = monsterWidth*0.015;
 var toothX1 = (mouthX-toothDiff);
 var toothY1 = mouthY-(mouthHeight/2);
 var toothX2 = (mouthX-toothDiff*2)-toothWidth-(faceWidth/8);
@@ -172,7 +192,7 @@ triangle(toothX1,toothY1,toothX2,toothY2,toothX3,toothY3);
 
 
 
-
+pop();
 }
 
 
@@ -189,156 +209,7 @@ ellipse(x+mouseXPos,y+mouseYPos,size*0.6,size*0.6);
 
 }
 
-function drawFace1(x, y, w, h, tilt_value, eye_value, mouth_value) {
-  push();
-  translate(x, y);
-  rotate(tilt_value);
 
-  var extent = 0;
-  if(h < w) {
-    extent = h / 2;
-  }
-  else {
-    extent = w / 2;
-  }
-  var scale = extent / 220.0;
-
-  fill(fg_color1);
-  ellipse(0, 0, 300 * scale, 400 * scale);
-
-  // eyes
-  if (eye_value === 1 || eye_value == 3) {
-    fill(bg_color1);
-    ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
-    fill(fg_color1);
-    ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
-
-  if (eye_value >= 2) {
-    fill(bg_color1);
-    ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
-    ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
-
-    fill(fg_color1);
-    ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-    ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
-
-  // mouth
-  fill(bg_color1);
-  ellipse(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
-  pop();
-}
-
-function drawFace2(x, y, w, h, hair_value, eye_value, blink_value) {
-  rectMode(CENTER);
-  push();
-  translate(x, y);
-
-  var extent = 0;
-  if(h < w) {
-    extent = h / 2;
-  }
-  else {
-    extent = w / 2;
-  }
-  var scale = extent / 220.0;
-
-  stroke(stroke_color3);
-  fill(fg_color3);
-  ellipse(0, 0, 300 * scale, 400 * scale);
-
-  // eyes. first check for blinking
-  if(blink_value > 0) {
-    fill(bg_color3);
-    ellipse(-50 * scale, -80 * scale, 50 * scale, 2 * scale);
-    ellipse( 50 * scale, -80 * scale, 50 * scale, 2 * scale);
-  }
-  else {
-    fill(bg_color3);
-    ellipse(-50 * scale, -80 * scale, 50 * scale, 18 * scale);
-    ellipse( 50 * scale, -80 * scale, 50 * scale, 18 * scale);
-
-    fill(fg_color3);
-    ellipse((-50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
-    ellipse(( 50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
-
-  // mouth
-  fill(bg_color3);
-  ellipse(0 * scale, 70 * scale, 150 * scale, 20 * scale);
-
-  // TODO: paramaterize hair
-  var follicles = [
-    [346,138],
-    [391,120],
-    [391,67],
-    [439,76],
-    [463,42],
-    [487,18],
-    [481,101],
-    [520,102],
-    [520,78],
-    [533,54],
-    [560,108],
-    [580,76],
-    [596,124],
-    [618,124]
-  ];
-
-  resetMatrix();
-  fill(colorHair);
-  var radius = hair_value * scale;
-  for(var i=0; i<follicles.length; i++) {
-    ellipse(240+follicles[i][0]/2, 120 + (follicles[i][1]/2), radius, radius);
-  }
-  rectMode(CORNER);
-  resetMatrix();
-}
-
-function drawFace3(x, y, w, h, width_value, eye_value, mouth_value) {
-  push();
-  rectMode(CENTER);
-  translate(x, y);
-  // rotate(width_value);
-
-  var extent = 0;
-  if(h < w) {
-    extent = h / 2;
-  }
-  else {
-    extent = w / 2;
-  }
-  var scale = extent / 220.0;
-
-  stroke(stroke_color2)
-  fill(fg_color2);
-  rect(0, 0, (300 + width_value) * scale, 400 * scale);
-
-  // eyes
-  if (eye_value === 1 || eye_value == 3) {
-    fill(bg_color2);
-    rect( 0, -80 * scale, 50 * scale, 30 * scale);
-    fill(fg_color2);
-    ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
-
-  if (eye_value >= 2) {
-    fill(bg_color2);
-    rect(-60 * scale, -80 * scale, 50 * scale, 30 * scale);
-    rect( 60 * scale, -80 * scale, 50 * scale, 30 * scale);
-
-    fill(fg_color2);
-    ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-    ellipse( 60 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
-
-  // mouth
-  fill(bg_color2);
-  rect(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
-  rectMode(CORNER);
-  pop();
-}
 
 function draw () {
   noStroke();
@@ -356,11 +227,19 @@ function draw () {
     // draw 1st face
     fill(bg_color1);
 
-    var faceWidth = map(s1, 0, 100, -75, -25);
-    var faceHeight = map(s2, 0, 100, -30, 30);
-    var eye_value = Math.floor(map(s3, 0, 100, 1, 3));
+    var m1X = width/6;
+    var m1Y = height/5;
+    var m1Height = 350;
 
-    drawMainFace(faceWidth,faceHeight,eye_value);
+    var faceWidth = map(s1, 0, 100, -(m1Height/6.66), -(m1Height/20));
+    var faceHeight = map(s2, 0, 100, -(m1Height/16.66), (m1Height/16.66));
+    var eye_value = Math.floor(map(s3, 0, 100, 1, 3));
+    var hornSize = map(s4,0,100,-(m1Height/10),(m1Height/10));
+    var blueColor = map(s5,0,100,-50,0);
+
+    drawMainFace(m1X,m1Y,faceWidth,faceHeight,eye_value,hornSize,blueColor,m1Height);
+    //drawMainFace(0,m1Y,faceWidth,faceHeight,eye_value,hornSize,blueColor,m1Height);
+    //drawMainFace(width/2,m1Y,faceWidth,faceHeight,eye_value,hornSize,blueColor,m1Height);
 
     //drawFace1(face_x, face_y, face_w, face_h, tilt_value, eye_value, mouth_value);
 
@@ -371,7 +250,7 @@ function draw () {
 
     var hair_value = map(s1, 0, 100, 2, 90);
     var blink_value = Math.floor(map(s3, 0, 100, 0, 1));
-    var eye_value = map(s2, 0, 100, -15, 15);
+    var eye_value = map(s2, 0, 100, 0, 100);
 
     //drawFace2(face_x, face_y, face_w, face_h, hair_value, eye_value, blink_value);
 
