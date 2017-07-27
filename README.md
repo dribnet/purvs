970c627ab3efdcb7ba2e0637af59f0392af7736f
Guest User <Guest@b-03-05-lab.mddn.vuw.ac.nz>
2017-07-27 14:15:03 +1200

    Task 3

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## PS1 MDDN 342 2017
 
-This README explains the three different paramaterized faces for part2. 
+This README describes the matrix of randomised faces completed for task 3 
+
+Face #1; and the design I am going to move forward with, is a parametric old man inspired by my baby face (#3).  The old man has configurable worry lines, mood, liver spots, glasses and ears. The liver spots parameter contains both scalar and discrete elements: the sketch will draw 1, 2, or 3 spots depending on the slider's value, and the spots will get progressively darker as the the value ramps up. The worry lines get darker & lighter. The man's mouth will curve up and down depending on the mouth parameter, and the size of his ears and the lenses of his glasses will change dependant on their respective variables.
 
 The second face is a version of the Mario & Luigi sketch I made earlier. This version casts luigi aside (don't worry, he's used to it!) and draws our sewer-spelunking hero with some features mapped to four of the sliders:
 	- Mousache slant
@@ -8,10 +10,11 @@ The second face is a version of the Mario & Luigi sketch I made earlier. This ve
 	- Moustache bushiness: modulates the size of the individual ellipses that make up the moustache. also modulates the size of mario's nose
 	- Cheek chubbiness
 
-I liked the art style that i used for my Mario. I thought that his cheeks made him look like a baby so I decided to make my next face that of a baby. I used 4 sliders for this onel giving control over:
+I liked the art style that I used for my Mario. I thought that his cheeks made him look like a baby so I decided to make my next face that of a baby. I used 4 sliders for this onel giving control over:
 	- nose size
 	- cheek chub
 	- mouth openness
 	- 'alertness' when baby is tired he starts to drool and has trouble keeping his eyes open
+
 
 In the third face I tried out a darker robot idea. This robot is always staring right at you and might have a wide or narrow head. His middle eye is a lazy eye.
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-var canvasWidth = 960;
-var canvasHeight = 500;
+var canvasWidth = 1100;
+var canvasHeight = 720;
 var slider1, slider2, slider3, slider4, slider5;
 var faceSelector;
 
@@ -34,8 +34,9 @@ var stroke_color3 = [50, 50, 50];
 
 var colorHair = [20, 20, 0];
 
-var m_x
-var m_y
+function changeRandomSeed() {
+    curRandomSeed = curRandomSeed + 1;
+}
 
 function setup() {
 
@@ -43,25 +44,20 @@ function setup() {
     var main_canvas = createCanvas(canvasWidth, canvasHeight);
     main_canvas.parent('canvasContainer');
 
+
+    curRandomSeed = int(focusedRandom(0, 100));
+
+    randButton = createButton('randomize');
+    randButton.mousePressed(changeRandomSeed);
+    randButton.parent('selector1Container');
+
     // create sliders
-    slider1 = createSlider(0, 100, 50);
-    slider2 = createSlider(0, 100, 50);
-    slider3 = createSlider(0, 100, 50);
-    slider4 = createSlider(0, 100, 50);
-    slider5 = createSlider(0, 100, 50);
-
-    slider1.parent('slider1Container');
-    slider2.parent('slider2Container');
-    slider3.parent('slider3Container');
-    slider4.parent('slider4Container');
-    slider5.parent('slider5Container');
-
     faceSelector = createSelect();
     faceSelector.option('1');
     faceSelector.option('2');
     faceSelector.option('3');
-    faceSelector.option('all')
-    faceSelector.value('all');
+    // faceSelector.option('all')
+    faceSelector.value('1');
     faceSelector.parent('selector1Container');
 
     // rotation in degrees
@@ -69,86 +65,79 @@ function setup() {
 }
 
 function draw() {
+	resetFocusedRandom(curRandomSeed);
+
     noStroke();
 
     var mode = faceSelector.value();
 
-    if (mode != 'all') {
-        if (mode == '1') {
-            background(bg_color1);
-        } else if (mode == '2') {
-            background(bg_color2);
-        } else if (mode == '3') {
-            background(bg_color3);
-        }
-    }
-
-    var s1 = slider1.value();
-    var s2 = slider2.value();
-    var s3 = slider3.value();
-    var s4 = slider4.value();
-    var s5 = slider5.value();
-
     // use same size / y_pos for all faces
-    var face_w = canvasWidth / 4;
-    var face_h = face_w;
-    var face_y = height / 2;
-    var face_x = width / 2;
+    var w = canvasWidth / 5;
+    var h = canvasHeight / 3;
 
     //face 1 : brown
-    if (mode == '1' || mode == 'all') {
+    if (mode == '1') {
 
         fill(bg_color1);
-        rect(0, 0, width / 3, height);
-        var ear_value = map(s1, 0, 100, 0.7, 1.3);
-        var smile_value = map(s3, 0, 100, -0.1, 2.1);
-        var eye_value = map(s2, 0, 100, 0.7, 1.3);
-        var wrinkle_value = map(s4,0,100,0,0.7);
-        var spots_value = map(s5,0,100,0,3);
+        rect(0, 0, width, height);
 
-        if (mode == 'all') {
-            face_x = width / 6;
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 5; j++) {
+                var y = h / 2 + h * i;
+                var x = w / 2 + w * j;
+                var ear_value = focusedRandom(0.7, 1.3);
+                var smile_value = focusedRandom(-0.1, 2.1);
+                var eye_value = focusedRandom(0.7, 1.3);
+                var wrinkle_value = focusedRandom(0, 0.7);
+                var spots_value = focusedRandom(0, 3);
+                drawFace1(x, y, w, h, ear_value, eye_value, smile_value, wrinkle_value, spots_value );
+            }
         }
-
-        drawFace1(face_x, face_y, face_w, face_h, ear_value, eye_value, smile_value,wrinkle_value,spots_value);
     }
+
     //face 2 : Mario
-    if (mode == '2' || mode == 'all') {
+    if (mode == '2') {
 
         fill(bg_color2);
-        rect(width / 3, 0, 2 * width / 3, height);
-        var stache_slant_value = map(s1, 100, 0, 0.8, 1.8);
-        var dilation_value = map(s2, 0, 100, 0.75, 1.25);
-        var stache_bush_value = map(s3, 0, 100, 0.9, 1.4);
-        var cheek_chub_value = map(s4, 0, 100, 0.9, 1.1);
-        if (mode == 'all') {
-            face_x = 3 * width / 6;
-        }
+        rect(0, 0, width, height);
 
-        drawFace2(face_x, face_y, face_w, face_h, stache_slant_value, stache_bush_value, cheek_chub_value, dilation_value);
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 5; j++) {
+                var y = h / 2 + h * i;
+                var x = w / 2 + w * j;
+                var stache_slant_value = focusedRandom(0.8, 1.8);
+                var dilation_value = int(focusedRandom(0.75, 1.25));
+                var stache_bush_value = focusedRandom(0.9, 1.4);
+                var cheek_chub_value = focusedRandom(0.9, 1.1);
+ 				drawFace2(x, y, w, h, stache_slant_value, stache_bush_value, cheek_chub_value, dilation_value);            }
+        }
+       
     }
     // face 3 : babby
-    if (mode == '3' || mode == 'all') {
+    if (mode == '3') {
 
-        fill(bg_color3);
-        rect(2 * width / 3, 0, 2 * width / 3, height);
-        var hair_volume_value = map(s1, 100, 0, 0.8, 1.8);
-        var nose_size_value = map(s2, 0, 100, 0.6, 1.4);
-        var mouth_gape_value = map(s3, 0, 100, 0.9, 1.4);
-        var cheek_chub_value = map(s4, 0, 100, 0.9, 1.1);
-        var drool_drip_value = map(s5, 0, 100, 0.9, 1.45);
-        if (mode == 'all') {
-            face_x = 5 * width / 6;
+		fill(bg_color2);
+        rect(0, 0, width, height);
+
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 5; j++) {
+                var y = h / 2 + h * i;
+                var x = w / 2 + w * j;
+                var hair_volume_value = focusedRandom(0.8, 1.8);
+                var nose_size_value = int(focusedRandom(0.6, 1.4));
+                var mouth_gape_value = focusedRandom(0.9, 1.4);
+                var cheek_chub_value = focusedRandom(0.9, 1.1);
+                var drool_drip_value = focusedRandom(0.9, 1.45);
+ 				drawFace3(x, y, w, h, hair_volume_value,mouth_gape_value,cheek_chub_value,drool_drip_value);            }
+ 				//x, y, w, h, hair_volume, mouth_size, cheek_chub, nose_size, drool_drip
         }
-
-        drawFace3(face_x, face_y, face_w, face_h, hair_volume_value, mouth_gape_value, cheek_chub_value, nose_size_value, drool_drip_value);
+       
     }
 
-}
-
+        }
 //_______________DRAW FUNCTIONS______________//
 
-function drawFace1(x, y, w, h, ears, eyes, smile,frown,spots) {
+function drawFace1(x, y, w, h, ears, eyes, smile, frown, spots) {
     //OLD
 
     push();
@@ -167,7 +156,7 @@ function drawFace1(x, y, w, h, ears, eyes, smile,frown,spots) {
     ellipse(-10, 84, 25, 25); // butt chin left
     ellipse(10, 84, 25, 25); //butt chin right
 
-    fill(215+10*spots);
+    fill(215 + 10 * spots);
     push();
     translate(-63, -40);
     rotate(23);
@@ -232,39 +221,39 @@ function drawFace1(x, y, w, h, ears, eyes, smile,frown,spots) {
     pop();
 
     //frown lines
-    var frown_color_value = editAlpha(blush_color,frown);
+    var frown_color_value = editAlpha(blush_color, frown);
     stroke(frown_color_value);
     strokeWeight(2);
-    curve(45,-70, 25,-50, -25,-50, -45,-70);
-   	curve(45,-50, 15,-43, -15,-43, -45,-50);
-   	curve(45,-25, 25,-35, -25,-35, -45,-25);
+    curve(45, -70, 25, -50, -25, -50, -45, -70);
+    curve(45, -50, 15, -43, -15, -43, -45, -50);
+    curve(45, -25, 25, -35, -25, -35, -45, -25);
 
-   	//liver spots
-   	var spot_color_value;
-   	spot_color_value = editAlpha(spot_color,spots/4+0.25);
-   	if(Math.floor(spots>0)){
-   		push();
-   		fill(spot_color_value);
-   		translate(23,-63);
-   		ellipse(0,0,4,4);
-   		pop();
-   	}
-   	if(Math.floor(spots>1)){
-   		push();
-   		fill(spot_color_value);
-   		translate(20,-71);
-   		rotate(-45);
-   		ellipse(0,0,8,6);
-   		pop();
-   	}
-   	if(Math.floor(spots>2)){
-   		push();
-   		fill(spot_color_value);
-   		translate(32,-68);
-   		rotate(52);
-   		ellipse(0,0,10,7);
-   		pop();
-   	}
+    //liver spots
+    var spot_color_value;
+    spot_color_value = editAlpha(spot_color, spots / 4 + 0.25);
+    if (Math.floor(spots > 0)) {
+        push();
+        fill(spot_color_value);
+        translate(23, -63);
+        ellipse(0, 0, 4, 4);
+        pop();
+    }
+    if (Math.floor(spots > 1)) {
+        push();
+        fill(spot_color_value);
+        translate(20, -71);
+        rotate(-45);
+        ellipse(0, 0, 8, 6);
+        pop();
+    }
+    if (Math.floor(spots > 2)) {
+        push();
+        fill(spot_color_value);
+        translate(32, -68);
+        rotate(52);
+        ellipse(0, 0, 10, 7);
+        pop();
+    }
     pop();
 
 }
@@ -318,7 +307,7 @@ function drawFace2(x, y, w, h, stache_slant, stache_bush, cheek_chub, dilation) 
     fill(nose_tone);
     ellipse(0, 20, 70 * nose_size_mod, 58 * nose_size_mod); //nose base
     noStroke();
-    fill(editAlpha(skin_tone,0.7));
+    fill(editAlpha(skin_tone, 0.7));
     ellipse(0, 16, 58 * nose_size_mod, 48 * nose_size_mod); //highlight
 
     // logo
