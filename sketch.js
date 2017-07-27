a1740f9828ec7ee7a770cc29c4c4e7bbca12f706
Daniel Tideman <djntideman@gmail.com>
2017-07-27 13:54:59 +1200

    I have finished face 3 and done 2 of the 3 sliders for face 1. I have implemented randomness in face 3.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
-var slider1, slider2, slider3, slider4, slider5;
+var slider1, slider2, slider3, slider4, slider5, button1;
 var faceSelector;
 
 function setup () {
@@ -14,6 +14,7 @@ function setup () {
   slider3 = createSlider(0, 100, 50);
   slider4 = createSlider(0, 100, 50);
   slider5 = createSlider(0, 100, 50);
+  //button1 = createButton();
 
   slider1.parent('slider1Container');
   slider2.parent('slider2Container');
@@ -48,7 +49,7 @@ var stroke_color3 = [50, 50, 50];
 
 var colorHair = [20, 20, 0];
 
-var changing_colour = [20,20,20];
+
 
 var redc = [178 ,34 ,34];
 var bluec = [30 ,144 ,255];
@@ -56,30 +57,35 @@ var greenc = [50 ,205, 50];
 var yellowc = [255 ,255, 0];
 var purplec = [147 ,112 ,219];
 
+//ar a = Math.floor(random(0,1) * 255);
+var back1 = [255 ,120, 82];
 
-function drawFace1(x, y, w, h,eye_value, mouth_value, changing_colour) {
+
+function drawFace1(x, y, w, h,eye_value, mouth_value, face_sharpness,nose_curve) {
   push();
   translate(x, y);
   rotate(0);
 
-  var extent = 0;
-  if(h < w) {
-    extent = h / 2;
-  }
-  else {
-    extent = w / 2;
-  }
-  var scale = extent / 220.0;
+  
 	
   // For the face, as I'm focusing on not eye detail but sharpness, I am going to have the face be from side-on.
   // In this section, the face sharpness will also be edited to get setup for the other features.
   // At this stage I am thinking about using arcs for the chin but I don't know how it will react exactly yet.
-  fill(changing_colour);
-  ellipse(0, 0, 300 * scale, 400 * scale);
+  fill(bluec);
+  ellipse(0, 0, 300 , 400 );
+  if(face_sharpness == 1){
+  	fill(bluec);
+  rect(-145,-20,165,218);
 
+  }
+  if(face_sharpness == 2){
+  	fill(bluec);
+  	quad(0,200,-100,200,-140,140,-115,120);
+
+  }
   // eyes
     fill(bg_color1);
-    ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
+    ellipse(-50 , -80 , 50 , 30 );
     //ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
 
     fill(fg_color1);
@@ -90,12 +96,35 @@ function drawFace1(x, y, w, h,eye_value, mouth_value, changing_colour) {
   //draw nose section
 	
   // This section will be where the bridge will be the arc parameter, with the base being flat.
-	
-	
+  strokeWeight(5);
+  stroke(bg_color1);
+  line(-90,50,-140,50);
+  //noFill();
+
+  //stroke(noStroke);
   // mouth(this has been edited so it is a line of a side view)
   fill(bg_color1);
   //ellipse(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
-  line(0,0,50,50);
+  if(nose_curve == 1){
+  line(-90,-50,-140,50);
+	}
+
+
+if(nose_curve == 2){
+line(-140,50,-110,10);
+line(-110,10,-90,-50);
+
+}
+
+if(nose_curve == 3){
+line(-140,50,-125,40);
+line(-125,40,-105,10);
+line(-105,10,-95,-20);
+line(-95,-20,-90,-50);
+
+}
+  strokeWeight(1);
+  //stroke(bg_color1);
 	   //ERROR HERE RN
 	
   //draw hair section
@@ -127,12 +156,11 @@ function drawFace2(x, y, w, h, hair_value, eye_value, blink_value) {
   ellipse(0, 0, 300 * scale, 400 * scale);
 
   // eyes. first check for blinking
-  if(blink_value > 0) {
+ 
     fill(bg_color3);
     ellipse(-50 * scale, -80 * scale, 50 * scale, 2 * scale);
     ellipse( 50 * scale, -80 * scale, 50 * scale, 2 * scale);
-  }
-  else {
+ 
     fill(bg_color3);
     ellipse(-50 * scale, -80 * scale, 50 * scale, 18 * scale);
     ellipse( 50 * scale, -80 * scale, 50 * scale, 18 * scale);
@@ -140,7 +168,7 @@ function drawFace2(x, y, w, h, hair_value, eye_value, blink_value) {
     fill(fg_color3);
     ellipse((-50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
     ellipse(( 50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
+ 
 
   // mouth
   fill(bg_color3);
@@ -150,59 +178,80 @@ var a = [1,2, 3, 4]
 
 
   // TODO: paramaterize hair
-  var follicles = [
-    [346,138],
-    [391,120],
-    [391,67],
-    [439,76],
-    [463,42],
-    [487,18],
-    [481,101],
-    [520,102],
-    [520,78],
-    [533,54],
-    [560,108],
-    [580,76],
-    [596,124],
-    [618,124]
-  ];
+
 
   resetMatrix();
-  fill(colorHair);
-  var radius = hair_value * scale;
-  for(var i=0; i<follicles.length; i++) {
-    ellipse(240+follicles[i][0]/2, 120 + (follicles[i][1]/2), radius, radius);
-  }
+
+ 
   rectMode(CORNER);
   resetMatrix();
 }
 
 function drawFace3(x, y, w, h, amount_of_colours, amount_of_polys, amount_of_features) {
+  
+ randomSeed(129);
   push();
   //rectMode(CENTER);
   translate(x, y);
   // rotate(width_value);
 
+if (amount_of_polys == 1){
+rectMode(CENTER);
+fill(back1);
+rect(0 , 0, width/3, height);
+
+rectMode(CORNER);
+}
+
+if(amount_of_polys == 2){
+	fill(purplec);
+triangle((0-(width/6)),(0-(height/2))  ,(0-(width/6)),(0+(height/2))  ,(0+(width/6)),(0-(height/2)));
+fill(redc);
+triangle((0+(width/6)),(0+(height/2))  ,(0-(width/6)),(0+(height/2))  ,(0+(width/6)),(0-(height/2)));
+}
+
+if(amount_of_polys == 3){
+	fill(bluec);
+triangle((0-(width/6)),(0-(height/2))  ,(0-(width/6)),(0+(height/2)), 0,(0-(height/2)) );
+fill(greenc);
+triangle(0,(0-(height/2)), (0-(width/6)),(0+(height/2)),(0+(width/6)),(0+(height/2))  );
+fill(yellowc);
+triangle((0+(width/6)),(0+(height/2)) ,0,(0-(height/2)),(0+(width/6)),(0-(height/2))  );
+}
+
+if(amount_of_polys == 4){
+	fill(yellowc);
+triangle( (0-(width/6)),(0-(height/2)) , (0-(width/6)),(0+(height/2)), 0,0);
+fill(greenc);
+triangle(0,0, (0-(width/6)),(0-(height/2)) , (0+(width/6)),(0-(height/2)));
+fill(redc);
+triangle(0,0, (0+(width/6)),(0-(height/2)) , (0+(width/6)),(0+(height/2)));
+fill(bluec);
+triangle(0,0, (0+(width/6)),(0+(height/2)), (0-(width/6)),(0+(height/2)));
+}
 
   
 
   if(amount_of_colours == 1){
   fill(redc);
-  triangle(-100, -100, 150 , 60 ,-100,150);
+  triangle(-100, -100, 150 , random(0,1) * 60 ,-100,150);
 }
  else if(amount_of_colours == 2){
   fill(bluec);
-  rect(-100, -100, 100 , 250 );
+  triangle(-100, random(0,1) * -100, 100 , random(0,1) * 150, 130,-80 );
   fill(yellowc);
-  rect(0,-100,100,250);
+  triangle(-100,-100,-100,150, 160,-20);
 }
 else if(amount_of_colours == 3){
-  fill(greenc);
-  rect(-100, -100, 200/3 , 250 );
+  fill(yellowc);
+  //rect(-100, -100, 200/3 , random(0,1) *250 );
+  triangle(-100, -100, 120 , random(0,1) *100, -120,160);
   fill(redc);
-  rect(-100 + 200/3,-100,200/3,250);
+  triangle(150,-100,-100,-140,50,80);
+  //rect(-100 + 200/3,-100,200/3,250);
   fill(bluec);
-  rect(-100 + 400/3,-100,200/3,250);
+  triangle(100,-100,-100,random(0,1) * -50, 80,110);
+  //rect(-100 + 400/3,-100,200/3,random(0,1) *250);
 }
 else if(amount_of_colours == 4){
   fill(bluec);
@@ -218,34 +267,62 @@ else if(amount_of_colours == 5){
  fill(redc);
  triangle(100,100,150,-100,0,0);
  fill(bluec);
- triangle(10,30,-100,-150,30,-100);
+ triangle(10,30,-100,random(0,1) * -150,30,-100);
  fill(greenc);
- triangle(-120,50,-50,-60,70,100);
+ triangle(-120,50,-50,-60,70,random(0,1) * 100);
  fill(yellowc);
  triangle(-40,150,-150,70,140,-80);
  fill(purplec);
- triangle(150,20,-50,-100,-140,0);
+ triangle(150,20,-50,-100,random(0,1) * -140,0);
 }
 
 
+if(amount_of_features >= 1){
+// eyes
+    fill(yellowc);	
+  	triangle(-50,-40,-30,-10,-10,random(0,1) * -40);
 
+}
 
-
-  // eyes
-    fill(yellowc);
-    //rect(-60 * scale, -80 * scale, 50 * scale, 30 * scale);
-    //rect( 60 * scale, -80 * scale, 50 * scale, 30 * scale);
-    
-  	triangle(-50,-40,-30,-10,-10,-40);
-
-  	//nose
+if(amount_of_features >= 2){
+//nose
   	fill(redc);
-  	triangle(-60,50,-30,50,-30,0);
+  	triangle(-60,50,-30,random(0,1) * 50,-30,0);
+}
 
-  // mouth
+if(amount_of_features >= 3){
+// mouth
   fill(greenc);
   //rect(-63 , 70, 75 , 50);
-  triangle(-63,70,10,70,-55,125);
+  triangle(-63,70,random(0,1) * 30,70,-55,125);
+}
+
+if(amount_of_features >=4){
+push();
+rotate(223);
+fill(purplec);
+triangle(0,50,73,50,0,random(0,1) * 90);
+pop();
+}
+
+if(amount_of_features >=5){
+fill(bluec);
+
+push();
+rotate(180*random(0,1));
+triangle(-20,-40,-10,-10,10,random(0,1) * 	-40);
+pop();
+}
+
+if(amount_of_features >=6){
+fill(yellowc);
+push();
+rotate(-180*random(0,1));
+triangle(-63,70,random(0,1) * 30,70,-55,125)
+pop();
+
+}
+  
   rectMode(CORNER);
   pop();
 }
@@ -288,8 +365,8 @@ function draw () {
     rect(0, 0, width/3, height);
 	// new variables  
 	var hair_curve = Math.floor(s1,0,100,0,2);
-	var face_sharpness = map(s2,0,100,0,90);
-	var nose_curve = map(s3,0,100,0,90);
+	var face_sharpness = Math.floor(map(s2,0,100,1,3));
+	var nose_curve = Math.floor(map(s3,0,100,1,3));
 	var changing_colour = map(s4, 0, 100, 0, 255);
 	  
 	//base variables
@@ -299,7 +376,7 @@ function draw () {
     if (mode == 'all') {
       face_x = width / 6;
     }
-    drawFace1(face_x, face_y, face_w, face_h, eye_value, mouth_value,changing_colour);    
+    drawFace1(face_x, face_y, face_w, face_h, eye_value, mouth_value,face_sharpness,nose_curve);    
   }
 
   if (mode == '2' || mode == 'all') {
@@ -322,13 +399,12 @@ function draw () {
   }
 
   if (mode == '3' || mode == 'all') {
-    // draw 3nd face
-    fill(bg_color3);
-    rect(2*width/3, 0, width, height);
+    
+    
 	//new variables
 	var amount_of_colours = Math.floor(map(s1,0,100,1,5));
 	var amount_of_polys = Math.floor(map(s2,0,100,1,4));
-	var amount_of_features = Math.floor(map(s3,0,100,2,6));
+	var amount_of_features = Math.floor(map(s3,0,100,1,6));
 	  
 	
    
