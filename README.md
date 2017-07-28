f634333375641947ff043243eff2c168bd9c3a3c
Hamzah Ali <thestabstab2@gmail.com>
2017-07-28 16:31:07 +1200

    Start of part 3

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 ## PS1 MDDN 342 2017
 
--UPDATED README!!
--Cleaned Code!!
+The type of person Zamasu is called, is a Kaioshin, Kai for short. They all aspire to one day become supreme kai’s and so I’ll be calling this a Supreme Kai Generator from this point on, as I'll be trying to incorporate features of other supreme kais into the faces. 
 
-This is the final version of the part 2 exercise. I reused both of my previous characters as I thought they had good potential for parameterized values, and included a new character called Flabebe, which is also a Pokemon. 
-
-I wanted to experiment with giving each parameterized slider a different value to change throughout each character. The main exception to this is the discrete value with does something similar between all characters. (This was because I wanted to adjust something that wasn't an eye blink, but couldn't think of anything else)
-
-The first slider changes rotation, rectangle border, and scale.
-The second slider holds all discrete variables.
-The third slider adjusts pupil scale, eye gleam position, and ear position. (The ears are asymmetrical because I found it more natural than the symmetrical version.)
-
-At this point I thought about which kind of style I would like to use and take into part 3. I decided on Zamasu because I felt like I enjoyed working on him the most, and his face has a lot of complexity and space to add more adjusting variables.
-
-I used the fourth and fifth slider to experiment with him further. 
-
-The fourth slider has a lerpColour value associated to it, which changes between the regular green, to a light pink. I wanted to see how colour changes would work. And finally the fifth slider adjusts his bottom jawline, this was to begin testing varying head shapes.
+As a start I inserted the face from part 2 along with the five parameterised values that already existed. It looks quite nice, but some glaring things that pop out to me as becoming a bit stale between the faces are the hair, ears, and earrings. These three features I’ll look to change next.
 
--- sketch.js
+++ sketch.js
@@ -1,42 +1,31 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
-var slider1, slider2, slider3, slider4, slider5;
-var faceSelector;
+var button;
+var curRandomSeed;
 
 function setup () {
   // create the drawing canvas, save the canvas element
   var main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
-  // create sliders
-  slider1 = createSlider(0, 100, 50);
-  slider2 = createSlider(0, 100, 50);
-  slider3 = createSlider(0, 100, 50);
-  slider4 = createSlider(0, 100, 50);
-  slider5 = createSlider(0, 100, 50);
+  curRandomSeed = int(focusedRandom(0, 100));
 
-  slider1.parent('slider1Container');
-  slider2.parent('slider2Container');
-  slider3.parent('slider3Container');
-  slider4.parent('slider4Container');
-  slider5.parent('slider5Container');
-
-  faceSelector = createSelect();
-  faceSelector.option('1');
-  faceSelector.option('2');
-  faceSelector.option('3');
-  faceSelector.option('all')
-  faceSelector.value('all');
-  faceSelector.parent('selector1Container');
+  randButton = createButton('randomize');
+  randButton.mousePressed(changeRandomSeed);
+  randButton.parent('selector1Container');
 
   // rotation in degrees
   angleMode(DEGREES);
 }
 
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+}
+
 // global variables for colors
-var bg_color1 = [10,24,7];
-var bg_color2 = [17, 29, 34];
-var bg_color3 = [40, 40, 90];
+var bg_color1 = [225, 206, 187];
+var bg_color2 = [47, 59, 64];
+var bg_color3 = [70, 70, 120];
 
 var fg_color1 = [151, 102, 52];
 var fg_color2 = [56, 91, 194];
@@ -60,13 +49,11 @@ var eyecolor2 = "rgb(15,15,15)";
 var ShinFaceColor1 = "rgb(233,207,243)";
 var ShinFaceColor2 = "rgb(213,187,223)";
 
-var colorHair = [20, 20, 0];
-
 function drawFace1(x, y, w, h, tilt_value, earring_value, pupilSize_value, faceColor_value, jawline_value) {
   push();
   translate(x, y);
   rotate(tilt_value);
-  scale(0.5);
+  scale(0.35);
 
   var c0 = color("blue");
   var c1 = color(ZamasuFaceColor1);
@@ -128,41 +115,41 @@ function drawFace1(x, y, w, h, tilt_value, earring_value, pupilSize_value, faceC
 
     //Potara Earing
   if (earring_value == 3) {
-	  noStroke();
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(135, 152, 10, 10);
-	  fill(ZamasuPotEaringColor3);
-	  ellipse(135, 170, 30, 30);
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(135, 140, 17, 17);
+    noStroke();
+    fill(ZamasuPotEaringColor1);
+    ellipse(135, 152, 10, 10);
+    fill(ZamasuPotEaringColor3);
+    ellipse(135, 170, 30, 30);
+    fill(ZamasuPotEaringColor1);
+    ellipse(135, 140, 17, 17);
   }
 
   if (earring_value ==2) {
-  	  noStroke();
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(135, 152, 10, 10);
-	  fill(ZamasuPotEaringColor3);
-	  ellipse(135, 170, 30, 30);
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(135, 140, 17, 17);
+      noStroke();
+    fill(ZamasuPotEaringColor1);
+    ellipse(135, 152, 10, 10);
+    fill(ZamasuPotEaringColor3);
+    ellipse(135, 170, 30, 30);
+    fill(ZamasuPotEaringColor1);
+    ellipse(135, 140, 17, 17);
 
       noStroke();
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(-135, 152, 10, 10);
-	  fill(ZamasuPotEaringColor3);
-	  ellipse(-135, 170, 30, 30);
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(-135, 140, 17, 17);
+    fill(ZamasuPotEaringColor1);
+    ellipse(-135, 152, 10, 10);
+    fill(ZamasuPotEaringColor3);
+    ellipse(-135, 170, 30, 30);
+    fill(ZamasuPotEaringColor1);
+    ellipse(-135, 140, 17, 17);
   }
 
     if (earring_value == 1) {
       noStroke();
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(-135, 152, 10, 10);
-	  fill(ZamasuPotEaringColor3);
-	  ellipse(-135, 170, 30, 30);
-	  fill(ZamasuPotEaringColor1);
-	  ellipse(-135, 140, 17, 17);
+    fill(ZamasuPotEaringColor1);
+    ellipse(-135, 152, 10, 10);
+    fill(ZamasuPotEaringColor3);
+    ellipse(-135, 170, 30, 30);
+    fill(ZamasuPotEaringColor1);
+    ellipse(-135, 140, 17, 17);
   }
 
     //hair
@@ -179,220 +166,47 @@ function drawFace1(x, y, w, h, tilt_value, earring_value, pupilSize_value, faceC
   quad(0, -90, -18, -148, -150, -69, -90, -2);
   quad(-150, -69, -90, -2, -140, 120, -180, 100);
   triangle(-140, 120, -180, 100, -145, 190);
-
-  pop();
-}
-
-function drawFace2(x, y, w, h, faceCurve_value, eye_value, eyePos_value) {
-  //rectMode(CENTER);
-  push();
-  translate(x, y);
-  translate(-25,0);
-  noStroke();
-
-    // Head Shape
-  fill("rgb(127,156,115)");
-  rect(-100, -100, 250, 235, 10 + faceCurve_value);
-
-    // Eyes
-  fill("rgb(52,120,169)");
-  rect(-100, -10, 80, 60);
-  rect(70, -10, 80, 60);
-
-  fill("rgb(255,255,255)");
-  if (eye_value == 1) {
-  	rect(-100 + eyePos_value, -10, 18, 60);
-}
-
-  if (eye_value == 2) {
-  	rect(-100 + eyePos_value, -10, 18, 60);
-  	rect(70 + eyePos_value, -10, 18, 60);
-}
-
-  if (eye_value == 3) {
-  	rect(70 + eyePos_value, -10, 18, 60);
-}
-    // Mouth
-  fill("rgb(213,210,211)");
-  rect(-20, -10, 90, 145);
-
-  noFill();
-  stroke("black");
-  strokeWeight(0.2);
-  beginShape();
-  vertex(50, -10);
-  vertex(0, 20);
-  vertex(50, 50);
-  vertex(0, 80);
-  vertex(50, 110);
-  vertex(10, 135);
-  endShape();
-
-    // Mandibles
-  noStroke();
-  fill("rgb(240,185,109)");
-  ellipse(-58, 90, 50, 50);
-  ellipse(108, 90, 50, 50);
-
-  pop();
-
-}
-
-function drawFace3(x, y, w, h, scale_value, cheek_value, ear_value) {
-  push();
-  rectMode(CENTER);
-  translate(x, y);
-  scale(scale_value);
-
-    // Ears
-  fill("rgb(243,242,238)");
-  push();
-  rotate(ear_value);
-  push();
-  rotate(-10);
-  ellipse(70, 5, 85, 20);
-  rotate(10);
-  ellipse(70, 5, 70, 20);
-  pop();
-  pop();
-
-
-  push();
-  rotate(ear_value);
-  push();
-  rotate(10);
-  ellipse(-70, 5, 85, 20);
-  rotate(-10);
-  ellipse(-70, 5, 70, 20);
-  pop();
-  pop();
-
-    // Head shape
-  fill("rgb(253,252,248)");
-  ellipse(0, 0, 80, 75);
-
-    // Head Balls
-  fill("rgb(230,216,112)");
-  ellipse(32, -27, 18, 18);
-  ellipse(-32, -27, 18, 18);
-
-  fill("rgb(240,226,122)");
-  ellipse(24, -27, 20, 20);
-  ellipse(-24, -27, 20, 20);
-
-  fill("rgb(250,236,132)");
-  ellipse(9, -28, 22, 22);
-  ellipse(-9, -28, 22, 22);
-  
-
-    // Eyes
-  fill("rgb(39,42,37)");
-  ellipse(10, 0, 10, 16);
-  ellipse(-10, 0, 10, 16);
-
-  fill("white");
-  ellipse(10, -4, 4, 6);
-  ellipse(-10, -4, 4, 6);
-
-    // Cheeks
-  fill("rgb(214,107,107)");
-
-  if (cheek_value == 1) {
-	  ellipse(-23, 16, 17, 11);
-  }
-
-  if (cheek_value == 2) {
-	  ellipse(23, 16, 17, 11);
-	  ellipse(-23, 16, 17, 11);
-  }
-
-  if (cheek_value == 3) {
-  	ellipse(23, 16, 17, 11);
-  }
-
-    // Mouth
-  stroke("rgb(39,42,37)");
-  noFill();
-  beginShape();
-  curveVertex(120, -30);
-  curveVertex(0, 10);
-  curveVertex(5, 20);
-  curveVertex(10,25);
-  endShape();
-
   pop();
 }
 
 function draw () {
+  resetFocusedRandom(curRandomSeed);
+
   noStroke();
-
-  var mode = faceSelector.value();
-
-  if (mode != 'all') {
-    if (mode == '1') {
-      background(bg_color1);
-    }
-    else if (mode == '2') {
-      background(bg_color2);
-    }
-    else if (mode == '3') {
-      background(bg_color3);
-    }
-  }
-
-  var s1 = slider1.value();
-  var s2 = slider2.value();
-  var s3 = slider3.value();
-  var s4 = slider4.value();
-  var s5 = slider5.value();
+  background(bg_color1);
 
   // use same size / y_pos for all faces
-  var face_w = canvasWidth / 4;
-  var face_h = face_w;
-  var face_y = height / 2;
-  var face_x = width / 2;
+  // var face_w = canvasWidth / 4;
+  // var face_h = face_w;
+  // var face_y = height / 2;
+  // var face_x = width / 2;
 
-  if (mode == '1' || mode == 'all') {
-    // draw 1st face
-    fill(bg_color1);
-    rect(0, 0, width/3, height);
-    var tilt_value = map(s1, 0, 100, -10, 10);
-    var pupilSize_value = map(s3, 0, 100, 0, 10);
-    var earring_value = Math.floor(map(s2, 0, 100, 1, 3.99));
-    var faceColor_value = map(s4, 0, 100, 0, 1);
-    var jawline_value = map(s5,0, 100, 0,30)
-    if (mode == 'all') {
-      face_x = width / 6;
+  // draw 1st face
+  fill(bg_color1);
+
+  tilt_value = focusedRandom(-10,10);
+  earring_value = Math.floor(focusedRandom(1, 3.99));
+  pupilSize_value =  focusedRandom(0, 10);
+  faceColor_value = focusedRandom(0, 1);
+  jawline_value = focusedRandom(0,30);
+
+
+  var w = canvasWidth / 5;
+  var h = canvasHeight / 3;
+  for(var i=0; i<3; i++) {
+    for(var j=0; j<5; j++) {
+      var y = h/2 + h*i;
+      var x = w/2 + w*j;
+      tilt_value = focusedRandom(-10,10);
+      earring_value = Math.floor(focusedRandom(1, 3.99));
+      pupilSize_value =  focusedRandom(0, 10);
+      faceColor_value = focusedRandom(0, 1);
+      jawline_value = focusedRandom(0,30);
+      drawFace1(x, y, w, h, tilt_value, earring_value, pupilSize_value, faceColor_value, jawline_value);
     }
-
-    drawFace1(face_x, face_y, face_w, face_h, tilt_value, earring_value, pupilSize_value, faceColor_value, jawline_value);    
   }
 
-  if (mode == '2' || mode == 'all') {
-    // draw 2nd face
-    fill(bg_color2);
-    rect(width/3, 0, 2*width/3, height);
-    var faceCurve_value = map(s1, 0, 100, 0, 30);
-    var eye_value = Math.floor(map(s2, 0, 100, 1, 3.99));
-    var eyePos_value = map(s3, 0, 100, 0, 62);
-    if (mode == 'all') {
-      face_x = 3 * width / 6;
-    }
-    drawFace2(face_x, face_y, face_w, face_h, faceCurve_value, eye_value, eyePos_value);
-  }
-
-  if (mode == '3' || mode == 'all') {
-    // draw 3nd face
-    fill(bg_color3);
-    rect(2*width/3, 0, width, height);
-    var scale_value = map(s1, 0, 100, 0.8, 1.3);
-    var ear_value = map(s3, 0, 100, -5, 5);
-    var cheek_value = Math.floor(map(s2, 0, 100, 0, 4));
-    if (mode == 'all') {
-      face_x = 5 * width / 6;
-    }
-    drawFace3(face_x, face_y, face_w, face_h, scale_value, cheek_value, ear_value);
-  }
+  // drawFace1(face_x, face_y, face_w, face_h, tilt_value, eye_value, mouth_value);    
 }
 
 function keyTyped() {
