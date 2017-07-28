5579e20383247732c18ef4a11aef25c11127ed06
Hamzah Ali <thestabstab2@gmail.com>
2017-07-28 15:45:34 +1200

    Part 2 of project 1

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 ## PS1 MDDN 342 2017
 
-This is the final version of the part 1 exercise. The two characters I choose to create were Zamasu (on the left) from ‘Dragon Ball Super’, and Charjabug (on the right) from Pokemon.
+This README explains the three different paramaterized faces for part2. 
 
-Starting with Zamasu, the art style of ‘Dragon Ball Super’ naturally has a lot of sharp and straight lines with their characters. This results in a lot of complex shapes, but my strategy was to use a lot of quads and triangles to try capture the base essence of what makes the character. 
+In my case, in each of my faces I use 3 sliders. For the first face I used my previous solution but added controls for the tilt, mouth openess, and number of eyes.
 
-To begin with, I took a straight facing picture of him and went on to photoshop. I mapped out the head structure with basic shapes, and then tried to replicate it in the code. This repeated with each feature until I was complete. 
+In the second version I was mostly thinking about eyes and hair. For the eyes, there is a true/false variable which determines if the face is blinking as well as a continuous variable for shifting the eye left and right. The hair ranges from stubble to bushy.
 
-I also removed the stroke to achieve a cleaner look.
-
-Charjabug went through a similar process as Zamasu involving using photoshop to map out the shapes then replicating them in javascript. However as a face he was much simpler to create. primarily using rectangles.
-
-Here are links to each character for reference:
-
-Zamasu - http://www.adnradio.cl/images_remote/325/3256984_n_vir3.jpg?u=260833
-Charjabug - https://cdn.bulbagarden.net/upload/thumb/e/ec/737Charjabug.png/250px-737Charjabug.png
+In the third face I tried out a darker robot idea. This robot is always staring right at you and might have a wide or narrow head. His middle eye is a lazy eye.
--- sketch.js
+++ sketch.js
@@ -1,20 +1,51 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
+var slider1, slider2, slider3, slider4, slider5;
+var faceSelector;
 
 function setup () {
   // create the drawing canvas, save the canvas element
-  main_canvas = createCanvas(canvasWidth, canvasHeight);
-
-  // position each element on the page
+  var main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
+
+  // create sliders
+  slider1 = createSlider(0, 100, 50);
+  slider2 = createSlider(0, 100, 50);
+  slider3 = createSlider(0, 100, 50);
+  slider4 = createSlider(0, 100, 50);
+  slider5 = createSlider(0, 100, 50);
+
+  slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+  slider3.parent('slider3Container');
+  slider4.parent('slider4Container');
+  slider5.parent('slider5Container');
+
+  faceSelector = createSelect();
+  faceSelector.option('1');
+  faceSelector.option('2');
+  faceSelector.option('3');
+  faceSelector.option('all')
+  faceSelector.value('all');
+  faceSelector.parent('selector1Container');
 
   // rotation in degrees
   angleMode(DEGREES);
 }
 
 // global variables for colors
-var bg_color = "grey";
-var fg_color1 = "rgb(177,215,151)";
+var bg_color1 = [10,24,7];
+var bg_color2 = [17, 29, 34];
+var bg_color3 = [40, 40, 90];
+
+var fg_color1 = [151, 102, 52];
+var fg_color2 = [56, 91, 194];
+var fg_color3 = [206, 207, 180];
+
+var stroke_color1 = [95, 52, 8];
+var stroke_color2 = [210, 219, 189];
+var stroke_color3 = [50, 50, 50];
+
 var ZamasuFaceColor1 = "rgb(177,215,151)";
 var ZamasuFaceColor2 = "rgb(157,195,131)";
 var ZamasuHairColor1 = "rgb(232,236,236)";
@@ -22,36 +53,79 @@ var ZamasuHairColor2 = "rgb(169,176,175)";
 var ZamasuPotEaringColor1 = "rgb(243,255,84)";
 var ZamasuPotEaringColor2 = "rgb(145,157,0)";
 var ZamasuPotEaringColor3 = "rgb(18,223,42)";
-var fg_color2 = "#999999";
-
 var stroke_color = "black";
 var eyecolor1 = "rgb(158,160,160)";
 var eyecolor2 = "rgb(15,15,15)";
 
-function draw () {
-  // background color
-  background(bg_color);
+var ShinFaceColor1 = "rgb(233,207,243)";
+var ShinFaceColor2 = "rgb(213,187,223)";
 
-  // stroke color
-  
+var colorHair = [20, 20, 0];
 
-    // move to position1, rotate, draw "head" ellipse
-    // Creating the basic head stucture
+function drawFace1(x, y, w, h, tilt_value, earring_value, pupilSize_value, faceColor_value, jawline_value) {
   push();
+  translate(x, y);
+  rotate(tilt_value);
+  scale(0.5);
+
+  var c0 = color("blue");
+  var c1 = color(ZamasuFaceColor1);
+  var c2 = color(ShinFaceColor1);
+
+  var faceColorBlend = lerpColor(c1, c2, faceColor_value);
+
+  var c1 = color(ZamasuFaceColor2)
+  var c2 = color(ShinFaceColor2)
+  var faceColorBlend2 = lerpColor(c1, c2, faceColor_value);
+
+  // var extent = 0;
+  // if(h < w) {
+  //   extent = h / 2;
+  // }
+  // else {
+  //   extent = w / 2;
+  // }
+  // var scale = extent / 220.0;
+
+  // fill(fg_color1);
+  // ellipse(0, 0, 300 * scale, 400 * scale);
+
+  // // eyes
+  // if (eye_value === 1 || eye_value == 3) {
+  //   fill(bg_color1);
+  //   ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
+  //   fill(fg_color1);
+  //   ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
+  // }
+
+  // if (eye_value >= 2) {
+  //   fill(bg_color1);
+  //   ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
+  //   ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
+
+  //   fill(fg_color1);
+  //   ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
+  //   ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
+  // }
+
+  // // mouth
+  // fill(bg_color1);
+  // ellipse(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
+
+  //Ears
+  fill(faceColorBlend2);
+  quad(146.5, 40, 280, -20, 160, 135, 40, 145);
+  quad(-146.5, 40, -280, -20, -160, 135, -40, 145);
+
   noStroke();
-  translate(960/4, 500/2);
-  rotate(0);
-  fill(ZamasuFaceColor1);
+  fill(faceColorBlend);
   ellipse(0, 0, 300, 350);
     
   //stroke("black");
-  quad(0, 40, 146.5, 40, 100, 185, 0, 230);
-  quad(0, 40, -146.5, 40, -100, 185, 0, 230);
+  quad(0, 40, 146.5, 40, 85+jawline_value, 185, 0, 215+(jawline_value/2));
+  quad(0, 40, -146.5, 40, -85-jawline_value, 185, 0, 215+(jawline_value/2));
 
-    //Ears
-  fill(ZamasuFaceColor2);
-  quad(146.5, 40, 280, -20, 160, 135, 113, 145);
-  quad(-146.5, 40, -280, -20, -160, 135, -113, 145);
+  
 
     // eye shapes
   fill("white");
@@ -72,11 +146,11 @@ function draw () {
 
     //pupils
   fill(eyecolor2);
-  ellipse(59, 69, 6, 6);
-  ellipse(-59, 69, 6, 6);
+  ellipse(59, 69, 6 + pupilSize_value, 6 + pupilSize_value);
+  ellipse(-59, 69, 6 + pupilSize_value, 6 + pupilSize_value);
 
     //nose
-  fill(ZamasuFaceColor2);
+  fill(faceColorBlend2);
   triangle(5, 100, 20, 130, 0, 145);
   triangle(5, 130, -13, 130, 0, 145);
 
@@ -85,6 +159,45 @@ function draw () {
   triangle(-20, 165, -20, 168, -30, 162);
   quad(-20, 165, 20, 165, 20, 170, -20, 168);
   triangle(20, 165, 20, 170, 40, 158);
+
+    //Potara Earing
+  if (earring_value == 3) {
+	  noStroke();
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(135, 152, 10, 10);
+	  fill(ZamasuPotEaringColor3);
+	  ellipse(135, 170, 30, 30);
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(135, 140, 17, 17);
+  }
+
+  if (earring_value ==2) {
+  	  noStroke();
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(135, 152, 10, 10);
+	  fill(ZamasuPotEaringColor3);
+	  ellipse(135, 170, 30, 30);
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(135, 140, 17, 17);
+
+      noStroke();
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(-135, 152, 10, 10);
+	  fill(ZamasuPotEaringColor3);
+	  ellipse(-135, 170, 30, 30);
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(-135, 140, 17, 17);
+  }
+
+    if (earring_value == 1) {
+      noStroke();
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(-135, 152, 10, 10);
+	  fill(ZamasuPotEaringColor3);
+	  ellipse(-135, 170, 30, 30);
+	  fill(ZamasuPotEaringColor1);
+	  ellipse(-135, 140, 17, 17);
+  }
 
     //hair
   stroke(ZamasuHairColor1);
@@ -101,61 +214,320 @@ function draw () {
   quad(-150, -69, -90, -2, -140, 120, -180, 100);
   triangle(-140, 120, -180, 100, -145, 190);
 
-    //Potara Earing
-  noStroke();
-  fill(ZamasuPotEaringColor1);
-  ellipse(135, 152, 10, 10);
-  fill(ZamasuPotEaringColor3);
-  ellipse(135, 170, 30, 30);
-  fill(ZamasuPotEaringColor1);
-  ellipse(135, 140, 17, 17);
+
 
   pop();
+}
 
-  // move to position2, rotate, draw "head" ellipse
+function drawFace2(x, y, w, h, faceCurve_value, eye_value, eyePos_value) {
+  //rectMode(CENTER);
   push();
-  translate(3 * 960 / 4, 500 / 2);
+  translate(x, y);
+  translate(-25,0);
+
+  //var extent = 0;
+  //if(h < w) {
+  //  extent = h / 2;
+  //}
+  //else {
+  //  extent = w / 2;
+  //}
+  //var scale = extent / 220.0;
+
+  //stroke(stroke_color3);
+  //fill(fg_color3);
+  //ellipse(0, 0, 300 * scale, 400 * scale);
+
+  //// eyes. first check for blinking
+  //if(blink_value > 0) {
+  //  fill(bg_color3);
+  //  ellipse(-50 * scale, -80 * scale, 50 * scale, 2 * scale);
+  //  ellipse( 50 * scale, -80 * scale, 50 * scale, 2 * scale);
+  //}
+  //else {
+  //  fill(bg_color3);
+  //  ellipse(-50 * scale, -80 * scale, 50 * scale, 18 * scale);
+  //  ellipse( 50 * scale, -80 * scale, 50 * scale, 18 * scale);
+
+  //  fill(fg_color3);
+  //  ellipse((-50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
+  //  ellipse(( 50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
+  //}
+
+
+  //// mouth
+  //fill(bg_color3);
+  //ellipse(0 * scale, 70 * scale, 150 * scale, 20 * scale);
+
+  //// TODO: paramaterize hair
+  //var follicles = [
+  //  [346,138],
+  //  [391,120],
+  //  [391,67],
+  //  [439,76],
+  //  [463,42],
+  //  [487,18],
+  //  [481,101],
+  //  [520,102],
+  //  [520,78],
+  //  [533,54],
+  //  [560,108],
+  //  [580,76],
+  //  [596,124],
+  //  [618,124]
+  //];
+
+  //resetMatrix();
+  //fill(colorHair);
+  //var radius = hair_value * scale;
+  //for(var i=0; i<follicles.length; i++) {
+  //  ellipse(240+follicles[i][0]/2, 120 + (follicles[i][1]/2), radius, radius);
+  //}
+  //rectMode(CORNER);
+  //resetMatrix();
+
   noStroke();
 
-  // Head Shape
+    // Head Shape
   fill("rgb(127,156,115)");
-  rect(-100, -100, 250, 235, 20);
+  rect(-100, -100, 250, 235, 10 + faceCurve_value);
 
-  // Eyes
+    // Eyes
   fill("rgb(52,120,169)");
-  rect(-100,-10,80,60); 
-  rect(70,-10,80,60); 
+  rect(-100, -10, 80, 60);
+  rect(70, -10, 80, 60);
 
   fill("rgb(255,255,255)");
-  rect(-100,-10,18,60);
-  rect(70,-10,18,60);
+  if (eye_value == 1) {
+  	rect(-100 + eyePos_value, -10, 18, 60);
+}
 
-  // Mouth
+  if (eye_value == 2) {
+  	rect(-100 + eyePos_value, -10, 18, 60);
+  	rect(70 + eyePos_value, -10, 18, 60);
+}
+
+  if (eye_value == 3) {
+  	rect(70 + eyePos_value, -10, 18, 60);
+}
+    // Mouth
   fill("rgb(213,210,211)");
-  rect(-20,-10,90,145); 
+  rect(-20, -10, 90, 145);
 
   noFill();
   stroke("black");
   strokeWeight(0.2);
   beginShape();
-	vertex(50, -10);
-	vertex(0, 20);
-	vertex(50, 50);
-	vertex(0, 80);
-	vertex(50, 110);
-	vertex(10, 135);
+  vertex(50, -10);
+  vertex(0, 20);
+  vertex(50, 50);
+  vertex(0, 80);
+  vertex(50, 110);
+  vertex(10, 135);
   endShape();
 
-  // Mandibles
+    // Mandibles
   noStroke();
   fill("rgb(240,185,109)");
-  ellipse(-58,90,50,50);
-  ellipse(108,90,50,50);
-
-  // 
-  
+  ellipse(-58, 90, 50, 50);
+  ellipse(108, 90, 50, 50);
 
   pop();
+
+}
+
+function drawFace3(x, y, w, h, scale_value, cheek_value, ear_value) {
+  push();
+  rectMode(CENTER);
+  translate(x, y);
+  scale(scale_value);
+  // rotate(width_value);
+
+  //var extent = 0;
+  //if(h < w) {
+  //  extent = h / 2;
+  //}
+  //else {
+  //  extent = w / 2;
+  //}
+  //var scale = extent / 220.0;
+
+  //stroke(stroke_color2)
+  //fill(fg_color2);
+  //rect(0, 0, (300 + width_value) * scale, 400 * scale);
+
+  //// eyes
+  //if (eye_value === 1 || eye_value == 3) {
+  //  fill(bg_color2);
+  //  rect( 0, -80 * scale, 50 * scale, 30 * scale);
+  //  fill(fg_color2);
+  //  ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
+  //}
+
+  //if (eye_value >= 2) {
+  //  fill(bg_color2);
+  //  rect(-60 * scale, -80 * scale, 50 * scale, 30 * scale);
+  //  rect( 60 * scale, -80 * scale, 50 * scale, 30 * scale);
+
+  //  fill(fg_color2);
+  //  ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
+  //  ellipse( 60 * scale, -80 * scale, 20 * scale, 20 * scale);
+  //}
+
+  //// mouth
+  //fill(bg_color2);
+  //rect(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
+  //rectMode(CORNER);
+
+    // Ears
+  fill("rgb(243,242,238)");
+  push();
+  rotate(ear_value);
+  push();
+  rotate(-10);
+  ellipse(70, 5, 85, 20);
+  rotate(10);
+  ellipse(70, 5, 70, 20);
+  pop();
+  pop();
+
+
+  push();
+  rotate(ear_value);
+  push();
+  rotate(10);
+  ellipse(-70, 5, 85, 20);
+  rotate(-10);
+  ellipse(-70, 5, 70, 20);
+  pop();
+  pop();
+
+    // Head shape
+  fill("rgb(253,252,248)");
+  ellipse(0, 0, 80, 75);
+
+    // Head Balls
+  fill("rgb(230,216,112)");
+  ellipse(32, -27, 18, 18);
+  ellipse(-32, -27, 18, 18);
+
+  fill("rgb(240,226,122)");
+  ellipse(24, -27, 20, 20);
+  ellipse(-24, -27, 20, 20);
+
+  fill("rgb(250,236,132)");
+  ellipse(9, -28, 22, 22);
+  ellipse(-9, -28, 22, 22);
+  
+
+    // Eyes
+  fill("rgb(39,42,37)");
+  ellipse(10, 0, 10, 16);
+  ellipse(-10, 0, 10, 16);
+
+  fill("white");
+  ellipse(10, -4, 4, 6);
+  ellipse(-10, -4, 4, 6);
+
+    // Cheeks
+  fill("rgb(214,107,107)");
+
+  if (cheek_value == 1) {
+	  ellipse(-23, 16, 17, 11);
+  }
+
+  if (cheek_value == 2) {
+	  ellipse(23, 16, 17, 11);
+	  ellipse(-23, 16, 17, 11);
+  }
+
+  if (cheek_value == 3) {
+  	ellipse(23, 16, 17, 11);
+  }
+
+    // Mouth
+  stroke("rgb(39,42,37)");
+  noFill();
+  beginShape();
+  curveVertex(120, -30);
+  curveVertex(0, 10);
+  curveVertex(5, 20);
+  curveVertex(10,25);
+  endShape();
+
+  pop();
+}
+
+function draw () {
+  noStroke();
+
+  var mode = faceSelector.value();
+
+  if (mode != 'all') {
+    if (mode == '1') {
+      background(bg_color1);
+    }
+    else if (mode == '2') {
+      background(bg_color2);
+    }
+    else if (mode == '3') {
+      background(bg_color3);
+    }
+  }
+
+  var s1 = slider1.value();
+  var s2 = slider2.value();
+  var s3 = slider3.value();
+  var s4 = slider4.value();
+  var s5 = slider5.value();
+
+  // use same size / y_pos for all faces
+  var face_w = canvasWidth / 4;
+  var face_h = face_w;
+  var face_y = height / 2;
+  var face_x = width / 2;
+
+  if (mode == '1' || mode == 'all') {
+    // draw 1st face
+    fill(bg_color1);
+    rect(0, 0, width/3, height);
+    var tilt_value = map(s1, 0, 100, -10, 10);
+    var pupilSize_value = map(s3, 0, 100, 0, 10);
+    var earring_value = Math.floor(map(s2, 0, 100, 1, 3.99));
+    var faceColor_value = map(s4, 0, 100, 0, 1);
+    var jawline_value = map(s5,0, 100, 0,30)
+    if (mode == 'all') {
+      face_x = width / 6;
+    }
+
+    drawFace1(face_x, face_y, face_w, face_h, tilt_value, earring_value, pupilSize_value, faceColor_value, jawline_value);    
+  }
+
+  if (mode == '2' || mode == 'all') {
+    // draw 2nd face
+    fill(bg_color2);
+    rect(width/3, 0, 2*width/3, height);
+    var faceCurve_value = map(s1, 0, 100, 0, 30);
+    var eye_value = Math.floor(map(s2, 0, 100, 1, 3.99));
+    var eyePos_value = map(s3, 0, 100, 0, 62);
+    if (mode == 'all') {
+      face_x = 3 * width / 6;
+    }
+    drawFace2(face_x, face_y, face_w, face_h, faceCurve_value, eye_value, eyePos_value);
+  }
+
+  if (mode == '3' || mode == 'all') {
+    // draw 3nd face
+    fill(bg_color3);
+    rect(2*width/3, 0, width, height);
+    var scale_value = map(s1, 0, 100, 0.8, 1.3);
+    var ear_value = map(s3, 0, 100, -5, 5);
+    var cheek_value = Math.floor(map(s2, 0, 100, 0, 4));
+    if (mode == 'all') {
+      face_x = 5 * width / 6;
+    }
+    drawFace3(face_x, face_y, face_w, face_h, scale_value, cheek_value, ear_value);
+  }
 }
 
 function keyTyped() {
