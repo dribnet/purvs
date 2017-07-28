ad1f90641632ae24719ab5388e7dc209a80213fb
Robbie Carmichael <robbi3reg@gmail.com>
2017-07-28 13:19:45 +1200

    modified first of the three portraits using 5 sliders

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup () {
 }
 
 // global variables for colors
-var bg_color1 = [225, 206, 187];
+var bg_color1 = "#E02B3B";
 var bg_color2 = [47, 59, 64];
 var bg_color3 = [70, 70, 120];
 
@@ -48,10 +48,15 @@ var stroke_color3 = [50, 50, 50];
 
 var colorHair = [20, 20, 0];
 
-function drawFace1(x, y, w, h, tilt_value, eye_value, mouth_value) {
+var gollum_skin ="#CAB4A7";
+var gollum_eyeskin ="#96897F";
+var gollum_eyes = "#54C2FF";
+var gollum_tooth ="#FFF3C2";
+
+function drawFace1(x, y, w, h, brow_value, eye_value, ear_value, eye_scale, mouth_width) {
   push();
   translate(x, y);
-  rotate(tilt_value);
+  // rotate(tilt_value);
 
   var extent = 0;
   if(h < w) {
@@ -62,30 +67,65 @@ function drawFace1(x, y, w, h, tilt_value, eye_value, mouth_value) {
   }
   var scale = extent / 220.0;
 
-  fill(fg_color1);
-  ellipse(0, 0, 300 * scale, 400 * scale);
+  
 
-  // eyes
-  if (eye_value === 1 || eye_value == 3) {
-    fill(bg_color1);
-    ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
-    fill(fg_color1);
-    ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
 
-  if (eye_value >= 2) {
-    fill(bg_color1);
-    ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
-    ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
+  // ears
+  fill(gollum_skin);
+  ellipse(0, -20 * scale, 450 * scale, ear_value * scale);
+  fill(gollum_eyeskin);
+  ellipse(0, -20 * scale, 430 * scale, ear_value/1.5 * scale);
+  
+  // face
+  fill(gollum_skin);
+  ellipse(0, 0, 340 * scale, 420 * scale);
 
-    fill(fg_color1);
-    ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-    ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
-  }
+  // wrinkles
+  strokeWeight(3);
+  stroke(0);
+  // line(-80 * scale, -120 * scale, 80 * scale, -120 * scale);
+  // line(-70 * scale, -130 * scale, 70 * scale, -130 * scale);
+  // line(-70 * scale, -140 * scale, 70 * scale, -140 * scale);
+  line(-80 * scale, brow_value * scale, 80 * scale, brow_value * scale);
+  line(-70 * scale, (brow_value - 10) * scale, 70 * scale, (brow_value - 10) * scale);
+  line(-60 * scale, (brow_value - 20) * scale, 60 * scale, (brow_value - 20) * scale);
+
+  noStroke();
+
+  //eye bags
+  fill(gollum_eyeskin);
+  ellipse(-60 * scale, -30 * scale, 110 * scale, eye_scale * scale + 20);
+  ellipse( 60 * scale, -30 * scale, 110 * scale, eye_scale * scale + 20);
+
+  fill(255);
+  // draw two eyes
+  ellipse(-60 * scale, -40 * scale, 120 * scale, eye_scale * scale+20);
+  ellipse( 60 * scale, -40 * scale, 120 * scale, eye_scale * scale+20);
+
+  // set fill and multiple ellipses for eyeballs
+  fill(gollum_eyes);
+  ellipse((-60 + eye_value) * scale, -40 * scale, 90 * scale, eye_scale * scale);
+  ellipse(( 60 + eye_value) * scale, -40 * scale, 90 * scale, eye_scale * scale);
+  fill(0);
+  ellipse((-60 + eye_value) * scale, -40 * scale, 50 * scale, eye_scale * scale);
+  ellipse(( 60 + eye_value) * scale, -40 * scale, 50 * scale, eye_scale * scale);
+  fill(255);
+  ellipse((-60 + eye_value) * scale, -50 * scale, 5 * scale, 7.5 * scale);
+  ellipse(( 60 + eye_value) * scale, -50 * scale, 5 * scale, 7.5 * scale);
 
   // mouth
-  fill(bg_color1);
-  ellipse(0 * scale, 70 * scale, 150 * scale, mouth_value * scale);
+  rectMode(CENTER);
+  noStroke();
+  fill(0);
+  rect(0, 50, (10 + mouth_width) * scale, 10 * scale);
+
+  fill(gollum_tooth);
+    strokeWeight(1);
+    stroke(0);
+  triangle(5 * scale, 90 * scale, 15 * scale, 70 * scale, 25 * scale, 90 * scale);
+  triangle(-5 * scale, 90 * scale, -15 * scale, 70 * scale, -25 * scale, 90 * scale);
+    
+
   pop();
 }
 
@@ -232,13 +272,16 @@ function draw () {
     // draw 1st face
     fill(bg_color1);
     rect(0, 0, width/3, height);
-    var tilt_value = map(s1, 0, 100, -90, 90);
-    var mouth_value = map(s3, 0, 100, 0, 200);
-    var eye_value = Math.floor(map(s2, 0, 100, 1, 3));
+    // var tooth_value = map(s5, 0, 100, -90, 90);
+    var ear_value = map(s3, 0, 200, 100, 300);
+    var eye_scale = map(s1, 10, 200, 10, 200);
+    var mouth_width = map(s2, 50, 150, 100, 200);
+    var eye_value = map(s4, 0, 100, -15, 15);
+    var brow_value = (map(s5, 0, 100, -100, -140));
     if (mode == 'all') {
       face_x = width / 6;
     }
-    drawFace1(face_x, face_y, face_w, face_h, tilt_value, eye_value, mouth_value);    
+    drawFace1(face_x, face_y, face_w, face_h, brow_value, eye_value, ear_value, eye_scale, mouth_width);    
   }
 
   if (mode == '2' || mode == 'all') {
