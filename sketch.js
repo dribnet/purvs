0a02775f5d587247a204d5b98af17222ff4961c8
jessfreire <freirejess@myvuw.ac.nz>
2017-08-03 12:53:15 +1200

    completed

--- sketch.js
+++ sketch.js
@@ -3,14 +3,16 @@ var canvasHeight = 500;
 var button;
 var curRandomSeed;
 var pickface;
+var img;
+
 
 function setup () {
-
+  
   pickface = createSelect();
   
 
 
-  // create the drawing canvas, save the canvas element
+// create the drawing canvas, save the canvas element
   var main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
@@ -20,7 +22,7 @@ function setup () {
   randButton.mousePressed(changeRandomSeed);
   randButton.parent('selector1Container');
 
-  // rotation in degrees
+// rotation in degrees
   angleMode(DEGREES);
 }
 
@@ -28,7 +30,7 @@ function changeRandomSeed() {
   curRandomSeed = curRandomSeed + 1;
 }
 
-// global variables for colors
+// colors
 var bg_color1 = [225, 206, 187];
 var bg_color2 = [153,184,150];
 var bg_color3 = [161, 159, 181];
@@ -49,6 +51,7 @@ var ears = [141,89,88];
 var eyes = [0,0,0];
 
 function drawPig(x, y, w, h, tilt_value, eye_value, mouth_value) {
+
   push();
   translate(x, y);
   rotate(tilt_value);
@@ -62,9 +65,16 @@ function drawPig(x, y, w, h, tilt_value, eye_value, mouth_value) {
   }
   var scale = extent / 220.0;
 
+//shadow
+  fill(202,185,168); 
+  ellipse(10,10,150,150);
+  
 //face
   fill(fg_color1);
   ellipse(0, 0, 400 * scale, 400 * scale);
+  fill(184,116,114);
+  arc(10,-15,90,50,-800, PI*QUARTER_PI, OPEN);
+  arc(1,-6,150,140,-120, PI*QUARTER_PI, OPEN);
 
 //cheeks
   fill(cheeks);
@@ -72,19 +82,34 @@ function drawPig(x, y, w, h, tilt_value, eye_value, mouth_value) {
   ellipse(-60,-10,mouth_value * scale,10);
 
 
-  if (eye_value = 2) {
-
     fill(eyes);
     ellipse(-25, -20, 20, 5);
     ellipse( 25, -20, 20, 5);
-  }
+    fill(221,217,218);
+    ellipse(-25,-20,5,eye_value,20);
+    ellipse(25,-20,5,eye_value,10);
 
-  //ears
+//ears
   fill(ears);
+  stroke(eyes);
   triangle(10, -85, 70, -50, 35, -30);
   triangle(-10, -85, -70, -50, -35, -30);
 
-  // nose
+//mouth
+  if (freckle_value === 2 || freckle_value == 2) {
+   stroke(12,0,0);
+   line(-10, 45, 10, 45);
+   line(10, 45, 15, 40);
+ }
+
+   else{
+   stroke(12,0,0);
+   line(-10, 45, 10, 45);
+
+   } 
+   
+
+// nose
   fill(ears);
   ellipse(0 * scale, 30 * scale, 150 * scale, mouth_value * scale);
   fill(eyes);
@@ -108,7 +133,11 @@ function drawMonkey(x, y, w, h, hair_value, eye_value, blink_value,lefttilt_valu
   }
   var scale = extent / 220.0;
 
-  //ears
+//shadow
+  fill(202,185,168); 
+  ellipse(10,10,150,150);
+
+//ears
   fill(fg_color3);
   ellipse(-50, -50, 50, 50);
   ellipse(50, -50, 50, 50);
@@ -116,18 +145,21 @@ function drawMonkey(x, y, w, h, hair_value, eye_value, blink_value,lefttilt_valu
   ellipse(-50, -50, 30, 30);
   ellipse(50, -50, 30, 30);
 
-  //face
-  //stroke(stroke_color3);
+//face
   fill(fg_color3);
   ellipse(0, 0, 400 * scale, 400 * scale);
+  
+  fill(59,35,11);
+  arc(0,-15,90,50,15, PI*QUARTER_PI, OPEN);
+  arc(1,-6,150,140,920, PI*QUARTER_PI, OPEN);
 
-  //eyes color
+//eyes color
   fill(169,111,63);
   ellipse(-27, -25, 50, 50);
   ellipse(27, -25, 50, 50);
 
-  // eyes. first check for blinking
-  if(blink_value > 25) {
+// eyes. first check for blinking
+  if(blink_value > 85) {
     fill(eyes);
     ellipse(-50 * scale, -80 * scale, 50 * scale, 2 * scale);
     ellipse( 50 * scale, -80 * scale, 50 * scale, 2 * scale);
@@ -142,7 +174,7 @@ function drawMonkey(x, y, w, h, hair_value, eye_value, blink_value,lefttilt_valu
     ellipse(( 50 + eye_value) * scale, -80 * scale, 20 * scale, 20 * scale);
   }
 
-  // mouth and nose
+// mouth and nose
   fill(169,111,63);
   ellipse(0 * scale, 70 * scale, 150 * scale, 130 * scale);
   fill(eyes);
@@ -150,23 +182,7 @@ function drawMonkey(x, y, w, h, hair_value, eye_value, blink_value,lefttilt_valu
   ellipse(10, 20, eyes_value,5, 2);
   rect(0,35,30,2);
 
-
-  // TODO: paramaterize hair
   var follicles = [
-    // [346,138],
-    // [391,120],
-    // [391,67],
-    // [439,76],
-    // [463,42],
-    // [487,18],
-    // [481,101],
-    // [520,102],
-    // [520,78],
-    // [533,54],
-    // [560,108],
-    // [580,76],
-    // [596,124],
-    // [618,124]
   ];
 
   resetMatrix();
@@ -193,7 +209,11 @@ function drawPanda(x, y, w, h, width_value, freckle_value, mouth_value, tilt_val
   }
   var scale = extent / 220.0;
 
-  //ears
+//shadow
+  fill(202,185,168); 
+  ellipse(10,10,150,150);
+
+//ears
   fill(51,51,51);
   ellipse(-70, -30, 50, 50);
   ellipse(70, -30, 50, 50);
@@ -201,21 +221,23 @@ function drawPanda(x, y, w, h, width_value, freckle_value, mouth_value, tilt_val
   ellipse(-70, -30, 30, 30);
   ellipse(70, -30, 30, 30);
 
-
-  //face
+//face
   fill(220,216,217);
   ellipse(0, 0, 400 * scale, 400 * scale);
 
-    //eyes
+  fill(203,199,200);
+  arc(1,-6,150,140,920, PI*QUARTER_PI, OPEN);
+
+//eyes
   fill(51,51,51);
   ellipse(-27, -25, 50, 50);
   ellipse(27, -25, 50, 50);
 
-  //nose
+//nose
   fill(51,51,51);
   triangle(-15, 10, 15, 10, 0, 30);
 
-  //mouth
+//mouth
    stroke(12,0,0);
    line(-0.5, 28, -0.5, 40);
    fill(eyes);
@@ -247,10 +269,10 @@ function drawPanda(x, y, w, h, width_value, freckle_value, mouth_value, tilt_val
     ellipse(-10,25,2,2);
   }
 
-  //eyes
+//eyes
   fill(220,216,217);
-  ellipse(-30,-25,20, eyes_value,5);
-  ellipse(30,-25,20, eyes_value,5);
+  ellipse(-30,-25,20, eyes_value,10);
+  ellipse(30,-25,20, eyes_value,10);
   ellipse(-30, -25, 2, 2);
   ellipse(30, -25, 2, 2);
 
@@ -305,6 +327,40 @@ function draw () {
   noStroke();
   background(bg_color1);
 
+//shadow
+  fill(202,185,168);
+  rect(0, 35, 960, 10);
+  rect(35, 0, 10, 500);
+  
+  fill(57,28,20);
+  rect(0, 0, 960, 30);
+  rect(0, 0, 30, 500);
+  rect(930, 0, 30, 500);
+  rect(0, 470, 960, 30);
+
+  fill(255,255,255);
+  rect(0, 30, 960, 10);
+  rect(30, 0, 10, 500);
+  rect(920, 30, 10, 500);
+  rect(0, 460, 920, 10);
+  
+  fill(250,220,134);
+  //top left
+  rect(0, 0, 100, 30);
+  rect(0, 0, 30, 100);
+  
+  //top right
+  rect(930, 0, 30, 100);
+  rect(860,0,80,30);
+  
+  //bottom left
+  rect(0, 470, 100, 30);
+  rect(0, 400, 30, 100);
+
+  //bottom right
+  rect(930, 400, 30, 100);
+  rect(860,470,80,30);
+  
   //draw faces
 
   fill(bg_color1);
@@ -319,9 +375,9 @@ function draw () {
       var x = w*1 + w*j;
       tilt_value = focusedRandom(0, 70);
       lefttilt_value = focusedRandom(0,-70);
-      eye_value = int(focusedRandom(1, 6));
+      eye_value = int(focusedRandom(1, 8));
       mouth_value = focusedRandom(60, 140);
-      eyes_value = focusedRandom(5,20);
+      eyes_value = focusedRandom(2,30);
       blink_value = focusedRandom(0,50);
       freckle_value = getRandomNumberOfFreckles();
       face(x, y, w, h, tilt_value, eye_value, mouth_value,lefttilt_value, eyes_value,freckle_value, blink_value);
@@ -329,6 +385,8 @@ function draw () {
   }
 
 }
+
+
 
 function keyTyped() {
   if (key == '!') {
