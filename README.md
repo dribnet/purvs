d8134dee6656dec941dacad31a974cc80bd62217
Henri Spencer <henrimspence@gmail.com>
2017-08-10 12:07:05 +1200

    I have brought the code from my last project across to start getting everything together for ps2.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## PS1 MDDN 342 2017
 
 
-Final README.
+##Final README.
 
-This should include a summary of all of the work leading up to the final version (which I am omitting in this example), so it should be longer than the other READMEs.
+##This should include a summary of all of the work leading up to the final version (which I am omitting in ##this example), so it should be longer than the other READMEs.
 
-In my final version I have used a shifted grid layout with some jitter.
+##In my final version I have used a shifted grid layout with some jitter.
+
+I have brought the code from my last project across to start getting everything together for ps2.
--- face.js
+++ face.js
@@ -5,20 +5,26 @@ function Face() {
   // these are state variables for a face
   // (your variables may be different)
   this.tilt_value = 0;
-  this.eye_value = 2;
+  this.eye_value = 3;
+  this.horn_value = 3;
   this.mouth_value = 0;
+  this.width_value = 2;
 
   // other variables can be in here too
   // these control the colors used
-  this.bg_color = [225, 206, 187];
-  this.fg_color = [151, 102, 52];
-  this.stroke_color = [95, 52, 8];
+  var minC = 100;
+  var maxC = 255;
+  this.bg_color = [92, 153, 149];
+  this.fg_color = [13, 204, 193];
+  this.fg_color2 = [255, 255, 255,];
+  this.stroke_color = [204, 21, 174];
 
   /*
    * Draw a face centered at x,y with an allowed
    * width and height of w,h.
    */  
-  this.draw1 = function(x, y, w, h) {
+
+this.draw1 = function(x, y, w, h) {
     // Uncomment to see drawing area
     // fill(255);
     // stroke(0);
@@ -41,32 +47,70 @@ function Face() {
 
     // head
     stroke(this.stroke_color);
+    this.fg_color[2] = Math.random() * (maxC - minC) + minC;
     fill(this.fg_color);
-    ellipse(0, 0, 300 * scale, 400 * scale);
-    noStroke();
 
-    // eyes
-    if (this.eye_value === 1 || this.eye_value == 3) {
-      fill(this.bg_color);
-      ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
-      fill(this.fg_color);
-      ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
+    rectMode(CENTER);
+    //rect(0, 0, 50, 50);
+    quad(-20 * scale*10, -20, 20, -20, 20, 40 + this.mouth_value, -20, 40 - this.tilt_value/2);
+  // eyes
 
-    if (this.eye_value >= 2) {
-      fill(this.bg_color);
-      ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
-      ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
+  if (this.eye_value == 1) {
+    fill(this.fg_color2);
+    ellipse( 0 + mouseX/x, -80 * scale + mouseY/y, 120 * scale, 120 * scale);
+    fill(this.fg_color);
+    ellipse(0 + mouseX/x, -80 * scale + mouseY/y, 40 * scale, 40 * scale);
+  }
 
-      fill(this.fg_color);
-      ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-      ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
+  if (this.eye_value == 2) {
+    fill(this.fg_color2);
+    rect(-60 * scale + mouseX/x/2, -80 * scale + mouseY/y, 80 * scale, 80 * scale);
+    rect( 60 * scale + mouseX/x/2, -80 * scale + mouseY/y, 80 * scale, 80 * scale);
 
-    // mouth
+    fill(this.fg_color);
+    ellipse( -10  + mouseX/x/2, -15  + mouseY/y, 40 * scale, 20 * scale);
+    ellipse( 10 + mouseX/x/2, -15 + mouseY/y, 40 * scale, 20 * scale);
+  }
+
+  if (this.eye_value == 3) {
     fill(this.bg_color);
-    ellipse(0 * scale, 70 * scale, 150 * scale, this.mouth_value * scale);
-    pop();
+    noStroke();
+    rect(-60 * scale, -60 * scale, 80 * scale, 20 * scale);
+    rect( 60 * scale, -60 * scale, 80 * scale, 20 * scale);
+    stroke(this.stroke_color);
+  }
+
+
+// horns two
+  if (this.horn_value == 2) {
+    fill(this.fg_color2);
+    stroke(this.stroke_color);
+    triangle(-25, -30, -15, -40 - this.tilt_value, -5, -30);
+    triangle(5, -30, 15, -40 - this.tilt_value, 25, -30);
+	}
+	else if(this.horn_value == 3){
+  	fill(this.fg_color);
+    triangle(-20, -30, 0, -40 - this.tilt_value, 20, -30);
+  }
+   
+
+
+  //Eyebrows
+  rect(-15, -25 - this.mouth_value/8, 50 * scale, 10 * scale);
+  rect(15, -25 - this.mouth_value/12, 50 * scale, 10 * scale);
+
+
+  // mouth
+  fill(this.bg_color);
+  noStroke();
+  rect(0 * scale, 70 * scale, 150 * scale, this.mouth_value * scale * 2);
+
+
+  //Teeth
+  fill (this.fg_color2);
+  rect(-10, 10, (15 + this.width_value) * scale, (30 + this.width_value) * scale);
+  rect(10, 10, (15 + this.width_value) * scale, (30 + this.width_value) * scale);
+  pop();
   }
 
   /*
@@ -74,6 +118,10 @@ function Face() {
    *    chin, right_eye, left_eye, right_eyebrow, left_eyebrow
    *    bottom_lip, top_lip, nose_tip, nose_bridge, 
    */  
+
+
+
+  
   this.draw2 = function(positions) {
     var nose_pos = average_point(positions.nose_bridge);
     var eye1_pos = average_point(positions.left_eye);
@@ -135,6 +183,7 @@ function Face() {
     this.eye_value = getRandomNumberOfEyes();
     this.tilt_value = focusedRandom(-70, 90, 8);
     this.mouth_value = focusedRandom(0, 50, 4, 1);
+    this.horn_value = getRandomNumberOfHorns();
   }
 }
 
@@ -142,10 +191,10 @@ function Face() {
 
 function getRandomNumberOfEyes() {
   random_result = focusedRandom(0, 100);
-  if(random_result < 8) {
+  if(random_result < 20) {
     return 1;
   }
-  else if(random_result < 12) {
+  else if(random_result < 40) {
     return 3;
   }
   else {
@@ -153,6 +202,18 @@ function getRandomNumberOfEyes() {
   }
 }
 
+function getRandomNumberOfHorns() {
+  random_result = focusedRandom(0, 100);
+  if(random_result < 20) {
+    return 1;
+  }
+  else if(random_result < 40) {
+    return 3;
+  }
+  else {
+    return 2;
+  }
+}
 // given a point, return the average
 function average_point(list) {
   var sum_x = 0;
