c634ab7a8f5df776abf818206478fb8c459154a5
Oliver Johnston <johnstoliv2@myvuw.ac.nz>
2017-08-10 13:10:10 +1200

    Second commit, plugged my robot in

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 ## PS1 MDDN 342 2017
 
 
-Started with git for ps2.
-
-First commit, no changed work.
+Plugged my robot faces into the grid. Included the random variables to create a different composition every click.
--- face.js
+++ face.js
@@ -20,11 +20,11 @@ function Face() {
    */  
   this.draw1 = function(x, y, w, h) {
     // Uncomment to see drawing area
-    // fill(255);
-    // stroke(0);
-    // rect(x-w/2, y-h/2, w, h);
-    // fill(0)
-    // ellipse(x, y, w, h);
+     //fill(255);
+     //stroke(0);
+     //rect(x-w/2, y-h/2, w, h);
+     //fill(0)
+     //ellipse(x, y, w, h);
 
     push();
     translate(x, y);
@@ -38,35 +38,211 @@ function Face() {
       extent = w / 2;
     }
     var scale = extent / 220.0;
+    var eye_value = focusedRandom(0, 3);
+    var ant = focusedRandom(0, 3);
+    var c1 = focusedRandom(153, 168);
+    var c2 = focusedRandom(153, 15);
+    var c3 = focusedRandom(153, 15);
+    var eye_size1 = focusedRandom(-5, 5);
+    var eye_size2 = focusedRandom(-10, 10);
+    var eye_size3 = focusedRandom(-30, 30);
+    var rot = focusedRandom(0, 3);
 
-    // head
-    stroke(this.stroke_color);
-    fill(this.fg_color);
-    ellipse(0, 0, 300 * scale, 400 * scale);
-    noStroke();
+    stroke(10);
 
-    // eyes
-    if (this.eye_value === 1 || this.eye_value == 3) {
-      fill(this.bg_color);
-      ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
-      fill(this.fg_color);
-      ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
+  fill(c1, c2, c3);
+  rect(-100 * scale, -100 * scale, 200 * scale, 200 * scale);
 
-    if (this.eye_value >= 2) {
-      fill(this.bg_color);
-      ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
-      ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
+  fill(c1 - 50, c2 - 50, c3 -50);
 
-      fill(this.fg_color);
-      ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-      ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
 
-    // mouth
-    fill(this.bg_color);
-    ellipse(0 * scale, 70 * scale, 150 * scale, this.mouth_value * scale);
+  beginShape();
+  vertex(-120* scale, -120* scale);
+  vertex(80* scale, -120* scale);
+  vertex(100* scale, -100* scale);
+  vertex(-100* scale, -100* scale);
+  vertex(-120* scale, -120* scale);
+  endShape();
+
+  beginShape();
+  vertex(-120* scale, -120* scale);
+  vertex(-120* scale, 80* scale);
+  vertex(-100* scale, 100* scale);
+  vertex(-100* scale, -100* scale);
+  vertex(-120* scale, -120* scale);
+  endShape();
+
+  fill(c1, c2, c3);
+
+
+  //antenna
+
+
+    //antenna1
+
+    
+
+    if(ant <= 1){
+    stroke(5);
+
+    beginShape();
+    vertex(-15* scale, -110* scale);
+    vertex(-7* scale, -165* scale);
+    vertex(7* scale, -165* scale);
+    vertex(15* scale, -110* scale);
+    vertex(-15* scale, -110* scale)
+    endShape();
+
+    fill(239, 206, 59);
+    ellipse(0* scale, -170* scale, 30* scale, 30* scale)
+
+  }
+
+  //antenna2
+
+  if(ant > 1 && ant < 2){
+    stroke(5);
+
+    push();
+    stroke(239, 235, 16);
+    beginShape();
+    noFill();
+    vertex(-35* scale, -180* scale);
+    vertex(-30* scale, -170* scale);
+    vertex(-25* scale, -190* scale);
+    vertex(-20* scale, -170* scale);
+    vertex(-15* scale, -190* scale);
+    vertex(-10* scale, -170* scale);
+    vertex(-5* scale, -190* scale);
+    vertex(0* scale, -170* scale);
+    vertex(5* scale, -190* scale);
+    vertex(10* scale, -170* scale);
+    vertex(15* scale, -190* scale);
+    vertex(20* scale, -170* scale);
+    vertex(25* scale, -190* scale);
+    vertex(30* scale, -170* scale);
+    vertex(35* scale, -180* scale);
+    endShape();
     pop();
+
+    beginShape();
+    vertex(-15* scale, -110* scale);
+    vertex(-7* scale, -135* scale);
+    vertex(7* scale, -135* scale);
+    vertex(15* scale, -110* scale);
+    vertex(-15* scale, -110* scale)
+    endShape();
+
+    beginShape();
+    vertex(-15* scale, -110* scale);
+    vertex(-55* scale, -140* scale);
+    vertex(-35* scale, -180* scale);
+    vertex(-35* scale, -145* scale);
+    vertex(-7* scale, -135* scale);
+    endShape();
+
+    beginShape();
+    vertex(15* scale, -110* scale);
+    vertex(55* scale, -140* scale);
+    vertex(35* scale, -180* scale);
+    vertex(35* scale, -145* scale);
+    vertex(7* scale, -135* scale);
+    endShape();
+
+    
+
+  }
+
+  if(ant >= 2) {
+    stroke(5);
+
+    beginShape();
+    vertex(-15* scale, -110* scale);
+    vertex(-7* scale, -185* scale);
+    vertex(7* scale, -185* scale);
+    vertex(15* scale, -110* scale);
+    vertex(-15* scale, -110* scale)
+    endShape();
+
+    beginShape();
+    vertex(-9* scale, -165* scale);
+    vertex(-47* scale, -205* scale);
+    vertex(-27* scale, -245* scale);
+    vertex(-27* scale, -210* scale);
+    vertex(-7* scale, -185* scale);
+    endShape();
+
+    beginShape();
+    vertex(9* scale, -165* scale);
+    vertex(47* scale, -205* scale);
+    vertex(27* scale, -245* scale);
+    vertex(27* scale, -210* scale);
+    vertex(7* scale, -185* scale);
+    endShape();
+
+
+
+  }
+
+
+  //mouth
+
+  fill(255, 255, 255);
+  rectMode(CENTER);
+    rect(0* scale, 40* scale, 100* scale, 30* scale);
+    fill(239, 206, 59);
+    rect(-36* scale, 40* scale, 23* scale, 26* scale);
+    rect(-12* scale, 40* scale, 23* scale, 26* scale);
+    rect(12* scale, 40* scale, 23* scale, 26* scale);
+    rect(36* scale, 40* scale, 23* scale, 26* scale);
+
+
+    //eyes
+
+    //eyes1
+
+
+
+    if(eye_value <= 1){
+
+    fill(0, 0, 0);
+
+    ellipse(-35* scale, -35* scale, (30 + eye_size1)* scale, (30 + eye_size1)* scale);
+    ellipse(35* scale, -35* scale, (30 + eye_size1)* scale, (30 + eye_size1)* scale);
+
+    fill(255, 255, 255);
+    ellipse(-30* scale, -45* scale, (5 + eye_size1)* scale, (5 + eye_size1)* scale);
+    ellipse(40* scale, -45* scale, (5 + eye_size1)* scale, (5 + eye_size1)* scale);
+
+  }
+
+    //eyes2
+
+    if(eye_value > 1 && eye_value < 2){
+
+    fill(0, 0, 0);
+
+    ellipse(0* scale, -35* scale, (70 + eye_size2)* scale, (70 + eye_size2)* scale);
+
+    fill(255, 255, 255);
+
+    ellipse(0* scale, -35* scale, (15 + eye_size2)* scale, (15 + eye_size2)* scale);
+  }
+
+  //eyes3
+
+  if(eye_value >= 2){
+
+    fill(255, 255, 255);
+    ellipse(0* scale, -40* scale, (150 + eye_size3)* scale, (30 + (eye_size3 / 5))* scale);
+
+    fill(0, 0, 0);
+    ellipse(0* scale, -40* scale, (140 + eye_size3)* scale, (25 + (eye_size3 / 5))* scale);
+
+
+  }
+  
+  pop();
   }
 
   /*
