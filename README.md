ba1cfac792bc27fed923cd70c621b14cb2c0c3e7
Robbie Carmichael <robbi3reg@gmail.com>
2017-08-17 14:18:59 +1200

    updated face.js with my own code from ps1 as well as some small modifications

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-## PS1 MDDN 342 2017
+## PS2 MDDN 342 2017
 
+PART 1:
 
-Final README.
+I have transferred my design from PS1 over to the new code, but have also made a few small adjustments based on the feedback from my peers.
 
-This should include a summary of all of the work leading up to the final version (which I am omitting in this example), so it should be longer than the other READMEs.
+A lot of the feedback concerned either distribution or the face not looking enough like Gollum's. In addition, the lack of variance in the randomness - this in particular I have tried to address.
 
-In my final version I have used a shifted grid layout with some jitter.
+First, I removed the bias to look to the right. This has resulted in a greater variety of eyesight directions. 
+
+Second, I slightly increased the size of the mouth so that it is easier to notice the different widths. 
+
+Last of all, I reduced the stroke weight of the eyebrows so that you could see the individual strokes at their smaller scale. There was some criticism around the brows falling down below the eyes. However, this was intentional in the first place in order to achieve the exaggerated cartoon eye bulge effect. 
+
--- face.js
+++ face.js
@@ -4,15 +4,24 @@
 function Face() {
   // these are state variables for a face
   // (your variables may be different)
-  this.tilt_value = 0;
-  this.eye_value = 2;
-  this.mouth_value = 0;
+  this.brow_value = -120;
+  this.eye_value = 5;
+  this.ear_value = 155;
+  this.eye_scale = 100;
+  this.mouth_width = 125;
 
   // other variables can be in here too
   // these control the colors used
-  this.bg_color = [225, 206, 187];
-  this.fg_color = [151, 102, 52];
-  this.stroke_color = [95, 52, 8];
+  // this.bg_color = [225, 206, 187];
+  // this.fg_color = [151, 102, 52];
+  // this.stroke_color = [95, 52, 8];
+
+  this.bg_color = "#E02B3B";
+  this.fg_color = "#E02B3B";
+  this.gollum_skin ="#CAB4A7";
+  this.gollum_eyeskin ="#96897F";
+  this.gollum_eyes = "#54C2FF";
+  this.gollum_tooth ="#FFF3C2";
 
   /*
    * Draw a face centered at x,y with an allowed
@@ -26,49 +35,79 @@ function Face() {
     // fill(0)
     // ellipse(x, y, w, h);
 
-    push();
-    translate(x, y);
-    rotate(this.tilt_value);
+  push();
+  translate(x, y);
+  
 
-    var extent = 0;
-    if(h < w) {
-      extent = h / 2;
-    }
-    else {
-      extent = w / 2;
-    }
-    var scale = extent / 220.0;
-
-    // head
-    stroke(this.stroke_color);
-    fill(this.fg_color);
-    ellipse(0, 0, 300 * scale, 400 * scale);
-    noStroke();
-
-    // eyes
-    if (this.eye_value === 1 || this.eye_value == 3) {
-      fill(this.bg_color);
-      ellipse( 0, -80 * scale, 50 * scale, 30 * scale);
-      fill(this.fg_color);
-      ellipse(-10 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
-
-    if (this.eye_value >= 2) {
-      fill(this.bg_color);
-      ellipse(-50 * scale, -80 * scale, 50 * scale, 30 * scale);
-      ellipse( 50 * scale, -80 * scale, 50 * scale, 30 * scale);
-
-      fill(this.fg_color);
-      ellipse(-60 * scale, -80 * scale, 20 * scale, 20 * scale);
-      ellipse( 40 * scale, -80 * scale, 20 * scale, 20 * scale);
-    }
-
-    // mouth
-    fill(this.bg_color);
-    ellipse(0 * scale, 70 * scale, 150 * scale, this.mouth_value * scale);
-    pop();
+  var extent = 0;
+  if(h < w) {
+    extent = h / 2;
   }
+  else {
+   extent = w / 2;
+  }
+  var scale = extent / 220.0;
+  
+  // ears
 
+  fill(this.gollum_skin);
+  ellipse(0, -20 * scale, 450 * scale, this.ear_value * scale);
+  fill(this.gollum_eyeskin);
+  ellipse(0, -20 * scale, 430 * scale, this.ear_value/1.5 * scale);
+  
+  // face
+  fill(this.gollum_skin);
+  ellipse(0, 0, 340 * scale, 420 * scale);
+
+  // wrinkles
+  strokeWeight(1.25);
+  stroke(0);
+  // line(-80 * scale, -120 * scale, 80 * scale, -120 * scale);
+  // line(-70 * scale, -130 * scale, 70 * scale, -130 * scale);
+  // line(-70 * scale, -140 * scale, 70 * scale, -140 * scale);
+  line(-80 * scale, this.brow_value * scale, 80 * scale, this.brow_value * scale);
+  line(-70 * scale, (this.brow_value - 10) * scale, 70 * scale, (this.brow_value - 10) * scale);
+  line(-60 * scale, (this.brow_value - 20) * scale, 60 * scale, (this.brow_value - 20) * scale);
+
+  noStroke();
+
+  //eye bags
+  fill(this.gollum_eyeskin);
+  ellipse(-60 * scale, -30 * scale, 110 * scale, this.eye_scale * scale + 20);
+  ellipse( 60 * scale, -30 * scale, 110 * scale, this.eye_scale * scale + 20);
+
+  fill(255);
+  // draw two eyes
+  ellipse(-60 * scale, -40 * scale, 120 * scale, this.eye_scale * scale+20);
+  ellipse( 60 * scale, -40 * scale, 120 * scale, this.eye_scale * scale+20);
+
+  // set fill and multiple ellipses for eyeballs
+  fill(this.gollum_eyes);
+  ellipse((-60 + this.eye_value) * scale, -40 * scale, 90 * scale, this.eye_scale * scale);
+  ellipse(( 60 + this.eye_value) * scale, -40 * scale, 90 * scale, this.eye_scale * scale);
+  fill(0);
+  ellipse((-60 + this.eye_value) * scale, -40 * scale, 50 * scale, this.eye_scale * scale);
+  ellipse(( 60 + this.eye_value) * scale, -40 * scale, 50 * scale, this.eye_scale * scale);
+  fill(255);
+  ellipse((-60 + this.eye_value) * scale, -50 * scale, 5 * scale, 7.5 * scale);
+  ellipse(( 60 + this.eye_value) * scale, -50 * scale, 5 * scale, 7.5 * scale);
+
+  // mouth
+  rectMode(CENTER);
+  noStroke();
+  fill(0);
+  rect(0, 110 * scale, (10 + this.mouth_width) * scale, 15 * scale);
+
+  fill(this.gollum_tooth);
+  strokeWeight(1);
+  stroke(0);
+  triangle(5 * scale, 110 * scale, 15 * scale, 90 * scale, 25 * scale, 110 * scale);
+  triangle(-5 * scale, 110 * scale, -15 * scale, 90 * scale, -25 * scale, 110 * scale);
+  
+  fill(this.bg_color);
+
+  pop();
+}
   /*
    * Draw a face with position lists that include:
    *    chin, right_eye, left_eye, right_eyebrow, left_eyebrow
@@ -95,15 +134,17 @@ function Face() {
     }
     var scale = extent / 220.0;
 
-    // Uncomment to see drawing area
-    // fill(255);
-    // stroke(0);
-    // rect(x-w/2, y-h/2, w, h);
-    // fill(0)
-    // ellipse(x, y, w, h);
+//     // Uncomment to see drawing area
+//     // fill(255);
+//     // stroke(0);
+//     // rect(x-w/2, y-h/2, w, h);
+//     // fill(0)
+//     // ellipse(x, y, w, h);
 
     push();
     translate(x, y);
+
+
     rotate(this.tilt_value);
 
     // head
@@ -128,13 +169,18 @@ function Face() {
     ellipse(eye2_pos[0], eye2_pos[1], 20 * scale, 20 * scale);
   }
 
-  /*
-   * Update internal state variables to a random state.
-   */  
+//   /*
+//    * Update internal state variables to a random state.
+//    */  
   this.randomize = function(values, size) {
-    this.eye_value = getRandomNumberOfEyes();
-    this.tilt_value = focusedRandom(-70, 90, 8);
-    this.mouth_value = focusedRandom(0, 50, 4, 1);
+    // this.eye_value = getRandomNumberOfEyes();
+    // this.tilt_value = focusedRandom(-70, 90, 8);
+    // this.mouth_value = focusedRandom(0, 50, 4, 1);
+    this.brow_value = focusedRandom(-90, -150, 1, -130);
+    this.eye_value = focusedRandom(-15, 15);
+    this.ear_value = focusedRandom(50, 250);
+    this.eye_scale = focusedRandom(10, 150, 1, 100);
+    this.mouth_width = focusedRandom(60, 200);
   }
 }
 
