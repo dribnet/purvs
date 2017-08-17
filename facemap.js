2e9657fa1ffa646560bb13cab1cf14f174f01ee4
Guest User <Guest@b-05-05-lab.mddn.vuw.ac.nz>
2017-08-17 14:55:53 +1200

    Updated to part 4  Please enter the commit message for your changes. Lines starting

--- facemap.js
+++ facemap.js
@@ -21,54 +21,60 @@ var stroke_color2 = ["#D492CD"];
 var stroke_color3 = ["#CC9378"];
 
 function FaceMap() {
-  this.eyebrowSkew = 0;
-  this.colorChoice = 1;
+	this.eyebrowSkew = 0;
+	this.colorChoice = 1;
+	this.bottom_lipE = 0.5;
+	this.squishY = 1;
+	this.squishX = 1;
 
-  this.draw = function(positions) {
-    var eyeSkew = map(this.eyebrowSkew, 0, 100, -10, 10);
-    var chosenColor = int(map(this.colorChoice, 0, 100, 1, 3));
+	this.draw = function(positions) {
+		var eyeSkew = map(this.eyebrowSkew, 0, 100, -10, 10);
+		var chosenColor = int(map(this.colorChoice, 0, 100, 1, 3));
+		var bottomExist = map(this.bottom_lipE, 0, 100, 0, 1);
+		var sqshY = map(this.squishY, 0, 100, 0.5, 1);
+		var sqshX = map(this.squishX, 0, 100, 0.5, 1.3);
   /*
    * Draw a face with position lists that include:
    *    chin, right_eye, left_eye, right_eyebrow, left_eyebrow
    *    bottom_lip, top_lip, nose_tip, nose_bridge, 
    */  
    if (chosenColor == 1) {
-    var foreground = bakg_color1;
-    var strakC = stroke_color1;
-    var bakg = fg_color1;
-  }
+   	var foreground = bakg_color1;
+   	var strakC = stroke_color1;
+   	var bakg = fg_color1;
+   }
 
-  if (chosenColor == 2) {
-    var foreground = fg_color2;
-    var strakC = stroke_color2;
-    var bakg = bakg_color2;
-  }
+   if (chosenColor == 2) {
+   	var foreground = fg_color2;
+   	var strakC = stroke_color2;
+   	var bakg = bakg_color2;
+   }
 
-  if (chosenColor == 3) {
-    var foreground = fg_color3;
-    var strakC = stroke_color3;
-    var bakg = bakg_color3;
-  }
-  var nose_pos = average_point(positions.nose_bridge);
-  var eye1_pos = average_point(positions.left_eye);
-  var eye2_pos = average_point(positions.right_eye);
-  var half_height = positions.chin[7][1] - nose_pos[1];
-  var face_width = positions.chin[positions.chin.length-1][0] - positions.chin[0][0];
+   if (chosenColor == 3) {
+   	var foreground = fg_color3;
+   	var strakC = stroke_color3;
+   	var bakg = bakg_color3;
+   }
+   var nose_pos = average_point(positions.nose_bridge);
+   var eye1_pos = average_point(positions.left_eye);
+   var eye2_pos = average_point(positions.right_eye);
+   var half_height = positions.chin[7][1] - nose_pos[1];
+   var face_width = positions.chin[positions.chin.length-1][0] - positions.chin[0][0];
 
-  var x = nose_pos[0];
-  var y = nose_pos[1];
-  var w = 2 * face_width;
-  var h = 2.5 * half_height;
+   var x = nose_pos[0];
+   var y = nose_pos[1];
+   var w = 2 * face_width;
+   var h = 2.5 * half_height;
 
-  var extent = 0;
-  if(h < w) {
-    extent = h / 2;
-  }
-  else {
-    extent = w / 2;
-  }
-  var scales = extent / 220.0;
-  var randome = random (4, 20);
+   var extent = 0;
+   if(h < w) {
+   	extent = h / 2;
+   }
+   else {
+   	extent = w / 2;
+   }
+   var scales = extent / 220.0;
+   var randome = random (4, 20);
     // Uncomment to see drawing area
     // fill(255);
     // stroke(0);
@@ -81,13 +87,13 @@ function FaceMap() {
     fill(foreground);
     beginShape();
     for(var i=0; i<positions.chin.length;i++) {
-      vertex(positions.chin[i][0], positions.chin[i][1]);
+    	vertex(positions.chin[i][0], positions.chin[i][1]);
     }
     for(var i=positions.right_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.right_eyebrow[i][0], positions.right_eyebrow[i][1]+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0], positions.right_eyebrow[i][1]+(eyeSkew*scales));
     }
     for(var i=positions.left_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.left_eyebrow[i][0], positions.left_eyebrow[i][1]-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0], positions.left_eyebrow[i][1]-(eyeSkew*scales));
     }
     endShape(CLOSE);
 
@@ -96,25 +102,25 @@ function FaceMap() {
     strokeWeight(2*scales);
     beginShape();
     for(var i=0; i<positions.chin.length;i++) {
-      vertex(positions.chin[i][0]+random (0, 5)*scales, positions.chin[i][1]+random (0, 5)*scales);
+    	vertex(positions.chin[i][0]+random (0, 5)*scales, positions.chin[i][1]+random (0, 5)*scales);
     }
     for(var i=positions.right_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
     }
     for(var i=positions.left_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
     }
     endShape(CLOSE);
 
     beginShape();
     for(var i=0; i<positions.chin.length;i++) {
-      vertex(positions.chin[i][0]+random (0, 5)*scales, positions.chin[i][1]+random (0, 5)*scales);
+    	vertex(positions.chin[i][0]+random (0, 5)*scales, positions.chin[i][1]+random (0, 5)*scales);
     }
     for(var i=positions.right_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
     }
     for(var i=positions.left_eyebrow.length-1; i>=0;i--) {
-      vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
     }
     endShape(CLOSE);
     pop();
@@ -123,38 +129,43 @@ function FaceMap() {
     fill(bakg);
 
     strokeWeight (1.5*scales);
+    scale(sqshX, sqshY);
     beginShape();
-    for(var i=0; i<positions.top_lip.length;i++) {
-      vertex(positions.top_lip[i][0], positions.top_lip[i][1]);
+    for(var i=0; i<positions.bottom_lip.length;i++) {
+    	vertex(positions.bottom_lip[i][0], positions.bottom_lip[i][1]);
     }
+    endShape(CLOSE);
+    noFill()
+    beginShape();
+    for(var i=0; i<positions.bottom_lip.length;i++) {
+    	vertex(positions.bottom_lip[i][0]+random (0, 2)*scales, positions.bottom_lip[i][1]+random (0, 2)*scales);
+    }
+    endShape(CLOSE);  
     endShape(CLOSE);
     beginShape();
     for(var i=0; i<positions.bottom_lip.length;i++) {
-      vertex(positions.bottom_lip[i][0], positions.bottom_lip[i][1]);
+    	vertex(positions.bottom_lip[i][0]+random (0, 2)*scales, positions.bottom_lip[i][1]+random (0, 2)*scales);
     }
     endShape(CLOSE);
-
+    
+    fill(bakg);
+    beginShape();
+    for(var i=0; i<positions.top_lip.length;i++) {
+    	vertex(positions.top_lip[i][0], positions.top_lip[i][1]);
+    }
+    endShape(CLOSE);
     noFill()
     beginShape();
     for(var i=0; i<positions.top_lip.length;i++) {
-      vertex(positions.top_lip[i][0]+random (0, 2)*scales, positions.top_lip[i][1]+random (0, 2)*scales);
+    	vertex(positions.top_lip[i][0]+random (0, 2)*scales, positions.top_lip[i][1]+random (0, 2)*scales);
     }
     endShape(CLOSE);
-    beginShape();
-    for(var i=0; i<positions.bottom_lip.length;i++) {
-      vertex(positions.bottom_lip[i][0]+random (0, 2)*scales, positions.bottom_lip[i][1]+random (0, 2)*scales);
-    }
-    endShape(CLOSE);
+    
     beginShape();
     for(var i=0; i<positions.top_lip.length;i++) {
-      vertex(positions.top_lip[i][0]+random (0, 2)*scales, positions.top_lip[i][1]+random (0, 2)*scales);
+    	vertex(positions.top_lip[i][0]+random (0, 2)*scales, positions.top_lip[i][1]+random (0, 2)*scales);
     }
-    endShape(CLOSE);
-    beginShape();
-    for(var i=0; i<positions.bottom_lip.length;i++) {
-      vertex(positions.bottom_lip[i][0]+random (0, 2)*scales, positions.bottom_lip[i][1]+random (0, 2)*scales);
-    }
-    endShape(CLOSE);
+
 
     // nose
 
@@ -163,7 +174,7 @@ function FaceMap() {
     beginShape();
     vertex(positions.nose_bridge[0][0], positions.nose_bridge[0][1]);
     for(var i=0; i<positions.nose_tip.length;i++) {
-      vertex(positions.nose_tip[i][0], positions.nose_tip[i][1]);
+    	vertex(positions.nose_tip[i][0], positions.nose_tip[i][1]);
     }
     endShape(CLOSE);
 
@@ -171,14 +182,14 @@ function FaceMap() {
     beginShape();
     vertex(positions.nose_bridge[0][0]+random (0, 5)*scales, positions.nose_bridge[0][1]+random (0, 5)*scales);
     for(var i=0; i<positions.nose_tip.length;i++) {
-      vertex(positions.nose_tip[i][0]+random (0, 5)*scales, positions.nose_tip[i][1]+random (0, 5)*scales);
+    	vertex(positions.nose_tip[i][0]+random (0, 5)*scales, positions.nose_tip[i][1]+random (0, 5)*scales);
     }
     endShape(CLOSE);
 
     beginShape();
     vertex(positions.nose_bridge[0][0]+random (0, 5)*scales, positions.nose_bridge[0][1]+random (0, 5)*scales);
     for(var i=0; i<positions.nose_tip.length;i++) {
-      vertex(positions.nose_tip[i][0]+random (0, 5)*scales, positions.nose_tip[i][1]+random (0, 5)*scales);
+    	vertex(positions.nose_tip[i][0]+random (0, 5)*scales, positions.nose_tip[i][1]+random (0, 5)*scales);
     }
 
     endShape(CLOSE);
@@ -198,63 +209,69 @@ function FaceMap() {
     fill(strakC);
     beginShape();
     for(var i=0; i<positions.right_eyebrow.length; i++) {
-      vertex(positions.right_eyebrow[i][0], positions.right_eyebrow[i][1]+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0], positions.right_eyebrow[i][1]+(eyeSkew*scales));
     }
     endShape(CLOSE);
     beginShape();
     for(var i=0; i<positions.left_eyebrow.length; i++) {
-      vertex(positions.left_eyebrow[i][0], positions.left_eyebrow[i][1]-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0], positions.left_eyebrow[i][1]-(eyeSkew*scales));
     }
     endShape(CLOSE);
 
     noFill();
     beginShape();
     for(var i=0; i<positions.right_eyebrow.length; i++) {
-      vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
     }
     endShape(CLOSE);
     beginShape();
     for(var i=0; i<positions.left_eyebrow.length; i++) {
-      vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
     }
     endShape(CLOSE);
     beginShape();
     for(var i=0; i<positions.right_eyebrow.length; i++) {
-      vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
+    	vertex(positions.right_eyebrow[i][0]+random (0, 5)*scales, positions.right_eyebrow[i][1]+random (0, 5)*scales+(eyeSkew*scales));
     }
     endShape(CLOSE);
     beginShape();
     for(var i=0; i<positions.left_eyebrow.length; i++) {
-      vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
+    	vertex(positions.left_eyebrow[i][0]+random (0, 5)*scales, positions.left_eyebrow[i][1]+random (0, 5)*scales-(eyeSkew*scales));
     }
     endShape(CLOSE);
-    strokeWeight(1);  
-  }
+    strokeWeight(1); 
+}
 
-    
-      this.setProperties = function(settings) {
-      this.eyebrowSkew = settings[0];
-      this.colorChoice = settings[1];
-    }
 
-    /* get internal properties as list of numbers 0-100 */
-      this.getProperties = function() {
-      properties = new Array(2);
-      properties[0] = this.eyebrowSkew;
-      properties[1] = this.colorChoice;
-      return properties;
-    }
+this.setProperties = function(settings) {
+	this.eyebrowSkew = settings[0];
+	this.colorChoice = settings[1];
+	this.bottom_lipE = settings[2];
+	this.squishY = settings[3];
+	this.squishX = settings [4];
+}
+
+/* get internal properties as list of numbers 0-100 */
+this.getProperties = function() {
+	properties = new Array(5);
+	properties[0] = this.eyebrowSkew;
+	properties[1] = this.colorChoice;
+	properties[2] = this.bottom_lipE;
+	properties[3] = this.squishY;
+	properties[4] = this.squishX;
+	return properties;
+}
 
 }
 // given a point, return the average
 function average_point(list) {
-  var sum_x = 0;
-  var sum_y = 0;
-  var num_points = 0;
-  for(var i=0; i<list.length; i++) {
-    sum_x += list[i][0];
-    sum_y += list[i][1];
-    num_points += 1; 
-  }
-  return [sum_x / num_points, sum_y / num_points];
+	var sum_x = 0;
+	var sum_y = 0;
+	var num_points = 0;
+	for(var i=0; i<list.length; i++) {
+		sum_x += list[i][0];
+		sum_y += list[i][1];
+		num_points += 1; 
+	}
+	return [sum_x / num_points, sum_y / num_points];
 }
