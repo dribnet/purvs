1166b64c8c8e19664dd18b819e85160ce8b4d4ee
Edward Taylor <edeetee@gmail.com>
2017-08-24 15:35:44 +1200

    fixed face page

--- face.js
+++ face.js
@@ -15,6 +15,8 @@ function Face() {
   this.fg_color = [151, 102, 52];
   this.stroke_color = [95, 52, 8];
 
+  colorMode(HSB);
+
   var bert_drawFace = color(45, 90, 95)
   var bert_nose = color(29, 96, 94)
 
@@ -29,11 +31,15 @@ function Face() {
 
   var elmo_face_color = color(0, 100, 80);
 
+
+  colorMode(RGB);
+
   /*
    * Draw a face centered at x,y with an allowed
    * width and height of w,h.
    */  
   this.draw1 = function(x, y, w, h) {
+    colorMode(HSB);
     // Uncomment to see drawing area
     // fill(255);
     // stroke(0);
@@ -86,65 +92,8 @@ function Face() {
     fill(ernie_nose)
     ellipse(0, 5, 65, 70);
     pop();
-  }
 
-  /*
-   * Draw a face with position lists that include:
-   *    chin, right_eye, left_eye, right_eyebrow, left_eyebrow
-   *    bottom_lip, top_lip, nose_tip, nose_bridge, 
-   */  
-  this.draw2 = function(positions) {
-    var nose_pos = average_point(positions.nose_bridge);
-    var eye1_pos = average_point(positions.left_eye);
-    var eye2_pos = average_point(positions.right_eye);
-    var half_height = positions.chin[7][1] - nose_pos[1];
-    var face_width = positions.chin[positions.chin.length-1][0] - positions.chin[0][0];
-
-    var x = nose_pos[0];
-    var y = nose_pos[1];
-    var w = 2 * face_width;
-    var h = 2.5 * half_height;
-
-    var extent = 0;
-    if(h < w) {
-      extent = h / 2;
-    }
-    else {
-      extent = w / 2;
-    }
-    var scale = extent / 220.0;
-
-    // Uncomment to see drawing area
-    // fill(255);
-    // stroke(0);
-    // rect(x-w/2, y-h/2, w, h);
-    // fill(0)
-    // ellipse(x, y, w, h);
-
-    push();
-    translate(x, y);
-    rotate(this.tilt_value);
-
-    // head
-    stroke(this.stroke_color);
-    fill(this.fg_color);
-    ellipse(0, 0, 300 * scale, 400 * scale);
-    noStroke();
-
-    // mouth
-    fill(this.bg_color);
-    ellipse(0 * scale, 70 * scale, 150 * scale, this.mouth_value * scale);
-    pop();
-
-    noStroke();
-
-    fill(this.bg_color);
-    ellipse(eye1_pos[0], eye1_pos[1], 50 * scale, 30 * scale);
-    ellipse(eye2_pos[0], eye2_pos[1], 50 * scale, 30 * scale);
-
-    fill(this.fg_color);
-    ellipse(eye1_pos[0], eye1_pos[1], 20 * scale, 20 * scale);
-    ellipse(eye2_pos[0], eye2_pos[1], 20 * scale, 20 * scale);
+    colorMode(RGB);
   }
 
   /*
