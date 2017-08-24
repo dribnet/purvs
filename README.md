8db72e7cf009653d5d61346669373e1ab8029a5a
Guest User <Guest@c-05-06-lab.mddn.vuw.ac.nz>
2017-08-24 19:46:05 +1200

    Added sliders

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## PS2 MDDN 342 2017
 
 
-I added all the main features to the face that I wanted to, including the hair. I only did the fringe/ bangs as I am focusing on the face, not the entire head. The face shape and facial features are all sharp to relfect the anime style of face I am going for. I also tested adding in a custom face with landmarks. Its kind of difficult to find images with a relevant context for my face. But since it is a fighting based anime, I figured real life images of people doing physical activities would be relevant enough.
+The face has been updated with slider values that change some features of the face. I tried to focus on things that would help seperate different faces from each other. Initially this involved a lot of colour and shading changes to the skin, hair and eyes. However I was trying to think of other visual chnages that would occur between faces, and would relate to my face. I notice the hair segments would be a good one, to indicate the type of hairstyle the inidivual has. Additionally if they have earrings or not. 
+
+With the colours, the skin was a interesting choice to make. There are a lot of bright and vibrant colours for skin tones for the specices of person Zamasu is, so I tried to translate this into a method of showing gender. More masculine faces will appear in the bright green shade, while more femenine faces will appear in a bright pink.
--- facemap.js
+++ facemap.js
@@ -15,8 +15,12 @@ var ZamasuFaceColor1 = "rgb(177,215,151)";
 var ZamasuFaceColor2 = "rgb(157,195,131)";
 var ZamasuFaceColor3 = "rgba(0,0,0, 0.2)";
 var ZamasuFaceColor4 = "rgba(0,0,0,0.15)";
+
 var ZamasuHairColor1 = "rgb(232,236,236)";
 var ZamasuHairColor2 = "rgb(169,176,175)";
+var ZamasuHairColor3 = "rgb(252,256,256)";
+var ZamasuHairColor4 = "rgb(212,216,216)";
+
 var ZamasuPotEaringColor1 = "rgb(243,255,84)";
 var ZamasuPotEaringColor2 = "rgb(145,157,0)";
 var ZamasuPotEaringColor3 = "rgb(18,223,42)";
@@ -25,11 +29,26 @@ var fg_color2 = "#999999";
 var stroke_color = "black";
 var eyecolor1 = "rgb(158,160,160)";
 var eyecolor2 = "rgb(15,15,15)";
-var eyecolor3 = "rgb(255,255,255)";
+var eyecolor3 = "rgb(255,255,255";
+var eyecolor4 = "rgb(211,198,213)";
+
+var ShinFaceColor1 = "rgb(233,145,239)";
+var ShinFaceColor2 = "rgb(203,115,209)";
+
+var FaceColor1 = "rgb(86,147,255)";
+var FaceColor2 = "rgb(66,127,235)";
+
+var FaceColor3 = "rgb(255,253,109)";
+var FaceColor4 = "rgb(225,223,89)";
 
 function FaceMap() {
-  this.hairLength = 50;
-  this.hairColor = 50;
+  this.faceColor_value = 50;
+  this.hairColor_value = 50;
+  this.faceColor2_value = 50;
+  this.earring_value = 1;
+  this.hairSegment1_value = 1;
+  this.hairSegment2_value = 1;
+  this.hairSegment3_value = 1;
 
   /*
    * Draw a face with position lists that include:
@@ -38,6 +57,35 @@ function FaceMap() {
    */  
   this.draw = function(positions) {
 
+    // Creating the lerp colours for sliders.
+    var c1 = color(ZamasuFaceColor1);
+    var c2 = color(ShinFaceColor1);
+    this.faceColor_value = map(this.faceColor_value, 0, 100, 0, 1);
+    var faceColorBlend = lerpColor(c1, c2, this.faceColor_value);
+
+    var c3 = color(ZamasuFaceColor2);
+    var c4 = color(ShinFaceColor2);
+    var faceColorBlend2 = lerpColor(c3, c4, this.faceColor_value);
+
+    var c9 = color(eyecolor1);
+    var c10 = color(eyecolor4);
+    this.faceColor2_value = map(this.faceColor2_value, 0, 100, 0, 1);
+    var eyeColorBlend = lerpColor(c9, c10, this.faceColor2_value);
+
+    var c11 = color(ZamasuHairColor3);
+    var c12 = color(ZamasuHairColor4);
+    this.hairColor_value = map(this.hairColor_value, 0, 100, 0, 1);
+    var hairColorBlend = lerpColor(c11, c12, this.hairColor_value);
+
+
+    //map functions
+    this.earring_value = map(this.earring_value, 0, 100, 1, 3);
+    this.hairSegment1_value = map(this.hairSegment1_value, 0, 100, 1, 3);
+    this.hairSegment2_value = map(this.hairSegment2_value, 0, 100, 1, 3);
+    this.hairSegment3_value = map(this.hairSegment3_value, 0, 100, 1, 3);
+
+
+    // Average positions of facial feature points
     var nose_pos = average_point(positions.nose_bridge);
     var eye1_pos = average_point(positions.left_eye);
     var eye2_pos = average_point(positions.right_eye);
@@ -86,7 +134,7 @@ function FaceMap() {
 
     // Ears
     noStroke();
-    fill(ZamasuFaceColor2);
+    fill(faceColorBlend2);
 
     // Left Ear
     beginShape();
@@ -131,40 +179,42 @@ function FaceMap() {
 
 	// Potara Earrings
 
-	fill(ZamasuPotEaringColor1);
-    ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.1 - (6 * scale * leftEarLength), 8 * scale, 8 * scale);
-    fill(ZamasuPotEaringColor3);
-    ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.2 - (6 * scale * leftEarLength), 15 * scale, 15 * scale);
-    fill(ZamasuPotEaringColor1);
-    ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] - (6 * scale * leftEarLength), 10 * scale, 10 * scale);
 
-    fill(ZamasuPotEaringColor1);
-    ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.1 - (6 * scale * -rightEarLength), 8 * scale, 8 * scale);
-    fill(ZamasuPotEaringColor3);
-    ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.2 - (6 * scale * -rightEarLength), 15 * scale, 15 * scale);
-    fill(ZamasuPotEaringColor1);
-    ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] - (6 * scale * -rightEarLength), 10 * scale, 10 * scale);
-
-    // Potara Earring Shading
-    noStroke();
-    fill(ZamasuFaceColor4);
-
-    if (leftEarLength < -rightEarLength) {
+    if (this.earring_value <= 2) {
+    	fill(ZamasuPotEaringColor1);
         ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.1 - (6 * scale * leftEarLength), 8 * scale, 8 * scale);
+        fill(ZamasuPotEaringColor3);
         ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.2 - (6 * scale * leftEarLength), 15 * scale, 15 * scale);
+        fill(ZamasuPotEaringColor1);
         ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] - (6 * scale * leftEarLength), 10 * scale, 10 * scale);
-    }
 
-    if (-rightEarLength < leftEarLength) {
+        fill(ZamasuPotEaringColor1);
         ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.1 - (6 * scale * -rightEarLength), 8 * scale, 8 * scale);
+        fill(ZamasuPotEaringColor3);
         ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.2 - (6 * scale * -rightEarLength), 15 * scale, 15 * scale);
+        fill(ZamasuPotEaringColor1);
         ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] - (6 * scale * -rightEarLength), 10 * scale, 10 * scale);
-    }
 
+        // Potara Earring Shading
+        noStroke();
+        fill(ZamasuFaceColor4);
+
+        if (leftEarLength < -rightEarLength) {
+            ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.1 - (6 * scale * leftEarLength), 8 * scale, 8 * scale);
+            ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] + 0.2 - (6 * scale * leftEarLength), 15 * scale, 15 * scale);
+            ellipse(positions.chin[2][0] + 0.1 - (10 * scale * leftEarLength), positions.chin[3][1] - (6 * scale * leftEarLength), 10 * scale, 10 * scale);
+        }
+
+        if (-rightEarLength < leftEarLength) {
+            ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.1 - (6 * scale * -rightEarLength), 8 * scale, 8 * scale);
+            ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] + 0.2 - (6 * scale * -rightEarLength), 15 * scale, 15 * scale);
+            ellipse(positions.chin[14][0] - 0.1 + (10 * scale * -rightEarLength), positions.chin[13][1] - (6 * scale * -rightEarLength), 10 * scale, 10 * scale);
+        }
+    }
 
     // head
     noStroke();
-    fill(ZamasuFaceColor1);
+    fill(faceColorBlend);
     beginShape();
     for(var i=0; i<positions.chin.length;i+=4) {
       vertex(positions.chin[i][0], positions.chin[i][1]);
@@ -184,7 +234,7 @@ function FaceMap() {
 
     // Mouth shapes
     noStroke();
-    fill(ZamasuFaceColor2);
+    fill(faceColorBlend2);
     beginShape();
         vertex(mouthTop_pos[0] - (20 * scale), mouthTop_pos[1]);
         vertex(mouthBottom_pos[0] - (20 * scale), mouthBottom_pos[1]);
@@ -207,7 +257,7 @@ function FaceMap() {
 
     // nose
     noStroke();
-    fill(ZamasuFaceColor2);
+    fill(faceColorBlend2);
 
     if (-rightEarLength < leftEarLength) {
         beginShape();
@@ -255,7 +305,7 @@ function FaceMap() {
         vertex(positions.right_eye[4][0], positions.right_eye[4][1]);
     endShape();
 
-    fill(eyecolor1);
+    fill(eyeColorBlend);
     ellipse(eye1_pos[0] + (5 * scale), eye1_pos[1] - (5 * scale), 22 * scale, 22 * scale);
     ellipse(eye2_pos[0] - (5 * scale), eye2_pos[1] - (5 * scale), 22 * scale, 22 * scale);
 
@@ -267,7 +317,7 @@ function FaceMap() {
 
     // right eyebrow
     noStroke();
-    fill(ZamasuHairColor1);
+    fill(hairColorBlend);
     beginShape();
     	vertex(positions.right_eyebrow[0][0], positions.right_eyebrow[0][1]);
     	vertex(positions.right_eyebrow[2][0], positions.right_eyebrow[2][1]);
@@ -277,7 +327,7 @@ function FaceMap() {
 
     // left eyebrow
     noStroke();
-    fill(ZamasuHairColor1);
+    fill(hairColorBlend);
     beginShape();
     	vertex(positions.left_eyebrow[0][0], positions.left_eyebrow[0][1]);
     	vertex(positions.left_eyebrow[2][0], positions.left_eyebrow[2][1]);
@@ -292,43 +342,48 @@ function FaceMap() {
 
     // Hair
     noStroke();
-    fill(ZamasuHairColor1);
+    fill(hairColorBlend);
 
     // Middle left segment
-    beginShape();
-        vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
-        vertex(positions.left_eyebrow[4][0] - (10 * scale), positions.left_eyebrow[4][1] - (90 * scale));
-        vertex(positions.left_eyebrow[3][0] - (12 * scale), positions.left_eyebrow[3][1] - (80 * scale));
-        vertex(positions.left_eyebrow[2][0] - (25 * scale), positions.left_eyebrow[2][1] - (80 * scale));
-        vertex(positions.left_eyebrow[1][0] - (50 * scale), positions.left_eyebrow[1][1] - (80 * scale));
-        vertex(positions.left_eyebrow[0][0] - (100 * scale), positions.left_eyebrow[0][1] - (80 * scale));
-    endShape();
-
+    if (this.hairSegment2_value <= 2) {
+        beginShape();
+            vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
+            vertex(positions.left_eyebrow[4][0] - (10 * scale), positions.left_eyebrow[4][1] - (90 * scale));
+            vertex(positions.left_eyebrow[3][0] - (12 * scale), positions.left_eyebrow[3][1] - (80 * scale));
+            vertex(positions.left_eyebrow[2][0] - (25 * scale), positions.left_eyebrow[2][1] - (80 * scale));
+            vertex(positions.left_eyebrow[1][0] - (50 * scale), positions.left_eyebrow[1][1] - (80 * scale));
+            vertex(positions.left_eyebrow[0][0] - (100 * scale), positions.left_eyebrow[0][1] - (80 * scale));
+        endShape();
+    }
     // Primary left segment
-    beginShape();
-        vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
-        vertex(positions.left_eyebrow[4][0] - (10 * scale), positions.left_eyebrow[4][1] - (60 * scale));
-        vertex(positions.left_eyebrow[3][0] - (12 * scale), positions.left_eyebrow[3][1] - (50 * scale));
-        vertex(positions.left_eyebrow[2][0] - (25 * scale), positions.left_eyebrow[2][1] - (40 * scale));
-        vertex(positions.left_eyebrow[1][0] - (50 * scale), positions.left_eyebrow[1][1] + (70 * scale));
-        vertex(positions.left_eyebrow[0][0] - (50 * scale), positions.left_eyebrow[0][1] + (120 * scale));
-        vertex(positions.left_eyebrow[3][0] + (12 * scale), positions.left_eyebrow[3][1] - (10 * scale));
-    endShape();
+    if (this.hairSegment1_value <= 2) {
+        beginShape();
+            vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
+            vertex(positions.left_eyebrow[4][0] - (10 * scale), positions.left_eyebrow[4][1] - (60 * scale));
+            vertex(positions.left_eyebrow[3][0] - (12 * scale), positions.left_eyebrow[3][1] - (50 * scale));
+            vertex(positions.left_eyebrow[2][0] - (25 * scale), positions.left_eyebrow[2][1] - (40 * scale));
+            vertex(positions.left_eyebrow[1][0] - (50 * scale), positions.left_eyebrow[1][1] + (70 * scale));
+            vertex(positions.left_eyebrow[0][0] - (50 * scale), positions.left_eyebrow[0][1] + (120 * scale));
+            vertex(positions.left_eyebrow[3][0] + (12 * scale), positions.left_eyebrow[3][1] - (10 * scale));
+        endShape();
+    }
 
     // middle segment
-    beginShape();
-        vertex(positions.right_eyebrow[0][0], positions.right_eyebrow[0][1] - (50 * scale));
-        vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
-        vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (50 * scale));
-        vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (110 * scale));
-        vertex(positions.right_eyebrow[0][0] - (5 * scale), positions.right_eyebrow[0][1] - (145 * scale));
-    endShape();
+    if (this.hairSegment3_value <= 2) {
+        beginShape();
+            vertex(positions.right_eyebrow[0][0], positions.right_eyebrow[0][1] - (50 * scale));
+            vertex(positions.right_eyebrow[0][0] - (20 * scale), positions.right_eyebrow[0][1] - (45 * scale));
+            vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (50 * scale));
+            vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (110 * scale));
+            vertex(positions.right_eyebrow[0][0] - (5 * scale), positions.right_eyebrow[0][1] - (145 * scale));
+        endShape();
 
-    beginShape();
-        vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (108 * scale));
-        vertex(positions.right_eyebrow[0][0] - (5 * scale), positions.right_eyebrow[0][1] - (143 * scale));
-        vertex(positions.right_eyebrow[0][0] - (75 * scale), positions.right_eyebrow[0][1] - (155 * scale));
-    endShape();
+        beginShape();
+            vertex(positions.left_eyebrow[4][0], positions.left_eyebrow[4][1] - (108 * scale));
+            vertex(positions.right_eyebrow[0][0] - (5 * scale), positions.right_eyebrow[0][1] - (143 * scale));
+            vertex(positions.right_eyebrow[0][0] - (75 * scale), positions.right_eyebrow[0][1] - (155 * scale));
+        endShape();
+    }
 
 
     // noStroke();
@@ -375,15 +430,25 @@ function FaceMap() {
 
   /* set internal properties based on list numbers 0-100 */
   this.setProperties = function(settings) {
-    this.hairLength = settings[0];
-    this.hairColor = settings[1];
+    this.faceColor_value = settings[0];
+    this.hairColor_value = settings[1];
+    this.faceColor2_value = settings[2];
+    this.earring_value = settings[3];
+    this.hairSegment1_value = settings[4];
+    this.hairSegment2_value = settings[5];
+    this.hairSegment3_value = settings[6];
   }
 
   /* get internal properties as list of numbers 0-100 */
   this.getProperties = function() {
-    properties = new Array(2);
-    properties[0] = this.hairLength;
-    properties[1] = this.hairColor;
+    properties = new Array(7);
+    properties[0] = this.faceColor_value;
+    properties[1] = this.hairColor_value;
+    properties[2] = this.faceColor2_value;
+    properties[3] = this.earring_value;
+    properties[4] = this.hairSegment1_value;
+    properties[5] = this.hairSegment2_value;
+    properties[6] = this.hairSegment3_value;
     return properties;
   }
 }
