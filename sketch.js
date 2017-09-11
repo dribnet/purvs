b1aeb716c31af22cc962e0c623470eca0927905b
Shane Watters <shane.watters.nz@gmail.com>
2017-09-11 15:20:25 +1200

    Initial creation of pattern. I have created an interesting pattern that can have many possibilities by adjusting different numbers.  This is a good start, next I will add some selectable options that will allow the viewer to change the appearance of the pattern.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,38 @@
+var i = 0, startX = 0, startY = 0;
 function setup () {
   createCanvas(960, 500);
+  frameRate(1);
+  startX = random(0, width); 
+  startY = random(0, height);
 }
 
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
+  var to = color('#EF4623');
+  var to = color('#f5ec25');
+  var from = color('#1878B3');
+  
+  //angleMode(DEGREES);
+  fill(255, 255, 255, 1);
+  
+  strokeWeight(0.1);
+  translate(startX, startY);  
+  // /print(width/12);
+  var x = 0;
+  var count = 0.05;
+  while(x < width){
+    var colour = lerpColor(from, to, count);
+    stroke(colour);
+    for (var i = 0; i < 16; i ++) {
+      var rand = random(0,100);
+      if(rand < 20){
+        stroke(random(255), random(255), random(255));
+      }
+      rect(x, 20, 10, 1 * x);
+      rotate(PI/8);
+    }
+    x = x + width/24;
+    count = count + 0.05;
   }
-  else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
 }
 
 function keyTyped() {
