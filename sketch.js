2b714f70b976f83c0d574d67c8386d0e7452f114
Shane Watters <shane.watters.nz@gmail.com>
2017-09-13 10:06:22 +1200

    Adding lerpColour functionality to my colour generation process. I have also added a reset button and shape size slider to my interactive controls. I need to add a few more controls to make it more interactive and I also want to add a mode selector so users can select between an auto mode and an interactive mode.

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
-//globle variables
-var startX = 0, startY = 0, radio, shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], h, s, b;
+//global variables
+var startX = 0, startY = 0, selector, shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], h, s, b, FROM, TO, slider1, resetButton;
 
 function setup () {
 	//set up the canvas
 	var main_canvas = createCanvas(960, 500);
 	main_canvas.parent('canvas-container');
 
-	// create radio button optionset for shape selection
-	radio = createRadio();
+	// set up the interactive controls
+	selector = createSelect();
 	for(var i=0; i < shapeOptions.length; i++){
-		radio.option(shapeOptions[i]);
+		selector.option(shapeOptions[i]);
 	}
-	radio.value(random(shapeOptions));
-	radio.parent('shape-selector-holder');
-	radio.changed(randomizeGlobalValues);
+	selector.value(random(shapeOptions));
+	selector.parent('shape-selector-holder');
+	selector.changed(randomizeGlobalValues);
+	
+	slider1 = createSlider(5, 20, 10);
+	slider1.parent("slider-1-holder");
+	
+	resetButton = createButton('reset');
+	resetButton.mousePressed(resetDrawing);
+	resetButton.parent('reset-button-holder');
 	
 	//set up some of the global options for p5.js
 	background(0);
@@ -25,34 +32,26 @@ function setup () {
 	randomizeGlobalValues();
 }
 
-var hueRanges = {
-        1 : [-20, 20, 0],
-        2 : [21, 60, 40],
-        3 : [61, 100, 80],
-        4 : [101, 140, 120],
-        5 : [141, 180, 160],
-        6 : [181, 220, 200],
-        7 : [221, 260, 240],
-        8 : [261, 300, 280],
-        9 : [301, 340, 320]
-    }
+
 	
 function draw () {
 	var to = color('#EF4623');
 	var to = color('#f5ec25');
 	var from = color('#1878B3');
 
-	strokeWeight(0.02);
+	strokeWeight(0.01);
 	translate(startX, startY);  
 	var x = 0;
-	var count = 0.05;
-	var shape = radio.value();
+	var count = 0;
+	var shape = selector.value();
+	var shapeSize =  slider1.value();
 	while(x < width){
-		var colour = lerpColor(from, to, count);
-		for (var i = 0; i < 16; i ++) {
+		var colour = lerpColor(FROM, TO, count);
+		for (var i = 0; i < 20; i ++) {
 			for (var j = -2; j <=2; j++) {
 				stroke(colour, random(15, 127));
 				stroke(h,s,b, 127 - (j * 16));
+				stroke(colour, 127 - (j * 16));
 				var rand = random(0,100);
 				if(rand < 0){
 					stroke(random(255), random(255), random(255), random(15, 127));
@@ -60,29 +59,54 @@ function draw () {
 				//call the function as detemined by the variable shape
 				//rect and ellipse are built in p5.js
 				//tri,hexa & octa are defined in this file
-				window[shape](x, 20, 10 + (j*2), 10 + (j*2));
+				window[shape](x, 20, shapeSize + (j*3), shapeSize + (j*3));
 				//window[shape](x, 20, 10 + j, (1 * x) + j);
 			}
-			rotate(PI/8);
+			rotate(PI/10);
 		}
-		x = x + width/24;
-		count = count + 0.05;
+		x = x + width/6;
+		count = count + 0.25;
 	}
 }
 
 function mousePressed() {
+print('sdfdf');
 	if (mouseButton == LEFT){
-		radio.value(random(shapeOptions));
+		selector.value(random(shapeOptions));
 		randomizeGlobalValues(mouseX, mouseY);
 	}
 }
 
-//set the values for startX and startY which determines where the pattern will begin drawing
+function resetDrawing(){
+	translate(-startX, -startY); 
+	clear();
+	background(0);
+	randomizeGlobalValues();
+}
+
+var hueRanges = {
+        1 : [-30, 30, 0],
+        2 : [31, 90, 60],
+        3 : [91, 150, 120],
+        4 : [151, 210, 180],
+        5 : [211, 270, 240],
+        6 : [271, 330, 300]
+    }
+	
 function randomizeGlobalValues(setX = false, setY = false){
-	var randomPointer = floor(random(1, 10));
+	var randomPointer = floor(random(1, 7));
 	h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
-	s = random(50, 100);
-	b = random(50, 100);
+	s = random(100, 100);
+	b = random(100, 100);
+	FROM = color(h, s, b);
+	randomPointer = randomPointer + 3;
+	if(randomPointer > 6){
+		randomPointer = randomPointer -6;
+	}
+	h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
+	s = random(100, 100);
+	b = random(100, 100);
+	TO = color(h, s, b);
 	startX = random(0, width); 
 	if(setX){
 		startX = setX;
@@ -119,15 +143,16 @@ function equilateral(x, y, width) {
  * @param {Number} radius   - radius of the hexagon
  */
 function hexa(x, y, radius) {
-  angleMode(RADIANS);
-  var angle = TWO_PI / 6;
-  beginShape();
-  for (var a = TWO_PI/12; a < TWO_PI + TWO_PI/12; a += angle) {
-    var sx = x + cos(a) * radius;
-    var sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
+	radius = radius / 2;
+	angleMode(RADIANS);
+	var angle = TWO_PI / 6;
+	beginShape();
+	for (var a = TWO_PI/12; a < TWO_PI + TWO_PI/12; a += angle) {
+	var sx = x + cos(a) * radius;
+	var sy = y + sin(a) * radius;
+		vertex(sx, sy);
+	}
+	endShape(CLOSE);
 }
 
 /*
@@ -138,15 +163,16 @@ function hexa(x, y, radius) {
  * @param {Number} radius   - radius of the octagon
  */
 function octa(x, y, radius) {
-  angleMode(RADIANS);
-  var angle = TWO_PI / 8;
-  beginShape();
-  for (var a = TWO_PI/16; a < TWO_PI + TWO_PI/16; a += angle) {
-    var sx = x + cos(a) * radius;
-    var sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
+	radius = radius / 2;
+	angleMode(RADIANS);
+	var angle = TWO_PI / 8;
+	beginShape();
+	for (var a = TWO_PI/16; a < TWO_PI + TWO_PI/16; a += angle) {
+	var sx = x + cos(a) * radius;
+	var sy = y + sin(a) * radius;
+		vertex(sx, sy);
+	}
+	endShape(CLOSE);
 }
 
 
