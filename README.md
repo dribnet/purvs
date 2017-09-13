dbce34433ae5459498d5322e0b44208c1a50293d
freirejess <freirejess@myvuw.ac.nz>
2017-09-14 10:52:32 +1200

    New shapes added and developments made with the background.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 17.2.MDDN342 PS3
 
 Going into this project my first step has been to do some research and start thinking of patterns and color schemes. So far, I've been inspired by the works of Holger Lippmann and the colour schemes used in his work. Next step is to keep developing the color scheme and start looking at patterns and shapes.
+
+UPDATE
+
+I've started looking further at the style and shapes to use for this project. I really like the idea of layering complex shapes under simplers ones so I'm currently trying to make progress with that.
--- sketch.js
+++ sketch.js
@@ -1,17 +1,46 @@
-function setup () {
+function setup() {
   createCanvas(960, 500);
+  background(57, 33, 21);
+  smooth();
+
+  var xstart = random(100),
+      xnoise = xstart,
+      ynoise = random(100);
+
+  for (var y = 0; y <= height; y+=5) {
+    ynoise += 0.1;
+    xnoise  = xstart;
+    for (var x = 0; x <= width; x+=5) {
+      xnoise += 0.1;
+      drawPoint(x, y, noise(xnoise, ynoise));
+    }
+  }
 }
 
-function draw () {
-  if (mouseIsPressed) {
-    stroke(191, 100, 27);
-    fill(57, 33, 21);
-  }
-  else {
-    stroke(168, 133, 95);
-    fill(57, 33, 21);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw (){
+
+  fill(57, 33, 21, 20);
+  noStroke();
+  ellipse(120, 120, 200, 200);
+  
+  fill(57, 33, 21, 40);
+  ellipse(200, 180, 150, 150);
+  
+  noFill();
+  stroke(191, 100, 27);
+  //strokeWeight(2);
+  ellipse(150, 230, 150, 150);
+  ellipse(220, 110, 50, 50);
+}
+
+function drawPoint(x, y, noiseFactor) {
+  push();
+  translate(x, y);
+  rotate(noiseFactor * radians(360));
+  //colorMode(HSB);
+  stroke(x*0.1 + 191, 100, 27, 90);
+  line(0, 0, 7, 0);
+  pop();
 }
 
 function keyTyped() {
