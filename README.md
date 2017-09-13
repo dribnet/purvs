53f0fc1458adce7555ae65c68e2a7b6bf45d507a
S-Williams42 <williasusa@myvuw.ac.nz>
2017-09-14 08:04:33 +1200

    part one first draft of a landscape pattern

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+This readme explains my assignment 3 sketch.
+I have experimented with a series of lines going form almost straight and contained to free and expressive. I have experimented with different design choices, such as background, rotation, and spacing, some of my experiments can be seen below.
+first experiments with drawing a simgle line
+![experiment](https://4.bp.blogspot.com/-CFmIFCiSrus/WbmIB-Qd0BI/AAAAAAAAASg/gNsky60XRMgrI767P9aM7bdpjeQr5Kg6wCK4BGAYYCw/s320/preview1.jpg)
+![experiment](https://2.bp.blogspot.com/-YIso4wcNglw/WbmIFcqZwjI/AAAAAAAAASo/N1nfcpuLkUkuvSOV-i3eCo59NhZak75tgCK4BGAYYCw/s320/preview2.jpg)
+![experiment](https://4.bp.blogspot.com/-cqFLMyqV01E/WbmIMjmmstI/AAAAAAAAASw/dY5eI3kfpa8EWaWRwfDKS1_sBWA8HWZKACK4BGAYYCw/s320/preview3.jpg)
+![experiment](https://4.bp.blogspot.com/-cqFLMyqV01E/WbmIMjmmstI/AAAAAAAAASw/dY5eI3kfpa8EWaWRwfDKS1_sBWA8HWZKACK4BGAYYCw/s320/preview3.jpg)
+
+some screanshots of nice results of the current version as it displays on my screen 
+![experiment](https://1.bp.blogspot.com/-8rRy8f52FBM/WbmITU80JNI/AAAAAAAAATA/VPy7kCPUdZsBnfH0rVEhzuRK1etx9q30wCK4BGAYYCw/s320/preview5.jpg)
+![experiment](https://1.bp.blogspot.com/-WBx-Aec2LFg/WbmIWmWT0EI/AAAAAAAAATI/Z_xqnzi-1TQ9JfB1FE8B1bl9xcNlS7EzACK4BGAYYCw/s320/preview8.jpg)
+![experiment](https://4.bp.blogspot.com/-8OYBooQKFJQ/WbmIbpJKzNI/AAAAAAAAATQ/tA6-mOrBtdQwy4WG57PbuDPCGUfaJc1NQCK4BGAYYCw/s320/preview9.jpg)
+
+
--- sketch.js
+++ sketch.js
@@ -1,19 +1,252 @@
+// global variables
+var w = 960;
+var h = 500;
+var curRandomSeed;
+
+//setup
 function setup () {
-  createCanvas(960, 500);
+  angleMode(DEGREES);
+  createCanvas(w, h);
+  curRandomSeed = int(focusedRandom(0, 100));
 }
 
+
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
-  }
-  else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  resetFocusedRandom(curRandomSeed);
+   background(155);
+  centrePattern(w,h);
+
+
+}
+
+function centrePattern(xW,yH){
+  push();
+  translate(xW/2,yH/2);
+  rotate(100);
+  translate(-xW/2,-yH/2);
+  rectMode(CORNER);
+  fill(239,235,202);
+  noStroke();
+rect(0,0,xW,yH);
+rect(xW/3,-yH/4,xW,yH);
+rect(-xW/3,yH/4,xW,yH);
+stroke(0);
+fill(0)
+  
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+
+//section of 3
+push();
+translate(0,-150);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+
+//section of 3
+push();
+translate(0,-300);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+
+//section of 3
+push();
+translate(0,300);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+
+
+//section of 3
+push();
+translate(0,150);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+
+
+
+
+//section of 3
+push();
+translate(0,-450);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+
+
+//section of 3
+push();
+translate(0,450);
+  wobblyXYLine(xW,yH);
+  push();
+  translate(0,30);
+wobblyXYLine(xW,yH);
+pop();
+push();
+translate(0,-40);
+wobblyXYLine(xW,yH);
+pop();
+pop();
+pop();
+}
+
+
+
+
+
+
+
+
+//creates a line that deviates but only in the y plane
+function wobblyYLine(xW, yH){
+  beginShape();
+vertex(0,0-focusedRandom(0,0));
+vertex(xW/20,yH/20-focusedRandom(-0,0));
+vertex(xW/15,yH/15-focusedRandom(-0,0));
+vertex(xW/10,yH/10-focusedRandom(-0,0));
+vertex(xW/2,yH/2-focusedRandom(-0,0));
+vertex(xW/1.9,yH/1.9-focusedRandom(-0,0));
+vertex(xW/1.8,yH/1.8-focusedRandom(-0,0));
+vertex(xW/1.7,yH/1.7-focusedRandom(-0,0));
+vertex(xW/1.6,yH/1.6-focusedRandom(-0,0));
+vertex(xW/1.5,yH/1.5-focusedRandom(-0,0));
+vertex(xW/1.4,yH/1.4-focusedRandom(-0,0));
+vertex(xW/1.3,yH/1.3-focusedRandom(-0,0));
+vertex(xW/1.2,yH/1.2-focusedRandom(-0,0));
+vertex(xW/1.1,yH/1.1-focusedRandom(-0,0));
+vertex(xW,yH-focusedRandom(-0,0));
+endShape();
+}
+
+//creates a line that deviates in both the x and y directions
+function wobblyYLine(xW, yH){
+  beginShape();
+vertex(0,0-focusedRandom(-0,0));
+vertex(xW/20-focusedRandom(-0,0),yH/20-focusedRandom(-0,0));
+vertex(xW/15-focusedRandom(-0,0),yH/15-focusedRandom(-0,0));
+vertex(xW/10-focusedRandom(-0,0),yH/10-focusedRandom(-0,0));
+vertex(xW/2-focusedRandom(-0,0),yH/2-focusedRandom(-0,0));
+vertex(xW/1.9-focusedRandom(-3,6),yH/1.9-focusedRandom(-0,0));
+vertex(xW/1.8-focusedRandom(-2,2),yH/1.8-focusedRandom(-0,0));
+vertex(xW/1.7-focusedRandom(-7,7),yH/1.7-focusedRandom(-0,0));
+vertex(xW/1.6-focusedRandom(-3,3),yH/1.6-focusedRandom(-0,0));
+vertex(xW/1.5-focusedRandom(-9,2),yH/1.5-focusedRandom(-0,0));
+vertex(xW/1.4-focusedRandom(-4,7),yH/1.4-focusedRandom(-0,0));
+vertex(xW/1.3-focusedRandom(-7,9),yH/1.3-focusedRandom(-0,0));
+vertex(xW/1.2-focusedRandom(-11,4),yH/1.2-focusedRandom(-0,0));
+vertex(xW/1.1-focusedRandom(22,11),yH/1.1-focusedRandom(-0,0));
+vertex(xW-focusedRandom(-0,0),yH-focusedRandom(-0,0));
+endShape();
+}
+
+//creates a line that deviates on only the y plane
+function wobblyXYLine(xW, yH){
+  beginShape();
+  //draws down from point (0,0) to point (xW, yH)
+vertex(0,0-focusedRandom(-0,0));
+vertex(xW/20-focusedRandom(-2,1),yH/20-focusedRandom(-0,0));
+vertex(xW/17-focusedRandom(-5,7),yH/17-focusedRandom(-13,23));
+vertex(xW/15-focusedRandom(-3,1),yH/15-focusedRandom(-12,11));
+vertex(xW/13-focusedRandom(-2,4),yH/13-focusedRandom(-26,18));
+vertex(xW/10-focusedRandom(-5,7),yH/10-focusedRandom(-13,28));
+vertex(xW/7-focusedRandom(-3,5),yH/7-focusedRandom(-27,28));
+vertex(xW/5-focusedRandom(-1,7),yH/5-focusedRandom(-23,18));
+vertex(xW/4-focusedRandom(-8,4),yH/4-focusedRandom(-37,28));
+vertex(xW/3-focusedRandom(-2,6),yH/3-focusedRandom(-44,38));
+vertex(xW/2-focusedRandom(-8,2),yH/2-focusedRandom(-37,12));
+vertex(xW/1.9-focusedRandom(-8,9),yH/1.9-focusedRandom(-49,50));
+vertex(xW/1.8-focusedRandom(-2,2),yH/1.8-focusedRandom(-11,22));
+vertex(xW/1.7-focusedRandom(-7,7),yH/1.7-focusedRandom(-54,33));
+vertex(xW/1.6-focusedRandom(-3,3),yH/1.6-focusedRandom(-44,64));
+vertex(xW/1.5-focusedRandom(-9,2),yH/1.5-focusedRandom(-74,44));
+vertex(xW/1.4-focusedRandom(-4,7),yH/1.4-focusedRandom(-32,77));
+vertex(xW/1.3-focusedRandom(-7,9),yH/1.3-focusedRandom(-70,50));
+vertex(xW/1.2-focusedRandom(-11,4),yH/1.2-focusedRandom(-55,65));
+vertex(xW/1.1-focusedRandom(22,11),yH/1.1-focusedRandom(-66,54));
+vertex(xW-focusedRandom(-0,0),yH-focusedRandom(-0,0));
+//heads back up to point 0,0!
+vertex(xW/1.15-focusedRandom(22,11),yH/1.15-focusedRandom(-66,54));
+vertex(xW/1.25-focusedRandom(-11,4),yH/1.25-focusedRandom(-55,65));
+vertex(xW/1.35-focusedRandom(-7,9),yH/1.35-focusedRandom(-70,50));
+vertex(xW/1.45-focusedRandom(-4,7),yH/1.45-focusedRandom(-32,77));
+vertex(xW/1.55-focusedRandom(-9,2),yH/1.55-focusedRandom(-74,44));
+vertex(xW/1.65-focusedRandom(-3,3),yH/1.65-focusedRandom(-44,64));
+vertex(xW/1.75-focusedRandom(-7,7),yH/1.75-focusedRandom(-54,33));
+vertex(xW/1.8-focusedRandom(-2,2),yH/1.8-focusedRandom(-11,22));
+vertex(xW/1.9-focusedRandom(-8,9),yH/1.9-focusedRandom(-49,50));
+vertex(xW/2-focusedRandom(-8,2),yH/2-focusedRandom(-37,12));
+vertex(xW/3-focusedRandom(-2,6),yH/3-focusedRandom(-44,38));
+vertex(xW/4-focusedRandom(-8,4),yH/4-focusedRandom(-37,28));
+vertex(xW/5-focusedRandom(-1,7),yH/5-focusedRandom(-23,18));
+vertex(xW/7-focusedRandom(-3,5),yH/7-focusedRandom(-27,28));
+vertex(xW/10-focusedRandom(-5,7),yH/10-focusedRandom(-13,28));
+vertex(xW/13-focusedRandom(-2,4),yH/13-focusedRandom(-26,18));
+vertex(xW/15-focusedRandom(-3,1),yH/15-focusedRandom(-12,11));
+vertex(xW/17-focusedRandom(-5,7),yH/17-focusedRandom(-13,23));
+vertex(xW/20-focusedRandom(-2,1),yH/20-focusedRandom(-0,0));
+
+endShape();
+}
+
+function mouseClicked() {
+  changeRandomSeed();
+}
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+  //lastSwapTime = millis();
 }
 
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+    else if (key == '@') {
+    saveBlocksImages(true);
+  }
+
 }
