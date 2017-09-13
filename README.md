c5724781e7fd93b9a4364c667b073889c4a6fa0a
HenryLeungVuw <liangyuhe@myvuw.ac.nz>
2017-09-14 00:05:21 +1200

    part1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+My program draw sequences of dots in three colours. And then it generates white lines randomly to coneect the dots with their neighbors.
--- sketch.js
+++ sketch.js
@@ -1,15 +1,58 @@
 function setup () {
   createCanvas(960, 500);
+  fill(0);
+  rect(0,0,960,500);
+  this.generate();
 }
 
 function draw () {
   if (mouseIsPressed) {
-    fill(0);
+    this.generate();
   }
-  else {
-    fill(255);
+}
+
+//The white lines are generated randomly
+function generate(){
+	fill(0);
+    rect(0,0,960,500);
+	for (var i=0; i<20; i++) {
+    for (var j=0; j<10; j++) {
+      stroke(255);
+      strokeWeight(3);
+      if(random(5)<1){
+      line(50*i,25+50*j,50+50*i,25+50*j);
+      }
+      if(random(5)<1){
+        line(50*i,25+50*j,50+50*i,75+50*j);
+      }
+      if(random(5)<1){
+        line(50*i,25+50*j,50*i,75+50*j);
+      }
+      if(random(5)<1){
+        line(50*i,25+50*j,50+50*i,-25+50*j);
+      }
+      noStroke();
+      if(j % 2 == 1 && i % 2 == 1){
+        fill(255,255,255);
+      }
+      else if(i%2==1){
+        fill(255,80,40);
+      }
+      else if(j%2==1){
+        fill(40,255,80);
+      }
+      else{
+        fill(255,255,40);
+      }
+      this.shape(50*i, 25+50*j, 25);
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function shape(posX, posY, size){
+  ellipse(posX, posY, size,size);
+  fill(0);
+  ellipse(posX, posY, size/2,size/2);
 }
 
 function keyTyped() {
