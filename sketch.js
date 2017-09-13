3542ab417f15af6f43132571cffd9079b48ba64c
ShaneW <sunnysideupteam@gmail.com>
2017-09-13 15:09:38 +1200

    Implementing functionality that allows the drawing to run in either auto or manually mode and adding a select to switch between these modes.

--- sketch.js
+++ sketch.js
@@ -1,87 +1,100 @@
-//global variables
-var startX = 0, startY = 0, selector, shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], h, s, b, FROM, TO, slider1, resetButton;
+//default values for MODE and lastChange time
+var MODE = 'auto', lastChange = 0;
+//random variables variables
+var startX = 0, startY = 0, fromColour, toColour;
+//shape options
+var shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'];
+//user control variables
+var selector1, selector2, slider1, resetButton;
 
 function setup () {
-	//set up the canvas
-	var main_canvas = createCanvas(960, 500);
-	main_canvas.parent('canvas-container');
+    //set up the canvas
+    var main_canvas = createCanvas(960, 500);
+    main_canvas.parent('canvas-container');
 
-	// set up the interactive controls
-	selector = createSelect();
-	for(var i=0; i < shapeOptions.length; i++){
-		selector.option(shapeOptions[i]);
-	}
-	selector.value(random(shapeOptions));
-	selector.parent('shape-selector-holder');
-	selector.changed(randomizeGlobalValues);
-	
-	slider1 = createSlider(5, 20, 10);
-	slider1.parent("slider-1-holder");
-	
-	resetButton = createButton('reset');
-	resetButton.mousePressed(resetDrawing);
-	resetButton.parent('reset-button-holder');
-	
-	//set up some of the global options for p5.js
-	background(0);
-	noFill();
-	rectMode(CENTER);
-	colorMode(HSB);
-	frameRate(5);
-	
-	randomizeGlobalValues();
+    // set up the interactive controls
+    selector1 = createSelect();
+    for(var i=0; i < shapeOptions.length; i++){
+        selector1.option(shapeOptions[i]);
+    }
+    selector1.value(random(shapeOptions));
+    selector1.parent('shape-selector-holder');
+
+    slider1 = createSlider(5, 20, 10);
+    slider1.parent("slider-1-holder");
+
+    selector2 = createSelect();
+    selector2.option('auto');
+    selector2.option('manual');
+    selector2.value('auto');
+    selector2.parent('mode-selector-holder');
+    selector2.changed(changeMode);
+
+    resetButton = createButton('reset');
+    resetButton.mousePressed(resetDrawing);
+    resetButton.parent('reset-button-holder');
+
+    //set up some of the global options for p5.js
+    background(0);
+    noFill();
+    rectMode(CENTER);
+    colorMode(HSB);
+    frameRate(5);
+
+    randomizeGlobalValues();
 }
 
 
-	
-function draw () {
-	var to = color('#EF4623');
-	var to = color('#f5ec25');
-	var from = color('#1878B3');
 
-	strokeWeight(0.01);
-	translate(startX, startY);  
-	var x = 0;
-	var count = 0;
-	var shape = selector.value();
-	var shapeSize =  slider1.value();
-	while(x < width){
-		var colour = lerpColor(FROM, TO, count);
-		for (var i = 0; i < 20; i ++) {
-			for (var j = -2; j <=2; j++) {
-				stroke(colour, random(15, 127));
-				stroke(h,s,b, 127 - (j * 16));
-				stroke(colour, 127 - (j * 16));
-				var rand = random(0,100);
-				if(rand < 0){
-					stroke(random(255), random(255), random(255), random(15, 127));
-				}
-				//call the function as detemined by the variable shape
-				//rect and ellipse are built in p5.js
-				//tri,hexa & octa are defined in this file
-				window[shape](x, 20, shapeSize + (j*3), shapeSize + (j*3));
-				//window[shape](x, 20, 10 + j, (1 * x) + j);
-			}
-			rotate(PI/10);
-		}
-		x = x + width/6;
-		count = count + 0.25;
-	}
+function draw () {
+    if(MODE === 'auto' && millis() > (lastChange + 10000)){
+        lastChange = millis();
+        selector1.value(random(shapeOptions));
+        randomizeGlobalValues();
+    }
+    var to = color('#EF4623');
+    var to = color('#f5ec25');
+    var from = color('#1878B3');
+
+    strokeWeight(0.01);
+    translate(startX, startY);
+    var x = 0;
+    var lerpAmount = 0;
+    var shape = selector1.value();
+    var shapeSize =  slider1.value();
+    var colour = lerpColor(fromColour, toColour, lerpAmount);
+    while(x < width){
+        for (var i = 0; i < 20; i ++) {
+            for (var j = -2; j <=2; j++) {
+                stroke(colour, 181 - (j * 16));
+                //call the function as detemined by the variable shape
+                //rect and ellipse are built in p5.js
+                //tri,hexa & octa are defined in this file
+                window[shape](x, 20, shapeSize + (j*3), shapeSize + (j*3));
+            }
+            rotate(PI/10);
+        }
+        x = x + width/6;
+        lerpAmount = lerpAmount + 0.25;
+        colour = lerpColor(fromColour, toColour, lerpAmount);
+    }
 }
 
 function mousePressed() {
-print('sdfdf');
-	if (mouseButton == LEFT){
-		selector.value(random(shapeOptions));
-		randomizeGlobalValues(mouseX, mouseY);
-	}
+    if (mouseButton == LEFT && mouseY <= 500){
+        randomizeGlobalValues(mouseX, mouseY);
+    }
 }
 
 function resetDrawing(){
-	translate(-startX, -startY); 
-	clear();
-	background(0);
-	randomizeGlobalValues();
+    translate(-startX, -startY);
+    clear();
+    background(0);
+    randomizeGlobalValues();
+}
+
+function changeMode(){
+    MODE = selector2.value();
 }
 
 var hueRanges = {
@@ -92,29 +105,28 @@ var hueRanges = {
         5 : [211, 270, 240],
         6 : [271, 330, 300]
     }
-	
+
 function randomizeGlobalValues(setX = false, setY = false){
-	var randomPointer = floor(random(1, 7));
-	h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
-	s = random(100, 100);
-	b = random(100, 100);
-	FROM = color(h, s, b);
-	randomPointer = randomPointer + 3;
-	if(randomPointer > 6){
-		randomPointer = randomPointer -6;
-	}
-	h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
-	s = random(100, 100);
-	b = random(100, 100);
-	TO = color(h, s, b);
-	startX = random(0, width); 
-	if(setX){
-		startX = setX;
-	}
-	startY = random(0, height);
-	if(setY){
-		startY = setY;
-	}
+    //set the from and to colours
+    var randomPointer = floor(random(1, 7));
+    var h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
+    fromColour = color(h, 100, 100);
+    randomPointer = randomPointer + 3;
+    if(randomPointer > 6){
+        randomPointer = randomPointer -6;
+    }
+    h = focusedRandom(hueRanges[randomPointer][0], hueRanges[randomPointer][1], 10, hueRanges[randomPointer][2]);
+    toColour = color(h, 100, 100);
+
+    //set the start position of next pattern
+    startX = random(0, width);
+    if(setX){
+        startX = setX;
+    }
+    startY = random(0, height);
+    if(setY){
+        startY = setY;
+    }
 }
 
 
@@ -143,16 +155,16 @@ function equilateral(x, y, width) {
  * @param {Number} radius   - radius of the hexagon
  */
 function hexa(x, y, radius) {
-	radius = radius / 2;
-	angleMode(RADIANS);
-	var angle = TWO_PI / 6;
-	beginShape();
-	for (var a = TWO_PI/12; a < TWO_PI + TWO_PI/12; a += angle) {
-	var sx = x + cos(a) * radius;
-	var sy = y + sin(a) * radius;
-		vertex(sx, sy);
-	}
-	endShape(CLOSE);
+    radius = radius / 2;
+    angleMode(RADIANS);
+    var angle = TWO_PI / 6;
+    beginShape();
+    for (var a = TWO_PI/12; a < TWO_PI + TWO_PI/12; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
 
 /*
@@ -163,16 +175,16 @@ function hexa(x, y, radius) {
  * @param {Number} radius   - radius of the octagon
  */
 function octa(x, y, radius) {
-	radius = radius / 2;
-	angleMode(RADIANS);
-	var angle = TWO_PI / 8;
-	beginShape();
-	for (var a = TWO_PI/16; a < TWO_PI + TWO_PI/16; a += angle) {
-	var sx = x + cos(a) * radius;
-	var sy = y + sin(a) * radius;
-		vertex(sx, sy);
-	}
-	endShape(CLOSE);
+    radius = radius / 2;
+    angleMode(RADIANS);
+    var angle = TWO_PI / 8;
+    beginShape();
+    for (var a = TWO_PI/16; a < TWO_PI + TWO_PI/16; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
 
 
@@ -181,5 +193,3 @@ function keyTyped() {
     saveBlocksImages();
   }
 }
-
-
