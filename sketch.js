afdc06bc0466afde3043e09c9a5b03bf87761d43
hannahdockerty <dockerhann@myvuw.ac.nz>
2017-09-14 15:41:08 +1200

    refine lilypad shape

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
 var nSeed;
+var rand = 30;
 
 function setup() {
   createCanvas(960, 500);
@@ -20,11 +21,52 @@ function generateShape(){
     var rad = 200 * noise(i * 0.01, i*0.01);
     var x = rad * cos(ang);
     var y = rad * sin(ang);
-    curveVertex(x, y);
 
-    line(0, 0, x, y);
+      if (i < 11 && i > 0){
+      x+= rand+((i/3)-15);
+    }else if (i==11){
+      y+= 4;
+      x+=2;
+    }
+    else if (i==12){
+      y+= i-9;
+    }else if (i==13){
+      y+= i-11;
+    }else if(i ==49){
+      x-=5;
+    }
+
+
+
+    curveVertex(x, y);
   }
   endShape(CLOSE);
+
+  stroke(219, 237, 154);
+
+  for (var i = 0; i < 50; i++) {
+        var ang = map(i, 0, 50, 0, TWO_PI);
+    var rad = 200 * noise(i * 0.01, i*0.01);
+    var x = rad * cos(ang);
+    var y = rad * sin(ang);
+
+        if (i < 11 && i > 0){
+      x+= rand+((i/3)-15);
+    }else if (i==11){
+      y+= 4;
+      x+=2;
+    }
+    else if (i==12){
+      y+= 1;
+    }
+
+
+
+    
+    if (i%2 == 1){
+    line(45, 0, x, y);
+    }
+}
  
   
 }
@@ -36,9 +78,6 @@ function draw() {
   translate(width/2, height/2);
   generateShape();
   pop();
-
-  
-
 }
 
 
@@ -46,7 +85,9 @@ function draw() {
 function refreshNoise(){
   nSeed += 1;
    noiseSeed(nSeed);
-   console.log(nSeed);
+
+   rand = random(25, 35);
+ 
 }
 
 function mousePressed(){
