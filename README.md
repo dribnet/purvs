0706a610d21532a905b906d15ea0ce85a492daae
ChristiaanSteenkamp <cjsteenkamp22@gmail.com>
2017-09-14 13:09:49 +1200

    save

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-17.2.MDDN342 PS3
-
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+A bit of a 70's feel using circles that will endlessly go
--- sketch.js
+++ sketch.js
@@ -1,15 +1,45 @@
+var Xaxis = 0;
+var Yaxis = 0;
+var num = 0;
+var color1 = 216;
+var color2 = 103
+var color3 = 7;
 function setup () {
   createCanvas(960, 500);
 }
 
+
 function draw () {
+  noFill();
   if (mouseIsPressed) {
-    fill(0);
+    Xaxis = 0;
   }
   else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  while(Xaxis < 1040){
+    while(Yaxis < 580){
+      num = focusedRandom(10,80);
+      fill(color1 + num, color2 + num, color3 + num);
+      ellipse(Xaxis, Yaxis, 80, 80);
+
+      num = focusedRandom(10,80);
+      fill(color1 + num, color2 + num, color3 + num);
+      ellipse(Xaxis, Yaxis, 60, 60);
+
+      num = focusedRandom(10,80);
+      fill(color1 + num, color2 + num, color3 + num);
+      ellipse(Xaxis, Yaxis, 40, 40);
+
+      num = focusedRandom(10,80);
+      fill(color1 + num, color2 + num, color3 + num);
+      ellipse(Xaxis, Yaxis, 20, 20);
+      Yaxis += 40
+    }
+    Yaxis = 0
+    Xaxis+= 40
+  }
+  
 }
 
 function keyTyped() {
