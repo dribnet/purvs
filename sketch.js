03fe1e6599096563286a5900a6f9ddbd71eedf84
Shane Watters <shane.watters.nz@gmail.com>
2017-09-14 15:16:33 +1200

    I have added another radioset to control the number iterations when drawing a pattern. I have also added a stop button so you turn the loop functionality off once you have a pattern that you like.

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var MODE = 'auto', lastChange = 0, changeFrequency = 10;
 //random variables variables
 var startX = 0, startY = 0, fromColour, toColour;
 //control options
-var shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], rotationOptions = [3,4,6,8,12];
+var shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], rotationOptions = [3,4,6,8,12], iterationOptions = [1, 3,4,6,8,12,24,48];
 //user control variables
 var selector1, selector2, slider1, radio, resetButton;
 
@@ -11,6 +11,21 @@ function setup () {
     //set up the canvas
     var main_canvas = createCanvas(960, 500);
     main_canvas.parent('canvas-container');
+
+    selector2 = createSelect();
+    selector2.option('auto');
+    selector2.option('manual');
+    selector2.value('auto');
+    selector2.parent('mode-selector-holder');
+    selector2.changed(changeMode);
+
+    resetButton = createButton('reset');
+    resetButton.mousePressed(resetDrawing);
+    resetButton.parent('button-holder');
+
+    resetButton = createButton('stop');
+    resetButton.mousePressed(stopDrawing);
+    resetButton.parent('button-holder');
 
     // set up the interactive controls
     selector1 = createSelect();
@@ -23,30 +38,27 @@ function setup () {
     slider1 = createSlider(5, 20, 10);
     slider1.parent("slider-1-holder");
 
-    radio = createRadio();
+    radio1 = createRadio();
     for(var i=0; i < rotationOptions.length; i++){
-        radio.option(rotationOptions[i]);
+        radio1.option(rotationOptions[i]);
     }
-    radio.value(random(rotationOptions));
-    radio.parent("radio-holder");
+    radio1.value(random(rotationOptions));
+    radio1.parent("radio-1-holder");
 
-    selector2 = createSelect();
-    selector2.option('auto');
-    selector2.option('manual');
-    selector2.value('auto');
-    selector2.parent('mode-selector-holder');
-    selector2.changed(changeMode);
-
-    resetButton = createButton('reset');
-    resetButton.mousePressed(resetDrawing);
-    resetButton.parent('reset-button-holder');
+    radio2 = createRadio();
+    for(var i=0; i < iterationOptions.length; i++){
+        radio2.option(iterationOptions[i]);
+    }
+    radio2.value(random(iterationOptions));
+    radio2.parent("radio-2-holder");
 
     //set up some of the global options for p5.js
     background(0);
     noFill();
+    strokeWeight(0.01);
     rectMode(CENTER);
     colorMode(HSB);
-    frameRate(8);
+    frameRate(5);
 
     randomizeGlobalValues();
 }
@@ -58,19 +70,26 @@ function draw () {
     if(MODE === 'auto' && millis() > (lastChange + (changeFrequency * 1000))){
         lastChange = millis();
         changeFrequency = random(5, 10);
-        selector1.value(random(shapeOptions));
-        slider1.value(random(5, 20));
-        radio.value(random(rotationOptions));
         randomizeGlobalValues();
     }
+    drawPattern();
+}
 
-    strokeWeight(0.01);
+function mousePressed() {
+    if (mouseButton == LEFT && mouseY <= 500 && mouseX <= 960){
+        lastChange = millis();
+        randomizeGlobalValues(mouseX, mouseY);
+    }
+}
+
+function drawPattern(){
     translate(startX, startY);
     var x = 0;
     var lerpAmount = 0;
     var shape = selector1.value();
     var shapeSize =  slider1.value();
-    var numOfRotations = radio.value();
+    var numOfRotations = radio1.value();
+    var numOfIterations = radio2.value();
     var colour = lerpColor(fromColour, toColour, lerpAmount);
     while(x < width){
         for (var i = 0; i < (numOfRotations * 2); i ++) {
@@ -83,60 +102,9 @@ function draw () {
             }
             rotate(PI/numOfRotations);
         }
-        x = x + width/3;
-        lerpAmount = lerpAmount + (1/3);
+        x = x + width/numOfIterations;
+        lerpAmount = lerpAmount + (1/numOfIterations/2);
         colour = lerpColor(fromColour, toColour, lerpAmount);
-    }
-}
-
-function mousePressed() {
-    if (mouseButton == LEFT && mouseY <= 500){
-        lastChange = millis();
-        randomizeGlobalValues(mouseX, mouseY);
-    }
-}
-
-function resetDrawing(){
-    translate(-startX, -startY);
-    clear();
-    background(0);
-    randomizeGlobalValues();
-}
-
-function changeMode(){
-    MODE = selector2.value();
-}
-
-var hueRanges = {
-        1 : [-30, 30, 0],
-        2 : [31, 90, 60],
-        3 : [91, 150, 120],
-        4 : [151, 210, 180],
-        5 : [211, 270, 240],
-        6 : [271, 330, 300]
-    }
-
-function randomizeGlobalValues(setX = false, setY = false){
-    //set the from and to colours
-    var randomPointer1 = floor(random(1, 7));
-    var h = focusedRandom(hueRanges[randomPointer1][0], hueRanges[randomPointer1][1], 10, hueRanges[randomPointer1][2]);
-    fromColour = color(h, 100, 100);
-    var randomPointer2 = floor(random(1, 7));
-    //this is used to make sure the second random pointer is not the same as the first one
-    while(randomPointer2 == randomPointer1) {
-        randomPointer2 = floor(random(1, 7));
-    }
-    h = focusedRandom(hueRanges[randomPointer2][0], hueRanges[randomPointer2][1], 10, hueRanges[randomPointer2][2]);
-    toColour = color(h, 100, 100);
-
-    //set the start position of next pattern
-    startX = random(0, width);
-    if(setX){
-        startX = setX;
-    }
-    startY = random(0, height);
-    if(setY){
-        startY = setY;
     }
 }
 
@@ -198,6 +166,61 @@ function octa(x, y, radius) {
     endShape(CLOSE);
 }
 
+function randomizeGlobalValues(setX = false, setY = false){
+    if(MODE === 'auto'){
+        selector1.value(random(shapeOptions));
+        slider1.value(random(5, 20));
+        radio1.value(random(rotationOptions));
+        radio2.value(random(iterationOptions));
+    }
+
+    var hueRanges = {
+        1 : [-30, 30, 0],
+        2 : [31, 90, 60],
+        3 : [91, 150, 120],
+        4 : [151, 210, 180],
+        5 : [211, 270, 240],
+        6 : [271, 330, 300]
+    }
+
+    //set the from and to colours
+    var randomPointer1 = floor(random(1, 7));
+    var h = focusedRandom(hueRanges[randomPointer1][0], hueRanges[randomPointer1][1], 10, hueRanges[randomPointer1][2]);
+    fromColour = color(h, 100, 100);
+    var randomPointer2 = randomPointer1 +3;
+    if(randomPointer2 > 6){
+        randomPointer2 = randomPointer2 - 6;
+    }
+    h = focusedRandom(hueRanges[randomPointer2][0], hueRanges[randomPointer2][1], 10, hueRanges[randomPointer2][2]);
+    toColour = color(h, 100, 100);
+
+    //set the start position of next pattern
+    startX = random(0, width);
+    if(setX){
+        startX = setX;
+    }
+    startY = random(0, height);
+    if(setY){
+        startY = setY;
+    }
+}
+
+
+function resetDrawing(){
+    translate(-startX, -startY);
+    clear();
+    background(0);
+    randomizeGlobalValues();
+    loop();
+}
+
+function stopDrawing(){
+    noLoop();
+}
+
+function changeMode(){
+    MODE = selector2.value();
+}
 
 function keyTyped() {
   if (key == '!') {
