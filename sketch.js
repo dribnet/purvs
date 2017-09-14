0fa8bf1b883dd1dd9c9e637fc4323f1bc3fe2346
ElectricPowerHouse <acidplaystf2@gmail.com>
2017-09-14 22:41:35 +1200

    iteration 2: added layers and an initial color scheme

--- sketch.js
+++ sketch.js
@@ -11,24 +11,41 @@ var shapeColors;
 
 function setup () {
   createCanvas(960, 500);
+  background(255);
   colWidth = width/cols;
   rowWidth = height/rows;
   shapeNum = 10;
 
   shapeTypes = new Array(rows);
+  shapeTypes2 = new Array(rows);
+  shapeTypes3 = new Array(rows);
   shapeColors = new Array(rows);
+  r = new Array(rows);
+  g = new Array(rows);
+  b = new Array(rows);
   shapeSize = new Array(rows);
   shapeRotate = new Array(rows);
 
+
   for(var row = 0; row< rows; row++){
       shapeTypes[row]=new Array(cols);
+      shapeTypes2[row]=new Array(cols);
+      shapeTypes3[row]=new Array(cols);
       shapeColors[row]=new Array(cols);
+      r[row] = new Array(cols);
+      g[row] = new Array(cols);
+      b[row] = new Array(cols);
       shapeSize[row]=new Array(cols);
       shapeRotate[row]=new Array(cols);
     for(var col = 0; col< cols; col++){
 
       shapeTypes[row][col] = 0;
+      shapeTypes2[row][col]= 0;
+      shapeTypes3[row][col]= 0;
       shapeColors[row][col] = 0;
+      r[row][col] = 0;
+      g[row][col] = 0;
+      b[row][col] = 0;
       shapeSize[row][col] = 0;
       shapeRotate[row][col] = 0;
 
@@ -36,65 +53,12 @@ function setup () {
   }
 
   randomizeValues();
+  drawWallpaper();
 
 
 }
 
 function draw () {
-
-  background(255);
-  strokeWeight(1);
-
-
-
-
-    for(var row = 0; row< rows-1; row++){
-      for(var col = 0; col< cols-1; col++){
-
-
-          push();
-          fill(shapeColors[row][col],50);
-          stroke(0);
-          strokeWeight(1);
-
-          sSize = shapeSize[row][col];
-          sRotate = shapeRotate[row][col];
-
-          rotate(radians(sRotate));
-
-          var shapeType = shapeTypes[row][col];
-
-          if(shapeType == 1){
-            rect(col*colWidth+(colWidth/2),row*rowWidth+(rowWidth/2),sSize,sSize);
-          }
-          else if(shapeType == 2){
-            //noFill();
-            rect(col*colWidth,row*rowWidth,sSize,sSize);
-          }
-          else if(shapeType == 3){
-
-            ellipse(col*colWidth,row*rowWidth,sSize,sSize);
-            line(col*colWidth+(colWidth/2),row*rowWidth+(rowWidth/2),((col+1)*colWidth)+(colWidth/2),(row+1)*rowWidth+(rowWidth/2));
-          }
-          else if(shapeType == 4){
-              ellipse(col*colWidth,row*rowWidth,sSize,sSize);
-          }
-          else if (shapeType== 5){
-            line(col*colWidth,row*rowWidth+(rowWidth/2),(col+1)*colWidth,row*rowWidth+(rowWidth/2));
-          }
-
-
-          pop();
-
-      }
-
-    }
-
-
-    strokeWeight(5);
-    noFill();
-    //ellipse(width-(width*0.25),height,height*0.4,height*0.4);
-
 
 }
 
@@ -108,6 +72,7 @@ function keyTyped() {
 function mouseClicked(){
 
     randomizeValues();
+    drawWallpaper();
 
 }
 
@@ -118,7 +83,14 @@ function randomizeValues(){
 
       randomNum = round(random(0,shapeNum));
       shapeTypes[row][col] = randomNum;
-      shapeColors[row][col] = random(0,150);
+      randomNum = round(random(0,shapeNum));
+      shapeTypes2[row][col] = randomNum;
+      randomNum = round(random(0,shapeNum));
+      shapeTypes3[row][col] = randomNum;
+      shapeColors[row][col] = random(0,255);
+      r[row][col] = random(0,255);
+      g[row][col] = random(0,255);
+      b[row][col] = random(0,255);
       shapeSize[row][col] = (colWidth/2)* round(random(1,3));
       shapeRotate[row][col] = random(-10,10);
       //shapeSize[row][col]
@@ -126,6 +98,81 @@ function randomizeValues(){
     }
 
   }
+
+}
+
+
+function drawWallpaper(){
+
+  //background(255);
+  background(237, 245, 255);
+
+  drawLayer(30,shapeTypes);
+  drawLayer(80,shapeTypes2);
+  drawLayer(150,shapeTypes3);
+
+
+}
+
+
+function drawLayer(opac,types){
+
+
+  strokeWeight(1);
+
+    for(var row = 0; row< rows-1; row++){
+      for(var col = 0; col< cols-1; col++){
+
+          push();
+          fill(r[row][col],g[row][col],255,opac*0.3);
+          stroke(r[row][col],g[row][col],255,opac);
+          strokeWeight(1);
+
+          sSize = shapeSize[row][col];
+          sRotate = shapeRotate[row][col];
+
+          rotate(radians(sRotate));
+
+          var shapeType = types[row][col];
+          var leftX = col*colWidth;
+          var rightX = (col+1)*colWidth;
+          var topY = row*rowWidth;
+          var bottomY = (row+1)*rowWidth;
+
+
+          if(shapeType == 1){
+            rect(leftX+(colWidth/2),topY+(rowWidth/2),sSize,sSize);
+          }
+          else if(shapeType == 2){
+            //noFill();
+            rect(leftX,topY,sSize,sSize);
+          }
+          else if(shapeType == 3){
+
+            //ellipse(col*colWidth,row*rowWidth,sSize,sSize);
+            line(leftX+(colWidth/2),topY+(rowWidth/2),rightX+(colWidth/2),bottomY+(rowWidth/2));
+            line(rightX+(colWidth/2),topY+(rowWidth/2),leftX+(colWidth/2),bottomY+(rowWidth/2));
+          }
+          else if(shapeType == 4){
+              ellipse(leftX,topY,sSize,sSize);
+          }
+          else if (shapeType== 5){
+            line(leftX,topY+(rowWidth/2),rightX,topY+(rowWidth/2));
+          }
+          else if(shapeType==6){
+            quad(leftX,topY+(rowWidth/2),leftX+(colWidth/2),topY,rightX,topY+(rowWidth/2),leftX+(colWidth/2),bottomY);
+          }
+          else if(shapeType==7){
+            line(leftX+(colWidth/2),topY+(rowWidth/2),rightX+(colWidth/2),bottomY+(rowWidth/2));
+          }
+
+
+          pop();
+
+      }
+
+    }
+
 
 
 
