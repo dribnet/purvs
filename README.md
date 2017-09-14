f49c5c0816cb9d650d95812089aec859891a16da
Guest User <Guest@b-03-05-lab.mddn.vuw.ac.nz>
2017-09-14 12:57:16 +1200

    initial

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+*This is a test for PS3 to test different tessellations. I am exploring the theory of wallpaper groups. 
+All possible permutations of patterns can be mathematically defined by 17 sets of rules called Wallpaper groups. In my experimentation I want to define a wallpaper group to work within before I make the design.*
+
+
+#### Wallpaper Groups
+A table with descriptions and diagrams of the wallpaper groups can be found [on wikipedia](https://commons.wikimedia.org/wiki/Wallpaper_group_diagrams)
+Clark University has a less mathematical explaination. [here](http://www2.clarku.edu/~djoyce/wallpaper/seventeen.html)
+
+A repeating pattern can always be defined by a quadrilateral polygon in which all opposite sides are parallel. ![](https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Wallpaper_group_diagram_pgg.svg/150px-Wallpaper_group_diagram_pgg.svg.png)
+
+Translation, rotation, reflection, and *gliding* (translation+reflection) of elements wihin this space is what characterises the 17 wallpaper groups.
+
+
+#### The Sketch
+The first test will be based on the 'p1' group: one with no rotation, reflection, or glides. 
+I want to try to write re-usable code when creating the p1 wallpaper. p1 is the simplest wallpaper group, and this exercise is and opportunity to make a flexible method of tiling an arbitrary quadrilateral poygon.
--- sketch.js
+++ sketch.js
@@ -1,19 +1,81 @@
+var col1 = "#f4eb42"
 function setup () {
   createCanvas(960, 500);
+  angleMode(DEGREES);
+  var origin = [width/2,height/2];
 }
 
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
+  background(225);
+  stroke(245);
+  fill(col1); 
+  beginShape();
+  for(vari=0; i<6; i++){
+
   }
-  else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  endShape();
+
 }
 
+
+function mousePressed(){
+
+} 
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
+
+
+function debugGrid(inc, no){ //increment size, number of increments
+  var lineSize;
+  if (width<height){
+    lineSize = height;
+  } 
+  else{
+    lineSize = width;
+  }
+  for(var i= -no;i<no;i++){
+    push();
+    translate(width/2,height/2 + i*inc);
+    line(-lineSize,0,lineSize,0);
+    pop();
+
+
+  }
+}
+
+function triOnEdge(x1,y1,x2,y2,B,C){ // returns a point given two other points and an angle relative to each
+	//lower case == angles
+	//upper case == side lengths
+	if(B+C < 180){
+		var a = distanceBetween([x1,y1],[x2,y2]);
+		var A = 180 - B - C;
+		var b = (a*sinOf(B))/sinOf(A);
+		var c = (a*sinOf(C))/sinOf(A);
+		console.log(findNewPoint(x1,y1,B,c));
+		console.log(findNewPoint(x2,y2,C,b));
+		return findNewPoint(x1,y2,B,c);
+	}
+}
+
+function distanceBetween(p1, p2) {
+    var a = p1[0] - p2[0];
+    var b = p1[1] - p2[1];
+    var c = Math.abs(Math.sqrt(a * a + b * b));
+    return c;
+}
+
+function findNewPoint(x, y, angle, distance) {
+    var result =[];
+
+    result[0] = Math.round(Math.cos(angle * Math.PI / 180) * distance + x);
+    result[1] = Math.round(Math.sin(angle * Math.PI / 180) * distance + y);
+
+    return result;
+}
+
+function sinOf(degrees){
+	return Math.sin(degrees*Math.PI/180);
 }
