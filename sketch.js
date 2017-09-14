7e1597824a36d8d1c56cdabb4fb54e602195db21
hannahdockerty <dockerhann@myvuw.ac.nz>
2017-09-14 14:10:19 +1200

    starting point

--- sketch.js
+++ sketch.js
@@ -1,15 +1,45 @@
+
+var curRandomSeed;
+
 function setup () {
+  curRandomSeed = int(focusedRandom(0, 100));
   createCanvas(960, 500);
 }
 
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+}
+
 function draw () {
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // }
+  // else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+
+  background(255);
+
   if (mouseIsPressed) {
-    fill(0);
+    changeRandomSeed();
   }
-  else {
-    fill(255);
+  resetFocusedRandom(curRandomSeed);
+
+  var x_steps = width / 30;
+  var y_steps = height / 30;
+
+  // for(var i=0;i<x_steps;i+=2) {
+  //   var shift = focusedRandom(-20, 20, 6);
+  //   var cur_x = i * 30 + shift;
+  //   line(cur_x, 0, cur_x, height);
+  // }
+
+  for(var j=0;j<y_steps;j+=2) {
+    var shift = focusedRandom(-10, 10, 6);
+    var cur_y = j * 30 + shift;
+    line(0, cur_y, width, cur_y);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
 
 function keyTyped() {
