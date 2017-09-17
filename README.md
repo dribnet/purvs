4bb16087e062c213823e8648b0628ffdf6bd6e77
Shakira Jensen <shakirajensencreative@gmail.com>
2017-09-18 09:50:25 +1200

    pattern base finised

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 17.2.MDDN342 PS3
 
-The base of my mid century modern wallpaper style is coming together. I've got singluar lines of diamonds and the next step is to vary them randomly and add different shapes in the backgroud. 
-
-I've used translation and rotation to achieve what I've got so far and I think this is the most effective way. I initially was working with verticies but getting them into a grid loop proved pretty difficult!
+The code now runs with randomised shapes and variable dots for each diamond size. I decided to make a few of the squares look like they were in the background because I think this will look good with my mid century themed shapes in between the foreground and the background.
--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var curRandomSeed;
 
+
 function setup (){
     curRandomSeed = int(focusedRandom(0, 100));
-    createCanvas(960, 500);
+    createCanvas(907, 500);
 }
 
 function changeRandomSeed() {
@@ -16,24 +17,67 @@ function draw (){
     var x = 0;
     var y = 0;
 
+// draws lines behind the diamonds
+    strokeWeight(0.5);
+    line(226.5, 0, 226.5, height);
+    line(452.5, 0, 452.5, height);
+    line(678.5, 0, 678.5, height);
+
+
+
+// draws rows
 rotate(radians(-45))
-for (var i = 0; i < 2060; i = i+80) { 
-  for (var j = 0; j < 1500; j = j+80) {
+for (var i = 0; i < 560; i = i+80) { 
+  for (var j = 0; j < 500; j = j+80) {
+    var dimensions = random(100, 150, 50);
+    var doubleDots = random(50, 100);
+
     push();
     translate(x, y);
     rotate(radians(90));
     noFill();
     stroke(0);
-    rect(x, y, focusedRandom(80, 85), focusedRandom(80, 85));
+    strokeWeight(0.5);
+
+    // diamonds that are seperated
+    rect(x, y, dimensions, dimensions);
+
+    push();
+    stroke(200);
+    strokeWeight(0.1);
+    rectMode(CENTER);
+    rect(x, y, dimensions, dimensions);
     pop();
+
+    noStroke();
+    fill(145, 187, 255);
+    // random dots around the pattern
+    // dots in center of points
+    if(dimensions < 120){
+    ellipseMode(CENTER);
+    ellipse(x, y, 10, 10);
+    }
+
+    // dots on top verticies of rects
+    if(dimensions > 140){
+    ellipseMode(CENTER);
+    ellipse(x, (y+doubleDots), 7, 7);
+    }
+
+    if(dimensions > 140){
+    ellipseMode(CENTER);
+    ellipse((x+doubleDots), y, 7, 7);
+    }
+
+    pop();
+
+
     x += 160;
     }
 x=0;
 y += 80; 
 
 }
-
-    //line(30, 0, 30, 60);
     // beginShape();
     // vertex(30, 0); // top point
     // vertex(60, 30);   // right point
