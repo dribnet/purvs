dad008165a64da8d705a37abd5d4b2d5177d73b0
DERIZERD <djntideman@gmail.com>
2017-09-18 17:20:41 +1200

    Land pattern started. Not sure how I want to assemble islands yet but I have the base pattern.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup () {
   createCanvas(960, 500);
-  curRandomSeed = focusedRandom(0, 1);
+  curRandomSeed = focusedRandom(0, 100);
 }
 
 function changeRandomSeed() {
@@ -18,8 +18,19 @@ function draw () {
  background(200);
   //rotateX(frameCount * 0.01);
   //rotateY(frameCount * 0.01);
+  //oceanwave();
+  islands();
+  
+}
 
-  fill(0,204,204);
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
+
+function oceanwave(){
+fill(0,204,204);
   stroke(255);
   strokeWeight(3);
   var w = 0;
@@ -42,10 +53,35 @@ function draw () {
 		w = w + 1;
 		//pop();
 	}
+
+
+
 }
 
-function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
+function islands(){
+fill(153,76,0);
+	stroke(76,153,0);
+	strokeWeight(4);
+
+	var w = 0;
+  var i = 0;
+  
+  	w = 0;
+	while(w < 100){
+		i = 0;
+  		while(i < 100){
+  		push();
+  		
+  		translate(i*15 - 100 + ((i+w) * curRandomSeed/20),w*20 - 50);
+  		rotate(frameCount * 0,01);
+  		//ellipsoid(50,50,50);
+  		quad(0,25,50,55,65,35,100,100);
+  		pop();
+  		i++;
+		}
+		
+		w = w + 1;
+		//pop();
+	}
+
 }
