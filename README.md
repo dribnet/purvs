75fae1aa282005d661c51fb251a91888380ab1bf
Henri Spencer <henrimspence@gmail.com>
2017-09-18 12:39:26 +1200

    Still trying to figure out how to make my pattern interesting, symmetrical but also able to be looped infinitely.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Initial trial code to tile. This was a trial to bring elements of my code into the pattern tile shown in class.
 
-This is just a quick sketch to try get an idea together. To zoom out from this and create a pattern like a spider web could have potential.
+This is just a quick sketch to try get an idea together. I am struggling with getting something I am happy with, but still hope to be able to make some sort of web pattern.
--- sketch.js
+++ sketch.js
@@ -6,6 +6,14 @@ curRandomSeed = int(focusedRandom(0, 100));
 
   var gridSize = 10;
 
+  // henriFunShape = createShape();
+  // henriFunShape.beginShape();
+  // 	//All vertexs go here
+  // 	star.vertex(10,40);
+  // 	star.vertex(20,60);
+  // 	star.vertex(30,30);
+  // henriFunShape.endShape(CLOSE);
+
 
 }
 
@@ -50,40 +58,69 @@ function mousePressed() {
       var y1 = y_grid_locations[j];
       var y2 = y_grid_locations[j+1];
 
-      rect(x1-15, y1-15, x2-15, y2-15);
-
+      //rect(x1, y1, x2, y2);
+      //henriFunShape.display();
      
-      translate (width/4, height/4);
-      noStroke();
-      fill (120);
-      rectMode(CENTER);
-      translate(width/4, height/1.5);
-      	//rotate(PI/0.9);
-      	//rect (480, 250, 10, 50);
-      	//translate(width/4, height/4);
-      rotate(PI/2.0);
-      	//rect (240, 250, 5, 55);
-      translate(width/4, height/4);
-      rotate(PI/3.0);
-      	//rect (180, 250, 25, 25);
-      	//translate(width/4, height/4);
-      rotate(PI/4.0);
-      	//rect (320, 250, 55, 55);
-      translate(width/8, height/4);
-      rotate(PI/5.0);
-      ellipse (480, 250, 5, 15);
+      //noStroke();
+     // fill (120);
+     // rectMode(CENTER);
+      //translate(width, height);
+      // /rotate(PI/0.9);
+  
+     //for(var k = 0; k <= 10; k++){
+     	beginShape();
+     	fill(focusedRandom(200,0,0));
+     	vertex(x1, y1);
+     	var foo = Math.random() * (6 - 3) + 3   
+     	for(var k = 0;k < foo;k++){
+     		vertex(x1 + 1*k, y1 + 1*k);
+     		}
+		//vertex(x2, y2);
+		//vertex(x2 - 50, y2 - 50);
+		//vertex(x1 + 30 , y2 + 30);
+		//vertex(x1 + 40, y1 + 40);
+     	//rotate(PI/Math.random());
+     	triangle(x1 + k, y1 + k, x2, y2 , x1, y1);
+     	//triangle(x1 - k, y1 - k, x2, y2 , x1, y1);
+     	endShape();
+     	//rotate(PI/0.3)
+ 		}	
 
-      fill(255);
-      //rotate(PI/3.0);
-      rect(x1+2, y1-5, 3, 10);
-      //rotate(PI/3.0);
-      rect(x1-2, y1-2, 10, 3);
-      stroke(255, 50);
-      line(x1, y1, width/2, height/2);
+  	  //rotate(PI/2.0);
+      // rotate(PI/3.0);
+      //rotate(PI/4.0);
+      //ellipse (480, 250, 5, 15);
+
+      //fill(255);
+      // //rotate(PI/3.0);
+     // rect((x1/25, y1/25, x2/25, y2/25));
+      // //rotate(PI/3.0);
+      // rect(x1-2, y1-2, 10, 3);
+      // stroke(255, 50);
+      // line(x1, y1, width/2, height/2);
 
     	}
-  	}
-
+  	//}
+// void display () {
+//   pushMatrix();
+//   translate(x, y);
+//   fill(102);
+//   stroke(255);
+//   strokeWeight(2);
+//   beginShape();
+//   vertex(0, -50);
+//   vertex(14, -20);
+//   vertex(47, -15);
+//   vertex(23, 7);
+//   vertex(29, 40);
+//   vertex(0, 25);
+//   vertex(-29, 40);
+//   vertex(-23, 7);
+//   vertex(-47, -15);
+//   vertex(-14, -20);
+//   endShape(CLOSE);
+//   popMatrix();
+// }
 
 
   // for (var x = gridSize; x <= width + gridSize; x += gridSize) {
