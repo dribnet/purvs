ec5d86230880e8968d2502e9bf9f94303b525f4c
Robbie Carmichael <robbi3reg@gmail.com>
2017-09-18 16:02:10 +1200

    updated images and code with my work for part 1 of assignment 3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+PART 1: Iterative Pattern (Wallpaper)
+
+I have used a series of for loops to generate my tiling patern for part 1 of assignment 3. I tried to design something that could not only be infinitely tiled but would look good at different scales (small diamonds or large diamonds). I narrowed the colours down to just two - which could be interchanged depending on the colours you wanted for your fabric, or to have as wallpaper in your room - as well as playing with stroke opacity for the final effect. 
+
--- sketch.js
+++ sketch.js
@@ -2,14 +2,47 @@ function setup () {
   createCanvas(960, 500);
 }
 
+
+
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
+
+  strokeWeight(2);
+  stroke(255, 198, 203);
+  fill(255);
+  
+  ellipseMode(CENTER);
+
+  var x = 0;
+  var y = 0;
+
+  for (var x = 0; x <= width; x += 10) {
+    for (var y = 0; y <= height; y += 10) {
+     ellipse(x, y, 10, 10);  
+    }     
   }
-  else {
-    fill(255);
+
+  var i = 0;
+  
+  stroke(255, 100);
+  strokeWeight(75);
+  for (var i = 0; i <= width + width/2; i += 200) {
+    line(i - width/2, -50, 0 + i, height+50);   
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  for (var i = 0; i <= width + width/2; i += 200) {
+    line(i, -50, -width/2 + i, height+50);   
+  }
+
+  stroke(255);
+  strokeWeight(25);
+  for (var i = 0; i <= width + width/2; i += 200) {
+    line(i - width/2, -50, 0 + i, height+50);   
+  }
+
+  for (var i = 0; i <= width + width/2; i += 200) {
+    line(i, -50, -width/2 + i, height+50);   
+  }
+
 }
 
 function keyTyped() {
