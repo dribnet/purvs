f9341031d1c7d3f717036e6c0db66a096d14c99c
Shane Watters <shane.watters.nz@gmail.com>
2017-09-18 13:45:09 +1200

    Adding a shuffleArray function that allows me to randomise the order that the colours are used. I have also added functionality to redraw the pattern on mouse click.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,4 @@
-//default values for MODE and lastChange time
-var MODE = 'auto', lastChange = 0, changeFrequency = 10;
-//random variables variables
-var startX = 0, startY = 0, fromColour, toColour;
-//control options
 var shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexa', 'octa'], rotationOptions = [3,4,6,8,12];
-
-var selector1, selector2, slider1, radio, resetButton;
 
 var bigHex = [
 				[200, 200], [280, 200], [280, 280], [200, 280], 
@@ -15,110 +8,57 @@ var bigHex = [
 				[200, 40], [280,40], [440, 200], [440, 280], 
 				[280,440], [200,440], [40, 280], [40, 200]
 			];
-			
-			
-
 
 function setup () {
     //set up the canvas
     var main_canvas = createCanvas(960, 500);
     main_canvas.parent('canvas-container');
-
-    selector2 = createSelect();
-    selector2.option('auto');
-    selector2.option('manual');
-    selector2.value('auto');
-    selector2.parent('mode-selector-holder');
-    selector2.changed(changeMode);
-
-    resetButton = createButton('reset');
-    resetButton.mousePressed(resetDrawing);
-    resetButton.parent('button-holder');
-
-    resetButton = createButton('stop');
-    resetButton.mousePressed(stopDrawing);
-    resetButton.parent('button-holder');
-
-    // set up the interactive controls
-    selector1 = createSelect();
-    for(var i=0; i < shapeOptions.length; i++){
-        selector1.option(shapeOptions[i]);
-    }
-    selector1.value(random(shapeOptions));
-    selector1.parent('shape-selector-holder');
-
-    slider1 = createSlider(5, 20, 10);
-    slider1.parent("slider-1-holder");
-
-    radio1 = createRadio();
-    for(var i=0; i < rotationOptions.length; i++){
-        radio1.option(rotationOptions[i]);
-    }
-    radio1.value(random(rotationOptions));
-    radio1.parent("radio-1-holder");
-
-
 	
     //set up some of the global options for p5.js
+    noLoop();
+    colorMode(HSB);
     background(0);
     noFill();
     rectMode(CENTER);
-    colorMode(HSB);
-    frameRate(60);
-	noLoop();
-
-    randomizeGlobalValues();
 }
 
 
 
 function draw () {
-    //if the mode is auto the pattern will change every 10 seconds
-    if(MODE === 'auto' && millis() > (lastChange + (changeFrequency * 1000))){
-        lastChange = millis();
-        changeFrequency = random(5, 10);
-        randomizeGlobalValues();
-    }
     drawPattern();
 }
 
 function mousePressed() {
-    if (mouseButton == LEFT && mouseY <= 500 && mouseX <= 960){
-        lastChange = millis();
-        randomizeGlobalValues(mouseX, mouseY);
-    }
+    clear();
+    background(0);
+    redraw();
 }
 
-var hueRanges = {
-        1 : [-20, 20, 0],
-        2 : [21, 60, 40],
-        3 : [61, 100, 80],
-        4 : [101, 140, 120],
-        5 : [141, 180, 160],
-        6 : [181, 220, 200],
-        7 : [221, 260, 240],
-        8 : [261, 300, 280],
-        9 : [301, 340, 320]
-    }
+var hueRanges = [
+                    [-20, 20, 0],
+                    [21, 60, 40],
+                    [61, 100, 80],
+                    [101, 140, 120],
+                    [141, 180, 160],
+                    [181, 220, 200],
+                    [221, 260, 240],
+                    [261, 300, 280],
+                    [301, 340, 320]
+                ];
 
 function drawPattern(){
-	translate(-480, -310);
-    var x = 0;
-    var lerpAmount = 0;
-    var shape = selector1.value();
-    var shapeSize =  slider1.value();
-    var numOfRotations = radio1.value();
-
-	for(var a = 1; a <= 9; a++){		
+    translate(-480, -310);
+    hueRanges = shuffleArray(hueRanges);
+	for(var a = 0; a <= 8; a++){		
 		var h = focusedRandom(hueRanges[a][0], hueRanges[a][1], 10, hueRanges[a][2]);
-		fromColour = color(h, 100, 100);
-		var toPointer = a +5;
-		if(toPointer > 9){
+		var fromColour = color(h, 100, 100);
+		var toPointer = a + 5;
+		if(toPointer > 8){
 			toPointer = toPointer - 9;
 		}
 		h = focusedRandom(hueRanges[toPointer][0], hueRanges[toPointer][1], 10, hueRanges[toPointer][2]);
-		toColour = color(h, 100, 100);
-		var colour = lerpColor(fromColour, toColour, lerpAmount);
+		var toColour = color(h, 100, 100);
+		var colour = "";
 		strokeWeight(0.3);
 		var lerpAmount = 0;
 		for (var pos = 0; pos < bigHex.length; pos++) {
@@ -128,7 +68,6 @@ function drawPattern(){
 			colour = lerpColor(fromColour, toColour, lerpAmount);
 			var lerpStep = 0.3333 / numOfRotations / 2;
 			for (var i = 0; i < (numOfRotations * 2); i ++) {
-				print(lerpAmount);
 				for (var j = 0; j <=5; j++) {
 					stroke(colour, 255 - (8*j));
 					//call the function as detemined by the variable shape
@@ -137,7 +76,6 @@ function drawPattern(){
 					window[shape](0, 20, 20 + (j*3), 20 + (j*3));
 				}
 				rotate(PI/numOfRotations);
-				//lerpAmount = lerpAmount + lerpStep;
 				
 			}
 			if(pos == 3){
@@ -158,12 +96,10 @@ function drawPattern(){
 		}
 		endShape(CLOSE);
 		translate(480, 0);
-		if(a == 3 || a == 6){
+		if(a == 2 || a == 5){
 			translate(-1200, 320);
 		}
 	}
-	lerpAmount = lerpAmount + (1/2);
-	colour = lerpColor(fromColour, toColour, lerpAmount);
 }
 
 
@@ -224,54 +160,32 @@ function octa(x, y, radius) {
     endShape(CLOSE);
 }
 
-	
-function randomizeGlobalValues(setX = false, setY = false){
-    if(MODE === 'auto'){
-        selector1.value(random(shapeOptions));
-        slider1.value(random(5, 20));
-        radio1.value(random(rotationOptions));
-    }
 
-    
+/*
+ * shuffles an array so that its elements are in a random order
+ * thanks to stackoverflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * @param  {Array} array        - array to shuffle
+ * @return {Array} array        - array after it has been shuffled
+ */
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    //set the from and to colours
-    var randomPointer1 = floor(random(1, 7));
-    var h = focusedRandom(hueRanges[randomPointer1][0], hueRanges[randomPointer1][1], 10, hueRanges[randomPointer1][2]);
-    fromColour = color(h, 100, 100);
-    var randomPointer2 = randomPointer1 +3;
-    if(randomPointer2 > 6){
-        randomPointer2 = randomPointer2 - 6;
-    }
-    h = focusedRandom(hueRanges[randomPointer2][0], hueRanges[randomPointer2][1], 10, hueRanges[randomPointer2][2]);
-    toColour = color(h, 100, 100);
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-    //set the start position of next pattern
-    startX = random(0, width);
-    if(setX){
-        startX = setX;
-    }
-    startY = random(0, height);
-    if(setY){
-        startY = setY;
-    }
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
-
-function resetDrawing(){
-    translate(-startX, -startY);
-    clear();
-    background(0);
-    randomizeGlobalValues();
-    loop();
-}
-
-function stopDrawing(){
-    noLoop();
-}
-
-function changeMode(){
-    MODE = selector2.value();
-}
 
 function keyTyped() {
   if (key == '!') {
