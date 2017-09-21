b1314820bc73dcf821c829fe9c2a4b5bf035a510
Guest User <Guest@b-05-05-lab.mddn.vuw.ac.nz>
2017-09-21 13:54:27 +1200

    updated to expand to wallpaper size

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 17.2.MDDN342 PS3
-
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+I changed route after finally understanding what the project actually was. I kept the arc aesthetic and decided to go for a 70's kitchen wallpaper design with an 80's colour palette.
--- sketch.js
+++ sketch.js
@@ -1,17 +1,49 @@
+
+var main_canvas , canvasSize = 1, canvasSelector, drawingMode = 'landscape', modeSelector;
+
 function setup () {
 	createCanvas(960, 500);
+
+	main_canvas = createCanvas(960, 500);
+    main_canvas.parent('canvas-container');
+
+    canvasSelector = createSelect();
+    canvasSelector.option(1);
+    canvasSelector.option(2);
+    canvasSelector.option(3);
+    canvasSelector.value(1);
+    canvasSelector.parent('canvas-selector-holder');
+    canvasSelector.changed(changeCanvasSize);
+
+    modeSelector = createSelect();
+    modeSelector.option('wallpaper');
+    modeSelector.option('landscape');
+    modeSelector.value('landscape');
+    modeSelector.parent('mode-selector-holder');
+    modeSelector.changed(changeMode);
+
 }
+
 var value = 0;
+var xoff = 0.0;
 
-function isEven(n) {
-	return n % 2 == 0;
-}
-
-function isOdd(n) {
-	return Math.abs(n % 2) == 1;
-}
 
 function draw () {
+	if (drawingMode === 'wallpaper'){
+		drawWallpaper();
+	}
+	
+}
+
+function mouseClicked() {
+	if (value == 1) {
+		value = 0;
+	}
+	else {
+		value =1;
+	}
+}
+function drawWallpaper(){
 	var numb = -60;
 	if (value == 0){
 		noStroke();
@@ -27,36 +59,70 @@ function draw () {
 		// 	strokeWeight(random(0.5,2));
 		// 	line (i, 0, i, height);
 		// }
-		stroke(232,12,163,250);
-		noFill();
-		strokeWeight(random(1,3));
-		arc (500, 250, -200, -200, random (PI, TWO_PI*0.8)-1.5, 0 );
-		stroke (31, 204, 178,250);
-		strokeWeight(random(1,4));
-		arc (500, 250, -200, -200, random (PI, TWO_PI*0.8)+1.2, 1 );
+		for (j = 100; j<height+1;j=j+200){
+			for (i = 100; i<width+100;i=i+200){
+				xoff = xoff+0.1;
+				var n= noise(xoff)*height;
 
+				stroke(232,12,163,250);
+				noFill();
+				strokeWeight(random(1,3));
+				arc (i, j, -200, -200, random (PI, TWO_PI*0.8)-1.5, 0 );
+				stroke (31, 204, 178,250);
+				strokeWeight(random(1,4));
+				arc (i, j, -200, -200, random (PI, TWO_PI*0.8)+1.2, 1 );
+			}
+		}
+		for (j = 100; j<height+100;j=j+200){
+			for (i = 100; i<width+100;i=i+200){
+				xoff = xoff+0.1;
+				var n= noise(xoff)*height;
 
-		
-		strokeWeight(random(1,3));
-		arc (600, 350, -200, -200, random (0, PI), 3 );
-		stroke(232,12,163,250);
-		strokeWeight(random(1,4));
-		arc (600, 350, -200, -200, random (0, PI), 4 );
-
+				stroke(232,12,163,250);
+				noFill();
+				strokeWeight(random(1,3));
+				arc (i, j, -100, -100, random (0, PI), 2 );
+				stroke (31, 204, 178,250);
+				strokeWeight(random(1,4));
+				arc (i, j, -100, -100, random (0, PI), 3 );
+			}
+		}
 
 		value = 1;
 	}
 	
+
+}
+function changeCanvasSize(){
+    if(drawingMode === 'wallpaper'){
+        canvasSize = canvasSelector.value();
+        if(canvasSize == 2){
+            main_canvas = resizeCanvas(1920, 1000);
+        }
+        else if(canvasSize == 3){
+            main_canvas = resizeCanvas(2880, 1500);
+        }
+        else {
+            main_canvas = resizeCanvas(960, 500);
+        }
+        clear();
+        background(0);
+        redraw();
+    }
 }
 
-function mouseClicked() {
-	if (value == 1) {
-		value = 0;
-	}
-	else {
-		value =1;
-	}
+function changeMode(){
+    drawingMode = modeSelector.value();
+    if(drawingMode === 'wallpaper'){
+    }
+    else if(drawingMode === 'landscape'){
+        loop();
+    }
+    clear();
+    background(0);
+    redraw();
 }
+
 
 function keyTyped() {
 	if (key == '!') {
