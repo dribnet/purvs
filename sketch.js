4df84fe2be8062d18e018a3fade8432f12f2c4eb
Guest User <Guest@b-05-05-lab.mddn.vuw.ac.nz>
2017-09-21 15:06:17 +1200

    added base landscape

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 
 var main_canvas , canvasSize = 1, canvasSelector, drawingMode = 'landscape', modeSelector;
+var mage, cya, mageA, cyaA;
 
 function setup () {
+	mage =  color(232,12,163);
+	cya =  color(31, 204, 178);
+
 	createCanvas(960, 500);
 
 	main_canvas = createCanvas(960, 500);
@@ -32,7 +36,9 @@ function draw () {
 	if (drawingMode === 'wallpaper'){
 		drawWallpaper();
 	}
-	
+	if (drawingMode === 'landscape'){
+		drawLandscape();
+	}
 }
 
 function mouseClicked() {
@@ -61,9 +67,6 @@ function drawWallpaper(){
 		// }
 		for (j = 100; j<height+1;j=j+200){
 			for (i = 100; i<width+100;i=i+200){
-				xoff = xoff+0.1;
-				var n= noise(xoff)*height;
-
 				stroke(232,12,163,250);
 				noFill();
 				strokeWeight(random(1,3));
@@ -93,6 +96,45 @@ function drawWallpaper(){
 	
 
 }
+
+function drawLandscape(){
+	xoff = xoff+0.1;
+	var n= noise(xoff)*height/random(10,50);
+	if (value == 0){
+		noStroke();
+		fill(5, 33, 29);
+		rect (0, 0, width, height);
+		for (j = 100; j<height+100;j=j+150){
+						var n= noise(xoff)*height/random(5,25);
+						stroke(31, 204, 178);
+						fill(232,12,163, 100+j/100);
+						strokeWeight(0.8);
+						beginShape();
+						vertex(-1,j+250);
+						vertex(0, j+(random (-n, n)));
+						vertex(random(50,100), j+(random (-n, n)));
+						vertex(random (110,150), j+(random (-n, n)));
+						vertex(random(160,250), j+(random (-n, n)));
+						vertex(random(260,350), j+(random (-n, n)));
+						vertex(random (360, 400), j+(random (-n, n)));
+						vertex(random (410, 450), j+(random (-n, n)));
+						vertex(random (460, 550), j+(random (-n, n)));
+						vertex(random (560, 600), j+(random (-n, n)));
+						vertex(random (610, 650), j+(random (-n, n)));
+						vertex(random (660, 750), j+(random (-n, n)));
+						vertex(random (760, 850), j+(random (-n, n)));
+						vertex(random (860, 900), j+(random (-n, n)));
+						vertex(random (910, 950), j+(random (-n, n)));
+						vertex(random (960, 970), j+(random (-n, n)));
+						vertex(1000, j+250);
+						endShape();
+					
+		}
+		value = 1;
+	}
+}
+
+
 function changeCanvasSize(){
     if(drawingMode === 'wallpaper'){
         canvasSize = canvasSelector.value();
@@ -116,7 +158,6 @@ function changeMode(){
     if(drawingMode === 'wallpaper'){
     }
     else if(drawingMode === 'landscape'){
-        loop();
     }
     clear();
     background(0);
@@ -126,6 +167,18 @@ function changeMode(){
 
 function keyTyped() {
 	if (key == '!') {
-		saveBlocksImages();
+		saveBlocksImages()
+	}
+	if (key == ' '){
+		print("Spacebar was hit")
+		if (drawingMode === 'wallpaper'){
+			drawingMode = 'landscape';
+			value = 0;
+		} 
+		else if (drawingMode === 'landscape') {
+			drawingMode = 'wallpaper';
+			value = 0;
+		}
+		print (drawingMode);
 	}
 }
