028ee98e8929fe3ca0e44bbe8d7ac6163de7339a
James Campbell <jb.campbell13@hotmail.com>
2017-09-23 13:00:02 +1200

    Development of selected art direction.
    Implemented arc shapes.
    Added sliders to adjust certain variables.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 17.2.MDDN342 PS3
 
-Initial start to the problem set. Looked at establishing a grid system with an object oriented focus to allow for easier design manipulation. Adapted example code into a more visually structured and rigid design.
+I've decided to emulate a 90's pattern design for my iterative wallpaper. The first stage I am looking into is the background squiggle designs. I am using arcs to create the squiggles across the same grid design I used in my initial iteration. I have included sliders to adjust stroke weight and the number of columns and rows.
--- sketch.js
+++ sketch.js
@@ -1,15 +1,42 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
 
-var dotMargin = 25;
-var numRows = 5;
-var numCols = 10;
+var slider1, slider2, slider3, slider4, slider5;
+var faceSelector;
+
+var dotMargin = 5;
+// var numRows = 10;
+// var numCols = 20;
 
 var curRandomSeed;
 
 function setup () {
   curRandomSeed = int(focusedRandom(0, 100));
   createCanvas(canvasWidth, canvasHeight);
+
+  // create sliders
+  slider1 = createSlider(0, 100, 50);
+  slider2 = createSlider(0, 100, 50);
+  slider3 = createSlider(0, 100, 50);
+  slider4 = createSlider(0, 100, 50);
+  slider5 = createSlider(0, 100, 50);
+
+  slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+  slider3.parent('slider3Container');
+  slider4.parent('slider4Container');
+  slider5.parent('slider5Container');
+
+  faceSelector = createSelect();
+  faceSelector.option('1');
+  faceSelector.option('2');
+  faceSelector.option('3');
+  faceSelector.option('all')
+  faceSelector.value('all');
+  faceSelector.parent('selector1Container');
+
+  angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 function changeRandomSeed() {
@@ -24,8 +51,15 @@ function draw () {
   background(255);
   resetFocusedRandom(curRandomSeed);
 
-  var x_steps = 1 + Math.floor(width / 80);
-  var y_steps = 1 + Math.floor(height / 80);
+  var s1 = slider1.value();
+  var s2 = slider2.value();
+  var s3 = slider3.value();
+  var s4 = slider4.value();
+  var s5 = slider5.value();
+
+  var lineWeight = map(s1, 0, 100, 4, 8);
+  var numRows = Math.floor(map(s2, 0, 100, 6, 10));
+  var numCols = Math.floor(map(s3, 0, 100, 15, 20));
 
   var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
   var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
@@ -47,8 +81,9 @@ function draw () {
     for(var j=0;j<numCols;j++) {
       var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
       var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
-      var shade = focusedRandom(60, 150, 3);
-      fill(shade);
+      //var shade = focusedRandom(60, 150, 3);
+      stroke(0);
+      strokeWeight(lineWeight);
       drawDot(x, y, dotRadius, color);
     }
   }
@@ -56,9 +91,9 @@ function draw () {
 }
 
 function drawDot(x, y, radius, color) {
-  noStroke();
-  // fill(color);
-  ellipse(x, y, radius, radius);
+  noFill();
+  var randAng = random(360);
+  arc(x , y , radius, radius, randAng, randAng + 40);
 }
 
 function keyTyped() {
