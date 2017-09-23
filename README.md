758a233815d4f77fd8dc3f0c644e48d3aa975903
Edward Taylor <edeetee@gmail.com>
2017-09-23 18:17:30 +1200

    Part 1

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+For this part of the project, I started by taking some of my code from Creating Coding II where I generated spheres with waves determining an offset on the radius.
+This generates interesting shapes that are extremely fluid and mesmerising to look at.
+
+I am quite interested in how sin/cos waves work through interference etc, and different ways that we can use it to show more organic and reactive movement over time in designs.
+
+This piece moved onto generating a grid of moving shapes, all driven by the same base interval so that they line up in the time domain. I started with rendering it with lines and tracers, which looked much like a mandela when the shapes were big enough, but ended up changing it to be a checkerboard fill pattern so that the edges of different objects overlap and create interesting complexity. I also made the largest sizes of shape loose some opacity so that the viewer gets to look into the innards every now and then.
+
+This piece required a lot of iteration, but I am happy with the result and how it looks very simple at first glance but exhibits an interesting and organic movement when time is spent being mesmerized by it.
--- sketch.js
+++ sketch.js
@@ -4,55 +4,113 @@ function setup () {
   angleMode(RADIANS)
   colorMode(HSB)
 
-  bgColor = color(0, 0, 100, 0.1)
-
-  slider = createSlider(5, 100, 20);
-  slider.position(10, 10);
-  slider.style('width', '200px');
-  slider.parent('cPoints')
-  slider.changed(function(){
-    cPoints = slider.value()
-  })
+  bgColor = color(0, 0, 0, 1)
 }
-let cPoints = 20;
+
+let size = 1;
+let gridSize = 40;
 
 function draw () {
-  cPoints = 20 + 19*sin(millis()/10000*TWO_PI)
-  //initial array size and elements
-  push()
-  translate(mouseX, mouseY)
-  drawCircle(null, genWaveMap(5, 5000))
-  // translate(0, -50)
-  fill(0, 0, 100)
-  drawCircle(50, genWaveMap(10, -400))
-  pop()
+  fill(0,0,0,0)
+  stroke(0,0,0)
+  strokeWeight(0.4)
+  noStroke();
 
+  // let gridSize = 25+5*st(10007+st(2023)+st(4061))
 
+  let modX = gridSize*2*t(2000)
+  let modY = gridSize*2*t(5000)
+  let fX = makeMap(-gridSize*4, windowWidth+gridSize*4);
+  let fY = makeMap(-gridSize*4, windowHeight+gridSize*4);
 
   background(bgColor)
+  let i = Math.floor(millis()/2000)+Math.floor(millis()/5000);
+  for(let x = modX-gridSize*4; x < windowWidth+gridSize*4+modX; x+= gridSize*2){
+    i++;
+    for(let y = modY-gridSize*4; y < windowHeight+gridSize*4+modY; y += gridSize*2){
+      i++;
+
+      let fx = fX(x)
+      let fy = fY(y)
+      if(1 < fx)
+        console.log(fx)
+      translate(x, y)
+      let heightI = 8000+fx*3000
+      // let heightI = 5000
+
+      let radius = gridSize*(1.4 -0.5*ct(heightI/2) -0.8*ct(heightI*6));
+
+      if(gridSize*2 < radius){
+        let alpha = Math.min((radius-gridSize*2)/(gridSize*2), 1);
+        fill(0, 0, i%2 == 0 ? 0 : 100, 1-alpha*0.8)
+      } else{
+        fill(0, 0, i%2 == 0 ? 0 : 100)
+      }
+
+      drawCircle(radius, 
+        genWaveMap(1+Math.floor(t(heightI*2)*4), 0.3*st(heightI)), 
+        t(heightI*4)*TWO_PI)
+
+      translate(-x, -y)
+    }
+  }
 }
 
-function drawCircle(radius, waveMap){
+function makeMap(low, high){
+  return val => (val-low)/(high-low)
+}
+
+/**
+ * 
+ * @param {Number} radius
+ * @param {ForEachCallback} waveMap 
+ * @param {Number} offset 
+ */
+function drawCircle(radius, waveMap, offset){
   radius = radius || 100
-  let circle = fillArray(cPoints, radius || 100)
+  waveMap = waveMap || (x => x)
+  offset = offset || 0
+
+  let points = 20
+
+  let circle = fillArray(points, radius)
   //wavy offset
-  .map(waveMap || (x => x))
+  .map(waveMap)
   //circle coordinates
   .map((radius, i, arr) => {
-    let ang = i/cPoints*TWO_PI
+    let ang = i/points*TWO_PI+offset
     return createVector(radius*sin(ang), -radius*cos(ang))
   })
 
-  drawCurve(circle, CLOSE)
+  drawCurve(circle)
 }
 
-function genWaveMap(waves, interval){
-  return  (radius, i) => radius+radius/10*sin(i/cPoints*TWO_PI*waves + millis()/interval*TWO_PI)
+function t(period){
+  return millis().mod(period)/period
+}
+
+function st(period){
+  return sin(t(period)*TWO_PI)
+}
+
+function ct(period){
+  return cos(t(period)*TWO_PI)
+}
+
+/**
+ * generates the wave over the radius of a circle
+ * @param {number} waves 
+ * @param {Number} height 
+ * @returns {ForEachCallback}
+ */
+function genWaveMap(waves, height){
+  height = height || 0.2
+  return (radius, i, arr) => radius+radius*height*cos(i/arr.length*TWO_PI*waves)
 }
 
 function drawCurve(points){
   beginShape()
-  for(let i = 0; i < points.length+3; i++){
+  for(let i = -1; i < points.length+2; i++){
     let point = points[i.mod(points.length)]
     curveVertex(point.x, point.y)
   }
