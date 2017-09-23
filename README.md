9bff815c046ec4e966bf8904d042e2c31d3ae3db
Robbie Carmichael <robbi3reg@gmail.com>
2017-09-24 12:40:30 +1300

    updated wallpaper to include slider functions, as well as readme to reflect these changes

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ PART 1: Iterative Pattern (Wallpaper)
 
 I have used a series of for loops to generate my tiling patern for part 1 of assignment 3. I tried to design something that could not only be infinitely tiled but would look good at different scales (small diamonds or large diamonds). I narrowed the colours down to just two - which could be interchanged depending on the colours you wanted for your fabric, or to have as wallpaper in your room - as well as playing with stroke opacity for the final effect. 
 
+I have now also added 3 sliders that can be used to adjust the wallpaper to change the diamond size, ellipse size, and shade. However, this meant needing to remove the noLoop(); which has impacted the performance.
+
--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
+var slider1, slider2, slider3;
+var inc = 0.01;
+
 function setup () {
   createCanvas(960, 500);
+  pixelDensity(1);
+
+  slider1 = createSlider(0, 100, 50);
+  slider2 = createSlider(10, 50, 25);
+  slider3 = createSlider(0, 255, 255);
+
+  slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+  slider3.parent('slider3Container');
 }
-
-
 
 function draw () {
 
+  //WALLPAPER
+
+  var s1v = slider1.value();
+  var s2v = slider2.value();
+  var s3v = slider3.value();
+
+
   strokeWeight(2);
-  stroke(255, 198, 203);
+  stroke(s3v, 198, 203);
   fill(255);
   
   ellipseMode(CENTER);
@@ -17,7 +34,7 @@ function draw () {
 
   for (var x = 0; x <= width; x += 10) {
     for (var y = 0; y <= height; y += 10) {
-     ellipse(x, y, 10, 10);  
+     ellipse(x, y, s1v/5, s1v/5);  
     }     
   }
 
@@ -34,7 +51,7 @@ function draw () {
   }
 
   stroke(255);
-  strokeWeight(25);
+  strokeWeight(s2v);
   for (var i = 0; i <= width + width/2; i += 200) {
     line(i - width/2, -50, 0 + i, height+50);   
   }
@@ -43,7 +60,27 @@ function draw () {
     line(i, -50, -width/2 + i, height+50);   
   }
 
-  noLoop();
+  // noLoop();
+
+  //GENERATIVE LANDSCAPE
+  //Code based on tutorials from The Coding Train
+
+  // var yoff = 0;
+
+  // loadPixels();
+  // for (var y = 0; y < height; y++) {
+  //   var xoff = 0;
+  //   for (var x = 0; x < width; x++) {
+  //     var index = (x + y * width) * 4;
+  //     var r = noise(xoff, yoff) * 255;
+  //     pixels[index + 0] = r;
+  //     pixels[index + 1] = r;
+  //     pixels[index + 2] = r;
+  //     pixels[index + 4] = r;
+  //   }
+  // }
+
+  
 
 }
 
