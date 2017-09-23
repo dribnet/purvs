0984180b42551e1c3c50e04c6593132a411a882b
Nathan Larson <nathnz@hotmail.co.nz>
2017-09-23 14:00:15 +1200

    Added water
    Tidied up the different heights, so the ground is always at the same spot, to give a cleaner look to the landscape. Started working on adding more to the landscape, by adding water. Also added sliders to control the size and variety of the landscape.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Started working on my landscape. For my inspiration I searched up isometric island, and based my design off what I saw, as I was wanting to do a floating island and thought isometric tile based would be the best option. 
 
 I have a function that draws a tile, and by creating more functions I will be able to add a lot of variety into my landscape. I also added a random variable to affect the height of each tile , to give the landscape more of a realistic look.
+
+Tidied up the different heights, so the ground is always at the same spot, to give a cleaner look to the landscape. Started working on adding more to the landscape, by adding water. Also added sliders to control the size and variety of the landscape.
--- sketch.js
+++ sketch.js
@@ -1,12 +1,30 @@
-var tileWidth = 80;
-var tileHeight = tileWidth/2;
-var boardSize = 7;
+var slider1, slider2, slider3, slider4, slider5;
 
-//Tile Variables
+//Terrian Variables
+var tileWidth = 40;
+var tileHeight = tileWidth/2;
 var tileDepth = 30;
+var boardSize = 12;
+var waterChance = 0.2;
 
 function setup () {
   createCanvas(960, 500);
+
+  // create sliders
+  slider1 = createSlider(0, 100, 50);
+  slider2 = createSlider(0, 100, 50);
+  slider3 = createSlider(0, 100, 50);
+  slider4 = createSlider(0, 100, 50);
+  slider5 = createSlider(0, 100, 50);
+
+  slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+  slider3.parent('slider3Container');
+  slider4.parent('slider4Container');
+  slider5.parent('slider5Container');
+
+  draw();
+
   drawShapes();
 }
 
@@ -21,18 +39,21 @@ function drawShapes(){
 	noStroke();
 	background(125,210,233);
 
-	translate(width/2, tileHeight*2);
+	translate(width/2, 80);
 
 	var rowSize = 0;
 	var startPos = 0;
-	drawGroundTile(0, 0);
 
 	//Draw first half of board
 	for(var y = 0; y < boardSize; y++){
 		for(var x = 0; x <= rowSize; x++){
 
-			drawGroundTile(startPos + x*tileWidth, y*tileHeight/2 + random(0,5));
+			var tileType = random(0,1);
 
+			if(tileType > waterChance)
+				drawGroundTile(startPos + x*tileWidth, y*tileHeight/2);
+			else
+				drawWaterTile(startPos + x*tileWidth, y*tileHeight/2);
 		}
 		startPos -= tileWidth/2;
 		rowSize++;
@@ -42,39 +63,113 @@ function drawShapes(){
 	for(var y = boardSize; y <= boardSize*2; y++){
 		for(var x = 0; x <= rowSize; x++){
 
-			drawGroundTile(startPos + x*tileWidth, y*tileHeight/2 + random(0,5));
+			var tileType = random(0,1);
+
+			if(tileType > waterChance)
+				drawGroundTile(startPos + x*tileWidth, y*tileHeight/2);
+			else
+				drawWaterTile(startPos + x*tileWidth, y*tileHeight/2);
 		}
 		startPos += tileWidth/2;
 		rowSize--;
 	}
 }
 
-function drawGroundTile(x, y){
+function drawWaterTile(x, y){
+
+	dy = -5;
 
 	//Draw Dirt underneath
-	fill(206,104,64);
+
+	//Draw left side
+	fill(64,112,183);
 
 	beginShape();
 
-	vertex(x - tileWidth/2, y);
+	vertex(x - tileWidth/2, y-dy);
 
 	vertex(x - tileWidth/2, y + tileDepth);
 	vertex(x, y + tileHeight/2 + tileDepth);
 
-	vertex(x, y);
+	vertex(x, y-dy);
 
 	endShape();
 
+
+	//draw right side
+	fill(39,81,143);
+
 	beginShape();
 
-	vertex(x + tileWidth/2, y);
+	vertex(x + tileWidth/2, y-dy);
 
 	vertex(x + tileWidth/2, y + tileDepth);
 	vertex(x, y + tileHeight/2 + tileDepth);
 
-	vertex(x, y);
+	vertex(x, y-dy);
 
 	endShape();
+
+	push();
+
+	//Translate grass to adjust for random height
+	translate(0, -dy);
+
+	//Draw Grass
+	fill(64,112,183);
+	
+	beginShape();
+
+	vertex(x - tileWidth/2 , y);
+	vertex(x , y - tileHeight/2);
+	vertex(x + tileWidth/2 , y);
+	vertex(x , y + tileHeight/2);
+	vertex(x - tileWidth/2 , y);
+
+	endShape();
+
+	pop();
+}
+
+function drawGroundTile(x, y){
+
+	dy = random(0,10);
+
+	//Draw Dirt underneath
+
+	//Draw left side
+	fill(206,104,64);
+
+	beginShape();
+
+	vertex(x - tileWidth/2, y-dy);
+
+	vertex(x - tileWidth/2, y + tileDepth);
+	vertex(x, y + tileHeight/2 + tileDepth);
+
+	vertex(x, y-dy);
+
+	endShape();
+
+
+	//draw right side
+	fill(170,66,52);
+
+	beginShape();
+
+	vertex(x + tileWidth/2, y-dy);
+
+	vertex(x + tileWidth/2, y + tileDepth);
+	vertex(x, y + tileHeight/2 + tileDepth);
+
+	vertex(x, y-dy);
+
+	endShape();
+
+	push();
+
+	//Translate grass to adjust for random height
+	translate(0, -dy);
 
 	//Draw Grass
 	fill(100,169,74);
@@ -88,6 +183,26 @@ function drawGroundTile(x, y){
 	vertex(x - tileWidth/2 , y);
 
 	endShape();
+
+	pop();
+}
+
+function draw(){
+
+	var s1 = slider1.value();
+    var s2 = slider2.value();
+    var s3 = slider3.value();
+    var s4 = slider4.value();
+    var s5 = slider5.value();
+
+    //Board Size Changes
+	tileWidth = map(s1, 0, 100, 35, 100);
+	tileHeight = tileWidth/2;
+	boardSize = Math.round(80/tileWidth * 8);
+	tileDepth = 30 - boardSize;
+	
+	//Terrian Variety
+	waterChance = map(s2, 0, 100, 0, 1);
 }
 
 function keyTyped() {
