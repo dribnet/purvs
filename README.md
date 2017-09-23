1773dbfea2de9247213d78a57dcc7d120a77b837
James Campbell <jb.campbell13@hotmail.com>
2017-09-23 22:29:33 +1200

    Added new shape designs.
    Added circle design.
    Added square design.
    Added zigzag design.
    Added sliders for shape rows and columns.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 17.2.MDDN342 PS3
 
-Continuing with the 90's theme, i have looked to include a colour option to the design. The colours used aim to strengthen and reinforce a sense of nostalgia. I also added a slider option to alternate between black and white and colour.
+Looking at source material, an essential feature of a 90's themed pattern appears to be in the use of geometric shapes alongside grid pattern. I used an object oriented system to generate the different shapes. To help showcase these shapes I have included sliders to modify the grid and column numbers.
--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
 
-var slider1, slider2, slider3, slider4, slider5;
+var slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider8;
 var faceSelector;
+var button;
 
-var dotMargin = 5;
-// var numRows = 10;
-// var numCols = 20;
+var squiggleMargin = 5;
+var shapeMargin = 20;
 
 var curRandomSeed;
 
@@ -22,12 +22,18 @@ function setup () {
   slider3 = createSlider(0, 100, 50);
   slider4 = createSlider(0, 100, 50);
   slider5 = createSlider(0, 100, 50);
+  slider6 = createSlider(0, 100, 50);
+  slider7 = createSlider(0, 100, 50);
+  slider8 = createSlider(0, 100, 50);
 
   slider1.parent('slider1Container');
   slider2.parent('slider2Container');
   slider3.parent('slider3Container');
   slider4.parent('slider4Container');
   slider5.parent('slider5Container');
+  slider6.parent('slider6Container');
+  slider7.parent('slider7Container');
+  slider8.parent('slider8Container');
 
   faceSelector = createSelect();
   faceSelector.option('1');
@@ -36,6 +42,10 @@ function setup () {
   faceSelector.option('all')
   faceSelector.value('all');
   faceSelector.parent('selector1Container');
+
+  randButton = createButton('randomize');
+  randButton.mousePressed(changeRandomSeed);
+  randButton.parent('selector1Container');
 
   angleMode(DEGREES);
   rectMode(CENTER);
@@ -47,10 +57,6 @@ function changeRandomSeed() {
   c.sort(function(a, b){return 0.5 - Math.random()});
 }
 
-function mousePressed() {
-  changeRandomSeed();
-}
-
 function draw () {
   resetFocusedRandom(curRandomSeed);
 
@@ -59,12 +65,18 @@ function draw () {
   var s3 = slider3.value();
   var s4 = slider4.value();
   var s5 = slider5.value();
+  var s6 = slider6.value();
+  var s7 = slider7.value();
+  var s8 = slider8.value();
 
   var lineWeight = map(s1, 0, 100, 4, 8);
-  var numRows = Math.floor(map(s2, 0, 100, 8, 12));
-  var numCols = Math.floor(map(s3, 0, 100, 18, 20));
+  var numSquiggleRows = Math.floor(map(s2, 0, 100, 8, 12));
+  var numSquiggleCols = Math.floor(map(s3, 0, 100, 18, 20));
   var lineLength = Math.floor(map(s4, 0, 100, 30, 80));
-  var color =  Math.floor(map(s5, 0, 100, 0, 2));
+  var color =  Math.floor(map(s5, 0, 100, 0, 1));
+  var numShapeRows = Math.floor(map(s6, 0, 100, 5, 8));
+  var numShapeCols =  Math.floor(map(s7, 0, 100, 6, 10));
+  var contentType =  Math.floor(map(s8, 0, 100, 0, 1));
 
   if (color < 1) {
     bg_color = 255;
@@ -76,33 +88,72 @@ function draw () {
 
   background(bg_color);
 
-  var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
-  var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
+  var circleFill = c[2];
+  var squareFill = c[3];
+  var zigzagFill = c[4];
+
+  var dotWidth = ((canvasWidth - (2 * squiggleMargin)) / numSquiggleCols) - squiggleMargin;
+  var dotHeight = ((canvasHeight - (2 * squiggleMargin)) / numSquiggleRows) - squiggleMargin;
+
+  var shapeWidth = ((canvasWidth - (2 * shapeMargin)) / numShapeCols) - shapeMargin;
+  var shapeHeight = ((canvasHeight - (2 * shapeMargin)) / numShapeRows) - shapeMargin;
 
   if( dotWidth > dotHeight ) {
     var dotDiameter = dotHeight;
-    var xMargin = (canvasWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
-    var yMargin = dotMargin;
+    var xMargin = (canvasWidth - ((2 * squiggleMargin) + (numSquiggleCols * dotDiameter))) / numSquiggleCols;
+    var yMargin = squiggleMargin;
   } else {
     var dotDiameter = dotWidth;
-    var xMargin = dotMargin;
-    var yMargin = (canvasHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
+    var xMargin = squiggleMargin;
+    var yMargin = (canvasHeight - ((2 * squiggleMargin) + (numSquiggleRows * dotDiameter))) / numSquiggleRows;
+  }
+
+  if( shapeWidth > shapeHeight ) {
+    var shapeDiameter = shapeHeight;
+    var shapeMarginX = (canvasWidth - ((2 * shapeMargin) + (numShapeCols * shapeDiameter))) / numShapeCols;
+    var shapeMarginY = shapeMargin;
+  } else {
+    var shapeDiameter = shapeWidth;
+    var shapeMarginX = shapeMargin;
+    var shapeMarginY = (canvasHeight - ((2 * shapeMargin) + (numShapeRows * shapeDiameter))) / numShapeRows;
   }
 
   var dotRadius = dotDiameter * 0.5;
 
-  // draw a circle at each location
-  for(var i=0;i<numRows;i++) {
-    for(var j=0;j<numCols;j++) {
-      var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
-      var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
-      drawDot(x, y, dotRadius, color, lineWeight, squiggle_color, lineLength);
+  var shapeRadius = shapeDiameter * 0.5;
+
+  // draw a squiggle at each location
+  for(var i=0;i<numSquiggleRows;i++) {
+    for(var j=0;j<numSquiggleCols;j++) {
+      var x = (j * (dotDiameter + xMargin)) + squiggleMargin + (xMargin / 2) + dotRadius;
+      var y = (i * (dotDiameter + yMargin)) + squiggleMargin + (yMargin / 2) + dotRadius;
+      strokeCap(ROUND);
+      drawSquiggle(x, y, dotRadius, color, lineWeight, squiggle_color, lineLength);
+    }
+  }
+
+  if (contentType == 1) {
+    for(var i=0;i<numShapeRows;i++) {
+      for(var j=0;j<numShapeCols;j++) {
+        var shapeX = (j * (shapeDiameter + shapeMarginX)) + shapeMargin + (shapeMarginX / 2) + shapeRadius;
+        var shapeY = (i * (shapeDiameter + shapeMarginY)) + shapeMargin + (shapeMarginY / 2) + shapeRadius;
+        
+        if(i%2 == 0) {
+          shapeX = shapeX + shapeMarginX / 2;
+        }
+        else {
+          shapeX = shapeX - shapeMarginX / 2;
+        }
+
+        drawShape(shapeX, shapeY, shapeRadius, circleFill, squareFill, zigzagFill);
+
+      }
     }
   }
 
 }
 
-function drawDot(x, y, radius, color, lineWeight, squiggleColor, lineLength) {
+function drawSquiggle(x, y, radius, color, lineWeight, squiggleColor, lineLength) {
   var randAng = random(360);
   var xPos = x + random(-10, 10);
   var yPos = y + random(-10, 10);
@@ -110,6 +161,7 @@ function drawDot(x, y, radius, color, lineWeight, squiggleColor, lineLength) {
   strokeWeight(lineWeight);
 
   if (color >= 1) {
+    noFill();
     stroke(0);
     arc(xPos + 3, yPos + 3, radius, radius, randAng, randAng + lineLength);
   }
@@ -117,6 +169,57 @@ function drawDot(x, y, radius, color, lineWeight, squiggleColor, lineLength) {
   noFill();
   stroke(squiggleColor);
   arc(xPos, yPos, radius, radius, randAng, randAng + lineLength);
+}
+
+function drawShape(x, y, radius, circleFill, squareFill, zigzagFill) {
+  noStroke();
+  var shapeType = Math.floor(random(1, 4));
+  var xPos = x + random(0, 0);
+  var yPos = y + random(0, 0);
+
+  if (shapeType == 1) {
+    fill(0);
+    drawCircle(x + 5, y + 5, radius);
+
+    fill(circleFill);
+    drawCircle(x, y, radius);
+  }
+  else if (shapeType == 2) {
+    fill(0);
+    drawSquare(xPos + 5, yPos + 5, radius);
+
+    fill(squareFill);
+    drawSquare(xPos, yPos, radius);
+  }
+  else {
+    strokeCap(SQUARE);
+    noFill();
+    strokeWeight(6 * radius / 20);
+
+    stroke(0);
+    drawZigZag(xPos + 5, yPos + 5, radius);
+
+    stroke(zigzagFill);
+    drawZigZag(xPos, yPos, radius);
+  }
+
+}
+
+function drawCircle(xPos, yPos, radius) {
+  ellipse(xPos, yPos, radius, radius);
+}
+
+function drawSquare(xPos, yPos, radius) {
+  rect(xPos, yPos, radius, radius);
+}
+
+function drawZigZag(xPos, yPos, radius) {
+    beginShape();
+    vertex(xPos - radius / 4, yPos - radius / 1.5);
+    vertex(xPos - radius / 4, yPos);
+    vertex(xPos + radius / 4, yPos);
+    vertex(xPos + radius / 4, yPos + radius / 1.5);
+    endShape();
 }
 
 function keyTyped() {
