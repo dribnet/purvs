a2bb394d061fb43e2d9da8f4f90debb890f588e3
James Campbell <jb.campbell13@hotmail.com>
2017-09-23 12:02:50 +1200

    Initial project direction.
    Established ellipse grid design.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+Initial start to the problem set. Looked at establishing a grid system with an object oriented focus to allow for easier design manipulation. Adapted example code into a more visually structured and rigid design.
--- sketch.js
+++ sketch.js
@@ -1,15 +1,64 @@
+var canvasWidth = 960;
+var canvasHeight = 500;
+
+var dotMargin = 25;
+var numRows = 5;
+var numCols = 10;
+
+var curRandomSeed;
+
 function setup () {
-  createCanvas(960, 500);
+  curRandomSeed = int(focusedRandom(0, 100));
+  createCanvas(canvasWidth, canvasHeight);
+}
+
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+}
+
+function mousePressed() {
+    changeRandomSeed();
 }
 
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
+  background(255);
+  resetFocusedRandom(curRandomSeed);
+
+  var x_steps = 1 + Math.floor(width / 80);
+  var y_steps = 1 + Math.floor(height / 80);
+
+  var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
+  var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
+
+  if( dotWidth > dotHeight ) {
+    var dotDiameter = dotHeight;
+    var xMargin = (canvasWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
+    var yMargin = dotMargin;
+  } else {
+    var dotDiameter = dotWidth;
+    var xMargin = dotMargin;
+    var yMargin = (canvasHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
   }
-  else {
-    fill(255);
+
+  var dotRadius = dotDiameter * 0.5;
+
+  // draw a circle at each location
+  for(var i=0;i<numRows;i++) {
+    for(var j=0;j<numCols;j++) {
+      var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
+      var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
+      var shade = focusedRandom(60, 150, 3);
+      fill(shade);
+      drawDot(x, y, dotRadius, color);
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+}
+
+function drawDot(x, y, radius, color) {
+  noStroke();
+  // fill(color);
+  ellipse(x, y, radius, radius);
 }
 
 function keyTyped() {
