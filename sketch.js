8cc3ac8f9a04ab13b6f8b7e940a3474ad588f78d
Marc <marc.gluyas@gmail.com>
2017-09-25 10:19:51 +1300

    Added more colour palettes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,37 @@
 let data;
 
-let iterations = 1;
-let maxIterations = 26;
+let iterations;
+let maxIterations = 25;
 let zoom = 2500;
+
+let palettes = [
+    ['#7847B2', '#FFFE99'], // purple + tan
+    ['#8CFFA6', '#FFA5AE'], // mint + pink
+    ['#FFA381', '#CAFFD4'], // peach + space grey
+    ['#FFF078', '#B21D40'], // sand + burgundy
+]
+let palette = [];
+let paletteNum;
 
 function setup () {
     createCanvas(960, 500);
+    angleMode(DEGREES);
+    init();
+}
+
+function init() {
     viewPos = createVector(Math.random(0.65) * width, Math.random(0.8) * height);
     console.log(viewPos);
-    angleMode(DEGREES);
+
     data = penrose.generate(960);
+    iterations = 0;
+
+    let newPalette
+    do {
+        newPalette = floor(random(palettes.length));
+    } while(newPalette == paletteNum);
+    paletteNum = newPalette;
+    palette = palettes[paletteNum];
 }
 
 function draw () {
@@ -20,17 +42,22 @@ function draw () {
         //clear()
         //scale(width/viewWidth, width/viewWidth);
         //rotate(random(72))
+        background(palette[0]);
         translate(-viewPos.x, -viewPos.y);
         scale(zoom, zoom);
 
         tris.forEach((tri) => {
             if (tri.acute) {
-                fill('#FFFE99');
+                fill(palette[1]);
             } else {
-                fill('#7847B2');
+                fill(palette[0]);
             }
 
+            strokeWeight(0.0000003 * zoom);
+            //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
+
             noStroke();
+            noSmooth();
             beginShape();
                 vertex(tri.points[0].x, tri.points[0].y);
                 vertex(tri.points[1].x, tri.points[1].y);
@@ -38,6 +65,9 @@ function draw () {
             endShape(CLOSE);
 
             strokeWeight(0.0000003 * zoom);
+            //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
+
+            smooth();
             stroke(0);
             line(tri.left.edge.a.x, tri.left.edge.a.y, tri.left.edge.b.x, tri.left.edge.b.y);
             line(tri.right.edge.a.x, tri.right.edge.a.y, tri.right.edge.b.x, tri.right.edge.b.y);
@@ -59,6 +89,10 @@ function draw () {
     //     fill(255);
     // }
     // ellipse(mouseX, mouseY, 80, 80);
+}
+
+function mouseClicked() {
+    init();
 }
 
 function keyTyped() {
