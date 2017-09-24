47c8ad15b89cc1b9aae29bc3e009d5b3a59ee81b
ashtondavvo <ashton.d173@live.com>
2017-09-25 09:24:01 +1300

    added noise

--- sketch.js
+++ sketch.js
@@ -4,12 +4,22 @@ var colour_red = [255,0,0];
 var colour_green = [0,255,0];
 var colour_blue = [0,0,255];
 
+var colour_tree = [0, 200, 0];
+var colour_tree_trunk = [139,69,19];
+
+var trees_xpos = new Array();
+var trees_ypos = new Array();
+var houses_xpos = new Array();
+var houses_ypos = new Array();
+
 let randomColourNum = 0;
 let drawMode = 0;
 
 function setup () {
   createCanvas(960, 500);
   randomColourNum = focusedRandom(0,3);
+  noiseSeed(random(0,1000));
+  noiseDetail(6,0.75);
 }
 
 function draw () {
@@ -32,9 +42,26 @@ function draw () {
     }
   }
   else{
-    background(255,255,255);
-    drawHouse(30, 20, 2);
-    drawTree(200, 100, 40);
+    rect(10, 10, 10, 10);
+    //var perlin_noise = noise(96, 50);
+    for(var i = 0; i < 20; i++){
+      for(var j = 0; j < 10; j++){
+        console.log(noise(i,j));
+        fill((255 * noise(i, j)), 0, 0);
+        stroke((255 * noise(i, j)), 0, 0);
+        //fill(255,0,0);
+        rect(i * 50, j * 50, 50, 50);
+        if(noise(i,j) > 0.5){
+          drawTree((i*50) +20, j*50, 10);
+        }
+        else{
+          drawHouse(i*50, j*50, 0.2);
+        }
+      }
+    }
+    //background(255,255,255);
+    //drawHouse(30, 20, 2);
+    //drawTree(200, 100, 40);
   }
 }
 
@@ -81,20 +108,22 @@ function drawHouse(xpos, ypos, size){
   stroke(colour_blue);
   rect(77 * size, 80 * size, 30 * size, 50 * size, 5, 5, 0, 0);
   pop();
+  push();
+  pop();
 }
 
 function drawTree(xpos, ypos, size){
   push();
   translate(xpos, ypos);
-  fill(colour_green);
-  stroke(colour_green);
+  fill(colour_tree);
+  stroke(colour_tree);
   triangle((size*0.5), 0, 0, (size * 2), size, (size * 2) );
   translate(0, size);
   triangle((size*0.5), 0, 0, (size * 2), size, (size * 2) );
   translate(0, size);
   triangle((size*0.5), 0, 0, (size * 2), size, (size * 2) );
-  fill(colour_red);
-  stroke(colour_red);
+  fill(colour_tree_trunk);
+  stroke(colour_tree_trunk);
   rect(size * 0.35,size*2,size * 0.3,size);
   pop();
 }
