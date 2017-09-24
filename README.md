d141d330b1f3a033c5e6b119bd2d82f2d5d8a4f4
Justina Ho <cyjustinaho@gmail.com>
2017-09-25 01:04:13 +1300

    landscape

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 17.2.MDDN342 PS3
 
-I have added sliders to my wallpaper pattern which will give some variation. These include circle size, shifting, rotating and colour. In my opinion I feel like the rotation element changes it up the most. This makes the pattern more interesting but also keeps within the pattern itself. 
+The yellow on my pattern inspired me to create a landscape that emulates sand dunes in a desert. I have created the base of it and will add more elements later such as changing the colours, adding cacti. 
+
+I have also changed the colour variable on my pattern wallpaper. I felt that the background change would make it more interesting.
+
+Noise Wave
+https://p5js.org/examples/math-noise-wave.html
--- sketch.js
+++ sketch.js
@@ -2,12 +2,17 @@ var curRandomSeed;
 var circleMove = 1;
 var slider1, slider2, slider3, slider4;
 var faceSelector;
+var patternMode = true;
+var noiseScale;
 
 function setup () {
   curRandomSeed = int(focusedRandom(0, 100));
   createCanvas(960, 500);
     var w = 960;
     var h = 500;
+    noiseScale = 0.001;
+    noiseScaleMid = 0.002;
+    noiseScaleBack = 0.003;
  
   slider1 = createSlider(0, 100, 50);
   slider2 = createSlider(0, 100, 50);
@@ -31,32 +36,36 @@ function setup () {
   rectMode(CENTER);
 }
 
-//function changeRandomSeed() {
-//  curRandomSeed = curRandomSeed + 1;
-//}
-//
-//function mousePressed() {
-//    changeRandomSeed();
-//}
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+}
+
+function mousePressed() {
+    changeRandomSeed();
+}
+
+
 
 function draw () {
 
-    background(214,183,85);
-
-  resetFocusedRandom(curRandomSeed);
     
+    
+
+    resetFocusedRandom(curRandomSeed);
+    noiseSeed(curRandomSeed);
   
   var s1 = slider1.value();
   var s2 = slider2.value();
   var s3 = slider3.value();
   var s4 = slider4.value();
+    
+    
 
   var size = map(s1, 0, 100, 8, 15);
   var shift = map(s2, 0, 100,50, 90);
   var rotation = map(s3, 0, 100,-3,9);
-  var colour = Math.floor(map(s4, 0, 100, 80, 131));
-    
-    
+  var colour =  Math.floor(map(s4, 0, 100, 0, 1));
+ 
     
   var x_steps = 1 + Math.floor(width / 20);
   var y_steps = 1 + Math.floor(height / 20);
@@ -75,19 +84,30 @@ function draw () {
     }
   }		
 
+if (patternMode){
+    
+     if (colour < 1) {
+         background(214,183,85);
+    fill (110,31,31);
+           stroke(255);
+        strokeWeight(1);
+    
+         
+  } else {
+   background(110,31,31);
+    fill(214,183,85); 
+      stroke(214,183,85);
+        strokeWeight(1);
+  }
+    
 
-  // draw a circle at each location
      cirlceMove = circleMove + 0.5;
      function move (i){return Math.sin(circleMove + i )* 10}
 
   for(var i=0;i<x_steps;i++) {
     for(var j=0;j<y_steps;j++) {
       var loc = grid_locations[i][j];
-      
 
-        stroke(255);
-        strokeWeight(1);
-         fill(colour,31,31);
       
        push();
        	scale(0.8);
@@ -112,9 +132,142 @@ function draw () {
 
 }
 
+    else{
+        
+        var yoff = 0.0;  
+        var sand_1 = random(170, 200);
+        var sand_2 = random(300, 350);
+        var sand_3 = random(350, 370);
+        var sand_4 = random(370, 400);
+        var sand_5 = random(600, 650);
+        var randomPlot = random(0, 200);
+
+  background(61);
+ stroke(255);
+        
+
+  fill(214,183,85);
+  beginShape(); 
+       var xoff = yoff; 
+       for (var x = 0; x <= width; x += 10) {
+           var y = map(noise(xoff), 0, 1, sand_1,50);
+           vertex(x, y); 
+           xoff += 0.05;
+        }
+
+      yoff += 0.01;
+      vertex(width, height);
+      vertex(0, height);
+      endShape(CLOSE);
+        
+        
+  beginShape(); 
+       var xoff = yoff; 
+      for (var x = 0; x <= width; x += 10) {
+        var y = map(noise(xoff), 0, 1, sand_2,100);
+        vertex(x, y); 
+        xoff += 0.05;
+      }
+      yoff += 0.05;
+      vertex(width, height);
+      vertex(0, height);
+  endShape(CLOSE);
+        
+        
+        
+ beginShape(); 
+   var xoff = yoff; 
+   for (var x = 0; x <= width; x += 10) {
+   var y = map(noise(xoff), 0, 1, sand_3 ,100);
+    vertex(x, y); 
+    xoff += 0.05;
+  }
+
+  yoff += 0.05;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+        
+        
+   beginShape(); 
+   var xoff = yoff;
+   for (var x = 0; x <= width; x += 10) {
+    var y = map(noise(xoff), 0, 1, sand_4,100);
+    vertex(x, y); 
+    xoff += 0.05;
+  }
+  yoff += 0.05;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);   
+        
+        
+ beginShape(); 
+   var xoff = yoff; 
+   for (var x = 0; x <= width; x += 10) {
+   var y = map(noise(xoff), 0, 1, sand_5,50);
+    vertex(x, y); 
+    xoff += 0.05;
+  }
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+    
+//    noStroke();
+//    
+//    fill(100,31,31);
+//    push();
+//    scale(0.6);
+//    translate(100,400);
+//beginShape();
+//vertex(50, 20);
+//vertex(65, 20);
+//vertex(65, 60);    
+//vertex(80,60); 
+//vertex (80,40);
+//vertex(95,40);
+//vertex (95,80);    
+//vertex(80,80);
+//vertex(65,80);    
+//vertex(65, 130);
+//vertex(50, 130);
+//endShape(CLOSE);
+//    pop();
+//    
+//    
+//      fill(100,31,31);
+//    push();
+//    scale(0.6);
+//    translate(200,400);
+//beginShape();
+//vertex(-50, 20);
+//vertex(-65, 20);
+//vertex(-65, 50);    
+//vertex(-80,50); 
+//vertex(-80,35);
+//vertex(-95,35);
+//vertex(-95,70);    
+//vertex(-80,70);
+//vertex(-65,70);    
+//vertex(-65, 130);
+//vertex(-50, 130);
+//endShape(CLOSE);
+//    pop();
+
+        
+        
+        
+    }
+ 
+}
+
 
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
+  }
+     else if (key == ' ') {
+    patternMode = !patternMode;
   }
 }
