a063a88e8dbdd97fd99388e1db2ca898c034fff0
ashtondavvo <ashton.d173@live.com>
2017-09-24 23:12:31 +1300

    added colour

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
 let size = 100;
 
+var colour_red = [255,0,0];
+var colour_green = [0,255,0];
+var colour_blue = [0,0,255];
+let randomColourNum = 0;
+
 function setup () {
   createCanvas(960, 500);
+  randomColourNum = focusedRandom(0,3);
 }
 
 function draw () {
-  if (mouseIsPressed) {
-    fill(0);
-    drawShape(mouseX, mouseY, 100);
+
+  if(mouseIsPressed){
+    randomColourNum = focusedRandom(0,3);
   }
+
+  background(255,255,225);
+
   for(var i = 0; i < 10; i++){
     for(var j = 0; j < 10; j++){
       drawShape((i * 100), (j * 100), 100);
@@ -19,26 +28,26 @@ function draw () {
       drawShape((i * 100) +50, (j * 100) + 50, 100);
     }
   }
-  for(var i = 0; i < 10; i++){
-    for(var j = 0; j < 10; j++){
-      drawShape((i * 100) +25, (j * 100) + 25, 100);
-    }
-  }
-  for(var i = 0; i < 10; i++){
-    for(var j = 0; j < 10; j++){
-      drawShape((i * 100) +75, (j * 100) + 75, 100);
-    }
-  }
-  for(var i = 0; i < 10; i++){
-    for(var j = 0; j < 10; j++){
-      drawShape((i * 100) +25, (j * 100) + 75, 100);
-    }
-  }
-  for(var i = 0; i < 10; i++){
-    for(var j = 0; j < 10; j++){
-      drawShape((i * 100) +75, (j * 100) + 25, 100);
-    }
-  }
+  // for(var i = 0; i < 10; i++){
+  //   for(var j = 0; j < 10; j++){
+  //     drawShape((i * 100) +25, (j * 100) + 25, 100);
+  //   }
+  // }
+  // for(var i = 0; i < 10; i++){
+  //   for(var j = 0; j < 10; j++){
+  //     drawShape((i * 100) +75, (j * 100) + 75, 100);
+  //   }
+  // }
+  // for(var i = 0; i < 10; i++){
+  //   for(var j = 0; j < 10; j++){
+  //     drawShape((i * 100) +25, (j * 100) + 75, 100);
+  //   }
+  // }
+  // for(var i = 0; i < 10; i++){
+  //   for(var j = 0; j < 10; j++){
+  //     drawShape((i * 100) +75, (j * 100) + 25, 100);
+  //   }
+  // }
 }
 
 function keyTyped() {
@@ -56,10 +65,24 @@ function drawShape(xpos, ypos, size){
   let middle = size * 0.5;
   let step = size * 0.05;
   for(i = 0; i < 10; i++){
+    setColour(i);
     line(middle, i * step, middle - (step*i), middle); 
     line(middle, i * step, middle + (step*i), middle);
     line(middle, size - (i * step), middle - (step*i), middle);
     line(middle, size - (i * step), middle + (step*i), middle);
   }
   pop();
+}
+
+function setColour(index){
+
+  if((randomColourNum * index) < 4){
+    stroke(colour_red);
+  }
+  else if((randomColourNum * index) > 5 && (randomColourNum * index) < 9){
+    stroke(colour_green);
+  }
+  else if((randomColourNum * index) > 9){
+    stroke(colour_blue);
+  }
 }
