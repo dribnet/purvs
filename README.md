ba7f9d0368c63959f7d2ce5d44731b7a4b55873a
Henri Spencer <henrimspence@gmail.com>
2017-09-25 12:26:52 +1300

    Final ps3 commit.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 17.2.MDDN342 PS3
 
-I am still trying to get my code towards something I'm happy with. It is low on detail and not quite what I'm after, but I do like how it builds as mouse is pressed.
+I started this project with the intention of making a spider web style of pattern, but ended up moving more towards this electric or wave stlyed texture that is open to how it is seen by the viewer. 
 
+The colours are intended to be complementary, and as the mouse is pressed, the pattern is generated, making it more powerful and vusually interesting before it fades away again.
 
-I am trying to work towards making my pattern like a spider web, with lots of detail going on but also connections that continue when it is tiled. This is still progressing, and I hope to use this pattern to make a mountain landscape in some way.
+The landscape uses this pattern as an abstract, wind styled texture, to add to a mountain range which has used a slightly altered version of the wallpaper code. I felt it was important to keep both elements of this program related so I have tried to change as little as possible.
+
+The final element in the code are the soft clouds above. They provide a contrast to the jagged, harsh mountains below and sharp angles of the wind lines.
+
+This is a well used scene, but I have tried to build it in a way with lots of lines and strong angles to bring a bit of character and an abstract point of view to it.
 
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
  var  drawingMode;
+ var gridSize = 10;
 
 function setup() {
 drawingMode = 'wallpaper';
@@ -9,41 +10,23 @@ drawingMode = 'wallpaper';
   rectMode(CORNERS);
 
   var gridSize = 10;
-  //   noLoop();
-  //   background(0);
-    rectMode(CENTER);
+  rectMode(CENTER);
 
 }
 
-  // henriFunShape = createShape();
-  // henriFunShape.beginShape();
-  //  //All vertexs go here
-  //  star.vertex(10,40);
-  //  star.vertex(20,60);
-  //  star.vertex(30,30);
-  // henriFunShape.endShape(CLOSE);
-
 function draw () {
-	background(0);
     if(drawingMode === 'wallpaper'){
 		noFill();
 		drawPattern();
-		//drawLandscape();
 	}
 	else if(drawingMode === 'landscape'){
-    	print("landscape")
 		drawLandscape();
+		drawBackground();
+		blendMode(LIGHTEST);
+		drawSky();
+		blendMode(NORMAL);
 	}
 }
-
-// function changeMode(){
-// 	drawingMode = modeSelector.value();
-// 	clear();
-// 	background(0);
-// 	redraw();
-// }
-
-
 
 function changeRandomSeed() {
   curRandomSeed = curRandomSeed + 1;
@@ -51,12 +34,11 @@ function changeRandomSeed() {
 
 function mousePressed(){
     changeRandomSeed();
-    //clear();
-    //background(0);
-    redraw();
+	redraw();
 	
 }
 
+// Colour palette, selected from noise values.
 function colorFromValue(v) {
   if (v < 0.3) {
     return color(55, 103, 255);
@@ -72,169 +54,180 @@ function colorFromValue(v) {
   }
 }
 
+// Altered version of Pattern, changing grid locations and shape dimensions to form peaks.
 
   function drawLandscape () {
-  	fill(0);
+  	colorMode(RGB);
+	fill(0);
+
   	rect(0, 0, 2000, 2000);
 
-	  noiseSeed(curRandomSeed);
-	  resetFocusedRandom(curRandomSeed);
+	noiseSeed(curRandomSeed);
+	resetFocusedRandom(curRandomSeed);
 
-	  var x_steps = 1 + Math.floor(width / 30);
-	  var y_steps = 1 + Math.floor(height / 30);
+	var x_steps = 1 + Math.floor(width / 30);
+	var y_steps = 1 + Math.floor(height / 30);
 
+	var x_grid_locations = new Array(x_steps);
+	var y_grid_locations = new Array(y_steps);
 
-	  //resetFocusedRandom(curRandomSeed);
-
-	  var x_grid_locations = new Array(x_steps);
-	  var y_grid_locations = new Array(y_steps);
-
-	  for(var i=0;i<x_steps;i++) {
-	    var shift = focusedRandom(-10, 10, 2);
-	    var cur_x = i * 30 + shift;
-	    x_grid_locations[i] = cur_x;
-	  }
-
-	  for(var j=0;j<y_steps;j++) {
-	    var shift = focusedRandom(-10, 10, 2);
-	    shift = shift + 500/j;
-	    var cur_y = j * 30 + shift;
-	    y_grid_locations[j] = cur_y;
-	  }
-
-	  for(var i=0;i<x_steps-1;i++) {
-	    for(var j=0;j<y_steps-1;j++) {
-	      var x_noise = i / 10.0;
-	      var y_noise = j / 10.0;
-	      var noiseVal = noise(x_noise, y_noise);
-	      var shade = colorFromValue(noiseVal);
-	      fill(shade);
-	      //boxes being drawn
-	      //fill(focusedRandom(0, 255, 3));
-	      var x1 = x_grid_locations[i];
-	      var x2 = x_grid_locations[i+1];
-	      var y1 = y_grid_locations[j];
-	      var y2 = y_grid_locations[j] + j*20;
-
-	      //rect(x1, y1, x2, y2);
-	      //henriFunShape.display();
-	     
-	      //noStroke();
-	     // fill (120);
-	     /// rectMode(CENTER);
-	      //translate(width, height);
-	      // /rotate(PI/0.9);
-	  
-	     //for(var k = 0; k <= 10; k++){
-	      beginShape();
-	      fill(focusedRandom(200,0,0));
-	      fill(shade);
-	      vertex(x1, y1);
-	      var foo = Math.random() * (6 - 3) + 3   
-	      for(var k = 0;k < foo;k++){
-	        vertex(x1 + 1*k, y1 + 1*k);
-	        }
-	    //vertex(x2, y2);
-	    //vertex(x2 - 50, y2 - 50);
-	    //vertex(x1 + 30 , y2 + 30);
-	    //vertex(x1 + 40, y1 + 40);
-	      //rotate(PI/Math.random());
-	      //rotate(PI/0.5);
-	      triangle(x1 + k, y1 + k, x2, y2 , x1, y1);
-	      //triangle(x1 + k/2, y1 + k/2, x2/2, y2/2 , x1/2, y1/2);
-	      //triangle(x1 + 5, y1 + 5, x2, y2 , x1, y1);
-	      //triangle(x1 + k, y1 + k, x1, y1 , x2, y1);
-	      //triangle(x2+ k,  y1+ k, x1, y1, x1 + k, y2 + k);
-
-
-	      endShape();
-          fill(0)
-	      //triangle(0, 0, 200, 0, 0, 500);
-		  //triangle(400, 0, 560, 0, 480, 500);
-	      //triangle(960, 0, 960, 500, 760, 0);
-	    } 
-
-	      //rotate(PI/2.0);
-	      // rotate(PI/3.0);
-	      //rotate(PI/4.0);
-	      //ellipse (480, 250, 5, 15);
-
-	      //fill(255);
-	      // //rotate(PI/3.0);
-	     // rect((x1/25, y1/25, x2/25, y2/25));
-	      // //rotate(PI/3.0);
-	      // rect(x1-2, y1-2, 10, 3);
-	      // stroke(255, 50);
-	      // line(x1, y1, width/2, height/2);
-
-	      }
-	    //}
-	// void display () {
-	//   pushMatrix();
-	//   translate(x, y);
-	//   fill(102);
-	//   stroke(255);
-	//   strokeWeight(2);
-	//   beginShape();
-	//   vertex(0, -50);
-	//   vertex(14, -20);
-	//   vertex(47, -15);
-	//   vertex(23, 7);
-	//   vertex(29, 40);
-	//   vertex(0, 25);
-	//   vertex(-29, 40);
-	//   vertex(-23, 7);
-	//   vertex(-47, -15);
-	//   vertex(-14, -20);
-	//   endShape(CLOSE);
-	//   popMatrix();
-	// }
-
-
-	  // for (var x = gridSize; x <= width + gridSize; x += gridSize) {
-	  //   for (var y = gridSize; y <= height - gridSize; y += gridSize) {
-	  //     translate (width/4, height/4);
-	  //     noStroke();
-	  //     fill (120);
-	  //     rectMode(CENTER);
-	  //     translate(width/3.8, height/0.9);
-	  //     rotate(PI/0.9);
-	  //     //rect (480, 250, 10, 50);
-	  //     translate(width/4, height/4);
-	  //     rotate(PI/2.0);
-	  //     //rect (240, 250, 5, 55);
-	  //     translate(width/4, height/4);
-	  //     rotate(PI/3.0);
-	  //     //rect (180, 250, 25, 25);
-	  //     translate(width/4, height/4);
-	  //     rotate(PI/4.0);
-	  //     //rect (320, 250, 55, 55);
-	  //     translate(width/4, height/4);
-	  //     rotate(PI/5.0);
-	  //     rect (320, 250, 10, 50);
-
-	  //     fill(255);
-	  //     rotate(PI/3.0);
-	  //     rect(x+2, y-5, 3, 10);
-	  //     rotate(PI/3.0);
-	  //     rect(x-2, y-2, 10, 3);
-	  //     stroke(255, 50);
-	  //     line(x, y, width/2, height/2);
-	  //   }
-	  // }
-
+	for(var i=0;i<x_steps;i++) {
+	  var shift = focusedRandom(-10, 10, 2);
+	  var cur_x = i * 30 + shift;
+	  x_grid_locations[i] = cur_x;
 	}
 
+	for(var j=0;j<y_steps;j++) {
+	  var shift = focusedRandom(-10, 10, 2);
+	  shift = shift + 500/j;
+	  var cur_y = j * 30 + shift;
+	  y_grid_locations[j] = cur_y;
+	}
+
+	for(var i=0;i<x_steps-1;i++) {
+	  for(var j=0;j<y_steps-1;j++) {
+	    var x_noise = i / 10.0;
+	    var y_noise = j / 10.0;
+	    var noiseVal = noise(x_noise, y_noise);
+	    var shade = colorFromValue(noiseVal);
+	    fill(shade);
+
+//boxes being drawn
+
+	    var x1 = x_grid_locations[i];
+	    var x2 = x_grid_locations[i+1];
+	    var y1 = y_grid_locations[j];
+	    var y2 = y_grid_locations[j] + j*40;
+	  
+
+	    beginShape();
+	    fill(focusedRandom(200,0,0));
+	    fill(random(0, 255));
+	    vertex(x1, y1);
+	    var foo = Math.random() * (6 - 3) + 3   
+	    for(var k = 0;k < foo;k++){
+	      vertex(x1 + 5*k, y1 + 1*k);
+	    }
+	    triangle(x1 + k^2, y1 + k^2, x2, y2 , x1, y1);
+	    triangle(x1 + 15, y1 + 15, x2, y2 , x1, y1);
+		endShape();
+
+	    } 
+	    }
+	}
+
+// This is the basic shapes used to block out parts of the pattern to form the mountain skyline.
+function drawBackground () {
+
+	resetFocusedRandom(curRandomSeed);
+	noiseSeed(curRandomSeed);
+   	fill (0);
+	blendMode(MULTIPLY);
+
+// Main triangles
+
+	triangle(0 + random(0, 100), 0, 160, 350 + random(0, 200), 300 + random(-200, 200), 0);
+	triangle(480 + random(0, 200), 0, 660, 350 + random(0, 200), 760 + random(-200, 200), 0);
+	triangle(-280 + random(0, 200), 0, -200 + random(-50, 200), 300 + random(-200, 200), 300 + random(-200, 200), 0); 
+	triangle(420, 0, 510, 350, 600 + random(-50, 200), 0); 
+	triangle(220 + random(0, 200), 0, 290, 350 + random(0, 200), 390 + random(-200, 200), 0); 
+	triangle(-100 + random(0, 200), 0, -50, 350 + random(0, 200), 180 + random(-200, 200), 0);
+	fill(150);
+	triangle(-50 + random(0, 100), 0, 70, 350 + random(0, 200), 200 + random(-200, 200), 0);
+	triangle(430 + random(0, 200), 0, 610, 350 + random(0, 200), 750 + random(-200, 200), 0);
+	triangle(-180 + random(0, 200), 0, -50 + random(-50, 200), 250 + random(-200, 200), 300 + random(-200, 200), 0); 
+	triangle(370 + random(0, 200), 0, 480, 350 + random(0, 200), 580 + random(-200, 200), 0); 
+	triangle(170 + random(0, 200), 0, 250, 350 + random(0, 200), 340 + random(-200, 200), 0); 
+	triangle(-50 + random(0, 200), 0, 50, 350 + random(0, 200), 130 + random(-200, 200), 0); 
+
+// Main triangles
+
+	triangle(0 + random(0, 100), 0, 160, 350 + random(0, 200), 300 + random(-200, 200), 0);
+	triangle(480 + random(0, 200), 0, 660, 350 + random(0, 200), 760 + random(-200, 200), 0);
+	triangle(-280 + random(0, 200), 0, -200 + random(-50, 200), 300 + random(-200, 200), 300 + random(-200, 200), 0); 
+	triangle(420, 0, 510, 350, 600 + random(-50, 200), 0); 
+	triangle(220 + random(0, 200), 0, 290, 350 + random(0, 200), 390 + random(-200, 200), 0); 
+	triangle(-100 + random(0, 200), 0, -50, 350 + random(0, 200), 180 + random(-200, 200), 0);
+	fill(150);
+	triangle(-50 + random(0, 100), 0, 70, 350 + random(0, 200), 200 + random(-200, 200), 0);
+	triangle(430 + random(0, 200), 0, 610, 350 + random(0, 200), 750 + random(-200, 200), 0);
+	triangle(-180 + random(0, 200), 0, -50 + random(-50, 200), 250 + random(-200, 200), 300 + random(-200, 200), 0); 
+	triangle(370 + random(0, 200), 0, 480, 350 + random(0, 200), 580 + random(-200, 200), 0); 
+	triangle(170 + random(0, 200), 0, 250, 350 + random(0, 200), 340 + random(-200, 200), 0); 
+	triangle(-50 + random(0, 200), 0, 50, 350 + random(0, 200), 130 + random(-200, 200), 0); 
+
+// Bigger shapes to ensure angled ridge line.
+
+	fill(0);
+	quad(0,0, 960,0, 960,300 + random(-50, 50), 0, 250 + random(-50, 50));
+	quad(0,0, 960,0, 960,150 + random(-30, 80), 0, 350 + random(-30, 80));
+	blendMode(NORMAL);
+
+// Shading
+
+	fill (0, 150 + random(-30, 50));
+
+	triangle(0, 0, 60, 500 + random(-50, 50), 500 + random(-100, 100), 0);
+	triangle(480, 0, 760, 500 + random(-100, 50), 960 + random(-100, 100), 0);
+	triangle(-280 + random(0, 50), 0, 100 + random(-50, 50), 500 + random(-200, 200), 400 + random(-50, 40), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(420, 0, 500, 400 + random(0, 50), 600 + random(-200, 200), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(220, 0, 150, 400 + random(0, 50), 290 + random(-200, 200), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(520 + random(0, 50), 0, 650, 400 + random(0, 50), 890 + random(-200, 200), 0); 
+	triangle(720 + random(0, 50), 0, 850, 500 + random(0, 50), 990 + random(-200, 200), 0); 
+	triangle(180 + random(0, 50), 0, 300, 400 + random(0, 50), 490 + random(-200, 200), 0); 
+
+	fill (0, 150 + random(-30, 50));
+
+	triangle(0, 0, 60, 500 + random(-50, 50), 500 + random(-100, 100), 0);
+	triangle(480, 0, 760, 500 + random(-100, 50), 960 + random(-100, 100), 0);
+	triangle(-280 + random(0, 50), 0, 100 + random(-50, 50), 500 + random(-200, 200), 400 + random(-50, 40), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(420, 0, 500, 400 + random(0, 50), 600 + random(-200, 200), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(220, 0, 150, 400 + random(0, 50), 290 + random(-200, 200), 0); 
+	fill (0, 150 + random(-30, 50));
+	triangle(520 + random(0, 50), 0, 650, 400 + random(0, 50), 890 + random(-200, 200), 0); 
+	triangle(720 + random(0, 50), 0, 850, 500 + random(0, 50), 990 + random(-200, 200), 0); 
+	triangle(180 + random(0, 50), 0, 300, 400 + random(0, 50), 490 + random(-200, 200), 0); 
+	
+	fill(0);
+
+// Draw wallpaper pattern as abstract skylines, representing wind patterns.
+	
+	drawPattern();
+
+
+}
+
+// Cloud pattern, using for loops to overlay lots of semi-transparent ellipses.
+function drawSky(){
+
+	
+	noStroke();
+	fill(180, 0 + random(0, 5));
+
+	for(x = 0;x < 180 + random(-50, 50);x++){
+	    ellipse(-150 + random(0, 1100), -130 + random(0, 360), 300 + random(0, 500), 100 + random(0, 100));
+	} 
+	for(x = 0;x < 130 + random(-30, 30);x++){
+	    ellipse(-150 + random(0, 1100), -330 + random(0, 360), 300 + random(0, 500), 100 + random(0, 100));
+	} 
+}
+
+/* Main wallpaper, using triangles and altered grid to form 
+	pattern that plays with perspective and builds into different texture.*/
+
 function drawPattern () {
+
   resetFocusedRandom(curRandomSeed);
   noiseSeed(curRandomSeed);
-  //resetFocusedRandom(curRandomSeed);
+  background(10, 8);
 
   var x_steps = 1 + Math.floor(width / 30);
   var y_steps = 1 + Math.floor(height / 30);
-
-
-  //resetFocusedRandom(curRandomSeed);
 
   var x_grid_locations = new Array(x_steps);
   var y_grid_locations = new Array(y_steps);
@@ -258,23 +251,14 @@ function drawPattern () {
       var noiseVal = noise(x_noise, y_noise);
       var shade = colorFromValue(noiseVal);
       fill(shade);
-      //boxes being drawn
-      //fill(focusedRandom(0, 255, 3));
+
+//boxes being drawn
+
       var x1 = x_grid_locations[i];
       var x2 = x_grid_locations[i+1];
       var y1 = y_grid_locations[j];
       var y2 = y_grid_locations[j+1];
 
-      //rect(x1, y1, x2, y2);
-      //henriFunShape.display();
-     
-      //noStroke();
-     // fill (120);
-     /// rectMode(CENTER);
-      //translate(width, height);
-      // /rotate(PI/0.9);
-  
-     //for(var k = 0; k <= 10; k++){
       beginShape();
       fill(focusedRandom(200,0,0));
       fill(shade);
@@ -283,38 +267,29 @@ function drawPattern () {
       for(var k = 0;k < foo;k++){
         vertex(x1 + 1*k, y1 + 1*k);
         }
-    //vertex(x2, y2);
-    //vertex(x2 - 50, y2 - 50);
-    //vertex(x1 + 30 , y2 + 30);
-    //vertex(x1 + 40, y1 + 40);
-      //rotate(PI/Math.random());
-      //rotate(PI/0.5);
+ //Triangles along grid       
       triangle(x1 + k, y1 + k, x2, y2 , x1, y1);
-      //triangle(x1 + 5, y1 + 5, x2, y2 , x1, y1);
-      //triangle(x1 + k, y1 + k, x1, y1 , x2, y1);
-      //triangle(x2+ k,  y1+ k, x1, y1, x1 + k, y2 + k);
-
-
       endShape();
     } 
       }
 }
 
+// Function to save images.
+
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+// Function to change modes.
+
 if (key == ' ') {
 	
   if(drawingMode === 'wallpaper') {
 			drawingMode = 'landscape';
-			//console.log("SPACE");
 	}
-  else if(drawingMode === 'landscape'){  		
-  			drawingMode = 'wallpaper';
-  			
+  else if(drawingMode === 'landscape'){ 
+  			background(0); 		
+  			drawingMode = 'wallpaper';	
   		}
-		
 	}
-  
 }
