2c48b6989ca6268b684eef89e4239831e135e96a
Marc <marc.gluyas@gmail.com>
2017-09-25 12:50:54 +1300

    Added flowers

--- sketch.js
+++ sketch.js
@@ -13,6 +13,8 @@ let palettes = [
 let palette = [];
 let paletteNum;
 
+let vines = [];
+
 function setup () {
     createCanvas(960, 500);
     angleMode(DEGREES);
@@ -32,63 +34,75 @@ function init() {
     } while(newPalette == paletteNum);
     paletteNum = newPalette;
     palette = palettes[paletteNum];
+
+    vines = [];
 }
 
 function draw () {
-    if (++iterations > maxIterations) return;
-    let {tris} = data;
     push();
     {
         //clear()
         //scale(width/viewWidth, width/viewWidth);
         //rotate(random(72))
-        background(palette[0]);
         translate(-viewPos.x, -viewPos.y);
         scale(zoom, zoom);
 
-        tris.forEach((tri) => {
-            if (tri.acute) {
-                fill(palette[1]);
-            } else {
-                fill(palette[0]);
+        if (iterations < maxIterations) {
+            background(palette[0]);
+            let {tris} = data;
+
+            tris.forEach((tri) => {
+                if (tri.acute) {
+                    fill(palette[1]);
+                } else {
+                    fill(palette[0]);
+                }
+
+                strokeWeight(0.0000003 * zoom);
+                //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
+
+                noStroke();
+                //noSmooth();
+                beginShape();
+                    vertex(tri.points[0].x, tri.points[0].y);
+                    vertex(tri.points[1].x, tri.points[1].y);
+                    vertex(tri.points[2].x, tri.points[2].y);
+                endShape(CLOSE);
+
+                strokeWeight(0.0000003 * zoom);
+                //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
+                stroke(0);
+                line(tri.left.edge.a.x, tri.left.edge.a.y, tri.left.edge.b.x, tri.left.edge.b.y);
+                line(tri.right.edge.a.x, tri.right.edge.a.y, tri.right.edge.b.x, tri.right.edge.b.y);
+                //line(tri.base.edge.a.x, tri.base.edge.a.y , tri.base.edge.b.x, tri.base.edge.b.y);
+            })
+
+            data = penrose.subdivide(data, 1, penrose.pruneAABB(
+                viewPos.copy().div(zoom),
+                viewPos.copy().div(zoom).add(createVector(width/zoom, height/zoom))
+            ));
+
+            if (++iterations == maxIterations) {
+                console.log("done");
+                for (let i = 0; i < 10; i++) {
+                    vines.push(new vine.Vine(data.tris[floor(random(data.tris.length))], [vine.Dir.base, vine.Dir.left]));
+                }
             }
+        } else {
+            vines.forEach(vine => {
+                if (vine.pos == null) return;
+                console.log(vine);
+                let oldPos = vine.pos;
+                vine.creep();
+                if (vine.pos == null) return;
 
-            strokeWeight(0.0000003 * zoom);
-            //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
-
-            noStroke();
-            noSmooth();
-            beginShape();
-                vertex(tri.points[0].x, tri.points[0].y);
-                vertex(tri.points[1].x, tri.points[1].y);
-                vertex(tri.points[2].x, tri.points[2].y);
-            endShape(CLOSE);
-
-            strokeWeight(0.0000003 * zoom);
-            //line(tri.points[0].x, tri.points[0].y, tri.points[2].x, tri.points[2].y)
-
-            smooth();
-            stroke(0);
-            line(tri.left.edge.a.x, tri.left.edge.a.y, tri.left.edge.b.x, tri.left.edge.b.y);
-            line(tri.right.edge.a.x, tri.right.edge.a.y, tri.right.edge.b.x, tri.right.edge.b.y);
-            //line(tri.base.edge.a.x, tri.base.edge.a.y , tri.base.edge.b.x, tri.base.edge.b.y);
-        })
+                stroke(palette[1]);
+                strokeWeight(0.005)
+                line(oldPos.points[1].x, oldPos.points[1].y, vine.pos.points[1].x, vine.pos.points[1].y);
+            })
+        }
     }
     pop();
-
-    data = penrose.subdivide(data, 1, penrose.pruneAABB(
-        viewPos.copy().div(zoom),
-        viewPos.copy().div(zoom).add(createVector(width/zoom, height/zoom))
-    ));
-
-    if (iterations == maxIterations) console.log("done");
-
-    // if (mouseIsPressed) {
-    //     fill(0);
-    // } else {
-    //     fill(255);
-    // }
-    // ellipse(mouseX, mouseY, 80, 80);
 }
 
 function mouseClicked() {
---
+++ vine.js
@@ -0,0 +1,52 @@
+const vine = {};
+
+(function(exports) {
+
+    exports.Dir = {
+        base : 0,
+        left : 1,
+        right : 2,
+    };;
+
+    function getEdge(tri, dir) {
+        switch (dir) {
+            case 0:
+                return tri.base.edge;
+            case 1:
+                return tri.left.edge;
+            case 2:
+                return tri.right.edge;
+            default:
+                console.log("hey");
+        }
+    }
+
+    function adjacentTri(tri, dir) {
+        let adjacent = getEdge(tri, dir).adjacent;
+        for (let i = 0; i < adjacent.length; i++) {
+            if (adjacent[i] != tri) return adjacent[i];
+        }
+        return null;
+    }
+
+    // pos: <Tri>
+    // edgeSequence: <[vine.Dir]>
+    exports.Vine = function(startPos, edgeSequence) {
+        this.pos = startPos
+
+        let cycle = false;
+        let index = 0;
+        this.creep = function(steps = 1) {
+            //if (cycle) return;
+
+            for (let i = 0; i < steps; i++) {
+                this.pos = adjacentTri(this.pos, edgeSequence[index]);
+                index = (index + 1) % edgeSequence.length;
+                // if (pos == startPos && index == 0) {
+                //     cycle = true;
+                // }
+            }
+        }
+    }
+
+})(vine);
