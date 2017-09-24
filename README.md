7b9d7e043c28c4d9357d082988fa6e0d764b6d8b
Samantha Gard <sam.gard@abletech.co.nz>
2017-09-24 14:18:52 +1300

    worked out code to draw isometric cube for tiling pattern

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 17.2.MDDN342 PS3
 
-This is the starter README for PS3. As you update your code, replace this text with information on your progress. Also be sure to periodically update your preview.jpg and thumbnail.png files.
+When looking into this first part of the assignment, I was really interested in using geometric shapes to create a tiling pattern.
+I've always been interested in isometric views - I'm a big fan of the game Monument Valley, which takes inspiration from M.C. Escher drawings, optical illusions, and impossible objects.
+
+I've decided to create a cube for my tiling pattern. So far, I've just been working on figuring out all the math required to make such an object. I mucked around with drawing it using sheared and rotated rects to begin with, but this got really confusing really quickly. So, I looked up how to draw hexagons (much easier than I thought), and then set about manipulating those first 6 points of the hexagon that I drew to get the rest of the cube's perspective.
--- sketch.js
+++ sketch.js
@@ -1,16 +1,165 @@
-function setup () {
+var bgColor = [242,240,241],
+    strokeColor = [53,53,53];
+
+var img;
+
+function setup() {
   createCanvas(960, 500);
 }
 
-function draw () {
-  if (mouseIsPressed) {
-    fill(0);
-  }
-  else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw() {
+  // set up background
+  noStroke();
+  fill(bgColor);
+  rect(0,0,width,height);
+
+  translate(width/2, height/2);
+
+  noFill();
+  strokeWeight(4);
+  stroke(strokeColor);
+  drawCube();
 }
+
+function drawCube() {
+  // draw base hexagon
+  rotate(PI/6.0);
+
+  push();
+  beginShape();
+  for(var i=0; i<6; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*200,sin(angle)*200);
+    pop();
+  }
+  endShape(CLOSE);
+  pop();
+
+  // draw inner lines to make cube
+  push();
+  for(var i=1; i<6; i+=2) {
+    var angle = PI*i/3;
+    line(0,0,cos(angle)*200,sin(angle)*200);
+  }
+  pop();
+
+  // draw left cube face inner
+  push();
+  translate(-15, 25);
+
+  beginShape();
+  for(var i=1; i<4; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*145,sin(angle)*145);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  beginShape();
+  for(var i=1; i<4; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*120,sin(angle)*120);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  var angle = PI*2/3;
+  line(0,0,cos(angle)*145,sin(angle)*145);
+
+  push();
+  translate(-30,0);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+
+  push();
+  translate(15,25);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+
+  pop();
+
+
+  // draw top cube face inner
+  push();
+  translate(-15, -25);
+  beginShape();
+  for(var i=3; i<6; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*145,sin(angle)*145);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  beginShape();
+  for(var i=3; i<6; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*120,sin(angle)*120);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  var angle = PI*4/3;
+  line(0,0,cos(angle)*145,sin(angle)*145);
+
+  push();
+  translate(15,-25);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+
+  push();
+  translate(-30,0);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+  pop();
+
+  // draw right cube face inner
+  push();
+  translate(30, 0);
+  beginShape();
+  for(var i=5; i<8; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*145,sin(angle)*145);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  beginShape();
+  for(var i=5; i<8; i++) {
+    push();
+    var angle = PI*i/3;
+    vertex(cos(angle)*120,sin(angle)*120);
+    pop();
+  }
+  vertex(0,0);
+  endShape(CLOSE);
+
+  var angle = PI*6/3;
+  line(0,0,cos(angle)*145,sin(angle)*145);
+
+  push();
+  translate(15,-25);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+
+  push();
+  translate(15,25);
+  line(0,0,cos(angle)*90,sin(angle)*90);
+  pop();
+
+  pop();
+}
+
 
 function keyTyped() {
   if (key == '!') {
