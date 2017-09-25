25d62c0ad4237e6b41b647cb56c177e8041ea395
Samantha Gard <sam.gard@abletech.co.nz>
2017-09-25 23:01:22 +1300

    Redrew inner parts of cube to allow for colour blocking Updated sliders

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 17.2.MDDN342 PS3
 
+### 22/09
 When looking into this first part of the assignment, I was really interested in using geometric shapes to create a tiling pattern.
 I've always been interested in isometric views - I'm a big fan of the game Monument Valley, which takes inspiration from M.C. Escher drawings, optical illusions, and impossible objects.
 
 I've decided to create a cube for my tiling pattern. So far, I've just been working on figuring out all the math required to make such an object. I mucked around with drawing it using sheared and rotated rects to begin with, but this got really confusing really quickly. So, I looked up how to draw hexagons (much easier than I thought), and then set about manipulating those first 6 points of the hexagon that I drew to get the rest of the cube's perspective.
+
+### 24/09
+I've now slightly changed the slider controls - the first turns on/off the randomised rotation of the cubes, while the second still controls the size of the cubes.
+I've also reduced the number of cubes drawn, as I feel they are quite an intricate shape, so less is more, especially as bigger cubes allow you to see the detail more easily.
+
+When refining this design I began to think about how I could make the design easier to look at. I knew colour would make a huge difference, but the way I'd draw the cube was a bit hacky, so this didn't work as I had originally drawn them. So, I decided to spend several hours reworking how I drew the 'supporting beams' of the cube so that I could use fills and have a shape I could use colour with! It's looking great now, and I'm hoping to try out some cool combinations/randomised colour palettes when I pick up on this again tomorrow.
--- sketch.js
+++ sketch.js
@@ -1,47 +1,75 @@
 var canvasWidth = 960;
 var canvasHeight = 500;
+var curRandomSeed;
 
-var slider1;
+var slider1, slider2;
 
-var bgColor = [242,240,241],
-    strokeColor = [53,53,53];
+var bgColor = [246,104,85],
+    strokeColor = [52,93,116],
+    cubeColor = [255,169,79];
 
 function setup() {
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
+  curRandomSeed = int(focusedRandom(0, 100));
+
   // create sliders
-  slider1 = createSlider(0, 100, 50);
+  slider1 = createSlider(0, 1, 0);
+  slider2 = createSlider(0, 100, 50);
 
   slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+}
+
+
+function changeRandomSeed() {
+  curRandomSeed += 1;
+}
+
+function mouseClicked(){
+	// if(mode == 'LandScape'){
+	// 	setupGrid();
+	// 	drawLandscape();
+	// }
+	// else {
+  	changeRandomSeed();
+	// 	drawShapes();
+  // }
 }
 
 function draw() {
+  resetFocusedRandom(curRandomSeed);
+
   // set up background
   noStroke();
   fill(bgColor);
   rect(0,0,width,height);
 
   var s1 = slider1.value();
-  // var gapSize = map(s1, 0, 100, -90, 90);
-  var gapSize = map(s1,0,100,0,1);
+  var s2 = slider2.value();
 
-  var w = width/8,
-      h = height/5;
+  var rotationState = s1;
+  var cubeSize = map(s2,0,100,.75,1.25);
 
-  for(var i=0; i<6; i++) {
-    for(var j=0; j<10; j++) {
+  var w = width/6,
+      h = height/3;
+
+  // drawCube(width/2,height/2,width,height,rotationState);
+
+  for(var i=0; i<4; i++) {
+    for(var j=0; j<6; j++) {
       // positioning the cube
       var y = h/2 + h*i,
           x = w/2 + w*j;
 
       // drawing the cube
-      drawCube(x,y,w,h,gapSize);
+      drawCube(x,y,w*cubeSize,h*cubeSize,rotationState);
     }
   }
 }
 
-function drawCube(x,y,w,h,gapSize) {
+function drawCube(x,y,w,h,rotationState) {
   push();
   translate(x,y);
 
@@ -51,14 +79,20 @@ function drawCube(x,y,w,h,gapSize) {
   var scale = extent / 250.0;
 
   // drawing style
-  noFill();
+  // noFill();
   strokeWeight(4*scale);
   stroke(strokeColor);
 
   // draw base hexagon
-  rotate(PI/6.0);
+  var cubeRotation = focusedRandom(0,100)*rotationState;
+  if (cubeRotation == 0) {
+    rotate(PI/6.0);
+  } else {
+    rotate(PI/6.0*cubeRotation);
+  }
 
   push();
+  fill(cubeColor);
   beginShape();
   for(var i=0; i<6; i++) {
     push();
@@ -81,6 +115,9 @@ function drawCube(x,y,w,h,gapSize) {
   push();
   translate(-15*scale, 25*scale);
 
+  fill(bgColor);
+
+  // first cutout
   beginShape();
   for(var i=1; i<4; i++) {
     push();
@@ -91,28 +128,39 @@ function drawCube(x,y,w,h,gapSize) {
   vertex(0,0);
   endShape(CLOSE);
 
+  fill(cubeColor);
+
+  // outer top
   beginShape();
-  for(var i=1; i<4; i++) {
-    push();
-    var angle = PI*i/3;
-    vertex(cos(angle)*120*scale,sin(angle)*120*scale);
-    pop();
-  }
-  vertex(0,0);
+  vertex(cos(PI)*120*scale,sin(PI)*120*scale);
+  vertex(cos(PI)*145*scale,sin(PI)*145*scale);
+  vertex(cos(PI*2/3)*145*scale,sin(PI*2/3)*145*scale);
+  vertex(cos(PI*2/3)*120*scale,sin(PI*2/3)*120*scale);
   endShape(CLOSE);
 
-  var angle = PI*2/3;
-  line(0,0,cos(angle)*145*scale,sin(angle)*145*scale);
+  // outer bottom
+  beginShape();
+  vertex(cos(PI*2/3)*145*scale,sin(PI*2/3)*145*scale);
+  vertex(cos(PI*2/3)*120*scale,sin(PI*2/3)*120*scale);
+  vertex(cos(PI*1/3)*120*scale,sin(PI*1/3)*120*scale);
+  vertex(cos(PI*1/3)*145*scale,sin(PI*1/3)*145*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(-30*scale,0);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner top
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI)*25*scale,sin(PI)*25*scale);
+  vertex(cos(PI*2/2.75)*110*scale,sin(PI*2/2.75)*110*scale);
+  vertex(cos(PI*2/3)*120*scale,sin(PI*2/3)*120*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(15*scale,25*scale);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner bottom
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI*1/3)*25*scale,sin(PI*1/3)*25*scale);
+  vertex(cos(PI*2/3.3)*110*scale,sin(PI*2/3.3)*110*scale);
+  vertex(cos(PI*2/3)*120*scale,sin(PI*2/3)*120*scale);
+  endShape(CLOSE);
 
   pop();
 
@@ -120,6 +168,10 @@ function drawCube(x,y,w,h,gapSize) {
   // draw top cube face inner
   push();
   translate(-15*scale, -25*scale);
+
+  fill(bgColor);
+
+  // first cutout
   beginShape();
   for(var i=3; i<6; i++) {
     push();
@@ -130,34 +182,49 @@ function drawCube(x,y,w,h,gapSize) {
   vertex(0,0);
   endShape(CLOSE);
 
+  fill(cubeColor);
+
+  // outer left
   beginShape();
-  for(var i=3; i<6; i++) {
-    push();
-    var angle = PI*i/3;
-    vertex(cos(angle)*120*scale,sin(angle)*120*scale);
-    pop();
-  }
-  vertex(0,0);
+  vertex(cos(PI)*120*scale,sin(PI)*120*scale);
+  vertex(cos(PI)*145*scale,sin(PI)*145*scale);
+  vertex(cos(PI*4/3)*145*scale,sin(PI*4/3)*145*scale);
+  vertex(cos(PI*4/3)*120*scale,sin(PI*4/3)*120*scale);
   endShape(CLOSE);
 
-  var angle = PI*4/3;
-  line(0,0,cos(angle)*145*scale,sin(angle)*145*scale);
+  // outer right
+  beginShape();
+  vertex(cos(PI*4/3)*145*scale,sin(PI*4/3)*145*scale);
+  vertex(cos(PI*4/3)*120*scale,sin(PI*4/3)*120*scale);
+  vertex(cos(PI*5/3)*120*scale,sin(PI*5/3)*120*scale);
+  vertex(cos(PI*5/3)*145*scale,sin(PI*5/3)*145*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(15*scale,-25*scale);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner left
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI)*25*scale,sin(PI)*25*scale);
+  vertex(cos(PI*4/3.15)*110*scale,sin(PI*4/3.15)*110*scale);
+  vertex(cos(PI*4/3)*120*scale,sin(PI*4/3)*120*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(-30*scale,0);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner right
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI*5/3)*25*scale,sin(PI*5/3)*25*scale);
+  vertex(cos(PI*4/2.86)*110*scale,sin(PI*4/2.86)*110*scale);
+  vertex(cos(PI*4/3)*120*scale,sin(PI*4/3)*120*scale);
+  endShape(CLOSE);
 
   pop();
 
   // draw right cube face inner
   push();
   translate(30*scale, 0);
+
+  fill(bgColor);
+
+  // first cutout
   beginShape();
   for(var i=5; i<8; i++) {
     push();
@@ -168,28 +235,39 @@ function drawCube(x,y,w,h,gapSize) {
   vertex(0,0);
   endShape(CLOSE);
 
+  fill(cubeColor);
+
+  // outer top
   beginShape();
-  for(var i=5; i<8; i++) {
-    push();
-    var angle = PI*i/3;
-    vertex(cos(angle)*120*scale,sin(angle)*120*scale);
-    pop();
-  }
-  vertex(0,0);
+  vertex(cos(PI*5/3)*120*scale,sin(PI*5/3)*120*scale);
+  vertex(cos(PI*5/3)*145*scale,sin(PI*5/3)*145*scale);
+  vertex(cos(PI*6/3)*145*scale,sin(PI*6/3)*145*scale);
+  vertex(cos(PI*6/3)*120*scale,sin(PI*6/3)*120*scale);
   endShape(CLOSE);
 
-  var angle = PI*6/3;
-  line(0,0,cos(angle)*145*scale,sin(angle)*145*scale);
+  // outer bottom
+  beginShape();
+  vertex(cos(PI*6/3)*145*scale,sin(PI*6/3)*145*scale);
+  vertex(cos(PI*6/3)*120*scale,sin(PI*6/3)*120*scale);
+  vertex(cos(PI*1/3)*120*scale,sin(PI*1/3)*120*scale);
+  vertex(cos(PI*1/3)*145*scale,sin(PI*1/3)*145*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(15*scale,-25*scale);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner top
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI*5/3)*25*scale,sin(PI*5/3)*25*scale);
+  vertex(cos(PI*1.94)*110*scale,sin(PI*1.94)*110*scale);
+  vertex(cos(PI*6/3)*120*scale,sin(PI*6/3)*120*scale);
+  endShape(CLOSE);
 
-  push();
-  translate(15*scale,25*scale);
-  line(0,0,cos(angle)*90*scale,sin(angle)*90*scale);
-  pop();
+  // inner bottom
+  beginShape();
+  vertex(0,0);
+  vertex(cos(PI*1/3)*25*scale,sin(PI*1/3)*25*scale);
+  vertex(cos(PI*2.06)*110*scale,sin(PI*2.06)*110*scale);
+  vertex(cos(PI*6/3)*120*scale,sin(PI*6/3)*120*scale);
+  endShape(CLOSE);
 
   pop();
 
