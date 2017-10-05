7b5fad8f181fd0b2d0b5d3bd607723a67a1a2fe0
Shane Watters <shane.watters.nz@gmail.com>
2017-10-05 18:18:32 +1300

    I have now got the basics of my high level design working. I have introduced a wider variety of colours and the pattern made by hexagons is drawing in the order I want it to. I have now also stroke weight for the hexagon outlines working well so when you zoom in they get bigger and the transparency effect I am using becomes more apparent.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,38 @@
+//for debugging
+var debug = false;
+
+var line_width = 2;
+ 
+//array of hue ranges used for selecting colour of the hex outline
+var huesArray = [0,30,60,90,120,150,180,210,240,270,300,330];
+				
+var highLevelCoOrdinates = [
+	[272, 512],
+	[512, 332],
+	[752, 512],
+	[512, 692],
+	[272, 872],
+	[32, 692],
+	[32, 332],
+	[272, 152],
+	[752, 152],
+	[992, 332],
+	[992, 692],
+	[752, 872],
+	[512, 1052],
+	[992, 1052],
+	[1232, 872],
+	[1232, 512],
+	[1232, 152],
+	[992, -28],
+	[512, -28],
+	[32, -28],
+	[-208, 152],
+	[-208, 512],
+	[-208, 872],
+	[32, 1052]
+];
+
   /*
  * This is the funciton to implement to make your own abstract design.
  *
@@ -13,17 +48,13 @@
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  console.log(y1, y2);
   p5.background(0);
+  p5.colorMode(p5.HSB);
   p5.rectMode(p5.CORNERS);
 
-  // The first red rectangle fills the entire space
-  var cx = p5.map(512-960/2, x1, x2, 0, 256);
-  var cy = p5.map(512-720/2, y1, y2, 0, 256);
-  var cx2 = p5.map(512+960/2, x1, x2, 0, 256);
-  var cy2 = p5.map(512+720/2, y1, y2, 0, 256);
-  p5.fill(255, 0, 0);
-  p5.rect(cx, cy, cx2, cy2);
+  if(debug){
+	drawFrame(p5, x1, x2, y1, y2,);
+  }
 
   // The second black rectangle is inset to form a frame inset by 20 units
   cx = p5.map(512-956/2, x1, x2, 0, 256);
@@ -33,56 +64,26 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   p5.fill(0);
   p5.rect(cx, cy, cx2, cy2);
 
-  var centerX = 512;
-  var centerY = 512;
+	var centerX = 512, centerY = 512;
 
-  for(var j =-1; j <= 1; j++){
-    centerY = 512  + (480 * j);
-    for(var i =-1; i <= 2; i++){
-      centerX = 512 -180 + (360 * i);
-      p5.stroke(0, 255, 0);
-      drawHexOutline(p5, centerX, centerY, x1, x2, y1, y2);  
-      for (var pos = 0; pos < bigHex.length; pos++) {
-          var xPos = bigHex[pos][0];
-          var yPos = bigHex[pos][1];
-          cx = p5.map(centerX + xPos, x1, x2, 0, 256);
-          cy = p5.map(centerY+ yPos, y1, y2, 0, 256);
-          p5.ellipse(cx, cy, 50);
-      }
+    for(var i=0; i < highLevelCoOrdinates.length; i++){
+		centerX = highLevelCoOrdinates[i][0];
+		centerY = highLevelCoOrdinates[i][1];
+		var colour = p5.color(huesArray[(i % 12)], 100, 100);
+		//draw the hexagon outline that groups all the glyphs together
+		for (var adjuster = -2; adjuster <= 2; adjuster++) {
+			drawHexOutline(p5, centerX, centerY, x1, x2, y1, y2, colour, adjuster * 2);
+		}
+		p5.strokeWeight(1);
+		for (var pos = 0; pos < bigHex.length; pos++) {
+			var xPos = bigHex[pos][0];
+			var yPos = bigHex[pos][1];
+			cx = p5.map(centerX + xPos, x1, x2, 0, 256);
+			cy = p5.map(centerY+ yPos, y1, y2, 0, 256);
+			p5.ellipse(cx, cy, 50);
+		}
     }
-  }
-
-  for(var j =-1; j <= 2; j++){
-    centerY = 512 - 240 + (480 * j);
-    for(var i =-1; i <= 1; i++){
-      centerX = 512 + (360 * i);
-      p5.stroke(0, 255, 255);
-      drawHexOutline(p5, centerX, centerY, x1, x2, y1, y2);  
-      for (var pos = 0; pos < bigHex.length; pos++) {
-          var xPos = bigHex[pos][0];
-          var yPos = bigHex[pos][1];
-          cx = p5.map(centerX + xPos, x1, x2, 0, 256);
-          cy = p5.map(centerY+ yPos, y1, y2, 0, 256);
-          p5.ellipse(cx, cy, 50);
-      }
-    }
-  }
-    
-  
-
 }
-
-var homeLevelCoordinates
-
-// var bigHex = [
-                
-//                 [40, -40], [40, 40], [-40, 40], [-40, -40],
-//                 [40, -120], [120, -40], [120, 40], [40, 120],
-//                 [-40, 120], [-120, 40], [-120, -40], [-40, -120],
-//                 [40,-200], [120, -120],[200, -40], [200, 40],
-//                 [120, 120], [40,200],[-40,200], [-120, 120],
-//                 [-200, 40], [-200, -40], [-120, -120], [-40, -200]
-//             ];
 
 var bigHex = [
                 
@@ -93,28 +94,36 @@ var bigHex = [
                 [90, 90], [30,150],[-30,150], [-90, 90],
                 [-150, 30], [-150, -30], [-90, -90], [-30, -150]
             ];
-//array of hue ranges used for selecting colour of the hex outline
-var hueRanges = [
-                    [-22.5, 22.5, 0],
-                    [22.5, 67.5, 45],
-                    [67.5, 112.5, 90],
-                    [112.5, 157.5, 135],
-                    [157.5, 202.5, 180],
-                    [202.5, 247.5, 225],
-                    [247.5, 292.5, 270],
-                    [292.5, 337.5, 315],
-                ];
+
 
 
 //draws the lines that connects the hexagons together
-function drawHexOutline(p5, centerX, centerY, x1, x2, y1, y2) {
+function drawHexOutline(p5, centerX, centerY, x1, x2, y1, y2, colour, adjuster) {
     p5.noFill();
     p5.beginShape();
-    p5.strokeWeight(  2);
-    var xPos, yPos, cx, cy, ad;
+    
+    var xPos, yPos, cx, cy;
+	var c_p00 = p5.map(0, x1, x2, 0, 256);
+	var c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+	var cur_line_width = c_plwidth - c_p00;
+	p5.strokeWeight(cur_line_width);
+	colour._array[3] = 1 - (0.2 * Math.abs(adjuster));
+    p5.stroke(colour);
     for (var pos = 12; pos < bigHex.length; pos++) {
         var xPos = bigHex[pos][0];
         var yPos = bigHex[pos][1];
+		if(xPos > 0){
+            xPos = xPos + adjuster;
+        }
+        else {
+            xPos = xPos - adjuster;   
+        }
+        if(yPos > 0){
+            yPos = yPos + adjuster;
+        }
+        else {
+            yPos = yPos - adjuster;   
+        }
         cx = p5.map(centerX + xPos, x1, x2, 0, 256);
         cy = p5.map(centerY + yPos, y1, y2, 0, 256);
         p5.vertex(cx, cy);
@@ -179,3 +188,14 @@ function octagon(p5, x, y, radius) {
     }
     p5.endShape(p5.CLOSE);
 }
+
+
+//red rectangle drawn to show the frame 
+function drawFrame(p5, x1, x2, y1, y2,){
+	var cx = p5.map(512-960/2, x1, x2, 0, 256);
+	var cy = p5.map(512-720/2, y1, y2, 0, 256);
+	var cx2 = p5.map(512+960/2, x1, x2, 0, 256);
+	var cy2 = p5.map(512+720/2, y1, y2, 0, 256);
+	p5.fill(0, 100, 100);
+	p5.rect(cx, cy, cx2, cy2);
+}	 
