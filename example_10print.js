cb21308a3cdb9bc8fc72344ae62deb23037265c7
James Campbell <jb.campbell13@hotmail.com>
2017-10-09 12:57:43 +1300

    sketch design added.

--- example_10print.js
+++ example_10print.js
@@ -4,38 +4,35 @@ var blue = "#0000AA"
 var light_blue = "#0088FF"
 
 function drawLayer(p5, slashsize, x1, x2, y1, y2, z) {
-  var noiseScale=1/16.0;
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
-  p5.strokeWeight(pixel_width);
+  var startx = slashsize * (Math.floor(x1 / slashsize) - 1);
+  var starty = slashsize * (Math.floor(y1 / slashsize) - 1);
+  var endx = slashsize * (Math.floor(x2 / slashsize) + 1);
+  var endy = slashsize * (Math.floor(y2 / slashsize) + 1);
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  for(var x = startx; x < endx; x += slashsize) {
+
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+    var width = p5.map(x + 50, x1, x2, 0, 256);
+    var length = p5.map(x + 100, x1, x2, 0, 256);
+
+    for(var y = starty; y < endy; y += slashsize) {
+
       var y_pos = p5.map(y, y1, y2, 0, 256);
-      var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
-      if (noiseValue < 0.5) {
-        p5.line(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+
+      if(p5.random(1) <= 1) {
+        p5.rect(x_pos, y_pos, width - x_pos, width - x_pos, 100);
       }
-      else {
-        p5.line(x_pos, y_pos+char_height, x_pos+char_width, y_pos);
-      }
+
     }
   }
 }
 
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  p5.noiseDetail(8,0.5);
+
   p5.background(blue);
-  p5.stroke(light_blue);
-  drawLayer(p5, 16, x1, x2, y1, y2, z);
+  p5.noStroke();
+  p5.fill(light_blue);
+  drawLayer(p5, 75, x1, x2, y1, y2, z);
+
 }
