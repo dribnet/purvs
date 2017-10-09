8ba47d2a94e40102aded6940214eba5c3c321bce
Lara Hayward <haywardlbl@gmail.com>
2017-10-09 15:08:13 +1300

    Part 2

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-## Powers of 10 PRINT
+Part 2
 
-A daydream of what might be hidden within the C64 10print maze
-using p5.js and leaflet.
+I've changed the example_fence.js code to fit my idea. I've just removed the lines, made the circles smaller, made the max movement larger so the stars look more randomly placed and gave the stars random colours.
 
-You can also use the Open link at right to open this app in a full screen
-window which additionally supports stateful URLs so
-you can bookmark favorite found locations.
--- example_fence.js
+++ example_fence.js
@@ -1,8 +1,10 @@
 var max_thickness = 64;
-var max_movement = 32;
-var ball_radius = 32;
+var max_movement = 150;
+var ball_radius = 3;
 var line_width = 8;
-var grid_size = 64;
+var grid_size = 50;
+var planetColours = new Array();
+
 
 function getOffsetPoint(p5, x, y, z, noiseScale) {
   var noiseX = p5.noise(x * noiseScale,
@@ -13,6 +15,9 @@ function getOffsetPoint(p5, x, y, z, noiseScale) {
   var offsetY = p5.map(noiseY, 0, 1, -max_movement, max_movement);
   return [x+offsetX, y+offsetY]
 }
+
+
+
 
 function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
@@ -44,15 +49,38 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   var cur_line_width = c_plwidth - c_p00;
   var cur_ball_radius = c_pball - c_p00;
 
-  p5.background(255);
-  p5.fill(0, 0, 128);
+  var c1 = p5.color(255, 219, 191); //orange
+  var c2 = p5.color(255, 214, 227); //pink
+  var c3 = p5.color(255, 254, 201); //yellow
+  var c4 = p5.color(224, 255, 216); //green
+  var c5 = p5.color(216, 228, 255); //blue
+
+  p5.append(planetColours, c1);
+  p5.append(planetColours, c2);
+  p5.append(planetColours, c3);
+  p5.append(planetColours, c4);
+  p5.append(planetColours, c5);
+
+  
+
+
+  p5.background(0, 4, 12);
+
+
+
+
+  
   for(var x=min_x; x<max_x; x+=grid_size) {
     for(var y=min_y; y<max_y; y+=grid_size) {
-      var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+      var shift_point = getOffsetPoint(p5, x, y, z, 0.5);
       var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
       var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
 
-      p5.strokeWeight(cur_line_width);
+      var t = p5.floor(p5.map(p5.noise(x, y), 0, 1, 0, 5));
+      p5.fill(planetColours[t]);
+      //console.log(t);
+
+      /*p5.strokeWeight(cur_line_width);
       p5.stroke(0, 0, 128);
       var shift_point2 = getOffsetPoint(p5, x+grid_size, y, z, 0.1);
       var x_pos2 = p5.map(shift_point2[0], x1, x2, 0, 256);
@@ -63,7 +91,8 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       var shift_point2 = getOffsetPoint(p5, x, y+grid_size, z, 0.1);
       var x_pos2 = p5.map(shift_point2[0], x1, x2, 0, 256);
       var y_pos2 = p5.map(shift_point2[1], y1, y2, 0, 256);
-      p5.line(x_pos, y_pos, x_pos2, y_pos2);
+      p5.line(x_pos, y_pos, x_pos2, y_pos2);*/
+
 
       p5.noStroke();
       p5.ellipse(x_pos, y_pos, cur_ball_radius);
