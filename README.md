09a4216187d40b0d4bd73152155a1ee20676d7fe
wynnhera <wynnheradesign@gmail.com>
2017-10-16 04:42:55 +1300

    starting to resemble coral landscape

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 ## Powers of 10 PRINT
 
-A daydream of what might be hidden within the C64 10print maze
-using p5.js and leaflet.
-
-You can also use the Open link at right to open this app in a full screen
-window which additionally supports stateful URLs so
-you can bookmark favorite found locations.
+finally made some noticable progress as it is starting to resemble my coral landscape. 
--- example_fence.js
+++ example_fence.js
@@ -1,14 +1,16 @@
 var max_thickness = 64;
-var max_movement = 32;
-var ball_radius = 32;
-var line_width = 8;
-var grid_size = 64;
+var max_movement = 20;
+var ball_radius = 20;
+var line_width = 5;
+var grid_size = 44;
+var inMapMode = true;
+
 
 function getOffsetPoint(p5, x, y, z, noiseScale) {
   var noiseX = p5.noise(x * noiseScale,
-                        y * noiseScale, z);
+                        y * noiseScale, z+20);
   var noiseY = p5.noise(x * noiseScale,
-                        y * noiseScale, z+50);
+                        y * noiseScale, z);
   var offsetX = p5.map(noiseX, 0, 1, -max_movement, max_movement);
   var offsetY = p5.map(noiseY, 0, 1, -max_movement, max_movement);
   return [x+offsetX, y+offsetY]
@@ -17,6 +19,23 @@ function getOffsetPoint(p5, x, y, z, noiseScale) {
 function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
 }
+/*
+  var x_steps = 1 + Math.floor(width / 10); 
+  var y_steps = 1 + Math.floor(height / 10);
+
+  // save grid locations
+  var grid_locations = new Array(x_steps);
+    for(var i=0;i<x_steps;i++) {
+    grid_locations[i] = new Array(y_steps);
+    for (var j = 0; j < y_steps; j++) {
+      x_pos = i * 20;
+      y_pos = j * 18;
+      if((j % 2) == 0){
+        x_pos = x_pos + 10;
+      }
+      grid_locations[i][j] = [x_pos, y_pos];
+    }
+  }*/
 
 /*
  * This is the funciton to implement to make your own abstract design.
@@ -29,7 +48,39 @@ function snap_to_grid(num, gsize) {
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
+
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+  /*if(inMapMode){
+  for(var i=0;i<x_steps;i++) {
+    grid_locations[i] = new Array(y_steps);
+    for (var j = 0; j < y_steps; j++) {
+      x_pos = i * 20;
+      y_pos = j * 18;
+      if((j % 2) == 0){
+        x_pos = x_pos + 5;
+      }
+      grid_locations[i][j] = [x_pos, y_pos];
+    }
+  }
+    // draw the landscape
+    for(var i=0;i<x_steps-1;i++) {
+      for(var j=0;j<y_steps-1;j++) {
+      
+        var loc = grid_locations[i][j];
+        var x1 = loc[0];
+        var y1 = loc[1]
+
+        var x_noise = x1/200.0;
+        var y_noise = y1/500.0;
+        var noiseVal = p5.noise(x_noise, y_noise);
+        var shade = colorFromValue(noiseVal);
+        p5.fill(shade);
+     
+        p5.ellipse(x1, y1, 20);
+        
+      }
+    }
+  }*/
   // debug - show border
   var max_shift = max_thickness + max_movement;
   var min_x = snap_to_grid(x1 - max_shift, grid_size);
@@ -44,29 +95,89 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   var cur_line_width = c_plwidth - c_p00;
   var cur_ball_radius = c_pball - c_p00;
 
+  var push_amount = 0.005;
+
+  function colorFromValue(v) {
+  if (v < 0.5) {
+    color1 = p5.color(240,139,175); //coral 
+    color2 = p5.color(6,255,255); //turquoise
+    c = p5.lerpColor(color1, color2, v*2); 
+    return c;
+  }
+  else if(v<0.7) {
+    return p5.color(6,196,249); //blue
+  }
+  else if(v<0.8) {
+   color3 = p5.color(4,248,215); //tealish
+    color4 = p5.color(240,139,175); //coral
+    c1 = p5.lerpColor(color3,color4);
+    return c1;
+  }
+  else {
+     return p5.color(235,93,244); //pink
+  }
+}
+
+// if 3+ deep
+if(zoom >=3){
+  p5.background(0);
+}else{
   p5.background(255);
-  p5.fill(0, 0, 128);
+}
+
+  var x_noise = x1/200.0;
+  var y_noise = y1/500.0;
+  var noiseVal = p5.noise(x_noise, y_noise);
+  var shade = colorFromValue(noiseVal);
+  p5.fill(235,93,244);
+
+
   for(var x=min_x; x<max_x; x+=grid_size) {
     for(var y=min_y; y<max_y; y+=grid_size) {
       var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+
       var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
       var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
 
       p5.strokeWeight(cur_line_width);
-      p5.stroke(0, 0, 128);
-      var shift_point2 = getOffsetPoint(p5, x+grid_size, y, z, 0.1);
+      p5.stroke(shade);
+      var shift_point2 = getOffsetPoint(p5, x+grid_size, y, z, push_amount); //amount of randomness
       var x_pos2 = p5.map(shift_point2[0], x1, x2, 0, 256);
       var y_pos2 = p5.map(shift_point2[1], y1, y2, 0, 256);
-      p5.line(x_pos, y_pos, x_pos2, y_pos2);
+     // p5.line(x_pos, y_pos, x_pos2, y_pos2);
 
-      p5.stroke(0, 128, 0);
-      var shift_point2 = getOffsetPoint(p5, x, y+grid_size, z, 0.1);
+      p5.stroke(shade);
+      var shift_point2 = getOffsetPoint(p5, x, y+grid_size, z, push_amount);
       var x_pos2 = p5.map(shift_point2[0], x1, x2, 0, 256);
       var y_pos2 = p5.map(shift_point2[1], y1, y2, 0, 256);
-      p5.line(x_pos, y_pos, x_pos2, y_pos2);
 
-      p5.noStroke();
+      p5.line(x_pos, y_pos, x_pos2, y_pos2);
       p5.ellipse(x_pos, y_pos, cur_ball_radius);
+      p5.ellipse(x_pos, y_pos-100, cur_ball_radius);
+      p5.noStroke();
+
+if(zoom <=2){
+        var shift_point = getOffsetPoint(p5,x,y,z*1,0.01*push_amount);
+        var x_pos2 = p5.map(shift_point2[0], x1, x2, 0, 256);
+        var y_pos2 = p5.map(shift_point2[1], y1, y2, 0, 256);
+
+        p5.fill(240,139,175);
+        p5.ellipse(x_pos, y_pos, 0.1*cur_ball_radius);
+
+      }
+      
+
+
+      
     }
+  }
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == ' ') {
+    inMapMode = !inMapMode;
   }
 }
