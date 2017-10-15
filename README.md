4603f0f6fbd0927a4cfa3b88e935c0cf701e5f86
Shakira Jensen <shakirajensencreative@gmail.com>
2017-10-16 10:31:40 +1300

    first layer section finished - clouds

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-PART 2 FINAL
+PART 3
 
-This is my completed part 2 for problem set 4. Wrapping my head around the code has been a real struggle, so I'm going for a simple grid map aesthetic. This is different from what I originally planned, but I think the final output will be more effective. 
+I have made a more dynamic cloud layer which revels more and more sections of the underlying land as you zoom. At this point I think this first section of graphical zoom is complete and I will start adding features within the land map layers. 
 
-I've based this off the 10 print example code and I think this is the most appropriate due to the structure of a mapped landscape. Currently there are 3 zoomable zones - Sky/clouds, map view, and land view. My goal is to add new features for every point of zoom. 
+I stuck with a simplistic grided system kept within a nested loop to create my cloud layers. It runs fairly slow at the moment and I'm unsure how to fix this at this stage. 
--- example_10print.js
+++ example_10print.js
@@ -1,9 +1,14 @@
 var white = "#ffffff"
 var blue = "#a4c6fc"
+var skyBlue = "#90bbe5"
 var green = "#50ad4c"
-var grey = "#bcc6d6"
+var darkSkyGrey = "#bcc6d6"
+var lightSkyGrey = "rgba(204, 212, 226, 0.8)"
+var whiteSky = "rgba(232, 236, 242, 0.5)"
 var noiseScale = 1/16.0;
 var noiseScale2 = 1/26.0;
+
+var varSkyCol = darkSkyGrey;
 
 
 function drawCloudLayer(p5, slashsize, x1, x2, y1, y2, z) {
@@ -17,8 +22,8 @@ function drawCloudLayer(p5, slashsize, x1, x2, y1, y2, z) {
 
   var char_width = 256 / ((x2-x1)/slashsize);
   var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
+  var pixel_width = char_width / 7;
+  var pixel_height = char_height / 7;
   p5.strokeWeight(pixel_width);
   p5.rectMode(p5.CORNERS);
 
@@ -29,12 +34,17 @@ function drawCloudLayer(p5, slashsize, x1, x2, y1, y2, z) {
       var n_y = y / slashsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
-     	if (noiseValue < 0.7) {
+     	if (noiseValue < 0.6) {
 	        p5.noStroke();
-	        p5.fill(grey);
+	        p5.fill(varSkyCol);
 	        p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
-		}
-	}
+		  }
+      if (noiseValue < 0.3 & noiseValue > 0.1 & z == 0) {
+          p5.noStroke();
+          p5.fill(varSkyCol);
+          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+      }
+	   }
   }
 
 }
@@ -62,7 +72,7 @@ function drawMapLayer(p5, slashsize, x1, x2, y1, y2, z) {
       var n_y = y / slashsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
-      if (noiseValue < 0.6) {
+      if (noiseValue < 0.5) {
           p5.noStroke();
           p5.fill(blue);
           p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
@@ -80,9 +90,33 @@ function drawLandLayer(p5, slashsize, x1, x2, y1, y2, z) {
 
 
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  p5.noiseDetail(8,0.5);
-  p5.background(white);
-  drawCloudLayer(p5, 6, x1, x2, y1, y2, z);
+
+  if(zoom >= 0) {
+    p5.noiseDetail(8,0.5);
+    p5.background(lightSkyGrey);
+    varSkyCol = darkSkyGrey;
+    drawCloudLayer(p5, 6, x1, x2, y1, y2, z);
+    varSkyCol = whiteSky;
+    drawCloudLayer(p5, 16, x1, x2, y1, y2, z);
+  }
+
+  if(zoom >= 1) {
+    p5.background(green);
+    p5.noiseDetail(8,0.5);
+    varSkyCol = lightSkyGrey;
+    drawMapLayer(p5, 2, x1, x2, y1, y2, z);
+    drawCloudLayer(p5, 13, x1, x2, y1, y2, z);
+
+  }
+
+  if(zoom >= 2) {
+    p5.noiseDetail(8,0.5);
+    p5.background(green);
+    varSkyCol = whiteSky;
+    drawMapLayer(p5, 2, x1, x2, y1, y2, z);
+    drawCloudLayer(p5, 13, x1, x2, y1, y2, z);
+
+  }
   
   if(zoom >= 3) {
     p5.background(green);
