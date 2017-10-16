a0aeadbe2e9bb0fc420242d3b241603128f5ac7d
Guest User <Guest@c-04-06-lab.mddn.vuw.ac.nz>
2017-10-16 19:47:18 +1300

    updated codes

--- drawgrid.js
+++ drawgrid.js
@@ -9,9 +9,112 @@
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
-function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+ var BGC = "#D963C9";
+ var LC = "#E8A65F";
+
+var max_zoom = 11;
+
+ var max_thickness = 64;
+ var max_movement = 32;
+ var ball_radius = 32;
+ var line_width = 8;
+
+ function snap_to_grid(num, gsize) {
+ 	return (num - (num % gsize));
+ }
+
+ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // debug - show border
-  p5.noFill();
-  p5.stroke(255, 0, 0)
+  // p5.rect (0,)
+  // p5.scale (zoom/10+1);
+  p5.fill(BGC);
+  p5.stroke (BGC);
   p5.rect(0, 0, 255, 255);
-}
+
+  p5.strokeWeight(1.5);
+
+  var max_shift = 250;
+  var grid_size = 50;
+  var min_x = snap_to_grid(x1 - max_shift, grid_size);
+  var max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  var min_y = snap_to_grid(y1 - max_shift, grid_size);
+  var max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
+
+
+  p5.noFill();
+
+  var noiseScale = 1;
+  p5.noiseDetail (8,0.5);
+  			for(var i=min_x; i<max_x; i=i+grid_size) {
+  				var n_x = p5.map(i, 0, 16, x1, x2);
+  				for(var j=min_y; j<max_y; j=j+grid_size) {
+  					var lx1 = p5.map (16, x1, x2, 0, 256);
+  					var lx2 = p5.map (i, x1, x2, 0, 256);
+  					var lx3 = p5.map (i+255, x1, x2, 0, 256);
+
+  					var sx3 = p5.map (i+82, x1, x2, 0, 256);
+
+  					var sy3 = p5.map (j+160, x1, x2, 0, 256);
+
+  					var ly1 = p5.map (j+255, y1, y2, 0, 256);
+  					var ly2 = p5.map (j, y1, y2, 0, 256);
+  					var n_y = p5.map(j, 0, 16, y1, y2);
+  					var noiseVal = p5.noise(n_x * noiseScale, n_y * noiseScale, z);
+
+  			var c_p00 = p5.map(0, x1, x2, 0, 256);
+  			var c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+  			var c_pball = p5.map(ball_radius, x1, x2, 0, 256);
+  			var cur_line_width = c_plwidth - c_p00;
+  			var cur_ball_radius = c_pball - c_p00;
+  			var x_pos = p5.map(i, x1, x2, 0, 256);
+  			var y_pos = p5.map(j, y1, y2, 0, 256);
+
+  			var x_pos2 = p5.map(i+255, x1, x2, 0, 256);
+  			p5.strokeWeight (noiseVal*(zoom*2+6));
+  			if (zoom == 3){
+  					p5.strokeWeight (noiseVal*(zoom+6));
+  					p5.stroke (158, 70, 90, 50);
+  					p5.fill (158, 70, 90, 50);
+  				}
+  			
+
+  		}
+  	} 
+  			if (zoom >=3){
+  				p5.stroke (158, 70, 90, 175);
+
+  				p5.fill (158, 70, 90, 175);
+
+
+
+  				
+  				
+  			}
+  	 				
+  	if (zoom <= 3){
+  		p5.stroke(LC);
+  		p5.line (lx2, ly2, lx3, ly1);
+  		p5.line (lx2, ly1, lx3, ly2);
+  	}
+  	  				if (zoom >= 3 && zoom <= 7){
+
+  					p5.line(x_pos, y_pos+10, x_pos2, y_pos);
+  				}
+  	if (zoom >=3 && zoom<= 6){
+  		p5.strokeWeight(noiseVal*(zoom*2+6));
+  		p5.ellipse (x_pos+10, y_pos+10, cur_ball_radius);
+  	}
+  	if (zoom>=7){
+  		p5.push();
+  		p5.scale (zoom/7);
+  		for (p=0; p<=p5.width; p=p+15){
+  			for (q=0; q<=p5.height; q=q+15){
+  				p5.stroke (60, 232, 230, zoom*5+100);
+  				p5.fill (79, 211, 255, zoom*5+100);
+  							p5.strokeWeight(noiseVal*(zoom+2));
+  			  				p5.triangle (p, q, p+14, q, p+7.5, q+14);
+  			  			}
+  		}
+  		p5.pop();
+  	}
+  }
--- example_framed_dot.js
+++ example_framed_dot.js
@@ -14,36 +14,121 @@
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   console.log(y1, y2);
-  p5.background(255);
+  p5.background("#704A33");
   p5.rectMode(p5.CORNERS);
 
-  // The first red rectangle fills the entire space
-  var cx = p5.map(512-960/2, x1, x2, 0, 256);
-  var cy = p5.map(512-720/2, y1, y2, 0, 256);
-  var cx2 = p5.map(512+960/2, x1, x2, 0, 256);
-  var cy2 = p5.map(512+720/2, y1, y2, 0, 256);
-  p5.fill(255, 0, 0);
-  p5.rect(cx, cy, cx2, cy2);
 
-  // The second black rectangle is inset to form a frame inset by 20 units
-  cx = p5.map(512-940/2, x1, x2, 0, 256);
-  cy = p5.map(512-700/2, y1, y2, 0, 256);
-  cx2 = p5.map(512+940/2, x1, x2, 0, 256);
-  cy2 = p5.map(512+700/2, y1, y2, 0, 256);
-  p5.fill(0);
-  p5.rect(cx, cy, cx2, cy2);
-
-  // Two ellipses with a radius of 50 units are then added.
-  var cx = p5.map(512, x1, x2, 0, 256);
-  var cy = p5.map(512, y1, y2, 0, 256);
-  var cx2 = p5.map(512+50, x1, x2, 0, 256);
-  p5.fill(0, 0, 255);
-  p5.ellipse(cx, cy, (cx2-cx));
-
-  // The second green ellipse is above and to the left of the first one.
-  var cx = p5.map(412, x1, x2, 0, 256);
+  var cx = p5.map(212, x1, x2, 0, 256);
   var cy = p5.map(412, y1, y2, 0, 256);
-  var cx2 = p5.map(412+50, x1, x2, 0, 256);
-  p5.fill(0, 255, 0);
-  p5.ellipse(cx, cy, (cx2-cx));
+
+  
+  p5.noFill();
+
+  ////// 
+
+  var sx = p5.map (412, x1, x2, 0, 556);
+  var sy = p5.map (392, y1, y2, 0, 556);
+  var xmod = p5.map (10, x1, x2, 0, 556);
+  var ymod1 = p5.map (98, y1, y2, 0, 556);
+  var ymod2 = p5.map (158, y1, y2, 0, 556);
+  var ymod3 = p5.map (251, y1, y2, 0, 556);
+  p5.beginShape();
+  p5.vertex (sx, sy);
+  p5.vertex (sx+xmod+xmod, sy);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod1);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod2);
+  p5.vertex (sx+xmod+xmod, sy+ymod3);
+  p5.vertex (sx, sy+ymod3);
+  p5.vertex (sx-xmod, sy+ymod2);
+  p5.vertex (sx-xmod, sy+ymod1);
+  p5.vertex (sx, sy);
+  p5.endShape();
+
+
+  /////
+p5.strokeWeight(zoom/10+1);
+p5.triangle(cx, cy, cx+ymod2, cy+ymod2, cx+xmod, cy);
+
+  var sx = p5.map (1550, x1, x2, 0, 556);
+  var sy = p5.map (1292, y1, y2, 0, 556);
+  var xmod = p5.map (100, x1, x2, 0, 556);
+  var ymod1 = p5.map (98, y1, y2, 0, 556);
+  var ymod2 = p5.map (158, y1, y2, 0, 556);
+  var ymod3 = p5.map (251, y1, y2, 0, 556);
+  p5.beginShape();
+  p5.vertex (sx, sy);
+  p5.vertex (sx+xmod+xmod, sy);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod1);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod2);
+  p5.vertex (sx+xmod+xmod, sy+ymod3);
+  p5.vertex (sx, sy+ymod3);
+  p5.vertex (sx-xmod, sy+ymod2);
+  p5.vertex (sx-xmod, sy+ymod1);
+  p5.vertex (sx, sy);
+  p5.endShape();
+
+  var sx = p5.map (0, x1, x2, 0, 556);
+  var sy = p5.map (22, y1, y2, 0, 556);
+  var xmod = p5.map (10, x1, x2, 0, 556);
+  var ymod1 = p5.map (98, y1, y2, 0, 556);
+  var ymod2 = p5.map (158, y1, y2, 0, 556);
+  var ymod3 = p5.map (251, y1, y2, 0, 556);
+  p5.beginShape();
+  p5.vertex (sx, sy);
+  p5.vertex (sx+xmod+xmod, sy);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod1);
+  p5.vertex (sx+xmod+xmod+xmod, sy+ymod2);
+  p5.vertex (sx+xmod+xmod, sy+ymod3);
+  p5.vertex (sx, sy+ymod3);
+  p5.vertex (sx-xmod, sy+ymod2);
+  p5.vertex (sx-xmod, sy+ymod1);
+  p5.vertex (sx, sy);
+  p5.endShape();
+ p5.beginShape();
+ p5.vertex (107, 2);
+ p5.vertex (147, 2);
+ p5.vertex (167, 100);
+ p5.vertex (167, 160);
+ p5.vertex (147, 253);
+ p5.vertex (107, 253);
+ p5.vertex (87, 160);
+ p5.vertex (87, 100);
+ p5.vertex (107, 2);
+ p5.endShape();
+
+p5.triangle(152, 2, 187, 2, 169.5, 95);
+p5.triangle(152, 253, 187, 253, 169.5, 165);
+
+ p5.beginShape();
+ p5.vertex (192, 2);
+ p5.vertex (232, 2);
+ p5.vertex (252, 100);
+ p5.vertex (252, 160);
+ p5.vertex (232, 253);
+ p5.vertex (192, 253);
+ p5.vertex (172, 160);
+ p5.vertex (172, 100);
+ p5.vertex (192, 2);
+ p5.endShape();
+
+p5.triangle(237, 2, 272, 2, 255, 95);
+p5.triangle(-28, 2, 17, 2, 0, 95);
+
+p5.triangle(-28, 253, 17, 253, 0, 165);
+p5.triangle(237, 253, 272, 253, 255, 165);
+
+  p5.beginShape();
+  p5.vertex (22, 2);
+  p5.vertex (62, 2);
+  p5.vertex (82, 100);
+  p5.vertex (82, 160);
+  p5.vertex (62, 253);
+  p5.vertex (22, 253);
+  p5.vertex (2, 160);
+  p5.vertex (2, 100);
+  p5.vertex (22, 2);
+  p5.endShape();
+
+p5.triangle(67, 2, 102, 2, 84.5, 95);
+p5.triangle(67, 253, 102, 253, 84.5, 165);
 }
