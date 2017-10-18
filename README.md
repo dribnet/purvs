fd7476d95eeba89d3b9f64498620aef1345fe3ff
ShakDesign <shakirajensen@hotmail.com>
2017-10-18 20:37:56 +1300

    final code upload from this version of problem set 4

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-PART 3
+PART 4 - FINAL
 
-This version shows the completed levels of zoom over 10 layers. Some of the colours are still in debugging mode which is something I will change in the coming versions. I've chosen a very similar methid for generation of each layer, however I think they vary enough graphically to be considered seperate. I also added the max zoom level and tour function in but am yet to focus it toward my code.
+My final map code can be found in shakira_map.js
+
+When beginning this problem set I was going to carry on from the last project with my abstract pattern as a starting point. As I started working with the starter code I decided I wanted to change my idea to a 2D map which is reminiscent of the one I created from problem set 3. 
+
+My three graphical sections consist of a cloud layer, a terrain/map layer, and a plant detail layer. While they’re all abstract I think they communicate the map idea well and the viewer can grasp the overall concept. All layers are generated in the same way and all are based off Tom's 10 print starter code. 
+
+The tour functions shows various points that I think communicate the overall idea the best, and show each of the graphical sections in their own light. 
+
+In further iterations I want to work on creating an ocean section so when the user zooms into the ocean they get a different detail view to the land. I think this will show more variation in the design and really grasp the viewer into exploring for more. While I haven't included this in this version I still think the variation is great enough. 
--- shakira_map.js
+++ shakira_map.js
@@ -1,4 +1,5 @@
-//Solution based on Toms 10 print example code
+/* Solution based on Toms 10 print example code. Zoomable landscape starts from the sky and flows into 
+high level detail plants. */
 
 var white = "#ffffff";
 var blue = "#a4c6fc";
@@ -8,7 +9,7 @@ var lightGreen = "#6bc667";
 var lightGreenAlph = "rgba(139, 206, 136, 0.1)";
 var backgroundGreen = "#83ce80";
 var landBaseGreen = "#71C66F";
-var landBaseBrown = "#bab15e";
+var landBaseBrown = "#508E62";
 var landBaseBrown2 = "#a58f61";
 var darkSkyGrey = "#bcc6d6";
 var lightSkyGrey = "rgba(204, 212, 226, 0.8)";
@@ -19,235 +20,178 @@ var varSkyCol = darkSkyGrey;
 var noiseScale = 1/16.0;
 var noiseScale2 = 16;
 
-/* OPTIONAL VARIABLES */
 /* what is the initial zoom level (defaults to 0) */
 var initialZoomLevel = 1;
-/* what is the maximum zoom level (make this at least 10. defaults to 16) */
+/* what is the maximum zoom level (defaults to 16) */
 var maxZoomLevel = 10;
 
-
-/* TOUR VARIABLES (required)
 /* the random number seed for the tour */
 var tourSeed = 100;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-  [5, 510, 507],
-  [7, 507, 506],
-  [3, 512, 515],
-  [1, 512, 512]
+  [1, 313.250000000000, 614.000000000000],
+  [3, 384.187500000000, 624.750000000000],
+  [5, 349.765625000000, 538.125000000000],
+  [8, 338.150390625000, 501.429687500000],
+  [10, 335.360839843750, 504.305664062500]
 ]
 
-function drawCloudLayer(p5, slashsize, x1, x2, y1, y2, z) {
+// draws the top level cloud layer
+function drawCloudLayer(p5, gridsize, x1, x2, y1, y2, z) {
+  var startx = gridsize * (Math.floor(x1/gridsize) - 1);
+  var starty = gridsize * (Math.floor(y1/gridsize) - 1);
+  var endx = gridsize * (Math.floor(x2/gridsize) + 1);
+  var endy = gridsize * (Math.floor(y2/gridsize) + 1);
 
-  // figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
+  var grid_width = 256 / ((x2-x1)/gridsize);
+  var grid_height = 256 / ((y2-y1)/gridsize);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 7;
-  var pixel_height = char_height / 7;
-  p5.strokeWeight(pixel_width);
   p5.rectMode(p5.CORNERS);
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  for(var x=startx; x<endx; x+=gridsize) {
+    var n_x = x / gridsize;
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+      for(var y=starty; y<endy; y+=gridsize) {
+      var n_y = y / gridsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
-     	if (noiseValue < 0.6) {
-	        p5.noStroke();
-	        p5.fill(varSkyCol);
-	        p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
-		  }
-
-      if (noiseValue < 0.3 & noiseValue > 0.1) {
-          p5.noStroke();
-          p5.fill(varSkyCol);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
-      }
-
+       	if (noiseValue < 0.6) {
+  	        p5.noStroke();
+  	        p5.fill(varSkyCol);
+  	        p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
+  		  }
+        if (noiseValue < 0.3 & noiseValue > 0.1) {
+            p5.noStroke();
+            p5.fill(varSkyCol);
+            p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
+        }
 	   }
   }
-
 }
 
-function drawMapLayer(p5, slashsize, x1, x2, y1, y2, z) {
+// draws the terrain map layer with varying zoom details
+function drawMapLayer(p5, gridsize, x1, x2, y1, y2, z) {
+  var startx = gridsize * (Math.floor(x1/gridsize) - 1);
+  var starty = gridsize * (Math.floor(y1/gridsize) - 1);
+  var endx = gridsize * (Math.floor(x2/gridsize) + 1);
+  var endy = gridsize * (Math.floor(y2/gridsize) + 1);
 
-  // figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
+  var grid_width = 256 / ((x2-x1)/gridsize);
+  var grid_height = 256 / ((y2-y1)/gridsize);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
-  p5.strokeWeight(pixel_width);
   p5.rectMode(p5.CORNERS);
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  for(var x=startx; x<endx; x+=gridsize) {
+    var n_x = x / gridsize;
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+    for(var y=starty; y<endy; y+=gridsize) {
+      var n_y = y / gridsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
       if (noiseValue < 0.5) {
           p5.noStroke();
           p5.fill(backgroundGreen);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+          p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
       }
-  }
+    }
   }
 
 }
 
-function drawLandLayer(p5, slashsize, x1, x2, y1, y2, z) {
-    // figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
+// draws the terrain map layer with varying zoom details
+function drawLandLayer(p5, gridsize, x1, x2, y1, y2, z) {
+  var startx = gridsize * (Math.floor(x1/gridsize) - 1);
+  var starty = gridsize * (Math.floor(y1/gridsize) - 1);
+  var endx = gridsize * (Math.floor(x2/gridsize) + 1);
+  var endy = gridsize * (Math.floor(y2/gridsize) + 1);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 7;
-  var pixel_height = char_height / 7;
-  p5.strokeWeight(pixel_width);
+  var grid_width = 256 / ((x2-x1)/gridsize);
+  var grid_height = 256 / ((y2-y1)/gridsize);
+
   p5.rectMode(p5.CORNERS);
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  for(var x=startx; x<endx; x+=gridsize) {
+    var n_x = x / gridsize;
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+    for(var y=starty; y<endy; y+=gridsize) {
+      var n_y = y / gridsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
       if (noiseValue < 0.6) {
           p5.noStroke();
           p5.fill(lightGreen);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+          p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
       }
       if (noiseValue < 0.7) {
           p5.noStroke();
           p5.fill(lightGreenAlph);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+          p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
       }
       if (noiseValue < 0.2) {
           p5.noStroke();
           p5.fill(alphaOverlay);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+          p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
       }
-     }
+    }
   }
 }
 
-function drawTreeLayer(p5, slashsize, x1, x2, y1, y2, z) {
-  // figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
+// draws basic tree layer - circles only
+function drawTreeLayer(p5, gridsize, x1, x2, y1, y2, z) {
+  var startx = gridsize * (Math.floor(x1/gridsize) - 1);
+  var starty = gridsize * (Math.floor(y1/gridsize) - 1);
+  var endx = gridsize * (Math.floor(x2/gridsize) + 1);
+  var endy = gridsize * (Math.floor(y2/gridsize) + 1);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
-  p5.strokeWeight(pixel_width);
+  var grid_width = 256 / ((x2-x1)/gridsize);
+  var grid_height = 256 / ((y2-y1)/gridsize);
+
   p5.ellipseMode(p5.CORNERS);
   
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  for(var x=startx; x<endx; x+=gridsize) {
+    var n_x = x / gridsize;
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+    for(var y=starty; y<endy; y+=gridsize) {
+      var n_y = y / gridsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseScaleCol = 2;
       var noiseValueCol = p5.noise(x * noiseScaleCol, y * noiseScaleCol, z);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
       var offset = p5.random(10, 20);
       if (noiseValue < 0.5) {
-          p5.fill(0, noiseValueCol*255, 0);
-          p5.ellipse(x_pos+offset, y_pos+offset, x_pos+char_width, y_pos+char_height);
+          p5.fill(80, noiseValueCol*242, 98);
+          p5.ellipse(x_pos+offset, y_pos+offset, x_pos+grid_width, y_pos+grid_height);
       }
     }
   }
 }
 
-function drawGrassLayer(p5, slashsize, x1, x2, y1, y2, z) {
-  // figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
+// draws high detail grass 
+function drawZoomGrass(p5, gridsize, x1, x2, y1, y2, z) {
+  var startx = gridsize * (Math.floor(x1/gridsize) - 1);
+  var starty = gridsize * (Math.floor(y1/gridsize) - 1);
+  var endx = gridsize * (Math.floor(x2/gridsize) + 1);
+  var endy = gridsize * (Math.floor(y2/gridsize) + 1);
 
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
-  p5.strokeWeight(pixel_width);
-  p5.ellipseMode(p5.CORNERS);
-  
+  var grid_width = 256 / ((x2-x1)/gridsize);
+  var grid_height = 256 / ((y2-y1)/gridsize);
 
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
+  p5.rectMode(p5.CORNERS); 
+
+  for(var x=startx; x<endx; x+=gridsize) {
+    var n_x = x / gridsize;
     var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
+    for(var y=starty; y<endy; y+=gridsize) {
+      var n_y = y / gridsize;
       var y_pos = p5.map(y, y1, y2, 0, 256);
       var noiseScaleCol = 2;
       var noiseValueCol = p5.noise(x * noiseScaleCol, y * noiseScaleCol, z);
       var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
       var offset = p5.random(10, 20);
-      if (noiseValue < 0.4) {
-          p5.fill(0);
-          p5.ellipse(x_pos+offset, y_pos+offset, x_pos+char_width, y_pos+char_height);
-      }
-    }
-  }
-}
-
-function drawZoomGrass(p5, slashsize, x1, x2, y1, y2, z) {
-// figure out top left and top right square then - 1 off grid
-  var startx = slashsize * (Math.floor(x1/slashsize) - 1);
-  var starty = slashsize * (Math.floor(y1/slashsize) - 1);
-  // figure out where the slash should be
-  var endx = slashsize * (Math.floor(x2/slashsize) + 1);
-  var endy = slashsize * (Math.floor(y2/slashsize) + 1);
-
-  var char_width = 256 / ((x2-x1)/slashsize);
-  var char_height = 256 / ((y2-y1)/slashsize);
-  var pixel_width = char_width / 8;
-  var pixel_height = char_height / 8;
-  p5.strokeWeight(pixel_width);
-  p5.rectMode(p5.CORNERS);
-  
-
-  for(var x=startx; x<endx; x+=slashsize) {
-    var n_x = x / slashsize;
-    var x_pos = p5.map(x, x1, x2, 0, 256);
-    for(var y=starty; y<endy; y+=slashsize) {
-      var n_y = y / slashsize;
-      var y_pos = p5.map(y, y1, y2, 0, 256);
-      var noiseScaleCol = 2;
-      var noiseValueCol = p5.noise(x * noiseScaleCol, y * noiseScaleCol, z);
-      var noiseValue = p5.noise(x * noiseScale, y * noiseScale, z);
-      var offset = p5.random(10, 20);
-      if (noiseValue < 0.4) {
-          p5.fill(noiseValueCol*255, 0, 0);
-          p5.rect(x_pos, y_pos, x_pos+char_width, y_pos+char_height);
+      if (noiseValue < 0.5) {
+          p5.fill(noiseValueCol*165, 143, 97);
+          p5.rect(x_pos, y_pos, x_pos+grid_width, y_pos+grid_height);
       }
     }
   }
@@ -255,18 +199,15 @@ function drawZoomGrass(p5, slashsize, x1, x2, y1, y2, z) {
 
 
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-
-  // view of clouds only
+  // light view of clouds only
   if(zoom >= 0) {
-    p5.noiseDetail(8,0.5);
     p5.background(lightSkyGrey);
     varSkyCol = darkSkyGrey;
     varSkyCol = whiteSky;
     drawCloudLayer(p5, 16, x1, x2, y1, y2, z);
   }
-
+  // detailed view of clouds only
   if(zoom >= 1) {
-    p5.noiseDetail(8,0.5);
     p5.background(lightSkyGrey);
     varSkyCol = darkSkyGrey;
     drawCloudLayer(p5, 6, x1, x2, y1, y2, z);
@@ -277,7 +218,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // view of clouds with bits of land showing
   if(zoom >= 2) {
     p5.background(lightGreen);
-    p5.noiseDetail(8,0.5);
     varSkyCol = lightSkyGrey;
     drawMapLayer(p5, 2, x1, x2, y1, y2, z);
     drawCloudLayer(p5, 13, x1, x2, y1, y2, z);
@@ -291,12 +231,11 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
     varSkyCol = whiteSky;
     drawMapLayer(p5, 2, x1, x2, y1, y2, z);
     drawCloudLayer(p5, 13, x1, x2, y1, y2, z);
-}
+  }
   
   // zoomed out view of land
   if(zoom >= 4) {
     p5.background(lightGreen);
-    p5.noiseDetail(8,0.5);
     drawMapLayer(p5, 2, x1, x2, y1, y2, z);
 
   }
@@ -304,7 +243,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // detail view of land terrain
   if(zoom >= 5) {
     p5.background(green);
-    p5.noiseDetail(8,0.5);
     drawLandLayer(p5, 2, x1, x2, y1, y2, z);
     drawMapLayer(p5, 2, x1, x2, y1, y2, z);
 
@@ -313,7 +251,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // detail view of land terrain +1
   if(zoom >= 6) {
     p5.background(green);
-    p5.noiseDetail(8,0.5);
     drawLandLayer(p5, 2, x1, x2, y1, y2, z);
     drawMapLayer(p5, 2, x1, x2, y1, y2, z);
 
@@ -322,57 +259,33 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // introduce trees
   if(zoom >= 7) {
     p5.background(landBaseGreen);
-    p5.noiseDetail(8,0.5);
     drawLandLayer(p5, 2, x1, x2, y1, y2, z);
-    drawTreeLayer(p5, 0.5, x1, x2, y1, y2, z);
+    drawTreeLayer(p5, 0.1, x1, x2, y1, y2, z);
 
   }
 
 
-  // tree zoom with second tree layer on top **
+  // tree zoom 
   if(zoom >= 8) {
-    p5.background(landBaseBrown);
-    p5.noiseDetail(8,0.5);
-    drawTreeLayer(p5, 0.1, x1, x2, y1, y2, z);
-    drawTreeLayer(p5, 0.5, x1, x2, y1, y2, z);
+    p5.background(landBaseGreen);
+    drawLandLayer(p5, 2, x1, x2, y1, y2, z);
+    drawTreeLayer(p5, 0.2, x1, x2, y1, y2, z);
 
   }
 
-
+  // tree zoom detail
   if(zoom >= 9) {
-    p5.background(landBaseBrown2);
-    p5.noiseDetail(8,0.5);
-    drawTreeLayer(p5, 0.1, x1, x2, y1, y2, z);
-    drawGrassLayer(p5, 0.1, x1, x2, y1, y2, z);
+    p5.background(landBaseBrown);
+    drawZoomGrass(p5, 0.01, x1, x2, y1, y2, z);
+    drawTreeLayer(p5, 0.25, x1, x2, y1, y2, z);
 
   }
 
-
+  // high level tree detail
   if(zoom >= 10) {
-    p5.background(landBaseBrown2);
-    p5.noiseDetail(8,0.5);
+    p5.background(landBaseBrown);
     drawZoomGrass(p5, 0.01, x1, x2, y1, y2, z);
     drawTreeLayer(p5, 0.01, x1, x2, y1, y2, z);
-    drawGrassLayer(p5, 0.01, x1, x2, y1, y2, z);
   }
   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
