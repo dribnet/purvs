22eab407d4afab70c7114d507024e609a6138e46
ChristiaanSteenkamp <cjsteenkamp22@gmail.com>
2017-10-19 12:05:35 +1300

    save

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-## Powers of 10 PRINT
+Fireworks.
 
-A daydream of what might be hidden within the C64 10print maze
-using p5.js and leaflet.
+The design I have worked with was with the concept of firworks exploding and being frozen in time. 
 
-You can also use the Open link at right to open this app in a full screen
-window which additionally supports stateful URLs so
-you can bookmark favorite found locations.
+Using the frame work geven to us I first by making ellipses appear on the grid at random locations using the noise function. From there I worked on giving them a diffrent color and shade to give them depth and go and some intrest. 
+
+On scrolling in I the noise on the fireworks start to clear up eventualy revealing the their core smaller particles with in the main boddy of the ellipse.
--- example_fence.js
+++ example_fence.js
@@ -1,25 +1,23 @@
 var max_thickness = 64;
 var max_movement = 32;
 var ball_radius = 32;
-var line_width = 8;
+var line_width = 10;
 var grid_size = 64;
-var ran_off_one_X;
-var ran_off_one_Y;
-var ran_off_two_X;
-var ran_off_two_Y;
-var ran_off_three_X;
-var ran_off_three_Y;
+var y_pos;
+var x_pos;
+var num;
+var ran_off_X; var ran_off_Y;
 
 /* the random number seed for the tour */
-var tourSeed = 301;
+var tourSeed = 0;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-  [1, 356.500000000000, 665.750000000000],
-  [3, 353.250000000000, 668.187500000000],
-  [4, 322.562500000000, 645.093750000000],
-  [5, 322.562500000000, 645.109375000000],
-  [7, 317.984375000000, 643.636718750000],
-  [3, 317.984375000000, 643.636718750000]
+  [0, 1249.500000000000, -1322.000000000000],
+  [2, 1249.875000000000, -1322.000000000000],
+  [4, 1222.218750000000, -1345.437500000000],
+  [6, 1224.257812500000, -1351.578125000000],
+  [6, 1209.695312500000, -1292.250000000000],
+  [2, 1196.625000000000, -1289.750000000000]
 ]
 
 function getOffsetPoint(p5, x, y, z, noiseScale) {
@@ -37,66 +35,93 @@ function snap_to_grid(num, gsize) {
 }
 
 
-function drawStamens(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2, drawLines) {
+function newColor(p5, x, y, n, z){
+      var noiseCol = p5.noise(x * n, y * n, z);
+
+      var rancol = p5.map(noiseCol, 0, 1, 0, 10);
+        if(rancol <= 4){
+          if (rancol <= 2 ){
+            p5.fill(164,64,1104,75);
+          } 
+          else if (rancol <= 3){
+            p5.fill(164,64,1104,50);
+          }
+          else{
+            p5.fill(164,64,1104,25);
+          }
+        } else {
+          if (rancol <= 5 ){
+            p5.fill(56,103,200,75);
+          } 
+          else if (rancol <= 6){
+            p5.fill(56,103,200,50);
+          }
+          else{
+            p5.fill(56,103,200,25);
+          }
+        }
+      
+}
+
+
+function detailOne(p5, x1, x2, y1, y2, z, pos_x, pos_y, rad1, rad2 ,zoom) {
   var offsets = [
-    [3 + ran_off_one_X,-1.5 + ran_off_one_Y],[3 + ran_off_one_X,-0.5 + ran_off_one_Y], [3 + ran_off_one_X,0.5 + ran_off_one_Y],[3 + ran_off_one_X,1.5 + ran_off_one_Y],
-    [2 + ran_off_one_X,2 + ran_off_one_Y],[2 + ran_off_one_X,1 + ran_off_one_Y], [2 + ran_off_one_X,0 + ran_off_one_Y], [2 + ran_off_one_X,-1 + ran_off_one_Y], [2 + ran_off_one_X,-2 + ran_off_one_Y],
-    [1 + ran_off_one_X,2.5 + ran_off_one_Y],[1 + ran_off_one_X,1.5 + ran_off_one_Y], [1 + ran_off_one_X,0.5 + ran_off_one_Y], [1 + ran_off_one_X,-0.5 + ran_off_one_Y], [1 + ran_off_one_X,-1.5 + ran_off_one_Y], [1 + ran_off_one_X,-2.5 + ran_off_one_Y],
-    [0 + ran_off_one_X,3 + ran_off_one_Y],[0 + ran_off_one_X,2 + ran_off_one_Y], [0 + ran_off_one_X,1 + ran_off_one_Y], [0 + ran_off_one_X,0 + ran_off_one_Y], [0 + ran_off_one_X,-1 + ran_off_one_Y], [0 + ran_off_one_X,-2 + ran_off_one_Y], [0 + ran_off_one_X,-3 + ran_off_one_Y],
-    [-1 + ran_off_one_X,2.5 + ran_off_one_Y],[-1 + ran_off_one_X,1.5 + ran_off_one_Y], [-1 + ran_off_one_X,0.5 + ran_off_one_Y], [-1 + ran_off_one_X,-0.5 + ran_off_one_Y], [-1 + ran_off_one_X,-1.5 + ran_off_one_Y], [-1 + ran_off_one_X,-2.5 + ran_off_one_Y],
-    [-2 + ran_off_one_X,2 + ran_off_one_Y],[-2 + ran_off_one_X,1 + ran_off_one_Y], [-2 + ran_off_one_X,0 + ran_off_one_Y], [-2 + ran_off_one_X,-1 + ran_off_one_Y], [-2 + ran_off_one_X,-2 + ran_off_one_Y],
-    [-3 + ran_off_one_X,-1.5 + ran_off_one_Y],[-3 + ran_off_one_X,-0.5 + ran_off_one_Y], [-3 + ran_off_one_X,0.5 + ran_off_one_Y],[-3 + ran_off_one_X,1.5 + ran_off_one_Y]
+    [3,-1.5],[3,-0.5], [3,0.5],[3,1.5],
+    [2,2],[2,1], [2,0], [2,-1], [2,-2],
+    [1,2.5],[1,1.5], [1,0.5], [1,-0.5], [1,-1.5], [1,-2.5],
+    [0,3],[0,2], [0,1], [0,0], [0,-1], [0,-2], [0,-3],
+    [-1,2.5],[-1,1.5], [-1,0.5], [-1,-0.5], [-1,-1.5], [-1,-2.5],
+    [-2,2],[-2,1], [-2,0], [-2,-1], [-2,-2],
+    [-3,-1.5],[-3,-0.5], [-3,0.5],[-3,1.5]
   ]
   var offsets2 = [
-    [2.55 + ran_off_two_X,-2 + ran_off_two_Y],[2.55 + ran_off_two_X,-1 + ran_off_two_Y],[2.55 + ran_off_two_X,0 + ran_off_two_Y],[2.55 + ran_off_two_X,1 + ran_off_two_Y],[2.55 + ran_off_two_X,2 + ran_off_two_Y],
-    [1.55 + ran_off_two_X,-2.5 + ran_off_two_Y],[1.55 + ran_off_two_X,1.5 + ran_off_two_Y], [1.55 + ran_off_two_X,0.5 + ran_off_two_Y],[1.55 + ran_off_two_X,-0.5 + ran_off_two_Y], [1.55 + ran_off_two_X,-1.5 + ran_off_two_Y], [1.55 + ran_off_two_X,2.5 + ran_off_two_Y],
-    [0.55 + ran_off_two_X,-3 + ran_off_two_Y],[0.55 + ran_off_two_X,2 + ran_off_two_Y], [0.55 + ran_off_two_X,1 + ran_off_two_Y], [0.55 + ran_off_two_X,0 + ran_off_two_Y], [0.55 + ran_off_two_X,-1 + ran_off_two_Y], [0.55 + ran_off_two_X,-2 + ran_off_two_Y],[0.55 + ran_off_two_X,3 + ran_off_two_Y],
-    [-0.55 + ran_off_two_X,-3 + ran_off_two_Y],[-0.55 + ran_off_two_X,2 + ran_off_two_Y], [-0.55 + ran_off_two_X,1 + ran_off_two_Y], [-0.5 + ran_off_two_X,0 + ran_off_two_Y],[-0.55 + ran_off_two_X,-1 + ran_off_two_Y], [-0.55 + ran_off_two_X,-2 + ran_off_two_Y],[-0.55 + ran_off_two_X,3 + ran_off_two_Y],
-    [-1.55 + ran_off_two_X,-2.5 + ran_off_two_Y],[-1.5 + ran_off_two_X,1.5 + ran_off_two_Y], [-1.55 + ran_off_two_X,0.5 + ran_off_two_Y], [-1.55 + ran_off_two_X,-0.5], [-1.55 + ran_off_two_X,-1.5 + ran_off_two_Y],[-1.55 + ran_off_two_X,2.5 + ran_off_two_Y],
-    [-2.55 + ran_off_two_X,-2 + ran_off_two_Y],[-2.55 + ran_off_two_X,-1 + ran_off_two_Y],[-2.55 + ran_off_two_X,-0 + ran_off_two_Y],[-2.55 + ran_off_two_X,1 + ran_off_two_Y],[-2.55 + ran_off_two_X,2 + ran_off_two_Y]
+    [2.55,-2],[2.55,-1],[2.55,0],[2.55,1],[2.55,2],
+    [1.55,-2.5],[1.55,1.5], [1.55,0.5],[1.55,-0.5], [1.55,-1.5], [1.55,2.5],
+    [0.55,-3],[0.55,2], [0.55,1], [0.55,0], [0.55,-1], [0.55,-2],[0.55,3],
+    [-0.55,-3],[-0.55,2], [-0.55,1], [-0.5,0],[-0.55,-1], [-0.55,-2],[-0.55,3],
+    [-1.55,-2.5],[-1.5,1.5], [-1.55,0.5], [-1.55,-0.5], [-1.55,-1.5],[-1.55,2.5],
+    [-2.55,-2],[-2.55,-1],[-2.55,-0],[-2.55,1],[-2.55,2]
   ]
   var offsets3 = [
-  [2.25 + ran_off_three_X,-1.5 + ran_off_three_Y],[2.25 + ran_off_three_X,-0.5 + ran_off_three_Y],[2.25 + ran_off_three_X,0.5 + ran_off_three_Y],[2.25 + ran_off_three_X,1.5 + ran_off_three_Y],
-  [1.25 + ran_off_three_X,-1 + ran_off_three_Y],[1.25 + ran_off_three_X,-2 + ran_off_three_Y],[1.25 + ran_off_three_X,-0 + ran_off_three_Y],[1.25 + ran_off_three_X,1 + ran_off_three_Y],[1.25 + ran_off_three_X,2 + ran_off_three_Y],
-  [0.4 + ran_off_three_X,-1.5 + ran_off_three_Y],[0.4 + ran_off_three_X,-2.5 + ran_off_three_Y],[0.4 + ran_off_three_X,-0.5 + ran_off_three_Y],[0.4 + ran_off_three_X,0.5 + ran_off_three_Y],[0.4 + ran_off_three_X,1.5 + ran_off_three_Y],[0.4 + ran_off_three_X,2.5 + ran_off_three_Y],
-  [-0.4 + ran_off_three_X,-1.5 + ran_off_three_Y],[-0.4 + ran_off_three_X,-2.5 + ran_off_three_Y],[-0.4 + ran_off_three_X,-0.5 + ran_off_three_Y],[-0.4 + ran_off_three_X,0.5 + ran_off_three_Y],[-0.4 + ran_off_three_X,1.5 + ran_off_three_Y],[-0.4 + ran_off_three_X,2.5 + ran_off_three_Y],
-  [-1.25 + ran_off_three_X,-1 + ran_off_three_Y],[-1.25 + ran_off_three_X,-2 + ran_off_three_Y],[-1.25 + ran_off_three_X,-0 + ran_off_three_Y],[-1.25 + ran_off_three_X,1 + ran_off_three_Y],[-1.25 + ran_off_three_X,2 + ran_off_three_Y],
-  [-2.25 + ran_off_three_X,-1.5 + ran_off_three_Y],[-2.25 + ran_off_three_X,-0.5 + ran_off_three_Y],[-2.25 + ran_off_three_X,0.5 + ran_off_three_Y],[-2.25 + ran_off_three_X,1.5 + ran_off_three_Y]
+    [2.25,-1.5],[2.25,-0.5],[2.25,0.5],[2.25,1.5],
+    [1.25,-1],[1.25,-2],[1.25,-0],[1.25,1],[1.25,2],
+    [0.4,-1.5],[0.4,-2.5],[0.4,-0.5],[0.4,0.5],[0.4,1.5],[0.4,2.5],
+    [-0.4,-1.5],[-0.4,-2.5],[-0.4,-0.5],[-0.4,0.5],[-0.4,1.5],[-0.4,2.5],
+    [-1.25,-1],[-1.25,-2],[-1.25,-0],[-1.25,1],[-1.25,2],
+    [-2.25,-1.5],[-2.25,-0.5],[-2.25,0.5],[-2.25,1.5]
   ]
-  var pixel_posx1 = p5.map(pos_x, x1, x2, 0, 256);
-  var pixel_posx2 = p5.map(pos_x+rad2, x1, x2, 0, 256);
-  var pixel_radius = pixel_posx2 - pixel_posx1;
+
   for(var i=0; i<offsets.length; i++) {
-    ran_off_one_X = p5.random(-0.5, 0.5);
-    ran_off_one_Y = p5.random(-0.5, 0.5);
     var offset = offsets[i];
-    var pixel_x = p5.map(pos_x+0.5*rad1*offset[0], x1, x2, 0, 256);
-    var pixel_y = p5.map(pos_y+0.5*rad1*offset[1], y1, y2, 0, 256);
-    p5.strokeWeight(0);
-    p5.fill(164,64,184,75);
-    p5.ellipse(pixel_x, pixel_y, pixel_radius);
+    sparks(p5, x1, x2, y1, y2, z, pos_x, pos_y, rad1, rad2 , zoom, offset)
   }
 
   for(var i=0; i<offsets2.length; i++) {
-    ran_off_two_X = p5.random(-0.5, 0.5);
-    ran_off_two_Y = p5.random(-0.5, 0.5);
     var offset2 = offsets2[i];
-    var pixel_x = p5.map(pos_x+0.5*rad1*offset2[0], x1, x2, 0, 256);
-    var pixel_y = p5.map(pos_y+0.5*rad1*offset2[1], y1, y2, 0, 256);
-    p5.strokeWeight(0);
-    p5.fill(164,64,184,50);
-    p5.ellipse(pixel_x, pixel_y, pixel_radius/1.25);
+    sparks(p5, x1, x2, y1, y2, z, pos_x, pos_y, rad1, rad2 , zoom, offset2)
   }
+
   for(var i=0; i<offsets3.length; i++) {
-    ran_off_three_X = p5.random(-0.5, 0.5);
-    ran_off_three_Y = p5.random(-0.5, 0.5);
     var offset3 = offsets3[i];
-    var pixel_x = p5.map(pos_x+0.5*rad1*offset3[0], x1, x2, 0, 256);
-    var pixel_y = p5.map(pos_y+0.5*rad1*offset3[1], y1, y2, 0, 256);
-    p5.strokeWeight(0);
-    p5.fill(164,64,184,25);
-    p5.ellipse(pixel_x, pixel_y, pixel_radius/1.5);
+    sparks(p5, x1, x2, y1, y2, z, pos_x, pos_y, rad1, rad2 , zoom, offset3)
   }
+}
+
+function sparks(p5, x1, x2, y1, y2, z, pos_x, pos_y, rad1, rad2 , zoom, offset){
+  var pixel_posx1 = p5.map(pos_x, x1, x2, 0, 256);
+  var pixel_posx2 = p5.map(pos_x+rad2, x1, x2, 0, 256);
+  var pixel_radius = pixel_posx2 - pixel_posx1;
+  var noiseScale = 1.0;
+
+    ran_off_X = p5.noise(pos_x * noiseScale, pos_y * noiseScale, z+31);
+    ran_off_Y = p5.noise(pos_x * noiseScale, pos_y * noiseScale, z+32);
+    ran_off_X = p5.map(ran_off_X, 0, 1, -0.5, 0.5);
+    ran_off_Y = p5.map(ran_off_Y, 0, 1, -0.5, 0.5);
+
+    var pixel_x = p5.map(pos_x+0.5*rad1*offset[0], x1, x2, 0, 256);
+    var pixel_y = p5.map(pos_y+0.5*rad1*offset[1], y1, y2, 0, 256);
+    newColor(p5, pos_x , pos_y , noiseScale, z+10);
+    p5.ellipse(pixel_x, pixel_y, pixel_radius/1.5);
 }
 
 /*
@@ -112,12 +137,13 @@ function drawStamens(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2, drawLines) {
  */
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // debug - show border
+
   var max_shift = max_thickness + max_movement;
   var min_x = snap_to_grid(x1 - max_shift, grid_size);
   var max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
   var min_y = snap_to_grid(y1 - max_shift, grid_size);
   var max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
-
+  var noiseScale = 1.0;
 
   var c_p00 = p5.map(0, x1, x2, 0, 256);
   var c_plwidth = p5.map(line_width, x1, x2, 0, 256);
@@ -125,6 +151,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   var cur_line_width = c_plwidth - c_p00;
   var cur_ball_radius = c_pball - c_p00;
 
+  p5.noStroke();
   p5.background(0);
   for(var x=min_x; x<max_x; x+=grid_size) {
     for(var y=min_y; y<max_y; y+=grid_size) {
@@ -132,34 +159,43 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
       var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
 
+      if(zoom <= 1){
+          newColor(p5, shift_point[0], shift_point[1] , noiseScale, z+10);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*2);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.75);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.5);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.25);
+       }
+       if(zoom <= 2){
+          newColor(p5, shift_point[0], shift_point[1] , noiseScale, z+10);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.75);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.5);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.25);
+       }
+       if(zoom <= 3){
+          newColor(p5, shift_point[0], shift_point[1] , noiseScale, z+10);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.5);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.25);
+       }
+       if(zoom <= 4){
+          newColor(p5, shift_point[0], shift_point[1] , noiseScale, z+10);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius*1.25);
+       }
 
-      p5.noStroke();
-      // if zoomed: first, draw petals *behind* the ellipse
-    
-      // draw ellipse
-      p5.fill(0, 0, 128);
-      if(zoom <= 2){
-        var rancol = p5.random(0,2);
-        if (rancol <= 0 ){
-          p5.fill(164,64,184,75);
-        } 
-        else if (rancol <= 1){
-          p5.fill(164,64,184,50);
-        }
-        else{
-          p5.fill(164,64,184,25);
-        }
-      p5.ellipse(x_pos, y_pos, cur_ball_radius);}
+      if(zoom >= 4 && zoom <= 5){
+          newColor(p5, shift_point[0], shift_point[1] , noiseScale, z+10);
+          p5.ellipse(x_pos, y_pos, cur_ball_radius);
+       }
 
-      // if zoomed: last draw stamens *in front of* the ellipse
-      if(zoom >= 3) {
-        // now if we are super zoomed, draw lines in the stamen
-        var drawLines = false;
-        if (zoom >= 5) drawLines = true;
-        p5.fill(0, 0, 255);
-        p5.stroke(0, 0, 128);
-        drawStamens(p5, x1, x2, y1, y2, shift_point[0], shift_point[1], ball_radius/3, line_width/2, drawLines);
-      }
+      if(zoom >= 6) {
+        detailOne(p5, x1, x2, y1, y2, z, shift_point[0], shift_point[1], ball_radius/3, line_width/2, zoom);  
+      }   
     }
   }
 }
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }  
+}
+
