762b27cf594dde4fa49ca7e6d76b1d1362ba2b44
Samantha Gard <sam.gard@abletech.co.nz>
2017-10-18 16:32:04 +1300

    Voronoi diagram drawing correctly Redrawing stars as cities at higher zoom level

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ I think I will fiddle around with how many 'stars' are drawn initially, and how 
 
 ### Part 3
 
-I've added some initial code for the Voronoi diagram/moutain background pattern. It's using the d3.js library to calculate the vertices and how the cells should be drawn, but p5.js to actually draw the shapes.
+I've added some initial code for the Voronoi diagram/mountain background pattern. It's using the d3.js library to calculate the vertices and how the cells should be drawn, but p5.js to actually draw the shapes.
 I think what I want the code to do is actually take the stars as the vertices points and then draw the cells around each of the stars - each mountain will have it's own 'star', which then means when it is zoomed in, each set of columns will be contained inside it's own mountain.
-I still have a lot to fiddle around with, especially cause I'm still trying to get my head around the whole 'drawing so that when it zooms in it doesn't redraw' thing. 
+I still have a lot to fiddle around with, especially cause I'm still trying to get my head around the whole 'drawing so that when it zooms in it doesn't redraw' thing.
+
+### Part 3 cont.
+
+I've now got the Voronoi diagram drawing properly - the clipping was affecting how the cells were being drawn at a higher zoom level, and am now passing the world_vertices of the cell polygon points through, so that the same colours are used across the different grids.
+I've also added in code for redrawing the 'stars' as 'cities'. This is not quite working properly, as I believe I'm passing in the wrong variable, which causes them to be redrawn each time I zoom in. Currently working on fixing this.
--- drawgrid2.js
+++ drawgrid2.js
@@ -1,20 +1,42 @@
-var max_movement = 32;
+var max_city_movement = 48;
+var max_bldg_movement = 4.0;
 var grid_size = 32;
 var mt_count = 32;
-var line_width = 8;
 
-function getOffsetPoint(p5, x, y, z, noiseScale) {
+function getOffsetPoint(p5, x, y, z, noiseScale, max_shift) {
   var noiseX = p5.noise(x * noiseScale,
                         y * noiseScale, z);
   var noiseY = p5.noise(x * noiseScale,
                         y * noiseScale, z+50);
-  var offsetX = p5.map(noiseX, 0, 1, -max_movement, max_movement);
-  var offsetY = p5.map(noiseY, 0, 1, -max_movement, max_movement);
+  var offsetX = p5.map(noiseX, 0, 1, -max_shift, max_shift);
+  var offsetY = p5.map(noiseY, 0, 1, -max_shift, max_shift);
   return [x+offsetX, y+offsetY]
 }
 
 function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
+}
+
+function drawCluster(p5, x1, x2, y1, y2, z, zoom, x_c, y_c, size) {
+// function drawCluster(p5, x1, x2, y1, y2, z, zoom, x_c, y_c, size, citySize) {
+  var bldgColor1 = p5.color(204,133,98),
+      bldgColor2 = p5.color(255,188,161);
+
+  console.log(size);
+  var num_bldgs = Math.floor(p5.map(size, 8, 18, 1, 12));
+
+  for(var i=0; i<num_bldgs; i++) {
+    var off = getOffsetPoint(p5, x_c, y_c, z+33+i, 0.01, max_bldg_movement);
+    var x_pos = p5.map(off[0], x1, x2, 0, 256);
+    var y_pos = p5.map(off[1], y1, y2, 0, 256);
+
+    var noiseBldg = p5.noise(x_pos, y_pos, z+73);
+    var sizeBldg = Math.floor(p5.map(noiseBldg, 0, 1, 1, size));
+
+    var bldgColor = p5.lerpColor(bldgColor1,bldgColor2,noiseBldg);
+    p5.fill(bldgColor);
+    p5.rect(x_pos, y_pos, sizeBldg, sizeBldg);
+  }
 }
 
 /*
@@ -28,8 +50,9 @@ function snap_to_grid(num, gsize) {
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
+
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  var max_shift = max_movement;
+  var max_shift = max_city_movement;
   var min_x = snap_to_grid(x1 - max_shift, grid_size);
   var max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
   var min_y = snap_to_grid(y1 - max_shift, grid_size);
@@ -39,40 +62,46 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   var ellipseColor = [255,255,255],
       mtColor1 = p5.color(62,175,171),
       mtColor2 = p5.color(23,111,118),
+      cityColor1 = p5.color(248,172,142),
+      cityColor2 = p5.color(239,155,133),
       bgColor = p5.lerpColor(mtColor1,mtColor2,0.5);
 
   p5.background(bgColor);
 
   var vertices = [];
+  var world_vertices = [];
 
-  // if(zoom >= 2) {
+  if(zoom >= 2) {
     for(var x=min_x; x<max_x; x+=grid_size) {
       for(var y=min_y; y<max_y; y+=grid_size) {
-        var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+        var shift_point = getOffsetPoint(p5, x, y, z, 0.1, max_city_movement);
         var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
         var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
         vertices.push([x_pos,y_pos]);
-
-        // using d3.js voronoi layout to calculate voronoi polygons
-        var voronoi = d3.geom.voronoi()
-        .clipExtent([
-          [0, 0],
-          [256, 256]
-        ]);
-
-        // create polygons using d3.js voronoi diagram
-        var polygons = voronoi(vertices);
-
+        world_vertices.push([shift_point[0], shift_point[1]]);
       }
     }
+    // using d3.js voronoi layout to calculate voronoi polygons
+    var voronoi = d3.geom.voronoi();
+
+    // create polygons using d3.js voronoi diagram
+    var polygons = voronoi(vertices);
 
     // draw polygons
     for(var j=0; j<polygons.length; j++) {
       var apolygon = polygons[j];
+      var avertex = world_vertices[j];
 
       // pick a random color
-      // var mtVal = p5.noise(x,y,z+10);
-      var mtVal = p5.random(0,1);
+      var mtVal = p5.noise(avertex[0],avertex[1],z+17);
+      var mtVal = p5.map(mtVal, 0.25, 0.75, 0, 1);
+      if(mtVal < 0) {
+        mtVal = 0;
+      }
+      if(mtVal > 1) {
+        mtVal = 1;
+      }
+
       var mtColor = p5.lerpColor(mtColor1,mtColor2,mtVal);
       p5.fill(mtColor);
       p5.noStroke();
@@ -84,12 +113,12 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       }
       p5.endShape(p5.CLOSE);
     }
-  // }
+  }
 
-  // draw stars
+  // draw stars/cities
   for(var x=min_x; x<max_x; x+=grid_size) {
     for(var y=min_y; y<max_y; y+=grid_size) {
-      var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+      var shift_point = getOffsetPoint(p5, x, y, z, 0.1, max_city_movement);
       var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
       var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
 
@@ -100,8 +129,33 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       var cur_ball_radius = c_pball - c_p00;
 
       p5.noStroke();
-      p5.fill(ellipseColor);
-      p5.ellipse(x_pos, y_pos, cur_ball_radius);
+      // stars
+      if(zoom < 3.9){
+        p5.fill(ellipseColor);
+        p5.ellipse(x_pos, y_pos, cur_ball_radius);
+      }
+
+      // cities
+      p5.rectMode(p5.RADIUS);
+      if (zoom > 3.9){
+        var cityVal = p5.map(cur_ball_radius, 40, 80, 0, 1);
+        if(cityVal < 0) {
+          cityVal = 0;
+        }
+        if(cityVal > 1) {
+          cityVal = 1;
+        }
+        var cityColor = p5.lerpColor(cityColor1,cityColor2,cityVal),
+            citySize = cur_ball_radius/8;
+
+        drawCluster(p5, x1, x2, y1, y2, z, zoom, shift_point[0], shift_point[1], citySize);
+        p5.fill(cityColor);
+        p5.rect(x_pos, y_pos, citySize, citySize);
+      }
     }
   }
+  // debug - show border
+  // p5.noFill();
+  // p5.stroke(255, 0, 0)
+  // p5.rect(0, 0, 255, 255);
 }
