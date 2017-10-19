18447f163b0f27315b31179810a35a9102935554
Christina Petris <christina.petris@outlook.com>
2017-10-19 14:18:05 +1300

    final

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-## Part	3:	Layering Details
+## Final
 
-There are three distinct zones now. Each zone gets more abstract. There is a cloud zone at first then it focuses onto the "buildings", finally there is a focus on more of a circular pattern in contrast. 
+This design is a combination of influences from Gatsby (Art Deco style) and Art Nouveau style. 
+
+After experimentations with the noise function I developed the idea of clouds overlaying a city like scape in an abstract way. 
+
+By figuring out how to make more and more intricate details as you zoom in I wanted to show the pattern become more rounded and take on from the Art Nouveau style more.
+
+Originally I wanted to show more contrast in colour. Ideally I wanted to make a bright contrast that appears the more you zoom in. The idea was to have a populated, polluted cityscape which as you zoom in it goes back more to nature. 
+
+Instead of going with that idea I decided I wanted to keep a more consitent theme with subtler changes. Maybe in the future I could experiment more with different colours. 
--- drawgrid.js
+++ drawgrid.js
@@ -15,9 +15,6 @@ var curve_edge_amount2 =0.1;
 var tourSeed = 100;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-//  [0, 673, 1180],
-//  [1,800,1048],
-//  [1, 512, 512],
     [0,854.000000000000,808.000000000000],
     [2,723.750000000000,910.000000000000],
     [3,724.000000000000,910.000000000000],
@@ -48,26 +45,11 @@ function drawRectangles(p5, x1, x2, y1, y2, pos_x, pos_y, size, thickness) {
   var pixel_size = pixel_posx2 - pixel_posx1;
   var lineWeight = line_posx1 - pixel_posx1;
   p5.strokeWeight(lineWeight);
-    
-//  p5.rect(pixel_posx1+pixel_size, pixel_y, pixel_size, pixel_size); 
-//  p5.rect(pixel_posx1, pixel_y, pixel_size, pixel_size);    
-//  p5.rect(pixel_posx1-pixel_size, pixel_y, pixel_size, pixel_size);
-//  p5.rect(pixel_posx1-pixel_size*2, pixel_y, pixel_size, pixel_size);
-//  p5.rect(pixel_posx1-pixel_size*3, pixel_y, pixel_size, pixel_size);
-    
-//  p5.ellipse(pixel_posx1+pixel_size, pixel_y, pixel_size, pixel_size); 
-//  p5.ellipse(pixel_posx1, pixel_y, pixel_size, pixel_size);    
-//  p5.ellipse(pixel_posx1-pixel_size, pixel_y, pixel_size, pixel_size);
-//  p5.ellipse(pixel_posx1-pixel_size*2, pixel_y, pixel_size, pixel_size);
-//  p5.ellipse(pixel_posx1-pixel_size*3, pixel_y, pixel_size, pixel_size);
 
   p5.ellipse(pixel_posx1+pixel_size, pixel_y, pixel_size, pixel_size); 
   p5.ellipse(pixel_posx1, pixel_y, pixel_size, pixel_size);    
   p5.ellipse(pixel_posx1-pixel_size, pixel_y, pixel_size, pixel_size);
-    
 
-
-  
 }
 
 function drawDetail(p5, x1, x2, y1, y2, pos_x_start, pos_y_start, pos_x_end, pos_y_end, thickness) {
@@ -96,20 +78,20 @@ function drawDetail(p5, x1, x2, y1, y2, pos_x_start, pos_y_start, pos_x_end, pos
  */
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
     p5.background(0);
-  var noiseScale=0.02; 
-//the higher the less grouped.
-  var noiseScale=0.01; 
-  p5.noiseDetail(8,0.5);
-  p5.noiseDetail(1,0.9);
-  p5.noStroke();
-          p5.rectMode(p5.center);
-    
+    var noiseScale=0.02; 
+    //the higher the less grouped.
+    var noiseScale=0.01; 
+    p5.noiseDetail(8,0.5);
+    p5.noiseDetail(1,0.9);
+    p5.noStroke();
+    p5.rectMode(p5.center);
 
-  var max_shift = max_thickness + max_movement;
-  var min_x = snap_to_grid(x1 - max_shift, grid_size);
-  var max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
-  var min_y = snap_to_grid(y1 - max_shift, grid_size);
-  var max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
+
+    var max_shift = max_thickness + max_movement;
+    var min_x = snap_to_grid(x1 - max_shift, grid_size);
+    var max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+    var min_y = snap_to_grid(y1 - max_shift, grid_size);
+    var max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
 
    
 ////the size of things   
@@ -129,79 +111,60 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
     var cur_rectWidth2 = c_prect2 - c_p00;
     var rect_detail = c_edge - c_p00;
 
-//    /*
-    //draw rectangles
-//if(zoom >= 5){
-//    rectangles(true,rect_detail/2,rect_detail/2,true,cur_line_width3);
-//    rectangles(true,rect_detail*2,rect_detail*2,true,cur_line_width3);
-//}
-       if(zoom >= 3){
-         rectangles(true,rect_detail/2,rect_detail,false,cur_line_width3);
-            
+    if(zoom >= 3){
+         rectangles(true,rect_detail/2,rect_detail,false,cur_line_width3);    
      }
-  p5.fill(0, 0, 128);
+    p5.fill(0, 0, 128);
     //clouds
     rectangles(true,0,0,false,cur_line_width2);
-//    if(zoom <= 2){
-        rectangles(false,0,0,false,cur_line_width);
-//    }
-  
+
+    rectangles(false,0,0,false,cur_line_width);
     
     function rectangles(buildings,xTrans, yTrans,grayScale,strkeWeight){
         p5.push();
         p5.translate(xTrans,yTrans);
-  for(var x=min_x; x<max_x; x+=grid_size) {
-    for(var y=min_y; y<max_y; y+=grid_size) {
-        //clouds:
-        var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
-        var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
-        var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
-        
-        var noiseValue = p5.noise(x * noiseScale,y*noiseScale);
+        for(var x=min_x; x<max_x; x+=grid_size) {
+        for(var y=min_y; y<max_y; y+=grid_size) {
+            //clouds:
+            var shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+            var x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
+            var y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
+
+            var noiseValue = p5.noise(x * noiseScale,y*noiseScale);
 
 
-        p5.fill(noiseValue*120,noiseValue*200);
-        p5.stroke(noiseValue*150,noiseValue*100);
-       
-//          if(noiseValue <0.1){
-//             p5.fill(20, 2, 8,noiseValue*255);
-//           p5.stroke(20, 2, 8,noiseValue*255*1.5);
-//        }
-//        else if(noiseValue <0.25){
-//                p5.fill(3, 0, 3,noiseValue*255);
-//           p5.stroke(3, 0, 3,noiseValue*255*1.5);
-//        }
-//      
-        
-        p5.strokeWeight(strkeWeight);
-         var rectSize = noiseValue*2;
-              
+            p5.fill(noiseValue*120,noiseValue*200);
+            p5.stroke(noiseValue*150,noiseValue*100);
 
-        if (buildings == true){
-            var x_pos = p5.map(x, x1, x2, 0, 256);
-            var y_pos = p5.map(y, y1, y2, 0, 256);
-            var rectSize = 1;
-            var noiseValue = p5.noise(x,y);
-            //looks cool
-            //console.log(noiseValue);
-            brightness=0.25;
-              if(zoom >= 2) {
-                   brightness=0.5;
-              }
-             if(zoom >= 3) {
-                   brightness=.75;
-              }
-            if(noiseValue<0.05) {
-       p5.noStroke();;
-       p5.fill(0);
-                p5.noFill();
-                 
-  } 
-            else if(noiseValue<0.1) {
-       p5.stroke(240,150,70,50*brightness); 
-       p5.fill(240,150,70,5*brightness);
-                 
-  }
+            p5.strokeWeight(strkeWeight);
+             var rectSize = noiseValue*2;
+
+
+            if (buildings == true){
+                var x_pos = p5.map(x, x1, x2, 0, 256);
+                var y_pos = p5.map(y, y1, y2, 0, 256);
+                var rectSize = 1;
+                var noiseValue = p5.noise(x,y);
+                //looks cool
+                //console.log(noiseValue);
+                brightness=0.25;
+                  if(zoom >= 2) {
+                       brightness=0.5;
+                  }
+                 if(zoom >= 3) {
+                       brightness=.75;
+                  }
+                if(noiseValue<0.05) {
+           p5.noStroke();;
+           p5.fill(0);
+                    p5.noFill();
+
+        } 
+                else if(noiseValue<0.1) {
+           p5.stroke(240,150,70,50*brightness); 
+           p5.fill(240,150,70,5*brightness);
+
+        }
   else if(noiseValue<0.25) {
     p5.stroke(230,175,75,75*brightness);
       p5.fill(230,175,75,10*brightness);
@@ -244,7 +207,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
         }
         else{
              p5.rect(x_pos, y_pos, cur_rectWidth2*rectSize, cur_rectWidth1*rectSize, rect_detail);
-            // p5.ellipse(x_pos, y_pos, cur_rectWidth1*rectSize, cur_rectWidth*rectSize);
         }
       
     }
@@ -262,30 +224,15 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   var max_x0 = snap_to_grid(x2 + grid_size, grid_size);
   var min_y0 = snap_to_grid(y1, grid_size);
   var max_y0 = snap_to_grid(y2 + grid_size, grid_size);
-//   if(zoom >= 7) {
-//      p5.noFill();
-//      p5.stroke(0, 255, 0)
-//           p5.stroke(100,100);
-//      for(var x=min_x0; x<max_x0; x+=0.025*grid_size) {
-//        for(var y=min_y0; y<max_y0; y+=0.025*grid_size) {
-//          drawRectangles(p5, x1, x2, y1, y2, x, y, 0.005*rectWidth, 0.0005*rectWidth);
-//          drawRectangles(p5, x1, x2, y1, y2, x, y, 0.005*rectWidth, 0.0005*rectWidth);
-//        }
-//      }
-//   }
+
   if(zoom >= 5) {
       p5.noFill();
       p5.stroke(100,100);
 
       for(var x=min_x0; x<max_x0; x+=0.25*grid_size) {
         for(var y=min_y0; y<max_y0; y+=0.25*grid_size) {
-      //var noiseScale = 0.04;
       var noiseValue = p5.noise(x,y);
-       // p5.fill(noiseValue*120,noiseValue*200);
         p5.stroke(noiseValue*150,noiseValue*100);
-          //drawRectangles(p5, x1, x2, y1, y2, x, y, 0.05*rectWidth, 0.005*rectWidth);
-             //drawRectangles(p5, x1, x2, y1, y2, x, y, 0.05*rectWidth, 0.005*rectWidth);
-             //drawRectangles(p5, x1, x2, y1, y2, x, y, 0.05*rectWidth, 0.005*rectWidth);
             drawRectangles(p5, x1, x2, y1, y2, x, y, 0.035*rectWidth, 0.0015*rectWidth);
             drawRectangles(p5, x1, x2, y1, y2, x, y, 0.07*rectWidth, 0.0035*rectWidth);
              p5.stroke(noiseValue*80,noiseValue*100);
