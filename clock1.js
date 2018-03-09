643165639c5df840ec96c10f1096db6526a25efb
Guest User <Guest@wg401-c-05-07.mddn.vuw.ac.nz>
2018-03-09 14:01:05 +1300

    Updated clock1.js with a picture of a 4

--- clock1.js
+++ clock1.js
@@ -1,5 +1,7 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
+const BLOCK_SIZE = 40;
+const NUM_LOOP = 4
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -11,12 +13,18 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+  background(1); // light black background
+  strokeWeight(0); // Stroke weight to 8 pixels
+  //the number 4
+  rect(CANVAS_WIDTH/6+(BLOCK_SIZE*NUM_LOOP-BLOCK_SIZE), CANVAS_HEIGHT/5, BLOCK_SIZE, BLOCK_SIZE*7);
+  rect(CANVAS_WIDTH/6, CANVAS_HEIGHT/5+(BLOCK_SIZE*4), BLOCK_SIZE*5, BLOCK_SIZE);
+  for(i=0; i < NUM_LOOP; i++){
+    j=i+1;
+    rect(CANVAS_WIDTH/6+(BLOCK_SIZE*i), CANVAS_HEIGHT/5+(BLOCK_SIZE*4)-(BLOCK_SIZE*j), BLOCK_SIZE, BLOCK_SIZE);  
+  }
+  
+  rect(CANVAS_WIDTH*0.75,CANVAS_HEIGHT/2 ,20 ,20);
+
 }
 
 // do not alter or remove this function
