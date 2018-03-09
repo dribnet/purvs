78cec5541f88c98668f63ccbc22f83d07a9d134e
David Kim <d.kim1423@gmail.com>
2018-03-09 13:55:20 +1300

    Maeda clock
    4 07 baby

--- clock1.js
+++ clock1.js
@@ -11,12 +11,23 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+  background(0); // light gray background
+  strokeWeight(0); // Stroke weight to 8 pixels
+  fill(255);
+  rect(300, 140, 30, 210);
+  rect(270,170,30,30);
+  rect(240,200,30,30);
+  rect(210,230,30,30);
+  rect(210,260,150,30);
+  fill(100);
+  rect(560,200,15,90);
+  rect(620,200,15,90);
+  rect(575,185,45,15);
+  rect(575,290,45,15);
+  rect(655,185,90,15);
+  rect(730,185,15,55);
+  rect(715,240,15,15);
+  rect(700,255,15,50);
 }
 
 // do not alter or remove this function
