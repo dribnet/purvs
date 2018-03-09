a25ad65f22d9f4380201ae30de3da90b956ff8ff
taylormatt7 <taylormatt7@myvuw.ac.nz>
2018-03-09 15:03:18 +1300

    Start of Clock #6. Difficulty with generations.
    I chose to recreate clock #6, my unfamiliarity with javascript's handling of arrays is causing a little difficulty, I'm possibly just too tired to wrap my head around it right at this moment.

--- clock1.js
+++ clock1.js
@@ -1,30 +1,81 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
 
-function setup () {
-  // create the drawing canvas, save the canvas element
-  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  main_canvas.parent('canvasContainer');
+const cellSize = 20;
+var w, h;
+var cells = [];
+var newCells = [];
+var cellColour;
+var textColour;
+var backgroundColour;
 
-  // you can optionally add your own code here if you also have setup code
+function setup() {
+	// create the drawing canvas, save the canvas element
+	let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+	main_canvas.parent('canvasContainer');
+
+	// you can optionally add your own code here if you also have setup code
+	w = CANVAS_WIDTH / cellSize;
+	h = CANVAS_HEIGHT / cellSize;
+	textColour = color(0xFF, 0xFF, 0x00);
+	cellColour = color(0x90);
+	backgroundColour = color(0x30);
+	for (let i = 0; i < w; i++) {
+		var line = [];
+		for (let j = 0; j < h; j++) {
+			line.push(random(1) < 0.1 ? true : false);
+		}
+		cells.push(line);
+	}
+	strokeWeight(1);
+
+	updateLife(25);
 }
 
 // Update this function to draw you own maeda clock
-function draw () {
-  background(0xAA, 0x00, 0xBB); // purple background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+function draw() {
+	translate(-0.5, -0.5);
+	background(0xBB); // nice grey background
+
+	stroke(backgroundColour);
+	for (let i = 0; i < w; i++) {
+		for (let j = 0; j < h; j++) {
+			fill(cells[i][j] ? cellColour : backgroundColour);
+			rect(i * cellSize, j * cellSize, w, h);
+		}
+	}
+	// if (frameCount % 60 === 0) {
+	// 	updateLife(1);
+	// }
+}
+
+function updateLife(generationNum) {
+	newCells = cells.slice(0);
+	for (let i = 0; i < generationNum; i++) {
+		for (let x = 0; x < w; x++) {
+			for (let y = 0; y < h; y++) {
+				let neighbourCount = 0;
+				for (let xLocal = -1; xLocal <= 1; xLocal++) {
+					for (let yLocal = -1; yLocal <= 1; yLocal++) {
+						if (xLocal === 0 && yLocal === 0) {
+							continue;
+						}
+						neighbourCount += (cells[(x + xLocal + w) % w][(y + yLocal + h) % h] ? 1 : 0);
+					}
+				}
+				newCells[x][y] = ((neighbourCount === 2) ? cells[x][y] : (neighbourCount === 3));
+			}
+		}
+	}
+	cells = newCells.slice(0);
 }
 
 // do not alter or remove this function
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
-  else if (key == '@') {
-    saveBlocksImages(true);
-  }
+	if (key == '!') {
+		saveBlocksImages();
+	}
+	else if (key == '@') {
+		saveBlocksImages(true);
+	}
 }
