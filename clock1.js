345758698ea9817b5c80eb1ace599cd22b0fd7a6
Guest User <Guest@wg401-i-07-07.mddn.vuw.ac.nz>
2018-03-09 13:48:14 +1300

    I have commited new files
    I have done some small changes to test if these will commit

---
+++ clock1.js
@@ -0,0 +1,43 @@
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 500;
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  main_canvas.parent('canvasContainer');
+
+  // you can optionally add your own code here if you also have setup code
+}
+
+// Update this function to draw you own maeda clock
+function draw () {
+  background(204); // light gray background
+  strokeWeight(8); // Stroke weight to 8 pixels
+  ellipse(480, 250, 190, 190);
+  // The rectangle draws on top of the ellipse
+  // because it comes after in the code
+  rect(500, 280, 260, 20);
+  point(240, 60);
+
+fill (50);
+
+triangle(347, 54, 392, 9, 392, 66);
+
+quad(158, 55, 199, 14, 392, 66, 351, 107);
+
+rect(50, 50, 80, 80)
+
+ellipse( 200, 200, 50, 50);
+
+
+}
+
+// do not alter or remove this function
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
