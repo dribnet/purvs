775da5893172ece3ba8300d6d02d570b3c1ed362
Guest User <Guest@wg401-i-01-07.mddn.vuw.ac.nz>
2018-03-09 15:14:25 +1300

    Updated README and clock.

--- README.md
+++ README.md
@@ -8,7 +8,3 @@ image above. You can also include
 [web links](https://en.wikipedia.org/wiki/Clock_of_the_Long_Now)
 to other pages that support your ideas.  The explanation should
 be short but compete, no more than [100 words](https://wordcounter.net/).
-
-Replace the "Outrageous clock" title above with the name of
-your clock. Also update both images keeping their original sizes:
-preview.jpg (960x500) and thumbnail.png (230x120).
--- clock1.js
+++ clock1.js
@@ -11,12 +11,24 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
+  background(0); // light gray background
+  //strokeWeight(8); // Stroke weight to 8 pixels
+  //ellipse(480, 250, 190, 190);
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
-  rect(500, 280, 260, 20);
+  //rect(500, 280, 260, 20);
+  fill(255);
+  rect(150, 150, 50, 250);
+  rect(200, 100, 100, 50);
+  rect(300, 150, 50, 250);
+  rect(200, 400, 100, 50);
+
+  rect(400, 150, 50, 250);
+  rect(450, 100, 100, 50);
+  rect(550, 150, 50, 250);
+  rect(450, 400, 100, 50);
+
+  rect(250, 100, 50, 50);
 }
 
 // do not alter or remove this function
