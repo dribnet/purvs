13db0684851b9c5020324f50fbe0cb7a52474b2a
Max Cadenhead <max.cadenhead98@gmail.com>
2018-03-09 14:25:04 +1300

    Updated with files for Part 2
    I have added the Part 2 files.

---
+++ clock1.js
@@ -0,0 +1,33 @@
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 500;
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  main_canvas.parent('canvasContainer');
+
+  // you can optionally add your own code here if you also have setup code
+}
+
+// Update this function to draw you own maeda clock
+function draw () {
+  background(150, 50, 150); // light gray background
+  strokeWeight(8); // Stroke weight to 8 pixels
+  ellipse(480, 250, 190, 190);
+  // The rectangle draws on top of the ellipse
+  // because it comes after in the code
+  rect(500, 250, 260, 20);
+  rect(300, 50, 500, 50);
+
+ellipse(MouseX, MouseY, 80, 80);
+}
+
+// do not alter or remove this function
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
