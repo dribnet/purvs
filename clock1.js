de9b1bb848a802b162537756f897eb816f6a5b2f
Guest User <Guest@wg401-d-02-06.mddn.vuw.ac.nz>
2018-03-09 14:16:13 +1300

    Saving work on copy clock tutorial.

--- clock1.js
+++ clock1.js
@@ -1,6 +1,6 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
-
+var a = 280;
 function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -12,11 +12,28 @@ function setup () {
 // Update this function to draw you own maeda clock
 function draw () {
   background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
+  strokeWeight(0.5); // Stroke weight to 8 pixels
+
+  //vertical line of circles for the "4"
+  ellipse(a, 100, 40, 40);
+  ellipse(a, 141, 40, 40);
+  ellipse(a, 182, 40, 40);
+  ellipse(a, 223, 40, 40);
+  ellipse(a, 264, 40, 40);
+  ellipse(a, 305, 40, 40);
+  ellipse(a, 346, 40, 40);
+  //horizontal line of circles for the "4"
+  ellipse(160, 264, 40, 40);
+  ellipse(200, 264, 40, 40);
+  ellipse(240, 264, 40, 40);
+  ellipse(320, 264, 40, 40);
+  //circles to finish the "4"
+  ellipse(239, 139, 40, 40);
+  ellipse(160, 223, 40, 40);
+  ellipse(200, 182, 40, 40);
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
-  rect(500, 280, 260, 20);
+  //rect(80, 50, 800, 400);
 }
 
 // do not alter or remove this function
