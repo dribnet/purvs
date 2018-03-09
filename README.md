771b4adce802f02684f6e2acb370929299601f5a
Guest User <Guest@wg401-d-06-06.mddn.vuw.ac.nz>
2018-03-09 15:24:11 +1300

    Made changes to class exercise files

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-This will be the first version of the clock.
+Maeda Clock
 
-My idea is to have 3 rings aligned vertically, 1 for hours, 1 for minutes, 1 for seconds, 
-and with small circles rotating around each ring to indicate the specific unit's amount. 
-
-In the drawing you can see that it says 2 hours, 3 minutes, and 6 seconds.
-
-The "AM" & "PM" signs would be lit or dim based on what part of the day it is.
+I chose clock #8 and decided to make the shapes simpler.
--- clock1.js
+++ clock1.js
@@ -11,14 +11,42 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-	arc(90, 60, 80, 80, 0, HALF_PI);
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
+  angleMode(RADIANS);
+  rectMode(CENTER);
+	// arc(90, 60, 80, 80, 0, HALF_PI);
+  background(0); // light gray background
+  fill(0,0,0);
+  stroke(50,150,200)
+  strokeWeight(3); // Stroke weight to 8 pixels
+  // ellipse(480, 250, 190, 190);
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
-  rect(500, 280, 260, 20);
+  // rect(500, 280, 260, 20);
+
+  line(300,300,300,340);
+  line(300,300,280,325);
+  line(280,325,310,325);
+
+stroke(30,100,150)
+scale(.5)
+translate(380,325)
+  line(300+100,300,300+100,336);
+  line(300+100,300,280+100,325);
+  line(280+100,325,310+100,325);
+
+      line(420,300,435,300); 
+      line(417,303,417,315);
+      line(438,303,438,315);
+      line(420,318,435,318); 
+        line(417,321,417,333);
+        line(438,321,438,333);
+        line(420,336,435,336); 
+    
+
 }
+
+  
+
 
 // do not alter or remove this function
 function keyTyped() {
