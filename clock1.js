80ebe31218d4b4fa81dc135db84210e89ddb4aba
Callum <callumbrowne.is@gmail.com>
2018-03-09 16:46:41 +1300

    Project Update: Maeda's Clock
    This update contains a new version of the clock, based one off of John Maedas 12 clocks. I choose to remake the 5th clock. I also added a new feature where if you click on one of the numbers, the others disappear so you get a clearer look at the current hour, minute, or second. If you click anywhere on the screen the numbers come back.
    
    I may add more to this over the next couple days.

--- clock1.js
+++ clock1.js
@@ -1,22 +1,88 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
+let pfont;
+let hide_Hours = false;
+let hide_Minutes = false;
+let hide_Seconds = false;
+
+function preload(){
+	pfont = loadFont('assets/prstart.ttf');
+}
 
 function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
-
-  // you can optionally add your own code here if you also have setup code
+  // you can optionally add your own code here if you also have setup code  
+  angleMode(DEGREES);
 }
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+  let h = hour();
+  let m = minute();
+  let s = second();
+  let text_Size = 200;
+  background(150); // light gray background
+  if(pfont!=null){
+	  textFont(pfont);
+  }
+  //Takes the Text and rotates around the screen
+  translate(CANVAS_WIDTH/2,CANVAS_HEIGHT/2);
+  textAlign(CENTER, CENTER);
+  //Hours
+  if(!hide_Hours){
+	textSize(text_Size);
+	rotate(h*15);  
+	fill(0);
+	text(h,0,0);
+	//Undo Rotation on the matrix
+	rotate(-(h*15)); 
+  }
+ 
+  //Minutes
+  if(!hide_Minutes){
+	textSize(text_Size*0.8);
+	rotate(m*6);
+	fill(255);
+	text(m,0,0);   
+	//Undo Rotation on the matrix
+	rotate(-(m*6)); 
+  }
+  
+  //Seconds
+  if(!hide_Seconds){
+	rotate(s*6);
+	fill(255,0,0);
+	textSize(text_Size*0.6);
+	text(s,0,0);
+  }
+
+}
+
+function mouseClicked(){
+	var pixelColour = get(mouseX, mouseY);
+	//Clicked on Hours
+	if(pixelColour[0] == 0 && pixelColour[1] == 0 && pixelColour[2] == 0 && pixelColour[3] == 255){
+		hide_Minutes = true;
+		hide_Seconds = true;
+	}
+	//Clicked on minutes
+	else if(pixelColour[0] == 255 && pixelColour[1] == 255 && pixelColour[2] == 255 && pixelColour[3] == 255){
+		hide_Hours = true;	
+		hide_Seconds = true;		
+	}
+	//Clicked on seconds
+	else if(pixelColour[0] == 255 && pixelColour[1] == 0 && pixelColour[2] == 0 && pixelColour[3] == 255){
+		hide_Hours = true;
+		hide_Minutes = true;
+	}
+	//Clicked elsewhere, restores clock
+	else{
+		hide_Hours = false;
+		hide_Minutes = false;
+		hide_Seconds = false;
+	}
 }
 
 // do not alter or remove this function
