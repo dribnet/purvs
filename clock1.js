5efb436515c7559cb65deaefab93c26b3f2c6136
samdarlington03 <darlinsamu@my.vuw.ac.nz>
2018-03-09 14:16:59 +1300

    Added p5 code.
    P5 code shows fake3D circles that move with the mouse.

---
+++ clock1.js
@@ -0,0 +1,52 @@
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 500;
+
+var mainx=480;
+var mainy=250;
+var xoutfactor=1;
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  main_canvas.parent('canvasContainer');
+
+  // you can optionally add your own code here if you also have setup code
+}
+
+// Update this function to draw you own maeda clock
+function draw () {
+  xoutfactor=map(mouseX,0,900,-2,2);
+  background(204); // light gray background
+  strokeWeight(6); // Stroke weight to 8 pixels
+  stroke(58, 71, 89);
+  fill(58, 71, 89);
+  ellipse(mainx-20*xoutfactor, mainy, 350, 400);
+  rect(mainx-20*xoutfactor,mainy-200,20*xoutfactor,400);
+  fill(63, 87, 124);
+  ellipse(mainx, mainy, 350, 400);
+  fill(58, 71, 89);
+  ellipse(mainx, mainy, 262.5, 300);
+  rect(mainx,mainy-150,20*xoutfactor,300);
+  fill(85, 129, 198);
+  ellipse(mainx+20*xoutfactor, mainy, 262.5, 300);
+  fill(58, 71, 89);
+  ellipse(mainx+20*xoutfactor, mainy, 175, 200);
+  rect(mainx+20*xoutfactor,mainy-100,20*xoutfactor,200);
+  fill(125, 168, 237);
+  ellipse(mainx+40*xoutfactor, mainy, 175, 200);
+  fill(58, 71, 89);
+  ellipse(mainx+40*xoutfactor, mainy, 87.5, 100);
+  rect(mainx+40*xoutfactor,mainy-50,20*xoutfactor,100);
+  fill(168, 202, 255);
+  ellipse(mainx+60*xoutfactor, mainy, 87.5, 100);
+}
+
+// do not alter or remove this function
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
