8c2ddafb299f6ed24b68d12bf9008eb98cdd39e9
Tom White <tom.white@vuw.ac.nz>
2018-03-09 14:51:31 +1300

    updated colors

--- clock1.js
+++ clock1.js
@@ -13,13 +13,24 @@ function setup () {
 function draw () {
   fill(0);
   background(0);
-  stroke(0, 255, 0);
   strokeWeight(5);
-  ellipse(width/2, height/2, 40, 40);
-  ellipse(width/2, height/2-100, 40, 40);
-  ellipse(width/2, height/2-50, 40, 40);
-  ellipse(width/2, height/2+100, 40, 40);
-  ellipse(width/2, height/2+50, 40, 40);
+  const spacing = 100;
+
+  stroke(255, 0, 0);
+  for(let i = -2*spacing; i <= 2*spacing; i = i+spacing) {
+    ellipse(width/2, height/2+i, 45, 45);
+  }
+
+  strokeWeight(3);
+  stroke(255, 255, 0);
+  for(let i = -2*spacing; i <= 2*spacing; i = i+spacing) {
+    ellipse(width/2, height/2+i, 40, 40);
+  }
+
+  stroke(0, 0, 255);
+  for(let i = -2*spacing; i <= 2*spacing; i = i+spacing) {
+    ellipse(width/2, height/2+i, 30, 30);
+  }
 }
 
 // do not alter or remove this function
