0a3b15c33fa3230c1dcf0de56f624596fe5b5152
Karu Skippr <karuskip@gmail.com>
2018-03-09 15:23:01 +1300

    Made clock read 1:10

--- clock1.js
+++ clock1.js
@@ -1,5 +1,6 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
+const width = 50;
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -11,12 +12,38 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
+ // light gray background
+  //strokeWeight(2); // Stroke weight to 8 pixels
+  
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
-  rect(500, 280, 260, 20);
+  fill(255);
+  //expand / decrease
+  var x = 150;
+  var y = 150;
+
+
+  background(50);
+  rect(x+100,y,40,100); //left 1
+  rect(x+100,y+100,40,100);
+
+  rect(x+200,y+30,50,50);//middle dots
+  rect(x+200,y+130,50,50);
+
+  rect(x+300,y,40,100);//right 1
+  rect(x+300,y+100,40,100);
+
+  rect(x+400,y,40,100);//right 0 left
+  rect(x+400,y+100,40,100);
+
+  rect(x+440,y+160,100,40);//right 0 bottom
+
+  rect(x+540,y,40,100);//right 0 right
+  rect(x+540,y+100,40,100);
+
+  rect(x+440,y,100,40);//right 0 top
+  
+
 }
 
 // do not alter or remove this function
