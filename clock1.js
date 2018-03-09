a79e1a8fe3e27dd03334664c8d389e6e25ac13ba
Melissa Marriott <marriomeli@wg401-b-05-06.mddn.vuw.ac.nz>
2018-03-09 14:04:07 +1300

    Copy clock

--- clock1.js
+++ clock1.js
@@ -11,12 +11,28 @@ function setup () {
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+  background(0); // black background
+  noStroke(); //remove stroke
+  
+  //4
+  fill(255); //white
+  rect(300, 250, 20, 40);
+  rect(320, 230, 20, 20);
+  rect(340, 210, 20, 20);
+  rect(360, 190, 20, 140);
+  rect(300, 270, 100, 20);
+  
+  //01
+  fill(140); //gray
+  rect(550, 240, 10, 50);
+  rect(590, 240, 10, 50);
+  rect(560, 230, 30, 10);
+  rect(560, 290, 30, 10);
+
+  rect(640, 230, 10, 70);
+  rect(630, 240, 10, 10);
+
+
 }
 
 // do not alter or remove this function
