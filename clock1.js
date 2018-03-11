be9d6aa9ce796961bcdcad6eb41852f496ce1f9f
BenJSpearman <bkiwi64@gmail.com>
2018-03-11 22:47:00 +1300

    Updated the clock.js and images

--- clock1.js
+++ clock1.js
@@ -2,13 +2,12 @@
 //2018
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
-angleMode=degrees;
-
 
 var sx;
 var sy;
-var t;
-var s;
+var t = 0;
+var ss;
+var ssf;
 //Creating a var for time, s for scale, and standard xy
 
 
@@ -16,22 +15,21 @@ function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
-
-  // you can optionally add your own code here if you also have setup code
+  angleMode(DEGREES);
 }
 
-// Update this function to draw you own maeda clock
+
 function draw () {
   background(0); // Black background
   strokeWeight(0); // Stroke weight to 0 pixels
 
+  ss = (sin(t)+1)*40
+  ssf = ((sin(-t)+1)*40)
 
-push();
-scale(sin(t));
   four();
   zero();
   seven();
-pop();
+
 
 
   t = t +1;
@@ -48,34 +46,36 @@ function keyTyped() {
 }
 
 //Creating a function for the standard sized blocks for the numbers
-//Will start as rect to get positioning right, will be changed into circles
+//Second version is for the number four, so they can alternate in growth
 function stdsq (sx,sy) {
-  rect(sx,sy,40,40);
+  rect(sx,sy,ss,ss);
 }
 
+function stdsqf (sx,sy) {
+  rect(sx,sy,ssf,ssf);
+}
 
 function four (){
 //Creates the number 4
 
 //Creating vars for use in function
-var xst = 380;
+var xst = 310;
 var yst = 50;
 var st = 40;
 
-scale();
 
 
 //Vertical bar
   for(i=1;i<=7;i++){
-    stdsq (xst,(yst+(i*st)));
+    stdsqf (xst,(yst+(i*st)));
   }
 //Diag bar
   for (i=1; i<=3; i++){
-    stdsq ((xst-(st*i)),(40+yst+(st*i)));
+    stdsqf ((xst-(st*i)),(40+yst+(st*i)));
   }
 //Horizontal bar
   for (i=1; i<=5; i++){
-    stdsq (((xst-160)+(st*i)),(yst+200));
+    stdsqf (((xst-160)+(st*i)),(yst+200));
   }
 
 
@@ -86,10 +86,11 @@ function zero (){
 //Creates the number 0
 
 //Creating vars for use in function
-var xst = 480;
+var xst = 450;
 var yst = 90;
 var st = 40;
 
+beginShape();
 //Top bar
   for (i=1; i<=3; i++){
       stdsq (((xst)+(st*i)), yst);
@@ -101,18 +102,15 @@ var st = 40;
   }
 
 //Left vertical bar
-  stdsq (xst, (yst+40));
-  stdsq (xst, (yst+80));
-  stdsq (xst, (yst+120));
-  stdsq (xst, (yst+160));
-  stdsq (xst, (yst+200));
+   for (i=1; i<=5; i++){
+    stdsq (xst, (yst+(st*i)));
+  }
 //Right vertical bar
-  stdsq ((xst+160), (yst+40));
-  stdsq ((xst+160), (yst+80));
-  stdsq ((xst+160), (yst+120));
-  stdsq ((xst+160), (yst+160));
-  stdsq ((xst+160), (yst+200));
+ for (i=1; i<=5; i++){
+    stdsq ((xst+160), (yst+(st*i)));
+  }
 }
+endShape();
 
 function seven (){
 //Creates the number 7
