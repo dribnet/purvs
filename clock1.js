0f0a11988e74fec10dd479d4aed3f757600bfdad
Michael Johnsen <johnsemich@wg401-f-03-06.mddn.vuw.ac.nz>
2018-03-12 18:22:13 +1300

    Got started with the code for my clock imitation.
    Found a way to randomly place squares so i didnt have to hard code tonnes of seperate squares. Rest to come tommorow.

--- clock1.js
+++ clock1.js
@@ -1,26 +1,34 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
-var x = random(0, 96);
-var y = random(0, 50);
 
 function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
+  background(204);
+
+  var x;
+  var y;
+  for(let i = 0; i<300; i++){
+    x = int(random(0, 96));
+    y = int(random(0, 50));
+    rect(x*10, y*10, 10, 10);
+  }
 
   // you can optionally add your own code here if you also have setup code
 }
 
 // Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
+   // light gray background
   strokeWeight(1); // Stroke weight to 8 pixels
   rect(20, 20, 10, 10);
   rect(20, 30, 10, 10);
   rect(30, 20, 10, 10);
   rect(30, 40, 10, 10);
-  rect(x*10, y*10, 10, 10);
-  rect(x*10, y*10, 50, 50);
+
+
+  
 
   ellipse(480, 250, 10, 10);
   ellipse(490, 250, 10, 10);
