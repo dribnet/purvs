9c50096528b9b43c3278c17a4e9d09a317255554
Michael Johnsen <johnsemich@wg401-e-01-03.mddn.vuw.ac.nz>
2018-03-13 15:01:11 +1300

    Coded a clock out of the bar examples given in class

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 ## PS1 MDDN 242 2017
 
-### Clock Concepts
+### Coded Clock	
 
-I forgot to change the read me file for the first upload, but pretty the upload is just my initial ideas and concepts. The second upload is me getting started on the imitation clock, where i had to work out a way to not have to hard code all the squares.
-
-This upload, I have refined my concepts down to ones that stem straight from my inspiration pictures on the top left and top right.
-What I have finalised is a joint idea from both concepts, although this can definately be extended further.
+This coded clock gives me a starting point for my actual clock idea. I just need to see how to make multiple circles in a circle formation that only pulse on a certain second so that the pulsing happens in a circular motion as opposed to all the circles pulsing at once on every second.
--- clock.js
+++ clock.js
@@ -11,4 +11,42 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+    let hours = obj.hours;
+    let minutes = obj.minutes;
+    let seconds = obj.seconds;
+    let millis = obj.millis;
+
+    background(120);
+
+     text("Second: " + seconds, 10, 62);
+     text("Hour: "   + hours, 10, 22);
+    text("Minute: " + minutes, 10, 42);
+    text("Millis: " + millis, 10, 82);
+
+    let secondCircleRadius = map(seconds, 0, 59, 0, 100)
+    let hourCircleRadius   = map(hours, 0, 23, 0, 100);
+    let minuteCircleRadius = map(minutes, 0, 59, 0, 100);
+    let millisCircleRadius = map(millis, 0, 1000, 0, 100);
+    let secondsWithFraction   = seconds + (millis / 1000.0);
+    let secondRadiusSmooth  = map(secondsWithFraction, 0, 60, 0, 100);
+    let col = map(millis, 0, 1000, 0, 255);
+    let colo = map(seconds, 0, 59, 0, 255);
+    let color = map(hours, 0, 23, 0, 255);
+
+    noFill();
+    ellipse(480, 300, 100, 100);
+    ellipse(480, 420, 100, 100);
+    ellipse(480, 180, 100, 100);
+    ellipse(480, 60, 100, 100);
+    fill(col, 180, 200);
+    ellipse(480, 420, millisCircleRadius, millisCircleRadius);
+    fill(colo, 180, 200);
+    ellipse(480, 300, secondRadiusSmooth, secondRadiusSmooth);
+    ellipse(480, 180, minuteCircleRadius, minuteCircleRadius);
+	fill(color, 180, 200);
+    ellipse(480, 60, hourCircleRadius, hourCircleRadius);
+
+
+
+
 }
