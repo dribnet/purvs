84256a3d69c8825d5d3739c4cfb0796914612a30
Guest User <Guest@wg401-g-03-06.mddn.vuw.ac.nz>
2018-03-13 14:53:51 +1300

    Still didnt work

--- clock1.js
+++ clock1.js
@@ -1,45 +1,45 @@
-const CANVAS_WIDTH = 960;
-const CANVAS_HEIGHT = 500;
+// const CANVAS_WIDTH = 960;
+// const CANVAS_HEIGHT = 500;
 
-function setup () {
+// function setup () {
 
-  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  main_canvas.parent('canvasContainer');
+//   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+//   main_canvas.parent('canvasContainer');
 
   
-}
+// }
 
 
-function draw () {
-  background(0); 
-  noStroke();
+// function draw () {
+//   background(0); 
+//   noStroke();
 
-  //Number Four
-  fill(255);
-  rect(160, 280, 200, 40);
-  rect(160,240,40,40);
-  rect(200,200,40,40);
-  rect(240, 160, 40,40);
-  rect(280,120,40,280);
+//   //Number Four
+//   fill(255);
+//   rect(160, 280, 200, 40);
+//   rect(160,240,40,40);
+//   rect(200,200,40,40);
+//   rect(240, 160, 40,40);
+//   rect(280,120,40,280);
 
-  //Number Seven
-  fill(150);
-  rect(550, 250, 10, 50);
-  rect(560,240,30,10);
-  rect(560,300,30,10);
-  rect(590, 250,10,50);
-  rect(610,240,60,10);
-  rect(660,250,10,20);
-  rect(650,270,10,10);
-  rect(640,280,10,30);
-}
+//   //Number Seven
+//   fill(150);
+//   rect(550, 250, 10, 50);
+//   rect(560,240,30,10);
+//   rect(560,300,30,10);
+//   rect(590, 250,10,50);
+//   rect(610,240,60,10);
+//   rect(660,250,10,20);
+//   rect(650,270,10,10);
+//   rect(640,280,10,30);
+// }
 
 
-function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
-  else if (key == '@') {
-    saveBlocksImages(true);
-  }
-}
+// function keyTyped() {
+//   if (key == '!') {
+//     saveBlocksImages();
+//   }
+//   else if (key == '@') {
+//     saveBlocksImages(true);
+//   }
+// }
