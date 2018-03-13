450496802c4df4383d628b9c72dffb7bd241028c
Sean Herbert <herbersean@wg401-d-06-06.mddn.vuw.ac.nz>
2018-03-13 15:07:01 +1300

    added number class.
    number class has arrays for each number 0-9 and can draw each on its own. Will look into storing a current number and update function to increment the current number so that the clock can start keeping track of time.

--- clock1.js
+++ clock1.js
@@ -1,25 +1,159 @@
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
+let num;
 
 function setup () {
-  // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
 
-  // you can optionally add your own code here if you also have setup code
+  num = new Number();
 }
 
-// Update this function to draw you own maeda clock
 function draw () {
-  background(204); // light gray background
-  strokeWeight(8); // Stroke weight to 8 pixels
-  ellipse(480, 250, 190, 190);
-  // The rectangle draws on top of the ellipse
-  // because it comes after in the code
-  rect(500, 280, 260, 20);
+  background(0);
+  num.draw(9);
 }
 
-// do not alter or remove this function
+class Number{
+	constructor(){
+		this.zero = [
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0]];
+
+		this.one = [
+		[0,0,1,0,0],
+		[0,1,1,0,0],
+		[1,0,1,0,0],
+		[0,0,1,0,0],
+		[0,0,1,0,0],
+		[0,0,1,0,0],
+		[1,1,1,1,1]];
+
+		this.two = [
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[0,0,0,0,1],
+		[0,0,0,1,0],
+		[0,0,1,0,0],
+		[0,1,0,0,0],
+		[1,1,1,1,1]];
+
+		this.three = [
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[0,0,0,0,1],
+		[0,0,1,1,0],
+		[0,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0]];
+
+		this.four = [
+		[0,0,0,1,0],
+		[0,0,1,1,0],
+		[0,1,0,1,0],
+		[1,0,0,1,0],
+		[1,1,1,1,1],
+		[0,0,0,1,0],
+		[0,0,0,1,0]];
+
+		this.five = [
+		[1,1,1,1,1],
+		[1,0,0,0,0],
+		[1,1,1,1,0],
+		[0,0,0,0,1],
+		[0,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0]];
+
+		this.six = [
+		[0,1,1,1,0],
+		[1,0,0,0,0],
+		[1,1,1,1,0],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0]];
+
+		this.seven = [
+		[1,1,1,1,1],
+		[0,0,0,0,1],
+		[0,0,0,0,1],
+		[0,0,0,1,0],
+		[0,0,1,0,0],
+		[0,0,1,0,0],
+		[0,0,1,0,0]];
+
+		this.eight = [
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,0]];
+
+		this.nine = [
+		[0,1,1,1,0],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[1,0,0,0,1],
+		[0,1,1,1,1],
+		[0,0,0,0,1],
+		[0,1,1,1,0]];
+	}
+
+	draw(num){
+		var array;
+		switch(num){
+			case 0:
+				array = this.zero;
+				break;
+			case 1:
+				array = this.one;
+				break;
+			case 2:
+				array = this.two;
+				break;
+			case 3:
+				array = this.three;
+				break;
+			case 4:
+				array = this.four;
+				break;
+			case 5:
+				array = this.five;
+				break;
+			case 6:
+				array = this.six;
+				break;
+			case 7:
+				array = this.seven;
+				break;
+			case 8:
+				array = this.eight;
+				break;
+			case 9:	
+				array = this.nine;
+				break;
+		}
+
+		noStroke();
+
+		for(var i = 0; i < 7; i++){
+			for(var j = 0; j < 5; j++){
+				if(array[i][j]){
+					rect(j*20, i*20, 20,20);
+				}
+			}
+		}
+	}
+}
+
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
