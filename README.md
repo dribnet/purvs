61af6522e461189b3934e96a8ccc2a472ad04a9c
Brad Yip <yipbrad@myvuw.ac.nz>
2018-03-13 15:46:02 +1300

    did more progress on my project.

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 ## PS1 MDDN 242 2017
 
-### John Maeda's Clock
+### Bubble Clock
 
-I recreated one of John Maedas clock where the numbers would scale different sizes in and out whilst changing opacity however i tried adding a twist in which the colours would change on the RBG scale over a sin wave.
-However i couldnt exactly get it to work but never the less it still changes colours.
+Here is the initial start to my idea. I've managed to get the bobbing and shadows done and i just started to do the text. I still need to add more animated special interactions for when the digits change etc.
 
-Replace the "Outrageous clock" title above with the name of
-your clock. Also update both images keeping their original sizes:
+
+Both images keeping their original sizes:
 preview.jpg (960x500) and thumbnail.png (230x120).
--- clock.js
+++ clock.js
@@ -1,7 +1,14 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
+
+let diameter = 40;
+let	shadowWidth = 35;
+let shadowHeight = 8;
 function draw_clock(obj) {
+
+
+
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
     //    obj.minutes goes from 0-59
@@ -11,4 +18,39 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+	let hours = obj.hours
+	let minutes = obj.minutes
+	let seconds = obj.seconds
+	let millis = obj.millis
+	background(200);
+
+	//Converting the timing objects into strings that can be separated
+	hours = hours.toString();	
+	minutes = minutes.toString();
+	seconds = seconds.toString();
+	console.log(hours[1]);	// Logging 2nd number in hours string. 
+
+    text("Hour: "   + hours[0], 10, 22);
+    text(hours[1], 50, 22);
+    text("Minute: " + minutes, 10, 42);
+    text("Second: " + seconds, 10, 62);
+    text("Millis: " + millis, 10, 82);
+
+	
+	
+
+
+
+
+	//circles
+	for(let i =0;i<8;i++){
+		let mil = millis+(1000/8)*i;	
+		let sinWave = sin(map(mil,0,1000,0,TWO_PI));
+		let sinWave2 = sin(map(mil,0,1000,0,TWO_PI)+PI/2.0);
+		let y = map(sinWave, 0, 1, 200, 203);
+		let bob = map(sinWave, 0, 1, 1, 3);
+		ellipse(235+60*i,y,diameter);	// circles		
+		ellipse(235+60*i, 240, shadowWidth + bob, shadowHeight + bob)	//circle shadows.
+	}
+
 }
