8e33de02db9f20a8300e55bad76f4f6eb65faba5
Guest User <Guest@wg401-b-04-06.mddn.vuw.ac.nz>
2018-03-13 15:42:57 +1300

    Progress done in class

--- clock.js
+++ clock.js
@@ -1,6 +1,19 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 500;
+
+
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  main_canvas.parent('canvasContainer');
+
+  // you can optionally add your own code here if you also have setup code
+}
+
 function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
@@ -11,4 +24,50 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+    let hours = obj.hours;
+    let minutes = obj.minutes;
+    let seconds = obj.seconds;
+    
+
+
+
+
+
+angleMode(RADIANS);
+
+    noFill();
+    strokeWeight(2);
+    background(0,125,200); 
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/5, 175, 175) 
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 125, 125)
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT/6, 80, 80)
+
+    	push(); 
+        
+    	 fill(255);
+    	 strokeWeight(1);
+    	 ellipse(CANVAS_WIDTH/2 - 88, CANVAS_HEIGHT  - CANVAS_HEIGHT/5, 15, 15)
+    	 ellipse(CANVAS_WIDTH/2 - 62.5, CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 15, 15)
+    	 ellipse(CANVAS_WIDTH/2 - 40, CANVAS_HEIGHT/6, 15, 15)
+    	pop();
+ 	
+
+ pmam();
 }
+rectMode(CENTER);
+function pmam(){
+    strokeWeight(5);
+rect(CANVAS_WIDTH - CANVAS_WIDTH/4 + 50, CANVAS_HEIGHT/2.2, 100, 75);  
+rect(CANVAS_WIDTH - CANVAS_WIDTH/1.2 + 10, CANVAS_HEIGHT/2.2, 100, 75);  
+text('word', 10, 30);
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
+
