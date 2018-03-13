b5f72d4076b0b8afa3a703937208efee4745bab0
taylormatt7 <taylormatt7@myvuw.ac.nz>
2018-03-13 15:32:54 +1300

    Finishing up for the session. Placement seems sorted.
    Next is adding the particle generation and drawing.

--- flaming_clock.js
+++ flaming_clock.js
@@ -10,8 +10,8 @@ class FlamingClock {
 
 	constructor() {
 		// helper variables
-		this.particleGap = width/50;
-		this.characterGap = width/8;
+		this.particleGap = width/60;
+		this.characterGap = width/10;
 		this.textColour = color(0xFF, 0x00, 0xFF);
 		this.backgroundColour  = color(0x30);
 
@@ -125,16 +125,16 @@ class FlamingClock {
 		for (var i = 0; i < 35; i++) {
 			if (this.numbers[characterIndex][i] === 1) {
 				fill(this.textColour);
-				rect(x + (i % 5) * this.particleGap,
-					(y + floor(i / 5)) * this.particleGap,
-					this.particleGap, this.particleGap);
+				ellipse(x + (i % 5) * this.particleGap,
+					y + floor(i / 5) * this.particleGap,
+					this.particleGap);
 			}
 		}
 	}
 
 	update(hour, minute, second, milli, alarm) {
-		var xOffset = 0;
-		var yOffset = 12;
+		var xOffset = this.particleGap+this.characterGap;
+		var yOffset = height/2;
 		background(0x00);
 		//hours
 		this.createCharacterParticles(floor(hour / 10), xOffset, yOffset);
