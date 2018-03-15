02dfe4f884b48d2814c546f1db2a7c111d034708
Jiayao Song <songjiay@wg401-b-02-06.mddn.vuw.ac.nz>
2018-03-16 12:38:42 +1300

    Add my first sketch clock

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 ## PS1 MDDN 242 2017
 
-### Chemical liquid clock
+### My clock
 
-The DiLou (water clock), which took one second for three drops, was a great convenience for people's lives at the time. However, this timing method is less accurate. Inspired by this timing method of ancient time instruments, I had the idea of a "chemical liquid clock". This clock comprises two main beakers. The left beaker represents the number of hours and the right beaker represents the minutes. The 'AM' or 'PM' will be displayed in an small area in the lower right corner. Every minute, a drop will drip over the beaker on the right side, at the same time the liquid in the beaker will increase by one layer. When the time changes from 'AM' to 'PM', the liquid in the beaker changes color.
+From last class, I started to code my clock.
 
-[water clock](https://en.wikipedia.org/wiki/Water_clock)
 
-[100 words](https://wordcounter.net/).
+
--- clock.js
+++ clock.js
@@ -1,14 +1,159 @@
-/*
- * us p5.js to draw a clock on a 960x500 canvas
- */ 
-function draw_clock(obj) {
-    // draw your own clock here based on the values of obj:
-    //    obj.hours goes from 0-23
-    //    obj.minutes goes from 0-59
-    //    obj.seconds goes from 0-59
-    //    obj.millis goes from 0-1000
-    //    obj.seconds_until_alarm is:
-    //        < 0 if no alarm is set
-    //        = 0 if the alarm is currently going off
-    //        > 0 --> the number of seconds until alarm should go off
+var canvasWidth = 960;
+var canvasHeight = 500;
+var main, d1, d2;
+var h, m, s, mil;
+var s;
+var s2 = 0;
+var i = 0;
+var time = 0;
+var flag = 0;
+var flag2 = 0;
+var secondsWithFraction, secondBarWidthSmooth;
+var millisBarWidth;
+var four_hours;
+
+function preload() {
+  main = loadImage("sketch_clock_main.png");
+  d1 = loadImage("leftD.png");
+  d2 = loadImage("RightD.png");
+  four_hours = loadImage("Left4hour.png");
+}
+
+function setup() {
+  /*
+  var main_canvas = createCanvas(canvasWidth, canvasHeight);
+  main_canvas.parent('canvasContainer');*/
+  createCanvas(960, 500);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+  image(main, width / 2, height / 2, 960, 500);
+}
+
+function ampm(xm, ym) {
+  noStroke();
+  if (h < 12) {
+    fill(255);
+  } else {
+    fill(255);
+  }
+  if (h < 12) {
+    //'A'
+    rect(-15 + xm, -25 + ym, 5, 5);
+    rect(-20 + xm, -20 + ym, 5, 5);
+    rect(-10 + xm, -20 + ym, 5, 5);
+    rect(-25 + xm, -15 + ym, 5, 30);
+    rect(-5 + xm, -15 + ym, 5, 30);
+    rect(-20 + xm, -5 + ym, 15, 5);
+  } else {
+    //'P'
+    rect(-25 + xm, -25 + ym, 5, 40);
+    rect(-20 + xm, -25 + ym, 15, 5);
+    rect(-20 + xm, -5 + ym, 15, 5);
+    rect(-5 + xm, -20 + ym, 5, 15);
+  }
+
+  //'M'
+  rect(5 + xm, -25 + ym, 5, 40);
+  rect(10 + xm, -20 + ym, 5, 5);
+  rect(15 + xm, -15 + ym, 5, 10);
+  rect(20 + xm, -20 + ym, 5, 5);
+  rect(25 + xm, -25 + ym, 5, 40);
+  //time 200
+
+  //use time function
+  textSize(60);
+  if (h <= 12) {
+    if (h < 10) {
+      text('0' + h, -180 + xm, 120 + ym);
+    } else {
+      text(h, -180 + xm, 120 + ym);
+    }
+  } else {
+    if ((h - 12) < 10) {
+      text('0' + (h - 12), -200 + xm, 120 + ym);
+    } else {
+      text((h - 12), -200 + xm, 120 + ym);
+    }
+  }
+
+
+  textSize(60);
+  if (m < 10) {
+    text('0' + m, -70 + xm, 120 + ym);
+  } else {
+    text(m, -70 + xm, 120 + ym);
+  }
+  strokeWeight(3);
+
+  if (h < 12) {
+    stroke(255);
+    //stroke(241, 122, 40);//am line orange
+  } else {
+    stroke(222, 10, 27); //pm line red
+  }
+
+  h = hour();
+  m = minute();
+
+}
+
+function draw(obj) {
+  h = hour();
+  m = minute();
+  s = second();
+  //let millis = obj.millis;
+  if ((time === 0) || (s === 0)) {
+    s2 = second();
+  }
+  background(200);
+  
+  
+  
+  push();
+  translate(570, 330);
+  if ((s - s2) == 1) {
+    i = i + 1;
+    s2 = s;
+    flag = 1;
+    flag2 = 1;
+    textSize(18);
+    text(s, 50, 50);
+    ampm(0, 0);
+    flag = 0;
+  } else {
+    textSize(18);
+    text(s, 50, 50);
+    ampm(0, 0);
+  }
+  time = time + 1;
+  pop();
+  
+  /*
+  secondsWithFraction   = s + (mil / 1000.0);
+  text('secondsWithFraction =' + secondsWithFraction , 50, 100);
+  secondBarWidthSmooth  = map(secondsWithFraction, 0, 60, 100, 250);
+  */
+
+  millisBarWidth = map(mil, 0, 1000, 100, 250);
+  dropRight(millisBarWidth);
+  text('yD=' + millisBarWidth, 50, 100);
+  
+  text("Millis: " + mil, 10, 82);
+  
+  image(four_hours, width / 2, height / 2, 960, 500);
+  image(main, width / 2, height / 2, 960, 500);
+  
+
+}
+function dropRight(yD){
+  //var opacity = map(mil, 100, 270, 255, 0);
+  image(d2, 567, yD, 12, 21);//567,100
+  //image(d2, 567, 270, 12, 21);//567, 270
+}
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  } else if (key == '@') {
+    saveBlocksImages(true);
+  }
 }
