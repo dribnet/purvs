03b0e0e977e8b692e2a4133ae143763eebd9ab0b
Josh Felizardo <josh.feliz03@gmail.com>
2018-03-16 12:43:27 +1300

    START OF LESSON PROGRESS

--- clock1.js
+++ clock1.js
@@ -1,30 +1,25 @@
-//CLOCK 7
-var fr = 30;
-var xpos = 145;
-var speed = 6;
-
 function setup() {
+
 createCanvas(960,500);
-frameRate(fr)
+angleMode(DEGREES);
+
 }
 
 function draw() {
 
-  translate(250,250);
-  background(0)
-  fill(0,0,250)
-  textSize(110)
-  textAlign(CENTER);
-  h = hour()
-  mi = minute()
-  se = second()
-  
-  xpos = xpos + speed;
-  if((xpos > 450) || (xpos < 0))
-  {
-    speed = speed * -1;
-  }
-     
-  text(h + ':' + mi, xpos, 250)
-  
+//background gradient *PLACEHOLDER COLOR*
+background(114, 136, 224);
+
+//setting up the variables
+let hr = hour();
+let mn = minute();
+let sc = second();
+
+        //drawing the sc arc
+            strokeWeight (3);
+            filter ( BLUR, 10); //blur to make it glow
+            stroke(255);
+            noFill();
+            let end = map(sc, 0, 10, 0, 360);
+            arc(960/2, 250, 300, 300,0,end);
 }
