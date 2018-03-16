e5894d00bac927f2704755b6973c36856251c019
Josh Felizardo <josh.feliz03@gmail.com>
2018-03-16 14:44:55 +1300

    Progress update, clock now showing seconds and minutes.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## PS1 MDDN 242 2017
 
-### John Maeda Clock #7
+### Turrell Inspired Clock
 
-This is my take on the swivel clock. This was a technically difficult task for me as I have not coded for the past 2 years but with the guidance of Tom and my peers I managed to produce an animated clock. I will iterate this into a better aesthetic, though not necessarily animated.
+From the last post I've changed the arc to be mapped with minutes and an ellipse that grows into 10 seconds. 
 
--- clock1.js
+++ clock1.js
@@ -2,10 +2,14 @@ function setup() {
 
 createCanvas(960,500);
 angleMode(DEGREES);
-
+smooth();
+filter( BLUR, 100);
 }
 
 function draw() {
+
+translate(960/2,250);
+rotate(-90);
 
 //background gradient *PLACEHOLDER COLOR*
 background(114, 136, 224);
@@ -14,12 +18,26 @@ background(114, 136, 224);
 let hr = hour();
 let mn = minute();
 let sc = second();
+let ml = millis();
 
-        //drawing the sc arc
+        //drawing the minute arc
             strokeWeight (3);
-            filter ( BLUR, 10); //blur to make it glow
             stroke(255);
             noFill();
-            let end = map(sc, 0, 10, 0, 360);
-            arc(960/2, 250, 300, 300,0,end);
+            let end = map(mn, 0, 60, 0, 360);
+            arc(0, 0, 300, 300,0,end);
+         
+         //drawing the minute arc's GLOW
+         filter(BLUR, 2);
+            strokeWeight (1);
+            stroke(255);
+            noFill();
+            let end1 = map(mn, 0, 60, 0, 360);
+            arc(0, 0, 300, 300,0,end1);
+        
+        //drawing the seconds ellipse
+       fill(255);
+        let end2 = map(sc, 0, 5, 0, 10);
+        ellipse(0,0,end2,end2, 0);
+
 }
