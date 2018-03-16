d811a3f3e432cbe6c1fd726b0befb3cf2085a427
Guest User <Guest@wg401-a-04-10.mddn.vuw.ac.nz>
2018-03-16 15:41:03 +1300

    Updated project files.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-ps1 Clock (WIP)
+Did much development today in class. Realized that the AM & PM squares were redundant, because we can know whether it's AM or PM by looking outside.
+
+Thought about and integrated stylistic choices. 
+
+Implemented the working clock text, but not sure how I will create an interesting moving visual for the clock, since the original idea of rotating circles around the bigger circles was too complicated.
+
+Made a colored stroke around each circle and mapped the stroke's rgb to hours,minutes,seconds. 
+ 
+
 
--- clock.js
+++ clock.js
@@ -3,8 +3,11 @@
  */ 
 const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 500;
+let secondsx = CANVAS_WIDTH/2 
+let minutesx = CANVAS_WIDTH/2
+let hoursx = CANVAS_WIDTH/2
 
-
+let x = 0
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -27,40 +30,86 @@ function draw_clock(obj) {
     let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
+    let millis = obj.millis;
+    let seconds_until_alarm = obj.seconds_until_alarm;
+
     
-
-
+       let hourBarWidth   = map(hours, 0, 23, 0, width);
+       let minuteBarWidth = map(minutes, 0, 59, 0, width);
+       let secondBarWidth = map(seconds, 0, 59, 0, width);
+       let millisBarWidth = map(millis, 0, 1000, 0, width);
 
 
 
 angleMode(RADIANS);
 
-    noFill();
+    fill(75)
     strokeWeight(2);
-    background(0,125,200); 
-    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/5, 175, 175) 
-    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 125, 125)
-    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT/6, 80, 80)
-
-    	push(); 
+    background(0,100,200); 
+    
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/5, 175, 175) //bottom ring
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 125, 125) //middle ring
+    ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT/6, 80, 80) //top ring
+      push();
+      strokeWeight(2);
+      stroke(hourBarWidth,minuteBarWidth,secondBarWidth)
+        ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/5, 175-5, 175-5) //bottom circle
+        ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 125-5, 125-5) //middle circle
+        ellipse(CANVAS_WIDTH/2,CANVAS_HEIGHT/6, 80-5, 80-5) //top circle
+      pop();
+    	
         
-    	 fill(255);
-    	 strokeWeight(1);
-    	 ellipse(CANVAS_WIDTH/2 - 88, CANVAS_HEIGHT  - CANVAS_HEIGHT/5, 15, 15)
-    	 ellipse(CANVAS_WIDTH/2 - 62.5, CANVAS_HEIGHT - CANVAS_HEIGHT/1.8, 15, 15)
-    	 ellipse(CANVAS_WIDTH/2 - 40, CANVAS_HEIGHT/6, 15, 15)
-    	pop();
+// push();
+      
+//        fill(255);
+//        strokeWeight(1);
+//        push();
+         
+//     	   ellipse(secondsx, CANVAS_HEIGHT  - CANVAS_HEIGHT/5 - 85, 15, 15) //seconds
+//        pop();
+//     	 ellipse(minutesx, CANVAS_HEIGHT - CANVAS_HEIGHT/1.8 - 60, 15, 15) //minutes
+//     	 ellipse(hoursx, CANVAS_HEIGHT/6 - 37, 15, 15) //hours
+// pop()
  	
+    fill(50); // dark grey
+    
+    push();
+      fill(255);
+      textSize(75)
+      text(""   + seconds, CANVAS_WIDTH/2 - 40, CANVAS_HEIGHT  - CANVAS_HEIGHT/5 + 25);
+    pop();
+    push();
+      fill(255);
+      textSize(40)
+      text("" + minutes, CANVAS_WIDTH/2 - 43 + 20, CANVAS_HEIGHT - CANVAS_HEIGHT/1.8 + 15);
+    pop();
+    push();
+      fill(255);
+      textSize(25)
+      text("" + hours, CANVAS_WIDTH/2 - 15, CANVAS_HEIGHT/6 + 7);
+    pop();
+    // textSize(20);
+    // text("" + millis, CANVAS_WIDTH/2, CANVAS_HEIGHT  - CANVAS_HEIGHT/5 + 55);
 
- pmam();
+              // secondsx = secondsx + obj.seconds
+              // if(secondsx > CANVAS_WIDTH/2 + 84){
+              //   secondsx = CANVAS_WIDTH/2 - 88
+              // }
+
+ // pmam();
 }
-rectMode(CENTER);
-function pmam(){
-    strokeWeight(5);
-rect(CANVAS_WIDTH - CANVAS_WIDTH/4 + 50, CANVAS_HEIGHT/2.2, 100, 75);  
-rect(CANVAS_WIDTH - CANVAS_WIDTH/1.2 + 10, CANVAS_HEIGHT/2.2, 100, 75);  
-text('word', 10, 30);
-}
+
+// function pmam(){
+//     strokeWeight(5);
+// rect(CANVAS_WIDTH - CANVAS_WIDTH/4 - 10, CANVAS_HEIGHT/2, 100, 75);  
+// rect(CANVAS_WIDTH - CANVAS_WIDTH/1.2 - 10, CANVAS_HEIGHT/2, 100, 75);  
+//   push();
+//     stroke(0,175,100)
+//     strokeWeight(2);
+//     rect(CANVAS_WIDTH - CANVAS_WIDTH/4 - 10+5.2, CANVAS_HEIGHT/2+5.2, 100-10, 75-10);  //lightblue
+//     rect(CANVAS_WIDTH - CANVAS_WIDTH/1.2 - 10+5.2, CANVAS_HEIGHT/2+5.2, 100-10, 75-10);  //lightblue
+//   pop();
+// }
 
 function keyTyped() {
   if (key == '!') {
@@ -70,4 +119,6 @@ function keyTyped() {
     saveBlocksImages(true);
   }
 }
+
+
 
