b5b4c5c79c5754d3df2614dcf8b03232b8429d83
Aliz Ritthisri <eiizteayzhy.px@gmail.com>
2018-03-16 15:41:28 +1300

    developing my ideal clock

--- clock.js
+++ clock.js
@@ -1,14 +1,107 @@
-/*
- * us p5.js to draw a clock on a 960x500 canvas
- */ 
-function draw_clock(obj) {
-    // draw your own clock here based on the values of obj:
-    //    obj.hours goes from 0-23
-    //    obj.minutes goes from 0-59
-    //    obj.seconds goes from 0-59
-    //    obj.millis goes from 0-1000
-    //    obj.seconds_until_alarm is:
-    //        < 0 if no alarm is set
-    //        = 0 if the alarm is currently going off
-    //        > 0 --> the number of seconds until alarm should go off
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 500;
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  main_canvas.parent('canvasContainer');
+  angleMode(DEGREES);
+
+
+  // you can optionally add your own code here if you also have setup code
+}
+
+// Update this function to draw you own maeda clock
+function draw () {
+
+  background(204); // light gray background
+  
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+  let ml = millis();
+  let d = day();
+  let m = month();
+  let y = year();
+
+  //second
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 300, 300);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end = map(sc, 0, 60, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 300, 300, 0, end);
+ 
+  // push();
+  // rotate(end);
+  stroke(255);
+  line(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, CANVAS_WIDTH/2 + 50, CANVAS_HEIGHT/2);
+  // pop();
+
+  //minute
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 280, 280);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end1 = map(mn, 0, 60, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 280, 280, 0, end1);
+
+  //hour
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 260, 260);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end2 = map(hr % 12, 0, 12, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 260, 260, 0, end2);
+
+  //millis
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 320, 320);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end3 = map(ml, 0, 1000, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 320, 320, 0, end3);
+
+  //day
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 240, 240);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end4 = map(d, 0, 1000, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 240, 240, 0, end4);
+
+  //month
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 220, 220);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end5 = map(m, 0, 1000, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 220, 220, 0, end5);
+
+  //year
+  strokeWeight(8);
+  stroke(255);
+  noFill();
+  ellipse(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 200, 200);
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end6 = map(y, 0, 1000, 0, 360);
+  arc(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 200, 200, 0, end6);
+
+  // fill(255);
+  // noStroke();
+  // text(hr + ':' + mn + ':' + sc, 100, 200);
+
 }
--- clock1.js
+++ clock1.js
@@ -5,35 +5,18 @@ function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
-  pixelDensity(1);
 
   // you can optionally add your own code here if you also have setup code
 }
 
 // Update this function to draw you own maeda clock
 function draw () {
-
   background(204); // light gray background
-  // loadPixels();
-  // for (var i = 0; i < height; i++){
-  //   for (var j = 0; j < width; j++){
-  //     var index = (i + j * 1000)*4;
-  //     pixels[index+0] = 120;
-  //     pixels[index+1] = i;
-  //     pixels[index+2] = 255;
-  //     pixels[index+3] = j;
-  //   }
-  // }
-
-  // updatePixels();
-
-  strokeWeight(1); // Stroke weight to 8 pixels
-  //oldclocks
-  ellipse(width/2, height/2, 300, 300);
-
+  strokeWeight(8); // Stroke weight to 8 pixels
+  ellipse(480, 250, 190, 190);
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
-  //rect(500, 280, 260, 20);
+  rect(500, 280, 260, 20);
 }
 
 // do not alter or remove this function
