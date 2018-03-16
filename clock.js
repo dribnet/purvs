7e32dee7d838bf8f042df99b46f2ea11b26f0d56
Guest User <Guest@wg401-i-02-07.mddn.vuw.ac.nz>
2018-03-16 15:10:15 +1300

    Working clock

--- clock.js
+++ clock.js
@@ -5,6 +5,8 @@
  const CANVAS_WIDTH = 960;
  const CANVAS_HEIGHT = 500;
  const BS = 10;
+ const posX = 2; 
+ const posY = 2.5;
 
  function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
@@ -17,11 +19,11 @@
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
 
-    let hours = obj.hours;
-    let minutes = obj.minutes;
-    let seconds = obj.seconds;
-    let millis = obj.millis;
-    let pulse = 2;
+    var hours = obj.hours;
+    var minutes = obj.minutes;
+    var seconds = obj.seconds;
+    var millis = obj.millis;
+    var pulse = 2;
 
 
 
@@ -29,7 +31,7 @@
     let r= map(minutes, 0, 59, 0, 255);
     let g= map(seconds, 0, 59, 0, 255);
     let b = map(millis, 0, 1000, 0, 255);
-    //noStroke();
+    noStroke();
 
     for (var i = 0; i < (CANVAS_WIDTH/BS); i++) {
     	for (var j = 0; j < (CANVAS_HEIGHT/BS); j++) {
@@ -37,44 +39,102 @@
     		rect(i*BS,j*BS,BS,BS);
     	}
     }
-    draw_Four();
-    draw_Zero();
-    draw_Eight();
-    draw_Three();
-    draw_One();
-    draw_Two();
-    draw_Five();
-    draw_Six();
-    draw_Seven();
-    draw_Nine();
+    fill(0);
+    draw_Colon();    
+    draw_Time(seconds,minutes,hours);
+  }
+
+  function draw_Time(seconds,minutes,hours){
+
+    var tenSeconds = seconds.toString()[0];
+    Draw_number(tenSeconds,2);
+    var singleSeconds = seconds.toString()[1];
+    Draw_number(singleSeconds,3);
+    var tenMinutes = minutes.toString()[0];
+    Draw_number(tenMinutes,1);
+    var singleMinutes= minutes.toString()[1];
+    Draw_number(singleMinutes,0);
+  }
+
+function Draw_number(time,pos){
+    if(time==1){
+      draw_One(pos)
+    }
+    else if(time==2){
+      draw_Two(pos)
+    }
+    else if(time==3){
+      draw_Three(pos)
+    }
+    else if(time==4){
+      draw_Four(pos)
+    }
+    else if(time==5){
+      draw_Five(pos)
+    }
+    else if(time==6){
+      draw_Six(pos)
+    }
+    else if(time==7){
+      draw_Seven(pos)
+    }
+    else if(time==8){
+      draw_Eight(pos)
+    }
+    else if(time==9){
+      draw_Nine(pos)
+    }
+    else {
+      draw_Zero(pos)
+    }
+
 }
 
+  
 
+  function myPosition(space){
+    if (space==0){
+      MyPos = 14*BS*-1;
+    }
+    if (space==1){
+      MyPos = 7*BS*-1;
+    }
+    if (space==2){
+      MyPos = 3*BS;
+    }
+    if (space==3){
+      MyPos = 10*BS;
+    }
+    else{
+      //MyPos = 0;
+    }
+    return(MyPos);
+  }
 
-function draw_Zero() {
+  function draw_Colon(space){
+    let posNo = 1;
+    let xRel = CANVAS_WIDTH/posX+posNo-BS;
+    let yRel = CANVAS_HEIGHT/posY;
 
-  	fill(0);
-  	let posX = 6; 
-  	let posY = 2.5;
-  	let posNo = 8*BS;
-  	let xRel = CANVAS_WIDTH/posX+posNo;
-  	let yRel = CANVAS_HEIGHT/posY;
+    rect(xRel,yRel+BS,BS,BS);
+    rect(xRel,yRel+BS*4,BS,BS);
+  }
 
-
-	//zero
-	rect(xRel,yRel,BS*3 ,BS);
-	rect(xRel,yRel+(BS*6),BS*3 ,BS);
-	rect(xRel-BS,yRel+BS,BS,BS*5);
-	rect(xRel+(BS*3),yRel+BS,BS,BS*5);
+  function draw_Zero(space) {
+  //the number 0
+  let posNo = myPosition(space);
+  let xRel = CANVAS_WIDTH/posX+posNo;
+  let yRel = CANVAS_HEIGHT/posY;
+  rect(xRel,yRel,BS*3 ,BS);
+  rect(xRel,yRel+(BS*6),BS*3 ,BS);
+  rect(xRel-BS,yRel+BS,BS,BS*5);
+  rect(xRel+(BS*3),yRel+BS,BS,BS*5);
 
 }
 
-function draw_One() {
-  //the number 4
-  fill(0);
-  let posX = 6; 
-  let posY = 2.5;
-  let posNo = 25*BS;
+function draw_One(space) {
+  //the number 1
+  let posNo = myPosition(space);
   let xRel = CANVAS_WIDTH/posX+posNo;
   let yRel = CANVAS_HEIGHT/posY;
 
@@ -84,13 +144,10 @@ function draw_One() {
   rect(xRel+(BS*loopNo-BS)-BS, yRel+BS, BS, BS);
 }
 
-function draw_Two() {
-  //the number 4
-  fill(0);
-  let posX = 6; 
-  let posY = 2.5;
-  let posNo = 31*BS;
-  let xRel = CANVAS_WIDTH/posX+posNo;
+function draw_Two(space) {
+  //the number 2
+  let posNo = myPosition(space);
+  let xRel = CANVAS_WIDTH/posX+posNo-BS;
   let yRel = CANVAS_HEIGHT/posY;
 
   let loopNo = 4;
@@ -107,13 +164,9 @@ function draw_Two() {
   }
 }
 
-  function draw_Three(){
-    //eight
-
-    fill(0);
-    let posX = 6; 
-    let posY = 2.5;
-    let posNo = 22*BS;
+function draw_Three(space){
+    //The number 3
+    let posNo = myPosition(space);
     let xRel = CANVAS_WIDTH/posX+posNo-BS;
     let yRel = CANVAS_HEIGHT/posY;
 
@@ -128,13 +181,11 @@ function draw_Two() {
 
   }
 
-function draw_Four() {
+  function draw_Four(space) {
   //the number 4
-  fill(0);
-  let posX = 6; 
-  let posY = 2.5;
-  let posNo = 0*BS;
-  let xRel = CANVAS_WIDTH/posX+posNo;
+
+  let posNo = myPosition(space);
+  let xRel = CANVAS_WIDTH/posX+posNo-BS;
   let yRel = CANVAS_HEIGHT/posY;
 
   let loopNo = 4;
@@ -147,49 +198,42 @@ function draw_Four() {
   }
 }
 
-function draw_Five(){
+function draw_Five(space){
 
-    fill(0);
-    let posX = 6; 
-    let posY = 2.5;
-    let posNo = 38*BS;
-    let xRel = CANVAS_WIDTH/posX+posNo;
-    let yRel = CANVAS_HEIGHT/posY;
-
-    rect(xRel,yRel,BS*5,BS);
-    rect(xRel,yRel+BS,BS,BS);
-    rect(xRel,yRel+(BS*2), BS*4, BS); 
-    rect(xRel+BS,yRel+(BS*6),BS*3 ,BS);
-    rect(xRel,yRel+(BS*5),BS,BS);
-    rect(xRel+(BS*4),yRel+(BS*3),BS,BS*3);
-
-
-}
-
-function draw_Six(){
-
-    fill(0);
-    let posX = 6; 
-    let posY = 2.5;
-    let posNo = 45*BS;
-    let xRel = CANVAS_WIDTH/posX+posNo;
-    let yRel = CANVAS_HEIGHT/posY;
-
-    rect(xRel+BS,yRel,BS*3,BS);
-    rect(xRel,yRel+BS,BS,BS*5);
-    rect(xRel,yRel+(BS*2), BS*4, BS); 
-    rect(xRel+BS,yRel+(BS*6),BS*3 ,BS);
-    rect(xRel+(BS*4),yRel+(BS*3),BS,BS*3);
-
-
-}
-
-function draw_Seven(){
-  fill(0);
-  let posX = 6; 
-  let posY = 2.5;
-  let posNo = 52*BS;
+  let posNo = myPosition(space);
   let xRel = CANVAS_WIDTH/posX+posNo;
+  let yRel = CANVAS_HEIGHT/posY;
+
+  rect(xRel,yRel,BS*5,BS);
+  rect(xRel,yRel+BS,BS,BS);
+  rect(xRel,yRel+(BS*2), BS*4, BS); 
+  rect(xRel+BS,yRel+(BS*6),BS*3 ,BS);
+  rect(xRel,yRel+(BS*5),BS,BS);
+  rect(xRel+(BS*4),yRel+(BS*3),BS,BS*3);
+
+
+}
+
+function draw_Six(space){
+
+
+  let posNo = myPosition(space);
+  let xRel = CANVAS_WIDTH/posX+posNo-BS;
+  let yRel = CANVAS_HEIGHT/posY;
+
+  rect(xRel+BS,yRel,BS*3,BS);
+  rect(xRel,yRel+BS,BS,BS*5);
+  rect(xRel,yRel+(BS*2), BS*4, BS); 
+  rect(xRel+BS,yRel+(BS*6),BS*3 ,BS);
+  rect(xRel+(BS*4),yRel+(BS*3),BS,BS*3);
+
+
+}
+
+function draw_Seven(space){
+
+  let posNo = myPosition(space);
+  let xRel = CANVAS_WIDTH/posX+posNo-BS;
   let yRel = CANVAS_HEIGHT/posY;
 
   rect(xRel,yRel,BS*5,BS);
@@ -203,13 +247,9 @@ function draw_Seven(){
   }
 }
 
-function draw_Eight(){
+function draw_Eight(space){
 	  //eight
-
-	  fill(0);
-	  let posX = 6; 
-	  let posY = 2.5;
-	  let posNo = 15*BS;
+    let posNo = myPosition(space);  
 	  let xRel = CANVAS_WIDTH/posX+posNo;
 	  let yRel = CANVAS_HEIGHT/posY;
 
@@ -222,15 +262,12 @@ function draw_Eight(){
 	  rect(xRel-BS+(BS*4),yRel+BS,BS,BS*2);
 	  rect(xRel-BS+(BS*4),yRel+(BS*4),BS,BS*2);
 
-}
+  }
 
-function draw_Nine(){
+  function draw_Nine(space){
 
-    fill(0);
-    let posX = 6; 
-    let posY = 2.5;
-    let posNo = 59*BS;
-    let xRel = CANVAS_WIDTH/posX+posNo;
+    let posNo = myPosition(space);
+    let xRel = CANVAS_WIDTH/posX+posNo-BS;
     let yRel = CANVAS_HEIGHT/posY;
 
     rect(xRel+BS,yRel,BS*3,BS);
@@ -239,7 +276,7 @@ function draw_Nine(){
     rect(xRel+BS,yRel+(BS*6),BS*3 ,BS);
     rect(xRel+(BS*4),yRel+(BS),BS,BS*5);
 
-}  
+  }  
 
 
 
