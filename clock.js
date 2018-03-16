7e74ba257863734ad295981f12b20dffa7a68d1b
Guest User <Guest@wg401-h-05-07.mddn.vuw.ac.nz>
2018-03-16 14:48:34 +1300

    updating animation

--- clock.js
+++ clock.js
@@ -16,18 +16,21 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
-let hours = obj.hours;
+	let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
 
     let secondSun = map(seconds, 0, 59, 0, width);
     let minuteRect = map(minutes, 0, 59, height, 0);
+    let hourR = map(hours, 0, 23, 30, 137);
+    let hourG = map(hours, 0, 23, 40, 182);
+    let hourB = map(hours, 0, 23, 50, 255);
 
-    background(130, 206, 250);
+    background(hourR, hourG, hourB);
     noStroke();
     fill(255, 255, 153);
-    ellipse(secondSun, 250, 100, 100);
-    fill(10, 60);
+    ellipse(secondSun, 250, 150, 150);
+    fill(200, 50);
     rect(0, minuteRect, width, height);
 }
