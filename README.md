bd0998a2a7d5f7033128e9e7e1def642d3555c39
Karu Skippr <karuskip@gmail.com>
2018-03-20 22:32:27 +1300

    I have updated some stuff like alarm

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Updated clock with ellipses and current time
 
-I have synced/mapped the time with the elipses that go along the screen with the current time. I have also synced the background to the current time (Red during the day, dark blue/black during the night). Now I have to experiment with changes to produce something more abstract.
+I have improved the day/night cycles to include a mapped sun and moon go across the screen. I have also made the clock more aesthetically pleasing, a few more touch ups and I should be complete. I haven kept the movement on the hours/minutes and seconds the same to portray the movement of a grandfather clock.
--- clock.js
+++ clock.js
@@ -8,30 +8,49 @@ var pulse;
 var r = 0;
 var sunrise;
 
+
 function draw_clock(obj) {
     let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
     let seconds_until_alarm = obj.seconds_until_alarm;
-
+	
     for(let i = 0; i<5;i++){
         
-        background(r,0,0);
+    background(r,0,0);
         noStroke();
         hourLoci = map(hours,0,23,25,CANVAS_WIDTH-25);   
         minLoci = map(minutes,0,59,25,CANVAS_WIDTH-25);   
         secLoci = map(seconds,0,59,25,CANVAS_WIDTH-25);  
         milLoci = map(millis,0,1000,25,CANVAS_WIDTH-25);    
+		
+		
         r = map(hourLoci,0,960,255,0); //changes from red to blue (day/night)
         pulse = map(milLoci/2,0,960,255,0);
-        sunrise  = map(hourLoci,0,500,255,0);
+        sunrise  = map(hourLoci,CANVAS_HEIGHT,0,200,0);
+		
+		
+		if(hours>17){
+			
+		fill(210,210,0); //make a yellow for day and blue for night
+    	ellipse(sunrise+15,120,50,50); //map it to go across screen and change colour 
+    	fill(r,0,0);
+    	ellipse(sunrise,110,50,50); //make a map and set the x value to it
+   		}
+   		else{
+		fill(204, 102, 0);
+    	ellipse(sunrise,120,60,60);
+    	fill(210,210,0);
+    	ellipse(sunrise,120,50,50);
+		
+			
+		}
+		
         if(minutes == 47){seconds_until_alarm = 0;}
         if(seconds_until_alarm == 0){background(pulse,50,pulse);} //when the alarms going off pulse background 
       
-
-    //note these are ordered like this on purpose to prevent overlapping issues
-
+    	//note these are ordered like this on purpose to prevent overlapping issues
 
         //pulse inner rects if the alarm goes off
         fill(255);
@@ -41,10 +60,6 @@ function draw_clock(obj) {
         else{fill(r,0,0);}
         rect(0,startY+58,CANVAS_WIDTH,10); 
        
-
-
-
-
         //seconds
         fill(255);
         rect(0,startY+112,CANVAS_WIDTH,25);
@@ -52,33 +67,56 @@ function draw_clock(obj) {
         if(seconds_until_alarm == 0){fill(pulse,50,pulse); }
         else{fill(r,0,0);}
         rect(0,startY+120,CANVAS_WIDTH,10); 
-        fill(255);
-        ellipse(secLoci,startY+126,50,50);
+		if(seconds_until_alarm !=0){
+        fill(r,0,0); //outta colour for circle
+        ellipse(secLoci,startY+126,60,60); //outta circle
+		}
+		fill(255); //inner circle
+		ellipse(secLoci,startY+126,50,50); //inner circle
         fill(0);
         if(seconds <10){text("0"+seconds,secLoci-7,startY+132);}
         else{text(seconds,secLoci-7,startY+132);}
 
         //minutes
+		if(seconds_until_alarm !=0){
+		fill(r,0,0); //outta colour for circle
+        ellipse(minLoci,startY+63,60,60); //outta circle
+		}
         fill(255);
         ellipse(minLoci,startY+63,50,50);
         fill(0);
         if(minutes<10)text("0"+minutes,minLoci-7,startY+69);
         else{text(minutes,minLoci-7,startY+69);}
+		
 
         //hours
-        fill(255);
+		
+       	fill(255);
         rect(0,237,CANVAS_WIDTH,25);
         rect(hourLoci-12,0,25,startY);
-        ellipse(hourLoci,startY,50,50);
+		if(seconds_until_alarm !=0){
+		fill(r,0,0);
+        ellipse(hourLoci,startY,60,60);
+		}
+		fill(255);
+		ellipse(hourLoci,startY,50,50);
+       
         fill(0);
         if(hours<10)text("0"+hours,hourLoci-6,startY+5);
         else{text(hours,hourLoci-5,startY+5);}
        
         //millis
+		
         fill(255);
         rect(0,startY+175,CANVAS_WIDTH,25);
+		if(seconds_until_alarm !=0){
+		fill(r,0,0);
+        ellipse(milLoci,startY+190,60,60);
+		}
         fill(255);
         ellipse(milLoci,startY+190,50,50);
+		
+		
         fill(0);
         if(millis<10)text("0"+millis,milLoci-7,startY+195);
         else{text(millis,milLoci-7,startY+195);}
@@ -107,26 +145,12 @@ function draw_clock(obj) {
     if(seconds <10){text("0"+seconds,625,150);}
     else{text(seconds,625,150);}
     textSize(12);
-    */
-   if(hours>19){
-    fill(210,210,0); //make a yellow for day and blue for night
-    ellipse(180,120,50,50); //map it to go across screen and change colour 
-    fill(r,0,0);
-    ellipse(160,110,50,50) //make a map and set the x value to it
-
-   }
-   else{
-    fill(255);
-    ellipse(180,120,50,50);
-
-   }
-   
-
     
-
-
-    }
-
+  
+    
+	
+*/
+	}
  
 }
 
