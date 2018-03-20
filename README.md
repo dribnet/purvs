616d438db658809f14cefc6f67ee1425818787f2
Eddie Smith <ejsmith9725@gmail.com>
2018-03-20 15:28:34 +1300

    Clock change to Vertical
    This is the newest version of my clock

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Clock Update
 
 
-I have updated the clock to now show the minutes like I had originally planned, the final output will be pretty abstract but it will show time passing which is what I was going for with my clock idea. I am still undecided on the hour hands so I just have a placeholder in for now which is the little horizontal dashes that follow the minute hand. For the alarm I think I will introduce color or maybe change the orientation of the lines. 
+I havve decided to iterate on my clock once again as I had an idea about how to intergrate the hours. The clock still keeps its abstract look but with now with the bars filling horizontally. The clock then fills across and down with the hour slowly filling the screen. I have also added in my alarm which flashes bars down to the current hour time I am unsure about what do for the build up as that is something I have to work on.
 
 
 
--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
- */ 
+ */
 function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
@@ -15,68 +15,53 @@ function draw_clock(obj) {
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
-
-    background(255);
+    let alarm = obj.seconds_until_alarm;
+    background(0);
     fill(0);
-    rect(0,0,960,500);
-    fill(28,100,100);
-
-    text("Hour: "   + hours, 10, 22);
-    text("Minute: " + minutes, 10, 42);
-    text("Second: " + seconds, 10, 62);
-    text("Millis: " + millis, 10, 82);
-
-    let hourBarPosition   = map(hours, 0, 23, 0, height-5);
-    let minuteBarPosition = map(minutes, 0, 59, 15, width-15);
-    let secondBarHeight = map(seconds, 0, 59, 5, height-5);
-    let millisBarHeight = map(millis, 0, 1000, 5, height-5);
-
-    noFill();
-	strokeWeight(2);
-	stroke(255);
-	quad(2,2,958,2,958,498,2,498)
-
-    
-
-
+    //text("Hour: "   + hours, 10, 22);
+    //text("Minute: " + minutes, 10, 42);
+    //text("Second: " + seconds, 10, 62);
+    //text("Millis: " + millis, 10, 82);
+    let hourBarPosition = map(hours, 0, 23, 10, height - 10);
+    let minuteBarPosition = map(minutes, 0, 59, 5, width - 10);
+    let secondBarWidth = map(seconds, 0, 59, 5, width - 5);
+    let millisBarHeight = map(millis, 0, 1000, 5, width - 5);
+    let alarmMilliesLine = map(millis, 0, 1000, 5, height - 5);
     noStroke();
-
-   
-
-
-    let secondsWithFraction   = seconds + (millis / 1000.0);
-    let secondBarWidthSmooth  = map(secondsWithFraction, 0, 60, 5, height-5);
-
-
+    //Smoothlines
+    let minutesWithFraction = minutes + (seconds / 60);
+    let minuteBarWidthSmooth = map(minutesWithFraction, 0, 60, 5, width - 10);
+    //hourLines
     fill(255);
-    rect(minuteBarPosition+5, 5, 2, secondBarWidthSmooth-5);
+    rect(5, hourBarPosition, minuteBarWidthSmooth, 2);
 
 
-//minute lines
-
-for (let i = 0; i < minutes; i++) {
-	let incMinutes = minuteBarPosition/minutes;
-    rect((incMinutes*i)+5, 5, 2, height-10);
-}
-//hour lines
-
-for (let i = 0; i < hours; i++) {
-	let incHours = hourBarPosition/hours;
-    rect(minuteBarPosition, (incHours*i)+5, 12, 2);
-}
-
-
-
-
-
-// do not alter or remove this function
-function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
-  else if (key == '@') {
-    saveBlocksImages(true);
-  }
-}
-
+    for (let i = 0; i < hours; i++) {
+        let incHours = hourBarPosition / hours;
+        let col = 20;
+        fill(col * i, 255, 255);
+        rect(5, (incHours * i) + 5, width - 10, 2);
+    }
+    if (alarm == 0) {
+        for (let i = 0; i < 100; i++) {
+            let incAlarm = millis / hourBarPosition;
+            let col = 5;
+            fill(col * i, 255, 255);
+            rect(5, (incAlarm * i) + 5, width - 10, 2);
+        }
+    } else {
+        let alarmMilliesLine = 0;
+    }
+    if (alarm > 0) {
+            fill()
+            rect(5, hourBarPosition, minuteBarWidthSmooth, 2);
+    }
+    // do not alter or remove this function
+    function keyTyped() {
+        if (key == '!') {
+            saveBlocksImages();
+        } else if (key == '@') {
+            saveBlocksImages(true);
+        }
+    }
 }
