f82a4637b35f904994b54d6a2485bce16265890a
BradYip <37090807+BradYip@users.noreply.github.com>
2018-03-22 00:53:58 +1300

    update
    i did more work on my project

--- README.md
+++ README.md
@@ -2,9 +2,5 @@
 
 ### Bubble Clock
 
-I've managed to give life to my idea and its starting to become what i had invisioned it to be. Theres still much more to be done like properly syncing the text and the circles as well as adding more animations and an alarm feature.
-
-I changed the colours of the shadows as i felt that having coloured shadows didnt really make sense.
-
-I also added rough animations for when the alarm is going off however they still need to be refined.
-
+My project is nearly completed. I managed to fix the jumping timings as well as fix the alarm going off code. Pretty happy with everything so far however i would like to change the font of the text which is proving quite difficult. I tried creating a local server through node but that didnt end well.
+ i could run it with my tutor to see if they have any idea.
--- clock.js
+++ clock.js
@@ -18,6 +18,14 @@ circleColours [5] = [244, 162, 97];
 circleColours [6] = [125,200,125];
 circleColours [7] = [251,195,202];
 
+let currentBall = 0;
+let jump = 0;
+let jumpcap = 120;
+let jumpspeed = 7;
+let isjumping = true;
+
+let count  = 0;
+let count2 = 0;
 
 function draw_clock(obj) {
 
@@ -32,7 +40,9 @@ function draw_clock(obj) {
     //        > 0 --> the number of seconds until alarm should go off
 
 	background(198,229,233); 
-	textSize(22);
+	textSize(24);
+
+
 
 	//Converting the integers into strings
 	let hours = obj.hours
@@ -55,7 +65,7 @@ function draw_clock(obj) {
     	timeStrings.push (hours[1]);
     }
 
-    timeStrings.push (": ");
+    timeStrings.push (":");
 
     if(minutes.length <= 1) {
     	timeStrings.push ("0");
@@ -65,7 +75,7 @@ function draw_clock(obj) {
     	timeStrings.push (minutes[1]);
     }
 
-    timeStrings.push (": ");
+    timeStrings.push (":");
 
     if(seconds.length <= 1){
     	timeStrings.push ("0");
@@ -79,34 +89,102 @@ function draw_clock(obj) {
 	for(let i =0;i<8;i++){
 		let mil = millis+(1000/8)*i;	
 		let sinWave = sin(map(mil,0,1000,0,TWO_PI));
+		let sinWave2 = sin(map(mil,0,1000,0,TWO_PI)); 
 		let y = map(sinWave, 0, 1, 200, 203);
 		let bob = map(sinWave, 0, 1, 1, 3);
 		noStroke();
 
+		//When the alarm is turned on
 		if(obj.seconds_until_alarm > 0 ){
+			fill(circleColours[i]);
+				if(currentBall == i){
+				//adding on extra y to sin wave
+				y = y - jump;
+					//ball starts jumpings
+					if(jump < jumpcap && isjumping){
+						jump = jump + jumpspeed;
 
-		}	
-		//circles
+						jumpspeed -= 0.2;
+					}
+						//when the ball is at the jump cap reset the isJumping boolean.
+					else if (jump >= jumpcap && isjumping){
+						isjumping = false;
+					}
+					// when its at the cap start falling.
+					else if(jump > 0 && !isjumping){
+						jump = jump - jumpspeed;
+
+						jumpspeed += 0.2;
+					}
+					// when its back to its start position set jump is true for the next ball
+					else if(!isjumping && jump <= 0){
+						isjumping = true;
+						if(currentBall < 7){
+							currentBall ++;
+							jumpspeed = 7;
+							}	//incrementing the current ball
+						else{
+							currentBall = 0;
+							jumpspeed = 7;}	//resetting the current ball	
+					}
+				ellipse(190 + circle_spacing * i, y + 30, circle_diameter);
+				//alter the shadow for when it drumps
+				fill(179,209,230);
+				ellipse(190 + circle_spacing  *i, 270, shadowWidth + (jumpspeed*1) - 9, shadowHeight + (jumpspeed*1)- 3);	//circle shadows.
+				}
+		else{
+			//draws the circles that arnt jumping
+			ellipse(190 + circle_spacing * i, y + 30, circle_diameter);
+			fill(179,209,230);
+			ellipse(190 + circle_spacing  *i, 270, shadowWidth + bob, shadowHeight + bob)	//circle shadows.
+			}
+		//drawing the text on the circle
+		fill(255);
+		//adding offsets to the Colons as they are offcenter
+		if(i == 2 || i == 5){
+			text(timeStrings[i], 186 + circle_spacing * i, y + 37 );
+		
+		}
+		else{
+		text(timeStrings[i], 183 + circle_spacing * i, y + 37 );
+		}
+	}
+		//When alarm is off
+		else if (obj.seconds_until_alarm < 0 || obj.seconds_until_alarm == 0){
 		fill(circleColours[i]);
 		ellipse(190 + circle_spacing * i, y + 30, circle_diameter);	// circles	
 		fill(179,209,230);
 		ellipse(190 + circle_spacing  *i, 270, shadowWidth + bob, shadowHeight + bob)	//circle shadows.
 		fill(255);
+		if(i == 2 || i == 5){
+		text(timeStrings[i], 186 + circle_spacing * i, y + 37 );
+		
+		}
+		else{
 		text(timeStrings[i], 183 + circle_spacing * i, y + 37 );
+		}
 
+		//alarm going off code.
+			if(obj.seconds_until_alarm == 0){
+				if(count < 1800){			
+					noFill();
+					strokeWeight(40);
+					stroke(251,195,202);
+					ellipse(750,232,count*1.6,count*1.6);
+					if(count > 200){					
+					stroke(242, 95, 92);
+					ellipse(190, 232, count2*1.6, count2*1.6);
+					count2++;
+					}
+					count++;
+				}else{
+					count = 0;
+					count2 = 0;
+				}
+
+
+			}	
+		}	
 	}
-
-
-//alarm going off code.
-	if(obj.seconds_until_alarm > 0){
-		noFill();
-		strokeWeight(40);
-		stroke(251,195,202);
-		ellipse(750,232,millis,millis);
-
-		stroke(242, 95, 92);
-		ellipse(190, 232, millis , millis);
-	}
-
 }
 
