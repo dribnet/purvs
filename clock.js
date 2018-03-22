18557461598d61b2013f10752d036bc4771f7efc
Guest User <Guest@wg401-i-03-07.mddn.vuw.ac.nz>
2018-03-22 17:37:02 +1300

    Finalised code and clock with alarm

--- clock.js
+++ clock.js
@@ -1,16 +1,10 @@
-/*
- * us p5.js to draw a clock on a 960x500 canvas
- */ 
+//Orbit clock
+//Made by Matt Cross 2018 for MDDN242
+
 function draw_clock(obj) {
-    // draw your own clock here based on the values of obj:
-    //    obj.hours goes from 0-23
-    //    obj.minutes goes from 0-59
-    //    obj.seconds goes from 0-59
-    //    obj.millis goes from 0-1000
-    //    obj.seconds_until_alarm is:
-    //        < 0 if no alarm is set
-    //        = 0 if the alarm is currently going off
-    //        > 0 --> the number of seconds until alarm should go off
+  
+
+//initial variables 
     let alarm = obj.seconds_until_alarm
     let hours = obj.hours;
     let minutes = obj.minutes;
@@ -20,33 +14,29 @@ function draw_clock(obj) {
     let sec = map(seconds, 0,60,0,360);
     let min = map(minutes, 60,0,360,0);
     let hou = map(hours, 0,12,0,360);
-    let secondswithfraction = seconds + (millis/1000);
-    let smooth = map(secondswithfraction,0,59,0,59);
 
+//Background aspects
     background(0);
     translate(480,250);
     rotate(180);
     angleMode(DEGREES);
 
+	Earth();
+	AlarmPress();
 
-
-// push();
-
-Earth();
-AlarmPress();
-
-// pop();
-
+//The rotation of the middle Islands
     push();
     rotate(hou);
     Islands();
     pop();
 
+//The rotation of the Moon
     push();
     rotate(sec);
     Second();
     pop();
 
+//The code for the stars to twinkle
     Twinkle();
     fill(255,255,0);
     ellipse(-400,-150,12*scale);
@@ -56,24 +46,19 @@ AlarmPress();
     fill(0,0,255);
     ellipse(350,-160,15*scale);
 
-push();
-translate(490,-170);
-rotate(180);
-    fill(100);
-    text("Hours: " + hours ,20, 22); 
-    text("Minutes: " + minutes ,20, 37); 
-    text("Seconds: " + seconds ,20, 52); 
-    text("alarm: " + alarm, 20, 66);
-pop();
+//Text to show time for positioning
+// push();
+// translate(490,-170);
+// rotate(180);
+//     fill(50);
+//     text("Hours: " + hours ,20, 22); 
+//     text("Minutes: " + minutes ,20, 37); 
+//     text("Seconds: " + seconds ,20, 52); 
+//     text("alarm: " + alarm, 20, 66);
+// pop();
 
-
+//Creating the pulsating effect for the stars
     function Twinkle() {
-        //if milli <= 500
-        // scale = map(mii, 0,500, small, large)
-        //if milli > 500
-        //scale = map(milli, 501, 999, large, small)
-
-    //draw things
 
     if(millis<=500){
     scale = map(millis, 0, 500, 0.8,1);       
@@ -85,18 +70,7 @@ pop();
 
     }
 
-    function Twinkle_V2() {
-
-    if(millis<=500){
-    scale = map(millis, 0, 500, 0.8,1);       
-    }
-    else if(millis>500){
-    scale = map(millis, 501, 999, 1,0.8);
-
-    }
-
-    }
-
+//Creates the main body of the Earth
     function Earth(){
 
    noStroke();
@@ -112,6 +86,8 @@ pop();
 
     }
 
+
+//Creates the islands to rotate on the Earth
     function Islands() {
 
 fill(0,200,0);
@@ -160,22 +136,21 @@ fill(255);
 
     }
 
-
+//Function for the moon
     function Second() {
 
 fill(200);
 ellipse(0,235,25,25);
-// ellipse(225,0,10,10);
-// ellipse(-225,0,10,10);
-// ellipse(0,-225,10,10);
+
 
 
 }
 
+//Creates the alarm and all effects for it
 function AlarmPress() {
 
     if (alarm == 0){
-    Twinkle_V2();
+    Twinkle();
        fill(0,0,150);
         ellipse(0,0,445*scale);
 
