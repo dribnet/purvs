7a79783ce706e2652c7ce24c90ab82d6b67117be
Sarah Phillips <phillisara4@myvuw.ac.nz>
2018-03-23 09:40:05 +1300

    added progress photo

--- clock1.js
+++ clock1.js
@@ -5,44 +5,228 @@ function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
+  
+  
 
 }
 
 // Update this function to draw you own maeda clock
-function draw () {
+function draw() {
+
+//Parameters:
+//obj.hours    Integer from 0 to 23 representing the hour of the day
+//obj.minutes  Integer from 0 to 59 representing the hour of the day
+//obj.seconds  Integer from 0 to 59 representing the hour of the day
+//obj.millis   Integer from 0 to 999 representing the milliseconds that have passed since the reported second
+//obj.seconds_until_alarm
+             //Float indicating the state of the alarm. The value will be:
+              //< 0 if the alarm is not set
+              //= 0 if the alarm is going off
+              //> 0 if the alarm is set. the float value then represents the number of seconds until the alarm goes off  background(20);
+  angleMode(DEGREES);
+  translate(CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
   background(20);
-  strokeWeight(2); 
+  rotate(-90);
+
 
 
   let hr = hour();
   let min = minute();
   let sec = second();
+  let mil = millis()
 
-  
+  let x = 1;
 
 
 
-//centre clock
- fill(250);
- stroke(0);
- ellipse(465, 250, 300, 300);
- //child birth
- ellipse(650, 250, 20, 20);
+ //seconds
+ noFill();
+ strokeWeight(4);
+ stroke(255);
+ let end = map(sec, 0, 60, 0, 360);
+ arc(0, 0, 490, 490, 0, end);
+
+
+ //minutes
+ noFill();
+ stroke(200);
+ let end2 = map(min, 0, 60, 0, 360);
+ arc(0, 0, 340, 340, 0, end2);
+
+
+ //hours
+ noFill();
+ stroke(150);
+ let end3 = map(hr%12, 0, 12, 0, 360);
+ arc(0, 0, 200, 200, 0, end3);
+
+
+ let angle = 0
+ 
+ //babies born
+ noFill();
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ ellipse(60, 0, 35, 35);
+ push();
+ let end4 = map(mil, 0, 14, 0, 360);
+ translate(60, 0);
+  rotate(90);
+ fill(255, 100, 150);
+ noStroke();
+ textSize(10);
+ text('Births', -12, 30);
+ noFill();
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ rotate(end4);
+ line(0, 0, 13, 0);
+ pop();
+
  //marriges
- ellipse(677.5, 250, 25, 25);
- //deaths
- ellipse(710, 250, 30, 30);
 
  noFill();
- stroke(232, 12, 122);
- ellipse(465, 250, 370, 370);
- ellipse(465, 250, 425, 425);
- ellipse(465, 250, 490, 490);
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ ellipse(60, 0, 35, 35);
+ push();
+ let end5 = map(mil, 0, 75, 0, 360);
+ translate(-40, 40);
+ textSize(10);
+ rotate(90);
+ fill(255, 100, 150);
+ noStroke();
+ text('Marriages', -20, -20);
+ noFill();
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ ellipse(0, 0, 35, 35);
+ rotate(end5);
+ line(0, 0, 13, 0);
+ pop();
+
+ //elderly
+ noFill();
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ ellipse(60, 0, 35, 35);
+ push();
+ let end6 = map(sec, 0, 16.7, 0, 360);
+ translate(-40, -40);
+ rotate(90);
+ fill(255, 100, 150);
+ noStroke();
+ textSize(10);
+ text('Elderly Deaths', -35, -20);
+ noFill();
+ stroke(255, 100, 150);
+ strokeWeight(1);
+ ellipse(0, 0, 35, 35);
+ rotate(end6);
+ line(0, 0, 13, 0);
+ pop();
+
+ //numbers
+
 
  strokeWeight(1);
- text(hr + ':' + min + ':' + sec, 445, 250);
+ textSize(32);
+ stroke(255);
+ rotate(90);
+ text('XII', -20, -200);
 
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('I', -20, -200);
 
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('II', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('III', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('IV', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('V', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('VI', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('VII', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('VIII', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('IX', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('X', -20, -200);
+
+ strokeWeight(1);
+ textSize(32);
+ stroke(255);
+ rotate(30);
+ text('XI', -20, -200);
+
+ let alarm = obj.seconds_until_alarm;
+   if (alarm == 0) {
+    if (second % 2 == 0) {
+    background(0,0,100);      
+    }
+    else {
+      background(100,100,0);      
+    }
+  }
+  else {
+    background(50);
+  }   
+  noStroke();
+  // is alarm going off in next 15 seconds
+  if (alarm > 0) {
+    fill(100);
+    rect(width-50, height-50, 40, 40);
+    if (alarm < 15.0) {
+      var box_w = map(alarm, 0, 15, width, 0);
+      var box_h = map(alarm, 0, 15, height, 0);
+      rect(width/2-box_w/2, height/2-box_h/2, box_w, box_h);
+    }
+  }
+  fill(255);
+
+ 
 
   // The rectangle draws on top of the ellipse
   // because it comes after in the code
