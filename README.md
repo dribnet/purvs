7225bbb1b25d667e5196aaa756983831acc280b1
Richie Prak <papaprak@gmail.com>
2018-03-22 22:15:31 +1300

    Final

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 ### Outrageous clock
 
-I am trying to figure out how to make the sun go in a circle and increase and decrease the acceleration. 
-
-The hour will represent the sun, moon, and change colours of the background. eg. when its night its black when its day its blue.
-
-Minutes will be birds.
-Seconds maybe people.
-Millis, I have no clue.
+Works like a generic clock, twist is has the shape of a bird and an ellipse hovering around it. The middle ellipse being its eye, and the hands being the eyelashes. The bottom purple thing is the next and the beak is the beak which I supposed to change color.
 
 
-When its night the sun will change into the moon
-When its day it will change into the sun.
+I have completely changed my original idea which was the sun going around the Earth because I found it to complicated. So I redeveloped and came up with a new concept which I liked and made the coding easier. 
+
+I came up with two concepts but the one I decided to select was the one that looks like a bird. The idea is simple, it runs like a simple generic clock but just looks like a bird or chicken. The ellipse (which is the millis) going around it is just adding something different. The beak was supposed to change color after the alarm but unfortunatly I didn't know how to make it work so I ended up avoiding making it. 
+
+I went with snowy like colors because I thought it just looked good.
+
+It took me a while to understand the whole map function and things for a while but I ended up understanding how to code it in the last week because I went with a new idea. Overall I ended up understand a lot more of the functions after completeting the clock.
+
+
--- clock.js
+++ clock.js
@@ -1,6 +1,5 @@
-var sun =0;
-var x = 0;
-var y = 0;
+
+var angle = 0;
 /*
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
@@ -14,63 +13,107 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+    //let hourBarWidth   = map(hours, 0, 50, 0, width);
+    //let minuteBarWidth = map(minutes, 0, 59, 0, width);
+    //let secondBarWidth = map(seconds, 0, 59, 0, width);
+    //let millisBarWidth = map(millis, 0, 1000, 0, width);
    
     let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
-
-    background(203, 246, 255);
+    let alarm = obj.seconds_until_alarm;
     
-    fill(000, 146, 76) 
-    strokeWeight(0);
-    text("Hour: "   + hours, 10, 22);
-    text("Minute: " + minutes, 10, 42);
-    text("Second: " + seconds, 10, 62);
-    text("Millis: " + millis, 10, 82);
+    background(203, 246, 255);
 
-    //stick that holds the sun
-    fill(84, 26, 4); 
-    rect(480, 250, 5, 200);
+    line(480, 250, 480, 500);
+    //line(480, 250, 100, 250);
+    line(480, 250, 700, 700)
+    fill(253, 246, 214);
 
+    stroke(226, 242, 242);
+    triangle(200, 350, 480, 350, 500, 200);
+
+    noFill();
+    strokeWeight(10);
+    stroke(253, 246, 214);
+    ellipse(480, 250, 500, 500);
+    
+    fill(253, 246, 214);
+    strokeWeight(10);
+    stroke(226, 242, 242);
+    ellipse(480, 250, 350, 350);
+
+    fill(207, 246, 254);
+    ellipse(480, 250, 50, 50);
+
+    noFill();
+    stroke(253, 246, 214);
+    ellipse(480, 250, 450, 450);
+
+    noFill();
+    strokeWeight(3);
+    stroke(212, 159, 218);
+    ellipse(480, 250, 480, 480);
 
     fill(000, 146, 76);
-    stroke(102, 204, 102);
-    strokeWeight(10);
-    ellipse(480, 500, 250, 250);
+  
+    let mill = map(millis, 0, 1000, 0, 360);
+    let sec = map(seconds, 0, 59, 0, 360);
+    let min = map(minutes, 0, 59, 0, 360);
+    let hr = map(hours % 12 , 0, 12, 0, 360);
 
-    let hourBarWidth   = map(hours, 0, 23, 0, width);
-    let minuteBarWidth = map(minutes, 0, 59, 0, width);
-    let secondBarWidth = map(seconds, 0, 59, 0, width);
-    let millisBarWidth = map(millis, 0, 1000, 0, width);
-
+    translate(480, 250);
+    angleMode(DEGREES);
+    rotate(-180);
+    
+    push();
+    fill(207, 246, 254);
+    rotate(sec);
     noStroke();
+    rect(0, 0, 5, 100);
+    pop();
     
-    sun = 480;
+    push();
+    rotate(mill);
+    fill(255);
+    ellipse(240, 0, 30, 30);
+    pop();
+    
+    push();
+    rotate(min);
+    fill(207, 246, 254);
+    strokeWeight(0);
+    rect(0, 0, 10, 200);
+    pop();
 
-    fill(255, 214, 98);
-    stroke(233, 162, 59);
-    strokeWeight(10);
-    ellipse(x, y, 120, 120);
+    push();
+    rotate(hr)
+    fill(207, 246, 254);
+    strokeWeight();
+    rect(0, 0, 10, 150);
+    pop();
 
-    x = x + 1;
-    y = y + 1;
+    if(alarm = 0){
+        fill(246, 177, 91);
+        triangle(200, 350, 480, 350, 500, 200);
+      
 
-    if(x > 600){
-        x = 0;
-        if(y > 600){
-            y = 0;
+
     }
-}
 
 }
 
-    //function sun(x, y){
-    
-    //fill(255, 214, 98);
-    //stroke(233, 162, 59);
-    //strokeWeight(10);
-    //ellipse(480, 250, 120, 120);
+
+    //fill(000, 146, 76) 
+    //strokeWeight(0);
+    //text("Hour: "   + hours, 10, 22);
+    //text("Minute: " + minutes, 10, 42);
+    //text("Second: " + seconds, 10, 62);
+    //text("Millis: " + millis, 10, 82);
+   
+
+  
 
 
 
