45aa6b2e1e30887a980da5eb609962eddb88a442
Yvonne Zou <pandora1995@hotmail.com>
2018-03-23 10:49:23 +1300

    add new file Final

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## PS1 MDDN 242 2017
 
-### As time gose by-clock
-My alarm is easy and it is just for the test, I think I will not use such alarm in my final work.
-I really change a lot from my concept but I still use the same movement.
+### Clock-'Diary of a seed'
+My final work is about a 'seed' story which happened in a crystal ball. 
+I choose this idea because of most of time when people talk about time they will be all really worried and anxious, so I give up my first concept about create a time-keeper or the real clock, for instead of them I create a kind of toy which can make people feel relaxed and peaceful when they look them.
+The seed is like a cartoon character and its eyes folowed millis, the rain also follow millis and the bud of seed grow up for each second.
+I use the flower to express minutes and hours, flower will bloom in 60 minutes and the color change from red to yellow in 24 hours.
--- clock.js
+++ clock.js
@@ -1,6 +1,10 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
- */ 
+ */
+function setup(){
+ 	createCanvas(960,500);
+    angleMode(DEGREES);
+ } 
 function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
@@ -11,37 +15,32 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
-
-    
-
     let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
-    let alarm = obj.seconds_until_alarm;
 
-    if (alarm < 0) {
-        background(221,160,221); 
-    }
-    else if (alarm > 0) {
-        background(255, 105, 180);        
-    }
-    else if (alarm == 0){
-        background(255,248,220);
-    }
-    
-    
-    strokeWeight(1); //  beige
-    fill(0,0,139);
-    text("Hour: "   + hours, 10, 142);
-    text("Minute: " + minutes, 10, 162);
-    text("Second: " + seconds, 10, 182);
-    text("Millis: " + millis, 10, 202);
+    background(221,160,221);
 
-    let hourBarHeight = map(hours, 0, 23, 100, 400);
+    let hourBarColor = map(hours, 0, 23, 0, 51);
+    let hourBarColor2 = map(hours, 0, 23, 0, 255);
+    let hourBarSize = map(hours, 0, 23, 5, 15);
+
+    
+    let minuteBarSize1 = map(minutes, 0, 59, 0, 15);
+    let minuteBarSize2 = map(minutes, 0, 59, 0, 20);
+    
+    let secondBarHeight = map(seconds, 0, 59, 220, 150);
+    let secondBarSize = map(seconds, 0, 59, 0, 15);
+
+
+    
     let millisBarHeight = map(millis, 0, 1000, 90,350);
     let millisBarHeight2 = map(millis, 0, 1000, 100,210);
+    let millisBarWidth = map(millis, 0, 1000, 377, 383);
+    let millisBarWidth2 = map(millis, 0, 1000, 437, 443);
 
+    //crystal ball
     strokeWeight(12);
     stroke(51);
     fill(222,184,135);
@@ -50,6 +49,70 @@ function draw_clock(obj) {
     stroke(51);
     fill(0,191,255);
     ellipse(480,210,400,400);
+
+    //grass-character-second
+    strokeWeight(6);
+    stroke(34,139,34);
+    noFill();
+    line(410,250,410,secondBarHeight);
+    noStroke();
+    fill(34,139,34)
+    arc(390,secondBarHeight,45,35,PI,0);
+    arc(430,secondBarHeight,45,35,PI,0);
+
+    //character
+    noStroke();
+    fill(222,184,135);
+    arc(400,230,25,25,PI,0);//left ear
+    arc(420,230,25,25,PI,0);//right ear
+
+    noStroke();
+    fill(222,184,135);
+    ellipse(410,285,120,120);//big body
+
+    noStroke();
+    fill(255,235,205);
+    ellipse(410,290,80,80);//small body
+
+    noStroke();
+    fill(51);
+    ellipse(380,270,35,35);//left eye
+    ellipse(440,270,35,35);//right eye
+
+    noStroke();
+    fill(255);
+    ellipse(millisBarWidth,275,20,20);//left eyeball
+    ellipse(millisBarWidth2,275,20,20);//right eyeball
+
+    strokeWeight(3);
+    stroke(51);
+    noFill();
+    arc(400,290,20,20,0,PI);
+    arc(420,290,20,20,0,PI);
+
+    //flower
+    strokeWeight(6);
+    stroke(34,139,34);
+    line(530,280,530,350);
+    noStroke();
+    fill(225,hourBarColor2,hourBarColor);
+    ellipse(530,245,minuteBarSize2,minuteBarSize2);//up
+    ellipse(544,251,minuteBarSize1,minuteBarSize1);
+    ellipse(510,265,minuteBarSize2,minuteBarSize2);//left
+    ellipse(516,251,minuteBarSize1,minuteBarSize1);
+    ellipse(550,265,minuteBarSize2,minuteBarSize2);//right
+    ellipse(516,279,minuteBarSize1,minuteBarSize1);
+    ellipse(530,285,minuteBarSize2,minuteBarSize2);//down
+    ellipse(544,279,minuteBarSize1,minuteBarSize1);
+    fill(255,255,0);
+    ellipse(530,265,30,30);//center
+    noStroke();
+    fill(255,99,41);
+    ellipse(530,265,hourBarSize,hourBarSize);
+    fill(34,139,34)
+    arc(515,305,30,30,0,PI);
+    arc(545,305,30,30,0,PI);
+
 
     //millis-rain
     noStroke()
@@ -65,4 +128,28 @@ function draw_clock(obj) {
     ellipse(440,90,95,75);
     ellipse(400,110,30,25);
     ellipse(540,110,30,25);
+
+    //colok
+    strokeWeight(1); //  beige
+    stroke(0,0,139);
+    fill(0,0,139);
+    textSize(35);
+    textFont('Comic Sans MS');
+    text(""   + hours, 420, 100);
+    text(":",470,95);
+    text("" + minutes, 490, 100);
+
+
+    //ground
+    noStroke();
+    fill(144,238,144);
+    arc(480,323,280,155,0,PI);
+
 }
+
+
+
+
+
+    
+    
