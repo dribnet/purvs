88f1b61b4ba0806ce2db6d106c24c91b5a97a622
HartleThom <gotjams1@gmail.com>
2018-03-23 12:52:09 +1300

    Updated READme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## PS1 MDDN 242 2017
 
-### Maeda's Clock
+### Overflow Clock
 
-This is a recreation of clock #8. However I added the dots of a digital clock and the ratio is at (from left to right) 1:0.5 or near 0.5. The clock doesnt move but if it were in constant motion the digital dots would still remain the same (30).
+Initially the concept of the clock was to 'overflow' the user with information whilst still presenting the current time differentiated. This was a play on the capacity of information as well as the mundane and simple design of a classic digital clock. This goes further overboard with the alarm. As the alarm goes off, every number is filled red until the minute itself is up. Although the current time may not be draw over the minute in which the alarm goes off, the indication of the current time is still present. 
+The design is not intended to be confusing but rather present the viewer with information that is not needed or not necessary and still upon a random chance (once per minute or every 60 seconds at least) draw the current time in a varrying color. 
--- clock1.js
+++ clock1.js
@@ -17,9 +17,11 @@ let y = 0; //defining y-axis value
 let fX = (CANVAS_WIDTH / 12); //the width of each number frame
 let fY = (fX * 1.33); //the height of each frame based on the ratio of width
 
+let cY = 0; //the constantly changing Y
+
 //Time modifiers
-let minute = 34; //the current minute
-let hour = 1; //the current hour
+let minute = 30; //the current minute
+let hour = 6; //the current hour
 
 //range modifier [minutes]
 let minUR = (minute / 60); //upper range modifier for the maximum limit
@@ -29,9 +31,10 @@ let mnuMod = minUR / 60; //the rate at which the minute Upper limit is increased
 let mnlMod = minLR / 60; //the rate at which the minute lower limit is increased
 
 //range modifier [hours]
+let hourLR = (hour / 12); //the lower range modifier
 let hourUR = (hour / 12); //the upper range modifier
 
-let hrMod = 0.0333; //the constant for lower rate
+let hrMod = hourLR / 30; //lower rate
 let hruMd = hourUR / 30; //rate for upper
 
 //The two values of minutes/hours
@@ -46,16 +49,38 @@ function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
-
   // you can optionally add your own code here if you also have setup code
 }
 
 //Draws the function(s)
-function draw () {
+function draw(){
   background(0); // black background
-  strokeWeight(2); // Stroke weight to 8 pixels
-  //rect(0, 600, 960, -600);
+  //let minute = obj.minutes;
+  //let hour = obj.hours;
   //dRow();
+  stroke(0);
+  zero(0,100,100,100);
+  one(100,100,100,100);
+  two(200, 100,100,100);
+  three(300, 100, 100, 100);
+  four(400, 100, 100, 100);
+  five(500, 100, 100, 100);
+  seven(700, 100, 100, 100);
+  nine(600, 100, 100, 100);
+}
+
+
+//generates a random value of time (rNum) and draws it
+function dRow() {
+	y = 0;
+	while(y < CANVAS_HEIGHT){
+		x = 0;
+		while(x < CANVAS_WIDTH){
+			rNum();
+			x = x+fX;
+		}
+		y = y + fY;
+	}
 }
 
 //Generates random value, r, and splits it into the first and second values
@@ -66,8 +91,9 @@ function nGen(){
 
 	//determines the value of the random number via the range
 	if(hFlip == true){r = random(0+(hrMod*hrRate), 13*(hruMd*hrRate));}
-	else{r = random(-1*(mnlMod*minRate), 60*(mnuMod*minRate));};
-	Math.floor(r); //rounds the float to the nearest int
+	else{r = random(-1+(mnlMod*minRate), 60-(mnuMod*minRate));};
+	r = Math.floor(r); //rounds the float to the nearest int
+	console.log(r);
 
 	//getting the two values of the current time in order to draw them correctly
 	fVal = Math.floor(r/10);
@@ -75,39 +101,89 @@ function nGen(){
 	else{sVal = r};
 }
 
-//generates a random value of time (rNum) and draws it
-function dRow() {
-	y = 0;
-	while(y < CANVAS_HEIGHT){
-		x = 0;
-		while(x < CANVAS_WIDTH){
-			rNum(x, y, fX, fY);
-			x = x+fX;
-		}
-		y = y + fY;
-	}
-}
-
+//checks rate and numbers 
 numCheck = function(){
 	if(minRate == 61){minRate = 0;};
 	if(hrRate == 31){hrRate = 0;};
 }
 
-function rNum(sX, sY, W, H){ //generates a random number within an hour
+function rNum(){ //generates a random number within an hour
 	numCheck(); //checks the rate
 	nGen(); //creates the random number to be generated based on the range of hours and minutes
 
 	//determines if the random number is the current time and changes the color
 	if(hFlip == true){
-		if(r == hour){background(255,0,0);console.log("hour");}
-		else{background(0);};
+		if(r == hour){fill(255,0,0);}
+		else{fill(255);};
 	}
 	else{
-		if(r == minute){background(255, 0, 0); console.log("minutes");}
-		else{background(0)};
+		if(r == minute){fill(255,0,0)}
+		else{fill(255);};
 	}
-
+	dDraw();
 	hFlip = !hFlip; //flips the hFlip value		
+}
+
+function aDraw(v, sX, sY, W, H){ //actually draws the values
+	if(v == 0){
+		zero(sX, sY, W, H);
+	}
+	else if(v == 1){
+		one(sX, sY, W, H);
+	}
+	else if(v == 2){
+		two(sX, sY, W, H);
+	}
+	else if(v == 3){
+		three(sX, sY, W, H);
+	}
+	else if(v == 4){
+		four(sX, sY, W, H);
+	}
+	else if(v == 5){
+		five(sX, sY, W, H);			
+	}
+	else if(v == 6){
+		six(sX, sY, W, H);
+	}
+	else if(v == 7){
+		seven(sX, sY, W, H);
+	}
+	else if(v == 8){
+		eight(sX, sY, W, H);
+	}
+	else if(v == 9){
+		nine(sX, sY, W, H);
+	};
+}
+
+function rDraw(val, sX, sY, W, H){
+	if(val == true){
+		aDraw(sX, sY, W/2, H);
+		aDraw(sX+(W/2), sY, W/2, H);
+	}
+	else{
+		aDraw(sX, sY, W, H);
+	}
+}
+
+function dDraw(){ //determines the draw and whether or not the frame is split
+	cY = map(minRate, 0-fY, CANVAS_HEIGHT+fY, 0, 100, false);
+	if(fVal > 1){ //the value is larger than 10 so the frame is split
+		rDraw(true, x, cY, fX, fY);
+	}
+	else{ //the value is smaller so full frame
+		rDraw(false, x, y, fX, fY);
+	};
+}
+
+let zero = function(sX, sY, W, H){
+	//horizontal
+	rect((sX + W*0.3), (sY - lBH*2), W*0.5, lBH)
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH)
+	//vertical
+	rect((sX + W*0.8), (sY - lBH*2), lBW, -H+(lBH*4));
+	rect((sX + (W * 0.2)), (sY - lBH*2), lBW, -H+(lBH*4));
 }
 
 let one = function(sX, sY, W, H){
@@ -144,23 +220,54 @@ let four = function(sX, sY, W, H){
 }
 
 let five = function(sX, sY, W, H){
-	rect((sX + W/2), (sY - lBH), lBW, -H+(lBH*2));
+	//horizontal
+	rect((sX + W*0.3), (sY - lBH*2), W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H*0.5) - lBH*0.5, W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH);
+	//vertical
+	rect((sX + W*0.8), (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
 }
 
 let six = function(sX, sY, W, H){
-	rect((sX + W/2), (sY - lBH), lBW, -H+(lBH*2));
+	//horizontal
+	rect((sX + W*0.3), (sY - lBH*2), W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H*0.5) - lBH*0.5, W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH);
+	//vertical
+	rect((sX + W*0.8), (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
 }
 
 let seven = function(sX, sY, W, H){
-	rect((sX + W/2), (sY - lBH), lBW, -H+(lBH*2));
+	//horizontal
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH)
+	//vertical
+	rect((sX + W*0.8), (sY - lBH), lBW, -H+(lBH*3));
 }
 
 let eight = function(sX, sY, W, H){
-	rect((sX + W/2), (sY - lBH), lBW, -H+(lBH*2));
+	//horizontal
+	rect((sX + W*0.3), (sY - lBH*2), W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H*0.5) - lBH*0.5, W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH);
+	//vertical
+	rect((sX + W*0.8), (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.8), (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
 }
 
 let nine = function(sX, sY, W, H){
-	rect((sX + W/2), (sY - lBH), lBW, -H+(lBH*2));
+	//horizontal
+	rect((sX + W*0.3), (sY - lBH*2), W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H*0.5) - lBH*0.5, W*0.5, lBH);
+	rect((sX + W*0.3), sY - (H-lBH), W*0.5, lBH);
+	//vertical
+	rect((sX + W*0.8), (sY - lBH*2), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.8), (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
+	rect((sX + W*0.3)-lBH, (sY - (H*0.5 + lBH*0.5)), lBW, -H+(lBH*7.5));
 }
 
 // do not alter or remove this function
