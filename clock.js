02d4b35211ff016af3df0def13aa8929ab82e07c
Jacob Ng <Jacobng.feifei@gmail.com>
2018-03-22 21:10:39 +1300

    yes

--- clock.js
+++ clock.js
@@ -9,68 +9,102 @@ function setup () {
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
 
-  // you can optionally add your own code here if you also have setup code
-  //current hour
-  initialH = hour();
-  //current minute
-  initialM = minute();
-  //current second
-  initialS = second();
+
   angleMode(DEGREES);
 }
 
 //draw clock
 function draw () {
- background(0)
-
-    translate(200, 200);
+  background(200);
+  noFill();
+  strokeWeight(2)
+  stroke(2)
+  ellipse(480, 250, 300, 300);
+  translate(480, 250);
   rotate(-90)
 
-  let h = hour();
+    let h = hour();
   let m = minute();
   let s = second();
-
+  let d = day();
+  let mi = millis(); 
 
   noFill()
     strokeWeight(40);
   stroke(74, 143, 229);
-  let end3 = map (h % 12, 0, 12, 0, 360);
-  arc (0, 0, 300, 300, 0, end3); 
+  let ha = map (h % 12, 0, 12, 0, 360);
+  arc (0, 0, 300, 300, 0, ha); 
 
   strokeWeight(20);
   stroke(75, 229, 98);
-  let end2 = map (m, 0, 60, 0, 360);
-  arc (0, 0, 300, 300, 0, end2); 
+  let ma = map (m, 0, 60, 0, 360);
+  arc (0, 0, 300, 300, 0, ma); 
 
   strokeWeight(8);
   stroke(234, 168, 60);
-  let end1 = map (s, 0, 60, 0, 360);
-  arc (0, 0, 300, 300, 0, end1); 
+  let sa = map (s, 0, 60, 0, 360);
+  arc (0, 0, 300, 300, 0, sa); 
+
+
+
+
+
+
 
   noStroke()
-  
+
     fill(74, 143, 229);
-  let end33 = map (h % 12, 0, 12, 0, 360);
-  arc (0, 0, 100, 100, 0, end33); 
-  
-    fill(75, 229, 98);
-  let end22 = map (m, 0, 60, 0, 360);
-  arc (0, 0, 80, 80, 0, end22); 
+  let ha1 = map (h % 12, 0, 12, 0, 360);
+  arc (0, 0, 100, 100, 0, ha1); 
+
+  fill(75, 229, 98);
+  let ma1 = map (m, 0, 60, 0, 360);
+  arc (0, 0, 80, 80, 0, ma1); 
 
   fill(234, 168, 60)
-  let end11 = map (s, 0, 60, 0, 360);
-  arc (0, 0, 50, 50, 0, end11); 
+    let sa1 = map (s, 0, 60, 0, 360);
+  arc (0, 0, 55, 55, 0, sa1); 
+
+
+
+
+  rotate(90)
+
+
+    push();
+  fill(255, 255, 255, 45);
+  textSize(205)
+    rotate(sa1)
+    rotate(-85)
+    text (s, -6, 7)
+
+    fill(255)
+    textSize(8)
+    text (mi, -6, 4)
+
+    pop();
+
+  push();
+  noStroke();
+  fill(255);
+
+
+  text ('DATE' + '  -  '+ d, -30, 90)
+
+    pop();
+
+  push();
+  noStroke()
+    fill(255);
+  textSize(20)
+
+    text (h + ':' + m, -25, -75)
+
+    pop();
     
-
-rotate(90)
-stroke(214, 0, 0)
-strokeWeight(1)
-   fill(240);
-   textSize(25)
-   text (h + ':' + m + ':' + s, -50, 5)
-
-
+    
 }
+
 
 
 // do not alter or remove this function
