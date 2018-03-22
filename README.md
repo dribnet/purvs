730b35d751ebbb6e6e65f1e63ba602425726831a
Guest User <Guest@vs-320-11.mddn.vuw.ac.nz>
2018-03-22 14:34:04 +1300

    Adding new updated files update of coconut tree first attempt.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## PS1 MDDN 242 2017
 
-### Maeda's 12 O'Clocks
+### Coconut Clock
 
-My first p5.js sketches where i recreated a still of one of his clocks. I didn't stray much from his original work i just adjusted the colour, adding red to make it stand out.
+My initial idea was to have a coconut tree as my clock. I orginally had planned to use a photoshop image to make the tops and leaves of the trees however as linking an image would be harder and we haven't been taught this so far i decided to simplify and have a more abstract approach, using the colours as opposed to coding the tree itself. I have used the bars so that its counting down time as opposed to adding time kind of looking more at how much time you have left in the day.
--- clock.js
+++ clock.js
@@ -11,4 +11,61 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+    let hours = obj.hours;
+    let minutes = obj.minutes;
+    let seconds = obj.seconds;
+    let millis = obj.millis;
+    background(204);
+
+    background(130, 197, 244); //  sky blue
+    fill(128,100,100); // dark grey
+    text("Hour: "   + hours, 10, 22);
+    text("Minute: " + minutes, 10, 42);
+    text("Second: " + seconds, 10, 62);
+   // text("Millis: " + millis, 10, 82);
+
+    let hourBarWidth   = map(hours, 0, 23, 0, height);
+    let minuteBarWidth = map(minutes, 0, 59, 0, height);
+    let secondBarWidth = map(seconds, 0, 59, 0, height);
+    //let millisBarWidth = map(millis, 0, 1000, 0, width);
+
+    //still rects
+    fill(17, 165, 78);
+    rect(800, 0, 50, 500);
+    //fill(154,205,50);
+    rect(450, 0, 50, 500);
+    //fill(46,139,87);
+    rect(100, 0, 50, 500);
+
+
+    noStroke();
+    //moving rects
+    fill(140,95,45);
+    rect(800, hourBarWidth, 50, 500);
+    fill(170,90,45);
+    rect(450, minuteBarWidth, 50, 500);
+    fill(195,135,63);
+    rect(100, secondBarWidth, 50, 500);
+    // fill(160)
+    // rect(0, 250, millisBarWidth, 50);
+
+fill(255, 250, 250);
+    ellipse(300, 200, 50, 50);
+    ellipse(650, 200, 50, 50);
+    ellipse(300, 300, 50, 50);
+    ellipse(650, 300, 50, 50);
+    
+
+
+    // Make a bar which *smoothly* interpolates across 1 minute.
+    // We calculate a version that goes from 0...60, 
+    // but with a fractional remainder:
+    let secondBarWidthChunky  = map(seconds, 0, 60, 0, height);
+    let secondsWithFraction   = seconds + (millis / 1000.0);
+    let secondBarWidthSmooth  = map(secondsWithFraction, 0, 60, 0, );
+
+    // fill(100, 100, 200)
+    // rect(0, 350, secondBarWidthChunky, 50);
+    // fill(120, 120, 240)
+    // rect(0, 400, secondBarWidthSmooth, 50);
 }
