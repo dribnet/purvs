6bf8bf0549d615c4689affe0e98208d6f19b2ad1
Guest User <Guest@wg401-f-05-06.mddn.vuw.ac.nz>
2018-03-22 13:33:44 +1300

    Updating Thumbnail and Preview.

--- clock_bars.js
+++ clock_bars.js
@@ -1,7 +1,16 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
+
+function setup(){
+    heart = loadImage("images/heart.png");
+    image(heart, 300, 100, 200, 200);
+}
+
 function draw_clock(obj) {
+    angleMode(DEGREES);
+    
+ 
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
     //    obj.minutes goes from 0-59
@@ -21,49 +30,36 @@ function draw_clock(obj) {
     text("Hour: "   + hours, 10, 22);
     text("Minute: " + minutes, 10, 42);
     text("Second: " + seconds, 10, 62);
-    text("Millis: " + millis, 10, 82);
+    text("Millis: " + millis, 10, 82); 
 
-    let hourBarWidth   = map(hours, 0, 23, 0, height/4*4);
-    let minuteBarWidth = map(minutes, 0, 59, 0, height/4*3);
-    let secondBarWidth = map(seconds, 0, 59, 0, height/4*2);
-    let millisBarWidth = map(millis, 0, 1000, 0, height/4);
+    //image(img, 200, 200, 200, 200);
 
-//smooth second bar
-    let secondsWithFraction   = seconds + (millis / 1000.0);
-    let secondBarWidthSmooth  = map(secondsWithFraction, 0, 60, 0, height/4*2);
+    translate(500, 250);
+    rotate(-90);
+    strokeWeight(4);
+    stroke(147);
+    noFill();
+    //the stepping seconds arc
+   // let endsec = map(seconds, 0, 60, 0, 360);
+   // arc(0, 0, 260, 260, 0, endsec);
 
-//smooth minute bar
-    let minutesWithFraction   = minutes + (seconds / 59);
-    let minuteBarWidthSmooth  = map(minutesWithFraction, 0, 60, 0, height/4*3);
+    //the smooth second arc
+    let endsecfraction = seconds + (millis / 1000.0);
+    let endsecsmooth = map (endsecfraction, 0, 60, 0, 360);
+    arc(0, 0, 260, 260, 0, endsecsmooth);
 
-    let milliXpos = map(seconds, 0, 60, 0, width-15);
-    let secXpos = map(minutes, 0, 60, 0, width-15);
-    let minXpos = map(hours, 0, 23, 0, width-30);
+    //the minute arc
+    noFill();
+    let endmin = map(minutes, 0, 60, 0, 360);
+    arc(0, 0, 280, 280, 0, endmin); 
 
-    noStroke();
-
-
-
-    //min bar
-    fill('#0D4A4D');
-    rect(minXpos, height, 30, -minuteBarWidthSmooth);
-
+    //the hour arc
+    noFill();
+    let endhour = map(hours, 0, 12, 0, 360);
+    arc(0, 0, 300, 300, 0, endhour);  
 
 
-//milli bar
-    fill('#67989A');
-    rect(milliXpos, height, 30, -millisBarWidth);
-   /* rect(milliXpos-10, height+20, 50, -millisBarWidth);
-    rect(milliXpos-20, height+40, 70, -millisBarWidth);
-    rect(milliXpos-30, height+60, 90, -millisBarWidth);
-    rect(milliXpos-40, height+80, 110, -millisBarWidth);
-    rect(milliXpos, height+100, 150, -millisBarWidth); */
-
-//second bar
-    fill('#417D80');
-    noStroke();
-    rect(secXpos, height, 30, -secondBarWidthSmooth);
-
+    rect(400, 150, 200, 200);
 
 
   /* 
