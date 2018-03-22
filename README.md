0c57b57e2bb256489a0747c7fae2e3a3b38de1fa
Yvonne Zou <pandora1995@hotmail.com>
2018-03-23 10:24:18 +1300

    Add Alarm

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## PS1 MDDN 242 2017
 
 ### As time gose by-clock
-
-The idea of this project is 'Time lapses', I choose the old timekeeping to be my clock and numbers will be fall dowm like sand from the sand clock. 
-
-Because we need include hours, mintues and second so I probably need three sand clocks. 
+My alarm is easy and it is just for the test, I think I will not use such alarm in my final work.
+I really change a lot from my concept but I still use the same movement.
--- clock.js
+++ clock.js
@@ -1,9 +1,6 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
- */
-function setup(){
- 	createCanvas(960,500);
- } 
+ */ 
 function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
     //    obj.hours goes from 0-23
@@ -14,14 +11,26 @@ function draw_clock(obj) {
     //        < 0 if no alarm is set
     //        = 0 if the alarm is currently going off
     //        > 0 --> the number of seconds until alarm should go off
+
+    
+
     let hours = obj.hours;
     let minutes = obj.minutes;
     let seconds = obj.seconds;
     let millis = obj.millis;
-    background(30,144,255);
+    let alarm = obj.seconds_until_alarm;
 
-
-    background(135,206,250);
+    if (alarm < 0) {
+        background(221,160,221); 
+    }
+    else if (alarm > 0) {
+        background(255, 105, 180);        
+    }
+    else if (alarm == 0){
+        background(255,248,220);
+    }
+    
+    
     strokeWeight(1); //  beige
     fill(0,0,139);
     text("Hour: "   + hours, 10, 142);
@@ -30,59 +39,30 @@ function draw_clock(obj) {
     text("Millis: " + millis, 10, 202);
 
     let hourBarHeight = map(hours, 0, 23, 100, 400);
-    let minuteBarHeight = map(minutes, 0, 59, 75, 425);
-    let secondBarColor = map(seconds, 0, 59, 0, 255);
-    let millisBarHeight = map(millis, 0, 1000, 130, 475);
-    let millisBarHeight2 = map(millis, 0, 1000, 50, 500);
-    let millisBarHeight3 = map(millis, 0, 1000, 100, 300);
-    let millisBarHeight4 = map(millis, 0, 1000, 20, 400);
-    let millisBarHeight5 = map(millis, 0, 1000, 70, 480);
+    let millisBarHeight = map(millis, 0, 1000, 90,350);
+    let millisBarHeight2 = map(millis, 0, 1000, 100,210);
 
+    strokeWeight(12);
+    stroke(51);
+    fill(222,184,135);
+    quad(560,250,400,250,300,490,660,490);
+    strokeWeight(12);
+    stroke(51);
+    fill(0,191,255);
+    ellipse(480,210,400,400);
 
-
-    //Hour
-    //strokeWeight(4);
-    //stroke(255);
-    //fill(220,20,60);
-    //ellipse(480, hourBarHeight, 200, 200);
-
-    //Minute
-    //strokeWeight(4);
-    //stroke(255);
-    //fill(255,127,80);
-    //ellipse(480, hourBarHeight, 150, 150);
-
-    //millis
-    //strokeWeight(4);
-    //stroke(0,191,255);
-    noStroke();
+    //millis-rain
+    noStroke()
     fill(0,0,205);
-    ellipse(80, millisBarHeight3, 25, 50);
-    ellipse(280, millisBarHeight2, 25, 50);
-    ellipse(480, millisBarHeight, 25, 50);//center
-    ellipse(680, millisBarHeight4, 25, 50);
-    ellipse(880, millisBarHeight5, 25, 50);
+    ellipse(520, millisBarHeight, 10, 30);
+    ellipse(420, millisBarHeight2, 10, 30);
 
-
-    //Second
     //cloud
-    //strokeWeight(4);
-    //stroke(0,191,255);
-    //fill(0,0,205);
     noStroke();
-    fill(secondBarColor);
-    ellipse(870,0,200,200);
-    ellipse(750,7,175,175);
-    ellipse(670,10,100,100);
-    ellipse(600,0,120,120);
-    ellipse(480,0,260,260);//Center
-    ellipse(300,0,200,200);
-    ellipse(200,5,100,100);
-    ellipse(50,5,250,250);
-    //ellipse(480, secondBarHeight, 50, 100);
-
-    
+    fill(255);
+    ellipse(530,80,75,55);
+    ellipse(490,80,100,95);
+    ellipse(440,90,95,75);
+    ellipse(400,110,30,25);
+    ellipse(540,110,30,25);
 }
-
-    
-    
