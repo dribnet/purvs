b6383eca1ad71ab68a4b9486ad0296a0a7abb9b1
Kathleen Griffin <griffin@outlook.co.nz>
2018-03-23 20:18:06 +1300

    returned most recent commit to be the final submission

--- clock.js
+++ clock.js
@@ -4,13 +4,12 @@
  const CANVAS_WIDTH = 960;
  const CANVAS_HEIGHT = 500;
  const PIXEL_SIZE = 30;
- const START_Y = 150;
- const FIRST_START_X = 320;
- const MID_START_X = 420;
- const SEC_START_X = 520;
- const SEC_GAP = 15;
- const MIN_GAP = 10
- const HOUR_GAP = 5;
+ const START_Y = 100;
+ const FIRST_HOUR_START_X = 100;
+ const SEC_HOUR_START_X = 300;
+ const FIRST_MIN_START_X = 500;
+ const SEC_MIN_START_X = 700;
+ const GAP =5;
  let num;
  let zero;
  let one;
@@ -25,94 +24,143 @@
  let tideLevel;
  let minutesTens;
  let secondsTens;
+ let seconds;
+ let alarmOn;
 
  function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
   //    obj.minutes goes from 0-59
   //    obj.seconds goes from 0-59
+  //    obj.millis goes from 0-1000
+  //    obj.seconds_until_alarm is:
+  //    < 0 if no alarm is set
+  //    = 0 if the alarm is currently going off
+  //    > 0 --> the number of seconds until alarm should go off
   
   let hours = obj.hours;
   let minutes = obj.minutes;
-  let seconds = obj.seconds;
+  seconds = obj.seconds;
   let minTotal = (hours * 60) + minutes; //total mintues of the day
   let alarm = obj.seconds_until_alarm;
   let main_canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   main_canvas.parent('canvasContainer');
-  background(0);
+  background(69,178,157);
+
+  //these tides are for Friday 23 March
+  //to continue for any date would just have to read a tide table from a file
+  //also assuming you don't check after 10:56pm or before 4:27am
+  //low tide: 267
+  //high tide: 636
+  //low tide: 1012
+  //high tide: 1324
+  if (minTotal < 636) { //first high tide of the day
+  	tideLevel   = map(minTotal, 267, 636, 0, height);
+  }
+  if (minTotal >= 636 && minTotal < 1012) {//after the first high tide & before second low tide
+  	tideLevel   = map(minTotal, 636, 1012, 0, height);
+  }
+  else if (minTotal >=1012) { //after the second low tide of the day
+  	tideLevel   = map(minTotal, 1012, 1376, 0, height);
+  }
+  //setup for drawing alarm progress bar
+  noStroke();
+  fill(51,77,92);
+  rect(0, height-tideLevel, width, tideLevel);
+  //if the alarm is going off
+  if (alarm == 0) {
+  	alarmOn = true;
+  }
+  //if the alarm isn't going off - show how long untl it does
+  let alarmTimer   = map(alarm, 0, 60, 0, height);
+  fill(226,122,63);
+  rect(0,height-alarmTimer,20,alarmTimer );
+  
+  //setup for drawing the bubbles that make up the numbers
   strokeWeight (2);
+  stroke(255,255,255);
   noFill();
 
   //a 2d array of each number storing how to draw it as booleans
   zero = [
-  [0,1,1,1,1,1,0],
-  [1,0,0,0,0,0,1],
-  [1,0,0,0,0,0,1],
-  [1,0,0,0,0,0,1],
-  [0,1,1,1,1,1,0],
+  [0,0,1,1,1,1,1,1,1,0,0],
+  [0,1,0,0,0,0,0,0,0,1,0],
+  [1,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,1],
+  [0,1,0,0,0,0,0,0,0,1,0],
+  [0,0,1,1,1,1,1,1,1,0,0],
   ];
   one = [
-  [0,1,0,0,0,0,1],
-  [1,0,0,0,0,0,1],
-  [1,1,1,1,1,1,1],
-  [0,0,0,0,0,0,1],
-  [0,0,0,0,0,0,1],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,0,0,0,1],
+  [0,1,0,0,0,0,0,0,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1],
+  [0,0,0,0,0,0,0,0,0,0,1],
+  [0,0,0,0,0,0,0,0,0,0,1],
   ];
   two = [
-  [0,1,0,0,0,0,1],
-  [1,0,0,0,0,1,1],
-  [1,0,0,0,1,0,1],
-  [1,0,0,1,0,0,1],
-  [0,1,1,0,0,0,1],
+  [0,0,1,1,0,0,0,0,1,1,1],
+  [0,1,0,0,0,0,0,1,0,0,1],
+  [1,0,0,0,0,0,1,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [0,1,0,0,1,0,0,0,0,0,1],
+  [0,0,1,1,0,0,0,0,0,0,1],
   ];
   three = [
-  [0,1,0,0,0,1,0],
-  [1,0,0,0,0,0,1],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [0,1,1,0,1,1,0],
+  [0,0,1,1,0,0,0,1,1,0,0],
+  [0,1,0,0,0,0,0,0,0,1,0],
+  [1,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [0,1,0,0,1,0,1,0,0,1,0],
+  [0,0,1,1,0,0,0,1,1,0,0],
   ];
   four = [
-  [0,0,0,1,1,0,0],
-  [0,0,1,0,1,0,0],
-  [0,1,0,0,1,0,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,0,1,0,0],
+  [1,1,1,1,1,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0,0],
+  [0,0,1,1,1,1,1,1,1,1,1],
+  [0,0,0,0,0,1,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0,0],
   ];
   five = [
-  [1,1,1,0,0,1,0],
-  [1,0,1,0,0,0,1],
-  [1,0,1,0,0,0,1],
-  [1,0,1,0,0,0,1],
-  [1,0,0,1,1,1,0],
+  [1,1,1,1,1,0,0,0,0,0,1],
+  [1,0,0,0,1,0,0,0,0,0,1],
+  [1,0,0,0,1,0,0,0,0,0,1],
+  [1,0,0,0,1,0,0,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,1,0],
+  [1,0,0,0,0,0,1,1,1,0,0],
   ];
   six = [
-  [0,1,1,1,1,1,0],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [0,1,0,0,1,1,0],
+  [0,0,1,1,1,1,1,1,1,0,0],
+  [0,1,0,0,1,0,0,0,0,1,0],
+  [1,0,0,0,1,0,0,0,0,0,1],
+  [1,0,0,0,1,0,0,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,1,0],
+  [1,0,0,0,0,0,1,1,1,0,0],
   ];
   seven = [
-  [1,0,0,0,0,0,0],
-  [1,0,0,0,0,1,1],
-  [1,0,0,0,1,0,0],
-  [1,0,0,1,0,0,0],
-  [1,1,1,0,0,0,0],
+  [1,0,0,0,0,0,0,0,1,1,1],
+  [1,0,0,0,0,0,0,1,0,0,0],
+  [1,0,0,0,0,0,1,0,0,0,0],
+  [1,0,0,0,0,1,0,0,0,0,0],
+  [1,0,0,0,1,0,0,0,0,0,0],
+  [1,1,1,1,0,0,0,0,0,0,0],
   ];
   eight = [
-  [0,1,1,0,1,1,0],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [0,1,1,0,1,1,0],
+  [0,0,1,1,0,0,0,1,1,0,0],
+  [0,1,0,0,1,0,1,0,0,1,0],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [0,1,0,0,1,0,1,0,0,1,0],
+  [0,0,1,1,0,0,0,1,1,0,0],
   ];
   nine = [
-  [0,1,1,0,0,1,0],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [1,0,0,1,0,0,1],
-  [0,1,1,1,1,1,0],
+  [0,0,1,1,0,0,0,0,1,0,0],
+  [0,1,0,0,1,0,0,0,0,1,0],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [1,0,0,0,0,1,0,0,0,0,1],
+  [0,1,0,0,0,1,0,0,0,1,0],
+  [0,0,1,1,1,1,1,1,1,0,0],
   ];
 
   //array of all the number arrays
@@ -120,104 +168,108 @@
 
   //figure out what number is in the 'tens' place of the seconds
   if (seconds < 10) {
-    var secFirst = new Number(FIRST_START_X, zero, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds], SEC_GAP);
+  	secondsTens = 0;
   }
   else if (seconds >=10 && seconds <20) {
-    var secFirst = new Number(FIRST_START_X, one, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds-10], SEC_GAP);
+  	secondsTens = 10;
   }
   else if (seconds >=20 && seconds <30) {
-    var secFirst = new Number(FIRST_START_X, two, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds-20], SEC_GAP);
+  	secondsTens = 20;
   }
   else if (seconds >=30 && seconds <40) {
-    var secFirst = new Number(FIRST_START_X, three, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds-30], SEC_GAP);
+  	secondsTens = 30;
   }
   else if (seconds >=40 && seconds <50) {
-    var secFirst = new Number(FIRST_START_X, four, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds-40], SEC_GAP);
+  	secondsTens = 40;
   }
   else if (seconds >=50 && seconds <60) {
-    var secFirst = new Number(FIRST_START_X, five, SEC_GAP);
-    var secSecond = new Number(SEC_START_X, numbers[seconds-50], SEC_GAP);
+  	secondsTens = 50;
   }
 
-
-
+//set up how to draw each number, first figure out digit in 'tens' position, then the 'ones' position
+ //creates as number obect (called a function in javascript?)
   if (hours < 10) {
-    var hourSecond = new Number(MID_START_X, numbers[hours], HOUR_GAP);
+  	var hourFirst = new Number(FIRST_HOUR_START_X, zero, minutesTens);
+  	var hourSecond = new Number(SEC_HOUR_START_X, numbers[hours], secondsTens);
   }
   else if (hours >= 10 && hours < 20) {
-    var hourFirst = new Number(FIRST_START_X, one, HOUR_GAP);
-    var hourSecond = new Number(SEC_START_X, numbers[hours-10], HOUR_GAP);
-    hourFirst.draw();
+  	var hourFirst = new Number(FIRST_HOUR_START_X, one, minutesTens);
+  	var hourSecond = new Number(SEC_HOUR_START_X, numbers[hours-10], secondsTens);
   }
   else if (hours > 20) {
-    var hourFirst = new Number(FIRST_START_X, two, HOUR_GAP);
-    var hourSecond = new Number(SEC_START_X, numbers[hours-20], HOUR_GAP);
-    hourFirst.draw();
+  	var hourFirst = new Number(FIRST_HOUR_START_X, two);
+  	var hourSecond = new Number(SEC_HOUR_START_X, numbers[hours-20], secondsTens);
   }
 
 
   if(minutes < 10) {
-    var minFirst = new Number(FIRST_START_X, zero, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, zero, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes], secondsTens);
   }
   else if (minutes >=10 && minutes <20) {
-    var minFirst = new Number(FIRST_START_X, one, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes-10], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, one, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes-10], secondsTens);
   }
   else if (minutes >=20 && minutes <30) {
-    var minFirst = new Number(FIRST_START_X, two, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes-20], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, two, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes-20], secondsTens);
   }
   else if (minutes >=30 && minutes <40) {
-    var minFirst = new Number(FIRST_START_X, three, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes-30], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, three, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes-30], secondsTens);
   }
   else if (minutes >=40 && minutes <50) {
-    var minFirst = new Number(FIRST_START_X, four, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes-40], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, four, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes-40], secondsTens);
   }
   else if (minutes >=50 && minutes <60) {
-    var minFirst = new Number(FIRST_START_X, five, MIN_GAP);
-    var minSecond = new Number(SEC_START_X, numbers[minutes-50], MIN_GAP);
+  	var minFirst = new Number(FIRST_MIN_START_X, five, secondsTens);
+  	var minSecond = new Number(SEC_MIN_START_X, numbers[minutes-50], secondsTens);
   }
 
-  
+//draw each number!
+  hourFirst.draw();
   hourSecond.draw();
   minFirst.draw();
   minSecond.draw();
-  secFirst.draw();
-  secSecond.draw();
 
 
 }
 
-//elapsed
-function Number(startX, number, spacing) {
+//how the numbers are drawn
+function Number(startX, number, elapsed) {
 	this.x = startX;
 	this.array = number; 
-  	this.gap = spacing;
+	this.done = elapsed;
 
 	this.draw = function() {
-		for (var i = 0; i < 5; i++) {
-			for (var j = 0; j < 7; j++) {
+		for (var i = 0; i < 6; i++) {
+			for (var j = 0; j < 11; j++) {
 				if (this.array[i][j] == true) {
-            if  (this.gap == SEC_GAP) {
-              stroke(0,0,255);
-            }
-            else if (this.gap == MIN_GAP) {
-            	stroke(255,255,0);
-            }
-            else if (this.gap == HOUR_GAP) {
-            	stroke(255,0,0);
-            }
-					ellipse(this.x + (i*PIXEL_SIZE), START_Y + (j*PIXEL_SIZE), PIXEL_SIZE - this.gap, PIXEL_SIZE - this.gap);
+          stroke(255);
+					noFill();
+					//move every 10 seconds
+					if  ((j-1)*10 >= (secondsTens)*2) {
+						fill(255);
+					}
+          //move one more every five seconds
+					if((j*10 >= (secondsTens)*2) && ((seconds - secondsTens) < 5)) {
+						fill(255);
+					}
+          //flash red on even seconds
+					if (alarmOn == true && (((seconds - secondsTens)%2) == 0)) {
+						stroke(239, 201,76);
+						fill(223,90,73);
+					}
+          //flash yellow on odd seconds
+					if (alarmOn == true && (((seconds - secondsTens)%2) != 0)) {
+						stroke(223,90,73);
+						fill(239, 201,76);
+					}
+          //draw the circle
+					rect(this.x + (i*PIXEL_SIZE), START_Y + (j*PIXEL_SIZE), PIXEL_SIZE - GAP, PIXEL_SIZE - GAP);
 				}
 			}
-   }
- }
+		}
+	}
 }
