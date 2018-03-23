40667d9f918ae7642212d1a78514ff99d5c7735d
George Robson <robsongeorge@hotmail.com>
2018-03-23 13:18:44 +1300

    added alarm, made changes to alot of the code, made the hands all rotate around their parent ie. minute around the hour.

--- clock.js
+++ clock.js
@@ -1,11 +1,6 @@
 /*
  * us p5.js to draw a clock on a 960x500 canvas
  */ 
- xx=0;
- yy=0;
- angle=0;
-
-
 
 function draw_clock(obj) {
     // draw your own clock here based on the values of obj:
@@ -22,82 +17,98 @@ function draw_clock(obj) {
     let seconds = obj.seconds;
     let millis = obj.millis;
 
+    let millisTT =  (seconds + (millis/1000)) / (60 / (2 * PI)) * 60 - PI/2;
+    let secondsWithFraction = (seconds + (millis / 1000.0)) / (60/ (2 * PI)) - PI/2 ;
+    let minutesWithFraction = (minutes + (seconds/60) + (millis / 1000.0 / 60)) / (60/ (2 * PI))- PI/2;
+    let hoursWithFraction = (hours/1.91) + (minutes/60/1.91) + seconds/60/60/1.91 +  - PI/2 ;
+    let days = hours/24; 
+    
+       let mouseX_map = map(mouseX, 0, width, 250, 255);
+       let mouseY_map = map(mouseY, 0, height, 250, 255);
 
-c1 = color(33,200,150);
- c2 = color(0,210,150);
+       let att = obj.seconds_until_alarm;
 
+    
+c1 = color(250);
+c2 = color(255);
+d = color(253, 223, 224)
+h = color(253, 187, 206)
+m = color(205, 163, 193)
+s = color(146, 137, 173)
+mm = color(96, 118, 140)
 
-let millisTT =  (seconds + (millis/1000)) / (60 / (2 * PI)) * 60 - PI/2;
-let secondsWithFraction = (seconds + (millis / 1000.0)) / (60/ (2 * PI)) - PI/2 ;
-let minutesWithFraction = (minutes + (seconds/60) + (millis / 1000.0 / 60)) / (60/ (2 * PI))- PI/2;
-let hoursWithFraction = (hours/1.91) + minutes/60/1.91 - PI/2 ;
+    
+function create_hand(radiusH, timing, size, c ,rad) {
+  xH=(radiusH)*cos(radians(degrees(timing)))  + width/2;
+  yH=(radiusH)*sin(radians(degrees(timing))) + height/2;
+  fill(c);
+   ellipse(xH, yH, size, size);
+translate(xH-width/2,yH-height/2)
 
-noStroke();
-     background(33,200,150);
+  length = 2;
+  stroke(c);
+  for (i = 0; i < 12; i = i+1) {
+cx=(rad)*cos(radians(degrees(PI/6*i)))  + width/2;
+cy=(rad)*sin(radians(degrees(PI/6*i))) + height/2;
 
+line(cx-length, cy-length, cx+length, cy+length)
+line(cx+length, cy-length, cx-length, cy+length)
+	}
+	  noStroke();
+ 
+}
 
 
 
 if (seconds%2 == 0){
-	fill(c1); // dark grey
+	fill(c1); 
 	background(c2);
 } else {
-	fill(c2); // dark grey
+	fill(c2); 
 	background(c1);
 }
 
-ellipse (width/2, height/2, millis*1.04, millis*1.04);
-
-fill(255); //white
-
-
-	xx=130*cos(radians(degrees(millisTT)))  + width/2;
-  yy=130*sin(radians(degrees(millisTT))) + height/2;
-  ellipse(xx, yy, 20, 20);
+ellipse (width/2, height/2, millis* sqrt((pow(width/2, 2) + pow(height/2,2)))/480, millis*sqrt((pow(width/2, 2) + pow(height/2,2)))/480);
+fill(0);
 
 
 
+create_hand(0, days, 30,mm, 130);
+
+create_hand(0, days, 30,mm, 130);
+create_hand(130, hoursWithFraction, 20, s,65);
+create_hand(65, minutesWithFraction, 15, m,33);
+create_hand(33, secondsWithFraction, 10, h, 0);
 
 
 
+    if (att > 0){
 
-  xx=190*cos(radians(degrees(secondsWithFraction)))  + width/2;
-  yy=190*sin(radians(degrees(secondsWithFraction))) + height/2;
-  ellipse(xx, yy, 10, 10);
+create_hand(17, att, 10,h, 0);
 
- xx=165*cos(radians(degrees(minutesWithFraction)))  + width/2;
-  yy=165*sin(radians(degrees(minutesWithFraction))) + height/2;
-  ellipse(xx, yy, 15, 15);
+if (seconds%2 == 0){
+	fill(255,0,0,20); 
+	background(250,0,0,20);
+} else {
+		fill(250,0,0,20); 
+	background(255,0,0,20);
+}
+	
+} else if  (att == 0){
+	create_hand(17, millisTT,5,d,width);
 
- xx=150*cos(radians(degrees(hoursWithFraction)))  + width/2;
-  yy=150*sin(radians(degrees(hoursWithFraction))) + height/2;
-  ellipse(xx, yy, 20, 20);
+if (seconds%2 == 0){
+	fill(255,0,0,40); 
+	background(250,0,0,40);
+} else {
+		fill(250,0,0,40); 
+	background(255,0,0,40);
+}
+}  
 
-
-
-let cX = 0;
-let cY = 0;
-let sz = 5
-
-
-for (i = 0; i < 12; i = i+1) {
-cx=130*cos(radians(degrees(PI/6*i)))  + width/2;
-cy=130*sin(radians(degrees(PI/6*i))) + height/2;
-
-
-ellipse(cx, cy, sz, sz);
-
-	}
-
-
-       fill(255,255,255); // dark grey
-    text("Hour: "   + hours, 10, 22);
-    text("Minute: " + minutes, 10, 42);
-    text("Second: " + seconds, 10, 62);
-    text("Millis: " + millisTT, 10, 82);
-
-text("Secs + Millis: " + secondsWithFraction, 10, 102);
-text("degrees: " + degrees(secondsWithFraction), 10, 122);
+else {
+	create_hand(17, millisTT,5,d,width);
+}
 
 }
 
