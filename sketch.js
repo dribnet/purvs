2f4252be4fd68a8dfb29a3d693c5e67d8700362b
Lucy Jaegers <lucysayshey@gmail.com>
2018-04-06 14:04:58 +1200

    Posting initial alphabet with three letters

--- sketch.js
+++ sketch.js
@@ -13,21 +13,42 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+ // "size": 80,
+ // "offsetx": 0,
+ // "offsety": 35,
+
+  "offset": 150,
+
+  "x1": 100,
+  "y1": 450,
+  "x2": 200,
+  "y2": 450
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+ // "size": 150,
+ // "offsetx": 0,
+ // "offsety": -145,
+
+  "offset": 450,
+
+  "x1": 500,
+  "y1": 50,
+  "x2": 500,
+  "y2": 450
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+// "size": 100,
+// "offsetx": 30,
+// "offsety": 0,
+
+  "offset": 700,
+
+  "x1": 900,
+  "y1": 50,
+  "x2": 900,
+  "y2": 450
 }
 
 const colorFront  = "#199cff";
@@ -50,13 +71,26 @@ function setup () {
 
 function drawLetter(posx, posy, scale, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  //let size2 = letterData["size"];
+  //let pos2x = posx + letterData["offsetx"];
+  //let pos2y = posy + letterData["offsety"];
+
+  let posx1 = letterData["x1"];
+  let posy1 = letterData["y1"];
+  let posx2 = letterData["x2"];
+  let posy2 = letterData["y2"];
+
+  let offset = letterData["offset"];
+
 
   // draw two circles
-  ellipse(posx, posy, 150, 150);
-  ellipse(pos2x, pos2y, size2, size2);
+  //ellipse(posx, posy, 150, 150);
+  //ellipse(pos2x, pos2y, size2, size2);
+
+
+  line(posx1, posy1, offset, canvasHeight/2);
+  line(posx2, posy2, offset, canvasHeight/2);
+
 }
 
 function draw () {
