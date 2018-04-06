e6312500024dcc8673cc7f40fa35603970dfda3e
Guest User <Guest@wg401-g-01-06.mddn.vuw.ac.nz>
2018-04-06 15:14:25 +1200

    hard coded A, B and C + changed read me

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
 
+
 /* 
  * my three variable per letter are:
  *
@@ -32,7 +33,7 @@ const letterC = {
 
 const colorFront  = "#199cff";
 const colorBack   = "#e3eded";
-const colorStroke = "#233f11";
+const colorStroke = "#000000";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -59,19 +60,83 @@ function drawLetter(posx, posy, scale, letterData) {
   ellipse(pos2x, pos2y, size2, size2);
 }
 
+
+
 function draw () {
   // clear screen
   background(colorBack);
+  angleMode(DEGREES);
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
-
+/*
   // draw the letters A, B, C from saved data
   drawLetter(center_x - 250, center_y, 10, letterA);
   drawLetter(center_x      , center_y, 10, letterB);
   drawLetter(center_x + 250, center_y, 10, letterC);
+*/
+
+// A
+
+strokeJoin(MITER);
+
+push();
+  translate(-200, 0);
+
+  noFill();
+  strokeWeight(10);
+  arc(center_x, center_y, 100, 100, 160, 290);
+
+  strokeWeight(4);
+  line(center_x, center_y, center_x-50, center_y);
+
+  strokeWeight(1);
+  fill(0);
+  triangle(center_x, center_y-50, center_x+10, center_y-50, center_x+5, center_y+50);
+
+  translate(+200, 0);
+pop();
+
+
+// B
+
+  noFill();
+  strokeWeight(10);
+  arc(center_x, center_y, 100, 100, 260, 100);
+
+  strokeWeight(1);
+  fill(0);
+  triangle(center_x, center_y+5, center_x+50, center_y-5, center_x+50, center_y-15);
+
+  strokeWeight(4);
+  line(center_x, center_y+43, center_x, center_y-43);
+
+
+//C
+
+push();
+  translate(+200, 0);
+
+  noFill();
+  strokeWeight(10);
+  arc(center_x, center_y, 100, 100, 60, 230);
+
+  strokeWeight(1);
+  fill(0);
+  triangle(center_x-15, center_y-40, center_x-5, center_y-50, center_x+40, center_y-20);
+
+  strokeWeight(4);
+  line(center_x, center_y+43, center_x, center_y-30);
+
+  translate(-200, 0);
+pop();
 }
+
+
+
+
+
 
 function keyTyped() {
   if (key == '!') {
