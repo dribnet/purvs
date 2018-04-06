00aad2df03ee010d247f871a48379beefda943c9
Guest User <Guest@wg401-j-02-04.mddn.vuw.ac.nz>
2018-04-06 15:38:00 +1200

    Created C and made most of my code.

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 (Replace this README with information about your alphabet. This is an example.)
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
+Each of my letters consist of 4 cresents made up of 2 arcs each, all based at the center and rotating outward from there. None of the postions are fixed and they all move with the letter and are postioned so they dont overlap most of the time.
 
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+The eight parameters per letter:
+  * `rot1`: Controls rotation of Arc1
+  * `rot2`: Controls rotation of Arc2
+  * `rot3`: Controls rotation of Arc3
+  * `rot4`: Controls rotation of Arc4
+  * `offset1`: Controls the X offset of Arc1
+  * `offset2`: Controls the X offset of Arc2
+  * `offset3`: Controls the X offset of Arc3
+  * `offset4`: Controls the X offset of Arc4
--- sketch.js
+++ sketch.js
@@ -13,21 +13,37 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offset1": 0,
+  "offset2": 0,
+  "offset3": 0,
+  "offset4": 0,
+  "rot1": 0,
+  "rot2": 0,
+  "rot3": 0,
+  "rot4": 0
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "offset1": 0,
+  "offset2": 0,
+  "offset3": 0,
+  "offset4": 0,
+  "rot1": 0,
+  "rot2": 0,
+  "rot3": 0,
+  "rot4": 0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "offset1": -60,
+  "offset2": -60,
+  "offset3": 0,
+  "offset4": 0,
+  "rot1": 90,
+  "rot2": -90,
+  "rot3": 0,
+  "rot4": 0
+
 }
 
 const colorFront  = "#b20000";
@@ -43,6 +59,7 @@ function setup () {
   fill(colorFront);
   stroke(colorStroke);
   strokeWeight(4);
+  angleMode(DEGREES);
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
@@ -50,13 +67,63 @@ function setup () {
 
 function drawLetter(posx, posy, scale, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+   let rot1 = letterData["rot1"];
+   let rot2 = letterData["rot2"];
+   let rot3 = letterData["rot3"];
+   let rot4 = letterData["rot4"];
+  let offset1 = letterData["offset1"];
+  let offset2 = letterData["offset2"];
+  let offset3 = letterData["offset3"];
+  let offset4 = letterData["offset4"];
+  
 
   // draw two circles
-  ellipse(posx, posy, 150, 150);
-  ellipse(pos2x, pos2y, size2, size2);
+  // ellipse(posx, posy, 150, 150);
+  // ellipse(pos2x, pos2y, size2, size2);
+  push();
+  translate(posx, posy);
+
+//arc1
+  push();
+  rotate(rot1);
+  fill(colorFront);
+  arc(offset1, 0, 200, 200, 80, 280, CHORD);
+  fill(colorBack);
+  noStroke();
+    arc(offset1+31, 0, 150, 200, 96, 264, CHORD);
+  pop();
+
+//arc2
+   push();
+   rotate(rot2);
+  fill(colorFront);
+  arc(offset2, 0, 200, 200, 80, 280, CHORD);
+  fill(colorBack);
+  noStroke();
+    arc(offset2+31, 0, 150, 200, 96, 264, CHORD);
+  pop();
+
+//arc3
+   push();
+   rotate(rot3);
+  fill(colorFront);
+  arc(offset3, 0, 200, 200, 80, 280, CHORD);
+  fill(colorBack);
+  noStroke();
+    arc(offset3+31, 0, 150, 200, 96, 264, CHORD);
+  pop();
+
+//arc4
+   push();
+   rotate(rot4);
+  fill(colorFront);
+  arc(offset4, 0, 100, 100, 80, 280, CHORD);
+  fill(colorBack);
+  noStroke();
+    arc(offset4+17, 0, 50, 100, 96, 264, CHORD);
+  pop();
+
+  pop();
 }
 
 function draw () {
