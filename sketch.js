e986e68201025685e85944d9a00768c6129adce4
Mitchell Barclay <mitchellbarclay2@gmail.com>
2018-04-08 18:05:08 +1200

    commiting new files

--- sketch.js
+++ sketch.js
@@ -13,15 +13,30 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  //"size": 80,
+  //"offsetx": 0,
+  //"offsety": 35
+
+    "L1x1": 200,
+    "L1Y1": 400,
+    "L1x2": 400,
+    "L1y2": 100,
+    "L2x1": 400,
+    "L2Y1": 100,
+    "L1x2": 600,
+    "L2y2": 400,
+    "L3x1": 300,
+    "L3y1": 200,
+    "L3x2": 500,
+    "L3y2": 200,
 }
 
 const letterB = {
   "size": 150,
   "offsetx": 0,
   "offsety": -145
+
+
 }
 
 const letterC = {
@@ -50,13 +65,35 @@ function setup () {
 
 function drawLetter(posx, posy, scale, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  //let size2 = letterData["size"];
+ // let pos2x = posx + letterData["offsetx"];
+ // let pos2y = posy + letterData["offsety"];
+    let L1x1 = letterData["L1x1"];
+    let L1Y1 = letterData["L1Y1"];
+    let L1x2 = letterData["L1x2"];
+    let L1y2 = letterData["L1y2"];
+    let L2x1 = letterData["L2x1"];
+    let L2Y1 = letterData["L2Y1"];
+    let L2x2 = letterData["L1x2"];
+    let L2y2 = letterData["L2y2"];
+    let L3x1 = letterData["L3x1"];
+    let L3y1 = letterData["L3y1"];
+    let L3x2 = letterData["L3x2"];
+    let L3y2 = letterData["L3y2"];
+
 
   // draw two circles
-  ellipse(posx, posy, 150, 150);
-  ellipse(pos2x, pos2y, size2, size2);
+  //ellipse(posx, posy, 150, 150);
+  //ellipse(pos2x, pos2y, size2, size2);
+
+
+strokeWeight(4);
+stroke(0)
+  line(L1x1,L1y1,L1x2, L1y2);
+  line(L2x1,L2y1,L2x2, L2y2);
+  line(L3x1,L3y1,L3x2, L3y2);
+
+
 }
 
 function draw () {
@@ -68,7 +105,7 @@ function draw () {
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, 10, letterA);
+  drawLetter(500, 500, 10, letterA);
   drawLetter(center_x      , center_y, 10, letterB);
   drawLetter(center_x + 250, center_y, 10, letterC);
 }
