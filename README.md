8b8c5da969403bbc01eb4dc0a13363cc08b15d05
Mitchell Barclay <mitchellbarclay2@gmail.com>
2018-04-09 10:33:17 +1200

    Updated readme and sketch
    Changed the background and altered the paramaters of my letters

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 ## PS2 MDDN 242 2018
 
-(Replace this README with information about your alphabet. This is an example.)
-
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
-
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+This is the first attempt at creating A B C that I'm happy with. Each letter is comprised of 3 individual lines, the paramaters being the co-ordinates of the start and end of each line. This leads to a total of 12 paramaters. I might want to consider seeing if I can narrow down these paramaters as this is the max allowed, but I do like the freedom of having complete control over the position and length of each line. 
--- sketch.js
+++ sketch.js
@@ -15,58 +15,73 @@ const canvasHeight = 500;
 const letterA = {
 
 
-
+//line1
  "x": 0,
- "y": -100,
+ "y": -50,
  "x2": 25,
- "y2": 0,
+ "y2": 50,
+
+ //line2
  "x3": 0,
- "y3": -100,
+ "y3": -50,
  "x4": -25,
- "y4": 0,
+ "y4": 50,
+
+ //line3
  "x5": -20,
- "y5": -20,
+ "y5": 30,
  "x6": 20,
- "y6": -20,
+ "y6": 30,
 }
 
 const letterB = {
 
-
+//line1
   "x": 0,
- "y": -100,
+ "y": -50,
  "x2": 0,
- "y2": 0,
+ "y2": 50,
+
+
+//line2
  "x3": 0,
- "y3": 0,
+ "y3": 50,
  "x4": 30,
- "y4": -20,
+ "y4": 30,
+
+
+//line3
  "x5": 30,
- "y5": -20,
+ "y5": 30,
  "x6": 0,
- "y6": -20,
+ "y6": 30,
 
 
 }
 
 const letterC = {
 
-  "x": 0,
- "y": -70,
+//line1
+ "x": 0,
+ "y": -20,
  "x2": 0,
- "y2": -30,
+ "y2": 20,
+
+ //line2
  "x3": 0,
- "y3": -70,
- "x4": 50,
- "y4": -100,
+ "y3": -20,
+ "x4": 25,
+ "y4": -50,
+
+ //line3
  "x5": 0,
- "y5": -30,
- "x6": 50,
- "y6": 0,
+ "y5": 20,
+ "x6": 25,
+ "y6": 50,
 }
 
 const colorFront  = "#199cff";
-const colorBack   = "#e3eded";
+const colorBack   = "#a097c4";
 const colorStroke = "#233f11";
 
 function setup () {
@@ -84,12 +99,6 @@ function setup () {
 }
 
 function drawLetter(x, y, letterData) {
-  // determine parameters for second circle
-  //let size2 = letterData["size"];
- // let pos2x = posx + letterData["offsetx"];
- // let pos2y = posy + letterData["offsety"];
-
-
 
    let posx = x + letterData["x"];
   let posy = y + letterData["y"];
@@ -109,19 +118,6 @@ function drawLetter(x, y, letterData) {
   let pos6x = x + letterData["x6"];
   let pos6y = y + letterData["y6"];
 
-
-  // draw two circles
-  //ellipse(posx, posy, 150, 150);
-  //ellipse(pos2x, pos2y, size2, size2);
-
-
-//strokeWeight(4);
-//stroke(0)
-  //line(L1x1,L1y1,L1x2, L1y2);
-  //line(L2x1,L2y1,L2x2, L2y2);
-  //line(L3x1,L3y1,L3x2, L3y2);
-
-  
 
   line(posx,posy,pos2x,pos2y)
   line(pos3x,pos3y,pos4x,pos4y)
