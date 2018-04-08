ae7c662b4858e8939ae85f7aff35cd153d0bda68
Mitchell Barclay <mitchellbarclay2@gmail.com>
2018-04-08 19:35:22 +1200

    created letters with my own set parameters
    Used my own parameters to turn 3 lines into A, B, and C. Also updating thumbnail and preview

--- sketch.js
+++ sketch.js
@@ -13,36 +13,56 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  //"size": 80,
-  //"offsetx": 0,
-  //"offsety": 35
 
-    "L1x1": 200,
-    "L1Y1": 400,
-    "L1x2": 400,
-    "L1y2": 100,
-    "L2x1": 400,
-    "L2Y1": 100,
-    "L1x2": 600,
-    "L2y2": 400,
-    "L3x1": 300,
-    "L3y1": 200,
-    "L3x2": 500,
-    "L3y2": 200,
+
+
+ "x": 0,
+ "y": -100,
+ "x2": 25,
+ "y2": 0,
+ "x3": 0,
+ "y3": -100,
+ "x4": -25,
+ "y4": 0,
+ "x5": -20,
+ "y5": -20,
+ "x6": 20,
+ "y6": -20,
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+
+
+  "x": 0,
+ "y": -100,
+ "x2": 0,
+ "y2": 0,
+ "x3": 0,
+ "y3": 0,
+ "x4": 30,
+ "y4": -20,
+ "x5": 30,
+ "y5": -20,
+ "x6": 0,
+ "y6": -20,
 
 
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+
+  "x": 0,
+ "y": -70,
+ "x2": 0,
+ "y2": -30,
+ "x3": 0,
+ "y3": -70,
+ "x4": 50,
+ "y4": -100,
+ "x5": 0,
+ "y5": -30,
+ "x6": 50,
+ "y6": 0,
 }
 
 const colorFront  = "#199cff";
@@ -63,23 +83,31 @@ function setup () {
   noLoop();
 }
 
-function drawLetter(posx, posy, scale, letterData) {
+function drawLetter(x, y, letterData) {
   // determine parameters for second circle
   //let size2 = letterData["size"];
  // let pos2x = posx + letterData["offsetx"];
  // let pos2y = posy + letterData["offsety"];
-    let L1x1 = letterData["L1x1"];
-    let L1Y1 = letterData["L1Y1"];
-    let L1x2 = letterData["L1x2"];
-    let L1y2 = letterData["L1y2"];
-    let L2x1 = letterData["L2x1"];
-    let L2Y1 = letterData["L2Y1"];
-    let L2x2 = letterData["L1x2"];
-    let L2y2 = letterData["L2y2"];
-    let L3x1 = letterData["L3x1"];
-    let L3y1 = letterData["L3y1"];
-    let L3x2 = letterData["L3x2"];
-    let L3y2 = letterData["L3y2"];
+
+
+
+   let posx = x + letterData["x"];
+  let posy = y + letterData["y"];
+
+  let pos2x = x + letterData["x2"];
+  let pos2y = y + letterData["y2"];
+
+  let pos3x = x + letterData["x3"];
+  let pos3y = y + letterData["y3"];
+
+  let pos4x = x + letterData["x4"];
+  let pos4y = y + letterData["y4"];
+
+  let pos5x = x + letterData["x5"];
+  let pos5y = y + letterData["y5"];
+
+  let pos6x = x + letterData["x6"];
+  let pos6y = y + letterData["y6"];
 
 
   // draw two circles
@@ -87,11 +115,20 @@ function drawLetter(posx, posy, scale, letterData) {
   //ellipse(pos2x, pos2y, size2, size2);
 
 
-strokeWeight(4);
-stroke(0)
-  line(L1x1,L1y1,L1x2, L1y2);
-  line(L2x1,L2y1,L2x2, L2y2);
-  line(L3x1,L3y1,L3x2, L3y2);
+//strokeWeight(4);
+//stroke(0)
+  //line(L1x1,L1y1,L1x2, L1y2);
+  //line(L2x1,L2y1,L2x2, L2y2);
+  //line(L3x1,L3y1,L3x2, L3y2);
+
+  
+
+  line(posx,posy,pos2x,pos2y)
+  line(pos3x,pos3y,pos4x,pos4y)
+  line(pos5x,pos5y,pos6x,pos6y)
+
+
+
 
 
 }
@@ -105,9 +142,11 @@ function draw () {
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter(500, 500, 10, letterA);
-  drawLetter(center_x      , center_y, 10, letterB);
-  drawLetter(center_x + 250, center_y, 10, letterC);
+  drawLetter(center_x  -200, center_y, letterA);
+  drawLetter(center_x      , center_y, letterB);
+  drawLetter(center_x + 250, center_y, letterC);
+
+
 }
 
 function keyTyped() {
