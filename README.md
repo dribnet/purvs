14bb13a72b9d6a9e6ddb3e133ec60f8aa3b3d0d3
Matthew Taylor <taylormatt7@myvuw.ac.nz>
2018-04-10 11:38:43 +1200

    Totally forgot to commit anything until now, oops! Still working on getting stuff going

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 ## PS2 MDDN 242 2018
 
-This is the code for getting started with PS2. The drop down
-selects previous saved settings. The `show data` button exposes
-the current drawing as JSON so it can be added to the code.
+This is a system where characters are drawn by the motion of 1-pixel particles tracing a path between
+four points that they approach in sequence.
 
-Replace this README with information about your alphabet.
+---
+
+###Changes:
+- Totally forgot to commit my code to git until now because I haven't been doing anything interesting to look at
+
+###Issues:
+- Currently nothing much to look at
+- Class variables are currently a bit of a mess
+- Lots of tidy-up needs doing
+
+###To-Do:
+- Commit to git more often! I've been bad about it
+- Get my particle systems drawing on-screen
--- style1.js
+++ style1.js
@@ -10,162 +10,276 @@ const canvasWidth = 960;
 const canvasHeight = 500;
 
 let savedValues = {
-  "A":
-    {
-      "box1": {
-        "position": -174,
-        "tilt": -47
-      },
-      "box2": {
-        "position": -104,
-        "tilt": -4
-      },
-      "box3": {
-        "position": -121,
-        "tilt": 58
-      }
-    },
-  "B":
-    {
-      "box1": {
-        "position": -191,
-        "tilt": -90
-      },
-      "box2": {
-        "position": -54,
-        "tilt": -45
-      },
-      "box3": {
-        "position": -12,
-        "tilt": 6
-      }
-    },
-  "C":
-    {
-      "box1": {
-        "position": -163,
-        "tilt": -84
-      },
-      "box2": {
-        "position": -191,
-        "tilt": 163
-      },
-      "box3": {
-        "position": 0,
-        "tilt": -27
-      }
-    }
-}
+	"A":
+		{
+			"vertex1": {
+				"x": -5,
+				"y": 5
+			},
+			"vertex2": {
+				"x": -5,
+				"y": -5
+			},
+			"vertex3": {
+				"x": 5,
+				"y": -5
+			},
+			"vertex4": {
+				"x": 5,
+				"y": 5
+			}
+		},
+	"B":
+		{
+			"vertex1": {
+				"x": -5,
+				"y": 5
+			},
+			"vertex2": {
+				"x": -5,
+				"y": -5
+			},
+			"vertex3": {
+				"x": 5,
+				"y": -5
+			},
+			"vertex4": {
+				"x": 5,
+				"y": 5
+			}
+		},
+	"C":
+		{
+			"vertex1": {
+				"x": -5,
+				"y": 5
+			},
+			"vertex2": {
+				"x": -5,
+				"y": -5
+			},
+			"vertex3": {
+				"x": 5,
+				"y": -5
+			},
+			"vertex4": {
+				"x": 5,
+				"y": 5
+			}
+		}
+};
 
-function setup () {
-  // create the drawing canvas, save the canvas element
-  main_canvas = createCanvas(canvasWidth, canvasHeight);
+let characters = [];
 
-  // rotation in degrees (more slider friendly)
-  angleMode(DEGREES);
+function setup() {
+	// create the drawing canvas, save the canvas element
+	main_canvas = createCanvas(canvasWidth, canvasHeight);
 
-  // create two sliders
-  pos1_slider = createSlider(-200, 200, 0);
-  tilt1_slider = createSlider(-180, 180, 0);
-  pos2_slider = createSlider(-200, 200, 0);
-  tilt2_slider = createSlider(-180, 180, 0);
-  pos3_slider = createSlider(-200, 200, 0);
-  tilt3_slider = createSlider(-180, 180, 0);
+	//pixelDensity(1); //hopefully will not need this. TODO: verify if needed
 
-  sel = createSelect();
-  sel.option('A');
-  sel.option('B');
-  sel.option('C');
-  sel.changed(letterChangedEvent);
+	// create two sliders
+	pos1_slider = createSlider(-200, 200, 0);
+	tilt1_slider = createSlider(-180, 180, 0);
+	pos2_slider = createSlider(-200, 200, 0);
+	tilt2_slider = createSlider(-180, 180, 0);
+	pos3_slider = createSlider(-200, 200, 0);
+	tilt3_slider = createSlider(-180, 180, 0);
 
-  button = createButton('show data');
-  button.mousePressed(buttonPressedEvent);
+	sel = createSelect();
+	sel.option('A');
+	sel.option('B');
+	sel.option('C');
+	sel.changed(letterChangedEvent);
 
-  // position each element on the page
-  main_canvas.parent('canvasContainer');
-  pos1_slider.parent('slider1Container');
-  tilt1_slider.parent('slider2Container');
-  pos2_slider.parent('slider3Container');
-  tilt2_slider.parent('slider4Container');
-  pos3_slider.parent('slider5Container');
-  tilt3_slider.parent('slider6Container');
+	button = createButton('show data');
+	button.mousePressed(buttonPressedEvent);
 
-  sel.parent(selectorContainer);
-  button.parent(buttonContainer);
+	// position each element on the page
+	main_canvas.parent('canvasContainer');
+	pos1_slider.parent('slider1Container');
+	tilt1_slider.parent('slider2Container');
+	pos2_slider.parent('slider3Container');
+	tilt2_slider.parent('slider4Container');
+	pos3_slider.parent('slider5Container');
+	tilt3_slider.parent('slider6Container');
+
+	sel.parent(selectorContainer);
+	button.parent(buttonContainer);
+
+	background(0x00);
+
+	characters.push(new Character("A", createVector(width/3, height/2), 2));
+	characters.push(new Character("B", createVector(width/2, height/2), 2));
+	characters.push(new Character("C", createVector(width/3*2, height/2), 2));
 }
 
 function sliderToDataObject() {
-  let obj = {};
-  obj["box1"] = {};
-  obj["box1"]["position"] = pos1_slider.value();
-  obj["box1"]["tilt"] = tilt1_slider.value();
-  obj["box2"] = {};
-  obj["box2"]["position"] = pos2_slider.value();
-  obj["box2"]["tilt"] = tilt2_slider.value();
-  obj["box3"] = {};
-  obj["box3"]["position"] = pos3_slider.value();
-  obj["box3"]["tilt"] = tilt3_slider.value();
-  return obj;
+	let obj = {};
+	obj["box1"] = {};
+	obj["box1"]["position"] = pos1_slider.value();
+	obj["box1"]["tilt"] = tilt1_slider.value();
+	obj["box2"] = {};
+	obj["box2"]["position"] = pos2_slider.value();
+	obj["box2"]["tilt"] = tilt2_slider.value();
+	obj["box3"] = {};
+	obj["box3"]["position"] = pos3_slider.value();
+	obj["box3"]["tilt"] = tilt3_slider.value();
+	return obj;
 }
 
 function dataObjectToSliders(obj) {
-  pos1_slider.value(obj["box1"]["position"]);
-  tilt1_slider.value(obj["box1"]["tilt"]);
-  pos2_slider.value(obj["box2"]["position"]);
-  tilt2_slider.value(obj["box2"]["tilt"]);
-  pos3_slider.value(obj["box3"]["position"]);
-  tilt3_slider.value(obj["box3"]["tilt"]);
+	pos1_slider.value(obj["box1"]["position"]);
+	tilt1_slider.value(obj["box1"]["tilt"]);
+	pos2_slider.value(obj["box2"]["position"]);
+	tilt2_slider.value(obj["box2"]["tilt"]);
+	pos3_slider.value(obj["box3"]["position"]);
+	tilt3_slider.value(obj["box3"]["tilt"]);
 }
 
 function letterChangedEvent() {
-  let item = sel.value();
-  dataObjectToSliders(savedValues[item]);
+	let item = sel.value();
+	dataObjectToSliders(savedValues[item]);
 }
 
 function buttonPressedEvent() {
-  let obj = sliderToDataObject();
-  json = JSON.stringify(obj, null, 2);
-  alert(json);
+	let obj = sliderToDataObject();
+	json = JSON.stringify(obj, null, 2);
+	alert(json);
 }
 
 const colorFront = [207, 222, 227];
 const colorBack = [29, 42, 46];
 
+/* supplied slider-slaved code, keeping it for later
 function drawPart(y_offset, pos, tilt) {
-  let middle_x = 2 * canvasWidth / 3;
-  let middle_y = canvasHeight / 2;
-  resetMatrix();
-  translate(middle_x + pos, middle_y + y_offset);
-  rotate(tilt);
+	let middle_x = 2 * canvasWidth / 3;
+	let middle_y = canvasHeight / 2;
+	resetMatrix();
+	translate(middle_x + pos, middle_y + y_offset);
+	rotate(tilt);
 
-  let scale = 10;
+	let scale = 10;
 
-  fill(colorFront);
-  // rect(-100,-100,100,100);
-  rect(-20*scale, -3*scale, 20*scale, 3*scale);
+	fill(colorFront);
+	// rect(-100,-100,100,100);
+	rect(-20 * scale, -3 * scale, 20 * scale, 3 * scale);
 }
 
 function drawFromSliders(y_offset, pos_slider, tilt_slider) {
-  let pos = pos_slider.value();
-  let tilt = tilt_slider.value();
-  drawPart(y_offset, pos, tilt);
+	let pos = pos_slider.value();
+	let tilt = tilt_slider.value();
+	drawPart(y_offset, pos, tilt);
 }
+*/
 
-function draw () {
-  background(colorBack);
-  fill(colorFront);
-  stroke(95, 52, 8);
+function draw() {
+	//background(colorBack);
+	//fill(colorFront);
 
-  drawFromSliders(-50, pos1_slider, tilt1_slider);
-  drawFromSliders(  0, pos2_slider, tilt2_slider);
-  drawFromSliders( 50, pos3_slider, tilt3_slider);
+	loadPixels();
+	let point;
+	for (let i = 0, l = characters.length; i < l; i++) {
+		for (let j = 0, m = characters[i].tracers.length; j < m; j++) {
+			//todo: code here to convert the following coordinates into the pixel positions
+			point = characters[i].tracers[j].position;
+			let address = round(point.x)*4 + round(point.y)*width*4;
+			//todo: ensure this portion of the code only handles pixel coordinates. Scaling is a Character problem.
+			pixels[address] = 0xFF;      //r
+			pixels[address+1] = 0xFF;    //g
+			pixels[address+2] = 0xFF;    //b
+		}
+	}
+	updatePixels();
 }
 
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
-  else if (key == '@') {
-    saveBlocksImages(true);
-  }
+	if (key === '!') {
+		saveBlocksImages();
+	}
+	else if (key === '@') {
+		saveBlocksImages(true);
+	}
+	if (key === 'P') {
+		noLoop();
+	}
+	if (key === 'S') {
+		loop();
+	}
+}
+
+class Character {
+	constructor(letter, locationVec, scale) {
+		this.scale = scale;
+		this.position = locationVec;
+		this.tracers = []; //array of tracers, x, y, current target point, current path ID
+		//vertices properties
+		this.vertPosition = []; //position of the four points, as expressed by a rotation and magnitude.
+			//not storing the rotation/magnitude portion directly because it's derived from sliders but maybe I should just
+			//store both of those and work it out from them, for smoother interpolation.
+		//Actually come to think of it I can have the character's overall position and add + that the vertices positions,
+		//particles and points remain screenspace, so character scale is a MAGNITUDE MULTIPLIER, and the interpolation
+		//values are from rotation and magnitude per point, applied to the vector - hooray for pvectors!
+		this.vertVariance = []; //how much variance there is in tracer targeting
+		this.vertMargin = []; //margin of error for particles approaching a point
+		this.targetTurnRate = []; //speed of tracer turning on that path in radians
+		this.chromaticEffectStrength = []; //fun colourful aberration effect.
+
+		//todo: consider a starting angle?
+
+	}
+	setValues() {
+
+	}
+	update() {
+		//tracer update calls go here
+		for (let i = 0, j = this.tracers.length; i < j; i++) {
+			this.tracers[i].update();
+		}
+		//mutation logic probably goes here
+	}
+
+	mutateTo() { //todo: implement this for interpolating between the current values and some target values by a constant rate.
+		//can this be made smooth and cool? How this will work is you'll specify the target values
+		// and it will mutate towards them by one step, no reason to make it care about what prior character was
+
+		//use multiplied difference with a small linear portion, so it slows down on approach.
+		//should particles approach the new position? That means targets need to be an offsetvector from vertices vector. that's doable though.
+	}
+}
+
+class Vertex { //TODO: transfer character vertex components to here
+
+}
+
+class Tracer {
+	constructor(character, targetIndex, location) {
+		this.parentCharacter = character;
+		this.targetIndex = targetIndex;
+		this.location = location; //todo: maybe just make this targetIndex-1
+		this.targetPosition = createVector(0,0);
+		this.targetMargin;
+		this.changeTarget(this.targetIndex);
+		this.velocity = createVector(0,1);
+	}
+	update() {
+		this.location.add(this.velocity);
+
+		//todo: make it check that the angleBetween this location and the target point matches the heading() of velocity
+		let dir = this.location.angleBetween(this.targetPosition)
+					- this.velocity.heading();
+		let turn = this.parentCharacter.targetTurnRate[this.targetIndex];
+		this.velocity.rotate(dir < -0.1 ? -turn : dir > 0.1 ? turn : 0);
+		if (this.location.dist(this.targetPosition) < this.targetMargin) {
+			this.targetIndex += (this.targetIndex+1)%4;
+			this.changeTarget(this.targetIndex);
+		}
+
+	}
+
+	changeTarget(n) { //TODO: maybe make this work on an offset instead of an absolute target, so that the splining works better?
+		this.targetPosition.set(this.parentCharacter.vertPosition[n].copy()
+			.add(createVector(random(this.parentCharacter.vertVariance[n]),0).rotate(random(TAU))));
+		this.targetMargin = this.parentCharacter.vertMargin[n];
+	}
+
 }
