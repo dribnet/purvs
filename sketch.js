a9215671a43ca3b7c5a1bd483ea23fd163ab5cc9
Guest User <Guest@wg401-c-07-07.mddn.vuw.ac.nz>
2018-04-10 15:20:44 +1200

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -13,9 +13,11 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "v1":130,200,
+  "v2":230,200,
+  "v3":230,300,
+  "v4":280,250,
+  "v5":130,300
 }
 
 const letterB = {
@@ -49,14 +51,30 @@ function setup () {
 }
 
 function drawLetter(posx, posy, scale, letterData) {
+  
+  //LetterA trial
+  let v1 = letterData["v1"];
+  let v2 = letterData["v2"];
+  let v3 = letterData["v3"];
+  let v4 = letterData["v4"];
+  let v5 = letterData["v5"];
+
+  beginShape();
+    vertex(v1);
+    vertex(v2);
+    vertex(v3);
+    vertex(v4);
+    vertex(v5);
+  endShape(CLOSE);
+
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  // size2 = letterData["size"];
+  //let pos2x = posx + letterData["offsetx"];
+  //let pos2y = posy + letterData["offsety"];
 
   // draw two circles
-  ellipse(posx, posy, 150, 150);
-  ellipse(pos2x, pos2y, size2, size2);
+  //ellipse(posx, posy, 150, 150);
+  //ellipse(pos2x, pos2y, size2, size2);
 }
 
 function draw () {
@@ -67,10 +85,23 @@ function draw () {
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
 
+  drawLetter(0,0,0,letterA);
+
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, 10, letterA);
-  drawLetter(center_x      , center_y, 10, letterB);
-  drawLetter(center_x + 250, center_y, 10, letterC);
+  //drawLetter(center_x - 250, center_y, 10, letterA);
+  //drawLetter(center_x      , center_y, 10, letterB);
+  //drawLetter(center_x + 250, center_y, 10, letterC);
+
+  //let width = 100;
+
+  //beginShape();
+    //vertex(130,200);
+    //vertex(230,200);
+    //vertex(230,300);
+    //vertex(180,250);
+    //vertex(130,300);
+  //endShape(CLOSE);
+
 }
 
 function keyTyped() {
