52695e720e365cc4ebf276968cd1247253ad432d
Guest User <Guest@wg401-h-06-07.mddn.vuw.ac.nz>
2018-04-10 14:55:31 +1200

    adding part2 files

---
+++ letters.js
@@ -0,0 +1,22 @@
+const alphabet = {
+  "default": {
+      "size": 10,
+      "offsetx": 0,
+      "offsety": 0
+  },
+  "A": {
+      "size": 50,
+      "offsetx": 0,
+      "offsety": 23
+  },
+  "B": {
+      "size": 100,
+      "offsetx": 0,
+      "offsety": -98
+  },
+  "C": {
+      "size": 68,
+      "offsetx": 15,
+      "offsety": 0
+  }
+}
