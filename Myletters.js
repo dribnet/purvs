2bafb49c140a9b1864d412b66cab59247496156b
Guest User <Guest@wg401-j-01-04.mddn.vuw.ac.nz>
2018-04-10 14:36:53 +1200

    Finished Alphabet

---
+++ Myletters.js
@@ -0,0 +1,188 @@
+const letterA = {
+
+  "TL": 0,
+  "TM": 1,
+  "TR": 0,
+  "ML": 0,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 0,
+  "BR": 1,
+  "Square": 0,
+  "Circle": 0,
+  "Triangle": 1
+}
+
+const letterB = {
+  "TL": 1,
+  "TM": 0,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 0,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+
+const letterC = {
+  "TL": 0,
+  "TM": 1,
+  "TR": 1,
+  "ML": 1,
+  "MM": 0,
+  "MR": 0,
+  "BL": 0,
+  "BM": 1,
+  "BR": 1,
+  "Square": 0,
+  "Circle": 1,
+  "Triangle": 0
+}
+
+const letterD = {
+  "TL": 1,
+  "TM": 1,
+  "TR": 0,
+  "ML": 1,
+  "MM": 0,
+  "MR": 1,
+  "BL": 1,
+  "BM": 1,
+  "BR": 0,
+  "Square": 1,
+  "Circle": 1,
+  "Triangle": 0
+}
+
+const letterE = {
+  "TL": 1,
+  "TM": 1,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 1,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+
+const letterF= {
+  "TL": 1,
+  "TM": 1,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 0,
+  "BR": 0,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+const letterG = {
+  "TL": 1,
+  "TM": 1,
+  "TR": 0,
+  "ML": 1,
+  "MM": 0,
+  "MR": 1,
+  "BL": 1,
+  "BM": 1,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 1,
+  "Triangle": 0
+}
+const letterH = {
+  "TL": 1,
+  "TM": 0,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 1,
+  "BL": 1,
+  "BM": 0,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+const letterI = {
+  "TL": 1,
+  "TM": 1,
+  "TR": 1,
+  "ML": 0,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 1,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+const letterJ = {
+  "TL": 0,
+  "TM": 1,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 1,
+  "BR": 0,
+  "Square": 1,
+  "Circle": 1,
+  "Triangle": 0
+}
+const letterK = {
+  "TL": 1,
+  "TM": 0,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 0,
+  "BL": 1,
+  "BM": 0,
+  "BR": 1,
+  "Square": 0,
+  "Circle": 0,
+  "Triangle": 1
+}
+const letterL = {
+  "TL": 1,
+  "TM": 0,
+  "TR": 0,
+  "ML": 1,
+  "MM": 0,
+  "MR": 0,
+  "BL": 1,
+  "BM": 1,
+  "BR": 0,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 0
+}
+const letterM = {
+  "TL": 1,
+  "TM": 1,
+  "TR": 1,
+  "ML": 1,
+  "MM": 1,
+  "MR": 1,
+  "BL": 1,
+  "BM": 0,
+  "BR": 1,
+  "Square": 1,
+  "Circle": 0,
+  "Triangle": 1
+}
