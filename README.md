31eae814a967fd537e6e5f7e82edb98e8956b68a
Cody Stiles <stilescody@myvuw.ac.nz>
2018-04-10 15:05:42 +1200

    Whole alphabet
    I have started working on the whole alphabet, this is simply a progress update.

--- README.md
+++ README.md
@@ -3,14 +3,11 @@
 Block Alphabet
 
 <<<<<<< HEAD
-This code is just my 'A'. 'B', and 'C' drawn out because I haven't figured out how to use the correct code yet, but this shows what I am trying to achieve.
+I have started creating my alphabet with a lot of help from a tutor which I am extremely thankful for.
 =======
 In the process of completing the entire alphabet, I have [made what changes?].
 This included updating the parameters to [add/remove parameters for?].
 >>>>>>> upstream/part2
 
-The three parameters per letter are now:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+My parameters are the 12 boxes that are either on or off depending on which letter they need to make.
 
--- draw_letters.js
+++ draw_letters.js
@@ -1,5 +1,5 @@
-const colorFront  = "#199cff";
-const colorStroke = "#233f11";
+const colorFront  = "pink";
+const colorStroke = "#84ceab";
 
 /*
  * Draw the letter given the letterData
@@ -15,11 +15,56 @@ function drawLetter(letterData) {
   strokeWeight(4);
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50+letterData["offsetx"];
-  let pos2y = 150+letterData["offsety"];
+  //let size2 = letterData["size"];
+  //let pos2x = 50+letterData["offsetx"];
+  //let pos2y = 150+letterData["offsety"];
 
   // draw two circles
-  ellipse(50, 150, 100, 100);
-  ellipse(pos2x, pos2y, size2, size2);
+  //ellipse(50, 150, 100, 100);
+ // ellipse(pos2x, pos2y, size2, size2);
+ if(letterData["b1"]==1){
+   //rect at location 1
+   rect(0,0,30,30);
+ }
+ if(letterData["b2"]==1){
+ rect(30,0,30,30);
+}
+ if(letterData["b3"]==1){
+ rect(60,0,30,30);
+}
+if(letterData["b4"]==1){
+ rect(0,30,30,30);
+}
+if(letterData["b5"]==1){
+ rect(30,30,30,30);
+}
+if(letterData["b6"]==1){
+ rect(60,30,30,30);
+}
+if(letterData["b7"]==1){
+ rect(0,60,30,30);
+}
+if(letterData["b8"]==1){
+ rect(30,60,30,30);
+}
+if(letterData["b9"]==1){
+ rect(60,60,30,30);
+}
+if(letterData["b10"]==1){
+ rect(0,90,30,30);
+}
+if(letterData["b11"]==1){
+ rect(30,90,30,30);
+}
+if(letterData["b12"]==1){
+ rect(60,90,30,30);
+}
+
+//loci 4 will be at 0, 30
+//loci 5 will be at 30, 30
+
+
+
+ rect(30,30,30,30);
+
 }
