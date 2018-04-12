c321db59c4e8830b7531f29c87b6ce87773c0d6c
Guest User <Guest@wg401-g-01-06.mddn.vuw.ac.nz>
2018-04-12 14:31:59 +1200

    Bezier Tests

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## PS2 MDDN 242 2018
-Wanted to extend what I was doing before and how it references Gestalt Principles.
-Testing with begin shape.
+Continuing the use of beginshape and gestalt principles going to try use only one quadraticVertex which requires seven parameters. 2 for the inital vertex placement, 2 for the control points, 2 for the 2nd anchor point locations and 1 for stroke weight.
--- sketch2.js
+++ sketch2.js
@@ -24,19 +24,21 @@ function draw () {
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
   fill(colorFront);
-  rect(center_x, center_y, 150, 150);
+  rect(center_x, center_y, 150, 150)
+  //translate(width/2,height/2);
+  noFill();
+  //stroke(255,0,225)
 
-  fill(colorStroke)
-  var numVertices = 3;
-  var spacing = 360 / numVertices
-  translate(width/2,height/2);
-  //rotate(360)
+  strokeWeight(20);
   beginShape();
-  for(var i = 0; i < 25; i++){
-  	var x = cos(radians(i * spacing)) * 100;
-  	var y = sin(radians(i * spacing)) * 100;
-  	vertex(x,y);
-  }
+  vertex(405,175)
+  quadraticVertex(700,360,405,325)
   endShape();
+
+  //references
+  //point(405,175) //1
+  //point(555,175) //2
+  //point(405,325) //3
+  //point(555,325) //4
 
 }
