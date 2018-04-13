415aee56bb5f9ffc7ad6d0c57640d2e190929278
Molisang <molijun801@gmail.com>
2018-04-13 12:42:52 +1200

    Part2 - 26 alphabets

--- README.md
+++ README.md
@@ -3,15 +3,13 @@
 <<<<<<< HEAD
 I was inspired by a Dutch furniture designer and architect, Gerrit Thomas Rietveld, especially his famous design, Red and Blue Chair. So I imitate the colors from the design to show my respect. Also, I utilized the transpancy to do this design in order to show that each color can influence each other. Even though red, blue and yellow are very simple color, they still have some new creation for each combination. 
 =======
-(Replace this README with information about your alphabet. This is an example.)
 
-In the process of completing the entire alphabet, I have [made what changes?].
-This included updating the parameters to [add/remove parameters for?].
+In the process of completing the entire alphabet, I have made 3 changes for the alphabet, including sizes of the arcs, x and y of the arcs.
 
 The three parameters per letter are now:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+  * `size` : radius of the arcs
+  * `x` : x offset of the arcs
+  * `y` : y offset of the arcs
 >>>>>>> upstream/part2
 
 In this design, I did not utilize rectangles because I don't want to make my design looks cold. Personally, I think the color combination of red, blue and yellow is a little bit cold, it is not as soft as some light colors. Therefore, I used half circles as elements for it to make it more acceptable.
--- draw_letters.js
+++ draw_letters.js
@@ -1,5 +1,5 @@
-const colorFront  = "#199cff";
-const colorStroke = "#233f11";
+// const colorFront  = "#199cff";
+// const colorStroke = "#233f11";
 
 /*
  * Draw the letter given the letterData
@@ -8,18 +8,57 @@ const colorStroke = "#233f11";
  * following bounding box guideline:
  * from (0,0) to (100, 200)
  */
+
+const color1  = 'rgba(250,210,0, 0.75)';
+const color2 = 'rgba(237,33,44, 0.75)';
+const color3 = 'rgba(31,36,102, 0.75)';
+// const colorBack   = "#e3eded";
+const colorStroke = "#233f11";
+
 function drawLetter(letterData) {
+  angleMode(DEGREES);
+
+  let posx = 0;
+  let posy = 0;
+  let size = 1;
+
+
+  let size2 = letterData["size"];
+  let size3 = letterData["size3"];
+  let size4 = letterData["size4"];
+
+  let posx1 = posx + letterData["x1"];
+  let posy1 = posy + letterData["y1"];
+  let posx2 = posx + letterData["x2"];
+  let posy2 = posy + letterData["y2"];
+  let posx3 = posx + letterData["x3"];
+  let posy3 = posy + letterData["y3"];
+
+  noStroke();
+
   // color/stroke setup
-  fill(colorFront);
-  stroke(colorStroke);
-  strokeWeight(4);
+  // fill(colorFront);
+  // stroke(colorStroke);
+  // strokeWeight(4);
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50+letterData["offsetx"];
-  let pos2y = 150+letterData["offsety"];
+  // let size2 = letterData["size"];
+  // let pos2x = 50+letterData["offsetx"];
+  // let pos2y = 150+letterData["offsety"];
 
   // draw two circles
-  ellipse(50, 150, 100, 100);
-  ellipse(pos2x, pos2y, size2, size2);
+  // ellipse(50, 150, 100, 100);
+  // ellipse(pos2x, pos2y, size2, size2);
+
+
+  fill(color2);
+  arc(posx1, posy1, size2, size2,180,360);//red
+
+  fill(color3);
+  arc(posx3, posy3, size4, size4, 0, 180);//blue
+  
+  fill(color1);
+  arc(posx2, posy2, size3, size3, 90, 270);//yellow
+  
+
 }
