a8de98e469afdb8a55691687c292ff7709723ffc
Caleb Moffatt <eythous@gmail.com>
2018-04-13 13:41:19 +1200

    Completed Alphabet and numbers.

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 ## PS2 MDDN 242 2018
 
-My letters are made up of 4 lines. The first point is set on each letter and parameters make up the other points. each letter can be made from these 4 lines and if the letter only requires 3 lines, then the fourth is used as a decoration.
+The letters of my alphabet are very literal, Im not trying to create an abstract alphabet but rather one that can be easily read. Due to the limitations on how ive set out my parameters, ive had each letter start in the bottom left of the bounding box. The first two lines share some parameters and as such are linked together. The third and fourth lines can be positioned as please. Some letters can be made up of three lines while others use four. With this in mind, the fourth line is used as a decorational line to make the letter look more interesting. The caveat of this is when the starting line is a unique line, such as in the letter 'X' where going from the bottom left to top right only requires 1 line. 
 
-<<<<<<< HEAD
+The numbers are a bit more abstract but are still pretty legible.
+
+=======
 The 12 parameters per letter:
   * `point1X` : X offset of the first point
   * `point1Y` : Y offset of the first point
+
   * `point2X` : X offset of the second point
   * `point2Y` : Y offset of the second point
+
   * `point3X` : X offset of the third point
-  * `point3Y` : Y offset of the third point   
+  * `point3Y` : Y offset of the third point 
+
   * `point4X` : X offset of the fourth point
   * `point4Y` : Y offset of the fourth point
+
   * `point5X` : X offset of the fifth point
   * `point5Y` : Y offset of the fifth point
+
   * `point6X` : X offset of the sixth point
   * `point6Y` : Y offset of the sixth point
 
 =======
-In the process of completing the entire alphabet, I have no changes from my initial idea. The current design philosiphies employed are working well from converting from drawn sketches to alphabet
+
+
 
--- letters.js
+++ letters.js
@@ -241,15 +241,15 @@ const alphabet = {
   },
   "T": {
       //second line
-      "point1X": 0, "point1Y": 35,
-      "point2X": 100, "point2Y": 55,
+      "point1X": 50, "point1Y": 35,
+      "point2X": 100, "point2Y": 35,
       //third line
-      "point3X": 0, "point3Y": 165,
-      "point4X": 100, "point4Y": 145,
+      "point3X": 0, "point3Y": 35,
+      "point4X": 50, "point4Y": 35,
       
       //final line (decoration)
-      "point5X": 100, "point5Y": 130,
-      "point6X": 100, "point6Y": 160
+      "point5X": 15, "point5Y": 50,
+      "point6X": 45, "point6Y": 50
   },
   "U": {
       //second line
@@ -278,15 +278,171 @@ const alphabet = {
   "W": {
       //second line
       "point1X": 0, "point1Y": 35,
-      "point2X": 65, "point2Y": 165,
+      "point2X": 0, "point2Y": 165,
       //third line
-      "point3X": 35, "point3Y": 165,
-      "point4X": 100, "point4Y": 35,
+      "point3X": 0, "point3Y": 165,
+      "point4X": 70, "point4Y": 35,
       
       //final line (decoration)
-      "point5X": 100, "point5Y": 35,
+      "point5X": 30, "point5Y": 165,
+      "point6X": 100, "point6Y": 35
+  },
+  "X": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 165,
+      //third line
+      "point3X": 0, "point3Y": 35,
+      "point4X": 100, "point4Y": 165,
+      
+      //final line (decoration)
+      "point5X": 30, "point5Y": 100,
+      "point6X": 70, "point6Y": 100
+  },
+  "Y": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 165,
+      //third line
+      "point3X": 50, "point3Y": 100,
+      "point4X": 0, "point4Y": 35,
+      
+      //final line (decoration)
+      "point5X": 60, "point5Y": 70,
+      "point6X": 28, "point6Y": 110
+  },
+  "Z": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 165,
+      "point4X": 100, "point4Y": 165,
+      
+      //final line (decoration)
+      "point5X": 30, "point5Y": 100,
+      "point6X": 70, "point6Y": 100
+  },
+  "1": {
+      //second line
+      "point1X": 0, "point1Y": 35,
+      "point2X": 40, "point2Y": 75,
+      //third line
+      "point3X": 0, "point3Y": 165,
+      "point4X": 40, "point4Y": 165,
+      
+      //final line (decoration)
+      "point5X": 15, "point5Y": 48,
+      "point6X": 15, "point6Y": 80
+  },
+  "2": {
+      //second line
+      "point1X": 0, "point1Y": 135,
+      "point2X": 100, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 165,
+      "point4X": 100, "point4Y": 165,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 35,
+      "point6X": 100, "point6Y": 35
+  },
+  "3": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 165,
+      "point4X": 0, "point4Y": 125,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 90,
+      "point6X": 50, "point6Y": 90
+  },
+  "4": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 35,
+      "point4X": 0, "point4Y": 100,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 100,
+      "point6X": 100, "point6Y": 100
+  },
+  "5": {
+      //second line
+      "point1X": 100, "point1Y": 100,
+      "point2X": 0, "point2Y": 100,
+      //third line
+      "point3X": 0, "point3Y": 35,
+      "point4X": 0, "point4Y": 100,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 35,
+      "point6X": 100, "point6Y": 35
+  },
+  "6": {
+      //second line
+      "point1X": 0, "point1Y": 35,
+      "point2X": 100, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 100,
+      "point4X": 100, "point4Y": 100,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 165,
+      "point6X": 100, "point6Y": 100
+  },
+  "7": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 80, "point3Y": 50,
+      "point4X": 95, "point4Y": 50,
+      
+      //final line (decoration)
+      "point5X": 30, "point5Y": 100,
+      "point6X": 70, "point6Y": 100
+  },
+  "8": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 0, "point3Y": 35,
+      "point4X": 100, "point4Y": 165,
+      
+      //final line (decoration)
+      "point5X": 0, "point5Y": 165,
       "point6X": 100, "point6Y": 165
   },
+  "9": {
+      //second line
+      "point1X": 100, "point1Y": 35,
+      "point2X": 0, "point2Y": 35,
+      //third line
+      "point3X": 50, "point3Y": 100,
+      "point4X": 0, "point4Y": 35,
+      
+      //final line (decoration)
+      "point5X": 85, "point5Y": 35,
+      "point6X": 70, "point6Y": 55
+  },
+  "0": {
+      //second line
+      "point1X": 0, "point1Y": 35,
+      "point2X": 100, "point2Y": 55,
+      //third line
+      "point3X": 0, "point3Y": 165,
+      "point4X": 100, "point4Y": 145,
+      
+      //final line (decoration)
+      "point5X": 100, "point5Y": 55,
+      "point6X": 100, "point6Y": 145
+  }
 }
 
 
