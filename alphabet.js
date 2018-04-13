c7d167d5d40a7c038f06eb2c3eb6f6e94eb589fb
Guest User <Guest@wg401-a-06-10.mddn.vuw.ac.nz>
2018-04-13 14:13:40 +1200

    the alphabet

--- alphabet.js
+++ alphabet.js
@@ -1,8 +1,8 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
-const colorLines   = "#000090";
+const colorBack    = "#01BB92";
+const colorLines   = "#1F2354";
 
 /* 
  * do not edit this rest of this file, instead edit the letter
@@ -59,7 +59,7 @@ function draw () {
     }
     line(left_margin, 200, right_margin, 200);
 
-    translate(left_margin+first_letter_offset_x, 0);
+    translate(left_margin+first_letter_offset_x, 50);
     for(let i=left_margin+first_letter_offset_x; i<right_margin-x_step+1; i+=x_step) {
       let letter = letters[cur_letter_index];
       if (letter in alphabet) {
