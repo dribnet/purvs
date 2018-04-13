076daeaedd48e2d06a2260b6ace33a7931884004
Guest User <Guest@vs-320-09.mddn.vuw.ac.nz>
2018-04-13 14:21:11 +1200

    Added the interpolation code
    Added the interpolation code and adopted it with my textstyle.

--- draw_letters.js
+++ draw_letters.js
@@ -13,6 +13,7 @@ function drawLetter(letterData) {
   fill(colorFront);
   stroke(colorStroke);
   strokeWeight(4);
+  strokeCap(ROUND);
 
   let ptx = 0;
   let pty = 0;
@@ -47,6 +48,34 @@ function drawLetter(letterData) {
   line(pos3x,pos3y,pos4x,pos4y);
   line(pos5x,pos5y,pos6x,pos6y);
   line(pos7x,pos7y,pos8x,pos8y);
+}
+function interpolate_letter(percent, oldObj, newObj){
+let new_letter = {};
+new_letter["ptx"] = map(percent, 0, 100, oldObj["ptx"], newObj["ptx"]);
+new_letter["pty"] = map(percent, 0, 100, oldObj["pty"], newObj["pty"]);
+
+new_letter["ptx2"] = map(percent, 0, 100, oldObj["ptx2"], newObj["ptx2"]);
+new_letter["pty2"] = map(percent, 0, 100, oldObj["pty2"], newObj["pty2"]);
+
+new_letter["ptx3"] = map(percent, 0, 100, oldObj["ptx3"], newObj["ptx3"]);
+new_letter["pty3"] = map(percent, 0, 100, oldObj["pty3"], newObj["pty3"]);
+
+new_letter["ptx4"] = map(percent, 0, 100, oldObj["ptx4"], newObj["ptx"]);
+new_letter["pty4"] = map(percent, 0, 100, oldObj["pty4"], newObj["pty4"]);
+
+new_letter["ptx5"] = map(percent, 0, 100, oldObj["ptx5"], newObj["ptx5"]);
+new_letter["pty5"] = map(percent, 0, 100, oldObj["pty5"], newObj["pty5"]);
+
+new_letter["ptx6"] = map(percent, 0, 100, oldObj["ptx6"], newObj["ptx6"]);
+new_letter["pty6"] = map(percent, 0, 100, oldObj["pty6"], newObj["pty6"]);
+
+new_letter["ptx7"] = map(percent, 0, 100, oldObj["ptx7"], newObj["ptx7"]);
+new_letter["pty7"] = map(percent, 0, 100, oldObj["pty7"], newObj["pty7"]);
+
+new_letter["ptx8"] = map(percent, 0, 100, oldObj["ptx8"], newObj["ptx8"]);
+new_letter["pty8"] = map(percent, 0, 100, oldObj["pty8"], newObj["pty8"]);
+
+return new_letter;
 }
 
 
--- interaction.js
+++ interaction.js
@@ -1,8 +1,8 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
-const colorLines   = "#000090";
+const colorBack    = "#ffbc14";
+const colorLines   = "#000000";
 
 /* 
  * do not edit this rest of this file, instead edit the letter
