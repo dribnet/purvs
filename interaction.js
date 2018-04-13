23bad518b577edbb330dce01f84592d6f46f134c
Guest User <Guest@wg401-a-06-10.mddn.vuw.ac.nz>
2018-04-13 14:30:03 +1200

    the alphabet

--- interaction.js
+++ interaction.js
@@ -1,8 +1,8 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
-const colorLines   = "#000090";
+const colorBack    = "#01BB92";
+const colorLines   = "#1F2354";
 
 /* 
  * do not edit this rest of this file, instead edit the letter
