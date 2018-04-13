7317ca6e055893ec139a1884994892dc79f548d7
Bradley <BradleyPeary@gmail.coms>
2018-04-13 12:45:16 +1200

    Added alphabet

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 ## PS2 MDDN 242 2018
 
-(Replace this README with information about your alphabet. This is an example.)
+My alphabet is a set of 4 circles which are all connected by lines.
 
-In the process of completing the entire alphabet, I have [made what changes?].
-This included updating the parameters to [add/remove parameters for?].
+I have 8 parameters per letter:
 
-The three parameters per letter are now:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+x : x of first circle
+y : y of first circle
+x2 : x of second circle
+y2 : y of second circle
+etc..
--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,5 @@
-const colorFront  = "#199cff";
-const colorStroke = "#233f11";
-
+const colorFront  = "#ffffff";
+const colorStroke = "#ffffff";
 /*
  * Draw the letter given the letterData
  *
@@ -15,11 +14,40 @@ function drawLetter(letterData) {
   strokeWeight(4);
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50+letterData["offsetx"];
-  let pos2y = 150+letterData["offsety"];
+  let posx = letterData["x"];
+  let posy = letterData["y"];
 
-  // draw two circles
-  ellipse(50, 150, 100, 100);
-  ellipse(pos2x, pos2y, size2, size2);
+  let pos2x = letterData["x2"];
+  let pos2y = letterData["y2"];
+
+  let pos3x = letterData["x3"];
+  let pos3y = letterData["y3"];
+
+  let pos4x = letterData["x4"];
+  let pos4y = letterData["y4"];
+
+  // draw four circles
+
+  ellipse(posx, posy, 13, 13);
+  ellipse(pos2x, pos2y, 13, 13);
+  ellipse(pos3x, pos3y, 13, 13);
+  ellipse(pos4x, pos4y, 13, 13);
+
+  stroke(255,255,255,150)
+
+  line(posx,posy,pos2x,pos2y)
+  line(posx,posy,pos3x,pos3y)
+  line(posx,posy,pos4x,pos4y)
+
+  line(pos2x,pos2y,posx,posy)
+  line(pos2x,pos2y,pos3x,pos3y)
+  line(pos2x,pos2y,pos4x,pos4y)
+
+  line(pos3x,pos3y,posx,posy)
+  line(pos3x,pos3y,pos2x,pos2y)
+  line(pos3x,pos3y,pos4x,pos4y)
+
+  line(pos4x,pos4y,posx,posy)
+  line(pos4x,pos4y,pos3x,pos3y)
+  line(pos4x,pos4y,pos2x,pos2y)
 }
