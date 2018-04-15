888f9d0a3faef52401302b9be342e3fa4480c8a5
Guest User <Guest@wg401-g-03-06.mddn.vuw.ac.nz>
2018-04-15 16:14:35 +1200

    Changed the code to represent my chosen font

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 (Replace this README with information about your alphabet. This is an example.)
 
-Ive had an idea for how i wanted to do my alphabet for a while. I want to use 1-3 triangles per letter changing the rotation, shape and fill throughout.
-
-I havent put in enough time yet to understand the code for what we did in class (or to transition it to what i would like it to so) so for now i hav just hard coded my triangles in in the positions i would like them to be.
-
-Im pretty sure its possible with 12 parameters by keeping the starting values of the triangles the same howeverill be able to tell a little more once getting into the groove of things.
+Through a bit of trial and error im working out how to do my own letters by changing around the code provided. 
+I have changed my idea a little bit to just iusing two triangles at most as this will allow me to change a max of 12 parameters through changing the six points of the two triangles. 
+So far the most parameters i have had to change is 10 (by keeping the starting point the same this saves me 2 parameters.)
--- sketch.js
+++ sketch.js
@@ -1,43 +1,112 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
 
+/* 
+ * my three variable per letter are:
+ *
+   size: radius of the second circle (in pixels)
+   offsetx: x offset (in pixels) of the second circle
+            relative to the first one
+   offsety: y offset (in pixels) of the second circle
+            relative to the first one
+ *
+ */
 
+const letterA = {
+ "pbx": +30,
+ "pby": -25,
+ "pcx": +90,
+ "pcy": +140,
+ "p1x": -40,
+ "p1y": +120,
+ "p2x": +30,
+ "p2y": -25,
+ "p3x": +90,
+ "p3y": +120,
+}
+
+const letterB = {
+ "pbx": -40,
+ "pby": -220,
+ "pcx": +90,
+ "pcy": -180,
+ "p1x": -40,
+ "p1y": +50,
+ "p2x": -40,
+ "p2y": -60,
+ "p3x": +50,
+ "p3y": -10,
+}
+
+ const letterC = {
+ // "pbx": +30,
+ // "pby": -25,
+ // "pcx": +90,
+ // "pcy": +140,
+ "p1x": -40,
+ "p1y": +50,
+ "p2x": +90,
+ "p2y": -25,
+ "p3x": +90,
+ "p3y": +120,
+
+}
+
+const colorFront  = "#194284";
+const colorBack   = "#e3eded";
+const colorStroke = "#194284";
 
 function setup () {
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
- background(180);
- 
+
+  // color/stroke setup
+  fill(colorFront);
+  stroke(colorStroke);
+  strokeWeight(4);
+
   // with no animation, redrawing the screen is not necessary
   noLoop();
 }
 
 function drawLetter(posx, posy, scale, letterData) {
   // determine parameters for second circle
-  // let size2 = letterData["size"];
-  // let pos2x = posx + letterData["offsetx"];
-  // let pos2y = posy + letterData["offsety"];
+  //First Triangle
+  let posbx = posx + letterData["pbx"];
+  let posby = posx + letterData["pby"];
+  let poscx = posx + letterData["pcx"];
+  let poscy = posx + letterData["pcy"];
+
+  //Second Triangle
+  let size2 = letterData["size"];
+  let pos2x = posx + letterData["p1x"];
+  let pos2y = posy + letterData["p1y"];
+  let pos3x = posx + letterData["p2x"]
+  let pos3y = posy + letterData["p2y"]
+  let pos4x = posx + letterData["p3x"]
+  let pos4y = posy + letterData["p3y"]
+  
   
 
   // draw two circles
-  // rect(480, 250 , 150, 150);
-  // rect(pos2x, pos2y, size2, size2);
+  triangle(posx -40, posy +120, posbx, posby, poscx, poscy);
+  triangle(pos2x, pos2y, pos3x, pos3y, pos4x, pos4y);
+
 }
 
 function draw () {
-  
-noStroke();
-fill(0);
-//A
-triangle(100, 400 ,200, 200, 300, 400);
-//B
-triangle(450, 400, 450, 250, 600, 325);
-triangle(450, 400, 450, 200, 550, 250);
-//C
-triangle(900, 400, 900, 200, 700, 300 );
-fill(180);
-triangle(850, 350, 850, 250, 950, 300);
+  // clear screen
+  background(colorBack);
+
+  // compute the center of the canvas
+  let center_x = canvasWidth / 2;  
+  let center_y = canvasHeight / 2;
+
+  // draw the letters A, B, C from saved data
+  drawLetter(center_x - 250, center_y, 10, letterA);
+  drawLetter(center_x      , center_y, 10, letterB);
+  drawLetter(center_x + 250, center_y, 10, letterC);
 }
 
 function keyTyped() {
@@ -47,4 +116,4 @@ function keyTyped() {
   else if (key == '@') {
     saveBlocksImages(true);
   }
-}
+}Dta
