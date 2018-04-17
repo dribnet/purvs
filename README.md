54b389963b7e926ceaee2bcd32b7dd4caa742954
Guest User <Guest@wg401-b-01-06.mddn.vuw.ac.nz>
2018-04-17 13:36:33 +1200

    s

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 ## PS2 MDDN 242 2018
 
-<<<<<<< HEAD
+I have completed the alphabet using my geometric Mayan-inspired font. I have chosen a gold colour scheme which draws back on this inspiration. I have
+spent a lot of time ensuring my letters animate smoothly and consistently with the rectangles all remaining in a position relative to each other. My main challenge in creating this alphabet were creating letters which usually consist of angles such as X and Y, when it came to creating these letters I opted for a more abstract approach using my arrangement of rectangles to resemble these letter-forms. Another challenge I had was ensuring each letter and number looked different from each other, such as 8 and B and O and 0. To overcome this challenge I implemented the use of my thinner rectangles, using their spacing to change up my letter forms (for example; 8 has more negative upper spacing than B). I have also put time into refining my code to make it more readable and clearly labelling each section of code, as well as naming variables appropriately. I have also added a few parameters, to make a total of nine. In terms of improvements, I would like to improve the aesthetic of my font to make it look more ancient. I am thinking I could possibly do something with the fill to achieve this look.
 
-I have refined and renamed my parameters so that they are all effective and are all named correctly. 
-After completing most of the alphabet in code today I realized I have to include a few more parameters as forming letters such as S, X, Z has been too challenging. I may simply have to map one of the variables to another of the rectangles, and control them both through one parameter. I am happy however with how my letters are animating they look smooth and clean. I now have more work to do figuring out what modifications my variables need in order to communicate the entire alphabet effectively.
+My nine parameters.
 
-
-I have refined my parameters to seven and they are: 
-
+      * "lengthV1": The length of the first vertical rect.
       * "lengthV2": The length of the second vertical rect.
       * "offsetxV1": x offset of the first vertical rect.
       * "offsetxV2": x offset of the second vertical rect.
       * "offsetyH1": y offset of the first horizontal rect.
       * "offsetyH2": y offset of the second horizontal rect.
       * "offsetyH3": y offset of the third horizontal rect.
-      * "offsetyV2" y offset of the secpond vertical rect.
+      * "offsetyV1" y offset of the first vertical rect.  
+      * "offsetyV2" y offset of the second vertical rect.
 
--- draw_letters.js
+++ draw_letters.js
@@ -1,46 +1,52 @@
-const colorFront  = "#199cff";
-const colorStroke = "#233f11";
+const colorFront  = "#fdbc00";
 
-/*
- * Draw the letter given the letterData
- *
- * Letters should always be drawn with the
- * following bounding box guideline:
- * from (0,0) to (100, 200)
- */
 function drawLetter(letterData) {
-  // color/stroke setup
+
+  // colour/stroke setup.
 
   let posx=0;
   let posy=0;
-  let scale = 0;
   fill(colorFront);
   noStroke ();
 
-  // determine parameters for second circle
-  let size2 = letterData["lengthV2"];
-  let pos3x = posx + letterData["offsetxV1"];
-  let pos4x = posx + letterData["offsetxV2"];
-  let pos2y = posy + letterData["offsetyH1"];
-  let pos3y = posy + letterData["offsetyH2"];
-  let pos4y = posy + letterData["offsetyH3"];
-  let pos5y = posy + letterData["offsetyV2"];
+  // determine parameters for vertical rectangles.
+  
+  let sizeV1 = letterData["lengthV1"];
+  let sizeV2 = letterData["lengthV2"];
+  let posV1x = posx + letterData["offsetxV1"];
+  let posV2x = posx + letterData["offsetxV2"];
+  let posV1y = posy + letterData["offsetyV1"];
+  let posV2y = posy + letterData["offsetyV2"];
 
-  // draw two circles
-  rect(pos3x, posy, 25, 200);
-  rect(pos4x, pos5y, 25, size2);
-  rect(0, pos2y, 100, 10);
-  rect(0, pos3y,100, 10);
-  rect(0, pos4y, 100, 10);
-}
+  // determine parameters for horizontal rectangles.
+
+  let posH1y = posy + letterData["offsetyH1"];
+  let posH2y = posy + letterData["offsetyH2"];
+  let posH3y = posy + letterData["offsetyH3"];
+
+  // draw vertical rectangles.
+
+  rect(posV1x, posV1y, 25, sizeV1);
+  rect(posV2x, posV2y, 25, sizeV2);
+
+  // draw horizontal rectangles.
+
+  rect(0, posH1y, 100, 10);
+  rect(0, posH2y, 100, 10);
+  rect(0, posH3y, 100, 10);
+
+}   
+
 function interpolate_letter(percent, oldObj, newObj) {
   let new_obj = {};
+  new_obj ["lengthV1"] = map(percent, 0, 100, oldObj["lengthV1"], newObj["lengthV1"]);
   new_obj ["lengthV2"] = map(percent, 0, 100, oldObj["lengthV2"], newObj["lengthV2"]);
   new_obj ["offsetxV1"] = map(percent, 0, 100, oldObj["offsetxV1"], newObj["offsetxV1"]);
   new_obj ["offsetxV2"] = map(percent, 0, 100, oldObj["offsetxV2"], newObj["offsetxV2"]);
   new_obj ["offsetyH1"] = map(percent, 0, 100, oldObj["offsetyH1"], newObj["offsetyH1"]);
   new_obj ["offsetyH2"] = map(percent, 0, 100, oldObj["offsetyH2"], newObj["offsetyH2"]);
   new_obj ["offsetyH3"] = map(percent, 0, 100, oldObj["offsetyH3"], newObj["offsetyH3"]);
+  new_obj ["offsetyV1"] = map(percent, 0, 100, oldObj["offsetyV1"], newObj["offsetyV1"]);
   new_obj ["offsetyV2"] = map(percent, 0, 100, oldObj["offsetyV2"], newObj["offsetyV2"]);
   return new_obj;
 }
