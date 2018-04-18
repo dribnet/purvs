3020b01cc784fb10c8b0ddb512c580cb31ad86fc
jackie xu <jackiexu@jackies-MacBook-Pro.local>
2018-04-19 11:43:32 +1200

    added purview file

--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,12 @@
 const colorFront  = "#199cff";
 const colorStroke = "#233f11";
 
+var swapWords = [
+  "COOLWORD",
+  "NEATFONT",
+  "ABCDEFGH"
+]
+
 /*
  * Draw the letter given the letterData
  *
