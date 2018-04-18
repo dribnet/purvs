a6b01972c8ad49ec1ee29ab1f9d385fc34f59a3c
Callum <callumbrowne.is@gmail.com>
2018-04-18 21:50:09 +1200

    Project Update: Tweaks to letters

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## PS2 MDDN 242 2018
 
-So I have slightly reworked the letters and their sizes to make things more consistant. However I am in a slight predicament. Currently the design feels a bit too basic and doesn't do a lot to seperate itself from other people who had the same sort of idea. I have the idea of having the Neon "Board" still visible but I need to still do some tweaks so that it doesn't make the whole thing look too messy. So curretly I am in the process of planning out the extra things to add:
-	
-	- Outer Glow which I hope to give a slight pulse effect 
-	- Changing and fading colours
+Ok so I have now realised that the animations aren't going to go as planned due to them not playing in the alphabet So for the time being I am going to try and see if there is something else I can do. 
+
+Plans so Far:
+
+	- Finish interpolation
+	- Keep tweaking letter sizes
 
 Current Paramters Are:
   * lines : an array of 1s and 0s that let the line algroithm know whether or not to "light up" or "turn on" a particular line
--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,7 @@
 const colorFront  = "#199cff";
 const colorStroke = "#e3eded";
 const lines = [[0,0, 50, 0], [50, 0, 100, 0], [100,0, 100, 100], [100,100, 100,200], [100,200, 50, 200], [50, 200, 0, 200], [0, 200, 0, 100], [0,100, 0, 0], [0,100, 50, 100], [50,100, 100, 100], [50, 0, 50, 100], [50,100,50, 200], [0,0,100,200], [0,200,100,0]];
+//const swapWords = ["FONTNAME", "PICKWORD",  "ETC12345"];
 
 /*
  * Draw the letter given the letterData
@@ -13,14 +14,21 @@ function drawLetter(letterData) {
   for(var i = 0; i < lines.length; i++){
 	if(letterData["lines"][i] == 1){
 		strokeWeight(9);
-		stroke(letterData["letterColour"]);
+		stroke(letterData["red"], letterData["green"], letterData["blue"], 0.25*255);
 		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
-		strokeWeight(3);
-		stroke("white");
-		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );
-	}else{
+		strokeWeight(5);
+		stroke(letterData["red"], letterData["green"], letterData["blue"]);
+		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
 		strokeWeight(1);
-		stroke("black");
-	}		
+		stroke("white");		
+	}else{
+		
+		strokeWeight(1.5);
+		stroke("#333333");
+		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
+		strokeWeight(.5);
+		stroke("white");
+	}	
+	line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
   }  
 }
