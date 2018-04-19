dd00214e9ec23410d8cfd74873b6855c9939ff98
Callum <callumbrowne.is@gmail.com>
2018-04-19 23:49:25 +1200

    Project Update: Updating Purview

--- draw_letters.js
+++ draw_letters.js
@@ -1,7 +1,8 @@
 const colorFront  = "#199cff";
 const colorStroke = "#e3eded";
-const lines = [[0,0, 50, 0], [50, 0, 100, 0], [100,0, 100, 100], [100,100, 100,200], [100,200, 50, 200], [50, 200, 0, 200], [0, 200, 0, 100], [0,100, 0, 0], [0,100, 50, 100], [50,100, 100, 100], [50, 0, 50, 100], [50,100,50, 200], [0,0,100,200], [0,200,100,0]];
-//const swapWords = ["FONTNAME", "PICKWORD",  "ETC12345"];
+const lines = [[10,10,90,190], [10,190,90,10], [10,10, 50, 10], [50, 10, 90, 10], [90,10, 90, 100], [90,100, 90,190], [90,190, 50, 190], [50, 190, 10, 190], [10, 190, 10, 100], [10,100, 10, 10], [10,100, 50, 100], [50,100, 90, 100], [50, 10, 50, 100], [50,100,50, 190]];
+swapWords = ["NEONTYPE", "HI WORLD",  "MDDN 242"];
+
 
 /*
  * Draw the letter given the letterData
@@ -21,14 +22,32 @@ function drawLetter(letterData) {
 		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
 		strokeWeight(1);
 		stroke("white");		
-	}else{
-		
-		strokeWeight(1.5);
-		stroke("#333333");
+	}else{		
+		strokeWeight(2.5);
+		stroke(color(33,33,33,128));
 		line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
-		strokeWeight(.5);
-		stroke("white");
+		strokeWeight(1);
+		stroke(color(200,200,200,128));
 	}	
 	line(lines[i][0], lines[i][1], lines[i][2], lines[i][3] );	
   }  
+}
+
+function interpolate_letter(percent, oldData, newData){
+	let new_letter = [];
+	var newColor;
+	if(percent >= 50){ //Fade in the new lines
+		new_letter["lines"] = newData["lines"];
+		var newC = color(newData["red"], newData["green"], newData["blue"], 255);		
+		newColor = lerpColor(color(0,0,0,0), newC, (percent/100)-0.2);
+	}else{ //Fade out the old lines
+		new_letter["lines"] = oldData["lines"];
+		var oC = color(oldData["red"], oldData["green"], oldData["blue"], 255);		
+		newColor = lerpColor(oC, color(0,0,0,0), (percent/100)+0.20);
+	}
+	new_letter["red"] = red(newColor);
+	new_letter["green"] = green(newColor);
+	new_letter["blue"] = blue(newColor);
+	return new_letter;
+	
 }
