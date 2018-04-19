85e9843b7af055e318bd40d71ec03f44a21bf8ef
robsongeor <robsongeorge@hotmail.com>
2018-04-20 11:56:49 +1200

    Each of my letters is composed using three equilateral triangles. Each triangle has 3 parameters, an X and Y position and a rotation. All three triangles receive a size parameter too.
    I created a draw triangle function which takes x, y, size and rotation and creates a equilateral triangle out of the parameters.
    
    using some trigonometry and pythagorus I was able to figure out the position of each point of a triangle as well as its centre point.
    
    Then by translating the canvas so the the 0,0 becomes the centre of the trinagle, the triangle is able to be rotated. After the triangle is rotated, the canvas position and rotation is reset.
    
    These are the 10 parameters per letter:
      * `size` : length of the sides of the triangle
    
      * `offx` : X position of the first triangle
      * `offy` : Y position of the first triangle
      * `rot` : rotation of the first triangle in degrees
    
      * `offx1` : X position of the second triangle
      * `offy1` : Y position of the second triangle
      * `rot2` : rotation of the second triangle in degrees
    
      * `offx2` : X position of the third triangle
      * `offy2` : Y position of the third triangle
      * `rot3` : rotation of the third triangle in degrees
    
    most of the math was worked out on paper, which made it considerably easier to implement.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 ## PS2 MDDN 242 2018
 
-(Replace this README with information about your alphabet. This is an example.)
+Each of my letters is composed using three equilateral triangles. Each triangle has 3 parameters, an X and Y position and a rotation. All three triangles receive a size parameter too.
 
-This was a test trying to figure out how to implement triangles to form letters, and seeing what parameters I would need to make each letter. I figured making my own drawTriangle function would allow me to easily create a triangle with only an x and y rather than ust using the triangle function which takes each point of the traiangle (x1,y1,x2,y2,x3,y3)
+I created a draw triangle function which takes x, y, size and rotation and creates a equilateral triangle out of the parameters.
+
+using some trigonometry and pythagorus I was able to figure out the position of each point of a triangle as well as its centre point. 
+
+Then by translating the canvas so the the 0,0 becomes the centre of the trinagle, the triangle is able to be rotated. After the triangle is rotated, the canvas position and rotation is reset. 
+
+These are the 10 parameters per letter:
+  * `size` : length of the sides of the triangle
+
+  * `offx` : X position of the first triangle
+  * `offy` : Y position of the first triangle
+  * `rot` : rotation of the first triangle in degrees
+
+  * `offx1` : X position of the second triangle
+  * `offy1` : Y position of the second triangle
+  * `rot2` : rotation of the second triangle in degrees
+
+
+  * `offx2` : X position of the third triangle
+  * `offy2` : Y position of the third triangle
+  * `rot3` : rotation of the third triangle in degrees
+
+most of the math was worked out on paper, which made it considerably easier to implement. 
--- sketch.js
+++ sketch.js
@@ -1,48 +1,114 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
+const sz = 80;
+
 
 /* 
- * my three variable per letter are:
- *
-   size: radius of the second circle (in pixels)
-   offsetx: x offset (in pixels) of the second circle
-            relative to the first one
-   offsety: y offset (in pixels) of the second circle
-            relative to the first one
- *
+These are the 10 parameters per letter:
+  * `size` : length of the sides of the triangle
+
+  * `offx` : X position of the first triangle
+  * `offy` : Y position of the first triangle
+  * `rot` : rotation of the first triangle in degrees
+
+  * `offx1` : X position of the second triangle
+  * `offy1` : Y position of the second triangle
+  * `rot2` : rotation of the second triangle in degrees
+
+
+  * `offx2` : X position of the third triangle
+  * `offy2` : Y position of the third triangle
+  * `rot3` : rotation of the third triangle in degrees
  */
 
 const letterA = {
-"offsetx": -20,
-"offsety": 30,
-
-"offsetx2": 20,
-"offsety2": 30,
-
-"offsetx3": 0,
-"offsety3": 0,
-
-"rotate": 0,
+  "size": 80,
+   "offx": 0,
+  "offy": 0,
+  "offx1": -sz/2,
+  "offy1":69,  
+   "offx2": sz/2,
+  "offy2": 69,
+  
+  "rot": 180,
+  "rot2": 180,
+  "rot3": 180,
 }
 
 const letterB = {
- "offsetx": -20,
-"offsety": 30,
-
-"offsetx2": 20,
-"offsety2": 30,
-
-"offsetx3": 0,
-"offsety3": 60,
-
-
-"rotate": 90,
+   "size": 80,
+   
+  "offx": 0,
+  "offy": 0,
+  "offx1": -46,
+  "offy1":40,
+   "offx2": -0,
+  "offy2": 80,
+  
+  "rot": 270,
+  "rot2": 90,
+  "rot3": 270,
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "size": 80,
+  
+  "offx": 0,
+  "offy": -40,
+  "offx1": -46,
+  "offy1":40  ,
+   "offx2": -0,
+  "offy2": 120,
+  
+  "rot": 270,
+  "rot2": 90,
+  "rot3": 270,
+}
+
+
+const letterD = {
+  "size": 80,
+  
+  "offx": -40,
+  "offy": 0,
+  "offx1": 29,
+  "offy1":40  ,
+   "offx2": -40,
+  "offy2": 80,
+  
+  "rot": 270,
+  "rot2": 270,
+  "rot3": 270,
+}
+
+const letterE = {
+  "size": 80,
+  
+  "offx": -30,
+  "offy": -40,
+  "offx1": -30,
+  "offy1":40  ,
+   "offx2": -30,
+  "offy2": 120,
+  
+  "rot": 270,
+  "rot2": 270,
+  "rot3": 270,
+}
+
+const letterF = {
+  "size": 80,
+  
+  "offx": 0,
+  "offy": -40,
+  "offx1": -0,
+  "offy1":40  ,
+   "offx2": -62,
+  "offy2": 103,
+  
+  "rot": 270,
+  "rot2": 270,
+  "rot3": 0,
 }
 
 const colorFront  = "#199cff";
@@ -54,67 +120,69 @@ function setup () {
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
-  // color/stroke setup
-  fill(colorFront);
-  stroke(colorStroke);
-  strokeWeight(4);
-
   // with no animation, redrawing the screen is not necessary
   noLoop();
 }
 
-function drawLetter(posx, posy, letterData) {
+function drawLetter(posx, posy, scale, letterData) {
 
+  // determine parameters for triangles
+  let pos1x = posx + letterData["offx"];
+  let pos1y = posy + letterData["offy"];
+  let rot = letterData["rot"];
+  
+  let pos2x = posx + letterData["offx1"];
+  let pos2y = posy + letterData["offy1"];
+  let rot2 = letterData["rot2"];
+   
+  let pos3x = posx + letterData["offx2"];
+  let pos3y = posy + letterData["offy2"];
+  let rot3 = letterData["rot3"];
+  
+  let size = letterData["size"];
+  
 
-  // determine parameters for second circle
-  let ox = letterData["offsetx"];
-  let oy = letterData["offsety"];
-
-  let ox2 = letterData["offsetx2"];
-  let oy2 = letterData["offsety2"];
-
-  let ox3 = letterData["offsetx3"];
-  let oy3 = letterData["offsety3"];
-
- // let rot = radians(letterData["rotate"])
-
-
-
-
-
-drawTri(posx+ox, posy+oy);
-drawTri(posx+ox2, posy+oy2);
-drawTri(posx+ox3, posy+oy3);
-  // draw 3 triangles
- 
-
- 
+  // draw three triangles
+ drawTriangle(pos1x,pos1y,size,rot);
+ drawTriangle(pos2x,pos2y,size,rot2);
+  drawTriangle(pos3x,pos3y,size,rot3);
 
 }
-function drawTri(x1,y1) {
+function drawTriangle (x,y,size,rotation){
 
 
-  // determine parameters for second circle
-  //let size2 = letterData["size"];
-let x2 = x1 -20;
-let y2 = y1 + 30;
-let x3 = x1 +20;
-let y3 = y1 +30;
-
-
-
-
-  // draw a triangle
-
-
-  triangle(x1,y1,x2,y2,x3,y3);
-
-
+  let a = pow(size,2); 
+  let b = pow(size/2, 2);
+  let triHeight = sqrt(a-b); //base of the tringle to its peak (a^2 - c^2 = b^2)
+ let centreY = tan(radians(30))* size/2; //trig to work out centre of triangle
  
+  x1 = - size/2; //coordinates of triangle 
+  y1 = - centreY
+  x2 =  size/2;
+  y2 = - centreY; 
+  x3 = 0;
+  y3 =  triHeight - centreY;
+  
+  
 
+  
+  
+  
+translate(x,y);
+rotate(radians(rotation)); 
+  fill(colorFront);
+  triangle(x1,y1,x2,y2, x3, y3);
+  fill(255);
+    ellipse(0,0,10,10)
+    
+     rotate(radians(-rotation)); 
+    translate(0-x,0-y);
+   
+     
 }
 
 function draw () {
+
   // clear screen
   background(colorBack);
 
@@ -122,20 +190,14 @@ function draw () {
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
 
-  // draw the letters A, B, C from saved data
- 
-
- //drawLetter(center_x-250, center_y, letterA);
-
- drawLetter(center_x-250, center_y, letterA);
-
- drawLetter(center_x, center_y, letterB);
-
-    //drawTri(center_x-250, center_y, letterA);
-  //drawLetter(center_x      , center_y, 10, letterB);
- // drawLetter(center_x + 250, center_y, 10, letterC);
-
-
+  
+  drawLetter(width/7, center_y, 10, letterA);
+  drawLetter(width/7*2     , center_y, 10, letterB);
+  drawLetter(width/7*3 , center_y, 10, letterC);
+  drawLetter(width/7*4 , center_y, 10, letterD);
+  drawLetter(width/7*5 , center_y, 10, letterE);
+  drawLetter(width/7*6 , center_y, 10, letterF);
+  drawLetter(width/7*7 , center_y, 10, letterG);
 }
 
 function keyTyped() {
