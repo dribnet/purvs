86aa68f8d7c2d51597729829b797cb490e83dfe3
Guest User <Guest@wg401-d-04-06.mddn.vuw.ac.nz>
2018-04-20 11:22:38 +1200

    Updating full alphabet.

--- draw_letters.js
+++ draw_letters.js
@@ -1,5 +1,5 @@
 const colorFront  = "#ffb7ce";
-const colorStroke = "#ffb7ce"; /*"#ffb7ce"*/
+const colorStroke = "#ffb7ce"; 
 
 /*
  * Draw the letter given the letterData
@@ -14,44 +14,95 @@ function drawLetter(letterData) {
   stroke(colorStroke);
   strokeWeight(4);
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
+  //first circle parameters
+  let size = letterData["size"];
+  let posx = letterData["posx"];
+  let posy = letterData["posy"];
+
+  //second circle parameters
+  let size2 = letterData["size2"];
   let pos2x = 50 + letterData["offsetx"];
   let pos2y = 150 + letterData["offsety"];
 
-  let startx = 50 + letterData["x1"];
+  //line parameters
+  let startx = letterData["x1"];
   let starty = 150 + letterData["y1"];
-  let endx = 50 + letterData["x2"];
+  let addx = letterData["x2"];
   let endy = 150 + letterData["y2"];
 
+  //arc parameters
+  let posarc = letterData["posarc"];
+  let arcx = letterData["arcx"];
+  let arcy = letterData["arcy"];
 
-  // draw two circles
-    fill(colorFront);
-    ellipse(50, 150, 100, 100);
+  //drawing an arc
+  noFill();
+  strokeWeight (30);
+  arc(arcx, arcy, 60, 60, posarc, PI);
+  strokeWeight (4);
+  fill(colorFront);
 
-    fill(colorBack);
-    stroke(colorBack);
-    ellipse(pos2x, pos2y, size2, size2);
 
-  //draw a line
+
+  //drawing two circles
+  fill(colorFront);
+  ellipse(posx, posy, size, size);
+  fill(colorBack);
+  stroke(colorBack);
+  ellipse(pos2x, pos2y, size2, size2);
+  
+    //drawing a line
   stroke(colorStroke);
   fill(colorFront);
   strokeWeight (30);
-  line(startx, starty, endx, endy);
+  line(startx, starty, startx, endy);
   strokeWeight (4);
+
 }
 
 
 function interpolate_letter(percent, oldObj, newObj) {
     let new_letter = {};
     new_letter["size"] = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+    new_letter["posx"] = map(percent, 0, 100, oldObj["posx"], newObj["posx"]);
+    new_letter["posy"] = map(percent, 0, 100, oldObj["posy"], newObj["posy"]);
+
+    new_letter["size2"] = map(percent, 0, 100, oldObj["size2"], newObj["size2"]);
     new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
     new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
-
     new_letter["x1"] = map(percent, 0, 100, oldObj["x1"], newObj["x1"]);
     new_letter["y1"] = map(percent, 0, 100, oldObj["y1"], newObj["y1"]);
     new_letter["x2"] = map(percent, 0, 100, oldObj["x2"], newObj["x2"]);
     new_letter["y2"] = map(percent, 0, 100, oldObj["y2"], newObj["y2"]);
 
+    new_letter["posarc"] = map(percent, 0, 100, oldObj["posarc"], newObj["posarc"]);
+    new_letter["arcx"] = map(percent, 0, 100, oldObj["arcx"], newObj["arcx"]);
+    new_letter["arcy"] = map(percent, 0, 100, oldObj["arcy"], newObj["arcy"]);
+
     return new_letter;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
