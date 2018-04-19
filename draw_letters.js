40d73520b285251f6590eeb7bbd98cff3b2d0988
Josh Felizardo <josh.feliz03@gmail.com>
2018-04-19 15:12:48 +1200

    INTERACTION

--- draw_letters.js
+++ draw_letters.js
@@ -45,6 +45,28 @@ function drawLetter(letterData) {
 
 //quads
 
-  //quad(-15, 0, 55, 0, 140, 200,70,200)
   quad(quadx1_1, quady1_1, quadx2_1, quady2_1, quadx3_1, quady3_1, quadx4_1,quady4_1);
+
+}
+
+function interpolate_letter(percent, oldObj, newObj) {
+  let new_letter = {};
+    
+  new_letter["qua_x1_1"] = map(percent, 0, 100, oldObj ["qua_x1_1"], newObj["qua_x1_1"]);
+  new_letter["qua_y1_1"] = map(percent, 0, 100, oldObj ["qua_y1_1"], newObj["qua_y1_1"]);
+  new_letter["qua_x2_1"] = map(percent, 0, 100, oldObj ["qua_x2_1"], newObj["qua_x2_1"]);
+  new_letter["qua_y2_1"] = map(percent, 0, 100, oldObj ["qua_y2_1"], newObj["qua_y2_1"]);
+  new_letter["qua_x3_1"] = map(percent, 0, 100, oldObj ["qua_x3_1"], newObj["qua_x3_1"]);
+  new_letter["qua_y3_1"] = map(percent, 0, 100, oldObj ["qua_y3_1"], newObj["qua_y3_1"]);
+  new_letter["qua_x4_1"] = map(percent, 0, 100, oldObj ["qua_x4_1"], newObj["qua_x4_1"]);
+  new_letter["qua_y4_1"] = map(percent, 0, 100, oldObj ["qua_y4_1"], newObj["qua_y4_1"]);
+
+  new_letter["size"]     = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+  new_letter["offsetx"]  = map(percent, 0, 100, oldObj ["offsetx"], newObj["offsetx"]);
+  new_letter["offsety"]  = map(percent, 0, 100, oldObj ["offsety"], newObj["offsety"]);
+  new_letter["size2"]    = map(percent, 0, 100, oldObj ["size2"],       newObj["size2"]);
+  new_letter["offsetx2"] = map(percent, 0, 100, oldObj ["offsetx2"], newObj["offsetx2"]);
+  new_letter["offsety2"] = map(percent, 0, 100, oldObj ["offsety2"], newObj["offsety2"]);
+  
+  return new_letter;
 }
