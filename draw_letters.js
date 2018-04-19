9d2ba6762359a302ea59c6bff5db0da322d2e601
Andrija Djorovic <AndrijaKiwi@gmail.com>
2018-04-19 17:39:56 +1200

    getting ready to add part4

--- draw_letters.js
+++ draw_letters.js
@@ -9,17 +9,49 @@ const colorStroke = "#233f11";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  // color/stroke setup
-  fill(colorFront);
-  stroke(colorStroke);
-  strokeWeight(4);
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50+letterData["offsetx"];
-  let pos2y = 150+letterData["offsety"];
+  let scale = 40;
+  
+  let offset = letterData["offset"];
 
-  // draw two circles
-  ellipse(50, 150, 100, 100);
-  ellipse(pos2x, pos2y, size2, size2);
+  let type = letterData["first"];
+  if(type==0){
+    strokeWeight(scale);
+    line(0, (offset*scale/2)+0+((scale+scale/4)*0), 0, (offset*scale/2)+0+((scale+scale/4)*0));
+  } else if(type==1){
+    strokeWeight(scale);
+    line(0-scale, (offset*scale/2)+0+((scale+scale/4)*0), 0+scale, (offset*scale/2)+0+((scale+scale/4)*0))
+  }
+  type = letterData["second"];
+  if(type==0){
+    strokeWeight(scale);
+    line(0, (offset*scale/2)+0+((scale+scale/4)*1), 0, (offset*scale/2)+0+((scale+scale/4)*1));
+  } else if(type==1){
+    strokeWeight(scale);
+    line(0-scale, (offset*scale/2)+0+((scale+scale/4)*1), 0+scale, (offset*scale/2)+0+((scale+scale/4)*1))
+  }
+  type = letterData["third"];
+  if(type==0){
+    strokeWeight(scale);
+    line(0, (offset*scale/2)+0+((scale+scale/4)*2), 0, (offset*scale/2)+0+((scale+scale/4)*2));
+  } else if(type==1){
+    strokeWeight(scale);
+    line(0-scale, (offset*scale/2)+0+((scale+scale/4)*2), 0+scale, (offset*scale/2)+0+((scale+scale/4)*2))
+  }
+  type = letterData["fourth"];
+  if(type==0){
+    strokeWeight(scale);
+    line(0, (offset*scale/2)+0+((scale+scale/4)*3), 0, (offset*scale/2)+0+((scale+scale/4)*3));
+  } else if(type==1){
+    strokeWeight(scale);
+    line(0-scale, (offset*scale/2)+0+((scale+scale/4)*3), 0+scale, (offset*scale/2)+0+((scale+scale/4)*3))
+  }
+  type = letterData["fifth"];
+  if(type==0){
+    strokeWeight(scale);
+    line(0, (offset*scale/2)+0+((scale+scale/4)*4), 0, (offset*scale/2)+0+((scale+scale/4)*4));
+  } else if(type==1){
+    strokeWeight(scale);
+    line(0-scale, (offset*scale/2)+0+((scale+scale/4)*4), 0+scale, (offset*scale/2)+0+((scale+scale/4)*4))
+  }
 }
--- letters.js
+++ letters.js
@@ -1,22 +1,298 @@
 const alphabet = {
   "default": {
-      "size": 10,
-      "offsetx": 0,
-      "offsety": 0
+      "first": 1,
+      "second": 1,
+      "third": 1,
+      "fourth": 1,
+      "fifth": 1,
+      "offset": 0
   },
   "A": {
-      "size": 50,
-      "offsetx": 0,
-      "offsety": 23
+      "first": 0,
+      "second": 1,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 3
   },
   "B": {
-      "size": 100,
-      "offsetx": 0,
-      "offsety": -98
+      "first": 1,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
   },
   "C": {
-      "size": 68,
-      "offsetx": 15,
-      "offsety": 0
-  }
+      "first": 1,
+      "second": 0,
+      "third": 1,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "D": {
+      "first": 1,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 2
+  },
+  "E": {
+      "first": 0,
+      "second": -1,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 4
+  },
+  "F": {
+      "first": 0,
+      "second": 0,
+      "third": 1,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "G": {
+      "first": 1,
+      "second": 1,
+      "third": 0,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "H": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "I": {
+      "first": 0,
+      "second": 0,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 3
+  },
+  "J": {
+      "first": 0,
+      "second": 1,
+      "third": 1,
+      "fourth": 1,
+      "fifth": -1,
+      "offset": 1
+  },
+  "K": {
+      "first": 1,
+      "second": 0,
+      "third": 1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "L": {
+      "first": 0,
+      "second": 1,
+      "third": 0,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "M": {
+      "first": 1,
+      "second": 1,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 3
+  },
+  "N": {
+      "first": 1,
+      "second": 0,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 3
+  },
+  "0": {
+      "first": 1,
+      "second": 1,
+      "third": 1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "P": {
+      "first": 0,
+      "second": 1,
+      "third": 1,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "Q": {
+      "first": 1,
+      "second": 1,
+      "third": 0,
+      "fourth": 1,
+      "fifth": -1,
+      "offset": 1
+  },
+  "R": {
+      "first": 0,
+      "second": 1,
+      "third": 0,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "S": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "T": {
+      "first": 1,
+      "second": -1,
+      "third": -1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 4
+  },
+  "U": {
+      "first": 0,
+      "second": 0,
+      "third": 1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "V": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": 1,
+      "fifth": -1,
+      "offset": 1
+  },
+  "W": {
+      "first": 0,
+      "second": 1,
+      "third": 1,
+      "fourth": -1,
+      "fifth": -1,
+      "offset": 2
+  },
+  "X": {
+      "first": 1,
+      "second": 0,
+      "third": 0,
+      "fourth": 1,
+      "fifth": -1,
+      "offset": 1
+  },
+  "Y": {
+      "first": 1,
+      "second": 0,
+      "third": 1,
+      "fourth": 1,
+      "fifth": -1,
+      "offset": 1
+  },
+  "Z": {
+      "first": 1,
+      "second": 1,
+      "third": 0,
+      "fourth": 0,
+      "fifth": -1,
+      "offset": 1
+  },
+  "1": {
+      "first": 1,
+      "second": 1,
+      "third": 1,
+      "fourth": 1,
+      "fifth": 1,
+      "offset": 0
+  },
+  "2": {
+      "first": 0,
+      "second": 1,
+      "third": 1,
+      "fourth": 1,
+      "fifth": 1,
+      "offset": 0
+  },
+  "3": {
+      "first": 0,
+      "second": 0,
+      "third": 1,
+      "fourth": 1,
+      "fifth": 1,
+      "offset": 0
+  },
+  "4": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": 1,
+      "fifth": 1,
+      "offset": 0
+  },
+  "5": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": 1,
+      "offset": 0
+  },
+  "6": {
+      "first": 0,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": 0,
+      "offset": 0
+  },
+  "7": {
+      "first": 1,
+      "second": 0,
+      "third": 0,
+      "fourth": 0,
+      "fifth": 0,
+      "offset": 0
+  },
+  "8": {
+      "first": 1,
+      "second": 1,
+      "third": 0,
+      "fourth": 0,
+      "fifth": 0,
+      "offset": 0
+  },
+  "9": {
+      "first": 1,
+      "second": 1,
+      "third": 1,
+      "fourth": 0,
+      "fifth": 0,
+      "offset": 0
+  },
+  "0": {
+      "first": 1,
+      "second": 1,
+      "third": 1,
+      "fourth": 1,
+      "fifth": 0,
+      "offset": 0
+  },
 }
