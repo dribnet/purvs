b7de84e68de68cb78fda172a4c4d4db03a6526bc
Guest User <Guest@wg401-d-04-06.mddn.vuw.ac.nz>
2018-04-20 13:47:30 +1200

    Commiting changes

--- exhibition.js
+++ exhibition.js
@@ -1,7 +1,7 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
+const colorBack    = "#d3efff";
 const colorLines   = "#000090";
 if (typeof colorFront === 'undefined') {
   colorFront   = "#199cff";
