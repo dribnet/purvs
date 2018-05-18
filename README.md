090e06e4967bf088e57acc71dbdd9da2652e4a9d
Reuben Spori <reuben.spori@gmail.com>
2018-05-18 15:01:00 +1200

    First attempt. updated everything and have done two of my own images.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+First attempt. Used a picture of friends and my cat to try and see how it works. I have found accedently that my pictures have an outline almost which I find quite interesting and want to try and potentially develop further later on in the project.
--- sketch.js
+++ sketch.js
@@ -19,13 +19,13 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
+  for(let i=0;i<75;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 15;
+    let halfSize = 25;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
@@ -35,7 +35,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 500) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
