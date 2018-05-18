a7897a1efac153571c38a0962748ba5131270d9e
Matt Cross <mettx@yahoo.co.nz>
2018-05-18 15:15:23 +1200

    Added my own images to the programe and played around with the settings a little bit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+As of right now I do not know what kind of image I will go for. I believe I may go for something like landscapes and city skylines or even portraits.
+Currently I have a image of Scarlett Johansson that I edited years ago, an image of my MDDN241 Porject 2 model, and Vincent Van Gogh's famous starry night.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -19,27 +19,31 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
+  for(let i=0;i<1;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 25;
+    let halfSize = 12.5;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      ellipse(x, y, pointSize, pointSize);  
+
     }
     else {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+
     }
+
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 10000) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
-  }
+  }    
+  console.log(100*renderCounter);
 }
 
 function keyTyped() {
