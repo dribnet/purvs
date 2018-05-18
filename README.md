a42246e888308d6de7f5782bd326c378e13f97e8
Eddie Smith <ejsmith9725@gmail.com>
2018-05-18 15:37:58 +1200

    First Trial

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
+For the first push I wanted to see what the detail was like when rendering so I grabbed a screenshot of an NZ focused google earth. It took about 30 mins to render the most detailed version and close to 10 for the less detailed.
+
 The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -24,11 +24,11 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 20;
+    let halfSize = 20;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      line(x, y, pointSize, pointSize);
     }
     else {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
@@ -37,7 +37,7 @@ function draw () {
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
-    noLoop();
+    //noLoop();
     // saveBlocksImages();
   }
 }
