f9515d2449f2964b56252b905dd09d4b63412c1e
Keegan Edwards <edwardkeeg@vuw.ac.nz>
2018-05-18 15:40:47 +1200

    Added my own images and changed the algorithim in sketch.js, updated README, preview and thumbnail

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+-18/5-
+Im not sure what sort of theme I want to go with for the images yet, as placeholders to play around with I used a render from a previous project, a picture from a game that i happened to have on my USB and the same Te Aro campus photo as before. 
+I removed the mask on my first two images, I want to play around with colour and try have over or under saturation in the masked area. 
+For the algorithim I have the ellipses draw and get smaller over time, they start out quite large then draw smaller and smaller which slowly adds in extra detail to the image and creates a nice layered effect. the ellipses also have a range when they draw so that they pick a random size between two values (which get smaller over time).
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -17,29 +17,48 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
+//let pointSize = 200;
+  let pSBig = 350;
+  let pSSmall = 300;
 
 function draw () {
-  for(let i=0;i<100;i++) {
+    for(let i=0;i<1;i++) {
+
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = floor(random(pSSmall, pSBig));
+
     fill(pix);
-    if(mask[0] > 128) {
+    //if(y<(sourceImg.height/2)){
       ellipse(x, y, pointSize, pointSize);
+    //}
+    //if(y>(sourceImg.height/2)){
+    //  rect(x-(pointSize/2), y-(pointSize/2), pointSize, pointSize); 
+    //}
+    if(pSBig>50){
+      pSBig-=0.5;
     }
-    else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    if(pSSmall>10){
+      pSSmall-=0.5;
     }
+    
+    //if(mask[0] > 128) {
+     // ellipse(x, y, pointSize, pointSize);
+    //}
+    //else {
+    //  rect(x-(pointSize/2), y-(pointSize/2), pointSize, pointSize);    
+    //}
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 6000) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
   }
+  //console.log(1*renderCounter);
+  //console.log(pointSize);
 }
 
 function keyTyped() {
