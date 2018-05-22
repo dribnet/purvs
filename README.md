585e9d862d9112e995a229d0dfa6dee2933f1921
Guest User <Guest@wg401-i-03-07.mddn.vuw.ac.nz>
2018-05-22 14:33:56 +1200

    Updated sketch, README, input_3, preview and thumbnail

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## Creative Coding 2: Custom Pixel
 
+-22/5-
+I am still using the same images as i haven't found final images to use yet, although I have changed the third image from the photo of Te Aro Campus to a simple black to white gradient so I could test my sketch easier and see different effects my code had.
+The algorithm now draws ellipses, squares and triangles. The shape that is drawn depends on the value of the colour from each point that is picked, so ellipses are a third of the colour space, squares the next third and triangles the last third. The shapes also still shrink over time which I think makes a nice layered affect where you can sometimes see bigger shapes that haven't been covered.
+I still haven't implemented any masking yet but now that I know how the colour works I will try out some different ideas.
+
 -18/5-
 Im not sure what sort of theme I want to go with for the images yet, as placeholders to play around with I used a render from a previous project, a picture from a game that i happened to have on my USB and the same Te Aro campus photo as before. 
 I removed the mask on my first two images, I want to play around with colour and try have over or under saturation in the masked area. 
--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let renderCounter=0;
 
 function preload() {
   sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -17,7 +17,8 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-//let pointSize = 200;
+  
+  //For the size of the shapes drawn
   let pSBig = 350;
   let pSSmall = 300;
 
@@ -28,37 +29,42 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = floor(random(pSSmall, pSBig));
+    let pointSize = floor(random(pSSmall, pSBig)); //picks random number between to values for the size of shapes
+    let halfSize = pointSize/2
+    let ro = map(pix[1], 0, 255, 0, 360);
 
     fill(pix);
-    //if(y<(sourceImg.height/2)){
-      ellipse(x, y, pointSize, pointSize);
-    //}
-    //if(y>(sourceImg.height/2)){
-    //  rect(x-(pointSize/2), y-(pointSize/2), pointSize, pointSize); 
-    //}
+
+    if(pix[0] < 85){  //Draws ellipse if colour value of point is < 85
+        ellipse(x, y, pointSize, pointSize);
+    }
+    if(pix[0] > 85 && pix[0] < 170){  //Draws square if colour value of point is > 85 & < 170
+        translate(x, y);
+        rotate(ro);
+        rect(0-halfSize, 0-halfSize, pointSize, pointSize);
+    }
+    if(pix[0] > 170){ //Draws traingle if colour value of point is > 170
+      translate(x, y);
+      rotate(ro);
+      triangle(0, 0, 0-halfSize, 0+pointSize, 0+halfSize, 0+pointSize);
+    }
+
+    //Reduces size of shapes over time, while restricting the minimum sizes
     if(pSBig>50){
-      pSBig-=0.5;
+      pSBig-=0.3;
     }
     if(pSSmall>10){
-      pSSmall-=0.5;
+      pSSmall-=0.3;
     }
-    
-    //if(mask[0] > 128) {
-     // ellipse(x, y, pointSize, pointSize);
-    //}
-    //else {
-    //  rect(x-(pointSize/2), y-(pointSize/2), pointSize, pointSize);    
-    //}
+
+    //console.log(pix);
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 6000) {
+  if(renderCounter > 3500) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
   }
-  //console.log(1*renderCounter);
-  //console.log(pointSize);
 }
 
 function keyTyped() {
