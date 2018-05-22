899f99af9fb3d7308e7cd44d0423647205e960f4
Guest User <Guest@wg401-i-07-07.mddn.vuw.ac.nz>
2018-05-22 13:56:02 +1200

    Updated images

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
+let CurRow=0;
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  sourceImg = loadImage("input_2.jpg");
+  maskImg = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -18,14 +18,18 @@ function setup () {
   maskImg.loadPixels();
 }
 
+let pointSize = 12.5;
+
 function draw () {
-  for(let i=0;i<1;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+    // let x = floor(random(sourceImg.width));
+    // let y = floor(random(sourceImg.height));
+    let x = i * pointSize;
+    let y = CurRow * pointSize;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 25;
-    let halfSize = 12.5;
+   
+    let halfSize = pointSize/2;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);  
@@ -37,13 +41,13 @@ function draw () {
     }
 
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10000) {
+  CurRow = CurRow + 1;
+  if(CurRow*pointSize > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
   }    
-  console.log(1*renderCounter);
+  console.log(1*CurRow);
 }
 
 function keyTyped() {
