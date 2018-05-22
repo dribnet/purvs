7272d220825003f96a853dab4df839a2dcfeb0cf
Nick Martin <martinnich2@myvuw.ac.nz>
2018-05-22 17:54:04 +1200

    Update 1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+With my pixel idea I want to make the background look more robotic and mechanical, while the foreground is more organic and natural. I am going to do this with shapes like with the pixels I'm going to use two squares and with the smaller one of the squares being black or while simulating a microchip and the organic one could be done with random shapes that use vectors.
+
+For my images I want to use pictures that I took in first year that I never used, or I want to use things I enjoy like a camera for photography or food.
+
+The layout at the moment is neat in the background and orginaized to show a robotic style which a calculated look and more random in the foreground to show a more organic style. I still need to improve and iterate my styles in the foreground and background and upload images.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -18,24 +18,34 @@ function setup () {
   maskImg.loadPixels();
 }
 
+let pointSize = 40;
+let elementSpacing = 20;
+let squareSize = 20;
+let circleSize = 40
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let dx = floor(random(elementSpacing/2));
+    let dy = floor(random(elementSpacing/2));
+    let x = i * elementSpacing;
+    let y = renderCounter * elementSpacing;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      x = x + dx;
+      y = y + dy;
+      ellipse(x-50, y-50, circleSize, circleSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      rect(x-halfSize, y-halfSize, squareSize, squareSize); 
+      fill(120);
+      rect(x-halfSize + squareSize/2, y-halfSize + squareSize/2, squareSize/4, squareSize/4);   
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter * elementSpacing > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
