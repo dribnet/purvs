bd9b197dbabdb196e6a364820e6f9fd0728ff164
Ehnel Bugas <ebugas459@outlook.com>
2018-05-22 14:06:51 +1200

    grid code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
+Episode 00
+
+somekind of grid
+
 The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let curRow = 0;
 
 function preload() {
   sourceImg = loadImage("input_1.jpg");
@@ -18,13 +19,18 @@ function setup () {
   maskImg.loadPixels();
 }
 
+let pointSize = 20;
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080;i++) {
+    //let x = floor(random(sourceImg.width));
+    //let y = floor(random(sourceImg.height));
+    let x = i * pointSize;
+    let y = curRow * pointSize;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
+    let halfsize = pointSize/2;
+    //editing the numbers below slows render but increases detail
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
@@ -34,8 +40,9 @@ function draw () {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+
+  curRow = curRow + 1;
+  if(curRow*pointSize > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
