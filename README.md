682d1422e357527a85287f634dd9e4bdb5617e2d
ruby <rubbyyjohnson@gmail.com>
2018-05-22 15:05:14 +1200

    new stuff

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-working on my own photo now and looking into other ways I can display it and use the program to enhance my photograph etc. 
-
+I am now looking at different ways to display my image and paying around with the different features. I dont think I will try this one again as I dont think it works well with this image. I might look into other images and see if I can see a theme to use for this project. 
 
 The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let pointSize = 40;
 
 function preload() {
   sourceImg = loadImage("input_3.jpg");
@@ -22,23 +23,25 @@ function setup () {
 function draw () {
 
   //colorMode(HSB,255);
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+    let x = int(i * pointSize);
+    let y = int(renderCounter * pointSize);
+    //let x = floor(random(sourceImg.width));
+    //let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    //let pointSize = 100;
+    let halfSize =pointSize/2;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize/5, pointSize/5);
+      ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize/5, pointSize/5);    
+      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 500) {
+  if(renderCounter >  1920/pointSize) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
