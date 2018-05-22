ca34842030a4a0bc97b1169ad4faccd28c70967b
Bradleypeary <bradleypeary@gmail.com>
2018-05-22 15:30:37 +1200

    experimenting

--- sketch.js
+++ sketch.js
@@ -13,29 +13,35 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(50);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
+  for(let i=0;i<200;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
+    let x2 = (random(x-200,x+200));
+    let y2 = (random(y-200,y+200));
     let pix = sourceImg.get(x, y);
+    let pix2 = sourceImg.get(x2, y2);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 50;
+    let halfSize = 10;
     fill(pix);
+    stroke(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      strokeWeight(7)
+      line(x, y, x2, y2);
     }
     else {
+      
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 20) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
