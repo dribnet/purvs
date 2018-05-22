38da93eb89d13ef84487f3e62f480ee7e4221ea6
Christian Sutherland <ythy_the_penguin@yahoo.co.nz>
2018-05-22 13:33:58 +1200

    two landscapes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-I took photos of personal objects like the teddy bear I had when I was younger, and my piano keyboard, because music has been a big part of my life.
+Photo of my piano keyboard.
 
+Changed the code to swap when it draws rectangles and ellipses.
+Ellipses used to draw when mask was white, but now 
+rectangles draw when mask is white.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_2.jpg");
+  maskImg = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -27,7 +27,7 @@ function draw () {
     let pointSize = 50;
     let halfSize = 50;
     fill(pix);
-    if(mask[0] > 128) {
+    if(mask[0] < 128) {
       ellipse(x, y, pointSize, pointSize);
     }
     else {
@@ -35,7 +35,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 40) {
+  if(renderCounter > 50) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
