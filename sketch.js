b5404178899fe86a055b0f6ff0a992f8f1709700
Molisang <molijun801@gmail.com>
2018-05-22 13:01:39 +1200

    Part 1

--- sketch.js
+++ sketch.js
@@ -24,8 +24,8 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 30;
+    let halfSize = 30;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
@@ -35,7 +35,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 100) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
