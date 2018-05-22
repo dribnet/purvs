52af04ef0c3429df3b1370fae64c17c9eb00169a
Cody Stiles <stilescody@myvuw.ac.nz>
2018-05-22 14:58:13 +1200

    New output
    I have played with a new code to change the output of the images.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-This is my practise image, it is my hand holding a crystal. I chose it because I thought the colors woud work well and I like the image's subject, the crystal.
-
-**EDIT**
-The three panels should now match correctly to the new works (which are shots from my short film for project 2 of another design course).
+The code has been modified using Tom's code to make the output more uniform and tidy. I like how the squares sit/fit tightly against one-another. The circles, however, I like a little less because of the white space they have inbetween each other. I think I will next try the different codes until I find one that creates an output I like and then I will try to edit it further to make it more my own style.
--- sketch.js
+++ sketch.js
@@ -2,9 +2,14 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
+//I have used some of Tom White's example code for my own project
+//https://bl.ocks.org/dribnet/2b5fdedb10367f0db717755169946832/b9eca66520db6771e0e0299da527af99496efb95
+
+
+
 function preload() {
-  sourceImg = loadImage("third.jpg");
-  maskImg = loadImage("thirdbw.png");
+  sourceImg = loadImage("second.jpg");
+  maskImg = loadImage("secondbw.png");
 }
 
 function setup () {
@@ -18,14 +23,19 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const pointSize = 40;
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+  	let x = int(i * pointSize);
+  	let y = int(renderCounter * pointSize);
+    //let x = floor(random(sourceImg.width));
+    //let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 60;
-    let halfSize = 50;
+    let halfSize = pointSize/2;
+    //let pointSize = 60;
+    //let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
@@ -35,7 +45,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 100) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
