22298602a1f3d3abe3bd33487b1f77be09e47865
Guest User <Guest@wg401-i-06-07.mddn.vuw.ac.nz>
2018-05-22 15:34:47 +1200

    progression of pixels for future images

--- sketch.js
+++ sketch.js
@@ -19,28 +19,42 @@ function setup () {
   maskImg.loadPixels();
 }
 
-let pointSize = 100;
+//let pointSize = 50;
+let elementSpacing = 30;
+//let elementSpacingH = 20;
+let squareSize = 20;
+
 
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
-   // let x = floor(random(sourceImg.width));
-   // let y = floor(random(sourceImg.height));
-    let x = i * pointSize;
-    let y = curRow * pointSize;
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let nx = floor(random(elementSpacing*2));
+    let ny = floor(random(elementSpacing*2));
+    let x = i * elementSpacing;
+    let y = curRow * elementSpacing;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+    let circleSize = (random(30, 120));
 
-    let halfSize = pointSize/2;
+    let halfSize = squareSize/2;
     fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+    if(mask[0] >140 && mask[0] <160){
+      rect(x-halfSize, y-halfSize, squareSize*2, squareSize*2);   
+
     }
-    else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);   
+    if(mask[0] > 160) {
+      rect(x-halfSize, y-halfSize, squareSize, squareSize*2);   
+    }
+    if (mask[0] < 140){
+      x = x + nx;
+      y = y + ny;
+      ellipse(x, y, cir0!cleSize, circleSize);
+      x = x + nx*2;
+      y = y + ny*2;
+      ellipse(x, y, circleSize/2, circleSize/2);
     }
   }
   curRow = curRow + 1;
-  if(curRow*pointSize > 1920) {
+  if(curRow*elementSpacing > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
