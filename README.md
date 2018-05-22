adb072eb9f8abf38bcbfeabbfb6bddcf0e418dfa
Guest User <Guest@wg401-i-07-07.mddn.vuw.ac.nz>
2018-05-22 15:26:32 +1200

    Played with code and updated image #2 and README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## Creative Coding 2: Custom Pixel
 
 As of right now I do not know what kind of image I will go for. I believe I may go for something like landscapes and city skylines or even portraits.
-Currently I have 3 images from my year one DSDN144 photographics project 2 which looked at light. My thought process for this project was to show big name companies products in a type of evil light.
+
+UPDATE #1
+Currently I have a image of Scarlett Johansson that I edited years ago, an image of my MDDN241 Porject 2 model, and Vincent Van Gogh's famous starry night.
+
+UPDATE #2
+Currently I have 3 images from my year one DSDN144 photographics project 2 which looked at light. My thought process for this project was to show big name companies products in a type of evil light. I am trying to figure out how I can shape the code to best represent this idea.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let sourceImg=null;
 let maskImg=null;
-let CurRow=0;
+let renderCounter=0;
 
 function preload() {
   sourceImg = loadImage("input_2.jpg");
@@ -12,42 +12,56 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
+  rectMode(CENTER);
   noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-let pointSize = 12.5;
+// let pointSize = 12.5;
+let triSize = 10;
+let elementSpacing = 12.5;
+let squareSize = 12.5
+let circleSize = 12.5
 
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
+  for(let i=0;i<1080/elementSpacing;i++) {
     // let x = floor(random(sourceImg.width));
     // let y = floor(random(sourceImg.height));
-    let x = i * pointSize;
-    let y = CurRow * pointSize;
+    let x = i * elementSpacing;
+    let y = renderCounter * elementSpacing;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
    
-    let halfSize = pointSize/2;
+    let halfSize = squareSize/2;
     fill(pix);
+    push();
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);  
+      stroke(0)
+      rect(x, y, squareSize*2, squareSize*2);
+      // fill(255, 100, 0);
+      // ellipse(x, y, circleSize, circleSize);
+      pop();
+      // triangle(x, y, x-halfSize, y-pointSize, x+halfSize, y-pointSize);
 
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      // rect(x-halfSize, y-halfSize, pointSize, pointSize); 
+      triangle(x, y+10, x-triSize, y-triSize, x+triSize, y-triSize);
+      // fill(100);   
+      // triangle(x, y+10, x-triSize/2, y-triSize/2, x+triSize/2, y-triSize/2);   
 
     }
 
   }
-  CurRow = CurRow + 1;
-  if(CurRow*pointSize > 1920) {
+  renderCounter = renderCounter + 1;
+  if(renderCounter*elementSpacing > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
   }    
-  console.log(1*CurRow);
+  console.log(1*renderCounter);
 }
 
 function keyTyped() {
