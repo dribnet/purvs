6b04ff0186a2b0e423ec26b8329ac68d3b6c4538
Guest User <Guest@wg401-i-06-07.mddn.vuw.ac.nz>
2018-05-22 13:49:54 +1200

    grid demo

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
+//let renderCounter=0;
+let curRow = 0;
 
 function preload() {
   sourceImg = loadImage("input_1.jpg");
@@ -18,14 +19,18 @@ function setup () {
   maskImg.loadPixels();
 }
 
+let pointSize = 100;
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+   // let x = floor(random(sourceImg.width));
+   // let y = floor(random(sourceImg.height));
+    let x = i * pointSize;
+    let y = curRow * pointSize;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+
+    let halfSize = pointSize/2;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
@@ -34,8 +39,8 @@ function draw () {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);   
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  curRow = curRow + 1;
+  if(curRow*pointSize > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
