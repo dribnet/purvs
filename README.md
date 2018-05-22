d5a282928393b8f96ce2830b5914e440620fb580
Reuben Spori <reuben.spori@gmail.com>
2018-05-22 14:52:25 +1200

    Tried to further my idea and devlopment. more progress and play around. updated everything too.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Creative Coding 2: Custom Pixel
 
 First attempt. Used a picture of friends and my cat to try and see how it works. I have found accedently that my pictures have an outline almost which I find quite interesting and want to try and potentially develop further later on in the project.
+At the moment I have played around with shaped and have tried to take insiration from Low Poly Art to make my images. I have some ideas on how I could develop it so there isnt any white space. I have played around already and found that the best way might be to map the shapes to the colour but I will have to play around to see if it works. I also want to implement a randomness to it but need to experiment with it.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_2.jpg");
+  maskImg = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -18,24 +18,38 @@ function setup () {
   maskImg.loadPixels();
 }
 
+  let pointSize = 15;
+
 function draw () {
-  for(let i=0;i<75;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+//    let x = floor(random(sourceImg.width));
+//    let y = floor(random(sourceImg.height));
+
+let x = int(i * pointSize);
+let y = int(renderCounter * pointSize);
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 15;
-    let halfSize = 25;
+    
+    let halfSize = pointSize/2;
     fill(pix);
+
+var x1 = random(pointSize);
+//stroke(1);
+//rect(x, y, pointSize, pointSize);
+    triangle(x+x1, y, x+pointSize, y+pointSize, x, y+pointSize);  
+    
+
+/*
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      triangle(x, y, x+pointSize, y+pointSize, x, y+pointSize);    
     }
+    */
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 500) {
+  if(renderCounter > 200) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
