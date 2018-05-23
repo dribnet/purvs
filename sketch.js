27d22df163cb87d7a571357bc6affa84575bee61
Sylvia Song <sylviasong9747@gmail.com>
2018-05-23 20:09:11 +1200

    update part 1

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
+    let pointSize = 10;
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
@@ -35,7 +35,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 50) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
