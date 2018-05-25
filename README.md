62d26278e64f255c2ae8b8deb2ff5009657924ed
MuHuang <victormuhuang@gmail.com>
2018-05-25 15:05:12 +1200

    Updating of first post

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Creative Coding 2: Custom Pixel
 
-This my render images' first post, first one is my cat pocket, second one is my teddy bear and last one is my city TV tower(Canton Tower). 
+After first post, I found that the pointSize represents the pixel of an image. Therefore, I changed the codes by referencing Tom White's new updated codes. I have not mask the image yet. I only replace the mask files by white background. I think that recttangles could be the great examples of symbolizing pixel. 
+And I will change the mask files in next step. 
--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+//https://bl.ocks.org/dribnet/2b5fdedb10367f0db717755169946832/b9eca66520db6771e0e0299da527af99496efb95
+//Reference to Tom White's updated codes. a
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
@@ -18,24 +20,31 @@ function setup () {
   maskImg.loadPixels();
 }
 
+  const pointSize = 12;
+
 function draw () {
-  for(let i=0;i<495;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+
+    let x = int(i * pointSize);
+    let y = int(renderCounter * pointSize);
+    //let x = floor(random(sourceImg.width));
+    //let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 50;
-    let halfSize = 50;
+    //let pointSize = 50;
+    //let halfSize = 50;
+    let halfSize = pointSize/2;
+
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+        rect(x-halfSize, y-halfSize, pointSize, pointSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+        ellipse(x, y, pointSize, pointSize);  
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
