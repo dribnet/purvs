d81da385a6e83fd75c1367c4e26d730790d02f78
Yutong Chen <cona991@gmail.com>
2018-05-25 14:22:46 +1200

    code change

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -24,18 +24,18 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 20;
+    let halfSize = 10;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      rect(x-halfSize, y-halfSize, pointSize, pointSize);  
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      ellipse(x, y, pointSize, pointSize);
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 120) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
