1083cc3df82dfccd2f6618573bba265bacf6730a
Sezura <33705559+Sezura@users.noreply.github.com>
2018-05-25 13:55:57 +1200

    Updated code(2nd script and 2nd mask) and readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+At this point I'm still playing with ideas, I quite like the mixture of the grid and the semi randomized grid. I also want to experiment more with the greyscale areas and the overall pixel density. I'm thinking if I change pixel density with different masks the code will need significant refacturing but overall could be worth it; seeing as at this point if the pixel density is to high the saveBlocksImages command crashes.
+
+### Update:
+
+I've now added multiple masks and the ability to use both the fully randomized script and the two grid style scripts in one porject.
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 /* Set to true to make final high-resolution version */
 const finalVersion = true;
+var SecondDraw = false;
 
 /* Default versions of variables */
 let elementSpacing = 40;
@@ -8,7 +9,7 @@ let squareSize = 40;
 
 /* Override some variables with high-resolution final version */
 if (finalVersion) {
-	let scale = 3;
+	let scale = 2;
 	elementSpacing = elementSpacing/scale;
 	circleSize = circleSize/scale;
 	squareSize = squareSize/scale;
@@ -17,12 +18,14 @@ if (finalVersion) {
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let renderCounterTwo=0;
 var PixelDensity = 100*2;
-var DrawCalls = 50;
+var DrawCalls = 250;
 
 function preload() {
-	sourceImg = loadImage("input_4.jpg");
-	maskImg = loadImage("mask_6.png");
+	sourceImg = loadImage("input_1.jpg");
+	maskImg = loadImage("mask_1.png");
+	maskTwoImg = loadImage("mask_5.png");
 }
 
 function setup () {
@@ -34,6 +37,7 @@ function setup () {
 	background(255);
 	sourceImg.loadPixels();
 	maskImg.loadPixels();
+	console.log("finalVersion is " + finalVersion);
 }
 
 function convertRgbToHsluv(c) {
@@ -48,28 +52,53 @@ function draw () {
 		let dy = floor(random(elementSpacing/2));
 		let pix = sourceImg.get(x, y);
 		let mask = maskImg.get(x, y);
+		// let maskTwo = maskTwoImg.get(x, y);
+		let x2 = floor(random(sourceImg.width));
+		let y2 = floor(random(sourceImg.height));
+		let pix2 = sourceImg.get(x2, y2);
+		let maskTwo = maskTwoImg.get(x2, y2);
+		
 		let halfSize = squareSize/2;
-		if(mask[0] > 128) {
+		if(maskTwo[0] > 128 && SecondDraw == true) {
+			fill(pix2);
+			noStroke();
+			ellipse(x2, y2, squareSize, squareSize);
+			// noStroke();
+			// fill(pix);
+			// star(x, y, squareSize, squareSize, 2); 
+		}
+		if(mask[0] > 128 && SecondDraw == false) {
 			noStroke();
 			fill(pix);
 			star(x, y, squareSize, halfSize, 4); 
-  		}
-  		else {
-	      // add random offsets
-	      x = x + dx;
-	      y = y + dy;
-	      // convert to grayscale (remove color, keep brightness in hsluv colorspace)
-	      let hsluvColor = convertRgbToHsluv(pix);
-	      fillHsluv(0, 0, hsluvColor[2]);
-	      stroke(3);
-	      star(x, y, squareSize, halfSize/1.5, 3);
-	  	}
+		}
+		else {
+			if( SecondDraw == false){
+		      // add random offsets
+		      x = x + dx;
+		      y = y + dy;
+		      // convert to grayscale (remove color, keep brightness in hsluv colorspace)
+		      let hsluvColor = convertRgbToHsluv(pix);
+		      fillHsluv(0, 0, hsluvColor[2]);
+		      stroke(3);
+		      star(x, y, squareSize, halfSize/1.5, 3);
+	  		}
+		}
 	}	
 	renderCounter = renderCounter + 1;
-	if(renderCounter > 1920/elementSpacing) {
-		console.log("Done!")
-		noLoop();
+	if(renderCounter > 1920/elementSpacing && SecondDraw == false) {
+		console.log("First Draw Done!")
+		SecondDraw = true;
+		//noLoop();
 	    // saveBlocksImages();
+	} 
+	if(SecondDraw) {
+		renderCounterTwo = renderCounterTwo + 1; 
+		if(renderCounterTwo > DrawCalls) {
+			console.log("Second Draw Done!")
+			noLoop();
+			// saveBlocksImages();
+		}
 	}
 }
 
