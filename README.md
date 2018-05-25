0cd4f04a51330790941252650fe6721569f42b18
Stella Vokia-Scarlett <scarlestel@myvuw.ac.nz>
2018-05-25 14:37:42 +1200

    Experimenting with spacing

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 ## Creative Coding 2: Custom Pixel
 
-I took each of these photos: starting with 3 different subjects so i can get an idea of the theme/images i want to look at using for my final. The first is of a quote related to faith, the second is lavender, and then a portrait of a friend. 
-Each image also has different colours, the first is quite dull, the second consists of just two colours and the third is very vibrant and colourful.
-
-Next i will look more at a theme between pictures and experimenting with spacing and grids or perhaps removing backgroud colour.
+Just looking at more subject matter, and playing around with spacing.
+I realised i don't like this look of smaller close together squares with more seperated circles.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+/* Default versions of variables */
+let elementSpacing = 30;
+let circleSize = 60;
+let squareSize = 40;
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -19,9 +24,12 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  // for(let i=0;i<100;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let x = int(i * elementSpacing);
+    let y = int(renderCounter * elementSpacing);
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let pointSize = 30; //let element spacing = 40  
@@ -35,7 +43,7 @@ function draw () {
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 100) {
+  if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
