153da7e272872d31e69d1817ffaae4b274bc1810
Vivian CHEN <479615538@qq.com>
2018-05-29 14:11:48 +1200

    Update the test render.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+These images are the photo I took last year in DSDN 144. I use them to do the test render and also practice how to mask the images by my own. 
--- sketch.js
+++ sketch.js
@@ -24,18 +24,18 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let pointSize = 40;
+    let halfSize = 20;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
     }
     else {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
-    }
+    }    
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 50) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
