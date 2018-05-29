20a94be9536f4a4c23aaa768b58b8deb97ec7166
William Shaw <williamjonathanshaw@gmail.com>
2018-05-29 13:47:39 +1200

    updated file size

--- sketch.js
+++ sketch.js
@@ -19,27 +19,32 @@ function setup () {
 }
 
 function draw () {
+  //define sizes and spacing
     let circleSize = 10;
     let elementSpacing = 10;
     let elementSpacing2 = 20;
     let squareSize = 20;
-
+  // set variables inside for loop
   for(let i=0;i<1080/elementSpacing;i++) {
     let x = int (i * elementSpacing);
     let y = int (renderCounter * elementSpacing);
     let x2 = int (i * elementSpacing2);
     let y2 = int (renderCounter * elementSpacing2);
+    //define random offsets
     let dx = floor (random(elementSpacing/2));
     let dy = floor (random(elementSpacing/2))
+    //fit source image
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    //let halfSize = squareSize/2;
+    //set random offsets
     x = x + dx;
     y = y + dy;
     fill(pix);
+    //draw ellipses within mask
     if(mask[0] > 128) {
       ellipse(x, y, circleSize, circleSize);
     }
+    //draw rectangles for background
     else {
       rect(x2, y2, squareSize, squareSize);     
     }
