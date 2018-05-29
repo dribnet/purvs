c4be5f951d2aa007a92a79c62871724a8a80b8f9
Molisang <molijun801@gmail.com>
2018-05-29 12:54:00 +1200

    Part2 - regular

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-I chose three images about lemon, a design from LUX exhibition, and yellow rose. Because I wanna see the influence of color and light on each object. 
+The inspiration of the pattern was from Ukiyo-e, a genre of Japanese art which flourished from the 17th through 19th centuries. Kanagawa Oki Nami Ura is a famous masterpiece from Katsushika Hokusai, one of the Japanese artists from the Japanese art. It is about great wave of the occean. So I created five circles for the masking part to show the wave. 
+
+Also, I reduced the lightness of another part to demonstarte the strong contract between vivid colors and dark colors.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,33 @@
+
+const finalVersion = true;
+
+
+let elementSpacing = 40;
+let circleSize = 75;
+// let circleSize2 = 60;
+// let circleSize3 = 45;
+// let circleSize4 = 30;
+// let circleSize5 = 15;
+let squareSize = 50;
+
+
+if (finalVersion) {
+	let elementSpacing = 20;
+	let circleSize = 37.5;
+	// let circleSize2 = 30;
+	// let circleSize3 = 22.5;
+	// let circleSize4 = 15;
+	// let circleSize5 = 7.5;
+	let squareSize = 25;
+}
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_2.jpg");
+  maskImg = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -12,30 +35,54 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  // angleMode(DEGREES);
+  // noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+function convertRgbToHsluv (c) {
+	return hsluv.rgbToHsluv([c[0]/255.0,c[1]/255.0,c[2]/255.0]);
+}
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let x = int(i * elementSpacing);
+    let y = int(renderCounter * elementSpacing);
+    // let x = floor(random(sourceImg.width));
+    // let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 30;
-    let halfSize = 30;
-    fill(pix);
+    let halfSize = squareSize/2;
+    // fill(pix);
+    let hsluvColor = convertRgbToHsluv(pix);
+    let circleSize2 = map(hsluvColor[1], 0, 100, 60, 70);
+    let circleSize3 = map(hsluvColor[1], 0, 100, 45, 55);
+    let circleSize4 = map(hsluvColor[1], 0, 100, 30, 40);
+    let circleSize5 = map(hsluvColor[1], 0, 100, 15, 25);
+
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      stroke(pix);
+      strokeWeight(2.5);
+      fill(10);
+      ellipse(x, y, circleSize, circleSize);
+      ellipse(x, y, circleSize2, circleSize2);
+      ellipse(x, y, circleSize3, circleSize3);
+      ellipse(x, y, circleSize4, circleSize4);
+      ellipse(x, y, circleSize5, circleSize5);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    	noStroke();
+    	fillHsluv(0,0,hsluvColor[2]);
+      rect(x-halfSize, y-halfSize, squareSize, squareSize); 
+  
     }
+
+
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 100) {
+  if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
