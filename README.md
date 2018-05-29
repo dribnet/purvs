372db9df4dd748a336a6ed370c16453a6f8e7ac3
Cody Stiles <stilescody@myvuw.ac.nz>
2018-05-29 15:16:48 +1200

    SmartMask
    These have been created using the SmartMask program. They are images taken from the internet - consisting of Extreme Frisbee, a couple standing infront of a rainbow in the desert and a bunch of potted plants.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-The code has been modified using Tom's code to make the output more uniform and tidy. I like how the squares sit/fit tightly against one-another. The circles, however, I like a little less because of the white space they have inbetween each other. I think I will next try the different codes until I find one that creates an output I like and then I will try to edit it further to make it more my own style.
+These images have been created using the SmartMask program. I have used masks such as 'person', 'frisbee' and 'potted plant' in my images. I like the way this program creates quality masks for images, but I also enjoy the freedom you have when making your own masks in photoshop.
+
+(The three images are of Extreme Frisbee with two players lunging for the frisbee, a couple standing infront of a rainbow in a desert, and a bunch of potted plants.) 
--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
+//I have used some of Tom White's example code for my own project
+//https://bl.ocks.org/dribnet/2b5fdedb10367f0db717755169946832/b9eca66520db6771e0e0299da527af99496efb95
+//https://bl.ocks.org/dribnet/2b5fdedb10367f0db717755169946832/a0a30d1ab172f2695fffb48b90e7aec031b9f6ce
+
+let elementSpacing = 50;
+let circleSize = 40;
+let squareSize = 40;
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-//I have used some of Tom White's example code for my own project
-//https://bl.ocks.org/dribnet/2b5fdedb10367f0db717755169946832/b9eca66520db6771e0e0299da527af99496efb95
-
-
-
 function preload() {
-  sourceImg = loadImage("second.jpg");
-  maskImg = loadImage("secondbw.png");
+  sourceImg = loadImage("23frisbee.jpg");
+  maskImg = loadImage("23frisbeebw.png");
 }
 
 function setup () {
@@ -18,7 +20,6 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -26,22 +27,22 @@ function setup () {
 const pointSize = 40;
 
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
-  	let x = int(i * pointSize);
-  	let y = int(renderCounter * pointSize);
+  for(let i=0;i<1080/elementSpacing;i++) {
+  	let x = int(i * elementSpacing);
+  	let y = int(renderCounter * elementSpacing);
     //let x = floor(random(sourceImg.width));
     //let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let halfSize = pointSize/2;
+    let halfSize = squareSize/2;
     //let pointSize = 60;
     //let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      ellipse(x, y, circleSize, circleSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      rect(x-halfSize, y-halfSize, squareSize, squareSize);    
     }
   }
   renderCounter = renderCounter + 1;
