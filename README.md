7f90176e5b272077e3dea9ae298a4e7c82f3cbca
Guest User <Guest@wg401-i-02-07.mddn.vuw.ac.nz>
2018-05-29 14:48:46 +1200

    updated code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Creative Coding 2: Custom Pixel
 
 My custom pixel is made up of a gridded circle layout where there is a mask with multiple different layers of a greyscale colour that is picked up by the sketch code. If the colour is within the set range it draws the circle on the grid at a different size, creating a scaling effect.
+
+I added an offset to see what it would like.
+After doing some research ive decided to go towards a roy lichtenstein art style for my finale image.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -20,7 +20,7 @@ function setup () {
 
 
 // const pointSize = 30;
-let elementSpacing = 25;
+let elementSpacing = 15;
 
 function draw () {
 
@@ -28,11 +28,14 @@ function draw () {
 
   for(let i=0;i<1080/elementSpacing;i++) {
 
+
     let x = int(i * elementSpacing);
     let y = int(renderCounter * elementSpacing);
 
-    // let x = floor(random(sourceImg.width));
-    // let y = floor(random(sourceImg.height));
+  	if(i%2 == 0){
+  	 y += elementSpacing/2;
+  	}
+
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let halfSize = elementSpacing/2;
@@ -40,25 +43,30 @@ function draw () {
 
     //change mask
     let circleSize = elementSpacing;
-    //o\0.53.87
     if(mask[0] == 0) { //mask layers
       noStroke()
       ellipse(x, y, circleSize, circleSize);
     }
-    if(mask[0] > 0 && mask[0] < 70) {
-      ellipse(x, y, circleSize * .8, circleSize * .8);
+    if(mask[0] > 0 && mask[0] < 50) {
+      ellipse(x, y, circleSize * .95, circleSize * .95);
     }
-    if(mask[0] == 87) {
-      ellipse(x, y, circleSize * .6 , circleSize * .6);
+    if(mask[0] > 50 && mask[0] < 100) {
+      ellipse(x, y, circleSize * .8 , circleSize * .8);
     }
-    else { //background dots
+    if(mask[0] > 100 && mask[0] < 150) {
+      ellipse(x, y, circleSize * .65 , circleSize * .65);
+    }
+    if(mask[0] > 150 && mask[0] < 200) {
+      ellipse(x, y, circleSize * .55 , circleSize * .55);
+    }
+    if(mask[0] > 200) { //background dots
       // rect(x-halfSize, y-halfSize, elementSpacing, elementSpacing);  
       ellipse(x, y,  circleSize * .5 ,circleSize * .5) ;
 
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 100) {	//render counter = number of times it goes thru the algorithim.
+  if(renderCounter > 400) {	//render counter = number of times it goes thru the algorithim.
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
