b4a650bb10d0ffb72b0831b69cec5e6cd7868ede
kayskip <karuskip@gmail.com>
2018-05-29 15:21:07 +1200

    Added tones of stuff, changes alg and different images and readme

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ## Creative Coding 2: Custom Pixel
 
+Tue 29th May
+I have worked out how to create two masks and implement them into my artwork piece. I have also taken polygon code from 
+https://p5js.org/es/examples/form-regular-polygon.html to create an almost 3D affect of the bushes in the first picture.
+I am now going to keep experimenting with the code I have created and try to produce something creative from my next few pictures.
+I am also going to create secondary masks for my other pictures.
+
+Fri 25th May
 For my theme I am leaning on the idea of the cultural enviroment taking over the natural enviroment. Merely touching on the idea of pollution and how we are destroying our earth.
 
-
+Tue 22nd May and Previous
 I have imported my own images and used the sample code given in class and changed it. So far I have done all 3 of my images with masks and replaced the old cartoon ones I was experimenting with earlier. I am now going to experiment with the code to create abstract images of my 3 imported images to get more ideas.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
+let maskTwo=null;
+let renderCounter=0; 
+let renderCounterTwo=0;
+var SecondDraw = false;
+var DrawCalls = 400;
+let circleSize = 40;
+const pointSize = 40;
+
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
+  maskTwoImg = loadImage("mask_1_2.png");
 }
 
 function setup () {
@@ -13,40 +21,81 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(0);
+  background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-const pointSize = 20;
-
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
+  for(let i = 0; i < 1080 / pointSize * 2; i++) {
+  	/* First Mask */
     let x = int(i * pointSize);
     let y = int(renderCounter * pointSize);
-    // let x = floor(random(sourceImg.width));
-    // let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+
+
+    /* Second Mask */
+	let x2 = floor(random(sourceImg.width));
+	let y2 = floor(random(sourceImg.height));
+	let pix2 = sourceImg.get(x2, y2);
+	let maskTwo = maskTwoImg.get(x2, y2);
     let halfSize = pointSize/2;
     fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+
+    /* Masking */
+    if(mask[0] > 128 && !SecondDraw) {
+    fill(pix);
+      polygon(x, y, pointSize, 6);
     }
-    else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    if(maskTwo[0] > 128 && SecondDraw) {
+    fill(pix2);
+    //triangle(x2-halfSize, y+pointSize, x2, y2, x2+halfSize, y2+pointSize); 
+    rect(x2, y2, circleSize, circleSize);
+    }
+     else {
+      fill(pix);
+      //triangle(x-halfSize, y+pointSize, x, y, x+halfSize, y+pointSize); 
+      rect(x, y, pointSize/1.1, pointSize/1.1);
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 1920/pointSize) {
-    console.log("Done!")
-    noLoop();
+  renderCounter ++;
+
+  /* First Loop */
+  if(renderCounter > 1920/pointSize && !SecondDraw) {
+    console.log("Done!");
+    SecondDraw = true;
+    //noLoop();
     // saveBlocksImages();
   }
+  /* Second Loop */
+  if(SecondDraw) {
+		renderCounterTwo++;
+		if(renderCounterTwo > DrawCalls) {
+			console.log("Done!");
+			noLoop();
+			// saveBlocksImages();
+		}
+	}
 }
-
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
+function convertRgbToHsluv(c) {
+	return hsluv.rgbToHsluv([c[0]/255.0, c[1]/255.0, c[2]/255.0]);
+}
+
+//polygon code from https://p5js.org/es/examples/form-regular-polygon.html
+function polygon(x, y, radius, npoints) {
+	angleMode(RADIANS);
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
