b131d29af0de5cbd064bc213c6ccc8be9d95d1e2
Jackie2017vic <jackiexu2017@gmail.com>
2018-05-31 19:11:06 +1200

    updated the sec version of 3d

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 ## Creative Coding 2: Custom Pixel
 
-The start
+
+progress
+
+I was inspired by the work form processing. The initial idea is if I transfer the whole thing to the Z axis and I may find a specific angle to give a sense of 3d to viewers.
+I referenced Isometric projection, An isometric view of an object can be obtained by choosing the viewing direction such that the angles between the projections of the x, y, and z-axes are all the same. It points out the mathematics method how to calculate the rotation angle.
+
+I push my experiment from 2d to 3d, that's the key point of my plan. I have done some research about how to control the camera viewpoint ortho() so that I can show the image by a setting-up angle.
+
+
+
+<!-- The start
 
 I spent about two weeks to explore the field of image processing both in p5js and processing. After I gained deeper understanding of it, I was going to think about custom pixels.
 
@@ -9,6 +19,6 @@ The first step is how to locate each pixel in the image. Definitely, there is a 
 Then, I roughly create different shapes for the pixels to take a glance of the visual effect.
 
 By the way, I was thinking of the animation effect for this project. I am betting that would be cool. surely, in this case, I have tried sorts of methods to figure it out.
-
+ -->
 <!-- The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
  -->
--- sketch.js
+++ sketch.js
@@ -2,56 +2,73 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
+let angle = 0;
+let w = 24;
+let ma;
+
 function preload() {
   sourceImg = loadImage("input_1.jpg");
   maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
-  let main_canvas = createCanvas(1080, 1920);
+  let main_canvas = createCanvas(1080, 1080, WEBGL);
   main_canvas.parent('canvasContainer');
+  ma = atan(1 /sqrt(2));
+  // let main_canvas = createCanvas(1080, 1920);
+  // main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
-  background(255);
+  // noStroke();
+  // background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 const pointSize = 20;
 
-function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
-    let x = int(i * pointSize);
-    let y = int(renderCounter * pointSize);
-    // let x = floor(random(sourceImg.width));
-    // let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let halfSize = pointSize/2;
-    fill(pix);
-    if(mask[0] > 128) {
-      // triangle(x-halfSize, y, x, y+pointSize, x+halfSize, y);
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      if ( x%pointSize == 0) {
-      	triangle(x-halfSize, y, x, y+pointSize, x+halfSize, y);
-      }
-      else {
-	  	triangle(x-halfSize, y+pointSize, x, y, x+halfSize, y+pointSize); 
+function draw() {
+  background(100);
+  // ortho();
+  ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 500);
+  translate(0, 50, -50);
+  // rotateX(PI /4);
+  rotateX(-QUARTER_PI);
+  rotateY(ma);
+  // translate(width / 2, height / 2);
+  rectMode(CENTER);
+  // rotateX(angle * 0.25);
+
+  let offset = 0;
+  for (let z = 0; z < height; z += w ) {
+    for (let x = 0; x < width; x += w) {
+      let pix = sourceImg.get(x, z);
+      let mask = maskImg.get(x, z);
+      fill(pix);
+      push();
+      let a = angle + offset;
+      let h = map(sin(a), -1, 1, 0, 100); 
+      // w; 
+      // map(sin(a), -1, 1, 0, 100);      
+
+      translate(x - width/2, 0, z - height/2);
+      // normalMaterial();
+
+      if(mask[0] > 128) {
+        h = h + 100;
       }
 
-      // triangle(x-halfSize, y+pointSize, x, y, x+halfSize, y+pointSize); 
-      // rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      box(w-2, h, w - 2);
+      offset += 0.1
+
+      pop();
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 1920/pointSize) {
-    console.log("Done!")
-    noLoop();
-    // saveBlocksImages();
-  }
+  angle += 0.1
+
+  console.log("Done!")
+  noLoop();
+
 }
 
 function keyTyped() {
