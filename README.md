62fb2cac847327b9e75ca678dc385f3e5a32863d
EmilyMonkman <emily.monkman@gmail.com>
2018-06-01 10:35:18 +1200

    update code, readme and pictures

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-Theses images are potos I took quite a while ago, I'm just using them as fill ins at the moment and they will probably change. I played around with the maps to see what different looks they can give and followed one of the example of code to change from the default look.
-Not sure if I like it at the moment but it is interesting trying things out.
+Still just playing around with the code a bit and seeing how I like things.
--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
-const finalVersion = true;
+const finalVersion = false;
 
-let pointSize = 40;
- if (finalVersion) {
-  pointSize = 20;
- }
+      elementSpacing = 10;
+      elementySpacing = 30;
+      circleSize = 10;
+      squareSize = 15;
+  
+  if (finalVersion) {
+    elementSpacing = 10;
+    elementySpacing = 30;
+    circleSize = 25;
+    squareSize = 15;
+  }
 
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+//   pointSize = 40;
+  
+// if (finalVersion) {
+//   pointSize = 20;
+// }
+
+      sourceImg=null;
+      maskImg=null;
+      renderCounter=0;
 
 function preload() {
   sourceImg = loadImage("input_3.jpg");
@@ -15,7 +28,8 @@ function preload() {
 }
 
 function setup () {
-  let main_canvas = createCanvas(1080, 1920);
+       main_canvas = createCanvas(1080, 1920);
+  
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
@@ -26,25 +40,27 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
-    let x = int(i * pointSize);
-    let y = int(renderCounter * pointSize);
-    // let x = floor(random(sourceImg.width));
-    // let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    
-    let halfSize = pointSize/2;
+  scale(0.5);
+  for( i=0;i<1080/elementSpacing;i++) {
+          x = int(i * elementSpacing);
+          y = int(renderCounter * elementySpacing);
+          dx = floor(random(elementSpacing/2));
+          dy = floor(random(elementySpacing/2));
+    x = x + dx;
+    y = y + dy;
+          pix = sourceImg.get(x, y);
+          mask = maskImg.get(x, y);
+          halfSize = squareSize/2;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      ellipse(x, y, circleSize, circleSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      rect(x-halfSize, y-halfSize, squareSize, squareSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 1920/pointSize) {
+  if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
