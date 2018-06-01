ee5bebb29c3b56a6eff88c264959714446cee4db
Guest User <Guest@wg401-b-02-06.mddn.vuw.ac.nz>
2018-06-01 14:25:50 +1200

    first commit

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("trialpic2.jpg");
+  maskImg = loadImage("mask_4.png");
 }
 
 function setup () {
@@ -19,16 +19,17 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
+  for(let i=0;i<500;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
+    let pointSize = ;
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
+      //ellipse(x-2,y-2, pointSize -10, pointSize -10)
     }
     else {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
