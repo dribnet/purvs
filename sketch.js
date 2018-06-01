399d5b47f83f05af78b37eb48234013acced29fb
ruby <rubbyyjohnson@gmail.com>
2018-06-01 15:16:07 +1200

    starrrr

--- sketch.js
+++ sketch.js
@@ -209,8 +209,8 @@ let renderCounter=0;
 
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
   //sourceImg = loadImage("input_1.jpg");
 }
 
