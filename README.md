ef9c92ccbc3f6e1213465c436b70da26f82df81f
Eddie Smith <ejsmith9725@gmail.com>
2018-06-01 15:27:48 +1200

    Back to original

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-No idea what to really do with the project, I feel like my current idea is lacking on all fronts. So I might just start form scratch and come up with a new idea entirely.
+Back to old idea again
+
+I talked to michael about my idea and got some ideas for where to take this, I'll choose some maps and draw an airline style map over them using a noise function of some sort. I think the end result will be quite good. I'll just have to figure out how to divy it up into 3 sections.
--- sketch.js
+++ sketch.js
@@ -1,14 +1,10 @@
 let sourceImg=null;
 let maskImg=null;
-let sourceImg2=null;
-let maskImg2=null;
 let renderCounter=0;
 
 function preload() {
   sourceImg = loadImage("input_1.jpg");
-  sourceImg2 = loadImage("input_2.jpg");
   maskImg = loadImage("mask_1.png");
-  maskImg2 = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -20,47 +16,46 @@ function setup () {
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
-  sourceImg2.loadPixels();
-  maskImg2.loadPixels();
-
 }
 
-const pointSize = 50;
+const pointSize = 25;
 
 function draw () {
+  let x1 = 0;
+  let y1 = 0;
+
   for(let i=0;i<1080;i++) {
-    //let x = floor(random(sourceImg.width));
-    //let y = floor(random(sourceImg.height));
     let x = int(i * pointSize);
     let y = int(renderCounter * pointSize);
-    let x2 = int(i * pointSize);
-    let y2 = int(renderCounter * pointSize);
-
     let pix = sourceImg.get(x, y);
-    let pix2 = sourceImg2.get(x, y);
-
     let mask = maskImg.get(x, y);
-    let mask2 = maskImg2.get(x, y);
-
     let halfSize = pointSize/2;
-    fill(pix,pix2);
-    stroke(255);
-    if(mask[0],mask2[0] > 128) {
-      beginShape();
-      vertex(x, y);
-      vertex(x2, y2);
-      endShape();
-      stroke(0);
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);
-      rect(x2-halfSize, y2-halfSize, pointSize, pointSize);
+    fill(pix);
+    stroke(pix);
+
+    if(mask[0] > 128) {
+      if(x1!=0) {
+        rect(x, y, pointSize, pointSize);
+
+        strokeWeight(5);
+        stroke(255);
+        line(x1+halfSize, y1+halfSize, x+halfSize, y+halfSize);
+        x1 = x;
+        y1 = y;
+      }
+      else{
+        x1 = x;
+        y1 = y;
+      }
     }
-    else {
-      stroke(0);
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    else{
+      noStroke();
+      rect(x, y, pointSize, pointSize);
     }
-  }
+}
+
   renderCounter = renderCounter + 1;
-  if(renderCounter > 1920) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
