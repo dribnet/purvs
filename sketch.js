96349872e54eeadf5947abbc44a4593c861d2881
Guest User <Guest@wg401-i-01-07.mddn.vuw.ac.nz>
2018-06-01 15:19:00 +1200

    updates

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 //let renderCounter=0;
 let curRow = 0;
+let loop2 = false;
+let loop2Done = false;
 
 function preload() {
   sourceImg = loadImage("input_1.png");
@@ -50,15 +52,47 @@ function draw () {
       //rect(x-halfSize, y-halfSize, squareSize/5, squareSize/5); 
     }
 
-	if(mask[0] >141 && mask[0] <159){
+	// if(mask[0] >141 && mask[0] <159){
+ //      x2 = x2 + nx;
+ //      y2 = y2 + ny;
+ //      fill(249, 235, 235, 150);
+ //      ellipse(x, y, circleSize, circleSize);
+ //    }
+  }
+
+  if (curRow*elementSpacing > 1920 || loop2 == true){
+    if (loop2 == false){
+      curRow = 0;
+    }
+    loop2 = true;
+    for (let j=0;j<1080/elementSpacing;j++) {
+
+    let nx = floor(random(elementSpacing2*2));
+    let ny = floor(random(elementSpacing2*2));
+    let x = j * elementSpacing;
+    let y = curRow * elementSpacing;
+    let x2 = j * elementSpacing2;
+    let y2 = curRow * elementSpacing2;
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    let circleSize = (random(30, 70));
+    let halfSize = squareSize/2;
+    fill(pix);
+
+      if(mask[0] >141 && mask[0] <159){
       x2 = x2 + nx;
       y2 = y2 + ny;
       fill(249, 235, 235, 150);
       ellipse(x, y, circleSize, circleSize);
     }
+    }
+    if (curRow*elementSpacing > 1920 && loop2 == true){
+      loop2Done = true;
   }
+}
+
   curRow = curRow + 1;
-  if(curRow*elementSpacing > 1920) {
+  if(curRow*elementSpacing > 1920 && loop2Done == true) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
