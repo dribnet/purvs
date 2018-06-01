e7dc56610458754d4b25f37b13514deff3c981ed
Eddie Smith <ejsmith9725@gmail.com>
2018-06-01 14:14:43 +1200

    Change of Plans

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-I was playing around trying to get an idea for something to display and thought of using the masks to draw an airline like map of connections around the  globe, I next need to figure out a way to display a map in portait mode and how to connect lines between different masks.
+No idea what to really do with the project, I feel like my current idea is lacking on all fronts. So I might just start form scratch and come up with a new idea entirely.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let sourceImg=null;
 let maskImg=null;
+let sourceImg2=null;
+let maskImg2=null;
 let renderCounter=0;
 
 function preload() {
   sourceImg = loadImage("input_1.jpg");
+  sourceImg2 = loadImage("input_2.jpg");
   maskImg = loadImage("mask_1.png");
+  maskImg2 = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -16,9 +20,12 @@ function setup () {
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  sourceImg2.loadPixels();
+  maskImg2.loadPixels();
+
 }
 
-const pointSize = 10;
+const pointSize = 50;
 
 function draw () {
   for(let i=0;i<1080;i++) {
@@ -26,19 +33,26 @@ function draw () {
     //let y = floor(random(sourceImg.height));
     let x = int(i * pointSize);
     let y = int(renderCounter * pointSize);
+    let x2 = int(i * pointSize);
+    let y2 = int(renderCounter * pointSize);
+
     let pix = sourceImg.get(x, y);
+    let pix2 = sourceImg2.get(x, y);
+
     let mask = maskImg.get(x, y);
+    let mask2 = maskImg2.get(x, y);
+
     let halfSize = pointSize/2;
-    fill(pix);
+    fill(pix,pix2);
     stroke(255);
-    if(mask[0] > 128) {
+    if(mask[0],mask2[0] > 128) {
       beginShape();
       vertex(x, y);
-      vertex(pointSize, pointSize);
-      vertex(x, y);
+      vertex(x2, y2);
       endShape();
       stroke(0);
       rect(x-halfSize, y-halfSize, pointSize, pointSize);
+      rect(x2-halfSize, y2-halfSize, pointSize, pointSize);
     }
     else {
       stroke(0);
