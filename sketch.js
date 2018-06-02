da53e650f7ee34c695f24f0e724a30b2b3fbb598
Aliz Ritthisri <eiizteayzhy.px@gmail.com>
2018-06-02 17:15:14 +1200

    020618

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let elementSpacing=20;
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -19,23 +20,25 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    // let x = floor(random(sourceImg.width));
+    // let y = floor(random(sourceImg.height));
+    let x = int(i*elementSpacing);
+    let y = int(renderCounter*elementSpacing);
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 50;
-    let halfSize = 50;
+    let pointSize = 20;
+    let halfSize = 20;
     fill(pix);
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+      rect(x-halfSize, y-halfSize, pointSize, pointSize);
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 50) {
+  if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
