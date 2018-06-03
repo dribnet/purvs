7183b35e40a63320173eb4ecfd27e7a38ae34b49
HanleyMakene <hanley.makene@hotmail.com>
2018-06-03 14:34:19 +1200

    Started working with a second pass

--- sketch.js
+++ sketch.js
@@ -3,73 +3,84 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+ 	sourceImg = loadImage("input_1.jpg");
+ 	maskImg = loadImage("mask_1.png");
+ 	maskImgb = loadImage("mask_1b.png");
 }
 
 function setup () {
-  let main_canvas = createCanvas(1080, 1920);
-  main_canvas.parent('canvasContainer');
+  	let main_canvas = createCanvas(1080, 1920);
+ 	main_canvas.parent('canvasContainer');
 
-  imageMode(CENTER);
-  noStroke();
-  background(255);
-  sourceImg.loadPixels();
-  maskImg.loadPixels();
+ 	imageMode(CENTER);
+ 	noStroke();
+ 	background(0);
+  	sourceImg.loadPixels();
+ 	maskImg.loadPixels();
+ 	maskImgb.loadPixels();
 }
 
 const pointSize = 40;
+let draw_pass = 0;
+let x1 = 0;
+let y1 = 0;
+
 function draw () {
-  let x1 = 0;
-  let y1 = 0;
-  for(let i=0;i<1080.pointSize;i++) {
-    //let x = int(i * pointSize);
-    //let y = int(renderCounter * pointSize);
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let halfSize = pointSize/2;
-    fill(pix);
-    stroke(pix);
+ 	//let x1 = 0;
+ 	//let y1 = 0;
+  	for(let i=0; i<50; i++) {
+	    //let x = int(i * pointSize);
+	    //let y = int(renderCounter * pointSize);
+	    let x = floor(random(sourceImg.width));
+	    let y = floor(random(sourceImg.height));
+	    let pix = sourceImg.get(x, y);
+	    let mask = maskImg.get(x, y);
+	    let maskb = maskImgb.get(x, y);
+	    let halfSize = pointSize/2;
+	    fill(pix);
+	    stroke(pix);
 
-    // original
-    /*if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
-    }
-}*/
-
-    // Test with connecting lines
-    if(mask[0] > 128) {
-    	if(x1!=0) {
-    		strokeWeight(5);
-    		line(x1+halfSize, y1+halfSize, x+halfSize, y+halfSize);
-    		x1 = x;
-    		y1 = y;
-    	}
-    	else{
-    		x1 = x;
-    		y1 = y;
-    	}
-    }
-    else{
-    	noStroke();
-    	rect(x, y, pointSize, pointSize);
-    }
-}
+	    if(draw_pass==0){
+		    // original
+		    if(mask[0] > 128 && draw_pass==0) {
+		      ellipse(x, y, pointSize, pointSize);
+		    }
+		    else {
+		      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+		    }
+	  	}
+		else if(draw_pass==1){
+	    	// connecting lines
+		    if(maskb[0] > 128 && draw_pass==1) {
+		    	if(x1!=0) {	
+		    		strokeWeight(5);
+		    		line(x1+halfSize, y1+halfSize, x+halfSize, y+halfSize);
+		    		x1 = x;
+		    		y1 = y;
+		    	}
+		    	else{
+		    		x1 = x;
+		    		y1 = y;
+		    	}
+		    }
+		}
+  	}
 
 
 
   
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 1920/pointSize) {
-    console.log("Done!")
-    noLoop();
-    // saveBlocksImages();
-  }
+	renderCounter = renderCounter + 1;
+	if(renderCounter > 100) {
+ 		if(draw_pass==0) {
+ 			renderCounter = 0;
+ 			draw_pass = 1;
+ 		}
+ 		else{
+    		console.log("Done!")
+    		noLoop();
+    	}
+    	// saveBlocksImages();
+    }
 }
 
 function keyTyped() {
