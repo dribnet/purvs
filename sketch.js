753d260b5cd99d3897ff801558407453c80fc7bf
Guest User <Guest@wg401-i-07-07.mddn.vuw.ac.nz>
2018-06-03 17:12:43 +1200

    refinements

--- sketch.js
+++ sketch.js
@@ -2,12 +2,24 @@ let sourceImg=null;
 let maskImg=null;
 //let renderCounter=0;
 let curRow = 0;
-let loop2 = false;
-let loop2Done = false;
+  
+  // let colours = [
+  // "219, 210, 213, 150",
+  // "218, 223, 232, 150",
+  // "168, 161, 164, 150",
+  // "255, 232, 241, 150",
+  // "232, 251, 255, 150"
+  // ];
+//let colour = random(0-4);
+//let colour = Math.ceil(Math.random() * 4);
+//let colour.sort(function(a, b) {return 0.5- Math.random()});
+//let colour = colours[Math.floor(Math.random()* 4)];
+//let colour = Math.random() * 4;
+//let blurCol = colours[colour];
 
 function preload() {
-  sourceImg = loadImage("input_1.png");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_3.png");
+  maskImg = loadImage("mask_3.png");
 }
 
 function setup () {
@@ -27,75 +39,60 @@ let elementSpacing2 = 50;
 //let elementSpacingH = 20;
 let squareSize = 15;
 
+let drawPass = 0;
 
 function draw () {
   for(let i=0;i<1080/elementSpacing;i++) {
-    let nx = floor(random(elementSpacing2*2));
-    let ny = floor(random(elementSpacing2*2));
     let x = i * elementSpacing;
     let y = curRow * elementSpacing;
+   
     let x2 = i * elementSpacing2;
     let y2 = curRow * elementSpacing2;
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
+    let dx = floor(random(elementSpacing2/2));
+    let dy = floor(random(elementSpacing2/2));
     let circleSize = (random(30, 70));
 
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+
     let halfSize = squareSize/2;
+
+    
+
     fill(pix);
 
-    if(mask[0] > 160) {
+    if((mask[0] > 160)&&(drawPass == 0)) {
       rect(x-halfSize, y-halfSize, squareSize*2, squareSize*2);   
-      //rect(x-halfSize, y-halfSize, squareSize, squareSize);
-    }
-    if (mask[0] < 140){
+    }  
+    if ((mask[0] < 140)&&(drawPass == 0)){
       rect(x-halfSize, y-halfSize, squareSize/2, squareSize*2);  
-      //rect(x-halfSize, y-halfSize, squareSize/5, squareSize/5); 
+    }
+      if((mask[0] >141 && mask[0] <159)&&(drawPass == 1)) {
+      // x2 = x2 + dx;
+      // y2 = y2 + dy;
+      let nx = x2 / 5.0;
+      let ny = y2 / 30.0;
+      let jit_x = 80*(noise(nx, ny, 0)-0.5);
+      let jit_y = 80*(noise(nx, ny, 10)-0.2)
+      //fill(219, 210, 213, 150);
+      //fill(random(colours));
+      //fill(blurCol);
+      fill(219, 210, 213, 150)
+      ellipse(x + jit_x, y + jit_y, circleSize, circleSize);
     }
 
-	// if(mask[0] >141 && mask[0] <159){
- //      x2 = x2 + nx;
- //      y2 = y2 + ny;
- //      fill(249, 235, 235, 150);
- //      ellipse(x, y, circleSize, circleSize);
- //    }
-  }
-
-  if (curRow*elementSpacing > 1920 || loop2 == true){
-    if (loop2 == false){
-      curRow = 0;
-    }
-    loop2 = true;
-    for (let j=0;j<1080/elementSpacing;j++) {
-
-    let nx = floor(random(elementSpacing2*2));
-    let ny = floor(random(elementSpacing2*2));
-    let x = j * elementSpacing;
-    let y = curRow * elementSpacing;
-    let x2 = j * elementSpacing2;
-    let y2 = curRow * elementSpacing2;
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let circleSize = (random(30, 70));
-    let halfSize = squareSize/2;
-    fill(pix);
-
-      if(mask[0] >141 && mask[0] <159){
-      x2 = x2 + nx;
-      y2 = y2 + ny;
-      fill(249, 235, 235, 150);
-      ellipse(x, y, circleSize, circleSize);
-    }
-    }
-    if (curRow*elementSpacing > 1920 && loop2 == true){
-      loop2Done = true;
-  }
 }
 
   curRow = curRow + 1;
-  if(curRow*elementSpacing > 1920 && loop2Done == true) {
+  if(curRow*elementSpacing > 1920) {
+  	if(drawPass == 0){
+  		curRow = 0;
+  		drawPass = 1;
+  	} else {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
+}
   }
 }
 
