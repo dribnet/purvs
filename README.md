9918690535bc20960edb4d22896ceca978cb495c
Callum Browne <callumbrowne.is@gmail.com>
2018-06-04 01:29:20 +1200

    Project Update. Algorithm changes. More in README

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-So I have been thinking of what I can do for a while now, and I don't really have a big message to say. Currently my idea for the whole project is "the importance of play". It more so just relates to me as someone who has grown up with video games my whole life, and how important they have been for be. It also plays into the idea of how important it is to take a step back from the everyday streeses of life and take time to enojoy what you love and relax.
-
-For the image I want to represent this idea by using my desk. My desk is the main part of my room. Its where I have my main setup where I spend the majority of my time working on uni, researching gmaes, and playing games, and catching up with friends, almost everyday. I plan on having many video game references in the image, with figures and stuff, as well as have on the monitor something like "Welcome Back" with other images and stuff surrounding it.
-
-For the algorithm itself I am thinking of using a primairly squares and triangles to build the images. Squares to represent old school pixel art, and the triangles to represent modern day 3d graphics. For the colours I am thinking about using the colours of the SNES, those being grey, green, blue, yellow, and red. This is because there is a wide variety of colours to build the images from, as well as the SNES being an iconic machine with a really recognisable colour pallet.
-
-All of this is still subject to change, but I think that this is a good direction.
-
-I have just been messing with the algorithm and trying to get it looking nice
+So after messing around with the code for a while, I am starting to get a feel for how I want the algorithm to look. I like the look of algorithms that give finer details so I will try and stick with that. I have decided to not change the colours for the time being as it doesn't look too great right now. For the 3 photos that I will do, I will still be going with the Desk idea and have decided on the photos. The left will have gadegets like headphones, the middel will have the monitor, and the right will have a stack of books. Basically I bunch of stuff that I like. I have also been messing with the photos and should have them in the next update with a more final design.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+const pointSize = 30;
+let count = 0;
 
 function preload() {
   sourceImg = loadImage("input_1.jpg");
@@ -18,41 +20,15 @@ function setup () {
 }
 
 function draw () {
-	/*
-	for(let row = 0; row < sourceImg.height; row++){
-		for(let col = 0; col < sourceImg.width; col++){
-			let pix = sourceImg.get(col, row);
-			let mask = maskImg.get(col, row);
-			let pointSize = 1;
-			let halfSize = pointSize/2;
-			// Decide on the colour
-			if(pix[0] > pix[1] && pix[0] > pix[2]){
-				pix = color(pix[0],0,0);
-			}else if(pix[1] > pix[0] && pix[1] > pix[2]){
-				pix = color(0,pix[1],0);
-			}else if(pix[2] > pix[1] && pix[2] > pix[0]){
-				pix = color(0,0,pix[2]);
-			}else{
-				pix = color(128,128,128);
-			}
-			fill(pix);
-			// If in the mask, use squares
-			if(mask[0] > 128) {
-			  rect(col-halfSize, row-halfSize, pointSize, pointSize);      
-			}else {
-			  ellipse(col, row, pointSize, pointSize);
-			}
-		}
-	}
-	*/	
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0; i<1080/pointSize; i++) {
+    let x = i * pointSize;
+    let y = renderCounter * pointSize;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 50;
     let halfSize = pointSize/2;
-	// Decide on the colour
+	fill(0,0,0);
+	rect(x-halfSize, y-halfSize, pointSize, pointSize); 
+	/*
 	if(pix[0] > pix[1] && pix[0] > pix[2]){
 		pix = color(pix[0],0,0);
 	}else if(pix[1] > pix[0] && pix[1] > pix[2]){
@@ -62,16 +38,22 @@ function draw () {
 	}else{
 		pix = color(128,128,128);
 	}
+	*/
     fill(pix);
-	// If in the mask, use squares
     if(mask[0] > 128) {
-	  rect(x-halfSize, y-halfSize, pointSize, pointSize);      
-    }else {
-      ellipse(x, y, pointSize, pointSize);
+		if(count % 2){
+			triangle(x-pointSize/2, y-pointSize/2, x, y+pointSize/2, x+pointSize/2, y-pointSize/2);
+		}else{
+			triangle(x-pointSize/2, y+pointSize/2, x, y-pointSize/2, x+pointSize/2, y+pointSize/2);
+		}		
+		count++;
+    }
+    else {
+		ellipse(x, y, pointSize, pointSize);   
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 20) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
