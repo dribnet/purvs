a2702b846941ab7c399100985cdc291516ca6318
Dual-Spec <john.r.hilbert@gmail.com>
2018-06-04 23:02:34 +1200

    Begining diamond disolve experiments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
+I was looking through a magazine one day and saw people holding the tools of their trade, always facing the camera. Always prominently showing the person and their trade or hobby clearly. I wanted to take these things and obscure them as much as possible. Anton, smoking his weed out front of his band's lockout, and Marie in front of the graffiti that she loves so much. I felt these images put more of the mystery on the person rather than the object, while at the same time dissolving the ego.  
 
-On second look, I found notes on why I was doing these obscured portraits when I was in France. So, what I had semi jokingly described as a series of obscured portraits of people in France actually had a deeper meaning. The rationale needs a little bit further work though, so I will update that in the next readme.
-As far as the renderer at this point, I am just working to see what sorts of pixel densities and passes will perhaps work for my custom shapes.
+In this experiment I am beginning to literally dissolve the individual themselves. I plan on doing a bit of a scale and fade away on the diamonds that makeup the subject's body. Thus further eroding the individual (yay) yet trying to retain some of the original intent of the photo.
--- sketch.js
+++ sketch.js
@@ -1,16 +1,47 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
+let currentRow=0;
 
+//Render Quality settings: 'low' 'med' 'high'
+let quality = "med";
+let diamondSize = 10;
+let circleSize = 10;
+let spacing = 10;
+
+//renderer Quality settings:
+switch(quality){
+  case "low":
+    diamondSize = 80;
+    circleSize = 80;
+    spacing = 80;
+    break;
+
+  case "med":
+    diamondSize = 30;
+    circleSize = 30;
+    spacing = 30;
+    break;
+
+  case "high":
+    diamondSize = 10;
+    circleSize = 10;
+    spacing = 10;
+    break;
+
+  default:
+    diamondSize = 50;
+}
+//Image loading for operations
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
+//Canvas and image information setup.
 function setup () {
   let main_canvas = createCanvas(1080, 1920);
   main_canvas.parent('canvasContainer');
-
+  angleMode(DEGREES);
   imageMode(CENTER);
   noStroke();
   background(195,68,47);
@@ -19,23 +50,28 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<1000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/diamondSize;i++) {
+    //let x = floor(random(sourceImg.width));
+    //let y = floor(random(sourceImg.height));
+    let x = i * spacing;
+    let y = currentRow * spacing;
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 25;
-    let halfSize = 35;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      //let halfSize = spacing/3;
+      push();
+      translate(x,y);
+      rotate(15);
+      rect(0, 0, diamondSize, diamondSize);
+      pop();
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);
+      ellipse(x, y, circleSize, circleSize);
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  currentRow = currentRow + 1;
+  if(currentRow *  spacing>1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
