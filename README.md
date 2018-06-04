923be4c1246fc3b59db9b1be688fe41c815f6737
Matthew Taylor <taylormatt7@myvuw.ac.nz>
2018-06-04 15:24:14 +1200

    Implemented majority of contrast-mapping function

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+The image shown is the result of my 'mask' generating code that essentially functions as a sort of edge detection, with white areas representing areas with higher contrast and black with less. I will be using this information to drive a turtle-based tendril visualisation.
+
+This code was actually largely written a little bit ago, but health problems have gotten in the way of working on this a bit.
+
+I believe this backing mask will serve my image concept of "Connectivity" well, by using the contrast areas for chance of tendril splitting.
+
+
+---
+
+#### Changes:
+
+- Implemented the majority of the contrast-mapping function.
+
+#### Issues:
+
+- Values might be a bit too noisy for my intended purpose.
+
+#### To-Do:
+
+- Implement a map-smoothing component to the delta map function.
+
+#### Notes:
+
+-
+
--- sketch.js
+++ sketch.js
@@ -1,49 +1,100 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImage = null;
+let deltaMap = [];
+
+const RED_MULT = 1;
+const GREEN_MULT = 1;
+const BLUE_MULT = 1;
+
+const REGION_SIZE = 30; //24 may be solid but figure it out per images
+const X_REGIONS = 1080/REGION_SIZE;
+const Y_REGIONS = 1920/REGION_SIZE;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+	sourceImage = loadImage("input_2.jpg");
 }
 
-function setup () {
-  let main_canvas = createCanvas(1080, 1920);
-  main_canvas.parent('canvasContainer');
+function setup() {
+	let main_canvas = createCanvas(1080, 1920);
+	main_canvas.parent('canvasContainer');
 
-  imageMode(CENTER);
-  noStroke();
-  background(255);
-  sourceImg.loadPixels();
-  maskImg.loadPixels();
+	pixelDensity(1);
+	imageMode(CENTER);
+	background(255);
+	sourceImage.loadPixels();
+	noStroke();
+	//image(sourceImage, 0, 0);
+
+	print("calculating map");
+	let t = millis();
+	calculateDeltaMap();
+	print("100%!");
+	let t2 = millis()-t;
+	print("map calculated in "+t2/1000 + " seconds.");
+	for (let i = 0; i < X_REGIONS; i++) {
+		for (let j = 0; j < Y_REGIONS; j++) {
+			fill(deltaMap[i][j]*255);
+			rect(i*REGION_SIZE, j*REGION_SIZE, REGION_SIZE, REGION_SIZE);
+		}
+	}
 }
 
-function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
-    }
-  }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
-    console.log("Done!")
-    noLoop();
-    // saveBlocksImages();
-  }
+function calculateDeltaMap() {
+	let minDelta = 255*255*255;
+	let maxDelta = 0;
+	for (let i = 0; i < X_REGIONS; i++) {
+		print(""+floor(i/X_REGIONS*100)+"%");
+		let col = [];
+		for (let j = 0; j < Y_REGIONS; j++) {
+			//print("y: "+j);
+			//getting the value range in a region
+			let minR = 255, maxR = 0, minG = 255, maxG = 0, minB = 255, maxB = 0;
+			for (let x = 0; x < REGION_SIZE; x++) {
+				for (let y = 0; y < REGION_SIZE; y++) {
+					let xPixel = i*REGION_SIZE*4+x*4;
+					let yPixel = j*REGION_SIZE*4320+y*4320;
+					let r = sourceImage.pixels[xPixel + yPixel];
+					let g = sourceImage.pixels[xPixel + yPixel + 1];
+					let b = sourceImage.pixels[xPixel + yPixel + 2];
+					minR = r < minR ? r : minR;
+					minB = b < minB ? b : minB;
+					minG = g < minG ? g : minG;
+
+					maxR = r > maxR ? r : maxR;
+					maxB = b > maxB ? b : maxB;
+					maxG = g > maxG ? g : maxG;
+				}
+			}
+			let delta = (maxR - minR) * RED_MULT + (maxG - minG) * GREEN_MULT + (maxB - minB) * BLUE_MULT;
+			//let delta = random(255)*random(255)*random(255);
+			minDelta = delta < minDelta ? delta : minDelta;
+			maxDelta = delta > maxDelta ? delta : maxDelta;
+			col.push(delta);
+		}
+		deltaMap.push(col);
+	}
+	for (let i = 0, xRegions = sourceImage.width/REGION_SIZE, yRegions = sourceImage.height/REGION_SIZE; i < xRegions; i++) {
+		for (let j = 0; j < yRegions; j++) {
+			deltaMap[i][j] = (deltaMap[i][j] - minDelta) / (maxDelta-minDelta);
+		}
+	}
+
+
+	// renderCounter = renderCounter + 1;
+	// if (renderCounter > 5) {
+	// 	console.log("Done!")
+	// 	noLoop();
+	// 	// saveBlocksImages();
+	// }
 }
 
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
+	if (key === '!') {
+		saveBlocksImages();
+	}
+	if (key === 'P') {
+		noLoop();
+	}
+	if (key === 'S') {
+		loop();
+	}
 }
