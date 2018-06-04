4c85095fb998bc238d6e7376419282287aea1b21
Yutong Chen <cona991@gmail.com>
2018-06-04 22:59:33 +1200

    random background

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
-
-I removed the background to focous on the people.
+The photos of this project were taken in the graduation ceremony, graduation is a very important thing for the student especially for the international student, We leave hometown thousand miles away for university education.
+The idea of making this custom pixel was to keep the image simple and clean and focus on the people, I removed the background of the picture instead of use coding to fill some circle with random colour.
--- sketch.js
+++ sketch.js
@@ -3,42 +3,79 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
   let main_canvas = createCanvas(1080, 1920);
   main_canvas.parent('canvasContainer');
-
   imageMode(CENTER);
   noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  // pixelDensity(1);
 }
 
+const pointSize = 30;
+const pointSize2 =30;
+
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/pointSize;i++) {
+    let x = int(i * pointSize);
+    let y = int(renderCounter * pointSize);
     let pix = sourceImg.get(x, y);
+    console.log(pix);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 10;
+    // let inc = 0.01;
+    // console.log(mask);
+
+    let halfSize = pointSize/2;
     fill(pix);
+    noStroke();
     if(mask[0] > 128) {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);
-    }
-    else {
       ellipse(x, y, pointSize, pointSize);
+      fill(255);
+      ellipse(x, y, pointSize/2, pointSize/2);
+      fill(pix);
+      ellipse(x, y, pointSize/3, pointSize/3);
+
+    }
+    // else if (mask[0] < 128 && mask[0] < 192) {
+    //   ellipse(x, y, pointSize/2, pointSize/2);
+    // }
+    //
+    // else if (mask[3] = 255) {
+    //   fill(0)
+    //   ellipse(x, y, pointSize, pointSize);
+    // }
+
+    else {
+      fill(pix);
+      rect(x-halfSize, y-halfSize, pointSize, pointSize);
+      fill(255);
+      rect(x-halfSize, y-halfSize, pointSize/2, pointSize/2);
+    }
+
+    if (pix[0] >= 255) {
+      let color1 = random(100,255);
+      // let color2 = random(0,255);
+      // let color3 = random(0,255);
+
+      fill(color1);
+      ellipse(x,y,pointSize2,pointSize2);
+      // fill(255);
+      // ellipse(x,y,pointSize2/2, pointSize2/2);
+
+
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 120) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
-    // saveBlocksImages();
+
   }
 }
 
