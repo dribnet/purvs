b7dd53a49d18015a0b1f4002fdef56549ef6c012
BenJSpearman <bkiwi64@gmail.com>
2018-06-04 21:11:16 +1200

    Fixed code to work, added a brush stroke size reference

--- sketch.js
+++ sketch.js
@@ -3,15 +3,18 @@
 var sourceImg=null;
 var maskImg=null;
 var renderCounter=0;
-var firstLayerCounter = 30;
-var mrenderCounter=0;
-var secondLayerCounter = 50;
-var frenderCounter=0;
-var thirdLayerCounter = 75;
-
 //Creating a "time" variable
-var t = 1;
+var t1 = 1;
+var t2 = 1;
+var t3 = 1;
+var rendlay1 = 200;
+var rendlay2 = 400;
+var rendlay3 = 700;
+var rendstop = rendlay3;
 
+var pointSize = 30;
+var medDetail = 10;
+var fineDetail = 5;
 
 function preload() {
 //Loading the three different image layers, the standard, the medium, and the fine
@@ -50,211 +53,197 @@ function setup () {
 }
 
 function draw () {
-  
 //For loop for the first base layer of paint, low res, so large brush stroke size (pointSize)
-if(renderCounter < firstLayerCounter){
+if(renderCounter < rendlay1){
+  firstLayer();
+}
+  
+//Second layer of paint, painting the "medium" level of detail  
+if(renderCounter > rendlay1 && renderCounter < rendlay2){
+  secondLayer();
+}  
+
+//Third and final layer, this one has tiny brush strokes for the fine detail
+if(renderCounter > rendlay2){
+  thirdLayer();
+}
+
+renderCounter = renderCounter + 1;
+console.log(renderCounter);
+
+if(renderCounter >= rendstop) {
+        console.log("Done!")
+        noLoop();
+        saveBlocksImages();
+      }
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+function firstLayer (){
   for(var i=0;i<50;i++) {
     var x = floor(random(sourceImg.width));
     var y = floor(random(sourceImg.height));
     var pix = sourceImg.get(x, y);
     var mask = maskImg.get(x, y);
-    var pointSize = 20;
     tint(pix);
     
     
 //If statements allow for even rotation and distribution of different brush stroke types
-      if(t == 1) {
+      if(t1 == 1) {
         image(strokeImg1,x, y, pointSize, 4*pointSize);
       }
-      if(t == 2) {
+      if(t1 == 2) {
         image(strokeImg2,x, y, pointSize, 4*pointSize);
       }
-      if(t == 3) {
+      if(t1 == 3) {
         image(strokeImg3,x, y, pointSize, 4*pointSize);
       }
-      if(t == 4) {
+      if(t1 == 4) {
         image(strokeImg4,x, y, pointSize, 4*pointSize);
       }
-      if(t == 5) {
+      if(t1 == 5) {
         image(strokeImg5,x, y, pointSize, 4*pointSize);
       }
-      if(t == 6) {
+      if(t1 == 6) {
         image(strokeImg6,x, y, pointSize, 4*pointSize);
       }
-      if(t == 7) {
+      if(t1 == 7) {
         image(strokeImg7,x, y, pointSize, 4*pointSize);
       }
-      if(t == 8) {
+      if(t1 == 8) {
         image(strokeImg8,x, y, pointSize, 4*pointSize);
       }
-      if(t == 9) {
+      if(t1 == 9) {
         image(strokeImg9,x, y, pointSize, 4*pointSize);
       }
-      if(t == 10) {
+      if(t1 == 10) {
         image(strokeImg10,x, y, pointSize, 4*pointSize);
       }
-      if(t == 11) {
+      if(t1 == 11) {
         image(strokeImg11,x, y, pointSize, 4*pointSize);
       }
 
 //Counting up the timer variable for use in if statements above   
-      if(t < 12){
-      t = t + 1;
+      if(t1 < 12){
+      t1 = t1 + 1;
       } else {
-        t = 1;
+        t1 = 1;
       }
-      
-    renderCounter = renderCounter + 1;
   }
 }
-  if(renderCounter >= firstLayerCounter) {
-        console.log("First layer done!")
-        //noLoop();
-        // saveBlocksImages();
-      }
-  
-  
-//
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//
 
-//Second layer of paint, painting the "medium" level of detail  
-if(mrenderCounter < secondLayerCounter){
+function secondLayer(){
   for(var j=0;j<50;j++) {
-      var mx = floor(random(medImg.width));
-      var my = floor(random(medImg.height));
-      var mpix = medImg.get(x, y);
-      var medDetail = 15
-      tint(mpix);
+      x = floor(random(sourceImg.width));
+      y = floor(random(sourceImg.height));
+      pix = medImg.get(x, y);
+      tint(pix);
       
-  
-        if(t == 1) {
-          image(strokeImg1,mx, my, medDetail, 4*medDetail);
+        if(t2 == 1) {
+          image(strokeImg1,x, y, medDetail, 4*medDetail);
         }
-        if(t == 2) {
-          image(strokeImg2,mx, my, medDetail, 4*medDetail);
+        if(t2 == 2) {
+          image(strokeImg2,x, y, medDetail, 4*medDetail);
         }
-        if(t == 3) {
-          image(strokeImg3,mx, my, medDetail, 4*medDetail);
+        if(t2 == 3) {
+          image(strokeImg3,x, y, medDetail, 4*medDetail);
         }
-        if(t == 4) {
-          image(strokeImg4,mx, my, medDetail, 4*medDetail);
+        if(t2 == 4) {
+          image(strokeImg4,x, y, medDetail, 4*medDetail);
         }
-        if(t == 5) {
-          image(strokeImg5,mx, my, medDetail, 4*medDetail);
+        if(t2 == 5) {
+          image(strokeImg5,x, y, medDetail, 4*medDetail);
         }
-        if(t == 6) {
-          image(strokeImg6,mx, my, medDetail, 4*medDetail);
+        if(t2 == 6) {
+          image(strokeImg6,x, y, medDetail, 4*medDetail);
         }
-        if(t == 7) {
-          image(strokeImg7,mx, my, medDetail, 4*medDetail);
+        if(t2 == 7) {
+          image(strokeImg7,x, y, medDetail, 4*medDetail);
         }
-        if(t == 8) {
-          image(strokeImg8,mx, my, medDetail, 4*medDetail);
+        if(t2 == 8) {
+          image(strokeImg8,x, y, medDetail, 4*medDetail);
         }
-        if(t == 9) {
-          image(strokeImg9,mx, my, medDetail, 4*medDetail);
+        if(t2 == 9) {
+          image(strokeImg9,x, y, medDetail, 4*medDetail);
         }
-        if(t == 10) {
-          image(strokeImg10,mx, my, medDetail, 4*medDetail);
+        if(t2 == 10) {
+          image(strokeImg10,x, y, medDetail, 4*medDetail);
         }
-        if(t == 11) {
-          image(strokeImg11,mx, my, medDetail, 4*medDetail);
+        if(t2 == 11) {
+          image(strokeImg11,x, y, medDetail, 4*medDetail);
         }
-  
-      
-      if(t < 12){
-      t = t + 1;
+
+      if(t2 < 12){
+      t2 = t2 + 1;
       } else {
-        t = 1;
+        t2 = 1;
       }
-      
-      mrenderCounter = mrenderCounter + 1;
-  }
-}  
-  if(mrenderCounter > secondLayerCounter) {
-      console.log("Second layer done!")
-      //noLoop();
-      // saveBlocksImages();
-    }
-  
-
-
-//
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//--------------------------------------------------------------------
-//
-  
-//Third and final layer, this one has tiny brush strokes for the fine detail
-if(frenderCounter < thirdLayerCounter){
-  for(var k=0;k<50;k++) {
-    var fx = floor(random(fineImg.width));
-    var fy = floor(random(fineImg.height));
-    var fpix = fineImg.get(x, y);
-    var fineDetail = 5
-    tint(fpix);
-    
-
-      if(t == 1) {
-        image(strokeImg1,fx,  fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 2) {
-        image(strokeImg2,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 3) {
-        image(strokeImg3,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 4) {
-        image(strokeImg4,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 5) {
-        image(strokeImg5,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 6) {
-        image(strokeImg6,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 7) {
-        image(strokeImg7,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 8) {
-        image(strokeImg8,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 9) {
-        image(strokeImg9,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 10) {
-        image(strokeImg10,fx, fy, fineDetail, 4*fineDetail);
-      }
-      if(t == 11) {
-        image(strokeImg11,fx, fy, fineDetail, 4*fineDetail);
-      }
-
-    
-    if(t < 12){
-    t = t + 1;
-    } else {
-      t = 1;
-    }
-    
-  }
-  frenderCounter = frenderCounter + 1;
   }
 }
 
-if(frenderCounter > thirdLayerCounter) {
-    console.log("Second layer done!")
-    noLoop();
-    // saveBlocksImages();
-  }
+function thirdLayer(){
+  for(var k=0;k<50;k++) {
+    x = floor(random(fineImg.width));
+    y = floor(random(fineImg.height));
+    pix = fineImg.get(x, y);
+    tint(pix);
+    
 
-function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
+      if(t3 == 1) {
+        image(strokeImg1,x,  y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 2) {
+        image(strokeImg2,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 3) {
+        image(strokeImg3,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 4) {
+        image(strokeImg4,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 5) {
+        image(strokeImg5,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 6) {
+        image(strokeImg6,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 7) {
+        image(strokeImg7,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 8) {
+        image(strokeImg8,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 9) {
+        image(strokeImg9,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 10) {
+        image(strokeImg10,x, y, fineDetail, 4*fineDetail);
+      }
+      if(t3 == 11) {
+        image(strokeImg11,x, y, fineDetail, 4*fineDetail);
+      }
+
+    
+      if(t3 < 12){
+      t3 = t3 + 1;
+      } else {
+        t3 = 1;
+      }
+    }
   }
 }
 
