7571137725ba7a2c6026bf17cd80a8849a9a0a44
William Shaw <williamjonathanshaw@gmail.com>
2018-06-05 12:39:55 +1200

    x

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -20,33 +20,27 @@ function setup () {
 
 function draw () {
   //define sizes and spacing
-    let circleSize = 5;
-    let elementSpacing = 5;
-    let elementSpacing2 = 10;
-    let squareSize = 10;
+    let shapeSize = 40;
+    let elementSpacing = 10;
+    let elementSpacing2 = 20;
+    let backgroundSize = 40;
   // set variables inside for loop
   for(let i=0;i<1080/elementSpacing;i++) {
     let x = int (i * elementSpacing);
     let y = int (renderCounter * elementSpacing);
     let x2 = int (i * elementSpacing2);
     let y2 = int (renderCounter * elementSpacing2);
-    //define random offsets
-    let dx = floor (random(elementSpacing/2));
-    let dy = floor (random(elementSpacing/2))
     //fit source image
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    //set random offsets
-    x = x + dx;
-    y = y + dy;
     fill(pix);
-    //draw ellipses within mask
+    //draw small pentagons for shape
     if(mask[0] > 128) {
-      ellipse(x, y, circleSize, circleSize);
+      polygon(x, y, shapeSize/8, shapeSize/8);
     }
-    //draw rectangles for background
+    //draw large pentagons for background
     else {
-      rect(x2, y2, squareSize, squareSize);  
+      polygon(x2, y2, backgroundSize/4, backgroundSize/8); 
     }
   }
   renderCounter = renderCounter + 1;
@@ -61,4 +55,15 @@ function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
+//function which creates a polygon
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
