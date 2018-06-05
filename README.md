736f91f22058ce6520c8a5966344d2608d9583d6
SilionZ <zhangsilion@gmail.com>
2018-06-05 14:06:05 +1200

    TEST 1

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Shenzhen MOCAPE, it basically showing the shapes of different object. The colour of these photos are very clean, is black and white tone.
+The images that I used are from Shenzhen MOCAP museum.
 
-In this version, I aim to use shape to show the light through the architecture. The artwork 2 is different sketch from Artwotk1 and 3, because i don't think one sketch could showing my concept.
+The image all about geometry, so I think it is quite connect with Pixel.
+
+I did not have too much change in this one, I simply changed the code to draw rectangle and rounds and irregular layout in the images.
+
+I quite like the result but It has not concept behind it.
--- sketch.js
+++ sketch.js
@@ -1,25 +1,10 @@
-/* Set to true to make final high-resolution version */
-const finalVersion = false;
-
-/* Default versions of variables */
-let elementSpacing = 40;
-let circleSize = 50;
-let squareSize = 20;
-
-/* Override some variables with high-resolution final version */
-if (finalVersion) {
-  elementSpacing = 20;
-  circleSize = 50;
-  squareSize = 10;
-}
-
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -28,52 +13,29 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  var c = color(65)
-  background(c);
+  background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-function owl(x, y, c, s) {
-  push();
-  translate(x, y);
-  triangle(10, 50, 20, 200, 100, 180);
-// arc(50, 50,50, 50, 0, PI + QUARTER_PI, PIE);
-  pop();
-}
-
-function lll(x, y, c, s) {
-  push();
-  translate(x, y);
-arc(50, 50,200, 200, 0, PI + QUARTER_PI, PIE);
-// triangle(150, 50, 150, 200, 50, 180);
-  pop();
-}
-
-
 function draw () {
-  for(let i=0;i<1080/elementSpacing;i++) {
-    let x = int(i * elementSpacing);
-    let y = int(renderCounter * elementSpacing);
-    let dx = floor(random(elementSpacing/2));
-    let dy = floor(random(elementSpacing/2));
-    x = x + dx;
-    y = y + dy;
+  for(let i=0;i<120;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let halfSize = squareSize/2;
+    let pointSize =100;
+    let halfSize = 80;
     fill(pix);
     if(mask[0] > 128) {
-      fill(pix);
-      // ellipse(x, y, circleSize, circleSize);
-      lll(x, y, pix, 0.8);
+      rect(x, y, pointSize, pointSize);
     }
     else {
-      owl(x, y, pix, 0.3);
+      ellipse(x-halfSize, y-halfSize, pointSize, pointSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 1920/elementSpacing) {
+  if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
