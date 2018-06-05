40a1e9488f4e00304b79972e19aca6c245c51321
Kathleen Griffin <griffin@outlook.co.nz>
2018-06-05 13:00:43 +1200

    tried a first image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+I first tried an image i took a while ago of my old scooter & a sunset. I chose this because I thought the sunset would look nice as a 'block' style blackground predominantly one colour, with slight variations in tone. The reason I also chose one with the scooter in the foreground is I'm thinknig my project will be something about transportation - perhaps in an urban setting. More on this to come! 
--- sketch.js
+++ sketch.js
@@ -1,49 +1,105 @@
+/* Set to true to make final high-resolution version */
+const finalVersion = false;
+
+/* Default versions of variables */
+let pointSize = 40;
+let doubPointSize = 80;
+
+/* Override some variables with high-resolution final version */
+if (finalVersion) {
+	pointSize = 20;
+}
+
+let dongles = [];
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
+
+let numPoints = 10;
+let points = [];
+
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+	sourceImg = loadImage("input_1.jpg");
+	maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
-  let main_canvas = createCanvas(1080, 1920);
-  main_canvas.parent('canvasContainer');
+	let main_canvas = createCanvas(1080, 1920);
+	main_canvas.parent('canvasContainer');
 
-  imageMode(CENTER);
-  noStroke();
-  background(255);
-  sourceImg.loadPixels();
-  maskImg.loadPixels();
+	imageMode(CENTER);
+	noStroke();
+	background(255);
+	sourceImg.loadPixels();
+	maskImg.loadPixels();
+
+
+	
 }
 
 function draw () {
-  for(let i=0;i<100;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+
+
+
+	for(let i=0;i<1080/pointSize;i++) {
+		let x = int(i * pointSize);
+		let y = int(renderCounter * pointSize);
+    // let x = floor(random(sourceImg.width));
+    // let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
-    let halfSize = 50;
+    let halfSize = pointSize/2;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+    	ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x-halfSize, y-halfSize, pointSize, pointSize);    
+    	rect(x-halfSize, y-halfSize, pointSize, pointSize); 
+
+
+    	
+
+
+
     }
-  }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
-    console.log("Done!")
-    noLoop();
+}
+renderCounter = renderCounter + 1;
+if(renderCounter > 1920/pointSize) {
+	console.log("Done!")
+	noLoop();
     // saveBlocksImages();
-  }
+}
 }
 
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
+	if (key == '!') {
+		saveBlocksImages();
+	}
 }
+
+
+class Dongle {
+
+	constructor(xPos, yPos, xOfst, yOfst) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.xOfst = xOfst;
+		this.yOfst = yOfst;
+	}
+
+	doDraw() {
+
+		alert(this.xPos);
+	}
+
+}
+
+let dongle = new Dongle( x, y);
+dongle.doDraw();
+
+
+
+
+
+
