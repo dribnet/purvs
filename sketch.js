428525c05528de816f6cc556c445f8b007e7f463
Guest User <Guest@wg401-h-07-07.mddn.vuw.ac.nz>
2018-06-05 21:08:11 +1200

    final push before adding purview.

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-const colorBack = "#e3eded";
+const colorBack = "#e3eded"; // using the colour from assignment 2, to make the backgound easy for the eyes
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_input_3.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -25,12 +25,12 @@ function draw () {
 
   for(let i=0;i<1080/pointSize;i++) {
     let x = int(i * pointSize);
-    let y = int(renderCounter * pointSize) - (4*renderCounter);
+    let y = int(renderCounter * pointSize) - (4*renderCounter); // used for the hexagon grid
     let halfSize = pointSize/2;
-    let pix = sourceImg.get(x+halfSize, y+halfSize);
-    let mask = maskImg.get(x+halfSize, y+halfSize);
-    if(mask[0] < 128) {
-      if(renderCounter%2 == 0){
+    let pix = sourceImg.get(x+halfSize, y+halfSize); // to curectly find colour 
+    let mask = maskImg.get(x+halfSize, y+halfSize); // to curectly find colour 
+    if(mask[0] < 128) { //crating the background using 3 hexagons (2 coloured and 1 white)
+      if(renderCounter%2 == 0){ // a small indent in the x axis, for the hexagon based grid 
         fill(pix);
         hexagon(x+(pointSize/2), y, pointSize);
         fill(255);
@@ -47,9 +47,9 @@ function draw () {
         hexagon(x, y, pointSize-(pointSize/2));
       }
     }
-    else {
+    else { //crating the foreground
       fill(pix);
-      if(renderCounter%2 == 0){
+      if(renderCounter%2 == 0){ // a small indent in the x axis, for the hexagon based grid
         hexagon(x+(pointSize/2), y, pointSize);
       }
       else{
@@ -65,7 +65,7 @@ function draw () {
   }
 }
 
-function hexagon(x, y, radius) {
+function hexagon(x, y, radius) { // draws a hexagon, rotated 90 degreases to fit in the grid
   var angle = TWO_PI / 6;
   beginShape();
   for (var a = -PI/2; a < TWO_PI*3/4; a += angle) {
