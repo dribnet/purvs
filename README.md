9c0a3a08bc429ba0790313e887f80d34cbee3e68
Nick Martin <martinnich2@myvuw.ac.nz>
2018-06-05 14:14:28 +1200

    Smartmask fix

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-Final Summary.
-The design that was used was made to represent the theme people in nature. There were originally supposed to be 3 layers to a mask but that changed to 5 due to the clothes pattern not suiting the skin and hair on the people and I wanted the people to be more detailed to show the people aspect to the design. The people pixels are all ordered and neat to represent man made objects which normally they are symmetrical and planned while the nature pixels used are random and jumbled to show what nature is like. The thing I liked the most in my design was the clothes pixel, I thought it was the best design for the pixel because even though most of it is black it shows colour and tone the best out of all the pixels.
+SmartMask
+
+In this commit I made the first layer in the masks using toms smartmask feature. I like the idea behind the smartmask but for windows users it is a hassel to use and I will not be using it aging because it would mean I have to transfer all of my files back over to the mac. If there was code that was added for the windows users I would use it again though. 
+
+With the first layer of masks it was useful to use this technique, but now I will have to photoshop and recode to fit in the masks as my code only cooperates with 2 different colours for the mask and I need it to work with 3 colours. Now I need to photoshop the greenery into the masks.
--- sketch.js
+++ sketch.js
@@ -2,9 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-//Image and mask loader
 function preload() {
-  sourceImg = loadImage("z_input_1.jpg");
+  sourceImg = loadImage("input_1.jpg");
   maskImg = loadImage("mask_1.png");
 }
 
@@ -19,85 +18,34 @@ function setup () {
   maskImg.loadPixels();
 }
 
-//size and spacing of pixels
 let pointSize = 40;
 let elementSpacing = 20;
 let squareSize = 20;
 let circleSize = 30
 
 function draw () {
-  for(let i=0;i<1180/elementSpacing;i++) {
-    //random element for nature
+  for(let i=0;i<1080/elementSpacing;i++) {
     let dx = floor(random(elementSpacing/2));
     let dy = floor(random(elementSpacing/2));
     let x = i * elementSpacing;
     let y = renderCounter * elementSpacing;
-    //colour
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let halfSize = 50;
     fill(pix);
-
-    //Nature
-    if(mask[0] == 0) {
-      noStroke();
+    if(mask[0] > 128) {
       x = x + dx;
       y = y + dy;
       ellipse(x-50, y-50, circleSize, circleSize);
     }
-    //Clothes
-    else if(mask[0] == 255){
-      push();
-      fill(0);
-      rect(x-halfSize, y-halfSize, squareSize, squareSize);
-      pop();
-      strokeWeight(2);
-      stroke(pix);
-      noFill();
-      ellipse(x-halfSize+10, y-halfSize+10, squareSize/2, squareSize/2);
-      ellipse(x-halfSize+5, y-halfSize+5, squareSize/2, squareSize/2);
-      ellipse(x-halfSize+15, y-halfSize+5, squareSize/2, squareSize/2);
-      ellipse(x-halfSize+5, y-halfSize+15, squareSize/2, squareSize/2);
-      ellipse(x-halfSize+15, y-halfSize+15, squareSize/2, squareSize/2);
-    }
-    //skin
-    else if(mask[0]>0 && mask[0]<100){
-      noStroke();
-      fill(pix);
-      rect(x-halfSize-5, y-halfSize, squareSize+2.5, squareSize+2.5);
-    }
-    //fur and hair
-    else if(mask[0]>100 && mask[0]<150){
-      strokeWeight(3);
-      stroke(pix);
-      noFill();
-      arc(x-halfSize+35, y-halfSize+16, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+37, y-halfSize+24, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+33, y-halfSize+22, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+30, y-halfSize+14, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+39, y-halfSize+20, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+31, y-halfSize+18, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+27, y-halfSize+14, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+29, y-halfSize+20, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+25, y-halfSize+18, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+23, y-halfSize+14, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-      arc(x-halfSize+21, y-halfSize+20, circleSize+20, circleSize+25, PI, PI + QUARTER_PI);
-     
-    }
-    //background/other
     else {
-      noStroke();
-      fill(pix);
-      beginShape();
-      vertex(x-halfSize, y-halfSize);
-      vertex(x-halfSize-20, y-halfSize+20);
-      vertex(x-halfSize, y-halfSize+40);
-      vertex(x-halfSize+20, y-halfSize+20);
-      endShape();
+      rect(x-halfSize, y-halfSize, squareSize, squareSize); 
+      fill(120);
+      rect(x-halfSize + squareSize/2, y-halfSize + squareSize/2, squareSize/4, squareSize/4);   
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter * elementSpacing > 2020) {
+  if(renderCounter * elementSpacing > 1920) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
