7af1a1159b133737805ba8f6c023041c4ac1a11a
Marithia <emzixv@gmail.com>
2018-06-05 17:27:40 +1200

    Final

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ let renderCounter=0;
  let tri3 = 40;
 
 function preload() {
-  sourceImg = loadImage("input_3.jpg");
-  maskImg = loadImage("mask_3.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
 }
 
 function setup () {
@@ -29,13 +29,11 @@ function setup () {
 }
 
 function draw () {
-  //for(let i=50;i<80;i++)
+
   for(let i=0;i<1080/elementSpacing;i++) {
     let x = int(i * elementSpacing);
     let y = int(renderCounter * elementSpacing);
     let istriangles = false;
-    //let x = floor(random(sourceImg.width));
-    //let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let halfSize = squareSize/2;
@@ -44,76 +42,42 @@ function draw () {
     let dy = floor(random(elementSpacing/2));
     x = x + dx;
     y = y + dy;
-    //z = z + dy;
 
     let a = 100;
 
     let pointSize = 10;
-    //let halfSize2 = 40;
-    //fill(pix, 10);//////////////////
-
-    //colorMode(RGB, 60, 60, 140);
     colorMode(RGB);
 
     if(mask[0] > 128) {
       noStroke();
-      //stroke(100, 55, 44);
-      //strokeWeight(7);
-      //ellipse(x, y, pointSize, pointSize);
-      //ellipse(x+30, y+60, circleSize, circleSize+40);
-      //triangle(x+30, y+60, z+10, circleSize, circleSize+40, circleSize);
-fill(pix[0], pix[1], pix[2], 40);
- push();
+
+    fill(pix[0], pix[1], pix[2], 40);
+    
+     push();
+
       translate(-10, 15);
-      //triangle(x-triSize3, y, x+triSize2, y-triSize3, x, y+triSize);
-      //triangle(x-triSize3, y, x+triSize, y-triSize2, x, y+triSize);
-   
       triangle(y, x+tri3, y, x-tri2, y, x);
       fill(pix[0], pix[1], pix[2], 30);
       rect(x, y, tri3, tri);
-      //triangle(x-triSize3, y, x-triSize3, y, x, y+triSize3);
-      //triangle(y, x-triSize, y, x-triSize3, x, x);
-      //stroke(pix);
-      //strokeWeight(0.5);
-      //rotate(-10);
       fill(pix[0], pix[1], pix[2], 50);
       triangle(x-tri2, y, x+tri, y-tri3, x, y+tri);
 
-      //triangle(y-triSize, y-triSize, y, x+triSize3, y, x+triSize2);
-      //triangle(x+triSize2, x, y, x, y+triSize3, x);
-
-      //rotate(a);
-      //translate(-10, 20);
-      //rotate(60);
-      //triangle(y, x+triSize3, y, x-triSize2, y, x);
-      //a = a + 0.1
-      //rect(x, y, squareSize, squareSize);
     pop();
-    }
+    }//End of flower
+
+    //Start of background
     else {
       fill(pix[0], pix[1], pix[2], 50);
 
       push();
-      //colorMode(RGB, 50, 20, 10);
-      // colorMode(HSL);
-      //rect(x-halfSize, y-halfSize, pointSize, pointSize); 
-      //rotate(60);
-      //ellipseMode(CENTER);
+
       translate(0, -10);
       noStroke();
-      //ellipse(x+30, y+80, circleSize+10, circleSize);
-      //rotate(-5);
-      //ellipse(x+80, y+15, circleSize+30, circleSize+20);
-      //a = a + 1;  
-      //rotate(0);
       triangle(x-triSize3, y+triSize2, x-triSize3, y+triSize3, x-triSize, y-triSize3);
-      //translate(0, 50);
-      //rotate(30);
-      //translate(100, -50);
-      //triangle(x+triSize, y+triSize2, x-triSize3, y+triSize3, x-triSize2, y+triSize3);
-      //triangle(x-triSize, y-triSize, y, x+triSize3, x-triSize2, x);  
+
       pop();
-    }
+    }//End of background
+    
   }
   renderCounter = renderCounter + 1;
 
