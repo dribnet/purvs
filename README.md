12bd2c4bbb3955fb51a5793c6f78cc6ddc8ff4cc
Molisang <molijun801@gmail.com>
2018-06-05 14:11:28 +1200

    SmartMask

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Theme
-Water cycle
+Experiment in SmartMask
 
-I chose three image from three different weather to explain my theme. 
-
-
-Elements
-Water goes through different forms: liquid, solid (snow) and vapor. The rain will drop on the ground. The river consist of water. Finally, the water will become vapor. 
-
-In this part, arc means rain. The white elements reflect snow. The little dot is for water vapour.  
-
-
-My Design
-The first image is about raining at dusk. This is my favorite one. The colors can define each part of the design. Also, the smooth change of the sky color is different from my third image. It isn't as agressive as the last one. The little dots can be seen clearly in the land part. The bright colors in the middle position reflect the result of raining on the land.  
-
-The second image talks about water cycle from a peaceful summer afternoon. The arc elements take the biggest propotion of the image. We can see only a few snowflowers in the image. And they are not really recognizable.  
-
-The third image is showing storm. I was trying to utilize the darkness of the sky to demonstrate the atmosphere before storm. Also, the grey stone of the sky can tell the coldness. We can see a contrast between the sky stone and land part (such as the orange rect).
-
-
-Smartmask
-Because there are two parts in my input images. One of them is the sky. Another one is the land. They are not specific objects. And I checked the masking categories. It didn't mention the sky or the land. Therefore, I didn'y use smartmask in this project.
+I tried to do something fun in the SmartMask. And I found there is a problem for this programming. Because there are two wine glasses in my third image. And when I run the program, it is not acceptable for it to use space for typing. So I used the numbers of the obejects to tell SmartMask to do the mask.   
--- sketch.js
+++ sketch.js
@@ -3,15 +3,13 @@ const finalVersion = true;
 
 
 let elementSpacing = 40;
-let snowSpacing = 150;
 let circleSize = 75;
+
 
 
 if (finalVersion) {
 	let elementSpacing = 20;
-	let snowSpacing = 75;
 	let circleSize = 37.5;
-
 }
 
 let sourceImg=null;
@@ -19,8 +17,8 @@ let maskImg=null;
 let renderCounter=0;
 
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("input_2.jpg");
+  maskImg = loadImage("mask_2.png");
 }
 
 function setup () {
@@ -30,7 +28,7 @@ function setup () {
   imageMode(CENTER);
   angleMode(DEGREES);
   // noStroke();
-  background(100);
+  background(10);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -39,84 +37,47 @@ function convertRgbToHsluv (c) {
 	return hsluv.rgbToHsluv([c[0]/255.0,c[1]/255.0,c[2]/255.0]);
 }
 
-
-function snowflower(x, y, c, s){
-	push();
-	rectMode(CENTER);
-	translate(x, y);
-	scale(s);  
-  	// stroke(c); 
-  	noStroke();
-  	fill(c);
-  	for (var i = 0; i <= 135; i = i + 45) {
-  		rotate(i);
-  		rect(0,0,2,15);
-  	}
-  	pop();
-}
-
-
-
 function draw () {
   for(let i=0;i<1080/elementSpacing;i++) {
     let x = int(i * elementSpacing);
     let y = int(renderCounter * elementSpacing);
     let dx = floor(random(elementSpacing));
     let dy = floor(random(elementSpacing));
-
-    let dx2 = floor(random(snowSpacing));
-    let dy2 = floor(random(snowSpacing));
-
-    let dx1 = floor(random(circleSize));
-    let dy1 = floor(random(circleSize));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     // fill(pix);
     let hsluvColor = convertRgbToHsluv(pix);
+    let circleSize2 = map(hsluvColor[1], 0, 100, 60, 70);
+    let circleSize3 = map(hsluvColor[1], 0, 100, 45, 55);
+    let circleSize4 = map(hsluvColor[1], 0, 100, 30, 40);
+    let circleSize5 = map(hsluvColor[1], 0, 100, 15, 25);
 
     let arcSize = map(hsluvColor[1], 0, 100, 40, 60);
-    let startAngle = map(hsluvColor[2], 0, 100, 180, 360);
-    let endAngle = map(hsluvColor[0], 0, 100, 359, 179);
-    let rectSize = arcSize;
-    let halfSize = rectSize/2;
-    let snowflowerSize = map(y,1920,0,0,1);
-    let snowflowerColor = map(y,0,1920,255,100);
+    let startAngle = map(hsluvColor[1], 0, 100, 180, 359);
+    let endAngle = map(hsluvColor[1], 0, 100, 360, 179);
+    let halfSize = arcSize/2;
 
-    let circleSize2 = map(y,1920,0,0,40);
-
-    if(mask[0] < 128) {
-    	noStroke();
-      push();
-      fill(pix);
-      rect(x, y, rectSize, rectSize);
-      pop();
-
-		x1 = dx1 + x;
-    	y1 = dy1 + y;
-      ellipse(x1, y1, circleSize2, circleSize2);
-
+    if(mask[0] > 128) {
+      stroke(pix);
+      strokeWeight(2.5);
+      fill(10);
+      ellipse(x, y, circleSize, circleSize);
+      ellipse(x, y, circleSize2, circleSize2);
+      ellipse(x, y, circleSize3, circleSize3);
+      ellipse(x, y, circleSize4, circleSize4);
+      ellipse(x, y, circleSize5, circleSize5);
     }
-
-
     else {
-    x1 = dx + x;
-    y1 = dy + y;
-    noStroke();
-    let hsluvColor = convertRgbToHsluv(pix);
-	fillHsluv(hsluvColor[0], 0, hsluvColor[2]/2);
-    rect(x, y, rectSize, rectSize);
+    x = dx + x;
+    y = dy + y;
+    	noStroke();
     fill(pix);
-	arc(x1-halfSize, y1-halfSize, arcSize, arcSize, startAngle, endAngle);
-
-	x = dx2 + x;
-	y = dy2 + y;
-	snowflower(x, y, snowflowerColor, snowflowerSize);
+      arc(x-halfSize, y-halfSize, arcSize, arcSize, startAngle, endAngle); 
+  
     }
 
 
   }
-
-
   renderCounter = renderCounter + 1;
   if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
