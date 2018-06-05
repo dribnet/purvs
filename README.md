ec21e209cd3faca20fa99c24e97a92bb4d6f2ec4
ruby <rubbyyjohnson@gmail.com>
2018-06-05 14:21:00 +1200

    updating new images

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-I have started working with a star pattern and trying to make this work with my photos. I am liking the out come of this and will keep experimenting and playing with the masks to see what i can get etc. 
+FINAL 
 
 The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -1,197 +1,3 @@
-// const finalVersion = false;
-
-
-// let elementSpacing = 50;
-// let circleSize = 50;
-// let squareSize = 40;
-// pointSize =20;
-
-
-// if (finalVersion) {
-//   elementSpacing = 20;
-//   circleSize = 25;
-//   squareSize = 20;
-// }
-
-// let sourceImg=null;
-// let maskImg=null;
-// let renderCounter=0;
-
-
-// function preload() {
-//   sourceImg = loadImage("input_1.jpg");
-//   maskImg = loadImage("mask_1.png");
-// }
-
-// function setup () {
-//   let main_canvas = createCanvas(1080, 1920);
-//   main_canvas.parent('canvasContainer');
-
-//   imageMode(CENTER);
-//   noStroke();
-//   background(255);
-//   sourceImg.loadPixels();
-//   maskImg.loadPixels();
-// }
-
-// function pattern (x,y,c,s){
-//  console.log("pattern");
-//     push();
-//     translate (x,y);
-//     fill(c);
-//     stroke(c);
-//     let starSize =  s*2;
-
-//     line(starSize*0,starSize*0, starSize*10,starSize*10);
-//     line(starSize*0,starSize*0, starSize*10,starSize*-10);
-//     line(starSize*0,starSize*0, starSize*-10,starSize*10);
-//     line(starSize*0,starSize*0, starSize*-10,starSize*-10);
-//     line(starSize*0,starSize*0, starSize*0,starSize*30);
-//     line(starSize*0,starSize*0, starSize*0,starSize*-30);
-//     line(starSize*0,starSize*0, starSize*30,starSize*0);
-//     line(starSize*0,starSize*0, starSize*-30,starSize*0);
-
-//     ellipse(starSize*10,starSize*10, starSize*2,starSize*2);
-//     ellipse(starSize*10,starSize*-10, starSize*2,starSize*2);
-//     ellipse(starSize*-10,starSize*10, starSize*2,starSize*2);
-//     ellipse(starSize*-10,starSize*-10, starSize*2,starSize*2);
-//     ellipse(starSize*0,starSize*30, starSize*4,starSize*4);
-//     ellipse(starSize*0,starSize*-30, starSize*4,starSize*4);
-//     ellipse(starSize*30,starSize*0, starSize*4,starSize*4);
-//     ellipse(starSize*-30,starSize*0,starSize*4,starSize*4);
-
-//     line(starSize*-30,starSize*0,starSize*-10,starSize*-10);
-//     line(starSize*0,starSize*-30,starSize*-10,starSize*-10);
-//     line(starSize*0,starSize*30,starSize*10,starSize*10);
-//     line(starSize*30,starSize*0,starSize*10,starSize*10);
-//     line(starSize*30,starSize*0,starSize*10,starSize*-10);
-//     line(starSize*0,starSize*-30,starSize*10,starSize*-10);
-//     line(starSize*0,starSize*30,starSize*-10,starSize*10);
-//     line(starSize*-30,starSize*0,starSize*-10,starSize*10);
-
-//     ellipse(starSize*25,starSize*25,starSize*1,starSize*1);
-//     ellipse(starSize*-25,starSize*-25,starSize*1,starSize*1);
-//     ellipse(starSize*-25,starSize*25,starSize*1,starSize*1);
-//     ellipse(starSize*25,starSize*-25,starSize*1,starSize*1);
-
-//     line(starSize*25,starSize*25,starSize*-10,starSize*10);
-//     line(starSize*25,starSize*25,starSize*10,starSize*-10);
-//     line(starSize*-25,starSize*-25,starSize*10,starSize*-10);
-//     line(starSize*-25,starSize*-25,starSize*-10,starSize*10);
-//     line(starSize*-25,starSize*25,starSize*10,starSize*10);
-//     line(starSize*-25,starSize*25,starSize*-10,starSize*-10);
-//     line(starSize*25,starSize*-25,starSize*10,starSize*10);
-//     line(starSize*25,starSize*-25,starSize*-10,starSize*-10);
-  
-//     pop();
-//   }
-
-
-
-// function splatter(x, y, c, s) {
-//   push();
-//   translate(x, y);
-//   scale(s);  // Set the createCanvas
-//   fill(c);
-
-//   let maxRand = 10;
-//   let minRand = -10;
-
-//   let posX = random(maxRand,minRand);
-//   let posY = random (maxRand,minRand);
-
-//   ellipse (200 + posX,2+posY,30,30);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   ellipse (25 + posX,30+posY,40,40);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   ellipse (0 + posX,0+posY,15,15);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   ellipse (50 + posX,50+posY,10,10);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   ellipse (40 + posX,15+posY,20,20);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   ellipse (0 + posX,40+posY,30,30);
-//    posX = random(maxRand,minRand);
-//    posY = random (maxRand,minRand);
-
-//   noStroke();
-//   strokeWeight(5);
-//   pop();
-
-
-// }
-// function convertRgbToHsluv(c) {
-//   return hsluv.rgbToHsluv([c[0]/255.0, c[1]/255.0, c[2]/255.0]);
-// }
-
-// function draw () {
-
-//    for(let i=0;i<400;i++) {
-//     let x = floor(random(sourceImg.width));
-//     let y = floor(random(sourceImg.height));  
-//     let dx = floor(random(elementSpacing/2));
-//     let dy = floor(random(elementSpacing/2));
-//     let pix = sourceImg.get(x, y);
-//     let mask = maskImg.get(x, y);
-//     let pointSize = 20;
-//     let halfSize = 50;
-//     fill(pix);
-//     if(mask[0] > 128) {
-//       splatter(x, y, pix, 1);
-//       //splatter(x, y, pix, pointSize/30);
-//       //ellipse(x,y,5,5);
-//     }
-//     else {
-
-//     for(let i=0;i<1080/elementSpacing;i++) {
-//     let x = int(i * elementSpacing);
-//     let y = int(renderCounter * elementSpacing);
-//     let dx = floor(random(elementSpacing/2));
-//     let dy = floor(random(elementSpacing/2));
-//     let pix = sourceImg.get(x, y);
-//     let mask = maskImg.get(x, y);
-//     let halfSize = squareSize/2;
-//     if(mask[0] < 128) {
-//       //pattern(x, y, pix, 0.5);
-//       //splatter(x, y, pix, pointSize/30);
-//       //pattern(x, y, pix, 0.5);
-//       //rect(x-halfSize, y-halfSize, squareSize/2, squareSize/2);  
-//     }
-//     else {
-//       // add random offsets
-//       x = x + dx;
-//       y = y + dy;
-//       // convert to grayscale (remove color, keep brightness in hsluv colorspace)
-//       let hsluvColor = convertRgbToHsluv(pix);
-//       fillHsluv(0, 0, hsluvColor[2]);
-//       //ellipse(x-halfSize, y-halfSize, squareSize/2, squareSize/2);    
-//     }
-//   }
-//   renderCounter = renderCounter + 1;
-//   if(renderCounter > 50) {
-//     console.log("Done!")
-//     noLoop();
-//     // saveBlocksImages();
-//   }
-// }
-// }
-// }
-
-// function keyTyped() {
-//   if (key == '!') {
-//     saveBlocksImages();
-//   }
-// }
 
 const finalVersion = true;
 
@@ -205,6 +11,7 @@ if (finalVersion) {
   elementSpacing = 10;
   circleSize = 25;
   squareSize = 20;
+  elementSpacing2 = 10;
 }
 
 let sourceImg=null;
@@ -215,8 +22,8 @@ let renderCounter=0;
 
 
 function preload() {
-  sourceImg = loadImage("input_2.jpg");
-  maskImg = loadImage("mask_2.png");
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.png");
   //sourceImg = loadImage("input_1.jpg");
 }
 
@@ -294,14 +101,47 @@ function pattern (x,y,c,s){
     stroke(c);
     let starSize =  s*1;
 
- line(starSize*0,starSize*0, starSize*10,starSize*10);
+
+   line(starSize*0,starSize*0, starSize*10,starSize*10);
     line(starSize*0,starSize*0, starSize*10,starSize*-10);
     line(starSize*0,starSize*0, starSize*-10,starSize*10);
     line(starSize*0,starSize*0, starSize*-10,starSize*-10);
-    line(starSize*0,starSize*0, starSize*0,starSize*20);
-    line(starSize*0,starSize*0, starSize*0,starSize*-20);
-    line(starSize*0,starSize*0, starSize*20,starSize*0);
-    line(starSize*0,starSize*0, starSize*-20,starSize*0);
+    line(starSize*0,starSize*0, starSize*0,starSize*30);
+    line(starSize*0,starSize*0, starSize*0,starSize*-30);
+    line(starSize*0,starSize*0, starSize*30,starSize*0);
+    line(starSize*0,starSize*0, starSize*-30,starSize*0);
+
+    ellipse(starSize*10,starSize*10, starSize*2,starSize*2);
+    ellipse(starSize*10,starSize*-10, starSize*2,starSize*2);
+    ellipse(starSize*-10,starSize*10, starSize*2,starSize*2);
+    ellipse(starSize*-10,starSize*-10, starSize*2,starSize*2);
+    ellipse(starSize*0,starSize*30, starSize*4,starSize*4);
+    ellipse(starSize*0,starSize*-30, starSize*4,starSize*4);
+    ellipse(starSize*30,starSize*0, starSize*4,starSize*4);
+    ellipse(starSize*-30,starSize*0,starSize*4,starSize*4);
+
+    line(starSize*-30,starSize*0,starSize*-10,starSize*-10);
+    line(starSize*0,starSize*-30,starSize*-10,starSize*-10);
+    line(starSize*0,starSize*30,starSize*10,starSize*10);
+    line(starSize*30,starSize*0,starSize*10,starSize*10);
+    line(starSize*30,starSize*0,starSize*10,starSize*-10);
+    line(starSize*0,starSize*-30,starSize*10,starSize*-10);
+    line(starSize*0,starSize*30,starSize*-10,starSize*10);
+    line(starSize*-30,starSize*0,starSize*-10,starSize*10);
+
+    ellipse(starSize*25,starSize*25,starSize*1,starSize*1);
+    ellipse(starSize*-25,starSize*-25,starSize*1,starSize*1);
+    ellipse(starSize*-25,starSize*25,starSize*1,starSize*1);
+    ellipse(starSize*25,starSize*-25,starSize*1,starSize*1);
+
+    line(starSize*25,starSize*25,starSize*-10,starSize*10);
+    line(starSize*25,starSize*25,starSize*10,starSize*-10);
+    line(starSize*-25,starSize*-25,starSize*10,starSize*-10);
+    line(starSize*-25,starSize*-25,starSize*-10,starSize*10);
+    line(starSize*-25,starSize*25,starSize*10,starSize*10);
+    line(starSize*-25,starSize*25,starSize*-10,starSize*-10);
+    line(starSize*25,starSize*-25,starSize*10,starSize*10);
+    line(starSize*25,starSize*-25,starSize*-10,starSize*-10);
   
     pop();
 
@@ -368,13 +208,14 @@ function draw () {
       //splatter(x, y, pix, 0.5);
     }
     else {
+
       // add random offsets
       x = x + dx;
       y = y + dy;
       // convert to grayscale (remove color, keep brightness in hsluv colorspace)
       let hsluvColor = convertRgbToHsluv(pix);
       fillHsluv(0, 0, hsluvColor[2]);
-      pattern2(x,y,pix,0.5);
+      pattern2(x,y,pix,1,elementSpacing2);
 
       //splatter(x, y, pix, elementSpacing/30);
     }
