a94d59465189e5ea30dad3b3050deb99939b9c9e
Kathleen Griffin <griffin@outlook.co.nz>
2018-06-05 15:59:52 +1200

    added foreground editing

--- sketch.js
+++ sketch.js
@@ -3,20 +3,18 @@ const finalVersion = false;
 
 /* Default versions of variables */
 let pointSize = 40;
-let size = 80;
+let bigPoint = 65;
 
 /* Override some variables with high-resolution final version */
 if (finalVersion) {
 	pointSize = 20;
 }
 
-let dongles = [];
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-
-let numPoints = 8;
+let numPoints = 15;
 let points = [];
 
 function preload() {
@@ -33,14 +31,9 @@ function setup () {
 	background(255);
 	sourceImg.loadPixels();
 	maskImg.loadPixels();
-
-
-	
 }
 
 function draw () {
-
-
 
 	for(let i=0;i<1080/pointSize;i++) {
 		let x = int(i * pointSize);
@@ -52,17 +45,41 @@ function draw () {
     let halfSize = pointSize/2;
     fill(pix);
     if(mask[0] > 128) {
-    	rect(x, y, pointSize, pointSize);
+    	stroke(pix);
+    	var r = pix[0];
+    	var g = pix[1];
+    	var b = pix[2];
+    	var a = pix[3];
+    	var fillcol;
+    	fill(pix)
+    	noStroke();
+    	rect(x, y, pointSize, pointSize); 
+    	fillcol = color(r+10,g+10,b+10,a);
+    	fill(fillcol);
+    	rect(x+pointSize/8, y+pointSize/8, pointSize*0.75, pointSize*0.75); 
+    	fillcol = color(r+20,g+20,b+20,a);
+    	fill(fillcol);
+    	rect(x+pointSize/4, y+pointSize/4, pointSize*0.5, pointSize*0.5); 
+    	fillcol = color(r+30,g+30,b+30,a);
+    	fill(fillcol);
+    	//i know this isn't how math works, but it's how my brain works
+    	rect(x+pointSize/2-pointSize/8, y+pointSize/2-pointSize/8, pointSize*0.25, pointSize*0.25); 
+    	
     }
     else {
-    	//rect(x-halfSize, y-halfSize, pointSize, pointSize); 
+    	//noStroke();
+    	//rect(x, y, pointSize, pointSize); 
 
 
     	stroke(pix);
-    	strokeWeight(0.5);
+    	strokeWeight(0.2);
+    	var m = numPoints;
+    	var n = numPoints;
     	for (k = 0; k < numPoints; k++) {
+    		n--
     		for (j = 0; j < numPoints; j++) {
-    			line(x - numPoints*1.5 + noise(k)*size, y - numPoints*1.5 + noise(j)*size, x  - numPoints*1.5 + noise(numPoints-k)*size, y - numPoints*1.5  + noise(numPoints-j)*size );
+    			n--
+    			line(x+ noise(k)*bigPoint - 10, y + noise(j)*bigPoint - 10, x + noise(n)*bigPoint - 10, y + noise(m)*bigPoint - 10 );
     		}
     	}
 
@@ -85,24 +102,7 @@ function keyTyped() {
 }
 
 
-class Dongle {
 
-	constructor(xPos, yPos, xOfst, yOfst) {
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.xOfst = xOfst;
-		this.yOfst = yOfst;
-	}
-
-	doDraw() {
-
-		alert(this.xPos);
-	}
-
-}
-
-let dongle = new Dongle( x, y);
-dongle.doDraw();
 
 
 
