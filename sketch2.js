d99b8bb042e50cab0815c5c34fd2762747899573
Fergus Lees <fergus9101@gmail.com>
2018-06-05 12:46:51 +1200

    Finished Final

---
+++ sketch2.js
@@ -0,0 +1,54 @@
+let sourceImg=null;
+let maskImg=null;
+let renderCounter=0;
+
+function preload() {
+  sourceImg = loadImage("input_1.jpg");
+  maskImg = loadImage("mask_1.bmp");
+}
+
+function setup () {
+  let main_canvas = createCanvas(1080, 1920);
+  main_canvas.parent('canvasContainer');
+
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  sourceImg.loadPixels();
+  maskImg.loadPixels();
+}
+
+function draw () {
+  stroke(255);
+  strokeWeight(2);
+  x = 50;
+  while (x < sourceImg.width) {
+    line(x,0,x,sourceImg.height);
+    x = x +50;
+  }
+
+  for(let i=0;i<100;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    let pointSize = 100;
+    let halfSize = 50;
+    fill(pix);
+    if(mask[0] > 128) {
+      ellipse(x, y, pointSize, pointSize);
+    }
+  }
+  renderCounter = renderCounter + 1;
+  if(renderCounter > 10) {
+    console.log("Done!")
+    noLoop();
+    // saveBlocksImages();
+  }
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
