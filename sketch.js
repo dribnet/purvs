6148420b2f7741707fc7675c1a8a35b830637201
Molisang <molijun801@gmail.com>
2018-06-05 12:40:00 +1200

    Part4

--- sketch.js
+++ sketch.js
@@ -3,12 +3,14 @@ const finalVersion = true;
 
 
 let elementSpacing = 40;
-// let circleSize = 75;
+let snowSpacing = 150;
+let circleSize = 75;
 
 
 if (finalVersion) {
 	let elementSpacing = 20;
-	// let circleSize = 37.5;
+	let snowSpacing = 75;
+	let circleSize = 37.5;
 
 }
 
@@ -38,6 +40,22 @@ function convertRgbToHsluv (c) {
 }
 
 
+function snowflower(x, y, c, s){
+	push();
+	rectMode(CENTER);
+	translate(x, y);
+	scale(s);  
+  	// stroke(c); 
+  	noStroke();
+  	fill(c);
+  	for (var i = 0; i <= 135; i = i + 45) {
+  		rotate(i);
+  		rect(0,0,2,15);
+  	}
+  	pop();
+}
+
+
 
 function draw () {
   for(let i=0;i<1080/elementSpacing;i++) {
@@ -45,6 +63,10 @@ function draw () {
     let y = int(renderCounter * elementSpacing);
     let dx = floor(random(elementSpacing));
     let dy = floor(random(elementSpacing));
+
+    let dx2 = floor(random(snowSpacing));
+    let dy2 = floor(random(snowSpacing));
+
     let dx1 = floor(random(circleSize));
     let dy1 = floor(random(circleSize));
     let pix = sourceImg.get(x, y);
@@ -55,20 +77,22 @@ function draw () {
     let arcSize = map(hsluvColor[1], 0, 100, 40, 60);
     let startAngle = map(hsluvColor[2], 0, 100, 180, 360);
     let endAngle = map(hsluvColor[0], 0, 100, 359, 179);
-    let halfSize = arcSize/2;
+    let rectSize = arcSize;
+    let halfSize = rectSize/2;
+    let snowflowerSize = map(y,1920,0,0,1);
+    let snowflowerColor = map(y,0,1920,255,100);
 
     let circleSize2 = map(y,1920,0,0,40);
 
     if(mask[0] < 128) {
-    	x1 = dx1 + x;
-    	y1 = dy1 + y;
     	noStroke();
- 
       push();
       fill(pix);
-      rect(x, y, arcSize, arcSize);
+      rect(x, y, rectSize, rectSize);
       pop();
 
+		x1 = dx1 + x;
+    	y1 = dy1 + y;
       ellipse(x1, y1, circleSize2, circleSize2);
 
     }
@@ -79,11 +103,14 @@ function draw () {
     y1 = dy + y;
     noStroke();
     let hsluvColor = convertRgbToHsluv(pix);
-	fillHsluv(hsluvColor[0], 0, hsluvColor[2]);
-    rect(x, y, arcSize, arcSize);
+	fillHsluv(hsluvColor[0], 0, hsluvColor[2]/2);
+    rect(x, y, rectSize, rectSize);
     fill(pix);
 	arc(x1-halfSize, y1-halfSize, arcSize, arcSize, startAngle, endAngle);
 
+	x = dx2 + x;
+	y = dy2 + y;
+	snowflower(x, y, snowflowerColor, snowflowerSize);
     }
 
 
