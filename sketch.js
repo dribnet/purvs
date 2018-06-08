3dcd31398ab5f28304e569bd74ba67deba15d39b
Sieni Faafuata <sienifaafuata@Sienis-MacBook-Pro.local>
2018-06-08 12:44:36 +1200

    Final version of ssketch and readme

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+/* Default versions of variables */
+let elementSpacing = 10;
+let circleSize = 0;
+let squareSize = 5;
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-
 function preload() {
-  sourceImg = loadImage("input_1.jpg");
-  maskImg = loadImage("mask_1.png");
+  sourceImg = loadImage("pupumba.jpg");
+  maskImg = loadImage("mask_pupumba.png");
 }
 
 function setup () {
@@ -20,23 +24,26 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<1080/pointSize;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  // for(let i=0;i<100;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let x = int(i * elementSpacing);
+    let y = int(renderCounter * elementSpacing);
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 50;
+    let pointSize = 22; //let element spacing = 40  
+    let halfSize = 11;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      ellipse(x, y, pointSize+circleSize, pointSize+circleSize);
     }
     else {
       rect(x-halfSize, y-halfSize, pointSize, pointSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 500) {
+  if(renderCounter > 1920/elementSpacing) {
     console.log("Done!")
     noLoop();
     // saveBlocksImages();
