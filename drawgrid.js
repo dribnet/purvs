12b19cdaeb02e4860778e5e98e1a6845576cd7e7
EJ Mallari <ejmal100@gmail.com>
2018-10-04 15:40:57 +1300

    Changes

--- drawgrid.js
+++ drawgrid.js
@@ -23,6 +23,18 @@ var tourPath = [
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+	p5.background(254, 234, 229);
+
+  p5.fill(240);
+  p5.quad(0, 0, 580, 100, 480, 200, 380, 100);
+  p5.stroke(5); 
+  p5.strokeWeight(7); 
+  p5.fill(240);
+  p5.quad(0, 0, 580, 150, 480, 250, 380, 150);
+  p5.fill(250);
+  p5.quad(0, 0, 510, 150, 480, 185, 450, 150);  
+
+
   // debug - show border
   // p5.noFill();
   // p5.stroke(255, 0, 0)
