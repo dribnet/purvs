5e7a79a6cae277c2ef978bf6578788ed0441a983
Wei Li <livia.l99999@gmail.com>
2018-10-04 15:41:51 +1300

    new sommit

--- drawgrid.js
+++ drawgrid.js
@@ -20,11 +20,34 @@ var tourPath = [
   [6, 512, 512]
 ]
 
+let ballx = 400
+let bally = 400
+let ballr = 100
+
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+  p5.background(255);
+
+  // Two ellipses with a radius of 50 units are then added.
+  let local_ballx = p5.map(ballx, x1, x2, 0, 256);
+  let local_bally = p5.map(bally, y1, y2, 0, 256);
+  let local_ballx_edge = p5.map((ballx + ballr), x1, x2, 0, 256);
+  let local_ball_r = local_ballx_edge - local_ballx;
+
+  p5.stroke(100);
+  for(let i=0; i<10; i++) {
+  	let shade = 128 + 128 / (i+1);
+  	let current_r = p5.map(i, 0, 9, local_ball_r, 0)
+	p5.fill(0, 0, shade);
+	p5.ellipse(local_ballx, local_bally, current_r);  	
+  }
+
   // debug - show border
-  // p5.noFill();
-  // p5.stroke(255, 0, 0)
-  // p5.rect(0, 0, 255, 255);
+  p5.noFill();
+  p5.stroke(0, 200, 200)
+  p5.rect(0, 0, 255, 255);
+  p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
+  let sizex = x2 - x1;
+  p5.text("width: " + sizex, 10, 40);
 }
