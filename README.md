0620b1de8c5d734944311720c925e4341ed07219
genovaeloi <ejgenova123@gmail.com>
2018-10-08 14:56:28 +1300

    flowers

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## PS4 MDDN 342 2018
-Im trying to update the code so that i can draw the flowers in a grid.
+I somewhat have draw the flowers into a grid
--- drawgrid.js
+++ drawgrid.js
@@ -1,5 +1,26 @@
+const max_thickness = 64;
+const  boxDimentions = 30;
+const grid_size = 64;
+
+/* the random number seed for the tour */
+var tourSeed = 301;
+/* triplets of locations: zoom, x, y */
+var tourPath = [
+  [1, 356.500000000000, 665.750000000000],
+  [3, 353.250000000000, 668.187500000000],
+  [4, 322.562500000000, 645.093750000000],
+  [5, 322.562500000000, 645.109375000000],
+  [7, 317.984375000000, 643.636718750000],
+  [3, 317.984375000000, 643.636718750000]
+]
+
+/* this function takes a coordinate and aligns to a grid of size gsize */
+function snap_to_grid(num, gsize) {
+  return (num - (num % gsize));
+}
+
 /*
- * This is the function to implement to make your own abstract design.
+ * This is the funciton to implement to make your own abstract design.
  *
  * arguments:
  * p5: the p5.js object - all draw commands should be prefixed with this object
@@ -9,54 +30,61 @@
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
-
-
-/* the random number seed for the tour */
-var tourSeed = 301;
-/* triplets of locations: zoom, x, y */
-var tourPath = [
-  [2, 512, 512],
-  [4, 512, 512],
-  [6, 512, 512]
-]
-
-// This version draws two rectangles and two ellipses.
-// The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-	  let cx=0, cy=0, cx2=0, cy2=0;
+  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
+  let max_shift = max_thickness;
 
-  p5.background('#8B632A');
-  p5.rectMode(p5.CORNERS);
+  /* this rectangle defines the region that will be drawn and includes a margin */
+  let min_x = snap_to_grid(x1 - max_shift, grid_size);
+  let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  let min_y = snap_to_grid(y1 - max_shift, grid_size);
+  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
 
 
- cx = p5.map(512-40/2, x1, x2, 0, 256);
-  cy = p5.map(512-70/2, y1, y2, 0, 256);
-  cx2 = p5.map(512+90/2, x1, x2, 0, 256);
-  cy2 = p5.map(512+70/2, y1, y2, 0, 256);
-  p5.fill(0, 0, 255);
-  p5.rect(cx, cy, cx2, cy2,20,0,20,0);
+  let c_p00 = p5.map(0, x1, x2, 0, 256);
+  let box = p5.map(boxDimentions, x1, x2, 0, 256);
+  let perimiter= box - c_p00;
 
+  p5.background(255);
+  p5.fill(0, 0, 128);
+  for(let x=min_x; x<max_x; x+=grid_size) {
+    for(let y=min_y; y<max_y; y+=grid_size) {
+      /* first compute the points to be drawn */
+      let x_pos = p5.map(x, x1, x2, 0, 256);
+      let y_pos = p5.map(y, y1, y2, 0, 256);
+
+      let x_pos_left = p5.map(x+grid_size, x1, x2, 0, 256);
+      let y_pos_down = p5.map(y+grid_size, y1, y2, 0, 256);
+
+      /* now draw all elements from back to front */
+
+     p5.translate(200,200);
+     p5.fill('#041370'); // blue
+     p5.rect(x_pos, y_pos,perimiter,perimiter,0, 20, 0, 20);
+     p5.rotate(p5.HALF_PI);
   
+  	 p5.fill('#422F14'); // brown
+     p5.rect(x_pos/4, y_pos/4,perimiter,perimiter,0, 20, 0, 20);
+     p5.rotate(p5.HALF_PI);
 
-	// translate(200,200);
-	// fill('#041370'); // blue
-	// rect(0, 0, 55, 55, 0, 20, 0, 20);
-	// rotate(HALF_PI);
-	
-	// fill('#041370'); // blue
-	// rect(0, 0, 55, 55, 0, 20, 0, 20);
-	// rotate(HALF_PI);
-	
-	// fill('#041370'); // blue
-	// rect(0, 0, 55, 55, 0, 20, 0, 20);
-	// rotate(HALF_PI);
-	
-	// fill('#041370'); // blue
-	// rect(0, 0, 55, 55, 0, 20, 0, 20);
-	// rotate(HALF_PI);
-	
-  // // debug - show border
-  // p5.noFill();
-  // p5.stroke(255, 0, 0)
-  // p5.rect(0, 0, 255, 255);
+     p5.fill('#FCDB85'); //yellow
+      p5.rect(x_pos/4, y_pos,perimiter,perimiter,0, 20, 0, 20);
+     p5.rotate(p5.HALF_PI);
+
+     p5.fill('#EE4502'); // red
+      p5.rect(x_pos/4, y_pos/4,perimiter,perimiter,0, 20, 0, 20);
+     p5.rotate(p5.HALF_PI);
+  
+    }
+  }
+
+  // debug - show border
+  p5.noFill();
+  p5.stroke(0, 200, 200)
+  p5.strokeWeight(1);
+  p5.rect(0, 0, 255, 255);
+  p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
+  let sizex = x2 - x1;
+  p5.text("width: " + sizex, 10, 40);
 }
+
