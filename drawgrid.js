737a2f409df61b347009781472d4e57799d88287
CJ Evans <cjev4ns@gmail.com>
2018-10-10 12:37:43 +1300

    coloured infinite grid

--- drawgrid.js
+++ drawgrid.js
@@ -22,42 +22,31 @@ function drawGrid (p5, x1, x2, y1, y2, z, zoom) {
 	p5.push ();
 	p5.scale (sc);
 	p5.translate (-x1, -y1);
-	
-//	x1 -= 10;
-//	y1 -= 10;
-//	x2 += 10;
-//	y2 += 10;
 
 	p5.background (255);
-	p5.noStroke ();
+//	p5.noStroke ();
 	
 	let grid = 100;
-	p5.rectMode (p5.CORNERS);
-	p5.fill (0, 100);
+//	p5.stroke (0, 100);
+	p5.strokeWeight (grid/3);
 	
-	for (let y = snap (y1, grid, -2); 
-			 y <= snap (y2, grid, +2);
-			 y += grid)
-		for (let x = snap (x1, grid, -2);
-				 x <= snap (x2, grid, +2);
-				 x += grid) {
-			/* cyan, top left */
-			p5.fill (55, 200, 200);
-			p5.arc (x, y, grid, grid, 0, p5.TAU/4);
+	let snap = point => p5.int (point/grid);
+	
+	let xStart = snap (x1)-2,
+			xEnd = snap (x2)+2,
+			yStart = snap (y1)-2,
+			yEnd = snap (y2)+2;
+	p5.print (xStart, xEnd, yStart, yEnd);
 
-			/* magenta, bottom left */
-			p5.fill (200, 55, 200);
-			p5.arc (x, y, grid, grid, p5.TAU * 3/4, 0);
-
-			/* green, top right */
-			p5.fill (55, 200, 55);
-			p5.arc (x, y, grid, grid, p5.TAU/4, p5.TAU/2);
-
-			/* red, bottom right */
-			p5.fill (200, 55, 55);
-			p5.arc (x, y, grid, grid, p5.TAU/2, p5.TAU * 3/4);
-		}
-//		p5.rect (x1, y, x2, y+grid/2);
+	for (let y = yStart; y <= yEnd; y++) {
+		p5.stroke (p5.noise (0, y, z)*255, 100, 150, 100);
+		p5.line (x1, y*grid, x2, y*grid);
+	}
+		
+	for (let x = xStart; x <= xEnd; x++) {
+		p5.stroke (p5.noise (x, 0, z)*255, 100, 150, 100);
+		p5.line (x*grid, y1, x*grid, y2);
+	}
 	
 	p5.pop ();
 	// debug - show border
@@ -66,10 +55,10 @@ function drawGrid (p5, x1, x2, y1, y2, z, zoom) {
 //	p5.rect(0, 0, 255, 255);
 }
 
-function snap (point, grid, offset) {
+/*function snap (point, grid, offset) {
 	
 	if (typeof offset == 'undefined')
 		return snap (point, grid, 0);
 	
 	return (Math.floor (point/grid) + offset) * grid;
-}
+}*/
