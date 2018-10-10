7584733731f1d4e505838e60c38e61873c6d057d
Emily Shaw <shawemil@wg401-d-03-06.mddn.vuw.ac.nz>
2018-10-11 12:45:14 +1300

    tree

--- drawgrid.js
+++ drawgrid.js
@@ -26,20 +26,24 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // debug - show border
    p5.noFill();
    p5.stroke(255, 0, 0)
-   p5.rect(0, 0, 255, 255);
-
+   //p5.rect(0, 0, 255, 255);
+p5.strokeWeight(0);
+p5.fill(99, 60, 34);
     p5.rect(100, 100, 25, 10);
+
+
+	p5.fill(34, 99, 36);
     p5.rect(90, 93, 45, 7);
     p5.rect(92.5, 86, 40, 7);
     p5.rect(95, 79, 35, 7);
     p5.rect(97.5, 72, 30, 7);
     p5.rect(100, 65, 25, 7);
-    p5.rect(100, 65, 20, 7);
+    p5.rect(102.5, 58, 20, 7);
 
 
 
 
-   p5.text("hello", 10,20);
+   //p5.text("hello", 10,20);
 
 
 }
