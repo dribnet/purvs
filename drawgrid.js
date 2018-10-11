0eda2f4952237f27327a2ff428a96102ffe951bf
Guest User <Guest@wg401-f-01-06.mddn.vuw.ac.nz>
2018-10-11 15:32:22 +1300

    zoomed out pattern done

--- drawgrid.js
+++ drawgrid.js
@@ -9,8 +9,10 @@
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
-const max_thickness = 10;
+const max_thickness = 200;
 const grid_size = 250;
+
+const line_width = 12;
 
 /* the random number seed for the tour */
 var tourSeed = 301;
@@ -40,57 +42,103 @@ function myPattern(p5, x, y, x1, x2, y1, y2)  {
   p5.arc(sm_arc_x, sm_arc_y, sm_arc_w, sm_arc_w, 100, 180); //small
 
   let zzz_x = p5.map(x-10, x1, x2, 0, 256);
+  let zzz_x1 = p5.map(x+10, x1, x2, 0, 256);
+  let zzz_x2 = p5.map(x+30, x1, x2, 0, 256);
+  let zzz_x3 = p5.map(x+50, x1, x2, 0, 256);
+  let zzz_x4 = p5.map(x+70, x1, x2, 0, 256);
+  let zzz_x5 = p5.map(x+90, x1, x2, 0, 256);
   let zzz_y = p5.map(y+80, y1, y2, 0, 256);
+  let zzz_y1 = p5.map(y+65, y1, y2, 0, 256);
   p5.beginShape(); //long zigzag
     p5.vertex(zzz_x, zzz_y);
-    p5.vertex(zzz_x+20, zzz_y-15);
-    p5.vertex(zzz_x+40, zzz_y);
-    p5.vertex(zzz_x+60, zzz_y-15);
-    p5.vertex(zzz_x+80, zzz_y);
-    p5.vertex(zzz_x+100, zzz_y-15);
+    p5.vertex(zzz_x1, zzz_y1);
+    p5.vertex(zzz_x2, zzz_y);
+    p5.vertex(zzz_x3, zzz_y1);
+    p5.vertex(zzz_x4, zzz_y);
+    p5.vertex(zzz_x5, zzz_y1);
   p5.endShape();
 
   let zz_x = p5.map(x-100, x1, x2, 0, 256);
+  let zz_x1 = p5.map(x-80, x1, x2, 0, 256);
+  let zz_x2 = p5.map(x-60, x1, x2, 0, 256);
+  let zz_x3 = p5.map(x-40, x1, x2, 0, 256);
   let zz_y = p5.map(y-80, y1, y2, 0, 256);
+  let zz_y1 = p5.map(y-60, y1, y2, 0, 256);
   p5.beginShape(); //zigzag
     p5.vertex(zz_x, zz_y);
-    p5.vertex(zz_x+20, zz_y+20);
-    p5.vertex(zz_x+40, zz_y);
-    p5.vertex(zz_x+60, zz_y+20);
+    p5.vertex(zz_x1, zz_y1);
+    p5.vertex(zz_x2, zz_y);
+    p5.vertex(zz_x3, zz_y1);
   p5.endShape();
 
-  let line_y = p5.map(x-30, y1, y2, 0, 256);
-  p5.line(zz_x, line_y, zz_x+20, line_y+15);
+  let line_x = p5.map(x-100, x1, x2, 0, 256);
+  let line_x1 = p5.map(x-80, x1, x2, 0, 256);
+  let line_y = p5.map(y-30, y1, y2, 0, 256);
+  let line_y1 = p5.map(y-15, y1, y2, 0, 256);
+  p5.line(line_x, line_y, line_x1, line_y1);
 
   let topV_x = p5.map(x+105, x1, x2, 0, 256);
+  let topV_x1 = p5.map(x+100, x1, x2, 0, 256);
+  let topV_x2 = p5.map(x+155, x1, x2, 0, 256);
   let topV_y = p5.map(y-80, y1, y2, 0, 256);
+  let topV_y1 = p5.map(y-120, y1, y2, 0, 256);
+  let topV_y2 = p5.map(y-110, y1, y2, 0, 256);
   p5.beginShape(); //top V
     p5.vertex(topV_x, topV_y);
-    p5.vertex(topV_x-5, topV_y-40);
-    p5.vertex(topV_x+50, topV_y-30);
+    p5.vertex(topV_x1, topV_y1);
+    p5.vertex(topV_x2, topV_y2);
   p5.endShape();
 
   let bottomV_x = p5.map(x-60, x1, x2, 0, 256);
+  let bottomV_x1 = p5.map(x-40, x1, x2, 0, 256);
+  let bottomV_x2 = p5.map(x-20, x1, x2, 0, 256);
   let bottomV_y = p5.map(y+100, y1, y2, 0, 256);
+  let bottomV_y1 = p5.map(y+120, y1, y2, 0, 256);
+  let bottomV_y2 = p5.map(y+110, y1, y2, 0, 256);
   p5.beginShape(); //bottom V
     p5.vertex(bottomV_x, bottomV_y);
-    p5.vertex(bottomV_x+20, bottomV_y+12);
-    p5.vertex(bottomV_x+40, bottomV_y+10);
+    p5.vertex(bottomV_x1, bottomV_y1);
+    p5.vertex(bottomV_x2, bottomV_y2);
   p5.endShape();
 
-  let t_x = p5.map(x+60, x1, x2, 0, 256);
-  let t_y = p5.map(y+30,  y1, y2, 0, 256);
-  p5.line(t_x, t_y, t_x+40, t_y-38); //T
-  p5.line(t_x+20, t_y-60, t_x+60, t_y-30);
+  let Tline_x = p5.map(x+60, x1, x2, 0, 256);
+  let Tline_y = p5.map(y+30,  y1, y2, 0, 256);
+  let Tline_x2 = p5.map(x+100, x1, x2, 0, 256);
+  let Tline_y2 = p5.map(y-8,  y1, y2, 0, 256);
+  p5.line(Tline_x, Tline_y, Tline_x2, Tline_y2);
+
+  let Tline2_x = p5.map(x+80, x1, x2, 0, 256);
+  let Tline2_y = p5.map(y-30,  y1, y2, 0, 256);
+  let Tline2_x2 = p5.map(x+120, x1, x2, 0, 256);
+  let Tline2_y2 = p5.map(y,  y1, y2, 0, 256);
+  p5.line(Tline2_x, Tline2_y, Tline2_x2, Tline2_y2);
+
+  let circle_x = p5.map(x, x1, x2, 0, 256);
+  let circle_y = p5.map(y, y1, y2, 0, 256);
+  let circle_origin_x = p5.map(0, x1, x2, 0, 256);
+  let circle_offset = p5.map(75, x1, x2, 0, 256);
+  let circle_radius = circle_offset - circle_origin_x;
+  p5.ellipse(circle_x, circle_y, circle_radius, circle_radius);
+
+  let sm_circle_x = p5.map(x+130, x1, x2, 0, 256);
+  let sm_circle_y = p5.map(y+40, y1, y2, 0, 256);
+  let sm_circle_origin_x = p5.map(0, x1, x2, 0, 256);
+  let sm_circle_offset = p5.map(12, x1, x2, 0, 256);
+  let sm_circle_radius = sm_circle_offset - sm_circle_origin_x;
+  p5.ellipse(sm_circle_x, sm_circle_y, sm_circle_radius, sm_circle_radius);
 }
 
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+
+  let c_p00 = p5.map(0, x1, x2, 0, 256);
+  let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+  let cur_line_width = c_plwidth - c_p00;
+
   p5.angleMode(p5.DEGREES);
   p5.background(255);
   p5.noFill();
-  p5.stroke("#f90250");
-  p5.strokeWeight(10);
+  p5.strokeWeight(cur_line_width);
 
   let max_shift = max_thickness;
   /* this rectangle defines the region that will be drawn and includes a margin */
@@ -101,83 +149,20 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 
   for(let x=min_x; x<max_x; x+=grid_size) {
     for(let y=min_y; y<max_y; y+=grid_size) {
-      let x_pos = p5.map(x, x1, x2, 0, 256);
-      let y_pos = p5.map(y, y1, y2, 0, 256);
 
-      let circle_x = p5.map(512, x1, x2, 0, 256);
-      radius = p5.map(580, x1, x2, 0, 256);
-      p5.ellipse(x_pos, y_pos, (radius-circle_x));
-
-      let arc_x = p5.map(x+20, x1, x2, 0, 256);
-      let arc_y = p5.map(y-60, y1, y2, 0, 256);
-      let arc_w_offset = p5.map(x+100, x1, x2, 0, 256);
-      let arc_w = arc_w_offset - arc_x;
-      p5.arc(arc_x, arc_y, arc_w, arc_w, 220, 10); //big
-
-      let sm_arc_x = p5.map(x-40, x1, x2, 0, 256);
-      let sm_arc_y = p5.map(y+40, y1, y2, 0, 256);
-      let sm_arc_w_offset = p5.map(x+20, x1, x2, 0, 256);
-      let sm_arc_w = sm_arc_w_offset - sm_arc_x;
-      p5.arc(sm_arc_x, sm_arc_y, sm_arc_w, sm_arc_w, 100, 180); //small
-
-      let zzz_x = p5.map(x-10, x1, x2, 0, 256);
-      let zzz_y = p5.map(y+80, y1, y2, 0, 256);
-      p5.beginShape(); //long zigzag
-        p5.vertex(zzz_x, zzz_y);
-        p5.vertex(zzz_x+20, zzz_y-15);
-        p5.vertex(zzz_x+40, zzz_y);
-        p5.vertex(zzz_x+60, zzz_y-15);
-        p5.vertex(zzz_x+80, zzz_y);
-        p5.vertex(zzz_x+100, zzz_y-15);
-      p5.endShape();
-
-      let zz_x = p5.map(x-100, x1, x2, 0, 256);
-      let zz_y = p5.map(y-80, y1, y2, 0, 256);
-      p5.beginShape(); //zigzag
-        p5.vertex(zz_x, zz_y);
-        p5.vertex(zz_x+20, zz_y+20);
-        p5.vertex(zz_x+40, zz_y);
-        p5.vertex(zz_x+60, zz_y+20);
-      p5.endShape();
-
-      let line_y = p5.map(x-30, y1, y2, 0, 256);
-      p5.line(zz_x, line_y, zz_x+20, line_y+15);
-
-      let topV_x = p5.map(x+105, x1, x2, 0, 256);
-      let topV_y = p5.map(y-80, y1, y2, 0, 256);
-      p5.beginShape(); //top V
-        p5.vertex(topV_x, topV_y);
-        p5.vertex(topV_x-5, topV_y-40);
-        p5.vertex(topV_x+50, topV_y-30);
-      p5.endShape();
-
-      let bottomV_x = p5.map(x-60, x1, x2, 0, 256);
-      let bottomV_y = p5.map(y+100, y1, y2, 0, 256);
-      p5.beginShape(); //bottom V
-        p5.vertex(bottomV_x, bottomV_y);
-        p5.vertex(bottomV_x+20, bottomV_y+12);
-        p5.vertex(bottomV_x+40, bottomV_y+10);
-      p5.endShape();
-
-      let t_x = p5.map(x+60, x1, x2, 0, 256);
-      let t_y = p5.map(y+30,  y1, y2, 0, 256);
-      p5.line(t_x, t_y, t_x+40, t_y-38); //T
-      p5.line(t_x+20, t_y-60, t_x+60, t_y-30);
-
-      p5.push();
-      let dot_x = p5.map(x+130, x1, x2, 0, 256);
-      let dot_y = p5.map(y+40, y1, y2, 0, 256);
-      p5.noStroke();
-      p5.fill("#f90250");
-      p5.ellipse(dot_x, dot_y, 14);
-      p5.pop();
+      p5.stroke(249, 2, 80, 120);
+      myPattern(p5, x, y, x1, x2, y1, y2);
+      p5.stroke(35, 170, 255, 120);
+      myPattern(p5, x+10, y+10, x1, x2, y1, y2);
+      p5.stroke(0);
+      myPattern(p5, x+5, y+5, x1, x2, y1, y2);
 
     }
   }
 
   //debug - show border
-  p5.noFill();
-  p5.stroke(255, 0, 0);
-  p5.strokeWeight(1);
-  p5.rect(0, 0, 255, 255);
+  // p5.noFill();
+  // p5.stroke(255, 0, 0);
+  // p5.strokeWeight(1);
+  // p5.rect(0, 0, 255, 255);
 }
