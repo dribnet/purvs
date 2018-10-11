e74613ac6623b4c2811dfc9681c1ef9d83e24c3b
Emily Shaw <shawemil@wg401-d-03-06.mddn.vuw.ac.nz>
2018-10-11 14:51:29 +1300

    responsive rectangle

--- drawgrid.js
+++ drawgrid.js
@@ -10,7 +10,6 @@
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
 
-
 /* the random number seed for the tour */
 var tourSeed = 301;
 /* triplets of locations: zoom, x, y */
@@ -26,5 +25,45 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // debug - show border
   // p5.noFill();
   // p5.stroke(255, 0, 0)
-  // p5.rect(0, 0, 255, 255);
+   //p5.rect(0, 0, 255, 255);
+    
+
+
+	let cx = p5.map(512, x1, x2, 0, 256);
+	let cy = p5.map(512, y1, y2, 0, 256);
+	let cx2 = p5.map(512+10, x1, x2, 0, 256);
+	tree(p5,cx,cy);
+
+	p5.strokeWeight(0);
+	//p5.ellipse(cx, cy, (cx2-cx));
+
+p5.rect(cx, cy, (cx2-cx),(cx2-cx)/4);
+p5.rect(cx, cy, (cx2-cx)/1.6,(cx2-cx)/4);
+
+
+
+}
+
+function tree (p5,x,y){
+
+	this.x=x;
+    this.y=y;
+	p5.strokeWeight(0);
+	p5.fill(99, 60, 34);
+    p5.rect(102.5, 100, 20, 10);
+
+
+	p5.fill(34, 99, 36);
+    p5.rect(90, 93, 45, 7);
+    p5.fill(38, 109, 40);
+    p5.rect(92.5, 86, 40, 7);
+    p5.fill(49, 137, 52);
+    p5.rect(95, 79, 35, 7);
+    p5.fill(58, 153, 61);
+    p5.rect(97.5, 72, 30, 7);
+    p5.fill(63, 175, 66);
+    p5.rect(100, 65, 25, 7);
+    p5.fill(74, 196, 77);
+    p5.rect(102.5, 58, 20, 7);
+
 }
