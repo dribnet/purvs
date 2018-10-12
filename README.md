7cadf056893bb74f1063a7d2abbd7164f948c297
Amy Wilson <amyy.ribbit@gmail.com>
2018-10-12 19:58:25 +1300

    first design

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## PS4 MDDN 342 2018
 
-My initial idea is based off my ps3 design. I will be converting it to 2D. When you zoom in, the swirls will be made up of little asteroids or planets and the stars will have more introcate and geometric detailing to them.
-
-To further this idea, I may reinclude the planets as 2D shapes that also gain more detail when zoomed in. I'd also like to play around with the colour scheme a bit more.
+I have created an initial version of my design that shows the starting point of my design. I will update it so that the spiral changes into the planets and the stars get more detail.
--- drawgrid.js
+++ drawgrid.js
@@ -9,7 +9,10 @@
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
-
+const max_thickness = 256;
+const grid_size = 256;
+const max_movement = 16;
+let starList;
 
 /* the random number seed for the tour */
 var tourSeed = 301;
@@ -20,11 +23,89 @@ var tourPath = [
   [6, 512, 512]
 ]
 
+function solarSystem(p5, x, y, x1, x2, y1, y2){
+	let centre_x = p5.map(x, x1, x2, 0, 256);
+	let centre_y = p5.map(y, y1, y2, 0, 256);
+
+	let radius = p5.map(20, x1, x2, 0, 256) - p5.map(0, x1, x2, 0, 256);
+	//stars
+	starList = [];
+	//draw stars random distance from sun
+	for(i = 0; i <100; i++){
+		newStar = new Star(p5.random(centre_x-x1,centre_x+x2), 
+		p5.random(centre_y-y1, centre_y+y2), 
+		p5.random(0, 255));
+		starList[i] = newStar;
+	}
+	var starSize = radius/5;
+	var boundary = 40;
+	for(star = 0; star < starList.length; star++){
+		let s = starList[star];
+		p5.noStroke();
+		s.starThickness = starSize/8;
+		p5.fill(255, s.colour);
+		p5.beginShape();
+		p5.vertex(s.xPos-starSize, s.yPos);
+		p5.bezierVertex(s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos, s.yPos-starSize);
+		p5.bezierVertex(s.xPos+s.starThickness, s.yPos-s.starThickness, s.xPos+s.starThickness, s.yPos-s.starThickness, s.xPos+starSize, s.yPos);
+		p5.bezierVertex(s.xPos+s.starThickness, s.yPos+s.starThickness, s.xPos+s.starThickness, s.yPos+s.starThickness, s.xPos, s.yPos+starSize);
+		p5.bezierVertex(s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos-s.starThickness, s.yPos+s.starThickness, s.xPos-starSize, s.yPos);
+		p5.endShape();   
+	}
+
+	let angle = 0;
+	let r = radius/2;
+	let spin = 0.1;
+	let grow = spin * radius/2;
+	for(count = 0; count <300; count+=3){
+		angle += spin;
+		r += grow;
+		p5.noStroke();
+		p5.fill(15+p5.map(count, 0, 300, 200, 0), 0, 5+p5.map(count, 0, 300, 250, 0));
+		let new_x = centre_x + p5.cos(angle)*r;
+		let new_y = centre_y + p5.sin(angle)*r;	
+		p5.ellipse(new_x, new_y, radius, radius);
+	}
+	
+
+
+}
+
+/* this function takes a coordinate and aligns to a grid of size gsize */
+function snap_to_grid(num, gsize) {
+  return (num - (num % gsize));
+}
+
+function Star(x, y, c) {
+	this.xPos = x;
+	this.yPos = y;
+	this.colour = c;
+	this.starThickness = 2;
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
+
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  // debug - show border
-  // p5.noFill();
-  // p5.stroke(255, 0, 0)
-  // p5.rect(0, 0, 255, 255);
+	p5.background(15, 0, 5);
+
+	let max_shift = max_thickness;
+
+	let min_x = snap_to_grid(x1 - max_shift, grid_size);
+  	let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  	let min_y = snap_to_grid(y1 - max_shift, grid_size);
+  	let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
+
+
+	for(let x=min_x; x<max_x; x+=grid_size) {
+    	for(let y=min_y; y<max_y; y+=grid_size) {
+			solarSystem(p5, x,y, x1, x2, y1, y2);
+		}		
+	}
 }
+
