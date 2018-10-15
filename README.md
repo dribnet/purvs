c021e8a77c9680dc374f14e827cb7f4ab25e05d9
Sandra Son <sandra.thi.son@gmail.com>
2018-10-15 15:18:43 +1300

    first cube

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## PS4 MDDN 342 2018
 
-For this intial idea I'm carrying on from my previous problem sketch. It starts off from a grid of squares and when you zoom into different squares it will have different cityscapes still with the blocky style. I think each city will have slightly different animations.
+I've finally added my cube into the new code, my next step is to put it in a grid.
--- drawgrid.js
+++ drawgrid.js
@@ -15,42 +15,179 @@
 var tourSeed = 301;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-  [2, 512, 512],
-  [4, 512, 512],
-  [6, 512, 512]
+  [1, 460, 500],
+  [3, 450, 410],
+  [5, 447, 426],
+  [3, 450, 410],
+  [3, 450, 620]
 ]
 
+// function owl(p5, x, y, x1, x2, y1, y2) {
+//   p5.stroke(0);
 
-function owl(x,y){
-	push();
-	translate(x,y);
-	stroke(0);
+//   // first draw the body
+//   let line_x0 = p5.map(x + 0, x1, x2, 0, 256);
+//   let line_x_offset = p5.map(x + 70, x1, x2, 0, 256);
+//   let x_strokewidth = line_x_offset - line_x0;
+//   let line_y1 = p5.map(y + -35, y1, y2, 0, 256);
+//   let line_y2 = p5.map(y + -65, y1, y2, 0, 256);
+
+//   p5.strokeWeight(x_strokewidth);
+//   p5.line(line_x0, line_y1, line_x0, line_y2);
+//   p5.noStroke();
+
+//   // then draw the eyes
+//   let eye_pos1_x = p5.map(x + -17.5, x1, x2, 0, 256);
+//   let eye_pos2_x = p5.map(x + 17.5, x1, x2, 0, 256);
+//   let eye_pos1_y = p5.map(y + -65, y1, y2, 0, 256);
+
+//   let eye_pos1_origin_x = p5.map(0, x1, x2, 0, 256);
+//   let eye_pos1_offset1 = p5.map(35, x1, x2, 0, 256);
+//   let eye_pos_radius1 = eye_pos1_offset1 - eye_pos1_origin_x;
+
+//   p5.fill(255);
+//   p5.ellipse(eye_pos1_x, eye_pos1_y, eye_pos_radius1, eye_pos_radius1); // Left eye dome
+//   p5.ellipse(eye_pos2_x, eye_pos1_y, eye_pos_radius1, eye_pos_radius1); // Left eye dome
+
+//   // then the chin
+//   let arc_x = p5.map(x + 0, x1, x2, 0, 256);
+//   let arc_y = p5.map(y + -65, y1, y2, 0, 256);
+//   let arc_w_offset = p5.map(x+70, x1, x2, 0, 256);
+//   let arc_w = arc_w_offset - arc_x;
+//   p5.arc(arc_x, arc_y, arc_w, arc_w, 0, p5.PI);  // Chin
+
+//   // the eyeballs
+//   let eyeball_x1 = p5.map(x + -14, x1, x2, 0, 256);
+//   let eyeball_x2 = p5.map(x + 14,  x1, x2, 0, 256);
+//   let eyeball_r = eye_pos_radius1 / 3;
+//   p5.fill(0);
+//   p5.ellipse(eyeball_x1, eye_pos1_y, eyeball_r, eyeball_r); // Left eye
+//   p5.ellipse(eyeball_x2, eye_pos1_y, eyeball_r, eyeball_r);  // Right eye
+
+//   // the beak
+//   let quad_x1 = p5.map(x + 0, x1, x2, 0, 256);
+//   let quad_y1 = p5.map(y + -65, y1, y2, 0, 256);
+//   let quad_x2 = p5.map(x + 4, x1, x2, 0, 256);
+//   let quad_y2 = p5.map(y + -51, y1, y2, 0, 256);
+//   let quad_x3 = p5.map(x + 0, x1, x2, 0, 256);
+//   let quad_y3 = p5.map(y + -44, y1, y2, 0, 256);
+//   let quad_x4 = p5.map(x + -4, x1, x2, 0, 256);
+//   let quad_y4 = p5.map(y + -51, y1, y2, 0, 256);
+//   p5.quad(quad_x1, quad_y1, quad_x2, quad_y2, quad_x3, quad_y3, quad_x4, quad_y4);
+// }
+
+
+
+function cube(p5, x, y, x1, x2, y1, y2) {
+
+	let quad_x1 = p5.map(x-75, x1, x2, 0, 256);
+	let quad_y1 = p5.map(y+0, y1, y2, 0, 256);
+	let quad_x2 = p5.map(x+0, x1, x2, 0, 256);
+	let quad_y2 = p5.map(y-60, y1, y2, 0, 256);
+	let quad_x3 = p5.map(x+75, x1, x2, 0, 256);
+	let quad_y3 = p5.map(y+0, y1, y2, 0, 256);
+	let quad_x4 = p5.map(x+0, x1, x2, 0, 256);
+	let quad_y4 = p5.map(y+60, y1, y2, 0, 256);
+
+	p5.fill('pink');
+	p5.quad(quad_x1, quad_y1, quad_x2, quad_y2, quad_x3, quad_y3, quad_x4, quad_y4);
+}
+
+function leftSide(p5, x, y, x1, x2, y1, y2) {
+
+	let left_x1 = p5.map(x-75, x1, x2, 0, 256);
+	let left_y1 = p5.map(y+0, y1, y2, 0, 256);
+	let left_x2 = p5.map(x+0, x1, x2, 0, 256);
+	let left_y2 = p5.map(y+60, y1, y2, 0, 256);
+	let left_x3 = p5.map(x+0, x1, x2, 0, 256);
+	let left_y3 = p5.map(y+100, y1, y2, 0, 256);
+	let left_x4 = p5.map(x-75, x1, x2, 0, 256);
+	let left_y4 = p5.map(y+40, y1, y2, 0, 256);
+
+	p5.fill('green');
+	p5.quad(left_x1, left_y1, left_x2, left_y2, left_x3, left_y3, left_x4, left_y4);
 
 }
+
+function rightSide(p5, x, y, x1, x2, y1, y2) {
+
+	let right_x1 = p5.map(x+75, x1, x2, 0, 256);
+	let right_y1 = p5.map(y+0, y1, y2, 0, 256);
+	let right_x2 = p5.map(x+0, x1, x2, 0, 256);
+	let right_y2 = p5.map(y+60, y1, y2, 0, 256);
+	let right_x3 = p5.map(x+0, x1, x2, 0, 256);
+	let right_y3 = p5.map(y+100, y1, y2, 0, 256);
+	let right_x4 = p5.map(x+75, x1, x2, 0, 256);
+	let right_y4 = p5.map(y+40, y1, y2, 0, 256);
+
+	p5.fill('yellow');
+	p5.quad(right_x1, right_y1, right_x2, right_y2, right_x3, right_y3, right_x4, right_y4);
+
+}
+
+function leftTop(p5, x, y, x1, x2, y1, y2) {
+
+	let left_tri_x1 = p5.map(x+0, x1, x2, 0, 256);
+	let left_tri_y1 = p5.map(y-30, y1, y2, 0, 256);
+	let left_tri_x2 = p5.map(x+38, x1, x2, 0, 256);
+	let left_tri_y2 = p5.map(y+0, y1, y2, 0, 256);
+	let left_tri_x3 = p5.map(x+0, x1, x2, 0, 256);
+	let left_tri_y3 = p5.map(y+30, y1, y2, 0, 256);
+
+
+	p5.fill('green');
+	p5.triangle(left_tri_x1, left_tri_y1, left_tri_x2, left_tri_y2, left_tri_x3, left_tri_y3);
+
+}
+
+function rightTop(p5, x, y, x1, x2, y1, y2) {
+
+	let right_tri_x1 = p5.map(x+0, x1, x2, 0, 256);
+	let right_tri_y1 = p5.map(y-30, y1, y2, 0, 256);
+	let right_tri_x2 = p5.map(x-38, x1, x2, 0, 256);
+	let right_tri_y2 = p5.map(y+0, y1, y2, 0, 256);
+	let right_tri_x3 = p5.map(x+0, x1, x2, 0, 256);
+	let right_tri_y3 = p5.map(y+30, y1, y2, 0, 256);
+
+
+	p5.fill('yellow');
+	p5.triangle(right_tri_x1, right_tri_y1, right_tri_x2, right_tri_y2, right_tri_x3, right_tri_y3);
+
+}
+
+
+
+
 
 
 
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  p5.background(200, 200, 255);
+  p5.background(255);
+
+	cube(p5, 412, 512, x1, x2, y1, y2 );
+	leftSide(p5, 412, 512, x1, x2, y1, y2 );
+	rightSide(p5, 412, 512, x1, x2, y1, y2 );
+	leftTop(p5, 412, 512, x1, x2, y1, y2 );
+	rightTop(p5, 412, 512, x1, x2, y1, y2 );
+  
 
 
 
+
+
+
+
+
+
+
+  // owl(p5, 412, 512, x1, x2, y1, y2);
+  // owl(p5, 612, 512, x1, x2, y1, y2);
+
+  // debug - show border
+ // p5.noFill();
+ //  p5.stroke(0, 200, 200)
+ //  p5.rect(0, 0, 255, 255);
 }
-
-
-
-
-
-
-
-//   // debug - show border
-//   p5.noFill();
-//   p5.stroke(0, 200, 200)
-//   p5.rect(0, 0, 255, 255);
-//   p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
-//   let sizex = x2 - x1;
-//   p5.text("width: " + sizex, 10, 40);
-// }
 
