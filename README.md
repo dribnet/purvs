d59acb86c718e2ed74d950bc5c8488209116250e
Finn Mitchell-Anyon <finn.mitchellanyon@gmail.com>
2018-10-15 16:14:57 +1300

    Improved cell generation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## Part 4 - Optimisation
+## Part 5
 
-I figured out that the majority of the reason that the higher zoom levels take so long to render is that each canvas is trying to draw every one of the thousands of circles, even if they are not visible within the frame. I put in an if statement which checks whether the circle will be visible before trying to draw it and the program runs a lot better.
+I've altered the circle generation code so that cells will not generate so big that their edge crosses outside the boundaries of their containing cell. This means that there is more of a defined edge to edge cluster as you zoom in.
--- drawgrid.js
+++ drawgrid.js
@@ -45,23 +45,17 @@ class cell{
 		var localEdge = p5.map((this.x + this.r), x1, x2, 0, 256);
 		var localR = localEdge - localX;
 
-
 			p5.fill(20,20,25);
 			p5.stroke(255);
 			if(localX >= 0-localR && localX <= 256+localR && localY >= 0-localR && localY <= 256+localR){
-			if(zoom < this.zoomThresh) {
-				
-					p5.ellipse(localX,localY,localR,localR);
-					// p5.noStroke();
-					// p5.fill(255);
-					// p5.text(localR,localX,localY);
-				
-			}
-			else {
-				for(let c of this.subCells){
-		   			c.draw(p5,x1,x2,y1,y2,zoom)
-		   		}
-		   	p5.colorMode(p5.RGB,255);
+				if(zoom < this.zoomThresh) {
+						p5.ellipse(localX,localY,localR,localR);
+				}
+				else {
+					for(let c of this.subCells){
+			   			c.draw(p5,x1,x2,y1,y2,zoom)
+			   		}
+			   	p5.colorMode(p5.RGB,255);
 			}
 		}
 	}
@@ -76,13 +70,11 @@ function generateCells(bigCell,cellCount,zoomThresh){
 			var point = randomInsideCircle(bigCell.x,bigCell.y,bigCell.r/2);
 			for (let c of bigCell.subCells){
 				if (isPointInsideCircle(point.x,point.y,c.x,c.y,c.r/2)) {
-					console.log("Inside circle");
 					continue restartLoop;
 				}
-				//console.log(isPointInsideCircle(point.x,point.y,c.x,c.y,c.r));
 			}
 
-			var size = findSmallestDistance(bigCell.subCells,point,bigCell.r/10)*2;
+			var size = findSmallestDistance(bigCell,point,bigCell.r/10)*2;
 			bigCell.subCells.push(new cell(point.x,point.y,size,zoomThresh));
 			break;
 		}
@@ -90,9 +82,9 @@ function generateCells(bigCell,cellCount,zoomThresh){
 }
 
 mainCell = new cell(0,0,500,1);
-generateCells(mainCell,300,4);
+generateCells(mainCell,1000,4);
 for(let c of mainCell.subCells){
-	generateCells(c,300,7);
+	generateCells(c,300,10);
 }
 
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
@@ -105,18 +97,13 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   let local_ballr = local_ballx_edge - local_ballx;
   let balla = p5.map(zoom,0,2,255,0);
 
-  p5.fill(255,balla);
-  p5.stroke(255);
+	p5.fill(255,balla);
+	p5.stroke(255);
 
    p5.noFill();
    p5.stroke(255)
-   //p5.rect(0, 0, 255, 255);
-   //p5.ellipse(127,127,127,127);
-   //p5.text("" + x1 + "," + y1, 50,50);
    mainCell.draw(p5,x1,x2,y1,y2,zoom);
-   //for(let c of cells){
-   //		c.draw(p5,x1,x2,y1,y2)
-   //}
+
 }
 
 function randomInsideCircle(x,y,radius){
@@ -135,13 +122,19 @@ function isPointInsideCircle(pointX,pointY,circleX,circleY,circleR){
 	else return false;
 }
 
-function findSmallestDistance(cellArray,point,maxCellSize){
-	var smallestDist = Math.random()*maxCellSize;
-	for(let c of cellArray){
+function findSmallestDistance(bigCell,point,maxCellSize){
+	var smallestDist = (Math.random()*maxCellSize)+maxCellSize/10;
+	for(let c of bigCell.subCells){
 		var a = point.x - c.x;
 		var b = point.y - c.y;
 		var dist = Math.sqrt(a*a + b*b)-(c.r/2);
 		if(dist < smallestDist) smallestDist = dist;
 	}
+
+	var a = point.x - bigCell.x;
+	var b = point.y - bigCell.y;
+	var dist = (Math.sqrt(a*a + b*b)-(bigCell.r/2))*-1;
+	if(dist < smallestDist) smallestDist = dist;
+
 	return smallestDist;
 }
