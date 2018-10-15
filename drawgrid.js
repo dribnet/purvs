5d14d21dfc92d227c0e102f769cb16822ed2ed03
Zoe Richards <richarzoe@myvuw.ac.nz>
2018-10-15 15:16:06 +1300

    trailling new code

--- drawgrid.js
+++ drawgrid.js
@@ -20,31 +20,28 @@ var tourPath = [
   [6, 512, 512]
 ]
 
-let ballx = 400
-let bally = 400
-let ballr = 100
+function circles(p5, x, y, x1, x2, y1, y2){
+  let circleX = p5.map(512, x1, x2, 0, 256);
+  let circleY = p5.map(512, y1, y2, 0, 256);
+  let circleR = p5.map(512-10, x1, x2, 0, 256);
+
+  p5.stroke(255);
+  p5.fill(0, 200, 200);
+  p5.ellipse(circleX, circleY, circleR);    
+  
+}
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   p5.background(255);
 
-  // Two ellipses with a radius of 50 units are then added.
-  let local_ballx = p5.map(ballx, x1, x2, 0, 256);
-  let local_bally = p5.map(bally, y1, y2, 0, 256);
-  let local_ballx_edge = p5.map((ballx + ballr), x1, x2, 0, 256);
-  let local_ball_r = local_ballx_edge - local_ballx;
-
-  p5.stroke(255);
-  for(let i=0; i<10; i++) {
-  	let shade = 128 + 128 / (i+1);
-  	let current_r = p5.map(i, 0, 9, local_ball_r, 0)
-	p5.fill(0, shade, shade);
-	p5.ellipse(local_ballx, local_bally, current_r);  	
-  }
-
+   circles(p5, 412, 512, x1, x2, y1, y2);
+    circles(p5, 612, 512, x1, x2, y1, y2);
   // debug - show border
-  p5.noFill();
-  p5.stroke(0, 200, 200)
-  p5.rect(0, 0, 255, 255);
-  p5.ellipse(25, 25, 50, 50);
+  // p5.noFill();
+  // p5.stroke(0, 200, 200)
+  // p5.rect(0, 0, 255, 255);
+  // p5.ellipse(25, 25, 50, 50);
 }
+
+
