69c0cfe360b493d582ef15d9a305624639f64283
Philip Matich <philipmatich@gmail.com>
2018-10-18 10:52:41 +1300

    Update
    Updated images, readme and drawgrid

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## PS4 MDDN 342 2018
 
-## Sketch
-
-For my initial idea I want to make a weird alien planet kinda thing. As you zoom in on each island of vines more detail comes up, such as the growing arcs from ps3. I want to try and achieve the same painted look as I did in the last project, as I think it'd look pretty cool here. I plan to achieve this with the same growing and fading shape effect from before
+I've been slacking quite a bit so am now trying to catch up. Just familiarising myself with the code at the moment.
--- drawgrid.js
+++ drawgrid.js
@@ -20,13 +20,17 @@ var tourPath = [
   [6, 512, 512]
 ]
 
+let ballx = 400
+let bally = 400
+let ballr = 100
+
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // temporary variables used for object placement
   let cx=0, cy=0, cx2=0, cy2=0;
 
-  p5.background(255);
+  p5.background(0);
   p5.rectMode(p5.CORNERS);
 
   // The first red rectangle fills the entire space
@@ -37,30 +41,28 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   p5.fill(255, 0, 0);
   p5.rect(cx, cy, cx2, cy2);
 
-  // The second black rectangle is inset to form a frame inset by 20 units
-  cx = p5.map(512-940/2, x1, x2, 0, 256);
-  cy = p5.map(512-700/2, y1, y2, 0, 256);
-  cx2 = p5.map(512+940/2, x1, x2, 0, 256);
-  cy2 = p5.map(512+700/2, y1, y2, 0, 256);
-  p5.fill(0);
-  p5.rect(cx, cy, cx2, cy2);
+  let local_ballx = p5.map(ballx, x1, x2, 0, 256);
+  let local_bally = p5.map(bally, y1, y2, 0, 256);
+  let local_ballx_edge = p5.map((ballx + ballr), x1, x2, 0, 256); // Work out where edge is by adding radius and center together
+  let local_ball_r = local_ballx_edge - local_ballx;
+  
+  for(i = 0; i < 10; i++){
+    let shade = 128 + 128 / (i+1);
+    let current_r = p5.map(i, 0, 9, local_ball_r, 0);
+    p5.fill(0, 0, shade);
+    p5.ellipse(local_ballx, local_bally, current_r);
+  }
 
   // Two ellipses with a radius of 50 units are then added.
   cx = p5.map(512, x1, x2, 0, 256);
   cy = p5.map(512, y1, y2, 0, 256);
-  cx2 = p5.map(512+50, x1, x2, 0, 256);
-  p5.fill(0, 0, 255);
-  p5.ellipse(cx, cy, (cx2-cx));
-
-  // The second green ellipse is above and to the left of the first one.
-  cx = p5.map(412, x1, x2, 0, 256);
-  cy = p5.map(412, y1, y2, 0, 256);
-  cx2 = p5.map(412+50, x1, x2, 0, 256);
-  p5.fill(0, 255, 0);
+  cx2 = p5.map(512+100, x1, x2, 0, 256);
+  cy2 = p5.map(512+50, y1, y2, 0, 256);
+  p5.fill(255);
   p5.ellipse(cx, cy, (cx2-cx));
 
   //debug - show border
-  p5.noFill();
+  /*p5.noFill();
   p5.stroke(255, 0, 0)
-  p5.rect(0, 0, 255, 255);
+  p5.rect(0, 0, 255, 255);*/
 }
