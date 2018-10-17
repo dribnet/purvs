b1bff009d2c7075097dd33ca2743ca7101ea054e
Erin O'Brien <erinobrien1997@hotmail.com>
2018-10-17 13:55:47 +1300

    more iterations

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## PS4 MDDN 342 2018
 
-I experimented a bit more with the zooming and also changed the background colour, which gives the image a different feel. I want to next play around with creating new elements with different shapes other than ellipses. For example when you zoom in, the very middle of the flower shape is a cross style shape so I think it would be cool to move onto a retangular/cross-style element so it looks like you are moving deeper and deeper into the flower. I think the overlapping is more effective on a black background, but I may look at changing the background colour only on specific zoom levels. 
-
-I also need to add some animation, and I want to play around with randomising colour to add more complexity and detail to the overall grid. 
-
+Iterated a bit further, tried to get animation to work but for some reason I can't figure it out. So have left that for now and just worked on the zooming.  
--- drawgrid.js
+++ drawgrid.js
@@ -1,8 +1,9 @@
 const max_thickness = 64;
 const ball_radius = 32;
 const ball_radius2 = 10;
+const ball_radius3 = 45;
 const line_width = 8;
-const grid_size = 64;
+const grid_size = 56;
 
 /* the random number seed for the tour */
 var tourSeed = 301;
@@ -21,7 +22,7 @@ function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
 }
 
-
+//1ST ZOOM FLOWERS (PINK)
 function drawEllipses(p5,x,y,x1,x2,y1,y2) {
  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
   let max_shift = max_thickness;
@@ -84,8 +85,6 @@ function drawEllipses(p5,x,y,x1,x2,y1,y2) {
       p5.ellipse(x_pos, y_pos, cur_ball_radius);
 
 
-      
-
       p5.fill(250,13,73,20); //pink
       p5.ellipse(right_circlex, y_pos, cur_ball_radius);
       p5.ellipse(x_pos, right_circley, cur_ball_radius);
@@ -106,7 +105,7 @@ function drawEllipses(p5,x,y,x1,x2,y1,y2) {
 }
 
 
-
+//2ND ZOOM FLOWERS (MORE SATURATED)
 function drawEllipses2(p5,x,y,x1,x2,y1,y2) {
  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
   let max_shift = max_thickness;
@@ -210,7 +209,7 @@ function drawEllipses2(p5,x,y,x1,x2,y1,y2) {
 }
 }
 
-
+//3RD ZOOM FLOWERS (DETAIL OF CENTER OVERLAPPING)
 function drawEllipses3(p5,x,y,x1,x2,y1,y2) {
  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
   let max_shift = max_thickness;
@@ -231,9 +230,9 @@ function drawEllipses3(p5,x,y,x1,x2,y1,y2) {
 
   let c_p00 = p5.map(0, x1, x2, 0, 256);
   let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
-  let c_pball = p5.map(ball_radius2, x1, x2, 0, 256);
+  let c_pball = p5.map(ball_radius3, x1, x2, 0, 256);
   let cur_line_width = c_plwidth - c_p00;
-  let cur_ball_radius2 = c_pball - c_p00;
+  let cur_ball_radius3 = c_pball - c_p00;
 
   p5.background(0);
   p5.fill(0, 0, 128);
@@ -248,87 +247,87 @@ function drawEllipses3(p5,x,y,x1,x2,y1,y2) {
       let y_pos_down = p5.map(y+grid_size, y1, y2, 0, 256);
 
 
- 	  let right_circlex = p5.map(x + 5, x1, x2, 0, 256);
-      let right_circley = p5.map(y - 5, y1, y2, 0, 256);
+ 	  let right_circlex = p5.map(x + 20, x1, x2, 0, 256);
+      let right_circley = p5.map(y - 20, y1, y2, 0, 256);
 
 
-      let left_circlex = p5.map(x - 5, x1, x2, 0, 256);
-      let left_circley = p5.map(y + 5, y1, y2, 0, 256);
+      let left_circlex = p5.map(x - 20, x1, x2, 0, 256);
+      let left_circley = p5.map(y + 20, y1, y2, 0, 256);
 
-      let upperleft_circlex = p5.map(x - 2.5, x1, x2, 0, 256);
-      let upperleft_circley = p5.map(y - 2.5, y1, y2, 0, 256);
+      let upperleft_circlex = p5.map(x -10, x1, x2, 0, 256);
+      let upperleft_circley = p5.map(y - 10, y1, y2, 0, 256);
 
-      let upperright_circlex = p5.map(x + 2.5, x1, x2, 0, 256);
-      let upperright_circley = p5.map(y - 2.5, y1, y2, 0, 256);
+      let upperright_circlex = p5.map(x +10, x1, x2, 0, 256);
+      let upperright_circley = p5.map(y -10, y1, y2, 0, 256);
 
-      let lowerleft_circlex = p5.map(x - 2.5, x1, x2, 0, 256);
-      let lowerleft_circley = p5.map(y + 2.5, y1, y2, 0, 256);
+      let lowerleft_circlex = p5.map(x -10, x1, x2, 0, 256);
+      let lowerleft_circley = p5.map(y +10, y1, y2, 0, 256);
 
-      let lowerright_circlex = p5.map(x + 2.5, x1, x2, 0, 256);
-      let lowerright_circley = p5.map(y + 2.5, y1, y2, 0, 256);
+      let lowerright_circlex = p5.map(x +10, x1, x2, 0, 256);
+      let lowerright_circley = p5.map(y +10, y1, y2, 0, 256);
 
        p5.stroke(0, 0, 150);
       p5.noStroke();
       p5.fill(100, 87, 255, 40);//lilac
-      p5.ellipse(x_pos, y_pos, cur_ball_radius2);
+      p5.ellipse(x_pos, y_pos, cur_ball_radius3);
 
 
-      
-
-      p5.fill(250,13,73,80); //pink
-      p5.ellipse(right_circlex, y_pos, cur_ball_radius2);
-      p5.ellipse(x_pos, right_circley, cur_ball_radius2);
-     
-      p5.ellipse(left_circlex, y_pos, cur_ball_radius2);
-      p5.ellipse(x_pos, left_circley, cur_ball_radius2);
-
-      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius2);
-
-      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius2);
-
-      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius2);
-
-      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius2);
-
-
+  
 
       p5.fill(100, 87, 255, 40);//lilac
-      p5.ellipse(right_circlex, y_pos, cur_ball_radius2*2);
-      p5.ellipse(x_pos, right_circley, cur_ball_radius2*2);
+      p5.ellipse(right_circlex, y_pos, cur_ball_radius3);
+      p5.ellipse(x_pos, right_circley, cur_ball_radius3);
      
-      p5.ellipse(left_circlex, y_pos, cur_ball_radius2*2);
-      p5.ellipse(x_pos, left_circley, cur_ball_radius2*2);
+      p5.ellipse(left_circlex, y_pos, cur_ball_radius3);
+      p5.ellipse(x_pos, left_circley, cur_ball_radius3);
 
-      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius2*2);
+      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius3);
 
-      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius2*2);
+      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius3);
 
-      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius2*2);
+      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius3);
 
-      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius2*2);
+      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius3);
 
 
-      p5.fill(250,13,73,80);//lilac
-      p5.ellipse(right_circlex, y_pos, cur_ball_radius2/1.5);
-      p5.ellipse(x_pos, right_circley, cur_ball_radius2/1.5);
+
+       p5.fill(250,13,73,20); //pink
+      p5.ellipse(right_circlex, y_pos, cur_ball_radius3);
+      p5.ellipse(x_pos, right_circley, cur_ball_radius3);
      
-      p5.ellipse(left_circlex, y_pos, cur_ball_radius2/1.5);
-      p5.ellipse(x_pos, left_circley, cur_ball_radius2/1.5);
+      p5.ellipse(left_circlex, y_pos, cur_ball_radius3);
+      p5.ellipse(x_pos, left_circley, cur_ball_radius3);
 
-      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius2/1.5);
+      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius3);
 
-      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius2/1.5);
+      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius3);
 
-      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius2/1.5);
+      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius3);
 
-      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius2/1.5);
+      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius3);
+
+
+      p5.fill(250,13,73,40);//pink
+      p5.ellipse(right_circlex, y_pos, cur_ball_radius3/1.5);
+      p5.ellipse(x_pos, right_circley, cur_ball_radius3/1.5);
+     
+      p5.ellipse(left_circlex, y_pos, cur_ball_radius3/1.5);
+      p5.ellipse(x_pos, left_circley, cur_ball_radius3/1.5);
+
+      p5.ellipse(upperleft_circlex, upperleft_circley, cur_ball_radius3/1.5);
+
+      p5.ellipse(upperright_circlex, upperright_circley, cur_ball_radius3/1.5);
+
+      p5.ellipse(lowerright_circlex, lowerright_circley, cur_ball_radius3/1.5);
+
+      p5.ellipse(lowerleft_circlex, lowerright_circley, cur_ball_radius3/1.5);
 
 }
 
 }
 }
 
-
+//INITIAL PATTERN
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   /* max_shift is the amount of overlap a tile can spill over into its neighbors */
   let max_shift = max_thickness;
@@ -389,11 +388,7 @@ if (zoom < 2) {
       p5.ellipse(left_circlex, y_pos, cur_ball_radius);
       p5.ellipse(x_pos, left_circley, cur_ball_radius);
 
-
-
-    }
-  
-    /* now draw all elements from back to front */
+     }
      
 
       if (zoom >= 2) {
