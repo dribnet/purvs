261f5fc1614772f76459f97da2dc6154e41914fd
Philip Matich <philipmatich@gmail.com>
2018-10-18 11:02:36 +1300

    Update
    Updated images, readme and drawgrid

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## PS4 MDDN 342 2018
 
-I've been slacking quite a bit so am now trying to catch up. Just familiarising myself with the code at the moment.
+Just a tiny bit of experimenting. Looks cool from different zoom levels.
--- drawgrid.js
+++ drawgrid.js
@@ -22,7 +22,7 @@ var tourPath = [
 
 let ballx = 400
 let bally = 400
-let ballr = 100
+let ballr = 300
 
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
@@ -33,14 +33,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   p5.background(0);
   p5.rectMode(p5.CORNERS);
 
-  // The first red rectangle fills the entire space
-  cx = p5.map(512-960/2, x1, x2, 0, 256);
-  cy = p5.map(512-720/2, y1, y2, 0, 256);
-  cx2 = p5.map(512+960/2, x1, x2, 0, 256);
-  cy2 = p5.map(512+720/2, y1, y2, 0, 256);
-  p5.fill(255, 0, 0);
-  p5.rect(cx, cy, cx2, cy2);
-
   let local_ballx = p5.map(ballx, x1, x2, 0, 256);
   let local_bally = p5.map(bally, y1, y2, 0, 256);
   let local_ballx_edge = p5.map((ballx + ballr), x1, x2, 0, 256); // Work out where edge is by adding radius and center together
@@ -50,16 +42,16 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
     let shade = 128 + 128 / (i+1);
     let current_r = p5.map(i, 0, 9, local_ball_r, 0);
     p5.fill(0, 0, shade);
-    p5.ellipse(local_ballx, local_bally, current_r);
+    p5.ellipse(local_ballx+(i*40), local_bally+(i*40), current_r);
   }
 
   // Two ellipses with a radius of 50 units are then added.
-  cx = p5.map(512, x1, x2, 0, 256);
+  /*cx = p5.map(512, x1, x2, 0, 256);
   cy = p5.map(512, y1, y2, 0, 256);
   cx2 = p5.map(512+100, x1, x2, 0, 256);
   cy2 = p5.map(512+50, y1, y2, 0, 256);
   p5.fill(255);
-  p5.ellipse(cx, cy, (cx2-cx));
+  p5.ellipse(cx, cy, (cx2-cx));*/
 
   //debug - show border
   /*p5.noFill();
