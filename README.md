f527cc8aec90ff9ff0c97fa0e1fc62d4aa708a75
Robert Jordan <robertconceptart@gmail.com>
2018-10-19 14:18:50 +1300

    Re-vamp

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## PS4 MDDN 342 2018
 
-I would like to use the overlap of semi-opaque triangles to create different patterns at different zoom levels. Rather than coding in different elements to appear at different points it is more interesting to have them occur as a result of the zoom. 
+I was having a lot of trouble getting my old code to function properly and map correctly with the zoom. So I have basically redone it all, using code from other students as a guide. I've decided to switch from triangles to quadratic shapes as this allows a lot more options. I have just have a basic pattern set up rihht now but I will continue to add to it.
--- drawgrid.js
+++ drawgrid.js
@@ -1,80 +1,5 @@
-const max_thickness = 64;
-const max_movement = 16;
-// const ball_radius = 32;
-// const line_width = 8;
-const grid_size = 64;
-
-/* the random number seed for the tour */
-var tourSeed = 301;
-/* triplets of locations: zoom, x, y */
-var tourPath = [
-  [1, 356.500000000000, 665.750000000000],
-  [3, 353.250000000000, 668.187500000000],
-  [4, 322.562500000000, 645.093750000000],
-  [5, 322.562500000000, 645.109375000000],
-  [7, 317.984375000000, 643.636718750000],
-  [3, 317.984375000000, 643.636718750000]
-]
-
-/* this function takes a coordinate and aligns to a grid of size gsize */
-function snap_to_grid(num, gsize) {
-  return (num - (num % gsize));
-}
-
-/* this function returns a point offset by noise at that location */
-function getOffsetPoint(p5, x, y, z, noiseScale) {
-  let offsetX = getRandomValue(p5, x, y, z, "offsetX", -max_movement, max_movement, noiseScale);
-  let offsetY = getRandomValue(p5, x, y, z, "offsetY", -max_movement, max_movement, noiseScale);
-  return [x+offsetX, y+offsetY]
-}
-
-// function drawPetals(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2) {
-//   const sqrt2 = 1.4142/2;
-//   let offsets = [
-//     [sqrt2, sqrt2],
-//     [-sqrt2, sqrt2],
-//     [-sqrt2, -sqrt2],
-//     [sqrt2, -sqrt2]
-//   ]
-//   let pixel_posx1 = p5.map(pos_x, x1, x2, 0, 256);
-//   let pixel_posx2 = p5.map(pos_x+rad2, x1, x2, 0, 256);
-//   let pixel_radius = pixel_posx2 - pixel_posx1;
-//   for(let i=0; i<offsets.length; i++) {
-//     let offset = offsets[i];
-//     let pixel_x = p5.map(pos_x+0.5*rad1*offset[0], x1, x2, 0, 256);
-//     let pixel_y = p5.map(pos_y+0.5*rad1*offset[1], y1, y2, 0, 256);
-//     p5.ellipse(pixel_x, pixel_y, pixel_radius);    
-//   }
-// }
-
-// function drawStamens(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2, drawLines) {
-//   const offsets = [
-//     [1, 1],
-//     [1, -1],
-//     [-1, 1],
-//     [-1, -1]
-//   ]
-//   let pixel_posx1 = p5.map(pos_x, x1, x2, 0, 256);
-//   let pixel_posx2 = p5.map(pos_x+rad2, x1, x2, 0, 256);
-//   let pixel_radius = pixel_posx2 - pixel_posx1;
-//   for(var i=0; i<offsets.length; i++) {
-//     let offset = offsets[i];
-//     let pixel_x = p5.map(pos_x+0.5*rad1*offset[0], x1, x2, 0, 256);
-//     let pixel_y = p5.map(pos_y+0.5*rad1*offset[1], y1, y2, 0, 256);
-//     p5.strokeWeight(0);
-//     p5.ellipse(pixel_x, pixel_y, pixel_radius);
-//     if(drawLines) {
-//       p5.strokeWeight(pixel_radius / 20);
-//       p5.line(pixel_x-pixel_radius, pixel_y, pixel_x+pixel_radius, pixel_y);
-//       p5.line(pixel_x, pixel_y-pixel_radius, pixel_x, pixel_y+pixel_radius);
-//       p5.strokeWeight(0);
-//       p5.ellipse(pixel_x, pixel_y, pixel_radius / 12);
-//     }  
-//   }
-// }
-
 /*
- * This is the funciton to implement to make your own abstract design.
+ * This is the function to implement to make your own abstract design.
  *
  * arguments:
  * p5: the p5.js object - all draw commands should be prefixed with this object
@@ -84,159 +9,181 @@ function getOffsetPoint(p5, x, y, z, noiseScale) {
  *
  * The destination drawing should be in the square 0, 0, 255, 255.
  */
-function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
-  let max_shift = max_thickness + max_movement;
+let do_animation = true;
+const max_thickness = 700;
+const grid_size = 250;
 
-  /* this rectangle defines the region that will be drawn and includes a margin */
+/* this function takes a coordinate and aligns to a grid of size gsize */
+function snap_to_grid(num, gsize) {
+  return (num - (num % gsize));
+}
+
+/* the random number seed for the tour */
+var tourSeed = 301;
+/* triplets of locations: zoom, x, y */
+var tourPath = [
+  [0, 512, 512],
+  [1, 474, 571],
+  [2, 410, 383],
+  [3, 346, 417],
+  [4, 357, 379],
+    
+]
+
+
+
+
+function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+    
+    let dz = p5.globalFrameCount / 100.0;
+    z = z + dz;
+    
+      p5.background(255,255,255);
+    
+/* max_shift is the amount of overlap a tile can spill over into its neighbors */
+let max_shift = max_thickness;
+    
+/* this rectangle defines the region that will be drawn and includes a margin */
   let min_x = snap_to_grid(x1 - max_shift, grid_size);
   let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
   let min_y = snap_to_grid(y1 - max_shift, grid_size);
-  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
-
-  // debug version: draw one
-  // let half_x = (x1 + x2) / 2;
-  // let half_y = (y1 + y2) / 2;
-  // min_x = snap_to_grid(half_x, grid_size);
-  // max_x = snap_to_grid(half_x + grid_size, grid_size);
-  // min_y = snap_to_grid(half_y, grid_size);
-  // max_y = snap_to_grid(half_y + grid_size, grid_size);
-
-  // let c_p00 = p5.map(0, x1, x2, 0, 256);
-  // let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
-  // let c_pball = p5.map(ball_radius, x1, x2, 0, 256);
-  // let cur_line_width = c_plwidth - c_p00;
-  // let cur_ball_radius = c_pball - c_p00;
-
-  p5.background(255);
-  for(let x=min_x; x<max_x; x+=grid_size) {
+  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);    
+    
+for(let x=min_x; x<max_x; x+=grid_size) {
     for(let y=min_y; y<max_y; y+=grid_size) {
-      // First compute shifted point in grid
-      let offsetX = getRandomValue(p5, x, y, z, "shiftX", -max_movement, max_movement, 0);
-      let offsetY = getRandomValue(p5, x, y, z, "shiftY", -max_movement, max_movement, 0);
-      let shifted_x = x + offsetX;
-      let shifted_y = y + offsetY;
-      let x_pos = p5.map(shifted_x, x1, x2, 0, 256);
-      let y_pos = p5.map(shifted_y, y1, y2, 0, 256);
-
-      // now compute shifted point one step to the left
-      let x_left = x + grid_size;
-      let y_left = y;
-      let offsetX_left = getRandomValue(p5, x_left, y_left, z, "shiftX", -max_movement, max_movement, 0);
-      let offsetY_left = getRandomValue(p5, x_left, y_left, z, "shiftY", -max_movement, max_movement, 0);
-      let shifted_x_left = x_left + offsetX_left;
-      let shifted_y_left = y_left + offsetY_left;
-      let x_pos_left = p5.map(shifted_x_left, x1, x2, 0, 256);
-      let y_pos_left = p5.map(shifted_y_left, y1, y2, 0, 256);
-
-      // lastly compute shifted point one step down
-      let x_down = x;
-      let y_down = y + grid_size;
-      let offsetX_down = getRandomValue(p5, x_down, y_down, z, "shiftX", -max_movement, max_movement, 0);
-      let offsetY_down = getRandomValue(p5, x_down, y_down, z, "shiftY", -max_movement, max_movement, 0);
-      let shifted_x_down = x_down + offsetX_down;
-      let shifted_y_down = y_down + offsetY_down;
-      let x_pos_down = p5.map(shifted_x_down, x1, x2, 0, 256);
-      let y_pos_down = p5.map(shifted_y_down, y1, y2, 0, 256);
-
-      /* now draw all elements from back to front */
-      // if (zoom < 2) {
-      //   p5.strokeWeight(cur_line_width);
-      //   p5.stroke(150, 0, 0);
-      //   // p5.line(x_pos, y_pos, x_pos_left, y_pos_left);
-      //   p5.stroke(0, 150, 0);
-      //   // p5.line(x_pos, y_pos, x_pos_down, y_pos_down);
-      // }
-
-      // if (zoom >= 2) {
-      //   p5.fill(0, 0, 255);
-      //   p5.noStroke();
-      //   drawPetals(p5, x1, x2, y1, y2, shifted_x, shifted_y, ball_radius, 2*line_width);        
-      // }
-
-      let point180 = p5.map(18, x1, x2, 0, 256);
-      let point80 = p5.map(8, x1, x2, 0, 256);
+        
 
 
+ // Red Diamonds
+          
+
+        if (zoom >=0) {
+             //DRAW QUADRATIC
+      let quad_1x = p5.map(x+ 0, x1, x2, 0, 256);
+      let quad_1y = p5.map(y+100, y1, y2, 0, 256);
+
+      let quad_2x = p5.map(x+ 90, x1, x2, 0, 256);
+      let quad_2y = p5.map(y-100, y1, y2, 0, 256); 
+
+      let quad_3x = p5.map(x+0, x1, x2, 0, 256);
+      let quad_3y = p5.map(y-200, y1, y2, 0, 256); 
+
+      let quad_4x = p5.map(x-90, x1, x2, 0, 256);
+      let quad_4y = p5.map(y-100, y1, y2, 0, 256); 
 
       p5.stroke(253, 50, 57, 150);
       p5.strokeWeight(3);
       p5.noStroke();
       p5.fill(253, 80, 87,100);
-      // p5.noFill();
-      // p5.angleMode(RADIANS);
-      // p5.triangle(x_pos, y_pos -200, x_pos - 100, y_pos + 100, x_pos + 100, y_pos + 100);
-
-      p5.triangle(x_pos, y_pos -180, x_pos - 80, y_pos + 80, x_pos + 80, y_pos + 80);
-      // p5.rotate(.050);
-      p5.triangle(x_pos, y_pos -140, x_pos - 40, y_pos + 40, x_pos + 40, y_pos + 40);
-      // p5.rotate(1.0);
-      p5.triangle(x_pos, y_pos -120, x_pos - 20, y_pos + 20, x_pos + 20, y_pos + 20);
-
-      // p5.triangle(x_pos, y_pos -100, x_pos -  0, y_pos +  0, x_pos +  0, y_pos +  0);
-
-
-      p5.fill(87, 80, 253,100);
-      // p5.triangle(x_pos, y_pos + 200, x_pos - 100, y_pos - 100, x_pos + 100, y_pos - 100);
-
-      p5.triangle(x_pos, y_pos + 180, x_pos - 80, y_pos - 80, x_pos + 80, y_pos - 80);
-      // p5.rotate(.050);
-      p5.triangle(x_pos, y_pos + 140, x_pos - 40, y_pos - 40, x_pos + 40, y_pos - 40);
-      // p5.rotate(1.0);
-      p5.triangle(x_pos, y_pos + 120, x_pos - 20, y_pos - 20, x_pos + 20, y_pos - 20);
-
-      if(zoom>=2){
-      p5.fill(253, 80, 87,100);;
-      p5.triangle(x_pos, y_pos -240, x_pos - 140, y_pos + 140, x_pos + 140, y_pos + 140);
-      p5.triangle(x_pos, y_pos -220, x_pos - 120, y_pos + 120, x_pos + 120, y_pos + 120);
-      p5.triangle(x_pos, y_pos -200, x_pos - 100, y_pos + 100, x_pos + 100, y_pos + 100);
-
-
-      p5.fill(87, 80, 253,100);
-      p5.triangle(x_pos, y_pos + 240, x_pos - 140, y_pos - 140, x_pos + 140, y_pos - 140);
-      p5.triangle(x_pos, y_pos + 220, x_pos - 120, y_pos - 120, x_pos + 120, y_pos - 120);
-      p5.triangle(x_pos, y_pos + 200, x_pos - 100, y_pos - 100, x_pos + 100, y_pos - 100);
-
-      // p5.fill(253, 80, 87,100);;
-      // p5.triangle(x_pos, y_pos -40, x_pos - 40, y_pos + 40, x_pos + 40, y_pos + 40);
-      // p5.triangle(x_pos, y_pos -20, x_pos - 20, y_pos + 20, x_pos + 20, y_pos + 20);
-      // p5.triangle(x_pos, y_pos -0 , x_pos - 0, y_pos + 0, x_pos + 0, y_pos + 0);
-
+      p5.beginShape();
+          p5.vertex(quad_1x, quad_1y);
+          p5.vertex(quad_2x, quad_2y);
+          p5.vertex(quad_3x, quad_3y);
+          p5.vertex(quad_4x, quad_4y);
+      p5.endShape();
+ 
       }
-      // 
-      //  if(zoom>=4){
-      // p5.fill(253, 80, 87,100);;
-      // p5.triangle(x_pos, y_pos -240, x_pos - 140, y_pos + 140, x_pos + 140, y_pos + 140);
-      // p5.triangle(x_pos, y_pos -220, x_pos - 120, y_pos + 120, x_pos + 120, y_pos + 120);
-      // p5.triangle(x_pos, y_pos -200, x_pos - 100, y_pos + 100, x_pos + 100, y_pos + 100);
+        
+        if (zoom >=0) {
+             //DRAW QUADRATIC
+      let quad_1x = p5.map(x+ 0, x1, x2, 0, 256);
+      let quad_1y = p5.map(y+80, y1, y2, 0, 256);
+
+      let quad_2x = p5.map(x+ 70, x1, x2, 0, 256);
+      let quad_2y = p5.map(y-80, y1, y2, 0, 256); 
+
+      let quad_3x = p5.map(x+0, x1, x2, 0, 256);
+      let quad_3y = p5.map(y-180, y1, y2, 0, 256); 
+
+      let quad_4x = p5.map(x-70, x1, x2, 0, 256);
+      let quad_4y = p5.map(y-80, y1, y2, 0, 256); 
+
+      p5.stroke(253, 50, 57, 150);
+      p5.strokeWeight(3);
+      p5.noStroke();
+      p5.fill(253, 80, 87,100);
+      p5.beginShape();
+          p5.vertex(quad_1x, quad_1y);
+          p5.vertex(quad_2x, quad_2y);
+          p5.vertex(quad_3x, quad_3y);
+          p5.vertex(quad_4x, quad_4y);
+      p5.endShape();
+ 
+      }
+        
+     
+        
+// Blue Diamonds
 
 
-      // p5.fill(87, 80, 253,100);
-      // p5.triangle(x_pos, y_pos + 240, x_pos - 140, y_pos - 140, x_pos + 140, y_pos - 140);
-      // p5.triangle(x_pos, y_pos + 220, x_pos - 120, y_pos - 120, x_pos + 120, y_pos - 120);
-      // p5.triangle(x_pos, y_pos + 200, x_pos - 100, y_pos - 100, x_pos + 100, y_pos - 100);
-      // }
 
+if (zoom >=0) {
+           //DRAW QUADRATIC
+      let quad_1x = p5.map(x+ 0, x1, x2, 0, 256);
+      let quad_1y = p5.map(y-100, y1, y2, 0, 256);
 
+      let quad_2x = p5.map(x+ 90, x1, x2, 0, 256);
+      let quad_2y = p5.map(y+100, y1, y2, 0, 256); 
 
+      let quad_3x = p5.map(x+0, x1, x2, 0, 256);
+      let quad_3y = p5.map(y+200, y1, y2, 0, 256); 
 
-      // if(zoom >= 3) {
-      //   // now if we are super zoomed, draw lines in the stamen
-      //   var drawLines = false;
-      //   if (zoom >= 5) drawLines = true;
-      //   p5.fill(0, 0, 255);
-      //   p5.stroke(0, 0, 128);
-      //   drawStamens(p5, x1, x2, y1, y2, shifted_x, shifted_y, ball_radius/3, line_width/2, drawLines);
-      // }
+      let quad_4x = p5.map(x-90, x1, x2, 0, 256);
+      let quad_4y = p5.map(y+100, y1, y2, 0, 256); 
+
+      p5.stroke(253, 50, 57, 150);
+      p5.strokeWeight(3);
+      p5.noStroke();
+      p5.fill(87, 80, 253,100);
+      p5.beginShape();
+          p5.vertex(quad_1x, quad_1y);
+          p5.vertex(quad_2x, quad_2y);
+          p5.vertex(quad_3x, quad_3y);
+          p5.vertex(quad_4x, quad_4y);
+      p5.endShape();
+        
+}
+     
+
+     if (zoom >=0) {
+             //DRAW QUADRATIC
+      let quad_1x = p5.map(x+ 0, x1, x2, 0, 256);
+      let quad_1y = p5.map(y-80, y1, y2, 0, 256);
+
+      let quad_2x = p5.map(x+ 70, x1, x2, 0, 256);
+      let quad_2y = p5.map(y+80, y1, y2, 0, 256); 
+
+      let quad_3x = p5.map(x+0, x1, x2, 0, 256);
+      let quad_3y = p5.map(y+180, y1, y2, 0, 256); 
+
+      let quad_4x = p5.map(x-70, x1, x2, 0, 256);
+      let quad_4y = p5.map(y+80, y1, y2, 0, 256); 
+
+      p5.stroke(253, 50, 57, 150);
+      p5.strokeWeight(3);
+      p5.noStroke();
+       p5.fill(87, 80, 253,100);
+      p5.beginShape();
+          p5.vertex(quad_1x, quad_1y);
+          p5.vertex(quad_2x, quad_2y);
+          p5.vertex(quad_3x, quad_3y);
+          p5.vertex(quad_4x, quad_4y);
+      p5.endShape();
+ 
+      }   
     }
-  }
+}
+    
+    
+    
+ 
+    
+    
+    //SHOW RED LINE GRID
+//   p5.noFill();
+//   p5.stroke(255, 100, 100)
+//   p5.rect(0, 0, 255, 255);
+    
+    
 
-  // debug - show border
-  // p5.noFill();
-  // p5.stroke(0, 200, 200)
-  // p5.strokeWeight(1);
-  // p5.rect(0, 0, 255, 255);
-  // p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
-  // let sizex = x2 - x1;
-  // p5.text("width: " + sizex, 10, 40);
 }
