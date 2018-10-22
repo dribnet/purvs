5867d5e755497f9f380dcfcf9eb9708c9950826a
Erin O'Brien <erinobrien1997@hotmail.com>
2018-10-22 13:16:39 +1300

    test

--- drawgrid.js
+++ drawgrid.js
@@ -1,6 +1,6 @@
 let do_animation = true;
 const max_thickness = 700;
-const grid_size = 100;
+const grid_size = 64;
 const line_width = 8;
 const ball_radius = 28;
 const ball_radius2 = 40;
@@ -54,39 +54,88 @@ for(let x=min_x; x<max_x; x+=grid_size) {
     
 
  if (zoom <2) {
-   //Top Diamond 1 Super Large
-
   let sineWave = p5.sin(p5.globalFrameCount/10);
   let pulse = p5.map(sineWave, -1, 1, 0, 40);
 
-      let x_centre_circle = p5.map(x, x1, x2, 0, 256);
-      let y_centre_circle = p5.map(y, y1, y2, 0, 256);
-      let x_right_circle = p5.map(x + 15, x1, x2, 0, 256);
-      let x_left_circle = p5.map(x - 15, x1, x2, 0, 256);
-      let y_top_circle = p5.map(y - 15, y1, y2, 0, 256);
-      let y_bottom_circle = p5.map(y + 15, y1, y2, 0, 256);
-
-
-      p5.noStroke();
-  
-      //Right Circle
-      p5.fill(100, 87, 255, 40);//lilac
-      p5.ellipse(x_right_circle, y_centre_circle, (cur_ball_radius + pulse));
-
-      //Left Circle
-      p5.ellipse(x_left_circle, y_centre_circle, (cur_ball_radius + pulse));
-
-      //Top Circle
-      p5.ellipse(x_centre_circle, y_top_circle, (cur_ball_radius + pulse));
-
-      //Bottom Circle
-      p5.ellipse(x_centre_circle, y_bottom_circle, (cur_ball_radius + pulse));
-
+ p5.translate(180, 150);
+  p5.noStroke();
+  p5.fill(100, 87, 255, 100);//lilac
+  for (var i = 0; i < 4; i ++) {
+    p5.ellipse(0, 20, (cur_ball_radius + pulse));
+    p5.rotate(p5.PI/2);
 
  
       }
+    }
 
-       if (zoom >=2) {
+     if (zoom ==2) {
+  let sineWave = p5.sin(p5.globalFrameCount/10);
+  let pulse2 = p5.map(sineWave, -1, 1, 0, 40);
+
+ p5.translate(180, 150);
+  p5.noStroke();
+  p5.fill(100, 87, 255, 100);//lilac
+  for (var i = 0; i < 6; i ++) {
+    p5.ellipse(0, 20, (cur_ball_radius/2 + pulse2));
+    p5.rotate(p5.PI/3);
+ 
+      }
+    }
+
+
+
+  if (zoom ==3) {
+  let sineWave = p5.sin(p5.globalFrameCount/10);
+  let pulse3 = p5.map(sineWave, -1, 1, -40, 40);
+
+  p5.translate(180, 150);
+  p5.noStroke();
+  p5.fill(250,13,73,40);//lilac
+  for (var i = 0; i < 8; i ++) {
+    p5.ellipse(0, 20, (cur_ball_radius/2 + pulse3));
+    p5.rotate(p5.PI/4);
+ 
+      }
+    }
+
+
+
+
+
+ // if (zoom <2) {
+ //   //Top Diamond 1 Super Large
+
+ //  let sineWave = p5.sin(p5.globalFrameCount/10);
+ //  let pulse = p5.map(sineWave, -1, 1, 0, 40);
+
+ //      let x_centre_circle = p5.map(x, x1, x2, 0, 256);
+ //      let y_centre_circle = p5.map(y, y1, y2, 0, 256);
+ //      let x_right_circle = p5.map(x + 15, x1, x2, 0, 256);
+ //      let x_left_circle = p5.map(x - 15, x1, x2, 0, 256);
+ //      let y_top_circle = p5.map(y - 15, y1, y2, 0, 256);
+ //      let y_bottom_circle = p5.map(y + 15, y1, y2, 0, 256);
+
+
+ //      p5.noStroke();
+  
+ //      //Right Circle
+ //      p5.fill(100, 87, 255, 40);//lilac
+ //      p5.ellipse(x_right_circle, y_centre_circle, (cur_ball_radius + pulse));
+
+ //      //Left Circle
+ //      p5.ellipse(x_left_circle, y_centre_circle, (cur_ball_radius + pulse));
+
+ //      //Top Circle
+ //      p5.ellipse(x_centre_circle, y_top_circle, (cur_ball_radius + pulse));
+
+ //      //Bottom Circle
+ //      p5.ellipse(x_centre_circle, y_bottom_circle, (cur_ball_radius + pulse));
+
+
+ 
+ //      }
+
+       /*if (zoom >=3) {
 
       let sineWave = p5.sin(p5.globalFrameCount/10);
       let pulse2 = p5.map(sineWave, -1, 1, -50, 20);
@@ -162,7 +211,7 @@ for(let x=min_x; x<max_x; x+=grid_size) {
       p5.ellipse(x_centre_circle, y_bottom_minicircle, (cur_ball_radius/2 + pulse2));
 
  
-      }
+      }*/
     
    
      
