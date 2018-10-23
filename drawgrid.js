d15ecbd0e4a72a00d0437fe842316160e76ade74
Guest User <Guest@wg401-b-06-06.mddn.vuw.ac.nz>
2018-10-23 16:36:59 +1300

    animation working for four zooms

--- drawgrid.js
+++ drawgrid.js
@@ -29,24 +29,35 @@ var circleWidth1 = 400/2;
 var circleWidth2 = 200/2;
 var circleWidth3 = 100/2;
 var baseAlpha = 11;
-var GlobalDrawCalls = 1500;
+var maxDrawCalls = 1500;
+var DrawsPerFrame = 15;
+var GlobalDrawCalls = maxDrawCalls/DrawsPerFrame;
 var ZoomLevel = 0;
 var toggleZoom = true;
 let do_animation = true;
 var t;
-var dz;
+var t2;
+var Frames;
 
 // This version draws two rectangles and two ellipses.
 // The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 	// console.log(t);
 
-	t = p5.globalFrameCount;
 
-	if(p5.globalFrameCount)
+
+	if(p5.globalFrameCount!=t){
+		Frames++;
+		t=p5.globalFrameCount;
+		console.log(Frames);
+	}
+
+	if(Frames<20){
+		p5.background(0);
+	}
 	
-  	console.log(dz);
-	// t = t + dz;
+  	
+	// t = t + Frames;
   	
  	// temporary variables used for object placement
 	let cx=0, cy=0, cx2=0, cy2=0;
@@ -57,91 +68,122 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 	
 	var moveVert = 200;
 	var moveHoriz = 200;
+	for (var i = 1; i <= DrawsPerFrame; i++) {
+		if (!zoom){
+			p5.strokeWeight(0.9);
+			DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 1, 3, 1, "purple");
+			DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 2, 2, 1, "purple");
+			DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 2, 1, "purple");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 1, "purple");
+			DrawRegressiveCircle(p5, x1, x2, y1, y2, z, zoom, 1);
+			// DrawCircleAnimate(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 1, "purple");
 
-	if (!zoom){
-		p5.strokeWeight(0.9);
-		GlobalDrawCalls = 1500;
-		// DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 1, 2, 1, "purple");
-		// DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 2, 3, 1, "purple");
-		// DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 2, 1, "purple");
-		// DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 1, "purple");
-		// DrawRegressiveCircle(p5, x1, x2, y1, y2, z, zoom, 1);
-		DrawCircleAnimate(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 1, "purple");
 
+		}else if(zoom == 1){
+			// GlobalDrawCalls = 1;
+			p5.strokeWeight(1.1);
+			DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 1, 2, 1, "purple");
+			DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 2, 3, 1);
+			DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 2, 1, 1, "purple");
+			DrawRegressiveCircle(p5, x1, x2, y1, y2, z, zoom, 1);
 
-	}else if(zoom == 1){
-		GlobalDrawCalls = 1;
-		p5.strokeWeight(1.1);
-		DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 1, 2, 1, "purple");
-		// DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, 2, 3, 1);
-		DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 2, 1, 1, "purple");
-		DrawRegressiveCircle(p5, x1, x2, y1, y2, z, zoom, 1);
+		}else if(zoom == 2){
+			// do_animation = false;
+			moveVert = 60;
+			moveHoriz = 60;
+			p5.strokeWeight(2.5);
+			DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.2, 2, "purple");
+			DrawEllipseTall(p5, x1, x2, y1+0, y2+0, z, zoom, 1.2, 2, "cyan");
+			DrawEllipseWide(p5, x1, x2, y1+0, y2+0, z, zoom, 1.2, 2, "cyan");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.6, 2.5, "black");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.6, 3, "black");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.4, 6, "black");
+			DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 1, 15, "black");
+			DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 1, 15, "black");
+			DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 1, 15, "black");
+			DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 1, 15, "black");
+		}else if(zoom == 3){
+			p5.strokeWeight(3.5);
+			moveVert = 34;
+			moveHoriz = 34;
 
-	}else if(zoom == 2){
-		// do_animation = false;
-		moveVert = 60;
-		moveHoriz = 60;
-		p5.strokeWeight(2);
-		DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.4, 2, "purple");
-		DrawEllipseTall(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2, "cyan");
-		DrawEllipseWide(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2, "cyan");
-		DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2.5, "black");
-		DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 3, "black");
-		DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 6, "black");
-		DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 0.2, 15, "black");
-		DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 0.2, 15, "black");
-		DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 0.2, 15, "black");
-		DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 0.2, 15, "black");
-	}else if(zoom == 3){
-		p5.strokeWeight(4);
-		moveVert = 34;
-		moveHoriz = 34;
+			// DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.4, 2, "purple");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 2, "purple");
+			DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, .5, 1.5, 9, "purple");
+			DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 1, 1.5, 9, "purple");		
+			DrawEllipseTall(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 2, "cyan");
+			DrawEllipseWide(p5, x1, x2, y1+0, y2+0, z, zoom, 1, 2, "cyan");
+			DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 2, 3, "black");
+			DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 2, 6, "black");
+			DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 1, 18, "black");
+			DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 1, 18, "black");
+			DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 1, 18, "black");
+			DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 1, 18, "black");
+			
 
-		// DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.4, 2, "purple");
-		DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 1.4, 2, "purple");
-		DrawValleyVertical(p5, x1, x2, y1, y2, z, zoom, .5, .8, 9, "purple");
-		DrawValleyHorizontal(p5, x1, x2, y1, y2, z, zoom, 1, .8, 9, "purple");		
-		DrawEllipseTall(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2, "cyan");
-		DrawEllipseWide(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2, "cyan");
-		// DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 2.5, "black");
-		DrawCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 3, "black");
-		DrawBezierCircle(p5, x1, x2, y1+0, y2+0, z, zoom, 0.8, 6, "black");
-		DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 0.2, 18, "black");
-		DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1+moveVert, y2+moveVert, z, zoom, 0.2, 18, "black");
-		DrawCircle(p5, x1+moveHoriz, x2+moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 0.2, 18, "black");
-		DrawCircle(p5, x1-moveHoriz, x2-moveHoriz, y1-moveVert, y2-moveVert, z, zoom, 0.2, 18, "black");
-		
+		}else if(zoom == 4){
 
-	}else if(zoom == 4){
+		}else if(zoom == 5){
 
-	}else if(zoom == 5){
+		}else if(zoom == 6){
 
-	}else if(zoom == 6){
-
+		}
 	}
-
-	p5.strokeWeight(0.9);
-	GlobalDrawCalls = 2000
+	
 
 	if(zoom){
 		if(zoom!=ZoomLevel){
-			dz = 0;
+			Frames = 0;
 			ZoomLevel = zoom;
-			console.log(ZoomLevel);
+			console.log("ZoomLevel "+ ZoomLevel);
 			// p5.background(0);
 		}	
 		toggleZoom = true;	
 	}else if(!zoom&&toggleZoom){
-		dz = 0;
+		Frames = 0;
 		ZoomLevel = 0;
 		toggleZoom = false;
-	 	console.log(0);
+	 	console.log("ZoomLevel "+0);
 	 	// p5.background(0);
 	}
 	
 
 	// p5.bezier(b1, b2, b3, b4, b5, b6, b7, b8);  	
   }
+
+	// function DrawCircle (p5, x1, x2, y1, y2, z, zoom, magnitude, scale, colour) {
+	// 	let x = 512;
+	// 	let y = 512;
+	// 	let noiseScale = 0.1;
+	// 	// p5.stroke(204, 78, 206,baseAlpha);
+		
+	// 	if(colour=="purple"){
+	// 		p5.stroke(204, 78, 206,baseAlpha);
+			
+	// 	}else if(colour=="cyan"){
+	// 		p5.stroke(96, 200, 224, baseAlpha);
+	// 	}else if(colour=="black"){
+	// 		p5.stroke(0, 0, 0, baseAlpha);
+	// 	}
+
+	// 	if (Frames<(GlobalDrawCalls*magnitude)){
+
+	// 		var angle1 = p5.random(0, 2 * p5.PI);
+	// 		var angle2 = p5.random(0, 2 * p5.PI);
+
+	// 		var xpos1 = (circleWidth2) + (circleWidth1/scale) * p5.cos(angle1);
+	// 		var ypos1 = circleWidth2 + (circleWidth1/scale) * p5.sin(angle1);
+	// 		var xpos2 = (circleWidth2) + (circleWidth1/scale) * p5.cos(angle2);
+	// 		var ypos2 = circleWidth2 + (circleWidth1/scale) * p5.sin(angle2)
+
+	// 		var l1 = p5.map(xpos1 + (x/1.3), x1, x2, 0, 256);
+	// 		var l2 = p5.map(ypos1 + (y/1.3), y1, y2, 0, 256);
+	// 		var l3 = p5.map(xpos2 + (x/1.3), x1, x2, 0, 256);
+	// 		var l4 = p5.map(ypos2 + (y/1.3), y1, y2, 0, 256);
+
+	// 		p5.line(l1,l2,l3,l4);
+	// 	}
+	// }
 
 	function DrawCircle (p5, x1, x2, y1, y2, z, zoom, magnitude, scale, colour) {
 		let x = 512;
@@ -157,42 +199,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-
-		for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
-
-			var angle1 = p5.random(0, 2 * p5.PI);
-			var angle2 = p5.random(0, 2 * p5.PI);
-
-			var xpos1 = (circleWidth2) + (circleWidth1/scale) * p5.cos(angle1);
-			var ypos1 = circleWidth2 + (circleWidth1/scale) * p5.sin(angle1);
-			var xpos2 = (circleWidth2) + (circleWidth1/scale) * p5.cos(angle2);
-			var ypos2 = circleWidth2 + (circleWidth1/scale) * p5.sin(angle2)
-
-			var l1 = p5.map(xpos1 + (x/1.3), x1, x2, 0, 256);
-			var l2 = p5.map(ypos1 + (y/1.3), y1, y2, 0, 256);
-			var l3 = p5.map(xpos2 + (x/1.3), x1, x2, 0, 256);
-			var l4 = p5.map(ypos2 + (y/1.3), y1, y2, 0, 256);
-
-			p5.line(l1,l2,l3,l4);
-		}
-	}
-
-	function DrawCircleAnimate (p5, x1, x2, y1, y2, z, zoom, magnitude, scale, colour) {
-		let x = 512;
-		let y = 512;
-		let noiseScale = 0.1;
-		// p5.stroke(204, 78, 206,baseAlpha);
-		
-		if(colour=="purple"){
-			p5.stroke(204, 78, 206,baseAlpha);
-			
-		}else if(colour=="cyan"){
-			p5.stroke(96, 200, 224, baseAlpha);
-		}else if(colour=="black"){
-			p5.stroke(0, 0, 0, baseAlpha);
-		}
-	  	if (dz<(GlobalDrawCalls*magnitude)){
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 			
 
 			var angle1 = p5.random(0, 2 * p5.PI);
@@ -225,9 +232,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-	  	for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
-
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
 
@@ -258,8 +263,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-	  	for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
@@ -291,8 +295,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-	  	for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
@@ -333,8 +336,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-	  	for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
@@ -368,8 +370,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		}else if(colour=="black"){
 			p5.stroke(0, 0, 0, baseAlpha);
 		}
-	  	for(let i=0; i< GlobalDrawCalls*magnitude; i++) {
-			let curz = i * 4;
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
@@ -397,8 +398,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 		let noiseScale = 0.1;
 		let alpha = 7;
 
-	  	for(let i=0; i< 1000; i++) {
-			let curz = i * 4;
+	  	if (Frames<(GlobalDrawCalls*magnitude)){
 
 			var angle1 = p5.random(0, 2 * p5.PI);
 			var angle2 = p5.random(0, 2 * p5.PI);
