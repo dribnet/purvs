14ea3d7a20918aef128c8ec14b6f985ad91b222e
Emily Shaw <shawemil@wg401-i-06-07.mddn.vuw.ac.nz>
2018-10-24 17:27:48 +1300

    ef

--- drawgrid.js
+++ drawgrid.js
@@ -1,135 +1,66 @@
 const max_thickness = 64;
+const max_movement = 1;
 const ball_radius = 32;
-
-
 const line_width = 8;
 const grid_size = 64;
-const max_movement = 100;
-
 let do_animation = true;
-// var r = random(255);
+
 /* the random number seed for the tour */
-var tourSeed = 301;
+var tourSeed = 150;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-  [1, 356.500000000000, 665.750000000000],
-  [3, 353.250000000000, 668.187500000000],
-  [4, 322.562500000000, 645.093750000000],
-  [5, 322.562500000000, 645.109375000000],
-  [7, 317.984375000000, 643.636718750000],
-  [3, 317.984375000000, 643.636718750000]
+  [2, -146, 1132],
+  [2, -149, 1257]
 ]
 
 /* this function takes a coordinate and aligns to a grid of size gsize */
-
-
-/* this function returns a point offset by noise at that location */
-function getOffsetPoint(p5, x, y, z, noiseScale) {
-  let noiseX = p5.noise(x * noiseScale,
-                        y * noiseScale, z);
-  let noiseY = p5.noise(x * noiseScale,
-                        y * noiseScale, z+50);
-  let offsetX = p5.map(noiseX, 0, 1, -max_movement, max_movement);
-  let offsetY = p5.map(noiseY, 0, 1, -max_movement, max_movement);
-  return [x+offsetX, y+offsetY]
-}
 function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
 }
-/*
- * This is the funciton to implement to make your own abstract design.
- *
- * arguments:
- * p5: the p5.js object - all draw commands should be prefixed with this object
- * x1, x2, y1, y2: draw the pattern contained in the rectangle x1,y1 to x2, y2
- * z: use this as the noise z offset (can be shifted)
- * zoom: current zoom level (starts at 0), useful to decide how much detail to draw
- *
- * The destination drawing should be in the square 0, 0, 255, 255.
- */
-function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 
-
-	  /* For animation: updated z based on global frame count */
-  let dz = p5.globalFrameCount / 100.0;
-  z = z + dz;
-
-   let phase = x * y;
-  let sineWave = p5.sin(phase + p5.globalFrameCount*3);
-  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
-  let max_shift = max_thickness + max_movement;
-
-  /* this rectangle defines the region that will be drawn and includes a margin */
-  let min_x = snap_to_grid(x1 - max_shift, grid_size);
-  let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
-  let min_y = snap_to_grid(y1 - max_shift, grid_size);
-  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
-
-  // debug version: draw one
-  // let half_x = (x1 + x2) / 2;
-  // let half_y = (y1 + y2) / 2;
-  // min_x = snap_to_grid(half_x, grid_size);
-  // max_x = snap_to_grid(half_x + grid_size, grid_size);
-  // min_y = snap_to_grid(half_y, grid_size);
-  // max_y = snap_to_grid(half_y + grid_size, grid_size);
-
-  let c_p00 = p5.map(0, x1, x2, 0, 256);
-  let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
-  let c_pball = p5.map(ball_radius, x1, x2, 0, 256);
-  let cur_line_width = c_plwidth - c_p00;
-  let cur_ball_radius = c_pball - c_p00;
-
-
-
-  p5.background(255);
-  p5.fill(0, 0, 128);
-  for(let x=min_x; x<max_x; x+=grid_size) {
-    for(let y=min_y; y<max_y; y+=grid_size) {
-      /* first compute all three points with offsets */
-      let shift_point = getOffsetPoint(p5, x, y, z, 0.1);
-      let x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
-      let y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
-
-      let shift_point_left = getOffsetPoint(p5, x+grid_size, y, z, 0.1);
-      let x_pos_left = p5.map(shift_point_left[0], x1, x2, 0, 256);
-      let y_pos_left = p5.map(shift_point_left[1], y1, y2, 0, 256);
-
-      let shift_point_down = getOffsetPoint(p5, x, y+grid_size, z, 0.1);
-      let x_pos_down = p5.map(shift_point_down[0], x1, x2, 0, 256);
-      let y_pos_down = p5.map(shift_point_down[1], y1, y2, 0, 256);
-
-       tree(p5, x , y, x1, x2, y1, y2, z, zoom);
-
-
-      /* now draw all elements from back to front */
-      // p5.strokeWeight(cur_line_width);
-      // p5.stroke(150, 0, 0);
-      // //p5.line(x_pos, y_pos, x_pos_left, y_pos_left);
-      // p5.stroke(0, 150, 0);
-      // //p5.line(x_pos, y_pos, x_pos_down, y_pos_down);
-
-      // p5.stroke(0, 0, 150);
-      // p5.noStroke();
-      // p5.rect(x_pos, y_pos, cur_ball_radius/2,cur_ball_radius/8);
-
-      // p5.fill(200);
-      //  p5.rect(x_pos+ (cur_ball_radius/20), y_pos-32, cur_ball_radius/2.5,cur_ball_radius/8);
-      //  p5.fill(210);
-      //  p5.rect(x_pos+ (cur_ball_radius/20), y_pos-64, cur_ball_radius/5,cur_ball_radius/8);
-
-     
-    }
-  }
-
- // debug - show border
-  // p5.noFill();
-  // p5.stroke(0, 200, 200)
-  // p5.strokeWeight(1);
-  // p5.rect(0, 0, 255, 255);
-  // p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
-  // let sizex = x2 - x1;
-  // p5.text("width: " + sizex, 10, 40);
+/* this function returns a point offset by noise at that location */
+function getOffsetPoint(p5, x, y, z, noiseScale) {
+  let offsetX = getRandomValue(p5, x, y, z, "offsetX", -max_movement, max_movement, noiseScale);
+  let offsetY = getRandomValue(p5, x, y, z, "offsetY", -max_movement, max_movement, noiseScale);
+  return [x+offsetX, y+offsetY]
 }
+
+function drawLights(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2, z) {
+  const sqrt2 = 1.4142/2;
+  let offsets = [
+    [0.380, -0.5], //1 (top)
+    [0.32, -0.44], //2
+    [0.23, -0.40], //3
+    [0.13, -0.38], //4
+    [0.23, -0.31], //5
+    [0.35, -0.27], //6
+    [0.50, -0.25], //7
+    [0.37, -0.18], //8
+    [0.23, -0.14], //9
+    [0.05, -0.13], //10
+    [0.2, -0.01], //11
+    [0.40, 0.04],
+    [0.59, 0.085]
+  ]
+
+  let phase = getRandomValue(p5, pos_x, pos_y, z, "phase", 0, 2*p5.PI, 0.1);
+  let freq = getRandomValue(p5, pos_x, pos_y, z, "freq", 10, 50, 0.1);
+  let sineWave = p5.sin(phase + (p5.globalFrameCount / freq));
+  let radiusScale = p5.map(sineWave, -1, 1, 0.80, 1.0);
+
+  let pixel_posx1 = p5.map(pos_x, x1, x2, 0, 256);
+  let pixel_posx2 = p5.map(pos_x+rad2, x1, x2, 0, 256);
+  let pixel_radius = pixel_posx2 - pixel_posx1;
+  pixel_radius = (radiusScale * pixel_radius)/25;
+  for(let i=0; i<offsets.length; i++) {
+    let offset = offsets[i];
+    let pixel_x = p5.map(pos_x+0.5*rad1*offset[0], x1, x2, 0, 256);
+    let pixel_y = p5.map(pos_y+0.5*rad1*offset[1], y1, y2, 0, 256);
+  
+    p5.rect(pixel_x, pixel_y, pixel_radius, pixel_radius);    
+  }
+}
+
 
 
 
@@ -162,9 +93,13 @@ function tree (p5, x, y, x1, x2, y1, y2, z, zoom){
   let basey = p5.map(y+2, y1, y2, 0, 256);
   let radiusbase = p5.map(x+12.9, x1, x2, 0, 256);
 //snow hole thing
-  let base2x = p5.map(x+4.9, x1, x2, 0, 256);
-  let base2y = p5.map(y+4, y1, y2, 0, 256);
-  let radiusbase2 = p5.map(x+14.9, x1, x2, 0, 256);
+  let base2x = p5.map(x+1.5, x1, x2, 0, 256);
+  let base2y = p5.map(y+3, y1, y2, 0, 256);
+  let radiusbase2 = p5.map(x+11.5, x1, x2, 0, 256);
+
+  let base3x = p5.map(x+1, x1, x2, 0, 256);
+  let base3y = p5.map(y+3.4, y1, y2, 0, 256);
+  let radiusbase3 = p5.map(x+11, x1, x2, 0, 256);
 
 
 
@@ -194,7 +129,8 @@ p5.rect(rect5x, rect5y, (radiusRect5-rect5x)/2.6,(radiusRect5-rect5x)/5);
 //zoom 2 introduces snow base around the botto of tree 
  if(zoom > 2){
 p5.fill(220);
-p5.ellipse(base2x, base2y, (radiusbase2-base2x)/1.5,(radiusbase2-base2x)/5);
+p5.rect(base2x, base2y, (radiusbase2-base2x)/1.5,(radiusbase2-base2x)/5);
+p5.rect(base3x, base3y, (radiusbase3-base3x)/1.3,(radiusbase3-base3x)/8);
 
 
 
@@ -269,111 +205,122 @@ if (zoom > 4){
   p5.rect(star3x, star3y, (radiusstar3-star3x)/10,(radiusstar3-star3x)/10);
 
 
-//light 1
-  let light1x = p5.map(x+2.9, x1, x2, 0, 256);
-  let light1y = p5.map(y-8, y1, y2, 0, 256);
-  let radiuslight1 = p5.map(x+12.9, x1, x2, 0, 256);
-
-  p5.fill(255,0,0);
-  p5.rect(light1x, light1y, (radiuslight1-light1x)/20,(radiuslight1-light1x)/20);
 
 
-  //2
-   let light3x = p5.map(x+5.6, x1, x2, 0, 256);
-  let light3y = p5.map(y-6.3, y1, y2, 0, 256);
-  let radiuslight3 = p5.map(x+15.6, x1, x2, 0, 256);
-  p5.rect(light3x, light3y, (radiuslight3-light3x)/20,(radiuslight3-light3x)/20);
+}
 
-  //3
-   let light4x = p5.map(x+4, x1, x2, 0, 256);
-  let light4y = p5.map(y-7, y1, y2, 0, 256);
-  let radiuslight4 = p5.map(x+14, x1, x2, 0, 256);
-  p5.rect(light4x, light4y, (radiuslight4-light4x)/20,(radiuslight4-light4x)/20);
-
-//light 4
-  let light2x = p5.map(x+7.2, x1, x2, 0, 256);
-  let light2y = p5.map(y-6, y1, y2, 0, 256);
-  let radiuslight2 = p5.map(x+17.2, x1, x2, 0, 256);
-  p5.rect(light2x, light2y, (radiuslight2-light2x)/20,(radiuslight2-light2x)/20);
-
-//5
-   let light5x = p5.map(x+6, x1, x2, 0, 256);
-  let light5y = p5.map(y-5, y1, y2, 0, 256);
-  let radiuslight5 = p5.map(x+16, x1, x2, 0, 256);
-  p5.rect(light5x, light5y, (radiuslight5-light5x)/20,(radiuslight5 - light5x)/20);
-
-  //6
-   let light6x = p5.map(x+4.5, x1, x2, 0, 256);
-  let light6y = p5.map(y-4.5, y1, y2, 0, 256);
-  let radiuslight6 = p5.map(x+14.5, x1, x2, 0, 256);
-  p5.rect(light6x, light6y, (radiuslight6-light6x)/20,(radiuslight6 - light6x)/20);
-
-    //7
-   let light7x = p5.map(x+3, x1, x2, 0, 256);
-  let light7y = p5.map(y-4.2, y1, y2, 0, 256);
-  let radiuslight7 = p5.map(x+13, x1, x2, 0, 256);
-  p5.rect(light7x, light7y, (radiuslight7-light7x)/20,(radiuslight7 - light7x)/20);
-
-  //8
-  let light8x = p5.map(x+1.4, x1, x2, 0, 256);
-  let light8y = p5.map(y-4, y1, y2, 0, 256);
-  let radiuslight8 = p5.map(x+11.4, x1, x2, 0, 256);
-  p5.rect(light8x, light8y, (radiuslight8-light8x)/20,(radiuslight8-light8x)/20);
-
-      //9
-   let light9x = p5.map(x+2.6, x1, x2, 0, 256);
-  let light9y = p5.map(y-3.2, y1, y2, 0, 256);
-  let radiuslight9 = p5.map(x+12.6, x1, x2, 0, 256);
-  p5.rect(light9x, light9y, (radiuslight9-light9x)/20,(radiuslight9 - light9x)/20);
-
-
-     //10
-   let light10x = p5.map(x+4.5, x1, x2, 0, 256);
-  let light10y = p5.map(y-2.6, y1, y2, 0, 256);
-  let radiuslight10 = p5.map(x+14.5, x1, x2, 0, 256);
-  p5.rect(light10x, light10y, (radiuslight10-light10x)/20,(radiuslight10 - light10x)/20);
-
-       //11
-   let light11x = p5.map(x+6.5, x1, x2, 0, 256);
-  let light11y = p5.map(y-2.2, y1, y2, 0, 256);
-  let radiuslight11 = p5.map(x+16.5, x1, x2, 0, 256);
-  p5.rect(light11x, light11y, (radiuslight11-light11x)/20,(radiuslight11 - light11x)/20);
-
-        //12
-   let light12x = p5.map(x+8.6, x1, x2, 0, 256);
-  let light12y = p5.map(y-2, y1, y2, 0, 256);
-  let radiuslight12 = p5.map(x+18.6, x1, x2, 0, 256);
-  p5.rect(light12x, light12y, (radiuslight12-light12x)/20,(radiuslight12 - light12x)/20);
-
-        //13
-   let light13x = p5.map(x+7, x1, x2, 0, 256);
-  let light13y = p5.map(y-0.7, y1, y2, 0, 256);
-  let radiuslight13 = p5.map(x+17, x1, x2, 0, 256);
-  p5.rect(light13x, light13y, (radiuslight13-light13x)/20,(radiuslight13 - light13x)/20);
-
-
-        //14
-   let light14x = p5.map(x+5, x1, x2, 0, 256);
-  let light14y = p5.map(y+0.3, y1, y2, 0, 256);
-  let radiuslight14 = p5.map(x+15, x1, x2, 0, 256);
-  p5.rect(light14x, light14y, (radiuslight14-light14x)/20,(radiuslight14 - light14x)/20);
-
-
-//15
-     let light15x = p5.map(x+2.5, x1, x2, 0, 256);
-  let light15y = p5.map(y+1, y1, y2, 0, 256);
-  let radiuslight15 = p5.map(x+12.5, x1, x2, 0, 256);
-  p5.rect(light15x, light15y, (radiuslight15-light15x)/20,(radiuslight15 - light15x)/20);
-
-         //17
-   let light17x = p5.map(x, x1, x2, 0, 256);
-  let light17y = p5.map(y+1.5, y1, y2, 0, 256);
-  let radiuslight17 = p5.map(x+10, x1, x2, 0, 256);
-  p5.rect(light17x, light17y, (radiuslight17-light17x)/20,(radiuslight17 - light17x)/20);
-	
 }
 
 
+/*
+ * This is the funciton to implement to make your own abstract design.
+ *
+ * arguments:
+ * p5: the p5.js object - all draw commands should be prefixed with this object
+ * x1, x2, y1, y2: draw the pattern contained in the rectangle x1,y1 to x2, y2
+ * z: use this as the noise z offset (can be shifted)
+ * zoom: current zoom level (starts at 0), useful to decide how much detail to draw
+ *
+ * The destination drawing should be in the square 0, 0, 255, 255.
+ */
+function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
+  let max_shift = max_thickness + max_movement;
 
+  /* For animation: updated z based on global frame count */
+  let dz = p5.globalFrameCount / 100.0;
+  z = z + dz;
 
+  /* this rectangle defines the region that will be drawn and includes a margin */
+  let min_x = snap_to_grid(x1 - max_shift, grid_size);
+  let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  let min_y = snap_to_grid(y1 - max_shift, grid_size);
+  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
+
+  // debug version: draw one
+  // let half_x = (x1 + x2) / 2;
+  // let half_y = (y1 + y2) / 2;
+  // min_x = snap_to_grid(half_x, grid_size);
+  // max_x = snap_to_grid(half_x + grid_size, grid_size);
+  // min_y = snap_to_grid(half_y, grid_size);
+  // max_y = snap_to_grid(half_y + grid_size, grid_size);
+
+  let c_p00 = p5.map(0, x1, x2, 0, 256);
+  let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+  let c_pball = p5.map(ball_radius, x1, x2, 0, 256);
+  let cur_line_width = c_plwidth - c_p00;
+  let cur_ball_radius = c_pball - c_p00;
+
+  p5.background(255);
+  for(let x=min_x; x<max_x; x+=grid_size) {
+    for(let y=min_y; y<max_y; y+=grid_size) {
+      // First compute shifted point in grid
+      let offsetX = getRandomValue(p5, x, y, z, "shiftX", -max_movement, max_movement, 0.1);
+      let offsetY = getRandomValue(p5, x, y, z, "shiftY", -max_movement, max_movement, 0.1);
+      let shifted_x = x + offsetX;
+      let shifted_y = y + offsetY;
+      let x_pos = p5.map(shifted_x, x1, x2, 0, 256);
+      let y_pos = p5.map(shifted_y, y1, y2, 0, 256);
+
+      // now compute shifted point one step to the left
+      let x_left = x + grid_size;
+      let y_left = y;
+      let offsetX_left = getRandomValue(p5, x_left, y_left, z, "shiftX", -max_movement, max_movement, 0.1);
+      let offsetY_left = getRandomValue(p5, x_left, y_left, z, "shiftY", -max_movement, max_movement, 0.1);
+      let shifted_x_left = x_left + offsetX_left;
+      let shifted_y_left = y_left + offsetY_left;
+      let x_pos_left = p5.map(shifted_x_left, x1, x2, 0, 256);
+      let y_pos_left = p5.map(shifted_y_left, y1, y2, 0, 256);
+
+      // lastly compute shifted point one step down
+      let x_down = x;
+      let y_down = y + grid_size;
+      let offsetX_down = getRandomValue(p5, x_down, y_down, z, "shiftX", -max_movement, max_movement, 0.1);
+      let offsetY_down = getRandomValue(p5, x_down, y_down, z, "shiftY", -max_movement, max_movement, 0.1);
+      let shifted_x_down = x_down + offsetX_down;
+      let shifted_y_down = y_down + offsetY_down;
+      let x_pos_down = p5.map(shifted_x_down, x1, x2, 0, 256);
+      let y_pos_down = p5.map(shifted_y_down, y1, y2, 0, 256);
+  
+      /* now draw all elements from back to front */
+      if (zoom < 2) {
+        p5.strokeWeight(cur_line_width);
+        p5.stroke(150, 0, 0);
+        //p5.line(x_pos, y_pos, x_pos_left, y_pos_left);
+        p5.stroke(0, 150, 0);
+        //p5.line(x_pos, y_pos, x_pos_down, y_pos_down);
+      }
+
+      if (zoom >= 2) {
+        p5.fill(0, 0, 255);
+        p5.noStroke();
+         }
+
+      p5.stroke(0, 0, 150);
+      p5.fill(0, 0, 128);
+      p5.noStroke();
+      //p5.ellipse(x_pos, y_pos, cur_ball_radius);
+tree(p5, x , y, x1, x2, y1, y2, z, zoom);
+
+drawLights(p5, x1, x2, y1, y2, shifted_x, shifted_y, ball_radius, 2*line_width, z);        
+     
+      if(zoom >= 3) {
+        // now if we are super zoomed, draw lines in the stamen
+        //var drawLines = false;
+        if (zoom >= 5) drawLines = true;
+        p5.fill(0, 0, 255);
+        p5.stroke(0, 0, 128);
+        ////drawStamens(p5, x1, x2, y1, y2, shifted_x, shifted_y, ball_radius/3, line_width/2, drawLines, z);
+      }
+    }
+  }
+
+  // debug - show border
+  // p5.noFill();
+  // p5.stroke(0, 200, 200)
+  // p5.strokeWeight(1);
+  // p5.rect(0, 0, 255, 255);
+  // p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
+  // let sizex = x2 - x1;
+  // p5.text("width: " + sizex, 10, 40);
 }
