47c8b3a30d0edd52eb4650437b2f70a22ad553bc
Harin Choi <chlgkfls@gmail.com>
2018-10-27 22:07:05 +1300

    added window

--- drawgrid.js
+++ drawgrid.js
@@ -2,7 +2,7 @@
 
 const max_thickness = 64;
 const line_width = 2;
-const grid_size = 256;
+const grid_size = 128;
 /*
  * This is the function to implement to make your own abstract design.
  *
@@ -33,16 +33,15 @@ var tourPath = [
 function brickWall(p5, x, y, x1, x2, y1, y2){
   let brick_x = p5.map(x, x1, x2, 0, 256);
   let brick_y = p5.map(y, y1, y2, 0, 256);
-  let rect_origin = p5.map(0, x1, x2, 0,256);
-  let rect_offset_x = p5.map(grid_size/3, x1, x2, 0,256);
-  let rect_offset_y = p5.map(grid_size/10, x1, x2, 0,256);
-  let brick_w = rect_offset_x - rect_origin;
-  let brick_h = rect_offset_y - rect_origin;
+  let brick_origin = p5.map(0, x1, x2, 0,256);
+  let brick_offset_x = p5.map(grid_size/3, x1, x2, 0,256);
+  let brick_offset_y = p5.map(grid_size/10, x1, x2, 0,256);
+  let brick_w = brick_offset_x - brick_origin;
+  let brick_h = brick_offset_y - brick_origin;
 
   var odd = true;
   var brick_start = 0;
 
-  p5.strokeWeight(2);
   p5.rect (brick_x, brick_y, brick_w, brick_h);
   p5.rect (brick_x + brick_w, brick_y,brick_w,brick_h);
   p5.rect (brick_x + brick_w*2, brick_y, brick_w, brick_h);
@@ -97,35 +96,59 @@ function brickWall(p5, x, y, x1, x2, y1, y2){
   p5.rect (brick_x + brick_w, brick_y + (brick_h * 9),brick_w, brick_h);
   p5.rect (brick_x + brick_w*2, brick_y + (brick_h * 9), brick_w, brick_h);
   p5.pop();
+}
 
+function WindowSill(p5, x, y, x1, x2, y1, y2){
+  let windowSill_x = p5.map(x, x1, x2, 0, 256);
+  let windowSill_y = p5.map(y, y1, y2, 0, 256);
+  let windowSill_origin = p5.map(0, x1, x2, 0,256);
+  let windowSill_offset_x = p5.map(grid_size, x1, x2, 0,256);
+  let windowSill_offset_y = p5.map(grid_size * 1.3, x1, x2, 0,256);
+  let windowSill_w = windowSill_offset_x - windowSill_origin;
+  let windowSill_h = windowSill_offset_y - windowSill_origin;
+  let window_sil = p5.map(grid_size/10, x1, x2, 0, 256);
 
-// //---- brick background ----//
-//   push();
-//   stroke(0);
-//   strokeWeight(stroke_basic*2);
+  // window sill
 
-//   rect(0, 0, 960, 480);
+  p5.rect(windowSill_x, windowSill_y, windowSill_w, windowSill_h); 
 
-//   strokeWeight(stroke_basic);
-//   translate(0, 0);
-  
-//   var odd = true;
-//   var start_w = 0;
+  // corner of window sill 
 
-//   for(var y_br=0; y_br<480; y_br+=brick_h){
-//     if (odd) {
-//       start_w = -brick_w/2;
-//     } else {
-//       start_w =  0;
-//     }
-//     for(var x_br=start_w; x_br<960; x_br+=brick_w){
-//     rectMode(CORNER);
-//     rect(x_br, y_br, brick_w, brick_h);
-//     }
-//     odd = !odd;
-//   }
-//   pop();
+  let corner_x1 = p5.map(x + 0, x1, x2, 0, 256); 
+  let corner_y1 = p5.map(y + grid_size * 1.3, y1, y2, 0, 256);
+  let corner_x2 = p5.map(x + grid_size/10, x1, x2, 0, 256); 
+  let corner_y2 = p5.map(y + grid_size * 1.2, y1, y2, 0, 256);
 
+  p5.line(corner_x1, corner_y1, corner_x2, corner_y2);
+}
+
+function Window(p5, x, y, x1, x2, y1, y2){
+  let window_x = p5.map(x, x1, x2, 0, 256);
+  let window_y = p5.map(y, y1, y2, 0, 256);
+  let window_origin = p5.map(0 + grid_size/10, x1, x2, 0,256);
+  let window_offset_x = p5.map(grid_size, x1, x2, 0,256);
+  let window_offset_y = p5.map(grid_size * 1.3, x1, x2, 0,256);
+  let window_w = window_offset_x - window_origin;
+  let window_h = window_offset_y - window_origin;
+  let window_sil = p5.map(grid_size/10, x1, x2, 0, 256);
+
+  p5.fill(0);
+  p5.rect(window_x, window_y, window_w, window_h); //window 
+}
+
+function Moon(p5, x, y, x1, x2, y1, y2){
+  let moon_x = p5.map(x + grid_size/ 2.5, x1, x2, 0, 256);
+  let moon_y = p5.map(y + grid_size/ 4, y1, y2, 0, 256);
+  let moon_x2 = p5.map(x + grid_size/ 2.2, x1, x2, 0, 256);
+
+  let moon_origin = p5.map(0, x1, x2, 0, 256); 
+  let moon_offset = p5.map(30, x1, x2, 0, 256);
+  let moon_radius = moon_offset - moon_origin;
+
+  p5.fill(255);
+  p5.ellipse(moon_x, moon_y, moon_radius, moon_radius);
+  p5.fill(0);
+  p5.ellipse(moon_x2, moon_y, moon_radius, moon_radius); // moon shadow
 }
 
 // The rectangles are 960x720 and centered at 512,512.
@@ -156,24 +179,33 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       // let y_pos_down = p5.map(y+grid_size, y1, y2, 0, 256);
 
       /* now draw all elements from back to front */
-      brickWall(p5, x, y, x1, x2, y1, y2);
+
+      if (zoom == 0) {
+        p5.push();
+        p5.strokeWeight(1);
+        brickWall(p5, x, y, x1, x2, y1, y2);
+        WindowSill(p5, grid_size, grid_size, x1, x2, y1, y2);
+        Window(p5, grid_size * 1.1, grid_size, x1, x2, y1, y2);
+        p5.pop();
+      }
+
+      if (zoom >= 1) {
+        p5.push();
+        p5.strokeWeight(2);
+        brickWall(p5, x, y, x1, x2, y1, y2);
+        WindowSill(p5, grid_size, grid_size, x1, x2, y1, y2);
+        Window(p5, grid_size * 1.1, grid_size, x1, x2, y1, y2);
+        Moon(p5, grid_size, grid_size, x1, x2, y1, y2);
+        p5.pop();
+      }
+      
     }
     
   }
 
-  // for(let x=min_x; x<max_x; x+=grid_size) {
-  //   for(let y=min_y; y<max_y; y+=grid_size) {
-
-  //       if (zoom > 0) {
-  //         p5.push();
-          
-  //         p5.pop();
-  //       }
-  //     }
-  //   }
 
 
-  // // debug - show border //
+  // debug - show border //
   // p5.strokeWeight(1);
   // p5.noFill();
   // p5.stroke(255, 0, 0)
