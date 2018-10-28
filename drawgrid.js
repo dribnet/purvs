9318687f984f7e671278a431519b39824044b2f7
Amy Wilson <amyy.ribbit@gmail.com>
2018-10-28 23:25:15 +1300

    added animation

--- drawgrid.js
+++ drawgrid.js
@@ -1,190 +1,111 @@
-/*
- * This is the function to implement to make your own abstract design.
- *
- * arguments:
- * p5: the p5.js object - all draw commands should be prefixed with this object
- * x1, x2, y1, y2: draw the pattern contained in the rectangle x1,y1 to x2, y2
- * z: use this as the noise z offset (can be shifted)
- * zoom: current zoom level (starts at 0), useful to decide how much detail to draw
- *
- * The destination drawing should be in the square 0, 0, 255, 255.
- */
-const max_thickness = 256;
-const grid_size = 256;
-const max_movement = 16;
-let starList;
-let randomStarValues;
+const max_thickness = 44;
+const max_movement = 50;
+const ball_radius = 32;
+const line_width = 8;
+const grid_size = 65;
+let do_animation = false;
 
 /* the random number seed for the tour */
 var tourSeed = 301;
 /* triplets of locations: zoom, x, y */
 var tourPath = [
-  [2, 512, 512],
-  [4, 512, 512],
-  [6, 512, 512]
+  [0, 1000, 512],
+  [1, 900, 512],
+  [3, 275, 1050]
 ]
 
-function layer2(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius){
-	//draw spiral
-	drawSpirals(p5, centre_x, centre_y, radius);
-	
-}
-
-
-function layer3(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius){
-	//draw planets
-	drawPlanets(p5, centre_x, centre_y, radius);
-	
-}
-
-
-function layer1(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius){
-	//draw stars
-	drawStars(p5, x, y, radius, x1, x2, y1, y2);
-	//draw lighter spiral
-	//drawSpirals(p5, centre_x, centre_y, radius);
-	//draw planets
-	//drawPlanets(p5, centre_x, centre_y, radius);
-	
-	
-}
-
-
-function drawStars(p5, x, y, radius, x1, x2, y1, y2){
-	let centre_x = p5.map(x, x1, x2, -128, 128);
-	let centre_y = p5.map(y, y1, y2, -128, 128);
-	//stars
-	starList = [];
-	//draw stars random distance from sun
-	for(i = 0; i <30; i+=3){
-		newStar = new Star(centre_x+randomStarValues[i], 
-		centre_y+randomStarValues[i+1], randomStarValues[i+2]);
-		starList[i] = newStar;
+function layer1(p5, radius, centre_x, centre_y, z){
+	let xPos = centre_x;
+	let yPos = centre_y;
+	let colour = p5.random(50, 255);
+	var starSize = p5.map(colour, 0, 255, radius/9, radius/6);
+	//draw colour behind it
+	p5.fill(p5.map(colour, 0, 255, 50, 200), 0, p5.random(colour, 255), 3);
+	p5.ellipse(xPos, yPos, starSize*5);
+	for(i = 0; i <10; i++){
+		p5.ellipse(p5.random(xPos-radius, xPos+radius/2), p5.random(yPos-radius/2, yPos+radius), starSize*9);
 	}
-	for(star = 0; star < starList.length; star+=3){
-		let s = starList[star];
-		//draw colour behind it
-		p5.fill(p5.map(s.colour, 0, 255, 50, 255), 0, p5.random(s.colour, 255), 1);
-		p5.ellipse(s.xPos, s.yPos, starSize*5);
-		for(i = 0; i <30; i++){
-			p5.ellipse(p5.random(s.xPos-2*radius, s.xPos+2*radius), p5.random(s.yPos-2*radius, s.yPos+2*radius), starSize*10);
-		}
-		p5.noStroke();
-		var starSize = p5.map(s.colour, 0, 255, radius/7, radius/3);
-		s.starThickness = starSize/8;
-		p5.fill(255, s.colour);
-		p5.beginShape();
-		p5.vertex(s.xPos-starSize, s.yPos);
-		p5.bezierVertex(s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos, s.yPos-starSize);
-		p5.bezierVertex(s.xPos+s.starThickness, s.yPos-s.starThickness, s.xPos+s.starThickness, s.yPos-s.starThickness, s.xPos+starSize, s.yPos);
-		p5.bezierVertex(s.xPos+s.starThickness, s.yPos+s.starThickness, s.xPos+s.starThickness, s.yPos+s.starThickness, s.xPos, s.yPos+starSize);
-		p5.bezierVertex(s.xPos-s.starThickness, s.yPos-s.starThickness, s.xPos-s.starThickness, s.yPos+s.starThickness, s.xPos-starSize, s.yPos);
-		p5.endShape();   
-	}
+	p5.noStroke();
+	let starThickness = starSize/8;
+	p5.fill(255, colour);
+	p5.beginShape();
+	p5.vertex(xPos-starSize, yPos);
+	p5.bezierVertex(xPos-starThickness, yPos-starThickness, xPos-starThickness, yPos-starThickness, xPos, yPos-starSize);
+	p5.bezierVertex(xPos+starThickness, yPos-starThickness, xPos+starThickness, yPos-starThickness, xPos+starSize, yPos);
+	p5.bezierVertex(xPos+starThickness, yPos+starThickness, xPos+starThickness, yPos+starThickness, xPos, yPos+starSize);
+	p5.bezierVertex(xPos-starThickness, yPos-starThickness, xPos-starThickness, yPos+starThickness, xPos-starSize, yPos);
+	p5.endShape();   
 	
 }
 
 
-function drawSpirals(p5, centre_x, centre_y, radius){
+function layer2(p5, centre_x, centre_y, radius){
+	let fifthRadius = radius/20;
+	let quarterRadius = radius/10;
+	let ellipseSize = radius/13;
 	let angle = 0;
 	let r = 0;
-	let spin = 0.04;
-	let grow = spin * radius/4;
-	for(count = 0; count <400; count+=2){
+	let spin = 0.2;
+	let grow = 0.05 * radius/4;
+	for(count = 0; count <35; count+=2){
 		angle += spin;
+		let twoAngle = angle + 2;
+		let fourAngle = angle + 4;
 		r += grow;
+		let scalar = r*3;
 		p5.noStroke();
-		p5.fill(70+p5.random(-30, 30), 0, 100+p5.random(-30, 30), p5.map(count, 0, 300, 250, 0));
-		let new_x = centre_x + p5.cos(angle+4)*r*3;
-		let new_y = centre_y + p5.sin(angle+4)*r*3;	
-		p5.ellipse(new_x, new_y, radius/5, radius/5);
-		new_x = centre_x + p5.cos(angle+2)*r*3;
-		new_y = centre_y + p5.sin(angle+2)*r*3;	
-		p5.ellipse(new_x, new_y, radius/5, radius/5);
-		new_x = centre_x + p5.cos(angle)*r*3;
-		new_y = centre_y + p5.sin(angle)*r*3;	
-		p5.ellipse(new_x, new_y, radius/5, radius/5);
-		for(i = 0; i < 2; i++){
-			let new_x = centre_x + p5.cos(angle)*r*3 + p5.random(-radius/4, radius/4);
-			let new_y = centre_y + p5.sin(angle)*r*3 + p5.random(-radius/4, radius/4);	
-			p5.ellipse(new_x, new_y, radius/7, radius/7);
-			new_x = centre_x + p5.cos(angle+4)*r*3 + p5.random(-radius/4, radius/4);
-			new_y = centre_y + p5.sin(angle+4)*r*3 + p5.random(-radius/4, radius/4);	
-			p5.ellipse(new_x, new_y, radius/7, radius/7);
-			new_x = centre_x + p5.cos(angle+2)*r*3 + p5.random(-radius/4, radius/4);
-			new_y = centre_y + p5.sin(angle+2)*r*3 + p5.random(-radius/4, radius/4);	
-			p5.ellipse(new_x, new_y, radius/7, radius/7);
+		p5.fill(70+p5.random(-10, 10), 0, 100+p5.random(-10, 10), p5.map(count, 0, 300, 50, 0));
+		let new_x = centre_x + p5.cos(fourAngle)*scalar;
+		let new_y = centre_y + p5.sin(fourAngle)*scalar;	
+		p5.ellipse(new_x, new_y, fifthRadius, fifthRadius);
+		new_x = centre_x + p5.cos(twoAngle)*scalar;
+		new_y = centre_y + p5.sin(twoAngle)*scalar;	
+		p5.ellipse(new_x, new_y, fifthRadius, fifthRadius);
+		new_x = centre_x + p5.cos(angle)*scalar;
+		new_y = centre_y + p5.sin(angle)*scalar;	
+		p5.ellipse(new_x, new_y, fifthRadius, fifthRadius);
+		for(i = 0; i < 4; i++){
+			let new_x = centre_x + p5.cos(angle)*scalar + p5.random(-quarterRadius, quarterRadius/4);
+			let new_y = centre_y + p5.sin(angle)*scalar + p5.random(-quarterRadius, quarterRadius);	
+			p5.ellipse(new_x, new_y, ellipseSize, ellipseSize);
+			new_x = centre_x + p5.cos(fourAngle)*scalar + p5.random(-quarterRadius, quarterRadius);
+			new_y = centre_y + p5.sin(fourAngle)*scalar + p5.random(-quarterRadius, quarterRadius);	
+			p5.ellipse(new_x, new_y, ellipseSize, ellipseSize);
+			new_x = centre_x + p5.cos(twoAngle)*scalar + p5.random(-quarterRadius, quarterRadius);
+			new_y = centre_y + p5.sin(twoAngle)*scalar + p5.random(-quarterRadius, quarterRadius);	
+			p5.ellipse(new_x, new_y, ellipseSize, ellipseSize);
 		}
 	}
 	//draw star
-	p5.fill(255, 255, 255, 6);
+	p5.fill(255, 255, 255, 3);
 	for(i = 0; i <20; i++){
-		p5.ellipse(centre_x, centre_y, radius-i*6, radius-i*6);
+		p5.ellipse(centre_x, centre_y, radius/3-i*4, radius/3-i*4);
 	}
 }
 
-
-function drawPlanets(p5, centre_x, centre_y, radius){
+function layer3(p5, centre_x, centre_y, radius, x, y, z){
+	let fifthRadius = radius/5;
+	let thirdRadius = radius/3;
+	let quarterRadius = radius/4;
+	let halfRadius = radius/2;
 	let angle = 0;
 	let r = 0;
 	let spin = 0.1;
 	let grow = spin * radius/4;
-	for(count = 0; count <400; count+=2){
+	for(count = 0; count <10; count+=2){
 		angle += spin;
 		r += grow;
-		p5.noStroke();
-		p5.fill(70+p5.random(-30, 30), 0, 100+p5.random(-30, 30), p5.map(count, 0, 300, 40, 0));
-		let new_x = centre_x + p5.cos(angle)*r*3 + p5.random(-radius/4, radius/4);
-		let new_y = centre_y + p5.sin(angle)*r*3 + p5.random(-radius/4, radius/4);	
-		p5.ellipse(new_x, new_y, radius/2, radius/2);
-		new_x = centre_x + p5.cos(angle+4)*r*3 + p5.random(-radius/4, radius/4);
-		new_y = centre_y + p5.sin(angle+4)*r*3 + p5.random(-radius/4, radius/4);	
-		p5.ellipse(new_x, new_y, radius/2, radius/2);
-		new_x = centre_x + p5.cos(angle+2)*r*3 + p5.random(-radius/4, radius/4);
-		new_y = centre_y + p5.sin(angle+2)*r*3 + p5.random(-radius/4, radius/4);	
-		p5.ellipse(new_x, new_y, radius/2, radius/2);
-		if((count%2)==0&&count<200){
-			//draw mini solar system
-			var starSize = radius/40;
-			var starThickness = starSize/8;
-			p5.fill(255);
-			p5.beginShape();
-			p5.vertex(new_x-starSize, new_y);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y-starThickness, new_x, new_y-starSize);
-			p5.bezierVertex(new_x+starThickness, new_y-starThickness, new_x+starThickness, new_y-starThickness, new_x+starSize, new_y);
-			p5.bezierVertex(new_x+starThickness, new_y+starThickness, new_x+starThickness, new_y+starThickness, new_x, new_y+starSize);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y+starThickness, new_x-starSize, new_y);
-			p5.endShape();   
-			new_x = centre_x + p5.cos(angle+4)*r*3 + p5.random(-radius/4, radius/4);
-			new_y = centre_y + p5.sin(angle+4)*r*3 + p5.random(-radius/4, radius/4);
-			p5.beginShape();
-			p5.vertex(new_x-starSize, new_y);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y-starThickness, new_x, new_y-starSize);
-			p5.bezierVertex(new_x+starThickness, new_y-starThickness, new_x+starThickness, new_y-starThickness, new_x+starSize, new_y);
-			p5.bezierVertex(new_x+starThickness, new_y+starThickness, new_x+starThickness, new_y+starThickness, new_x, new_y+starSize);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y+starThickness, new_x-starSize, new_y);
-			p5.endShape();   
-			new_x = centre_x + p5.cos(angle)*r*3 + p5.random(-radius/4, radius/4);
-			new_y = centre_y + p5.sin(angle)*r*3 + p5.random(-radius/4, radius/4);	
-			p5.beginShape();
-			p5.vertex(new_x-starSize, new_y);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y-starThickness, new_x, new_y-starSize);
-			p5.bezierVertex(new_x+starThickness, new_y-starThickness, new_x+starThickness, new_y-starThickness, new_x+starSize, new_y);
-			p5.bezierVertex(new_x+starThickness, new_y+starThickness, new_x+starThickness, new_y+starThickness, new_x, new_y+starSize);
-			p5.bezierVertex(new_x-starThickness, new_y-starThickness, new_x-starThickness, new_y+starThickness, new_x-starSize, new_y);
-			p5.endShape();   
+		let scalar = r*3;
+		p5.fill(255, 255, 255, 10-count);
+		for(let j = 0; j < 30; j++){
+			for(let i = 0; i < p5.radians(360); i+= p5.radians(45)){
+				let new_x = centre_x + p5.cos(angle+i)*scalar+p5.random(-fifthRadius, fifthRadius);
+				let new_y = centre_y + p5.sin(angle+i)*scalar+p5.random(-fifthRadius, fifthRadius);	
+				p5.ellipse(new_x, new_y, fifthRadius, fifthRadius);
+			}
 		}
+		
 	}
-	p5.fill(255, 255, 255, 1);
-	for(i = 0; i < 255; i++){
-		p5.ellipse(centre_x, centre_y, radius/10+i/2, radius/10+i/2);
-	}
-	p5.fill(255, 255, 255, 10);
-	for(i = 0; i < 80; i++){
-		p5.fill(255, 255, 255, 40-i*2);
-		p5.ellipse(centre_x, centre_y, radius/3+i*3, radius/3+i*3);
-	}
-	
 }
 
 
@@ -193,52 +114,67 @@ function snap_to_grid(num, gsize) {
   return (num - (num % gsize));
 }
 
-function Star(x, y, c) {
-	this.xPos = x;
-	this.yPos = y;
-	this.colour = c;
-	this.starThickness = 2;
+/* this function returns a point offset by noise at that location */
+function getOffsetPoint(p5, x, y, z, noiseScale) {
+  let offsetX = getRandomValue(p5, x, y, z, "offsetX", -max_movement, max_movement, noiseScale);
+  let offsetY = getRandomValue(p5, x, y, z, "offsetY", -max_movement, max_movement, noiseScale);
+  return [x+offsetX, y+offsetY]
 }
 
-
-// This version draws two rectangles and two ellipses.
-// The rectangles are 960x720 and centered at 512,512.
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
-	p5.background(0, 0, 5);
+  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
+  let max_shift = max_thickness + max_movement;
 
-	let max_shift = max_thickness;
+  /* For animation: updated z based on global frame count */
+  let dz = p5.globalFrameCount/2000;
+  z = z + dz;
 
-	let min_x = snap_to_grid(x1 - max_shift, grid_size);
-  	let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
-  	let min_y = snap_to_grid(y1 - max_shift, grid_size);
-  	let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
+  /* this rectangle defines the region that will be drawn and includes a margin */
+  let min_x = snap_to_grid(x1 - max_shift, grid_size);
+  let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  let min_y = snap_to_grid(y1 - max_shift, grid_size);
+  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
 
-	let radius = p5.map(20, x1, x2, 0, 256) - p5.map(0, x1, x2, 0, 256);
-	//get random values to draw stars
-	//stars
-	randomStarValues = [];
-	//draw stars random distance from sun
-	for(i = 0; i <30; i+=3){
-		randomStarValues[i] = p5.random(-5*radius,5*radius);
-		randomStarValues[i+1] = p5.random(-5*radius, 5*radius);
-		randomStarValues[i+2] = p5.random(0, 255);
-	}
-	
-	
-	for(let x=min_x; x<max_x; x+=grid_size) {
-    	for(let y=min_y; y<max_y; y+=grid_size) {
-			let centre_x = p5.map(x, x1, x2, 0, 256);
-			let centre_y = p5.map(y, y1, y2, 0, 256);
+  let c_p00 = p5.map(0, x1, x2, 0, 256);
+  let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+  let c_pball = p5.map(ball_radius, x1, x2, 0, 256);
+  let cur_line_width = c_plwidth - c_p00;
+  let cur_ball_radius = c_pball - c_p00;
 
-			if(zoom <1){
-				layer1(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius);
-			}
-			else if(zoom < 3){
-				layer2(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius);
-			}
-			else{
-				layer3(p5, x,y, x1, x2, y1, y2, centre_x, centre_y, radius);
-			}
-		}		
-	}
+  p5.background(0, 0, 5);
+  //stars
+  randomStarValues = [];
+  //draw stars random distance from sun
+  for(i = 0; i <30; i+=3){
+	randomStarValues[i] = p5.random(-5*cur_ball_radius,5*cur_ball_radius);
+	randomStarValues[i+1] = p5.random(-5*cur_ball_radius, 5*cur_ball_radius);
+	randomStarValues[i+2] = p5.random(0, 255);
+  }
+  for(let x=min_x; x<max_x; x+=grid_size) {
+    for(let y=min_y; y<max_y; y+=grid_size) {
+      // First compute shifted point in grid
+      let offsetX = getRandomValue(p5, x, y, z, "shiftX", -max_movement, max_movement, 0.1);
+      let offsetY = getRandomValue(p5, x, y, z, "shiftY", -max_movement, max_movement, 0.1);
+      let shifted_x = x + offsetX;
+      let shifted_y = y + offsetY;
+      let x_pos = p5.map(shifted_x, x1, x2, 0, 256);
+      let y_pos = p5.map(shifted_y, y1, y2, 0, 256);
+
+      /* now draw all elements from back to front */
+      p5.fill(0, 0, 255);
+      p5.noStroke();  
+	  if(zoom<1){
+		  layer1(p5, cur_ball_radius, x_pos, y_pos, z);
+	  }
+	  else if(zoom<3){
+		  do_animation = true;
+		  layer2(p5, x_pos, y_pos, cur_ball_radius);  
+	  }
+	  else if((x%4)==0&&(y%4)==0){
+		  do_animation = false;
+		  layer3(p5, x_pos, y_pos, cur_ball_radius, x, y, z);  
+	  }
+    }
+  }
+
 }
