6a1b12b935490d1da7fdc3651c21213e3062185c
changheon1 <changheon1@hotmail.com>
2018-10-28 14:11:30 +1300

    part 11~16 now

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## PS4 MDDN 342 2018
 
-After the zoom 5 I have created some text saying follow me, so it sounds like something is calling you from the monitor.
-I will still make the user to follow along the middle point which has an empty ellipse at the moment but I will keep adding things in here just to make things more interesting. 
-
-Next step would be adding details to the middle point, might make a ghost's back or something like that.
+Nothing really I could think of and not enough time to spend more time on the middle part 5~10.
+after the 10th zoom I will make the background black(opposite style of how it went previously).
+Currently thinking of something that would be interesting to see at the end.
--- drawgrid.js
+++ drawgrid.js
@@ -30,13 +30,25 @@ function snap_to_grid(num, gsize){
   return (num - (num % gsize));
 }
 function myCircle(p5, x, y, x1, x2, y1, y2, posx, posy, rad1, rad2){
-  let pos1 = p5.map(posx, x, x1, 0 , 256);
-  let pos2 = p5.map(posx+ rad2, x, x1, 0 , 256);
-  let radius = pos2 - pos1;
-  p5.fill(0);
-  p5.ellipse(pos1, pos2, radius);
+  const sqrt2 = 1.4142/2;
+  let offsets = [
+    [sqrt2, sqrt2],
+    [-sqrt2, sqrt2],
+    [-sqrt2, -sqrt2],
+    [sqrt2, -sqrt2]
+  ]
+  let pixel_posx1 = p5.map(posx, x1, x2, 0, 256);
+  let pixel_posx2 = p5.map(posx+rad2, x1, x2, 0, 256);
+  let pixel_radius = pixel_posx2 - pixel_posx1;
+  for(let i=0; i<offsets.length; i++) {
+    let offset = offsets[i];
+    let pixel_x = p5.map(posx+0.5*rad1*offset[0], x1, x2, 0, 256);
+    let pixel_y = p5.map(posy+0.5*rad1*offset[1], y1, y2, 0, 256);
+    p5.ellipse(pixel_x, pixel_y, pixel_radius);    
+  }
 }
-function myRects(p5, x1, x2, y1, y2, posx, posy, size1 ,size2){
+function makeTexts(p5, x1, x2, y1, y2, pos_x, pos_y, rad1, rad2){
+
 }
 
 // This version draws two rectangles and two ellipses.
@@ -44,36 +56,13 @@ function myRects(p5, x1, x2, y1, y2, posx, posy, size1 ,size2){
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   // debug - show border
 // temporary variables used for object placement
-  let cx1=[], cy1=[], cx2=[], cy2=[];
-  p5.background(255);
+  if(zoom < 11){
+    p5.background(255);
+  }else{
+    p5.background(0);
+  }
   p5.rectMode(p5.CORNER);
 
-  // if(zoom == 0){
-  //   cx1[0] = x1; cy1[0] = y1; cx2[0] = x2; cy2[0] = y2;
-  // }
-  // if(zoom == 1){
-  //   cx1[1] = x1; cy1[1] = y1; cx2[1] = x2; cy2[1] = y2;
-  // }
-  // if(zoom == 2){
-  //   cx1[2] = x1; cy1[2] = y1; cx2[2] = x2; cy2[2] = y2;
-  // }
-  // if(zoom == 3){
-  //   cx1[3] = x1; cy1[3] = y1; cx2[3] = x2; cy2[3] = y2;
-  // }
-  // if(zoom == 4){
-  //   cx1[4] = x1; cy1[4] = y1; cx2[4] = x2; cy2[4] = y2;
-  // }
-  // if(zoom == 5){
-  //   cx1[5] = x1; cy1[5] = y1; cx2[5] = x2; cy2[5] = y2;
-  // }
-
-// example ellipse
-  // for(let i = 0 ; i < 10; i++){
-  // 	let shade = 128 + 128 / (i+1);
-  // 	let current_r = p5.map(i, 0, 9, local_ball_r, 0);
-  // 	p5.fill(0,0,shade);
-  // 	p5.ellipse(local_ballx, local_bally, current_r);
-  // }
   let local_rectx = p5.map(rectx, x1, x2, 0, 256);
   let local_recty = p5.map(recty, y1, y2, 0, 256);
   let local_rectx_edge = p5.map((rectx + rect_size), x1, x2, 0, 256);
@@ -82,6 +71,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   let miny = snap_to_grid(y1 - 50, grid_size);
   let maxy = snap_to_grid(y2 + 50, grid_size);
   let local_rect_size = local_rectx_edge - local_rectx;
+  let ghost_size = 120;
   for(let x = minx; x < maxx; x+=grid_size){
     for(let y = miny; y < maxy; y+= grid_size){
       let xpos = p5.map(x, x1, x2, 0 , 256);
@@ -130,9 +120,6 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
         p5.rect(xpos, ypos, local_rect_size, local_rect_size);
         p5.fill(0,255,0);
         p5.ellipse(xpos+local_rect_size-5, ypos+local_rect_size/2, 5);
-      }else if(zoom == 5){
-        p5.noFill(0,0,0);
-        // p5.rect(xpos,ypos,local_rect_size,local_rect_size);
       }
 
 
@@ -141,11 +128,12 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
   //5~11 text 'follow me'
   if(zoom >= 5 && zoom < 11){
     p5.noFill();
-    p5.stroke(255, 0, 0)
+    p5.stroke(255, 0, 0);
     p5.strokeWeight(1);
+    let opac = 180;
     let ran_value1 = p5.random(local_rect_size);
     let ran_value2 = p5.random(local_rect_size);
-
+    
     if(zoom > 6){
       p5.strokeWeight(3);
       for(var i = 0; i < zoom; i ++){
@@ -159,50 +147,22 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
       p5.text("follow me", ran_value1, ran_value1);
       p5.text("follow me", ran_value2, ran_value2);
     }
+    p5.strokeWeight(1);
+    p5.noStroke();
+
+    p5.fill(167,243,243,opac);
+    
+    p5.ellipse(local_rectx,local_recty,ghost_size,ghost_size);
   }
-  p5.strokeWeight(1);
-  p5.ellipse(local_rectx,local_recty,30,30);
+  // myCircle(p5,x1,x2,y1,y2,local_rectx, local_recty, 120, 120);
+  p5.noFill();
+  p5.stroke(255);
+  p5.ellipse(local_rectx,local_recty,ghost_size,ghost_size);
 
-
-  // if(zoom >= 5 && zoom <= 10){
-  //   let tempx1, tempx2, tempy1, tempy2;
-  //   if(zoom == 5){
-  //     tempx1 = cx1[0]; tempx2 = cx2[0]; tempy1 = cy1[0]; tempy2 = cy2[0];
-  //   }else if(zoom == 6){
-  //     tempx1 = cx1[1]; tempx2 = cx2[1]; tempy1 = cy1[1]; tempy2 = cy2[1];
-  //   }else if(zoom == 7){
-  //     tempx1 = cx1[2]; tempx2 = cx2[2]; tempy1 = cy1[2]; tempy2 = cy2[2];
-  //   }else if(zoom == 8){
-  //     tempx1 = cx1[3]; tempx2 = cx2[3]; tempy1 = cy1[3]; tempy2 = cy2[3];
-  //   }else if(zoom == 9){
-  //     tempx1 = cx1[4]; tempx2 = cx2[4]; tempy1 = cy1[4]; tempy2 = cy2[4];
-  //   }
-  //   let local_rectx = p5.map(rectx, tempx1, tempx2, 0, 256);
-  //   let local_recty = p5.map(recty, tempy1, tempy2, 0, 256);
-  //   let local_rectx_edge = p5.map((rectx + rect_size), tempx1, tempx2, 0, 256);
-  //   let minx = snap_to_grid(tempx1 - 50, grid_size);
-  //   let maxx = snap_to_grid(tempx2 + 50, grid_size);
-  //   let miny = snap_to_grid(tempy1 - 50, grid_size);
-  //   let maxy = snap_to_grid(tempy2 + 50, grid_size);
-  //   let local_rect_size = local_rectx_edge - local_rectx;
-  //   for(let x = minx; x < maxx; x+=grid_size){
-  //     for(let y = miny; y < maxy; y+= grid_size){
-  //       let xpos = p5.map(x, tempx1, tempx2, 0 , 256);
-  //       let ypos = p5.map(y, tempy1, tempy2, 0 , 256);
-  //       if(zoom == 6){
-
-  //       }
-  //     }
-  //   }
-  // }
+  
   p5.strokeWeight(3);
   p5.stroke(0,255,0);
-  // // The second green ellipse is above and to the left of the first one.
-  // cx = p5.map(412, x1, x2, 0, 256);
-  // cy = p5.map(412, y1, y2, 0, 256);
-  // cx2 = p5.map(412+50, x1, x2, 0, 256);
-  // p5.fill(0, 255, 0);
-  // p5.ellipse(cx, cy, (cx2-cx));
+  
 
   // debug - show border
   // p5.noFill();
