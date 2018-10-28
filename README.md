783c6425b8c39c5ccae85d960f4cb10335ba80cd
Tyler Maddren <tylermaddren@gmail.com>
2018-10-29 01:27:09 +1300

    FINAL

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 ## PS4 MDDN 342 2018
 
 
+FINAL: 
+The final design I have created is to magnify into the water of a Koi Pond the zoom
+layers explore what the water contains and then explores the water molecule H2O. 
+
+Zoom 0 - Koi Pond w/swimming fish
+Zoom 1 - Zoomed in view of the fish moving as a pack through the pond.
+Zoom 2 - Water Particles begin to emerge, moving in a random way.
+Zoom 3 - Water molecule displayed as a graphic 
+Zoom 4 - Displays the Water molecule as H2O. 
+
+Review: Overall I am happy with the design I have produced. I found it very difficult to 
+set the parameters of the grid and remapping each zoom function so I could draw outside 
+of the grid. However, I managed to work around most of my issues and managed to get a working
+final design.   
+
+
 FOURTH:
 The boat design didn't turn out how I would have liked it to, so I have gone with the same
 concept but made a Koi fish pond instead. This design has allowed me to add randomness 
--- drawgrid.js
+++ drawgrid.js
@@ -18,33 +18,21 @@ var ydirection = 1; // Top to Bottom
 
 let do_animation = true;
 
-
-
-
-
-
 /* the random number seed for the tour */
 var tourSeed = 301;
+
 /* triplets of locations: zoom, x, y */
+
 var tourPath = [
-  [1, 356.500000000000, 665.750000000000],
-  [3, 353.250000000000, 668.187500000000],
-  [4, 322.562500000000, 645.093750000000],
-  [5, 322.562500000000, 645.109375000000],
-  [7, 317.984375000000, 643.636718750000],
-  [3, 317.984375000000, 643.636718750000]
+  [0, 618.824218750000, 646.460937500000],
+  [1, 1020.937500000000, 236.453125000000],
+  [2, 471.968750000000, 524.976562500000],
+  [3, 484.984375000000, 531.988281250000],
+  [4, 454.641113281250, 389.649169921875]
+  
 ]
 
-// function displayPictures(){
-//   one = p5.loadImage("assets/water.png", function() {
-//     p5.image(one,0,0); 
-//   });
-// }
-/* this function takes a coordinate and aligns to a grid of size gsize */
-
 function setup(){
-	//fish = new p5.Vector(0, 0);
-	//movement = new p5.Vector(random(1), random(1));
 	for (var i = 0; i < 10; i++){
 		fish[i] = new p5.Vector(0, 0);
 		movement[i] = new p5.Vector(random(1), random(1));
@@ -58,7 +46,6 @@ function setup(){
 
 }
 
-
 function move(fish, movement, x, y){
 	var newX = fish.x + movement.x;
 	var newY = fish.y + movement.y;
@@ -68,7 +55,19 @@ function move(fish, movement, x, y){
 	if (newX>=200||newX<=10){
 		movement.x*=-1;
 	}
-	//console.log(newX);
+	var newFish = new p5.Vector(newX, newY);
+	return newFish;
+}
+
+function move2(fish, movement, x, y){
+	var newX = fish.x + movement.x;
+	var newY = fish.y + movement.y;
+	if (newY>=155||newY<=10){
+		movement.y*=-1;
+	}
+	if (newX>=200||newX<=10){
+		movement.x*=-1;
+	}
 	var newFish = new p5.Vector(newX, newY);
 	return newFish;
 }
@@ -100,53 +99,25 @@ function drawFish(fish, p5, x, y, x1, x2, y1, y2, z, zoom){
 	}
 }
 
- 
-
 
 function firstZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
 
-	  let max_shift = max_thickness*2 + max_movement*2;
 
 	/* For animation: updated z based on global frame count */
   let dz = p5.globalFrameCount / 100.0;
   z = z + dz;
 
-	p5.background(0, 255, 0)
-	  //set x,y
+	p5.background(48, 132, 62)
+//set x,y
   let start_x = p5.map(x + -380, x1, x2, 0, 256);
   let width = p5.map(x + 480, x1, x2, 0, 256);
   
-  let bezX1 = p5.map(x - 100, x1, x2, 0, 256);
-  let bezY1 = p5.map(y + 100, x1, x2, 0, 256);
 
-  let bezX2 = p5.map(x, x1, x2, 0, 256);
-  let bezY2 = p5.map(y + 180, x1, x2, 0, 256);
-
-  let bezX3 = p5.map(x + 170, x1, x2, 0, 256);
-  let bezY3 = p5.map(y + 70, x1, x2, 0, 256);
-
-  let bezX4 = p5.map(x + 300, x1, x2, 0, 256);
-  let bezY4 = p5.map(y, x1, x2, 0, 256);
-
-  let bezX5 = p5.map(x + 100, x1, x2, 0, 256);
-  let bezY5 = p5.map(y - 280, x1, x2, 0, 256);
-
-  let bezX6 = p5.map(x - 300, x1, x2, 0, 256);
-  let bezY6 = p5.map(y - 190, x1, x2, 0, 256);
-
-
-  let cover_x2 = p5.map(x + 100, x1, x2, 0, 256);
-  let cover_x3 = p5.map(x + 120 , x1, x2, 0, 256);
-  let cover_x4 = p5.map(x + 210, x1, x2, 0, 256);
-  
+ 
   let start_y = p5.map(y + -330, y1, y2, 0, 256);
   let height = p5.map(y + 220, y1, y2, 0, 256);
   
-  let cover_y2 = p5.map(y + -100 , y1, y2, 0, 256);
-  let cover_y3 = p5.map(y + 200 , y1, y2, 0, 256);
-  let cover_y4 = p5.map(y+ -80  , y1, y2, 0, 256);
-  let cover_y5 = p5.map(y+ -210  , y1, y2, 0, 256);
-  	
+
   	var mid_x = start_x + ((width-start_x)/2);
   	
   	
@@ -155,7 +126,7 @@ function firstZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
   	var stand = 0;
 
 
-	p5.fill(0,0,190);
+	p5.fill(48, 131, 188);
 	p5.beginShape();
 	p5.curveVertex(mid_x -70*4, mid_y +20*4);
 	p5.curveVertex(mid_x, mid_y);
@@ -203,7 +174,6 @@ function firstZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
     ydirection *= -1;
   }
 
-
   p5.fill(255, 0, 0);
   // Draw the shape
   if(ypos<mid_y+150&&ypos>mid_y-100&&xpos<mid_x+230 && xpos>mid_x-210){
@@ -212,6 +182,7 @@ function firstZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
   		fish[i].x = random(mid_x-100, mid_x-200);
   		fish[i].y = random(mid_y+100, mid_y+200);
   	}
+
 
 
   	
@@ -235,128 +206,69 @@ function firstZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
 		p5.pop();  
   }
 }
-  
-
-
-
 }
 
 
 
-//   function drawBG(p5, x1, x2, y1, y2, z, zoom) {
+function secondZoom(p5, x, y, x1, x2, y1, y2, z){
 
-//   }
-// console.log("zoom is" + zoom);
+	p5.background(48, 131, 188);
+	  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
 
-//   //debug - show border
-//   // p5.noFill();
-//   // p5.stroke(0, 200, 200)
-//   // p5.strokeWeight(1);
-//   // p5.rect(0, 0, 255, 255);
-//   // p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
-//   // let sizex = x2 - x1;
-//   // p5.text("width: " + sizex, 10, 40);
-// }
+  let dz = p5.globalFrameCount / 100.0;
+  z = z + dz;
 
-function secondZoom(p5, x, y, x1, x2, y1, y2){
+  let start_x = p5.map(x + -256, x1, x2, 0, 32);
+  let width = p5.map(x + 768, x1, x2, 0, 32);
+  let start_y = p5.map(y + -384, y1, y2, 0, 32);
+  let height = p5.map(y + 640, y1, y2, 0, 32);
+  var mid_x = start_x + ((width-start_x)/2);	
+  var mid_y = start_y + ((height-start_y)/2);
 
-	p5.background(0, 255, 0)
+	// Update the position of the shape
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
 
-	  //set x,y
-  let start_x = p5.map(x + -280, x1, x2, 0, 256);
-  let width = p5.map(x + 480, x1, x2, 0, 256);
-  
-  let bezX1 = p5.map(x - 100, x1, x2, 0, 256);
-  let bezY1 = p5.map(y + 100, x1, x2, 0, 256);
+  // Test to see if the shape exceeds the boundaries of the screen
+  // If it does, reverse its direction by multiplying by -1
+  if (xpos > start_x - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > start_y - rad || ypos < rad) {
+    ydirection *= -1;
+  }
 
-  let bezX2 = p5.map(x, x1, x2, 0, 256);
-  let bezY2 = p5.map(y + 180, x1, x2, 0, 256);
-
-  let bezX3 = p5.map(x + 170, x1, x2, 0, 256);
-  let bezY3 = p5.map(y + 70, x1, x2, 0, 256);
-
-  let bezX4 = p5.map(x + 300, x1, x2, 0, 256);
-  let bezY4 = p5.map(y, x1, x2, 0, 256);
-
-  let bezX5 = p5.map(x + 100, x1, x2, 0, 256);
-  let bezY5 = p5.map(y - 280, x1, x2, 0, 256);
-
-  let bezX6 = p5.map(x - 300, x1, x2, 0, 256);
-  let bezY6 = p5.map(y - 190, x1, x2, 0, 256);
+  p5.fill(255, 0, 0);
+  // Draw the shape
+  if(ypos<mid_y+250&&ypos>mid_y-200&&xpos<mid_x+430 && xpos>mid_x-410){
+  for (var i = 0; i < 1; i++){
+  	if(fish[i].x==0 && fish[i].y==0){
+  		fish[i].x = random(mid_x-100, mid_x-200);
+  		fish[i].y = random(mid_y+100, mid_y+200);
+  	}
 
 
-  let cover_x2 = p5.map(x + 100, x1, x2, 0, 256);
-  let cover_x3 = p5.map(x + 120 , x1, x2, 0, 256);
-  let cover_x4 = p5.map(x + 210, x1, x2, 0, 256);
-  
-  let start_y = p5.map(y + -230, y1, y2, 0, 256);
-  let height = p5.map(y + 220, y1, y2, 0, 256);
-  
-  let cover_y2 = p5.map(y + -100 , y1, y2, 0, 256);
-  let cover_y3 = p5.map(y + 200 , y1, y2, 0, 256);
-  let cover_y4 = p5.map(y+ -80  , y1, y2, 0, 256);
-  let cover_y5 = p5.map(y+ -210  , y1, y2, 0, 256);
   	
-  	var mid_x = start_x + ((width-start_x)/2);
-  	
-  	
-  	var mid_y = start_y + ((height-start_y)/2);
-
-  	var stand = 0;
-
-
-	p5.fill(0,0,190);
-	p5.beginShape();
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.curveVertex(mid_x +110*4, mid_y +100*4);
-	p5.curveVertex(mid_x +50*4, mid_y +85*4);
-	p5.curveVertex(mid_x, mid_y +100*4);
-	p5.curveVertex(mid_x -50*4, mid_y +70*4);
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.endShape();
-
-	//p5.fill(169, 169, 169);
-	p5.strokeWeight(10);
-	p5.stroke(169, 169, 169);
-
-	p5.beginShape();
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.curveVertex(mid_x +110*4, mid_y +100*4);
-	p5.curveVertex(mid_x +50*4, mid_y +85*4);
-	p5.curveVertex(mid_x, mid_y +100 *4);
-	p5.curveVertex(mid_x -50*4, mid_y +70*4);
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.endShape();
-
-	p5.beginShape();
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.curveVertex(mid_x +110*4, mid_y +100*4);
-	p5.curveVertex(mid_x +50*4, mid_y +85*4);
-	p5.curveVertex(mid_x, mid_y +100 *4);
-	p5.curveVertex(mid_x -50*4, mid_y +70*4);
-	p5.curveVertex(mid_x -70*4, mid_y +20*4);
-	p5.curveVertex(mid_x, mid_y);
-	p5.curveVertex(mid_x +100*4, mid_y);
-	p5.curveVertex(mid_x+120*4, mid_y +70*4);
-	p5.endShape();
-
-
-   
+  		fish[i] = move2(fish[i], movement[i], mid_x, mid_y);
+  		p5.noStroke();
+		p5.push();
+		p5.noStroke();
+		p5.fill(210, 0, 10);
+		p5.translate(mid_x, -mid_y);
+		p5.rotate(movement[i].x, movement[i].y);
+		p5.beginShape();
+		var headSize = 100;
+		p5.curveVertex(0, -0.5 * headSize);
+		p5.curveVertex(0.2 * headSize, 0);
+		p5.curveVertex(0, 1.5 * headSize);
+		p5.curveVertex(-0.2 * headSize, 0);
+		p5.curveVertex(0, -0.5 * headSize);
+		p5.curveVertex(0.2 * headSize, 0);
+		p5.curveVertex(0, 1.5 * headSize);
+		p5.endShape();
+		p5.pop();  
+  }
+}
 }
 
 function thirdZoom(p5, x, y, x1, x2, y1, y2, z ,zoom){
@@ -399,19 +311,19 @@ function thirdZoom(p5, x, y, x1, x2, y1, y2, z ,zoom){
       var dX;
       var dY; 
 
-      dX = x_pos +1;
-      dy = y_pos +1;
+      dX = x_pos +10;
+      dy = y_pos +10;
 
       p5.stroke(255, 0, 0);
-      p5.fill(0, 0, 128);
+      p5.fill(48, 131, 188);
       p5.stroke(255);
+      p5.strokeWeight(5);
       p5.ellipse(x_pos, y_pos, cur_ball_radius*3);
 
     }
   }
  }
       
-
 function fourthZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
 
 	  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
@@ -452,17 +364,63 @@ function fourthZoom(p5, x, y, x1, x2, y1, y2, z, zoom){
 		p5.fill(100);
         p5.ellipse(x_pos-100, y_pos+100, 200);
         p5.ellipse(x_pos+100, y_pos+100, 200);    
-        p5.fill(0, 0, 255);
+        p5.fill(48, 131, 188);
         p5.stroke(255);
         p5.ellipse(x_pos, y_pos, cur_ball_radius);
     }
 }
 }
 
+function lastZoom(p5, x, y, x1, x2, y1, y2, z){
 
-function lastZoom(p5, x, y, x1, x2, y1, y2){
+	  /* max_shift is the amount of overlap a tile can spill over into its neighbors */
+  let max_shift = max_thickness + max_movement;
+
+  let dz = p5.globalFrameCount / 100.0;
+  z = z + dz;
+  /* this rectangle defines the region that will be drawn and includes a margin */
+  let min_x = snap_to_grid(x1 - max_shift, grid_size);
+  let max_x = snap_to_grid(x2 + max_shift + grid_size, grid_size);
+  let min_y = snap_to_grid(y1 - max_shift, grid_size);
+  let max_y = snap_to_grid(y2 + max_shift + grid_size, grid_size);
 
 
+  let c_p00 = p5.map(0, x1, x2, 0, 256);
+  let c_plwidth = p5.map(line_width, x1, x2, 0, 256);
+  let c_pball = p5.map(ball_radius, x1, x2, 0, 256);
+  let cur_line_width = c_plwidth - c_p00;
+  let cur_ball_radius = c_pball - c_p00;
+
+  p5.background(255);
+  for(let x=min_x; x<max_x; x+=grid_size) {
+    for(let y=min_y; y<max_y; y+=grid_size) {
+      /* first compute all three points with offsets */
+      let shift_point = getOffsetPoint(p5, x, y, z, 0.1);
+      let x_pos = p5.map(shift_point[0], x1, x2, 0, 256);
+      let y_pos = p5.map(shift_point[1], y1, y2, 0, 256);
+
+      let shift_point_left = getOffsetPoint(p5, x+grid_size, y, z, 0.1);
+      let x_pos_left = p5.map(shift_point_left[0], x1, x2, 0, 256);
+      let y_pos_left = p5.map(shift_point_left[1], y1, y2, 0, 256);
+
+      let shift_point_down = getOffsetPoint(p5, x, y+grid_size, z, 0.1);
+      let x_pos_down = p5.map(shift_point_down[0], x1, x2, 0, 256);
+      let y_pos_down = p5.map(shift_point_down[1], y1, y2, 0, 256);
+
+	
+		p5.fill(100);
+		p5.textSize(300);
+
+        p5.text("H", x_pos-300, y_pos+300);
+        p5.text("H", x_pos+300, y_pos+300);    
+        p5.fill(48, 131, 188);
+        p5.stroke(255);
+
+        
+        p5.fill(48, 131, 188);
+  		p5.text("O", x_pos, y_pos);
+    }
+}
 }
 
 function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
@@ -471,11 +429,8 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
 	count++;
     
     if (zoom < 1){
-
     	p5.background(255);
-    	//drawBG(p5, x1, x2, x1, x2, y1, y2, z, zoom);
       	firstZoom(p5, 412, 512, x1, x2, y1, y2);
-      	//draw(p5, x1, x2, x1, x2, y1, y2, z, zoom);
     }
     if(zoom > 0){
     	p5.background(255);
@@ -491,7 +446,7 @@ function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
     }
     if (zoom > 3){
     	p5.background(255);
-		lastZoom(p5, 412, 512, x1, x2, y1, y2, zoom);
+		lastZoom(p5, 412, 512, x1, x2, y1, y2, z);
     }
 }
 
@@ -509,34 +464,7 @@ class Fish {
 
 	function drawFish(p5, x, y, x1, x2, y1, y2, z, zoom){
 
-		
-		
 	}
 
+	}
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // debug - show border
-  // p5.noFill();
-  // p5.stroke(0, 200, 200)
-  // p5.strokeWeight(1);
-  // p5.rect(0, 0, 255, 255);
-  // p5.text("corner: (" + x1 + "," + y1 + ")", 10, 20);
-  // let sizex = x2 - x1;
-  // p5.text("width: " + sizex, 10, 40);
-
-
-
