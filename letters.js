833219318bc985a6644769f3ae1c91b91c1f52ce
university of wellington creative coding <43123953+uwcc@users.noreply.github.com>
2019-03-25 03:25:19 +0000

    Initial version of PS2 alphabet starting code

---
+++ letters.js
@@ -0,0 +1,22 @@
+const alphabet = {
+  "default": {
+    "size": 40,
+    "offsetx": 0,
+    "offsety": 0
+  },
+  "A": {
+    "size": 40,
+    "offsetx": 0,
+    "offsety": 17
+  },
+  "B": {
+    "size": 75,
+    "offsetx": 0,
+    "offsety": -70
+  },
+  "C": {
+    "size": 50,
+    "offsetx": 15,
+    "offsety": 0
+  }
+}
