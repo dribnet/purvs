416379a21e8255fdb9870292af7c42ec24b90af8
Caitlin Sue <caitlinsue06@gmail.com>
2019-03-26 15:38:13 +1300

    Updated readme, sketch.js and images

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 ## PS2 MDDN 242 2019
 
-Changed some of the colours to get a feel for the new code.
+Started with my new code idea which is to use lines to represent the letters. 
 
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+I drew out all the letters on paper and ran into some problems. When I draw a lettter like C there needs to be a gap inbetween the lines to show the open part of the C so that it doesn't look like an O. Hazel suggested creating a shape that will match the background colour and go over the lines to give it the illusion of space inbetween.
+
+I have also decided that there will be 4 lines drawn for each letter and there will be an even space inbetween each line.
+
+The eight parameters per letter:
+  * `Yshift1` : top of line position for first line
+  * `Yshift2` : top of line position for second line
+  * `Yshift3` : top of line position for third line
+  * `Yshift4` : top of line position for fourth line  
+  
+
+  * `length1` : length/ bottom of line position for first line
+  * `length2` : length/ bottom of line position for second line
+  * `length3` : length/ bottom of line position for third line
+  * `length4` : length/ bottom of line position for fourth line
+ 
 
--- sketch.js
+++ sketch.js
@@ -13,27 +13,47 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offsetX1": 85,
+  "offsetX2": 80,
+  "offsetX3": 85,
+  "offsetX4": 75,
+ 
+  "offsetY1": 20,
+  "offsetY2": 30,
+  "offsetY3": 20,
+  "offsetY4": 50,
+
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "offsetX1": 65,
+  "offsetX2": 85,
+  "offsetX3": 90,
+  "offsetX4": 95,
+ 
+  "offsetY1": 50,
+  "offsetY2": 25,
+  "offsetY3": 15,
+  "offsetY4": 5,  
+
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "offsetX1": 85,
+  "offsetX2": 80,
+  "offsetX3": 75,
+  "offsetX4": 80,
+ 
+  "offsetY1": 50,
+  "offsetY2": 55,
+  "offsetY3": 65,
+  "offsetY4": 55,
+ 
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#FF0000";
 const colorBack    = "#FFD745";
 const colorStroke  = "#FFFF00";
+const lineSpacing = 10;
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -49,16 +69,22 @@ function setup () {
 }
 
 function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  // determine parameters for lines
+  let Yshift1 = letterData["offsetX1"];
+  let Yshift2 = letterData["offsetX2"];
+  let Yshift3 = letterData["offsetX3"];
+  let Yshift4 = letterData["offsetX4"];
+ 
+  let length1 = letterData["offsetY1"];
+  let length2 = letterData["offsetY2"];
+  let length3 = letterData["offsetY3"];
+  let length4 = letterData["offsetY4"]; 
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  // draw four lines
+  line(posx + lineSpacing, posy + Yshift1, posx + lineSpacing, posy + length1 + Yshift1);
+  line(posx + lineSpacing*2, posy + Yshift2, posx + lineSpacing*2, posy + length2 + Yshift2);
+  line(posx + lineSpacing*3, posy + Yshift3, posx + lineSpacing*3, posy + length3 + Yshift3);
+  line(posx + lineSpacing*4, posy + Yshift4, posx + lineSpacing*4, posy + length4 + Yshift4);
 }
 
 function draw () {
