4f60f84a247b40ba2d033c83f899700fe766ee33
Wenlin Zhang <lynnetezh@gmail.com>
2019-03-26 14:20:28 +1300

    test

--- sketch.js
+++ sketch.js
@@ -12,16 +12,18 @@ const canvasHeight = 500;
  *
  */
 
+ //angleMode(DEGREES);
+
 const letterA = {
   "size": 80,
   "offsetx": 0,
-  "offsety": 35
+  "offsety": -135
 }
 
-const letterB = {
+const letterB = {  
   "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "offsetx":0,
+  "offsety": -45
 }
 
 const letterC = {
@@ -30,9 +32,9 @@ const letterC = {
   "offsety": 0
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
+const colorFront1  = "#052342";
+const colorFront2  = "#0c427a";
+const colorBack    = (30);
 const colorStroke  = "#233f11";
 
 function setup () {
@@ -50,15 +52,20 @@ function setup () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
+  noStroke();
   let size2 = letterData["size"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let pos3x = pos2x + letterData["offsety"];
+  let pos3y = pos2y + letterData["offsety"];
 
   // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  fill(255);
+  triangle(posx,posy-200,posx-100,posy + 100,posx+100,posy+100);
+  //ellipse(posx, posy, 150, 150);
+  fill(0);
+  triangle(pos2x, pos2y, pos2x-50,pos2y + 300,pos2x+50,pos2y+300)
+  //ellipse(pos2x, pos2y, size2, size2);
 }
 
 function draw () {
