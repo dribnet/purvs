95849369a0f629a28024a624f2ccfaa1fe7c80ea
Rebecca Campbell-Redl <bex.campbell.r@gmail.com>
2019-03-26 15:07:48 +1300

    blerg

--- sketch.js
+++ sketch.js
@@ -15,34 +15,39 @@ const canvasHeight = 500;
 const letterA = {
   "offsetx1":-75,
   "offsety1":75,
-  "offsetx2":75,
-  "offsety2":75,
-  "offsetx3":0,
-  "offsety3":-75
+  "offsetx2":0,
+  "offsety2":-75,
+  "offsetx3":75,
+  "offsety3":75,
+  "offset4x":0,
+  "offset4y":0
 }
 
 const letterB = {
-  "offsetx1":-75,
-  "offsety1":-75,
+  "offsetx1":75,
+  "offsety1":0,
   "offsetx2":-75,
   "offsety2":75,
-  "offsetx3":75,
-  "offsety3":50
+  "offsetx3":-75,
+  "offsety3":0,
+  "offset4x":-75,
+  "offset4y":-75
 }
 
 const letterC = {
   "offsetx1":75,
   "offsety1": -75,
-  "offsetx2":75,
-  "offsety2":75,
-  "offsetx3":-75,
-  "offsety3":0
+  "offsetx2":-75,
+  "offsety2":0,
+  "offsetx3":75,
+  "offsety3":75,
+  "offset4x":0,
+  "offset4y":0
 }
 
 const colorFront1  = "#ffffff";
 const colorFront2  = "#000000";
-const colorBack    = "#dddddd";
-const colorStroke  = "#AAAAAA";
+const colorBack    = "#aaaaaa";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -60,20 +65,22 @@ function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
   rectMode(CENTER);
 
-  let pos2x = posx + letterData["offsetx1"];
-  let pos2y = posy + letterData["offsety1"];
-  let pos3x = posx + letterData["offsetx2"];
-  let pos3y = posy + letterData["offsety2"];
-  let pos4x = posx + letterData["offsetx3"];
-  let pos4y = posy + letterData["offsety3"];
+  let pos1x = posx + letterData["offsetx1"];
+  let pos1y = posy + letterData["offsety1"];
+  let pos2x = posx + letterData["offsetx2"];
+  let pos2y = posy + letterData["offsety2"];
+  let pos3x = posx + letterData["offsetx3"];
+  let pos3y = posy + letterData["offsety3"];
+  let pos4x = posx + letterData["offset4x"];
+  let pos4y = posy + letterData["offset4y"];
 
   // draw two circles
+  // fill(colorFront1);
+  // rect(posx,posy,150,150);
   fill(colorFront1);
-  rect(posx,posy,150,150);
+  triangle(pos1x,pos1y,pos2x,pos2y,pos3x,pos3y);
   fill(colorFront2);
-  triangle(pos2x,pos2y,pos4x,pos4y,pos3x,pos3y);
-  fill(colorStroke);
-  triangle(pos2x,pos2y,posx,posy,pos3x,pos3y);
+  triangle(pos1x,pos1y,pos4x,pos4y,pos3x,pos3y);
 }
 
 function draw () {
