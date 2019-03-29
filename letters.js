068ba9af425269601759d2260542bd40df9126e5
Leigh Tuala-Lauvao <tualalauvao@gmail.com>
2019-03-29 13:51:47 +1300

    drawn letters

--- letters.js
+++ letters.js
@@ -1,16 +1,16 @@
 const alphabet = {
   "default": {
-    "size": 40,
+    "size": 50,
     "offsetx": 0,
     "offsety": 0
   },
   "A": {
-    "size": 40,
+    "size": 50,
     "offsetx": 0,
     "offsety": 17
   },
   "B": {
-    "size": 75,
+    "size": 40,
     "offsetx": 0,
     "offsety": -70
   },
