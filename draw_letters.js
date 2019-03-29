89ea72437787adfc571a9f2af28d7abb25f2dc62
Dylan Wansbrough <33749606+Dylan-Wansbrough@users.noreply.github.com>
2019-03-29 15:25:02 +1300

    trying out my style

--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,6 @@
 const colorFront1  = "#199cff";
 const colorFront2  = "#59ccff";
-const colorStroke  = "#233f11";
+const colorStroke  = "#000000";
 
 /*
  * Draw the letter given the letterData
@@ -12,18 +12,35 @@ const colorStroke  = "#233f11";
 function drawLetter(letterData) {
   // color/stroke setup
   stroke(colorStroke);
-  strokeWeight(4);
+  strokeWeight(10);
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+  let posx = 50  + letterData["offsetx"];
+  let posy = 150 + letterData["offsety"];
+  let posx2 = 50  + letterData["offsetx2"];
+  let posy2 = 150 + letterData["offsety2"];
+
+  let posx3 = 50  + letterData["offsetx3"];
+  let posy3 = 150 + letterData["offsety3"];
+  let posx4 = 50  + letterData["offsetx4"];
+  let posy4 = 150 + letterData["offsety4"];
+
+  let posx5 = 50  + letterData["offsetx5"];
+  let posy5 = 150 + letterData["offsety5"];
+  let posx6 = 50  + letterData["offsetx6"];
+  let posy6 = 150 + letterData["offsety6"];
+
+  let posx7 = 50  + letterData["offsetx7"];
+  let posy7 = 150 + letterData["offsety7"];
+  let posx8 = 50  + letterData["offsetx8"];
+  let posy8 = 150 + letterData["offsety8"];
 
   // draw two circles
   fill(colorFront1);
-  ellipse(50, 150, 75, 75);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  line(posx, posy, posx2, posy2);
+  line(posx3, posy3, posx4, posy4);
+  line(posx5, posy5, posx6, posy6);
+  line(posx7, posy7, posx8, posy8);
+  strokeWeight(4);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
