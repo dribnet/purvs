298e943109e18cd636c01b5f5abffc543fcbe3f1
Trish Nuesca <trish_nuesca@yahoo.com>
2019-04-02 12:40:02 +1300

    Have put in my alphabet in alphabet html. Changed each letter on letter.js. Updated preview image and thumbnail image to current state of alphabet. Replaced index.html to contents of alphabet.html so its the main page.

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 ## PS2 MDDN 242 2019
 
-INITIAL SKETCHES:
+DESIGN THE ALPHABET:
 
-I decided to stick to ellipses as I like the look of them better and main focusef on lower cases. The only thing that changes in position and size are the smaller ellipses. I also altered the colours to ones which I preferred. I'm not 100% sure if I would keep to using ellipses for the actual project as I'd like to brainstorm more ideas and develop them a lot more.
+A style which I've taken from part one are the curves and circles. After doing a lot of research and looking into the history of writing, I've found out about hieroglyphics. This then led me to searching up glyphs and have found this symbol for a moon glyph. It consists of a straight line, two arcs and two small circles. I then decided to take insipiration from this glyph and base my alphabet on its elements to sort of create an alphabet which has a modern take on hieroglyphics but making the letters be a lot more readable (not symbols). For the line, the arcs and the circles, here are my parameters.
+
+Parameters:
+line_x1 = x position for starting point of line
+line_y1 = y position for starting point of line
+line_x2	= x position for end point of line
+line_y2	= y position for end point of line
+arc1_posx = x position for first arc
+arc1_posy = y position for first arc 
+arc2_posx = x position for second arc
+arc2_posy = y position for second arc
+start_a1 = start angle for arc 1
+end_a1 = end angle for arc 1
+start_a2 = start angle for arc 2
+end_a2 = end angle for arc 2
+circ1_offset_x = offset x position for circle 1
+circ1_offset_y = offset y position for circle 1
+circ2_offset_x = offset x position for circle 2
+circ2_offset_y = offset y position for circle 2
--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,7 @@
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorStroke  = "#233f11";
+angleMode(DEGREES);
+// const colorFront1  = "#199cff";
+// const colorFront2  = "#59cc00";
+// const colorStroke  = "#233f11";
 
 /*
  * Draw the letter given the letterData
@@ -10,20 +11,59 @@ const colorStroke  = "#233f11";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  // color/stroke setup
-  stroke(colorStroke);
-  strokeWeight(4);
+  // // color/stroke setup
+  // stroke(colorStroke);
+  // strokeWeight(4);
+  //
+  // // determine parameters for second circle
+  // let size2 = letterData["size"];
+  // let pos2x = 50  + letterData["offsetx"];
+  // let pos2y = 150 + letterData["offsety"];
+  //
+  // // draw two circles
+  // fill(colorFront1);
+  // ellipse(50, 150, 75, 75);
+  // fill(colorFront2);
+  // ellipse(pos2x, pos2y, size2, size2);
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+  //Parameters for line
+  let line_x1 = 50 + letterData["line_x1"];
+  let line_x2 = 50 + letterData["line_x2"];
+  let line_y1 = 0 + letterData["line_y1"];
+  let line_y2 = 200 + letterData["line_y2"];
+  //Parameters for arcs
+  let arc1_x = 50 + letterData["arc1_posx"];
+  let arc1_y = 90 + letterData["arc1_posy"];
+  let arc2_x = 50 + letterData["arc2_posx"];
+  let arc2_y = 170 + letterData["arc2_posy"];
+  let arc1_start = letterData["start_a1"];
+  let arc1_end = letterData["end_a1"];
+  let arc2_start = letterData["start_a2"];
+  let arc2_end = letterData["end_a2"];
+  //Parameters for circles
+  let circ1_x = 50 + letterData["circ1_offset_x"];
+  let circ1_y = 90 + letterData["circ1_offset_y"];
+  let circ2_x = 50 + letterData["circ2_offset_x"];
+  let circ2_y = 160 + letterData["circ2_offset_y"];
+  //Constants
+  let arc_w = 80;
+  let arc_h = 80;
+  let circ_w = 20;
+  let circ_h = 20;
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(50, 150, 75, 75);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+
+  //Color/Stroke setup
+  stroke(0);
+  strokeWeight(3)
+  noFill();
+  //Draw moon glyph
+  line(line_x1, line_y1,line_x2,line_y2);
+  arc(arc1_x, arc1_y, arc_w, arc_h, radians(arc1_start), radians(arc1_end));
+  arc(arc2_x, arc2_y, arc_w, arc_h, radians(arc2_start), radians(arc2_end));
+  //Draw 2 filled circles
+  fill(0);
+  ellipse(circ1_x, circ1_y, circ_w, circ_h);
+  ellipse(circ2_x, circ2_y, circ_w, circ_h);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
