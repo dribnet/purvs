453bd501cd62ca4d82508c266eb8d8c3aa787dc3
Dylan Wansbrough <33749606+Dylan-Wansbrough@users.noreply.github.com>
2019-04-02 19:44:04 +1300

    updated alphabet
    added a editor and redone some of the  letters

--- draw_letters.js
+++ draw_letters.js
@@ -15,24 +15,24 @@ function drawLetter(letterData) {
   strokeWeight(10);
 
   let posx = 50  + letterData["offsetx"];
-  let posy = 150 + letterData["offsety"];
+  let posy = 165 + letterData["offsety"];
   let posx2 = 50  + letterData["offsetx2"];
-  let posy2 = 150 + letterData["offsety2"];
+  let posy2 = 165 + letterData["offsety2"];
 
   let posx3 = 50  + letterData["offsetx3"];
-  let posy3 = 150 + letterData["offsety3"];
+  let posy3 = 165 + letterData["offsety3"];
   let posx4 = 50  + letterData["offsetx4"];
-  let posy4 = 150 + letterData["offsety4"];
+  let posy4 = 165 + letterData["offsety4"];
 
   let posx5 = 50  + letterData["offsetx5"];
-  let posy5 = 150 + letterData["offsety5"];
+  let posy5 = 165 + letterData["offsety5"];
   let posx6 = 50  + letterData["offsetx6"];
-  let posy6 = 150 + letterData["offsety6"];
+  let posy6 = 165 + letterData["offsety6"];
 
   let posx7 = 50  + letterData["offsetx7"];
-  let posy7 = 150 + letterData["offsety7"];
+  let posy7 = 165 + letterData["offsety7"];
   let posx8 = 50  + letterData["offsetx8"];
-  let posy8 = 150 + letterData["offsety8"];
+  let posy8 = 165 + letterData["offsety8"];
 
   // draw two circles
   fill(colorFront1);
@@ -45,9 +45,22 @@ function drawLetter(letterData) {
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
-  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
+  new_letter["offfsetx"]    = map(percent, 0, 100, oldObj["offfsetx"], newObj["offfsetx"]);
   new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
+  new_letter["offfsetx2"]    = map(percent, 0, 100, oldObj["offfsetx2"], newObj["offfsetx2"]);
+  new_letter["offsety2"] = map(percent, 0, 100, oldObj["offsety2"], newObj["offsety2"]);
+  new_letter["offfsetx3"]    = map(percent, 0, 100, oldObj["offfsetx3"], newObj["offfsetx3"]);
+  new_letter["offsety3"] = map(percent, 0, 100, oldObj["offsety3"], newObj["offsety3"]);
+  new_letter["offfsetx4"]    = map(percent, 0, 100, oldObj["offfsetx4"], newObj["offfsetx4"]);
+  new_letter["offsety4"] = map(percent, 0, 100, oldObj["offsety4"], newObj["offsety4"]);
+  new_letter["offfsetx5"]    = map(percent, 0, 100, oldObj["offfsetx5"], newObj["offfsetx5"]);
+  new_letter["offsety5"] = map(percent, 0, 100, oldObj["offsety5"], newObj["offsety5"]);
+  new_letter["offfsetx6"]    = map(percent, 0, 100, oldObj["offfsetx6"], newObj["offfsetx6"]);
+  new_letter["offsety6"] = map(percent, 0, 100, oldObj["offsety6"], newObj["offsety6"]);
+  new_letter["offfsetx7"]    = map(percent, 0, 100, oldObj["offfsetx7"], newObj["offfsetx7"]);
+  new_letter["offsety7"] = map(percent, 0, 100, oldObj["offsoffsety7ety"], newObj["offsety7"]);
+  new_letter["offfsetx8"]    = map(percent, 0, 100, oldObj["offfsetx8"], newObj["offfsetx8"]);
+  new_letter["offsety8"] = map(percent, 0, 100, oldObj["offsety8"], newObj["offsety8"]);
   return new_letter;
 }
 
--- letters.js
+++ letters.js
@@ -1,6 +1,5 @@
 const alphabet = {
   "default": {
-    "size": 40,
     "offsetx": 0,
     "offsety": 0
   },
@@ -17,99 +16,216 @@ const alphabet = {
     "offsety5": -125,
     "offsetx6": -50,
     "offsety6": 0,
-    "offsetx7": -35,
+    "offsetx7": -30,
     "offsety7": -10,
-    "offsetx8": 35,
+    "offsetx8": 30,
     "offsety8": -10,
   },
   "B": {
+
+  },
+  "C": {
     "offsetx": -50,
-    "offsety": -125,
-    "offsetx2": -50,
-    "offsety2": 0,
+    "offsety": -62.5,
+    "offsetx2": 50,
+    "offsety2": -125,
     "offsetx3": -50,
-    "offsety3": -125, 
+    "offsety3": -62.5, 
     "offsetx4": 50,
-    "offsety4": -62.5,
-    "offsetx5": 50,
-    "offsety5": -62.5,
-    "offsetx6": -50,
-    "offsety6": 0,
-    "offsetx7": -50,
+    "offsety4": 0,
+    "offsetx5": -12,
+    "offsety5": -85,
+    "offsetx6": -12,
+    "offsety6": -40,
+  },
+  "D": {
+
+  },
+  "E": {
+    "offsetx": -50,
+    "offsety": -62.5,
+    "offsetx2": 50,
+    "offsety2": -125,
+    "offsetx3": -50,
+    "offsety3": -62.5, 
+    "offsetx4": 50,
+    "offsety4": 0,
+    "offsetx5": -12,
+    "offsety5": -85,
+    "offsetx6": -12,
+    "offsety6": -40,
+    "offsetx7": -10,
     "offsety7": -62.5,
     "offsetx8": 50,
     "offsety8": -62.5,
   },
-  "C": {
+  "F": {
+
+  },
+  "G": {
+
+  },
+  "H": {
+
+  },
+  "I": {
+
+  },
+  "J": {
+
+  },
+  "K": {
+
+  },
+  "L": {
+
+  },
+  "M": {
+
+  },
+  "N": {
+
+  },
+  "O": {
+
+  },
+  "P": {
+
+  },
+  "Q": {
+
+  },
+  "R": {
+
+  },
+  "S": {
+
+  },
+  "T": {
     "offsetx": -50,
     "offsety": -125,
-    "offsetx2": -50,
-    "offsety2": 0,
+    "offsetx2": 50,
+    "offsety2": -125,
+    "offsetx3": 0,
+    "offsety3": 0, 
+    "offsetx4": 0,
+    "offsety4": -125,
+    "offsetx5": 0,
+    "offsety5": -125,
+    "offsetx6": 50,
+    "offsety6": -68.5, 
+    "offsetx7": 0,
+    "offsety7": -125,
+    "offsetx8": -50,
+    "offsety8": -68.5,
+  },
+  "U": {
+
+  },
+  "V": {
+
+  },
+  "W": {
+
+  },
+  "X": {
+    "offsetx": -50,
+    "offsety": -125,
+    "offsetx2": 50,
+    "offsety2": -125,
     "offsetx3": -50,
     "offsety3": -125, 
     "offsetx4": 50,
-    "offsety4": -85,
-    "offsetx5": 50,
-    "offsety5": -40,
-    "offsetx6": -50,
+    "offsety4": 0,
+    "offsetx5": -50,
+    "offsety5": 0,
+    "offsetx6": 50,
     "offsety6": 0, 
     "offsetx7": -50,
     "offsety7": 0,
     "offsetx8": 50,
-    "offsety8": -40,
+    "offsety8": -125,
   },
-  "D": {
+  "Y": {
+
+  },
+  "Z": {
     "offsetx": -50,
     "offsety": -125,
-    "offsetx2": -50,
-    "offsety2": 0,
-    "offsetx3": -50, 
-    "offsety3": -125,
-    "offsetx4": 50,
-    "offsety4": -62.5,
-    "offsetx5": 50,
-    "offsety5": -62.5,
-    "offsetx6": -50,
-    "offsety6": 0,
-    "offsetx7": -12,
-    "offsety7": -85,
-    "offsetx8": -12,
-    "offsety8": -40,
-  },
-  "E": {
-    "offsetx": -50,
-    "offsety": -125,
-    "offsetx2": -50,
-    "offsety2": 0,
-    "offsetx3": -50,
-    "offsety3": -125, 
-    "offsetx4": 50,
-    "offsety4": -85,
-    "offsetx5": 50,
-    "offsety5": -40,
-    "offsetx6": -50,
-    "offsety6": 0, 
-    "offsetx7": -12,
-    "offsety7": -85,
-    "offsetx8": -12,
-    "offsety8": -40,
-  },
-  "F": {
-    "offsetx": -50,
-    "offsety": -125,
-    "offsetx2": -50,
-    "offsety2": 0,
-    "offsetx3": -50,
-    "offsety3": -125, 
-    "offsetx4": 50,
-    "offsety4": -85,
+    "offsetx2": 50,
+    "offsety2": -125,
+    "offsetx3": 0,
+    "offsety3": 0, 
+    "offsetx4": 0,
+    "offsety4": -125,
     "offsetx5": -50,
     "offsety5": 0,
-    "offsetx6": -50,
+    "offsetx6": 50,
     "offsety6": 0, 
-    "offsetx7": -12,
-    "offsety7": -85,
-    "offsetx8": -12,
-    "offsety8": -40,
-  }
+    "offsetx7": -50,
+    "offsety7": 0,
+    "offsetx8": 50,
+    "offsety8": -125,
+  },
+  "0": {
+    "offsetx": -50,
+    "offsety": -62.5,
+    "offsetx2": 0,
+    "offsety2": -125,
+    "offsetx3": -50,
+    "offsety3": -62.5, 
+    "offsetx4": 0,
+    "offsety4": 0,
+    "offsetx5": 0,
+    "offsety5": 0,
+    "offsetx6": 50,
+    "offsety6": -62.5, 
+    "offsetx7": 50,
+    "offsety7": -62.5,
+    "offsetx8": 0,
+    "offsety8": -125,
+  },
+  "1": {
+
+  },
+  "2": {
+
+  },
+  "3": {
+
+  },
+  "4": {
+
+  },
+  "5": {
+
+  },
+  "6": {
+    "offsetx": -50,
+    "offsety": -125,
+    "offsetx2": -50,
+    "offsety2": 0,
+    "offsetx3": -50,
+    "offsety3": -85, 
+    "offsetx4": 50,
+    "offsety4": 0,
+    "offsetx5": -50,
+    "offsety5": -125,
+    "offsetx6": 50,
+    "offsety6": -125, 
+    "offsetx7": -50,
+    "offsety7": 0,
+    "offsetx8": 50,
+    "offsety8": 0,
+  },
+  "7": {
+
+  },
+  "8": {
+
+  },
+  "9": {
+
+  },
+
 }
