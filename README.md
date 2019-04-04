509afc776bddb034993ae11653f6d43dfa372185
kichayn <kikikunsang@gmail.com>
2019-04-04 19:35:44 +1300

    develop alphabet.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 ## PS2 MDDN 242 2019
 
 
-I use 2 arc and one rct to create my alphabet and I use 12 parameters in my code.
+I use 2 arc and one rct to create my alphabet and I use 12 parameters in my code. This is the complete alpabet.
+My original idea comes from the chinese cuttinng art. I use two arac to cut the rct to bulid letter and number. color idea comes from the Egyptian wall art. 
+
+size     -the x wide of first arc
+sizee11  -the y wide of first circle
+offsetx  -the x position of first arc
+offsety  -the y position of first arc
+
+sizee21  -the x wide of sec arc
+sizee22  -the y wide of sec arc
+offsetx1 -the x position of sec arc
+offsety1 -the y position of sec arc
+
+arc11    -the begin of first arc
+arc12    -the end of first arc
+arc21    -the begin of sec arc
+arc22    -the end of sec arc
--- letters.js
+++ letters.js
@@ -5,10 +5,12 @@ const alphabet = {
     "sizee11": 50,
     "offsetx": -10,
     "offsety": -40,
+    
     "sizee21": 50,
     "sizee22": 50,
     "offsetx1": -10,
     "offsety2": 20,
+   
     "arc11": 0,
     "arc12": 3.14,
     "arc21": 0,
@@ -370,4 +372,301 @@ const alphabet = {
     "arc22": 3.14+1.57
 
 },
+  "U": {
+    "size": 50,
+    "sizee11": 100,  
+    "offsetx": -10,
+    "offsety": -90,
+   
+    "sizee21": 50,
+    "sizee22": 50,
+    "offsetx1": -10,
+    "offsety2": -90,
+   
+    "arc11": 3.14+1.57+1,
+    "arc12": 3.14+1.57-1,
+    "arc21": 3.14+1.57+1,
+    "arc22": 3.14+1.51-1
+  },
+ "V": {
+    "size": 50,
+    "sizee11": 50,  
+    "offsetx": -10,
+    "offsety": -30,
+   
+    "sizee21": 50,
+    "sizee22": 50,
+    "offsetx1": -10,
+    "offsety2": -20,
+   
+    "arc11": 3.14+1.57-1,
+    "arc12": 3.14+1.57+1,
+    "arc21": 3.14+1.57+1,
+    "arc22": 3.14+1.51-1
+  },
+
+   "W": {
+    "size": 25,
+    "sizee11": 230,
+    "offsetx": -28,
+    "offsety": -100,
+    
+    "sizee21": 25,
+    "sizee22": 230,
+    "offsetx1": 8,
+    "offsety2": -100,
+    
+    "arc11": 0,
+    "arc12": 3.14,
+    "arc21": 0,
+    "arc22": 3.14
+
+
+  },
+"X": {
+     "size": 50,
+    "sizee11": 70,
+    "offsetx": -50,
+    "offsety": -18,
+    
+    "sizee21": 50,
+    "sizee22": 70,
+    "offsetx1": 30,
+    "offsety2": -18,
+    
+    "arc11": -1.57,
+    "arc12": 1.57,
+    "arc21": 1.57,
+    "arc22": -1.57
+
+  },
+
+"Y": {
+    "size": 50,
+    "sizee11": 80,
+    "offsetx": -10,
+    "offsety": -90,
+    
+    "sizee21": 120,
+    "sizee22": 50,
+    "offsetx1": -50,
+    "offsety2": 10,
+    
+    "arc11": 0,
+    "arc12": 3.14,
+    "arc21": 3.14+1.57,
+    "arc22": 1.57
+
+  },
+"Z": {
+     "size": 130,
+    "sizee11": 50,
+    "offsetx": 30,
+    "offsety": 15,
+    
+    "sizee21": 130,
+    "sizee22": 50,
+    "offsetx1": -50,
+    "offsety2": -50,
+    
+    "arc11": 1.57,
+    "arc12": -1.57,
+    "arc21": -1.57,
+    "arc22": 1.57
+
+}, 
+
+   "0": {
+    "size": 50,
+    "sizee11": 100,
+    "offsetx": -10,
+    "offsety": -20,
+   
+    "sizee21": 50,
+    "sizee22": 100,
+    "offsetx1": -10,
+    "offsety2": -13,
+   
+    "arc11": 3.14,
+    "arc12": 0,
+    "arc21": 0,
+    "arc22": 3.14
+
+  },
+   "1": {
+     "size": 65,
+    "sizee11": 120,
+    "offsetx": -50,
+    "offsety": -10,
+    
+    "sizee21": 65,
+    "sizee22": 140,
+    "offsetx1": 30,
+    "offsety2": -20,
+    
+    "arc11": -1.57,
+    "arc12": 1.57,
+    "arc21": 1.57,
+    "arc22": -1.57
+
+},
+"2": {
+     "size": 130,
+    "sizee11": 20,
+    "offsetx": 30,
+    "offsety": 25,
+    
+    "sizee21": 130,
+    "sizee22": 80,
+    "offsetx1": -50,
+    "offsety2": -35,
+    
+    "arc11": 1.57,
+    "arc12": -1.57,
+    "arc21": -1.57,
+    "arc22": 1.57
+
+}, 
+
+  "3": {
+     "size": 130,
+    "sizee11": 50,
+    "offsetx": -50,
+    "offsety": -50,
+    
+    "sizee21": 130,
+    "sizee22": 50,
+    "offsetx1": -50,
+    "offsety2": 15,
+    
+    "arc11": -1.57,
+    "arc12": 1.57,
+    "arc21": -1.57,
+    "arc22": 1.57
+
+},
+"4": {
+    "size": 50,
+    "sizee11": 80,
+    "offsetx": -10,
+    "offsety": -90,
+    
+    "sizee21": 120,
+    "sizee22": 120,
+    "offsetx1": -50,
+    "offsety2": 50,
+    
+    "arc11": 0,
+    "arc12": 3.14,
+    "arc21": 3.14+1.57,
+    "arc22": 0
+
+  },
+
+  "5": {
+     "size": 130,
+    "sizee11": 80,
+    "offsetx": 30,
+    "offsety": -35,
+    
+    "sizee21": 130,
+    "sizee22": 20,
+    "offsetx1": -50,
+    "offsety2": 25,
+    
+    "arc11": 1.57,
+    "arc12": -1.57,
+    "arc21": -1.57,
+    "arc22": 1.57
+
+}, 
+
+  "6": {
+     "size": 130,
+    "sizee11": 30,
+    "offsetx": 30,
+    "offsety": -60,
+    
+    "sizee21": 60,
+    "sizee22": 60,
+    "offsetx1": -10,
+    "offsety2": 15,
+    
+    "arc11": 1.57,
+    "arc12": -1.57,
+    "arc21": 0,
+    "arc22": 6.75
+
+}, 
+
+  "7": {
+     "size": 130,
+    "sizee11": 260,
+    "offsetx": -50,
+    "offsety": 50,
+    
+    "sizee21": 130,
+    "sizee22": 260,
+    "offsetx1": -50,
+    "offsety2": 50,
+    
+    "arc11": 1.57+3.14,
+    "arc12": 0,
+    "arc21": 1.57+3.14,
+    "arc22": 0
+  },
+  "8": {
+   "size": 50,
+    "sizee11": 50,
+    "offsetx": -10,
+    "offsety": -45,
+    
+    "sizee21": 65,
+    "sizee22": 65,
+    "offsetx1": -10,
+    "offsety2": 5,
+   
+    "arc11": 0,
+    "arc12": 6.28,
+    "arc21": 0,
+    "arc22": 6.28
+  },
+
+   "9": {
+     "size": 130,
+    "sizee11": 30,
+    "offsetx": -50,
+    "offsety": 25,
+    
+    "sizee21": 60,
+    "sizee22": 60,
+    "offsetx1": -10,
+    "offsety2": -45,
+    
+    "arc11": -1.57,
+    "arc12": 1.57,
+    "arc21": 0,
+    "arc22": 6.75
+
+}, 
+"?": {
+     "size": 130,
+    "sizee11": 130,
+    "offsetx": 30,
+    "offsety": 50,
+    
+    "sizee21": 130,
+    "sizee22": 60,
+    "offsetx1": -50,
+    "offsety2": -50,
+    
+    "arc11": 3.14,
+    "arc12": -1.57,
+    "arc21": -1.57,
+    "arc22": 1.57
+
+}, 
+
+
+
 }
