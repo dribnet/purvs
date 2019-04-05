23a9d0d6c9ccf086dc667a207559c70dd459b6e4
Tom White <tom.white@vuw.ac.nz>
2019-04-05 14:15:17 +1300

    First interpolation example

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Class Example
 
-This shows and editor example using [Hannah's 2 triangle system](https://bl.ocks.org/yeehann/98662b4e8cb0527eaf00b7609160d7cc/14e9b26ec0ace81f13c8d6cc17aac07711a11b3b).
+Custom Interpolation Example: ball motion in y is more abrupt because interpolation pauses at beginning and end.
 
--- draw_letters.js
+++ draw_letters.js
@@ -1,4 +1,4 @@
-const colorFront1  = "#fae";
+const colorFront1  = "#199cff";
 const colorFront2  = "#59ccff";
 const colorStroke  = "#233f11";
 
@@ -15,55 +15,31 @@ function drawLetter(letterData) {
   strokeWeight(4);
 
   // determine parameters for second circle
-  //let size2 = letterData["size"];
-
-  let FirstPosx = letterData["T11x"];
-  let FirstPosy = letterData["T11y"]
-  let SecondPosx = letterData["T12x"];
-  let SecondPosy = letterData["T12y"];
-  let ThirdPosx = letterData["T13x"];
-  let ThirdPosy = letterData["T13y"];
-
-  let SFirstPosx = letterData["T21x"];
-  let SFirstPosy = letterData["T21y"]
-  let SSecondPosx = letterData["T22x"];
-  let SSecondPosy = letterData["T22y"];
-  let SThirdPosx = letterData["T23x"];
-  let SThirdPosy = letterData["T23y"];
+  let size2 = letterData["size"];
+  let pos2x = 50  + letterData["offsetx"];
+  let pos2y = 150 + letterData["offsety"];
 
   // draw two circles
-//  var size = width * 0.05;
-// translate(width/2, height/2);
-// triangle(0, 0, size, size*2, -size, size*2);
-// }
-
-//draw first tri
-fill(colorFront1);
-triangle(FirstPosx, FirstPosy, SecondPosx, SecondPosy, ThirdPosx, ThirdPosy);
-
-//draw second tri
-fill(colorFront2);
-triangle(SFirstPosx, SFirstPosy, SSecondPosx, SSecondPosy, SThirdPosx, SThirdPosy);
+  fill(colorFront1);
+  ellipse(50, 150, 75, 75);
+  fill(colorFront2);
+  ellipse(pos2x, pos2y, size2, size2);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  new_letter["T11x"] = map(percent, 0, 100, oldObj["T11x"], newObj["T11x"]);
-  new_letter["T11y"] = map(percent, 0, 100, oldObj["T11y"], newObj["T11y"]);
-  new_letter["T12x"] = map(percent, 0, 100, oldObj["T12x"], newObj["T12x"]);
-  new_letter["T12y"] = map(percent, 0, 100, oldObj["T12y"], newObj["T12y"]);
-  new_letter["T13x"] = map(percent, 0, 100, oldObj["T13x"], newObj["T13x"]);
-  new_letter["T13y"] = map(percent, 0, 100, oldObj["T13y"], newObj["T13y"]);
-  new_letter["T21x"] = map(percent, 0, 100, oldObj["T21x"], newObj["T21x"]);  
-  new_letter["T21y"] = map(percent, 0, 100, oldObj["T21y"], newObj["T21y"]); 
-  new_letter["T22x"] = map(percent, 0, 100, oldObj["T22x"], newObj["T22x"]);  
-  new_letter["T22y"] = map(percent, 0, 100, oldObj["T22y"], newObj["T22y"]);  
-  new_letter["T23x"] = map(percent, 0, 100, oldObj["T23x"], newObj["T23x"]);
-  new_letter["T23y"] = map(percent, 0, 100, oldObj["T23y"], newObj["T23y"]);
+  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
 
-
- 
-
+  if(percent < 40) {
+    new_letter["offsety"] = oldObj["offsety"];
+  }
+  else if(percent > 60) {
+    new_letter["offsety"] = newObj["offsety"];
+  }
+  else {
+    new_letter["offsety"] = map(percent, 40, 60, oldObj["offsety"], newObj["offsety"]);
+  } 
 
   return new_letter;
 }
