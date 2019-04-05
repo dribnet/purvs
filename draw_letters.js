449ff6925e6664a95acfc5b34bdd93c3046d72eb
Guest User <Guest@wg401-g-02-06.mddn.vuw.ac.nz>
2019-04-05 13:18:24 +1300

    Changed so removed dots that arent used and changed colours.

--- draw_letters.js
+++ draw_letters.js
@@ -1,7 +1,3 @@
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorStroke  = "#233f11";
-
 /*
  * Draw the letter given the letterData
  *
@@ -11,12 +7,13 @@ const colorStroke  = "#233f11";
  */
 function drawLetter(letterData) {
   // color/stroke setup
-  stroke(colorStroke);
   strokeWeight(4);
 
   const black  = "#000000";
   const colorBack   = "#000000";
-  const white  = "#3d6d96";
+  const dark  = "#ce2c00";
+  const mid = "#ff5426";
+  const light = "#ff913d";
 
    //draw lines 
 
@@ -24,63 +21,90 @@ function drawLetter(letterData) {
   stroke(black); 
   strokeWeight(15); 
 
+  //Create variables from letterData
+  p1p2 = letterData["p1p2"];
+  p3p4 = letterData["p3p4"];
+  p5p6 = letterData["p5p6"];
+  p1p3 = letterData["p1p3"];
+  p2p4 = letterData["p2p4"];
+  p3p5 = letterData["p3p5"];
+  p4p6 = letterData["p4p6"];
+  p1p6 = letterData["p1p6"];
+  p2p5 = letterData["p2p5"];
+
+  let dotSize = 15;
   //HORIZONTAL 
 
-  if(letterData["p1p2"]==1){ 
+  if(p1p2==1){ 
     line(10, 20, 80, 20); 
   } 
 
-  if(letterData["p3p4"]==1){ 
+  if(p3p4==1){ 
     line(10, 100, 80, 100); 
   } 
 
-  if(letterData["p5p6"]==1){ 
+  if(p5p6==1){ 
     line(10, 180, 80, 180); 
   } 
 
    //VERTICAL 
 
-  if(letterData["p1p3"]==1){ 
+  if(p1p3==1){ 
    line(10, 20, 10, 100);
   }
 
-  if(letterData["p2p4"]==1){
+  if(p2p4==1){
     line(80, 20, 80, 100);
   } 
 
-  if(letterData["p3p5"]==1){
+  if(p3p5==1){
     line(10, 100, 10, 180);
   }
 
-  if(letterData["p4p6"]){
+  if(p4p6==1){
     line(80, 100, 80, 180);
   }
 
 //DIAGONAL
- if(letterData["p1p6"]==1){
+ if(p1p6==1){
   line(10, 20, 80, 180);
 }
 
-  if(letterData["p2p5"]==1){
+  if(p2p5==1){
     line(80, 20, 10, 180);
   }
    
 
-  //draw dots 
-  fill(white); 
-  stroke(white); 
+//DRAW DOTS
   strokeWeight(5); 
+  fill(dark); 
+  stroke(dark); 
+ 
+if (p1p2==1 || p1p3==1 || p1p6==1){
+  ellipse(10, 20, dotSize);//top left p1 
+}
+if (p1p2==1 || p2p4==1 || p2p5==1){
+  ellipse(80, 20, dotSize);//top right p2 
+}
+  fill(mid);
+  stroke(mid);
 
-  ellipse(10, 20, 20);//top left p1 
-  ellipse(80, 20, 20);//top right p2 
+if(p3p4==1 || p1p3==1 || p3p5==1){
+  ellipse(10, 100, dotSize); //mid left p3 
+}
+if(p3p4==1 || p2p4==1 || p4p6==1){
+  ellipse(80, 100, dotSize); //mid right p4 
+}
+  fill(light);
+  stroke(light);
+if(p5p6==1 || p3p5==1 || p2p5==1){
+  ellipse(10, 180, dotSize); //bottom left p5 
+}
+if(p5p6==1 || p4p6==1 || p1p6==1){
+  ellipse(80, 180, dotSize); //bottom right p6 
+}
 
-  ellipse(10, 100, 20); //mid left p3 
-  ellipse(80, 100, 20); //mid right p4 
-
-  ellipse(10, 180, 20); //bottom left p5 
-  ellipse(80, 180, 20); //bottom right p6 
-  }
-
+}
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
   new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
