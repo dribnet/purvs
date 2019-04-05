79202dc222f24b1124f3a64524ccb1b21647fd24
Hye Jin Park <parkhye@wg401-a-07-10.mddn.vuw.ac.nz>
2019-04-05 15:14:26 +1300

    hexagon

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
+const rad = 15;
 
 /* 
  * my three variable per letter are:
@@ -12,21 +13,24 @@ const canvasHeight = 500;
  *
  */
 
+//https://image.shutterstock.com/display_pic_with_logo/2883424/276509240/stock-vector-colorful-connecting-hexagon-alphabet-letters-276509240.jpg
+//https://editor.p5js.org/Rito/sketches/BJ5D_KeTg
+
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  // "size": 80,
+  "offsetx": 26,
+  "offsety": 0
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  // "size": 150,
+  "offsetx": 26,
+  "offsety": 0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
+  // "size": 100,
+  "offsetx": 26,
   "offsety": 0
 }
 
@@ -34,6 +38,9 @@ const colorFront1  = "#8ee2e0";
 const colorFront2  = "#f495a3";
 const colorBack    = "#edf1e9";
 // const colorStroke  = "#233f11";
+
+// let bugs = [];
+
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -44,22 +51,36 @@ function setup () {
   // stroke(colorStroke);
   // strokeWeight(0);
   noStroke();
-
   // with no animation, redrawing the screen is not necessary
-  noLoop();
+  // noLoop();
+  
 }
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
+  // let size2 = letterData["size"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let w = sqrt(3) * rad;
+  let h = 2 * rad;
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  noFill();
+  stroke(colorFront1);
+  strokeWeight(1);
+  polygon(posx, posy, rad, 6);
+
+  // fill(colorFront2);
+  // noStroke();
+  // polygon(pos2x, pos2y, rad, 6);
+
+  for (let i = 0; i <= 180; i +=w){
+    polygon(i+w, 150, rad, 6);
+  }
+  for (let i = 0; i <=180; i +=w){
+    polygon( w*1.5, i+w+150, rad, 6);
+  }
+  // https://www.redblobgames.com/grids/hexagons/#coordinates-offset
+
 }
 
 function draw () {
@@ -74,7 +95,13 @@ function draw () {
   drawLetter(center_x - 250, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
+
+  //   for (let i = 0; i < bugs.length; i++){
+  //   bugs[i].move()
+  //   bugs[i].display();
+  // }
 }
+
 
 function keyTyped() {
   if (key == '!') {
@@ -84,3 +111,30 @@ function keyTyped() {
     saveBlocksImages(true);
   }
 }
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints
+  beginShape();
+  for (let a = 0; a < TWO_PI ; a += angle) {
+    let sx = x + sin(a) * radius;
+    let sy = y + cos(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+// class Jitter{
+//   constructor(){
+//     this.x = random(width);
+//     this.y = random(height);
+//     this.diameter = 10;
+//     this.speed = 1;
+//   }
+//   move(){
+//     this.x += random(-this.speed, this.speed);
+//     this.y += random(-this.speed, this.speed);
+//   }
+//   display(){
+//     ellipse(this.x, this.y, this.diameter, this.diameter);
+//   }
+// }
