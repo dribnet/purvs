814fe1485cc22d7a72dee4da642c930f1169ee82
maiahill <maiatwhill@gmail.com>
2019-04-07 16:23:08 +1200

    updating readme and git

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## PS2 MDDN 242 2019
 
-Alphabet
+Revising Alphabet
 
 
-I modelled the alphabet off of my initial sketch, trying to form the letters (mostly) using the negative space of the first, dark green square. This square doesn't move around, rather there are three smaller squares that move and form each letter. These three squares aren't always present, for the majority of the time it is just the main square and two moving ones. The third square shares the size parameter with the second square, so it just needs it's own x and y coordinates.
+I found the first alphabet could be quite hard to read, so I created a fill for the rectangles to block out the main white square and create a stronger visual impact. I also had to edit some of the placements of the rects as this version is less abstract than the previous and doesn't suit the same positioning. Most letters are able to keep within the diameter of the main rect, though in some cases (t, y, 1, and ?) there is some overlap so as to have a proportionate, readable letter. I changed to a monochromatic blue colour scheme as I just thought it looked a bit nicer. The basic principles are still the same though, with the main square staying in the same place and four squares moving around forming the letters. I added a fourth square so that the x could fit the style better. The third square shares the size parameter with the second square, and the fourth with the primary, so only seperate x and y coordinates are needed.
 
-There are between four to eight parameters per letter:
-  * `primary_size` : diameter of the primary rectangle
+There are between four to ten parameters per letter:
+  * `primary_size` : diameter of the primary and fourth rectangles
   * `secondary_size` : diameter of the second and third rectangles
   * `prim_offsetx` : x offset of the primary rectangle relative to the first one
   * `prim_offsety` : y offset of the primary rectangle relative to the first one
@@ -14,4 +14,6 @@ There are between four to eight parameters per letter:
   * `sec_offsety` : y offset of the secondary rectangle relative to the first one
   * `third_offsetx` : x offset of the third rectangle relative to the first one
   * `third_offsety` : y offset of the third rectangle relative to the first one
+  * `fourth_offsetx` : x offset of the fourth rectangle relative to the first one
+  * `fourth_offsety` : y offset of the fourth rectangle relative to the first one
 
--- letters.js
+++ letters.js
@@ -7,64 +7,64 @@ const alphabet = {
   },
   "A": {
     "primary_size": 15,
-    "secondary_size": 45,
+    "secondary_size": 30,
     "prim_offsetx": 27.5,
-    "prim_offsety": 25,
-    "sec_offsetx": 12.5,
+    "prim_offsety": 22.5,
+    "sec_offsetx": 20,
     "sec_offsety": 50
   },
   "B": {
-    "primary_size": 40,
+    "primary_size": 30,
     "secondary_size": 15,
-    "prim_offsetx": 17.5,
-    "prim_offsety": 20,
+    "prim_offsetx": 25,
+    "prim_offsety": 25,
     "sec_offsetx": 0,
-    "sec_offsety": -25
+    "sec_offsety": -15
   },
   "C": {
-    "primary_size": 50,
+    "primary_size": 45,
     "secondary_size": 0,
-    "prim_offsetx": 35,
-    "prim_offsety": 15
+    "prim_offsetx": 25,
+    "prim_offsety": 17.5
   },
   "D": {
-    "primary_size": 40,
+    "primary_size": 30,
     "secondary_size": 15,
     "prim_offsetx": 12.5,
-    "prim_offsety": 20,
+    "prim_offsety": 25,
     "sec_offsetx": 55,
-    "sec_offsety": -25
+    "sec_offsety": -15
   },
   "E": {
     "primary_size": 20,
     "secondary_size": 20,
-    "prim_offsetx": 57.5,
+    "prim_offsetx": 50,
     "prim_offsety": 15,
-    "sec_offsetx": 57.5,
+    "sec_offsetx": 50,
     "sec_offsety": 45
   },
   "F": {
     "primary_size": 20,
-    "secondary_size": 40,
-    "prim_offsetx": 57.5,
+    "secondary_size": 30,
+    "prim_offsetx": 50,
     "prim_offsety": 15,
     "sec_offsetx": 40,
     "sec_offsety": 50
   },
   "G": {
-    "primary_size": 45,
-    "secondary_size": 12.5,
-    "prim_offsetx": 35,
+    "primary_size": 12.5,
+    "secondary_size": 25,
+    "prim_offsetx": 15,
     "prim_offsety": 15,
-    "sec_offsetx": 52,
-    "sec_offsety": 42
+    "sec_offsetx": 0,
+    "sec_offsety": 45
   },
   "H": {
-    "primary_size": 30,
-    "secondary_size": 30,
-    "prim_offsetx": 20,
-    "prim_offsety": -10,
-    "sec_offsetx": 20,
+    "primary_size": 20,
+    "secondary_size": 20,
+    "prim_offsetx": 25,
+    "prim_offsety": 0,
+    "sec_offsetx": 25,
     "sec_offsety": 60
   },
   "I": {
@@ -75,43 +75,43 @@ const alphabet = {
   },
   "J": {
     "primary_size": 15,
-    "secondary_size": 50,
+    "secondary_size": 40,
     "prim_offsetx": 55,
     "prim_offsety": -25,
-    "sec_offsetx": -15,
-    "sec_offsety": -10
+    "sec_offsetx": 0,
+    "sec_offsety": 0
   },
   "K": {
-    "primary_size": 30,
-    "secondary_size": 30,
-    "prim_offsetx": 55,
-    "prim_offsety": -15,
-    "sec_offsetx": 55,
-    "sec_offsety": 65
+    "primary_size": 15,
+    "secondary_size": 15,
+    "prim_offsetx": 30,
+    "prim_offsety": 0,
+    "sec_offsetx": 25,
+    "sec_offsety": 65,
+    "third_offsetx": 55,
+    "third_offsety": 30
   },
   "L": {
-    "primary_size": 55,
+    "primary_size": 40,
     "secondary_size": 0,
     "prim_offsetx": 30,
-    "prim_offsety": -10
+    "prim_offsety": 0
   },
   "M": {
-    "primary_size": 15,
-    "secondary_size": 5,
-    "prim_offsetx": 27.5,
-    "prim_offsety": 5,
-    "sec_offsetx": 15,
-    "sec_offsety": 70,
-    "third_offsetx": 52.5,
-    "third_offsety": 70
+    "primary_size": 10,
+    "secondary_size": 10,
+    "prim_offsetx": 47.5,
+    "prim_offsety": 70,
+    "sec_offsetx": 12.5,
+    "sec_offsety": 70
   },
   "N": {
-    "primary_size": 45,
-    "secondary_size": 7.5,
-    "prim_offsetx": 12.5,
-    "prim_offsety": 45,
+    "primary_size": 40,
+    "secondary_size": 10,
+    "prim_offsetx": 15,
+    "prim_offsety": 40,
     "sec_offsetx": 0,
-    "sec_offsety": -12.5
+    "sec_offsety": -10
   },
   "O": {
     "primary_size": 25,
@@ -121,180 +121,182 @@ const alphabet = {
   },
   "P": {
     "primary_size": 10,
-    "secondary_size": 45,
+    "secondary_size": 35,
     "prim_offsetx": 45,
     "prim_offsety": 15,
     "sec_offsetx": 35,
     "sec_offsety": 45
   },
   "Q": {
-    "primary_size": 40,
-    "secondary_size": 15,
-    "prim_offsetx": 15,
-    "prim_offsety": 20,
+    "primary_size": 30,
+    "secondary_size": 10,
+    "prim_offsetx": 20,
+    "prim_offsety": 25,
     "sec_offsetx": 45,
-    "sec_offsety": 72.5
+    "sec_offsety": 70
   },
   "R": {
-    "primary_size": 50,
-    "secondary_size": 7.5,
+    "primary_size": 40,
+    "secondary_size": 10,
     "prim_offsetx": 30,
     "prim_offsety": 40,
     "sec_offsetx": 0,
-    "sec_offsety": -12.5
+    "sec_offsety": -10
   },
   "S": {
     "primary_size": 20,
     "secondary_size": 20,
-    "prim_offsetx": 60,
+    "prim_offsetx": 50,
     "prim_offsety": 15,
-    "sec_offsetx": -7.5,
+    "sec_offsetx": 0,
     "sec_offsety": 47.5
   },
   "T": {
-    "primary_size": 45,
-    "secondary_size": 45,
+    "primary_size": 35,
+    "secondary_size": 35,
     "prim_offsetx": 55,
     "prim_offsety": 45,
-    "sec_offsetx": -30,
+    "sec_offsetx": -20,
     "sec_offsety": 45
   },
   "U": {
     "primary_size": 40,
-    "secondary_size": 7.5,
+    "secondary_size": 10,
     "prim_offsetx": 15,
-    "prim_offsety": -7.5,
-    "sec_offsetx": 62.5,
-    "sec_offsety": 85
+    "prim_offsety": 0,
+    "sec_offsetx": 60,
+    "sec_offsety": 80
   },
   "V": {
     "primary_size": 30,
     "secondary_size": 0,
     "prim_offsetx": 20,
-    "prim_offsety": -7.5
+    "prim_offsety": 0
   },
   "W": {
     "primary_size": 15,
-    "secondary_size": 5,
+    "secondary_size": 10,
     "prim_offsetx": 27.5,
-    "prim_offsety": 60,
-    "sec_offsetx": 12.5,
-    "sec_offsety": 5,
-    "third_offsetx": 52.5,
-    "third_offsety": 5
+    "prim_offsety": 65,
+    "sec_offsetx": 10,
+    "sec_offsety": 0,
+    "third_offsetx": 50,
+    "third_offsety": 0
   },
   "X": {
-    "primary_size": 30,
-    "secondary_size": 30,
-    "prim_offsetx": -15,
-    "prim_offsety": 70,
-    "sec_offsetx": 55,
-    "sec_offsety": -10
+    "primary_size": 15,
+    "secondary_size": 12.5,
+    "prim_offsetx": 27.5,
+    "prim_offsety": 0,
+    "sec_offsetx": 57.5,
+    "sec_offsety": 35,
+    "third_offsetx": 0,
+    "third_offsety": 35,
+    "fourth_offsetx": 27.5,
+    "fourth_offsety": 65
   },
   "Y": {
-    "primary_size": 30,
-    "secondary_size": 40,
-    "prim_offsetx": 20,
-    "prim_offsety": -15,
-    "sec_offsetx": -25,
-    "sec_offsety": 47.5,
+    "primary_size": 20,
+    "secondary_size": 25,
+    "prim_offsetx": 25,
+    "prim_offsety": 0,
+    "sec_offsetx": -10,
+    "sec_offsety": 55,
     "third_offsetx": 55,
-    "third_offsety": 47.5
+    "third_offsety": 55
   },
   "Z": {
     "primary_size": 20,
     "secondary_size": 20,
-    "prim_offsetx": -7.5,
+    "prim_offsetx": 0,
     "prim_offsety": 15,
-    "sec_offsetx": 60,
+    "sec_offsetx": 50,
     "sec_offsety": 47.5
   },
   "0": {
-    "primary_size": 45,
+    "primary_size": 40,
     "secondary_size": 0,
-    "prim_offsetx": 12.5,
-    "prim_offsety": 17.5
+    "prim_offsetx": 15,
+    "prim_offsety": 20
   },
   "1": {
-    "primary_size": 60,
+    "primary_size": 45,
     "secondary_size": 0,
-    "prim_offsetx": -35,
-    "prim_offsety": 27.5,
+    "prim_offsetx": -10,
+    "prim_offsety": 35,
   },
   "2": {
     "primary_size": 30,
     "secondary_size": 20,
-    "prim_offsetx": -7.5,
+    "prim_offsetx": 0,
     "prim_offsety": 15,
-    "sec_offsetx": 60,
+    "sec_offsetx": 50,
     "sec_offsety": 47.5
   },
   "3": {
     "primary_size": 20,
     "secondary_size": 20,
-    "prim_offsetx": -10,
+    "prim_offsetx": 0,
     "prim_offsety": 15,
-    "sec_offsetx": -10,
+    "sec_offsetx": 0,
     "sec_offsety": 45
   },
   "4": {
-    "primary_size": 25,
-    "secondary_size": 15,
-    "prim_offsetx": 15,
-    "prim_offsety": 20,
-    "sec_offsetx": 50,
-    "sec_offsety": 75,
-    "third_offsetx": 75,
-    "third_offsety": 45
+    "primary_size": 35,
+    "secondary_size": 12.5,
+    "prim_offsetx": 35,
+    "prim_offsety": 0,
+    "sec_offsetx": 0,
+    "sec_offsety": 67.5,
+    "third_offsetx": 57.5,
+    "third_offsety": 67.5
   },
   "5": {
     "primary_size": 20,
     "secondary_size": 30,
-    "prim_offsetx": 60,
+    "prim_offsetx": 50,
     "prim_offsety": 15,
-    "sec_offsetx": -7.5,
+    "sec_offsetx": 0,
     "sec_offsety": 40
   },
   "6": {
-    "primary_size": 32.5,
-    "secondary_size": 15,
-    "prim_offsetx": 22.5,
-    "prim_offsety": 30,
-    "sec_offsetx": 0,
-    "sec_offsety": -20
+    "primary_size": 12.5,
+    "secondary_size": 30,
+    "prim_offsetx": 40,
+    "prim_offsety": 55,
+    "sec_offsetx": 40,
+    "sec_offsety": 15
   },
   "7": {
-   "primary_size": 60,
+   "primary_size": 40,
     "secondary_size": 10,
-    "prim_offsetx": -20,
-    "prim_offsety": 27.5,
-    "sec_offsetx": 85,
-    "sec_offsety": 30
+    "prim_offsetx": 0,
+    "prim_offsety": 40,
+    "sec_offsetx": 80,
+    "sec_offsety": 37.5
   },
   "8": {
-    "primary_size": 20,
-    "secondary_size": 20,
-    "prim_offsetx": 25,
-    "prim_offsety": 10,
-    "sec_offsetx": 25,
-    "sec_offsety": 50
+    "primary_size": 15,
+    "secondary_size": 15,
+    "prim_offsetx": 27.5,
+    "prim_offsety": 15,
+    "sec_offsetx": 27.5,
+    "sec_offsety": 52.5
   },
   "9": {
-    "primary_size": 50,
+    "primary_size": 35,
     "secondary_size": 10,
-    "prim_offsetx": -15,
-    "prim_offsety": 40,
+    "prim_offsetx": 0,
+    "prim_offsety": 45,
     "sec_offsetx": 15,
     "sec_offsety": 15
   },
   "?": {
     "primary_size": 15,
-    "secondary_size": 40,
-    "prim_offsetx": 45,
+    "secondary_size": 35,
+    "prim_offsetx": 50,
     "prim_offsety": 90,
-    "sec_offsetx": -12.5,
-    "sec_offsety": 50,
-    "third_offsetx": 10,
-    "third_offsety": 25
+    "sec_offsetx": 0,
+    "sec_offsety": 45,
   }
 }
