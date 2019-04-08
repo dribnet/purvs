c66f4339aba9927449ef5b282971c100596894b9
Hye Jin Park <parkhye@wg401-d-01-06.mddn.vuw.ac.nz>
2019-04-08 12:59:46 +1200

    sketch modified

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
-const rad = 15;
+const rad = 10;
+
+
+
 
 /* 
  * my three variable per letter are:
@@ -13,33 +16,37 @@ const rad = 15;
  *
  */
 
-//https://image.shutterstock.com/display_pic_with_logo/2883424/276509240/stock-vector-colorful-connecting-hexagon-alphabet-letters-276509240.jpg
-//https://editor.p5js.org/Rito/sketches/BJ5D_KeTg
+// https://image.shutterstock.com/display_pic_with_logo/2883424/276509240/stock-vector-colorful-connecting-hexagon-alphabet-letters-276509240.jpg
+// https://editor.p5js.org/Rito/sketches/BJ5D_KeTg
+// https://www.redblobgames.com/grids/hexagons/#coordinates-offset
+// http://1.bp.blogspot.com/-rAqRNBJmz2M/TdER5PDZX6I/AAAAAAAAC7o/CLMbuXNrsOw/s1600/IMG_4388.jpg
 
 const letterA = {
   // "size": 80,
-  "offsetx": 26,
-  "offsety": 0
+  // "offsetx": 0,
+  // "offsety": 0
+  // "on": "142,226,224";
+  // "off": noFill();
 }
 
 const letterB = {
   // "size": 150,
-  "offsetx": 26,
-  "offsety": 0
+  // "offsetx": 0,
+  // "offsety": 0
 }
 
 const letterC = {
   // "size": 100,
-  "offsetx": 26,
-  "offsety": 0
+  // "offsetx": 0,
+  // "offsety": 0
 }
 
 const colorFront1  = "#8ee2e0";
 const colorFront2  = "#f495a3";
 const colorBack    = "#edf1e9";
+
 // const colorStroke  = "#233f11";
 
-// let bugs = [];
 
 
 function setup () {
@@ -59,47 +66,37 @@ function setup () {
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
   // let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
-  let w = sqrt(3) * rad;
-  let h = 2 * rad;
+  // let pos2x = posx + letterData["offsetx"];
+  // let pos2y = posy + letterData["offsety"];
+  // let lightOn = letterData["on"];
+  // let lightOff = letterData["off"];
+  // for (let i = 0; i <= 180; i +=w){
+  //   polygon(i+w, 100, rad, 6);
+  //   polygon(i+w2,100+3/4*h, rad, 6);
+  // }
+  hexGrid(posx, posy)
 
-  noFill();
-  stroke(colorFront1);
-  strokeWeight(1);
-  polygon(posx, posy, rad, 6);
-
-  // fill(colorFront2);
-  // noStroke();
-  // polygon(pos2x, pos2y, rad, 6);
-
-  for (let i = 0; i <= 180; i +=w){
-    polygon(i+w, 150, rad, 6);
-  }
-  for (let i = 0; i <=180; i +=w){
-    polygon( w*1.5, i+w+150, rad, 6);
-  }
-  // https://www.redblobgames.com/grids/hexagons/#coordinates-offset
 
 }
 
 function draw () {
   // clear screen
   background(colorBack);
-
+  // let w = sqrt(3) * rad;
+  // let h = 2 * rad;
+  // let w2 = w+1/2*w;
   // compute the center of the canvas
-  let center_x = canvasWidth / 2;  
-  let center_y = canvasHeight / 2;
+  let w = sqrt(3) * rad;
+  let h = 2 * rad; 
+  let center_x = canvasWidth / 2 - w*2;  
+  let center_y = canvasHeight / 2 - h*2;
+
 
   // draw the letters A, B, C from saved data
   drawLetter(center_x - 250, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
 
-  //   for (let i = 0; i < bugs.length; i++){
-  //   bugs[i].move()
-  //   bugs[i].display();
-  // }
 }
 
 
@@ -112,6 +109,116 @@ function keyTyped() {
   }
 }
 
+function hexGrid(posx, posy){
+  let w = sqrt(3) * rad;
+  let h = 2 * rad;  
+  let w2 = 1/2*w;
+  let h2 = 3/4*h;
+
+  noFill();
+  stroke(colorFront1);
+  strokeWeight(1);
+
+  //first row
+  polygon(posx, posy, rad, 6);
+  polygon(posx+w, posy, rad, 6);
+  polygon(posx+2*w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+3*w, posy, rad, 6);
+  noFill();
+  polygon(posx+4*w, posy, rad, 6);
+  polygon(posx+5*w, posy, rad, 6);
+  polygon(posx+6*w, posy, rad, 6);
+
+  //second row
+  push();
+  translate(w2, h2);
+  polygon(posx, posy, rad, 6);
+  polygon(posx+w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+2*w, posy, rad, 6);
+  polygon(posx+3*w, posy, rad, 6);
+  noFill();
+  polygon(posx+4*w, posy, rad, 6);
+  polygon(posx+5*w, posy, rad, 6);
+  pop();
+
+  //third row
+  push();
+  translate(0,2*h2);
+  polygon(posx, posy, rad, 6);
+  polygon(posx+w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+2*w, posy, rad, 6);
+  noFill();
+  polygon(posx+3*w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+4*w, posy, rad, 6);
+  noFill();
+  polygon(posx+5*w, posy, rad, 6);
+  polygon(posx+6*w, posy, rad, 6);
+  pop();
+
+  //fourth row
+  push();
+  translate(w2, 3*h2);
+  polygon(posx, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+w, posy, rad, 6);
+  noFill();
+  polygon(posx+2*w, posy, rad, 6);
+  polygon(posx+3*w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+4*w, posy, rad, 6);
+  noFill();
+  polygon(posx+5*w, posy, rad, 6);
+  pop();
+
+  //fifth row
+  push();
+  translate(0, 4*h2);
+  polygon(posx, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+w, posy, rad, 6);
+  polygon(posx+2*w, posy, rad, 6);
+  polygon(posx+3*w, posy, rad, 6);
+  polygon(posx+4*w, posy, rad, 6);
+  polygon(posx+5*w, posy, rad, 6);
+  noFill();
+  polygon(posx+6*w, posy, rad, 6);
+  pop();
+
+  //sixth row
+  push();
+  translate(w2, 5*h2);
+  fill(colorFront1);
+  polygon(posx, posy, rad, 6);
+  noFill();
+  polygon(posx+w, posy, rad, 6);
+  polygon(posx+2*w, posy, rad, 6);
+  polygon(posx+3*w, posy, rad, 6);
+  polygon(posx+4*w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+5*w, posy, rad, 6);
+  noFill();
+  pop();
+
+  //seventh row
+  push();
+  translate(0, 6*h2);
+  fill(colorFront1);
+  polygon(posx, posy, rad, 6);
+  noFill();
+  polygon(posx+w, posy, rad, 6);
+  polygon(posx+2*w, posy, rad, 6);
+  polygon(posx+3*w, posy, rad, 6);
+  polygon(posx+4*w, posy, rad, 6);
+  polygon(posx+5*w, posy, rad, 6);
+  fill(colorFront1);
+  polygon(posx+6*w, posy, rad, 6);
+  pop();
+}
+
 function polygon(x, y, radius, npoints) {
   let angle = TWO_PI / npoints
   beginShape();
@@ -122,19 +229,3 @@ function polygon(x, y, radius, npoints) {
   }
   endShape(CLOSE);
 }
-
-// class Jitter{
-//   constructor(){
-//     this.x = random(width);
-//     this.y = random(height);
-//     this.diameter = 10;
-//     this.speed = 1;
-//   }
-//   move(){
-//     this.x += random(-this.speed, this.speed);
-//     this.y += random(-this.speed, this.speed);
-//   }
-//   display(){
-//     ellipse(this.x, this.y, this.diameter, this.diameter);
-//   }
-// }
