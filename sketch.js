2158377a180e1fb648762c0f71c6609399ddab63
Hye Jin Park <parkhye@wg401-h-01-07.mddn.vuw.ac.nz>
2019-04-08 19:15:48 +1200

    Major sketch edit. Need to find out what is making the strokes invisible on first row of B. Need to find out arranging each letters without the help of translate.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,56 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
+
+//basic numbers for hexagonal grid
 const rad = 10;
+const w = Math.sqrt(3) * rad;
+const h = 2 * rad;
+
+//x values for first hexagons in even rows
+const wEvenRow = 1/2*w;
+const w2 = rad+wEvenRow;
+
+//y values for each row
+const hDistance = 3/4*h;
+const h2 = rad+hDistance;
+const h3 = rad+2*hDistance;
+const h4 = rad+3*hDistance;
+const h5 = rad+4*hDistance;
+const h6 = rad+5*hDistance;
+const h7 = rad+6*hDistance;
 
 
+const polygons = [
+      //first row
+      [rad, rad, rad, 6],[rad+w, rad, rad, 6],[rad+2*w, rad, rad, 6],[rad+3*w, rad, rad, 6],[rad+4*w, rad, rad, 6],[rad+5*w, rad, rad, 6],[rad+6*w, rad, rad, 6],
+      
+      //second row
+      [w2, h2, rad, 6], [w2+w, h2, rad, 6], [w2+2*w, h2, rad, 6], [w2+3*w, h2, rad, 6], [w2+4*w, h2, rad, 6], [w2+5*w, h2, rad, 6],
 
+      //third row
+      [rad, h3, rad, 6],[rad+w, h3, rad, 6],[rad+2*w, h3, rad, 6],[rad+3*w, h3, rad, 6],[rad+4*w, h3, rad, 6],[rad+5*w, h3, rad, 6],[rad+6*w, h3, rad, 6],
+      
+      //fourth row
+      [w2, h4, rad, 6], [w2+w, h4, rad, 6], [w2+2*w, h4, rad, 6], [w2+3*w, h4, rad, 6], [w2+4*w, h4, rad, 6], [w2+5*w, h4, rad, 6],
+
+      //fifth row
+      [rad, h5, rad, 6],[rad+w, h5, rad, 6],[rad+2*w, h5, rad, 6],[rad+3*w, h5, rad, 6],[rad+4*w, h5, rad, 6],[rad+5*w, h5, rad, 6],[rad+6*w, h5, rad, 6],
+      
+      //sixth row
+      [w2, h6, rad, 6], [w2+w, h6, rad, 6], [w2+2*w, h6, rad, 6], [w2+3*w, h6, rad, 6], [w2+4*w, h6, rad, 6], [w2+5*w, h6, rad, 6],
+
+      //seventh row
+      [rad, h7, rad, 6],[rad+w, h7, rad, 6],[rad+2*w, h7, rad, 6],[rad+3*w, h7, rad, 6],[rad+4*w, h7, rad, 6],[rad+5*w, h7, rad, 6],[rad+6*w, h7, rad, 6],
+      
+      ];
 
 /* 
  * my three variable per letter are:
  *
-   size: radius of the second circle (in pixels)
-   offsetx: x offset (in pixels) of the second circle
-            relative to the first one
-   offsety: y offset (in pixels) of the second circle
-            relative to the first one
+   polygons: binary signal for 46 hexagons to either color up or not to draw each letters
+   red: R value
+   green: G value
+   blue: B value
  *
  */
 
@@ -21,31 +59,47 @@ const rad = 10;
 // https://www.redblobgames.com/grids/hexagons/#coordinates-offset
 // http://1.bp.blogspot.com/-rAqRNBJmz2M/TdER5PDZX6I/AAAAAAAAC7o/CLMbuXNrsOw/s1600/IMG_4388.jpg
 
+// default polygons array
+//                 /*row1*/0, 0, 0, 0, 0, 0, 0,
+//                   /*row2*/0, 0, 0, 0, 0, 0,
+//                 /*row3*/0, 0, 0, 0, 0, 0, 0,
+//                   /*row4*/0, 0, 0, 0, 0, 0,
+//                 /*row5*/0, 0, 0, 0, 0, 0, 0,
+//                   /*row6*/0, 0, 0, 0, 0, 0,
+//                 /*row7*/0, 0, 0, 0, 0, 0, 0
+
 const letterA = {
-  // "size": 80,
-  // "offsetx": 0,
-  // "offsety": 0
-  // "on": "142,226,224";
-  // "off": noFill();
+  "polygons" : [/*row1*/0, 0, 0, 1, 0, 0, 0,
+                  /*row2*/0, 0, 1, 1, 0, 0,
+                /*row3*/0, 0, 1, 0, 1, 0, 0,
+                  /*row4*/0, 1, 0, 0, 1, 0,
+                /*row5*/0, 1, 1, 1, 1, 1, 0,
+                  /*row6*/1, 0, 0, 0, 0, 1,
+                /*row7*/1, 0, 0, 0, 0, 0, 1],
+  "red": 142, "green": 226, "blue": 224
 }
 
 const letterB = {
-  // "size": 150,
-  // "offsetx": 0,
-  // "offsety": 0
-}
+  "polygons" : [/*row1*/1, 1, 1, 1, 1, 1, 0,
+                  /*row2*/1, 0, 0, 0, 0, 1,
+                /*row3*/1, 1, 0, 0, 0, 1, 0,
+                  /*row4*/1, 1, 1, 1, 1, 0,
+                /*row5*/1, 1, 0, 0, 0, 1, 0,
+                  /*row6*/1, 0, 0, 0, 0, 1,
+                /*row7*/1, 1, 1, 1, 1, 1, 0],
+  "red": 244, "green": 149, "blue": 163
+ }
 
 const letterC = {
-  // "size": 100,
-  // "offsetx": 0,
-  // "offsety": 0
+  "polygons" : [/*row1*/0, 0, 1, 1, 1, 0, 1,
+                  /*row2*/1, 1, 0, 0, 1, 1,
+                /*row3*/0, 1, 0, 0, 0, 0, 0,
+                  /*row4*/1, 1, 0, 0, 0, 0,
+                /*row5*/0, 1, 0, 0, 0, 0, 0,
+                  /*row6*/1, 1, 0, 0, 0, 1,
+                /*row7*/0, 0, 1, 1, 1, 1, 0],
+  "red": 244, "green": 149, "blue": 163
 }
-
-const colorFront1  = "#8ee2e0";
-const colorFront2  = "#f495a3";
-const colorBack    = "#edf1e9";
-
-// const colorStroke  = "#233f11";
 
 
 
@@ -53,49 +107,53 @@ function setup () {
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
-
-  // color/stroke setup
-  // stroke(colorStroke);
-  // strokeWeight(0);
   noStroke();
   // with no animation, redrawing the screen is not necessary
   // noLoop();
   
 }
 
-function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  // let size2 = letterData["size"];
-  // let pos2x = posx + letterData["offsetx"];
-  // let pos2y = posy + letterData["offsety"];
-  // let lightOn = letterData["on"];
-  // let lightOff = letterData["off"];
-  // for (let i = 0; i <= 180; i +=w){
-  //   polygon(i+w, 100, rad, 6);
-  //   polygon(i+w2,100+3/4*h, rad, 6);
-  // }
-  hexGrid(posx, posy)
+function drawLetter(x,y,letterData) {
+
+  for(var i = 0; i < polygons.length; i++){
+  if(letterData["polygons"][i] == 1){ //Draw lit up hexagon
+    fill(letterData["red"], letterData["green"], letterData["blue"]);
+    polygon(polygons[i][0], polygons[i][1], polygons[i][2], polygons[i][3]);    
+  }else{  //Draw hexagon
+    noFill();
+    stroke(129, 144, 168);
+    strokeWeight(1);
+    polygon(polygons[i][0], polygons[i][1], polygons[i][2], polygons[i][3]);  
+  } 
+  polygon(polygons[i][0], polygons[i][1], polygons[i][2], polygons[i][3]);  
+  }  
 
 
 }
 
-function draw () {
+function draw() {
   // clear screen
   background(colorBack);
-  // let w = sqrt(3) * rad;
-  // let h = 2 * rad;
-  // let w2 = w+1/2*w;
   // compute the center of the canvas
-  let w = sqrt(3) * rad;
-  let h = 2 * rad; 
-  let center_x = canvasWidth / 2 - w*2;  
-  let center_y = canvasHeight / 2 - h*2;
+  let center_x = canvasWidth/2 -7*wEvenRow;  
+  let center_y = canvasHeight / 2 -70;
 
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
+  push();
+  translate(center_x-250, center_y);
+  drawLetter(0,0, letterA);
+  pop();
+
+  push();
+  translate(center_x, center_y);
+  drawLetter(0,0, letterB);
+  pop();
+
+  push();
+  translate (center_x+250, center_y);
+  drawLetter(0,0, letterC);
+  pop();
 
 }
 
@@ -109,115 +167,6 @@ function keyTyped() {
   }
 }
 
-function hexGrid(posx, posy){
-  let w = sqrt(3) * rad;
-  let h = 2 * rad;  
-  let w2 = 1/2*w;
-  let h2 = 3/4*h;
-
-  noFill();
-  stroke(colorFront1);
-  strokeWeight(1);
-
-  //first row
-  polygon(posx, posy, rad, 6);
-  polygon(posx+w, posy, rad, 6);
-  polygon(posx+2*w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+3*w, posy, rad, 6);
-  noFill();
-  polygon(posx+4*w, posy, rad, 6);
-  polygon(posx+5*w, posy, rad, 6);
-  polygon(posx+6*w, posy, rad, 6);
-
-  //second row
-  push();
-  translate(w2, h2);
-  polygon(posx, posy, rad, 6);
-  polygon(posx+w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+2*w, posy, rad, 6);
-  polygon(posx+3*w, posy, rad, 6);
-  noFill();
-  polygon(posx+4*w, posy, rad, 6);
-  polygon(posx+5*w, posy, rad, 6);
-  pop();
-
-  //third row
-  push();
-  translate(0,2*h2);
-  polygon(posx, posy, rad, 6);
-  polygon(posx+w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+2*w, posy, rad, 6);
-  noFill();
-  polygon(posx+3*w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+4*w, posy, rad, 6);
-  noFill();
-  polygon(posx+5*w, posy, rad, 6);
-  polygon(posx+6*w, posy, rad, 6);
-  pop();
-
-  //fourth row
-  push();
-  translate(w2, 3*h2);
-  polygon(posx, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+w, posy, rad, 6);
-  noFill();
-  polygon(posx+2*w, posy, rad, 6);
-  polygon(posx+3*w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+4*w, posy, rad, 6);
-  noFill();
-  polygon(posx+5*w, posy, rad, 6);
-  pop();
-
-  //fifth row
-  push();
-  translate(0, 4*h2);
-  polygon(posx, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+w, posy, rad, 6);
-  polygon(posx+2*w, posy, rad, 6);
-  polygon(posx+3*w, posy, rad, 6);
-  polygon(posx+4*w, posy, rad, 6);
-  polygon(posx+5*w, posy, rad, 6);
-  noFill();
-  polygon(posx+6*w, posy, rad, 6);
-  pop();
-
-  //sixth row
-  push();
-  translate(w2, 5*h2);
-  fill(colorFront1);
-  polygon(posx, posy, rad, 6);
-  noFill();
-  polygon(posx+w, posy, rad, 6);
-  polygon(posx+2*w, posy, rad, 6);
-  polygon(posx+3*w, posy, rad, 6);
-  polygon(posx+4*w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+5*w, posy, rad, 6);
-  noFill();
-  pop();
-
-  //seventh row
-  push();
-  translate(0, 6*h2);
-  fill(colorFront1);
-  polygon(posx, posy, rad, 6);
-  noFill();
-  polygon(posx+w, posy, rad, 6);
-  polygon(posx+2*w, posy, rad, 6);
-  polygon(posx+3*w, posy, rad, 6);
-  polygon(posx+4*w, posy, rad, 6);
-  polygon(posx+5*w, posy, rad, 6);
-  fill(colorFront1);
-  polygon(posx+6*w, posy, rad, 6);
-  pop();
-}
 
 function polygon(x, y, radius, npoints) {
   let angle = TWO_PI / npoints
