f66c48f3b742dfcece204bbffd53881a90b013ca
Hannah Yee <hannahy@hotmail.co.nz>
2019-04-09 16:35:24 +1200

    interaction

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 ## PS2 MDDN 242 2019
-I have completed the whole alphabet and numbers.
+I have added in the rotation for the interaction...
+However i am unsure why the ending spot of the rotation is leaving the letter on a weird angle.
 
-I aimed to keep my designs to use shapes as close to a triangle as possible without turning them into lines too often.
-
-The sketches i made on paper have been completely altered in my final design as i have been able to experiment and see what other combinations of triangles i could make to form the characters and numbers.
-
-My original sketches i drew out were possible but i managed to be more creative when actually implementing the work.
-
-My next step is to play around with the interaction and decide on the exhibition words.
+Need to work out how to fix this...
--- draw_letters.js
+++ draw_letters.js
@@ -1,49 +1,61 @@
-const colorFront1  = "#fae";
+const colorFront1  = "#fae"; 
 const colorFront2  = "#59ccff";
 const colorStroke  = "#FFFFFF";
 
-/*
- * Draw the letter given the letterData
- *
- * Letters should always be drawn with the
- * following bounding box guideline:
- * from (0,0) to (100, 200)
- */
 function drawLetter(letterData) {
   // color/stroke setup
   stroke(colorStroke);
   strokeWeight(4);
 
-  // determine parameters for second circle
-  //let size2 = letterData["size"];
+  // triangle parameters
 
-  let FirstPosx = letterData["T11x"];
-  let FirstPosy = letterData["T11y"]
-  let SecondPosx = letterData["T12x"];
-  let SecondPosy = letterData["T12y"];
-  let ThirdPosx = letterData["T13x"];
-  let ThirdPosy = letterData["T13y"];
+  let FirstPosx = letterData["T11x"]; // x First point of triangle 1
+  let FirstPosy = letterData["T11y"] // y First point of triangle 
+  let SecondPosx = letterData["T12x"]; // x Second point of triangle 1
+  let SecondPosy = letterData["T12y"]; // y Second point of triangle 1
+  let ThirdPosx = letterData["T13x"]; // x Third point of triangle 1
+  let ThirdPosy = letterData["T13y"]; // y Third point of triangle 1
 
-  let SFirstPosx = letterData["T21x"];
-  let SFirstPosy = letterData["T21y"]
-  let SSecondPosx = letterData["T22x"];
-  let SSecondPosy = letterData["T22y"];
-  let SThirdPosx = letterData["T23x"];
-  let SThirdPosy = letterData["T23y"];
+  let SFirstPosx = letterData["T21x"]; // x First point of triangle 2
+  let SFirstPosy = letterData["T21y"] // y First point of triangle 2
+  let SSecondPosx = letterData["T22x"]; // x Second point of triangle 2
+  let SSecondPosy = letterData["T22y"]; // y Second point of triangle 2
+  let SThirdPosx = letterData["T23x"]; // x Third point of triangle 2
+  let SThirdPosy = letterData["T23y"]; // y Third point of triangle 2
 
-  // draw two circles
-//  var size = width * 0.05;
-// translate(width/2, height/2);
-// triangle(0, 0, size, size*2, -size, size*2);
-// }
+  let rotatetri1 = letterData ["rot1"]; // Rotation for first triangle
+  let rotatetri2 = letterData ["rot2"]; // Rotation for second triangle
 
-//draw first tri
-fill(colorFront1);
-triangle(FirstPosx, FirstPosy, SecondPosx, SecondPosy, ThirdPosx, ThirdPosy);
+push();
+translate(50,70);
+scale(0.5);
 
-//draw second tri
-fill(colorFront2);
-triangle(SFirstPosx, SFirstPosy, SSecondPosx, SSecondPosy, SThirdPosx, SThirdPosy);
+  push();
+    rotate(rotatetri1);
+    triangle(FirstPosx, FirstPosy, SecondPosx, SecondPosy, ThirdPosx, ThirdPosy);
+  pop(); 
+
+  push();
+    rotate(rotatetri2)
+    triangle(SFirstPosx, SFirstPosy, SSecondPosx, SSecondPosy, SThirdPosx, SThirdPosy);
+  pop();
+
+  //draw first tri
+  push();
+    fill(colorFront1);
+    rotate(rotatetri1);
+    triangle(FirstPosx, FirstPosy, SecondPosx, SecondPosy, ThirdPosx, ThirdPosy);
+  pop();
+
+  //draw second tri
+  push();
+    fill(colorFront2);
+    rotate(rotatetri2);
+    triangle(SFirstPosx, SFirstPosy, SSecondPosx, SSecondPosy, SThirdPosx, SThirdPosy);
+  pop();
+
+pop();
+
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
@@ -60,7 +72,8 @@ function interpolate_letter(percent, oldObj, newObj) {
   new_letter["T22y"] = map(percent, 0, 100, oldObj["T22y"], newObj["T22y"]);  
   new_letter["T23x"] = map(percent, 0, 100, oldObj["T23x"], newObj["T23x"]);
   new_letter["T23y"] = map(percent, 0, 100, oldObj["T23y"], newObj["T23y"]);
-
+  new_letter["rot1"] = map(percent, 0, 100, oldObj["rot1"], newObj["rot1"]);
+  new_letter["rot2"] = map(percent, 0, 100, oldObj["rot2"], newObj["rot2"]);
 
  
 
@@ -69,7 +82,7 @@ function interpolate_letter(percent, oldObj, newObj) {
 }
 
 var swapWords = [
-  "ABBAABBA",
-  "CAB?CAB?",
-  "BAAAAAAA"
+  "SHARPEST",
+  "TRIANGLE",
+  "ALPHABET"
 ]
