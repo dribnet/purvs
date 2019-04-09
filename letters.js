70298f4c7cffc431c51a229f4798d52ed291bdfd
aocqiu <15853213130@163.com>
2019-04-09 15:38:18 +1200

    1

--- letters.js
+++ letters.js
@@ -1,22 +1,76 @@
 const alphabet = {
-  "default": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 0
-  },
+    "default": {
+    "size1": 50,
+    "size2": 25,
+    "size3": 12.5,
+    "size4": 12.5,
+
+    "posX1": 0,
+    "posY1": 0,
+
+    "posX2": 0,
+    "posY2": 0,
+
+    "posX3": 0,
+    "posY3": 0,
+
+    "posX4": 0,
+    "posY4": 0,
+  },  
+
   "A": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
+    "size1": 50,
+    "size2": 25,
+    "size3": 12.5,
+    "size4": 12.5,
+
+    "posX1": 0,
+    "posY1": 0,
+
+    "posX2": 25,
+    "posY2": 25,
+
+    "posX3": 6.25,
+    "posY3": 112.5,
+
+    "posX4": 68.75,
+    "posY4": 112.5,
+  },  
+
   "B": {
-    "size": 75,
-    "offsetx": 0,
-    "offsety": -70
-  },
+    "size1": 25,
+    "size2": 50,
+    "size3": 25,
+    "size4": 25,
+
+    "posX1": 25,
+    "posY1": 50,
+
+    "posX2": 0,
+    "posY2": 0,
+
+    "posX3": 25,
+    "posY3": 25,
+
+    "posX4": 25,
+    "posY4": 75,
+  },  
   "C": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size1": 50,
+    "size2": 25,
+    "size3": 12.5,
+    "size4": 12.5,
+
+    "posX1": 0,
+    "posY1": 0,
+
+    "posX2": 25,
+    "posY2": 25,
+
+    "posX3": 6.25,
+    "posY3": 112.5,
+
+    "posX4": 68.75,
+    "posY4": 112.5,
   }
 }
