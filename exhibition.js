8091304bccad3e6ce716ec8dfc063f9c92463f42
unknown <jamesmagallanes@gmail.com>
2019-04-12 04:29:53 +1200

    Updated interaction and exhibition pages to match colour scheme.

--- exhibition.js
+++ exhibition.js
@@ -1,7 +1,7 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
+const colorBack    = "#53565b";
 const colorFront   = "#199cff";
 const colorLines   = "#000090";
 
--- interaction.js
+++ interaction.js
@@ -1,7 +1,7 @@
 /*
  * Here are some things you can edit
  */
-const colorBack    = "#e3eded";
+const colorBack    = "#53565b";
 const colorLines   = "#000090";
 
 /* 
