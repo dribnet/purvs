b2af3c29917fc9256f0d3ca4650d000e607d8372
Ziyi Chen <chenziyi2@myvuw.ac.nz>
2019-04-11 17:35:56 +1200

    my sketch

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
+  "size": 100,
   "offsetx": 0,
   "offsety": 35
 }
 
 const letterB = {
-  "size": 150,
+  "size": 100,
   "offsetx": 0,
   "offsety": -145
 }
@@ -30,8 +30,9 @@ const letterC = {
   "offsety": 0
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
+
+const colorFront2  = "#900C3F";
+const colorFront1  = "#581845";
 const colorBack    = "#e3eded";
 const colorStroke  = "#233f11";
 
@@ -54,11 +55,76 @@ function drawLetter(posx, posy, letterData) {
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  // draw A
+  stroke(colorFront1);
+  noFill();
+  strokeWeight(2);
+  arc(220,100,size2,size2,radians(60),radians(200));
+  arc(220,100,size2,size2,radians(250),radians(30));
+  line(203,53,185,100);
+  line(216,50,187,137);
+  line(206,80,244,142);
+  line(215,50,263,124);
+
+  strokeWeight(10);
+  stroke(colorFront2);
+  point(244,142);
+  point(185,99);
+
+    // Z
+    stroke(colorFront1);
+  noFill();
+  strokeWeight(2);
+  arc(100,100,size2,size2,radians(330),radians(210));
+  arc(100,100,size2,size2,radians(250),radians(290));
+  line(58,74,100,100);
+  line(58,123,100,100);
+  line(83,53,150,100);
+  line(61,130,105,105);
+  line(149,108,121,89);
+stroke(colorFront2);
+  strokeWeight(10);
+  point(58,74);
+  point(142,74);
+
+  
+      // L
+  strokeWeight(2);
+  arc(460,100,100,100,radians(60),radians(200));
+  arc(460,100,100,100,radians(250),radians(30));
+
+beginShape();
+vertex(442, 54);
+vertex(442, 80);
+vertex(505,80);
+endShape(OPEN);
+  line(424,66,424,105);
+  line(424,105,509,105);
+  strokeWeight(10);
+  point(424,66);
+  point(503,126);
+  
+   
+    // M
+  strokeWeight(2);
+  arc(340,100,100,100,radians(160),radians(200));
+  arc(340,100,100,100,radians(250),radians(130));
+
+beginShape();
+vertex(303, 88);
+vertex(291, 101);
+vertex(336,119);
+vertex(389, 101);
+vertex(375, 87);
+endShape(OPEN);
+  line(293,82,338,100);
+  line(338,100,386,82);
+  line(307,109,307,138);
+  line(370,109,370,140);
+  strokeWeight(10);
+  point(322,54);
+  point(293,117);
+
 }
 
 function draw () {
