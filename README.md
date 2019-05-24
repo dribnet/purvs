56e21f4b0303855520791e6b644a6d27628bedda
Caitlin Sue <caitlinsue06@gmail.com>
2019-05-24 15:33:21 +1200

    updated files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+The images I used for todays experiment are of graffiti around Wellington. I am still trying to come up with a meaningful theme but for now I am just experimenting with the code.
+
+The output I came up with today was sort of an accident - I drew a line but didn't change the second x and y positions so they are all drawing to one point. I actually like how this turned out and I think it's interesting how it pulls the colours out.
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -31,11 +31,17 @@ function draw () {
     let pointSize = 20;
     let halfSize = 50;
     fill(pix);
+    stroke(pix)
+
+
+//WHITE MASK --------------------------------------------------------
     if(mask[0] > 128) {
       ellipse(x, y, pointSize, pointSize);
     }
+
+//BLACK MASK --------------------------------------------------------
     else {
-      rect(x, y, pointSize, pointSize);    
+      line(x, y, pointSize, pointSize);
     }
   }
   renderCounter = renderCounter + 1;
@@ -43,12 +49,198 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
-function keyTyped() {
+function keyTyped () {
   if (key == '!') {
     saveBlocksImages();
   }
 }
+
+// function drawGlyph () {
+//     this.draw = function(values, size) {
+//   let H = values [0];
+//   let S = values [1];
+//   let L = values [2];
+
+// angleMode(DEGREES);
+
+// translate(0.5* size, 0.5* size);
+
+// let offset = size/2;
+
+// //SEGMENT SIZE -------------------------------------------------------
+
+// //map hue to the 2 bottom segment sizes
+// //if statement that makes the highest and lowest range look the same
+// if (H <= 180){
+//   var hueSegSize = map (H, 0, 360, 0.2, 1.8);
+// }
+
+// if (H > 180){
+//   var hueSegSize = map (H, 0, 360, 1.8, 0.2);
+// }
+
+// //map saturation to the top segment size
+// var satSegSize = map (S, 0, 100, 0.2, 1);
+
+// //SCALE ---------------------------------------------------------------
+
+// //map lightness to scale of glyph
+// var lightScale = map (L, 0, 100, 0.5, 1);
+
+// //FILL COLOUR ---------------------------------------------------------
+
+// //map hue to 2 segment fill colours
+// //if statement that makes highest and lowest range look the same
+// if (H <= 180){
+//   var hueFill = map (H, 0, 360, 20, 150);
+// }
+
+// if (H > 180){
+//   var hueFill = map (H, 0, 360, 150, 20);
+// }
+
+// //map saturation to 1 segment fill colour
+// var satFill = map (S, 0 ,100, 20, 90);
+
+// //STROKE COLOUR --------------------------------------------------------
+
+// //map hue to 2 segment stroke colours
+// if (H <= 180){
+//   var hueStroke = map (H, 0, 360, 100, 20);
+// }
+
+// if (H > 180){
+//   var hueStroke = map (H, 0, 360, 20, 100);
+// }
+
+// //map saturation to stroke colour
+// var satStroke = map (S, 0 ,100, 90, 20);
+
+// //SEGMENT LOOPS ---------------------------------------------------------
+
+// //for loop that repeats and rotates each segment
+// for (var i=0; i<3; i++){
+
+//   //HUE segment
+//   if(i==0){
+//     push();
+
+//     //determine stroke colour using saturation variable
+//     strokeUniform(satStroke);
+//     strokeWeight(1);
+
+//     //determine quad fill using saturation variable
+//     fillUniform(satFill);
+
+//     //rotate quad 3 times
+//     rotate (i*120);
+
+//     //use lightness variable to scale glyph
+//     scale(lightScale);
+
+//     //determine quad points using saturation variable
+//     drawSegment(satSegSize);
+
+//     //repeat segments to create lines
+//     for (var j=0; j<3; j++){
+//       drawSegment(satSegSize-i*0.1, size);
+//     }
+//     pop();
+//   }
+
+//   //SATURATION segment
+//   if(i==1){
+//     push();
+
+//     strokeUniform(hueStroke);
+//     strokeWeight(1);
+
+//     fillUniform(hueFill);
+
+//     rotate (i*120);
+
+//     scale(lightScale);
+
+//     drawSegment(hueSegSize);
+    
+//     for (var j=0; j<3; j++){
+//       drawSegment(hueSegSize-i*0.01, size);
+//     }
+//     pop();
+//   }
+
+//   //LIGHTNESS segment
+//   if(i==2){
+//     push();
+
+//     strokeUniform(hueStroke);
+//     strokeWeight(1);
+
+//     fillUniform(hueFill);
+
+//     rotate (i*120);
+
+//     scale(lightScale);
+
+//     drawSegment(hueSegSize);
+
+//     for (var j=0; j<3; j++){
+//       drawSegment(hueSegSize-i*0.005, size);
+//     }
+//     pop();
+//   }
+// }
+
+// //OUTER ELLIPSES ------------------------------------------------------------------------
+// //determine ellipse size
+// let cur_radius1 = size / 2;
+// let cur_radius2 = size / 2.5;
+
+// //visual info for ellipses
+// fillUniform(50);
+// noStroke();
+
+// //draws ellipses 3 times around circle
+// for (var i=0; i<3; i++){
+//   let angle = 360*i/3;
+//   let pos1_x = cur_radius1 * sin(angle)
+//   let pos1_y = cur_radius1 * cos(angle)
+//   ellipse(pos1_x, pos1_y, size/60);
+//   let pos2_x = cur_radius2 * sin(angle)
+//   let pos2_y = cur_radius2 * cos(angle)
+//   ellipse(pos2_x, pos2_y, size/30);
+// }
+
+// //FUNCTIONS ------------------------------------------------------------------------------
+
+// //segment function
+// function drawSegment(scaling){
+//   //center of canvas
+//   var pixel = 0.005*size;
+//   scale(scaling);
+
+//   //draws quad
+//   quad (pixel*0,pixel*0, pixel*-85,pixel*-49.2, pixel*0, pixel*-100, pixel*85,pixel*-49.2);
+// }
+//   }
+
+// }
+
+// //circle functions
+// function onSphere(radius, angle){
+//   return [sin(angle)*radius, cos(angle)*radius]
+// }
+
+// //outer ellipse functions
+// function drawEllipse(ellipseScale, ellipseXpos, ellipseYpos, size){
+//   //center of canvas
+//   var placement = 0.005*size;
+//   //draw ellipse
+//   ellipse (placement*ellipseXpos, placement*ellipseYpos, ellipseScale, ellipseScale);
+// }
+
+// }
