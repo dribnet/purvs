950a25f8241f33935a80ddbcb4e8f050d9ecc81a
Leigh Tuala-Lauvao <tualalauvao@gmail.com>
2019-05-28 11:43:35 +1200

    Part 2- Theme images

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-- Friday 5:21pm:
+Updated the code and changed it - it now looks way different than how it originally did. I used another image I have of an uncle playing basketball. In relation to my theme, Pasifika Identity in Aotearoa New Zealand, I look at it like Pasifika persons aren't limited to their cultural performances. For instance, in my family, we are culturally and physically diverse in the fact that a lot of them play sports (mainly basketball) and I guess it just highlights a sense of diversity within the Pasifika culture. However, trying not to get too preachy, it is a different portrayal of a pasifika male participating in an activity that is not cultural.
 
-I tried doing lines with my image but it didn't look so well, maybe I will try to play around with the code more. Changed colours of the transparent highlights that I added.
-
-Will look at the code and play around with it more.
-
-- Sunday 26 May - 6:00pm
-
-I couldn't commit from home and didn't want to break my files so I'll just write the day and what I changed because I'm still experimenting with my images and the code. I added a 3rd image- greyscale image too. With the masking I tried to see what it would be like to also mask bits of the background that aren't a part of the subject. Again, still experimenting, trying to see what images I will use and how I will implement the code with them.
-
-- Sunday 26 May - 8:27pm
-
-I added in a different image of my dads tattoo that I took about 2 years ago. With my masking, I had a black background and coloured the patterns black, giving it a kind of highlight so the viewer can see the patterns.
-
-I want to try and have some lines of some sort in the background in a different colour, while have the actual subject coded in a way that they become the focal point- will play with masking and code.
-
-Also want to try with other images. Since my theme is Pasifika Identity in Aotearoa New Zealand I was thinking of not only having images of my family doing cultural performances, but other images of them participating in sports such as basketball because I have images of family that are in our home photo albums.
+In terms of code, I wanted to use white strokes of the patterns in the background to emphasise and really highlight my subject as the focal point. Creates a contrast too. Maybe in my masking I will go back and blur the faces in all my images out more. 
--- sketch.js
+++ sketch.js
@@ -1,23 +1,23 @@
 // NEW CODE AFTER COMMIT FOR OLDER CODE //
 
 // /* Set to true to make final high-resolution version */
-// const finalVersion = true;
+const finalVersion = true;
 //
 // /* Default versions of variables */
-// let pointSize = 40;
+let pointSize = 40;
 //
 // /* Override some variables with high-resolution final version */
-// if (finalVersion) {
-//   pointSize = 10;
-// }
+if (finalVersion) {
+  pointSize = 10;
+ }
 
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -31,11 +31,11 @@ function setup () {
   imageMode(CENTER);
 
   //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-  // stroke(0);
-  // background(255, 90, 0, 100);
-
-  noStroke();
+  stroke(0);
   background(255);
+
+  // noStroke();
+  // background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -43,68 +43,68 @@ function setup () {
 function draw () {
 
   //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-  // for(let i=0;i<1000/pointSize;i++) {
-  //   let x = int(i * pointSize);
-  //  let y = int(renderCounter * pointSize);
+  for(let i=0;i<1000/pointSize;i++) {
+    let x = int(i * pointSize);
+   let y = int(renderCounter * pointSize);
 
-  for(let i=0;i<1900;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  // for(let i=0;i<1900;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
 
 //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-    //let halfSize = pointSize/2;
+    let halfSize = pointSize/2;
 
-    let pointSize = 20;
-    let halfSize = 40;
-    let pointSize2 = 2;
-    let halfSize2 = 10;
+    // let pointSize = 20;
+    // let halfSize = 40;
+    // let pointSize2 = 2;
+    // let halfSize2 = 10;
 
     fill(pix);
     //stroke(pix);
-
+    
     if(mask[0] > 128) {
       // OLD CODE - COMMIT
-      rect(x, y, pointSize, pointSize);
-      fill(255, 101, 38, 30);
-      stroke(0);
-      strokeWeight(0.1);
-      rect(x, y, pointSize, pointSize);
+      // rect(x, y, pointSize, pointSize);
+      // fill(255, 101, 38, 30);
+      // stroke(0);
+      // strokeWeight(0.1);
+      // rect(x, y, pointSize, pointSize);
 
 
 //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-      //rect(x, y, pointSize*1.5, pointSize*1.5);
+      rect(x, y, pointSize*1.5, pointSize*1.5);
 
     }
     else {
       // OLD CODE - COMMIT
-      rect(x, y, halfSize2, pointSize);
-      fill(110, 80, 38, 40);
-      rect(x, y, pointSize2, halfSize);
+      // rect(x, y, halfSize2, pointSize);
+      // fill(110, 80, 38, 40);
+      // rect(x, y, pointSize2, halfSize);
 
 
 //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-      // blur(3);
-      // rect(x-halfSize, y-halfSize, pointSize*.8, pointSize*.8,8);
+      blur(3);
+      rect(x-halfSize, y-halfSize, pointSize*.8, pointSize*.8,8);
 
     }
   }
   renderCounter = renderCounter + 1;
 
   //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
-  // if(renderCounter > 1920/pointSize) {
+  if(renderCounter > 1920/pointSize) {
 
-  if(renderCounter > 10) {
+  // if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
      //saveArtworkImage(outputFile);
   }
-}
 
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
 }
