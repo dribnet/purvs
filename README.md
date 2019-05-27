8db7d4b44f8248dc53e268ee6daa205d47f2ce49
Leigh Tuala-Lauvao <tualalauvao@gmail.com>
2019-05-27 15:04:06 +1200

    Sketch edits

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 ## Creative Coding 2: Custom Pixel
 
-Photographic Sketch Update - Part 1.
+- Friday 5:21pm:
 
-I used a second image and put it as input2 just to see what the 2 images look like next to eachother. I think I want to try and incorporate lines like in the example code. 
+I tried doing lines with my image but it didn't look so well, maybe I will try to play around with the code more. Changed colours of the transparent highlights that I added.
+
+Will look at the code and play around with it more.
+
+- Sunday 26 May - 6:00pm
+
+I couldn't commit from home and didn't want to break my files so I'll just write the day and what I changed because I'm still experimenting with my images and the code. I added a 3rd image- greyscale image too. With the masking I tried to see what it would be like to also mask bits of the background that aren't a part of the subject. Again, still experimenting, trying to see what images I will use and how I will implement the code with them.
+
+- Sunday 26 May - 8:27pm
+
+I added in a different image of my dads tattoo that I took about 2 years ago. With my masking, I had a black background and coloured the patterns black, giving it a kind of highlight so the viewer can see the patterns.
+
+I want to try and have some lines of some sort in the background in a different colour, while have the actual subject coded in a way that they become the focal point- will play with masking and code.
+
+Also want to try with other images. Since my theme is Pasifika Identity in Aotearoa New Zealand I was thinking of not only having images of my family doing cultural performances, but other images of them participating in sports such as basketball because I have images of family that are in our home photo albums.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,23 @@
+// NEW CODE AFTER COMMIT FOR OLDER CODE //
+
+// /* Set to true to make final high-resolution version */
+// const finalVersion = true;
+//
+// /* Default versions of variables */
+// let pointSize = 40;
+//
+// /* Override some variables with high-resolution final version */
+// if (finalVersion) {
+//   pointSize = 10;
+// }
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
 let sourceFile = "input_2.jpg";
 let maskFile   = "mask_2.png";
-let outputFile = "artwork_2.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -16,6 +29,11 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
+
+  //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+  // stroke(0);
+  // background(255, 90, 0, 100);
+
   noStroke();
   background(255);
   sourceImg.loadPixels();
@@ -23,31 +41,65 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<1500;i++) {
+
+  //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+  // for(let i=0;i<1000/pointSize;i++) {
+  //   let x = int(i * pointSize);
+  //  let y = int(renderCounter * pointSize);
+
+  for(let i=0;i<1900;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 13;
-    let halfSize = 50;
+
+//NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+    //let halfSize = pointSize/2;
+
+    let pointSize = 20;
+    let halfSize = 40;
+    let pointSize2 = 2;
+    let halfSize2 = 10;
+
     fill(pix);
+    //stroke(pix);
+
     if(mask[0] > 128) {
+      // OLD CODE - COMMIT
       rect(x, y, pointSize, pointSize);
-      //fill(200, 90, 55, 20);
-      ellipse(x, y, pointSize, pointSize);
+      fill(255, 101, 38, 30);
+      stroke(0);
+      strokeWeight(0.1);
+      rect(x, y, pointSize, pointSize);
+
+
+//NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+      //rect(x, y, pointSize*1.5, pointSize*1.5);
+
     }
     else {
-      ellipse(x, y, pointSize, pointSize);
-      fill(0, 65, 100, 40);
-      rect(x, y, pointSize, pointSize); 
+      // OLD CODE - COMMIT
+      rect(x, y, halfSize2, pointSize);
+      fill(110, 80, 38, 40);
+      rect(x, y, pointSize2, halfSize);
+
+
+//NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+      // blur(3);
+      // rect(x-halfSize, y-halfSize, pointSize*.8, pointSize*.8,8);
+
     }
   }
   renderCounter = renderCounter + 1;
+
+  //NEW CODE FOR AFTER COMMIT OF OLDER CODE//
+  // if(renderCounter > 1920/pointSize) {
+
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    //saveArtworkImage(outputFile);
+     //saveArtworkImage(outputFile);
   }
 }
 
