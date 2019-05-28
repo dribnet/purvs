43713f43e96376b21fa902beb0116ad84dc93817
Maia Hill <maiatwhill@gmail.com>
2019-05-28 14:58:12 +1200

    editing

--- sketch.js
+++ sketch.js
@@ -21,56 +21,62 @@ function setup () {
   maskImg.loadPixels();
 }
 
-const tile_width = 4;
+const tile_width = 3;
 const tile_height = 10;
 const tile_step_x = 6;
 const tile_step_y = 10;
 
 function draw () {
+  //   background(0, 102, 51);
 
-
-  // version 1: just draw all the tiles
-  for(let y=0; y<height; y = y + tile_step_y) {
-    for(let x=0; x<width; x = x + tile_step_x) {
-      let pix = sourceImg.get(x, y);
-      let mask = maskImg.get(x, y);
-      fill(pix);
-      if(mask[0] > 128) {
-        stroke(pix);
-        ellipse(x, y, tile_step_x, tile_step_y);
-      }
-      else {
-        noStroke();
-        fill(0, 153, 76);
-        rect(x, y, tile_width, tile_height);
-      }
-    }
-  }
-
-  // for(let i=0;i<5000;i++) {
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pix = sourceImg.get(x, y);
-  //   let mask = maskImg.get(x, y);
-  //   let pointSize = 20;
-  //   let rectSize = 2;
-  //   let halfSize = 6;
-  //   let halfrectSize = 6;
-  //   if(mask[0] > 128) {
+  // // version 1: just draw all the tiles
+  // for(let y=0; y<height; y = y + tile_step_y) {
+  //   for(let x=0; x<width; x = x + tile_step_x) {
+  //     let pix = sourceImg.get(x, y);
+  //     let mask = maskImg.get(x, y);
+  //     let pointSize = 30;
+  //      let rectSize = 40;
+  //      let halfSize = 5;
+  //      let halfrectSize = 50;
   //     fill(pix);
-  //     stroke(pix);
-  //     rect(x, y, rectSize, halfrectSize);   
-  //     ellipse(x, y, pointSize, halfSize); 
+  //     if(mask[0] > 128) {
+  //       fill(pix);
+  //       stroke(pix);
+  //       ellipse(x, y, pointSize, halfSize);
+  //       rect(x-30, y, rectSize, halfrectSize);   
+  //     }
+  //     else {
+  //       noStroke();
+  //       fill(0, 153, 76);
+  //       rect(x, y, tile_width, tile_height);
+  //     }
   //   }
-  //   else {
-  //     fill(253, 196, 62);
-  //     stroke(253, 196, 62);
-  //     ellipse(x, y, halfSize, pointSize);
-  //     fill(255, 204, 51);
-  //     stroke(255, 204, 51);
-  //     ellipse(x, y, pointSize, halfSize); 
-  //   }
+  // }
 
+  for(let i=0;i<5000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    let pointSize = 20;
+    let rectSize = 2;
+    let halfSize = 6;
+    let halfrectSize = 6;
+    if(mask[0] > 128) {
+      fill(pix);
+      stroke(pix);
+      rect(x, y, rectSize, halfrectSize);   
+      ellipse(x, y, pointSize, halfSize); 
+    }
+    else {
+      fill(253, 196, 62);
+      stroke(253, 196, 62);
+      rect(x, y, halfSize, pointSize);
+      fill(255, 204, 51);
+      stroke(255, 204, 51);
+      rect(x, y, pointSize, halfSize); 
+    }
+}
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
