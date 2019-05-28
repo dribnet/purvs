1d4624db33fbdfd3e2a99d6df7d933d577845476
Robert Wilkins <robertwilkins741@gmail.com>
2019-05-28 14:37:16 +1200

    Second pass at petals

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Creative Coding 2: Custom Pixel
 
 
-The left image is my first pass at this assignment. It's just a placeholder image at the moment, but it depicts my very precious and dear late pet, so it still has personal significance.  I'm considering it somewhat of a failed experiment. My vision for this image was where ever there is mask, a series of petals unfold toward and frame the subject. However, it either needs a ton of iteration or it's not a very good idea.
+#### Theme
+My theme for enjoying and appreciating life. This images are a series of smiling people. Left image is me after I'd gone hiking along the top of a hill in a thunderstorm at midnight, and I love storms so it was a wonderful experience. The middle is an image of my friend when he was here on holdiay, and we were standing in front a waterfall, and the third is my parents on their 26th year of unbelievably happy marriage. The petals are a play on stop to smell the roses.
 
+#### Technical
 How it works is a modified version of the base pointillism example except where the mask is above 50, draw a big petal graphic that's been tinted a certain colour. The colour fades based on the alpha value between the colour of the pixel and cyan. The actual petal graphic is an image that's randomly chosen from a range of white images with a transparent backgrounds. This has some cool potential applications, where now I can choose an arbitrary series of graphics and use them to build my image, but the current iteration is a bit lack luster.  
--- sketch.js
+++ sketch.js
@@ -3,21 +3,19 @@ let maskImg=null;
 let renderCounter=0;
 let graphicElements=[];
 
-let sourceFile = "input_5.jpg";
+let sourceFile = "input_3.jpg";
 let maskFile   = "mask_5.png";
-let outputFile = "artwork_5.png";
+let outputFile = "artwork_3.png";
 
-let graphicFile1 = "graphic_2.png";
-let graphicFile2 = "graphic_3.png";
+let graphicFile1 = "graphic.png";
+// let graphicFile2 = "graphic_3.png";
 
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
   graphicElements[0] = loadImage(graphicFile1);
-  graphicElements[1] = loadImage(graphicFile2);
-
-
+  // graphicElements[1] = loadImage(graphicFile2);
 }
 
 function setup () {
@@ -33,17 +31,17 @@ function setup () {
 }
 
 function draw () {
-  let petalColour = color(120, 255, 255, 100);
+  let petalColour = color(194, 30, 86, 100);
   angleMode(DEGREES);
-  for(let i=0;i<1500;i++) {
+  for(let i=0;i<3000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = color(sourceImg.get(x, y));
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
+    let pointSize = 10;
     let halfSize = 50;
     if(mask[0] > 50) {
-      if (random(0,1) > 0.05) continue;
+      if (random(0,1) > 0.01) continue;
       let petalCol = lerpColor(pix, petalColour, mask[0]/500.0);
       petalCol = color(petalCol.levels[0], petalCol.levels[1], petalCol.levels[2]);
       push();
@@ -51,12 +49,13 @@ function draw () {
         rotate(random(-20, 20));
         if (random(0, 1) > 0.5 ) scale(-1, 1);
         tint(petalCol);
-        image(graphicElements[round(random(0,1))], 0 , 0);
+        image(graphicElements[0], 0 , 0);
       pop();
     }
     else {
       fill(pix);
-      ellipse(x, y, pointSize, pointSize);
+      let randSize = random(0.3, 1.2);
+      ellipse(x, y, pointSize*randSize, pointSize*randSize);
 
     }
   }
