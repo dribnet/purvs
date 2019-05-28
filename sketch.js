676f4083d55328033a30115f23855a8773e6e422
LeKringe <augavi@myvuw.ac.nz>
2019-05-28 13:52:30 +1200

    Changed thumbnail, preview and Artwork.

--- sketch.js
+++ sketch.js
@@ -2,9 +2,17 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+// let sourceFile = "input_1.jpg";
+// let maskFile   = "mask_1.png";
+// let outputFile = "artwork_1.png";
+
+// let sourceFile = "input_2.jpg";
+// let maskFile   = "mask_2.png";
+// let outputFile = "artwork_2.png";
+
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,12 +31,12 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
+  for(let i=0;i<5000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
+    let pointSize = 10;
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
@@ -43,7 +51,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
