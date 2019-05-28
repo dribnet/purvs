ee6d71048d8a92b68305fd947490e7452ec4a7d0
Guest User <Guest@vs320-07-09.mddn.vuw.ac.nz>
2019-05-28 16:33:59 +1200

    test push of possible photographs.

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,19 +23,31 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<3000;i++) {
+
+  //fill(255, 227, 117,20);
+  //rect(0,0,704,1252);
+
+  for(let i=0;i<8000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
+    let pointSize = 15;
+    let halfSize = 30;
     fill(pix);
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+      ellipse(x, y, pointSize+2, pointSize+2);
+      push(); 
+      fill(200,0,0,10);
+      ellipse(x, y, pointSize-2, pointSize-2);   
+      pop();
+      fill(0,30,200,10);
+      ellipse(x, y, pointSize-2, pointSize-2);   
+      pop();
     }
     else {
-      rect(x, y, pointSize, pointSize);    
+      rect(x, y, pointSize, pointSize); 
+
     }
   }
   renderCounter = renderCounter + 1;
