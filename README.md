d56176e9d2c30c046ab930c23389f14a54868d14
Dylan Wansbrough <33749606+Dylan-Wansbrough@users.noreply.github.com>
2019-05-28 14:22:28 +1200

    Testing sepia

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Progress 
+I have got my photo to start appearing in sepia. It was a pain but Ive mapped it too the lightness and now I need to look at making it look damaged then polishing it from there. 
+
+## Idea 
+So my idea is taking a image and making it look like a old timey photo. I want to take photos of modern tecnology and make them look like old pictures. This is to show the porgression of technology and how what we have now will soon be an obselete technoology. I would do this by changing the colours of pixels to make it look greyscale or old brown tinted photos. I would use the mask to mark where to make scracthes and scuffss etc. 
+
 ## My curent Ideas
 I missed a couple classes so I've been trying to get back on track. Looking through all of the stuff the one that really stood out to me was Ryan Alexanders work. The white or whitish image on a dark background I feel couold be a realy interesting tool to explore. In Ryans case I think the image comes out kinda old looking and that is kinda intersting. I just did a little testing with it.
 
--- sketch.js
+++ sketch.js
@@ -22,23 +22,54 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tileHeight = 1;
+const tileWidth = 2;
+
+
+
 function draw () {
-  for(let i=0;i<12000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 50;
-    fill(252);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      fill(32, 33, 27);
-      ellipse(x, y, pointSize, pointSize);    
-    }
+
+  for(let y = 0; y<height; y= y + tileHeight){
+      for(let x = 0; x<width; x= x + tileHeight){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+
+        colorMode(HSB,360);
+
+        //sepia colour range
+        let darkest = color(21, 73, 54); 
+        let lightest = color(23,99,192); 
+
+        let lightness = map(pix[1], 0, 240, 0, 1);
+
+        let sepia = lerpColor(darkest, lightest, lightness);
+
+        fill(sepia);
+        if(mask[0] > 128) {
+         rect(x, y, tileHeight, tileWidth);
+        }else{
+          fill(240);
+          rect(x,y, tileHeight,tileHeight);
+        }
+        
+      }
   }
+  // for(let i=0;i<56000;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   let pointSize = 3;
+  //   let halfSize = 50;
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+  //     rect(x, y, pointSize, pointSize);
+  //   }
+    // else {
+    //   fill(32, 33, 27);
+    //   ellipse(x, y, pointSize, pointSize);    
+    // }
+  // }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
