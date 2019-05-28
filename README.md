3b86317af7b90be26d035912ff5a8a233e74e333
Dylan Wansbrough <33749606+Dylan-Wansbrough@users.noreply.github.com>
2019-05-28 17:31:01 +1200

    sepia working

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Progress 
-I have got my photo to start appearing in sepia. It was a pain but Ive mapped it too the lightness and now I need to look at making it look damaged then polishing it from there. 
+My theme is forever eveolving.
+
+I have got my photo to start appearing in sepia. It was a pain but Ive mapped it too the lightness and now I need to look at making it look damaged then polishing it from there. I always want to make differet masking layers for different colours of damage.
 
 ## Idea 
 So my idea is taking a image and making it look like a old timey photo. I want to take photos of modern tecnology and make them look like old pictures. This is to show the porgression of technology and how what we have now will soon be an obselete technoology. I would do this by changing the colours of pixels to make it look greyscale or old brown tinted photos. I would use the mask to mark where to make scracthes and scuffss etc. 
--- sketch.js
+++ sketch.js
@@ -28,13 +28,11 @@ const tileWidth = 2;
 
 
 function draw () {
-
+  colorMode(HSB,250);
   for(let y = 0; y<height; y= y + tileHeight){
       for(let x = 0; x<width; x= x + tileHeight){
         let pix = sourceImg.get(x, y);
         let mask = maskImg.get(x, y);
-
-        colorMode(HSB,360);
 
         //sepia colour range
         let darkest = color(21, 73, 54); 
@@ -47,29 +45,32 @@ function draw () {
         fill(sepia);
         if(mask[0] > 128) {
          rect(x, y, tileHeight, tileWidth);
-        }else{
-          fill(240);
+        }
+        else if(mask[0] < 128 && mask[0] > 56){
+          fill(33, 70, 300);
+          rect(x,y, tileHeight,tileHeight);
+        }
+        else{
+          fill(250);
           rect(x,y, tileHeight,tileHeight);
         }
         
       }
+  } 
+
+ //leaves white dots all over the photo randomly to make it look older
+
+  for(let i=0;i<100;i++) {
+     let x = floor(random(sourceImg.width));
+     let y = floor(random(sourceImg.height));
+     let mask = maskImg.get(x, y);
+
+     if(mask[0] > 128) {
+       fill(240);
+       rect(x, y, tileHeight, tileHeight);
+     }
   }
-  // for(let i=0;i<56000;i++) {
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pix = sourceImg.get(x, y);
-  //   let mask = maskImg.get(x, y);
-  //   let pointSize = 3;
-  //   let halfSize = 50;
-  //   fill(pix);
-  //   if(mask[0] > 128) {
-  //     rect(x, y, pointSize, pointSize);
-  //   }
-    // else {
-    //   fill(32, 33, 27);
-    //   ellipse(x, y, pointSize, pointSize);    
-    // }
-  // }
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
