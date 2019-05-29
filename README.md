58461c07930c4be62c0a8d34424605c5bcfd5de8
Zi Yong Zed Ng <ngziy@myvuw.ac.nz>
2019-05-30 10:09:02 +1200

    Experiment 2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Currently testing with the idea of creating 8 different shapes to show different in tones in the photos.
+After looking at the last experiement, the images kind of reminded me of Vincent Van Gogh's painting. Hence I have decided to create "brush strokes" and creating a impressionism-like image.
 
 The idea to do show 2 places that I currently call home. The image on the left is shot at the waterfront in Wellington, New Zealand. The image on the right is shot at a decommissioned cross-country train track in Singapore. The image is the flags of the two nations.
--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(704, 1252);
   main_canvas.parent('canvasContainer');
-
+  angleMode(DEGREES)
   imageMode(CENTER);
   noStroke();
   background(255);
@@ -22,8 +22,101 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<5000;i++) {
+
+function brush1(x, y) {
+  for (i = 0; i < 20; i++) {
+    let pointSize = 5;
+    let a = random(-5, 5)
+    let b = random(-5, 5)
+    noStroke();
+    ellipse(x - a, y - b, pointSize);
+  }
+}
+
+function brush2(x,y){
+  for (i=0; i<15;i++){
+    let a = random(2,10)
+    offset = random(2,10)
+    strokeWeight(random(1,2))
+    line(x,y+offset,x+a,y-a+offset)
+  }
+}
+
+function brush3(x,y){
+  push()
+    
+    noStroke()
+    rectMode(CENTER)
+    translate(x,y)
+    rotate(random(30, 60))
+    let pointSize = 5;
+    rect(0,0, pointSize,pointSize)
+    rect(5,5,pointSize,pointSize)
+    rect(-5,-5,pointSize,pointSize)
+    rect(5,-5,pointSize,pointSize)
+    rect(-5,5,pointSize,pointSize)
+  pop()
+}
+
+function brush4(x,y){
+  noStroke()
+ beginShape()
+ vertex(x+2,y+8)
+ vertex(x+4,y+9)
+ vertex(x-7,y+6)
+ vertex(x-3,y+6)
+ vertex(x-5,y+9)
+ vertex(x+9,y-5)
+ endShape(CLOSE)
+}
+
+function brush5(x,y){
+  push()
+    translate(x,y)
+    noStroke()
+    let pointSize=7
+    let a = random(0,3)
+    let b = random(0,3)
+    for (i=0; i < 9; i++){
+      rotate(random(30,60))
+      ellipse(0+a,0+b,pointSize,pointSize/3)
+    }
+  pop()
+}
+
+function brush6(x,y){
+  strokeCap(ROUND);
+  strokeWeight(4.0);
+  line(x,y,x+7,y+7)
+  line(x,y,x-7,y+7)
+}
+
+function brush7(x,y){
+  let size = random(4, 10)
+  ellipse(x,y,size)
+}
+
+function brush8(x,y){
+  strokeCap(ROUND);
+  push()
+  push()
+  stroke(200)
+  strokeWeight(3.0);
+  line(x,y,x-3,y+3)
+  line(x,y,x+3,y+3)
+  line(x,y,x-3,y-3)
+  line(x,y,x+3,y-3)
+  pop()
+  strokeWeight(2.0);
+  line(x,y,x-3,y+3)
+  line(x,y,x+3,y+3)
+  line(x,y,x-3,y-3)
+  line(x,y,x+3,y-3)
+  pop()
+}
+
+function draw() {
+  for (let i = 0; i < 3100; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
@@ -31,45 +124,32 @@ function draw () {
     let pointSize = 10;
     fill(pix);
 
-    if(mask[0] >= 0 && mask[0] <36) {
-      stroke('red')
-      rect(x, y, pointSize, pointSize);
-      console.log("0")
-    }
-    else if(mask[0] >= 36 && mask[0] <72){
-      stroke('blue')
-      ellipse(x, y, pointSize, pointSize);
-      console.log("36")    
-    }
-    else if(mask[0] >= 72 && mask[0] <108){
-      stroke('green')
-      line(x, y, x+4, y+4);
-      console.log("72")    
-    }
-    else if(mask[0] >= 108 && mask[0] <144){
-      stroke('yellow')
-      triangle(x, y, x+3, y+3, x-4, y-4);
-      console.log("108")    
-    }
-    else if(mask[0] >= 144 && mask[0] <180){
-      stroke('blue')
-      rect(x, y, pointSize, pointSize);
-      console.log("144")    
-    }
-    else if(mask[0] >= 180 && mask[0] <216){
-      stroke('yellow')
-      ellipse(x, y, pointSize, pointSize);
-      console.log("180")    
-    }
-    else if(mask[0] >= 216 && mask[0] <255){
-      stroke('red')
-      line(x, y, x+4, y+4);
-      console.log("216")    
-    }
-    else {
-      stroke('orange')
-      triangle(x, y, x+13, y+13, x-14, y-14);
-      console.log("255")      
+
+    if (mask[0] >= 0 && mask[0] < 36) {
+      brush1(x, y)
+    } else if (mask[0] >= 36 && mask[0] < 72) {
+        push()
+          stroke(pix)
+          brush2(x,y)
+        pop()
+    } else if (mask[0] >= 72 && mask[0] < 108) {
+        brush3(x,y)
+    } else if (mask[0] >= 108 && mask[0] < 144) {
+        brush4(x,y)
+    } else if (mask[0] >= 144 && mask[0] < 180) {
+        brush5(x,y)
+    } else if (mask[0] >= 180 && mask[0] < 216) {
+         push()
+          stroke(pix)
+          brush6(x,y)
+        pop()
+    } else if (mask[0] >= 216 && mask[0] < 255) {
+      brush7(x,y)
+    } else {
+         push()
+          stroke(pix)
+          brush8(x,y)
+        pop()
     }
   }
 
@@ -78,7 +158,7 @@ function draw () {
 
 
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
