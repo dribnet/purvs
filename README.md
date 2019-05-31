7c324c02978734bed263e759313ff4b101cca0e6
Caitlin Sue <caitlinsue06@gmail.com>
2019-05-31 17:20:34 +1200

    Added layer and opacity

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## Creative Coding 2: Custom Pixel
 
-I added a third mask to all my images for the billboard. I think this works better but I may have made the ey for the second image too big. 
+Today I spoke to Hazel about my project and how I can use the glyphs and masks to show my message. Hazel said it would help to bring out the billboards and my message by blurring the background. 
 
-The ellipses drawing on the billboard mask are also clashing with the eye mask and encroaching on the eye and making it appear smaller.
+I used Tom's code from today and applied it to the background, then decreased the opacity of the background to mask to help bring out the foreground masks. 
 
-Next I want to experiment with more intricate and interesting ways to fill the masks and have these relate more to the mask that they are actually filling.
+I then incorporated the layering that Tom showed in class. I drew the eyeball on the second layer, on top of the first so that it stands out more. 
+
+I am happy with this progress so far but still want to further improve and detail the glyphs that draw on the masks.
--- sketch.js
+++ sketch.js
@@ -23,38 +23,88 @@ function setup () {
 }
 
 //TILE CONSTANTS ----------------------------------------------------
+
 const tile_width = 20;
-const tile_width2 = 8;
 
 const tile_height = 8;
-const tile_height2 = 20;
 
-const tile_step_x = 9;
-const tile_step_y = 9;
+const tile_step_x = 20;
+const tile_step_x2 = 9;
+
+const tile_step_y = 20;
+const tile_step_y2 = 9;
+
+//MASK FILL GLYPH FUNCTIONS
+function draw_special_rect(x, y, width, height) {
+  rect(x, y, width, height);
+  let die_roll = random([0, 1, 2, 3]);
+  if(die_roll == 0) {
+    fill(0, 0, 0, 50);
+    ellipse(x, y, width/2, height/2);
+  }
+  else if (die_roll == 1) {
+    fill(255, 255, 255, 50);
+    ellipse(x, y, width/2, height/2);
+  }
+  else if (die_roll == 2) {
+    fill(255, 255, 255, 50);
+    ellipse(x, y, width/2, height/2);
+    fill(0, 0, 0, 50);
+    ellipse(x, y, width/4, height/4);
+  }
+}
+
 
 //DRAW FUNCTION -----------------------------------------------------
 
 function draw () {
+
+  //FIRST LOOP
   //TILE FUNCTION 
-  for (let y=0; y<height; y= y + tile_step_y) {
-    for (let x=0; x<width; x = x + tile_step_x) {
+  for (let y=0; y<height; y= y + tile_step_y2) {
+    for (let x=0; x<width; x = x + tile_step_x2) {
       let pix = sourceImg.get(x,y);
       let mask = maskImg.get(x,y);
-      fill(pix)
 
-      //WHITE MASK 255 background
-      if(mask[0] > 200){       
-        rect(x, y, tile_width, tile_height);
+      //WHITE MASK 255 ~ background
+      if(mask[0] > 200){
+        //fills with less opacity
+        fill(pix[0], pix[1], pix[2], 50);
+        draw_special_rect(x,y, 20, 20);
      } 
 
-    //GREY MASK billboard
-    if(mask[0] > 100 && mask[0] < 255){
-      ellipse (x, y, 20, 20);
+      //GREY MASK ~ billboard
+      if(mask[0] > 100 && mask[0] < 255){
+        fill(pix)
+        ellipse (x, y, 20, 20);
      }
 
-    //BLACK MASK 0 eye
-    if(mask[0] < 10) {
-      ellipse(x, y, 6, 6);
+      //BLACK MASK 0 ~ eye
+     if(mask[0] < 10) {
+        fill(pix)
+        ellipse (x, y, 20, 20);     }
+
+    }
+  }
+
+  //SECOND LOOP
+  for (let y=0; y<height; y= y + tile_step_y2) {
+    for (let x=0; x<width; x = x + tile_step_x2) {
+      let pix = sourceImg.get(x,y);
+      let mask = maskImg.get(x,y);
+
+      //WHITE MASK 255 ~ background
+      if(mask[0] > 200){
+     } 
+
+      //GREY MASK ~ billboard
+      if(mask[0] > 100 && mask[0] < 255){
+     }
+
+      //BLACK MASK 0 ~ eye
+     if(mask[0] < 10) {
+      fill(255);
+        ellipse(x, y, 6, 6);
      }
 
     }
@@ -62,11 +112,11 @@ function draw () {
 
 //DRAWS IMAGE 10 TIMES
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 5) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-       saveArtworkImage(outputFile);
+     saveArtworkImage(outputFile);
   }
 }
 
