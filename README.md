b85ef4ccaec37d21ca95551b4b9ef61bdb106ceb
Wenlin Zhang <lynnetezh@gmail.com>
2019-05-31 15:49:10 +1200

    test

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-## Creative Coding 2: Custom Pixel
+## Crime Scene
 
-Updating the preview image and thumbnail.
+My theme for this project is a crime scene. The reasons that I choose this topic is I love photos, novels and anything that contains mystery elements. Therefore, when I first got the brief of this project, I want to do something that contains mystery elements. Finally, I decided to take some photos of the crime scene. The reason for that is choosing a crime scene so I can create a mystery atmosphere(blue/cool hue) and the colour of blood has a strong contrast to its cold hue. The story of this series of photos is a murderer that murdered someone and leave her criminal of the scene. Those three images are like the process of the detective is solving the case and reconstruct the criminal of the scene. 
+
+The first image is two golets with red liquid and one of the golet contains a red lip print on it. 
+The second image will focus on a face, there is blood drop on the face. (Creppy smile and red pupil).
+The third image is a hand that is grabbing a knife with blood.
+
+However, what images that I'm having is not the final photos and those photos is more like showing my theme. 
+
+The order of the images is the timeline of the murder.
+
+I had think of do I have to add a image of 'dead' body into this series of photos, it may  made the theme more clear. However, at the end I feel it's better to concentrate on displaying the details of the crime scene, it would made the this series of images more jinzhi and will evoke a strong emotion. 
+
+
+
+-----------    What am I doing today
+ 
+On the above I said this serie of images is more like the detective is reconstructe the crime scene in her/his head. Therefore, I tried to do this today. The elements that I used are 
+
+Firstly,'crime board': sticky note (time, spot), red string. 
+
+In order to standout the important part of the images, I am going to make the handpart be full resolution.
--- sketch.js
+++ sketch.js
@@ -23,32 +23,77 @@ function setup () {
 }
 
 function draw () {
+
+     // rect(0,0,704, 1252);
   for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 100;
+    let pointSize = 7;
+    let offset = pointSize - 4;
+    let offset2 = pointSize - 3;
+    let halfSize = 20;
+     let x1 = floor(random(sourceImg.width));
+      let y1 = floor(random(sourceImg.height));
+
     fill(pix);
     if(mask[0] > 128) {
+      noStroke();
       ellipse(x, y, pointSize, pointSize);
+      
     }
     else {
-      rect(x, y, pointSize, pointSize);
-      // let x1 = floor(random(sourceImg.width));
-      // let y1 = floor(random(sourceImg.height));
-      // stroke(pix);
-      // line(x, y, x1, y1);    
+        noStroke();
+        rect(x, y, halfSize, halfSize);
+    
+    
     }
+
+    fill(250, 0 ,0);
+    // rect(100,100, 300, 300);
+    stroke(255, 0,0);
+    strokeWeight(1);
+    line(538, 202, 533, 526);
+    line(538, 202, 570, 644);
+    line(538, 202, 57, 319);
+    line(533, 526, 570, 644);
+    line(459, 440, 57, 319);
+    line(57, 319, 533, 526);
+    line(57, 319, 533, 526);
+     line(374, 840, 702, 600);
+
+    line(570, 644, 57, 319);
+    line(57, 319, 374, 840);
+    line(374, 840, 459, 440);
+
+    ellipse(538, 202, 8,8);
+    ellipse(533, 526, 8,8);
+    ellipse(570, 644, 8,8);
+    ellipse(57, 319, 8,8);
+    ellipse(374, 840, 8,8);
+    ellipse(459, 440, 8,8);
+     
+    noStroke();
+    fill('#dbdfa8');
+    rect(515, 160,130,30);
+     fill(171, 176, 106, 5);
+    rect(520, 165,130, 30);
+    fill(0);
+    textFont('Chalkduster');;
+    text('VICTOM Blood', 520, 180);
+
   }
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
+ 
+    
 }
 
 function keyTyped() {
