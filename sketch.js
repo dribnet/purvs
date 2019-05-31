9fdec6b6a12ff96e1d9256b5bc5d2f2c6273a1f3
Maia Hill <maiatwhill@gmail.com>
2019-05-31 14:14:30 +1200

    updated image1

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "artwork_2.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -16,14 +16,14 @@ function setup () {
   main_canvas.parent('canvasContainer');
   imageMode(CENTER);
   noStroke();
-  background(28, 140, 226);
+  background(158, 183, 177);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 const tile_width = 10;
-const tile_height = 13;
-const tile_step_x = 10;
+const tile_height = 18;
+const tile_step_x = 30;
 const tile_step_y = 20;
 
 function draw () {
@@ -41,27 +41,30 @@ function draw () {
       if(mask[0] < 128) {
         noStroke();
         fill(pix);
-        rect(x, y, tile_width, tile_height);
+        rect(x, y-10, tile_width, tile_height);
       }
       else {
       }
     }
   }
 
-  for(let i=0;i<5000;i++) {
+  for(let i=0;i<6000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let pointSize = 20;
-    let rectSize = 2;
-    let halfSize = 6;
-    let halfrectSize = 6;
+    let halfSize = 3;
     if(mask[0] > 128) {
+      // fill(0);
+      // stroke(pix);   
+      // ellipse(x, y, pointSize, halfSize); 
+      // ellipse(x, y, pointSize/2, halfSize/2); 
+      // ellipse(x, y, pointSize/4, halfSize/4); 
       fill(pix);
       stroke(pix);
-      rect(x, y, rectSize, halfrectSize);   
-      ellipse(x, y, pointSize, halfSize); 
+      ellipse(x, y, halfSize, pointSize);
+      ellipse(x, y, pointSize, halfSize);
     }
     else {
 
@@ -73,7 +76,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+     //saveArtworkImage(outputFile);
   }
 }
 
