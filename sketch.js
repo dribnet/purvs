0ce28c82e468eeff893fe62738d5c4002686c334
Lennart Schiffers <mail@lennart-schiffers.de>
2019-05-31 12:44:50 +1200

    added input 4 for a test

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_4.jpg";
-let maskFile   = "mask_4.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "artwork_1.png";
 
 function preload() {
