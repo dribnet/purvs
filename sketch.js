0bcaf58763d91b957ac6616901e68dbc4141d33d
Wenlin Zhang <lynnetezh@gmail.com>
2019-05-31 15:59:55 +1200

    test

--- sketch.js
+++ sketch.js
@@ -90,7 +90,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     saveArtworkImage(outputFile);
   }
  
     
