140e7ad5efb9652816a73ac04512e2c88c5c1537
Daniel McLean <Danhanado@hotmail.com>
2019-05-31 14:43:32 +1200

    Code for grey area created and more eexperiments

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -21,23 +21,47 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
+const tile_width = 8;
+const tile_height = 8;
+const tile_step_x = 8;
+const tile_step_y = 8;
 
+function draw_some_lines (x,y){
+  for(let i=0; i<10; i=i+1)  {
+    let pix = sourceImg.get(x, y);
+    let dx1 = random(-10,10)
+    let dy1 = random(-10,10)
+    let dx2 = random(-10,10)
+    let dy2 = random(-10,10)
+    let coin_flip = random(0,1)
+    if (coin_flip == 0){
+      stroke(0);
+    }
+    else {
+      stroke(255);
+    }
+    stroke(pix);
+    line(x+dx1,y+dx1,x+dx2,y+dx2);
+  }
+}
 function draw () {
   for(let i=0;i<8000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
+    let pointSize = random(10,20);
     let halfSize = 50;
     fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+    if(mask[0] < 10) {
+      draw_some_lines(x,y);
     }
     else {
-      ellipse(x, y, pointSize/2, pointSize/2);    
+      noStroke();
+      ellipse(x, y, pointSize, pointSize);    
     }
   }
+  
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
