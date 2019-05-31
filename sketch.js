95a4ca6dd5a1c0cff232d62c6cd0492e3553c778
Reuben McSweeney <reuben.mcsweeney@gmail.com>
2019-05-31 17:33:37 +1200

    stuff

--- sketch.js
+++ sketch.js
@@ -30,81 +30,57 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = random(1,15);
+    let pointSize = random(4,15);
     let halfSize = 50;
+    let dist = [0,0,0,0,0,0,0,0,0,0,0,0,0,20,-20];
+    let dist1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,,0,0,0,0,0,0,0,0,0,0,0,40,-40];
+
     
     let grayscale = (pix[0]+pix[1]+pix[2])/3
     fill(grayscale);
     stroke(grayscale)
+
     if(mask[0] > 128) {
       //line(x, y, x+pix[2]/100, pix[1]/100);
       //print(pix);
       //triangle(x, y, x+pointSize, y+(pointSize*(2/3)), x+pointSize, y-(pointSize*(2/3)));
-      //ellipse(x, y, pointSize, pointSize);  
-      let pointSizeTri = random(3,15); 
-      push();
-      translate(x,y);
-      rotate(random(1,360))
-      triangle(0, 0, x+pointSizeTri, y+(pointSizeTri*(2/3)), x+pointSizeTri, y-(pointSizeTri*(2/3)));    
-      pop();  
+      let outliersx = random(dist)
+      let outliersy = random(dist)
+
+      let outliersx1 = random(dist1)
+      let outliersy1 = random(dist1)
+
+      if(outliersx == 20 || outliersy == 20 || outliersx == -20 || outliersy == -20){
+        let pointSize = 5
+        ellipse(x+outliersx, y+outliersy, pointSize, pointSize); 
+      }
+      if(outliersx1 == 40 || outliersy1 == 40 || outliersx1 == -40 || outliersy1 == -40){
+        let pointSize = 3
+        ellipse(x+outliersx1, y+outliersy1, pointSize, pointSize); 
+      }
+      //print(outliersx,outliersy)
+      ellipse(x, y, pointSize, pointSize);   
+      //ellipse(x+outliersx, y+outliersy, pointSize, pointSize);
     }
     else {
 
       fill(pix)
       stroke(pix)
       let pointSizeTri = random(3,15);
+      let shift = -pointSizeTri/3
       push();
       translate(x,y);
       rotate(random(1,360))
-      triangle(0, 0, 0+pointSizeTri, 0+(pointSizeTri*(2/3)), 0+pointSizeTri, 0-(pointSizeTri*(2/3)));    
+      triangle(shift, shift, shift+pointSizeTri, shift+(pointSizeTri*(2/3)), shift+pointSizeTri, shift-(pointSizeTri*(2/3)));    
       pop();  
     }
   }
-if(renderCounter >= 9){
-    for(let i=0;i<130000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = random(1,15);
-    let halfSize = 50;
-    
-    let grayscale = (pix[0]+pix[1]+pix[2])/3
-    fill(grayscale);
-    stroke(grayscale)
-    if(mask[0] > 128) {
-      //line(x, y, x+pix[2]/100, pix[1]/100);
-      //print(pix);
-      //triangle(x, y, x+pointSize, y+(pointSize*(2/3)), x+pointSize, y-(pointSize*(2/3)));
-      //ellipse(x, y, pointSize, pointSize);  
-      let pointSizeTri = random(3,15); 
-      push();
-      translate(x,y);
-      rotate(random(1,360))
-      //triangle(0, 0, x+pointSizeTri, y+(pointSizeTri*(2/3)), x+pointSizeTri, y-(pointSizeTri*(2/3)));    
-      pop();  
-    }
-    else {
-
-      fill(pix)
-      stroke(pix)
-      let pointSizeTri = random(3,15);
-      push();
-      translate(x,y);
-      rotate(random(1,360))
-      triangle(0, 0, 0+pointSizeTri, 0+(pointSizeTri*(2/3)), 0+pointSizeTri, 0-(pointSizeTri*(2/3)));    
-      pop();  
-    }
-  }
-}
-
-
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-   saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
