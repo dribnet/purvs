17fbf9dcd8e0c0a0360e733648f25a9c263e3b80
Elena Lee <ejlee5342@gmail.com>
2019-05-31 12:43:37 +1200

    image files updated and sketch.js changed.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## Creative Coding 2: Custom Pixel - 28/05/19
+## Creative Coding 2: Custom Pixel - 30/05/19
 
 Experimenting and Ideation
 
-I have updated the input images to some photos I took in/on the way to Hong Kong. I haven't made many changes to the code yet at the moment.
-I'm still thinking about the theme I want to explore. Currently I'm thinking about either sustainability, or the importance of spending time with your loved ones.
-As for the actual images, last class Phoebe suggested scanning drawings I've done of images and using them, which is a really cool idea that I want to try use. 
+I have updated the input images to some photos I took in/on the way to Hong Kong. I haven't made many changes to the code yet at the moment, I'm just trying out the grid code from the class example, and am fiddling with using lines.
+I'm still thinking about the theme I want to explore. Currently I'm thinking about either sustainability, or the importance of spending time with your loved ones
+I might go to the botanic gardens or have a look at photos I already have. 
--- sketch.js
+++ sketch.js
@@ -22,30 +22,62 @@ function setup () {
   maskImg.loadPixels();
 }
 
+
+
+const tile_width = 20;
+const tile_height = 20;
+const tile_step_x = 2;
+const tile_step_y = 20;
+
+//image1 const tile_width = 8;
+//const tile_height = 8;
+
+
 function draw () {
-  for(let i=0;i<20000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 5;
-    let halfSize = 50;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+//  for(let i=0;i<5000;i++) {
+  //  let x = floor(random(sourceImg.width));
+//    let y = floor(random(sourceImg.height));
+//    let pix = sourceImg.get(x, y);
+//    let mask = maskImg.get(x, y);
+//    fill(pix);
+//    stroke(pix);
+
+for(let y=0; y<height; y = y + tile_step_y) {
+    for(let x=0; x<width; x = x + tile_step_x) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
+      if(mask[0] > 128) {
+        rect(x, y, tile_step_x, tile_step_y);
+      }
+      else {
+        ellipse(x, y, tile_width, tile_height);
+      }
     }
-    else {
-      rect(x, y, pointSize, pointSize);    
+
     }
+//image 1
+    //if(mask[0] > 128) {
+    //  let pointSize = 2;
+    //  let x2 = floor(random(sourceImg.width));
+    //  let y2 = floor(random(sourceImg.height));
+    //  line(x, y, 128, 1038);
+
+  //  }
+    //else {
+
+    //  let pointSize = 20;
+    //  ellipse(x, y, pointSize, pointSize);
+  //  }
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+  //saveArtworkImage(outputFile);
   }
-}
+
 
 function keyTyped() {
   if (key == '!') {
