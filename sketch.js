3ea557073e19bc9e33a344bef1be08006f1d47f8
MingMattY <mattmynz@gmail.com>
2019-06-01 18:13:55 +1200

    finished

--- sketch.js
+++ sketch.js
@@ -14,7 +14,6 @@ function preload() {
 function setup () {
   let main_canvas = createCanvas(704, 1252);
   main_canvas.parent('canvasContainer');
-
   imageMode(CENTER);
   noStroke();
   background(255);
@@ -22,45 +21,43 @@ function setup () {
   maskImg.loadPixels();
 }
 
-
-const tile_width = 20;
-const tile_height = 20;
-
 function draw () {
   for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-     let pointSize = 30;
-    let pointSize2 = 5;
-    let halfSize = 5;
-    let halfSize2 = 5;
+    let greyA = 30;
+    let blackA = 4;
     let round = random(2,3);
-    let round2 = random(30, 40);
-    let PS = map(round, 0, width, 0, height);
-    let PS2 = map(round2,0,width,0,height);
+    let round2 = random(10, 20);
+    let position = map(round, 0, width, 0, height);
+    let position2 = map(round2,0,width,0,height);
     fill(pix);
     stroke(pix);
 
-    if(mask[0] > 128) {
-      //ellipse(x, y, PS, PS2);
+    if(mask[0] == 255) {
+      fill(pix);
       strokeWeight(1);
       let ff = random(0,200);
-      stroke(ff,30);
-      rect(x,y,PS2,PS/PS2);
-      rect(x+random(20,30),y-random(20,30),PS,PS2);
+      stroke(ff,20);
+      rect(x,y,position2,position/position2);
+      rect(x+random(20,30),y-random(20,30),position,position2);
           }
+    else if(mask[0] == 0) {
+      rect(x, y, blackA ,blackA);
+      ellipse(x, y, blackA, blackA); 
+    }
     else {
-      rect(x,y,halfSize,halfSize/2);
       noFill();
       stroke(pix);
       strokeWeight(1);
-      ellipse(x, y, halfSize2, halfSize2); 
-
- 
+      rect(x,y,greyA,greyA); 
     }
   }
+
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
