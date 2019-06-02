cc41996428bc1f6d45f98d4c9d1366c486516e8e
yuxuanbai <bai_yuxuan@outlook.com>
2019-06-03 04:00:44 +1200

    23

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
 let outputFile = "artwork_1.png";
 
 function preload() {
@@ -21,8 +21,8 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-const rect_width = 12;
-const rect_height = 4;
+const rect_width = 22;
+const rect_height = 3;
 const x1 = 4;
 const y1 = 6;
 function draw () {
