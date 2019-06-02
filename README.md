56b6cbd8b9a945a40a8d3467ec917fab629245f3
Zhaolin Wu <q34530449@gmail.com>
2019-06-02 21:01:55 +1200

    1

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## Creative Coding 2: Custom Pixel
 
-p2:in thispart i draw 3 layer for my image, i draw some picture on the mask, it looks fun, and i will change the code later  , see how it work if it doesnt work well i have sraw the mask again.
+
+p3: in this part i change my code to draw my photo, i change cut position of the runset image, it can my mask more easy to draw and it have more space for the sky. changing the code make sure it looks good for my final.
+
+p2:in this part i draw 3 layer for my image, i draw some picture on the mask, it looks fun, and i will change the code later  , see how it work if it doesnt work well i have sraw the mask again.
 
 p1:the 3 photo i select is sunrise mid-day and sunset, i take those photo on those day, the color change of the sky especially interesting. i begin  to use my own code to draw this 3 image now.
 
--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "artwork_1.png";
 
 function preload() {
@@ -23,19 +23,32 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<10000;i++) {
+  for(let i=0;i<6000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let pointSize = 10;
-    let halfSize = 50;
     fill(pix);
+    noStroke();
     if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+
+
+      
+      push();
+      rotate(PI/400);
+      rect(x, y, pointSize, pointSize);
+      pop();
+      
+
+      
     }
     else {
-      rect(x, y, pointSize, pointSize);    
+      push();
+     
+      rect(x, y, pointSize*10, pointSize/25);
+      pop();
+      
     }
   }
   renderCounter = renderCounter + 1;
