ab59000d22a528bd51071ec22300b8b410cc955e
Eugene Park <parkeuge@myvuw.ac.nz>
2019-06-02 20:29:35 +1200

    Disabled image downloading

--- sketch.js
+++ sketch.js
@@ -64,7 +64,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
 }
 
