a60fa3e6bcbd383a9a677f43dbdd21122b99d35b
Tessa Chen <hsinchen.0212@gmail.com>
2019-06-02 21:49:25 +1200

    Edited sketch.js, updated all images and masks.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Creative Coding 2: Custom Pixel
-Today, I tested out a new theme. I wanted to use double exposure to show a person's homeland and how much they miss it. I've only managed to change one of the images and it does look quite nice but I think there's a lot more details I can alter. However, I think I should stick to the siblings theme because I may not have enough time to take oter images of other people and their homelands, whereas I could take photos of me and my brother anytime. I'll be working through the portraits during the weekend and focus on the code after that. 
+
+I've finally finished taking the photographs based on me and my older brother. I've begun testing out different patterns but haven't found anything satisfying yet. 
--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_4.jpg";
-let maskFile   = "mask_4.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "artwork_1.png";
 
 function preload() {
@@ -16,29 +16,74 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
-  background(150);
+  background(175);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+  // const tile_width = 5.5;
+  // const tile_height = 5.5;
+
 function draw () {
-  for(let i=0;i<4000; i++) {
+
+  // for(let i=0;i<5000; i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   let pointSize = 10;
+  //   let halfSize = 10;
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+  //     ellipse(x, y, 10, 10);
+  //     // rect(x-5, y, 10, 5);
+  //   }
+  //   else {
+  //     rect(x, y, 5, 30);    
+  //   }
+  // }
+  let tile_width = 5.5;
+  let tile_height = 5.5;
+  strokeWeight(0.3);
+  for(let y = 0; y < height; y = y + tile_height){
+    for(let x = 0; x < width; x = x + tile_width){
+      let pix = sourceImg.get(x,y);
+      let mask = maskImg.get(x,y);
+      fill(pix);
+      stroke(pix);
+      if(mask[0] >= 0 && mask[0] < 128) {
+        line(x, y, x+20, y+20);
+        line(x+20, y, x, y+20);  
+      }
+
+      else if (mask[0] >= 128 && mask[0] < 216) {
+        let tile_width = 5;
+        let tile_height = 50;
+        stroke(80);
+        strokeWeight(0.1);
+        rect(x, y, tile_width, tile_height);
+        line(x + 2.5, y, x + 2.5, y+50); 
+      }
+
+      else {
+        let tile_width = 5.5;
+        let tile_height = 5.5;
+        rect(x, y, tile_width, tile_height);
+      }
+    }
+  }
+
+  for(let i=0;i<5000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 10;
-    fill(pix);
-    if(mask[0] > 128) {
+    if(mask[0] >= 128 && mask[0] < 216) {
+      fill(50, 100);
       ellipse(x, y, 10, 10);
-      rect(x-5, y, 10, 5);
-    }
-    else {
-      rect(x, y, 5, 15);    
     }
   }
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
