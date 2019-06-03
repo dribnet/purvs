abb5fe0b27462c1742682f8ac3b842fed287d69a
Keegan Downey-Graham <downeykeeg@vs320-cintiq-02-03.mddn.vuw.ac.nz>
2019-06-03 21:20:50 +1200

    Update on my process. Currently working on image 2.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+The 3 images I have used are:
+1 - A photo I have taken of my little brother at lorenzen bay, Raglan.
+2 - Sunset at Ngarunui beach, Raglan.
+3 - Power lines in central park, Brooklyn.
+
+For my images I will focus on using patterns/shapes created in the masking process to add an interesting element to picture.
+
+For example image no.2
+- Haven't completed it yet but that is the direction I will be heading in.
--- sketch.js
+++ sketch.js
@@ -22,10 +22,83 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tile_width = 10;//for tiles
+const tile_height = 5;//for tiles
+const tile_step_x = 6;//for tile space --------------comment out if not using tiles
+const tile_step_y = 6;//for tile space ---- want to keep these numbers higher (const use for mask tiles)
+
 function draw () {
 
-  //fill(255, 227, 117,20);
-  //rect(0,0,704,1252);
+let pointSize = 20;
+
+
+background(50,50,50,60);
+for(let y =0; y<height; y = y + tile_step_y){
+for(let x =0; x<height; x = x + tile_step_x){
+  let pix = sourceImg.get(x,y);
+  let mask = maskImg.get(x, y);
+  fill(pix);
+  if(mask[0]>128){
+    ellipse(x, y, tile_step_x, tile_step_y);
+    fill(255, 219, 104,5);
+      rect(x, y, pointSize, pointSize);   
+      pop();
+  }
+  else{
+  rect(x, y, tile_width, tile_height);
+  }
+}
+}
+
+
+
+/*--------------------------------------------SIMPLE TILES
+for(let y =0; y<height; y = y + tile_height){
+for(let x =0; x<height; x = x + tile_width){
+  let pix = sourceImg.get(x,y);
+  fill(pix);
+  rect(x, y, tile_width, tile_height);
+  }
+}
+
+*/
+
+
+//------------------------------------------ LINES
+/*
+for(let i=0;i<4000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    fill(pix);
+    stroke(pix);
+
+    if(mask[0] > 128) {
+      let pointSize = 10;
+      ellipse(x, y, pointSize, pointSize);
+    }
+
+    else {
+      let pointSize = 2;
+      let x2 = floor(random(sourceImg.width));
+      let y2 = floor(random(sourceImg.height));
+
+      line(x, y, x2, y2);
+    }
+     
+
+}  
+
+
+*/
+
+
+//----------------------------------------------------- SQUARES AND CIRCLES
+/*
+  fill(255, 227, 117,20);
+  rect(0,0,704,1252);
 
   for(let i=0;i<8000;i++) {
     let x = floor(random(sourceImg.width));
@@ -49,7 +122,8 @@ function draw () {
       rect(x, y, pointSize, pointSize); 
 
     }
-  }
+    */
+    //---------------------------------------------------
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
