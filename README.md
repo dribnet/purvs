4565179f15097419d34e675b3f1bb575da2770d9
CherynJ <389290610@qq.com>
2019-06-03 14:25:58 +1200

    modified: images

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Theme2:
 
-I adjust the input images to make them have more contrast. 
+I try with the three layouts to render the images.One for background, one for the portait and one for the inverted image. However, there something wrong with the coding.I can't let the pixels on each layout show with different size and style. And the second image can't be dowload.
+
+
+//I adjust the input images to make them have more contrast. 
 I use step codes to let Pixel of background row orderly. The potraits are the main body of the work, so i try different way to highlight them. The pixels of main body are in random position to make contrast with the background.
 
-
-
-I tried with another theme IVERTED IMAGES.These three images record on different year.
-The first one is real iversted image which recored beside the water.I think the visualization interesting since the portrait is colorful and the inversted portait is colorless.
+//The first one is real iversted image which recored beside the water.I think the visualization interesting since the portrait is colorful and the inversted portait is colorless.
 The second one is faked inverted image. It recored beside the water,too. I copy and rotate to create the inverted potrait. However this inverted portrait is too strong.It doesn't reach the visulization I expect.
 The third image is faked inverted image. It hs the same issue with the second one.
 
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -27,42 +27,70 @@ const tile_height = 8;
 const tile_step_x = 8;
 const tile_step_y = 8;
 
+const back_width = 55;
+const back_height = 55;
+
+
+
+
+
+
+function draw_special_rect(x, y, width, height) {
+  rect(x, y, width, height);
+  let die_roll = random([0, 1, 2, 3]);
+  if(die_roll == 0) {
+    //fill(0, 0, 0, 50);
+    ellipse(x, y, width, height);
+  }
+  else if (die_roll == 1) {
+    //fill(255, 255, 255, 50);
+    ellipse(x, y, width, height);
+  }
+  else if (die_roll == 2) {
+    //fill(255, 255, 255, 50);
+    ellipse(x, y, width, height);
+    //fill(0, 0, 0, 50);
+    ellipse(x, y, width, height);
+  }
+
+}
+
 function draw () {
-  //background(255,96,96);
+  //background(50);
 
 
   for(let y=0; y<height; y = y + tile_step_y) {
     for(let x=0; x<width; x = x + tile_step_x) {
       let pix = sourceImg.get(x, y);
-      
       let mask = maskImg.get(x, y);
-        let x2 = floor(random(sourceImg.width));
-        let y2 = floor(random(sourceImg.height));
-       let pointSize = 8;
       fill(pix);
-      if(mask[0] > 128) {
-        rect(x, y, tile_width, tile_height);
-        var die = int(random(0, 4));
-        if (die == 0) {
-          var gray = int(random(0, 102));
-          var scalar = random(0.2, 0.45);
+      stroke(pix);
+      if(mask[0] == 0) {
+        // draw_some_lines(x, y);
+        stroke(1);
+         rect(x, y, tile_width, tile_height);
+  
              stroke(1);
-        }
       }
-      else {
-       
+      if(mask[0] == 255) {
+        // draw_some_lines2(x, y);
         noFill();
       stroke(pix);
       strokeWeight(1);
       //ellipse(x+x2, y+y2, pointSize, pointSize);
-      squareBase = map(y, 0, 1080, 35, 2);
+      squareBase = map(y, 0, 500, 35, 2);
       squareSize = squareBase + floor(random(3, 15));
-      ellipse(x+x2, y+y2, squareSize, squareSize);    
-      //fill(255, 255, 0, 40);
-      //ellipse(x-x2, y-y2, pointSize, pointSize);
+      rect(x, y, squareSize, squareSize);  
+      }
+      else {
+        draw_special_rect(x, y, tile_width, tile_height);
       }
     }
   }
+
+
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
