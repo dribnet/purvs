ed34f8d7327cbd59e3e84bc73a7c04ef7830a0e8
Wenlin Zhang <lynnetezh@gmail.com>
2019-06-03 17:33:03 +1200

    test

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -22,6 +22,20 @@ function setup () {
   maskImg.loadPixels();
 }
 
+
+function drop (x, y, g, s) {
+ //  beginShape();
+ // strokeWeight(1);
+ // vertex(0,-5);
+ // quadraticVertex(3, 0, 0, 1);
+ // quadraticVertex(-3,0, 0, -5);
+ // endShape(CLOSE);
+fill(255, 0,0);
+ ellipse(0,0,15);
+}
+
+
+
 function draw () {
 
      // rect(0,0,704, 1252);
@@ -33,67 +47,134 @@ function draw () {
     let pointSize = 7;
     let offset = pointSize - 4;
     let offset2 = pointSize - 3;
-    let halfSize = 20;
+    let halfSize = 5;
+    let halfSize2 =12;
      let x1 = floor(random(sourceImg.width));
       let y1 = floor(random(sourceImg.height));
+      let r = pix[0];
+      let g = pix[1];
+      let b = pix[2];
 
     fill(pix);
-    if(mask[0] > 128) {
+    if(mask[0] > 200) {
       noStroke();
-      ellipse(x, y, pointSize, pointSize);
-      
+      //ellipse(x, y, pointSize, pointSize);
+     
+
     }
     else {
-        noStroke();
-        rect(x, y, halfSize, halfSize);
-    
-    
+        //noStroke();
+      //  fill(255, 0, 0);
+
+        rect(x, y, halfSize, halfSize2);
+          //  fill(pix, 50);
+          rect(x, y, halfSize, halfSize);
+var die = int(random(0,100));
+    if (die == 5) {
+      var gray = int(random(0, 100));
+      var gray2 = int(random(0, 50));
+      var scalar = random (0.2, 0.45);
+     ellipse(x,y,scalar,gray);
+    ellipse(x,y,gray2,scalar);
+         noStroke();
+         fill(255, 0,0);
+         ellipse(x,y,50,1);
+
     }
+      }
 
-    fill(250, 0 ,0);
-    // rect(100,100, 300, 300);
-    stroke(255, 0,0);
-    strokeWeight(1);
-    line(538, 202, 533, 526);
-    line(538, 202, 570, 644);
-    line(538, 202, 57, 319);
-    line(533, 526, 570, 644);
-    line(459, 440, 57, 319);
-    line(57, 319, 533, 526);
-    line(57, 319, 533, 526);
-     line(374, 840, 702, 600);
 
-    line(570, 644, 57, 319);
-    line(57, 319, 374, 840);
-    line(374, 840, 459, 440);
-
-    ellipse(538, 202, 8,8);
-    ellipse(533, 526, 8,8);
-    ellipse(570, 644, 8,8);
-    ellipse(57, 319, 8,8);
-    ellipse(374, 840, 8,8);
-    ellipse(459, 440, 8,8);
-     
-    noStroke();
-    fill('#dbdfa8');
-    rect(515, 160,130,30);
-     fill(171, 176, 106, 5);
-    rect(520, 165,130, 30);
-    fill(0);
-    textFont('Chalkduster');;
-    text('VICTOM Blood', 520, 180);
 
   }
+//fill(255, 0,0);
+
+
+  for(let y=0; y<height; y = y + 1) {
+     for(let x=0; x<width; x = x + 1) {
+       let pix = sourceImg.get(x, y);
+       let mask = maskImg.get(x, y);
+       if(mask[0] == 255) {
+         let r = pix[0];
+         let g = pix[1];
+         let b = pix[2];
+        // let g = pix[1];
+         stroke(r,g,b);
+         point(x, y);
+        
+          fill(255, 0, 0);
+    var die = int(random(0,10));
+    if (die == 5) {
+      var gray = int(random(0, 100));
+      var gray2 = int(random(0, 50));
+      var scalar = random (0.2, 0.45);
+     ellipse(x,y,scalar,gray);
+    ellipse(x,y,gray2,scalar);
+         noStroke();
+         fill(255, 0,0);
+         ellipse(x,y,50,1);
+
+    }
+     
+       }
+     }
+   }
+
+
+
+for(let y=0; y<height; y = y + 1) {
+     for(let x=0; x<width; x = x + 1) {
+       let pix = sourceImg.get(x, y);
+       let mask = maskImg.get(x, y);
+       if(mask[0] == 100) {
+       let r = pix[0];
+         let g = pix[1];
+         let b = pix[2];
+        // let g = pix[1];
+         stroke(r,g,b);
+         point(x, y);
+     
+       }
+     }
+   }
+
+
+
+
+   fill(250, 0 ,0);
+   noStroke();
+   stroke(255, 0,0);
+   strokeWeight(1);
+   line(322, 361, 47, 761);
+   line(47, 761,50, 1010);
+   line(50, 1010, 653, 354);
+   line(653, 354,47, 761);
+   line(322, 361,653, 354);
+   
+
+   ellipse(322, 361, 8,8);
+   ellipse(653, 354, 8,8);
+   ellipse(47, 761, 8,8);
+   ellipse(50, 1010, 8,8);
+
+
+   
+   fill(255);
+   noStroke();
+   textFont('Chalkduster');;
+   text('Long Hair', 280, 589);
+   // text('Antisocial Personality', 100, 864);
+   // text('No fingerprint', 201, 186);
+   // text('Murderer',560, 100);
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
- 
-    
+
+
 }
 
 function keyTyped() {
