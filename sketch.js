aa8bc1a021e7b406781012c7710e4a49f118b2c2
Francesca Armstrong <armstrfran@myvuw.ac.nz>
2019-06-03 13:16:12 +1200

    updated code to include green block out

--- sketch.js
+++ sketch.js
@@ -24,14 +24,16 @@ function setup () {
 const tile_width = 5;
 const tile_height = 5;
 
-const triangle_width = 6;
-const triangle_height = 6;
+const triangle_width = 4;
+const triangle_height = 4;
 
 const tile_step_x = 10;
 const tile_step_y = 10;
 
 function draw () {
+  strokeWeight(1);
 
+//pass 1
   for(let y = 0; y<height; y = y+tile_height){
     for (let x = 0; x<width; x = x+tile_width){
       let pix = sourceImg.get(x,y);
@@ -39,52 +41,56 @@ function draw () {
       fill(pix);
     
       //if white on mask
-      if(mask[0] > 128){
-      fill(30);
-      rect(x,y,tile_width, tile_height);
-      fill(pix);   
-        triangle(x, y-(tile_height/2), x+(tile_width/2), y+(tile_height/2), x-(tile_width/2), y+(tile_height/2)); 
-          }
-
+      if(mask[0] > 200){
+       fill(225, 242, 229);
+       stroke(225, 242, 229);
+       rect(x,y,tile_width, tile_height);
+       fill(88, 153, 135);
+       stroke(88, 153, 135);
+       rectMode(CENTER);
+       rect(x, y, tile_width, tile_height/2);
+      }
+     
       //if black on mask
       else{
+        fill(30);
+        stroke(30);
+        rect(x,y,tile_width, tile_height);
+        fill(pix); 
+        stroke(pix); 
+        triangle(x, y-(tile_height/2), x+(tile_width/2), y+(tile_height/2), x-(tile_width/2), y+(tile_height/2)); 
+      }
+    }
+  }
+
+
+
+  //pass 2
+   for(let y = 0; y<height; y = y+tile_height){
+    for (let x = 0; x<width; x = x+tile_width){
+      let pix = sourceImg.get(x,y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
+      stroke(pix); 
+    
+      //if white on mask
+      if(mask[0] < 100){
         fill(0);
+        stroke(0); 
         rect(x, y, tile_width, tile_height);
       }
     }
   }
 
- /*
-  for(let i=0;i<2000;i++) {
-    //pick x and y
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    //colours of images and mask
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize1 = 5;
-    let pointSize2 = 5;
-    let halfSize = 50;
-    fill(pix);
-   
 
-   if(mask[0] > 128) {
-     rect(x, y, pointSize1, pointSize1);
-      
 
-    }
-    else {
-      rect(x, y, pointSize2, pointSize2);    
-    }
-  }
-  */
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-   saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
