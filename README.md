0bdd3f44e5a77b1dcd1daf04c78f7d6e02f713aa
Elena Lee <ejlee5342@gmail.com>
2019-06-04 12:50:18 +1200

    new changes made to sketch.js, image files and readme updated accordingly.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-## Creative Coding 2: Custom Pixel - 31/05/19
+## Creative Coding 2: Custom Pixel - 03/06/19
 
 Progress Update
 
-I'm still experimenting with and trying to get my head around using grids, and I came up with this. I like it because I could use it to completely obscure things in an image like they've gone missing or disappeared. If I use sustainability as a theme, I could use this to take trees/natural elements out of the picture. 
+I have been experimenting with grids, and the idea of obscuring parts of the image. Initially I was going to take photos around Wellington, of the botanic gardens, waterfront and city, and 'cut out' trees, leaves, and the water to show the impact we are doing to the environment.
+
+After doing some digging in my camera roll I found these images I took of my sister, which all had similar colour schemes and were in natural environments.
+
+I have used ellipses with stroke only for masked out areas of natural elements. This makes it look like nature is 'taking over' / growing. Plus a circle represents the cyclical nature of life.
+
+I have masked out the figure and used the rect function with a strong black stroke, to semi obscure the details of the figure, like they are a void/ a bad thing that is detrimental to the world around it, but also to show that they are unimportant in the grand scheme of things.
+
+I have masked out man made elements in each image with a grey mask, and used rect, with a dark grey stroke. Each consecutive image has increasing man made elements in it. Initially I considered using an image with buildings as my last image to show this more, but it didn't fit as well with the other two images.
+
+I have masked out the top area of each image with a lighter grey, and used the line function with a  purple stroke to create the 'sky' - which is only actually visible in the last image. This gives them a surreal feel and other worldly feel - to show how small people are in comparison to nature. I also decided to mask out some lines in the sky of each picture - either where the clouds roughly are, or where the hills are in the actual photo. This helps balance out the image and also gives it a more graphic look. 
--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mas_2.png";
 let outputFile = "artwork_3.png";
 
 function preload() {
@@ -17,70 +18,88 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-//coool stuff
-//const tile_width = 2;
-//const tile_height = 20;
-//const tile_step_x = 10;
-//const tile_step_y = 20;
 
-const tile_width = 2;
-const tile_height = 20;
-const tile_step_x = 10;
-const tile_step_y = 20;
+const tile_width = 8;
+const tile_height = 8;
+const tile_step_x =  8;
+const tile_step_y =8;
 
-//image1 const tile_width = 8;
-//const tile_height = 8;
 
 
 function draw () {
-//  for(let i=0;i<5000;i++) {
-  //  let x = floor(random(sourceImg.width));
-//    let y = floor(random(sourceImg.height));
-//    let pix = sourceImg.get(x, y);
-//    let mask = maskImg.get(x, y);
-//    fill(pix);
-//    stroke(pix);
+
 
 for(let y=0; y<height; y = y + tile_step_y) {
     for(let x=0; x<width; x = x + tile_step_x) {
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
-      fill(pix);
-      if(mask[0] > 128) {
-                ellipse(x, y, tile_width, tile_height);
 
-        
+
+//white mask
+      noFill();
+      stroke(pix) ;
+      strokeWeight(1.5);
+
+
+      ;
+      if(151 < mask[0] && mask[0] > 180) {
+      ellipse(x, y, tile_step_x+3, tile_step_y+3);
+      //console.log('white is true');
       }
+//grey mask
+      else if (mask[0] ==97){
+        fill(pix);
+        stroke(20) ;
+        strokeWeight(3);
+        rect(x, y, tile_width-3, tile_height-3);
+
+     console.log('grey1 is true');
+
+      }
+//light grey mask
+      else if(mask[0] == 180){
+        fill(pix);
+      //  stroke(221, 255, 0);
+      stroke(203,0,255);
+    //  stroke(255,0,242);
+  //  stroke(255,0,59);
+    //stroke(170,0,255);
+
+        strokeWeight(5);
+        line(x, y, x+10, y+10);
+
+       //console.log('180 is true');
+      }
+//dark grey mask
+   else if(mask[0] == 56){
+     stroke(228, 127, 255);
+     strokeWeight(5);
+     line(x, y, x+10, y+10);
+   }
+
+
+      //black mask
       else {
-        rect(x, y, tile_step_x, tile_step_y);
+      stroke(0);
+      fill(pix);
+      strokeWeight(5);
+      rect(x, y, tile_width-2, tile_height-2);
+  //  console.log('black is true');
       }
     }
 
-    }
-//image 1
-    //if(mask[0] > 128) {
-    //  let pointSize = 2;
-    //  let x2 = floor(random(sourceImg.width));
-    //  let y2 = floor(random(sourceImg.height));
-    //  line(x, y, 128, 1038);
 
-  //  }
-    //else {
-
-    //  let pointSize = 20;
-    //  ellipse(x, y, pointSize, pointSize);
-  //  }
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    //ssaveArtworkImage(outputFile);
+// saveArtworkImage(outputFile);
   }
 
 
@@ -88,4 +107,5 @@ function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
 }
