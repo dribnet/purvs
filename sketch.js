0c6a0523c56e0e3247d74f2c77bd4a66ad08b401
Vaniza Ivy Nacion <vabizaivyd@gmail.com>
2019-06-04 14:33:00 +1200

    Grid techniques

--- sketch.js
+++ sketch.js
@@ -22,28 +22,48 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tile_width =4;
+const tile_height = 10;
+const tile_step_x = 5;
+const tile_step_y = 5;
+
 function draw () {
-  for(let i=0;i<5000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 50;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      rect(x, y, pointSize, pointSize);    
+  for(let y = 0; y < height; y = y + tile_step_y){
+    for(let x = 0; x < width; x = x + tile_step_x){
+       let pix = sourceImg.get(x, y);
+       let mask = maskImg.get(x, y);
+       fill(pix);
+       if (mask[0] > 128){
+         rect (x,y, tile_width, tile_height);
+       }else{
+         rect(x, y, tile_step_x, tile_step_y);
+       }
+
     }
   }
+
+
+  // for(let i=0;i<5000;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   let pointSize = 10;
+  //   let halfSize = 50;
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+  //     ellipse(x, y, pointSize, pointSize);
+  //   }
+  //   else {
+  //     rect(x, y, pointSize, pointSize);
+  //   }
+  // }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-   //saveArtworkImage(outputFile);
+   // saveArtworkImage(outputFile);
   }
 }
 
