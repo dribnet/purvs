b8b6cdce8fd1b86fff209e61eab10e710c8380f9
Rielle <riellebatuigas@gmail.com>
2019-06-04 17:48:58 +1200

    ecerything done again

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 ## Creative Coding 2: Custom Pixel
 
-The three images on top are my experiment not my finals. I just wanted to see how I can do this assignment and what I can do for my theme. 
-
-The pictures I have chosen are pictures of different coloured sunset I have took when I was back in Nelson. The family house in Nelson has a beautiful view of the beach and the sunset. It's sometimes different colours. So I had to choose that theme as I always love watching the sunset. 
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,16 +23,15 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<5000;i++) {
+  for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
+    let pointSize =10;
     fill(pix);
     if(mask[0] > 128) {
-      let pointSize = 20;
+      // let pointSize = 20;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
