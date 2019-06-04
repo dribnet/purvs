29d88f9cd595e321737f44d77d849a16e1299981
CherynJ <389290610@qq.com>
2019-06-04 16:01:19 +1200

    modified: images

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -50,36 +50,36 @@ function draw () {
       let mask = maskImg.get(x, y);
       fill(pix);
       stroke(pix);
-      if(mask[0] == 0) {
+      if(mask[0] < 110) {
         // draw_some_lines(x, y);
         noStroke();
         let offset = random(-3,3);
-        ellipse(x+offset, y+offset, tile_width*1.5, tile_height*1.5);
+        ellipse(x+offset, y+offset, tile_width*1.2, tile_height*1.2);
   
              
       }
-      if(mask[0] == 255) {
+      if(mask[0] > 140) {
         // draw_some_lines2(x, y);
         noFill();
       stroke(pix);
       strokeWeight(1);
       //ellipse(x+x2, y+y2, pointSize, pointSize);
       squareBase = map(y, 0, 500, 35, 2);
-      squareSize = squareBase + floor(random(3, 15));
+      squareSize = squareBase + floor(random(3, 56));
       let offset = random(-3,3);
       rect(x, y, squareSize, squareSize);  
       } 
-      else if (mask[0] > 80 && mask[0] < 200){
+      else if (mask[0] > 110 && mask[0] < 140){
         stroke(255, 204, 0);
         fill(255, 204, 0);
         let offset = random(-3,3);
-        let pointSize = 5
-        rect(x+offset, y+offset, tile_width, tile_height);
+        let pointSize = 10
+        ellipse(x+offset, y+offset, pointSize, pointSize);
       }
     }
   }
 
-  for(let y=0; y<height; y = y + tile_step_y2) {
+   for(let y=0; y<height; y = y + tile_step_y2) {
     for(let x=0; x<width; x = x + tile_step_x2) {
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
@@ -92,6 +92,7 @@ function draw () {
       }
     }
   }
+
 
 
 
