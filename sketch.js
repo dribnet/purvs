32bcc622c84d0338404e18b04fd8cadf7872562f
Rielle <riellebatuigas@gmail.com>
2019-06-04 17:16:27 +1200

    Finished

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,19 +23,25 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
+  for(let i=0;i<5000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 100;
+    let pointSize = 20;
     let halfSize = 50;
     fill(pix);
     if(mask[0] > 128) {
+      let pointSize = 20;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x, y, pointSize, pointSize);    
+      let pointSize = 3;
+      let x2 = floor(random(sourceImg.width));
+      let y2 = floor(random(sourceImg.height));
+      rect(x, y, pointSize, pointSize);
+      arc(x, y, pointSize, pointSize, 0, PI + QUARTER_PI, PIE);
+
     }
   }
   renderCounter = renderCounter + 1;
