3b1afddd53f503be4cac52375be01fa101426881
Heidi Truong <hien.truongg@gmail.com>
2019-06-04 12:27:42 +1200

    I didnt push it up correctly and just save it in the files. So here I'm just undo and push up to show the process

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -22,22 +22,28 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tile_width = 8;
+const tile_height = 8;
+const tile_step_x = 10;
+const tile_step_y = 10;
+
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+ background(50);
+  for(let y=0; y<height; y=y+tile_step_y){
+    for(let x=0; x<width; x=x+tile_step_x){
+      let pix = sourceImg.get(x,y);
+      let mask = maskImg.get(x,y);
+      fill(pix);
+      if(mask[0]>128){
+      rect(x,y,tile_step_x,tile_step_y);
     }
-    else {
-      rect(x, y, pointSize, pointSize);    
+    else{
+      rect(x,y,tile_width,tile_height);
+    }
+
     }
   }
+  
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
