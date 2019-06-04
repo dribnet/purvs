d173f0b6b48b4a4b9f13715022b47479417ff483
Reuben McSweeney <reuben.mcsweeney@gmail.com>
2019-06-04 15:52:01 +1200

    Final

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(150);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -25,54 +25,75 @@ function setup () {
 function draw () {
   // x and y are position of pixels
 
-  for(let i=0;i<13000;i++) {
+  for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     let pointSize = random(4,15);
     let halfSize = 50;
+    // Array to set probability of dot generating
     let dist = [0,0,0,0,0,0,0,0,0,0,0,0,0,20,-20];
     let dist1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,,0,0,0,0,0,0,0,0,0,0,0,40,-40];
 
-    
+
+    let spikeprob = [0,0,0,0,0,0,0,1];
+    let spike = random(spikeprob)
+
+
+   // Changing colour of background to gray
     let grayscale = (pix[0]+pix[1]+pix[2])/3
     fill(grayscale);
     stroke(grayscale)
 
     if(mask[0] > 128) {
-      //line(x, y, x+pix[2]/100, pix[1]/100);
-      //print(pix);
-      //triangle(x, y, x+pointSize, y+(pointSize*(2/3)), x+pointSize, y-(pointSize*(2/3)));
+    // picking random number from array
       let outliersx = random(dist)
       let outliersy = random(dist)
 
       let outliersx1 = random(dist1)
       let outliersy1 = random(dist1)
 
+   // Generate dot
       if(outliersx == 20 || outliersy == 20 || outliersx == -20 || outliersy == -20){
         let pointSize = 5
-        ellipse(x+outliersx, y+outliersy, pointSize, pointSize); 
+        ellipse(x+outliersx, y+outliersy, pointSize, pointSize);
       }
       if(outliersx1 == 40 || outliersy1 == 40 || outliersx1 == -40 || outliersy1 == -40){
         let pointSize = 3
-        ellipse(x+outliersx1, y+outliersy1, pointSize, pointSize); 
+        ellipse(x+outliersx1, y+outliersy1, pointSize, pointSize);
       }
-      //print(outliersx,outliersy)
-      ellipse(x, y, pointSize, pointSize);   
-      //ellipse(x+outliersx, y+outliersy, pointSize, pointSize);
+      let pointSize = random(4,15);
+      //Generate background
+      ellipse(x, y, pointSize, pointSize);
+
     }
     else {
 
-      fill(pix)
-      stroke(pix)
+      fill(pix);
+      stroke(pix);
       let pointSizeTri = random(3,15);
-      let shift = -pointSizeTri/3
+      //Centers triangle on coordinates
+      let shift = -pointSizeTri/3;
+      // Rotating and generating triangles
       push();
       translate(x,y);
-      rotate(random(1,360))
-      triangle(shift, shift, shift+pointSizeTri, shift+(pointSizeTri*(2/3)), shift+pointSizeTri, shift-(pointSizeTri*(2/3)));    
-      pop();  
+      rotate(random(1,360));
+      triangle(shift, shift, shift+pointSizeTri, shift+(pointSizeTri*(2/3)), shift+pointSizeTri, shift-(pointSizeTri*(2/3)));
+      pop();
+
+      if (renderCounter >= 10 && spike == 1 ){
+        let stretchx = random(15,30);
+        let stretchy = random(15,30);
+        pointSizeTri = 7;
+        strokeWeight = 0;
+        push();
+        translate(x,y);
+        rotate(random(1,360));
+        triangle(shift, shift, shift+pointSizeTri, shift+(pointSizeTri*(2/3)), shift+pointSizeTri + stretchx, shift-(pointSizeTri*(2/3)) + stretchy);
+        pop();
+
+      }
     }
   }
   renderCounter = renderCounter + 1;
