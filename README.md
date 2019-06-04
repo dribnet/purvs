bee254146a07542fbf3bfef92566d01a214080aa
Chelsey Brown <Chelseybrown2009@icloud.com>
2019-06-04 16:07:10 +1200

    introducing photographs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Starting out, I wanted to gather a understanding of the code and project. I began by playing with the code, understanding the vaariables, andbegan inserting my own images. 
+The theme I have decided to dedicate this project to, are my pets, both lost and living. The project encouraged us to create and generate work that has meaning to us, and rather than using photographs of myself, my friends, family or hobbies, I've decided to go in a direction that is sometimes overshadowed by life itself, our pets. Regardless of what we're going through and how busy our lives are, our pets are always there for us, so for this project I have decided to shine light on the animals we chose to bring in our lives, and pay tribute everything they bring us. 
+
+Overall I am happy with my final iterations and believe that I have created work that is to the best of my abilities.
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -28,22 +28,61 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
+    let pointSize = 100;
+
+    //fixed to all inputs
     fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+    noStroke();
+    if(mask[0] >= 155) {
+    rect (x, y, x/5, y/5);
     }
-    else {
-      rect(x, y, pointSize, pointSize);    
-    }
+
   }
+
+//input 2
+    const tile_width3 = 8;
+    const tile_height3 = 8;
+
+    for (let y = 0; y < height; y = y + tile_height3){
+      for (let x = 0; x < width; x = x + tile_width3){
+        let pix = sourceImg.get (x, y);
+        let mask = maskImg.get (y, x);
+        noFill();
+        stroke(pix);
+        if (mask[0] >= 50 && mask[0] < 150){
+          push();
+          stroke(pix);
+          strokeWeight(2);
+          line (x+10, y-10, x-10, y-10);
+          pop();
+        }
+      }
+    }
+
+    // background
+
+    for (let y = 0; y < height; y = y + tile_height6){
+      for (let x = 0; x < width; x = x + tile_width6){
+        let pix = sourceImg.get (x, y);
+        let mask = maskImg.get (x, y);
+        noFill();
+        stroke(pix);
+        strokeWeight(2);
+        if (mask[0] <= 0){
+          ellipse (x, y, tile_width6/2.5, tile_height6/2.5);
+        }
+      }
+    }
+
+
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-  //saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
@@ -52,3 +91,6 @@ function keyTyped() {
     saveBlocksImages();
   }
 }
+  
+
+  
