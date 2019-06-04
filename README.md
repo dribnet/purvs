66e9828b21dcc25887b272e3ead27df7822f3ad1
Wenlin Zhang <lynnetezh@gmail.com>
2019-06-04 13:10:24 +1200

    test

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## Crime Scene
 
-I finished edit one of the three images and there are half of the works that need to done. 
-My furthering works is going to finished the design as I planed before and keep polishing the works. 
+The final submission:
 
-This series of work is relate to crime scene and it's appear the imagination from a detective's head. Therefore, those images will contain a lot of elements that relate to detective film. 
+The inspiration of this project is from Crime Scene. The main hue of those images is dark blue, mystery. What I try to display is making a crime scene appear in viewer's mind when they look at those photos. However, this series of work is not display how a murderer muder someone. This is more like shows the process of a detective is solving the case. Therefore, there is a lot of elements from detective films (detective's crazy wall, lots of notes). I used different ways to treat the images.  
+
+The story of those images is a murderer that killed someone. A detective used the clue that leave in the scene and analysed the process of crime. 
+
+The First image is a two goblet
+
+//unfinished 
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "artwork_2.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,32 +23,23 @@ function setup () {
 }
 
 
-function drop (x, y, g, s) {
- //  beginShape();
- // strokeWeight(1);
- // vertex(0,-5);
- // quadraticVertex(3, 0, 0, 1);
- // quadraticVertex(-3,0, 0, -5);
- // endShape(CLOSE);
-fill(255, 0,0);
- ellipse(0,0,15);
-}
+
 
 
 
 function draw () {
 
      // rect(0,0,704, 1252);
-  for(let i=0;i<10000;i++) {
+  for(let i=0;i<16000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 7;
+    let pointSize = 15;
     let offset = pointSize - 4;
     let offset2 = pointSize - 3;
-    let halfSize = 5;
-    let halfSize2 =12;
+    let halfSize = 3;
+    let halfSize2 =100;
      let x1 = floor(random(sourceImg.width));
       let y1 = floor(random(sourceImg.height));
       let r = pix[0];
@@ -56,86 +47,131 @@ function draw () {
       let b = pix[2];
 
     fill(pix);
-    if(mask[0] > 200) {
+    if(mask[0] > 250) {
       noStroke();
-      //ellipse(x, y, pointSize, pointSize);
-     
+      //rect(x, y, halfSize, halfSize2);
+      // var die = int(random(0,100));
+      //     if (die == 5) {
+      //       var gray = int(random(0, 100));
+      //       var gray2 = int(random(0, 50));
+      //       var scalar = random (0.2, 0.45);
+      //      ellipse(x,y,scalar,gray);
+      //     ellipse(x,y,gray2,scalar);
+      //          noStroke();
+      //          fill(255, 0,0);
+      //          ellipse(x,y,5,5);
+      //     }
 
+      //noFill();
+      stroke(pix);
+      rect(x, y, 2, 50);
     }
-    else {
+    else if (mask[0] < 10) {
         //noStroke();
       //  fill(255, 0, 0);
+        //
+        // rect(x, y, halfSize, halfSize2);
+        //   //  fill(pix, 50);
+        //   rect(x, y, halfSize, 20);
+          noFill();
+          stroke(pix);
+         rect(x, y, pointSize, pointSize);
 
-        rect(x, y, halfSize, halfSize2);
-          //  fill(pix, 50);
-          rect(x, y, halfSize, halfSize);
-var die = int(random(0,100));
-    if (die == 5) {
-      var gray = int(random(0, 100));
-      var gray2 = int(random(0, 50));
-      var scalar = random (0.2, 0.45);
-     ellipse(x,y,scalar,gray);
-    ellipse(x,y,gray2,scalar);
-         noStroke();
-         fill(255, 0,0);
-         ellipse(x,y,50,1);
+             var die = int(random(0,100));
+     if (die == 1) {
+       var gray = int(random(0, 50));
+       var gray2 = int(random(0, 50));
+       var scalar = random (0.2, 0.45);
+       fill( 255, 0,0);
+      ellipse(x,y,5,5);
+      ellipse(x,y,scalar, gray);
+     }
 
-    }
+         
+
       }
-
-
 
   }
 //fill(255, 0,0);
 
-
+//lip print
   for(let y=0; y<height; y = y + 1) {
      for(let x=0; x<width; x = x + 1) {
        let pix = sourceImg.get(x, y);
        let mask = maskImg.get(x, y);
-       if(mask[0] == 255) {
+       if(mask[0] == 157) {
          let r = pix[0];
          let g = pix[1];
          let b = pix[2];
         // let g = pix[1];
          stroke(r,g,b);
          point(x, y);
-        
-          fill(255, 0, 0);
-    var die = int(random(0,10));
-    if (die == 5) {
-      var gray = int(random(0, 100));
-      var gray2 = int(random(0, 50));
-      var scalar = random (0.2, 0.45);
-     ellipse(x,y,scalar,gray);
-    ellipse(x,y,gray2,scalar);
-         noStroke();
-         fill(255, 0,0);
-         ellipse(x,y,50,1);
 
+    //       fill(255, 0, 0);
+    // var die = int(random(0,10));
+    // if (die == 5) {
+    //   var gray = int(random(0, 100));
+    //   var gray2 = int(random(0, 50));
+    //   var scalar = random (0.2, 0.45);
+    //  ellipse(x,y,scalar,gray);
+    //  ellipse(x,y,gray2,scalar);
+    //      noStroke();
+    //      fill(255, 0,0);
+    //      ellipse(x,y,50,1);
+    //
+    // }
+
+       }
+     }
+   }
+
+
+   for(let y=0; y<height; y = y + 1) {
+      for(let x=0; x<width; x = x + 1) {
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+        if(mask[0] == 106) {
+          let r = pix[0];
+          let g = pix[1];
+          let b = pix[2];
+         // let g = pix[1];
+          stroke(r,g,b);
+        //  point(x, y);
+          // fill(255, 0, 0);
+     var die = int(random(0,20));
+     if (die == 5) {
+       var gray = int(random(0, 50));
+       var gray2 = int(random(0, 50));
+       var scalar = random (0.2, 0.45);
+      ellipse(x,y,scalar,gray);
+     }
+
+
+
+
+
+
+        }
+      }
     }
-     
-       }
-     }
-   }
 
 
 
-for(let y=0; y<height; y = y + 1) {
-     for(let x=0; x<width; x = x + 1) {
-       let pix = sourceImg.get(x, y);
-       let mask = maskImg.get(x, y);
-       if(mask[0] == 100) {
-       let r = pix[0];
-         let g = pix[1];
-         let b = pix[2];
-        // let g = pix[1];
-         stroke(r,g,b);
-         point(x, y);
-     
-       }
-     }
-   }
+// for(let y=0; y<height; y = y + 1) {
+//      for(let x=0; x<width; x = x + 1) {
+//        let pix = sourceImg.get(x, y);
+//        let mask = maskImg.get(x, y);
+//        if(mask[0] == 156) {
+//        let r = pix[0];
+//          let g = pix[1];
+//          let b = pix[2];
+//         // let g = pix[1];
+//          stroke(r,g,b);
+//          point(x, y);
+//
+//        }
+//      }
+//    }
 
 
 
@@ -144,34 +180,36 @@ for(let y=0; y<height; y = y + 1) {
    noStroke();
    stroke(255, 0,0);
    strokeWeight(1);
-   line(322, 361, 47, 761);
-   line(47, 761,50, 1010);
-   line(50, 1010, 653, 354);
-   line(653, 354,47, 761);
-   line(322, 361,653, 354);
-   
+   line(249, 430, 608, 499);
+   line(608, 499,259, 1031);
+   line(259, 1031, 249, 430);
+   line(608, 499,256, 734);
+   line(259, 1031,581, 965);
+   //
+   //
+   ellipse(249, 430, 8,8);
+   ellipse(608, 499, 8,8);
+   ellipse(259, 1031, 8,8);
+   ellipse(256, 734, 8,8);
+    ellipse(581, 965, 8,8);
 
-   ellipse(322, 361, 8,8);
-   ellipse(653, 354, 8,8);
-   ellipse(47, 761, 8,8);
-   ellipse(50, 1010, 8,8);
 
-
-   
+   //
    fill(255);
    noStroke();
-   textFont('Chalkduster');;
-   text('Long Hair', 280, 589);
-   // text('Antisocial Personality', 100, 864);
-   // text('No fingerprint', 201, 186);
-   // text('Murderer',560, 100);
+   textSize(16);
+   textFont('Chalkduster');
+   text('Blood and wine mixture', 135, 1075);
+   text('No trace', 580, 481);
+   text('From victim', 199, 415);
+   // text('Women',249, 596);
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+ saveArtworkImage(outputFile);
   }
 
 
