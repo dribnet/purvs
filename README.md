57e908855bb387f8f0dfa20ade580b7c0161cbf1
Leigh Tuala-Lauvao <tualalauvao@gmail.com>
2019-06-04 13:44:27 +1200

    Purview

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Creative Coding 2: Custom Pixel
 
-Tuesday 04 June.
+THEME: PASIFIKA IN AOTEAROA NEW ZEALAND
 
-This is not the final as of yet. I added some fill colour to the background triangles just to see what it looks like. It does bring out the greyscale images well but not sure about it meshing together with the final image. I still like, however, I might go back and experiment more with the code to get a final look that I am entirely happy with. 
 
-In terms of experimenting with the code I will look at the colour, stroke and tiles to see what else I can implement. 
 
--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "artwork_1.png";
 
 function preload() {
@@ -26,7 +26,7 @@ function setup () {
 const tile_width = 8;
 const tile_height = 10;
 const tile_step_x = 6;
-const tile_step_y = 10;
+const tile_step_y = 8;
 
 //--- DRAW TILES AND PIXELS ---//
 function draw () {
@@ -43,7 +43,7 @@ function draw () {
       else {
         stroke(255);
         triangle(x+(tile_width/2), y, x + tile_width, y + tile_height, x, y + tile_height);
-        fill(233, 45, 79, 200);
+        //fill(233, 45, 79, 200);
         triangle(x+(tile_width/2), y, x + tile_width, y + tile_height, x + (tile_width*1.5),y);
 
       }
