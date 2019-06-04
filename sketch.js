7be6efc5c6746fddb1bbf41e4cacaf1dc1e0785d
MuteShockVUW <changbill@myvuw.ac.nz>
2019-06-04 13:06:12 +1200

    Final changes. Purview updates coming later

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-function flake (x, y){
+function flake (x, y){ //8 point star
 	push()
 	translate(x, y)
     for(i=0; i<10; i++){
@@ -37,7 +37,7 @@ function flake (x, y){
     }
     pop()
 }
-function flare (x, y){
+function flare (x, y){ //parallelogram
 	beginShape();
 	  vertex(x-10, y-5);
 	  vertex(x+10, y-5);
@@ -46,7 +46,7 @@ function flare (x, y){
      // vertex(x-30, y+20);
 	endShape(CLOSE);
 }
-function star (x, y){
+function star (x, y){ //4 point star
 	beginShape()
       vertex(x, y);
       vertex(x+5, y-15);
@@ -58,17 +58,17 @@ function star (x, y){
       vertex(x-5, y-15);
     endShape(CLOSE);
 }
-function blade(x, y){
+function blade(x, y){ //3 point star
 	beginShape()
-	  vertex(x-20, y-20);
-	  vertex(x, y-10);
-	  vertex(x+20, y-20);
-	  vertex(x+5, y);
-	  vertex(x, y+20);
-	  vertex(x-5, y);
+	  vertex(x-15, y-20);
+	  vertex(x, y-7);
+	  vertex(x+15, y-20);
+	  vertex(x+2, y);
+	  vertex(x, y+15);
+	  vertex(x-2, y);
 	endShape(CLOSE);
 }
-function draw () {
+function draw () { //4 grayscales used to represent change in elements
   for(let i=0;i<4000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -77,22 +77,22 @@ function draw () {
     let pointSize = 20;
     let halfSize = 50;
     fill(pix);
-    if(mask[0] >= 0 && mask [0] < 36) {
+    if(mask[0] >= 0 && mask [0] < 36) { //grayscale for 3 point star
     	push()
     	blade(x, y)
     	pop()
    }
-    else if (mask [0] >= 36 && mask [0] < 116){
+    else if (mask [0] >= 36 && mask [0] < 116){ //grayscale for 4 point star
     	push()
     	star(x, y)
     	pop()
     }
-    else if (mask [0] >= 116 && mask [0] < 255){
+    else if (mask [0] >= 116 && mask [0] < 255){ //grayscale for 8 point star
     	push()
     	flake(x, y)
     	pop()
     }
-    else {
+    else { //grayscale for parallelogram
     	push()
     	flare(x, y)
     	pop()
