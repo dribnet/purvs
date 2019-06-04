db0b5e2ea20f9ffcc4bcb5a94932d786fd7ac926
Coco Ma <www.wangjiexi@gmail.com>
2019-06-04 15:33:57 +1200

    final

--- sketch.js
+++ sketch.js
@@ -22,32 +22,62 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tile_width = 10;
+const tile_height = 10;
+const tile_step_x = 5;
+const tile_step_y = 5;
+
+
+
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  // for(let i=0;i<2000;i++) {
+    // let x = floor(random(sourceImg.width));
+    // let y = floor(random(sourceImg.height));
+
+for(let y=0; y<height; y = y + tile_step_y) {
+    for(let x=0; x<width; x = x + tile_step_x) {
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
-    fill(pix);
-    stroke(pix);
-    if(mask[0] > 128) {
-      rect(x, y, pointSize, pointSize);   
-    }
-    else {
-        let pointSize = 1;
+    
+
+    if(mask[0] < 250) {
+      let pointSize = 1;
       let x2 = floor(random(sourceImg.width));
-      let y2 = floor(random(sourceImg.height));
-      line(x, y, x2, y2);      
+      let y2 = floor(random(30));
+      stroke(pix);
+      strokeWeight(1);
+      noFill()
+      line(x, y+y2, x2, y+y2); 
     }
   }
+// }
+}
+
+    for(let y=0; y<height; y = y + tile_step_y) {
+    for(let x=0; x<width; x = x + tile_step_x) {
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+   
+
+    if(mask[0] > 250) {
+      stroke(pix);
+      strokeWeight(1)
+      squareBase = map(y, 0, 700, 25, 8);
+      squareSize = squareBase + floor(random(13, 8));
+      noFill();
+      rect(x, y, squareBase, squareSize);
+    }
+  }
+// }
+}
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     //saveArtworkImage(outputFile);
   }
 }
 
