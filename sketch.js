89dc4adf75cd38a91c146ed82bd67144cbeec83b
Hansol Gal <losnah29@gmail.com>
2019-06-04 12:03:43 +1200

    sketch, readme

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
+/* Set to true to make final high-resolution version */
+const finalVersion = false;
+
+/* Default versions of variables */
+let elementSpacing = 20;
+let squareSize = 30;
+let pointSize = 30;
+
+/* Override some variables with high-resolution final version */
+if (finalVersion) {
+  elementSpacing = 20;
+  squareSize = 30;
+  pointSize = 30;
+}
+
 let sourceImg=null;
+
+// Mask for model
 let maskImg=null;
+
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let putputFile = "artwork_3.png";
 
 function preload() {
-  sourceImg = loadImage(sourceFile);
-  maskImg = loadImage(maskFile);
+  sourceImg = loadImage("input_3.jpg");
+  maskImg = loadImage("mask_3.png");
+
+  //Clothes image
+  cl = loadImage("3.png");
 }
 
 function setup () {
@@ -16,36 +35,77 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  noStroke();  
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+const tile_width = 5.5;
+const tile_height = 5.5;
+
+
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<1080/elementSpacing;i++) {
+    let x = int(i * elementSpacing);
+    let y = int(renderCounter * elementSpacing);
+    let dx = floor(random(elementSpacing/2));
+    let dy = floor(random(elementSpacing/2));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 30;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
+    let halfSize = squareSize/2;
+
+    strokeWeight(0.3);
+    for(let y = 0; y < height; y = y + tile_height){
+    for(let x = 0; x < width; x = x + tile_width){}
     }
+
+
+    if(mask[0] > 128) {
+      push();
+      rect(x, y, tile_width, tile_height);
+      pop();
+    }
+
     else {
-      rect(x, y, halfSize, halfSize);    
+      push();
+      // Add random offsets
+      x = x + dx;
+      y = y + dy;
+      stroke(pix);
+      // Outline of squares
+      strokeWeight(2);
+      noFill();
+      rect(x-halfSize, y-halfSize, squareSize, squareSize);   
+      pop();
     }
   }
+
+
+
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 1920/pointSize) {
     console.log("Done!")
     noLoop();
-    // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+
+    //clothes
+    image(cl,350,630);
   }
 }
+
+function star(x,y) {
+  push();
+  noStroke();
+  scale(0.2);
+
+  push();
+  fill(255);
+  stroke(255);
+  strokeWeight();
+  ellipse(100, 100, 30, 30);
+}
+
+
 
 function keyTyped() {
   if (key == '!') {
