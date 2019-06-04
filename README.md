18ad71b3a9758981ec0e6b55b468ec6ac8343693
Alex Birkett <blex.airkett@gmail.com>
2019-06-04 17:36:57 +1200

    Update 5 Edit Code and Masks

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## Creative Coding 2: Custom Pixel
 
-Update 4
+Update 5
 
-I thought that this addition was successful as long as I can successfully input these silhouttes without them taking focus over the photographs own narrative. To test this better Ī resized and repositioned the sillhouttes to act as if they are currently working on the paintings, this can be seen most successfully in artwork_2. However once I input the grey colour code, it will recognise the silhoutte as a different tone and won't simply draw as black which it is currently doing. 
+The final step was to edit the code to allow the grey coloured masks to be presented clearly, to do so I created four layers. One for each colour on my masked colours. I also created a fourth mask colour, dark grey, which was placed over the painted elements within each photograph. 
 
-The code was also edited to be cleaner, using the rectangle grid code.
+I then edited the tile step for each layer allowing me to adjust the resolution for each mask colour individually. The aim of this to show the important sections of the image to strengthen the message within the photographs. To do so I had the lowest tile step for the dark grey so the painted element was very clear and readible. The silhoutte was second lowest allowing it be recognizable and obviously part of the narrative. While blending into the ellipses of the building/environment around the painting which were size higher at .5 apart.   
+
+The only issue with the work is that their are sections on each mask that appear black in the final design. This is frustrating as I haven't been able to find a solution to this error, if you compare the mask directly to the final coded design OR the mask with the photo you'll see that they line up perfectly. In addition you can remove each layer in the code, and these black lines with still appear. To combat this I changed from rectangles to ellipses as this allowed the rectangle lines to not 
 
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -17,56 +17,79 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-const tile_width = 6;
-const tile_height = 6;
-const tile_step_x = 8;
-const tile_step_y = 8;
+const tile_width = 3;
+const tile_height = 3;
+//white
+const tile_step_x1 = 3;
+const tile_step_y1 = 3;
+//light grey
+const tile_step_x2 = 2;
+const tile_step_y2 = 2;
+//dark grey
+const tile_step_x3 = 1;
+const tile_step_y3 = 1;
+//black
+const tile_step_x4 = 3.5;
+const tile_step_y4 = 3.5;
+
 
 function draw () {
-for(let y=0; y<height; y = y + tile_step_y) {
-  for(let x=0; x<width; x = x + tile_step_x) {
-  let pix = sourceImg.get(x, y); 
-  let mask = maskImg.get(x, y); 
-  fill(pix);
-  if(mask[0] > 127) {
-    rect(x, y, tile_step_x/1.1, tile_step_y/1.1);
-  }
-  else {
-    rect(x, y, tile_step_x /1.3, tile_step_y /1.3);
-  }
-}
-}
+  background(0);
 
-  /*
-  for(let i=0;i<2000;i++) {
-
-   //figure out x + y
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    //colour of picture (RGB), colour of mask (B & W)
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 5;
-    fill(pix);
-    if(mask[0] > 128) {
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      rect(x, y, pointSize/2, pointSize/2);    
+//white mask layer
+  for(let y=0; y<height; y = y + tile_step_y1) {
+    for(let x=0; x<width; x = x + tile_step_x1) {
+      let pix = sourceImg.get(x, y); 
+      let mask = maskImg.get(x, y); 
+      fill(pix);
+      if(mask[0] > 250) {
+        ellipse(x, y, tile_step_x1, tile_step_y1);
+      }
     }
   }
-  */
+//black mask layer
+  for(let y=0; y<height; y = y + tile_step_y4) {
+    for(let x=0; x<width; x = x + tile_step_x4) {
+      let pix = sourceImg.get(x, y); 
+      let mask = maskImg.get(x, y); 
+      fill(pix);
+      if(mask[0] <= 0) {
+        ellipse(x, y, tile_step_x4, tile_step_y4);
+      }
+    }
+  }
+//light grey mask layer
+  for(let y=0; y<height; y = y + tile_step_y2) {
+    for(let x=0; x<width; x = x + tile_step_x2) {
+      let pix = sourceImg.get(x, y); 
+      let mask = maskImg.get(x, y); 
+      fill(pix);
+      if(mask[0] <= 250 && mask[0] > 150) {
+        ellipse(x, y, tile_step_x2, tile_step_y2);
+      }
+    }
+  }
+//dark grey mask layer
+  for(let y=0; y<height; y = y + tile_step_y3) {
+    for(let x=0; x<width; x = x + tile_step_x3) {
+      let pix = sourceImg.get(x, y); 
+      let mask = maskImg.get(x, y); 
+      fill(pix);
+      if(mask[0] <= 130 && mask[0] > 10) {
+        ellipse(x, y, tile_step_x3, tile_step_y3);
+      }
+    }
+  }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
-    // uncomment this to save the result
+   // uncomment this to save the result
     saveArtworkImage(outputFile);
   }
 }
