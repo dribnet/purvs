1a8297ef27d5851e7caf0dae06b165d995241838
machersand <weazel.sidekick@gmail.com>
2019-06-04 13:44:59 +1200

    First Commit
    First Commits submitted

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+The images above are of random pedestrians around the Wellington CBD and outskirts. I've decided to base my theme around "Urban Isolation" - the idea that despite us being constantly surrounded by people at all times in cities, many are made to feel alone nonethless. It is as if the constant influx of people depersonalizes us and make us feel that much less in touch with everybody around us.
+
+Thus when picking my subjects I wanted to focus on concepts of distances, through rack focus, as well as portraying them in contrast to something. This could be other groups of people or large empty spaces - anything that further emphasizes my theme.
+
+The code utilizes a simple background grid so far, and simply blurs out the foreground. Although I'm working on making the code more complex, as well as making the foreground stand out more from the background
--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "artwork_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -22,28 +22,56 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tile_width = 4;
+const tile_height = 6;
+const tile_step_x = 6;
+const tile_step_y = 10;
+
 function draw () {
-  for(let i=0;i<2000;i++) {
+  background(50);
+
+  // version 1: just draw all the tiles
+  for(let y=0; y<height; y = y + tile_step_y) {
+    for(let x=0; x<width; x = x + tile_step_x) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
+      if(mask[0] > 128) {
+        rect(x, y, tile_step_x, tile_step_y);
+      }
+      else {
+        rect(x, y, tile_width, tile_height);
+      }
+    }
+  }
+
+/*
+  for(let i=0;i<1000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let pointSize = 20;
-    let halfSize = 50;
     fill(pix);
+    stroke(pix);
     if(mask[0] > 128) {
+      let pointSize = 10;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
-      rect(x, y, pointSize, pointSize);    
+      let pointSize = 2;
+      let x2 = floor(random(sourceImg.width));
+      let y2 = floor(random(sourceImg.height));
+      // rect(x, y, pointSize, pointSize);
+      line(x, y, x2, y2);    
     }
   }
+*/
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     saveArtworkImage(outputFile);
   }
 }
 
