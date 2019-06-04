ccfa6cdba0e6377bace6875fef6749dc2ba4bae0
Lennart Schiffers <mail@lennart-schiffers.de>
2019-06-04 15:07:45 +1200

    added third picture

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "artwork_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "artwork_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -25,23 +25,44 @@ function setup () {
 const tile_width = 6;
 const tile_height = 6;
 
+const tile_width1 = 14;
+const tile_height1 = 14;
+
 function draw () {
   for(let y=0; y<height; y = y + tile_height +2) {
     for(let x=0; x<width; x = x + tile_width +2) {
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
-      fill(pix);
-      if(mask[0] > 128) {
-        let shade = pix[2];
+      let shade = pix[0];
+
+      if(mask[0] == 255) {
         fill(shade);
         rect(x, y, tile_width, tile_height);
       }
-      else {
+      if(mask[0] == 0) {
         fill(pix);
+        rect(x, y, tile_width, tile_height);
+      }
+      if(mask[0] == 155) {
+        fill(255);
         rect(x, y, tile_width, tile_height);
       }
     }
   }
+  for(let y=0; y<height; y = y + tile_height1 +2) {
+    for(let x=0; x<width; x = x + tile_width1 +2) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      let shade = pix[0];
+      if(mask[0] == 155) {
+        fill(shade);
+        rect(x, y, tile_width1, tile_height1);
+      }
+    }
+  }
+    
+  
+
 
 
   
@@ -50,7 +71,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    //saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
