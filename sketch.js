dccc76d38a768d653eb6de7ad880a3b655edf9eb
Francesca Armstrong <armstrfran@myvuw.ac.nz>
2019-06-04 15:16:31 +1200

    Final update

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "artwork_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -21,8 +21,8 @@ function setup () {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-const tile_width = 4;
-const tile_height = 4;
+const tile_width = 4.5;
+const tile_height = 4.5;
 
 function draw () {
   strokeWeight(1);
