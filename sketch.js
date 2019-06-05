87d3f2dc56acc39834b6e22506e5162d309afd5d
Vaniza Ivy Nacion <vabizaivyd@gmail.com>
2019-06-05 12:25:43 +1200

    Final commmit

--- sketch.js
+++ sketch.js
@@ -22,10 +22,10 @@ function setup () {
   maskImg.loadPixels();
 }
 
-const tile_width =4;
-const tile_height = 8;
-const tile_step_x = 5;
-const tile_step_y = 8;
+const tile_width =7;
+const tile_height = 10;
+const tile_step_x = 8;
+const tile_step_y = 11;
 
 function cross(x,y,s){
   push();
@@ -39,50 +39,77 @@ function cross(x,y,s){
   pop();
 }
 
+function dots(x,y,s){
+  push();
+  translate(x,y);
+  scale(s);
+  let pix = sourceImg.get(x, y);
+  stroke(pix);
+  strokeWeight(8);
+  ellipse(0,0,10,10);
+  pop();
+}
+
 
 
 function draw () {
+
   for(let y = 0; y < height; y = y + tile_step_y){
     for(let x = 0; x < width; x = x + tile_step_x){
        let pix = sourceImg.get(x, y);
        let mask = maskImg.get(x, y);
        fill(pix);
-       // stroke(pix);
+       //WHITE
+       if (mask[0] == 255){
+         // rect (x,y, tile_width, tile_height);
+       }
+       else{
+         var die = int(random(0,6));
+         if (die == 3){
+          dots(x,y, 0.25)
+          }
+         }
+       //BLACK
+       if(mask[0] == 0){
+         // rect(x, y, tile_step_x, tile_step_y);
+         // var die = int(random(0,6));
+         // if (die == 3){
+         //   var scale = random(0.5, 0.8);
+         //   cross(x,y,scale);
+         }//GREY
+         else{
+         var die = int(random(0,6));
+         if (die == 3){
+          dots(x,y, 0.25)
+          }
+         }
+       }
+  }
 
-       if (mask[0] > 128){
+  for(let y = 0; y < height; y = y + tile_step_y){
+    for(let x = 0; x < width; x = x + tile_step_x){
+       let pix = sourceImg.get(x, y);
+       let mask = maskImg.get(x, y);
+       fill(pix);
+       //WHITE
+       if (mask[0] == 255){
+        fill(pix);
          rect (x,y, tile_width, tile_height);
-
-       }else{
+       }
+       //BLACK
+       if(mask[0] == 0){
          rect(x, y, tile_step_x, tile_step_y);
          var die = int(random(0,6));
          if (die == 3){
-           var scale = random(0.5, 0.8);
+           var scale = random(0.2, 0.5);
            cross(x,y,scale);
          }
-
-
-         // draw_line(x,y);
+       }else{
+        // dots(x,y, 0.5);
        }
-
     }
   }
 
-
-  // for(let i=0;i<5000;i++) {
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pix = sourceImg.get(x, y);
-  //   let mask = maskImg.get(x, y);
-  //   let pointSize = 10;
-  //   let halfSize = 50;
-  //   fill(pix);
-  //   if(mask[0] > 128) {
-  //     ellipse(x, y, pointSize, pointSize);
-  //   }
-  //   else {
-  //     rect(x, y, pointSize, pointSize);
-  //   }
-  // }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
@@ -91,6 +118,7 @@ function draw () {
    saveArtworkImage(outputFile);
   }
 }
+
 
 function keyTyped() {
   if (key == '!') {
