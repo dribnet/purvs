4beb9afefcaed9cdc83c7b9be2baa9f468895775
Robert Wilkins <robertwilkins741@gmail.com>
2019-06-07 11:07:32 +1200

    New polygonal/isolationist project

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 ## Creative Coding 2: Custom Pixel
 
 
-#### Theme - Stop and smell the roses
-My theme is about enjoying life. This images are a series of smiling people. Left image is me after I'd gone hiking along the top of a hill in a thunderstorm at midnight. The middle is an image of my friend when he was in New Zealand on holiday standing in front a waterfall, and the third is my parents on their 26th year of an unbelievably happy marriage. The idea is to play off of the phrase, 'stop and smell the roses' to remind people to do just that.
+#### The Loneliest Generation
+I've decided to take a dramatic turn with my project. My original idea had a naive message of "Hey, we should be more happy". I'm shifting that message to "We aren't happy. Here's why". My aim is to provoke people into thinking more deeply about how technology is shifting our social lives fundamentally. What was once social is now parasocial, where we interact socially by *looking at* each others lives instead of engaging in them.
 
-This theme is, in my opinion, pretty shallow. It might be a better idea to think more deeply ont he issue, e.g. *why* people don't stop and smell the roses. But I think I might find a system that's cooler and find a theme around that. I'm considering some sort of a polygonal field. 
+In the past four weeks, 650,000 New Zealanders have felt lonely. Of those people, the largest group were aged between aged 15-24. That number has risen 70% in just two years.
 
+The pictures I've taken are meant to show how technology is contributing to us being in our own personal sea of isolation. The effect on top of them now composes the image out of a sea of polygons. The reason for this is two-fold: first, so to represent the network-like connections in our digital world. And second, to represent how our lives have become more and more digital (as triangles are how 3d models are stored digitally).  
+ 
 #### Technical
-How it works is a modified version of the base pointillism example except where the mask is above 50, draw a big petal graphic that's been tinted a certain colour. The colour of both the graphic and the pixel fades based on the alpha value between the colour of the pixel and rose red. The actual petal graphic is a desaturated image with a transparent background. This has some cool potential applications, where now I can choose an arbitrary series of graphics and use them to build my image, but the current iteration is a bit lack luster.  
+How it works, is it generates a perfect grid of points on the screen. It then offsets each point in a random direction by a random amount. It then goes through each point and draws two triangles, filling out a square between itself, the point next to it, the point below it, and the point next to the point below it. The colour used for each triangle is sampled from the average position of its three points. Currently the mask isn't used, but I'm hoping to remedy that.
--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+// import toxi.geom.*;
+// import toxi.geom.mesh2d.Voronoi;
+// import Voronoi from toxi.geom.mesh2d;
+
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 let graphicElements=[];
 
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_4.png";
-let outputFile = "artwork_2.png";
+let sourceFile = "input_3.png";
+let maskFile   = "mask_1.png";
+let outputFile = "artwork_3.png";
 
 let graphicFile1 = "graphic.png";
 // let graphicFile2 = "graphic_3.png";
@@ -14,8 +18,7 @@ let graphicFile1 = "graphic.png";
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
-  graphicElements[0] = loadImage(graphicFile1);
-  // graphicElements[1] = loadImage(graphicFile2);
+
 }
 
 function setup () {
@@ -27,48 +30,81 @@ function setup () {
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
-
+  // sourceImg.filter(BLUR, 50)
 }
 
 function draw () {
-  let petalColour = color(194, 30, 86, 100);
-  angleMode(DEGREES);
-  for(let i=0;i<9000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = color(sourceImg.get(x, y));
-    let mask = maskImg.get(x, y);
-    let pointSize = 10;
-    let halfSize = 50;
-    if(mask[0] > 200) {
-      if (random(0,1) > 0.01) continue;
-      let petalCol = lerpColor(pix, petalColour, mask[0]/500.0);
-      petalCol = color(petalCol.levels[0], petalCol.levels[1], petalCol.levels[2]);
-      push();
-        translate(x, y);
-        rotate(random(-20, 20));
-        if (random(0, 1) > 0.5 ) scale(-1, 1);
-        tint(petalCol);
-        image(graphicElements[0], 0 , 0);
-      pop();
-    }
-    else {
-      let petalCol = lerpColor(pix, color(194/2, 30/2, 86/2, 100), mask[0]/255.0);
-      fill(petalCol);
-      let randSize = random(0.3, 1.2);
-      ellipse(x, y, pointSize*randSize, pointSize*randSize);
+  let outerPointSpacing = 60;
+  let outerPoints = getPoints(outerPointSpacing);
+
+  // Inner Points
+  let innerPointSpacing = 15;
+  let innerPoints = getPoints(innerPointSpacing);
+
+  // Draw inner points
+  strokeWeight(0.7);
+  for (let i = 0; i < innerPoints.length-1; i++) {
+    for (let j = 0; j < innerPoints[i].length-1; j++) {
+      let p1 = innerPoints[i][j];
+      let p2 = innerPoints[i+1][j];
+      let p3 = innerPoints[i][j+1];
+      let p4 = innerPoints[i+1][j+1];
+
+      // upper left triangle
+      let x = (p1.x + p2.x + p3.x) / 3.0;
+      let y = (p1.y + p2.y + p3.y) / 3.0;
+
+      // if (maskImg.get(x,y)[0] < 50) continue;
+
+      let pix = color(sourceImg.get(x,y));
+      // stroke(lerpColor(pix, color(255), 0.2));
+      stroke(pix);
+      fill(pix);
+      triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+
+      // lower right triangle
+      x = (p2.x + p3.x + p4.x) / 3.0;
+      y = (p2.y + p3.y + p4.y) / 3.0;
+      pix = color(sourceImg.get(x,y));
+      fill(pix);
+      triangle(p2.x, p2.y, p4.x, p4.y, p3.x, p3.y);
+
+      // fill(lerpColor(pix, color(255), 0.6));
+      // ellipse(p1.x, p1.y, 3,3);
 
     }
   }
 
 
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 0) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     saveArtworkImage(outputFile);
   }
+}
+
+
+function getPoints(pointSpacing) {
+  let points = [];
+  for (let y = 0; y < sourceImg.height; y+=pointSpacing) {
+    points.push([]);
+    for (let x = 0; x < sourceImg.width; x+=pointSpacing) {
+
+      let randVec = p5.Vector.random2D();
+      randVec.setMag(pointSpacing/5, pointSpacing/3);
+      let yindex = round(y/pointSpacing);
+      if (yindex % 2 == 0){
+        points[points.length - 1].push(createVector(x + randVec.x, y + randVec.y));
+      } else {
+        points[points.length - 1].push(createVector(x + randVec.x, y + randVec.y));
+
+      }
+    }
+
+  }
+  return points;
 }
 
 function keyTyped() {
