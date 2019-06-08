dd1d583522709ccc656263dbdf1290efa5812b34
Ziyi Chen <chenziyi2@myvuw.ac.nz>
2019-06-08 16:36:36 +1200

    dd

--- sketch.js
+++ sketch.js
@@ -115,7 +115,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+     //saveArtworkImage(outputFile);
   }
 }
 
