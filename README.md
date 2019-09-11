74ebde25c0ec4e3acc2296aca63f82e02ec70dde
Francesca Armstrong <armstrfran@myvuw.ac.nz>
2019-09-12 11:09:19 +1200

    arrangement

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 POSSIBLE ARRANGEMENT
 
-Trying out the idea of possibily having bear bodies for the final arrangement but not sure if I like how they are looking so I definitely need to play around with this further.
+I'm more or less happy with this arrangement for my final hand in. I have tried to create more of a focal point but I have added some faces around the edge to show more of them. I'm not sure if there is a minimum number of faces needed in the final hand in and I can't really tell in the brief so hopefully there isn't.
--- arrangement.js
+++ arrangement.js
@@ -46,30 +46,107 @@ function draw () {
   resetFocusedRandom(curRandomSeed);
 
   // clear screen
-  background(bg_color1);
+  background(bg_color1, 50);
   noStroke();
 
 
 
   //Background
+  
+  //BACKGROUND SETTING
+  fill(4, 46, 17, 75);
+  beginShape();
+  curveVertex(canvasWidth/2 - canvasWidth/6 , canvasHeight/1.55);
+  curveVertex(canvasWidth/2 + canvasWidth/6 , canvasHeight/1.55);
+  curveVertex(canvasWidth/2 + canvasWidth/3.6, canvasHeight/1.55);
+  curveVertex(canvasWidth/2 + canvasWidth/3.6, canvasHeight/1.16);
+  curveVertex(canvasWidth/2 - canvasWidth/3.6, canvasHeight/1.16);
+  curveVertex(canvasWidth/2 - canvasWidth/3.6, canvasHeight/1.55);
+  endShape(CLOSE);
 
+
+  //Bamboo shoots
+  push()
+  translate(0, 5);
+  drawBamboo();
+  translate(-30, 20);
+  rotate(-7);
+  drawBamboo();
+  pop();
   push();
-  drawPandaBody();
-  translate(162, 0);
-  drawBrownBearBody();
-  translate(162, 0);
-  drawPolarBearBody();
+  translate(canvasWidth/2, 0);
+  drawBamboo();
+  translate(30, -10);
+  rotate(10);
+  drawBamboo();
   pop();
 
+  fill(255, 75);
+  rectMode(CENTER);
+  rect(canvasWidth/6, canvasHeight/2, 10, canvasHeight);
+  rect(canvasWidth - canvasWidth/6, canvasHeight/2, 10, canvasHeight);
 
+
+  //BODIES
+  push();
+  translate(272.5, -65);
+  drawBrownBearBody();
+  translate(-360, 0);
+  drawPolarBearBody();
+  translate(180, 0);
+  drawPandaBody();
+  pop();
+
+  //FACES
+
+  
+
+
+
+  //DRAW MAIN FACES
+  push();
+  translate(canvasWidth/2 +10, canvasHeight/2.8);
+  scale(8.5);
+
+  //*********FACE ONE********
+
+  let tallness_value1 = focusedRandom(0,100, 5, 85);
+  let ear_value1 = focusedRandom(0,3 );
+  let cheek_value1 = focusedRandom(0,3, 2.5, 0);
+  let mouth_value1 = focusedRandom(0,100, 5, 5);
+  let eye_value1 = focusedRandom(0,100,5, 80);
+  
+    
+  drawFace1(tallness_value1, ear_value1, cheek_value1, mouth_value1, eye_value1);
+  translate(21, 0);
+
+  //********FACE TWO**********
+  let tallness_value2 = focusedRandom(0, 100, 5, 60);
+  let ear_value2 = focusedRandom(0,3, 3, 0);
+  let eye_value2 = focusedRandom(0,3);
+  let mouth_value2 = focusedRandom(0, 100, 2.5, 50);
+  let mouth_curve2 = focusedRandom(0, 100, 0, 5, 5);
+    
+  drawFace2(tallness_value2, ear_value2, eye_value2, mouth_value2, mouth_curve2);
+  translate(-42, 0);
+
+   //********FACE THREE********
+  let tallness_value3 = focusedRandom(0,100, 5, 60);
+  let ear_value3 = focusedRandom(0,3, 2, 1);
+  let mouth_value3 = focusedRandom(0,100, 0);
+  let mouth_width3 = focusedRandom(0,100, 3, 50);
+  let eye_value3 = focusedRandom(0,3);
+      
+  drawFace3(tallness_value3, ear_value3, mouth_value3, mouth_width3, eye_value3);
+  pop();
 
 
 
   // draw a 7x4 grid of faces
   let w = canvasWidth / 6;
   let h = canvasHeight / 3;
-  for(let i=0; i<2; i++) {
-    for(let j=0; j<6; j++) {
+  for(let i=0; i<3; i++) {
+    for(let j=0; j<1; j++) {
 
       let y = h/2 + h*i;
       let x = w/2 + w*j;
@@ -79,7 +156,7 @@ function draw () {
       scale(w/25, h/25);
       
         if((i+j)%3 == 0) {
-          /*********FACE ONE********/
+          //*********FACE ONE********
           let tallness_value1 = focusedRandom(0,100, 4, 75);
           let ear_value1 = focusedRandom(0,3 );
           let cheek_value1 = focusedRandom(0,3, 2.5, 0);
@@ -89,7 +166,7 @@ function draw () {
         }
 
         else if((i+j)%3 == 1){
-          /********FACE TWO**********/
+          //********FACE TWO**********
           let tallness_value2 = focusedRandom(0, 100, 3, 65);
           let ear_value2 = focusedRandom(0,3, 3, 0);
           let eye_value2 = focusedRandom(0,3);
@@ -98,7 +175,7 @@ function draw () {
           drawFace2(tallness_value2, ear_value2, eye_value2, mouth_value2, mouth_curve2);
         }
         else {
-          /********FACE THREE********/
+          //********FACE THREE********
           let tallness_value3 = focusedRandom(0,100, 3, 60);
           let ear_value3 = focusedRandom(0,3, 2, 1);
           let mouth_value3 = focusedRandom(0,100, 0);
@@ -109,12 +186,66 @@ function draw () {
         pop();
     }
 
-      
   }
+
+  for(let i=0; i<3; i++) {
+    for(let j=5; j<6; j++) {
+
+      let y = h/2 + h*i;
+      let x = w/2 + w*j;
+      // center face
+      push();
+      translate(x, y);
+      scale(w/25, h/25);
+      
+        if((i+j)%3 == 0) {
+          //*********FACE ONE********
+          let tallness_value1 = focusedRandom(0,100, 4, 75);
+          let ear_value1 = focusedRandom(0,3 );
+          let cheek_value1 = focusedRandom(0,3, 2.5, 0);
+          let mouth_value1 = focusedRandom(0,100, 5, 5);
+          let eye_value1 = focusedRandom(0,100,5, 80);
+          drawFace1(tallness_value1, ear_value1, cheek_value1, mouth_value1, eye_value1);
+        }
+
+        else if((i+j)%3 == 1){
+          //********FACE TWO**********
+          let tallness_value2 = focusedRandom(0, 100, 3, 65);
+          let ear_value2 = focusedRandom(0,3, 3, 0);
+          let eye_value2 = focusedRandom(0,3);
+          let mouth_value2 = focusedRandom(0, 100, 2.5, 50);
+          let mouth_curve2 = focusedRandom(0, 100, 0, 5, 5);
+          drawFace2(tallness_value2, ear_value2, eye_value2, mouth_value2, mouth_curve2);
+        }
+        else {
+          //********FACE THREE********
+          let tallness_value3 = focusedRandom(0,100, 3, 60);
+          let ear_value3 = focusedRandom(0,3, 2, 1);
+          let mouth_value3 = focusedRandom(0,100, 0);
+          let mouth_width3 = focusedRandom(0,100, 3, 50);
+          let eye_value3 = focusedRandom(0,3);
+          drawFace3(tallness_value3, ear_value3, mouth_value3, mouth_width3, eye_value3);
+        }
+        pop();
+    }
+
+  }
+   
+}
+function drawBamboo(){
+  fill(122, 150, 132);
+  rectMode(CENTER);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/3, 15, 300, 5);
+  fill(155, 156, 135);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/2.3, 15, 10);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/3, 15, 10);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/4, 15, 10);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/6, 15, 10);
+  rect(canvasWidth/2 - canvasWidth/4, canvasHeight/12, 15, 10);
 }
 
 function drawPandaBody(){
-  fill(225);
+  fill(215);
   rectMode(CENTER);
   rect(canvasWidth/2 - canvasWidth/12, canvasHeight/2 + canvasHeight/4.5, canvasWidth/7.5, canvasHeight/2.5, 80);
   fill(40);
@@ -173,9 +304,8 @@ function drawBrownBearBody(){
   fill(mid_brown);
   rectMode(CENTER);
   rect(canvasWidth/2 - canvasWidth/12, canvasHeight/2 + canvasHeight/4.5, canvasWidth/7.5, canvasHeight/2.5, 80);
-  fill(light_brown);
   rect(canvasWidth/2 - canvasWidth/12, canvasHeight/2 + canvasHeight/7, canvasWidth/8, canvasHeight/7);
-
+  fill(light_brown);
   push();
   translate(canvasWidth/2 - canvasWidth/6.8, canvasHeight/2+ canvasHeight/6);
   rotate(20);
@@ -230,9 +360,9 @@ function drawPolarBearBody(){
   fill(beige);
   rectMode(CENTER);
   rect(canvasWidth/2 - canvasWidth/12, canvasHeight/2 + canvasHeight/4.5, canvasWidth/7.5, canvasHeight/2.5, 80);
-  fill(dark_brown);
+  
   rect(canvasWidth/2 - canvasWidth/12, canvasHeight/2 + canvasHeight/7, canvasWidth/8, canvasHeight/7);
-
+  fill(dark_brown);
   push();
   translate(canvasWidth/2 - canvasWidth/6.8, canvasHeight/2+ canvasHeight/6);
   rotate(20);
