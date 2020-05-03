24ef55969fcd0e3f22f417f61c99ff6f7d7009d5
wrightconn <connorwright247@gmail.com>
2020-05-04 11:13:25 +1200

    MY FIRST ITERATION

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 ## MDDN 242 2020 Assignment 2
+MY FIRST ITERATIONS
 
-(Replace this README with information about your alphabet. This is an example.)
+Before starting my code I used gird paper to draw some designs to figure out what kind of style I wanted to go for. I decided that it would be cool to work with different kinds of shapes to build this letter form. Lots of the designs that I crafted were going to use too many parameters if I decided to go with them. I initially wanted to keep the proportions of the shapes all the same and just adjust the size and rotation but after playing around with the sketch code I noticed that it was going to work for every letter and would use too many parameters. 
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
+I have used 20 parameters which consist of one rectangle, two arcs and also a tringle but I'm currently not using the triangle so I may remove this completely. I want this style of my letter to be clean and minimal, designs that give off enough shape to recognise the letter. I'm going for a playful blocky look.  For the colour palette, I have gone with a soothing light blues to a dark navy blue. One thing I wpouldnt mind trying is playing with the opacity of the letters, i think this will create some cool designs especially when the shapes overlap.
 
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+The twenty parameters per letter:
+  * `rectX` : x location of the rectangle
+  * `rectY` : y location of the rectangle
+  * `rectW` : width of the rectangle
+  * `rectH` : height of the rectangle
+  * `arcW` : width of the arc
+  * `arcH` : height of the arc
+  * `arcX` : x location of the first arc
+  * `arcY` : y location of the first arc
+  * `arcS` : angle to start the first arc
+  * `arcE` : angle to end the first arc
+  * `arcX` : x location of the second arc
+  * `arcY` : y location of the second arc
+  * `arcS` : angle to start the second arc
+  * `arcE` : angle to end the second arc
+  * `triX1` : x location of the first point of the triangle
+  * `triY1` : Y location of the first point of the triangle
+  * `triX2` : x location of the second point of the triangle
+  * `triY2` : Y location of the second point of the triangle
+  * `triX3` : x location of the third point of the triangle
+  * `triY3` : Y location of the thrid point of the triangle
--- sketch.js
+++ sketch.js
@@ -1,75 +1,187 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
-
+const PI = 3.14159;
+const TWO_PI = PI*2;
+const HALF_PI = PI/2;
 /* 
- * my three variable per letter are:
+ * my twenty variable per letter are:
  *
-   size: radius of the second circle (in pixels)
-   offsetx: x offset (in pixels) of the second circle
-            relative to the first one
-   offsety: y offset (in pixels) of the second circle
-            relative to the first one
+   rectX: x location of the rectangle
+   rectY: y location of the rectangle
+   rectW: width of the rectangle
+   rectH: height of the rectangle
+   arcW: width of the arc
+   arcH: height of the arc
+   arcX: x location of the first arc
+   arcY: y location of the first arc
+   arcS: angle to start the first arc
+   arcE: angle to end the first arc
+   arcX: x location of the second arc
+   arcY: y location of the second arc
+   arcS: angle to start the second arc
+   arcE: angle to end the second arc
+   triX1: x location of the first point of the triangle
+   triY1: Y location of the first point of the triangle
+   triX2: x location of the second point of the triangle
+   triY2: Y location of the second point of the triangle
+   triX3: x location of the third point of the triangle
+   triY3: Y location of the thrid point of the triangle
  *
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+    // RECTANGLE
+    "rectX" : 130,
+    "rectY" : 245,
+    "rectW" : 80,
+    "rectH" : 80,
+    // ARC 
+    "arcW"  : 80,
+    "arcH"  : 80,
+    // ARC ONE
+    "arcX"  : 170,
+    "arcY"  : 245,
+    "arcS"  : TWO_PI-PI,
+    "arcE"  : -HALF_PI,
+    //ARC TWO
+    "arcX2" : 170,
+    "arcY2" : 245,
+    "arcS2" : PI+HALF_PI,
+    "arcE2" : 0,
+    // TRIANGLE
+    "triX1" : 0,
+    "triY1" : 0,
+    "triX2" : 0,
+    "triY2" : 0,
+    "triX3" : 0,
+    "triY3" : 0
+   
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+    // RECTANGLE
+    "rectX" : 430,
+    "rectY" : 205,
+    "rectW" : 30,
+    "rectH" : 120,
+    // ARC
+    "arcW" : 80,
+    "arcH" : 60,
+    // ARC ONE
+    "arcX" : 460,
+    "arcY" : 235,
+    "arcS" : PI+HALF_PI,
+    "arcE" : HALF_PI,
+    // ARC TWO
+    "arcX2" : 460,
+    "arcY2" : 295,
+    "arcS2" : PI+HALF_PI,
+    "arcE2" : HALF_PI,
+    // TRIANGLE
+    "triX1" : 0,
+    "triY1" : 0,
+    "triX2" : 0,
+    "triY2" : 0,
+    "triX3" : 0,
+    "triY3" : 0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+    // RECTANGLE
+    "rectX" : 765,
+    "rectY" : 205,
+    "rectW" : 30,
+    "rectH" : 120,
+    // ARC
+    "arcW" : 120,
+    "arcH" : 120,
+    // ARC ONE
+    "arcX" : 765,
+    "arcY" : 265,
+    "arcS" : HALF_PI,
+    "arcE" : PI+HALF_PI,
+    // ARC TWO
+    "arcX2" :795,
+    "arcY2" : 265,
+    "arcS2" : HALF_PI,
+    "arcE2" : PI+HALF_PI,
+    // TRIANGLE
+    "triX1" : 0,
+    "triY1" : 0,
+    "triX2" : 0,
+    "triY2" : 0,
+    "triX3" : 0,
+    "triY3" : 0
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
-const colorStroke  = "#233f11";
+
+const colorBack    = "#EBF0F5"; // BLUEY WHITE
+const colourArc1   = "#5488A3"; // DARK BLUE
+const colourArc2   = "#98CCD3"; // LIGHT BLUE
+const colourTri    = "#3a3a3a"; // DARK GREY
+const colourRect   = "#364E68"; // NAVY BLUE
 
 function setup () {
-  // create the drawing canvas, save the canvas element
+  // CREATE THE DRAWING CANVAS, SAVE THE CANVAS ELEMENT
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
-  // color/stroke setup
-  stroke(colorStroke);
-  strokeWeight(4);
 
-  // with no animation, redrawing the screen is not necessary
+  // WITH NO ANIMATION, REDRAWING THE SCREEN IS NOT NECESSARY
   noLoop();
 }
 
 function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  // PARAMETERS FOR THE LETTERS
+  // ARC ONE
+  let arcX = letterData["arcX"];
+  let arcY = letterData["arcY"];
+  let arcStart = letterData["arcS"];
+  let arcEnd = letterData["arcE"];
+  // ARC TWO
+  let arcX2 = letterData["arcX2"];
+  let arcY2 = letterData["arcY2"];
+  let arcStart2 = letterData["arcS2"];
+  let arcEnd2 = letterData["arcE2"];
+  // ARC SHARED PARAMETERS
+  let arcHeight = letterData["arcH"];
+  let arcWidth = letterData["arcW"];
+  //RECTANGLE 
+  let rectangleX = letterData["rectX"];
+  let rectangleY = letterData["rectY"];
+  let rectangleWidth = letterData["rectW"];
+  let rectangleHeight = letterData["rectH"];
+  // TRIANGLE
+  let triangleX1 = letterData["triX1"];
+  let triangleY1 = letterData["triY1"];
+  let triangleX2 = letterData["triX2"];
+  let triangleY2 = letterData["triY2"];
+  let triangleX3 = letterData["triX3"];
+  let triangleY3 = letterData["triY3"];
+  
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  // DARWING THE SHAPES
+  fill(colourTri);
+  triangle(triangleX1,triangleY1,triangleX2,triangleY2,triangleX3,triangleY3);
+  fill(colourArc1);
+  noStroke();
+  arc(arcX,arcY,arcWidth,arcHeight,arcStart,arcEnd);
+  fill(colourArc2);
+  noStroke();
+  arc(arcX2,arcY2,arcWidth,arcHeight,arcStart2,arcEnd2);
+  fill(colourRect);
+  rect(rectangleX,rectangleY,rectangleWidth,rectangleHeight);
 }
 
 function draw () {
-  // clear screen
+  // CLEAR SCREEN  
   background(colorBack);
 
-  // compute the center of the canvas
+  // COMPUTE THE CENTER OF THE CANVAS
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
 
-  // draw the letters A, B, C from saved data
+  // DRAW THE LETTERA A, B, C FROM SAVED DATA
   drawLetter(center_x - 250, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
