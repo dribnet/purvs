075e5206d1227c39dc4747d64bd87e77fa4db8c1
doroxv <61666968+doroxv@users.noreply.github.com>
2020-05-04 21:03:14 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,27 +13,45 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "x": -40,
+  "y": 0,   // line
+  "x2": 60,
+  "y2": 0,
+  "circX":  15,
+  "circY": 15,
+  
+  
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "x": -60,
+  "y": 100,   // line
+  "x2": -60,
+  "y2": -105,
+  "circX": 15,
+  "circY": 15 ,
+  "circX1": 15 ,
+  "circY1": 15,
+  "circX2": 15,
+  "circY2":  15,
+  
+ 
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "circX3":15 ,
+  "circY3": 15 ,
+  "circX4":15,
+  "circY4":15 ,
+  "start":100 ,
+  "stop": 170,
+  "start1":210 ,
+  "stop1": 315,
+  
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
-const colorStroke  = "#233f11";
+const colorBgd    = "#e5d3b3"; // tan background
+const colorStroke  = "#d2b48c"; // darker tan stroke
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -42,28 +60,62 @@ function setup () {
 
   // color/stroke setup
   stroke(colorStroke);
-  strokeWeight(4);
+  strokeWeight(30);
 
+  // angle mode from radians to degrees for arc
+  angleMode(DEGREES)
   // with no animation, redrawing the screen is not necessary
   noLoop();
 }
 
 function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  // determine parameters for lines
+  let lineX1 = posx + letterData["x"];
+  let lineY1 = posy + letterData["y"];
+  let lineX2 = posx + letterData["x2"];
+  let lineY2 = posy + letterData["y2"];
+
+  // determine start and stop parameters for the arc
+  let arcStart = letterData["start"];
+  let arcStop = letterData["stop"];
+  let arcStart1 = letterData["start1"];
+  let arcStop1 = letterData["stop1"];
+
+  //determine circle parameters
+  let circPosX = letterData["circX"];
+  let circPosY = letterData["circY"];
+  let circPosX1 = letterData["circX1"];
+  let circPosY1 = letterData["circY1"];
+  let circPosX2 = letterData["circX2"];
+  let circPosY2 = letterData["circY2"];
+  let circPosX3 = letterData["circX3"];
+  let circPosY3 = letterData["circY3"];
+  let circPosX4 = letterData["circX4"];
+  let circPosY4 = letterData["circY4"];
+  
+  
+  // draw arc and line stroke
+  noFill();
+  arc(posx-33, posy+30, 145, 130, arcStart, arcStop);
+  arc(posx-30, posy-40, 145, 130, arcStart1, arcStop1);
+  line(lineX1, lineY1, lineX2, lineY2);
+ 
+
+  // draw circles
+  fill(colorStroke);
+  strokeWeight(30);
+  ellipse(posx+10, posy-95, circPosX, circPosY);
+  ellipse(posx+10, posy, circPosX1, circPosY1);
+  ellipse(posx+10, posy+95, circPosX2, circPosY2);
+  ellipse(posx-114, posy-17, circPosX3, circPosY3);
+  ellipse(posx+20, posy+95, circPosX4, circPosY4);
+ 
 }
 
 function draw () {
   // clear screen
-  background(colorBack);
+  background(colorBgd);
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;  
