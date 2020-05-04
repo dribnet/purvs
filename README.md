27270812b372aa1d569bb16932dfa74faa72be3c
wireprach <wireprach@users.noreply.github.com>
2020-05-04 20:17:29 +1200

    Update original sketch and README
    The README, sketch file, preview and thumbnail images have all been updated to show my current design for ABC.

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 ## MDDN 242 2020 Assignment 2
+4/05/2020
+From the beginning, I was interested in using arcs and rectangles to explore possible letter forms. To ensure I was keeping under the maximum number of parameters allowed, my goal was to achieve a letter design that was as minimal as possible while still being readable. At this point, I wanted to focus on literal forms rather than abstract, so that I could begin finding multiple ways to design my letters.
+For this design, each letter consists of fourteen parameters. Each letter consists of two arcs, and one rectangle. While the positions of the shapes vary according to the letter, the width, height and point differences between start and end of the arcs remain the same. The width and height of the rectangle can also be adjusted. For this design, I wanted to create a geometric alphabet that was fun and sweet to look at. I almost considered having the parameters of the rectangle set to 0 for the c, but I ended up deciding against it, as I wanted every shape to be visible. I am very interested to experiment with shape opacity, as this could add an interesting texture to my forms.   
 
-(Replace this README with information about your alphabet. This is an example.)
-
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
-
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+The fourteen parameters per letter:
+- "rectX" : x position of the rectangle
+- "rectY" : y location of the rectangle
+- "rectW" : width of the rectangle
+- "rectH" : height of the rectangle
+- "arcW" : width of both arcs
+- "arcH" : height of both arcs
+- "arcX" : x position of arc 1
+- "arcY" : y position of arc 1
+- "arcS" : starting point of arc 1
+- "arcE" : ending point of arc 1
+- "arcX2" : x position of arc 2
+- "arcY2" : y position of arc 2
+- "arcS2" : starting point of arc 2
+- "arcE2" : ending point of arc 2
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
 
-/* 
+/*
  * my three variable per letter are:
  *
    size: radius of the second circle (in pixels)
@@ -13,52 +13,130 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  //rectangle
+  "rectX" : 0,
+  "rectY" : 0,
+  "rectW" : 33,
+  "rectH" : 82,
+  //arc
+  "arcW" : 80,
+  "arcH" : 80,
+
+  //arc 1
+  "arcX" : -20,
+  "arcY" : 40,
+  "arcS" : 45,
+  "arcE" : 225,
+
+  //arc 2
+  "arcX2" : -20,
+  "arcY2" : 40,
+  "arcS2" : 135,
+  "arcE2" : 315,
+
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  //rectangle
+  "rectX" : -30,
+  "rectY" : -30,
+  "rectW" : 35,
+  "rectH" : 112,
+  //arc
+  "arcW" : 80,
+  "arcH" : 80,
+
+  //arc 1
+  "arcX" : 25,
+  "arcY" : 40,
+  "arcS" : 225,
+  "arcE" : 45,
+
+  //arc 2
+  "arcX2" : 25,
+  "arcY2" : 40,
+  "arcS2" : 315,
+  "arcE2" : 135,
+
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  //rectangle
+  "rectX" : 10,
+  "rectY" : 12,
+  "rectW" : 28,
+  "rectH" : 55,
+  //arc
+  "arcW" : 80,
+  "arcH" : 80,
+
+  //arc 1
+  "arcX" : 10,
+  "arcY" : 40,
+  "arcS" : 45,
+  "arcE" : 225,
+
+  //arc 2
+  "arcX2" : 10,
+  "arcY2" : 40,
+  "arcS2" : 135,
+  "arcE2" : 315,
+
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
-const colorStroke  = "#233f11";
+const colorFront1  = "#6dc9c9"; //tiffany blue
+const colorFront2  = "#ffb2b2"; //coral pink
+const colorFront3  = "#ffd0b5"; //salmon pink
+const colorBack    = "#fffacc"; //pastel yellow
 
 function setup () {
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
-  // color/stroke setup
-  stroke(colorStroke);
-  strokeWeight(4);
+  angleMode(DEGREES);
+  noStroke();
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
 }
 
 function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  //letter parameters
+  //rectangle
+  let rectangleX = posx + letterData ["rectX"];
+  let rectangleY = posy + letterData ["rectY"];
+  let rectangleWid = letterData ["rectW"];
+  let rectangleHei = letterData ["rectH"];
 
-  // draw two circles
+  //arc 1
+  let arcX = posx + letterData ["arcX"];
+  let arcY = posy + letterData ["arcY"];
+  let arcS = letterData ["arcS"];
+  let arcE = letterData ["arcE"];
+
+  //arc 2
+  let arcX2 = posx + letterData ["arcX2"];
+  let arcY2 = posy + letterData ["arcY2"];
+  let arcS2 = letterData ["arcS2"];
+  let arcE2 = letterData ["arcE2"];
+
+  //Shared arc parameters
+  let arcWid = letterData ["arcW"];
+  let arcHei = letterData ["arcH"];
+
+  //rectangle
   fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
+  rect(rectangleX, rectangleY, rectangleWid, rectangleHei)
+
+  //arc 1
   fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  arc(arcX, arcY, arcWid, arcHei, arcS, arcE);
+
+  //arc 2
+  fill(colorFront3);
+  arc(arcX2, arcY2, arcWid, arcHei, arcS2, arcE2);
+
 }
 
 function draw () {
@@ -66,7 +144,7 @@ function draw () {
   background(colorBack);
 
   // compute the center of the canvas
-  let center_x = canvasWidth / 2;  
+  let center_x = canvasWidth / 2;
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
