bfda8697477cb66c5464529878bc83aff8a84141
WishCotton <52726327+WishCotton@users.noreply.github.com>
2020-05-06 00:02:27 +1200

    A,B and C

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 ## MDDN 242 2020 Assignment 2
+5/4/20
+A fun preview of my alphabet! the code is super messy and definetly not the most clean and reasonable way of doing it because I got really confused at how everything was working at first and then by the time I figured it out everything was already all messy but I get it now so I should be all good for the real deal. The idea is tht I'm drawing my letters using the base lines with a square as a base. Lines can connect between the four corners and the centers of each of the squares lines to create the letters. Everytime there is a closed off shape in a letter there is a blue square in the center. I'm not sure on the color theam yet, I usually just use really basic colors at first and then refine a theam later. Also I know the A looks like a tecno elder wand. That was unintentional.
 
-(Replace this README with information about your alphabet. This is an example.)
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
-
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
 
--- sketch.js
+++ sketch.js
@@ -13,27 +13,33 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  //"size": 80,
+  "offsetx": -30,
+  "offsety": -50,
+  "W":0
+  //"H":0
+
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  //"size": 150,
+  "offsetx": -30,
+  "offsety": -50,
+  "A":0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+	"C": 0
+ // "size": 100,
+ // "offsetx": 30,
+  //"offsety": 0
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
+const colorFront1  = "#FFFFFF";
+const colorFront2  = "#0000ff";
+const colorBack    = "#000000";
 const colorStroke  = "#233f11";
+const lineColor    = "#ff0000";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -42,7 +48,8 @@ function setup () {
 
   // color/stroke setup
   stroke(colorStroke);
-  strokeWeight(4);
+  strokeWeight(3);
+
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
@@ -53,12 +60,38 @@ function drawLetter(posx, posy, letterData) {
   let size2 = letterData["size"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
-
+  let across = posx+letterData["W"];
+  let acrossB = posx+ letterData["A"];
+  let acrossC = posx+ letterData["C"];
   // draw two circles
   fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
+  stroke(colorBack);
+  //rect(posx-80, posy-100, 150, 150);
   fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  
+  stroke(lineColor);
+  line(across-5,posy-100,across+70,posy+50);
+  line(across-5,posy-100,across-80,posy+50);
+
+  line(across-5,posy-100,across-5,posy+50);
+   line(across+70,posy+50,across-80,posy+50);
+   //B
+   line(acrossB+70,posy-25,acrossB-80,posy-25);
+   line(acrossB+70,posy-100,acrossB+70,posy+50);
+   line(acrossB-80,posy-100,acrossB-80,posy+50);
+   line(acrossB-80,posy-100,acrossB+70,posy-100);
+   line(acrossB-80,posy+50,acrossB+70,posy+50);
+   //C
+   line(acrossC-80,posy+50,acrossC+70,posy+50);
+   line(acrossC-80,posy-100,acrossC+70,posy-100)
+   line(acrossC-80,posy-100,acrossC-80,posy+50);
+
+   rect(pos2x, pos2y,50,50);
+
+  //
+  //line(across,across-75,across-75,across+75);
+
+  //ellipse(pos2x, pos2y, size2, size2);
 }
 
 function draw () {
@@ -73,6 +106,7 @@ function draw () {
   drawLetter(center_x - 250, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
+  
 }
 
 function keyTyped() {
