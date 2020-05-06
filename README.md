8fc2911d729703f3afca7e08660553d214f365d8
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-05-06 15:28:34 +1200

    Created ABC
    Made my ABC letter forms and decided on parameters for the alphabet

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 ## MDDN 242 2020 Assignment 2
 
-(Replace this README with information about your alphabet. This is an example.)
+6/5/2020
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
+Each of my letters is made up of a combination of 3 shapes, each can be a triangle, rectangle or circle. Each shape is represented in a different colour
+(first is green, second is yellow, third is orange)
 
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+There are 12 parameters for the letters, which basically summarize to:
+For each shape (so * 3)
+	Shape type (eg tri, circ or rect)
+	Shape pos1X
+	Shape pos1Y
+	Shape pos2X
+	Shape pos2Y
+
+The circles and rectangles are drawn using CORNERS mode, so pos1 and pos2 define the left top and bottom right corners of the shape.
+By using CORNERS, it removes the need for me to define the size of the shape explicitly
+The triangles are drawn with a tiny bit of math. Pos1 is always the point when it's the only point on that particular y coordinate,
+and pos2 is always the point with another point on the same y coordinate. Point 3 on the triangle is found by find the difference between
+pos1x and pos2x, * by 2 to get width, and the y is the as pos2. My triangles are always isoceles triangles, with symmetry across the 
+y axis.
 
--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 const canvasWidth = 960;
 const canvasHeight = 500;
-
 /* 
  * my three variable per letter are:
  *
@@ -13,21 +12,56 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "shape1type" : "tri",
+  "shape1pos1x" : 40,
+  "shape1pos1y" : 10,
+  "shape1pos2x" : 10,
+  "shape1pos2y" : 60,
+  "shape2type" : "null",
+  "shape2pos1x" : 0,
+  "shape2pos1y" : 0,
+  "shape2pos2x" : 0,
+  "shape2pos2y" : 0,
+  "shape3type" : "null",
+  "shape3pos1x" : 0,
+  "shape3pos1y" : 0,
+  "shape3pos2x" : 0,
+  "shape3pos2y" : 0
 }
-
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "shape1type" : "circ",
+  "shape1pos1x" : 10,
+  "shape1pos1y" : 10,
+  "shape1pos2x" : 35,
+  "shape1pos2y" : 35,
+  "shape2type" : "circ",
+  "shape2pos1x" : 10,
+  "shape2pos1y" : 35,
+  "shape2pos2x" : 35,
+  "shape2pos2y" : 60,
+  "shape3type" : "null",
+  "shape3pos1x" : 0,
+  "shape3pos1y" : 0,
+  "shape3pos2x" : 0,
+  "shape3pos2y" : 0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "shape1type" : "circ",
+  "shape1pos1x" : 10,
+  "shape1pos1y" : 10,
+  "shape1pos2x" : 60,
+  "shape1pos2y" : 60,
+  "shape2type" : "circ",
+  "shape2pos1x" : 35,
+  "shape2pos1y" : 15,
+  "shape2pos2x" : 75,
+  "shape2pos2y" : 55,
+  "shape3type" : "null",
+  "shape3pos1x" : 0,
+  "shape3pos1y" : 0,
+  "shape3pos2x" : 0,
+  "shape3pos2y" : 0
 }
 
 const colorFront1  = "#199cff";
@@ -35,13 +69,14 @@ const colorFront2  = "#59ccff";
 const colorBack    = "#e3eded";
 const colorStroke  = "#233f11";
 
+
 function setup () {
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
   // color/stroke setup
-  stroke(colorStroke);
+  //stroke(colorStroke);
   strokeWeight(4);
 
   // with no animation, redrawing the screen is not necessary
@@ -49,16 +84,43 @@ function setup () {
 }
 
 function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+ let i = letterData["shape1type"];
+ let c = 0;
+ let col1 = color(79, 214, 93) ;
+ let col2 = color(240, 242, 92);
+ let col3 = color(250, 113, 67);
+ let values = [letterData["shape1type"], letterData["shape1pos1x"], letterData["shape1pos1y"], letterData["shape1pos2x"],letterData["shape1pos2y"],letterData["shape2type"], letterData["shape2pos1x"], letterData["shape2pos1y"],letterData["shape2pos2x"], letterData["shape2pos2y"],letterData["shape3type"], letterData["shape3pos1x"],letterData["shape3pos1y"],letterData["shape3pos2x"], letterData["shape3pos2y"]]
+ while (i.valueOf() !== "null") {
+    let type = values[c];
+    let pos1 = createVector(values[c+1], values[c+2]);
+    let pos2 = createVector(values[c+3], values[c+4]);
+    let curCol;
+    if (c === 0) { curCol = col1; }
+    else if (c === 5) { curCol = col2; }
+    else { curCol = col3; }
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+    if (type.valueOf() === "tri") {
+      let width = (pos1.x - pos2.x) * 2;
+      fill(curCol);
+      triangle(posx + pos1.x, posy + pos1.y, posx + pos2.x, posy + pos2.y, posx + width + pos2.x, posy + pos2.y);
+
+    }
+    else if (type.valueOf() === "circ") {
+      ellipseMode(CORNERS);
+      fill(curCol);
+      ellipse(posx + pos1.x, posy + pos1.y, posx + pos2.x, posy + pos2.y);
+    }
+    else if (type.valueOf() == "rect") {
+      rectMode(CORNERS);
+      fill(curCol);
+      rect(posx + pos1.x, posy + pos1.y, posx + pos2.x, posy + pos2.y);
+    }
+
+    if (c === 10) { break;}
+    c += 5;
+    i = values[c];
+ }
+
 }
 
 function draw () {
