4bc8c16b5dc804da4702613435166951523d701a
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-05-06 16:28:25 +1200

    Started Alphabet
    Implemented my abc code into the alphabet file, and created forms for D, E, and F. Also updated preview and thumbnail files.

--- draw_letters.js
+++ draw_letters.js
@@ -14,16 +14,42 @@ function drawLetter(letterData) {
   stroke(colorStroke);
   strokeWeight(4);
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+   let i = letterData["shape1type"];
+   let c = 0;
+   let col1 = color(79, 214, 93,150) ;
+   let col2 = color(240, 242, 92,150);
+   let col3 = color(250, 113, 67,150);
+   let values = [letterData["shape1type"], letterData["shape1pos1x"], letterData["shape1pos1y"], letterData["shape1pos2x"],letterData["shape1pos2y"],letterData["shape2type"], letterData["shape2pos1x"], letterData["shape2pos1y"],letterData["shape2pos2x"], letterData["shape2pos2y"],letterData["shape3type"], letterData["shape3pos1x"],letterData["shape3pos1y"],letterData["shape3pos2x"], letterData["shape3pos2y"]]
+   while (i.valueOf() !== "null") {
+      let type = values[c];
+      let pos1 = createVector(values[c+1], values[c+2]);
+      let pos2 = createVector(values[c+3], values[c+4]);
+      let curCol;
+      if (c === 0) { curCol = col1; }
+      else if (c === 5) { curCol = col2; }
+      else { curCol = col3; }
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(50, 150, 75, 75);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+      if (type.valueOf() === "tri") {
+        let width = (pos1.x - pos2.x) * 2;
+        fill(curCol);
+        triangle(pos1.x, 50 + pos1.y, pos2.x, 50 + pos2.y, width + pos2.x, 50 + pos2.y);
+
+      }
+      else if (type.valueOf() === "circ") {
+        ellipseMode(CORNERS);
+        fill(curCol);
+        ellipse(pos1.x, 50 + pos1.y, pos2.x, 50 + pos2.y);
+      }
+      else if (type.valueOf() == "rect") {
+        rectMode(CORNERS);
+        fill(curCol);
+        rect(pos1.x, 50 + pos1.y, pos2.x, 50 + pos2.y);
+      }
+
+      if (c === 10) { break;}
+      c += 5;
+      i = values[c];
+   }
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
