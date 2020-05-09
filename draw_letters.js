13ffc58db18e264c75364c5b565b33dbfeae6728
PatelShiv7 <61666967+PatelShiv7@users.noreply.github.com>
2020-05-09 17:06:55 +1200

    Adding a few more letters
    Added D,E and F

--- draw_letters.js
+++ draw_letters.js
@@ -11,19 +11,33 @@ const colorStroke  = "#233f11";
  */
 function drawLetter(letterData) {
   // color/stroke setup
-  stroke(colorStroke);
-  strokeWeight(4);
+  let pos2x = 5 + letterData["offsetx"];
+  let pos2y = 100 + letterData["offsety"];
+  let topRectW = letterData["topRectWidth"];
+  let middleRectW = letterData["middleRectWidth"];
+  let botRectW = letterData["bottomRectWidth"];
+  let topRectH = letterData["topRectHeight"];
+  let middleRectH = letterData["middleRectHeight"];
+  let botRectH = letterData["bottomRectHeight"];
+  let leftRectW = letterData["leftRectWidth"];
+  let leftRectH = letterData["leftRectHeight"];
+  let rightRectW = letterData["rightRectWidth"];
+  let rightRectH = letterData["rightRectHeight"];
+  let leftHalfRectH = letterData["leftRectHalfHeight"];
 
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
-
-  // draw two circles
   fill(colorFront1);
-  ellipse(50, 150, 75, 75);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+    fill(colorFront1)
+    rect(pos2x,0,leftRectW,leftRectH); //left vertical strip (A,B,C)
+    fill(colorFront2)
+    rect(pos2x + 75, 0, rightRectW, rightRectH); //right vertical strip (A,B)
+    fill(colorStroke)
+    rect(pos2x,pos2y - 45,middleRectW, middleRectH); //middle horizontal strip (A,B)
+    rect(pos2x,pos2y - 135,topRectW,topRectH); //top horizontal strip (A,B,C)
+    rect(pos2x,pos2y + 45, botRectW, botRectH); //bottom horizontal strip (B,C)
+
+    rect(pos2x,pos2y - 40,leftRectW,leftHalfRectH); //left vertical half strip (D)
+    
+
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
