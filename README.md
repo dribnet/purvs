6e5db7e3aa28fcaf567a173fdaad261efd7f0300
JamieLum <61666463+JamieLum@users.noreply.github.com>
2020-05-11 11:57:47 +1200

    more letters, animation

--- README.md
+++ README.md
@@ -1,21 +1,12 @@
 ## MDDN 242 2020 Assignment 2
 
-Concept Work:
-This alphabet is made out of twotransparent arcs, which create the approximate form of english letters where they overlap. The bluish arc sits to the left and top, and the red to the right and bottom, in that order of priority. As it stands, this totals 10 parameters per letter.
+Concept:
+This alphabet is made out of two transparent arcs, which create the approximate form of english letters where they overlap. The goal is to make a full, somewhat readable alphabet from only two arcs. The bluish arc sits to the left and top, and the red to the right and bottom, in that order of priority. As it stands, this totals 10 parameters per letter.
 
-6/05/20a
-Move out of sketch.js.
+====================================================================================
 
-Transferred sketch data into draw_letters.js and letters.js. Rescaled and positioned existing letters appropriately.
+More new letters, animation added.
 
+L, M, N, O, P, S, U, V, and W now all exist. B and J, and I have ben modified. Interpolation between letters have been added. Unfortunately, I've made my arcs swap sides a lot, making for some odd movements. I'll have to see if I can fix that at some point.
 
-6/5/20b
-More letters.
-
-D, E, F, G, H, I, and J have been made. I'm not sure how far I'll be able to take just two arcs, but I'm gonna run as far as it'll go. Not so sure about G or F, but they exist.
-
-
-1/05/20
-More letters, default changed. README fixed.
-
-K, X, and Y have been added. Not super sure about K or Y, but at least I have them. Default is now two semicircular arcs which do not overlap. Tweaked F to fit newly added rule that red should have more weight to the right and bottom. Cut out and replaced default readme.
+Exhibition now also says my name. Yay.
--- draw_letters.js
+++ draw_letters.js
@@ -1,9 +1,9 @@
 const red1 = 230;
-const green1 = 0;
-const blue1 = 0;
-const red2 = 0;
-const green2 = 100;
-const blue2 = 100;
+const green1 = 30;
+const blue1 = 20;
+const red2 = 20;
+const green2 = 90;
+const blue2 = 110;
 
 /*
  * Draw the letter given the letterData
@@ -29,23 +29,30 @@ function drawLetter(letterData) {
   // draw arcs
   angleMode(DEGREES);
   noStroke();
-  fill(red1, green1, blue1, 130);
+  fill(red1, green1, blue1, 150);
   arc(arcX1, arcY1, arcSize1, arcSize1, arcStart1, arcStop1);
-  fill(red2, green2, blue2, 130);
+  fill(red2, green2, blue2, 150);
   arc(arcX2, arcY2, arcSize2, arcSize2, arcStart2, arcStop2);
 
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
-  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
-  new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
+  new_letter["x1"]    = map(percent, 0, 100, oldObj["x1"], newObj["x1"]);
+  new_letter["y1"] = map(percent, 0, 100, oldObj["y1"], newObj["y1"]);
+  new_letter["s1"] = map(percent, 0, 100, oldObj["s1"], newObj["s1"]);
+  new_letter["start1"]    = map(percent, 0, 100, oldObj["start1"], newObj["start1"]);
+  new_letter["stop1"]    = map(percent, 0, 100, oldObj["stop1"], newObj["stop1"]);
+  new_letter["x2"]    = map(percent, 0, 100, oldObj["x2"], newObj["x2"]);
+  new_letter["y2"]    = map(percent, 0, 100, oldObj["y2"], newObj["y2"]);
+  new_letter["s2"]    = map(percent, 0, 100, oldObj["s2"], newObj["s2"]);
+  new_letter["start2"]    = map(percent, 0, 100, oldObj["start2"], newObj["start2"]);
+  new_letter["stop2"]    = map(percent, 0, 100, oldObj["stop2"], newObj["stop2"]);
   return new_letter;
 }
 
 var swapWords = [
-  "ABBAABBA",
-  "CAB?CAB?",
+  "JAMIELUM",
+  "?WEMMEW?",
   "BAAAAAAA"
 ]
