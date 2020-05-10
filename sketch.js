aa19d9574fea3214d0ee3382b88e031a327f23ad
GussyBoy5432 <61666925+GussyBoy5432@users.noreply.github.com>
2020-05-10 20:16:34 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,27 +13,73 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "sectionx": -70,
+  "sectiony": 40,
+  "section2x": 0, 
+  "section2y": -100, 
+  "section3x": 0, 
+  "section3y": 40, 
+  "strokeW": 0,
+  "sectionx2": -45,
+  "sectiony2": 40,
+  "section2x2": 0, 
+  "section2y2": -50, 
+  "section3x2": 50, 
+  "section3y2": 40,
+
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "sectionx2": -70,
+  "sectiony2": -100,
+  "section2x2": -70, 
+  "section2y2": 40, 
+  "section3x2": 20, 
+  "section3y2": 40,
+  "sectionx": -40,
+  "sectiony": 40,
+  "section2x": 60, 
+  "section2y": 40, 
+  "section3x": 60, 
+  "section3y": -30, 
+  "strokeW": 0,
+
+
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "sectionx": -70,
+  "sectiony": 40,
+  "section2x": -70, 
+  "section2y": -100, 
+  "section3x": 50, 
+  "section3y": 40, 
+  "strokeW": 0,
+  "sectionx2": -70,
+  "sectiony2": 40,
+  "section2x2": -70, 
+  "section2y2": -100, 
+  "section3x2": 50, 
+  "section3y2": -100,
+
 }
 
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
-const colorBack    = "#e3eded";
-const colorStroke  = "#233f11";
+const colorFront1  = "#EFBDEB";
+const colorFront2  = "#B68CB8";
+const colorFront3  = "#6461A0";
+const colorBackground    = "#000000";
+const colorStroke  = "#114B5F";
+
+//Blue
+const red1 = 0
+const green1 = 234
+const blue1 = 255
+
+//Green
+const red2 = 0
+const green2 = 255
+const blue2 = 140
+
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -42,7 +88,6 @@ function setup () {
 
   // color/stroke setup
   stroke(colorStroke);
-  strokeWeight(4);
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
@@ -50,20 +95,34 @@ function setup () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  let pos2x = posx + letterData["sectionx"];
+  let pos2y = posy + letterData["sectiony"];
+  let pos3x = posx + letterData["section2x"];
+  let pos3y = posy + letterData["section2y"];
+  let pos4x = posx + letterData["section3x"];
+  let pos4y = posy + letterData["section3y"];
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  let pos2x2 = posx + letterData["sectionx2"];
+  let pos2y2 = posy + letterData["sectiony2"];
+  let pos3x2 = posx + letterData["section2x2"];
+  let pos3y2 = posy + letterData["section2y2"];
+  let pos4x2 = posx + letterData["section3x2"];
+  let pos4y2 = posy + letterData["section3y2"];
+
+
+  fill(red1,green1,blue1);
+  triangle(pos2x, pos2y,pos3x, pos3y, pos4x, pos4y);
+
+  fill(red2,green2,blue2);
+  triangle(pos2x2,pos2y2,pos3x2, pos3y2, pos4x2, pos4y2);
+
+ 
+
 }
 
 function draw () {
   // clear screen
-  background(colorBack);
+  background(colorBackground);
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;  
@@ -71,8 +130,11 @@ function draw () {
 
   // draw the letters A, B, C from saved data
   drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
+  drawLetter(center_x, center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
+
+
+
 }
 
 function keyTyped() {
