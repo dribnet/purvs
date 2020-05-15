db9f08bfd8a555f1868a179215cfa8ac3fbe90e2
whitefalconnz <Whitefalconnz@gmail.com>
2020-05-16 11:36:08 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,24 +17,28 @@ const letterA = {
   "sizey": 90,
   "offsetrectx": 37,
   "offsetrecty": 10,
-  "trianglePoint1ax": 68, //small
-  "trianglePoint1ay": 80,
-    "trianglePoint2ax": 50,
-  "trianglePoint2ay": 80,
-    "trianglePoint3ax": 20,
-  "trianglePoint3ay": 100,
-      "trianglePoint1bx": 0, //big
-  "trianglePoint1by": 50,
-    "trianglePoint2bx": -10,
-  "trianglePoint2by": 0,
-    "trianglePoint3bx": 0,
-  "trianglePoint3by": -20,
-     "trianglePoint1cx": 0,
-  "trianglePoint1cy": 0,
-    "trianglePoint2cx": 0,
-  "trianglePoint2cy": 0,
-    "trianglePoint3cx": -20,
-  "trianglePoint3cy": 0,
+  // "trianglePoint1ax": 68, //small
+  // "trianglePoint1ay": 80,
+  //   "trianglePoint2ax": 50,
+  // "trianglePoint2ay": 80,
+  //   "trianglePoint3ax": 20,
+  // "trianglePoint3ay": 100,
+  //     "trianglePoint1bx": 0, //big
+  // "trianglePoint1by": 50,
+  //   "trianglePoint2bx": -10,
+  // "trianglePoint2by": 0,
+  //   "trianglePoint3bx": 0,
+  // "trianglePoint3by": -20,
+  //    "trianglePoint1cx": 0,
+  // "trianglePoint1cy": 0,
+  //   "trianglePoint2cx": 0,
+  // "trianglePoint2cy": 0,
+  //   "trianglePoint3cx": -20,
+  // "trianglePoint3cy": 0,
+  "triangleTranslatex":0,
+  "triangleTranslatey":-100,
+  "rotateTriangle":0,
+  "scaleTriangle":1
 }
 
 const letterB = {
@@ -42,24 +46,28 @@ const letterB = {
    "sizey": 190,
   "offsetrectx": 0,
   "offsetrecty": -90,
-    "trianglePoint1ax": 0,
-  "trianglePoint1ay": 0,
-    "trianglePoint2ax": 0,
-  "trianglePoint2ay": 0,
-    "trianglePoint3ax": 0,
-  "trianglePoint3ay": 0,
-      "trianglePoint1bx": 0,
-  "trianglePoint1by": 0,
-    "trianglePoint2bx": 0,
-  "trianglePoint2by": 0,
-    "trianglePoint3bx": 0,
-  "trianglePoint3by": -20,
-     "trianglePoint1cx": 0,
-  "trianglePoint1cy": 0,
-    "trianglePoint2cx": 0,
-  "trianglePoint2cy": -10,
-    "trianglePoint3cx": 0,
-  "trianglePoint3cy": 0,
+  //   "trianglePoint1ax": 0,
+  // "trianglePoint1ay": 0,
+  //   "trianglePoint2ax": 0,
+  // "trianglePoint2ay": 0,
+  //   "trianglePoint3ax": 0,
+  // "trianglePoint3ay": 0,
+  //     "trianglePoint1bx": 0,
+  // "trianglePoint1by": 0,
+  //   "trianglePoint2bx": 0,
+  // "trianglePoint2by": 0,
+  //   "trianglePoint3bx": 0,
+  // "trianglePoint3by": -20,
+  //    "trianglePoint1cx": 0,
+  // "trianglePoint1cy": 0,
+  //   "trianglePoint2cx": 0,
+  // "trianglePoint2cy": -10,
+  //   "trianglePoint3cx": 0,
+  // "trianglePoint3cy": 0,
+  "triangleTranslatex":-300,
+  "triangleTranslatey":-500,
+  "rotateTriangle":0,
+  "scaleTriangle":1
 }
 
 const letterC = {
@@ -67,24 +75,28 @@ const letterC = {
    "sizey": 100,
   "offsetrectx":0,
   "offsetrecty": 0,
-    "trianglePoint1ax": 80,
-  "trianglePoint1ay": 82,
-    "trianglePoint2ax": 50,
-  "trianglePoint2ay": 120,
-    "trianglePoint3ax": 0,
-  "trianglePoint3ay": 100,
-     "trianglePoint1bx": 0,
-  "trianglePoint1by": -50,
-    "trianglePoint2bx": 0,
-  "trianglePoint2by": 50,
-    "trianglePoint3bx": 0,
-  "trianglePoint3by": -50,
-     "trianglePoint1cx": 0,
-  "trianglePoint1cy": 0,
-    "trianglePoint2cx": 0,
-  "trianglePoint2cy": 0,
-    "trianglePoint3cx": 0,
-  "trianglePoint3cy": 0,
+  //   "trianglePoint1ax": 80,
+  // "trianglePoint1ay": 82,
+  //   "trianglePoint2ax": 50,
+  // "trianglePoint2ay": 120,
+  //   "trianglePoint3ax": 0,
+  // "trianglePoint3ay": 100,
+  //    "trianglePoint1bx": 0,
+  // "trianglePoint1by": -50,
+  //   "trianglePoint2bx": 0,
+  // "trianglePoint2by": 50,
+  //   "trianglePoint3bx": 0,
+  // "trianglePoint3by": -50,
+  //    "trianglePoint1cx": 0,
+  // "trianglePoint1cy": 0,
+  //   "trianglePoint2cx": 0,
+  // "trianglePoint2cy": 0,
+  //   "trianglePoint3cx": 0,
+  // "trianglePoint3cy": 0,
+  "triangleTranslatex":-680,
+  "triangleTranslatey":-212,
+  "rotateTriangle":3,
+  "scaleTriangle":1
 }
 
 // const letterC = {
@@ -124,30 +136,49 @@ function drawLetter(posx, posy, letterData) {
   let size2y = letterData["sizey"];
   let pos2x = posx + letterData["offsetrectx"];
   let pos2y = posy + letterData["offsetrecty"];
-  let postri1ax = posx + letterData["trianglePoint1ax"];
-  let postri1ay = posy + letterData["trianglePoint1ay"];
-  let postri2ax = posx + letterData["trianglePoint2ax"];
-  let postri2ay = posy + letterData["trianglePoint2ay"];                                                            
-  let postri3ax = posx + letterData["trianglePoint3ax"];
-  let postri3ay = posy + letterData["trianglePoint3ay"];
-  let postri1bx = posx + letterData["trianglePoint1bx"];
-  let postri1by = posy + letterData["trianglePoint1by"];
-  let postri2bx = posx + letterData["trianglePoint2bx"];
-  let postri2by = posy + letterData["trianglePoint2by"];
-  let postri3bx = posx + letterData["trianglePoint3bx"];
-  let postri3by = posy + letterData["trianglePoint3by"];
-  let postri1cx = posx + letterData["trianglePoint1cx"];
-  let postri1cy = posy + letterData["trianglePoint1cy"];
-  let postri2cx = posx + letterData["trianglePoint2cx"];
-  let postri2cy = posy + letterData["trianglePoint2cy"];
-  let postri3cx = posx + letterData["trianglePoint3cx"];
-  let postri3cy = posy + letterData["trianglePoint3cy"];
-
+  // let postri1ax = posx + letterData["trianglePoint1ax"];
+  // let postri1ay = posy + letterData["trianglePoint1ay"];
+  // let postri2ax = posx + letterData["trianglePoint2ax"];
+  // let postri2ay = posy + letterData["trianglePoint2ay"];                                                            
+  // let postri3ax = posx + letterData["trianglePoint3ax"];
+  // let postri3ay = posy + letterData["trianglePoint3ay"];
+  // let postri1bx = posx + letterData["trianglePoint1bx"];
+  // let postri1by = posy + letterData["trianglePoint1by"];
+  // let postri2bx = posx + letterData["trianglePoint2bx"];
+  // let postri2by = posy + letterData["trianglePoint2by"];
+  // let postri3bx = posx + letterData["trianglePoint3bx"];
+  // let postri3by = posy + letterData["trianglePoint3by"];
+  // let postri1cx = posx + letterData["trianglePoint1cx"];
+  // let postri1cy = posy + letterData["trianglePoint1cy"];
+  // let postri2cx = posx + letterData["trianglePoint2cx"];
+  // let postri2cy = posy + letterData["trianglePoint2cy"];
+  // let postri3cx = posx + letterData["trianglePoint3cx"];
+  // let postri3cy = posy + letterData["trianglePoint3cy"];
+  let translatex = posx + letterData["triangleTranslatex"]
+  let translatey = posy + letterData["triangleTranslatey"]
+  let rotateTri = letterData["rotateTriangle"]
+  let scaleTri = letterData["scaleTriangle"]
   // draw two circles
+  angleMode(DEGREES)
   fill(colorFront1);
-  triangle(postri1ax, postri1ay, postri2ax+50,postri2ay-50,postri3ax+100,postri3ay);
-  triangle(postri1bx, postri1by+50, postri2bx+50,postri2by,postri3bx+100,postri3by+50);
-  triangle(postri1cx, postri1cy+100, postri2cx+50,postri2cy+50,postri3cx+100,postri3cy+100);
+  push()
+  translate(translatex,translatey)
+  scale(scaleTri)
+  rotate(rotateTri)
+  triangle(posx, posy-50, posx+50,posy-100,posx+100,posy-50);
+  pop()
+  push()
+   translate(translatex,translatey)
+  scale(scaleTri)
+  rotate(rotateTri)
+  triangle(posx, posy, posx+50,posy-50,posx+100,posy);
+  pop()
+  push()
+  translate(translatex,translatey)
+  scale(scaleTri)
+  rotate(rotateTri)
+  triangle(posx, posy+50, posx+50,posy,posx+100,posy+50);
+  pop()
   fill(colorFront2)
   push();
   strokeWeight(0.1)
