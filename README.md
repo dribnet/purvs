44b62f3d61e3832594c6de58230885b3f3c83f3a
cindyrrose <61666889+cindyrrose@users.noreply.github.com>
2020-05-16 16:30:43 +1200

    Sketch Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## MDDN 242 2020 Assignment 2
 
-[13/05] Played around a bit with the sketch.js file and also I got a new laptop so testing the commit to master feature from GitHub
+[16/05] I played around with the sketch to get a gist of how to do things as I was quite unfamiliar with this type of code, as we've never done this sort of thing before.
 
--- sketch.js
+++ sketch.js
@@ -12,23 +12,40 @@ const canvasHeight = 500;
  *
  */
 
-const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
-}
+// const letterA = {
+//   "size": 80,
+//   "offsetx": 0,
+//   "offsety": 35
+// }
 
-const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
-}
+  const letterA = {
+    "offsetx": -25,
+    "offsety": 50,
+    "triangleX1": -40,
+    "triangleY1": 50,
+    "triangleX2": 0,
+    "triangleY2": -80,
+    "triangleX3": 40,
+    "triangleY3": 50,
+  }
 
-const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
-}
+// const letterB = {
+//   "size": 150,
+//   "offsetx": 0,
+//   "offsety": -145
+// // }
+
+// const letterB = {
+//   "size": 150,
+//   "offsetx": 0,
+//   "offsety": -145
+// }
+
+// const letterC = {
+//   "size": 100,
+//   "offsetx": 30,
+//   "offsety": 0
+// }
 
 const colorFront1  = "#DB7F8E";
 const colorFront2  = "#FFDBDA";
@@ -48,30 +65,40 @@ function setup () {
   noLoop();
 }
 
-function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+// function drawLetter(posx, posy, letterData) {
+//   // determine parameters for second circle
+//   let size2 = letterData["size"];
+//   let pos2x = posx + letterData["offsetx"];
+//   let pos2y = posy + letterData["offsety"];
 
-  // draw two circles
-  fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
-}
+//   // draw two circles
+//   fill(colorFront1);
+//   ellipse(posx, posy, 150, 150);
+//   fill(colorFront2);
+//   ellipse(pos2x, pos2y, size2, size2);
+// }
 
 function drawLetter1(posx, posy, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
+  //let size2 = letterData["size"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let trix1 = posx + letterData["triangleX1"];
+  let triy1 = posy + letterData["triangleY1"];
+  let trix2 = posx + letterData["triangleX2"];
+  let triy2 = posy + letterData["triangleY2"];
+  let trix3 = posx + letterData["triangleX3"];
+  let triy3 = posy + letterData["triangleY3"];
+
 
   // draw two circles
   fill(colorFront1);
   ellipse(posx, posy, 150, 150);
+  // fill(colorFront2);
+  // ellipse(pos2x, pos2y, size2, size2);
   fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  //noStroke();
+  triangle(trix1, triy1, trix2, triy2, trix3, triy3);
 
 }
 
