d5adb1411a4ecf3d9a46da695bfa8ea9cde58cb4
cindyrrose <61666889+cindyrrose@users.noreply.github.com>
2020-05-16 19:00:22 +1200

    Worked on Sketch

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## MDDN 242 2020 Assignment 2
 
-[16/05] I played around with the sketch to get a gist of how to do things as I was quite unfamiliar with this type of code, as we've never done this sort of thing before.
+[16/05] I did a sketch with an idea of what I want to go for my letters. I drew my inspiration from mathematic symbols, mainly derived from the division symbol. My basic shapes will be lines and ellipses. I found it a bit difficult to work under 12 variables but that is what I love about this challenge! 
 
+Here is my sketch: https://prnt.sc/shrcc5
+
+However, the letters may change.
--- sketch.js
+++ sketch.js
@@ -12,40 +12,48 @@ const canvasHeight = 500;
  *
  */
 
-// const letterA = {
-//   "size": 80,
-//   "offsetx": 0,
-//   "offsety": 35
-// }
-
-  const letterA = {
-    "offsetx": -25,
-    "offsety": 50,
-    "triangleX1": -40,
-    "triangleY1": 50,
-    "triangleX2": 0,
-    "triangleY2": -80,
-    "triangleX3": 40,
-    "triangleY3": 50,
-  }
-
-// const letterB = {
-//   "size": 150,
-//   "offsetx": 0,
-//   "offsety": -145
-// // }
-
-// const letterB = {
-//   "size": 150,
-//   "offsetx": 0,
-//   "offsety": -145
-// }
-
-// const letterC = {
-//   "size": 100,
-//   "offsetx": 30,
 //   "offsety": 0
-// }
+   const letterA = {
+    "sizeLine": 80,
+    "sizeCirc": 30,
+    "circx1": 35,
+    "circy1": -10,
+    "circx2": -75,
+    "circy2": -10,
+    "linex1": -100, //left
+    "liney1": -100, //top
+    "linex2": -20,
+    "liney2": 100,
+    "liney3": 100,
+   }
+
+const letterB = {
+    "sizeLine": 120,
+    "sizeCirc": 160,
+    "circx1": 0,
+    "circy1": 0,
+    "linex1": -110, //left
+    "liney1": 0, //top
+    "linex2": -10,
+    "liney2": 0,
+    "liney3": 0,
+
+   }
+
+const letterC = {
+    "sizeLine": 100,
+    "sizeCirc": 30,
+    // "circx1": 35,
+    // "circy1": -30,
+    // "circx2": -50,
+    // "circy2": -10,
+    "linex1": -20, // -20
+    "liney1": -150, // 130
+    "linex2": -70, // ! -70 !
+    "liney2": 20, //! 20 !
+    "liney3": -100, //!
+
+   }
 
 const colorFront1  = "#DB7F8E";
 const colorFront2  = "#FFDBDA";
@@ -65,40 +73,30 @@ function setup () {
   noLoop();
 }
 
-// function drawLetter(posx, posy, letterData) {
-//   // determine parameters for second circle
-//   let size2 = letterData["size"];
-//   let pos2x = posx + letterData["offsetx"];
-//   let pos2y = posy + letterData["offsety"];
-
-//   // draw two circles
-//   fill(colorFront1);
-//   ellipse(posx, posy, 150, 150);
-//   fill(colorFront2);
-//   ellipse(pos2x, pos2y, size2, size2);
-// }
-
-function drawLetter1(posx, posy, letterData) {
+function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
-  //let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
-  let trix1 = posx + letterData["triangleX1"];
-  let triy1 = posy + letterData["triangleY1"];
-  let trix2 = posx + letterData["triangleX2"];
-  let triy2 = posy + letterData["triangleY2"];
-  let trix3 = posx + letterData["triangleX3"];
-  let triy3 = posy + letterData["triangleY3"];
-
+  let sizeLine1 = letterData["sizeLine"];
+  let sizeCirc1 = letterData["sizeCirc"];
+  let circ2x = posx + letterData["circx1"];
+  let circ2y = posy + letterData["circy1"];
+  let circ3x = posx + letterData["circx2"];
+  let circ3y = posy + letterData["circy2"];
+  let linePosX1 = posx + letterData["linex1"];
+  let linePosY1 = posy + letterData["liney1"];
+  let linePosX2 = posx + letterData["linex2"];
+  let linePosY2 = posy + letterData["liney2"];
+  let linePosY3 = posy + letterData["liney3"];
 
   // draw two circles
   fill(colorFront1);
-  ellipse(posx, posy, 150, 150);
-  // fill(colorFront2);
-  // ellipse(pos2x, pos2y, size2, size2);
+  ellipse(circ2x, circ2y, sizeCirc1, sizeCirc1);
+
+  fill(colorFront1);
+  ellipse(circ3x, circ3y, sizeCirc1, sizeCirc1);
+
   fill(colorFront2);
-  //noStroke();
-  triangle(trix1, triy1, trix2, triy2, trix3, triy3);
+  line(linePosX1, linePosY3, linePosX1 + sizeLine1, linePosY1);
+  line(linePosX2, linePosY1, linePosX2 + sizeLine1, linePosY2);
 
 }
 
@@ -111,9 +109,9 @@ function draw () {
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter1(center_x - 250, center_y, letterA);
-  drawLetter1(center_x      , center_y, letterB);
-  drawLetter1(center_x + 250, center_y, letterC);
+  drawLetter(center_x - 250, center_y, letterA);
+  drawLetter(center_x      , center_y, letterB);
+  drawLetter(center_x + 250, center_y, letterC);
 }
 
 function keyTyped() {
