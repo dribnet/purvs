bff0d3a003b4664656b29d3dc3145714ec33b52c
wireprach <wireprach@users.noreply.github.com>
2020-05-18 15:12:03 +1200

    Update README, js. files and images
    For this commit, I added letters h to p. This set had me exploring new ways to use the arcs to form new shapes, and as such, all files, images and the README have been updated.

--- letters.js
+++ letters.js
@@ -160,49 +160,208 @@ const alphabet = {
     "arcE3" : 43.17,
   },
   "H": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 37,
+    "arcY" : 100,
+    "arcS" : 7.85,
+    "arcE" : 23.55,
+
+    //arc 2
+    "arcX2" : 52,
+    "arcY2" : 173,
+    "arcS2" : 35.3,
+    "arcE2" : 19.6,
+
+    //arc 3
+    "arcX3" : 52,
+    "arcY3" : 172,
+    "arcS3" : 27.4,
+    "arcE3" : 11.7,
   },
   "I": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 40,
+    "arcY" : 130,
+    "arcS" : 23.55,
+    "arcE" : 7.85,
+
+    //arc 2
+    "arcX2" : 72,
+    "arcY2" : 162,
+    "arcS2" : 7.85,
+    "arcE2" : 23.55,
+
+    //arc 3
+    "arcX3" : 67,
+    "arcY3" : 157,
+    "arcS3" : 27.47,
+    "arcE3" : 43.17,
+
   },
   "J": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 36,
+    "arcY" : 135,
+    "arcS" : 23.55,
+    "arcE" : 7.85,
+
+    //arc 2
+    "arcX2" : 38,
+    "arcY2" : 217,
+    "arcS2" : 43.17,
+    "arcE2" : 27.47,
+
+
+    //arc 3
+    "arcX3" : 73,
+    "arcY3" : 165,
+    "arcS3" : 7.85,
+    "arcE3" : 23.55,
+
   },
   "K": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 45,
+    "arcY" : 100,
+    "arcS" : 7.85,
+    "arcE" : 23.55,
+
+    //arc 2
+    "arcX2" : 65,
+    "arcY2" : 165,
+    "arcS2" : 19.6,
+    "arcE2" : 3.9,
+
+    //arc 3
+    "arcX3" : 65,
+    "arcY3" : 165,
+    "arcS3" : 27.47,
+    "arcE3" : 43.17,
   },
   "L": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 40,
+    "arcY" : 100,
+    "arcS" : 23.55,
+    "arcE" : 7.85,
+
+    //arc 2
+    "arcX2" : 72,
+    "arcY2" : 162,
+    "arcS2" : 7.85,
+    "arcE2" : 23.55,
+
+    //arc 3
+    "arcX3" : 63,
+    "arcY3" : 147,
+    "arcS3" : 27.47,
+    "arcE3" : 43.17,
   },
   "M": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 0,
+    "arcY" : 160,
+    "arcS" : 23.55,
+    "arcE" : 7.85,
+
+    //arc 2
+    "arcX2" : 35,
+    "arcY2" : 160,
+    "arcS2" : 23.55,
+    "arcE2" : 7.85,
+
+    //arc 3
+    "arcX3" : 65,
+    "arcY3" : 160,
+    "arcS3" : 23.55,
+    "arcE3" : 7.85,
+
   },
   "N": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 12,
+    "arcY" : 165,
+    "arcS" : 23.55,
+    "arcE" : 7.85,
+
+    //arc 2
+    "arcX2" : 57,
+    "arcY2" : 173,
+    "arcS2" : 35.3,
+    "arcE2" : 19.6,
+
+    //arc 3
+    "arcX3" : 57,
+    "arcY3" : 172,
+    "arcS3" : 27.4,
+    "arcE3" : 11.7,
+
   },
   "O": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 50,
+    "arcY" : 162,
+    "arcS" : 11.7,
+    "arcE" : 19.6,
+
+    //arc 2
+    "arcX2" : 50,
+    "arcY2" : 162,
+    "arcS2" : 19.6,
+    "arcE2" : 3.9,
+
+    //arc 3
+    "arcX3" : 50,
+    "arcY3" : 162,
+    "arcS3" : 27.47,
+    "arcE3" : 43.17,
   },
   "P": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "arcW" : 75,
+    "arcH" : 75,
+
+    //arc 1
+    "arcX" : 37,
+    "arcY" : 230,
+    "arcS" : 7.85,
+    "arcE" : 23.55,
+
+    //arc 2
+    "arcX2" : 62,
+    "arcY2" : 165,
+    "arcS2" : 3.9,
+    "arcE2" : 19.6,
+
+    //arc 3
+    "arcX3" : 62,
+    "arcY3" : 165,
+    "arcS3" : 43.17,
+    "arcE3" : 27.47,
+
   },
   "Q": {
     "size": 50,
