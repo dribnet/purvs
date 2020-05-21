797b1158f836a9b119c05d26eb8e6304fac462d4
Jtopolegend <52726354+Jtopolegend@users.noreply.github.com>
2020-05-21 21:33:35 +1200

    Drawing Alphabet Part 1 v2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## MDDN 242 2020 Assignment 2
 
 Part One of Alphabet sketch going well so far i have continued to progress my arc sketches into more complete letter shape symbols that resemble its counterparts pretty well so far through lines curves angles that replicate what it is trying to represent in the alphabet 
+
+Note: First sumbit of this wont have the Leter JS code as i forgot to include it in my git copy this one will and allow you to click on it to see how it works in the JS/html files in the web browser
--- letters.js
+++ letters.js
@@ -5,69 +5,147 @@ const alphabet = {
     "offsety": 0
   },
   "A": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
+    "size": 100,
+    "offsetx": 95,
+    "offsety": -50,
+    "startArc": -55,
+    "stopArc":180
+
   },
   "B": {
-    "size": 75,
-    "offsetx": 0,
-    "offsety": -70
+    "size": 105,
+    "offsetx": 87,
+    "offsety": -55,
+    "startArc": 270,
+    "stopArc":110
   },
   "C": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 105,
+    "offsetx": 90,
+    "offsety": -50,
+    "startArc": 90,
+    "stopArc":300
   },
   "D": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 105,
+    "offsetx": 90,
+    "offsety": -65,
+    "startArc": 277.2,
+    "stopArc":90
   },
   "E": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 100,
+    "offsetx": 90,
+    "offsety": -46,
+    "startArc": 360,
+    "stopArc":180,
+
+    "size2": 100,  // Second Arc
+    "offsetx2": 60,
+    "offsety2": -46,
+    "startArc2": 180,
+    "stopArc2":30
+
   },
   "F": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 100,
+    "offsetx": 90,
+    "offsety": -50,
+    "startArc": 90,
+    "stopArc":300,
+
+    "size2": 60,
+    "offsetx2": 55,
+    "offsety2": -30,
+    "startArc2": 180,
+    "stopArc2":30
   },
   "G": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 100,
+    "offsetx":70,
+    "offsety": -56,
+    "startArc":79.2,
+    "stopArc":360,
+
+    "size2": 60,
+    "offsetx2": 110,
+    "offsety2": -30,
+    "startArc2": 277.2,
+    "stopArc2":90
+
   },
   "H": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 80,
+    "offsetx": 85,
+    "offsety": -40,
+    "startArc": 270,
+    "stopArc":90,
+
+    "size2": 80,
+    "offsetx2": 85,
+    "offsety2": -60,
+    "startArc2": 90,
+    "stopArc2":270
   },
   "I": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+   "size": 50,
+    "offsetx": 20,
+    "offsety": -56,
+    "startArc":180,
+    "stopArc":180,
+
+    "size2": 100,
+    "offsetx2": 100,
+    "offsety2": -40,
+    "startArc2": 90,
+    "stopArc2":270
+
   },
   "J": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 90,
+    "offsetx": 55,
+    "offsety": -56,
+    "startArc":180,
+    "stopArc":0,
+
+    "size2": 100,
+    "offsetx2": 105,
+    "offsety2": -40,
+    "startArc2": 90,
+    "stopArc2":300
   },
   "K": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 100,
+    "offsetx": 55,
+    "offsety": -40,
+    "startArc":90,
+    "stopArc":300,
+
+    "size2": 100,
+    "offsetx2": 95,
+    "offsety2": -30,
+    "startArc2": 277.2,
+    "stopArc2":130
   },
   "L": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 90,
+    "offsetx": 55,
+    "offsety": -50,
+    "startArc":90,
+    "stopArc":270,
+
+    "size2": 80,
+    "offsetx2": 100,
+    "offsety2": -40,
+    "startArc2": 0,
+    "stopArc2":180
   },
   "M": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "size": 100,
+    "offsetx": 100,
+    "offsety": -50,
+    "startArc": 129.6,
+    "stopArc":40,
   },
   "N": {
     "size": 50,
