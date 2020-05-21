4d43cc9afc4f6a4a2af20e316a95676938e69ebd
WatsonHayd <watsonhayd@myvuw.ac.nz>
2020-05-21 19:11:43 +1200

    Exhibition and Fixes

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 ## MDDN 242 2020 Assignment 2
 
-Thanks to the feedback from Phoebe, the boundary boxes around my letters are fixed and in their proper position. I have also changed the background measurement line colors in each of the files. Before they were a dark blue that was way to hard to see, now they are a light gray that is visible but doesnt take away from the overall letter design infront. I am still yet to complete the exhibition and maybe touch up some letters.
+I have made my exhibition words that I will keep for the final submission tomorrow. I have also made 2 new  parameters named: "rect2x" and "rect2y". The reason I made these we because I wasnt happy with a few of my letters and how they looked, namely T, X and Y. I tried making these letters look as good as possible with the last parameters but they still didnt look how I wanted them to. So I created these 2 new parameters in order to have a second rectangle shape in my letters. Using this new rectangle I made those three letters look a ton better than they did before. After adding a new parameter I also had to create it in the "Interaction.js" file so the ne wshape would smoothly transition around the screen. I am now doing some final touchups before submission, really there is not much left to do apart from update with the final readme and statement.
 
-Currently I am sitting on 10 parameters:
+Also I named my font "Neoblock", I wanted to have the word Neon but couldnt fit all of the words in the exhibition so Neoblock it was.
+
+I am now on 12 parameters:
 sizex
 sizey
 offsetx
 offsety
+rect2x
+rect2y
 cirsize1
 cirsize2
 ciroffsetx
--- draw_letters.js
+++ draw_letters.js
@@ -18,6 +18,8 @@ function drawLetter(letterData) {
   let size2 = letterData["sizey"]
   let pos2x = 50 + letterData["offsetx"];
   let pos2y = 100 + letterData["offsety"];
+  let rect2posx = 50 + letterData["rect2x"];
+  let rect2posy = 100 + letterData["rect2y"];
   let cirpos2x = 50 + letterData["ciroffsetx"]
   let cirpos2y = 100 + letterData["ciroffsety"]
   let cirs1 = letterData["cirsize1"]
@@ -34,6 +36,7 @@ function drawLetter(letterData) {
   ellipse(cirpos2x, cirpos2y, cirs1, cirs2)
   ellipse(cir2pos2x, cir2pos2y, cirs1, cirs2)
   rect(pos2x, pos2y, size1, size2)
+  rect(rect2posx, rect2posy, size1, size2)
 
   rectMode(CORNER) //Center boundary boxes
 }
@@ -45,17 +48,19 @@ function interpolate_letter(percent, oldObj, newObj) {
   new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
   new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
 
-new_letter["ciroffsetx"]    = map(percent, 0, 100, oldObj["ciroffsetx"], newObj["ciroffsetx"]);
-new_letter["ciroffsety"]    = map(percent, 0, 100, oldObj["ciroffsety"], newObj["ciroffsety"]);
-new_letter["cirsize1"]    = map(percent, 0, 100, oldObj["cirsize1"], newObj["cirsize1"]);
-new_letter["cirsize2"]    = map(percent, 0, 100, oldObj["cirsize2"], newObj["cirsize2"]);
-new_letter["cir2offsetx"]    = map(percent, 0, 100, oldObj["cir2offsetx"], newObj["cir2offsetx"]);
-new_letter["cir2offsety"]    = map(percent, 0, 100, oldObj["cir2offsety"], newObj["cir2offsety"]);
+  new_letter["ciroffsetx"]    = map(percent, 0, 100, oldObj["ciroffsetx"], newObj["ciroffsetx"]);
+  new_letter["ciroffsety"]    = map(percent, 0, 100, oldObj["ciroffsety"], newObj["ciroffsety"]);
+  new_letter["cirsize1"]    = map(percent, 0, 100, oldObj["cirsize1"], newObj["cirsize1"]);
+  new_letter["cirsize2"]    = map(percent, 0, 100, oldObj["cirsize2"], newObj["cirsize2"]);
+  new_letter["cir2offsetx"]    = map(percent, 0, 100, oldObj["cir2offsetx"], newObj["cir2offsetx"]);
+  new_letter["cir2offsety"]    = map(percent, 0, 100, oldObj["cir2offsety"], newObj["cir2offsety"]);
+  new_letter["rect2x"]    = map(percent, 0, 100, oldObj["rect2x"], newObj["rect2x"]);
+  new_letter["rect2y"]    = map(percent, 0, 100, oldObj["rect2y"], newObj["rect2y"]);
   return new_letter;
 }
 
 var swapWords = [
-  "ABBAABBA",
-  "CAB?CAB?",
-  "BAAAAAAA"
+  "NEOBLOCK",
+  "CREATIVE",
+  "CODING02"
 ]
