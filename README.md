5019e583e5b9d9419f5f901e5f5bfeb2d6144a76
Phoebe Zeller <phoebe.zeller@hotmail.com>
2020-05-27 11:00:47 +1200

    function in pixel

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Creative Coding 2: Custom Pixel
-I have altered my sketch.js code. It now draws stars where ever there is white in the mask. This is an example using nested for loops to create a grid.
+I have altered my sketch.js code.
+In this version it draws a grid of semi transparent squares. Then it draws opaque stars on top.  
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,73 +23,46 @@ function setup() {
   maskImg.loadPixels();
 }
 
-const tileHeight = 10;
-const tileWidth = 10;
+const tileHeight = 30;
+const tileWidth = 30;
 
-const x_step = 10;
-const y_step = 20;
+const x_step = 30;
+const y_step = 30;
 
 function draw() {
 
-  for(var x = 0; x < sourceImg.width; x = x+ x_step){
-    for(var y = 0; y < sourceImg.height; y = y+ y_step){
-        let pix = sourceImg.get(x, y);
-        let mask = maskImg.get(x, y);
-        fill(pix);
-        stroke(pix);
-         if (mask[0] > 128) {
-        rect(x,y,tileWidth,tileHeight);
-  }
-  else{
-    rect(x,y,x_step,y_step);
-  }
-}
+  for (var x = 0; x < sourceImg.width; x = x + x_step) {
+    for (var y = 0; y < sourceImg.height; y = y + y_step) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
 
-}
-
-  //
-  // for (let i = 0; i < 1000; i++) {
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pix = sourceImg.get(x, y);
-  //   let mask = maskImg.get(x, y);
-  //   fill(pix);
-  //   stroke(pix);
-  //
-  //   let pointSize = 20;
-  //   let dice = random(1, 6);
-  //   if (mask[0] > 128) {
-  //     //line(x, y, x + pointSize, y); // horizonal "windy
-  //     drawStar(x,y,15);
-  //   } else {
-  //     if (dice > 5) {
-  //       line(x, y, x + pointSize, y); // horizonal "windy"
-  //     } else {
-  //       line(x, y, x, y + pointSize); // vertical "rainy"
-  //
-  //     }
-  //   }
-  // }
+      fill(pix[0],pix[1],pix[2],50);
+      noStroke();
+      rect(x, y, tileWidth, tileHeight);
+      drawStar(x,y,10,pix);
+    }
+  }
 
 
 
   renderCounter = renderCounter + 1;
-  if (renderCounter > 10) {
+  if (renderCounter > 2) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
 }
 
-function drawStar(x,y,size){
+function drawStar(x, y, size,c) {
+  stroke(c);
   push();
   //strokeWeight(4);
-  translate(x,y);
-  for(var i = 0; i < 10; i++){
-    line(size,0,-size,0);
-      rotate(360/ i);
-    }
+  translate(x+15, y+15);
+  for (var i = 0; i < 10; i++) {
+    line(size, 0, -size, 0);
+    rotate(360 / i);
+  }
   pop();
 }
 
