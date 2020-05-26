877fec607e20568a794df74646b74f53684d3b10
Phoebe Zeller <phoebe.zeller@hotmail.com>
2020-05-26 14:09:49 +1200

    function example

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -25,22 +25,20 @@ function setup() {
 
 function draw() {
 
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 1000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
     stroke(pix);
-    console.log(mask);
 
     let pointSize = 20;
     let dice = random(1, 6);
     if (mask[0] > 128) {
-      line(x, y, x + pointSize, y); // horizonal "windy"
-
-    }
-    else {
+      //line(x, y, x + pointSize, y); // horizonal "windy
+      drawStar(x,y,15);
+    } else {
       if (dice > 5) {
         line(x, y, x + pointSize, y); // horizonal "windy"
       } else {
@@ -48,27 +46,32 @@ function draw() {
 
       }
     }
-
-
-    // if(mask[0] > 128) {
-    //   let pointSize = 20;
-    //   ellipse(x, y, pointSize, pointSize);
-    // }
-    //
-    // else {
-    //   let pointSize = 10;
-    //   rect(x, y, pointSize, pointSize);
-    // }
   }
+
+
 
   renderCounter = renderCounter + 1;
   if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
+
     // uncomment this to save the result
     //saveArtworkImage(outputFile);
   }
 }
+
+function drawStar(x,y,size){
+  push();
+  //strokeWeight(4);
+  translate(x,y);
+  for(var i = 0; i < 10; i++){
+    line(size,0,-size,0);
+      rotate(360/ i);
+    }
+  pop();
+}
+
+
 
 function keyTyped() {
   if (key == '!') {
