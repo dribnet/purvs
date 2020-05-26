4a3c4e3f57789d690853bd000344c23027051e8a
Phoebe Zeller <phoebe.zeller@hotmail.com>
2020-05-26 12:41:01 +1200

    first code example

--- sketch.js
+++ sketch.js
@@ -1,51 +1,72 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  //noStroke();
   background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<2000;i++) {
+function draw() {
+
+  for (let i = 0; i < 2000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
+    stroke(pix);
+    console.log(mask);
 
-    if(mask[0] > 128) {
-      let pointSize = 20;
-      ellipse(x, y, pointSize, pointSize);
+    let pointSize = 20;
+    let dice = random(1, 6);
+    if (mask[0] > 128) {
+      line(x, y, x + pointSize, y); // horizonal "windy"
+
     }
     else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
+      if (dice > 5) {
+        line(x, y, x + pointSize, y); // horizonal "windy"
+      } else {
+        line(x, y, x, y + pointSize); // vertical "rainy"
+
+      }
     }
+
+
+    // if(mask[0] > 128) {
+    //   let pointSize = 20;
+    //   ellipse(x, y, pointSize, pointSize);
+    // }
+    //
+    // else {
+    //   let pointSize = 10;
+    //   rect(x, y, pointSize, pointSize);
+    // }
   }
+
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
