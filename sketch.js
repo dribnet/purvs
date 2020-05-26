f13838a0fe7d4d097e2e47763f68fbb59770332b
WishCotton <52726327+WishCotton@users.noreply.github.com>
2020-05-27 10:52:34 +1200

    output update

--- sketch.js
+++ sketch.js
@@ -31,12 +31,12 @@ function draw () {
     let mask = maskImg.get(x, y);
     fill(pix);
     if(mask[0] > 128) {
-      let pointSize = 50;
+      let pointSize = 20;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
       let pointSize = 10;
-      rect(x, y, pointSize, pointSize);    
+      rect(x, y, pointSize, pointSize);
     }
   }
   renderCounter = renderCounter + 1;
@@ -44,7 +44,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     saveArtworkImage(outputFile);
   }
 }
 
