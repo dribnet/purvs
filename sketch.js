e8dc4481f4478de132e63e7eb7f6f2f94875ec9f
CodyNZ <61666737+CodyNZ@users.noreply.github.com>
2020-05-27 12:24:01 +1200

    Updated masks and sketch.js code
    Figured out that my mask images were far too large and that's why they weren't having an effect on the output. Fixed it by running the images through photoshop. So far I have a "painting effect" similar to what Phoebe created in one of the tutorial videos. The masks arent yet affecting it - will add this funciotnality soon. So far my inital idea for my theme that I quite like is "Nature in the city" (examining how and where we can escape to nature within Wellington city, and how that can be beneficial for all sorts of reasons, mental health etc.

--- sketch.js
+++ sketch.js
@@ -17,30 +17,43 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  //noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
+  for(let i=0;i<2500;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 8;
-      ellipse(x, y, pointSize, pointSize);
+    //console.log(mask);
+
+    stroke(pix);
+    let pointSize = 40;
+    let dice = random(1,6);
+    if (dice > 5){
+      line(x, y, x+pointSize, y);
     }
     else {
-      let pointSize = 6;
-      rect(x, y, pointSize, pointSize);
+      line(x, y, x, y+pointSize);
     }
+
+
+    // if(mask[0] > 128) {
+    //   let pointSize = 60;
+    //   ellipse(x, y, pointSize, pointSize);
+    // }
+    // else {
+    //   let pointSize = 8;
+    //   rect(x, y, pointSize, pointSize);
+    // }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 30) {
+  if(renderCounter > 40) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
