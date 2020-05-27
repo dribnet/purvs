954a58d857f364533978da515dfd943906b80385
George-McKendry <61666943+George-McKendry@users.noreply.github.com>
2020-05-28 00:09:46 +1200

    Working on the first image (blended thin lines)

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -17,36 +17,61 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  //noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 30;
-    ellipse(x, y, pointSize, pointSize);
-    }
+   for(let i=0;i<1000;i++) {
+     let x = floor(random(sourceImg.width));
+     let y = floor(random(sourceImg.height));
+     let pix = sourceImg.get(x, y);
+     let mask = maskImg.get(x, y);
+     fill (pix);
+     stroke(pix);
+     fill(pix);
+     let pointSize = 10;
+     if (mask[0]> 128) {
+       masked(x,y);
+
+     } else
+     line (x-20, y, x+pointSize*5, y);
+   }
+
+   //console.log (mouseX)
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+  //     let pointSize = 10;
+  //     ellipse(x, y, pointSize+5, pointSize+5);
+  //   }
+  //   else {
+  //     let pointSize = 30;
+  //   rect(x, y, pointSize+20, pointSize-10);
+  //   }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 1) {
     console.log("Done!")
     noLoop();
     //uncomment this to save the result
     //saveArtworkImage(outputFile);
   }
+
+function masked (x,y) {
+    let pointSize = 10;
+  if (x<400, x>1100){
+    line (x-20, y, x+5, y)
+  }else
+      line (x-20, y, x+pointSize*1.5, y);
 }
+
+
 
 function keyTyped() {
   if (key == '!') {
