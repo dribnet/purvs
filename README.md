5a1e723b94e1f190ac083fd0308988fe6866a9f2
Kiera <61666919+mckechkier@users.noreply.github.com>
2020-05-28 11:39:54 +1200

    playing around with the code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+I used Phoebes's template code from the tutorial on blackboard and have just been playing around with the different pieces of code to get more comfortable with it. I think the photos look really cool so far even with just this sort of basic code. They're all photos I took of my younger brother on his skateboard. The colour wave is really autumy and warm so I really like how the colours are displaying through the pixels. I'm not 100% sure just yet if I want to keep using these photos, I love how vibrant the third one is, so I may take some more of him with a brighter range of colours but then I also do really like the green and beige colour wave in the first two so I'm a little conflicted.
+
+I'm going to keep playing and creating my own code now to try and establish a theme. 
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 //before code is up n running, process of showing the image
 function preload() {
@@ -18,31 +18,35 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  //noStroke();
   background(157, 145, 79);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
+  for(let i=0;i<2000;i++) { //The 2000 is how much pixels(in this case rects) there are on the screen, higher the number the more densly packed the pixels are
+    let x = floor(random(sourceImg.width)); //Using a random X & random Y
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
+    stroke(pix);
 
 
     if(mask[0] > 128) {
       let pointSize = 20;
       //triangle(x,y,38,10,pointSize,pointSize) //58, 20
-      //ellipse(x, y, pointSize, pointSize);
-      rect(x, y, pointSize, pointSize);
+      ellipse(x, y, pointSize, pointSize);
+      //rect(x, y, pointSize, pointSize);
+      //drawStar(x,y,40);
+
     }
     else {
       let pointSize = 35;
       //rect(x, y, pointSize, pointSize);
-      ellipse(x, y, pointSize, pointSize);
+      //ellipse(x, y, pointSize, pointSize);
+      drawStar(x,y,16);
     }
   }
   renderCounter = renderCounter + 1;
@@ -50,8 +54,19 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
+}
+
+function drawStar(x,y,size) {
+push();
+strokeWeight(7);
+translate(x,y)
+for (var i = 0; i < 20; i++){ //10
+line(size, 0, -size, 0);
+rotate(360 % i);
+}
+pop();
 }
 
 function keyTyped() {
