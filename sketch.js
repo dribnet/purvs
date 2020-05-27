4a1522f2f990b445466e4df619be747d17807d7b
Madisyn-0 <61666971+Madisyn-0@users.noreply.github.com>
2020-05-27 19:22:00 +1200

    Grid example and new image

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -25,31 +25,54 @@ function setup() {
   maskImg.loadPixels();
 }
 
+const tileHeight = 10;
+const tileWidth = 10;
+
+const x_step = 12;
+const y_step = 12;
+
 function draw() {
-  for (let i = 0; i < 3200; i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    stroke(pix);
 
-    let pointSize = 20;
+  for(var x = 0; x < sourceImg.width; x = x+ x_step){
+    for(var y = 0; y < sourceImg.height; y = y+ y_step){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+        fill(pix);
+        stroke(pix);
+        //rect(x,y,tileWidth,tileHeight);
+          if (mask[0] < 128) { //if mask is black
+            strokeWeight(7)
+            line(x, y, x + x_step, y + y_step); // horizonal "windy"
 
-    if (mask[0] < 128) { //if mask is black
-
-      strokeWeight(5)
-      line(x, y, x + pointSize, y); // horizonal "windy"
-
-    } else {
-      let pointSize = 20;
-      ellipse(x, y, pointSize, pointSize);
-
-      // let pointSize = 10;
-      // rect(x, y, pointSize, pointSize);
-    }
-
+          } else {
+            let pointSize = 20;
+            ellipse(x, y, pointSize, pointSize);
+            // let pointSize = 10;
+            // rect(x, y, pointSize, pointSize);
+         }
   }
+}
+
+  // for (let i = 0; i < 3200; i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   fill(pix);
+  //   stroke(pix);
+  //
+  //   let pointSize = 20;
+  //   if (mask[0] < 128) { //if mask is black
+  //     strokeWeight(4)
+  //     line(x, y, x + x_step, y + y_step); // horizonal "windy"
+  //
+  //   } else {
+  //     let pointSize = 20;
+  //     ellipse(x, y, pointSize, pointSize);
+  //     // let pointSize = 10;
+  //     // rect(x, y, pointSize, pointSize);
+  //   }
+  // }
 
   renderCounter = renderCounter + 1;
   if (renderCounter > 10) {
