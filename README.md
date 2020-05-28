1ac9b882f750c1198584ecc4a0a555c6814d6855
Lexie Etherington <lexie202@gmail.com>
2020-05-28 22:25:02 +1200

    Updated README, input_2, Input_3, output_2, output_3, preview, sketch, thumbnail

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 ## Creative Coding 2: Custom Pixel
 
-Today, I have been able to add in my own images and change my mask. I found it really helpful in creating my mask using procreate and layering the images on top and changing the opacity down. I will definetly use this techinque for the rest of my images. Next step for me now that I have a base of how to work with the code and its functions, is selecting the theme for my images or taking more if need be
+Today I have been playing around with different images I can use for my final, I think I will continue using my flower photos as they display a lot of colour variety. I will need to consider the flowers having different angles as well as making the code make the images look like they are flowers
+
 
 The images above are photos of Keukenhof flower garden in Lisse, Netherlands, algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "output_2.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -24,20 +24,23 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
+
+    for(let i=0;i<2000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 20;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 5;
-      ellipse(x, y, pointSize, pointSize);
-    }
+    stroke(pix);
+    strokeWeight(4);
+   if(mask[0] > 128) {
+           let pointSize = 10;
+     line(x, y, x+pointSize*3, y);
+      }
+         else {
+      let pointSize = 10;
+     rect(x, y, pointSize, pointSize+5);
+      }
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
