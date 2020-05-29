0e98cc04eacc07eed9c6baff3b96f69d91ce4196
GussyBoy5432 <61666925+GussyBoy5432@users.noreply.github.com>
2020-05-29 17:22:50 +1200

    Used Rain Effect on Slinky

--- sketch.js
+++ sketch.js
@@ -17,34 +17,42 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
+  //noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<2000;i++) {
+  for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
-    }
+    stroke(pix);
+       let pointSize = 50;
+       let dice = random(1,6);
+       if(dice == 6){
+       line(x, y, pointSize+x, y);
+       }
+      else{
+       line(x, y, x, y+pointSize);
+}
+  //    let pointSize = 50;
+  //    ellipse(x, y, pointSize, pointSize);
+  //  }
+  //  else {
+  //    let pointSize = 10;
+  //    rect(x, y, pointSize, pointSize);
+  //  }
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
 }
 
