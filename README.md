4488b7b3f2110ea63e940b6994dd43ded5cfcb1d
PatelShiv7 <61666967+PatelShiv7@users.noreply.github.com>
2020-05-29 17:57:30 +1200

    2nd image
    Creating masks for 2nd picture

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
 For this problem set, I'm thinking of doing something based on tradition as I feel like it'll have more meaning compared to something that isn't. This relates back towards one of the universities main message on telling a story with heritage. So far, I've updated one of the photos, and will update the rest in the next session.
+
+Started doing my second image, quite happy with how it's looking currently. I figured out that I wanted to mask the textures on the 'saris' so that it makes it more clear for the user. This is potentially an image I'll be using for my final as I'm quite happy on how it looks on the page. Currently in terms of ideas, I'm still not entirely sure what my overall theme is, so I'm still gonna base my designs around Indian heritage until I decided of one. My next steps are to finish the masking texture on the second output, then start to think of some new ideas for the third image.
--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -14,22 +15,31 @@ function preload() {
 function setup () {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
+
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(0);
   sourceImg.loadPixels();
+  maskImg.loadPixels();
 }
+
 function draw () {
+  for(let i=0;i<10000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+
     fill(pix);
+
     if(mask[0] > 128) {
-      let pointSize = 50;
+      let pointSize = 10;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
       let pointSize = 10;
       rect(x, y, pointSize, pointSize);
+    }
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
