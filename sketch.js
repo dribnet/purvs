f7b9db562e461f5f3ec1576ba2269fbdafca6802
PatelShiv7 <61666967+PatelShiv7@users.noreply.github.com>
2020-05-31 10:34:05 +1200

    Adding grids
    Using grids

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "output_2.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -21,9 +21,26 @@ function setup () {
   background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+
 }
+const tileHeight = 10;
+const tileWidth = 10;
+
+const x_step = 15; //space between this varable and tile width (5)
+const y_step = 15; //space between this variable and tile height (5)
+
 
 function draw () {
+
+    for(var x = 0; x < sourceImg.width; x = x + x_step){
+      for(var y = 0; y < sourceImg.height; y = y + y_step){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+        fill(pix);
+        stroke(pix);
+        rect(x,y,tileWidth,tileHeight);
+      }
+    }
   for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -41,6 +58,7 @@ function draw () {
       rect(x, y, pointSize, pointSize);
     }
   }
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
