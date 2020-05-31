ff76beeded054f51104af16a1ec83746b5773aae
whitefalconnz <Whitefalconnz@gmail.com>
2020-05-31 15:56:39 +1200

    updated everything

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+31/05/2020
+
+My overall idea for this project is too do to use photos of area of bush walks, this has personal significance to me because I used to go on many bush walks with my dad when I was younger.
+With the masks I want to make the thing that shows up in the mask clearly stand out from the rest but be composed of the same elements. Ideas I have to make the mask stand out at the moment are; change the angle, change opacity, change colour, make the elements bigger.
+
+I have loaded two of my photos into the project folder and I have started messing around with different functions to create the affect of something being painted.
+
+To try and achieve this at the moment I have created a function with multiple bezier curves seperated by 20 pixels each. And then have made them go in random places on the canvas 2000 times. Moving forward I want to seperate the beziers by a smaller amount and add more curves like a brush would have.
+I also want to use multiples layers of 'paint' so that it is easier to make out what the picture is and to add more interest/detail.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let maskFile = "mask_1.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -12,40 +12,76 @@ function preload() {
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
-
+  angleMode(DEGREES)
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(10);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
+const x_steps = 15
+const y_steps = 15
+// const tileheight = 20
+// const tilewidth = 20
 
-function draw () {
-  for(let i=0;i<2000;i++) {
+function draw() {
+
+  for (let i = 0; i < 2000; i++) {
     let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+    let y = floor(random(sourceImg.width));
+    let mask = maskImg.get(x, y)
     let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);    
+  if (mask[0] > 120) {
+    strokeWeight(1)
+      stroke(pix);
+      noFill()
+      let tilewidth=20
+      let tileheight=20
+      brushstroke2(x, y);
+    } else {
+      strokeWeight(1)
+      stroke(pix,50);
+      noFill()
+      let tilewidth=5
+      let tileheight=5
+    brushstroke1(x,y);
+    brushstroke2(x,y);
+      // for (var x = 0; x < sourceImg.width; x = x + x_steps) {
+      //   for (var y = 0; y < sourceImg.height; y = y + y_steps);
+      // let x = floor(random(sourceImg.width));
+      // let y = floor(random(sourceImg.height));
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
+}
+
+function brushstroke1(x,y){
+  push()
+  translate(x,y)
+  bezier(65, 0, 40, 40, 50, 50, 15, 40);
+  translate(0,20)
+  bezier(85, 0, 40, 40, 50, 50, 15, 60);
+  pop()
+
+}
+function brushstroke2(x,y){
+  push()
+  translate(x,y)
+  rotate(50)
+  bezier(65, 0, 40, 40, 50, 50, 15, 40);
+  translate(0,20)
+  bezier(85, 0, 40, 40, 50, 50, 15, 60);
+  pop()
+
 }
 
 function keyTyped() {
