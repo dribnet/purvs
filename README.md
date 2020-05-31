13f7836ee48542a96f5ced09b28aa417151913c4
Bee Huang <barbarahuang@hotmail.co.uk>
2020-06-01 07:04:29 +1200

    Grid Technique

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 ## Creative Coding 2: Custom Pixel
 
-01/06/20 - Theme: 2020 Wellington New Years Festival
+01/06/20 - Grid Techniques
 The images above are photos of the new years festival in Wellington 2020 processed with a lightly modified version of the p5.js Pointillism example.
 
-I thought it would be a good idea to continue using the theme of the new years festival that happened this year before quarantine. It highlights a time where the Wellington community came together and created this event for anyone to come celebrate. Now would be the perfect time to theoretically apply these images to a billbaord because they are a reflection of the highlights early this year.
-These images were just taken on my phone and I took them because they were the bright colourful objects that caught my attention.
+In this experiment I try a uniform gridding technique using the same masks as my (previous) 'theme' readme. I followed along with pheobe's gridding tutorial and this was my result. I found the outputs quite satifying to look at because the squares and circles were all in line with each other. I especially like the duck output because there is two different shades of grey. The light gray in front creates the illusion of water and because I decided to draw my mask in a wave form it really works with the image. I will try to add this look into my other images. In order to do this I may have to incoporate so photo manipulation.
 
-Currently I am not too sure what I will end up with for the pixel design but these images have great poteintial to help create something cool! I have kept the pixel design in this version all the same and only changed the mask to prepare myself for this project.
+
+In my next experiment I will try a more irregular grid layout. I will do this by inputting a crasshatch texture or maybe some stars.
+
 My pixel design consists of:
-
 Masked:
 let pointSize = 5;
-rect(x, y, pointSize+50, pointSize);
+if (mask[0] > 128) {
+rect(x,y,tileWidth,tileHeight); //tile lines
 
 Unmasked:
-let pointSize = 5;
-rect(x, y, pointSize, pointSize);
+else{
+ellipse(x,y,x_step,y_step); //the circles
 
 
 
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,30 +23,55 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 5;
-      rect(x, y, pointSize+50, pointSize);
-    }
-    else {
-      let pointSize = 5;
-      rect(x, y, pointSize, pointSize);
-    }
+const tileHeight = 10; //tile hieght
+const tileWidth = 10; //tile width
+
+const x_step = 30; //circle width
+const y_step = 30; //circle height
+
+function draw() {
+
+  for(var x = 0; x < sourceImg.width; x = x+ x_step){
+    for(var y = 0; y < sourceImg.height; y = y+ y_step){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+        fill(pix);
+        stroke(pix);
+         if (mask[0] > 128) {
+        rect(x,y,tileWidth,tileHeight); //tile lines
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
-    console.log("Done!")
-    noLoop();
-    // uncomment this to save the result
-    saveArtworkImage(outputFile);
+  else{
+    ellipse(x,y,x_step,y_step); //the circles
+
   }
 }
+
+}
+}
+
+  // for(let i=0;i<2000;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+  //     let pointSize = 5;
+  //     rect(x, y, pointSize+50, pointSize);
+  //   }
+  //   else {
+  //     let pointSize = 5;
+  //     rect(x, y, pointSize, pointSize);
+  //   }
+  // }
+  // renderCounter = renderCounter + 1;
+  // if(renderCounter > 10) {
+  //   console.log("Done!")
+  //   noLoop();
+    // uncomment this to save the result
+    // saveArtworkImage(outputFile);
+//   }
+// }
 
 function keyTyped() {
   if (key == '!') {
