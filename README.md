417fbee8f333b43bff8b88d740fbed970e899b81
Lexie Etherington <lexie202@gmail.com>
2020-06-02 11:55:25 +1200

    Updated README, output1, output2,output3, preview, sketch and thumbnail

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 
 ## Creative Coding 2: Custom Pixel
 
-Today I am yet to figure out how to create more layer colours but in the meantime, I thought it would be interesting to revesre the mask as the other styles I have used the white on the flowers and black on the background. I think this has made a really interesting look and gives interest between the outputs without needing to change the code
+Today, with the help of Phoebe, I am now able to have different layers of mask, I also think it could be interesting to see if I could use a draw function to either draw a shape resebling a leaf or a flower. I will develop this idea further and see where it takes me.
 
+Also I am having trouble with the draw leaf function. The random function isn't as controled and the red dots are appear on other parts of the background. Not too hard to fix just annoying more than anything!
 
 The images above are photos of Keukenhof flower garden in Lisse, Netherlands, algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -26,36 +26,62 @@ function setup () {
 
 function draw () {
 
-    for(let i=0; i<2000;i++) {
+    for(let i=0; i<1200;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
     stroke(pix);
-    strokeWeight(10);
+    strokeWeight(3);
 
-   if(mask[0] > 1) {
-           let pointSize = 40;
-           line(x-10, y, x+pointSize/5, y+pointSize/3);
+   if(mask[0] == 255) {
+           let pointSize1 = 60;
+           line(x-10, y, x+pointSize1/5, y+pointSize1/3);
       }
-  else if(1 >= mask[0] && mask[0] > 200){
-          let pointSize = 50;
-          rect(x, y, pointSize,pointSize+5);
+  else
+   if(mask[0] > 170 && mask[0] < 200){
+          let pointSize2 = 10;
+          ellipse(x, y, pointSize2,pointSize2+5);
   }
-         else {
-        let pointSize = 3;
-        ellipse(x, y, pointSize,pointSize+5);
+        else {
+          let dice = random(1,6);
+          if (dice < 5) {
+        let pointSize3 = 3;
+      ellipse(x, y, pointSize3, pointSize3+5);
+
+      } else {
+//if(mask[0] > 100 && mask[0] < 170){
+      let pointSize3 = 10;
+    //    rect(x, y, pointSize3+5, pointSize3+5);
+      drawLeaf(x,y,pointSize3);
+
+        //Make it a begin shape vibe either leaf looking or flower looking
+
       }
+      }
+    }
   }
+
+  function drawLeaf(x,y,size){
+    push();
+    for(var i = 0; i < 5; i++){
+      let pointSize4 = 3
+      //ellipse(x,y,pointSize4);
+    // ellipse(x, 0, 10, 20);
+      rotate(360/i);
+    }
+  pop();
+  }
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-  //saveArtworkImage(outputFile);
+  saveArtworkImage(outputFile);
   }
-}
+
 
 function keyTyped() {
   if (key == '!') {
