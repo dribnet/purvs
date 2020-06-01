7f1dce88c7fb5d47ba92ef3d918456141f90f991
Newto4544 <61666882+Newto4544@users.noreply.github.com>
2020-06-01 20:15:12 +1200

    Tidying up and new features

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
-if(mask[0] < 5 && i < 3000 ){ //Black, background
-  let pointSize = 9;
+if(mask[0] < 5 && i < 3500 ){ //Black, background
+  let pointSize = 8;
   strokeWeight(0)
+  stroke(0)
   rect(x, y, pointSize, pointSize,1);
 }
-
-else if(mask[0] > 5 && mask[0] < 200) { //Grey, Foreground
+else if(mask[0] > 100 && mask[0] < 200) { //light Grey, Sky
+  let pointSize = 7;
+  strokeWeight(0);
+  fill(pix[0], pix[1], pix[2], 255)
+  ellipse(x, y, pointSize, pointSize)
+}
+else if(mask[0] > 5 && mask[0] < 100) { //Dark Grey, Foreground
   let pointSize = 4;
   strokeWeight(0);
   rect(x, y, pointSize, pointSize,2)
 }
-
-else if(mask[0] > 200 && i < 2000){ //White, water
+else if(mask[0] > 200 && i < 1500){ //White, water
   let pointSize = random(4, 10);
-  stroke(pix)
+  stroke(pix[0], pix[1], pix[2])
   strokeWeight(pointSize)
     line(x - random(5,20), y, x + random(5,20), y)
 }
 
-Added and fine tuned the random variables drawing the water
-Updated mask to include rope in the foreground
+Fixes / Cleanup
 
-Issues:
+  - Cleaned up code, now only runs 8000 times rather than 15000
+
+Added:
+
+  + Fourth mask, Masks now include, Foreground, background, sky, and water
+  + Started code for adjusting transparency of water
+
+Issues / progress:
+
   - Water mask bleeds over other masks
   - water looks flat, might add transparency to edges to soften edges make it look more fluid.
-
-  - Might add a third mask for the sky to further differentiate it between the background
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "output_1.png";
 
 angleMode(DEGREES)
@@ -26,7 +26,7 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i < 15000;i++) {
+  for(let i=0;i < 8000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
@@ -34,21 +34,26 @@ function draw () {
 
     fill(pix);
 
-    if(mask[0] < 5 && i < 3000 ){ //Black, background
-      let pointSize = 9;
+    if(mask[0] < 5 && i < 3500 ){ //Black, background
+      let pointSize = 8;
       strokeWeight(0)
+      stroke(0)
       rect(x, y, pointSize, pointSize,1);
     }
-
-    else if(mask[0] > 5 && mask[0] < 200) { //Grey, Foreground
+    else if(mask[0] > 100 && mask[0] < 200) { //light Grey, Sky
+      let pointSize = 7;
+      strokeWeight(0);
+      fill(pix[0], pix[1], pix[2], 255)
+      ellipse(x, y, pointSize, pointSize)
+    }
+    else if(mask[0] > 5 && mask[0] < 100) { //Dark Grey, Foreground
       let pointSize = 4;
       strokeWeight(0);
       rect(x, y, pointSize, pointSize,2)
     }
-
-    else if(mask[0] > 200 && i < 2000){ //White, water
+    else if(mask[0] > 200 && i < 1500){ //White, water
       let pointSize = random(4, 10);
-      stroke(pix)
+      stroke(pix[0], pix[1], pix[2])
       strokeWeight(pointSize)
         line(x - random(5,20), y, x + random(5,20), y)
     }
