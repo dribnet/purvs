3c036a4a7878e66f003a1e8450364057381cc20b
ruddelgrac <61666879+ruddelgrac@users.noreply.github.com>
2020-06-01 19:37:54 +1200

    Finalised my images and theme
    FInalised my images and theme and began developing what I wanted my pixels to look like

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,20 +23,31 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const squareHeight=20;
+const squareWidth=20;
+
 function draw () {
   for(let i=0;i<2000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
+    fill(pix[0], pix[1], pix[2], 60);
+
+//if the mask is close to black
+    if(mask[0]) {
+      rect(x,y, squareWidth,squareHeight);
+      Sea();
     }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
+    //if the mask isn't black
+    if(mask[255]) {
+      rect(x, y, squareWidth,squareHeight);
+      Earth();
+    }
+    //if the mask isn't black
+    if(mask[97]) {
+      rect(x, y, squareWidth,squareHeight);
+      Sky();
     }
   }
   renderCounter = renderCounter + 1;
@@ -46,6 +57,28 @@ function draw () {
     // uncomment this to save the result
   //saveArtworkImage(outputFile);
   }
+}
+
+//draw on the black parts of the mask (the sea)
+function Sea(){
+  //noStroke();
+//  ellipse(x,y, 5,5);
+
+
+}
+
+//draw on the white parts of the mask (the earth)
+function Earth(){
+    noStroke();
+    ellipse(x,y, 5,5);
+    ellipse(x*2,y*2, 5,5);
+    ellipse(x*4,y*4, 5,5);
+    ellipse(x*6,y*6, 5,5);
+    ellipse(x*8,y*8, 5,5);
+    rect(x,y, 10,10);
+}
+function Sky(){
+
 }
 
 function keyTyped() {
