210e29f0ad6eaf101be5db26cd5abae9a6c17e64
wrightconn <connorwright247@gmail.com>
2020-06-01 17:02:19 +1200

    CROSS-HATCHING EXPERIMENTING TWO

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-CROSS-HATCHING EXPERIMENTING
+CROSS-HATCHING EXPERIMENTING 2
 
-Today after watching the videos I added in that cross-hatching effect into my code by using lines, I like how this looks on the animals since it makes them look like they have fur and with the last one it looks like grass. Watching the videos and playing around with the code has helped me to understand the basics for this assignment. I liked the pixels in a grid formation, that's something I want to try and incorporate within my final since its very clean and structured.
+Just experimenting with adding in functions to the code, getting that star effect. I made it so it looked blurry and fluffy. Parts of the background become unclear and loses its detail.  
 
 Processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
-// change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+// CHANGE FOR APPROPIATE IMAGE
+let sourceFile = "input_2.jpg";
+let maskFile = "mask_2.png";
+let outputFile = "output_2.png";
 
+// PROCESS SHOWING IMAGE
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
 
@@ -23,34 +24,49 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<4000;i++) {
+// DARWING PATTERN AND DESIGN
+function draw() {
+  for (let i = 0; i < 5000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 20;
-      noStroke()
-      rect(x, y, pointSize, pointSize);
-    }
-    else {
+    if (mask[0] > 128) {
+      let pointSize = 5;
+      fill(pix);
+      strokeWeight(2)
+      line (x+pointSize, y, x -pointSize, y);
+      line (x, y+pointSize, x, y-pointSize);
+
+    } else {
       let pointSize = 10;
       stroke(pix);
-      strokeWeight(3)
-      line(x, y, x + pointSize, y);
-      line(x, y, x , y+ pointSize);
+      //strokeWeight(3)
+      drawStar(x,y,10);
+      //line(x, y, x + pointSize, y);
+      //line(x, y, x, y + pointSize);
 
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
-    // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    // UNCOMMENT TO SAVE
+    //saveArtworkImage(outputFile);
   }
+}
+
+function drawStar(x, y, size) {
+  push();
+  strokeWeight(0.5);
+  translate(x, y)
+  for (var i = 0; i < 10; i++) {
+    line(size, 0, -size, 0);
+    rotate(360 / i);
+  }
+  pop();
 }
 
 function keyTyped() {
