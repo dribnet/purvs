08c661ad7ded71d30069a0e904ec59ec4cf2ffcb
Phoebe Zeller <phoebe.zeller@hotmail.com>
2020-06-01 14:26:49 +1200

    colour example

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Creative Coding 2: Custom Pixel
 I have altered my sketch.js code.
-In this version it draws a grid of semi transparent squares. Then it draws opaque stars on top.  
+This version modifies the colour in some sections
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -19,6 +19,7 @@ function setup() {
   imageMode(CENTER);
   //noStroke();
   background(0);
+  strokeWeight(1);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -31,22 +32,45 @@ const y_step = 30;
 
 function draw() {
 
-  for (var x = 0; x < sourceImg.width; x = x + x_step) {
-    for (var y = 0; y < sourceImg.height; y = y + y_step) {
+  for (let i = 0; i < 3000; i++) {
+      let x = floor(random(sourceImg.width));
+      let y = floor(random(sourceImg.height));
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
+      fill(pix);
+      stroke(pix);
 
-      fill(pix[0],pix[1],pix[2],50);
-      noStroke();
-      rect(x, y, tileWidth, tileHeight);
-      drawStar(x,y,10,pix);
+      let pointSize = 20;
+      let dice = random(1, 6);
+
+      if (mask[0] > 128) {
+        line(x, y, x + pointSize, y); // horizonal "windy
+      } else {
+        if(pix[1] > pix[0]){
+          strokeWeight(3);
+          let pixMod = sourceImg.get(x, y);
+          pixMod[0] = pixMod[0]*2;
+          pixMod[1] = pixMod[1];
+          pixMod[2] = pixMod[2]*2;
+
+          stroke(pixMod);
+          //stroke(pix);
+
+        }else{
+          stroke(pix);
+          strokeWeight(1);
+        }
+        if (dice > 5) {
+          line(x, y, x + pointSize, y); // horizonal "windy"
+        } else {
+          line(x, y, x, y + pointSize); // vertical "rainy"
+
+        }
+      }
     }
-  }
-
-
 
   renderCounter = renderCounter + 1;
-  if (renderCounter > 2) {
+  if (renderCounter > 6) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
