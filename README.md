e73d87b0188372a3fce76c6291246ddadf4631cc
LishaSmith <61666963+LishaSmith@users.noreply.github.com>
2020-06-01 20:33:25 +1200

    Played with Colour and Mask values
    Added some pops of colour in my images, at the moment it's blue, and a bit messy. Also made it so sketch.js recognises grey as a different area.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Creative Coding 2: Custom Pixel
 
-I created a function within a mask that draws the crosshatch effect within the specified value. I also added in a final image. I am aiming for a very mellow, warm toned aesthetic. I liked the concept of having my image transformed into paintings.
-My first two images have the crosshatch effect with my third just being vertical lines. My second image uses rectangles, I wanted to see if rectangles worked better with the lines. I quite like how it looks on the building but I will have to see if it works with the other images.
+I looked at manipulating colour using an if statement. I played around with values until I got a look that I liked. Image 2 was a problem because there was a lot of green and dark tones, so either it manipulated nothing at all or the entire thing. I solved this by bringing in a new branch on my if statement, this branch would take anything in between pure black and white and make something new out of it.
+I decided to make these colour accents with my lines. As well as this, I changed my lines to be somewhat slanted, it gave a very blended and soothing look combined with the accents. I played around with the grey element on image 2 specifically.
+Colour-wise I wanted an orange, to make the image seem more lively, but it looked really contrasting and out of place. After some messing around I decided on a soft blue colour.
+I quite like how this pop of colour looks (especially on image 3), it adds a new element to all of my images. Creating a different feel. In future, I will look at making it so my images relate to eachother a lot more. Deciding abstract pops of colour or subtler blending. I will clean up my masks so that its more specific.
 
-
-I've taken images from a recent visit to the Auckland Botanical Gardens, this is because I enjoy visiting this place and I love nature.
+I've taken images from a recent visit to the Auckland Botanical Gardens, this place is very special to me. I absolutely love nature and landscapes. I feel that after the long amount of time we all spent indoors, it's nice to remember just how peaceful and beautiful our planet really is.
--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "output_2.png";
+let sourceFile = "input_3.jpg";
+let maskFile = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
 
@@ -24,42 +24,62 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<5000;i++) {
+function draw() {
+  for (let i = 0; i < 4000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-
     fill(pix);
     stroke(pix);
     strokeWeight(5);
-    if(mask[0] > 128) {
+
+
+    if (mask[0] == 255){ //white
       let pointSize = 10;
       rect(x, y, pointSize, pointSize);
       //ellipse(x, y, pointSize, pointSize);
-    }
-    else {
+    } else if (mask[0] > 50 && mask [0] < 200){ //grey
+      stroke(pix);
+      drawHatch(x, y, 5);
+    } else { //black
+      if (pix[1] > pix[2]) { //if the green value is greater than the blue
+        drawBlue(x, y)
+      } else {
+        stroke(pix); //use the regular image rgb
+      }
       drawHatch(x, y, 5)
     }
+
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-   //saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
-function drawHatch(x, y, size){
+//draw diagonal crosshatch effect
+function drawHatch(x, y, size) {
   let pointSize = 15;
   let dice = random(1, 6);
-  if (dice > 4) {
+  if (dice > 5) {
     line(x, y, x + pointSize, y);
   } else {
-    line(x, y, x, y + pointSize);
+    line(x + (pointSize/2), y, x, y + pointSize);
   }
+}
+
+//draw blue accents using the stroke
+function drawBlue(x, y) {
+  let pixMod = sourceImg.get(x, y);
+  pixMod[0] = pixMod[0];
+  pixMod[1] = pixMod[1];
+  pixMod[2] = pixMod[2] * 2;
+  stroke(pixMod);
+
 }
 
 function keyTyped() {
