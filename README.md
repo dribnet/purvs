d7dbacff3001fc21ca89da2426ffcb3a40664baf
Liam710 <liamsaps4d@gmail.com>
2020-06-02 19:32:46 +1200

    experimenting with pixel

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos from a trip to Japan I went on in 2015. They all share similar colours and moods so I thought they would be good subjects for this project. I made some simple masks to highlight the main subject of each photo.
+The images above are photos from a trip to Japan I went on in 2015. They all share similar colours and moods so I thought they would be good subjects for this project.
 
-At the moment I am just using the example code while I familiarise myself with it. Next, I will begin experimenting with my own custom pixel.
+I am experimenting with using lines to represent my pixels. The detailed part uses sharp caps, while the background is rounded. I added a variable that can take advantage of gradients. I will be sure to tinker with the gradients to produce some cool effects.
--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
+
 }
 
 function setup () {
@@ -17,28 +18,39 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
-  background(255);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+const gridlen = width/300
+const gridhi = height/100
+
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
+  for(let i=0;i<5000;i++) {
+    let x = random(sourceImg.width);
+    let y = random(sourceImg.height);
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+    let pointSize = 50;
+    let len = 255-mask
     fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
+    stroke(pix);
+    if(mask[0] > 50) {
+      strokeWeight(mask[0]/100);
+      strokeCap(ROUND);
       let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
+      line(x,y,x,y+pointSize)
+    } else{
+      strokeWeight(5);
+      strokeCap(SQUARE);
+      let pointSize = 10;
+      line(x,y-pointSize,x,y+pointSize)
     }
   }
+
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
