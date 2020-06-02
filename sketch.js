7b6607519d2b7801f90c7fd1d60c9bb8c56f9626
Jamie Gambles <gamblejami@myvuw.ac.nz>
2020-06-03 01:52:38 +1200

    Experimenting with code, and following grid tutorial

--- sketch.js
+++ sketch.js
@@ -1,50 +1,52 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
   background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
+let tilewidth = 10
+let tileheight = 10
+
+
+
+function draw() {
+
+  for (var x = 0; x < sourceImg.width; x = x + tilewidth+4) {
+    for (var y = 0; y < sourceImg.height; y = y + tileheight) {
+
+let pix = sourceImg.get(x, y)
+console.log(pix)
+strokeWeight(0)
+stroke(pix[0])
+fill(pix[1])
+      rect(x, y,pix[0]/20, tileheight)
     }
   }
+
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 15) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
