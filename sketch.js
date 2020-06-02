47fa80b968fc5b361aec3018da4aed547ae7de50
Jamie Gambles <gamblejami@myvuw.ac.nz>
2020-06-03 10:08:45 +1200

    its just an experiment bro

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
+let sourceFile = "input_3.jpg";
 let maskFile = "mask_1.png";
 let outputFile = "output_1.png";
 
@@ -29,24 +29,26 @@ let tileheight = 10
 
 function draw() {
 
-  for (var x = 0; x < sourceImg.width; x = x + tilewidth+4) {
+beginShape()
+  for (var x = 0; x < sourceImg.width; x = x + tilewidth) {
     for (var y = 0; y < sourceImg.height; y = y + tileheight) {
 
 let pix = sourceImg.get(x, y)
 console.log(pix)
-strokeWeight(0)
+strokeWeight(1)
 stroke(pix[0])
-fill(pix[1])
-      rect(x, y,pix[0]/20, tileheight)
+noFill()
+      vertex(x - pix[0]/10, y)
     }
   }
+  endShape()
 
   renderCounter = renderCounter + 1;
   if (renderCounter > 15) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    //saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
