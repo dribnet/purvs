d985cc128e9237a0024fb173edfef3d24145f908
whitefalconnz <Whitefalconnz@gmail.com>
2020-06-04 22:28:11 +1200

    updated sketch
    adding more detail with mask, layers and colour

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 31/05/2020
 
+4pm
 My overall idea for this project is too do to use photos of area of bush walks, this has personal significance to me because I used to go on many bush walks with my dad when I was younger.
 With the masks I want to make the thing that shows up in the mask clearly stand out from the rest but be composed of the same elements. Ideas I have to make the mask stand out at the moment are; change the angle, change opacity, change colour, make the elements bigger.
 
@@ -9,3 +10,7 @@ I have loaded two of my photos into the project folder and I have started messin
 
 To try and achieve this at the moment I have created a function with multiple bezier curves seperated by 20 pixels each. And then have made them go in random places on the canvas 2000 times. Moving forward I want to seperate the beziers by a smaller amount and add more curves like a brush would have.
 I also want to use multiples layers of 'paint' so that it is easier to make out what the picture is and to add more interest/detail.
+
+10pm
+I added a gray part of my mask too try and add in the detail off the leaves for input_1. I need to add in a similar mask for input_3 too add in the detail of it's background.
+I made the brush strokes, hair thickness come out randomly to give the render a more organic feel.
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -22,87 +22,120 @@ function setup() {
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-const x_steps = 15
-const y_steps = 15
+const x_steps = 10
+const y_steps = 10
 // const tileheight = 20
 // const tilewidth = 20
 
 function draw() {
-for (let a = 0; a < 50000; a++) {
-  let x = floor(random(sourceImg.width));
-  let y = floor(random(sourceImg.width));
-  let mask = maskImg.get(x, y)
-  let pix = sourceImg.get(x, y);
-  fill(pix)
-  noStroke()
-  watercolour(x,y,5,5)
 
+  for (var x1 = 0; x1 < sourceImg.width; x1 = x1 + x_steps) {
+    for (var y1 = 0; y1 < sourceImg.height; y1 = y1 + y_steps){;
+    let mask = maskImg.get(x1, y1)
+    let pix = sourceImg.get(x1, y1);
+    if (mask[0] > 180) {
+    if (pix[0] > 30 && pix[0] < 110 && pix[1] >0 && pix[1] < 100 && pix[2] > 0 && pix[2] < 100) {
+      let pixMode = sourceImg.get(x1, y1)
+      pixMode[0] = random(56,80)
+      pixMode[1] = random(45,90)
+      pixMode[2] = random(36,70)
+
+      noFill()
+      watercolour(x1, y1, 10, 10)
+      strokeWeight(2)
+      stroke(pixMode);
+      noFill()
+    }
+  }
 }
-  for (let i = 0; i < 10000; i++) {
+}
+    for (let b = 0; b < 10000; b++) {
+      let x2 = floor(random(sourceImg.width));
+      let y2 = floor(random(sourceImg.width));
+      let pix2 = sourceImg.get(x2, y2);
+      let mask2 = maskImg.get(x2, y2);
+      stroke(pix2);
+      fill(pix2)
+      if (mask2[0] > 0){
+      }else{
+      brushstroke1(x2, y2, 1, random(9,12), 30, random(0.3,0.7));
+      brushstroke2(x2, y2, 1, random(9,12), 120, random(0.3,0.7));
+    }
+  }
+//
+  for (let c = 0; c < 50000; c++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.width));
     let mask = maskImg.get(x, y)
     let pix = sourceImg.get(x, y);
-
-  if (mask[0] > 120) {
-    noStroke()
     fill(pix)
-    watercolour(x,y,10,10)
-    strokeWeight(2)
-      stroke(pix);
-      noFill()
-    } else {
-      stroke(pix);
-      fill(pix)
-      // let tilewidth=5
-      // let tileheight=5
-      brushstroke1(x,y,1 ,10,30,0.5);
-      brushstroke2(x,y,1,10,120,0.5);
-      // for (var x = 0; x < sourceImg.width; x = x + x_steps) {
-      //   for (var y = 0; y < sourceImg.height; y = y + y_steps);
-      // let x = floor(random(sourceImg.width));
-      // let y = floor(random(sourceImg.height));
+    noStroke()
+    if(mask[0]>10){
+    watercolour(x, y, 5, 5)
+  }
+  }
+
+  for (let a = 0; a < 80000; a++) {
+    let x3 = floor(random(sourceImg.width));
+    let y3 = floor(random(sourceImg.width));
+    let mask1 = maskImg.get(x3, y3)
+    let pix1 = sourceImg.get(x3, y3);
+    fill(pix1)
+    noStroke()
+    if(mask1[0] >90 && mask1[0]<100){
+    watercolour(x3, y3, 5, 5)
+  }
+}
+
+
+    // for(let b=0; b<10; b++){
+
+// }
+    renderCounter = renderCounter + 1;
+    if (renderCounter > 10) {
+      console.log("Done!")
+      noLoop();
+      // uncomment this to save the result
+      // saveArtworkImage(outputFile);
     }
   }
-  renderCounter = renderCounter + 1;
-  if (renderCounter > 10) {
-    console.log("Done!")
-    noLoop();
-    // uncomment this to save the result
-    // saveArtworkImage(outputFile);
-  }
-}
-function watercolour(x,y,w,h){ ///not really water colour et
-rect(x,y,w,h)
-}
-function brushstroke1(x,y,nstrokes,brushWidth,rotation,hairThickness){
-  for(let c=0; c<brushWidth*nstrokes; c=c+brushWidth){
-  push()
-  strokeWeight(hairThickness)
-  translate(x,y-20)
-  rotate(rotation)
-  scale(1)
-  translate(0,c)
-  bezier(85, 20, 40, 40, 50, 50, 15, 60);
-  pop()
-}
-}
-function brushstroke2(x,y,nstrokes,brushWidth,rotation,hairThickness){
 
-  for(let c=0; c<brushWidth*nstrokes; c=c+brushWidth){
-  push()
-  strokeWeight(hairThickness)
-  translate(x,y-20)
-  rotate(rotation)
-  scale(1)
-  translate(0,c)
-  bezier(85, 20, 40, 40, 50, 50, 15, 60);
-  pop()
-}
-}
+  function watercolour(x, y, w, h) { ///not really water colour et
 
-function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
+    rect(x, y, w, h)
+
+
   }
-}
+
+  function brushstroke1(x, y, nstrokes, brushWidth, rotation, hairThickness) {
+    for (let c = 0; c < brushWidth * nstrokes; c = c + brushWidth) {
+      push()
+      strokeWeight(hairThickness)
+      translate(x, y - 20)
+      rotate(rotation)
+      scale(1)
+      translate(0, c)
+      bezier(85, 20, 40, 40, 50, 50, 15, 60);
+      pop()
+    }
+  }
+
+  function brushstroke2(x, y, nstrokes, brushWidth, rotation, hairThickness) {
+
+    for (let c = 0; c < brushWidth * nstrokes; c = c + brushWidth) {
+      push()
+      strokeWeight(hairThickness)
+      translate(x, y - 20)
+      rotate(rotation)
+      scale(1)
+      translate(0, c)
+      bezier(85, 20, 40, 40, 50, 50, 15, 60);
+      pop()
+    }
+  }
+
+  function keyTyped() {
+    if (key == '!') {
+      saveBlocksImages();
+    }
+  }
