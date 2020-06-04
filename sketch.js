c4e2282d938a9f957d3c43620ed8b2798c984914
Jamie Gambles <gamblejami@myvuw.ac.nz>
2020-06-05 01:14:30 +1200

    Abstraction and Parallax

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
+let sourceFile = "input_1.jpg";
 let maskFile = "mask_1.png";
 let outputFile = "output_1.png";
 
@@ -17,7 +17,7 @@ function setup() {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  background(255);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -29,26 +29,40 @@ let tileheight = 10
 
 function draw() {
 
-beginShape()
   for (var x = 0; x < sourceImg.width; x = x + tilewidth) {
+    beginShape()
     for (var y = 0; y < sourceImg.height; y = y + tileheight) {
 
 let pix = sourceImg.get(x, y)
 console.log(pix)
-strokeWeight(1)
-stroke(pix[0])
-noFill()
-      vertex(x - pix[0]/10, y)
+strokeWeight(2)
+stroke(255, 50)
+fill(pix)
+      vertex(x + (960-x)/(pix[0]/10), y + (320-y)/(pix[0]/10))
     }
+    endShape()
   }
-  endShape()
+    for (var y = 0; y < sourceImg.height; y = y + tileheight) {
+
+    beginShape()
+  for (var x = 0; x < sourceImg.width; x = x + tilewidth) {
+
+let pix = sourceImg.get(x, y)
+console.log(pix)
+strokeWeight(2)
+stroke(255,50)
+noFill()
+      vertex(x + (960-x)/(pix[0]/10), y + (320-y)/(pix[0]/10))
+    }
+    endShape()
+  }
 
   renderCounter = renderCounter + 1;
   if (renderCounter > 15) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
