c5ba790753a8c4e6843e84d007ca5ac91336ac26
Rosa <rosamac31@yahoo.co.nz>
2020-06-05 17:14:03 +1200

    water colour and brush effect

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  rectMode(CENTER);
   angleMode(DEGREES);
 //  noStroke();
   background(164,179,198);
@@ -26,22 +25,34 @@ function setup () {
 }
 
 function draw () {
-  for (let i = 0; i < 1500; i++) {
-   let x = floor(random(sourceImg.width));
-   let y = floor(random(sourceImg.height));
-   let pix = sourceImg.get(x, y);
-   let mask = maskImg.get(x, y);
-   fill(pix);
-   stroke(pix);
+ for (let i = 0; i < 1500; i++) {
+  let x = floor(random(sourceImg.width));
+  let y = floor(random(sourceImg.height));
+  let pix = sourceImg.get(x, y);
+  let mask = maskImg.get(x, y);
+  fill(pix);
+  stroke(pix);
 
-   if (mask[0] > 128) {
-     drawPaint(x,y,10);
-   }
-   else {
-     let pointSize = 15;
-     rect(x, y, pointSize, pointSize);
+  if (mask[0] < 128) {
+     let pointSize = 45;
+     noStroke();
+     fill(pix[0],pix[1],pix[2],20);
+     ellipse(x, y, pointSize*2, pointSize);
    }
  }
+ for (let i = 0; i < 3000; i++) {
+  let x = floor(random(sourceImg.width));
+  let y = floor(random(sourceImg.height));
+  let pix = sourceImg.get(x, y);
+  let mask = maskImg.get(x, y);
+  fill(pix);
+  stroke(pix);
+
+  if (mask[0] > 128) {
+    drawPaint(x,y,8);
+  }
+}
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
@@ -51,13 +62,18 @@ function draw () {
   }
 }
 
+
 function drawPaint(x,y,size){
   push();
-  strokeWeight(8);
+  strokeWeight(6);
   translate(x,y);
-  for(var i = 0; i < 3; i++){
-    line(size,0,-size,0);
-      rotate(50);
+  for(var i = 0; i < 5; i++){
+    if(i==1 || i==3 || i==5){
+      line(0+i*4,0,size+i*4,size);
+    }
+    else{
+    line(0+i*3,7,size+i*3,size+7);
+  }
     }
   pop();
 
