6244ff55ae8b66a99fdaacb7690c6ae0d122622f
WishCotton <52726327+WishCotton@users.noreply.github.com>
2020-06-05 14:19:24 +1200

    Grid Test

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
+let i = 0;
+
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -18,36 +20,68 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255,0,0);
+
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+const tileHeight = 7;
+const tileWidth = 7;
+
 function draw () {
-  for(let i=0;i<10000;i++) {
+background(i,0,0);
+  for(var x = 0; x < sourceImg.width; x = x + tileWidth){
+    for(var y = 0; y< sourceImg.height; y = y + tileHeight){
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
+      if(mask[0] > 128) {
+        i=0;
+        let pointSize = 5;
+        ellipse(x, y, tileWidth, tileHeight);
+      }
+      else {
+        i = 200;
+        stroke(pix);
+        strokeWeight(3);
+        let pointSize = 5;
+        //rect(x, y, pointSize, pointSize);
+
+      line(x,y,x+5,y+5);
+      line(x,y,x-5,y-5);
+
+      }
+    }
+  }
+  /*for(let i=0;i<10000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
+
     if(mask[0] > 128) {
       let pointSize = 5;
       ellipse(x, y, pointSize, pointSize);
     }
     else {
-      stroke(pix);
-      let pointSize = 10;
+  stroke(pix);
+      let pointSize = 5;
       //rect(x, y, pointSize, pointSize);
-      line(x,y,x+5,y+5);
-      line(x,y,x-5,y-5);
-    }
-  }
+
+      line(x,y,x+30,y);
+      //line(x,y,x-5,y-5);
+
+
+    }*/
+
+
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     saveArtworkImage(outputFile);
   }
 }
 
