fd6c981fc9ce99cf6aa43c7e2e907abac0c901e2
wrightconn <connorwright247@gmail.com>
2020-06-06 14:26:48 +1200

    LAYERING MY CODE

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-REFINING MY FILTER TO MY THEME
+STRUCTURING AND LAYERING MY FILTER
 
-Today I swapped out my middle image so that my series and theme was a bit more clear and cohesive. With my theme highlighting the pressure people feel on how they should look due to our societies beauty standards. My series represents how people feel angry and upset when they look at themselves and how people are pressured to look a certain way feeling like they are stuck behind a wall/mask and trying to break through to reach out to the other side where they can be free.
-
-I changed my filters to focus more on my theme. I went back on my first images mask and added in more detail by masking the hair in grayscale. I will need to go back and adjust my other 2 masks. Since my theme is based around self-image with society's beauty standards I wanted to have good contrast with sharp shapes and soft shapes. The background is made out of triangles in a grid format to look like shards of glass, and to represent society's harsh standards and expectations. Then I have her face and hair as dots and arcs to show a soft and delicate individual. The waves in the hair are all over the place so I need to work on structuring that a bit more. 
+Today I further developed the masks for all my photos. I ended up layering my code so that I could cover up any parts that bleed out of there masks. In my last commit, I was using the pixels in a grid and the pointillism code however in order to have my filters to looked more structured I decided to just use the pixels in a grid formation. This overall looks a lot cleaner and works with all 3 photos. Next, I want to add in some more detail that relates to my model feeling upset and angry maybe coding an upset face, this is something I will experiment with next.
--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ const y_step = 5;
 
 // DARWING PATTERN AND DESIGN
 function draw() {
-
+  // BACKGROUND
   for (var x = 0; x < sourceImg.width; x = x + x_step) {
     for (var y = 0; y < sourceImg.height; y = y + y_step) {
       let pix = sourceImg.get(x, y);
@@ -41,38 +41,65 @@ function draw() {
         //fill(pix) //(pix[0], pix[1], pix[2], 80);
         noFill()
         strokeWeight(2)
+        //stroke(pix)
         stroke(pix[0], pix[1], pix[2], 80)
         triangle(x, y, x + tileWidth * 4, y + tileHeight * 4, x - tileWidth * 5, y + tileHeight * 3);
-        //rect(x, y, tileWidth, tileHeight); // THIS COVERS UP ANY WHITE LINES FROM MASK
-      // WHITE MASK (GIRL) ACTS AS BACKGROUND COLOUR
-    } else {
+      // (GIRL) THIS IS THE COLOUR OF THE GIRL TO HIDE ANY WHITE LINES FROM THE MASKS
+    }else {
         noStroke()
         fill(pix[0], pix[1], pix[2], 50);
         rect(x, y, tileWidth, tileHeight);
       }
     }
   }
-
-// GREY AND WHITE MASK
-  for (let i = 0; i < 9000; i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    // WHITE MASK (DOTS)
-    if (mask[0] > 200) {
-      let pointSize = 4;
-      fill(pix);
-      noStroke();
-      ellipse(x, y, pointSize, pointSize);
-      // GREY MAKS (HAIR WAVES)
-    } else if (mask[0] >= 180 && mask[0] < 200) {
-      let pointSize2 = 20;
-      drawWaves(x, y, 20, 20, pix);
+  // HAIR
+  for (var x = 0; x < sourceImg.width; x = x + x_step) {
+    for (var y = 0; y < sourceImg.height; y = y + y_step) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      // GREY MASK (HAIR)
+      if (mask[0] == 195) {
+        drawWaves(x, y, 15, 15, pix);
+      }
     }
-
   }
+  // SKIN
+  for (var x = 0; x < sourceImg.width; x = x + x_step) {
+    for (var y = 0; y < sourceImg.height; y = y + y_step) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      // WHITE MASK (SKIN)
+      if (mask[0] > 200) {
+        stroke(pix);
+        fill(pix);
+        strokeWeight(1);
+        ellipse(x, y, tileWidth-0.5, tileHeight-0.5);
+        ellipse(x + 3, y + 3, tileWidth-0.5, tileHeight-0.5);
+      }
+    }
+  }
+
+  //
+  // // GREY AND WHITE MASK
+  //   for (let i = 0; i < 9000; i++) {
+  //     let x = floor(random(sourceImg.width));
+  //     let y = floor(random(sourceImg.height));
+  //     let pix = sourceImg.get(x, y);
+  //     let mask = maskImg.get(x, y);
+  //     fill(pix);
+  //     // WHITE MASK (DOTS)
+  //     if (mask[0] > 200) {
+  //       let pointSize = 4;
+  //       fill(pix);
+  //       noStroke();
+  //       ellipse(x, y, pointSize, pointSize);
+  //       // GREY MAKS (HAIR WAVES)
+  //     } else if (mask[0] >= 180 && mask[0] < 200) {
+  //       let pointSize2 = 20;
+  //       drawWaves(x, y, 20, 20, pix);
+  //     }
+  //
+  //   }
 
   renderCounter = renderCounter + 1;
   if (renderCounter > 10) {
@@ -99,7 +126,7 @@ function drawWaves(x, y, sizeW, sizeH, c) {
   push();
   angleMode(DEGREES);
   stroke(c)
-  strokeWeight(3)
+  strokeWeight(2)
   noFill()
   arc(x, y, sizeW, sizeH, 0, 90);
   arc(x + 30, y - 20, sizeW, sizeH, 0, 90);
