b806ef87001eb999853738ecffca8f32aca8742e
Katebbb <50813573+Katebbb@users.noreply.github.com>
2020-06-06 23:09:38 +1200

    new concept

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Project updates for Karen O'Brien
 
-In this test I played around with whether or not I want to make my images look like they've been needle painted or embroidered. Since cross stitch is very organized I decided that this code is a bit too random for it and I'm going to experiment with a needle painted embroidery look instead.
+Last night I had a sudden idea to completely change my idea from embroidery themed to a thermal image theme. With this, I'm also changing this project from a more personal based image series to a social/ current issue series. Not only do I feel that the bright colours of a thermal coloured image will be significantly more effective and eyecatching as art on a billboard in the middle of the CBD, but I think that I will be able to create a much more interesting and meaningful message.
 
-For this project I want to take inspiration from both my own and many of my family member's passion for embroidery by taking photos of places/ objects of significance to my family and making them look like pieces of embroidery or cross stitch. To do this I plan to block out each object in different colours then apply an overlay that makes the image look as if it has been embroidered.
-
-Some examples of images I am thinking of using are of flowers from my Nanna's garden and backyard, although I'm worried that I won't be able to portray the message I'm going for so I need to make sure that all the images I use have a strong sense of nostalgia. Currently I've put in some temporary photos and masks of some pictures I took while abroad in Europe a few years ago, while these are a bit further from what I want the final images to be, they still have significance to my family as these are some of my Grandparents (and my own) favourite places in the world.
+As New Zealand recovers from the Covid-19 pandemic, I want to use this project to remind Wellington of how quickly diseases can spread and the importance of social distancing. In my image series I will be using the warmest colours of bright red, orange and yellow to represent a person infected with Covid-19, contrasting with the blue shades of the background. 
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -24,25 +24,65 @@ function setup () {
 }
 
 function draw () {
-  for(let i=0;i<3000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-      let pointSize = 2;
-      stroke(pix);
-      strokeWeight(pointSize);
-      line(x,y,x-5,y+5);
-      line(x-5,y-5,x+5,y+5);
+  var count = 0;
+  for(let i=0;i<width;i++) {
+    for(let r=0;r<height;r++){
+      let pix = sourceImg.get(i, r);
+      let mask = maskImg.get(i, r);
+      fill(pix);
+
+      if(mask[0] >= 0 && mask[0] < 50){
+        //blue
+        var blue = map(mask[0],0,50,255,61);
+        fill(0,0,blue);
+        rect(i,r,2);
+
+      }else if(mask[0] >= 51 && mask[0] < 100){
+        //green
+        var green = map(mask[0],51,100,125,222);
+        var blue = map(mask[0],51,100,29,52);
+        fill(0,green,blue);
+        rect(i,r,2);
+
+      }else if(mask[0] >= 101 && mask[0] < 150){
+        //yellow
+        var red = map(mask[0],101,150,155,242);
+        var green = map(mask[0],101,150,163,255);
+        fill(red,green,0);
+        rect(i,r,2);
+
+      }else if(mask[0] >= 151 && mask[0] < 200){
+        //orange
+        var red = map(mask[0],151,200,194,255);
+        var green = map(mask[0],151,200,116,153);
+        fill(red,green,0);
+        rect(i,r,2);
+
+      }
+      else if(mask[0] >= 201 && mask[0] < 255){
+        //red
+        var red = map(mask[0],201,255,196,255);
+        var green = map(mask[0],201,255,7,8);
+        fill(red,green,0);
+        rect(i,r,2);
+
+      }
+      else {
+        fill(0,0,0);
+        rect(i,r,2);
+      }
+
+    }
   }
   renderCounter = renderCounter + 1;
+  console.log(count);
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
      saveArtworkImage(outputFile);
   }
+   noLoop();
 }
 
 function keyTyped() {
