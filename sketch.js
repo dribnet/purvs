e828006ad8607f7345cf304906f4c0ff287e5424
WishCotton <52726327+WishCotton@users.noreply.github.com>
2020-06-06 15:23:40 +1200

    bug fixes and new look

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
-let i = 0;
+let i = 200;
 
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -17,7 +17,7 @@ function preload() {
 function setup () {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
-
+  background(i,0,0);
   imageMode(CENTER);
   noStroke();
 
@@ -25,26 +25,26 @@ function setup () {
   maskImg.loadPixels();
 }
 
-const tileHeight = 7;
-const tileWidth = 7;
+const tileHeight = 5;
+const tileWidth = 5;
 
 function draw () {
-background(i,0,0);
+
   for(var x = 0; x < sourceImg.width; x = x + tileWidth){
     for(var y = 0; y< sourceImg.height; y = y + tileHeight){
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
       fill(pix);
-      if(mask[0] > 100) {
-        i=0;
+      if(mask[0] > 140) {
+
         let pointSize = 5;
         ellipse(x, y, tileWidth, tileHeight);
       }
-      if(mask[0]>0 && mask[0]<110){
-        i=100;
-
+      if(mask[0]>10 && mask[0]<140){
+        stroke(0);
+        point(x,y);
       }
-      else {
+      if(mask[0]<20) {
         i = 200;
         stroke(pix);
         strokeWeight(3);
