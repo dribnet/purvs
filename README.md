89f38f8966f7c6d4c974d62a28bac417dff87250
Gandorth <61823132+Gandorth@users.noreply.github.com>
2020-06-06 17:27:49 +1200

    Knock

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-6/6/2020, 14:05
+6/6/2020, 17:30
 
-Stamps.
+Grid.
 
-More progressive experimentation, more of an idea behind potential experiments.
+I quite like where this is going, I think it has potential to be something pretty deep and charismatic. For now, I am merely going to commit this as more progression reference.
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.png";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_3.png";
+let maskFile   = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -23,30 +23,50 @@ function setup () {
   maskImg.loadPixels();
 }
 
+const tileHeight = 10;
+const tileWidth = 10;
+
+const x_step = 15;
+const y_step = 25;
+
 function draw () {
 
-  for(let i = 0; i < 3000; i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    stroke(pix);
-    console.log(mask);
-
-    let pointSize = (10);
-    let dice = random(1, 6);
-    if (mask[0] > 128) {
-     //if (dice > 4){
-     line(x, y, x + pointSize - 20, y); //Wind
-
-   } else {
-     if (dice > 5){
-       line(x, y, x + pointSize - 20, y); //Wind
-     } else {
-       line(x, y, x, y + pointSize); //Rain
-    }
+  for(var x = 0; x < sourceImg.width; x = x+ x_step){
+    for(var y = 0; y < sourceImg.height; y = y+ y_step){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+        fill(pix);
+        stroke(pix);
+         if (mask[0] > 128) {
+        rect(x,y,tileWidth,tileHeight);
   }
+  else{
+    rect(x,y,x_step,y_step);
+  }
+}
+
+
+  //for(let i = 0; i < 3000; i++) {
+  //  let x = floor(random(sourceImg.width));
+  //  let y = floor(random(sourceImg.height));
+  //  let pix = sourceImg.get(x, y);
+  //  let mask = maskImg.get(x, y);
+  //  fill(pix);
+  //  stroke(pix);
+
+  //  let pointSize = (10);
+    //let dice = random(1, 6);
+  //  if (mask[0] > 128) {
+     //if (dice > 4){
+    // line(x, y, x + pointSize - 20, y); //Wind
+
+  // } else {
+  //   if (dice > 5){
+    //   line(x, y, x + pointSize - 20, y); //Wind
+  //   } else {
+  //     line(x, y, x, y + pointSize); //Rain
+  //  }
+//  }
 
     //if(mask[0] > 128) {
     //  let pointSize = 50;
@@ -62,7 +82,7 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     //saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
 }
 
