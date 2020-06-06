aec337e03777f4292983b949c1608bb73df4efc3
doroxv <>
2020-06-06 20:32:37 +1200

    added sis baby photo and white scratch marks

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,27 +18,57 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
+const tileWidth = 3;
+const tileHeight = 3;
+
 function draw () {
-  for(let i=0;i<2000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);    
-    }
+  colorMode(HSB,250);
+  for(let y = 0; y<height; y= y + tileHeight){
+      for(let x = 0; x<width; x= x + tileHeight){
+        let pix = sourceImg.get(x, y);
+        let mask = maskImg.get(x, y);
+
+        // black and white filter
+        let darkest = color(36, 4, 30);
+        let lightest = color(40,0,100);
+
+        let lightness = map(pix[1], 0, 200, 0, 1);
+
+        let b_w = lerpColor(darkest, lightest, lightness);
+
+        fill(b_w);
+        if(mask[0] > 128) {
+         rect(x, y, tileHeight, tileWidth);
+        }
+        else if(mask[0] < 128 && mask[0] > 56){
+          fill(250);
+          rect(x,y, tileHeight,tileHeight);
+        }
+        else{
+          fill(250);
+          rect(x,y, tileHeight,tileHeight);
+        }
+
+      }
   }
+
+ //white scratches to make an "old filter"
+
+  for(let i=0;i<100;i++) {
+     let x = floor(random(sourceImg.width));
+     let y = floor(random(sourceImg.height));
+     let mask = maskImg.get(x, y);
+
+     if(mask[0] > 128) {
+       fill(250);
+       rect(x, y, tileHeight, tileHeight);
+     }
+}
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
