23bcf6660d235f8c538bb4aeb3d4f294775f2dee
Kayla Palmer <56278807+straycatss@users.noreply.github.com>
2020-06-07 19:03:35 +1200

    added new mask layer

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(171, 187, 209);
+  background(51, 45, 45);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -27,52 +27,34 @@ function draw () {
 
   let tilewidth  =6;
   let tileheight = 6;
-  let squarew =5;
-  let squareh = 5;
+  let squarew =4.5;
+  let squareh = 6;
   strokeWeight(1.5);
   for(var xtile = 0; xtile < sourceImg.width; xtile= xtile+tilewidth){
     for(var ytile = 0; ytile < sourceImg.height;  ytile= ytile+tileheight){
     let pix = sourceImg.get(xtile, ytile);
     let mask = maskImg.get(xtile, ytile);
-    let strokepix = sourceImg.get(xtile, ytile);
-    strokepix[0] = strokepix[0]/2;
-    //stroke (strokepix);
-
-    if (mask[0] <128){
-      if(pix[0]<100 &&pix[1]<90&&pix[2]<90){
-        pix[0] = pix[0]*9;
-        pix[1] = pix[1]*7;
-        pix[2] = pix[2]*10;
-        if (pix [0]>=255&&pix[1]>=255&&pix[2]>=255){
-          pix[0]= 207;
-          pix [1]=  171;
-          pix[2]= 209;
-        }
-      }
-      fill (pix);
+    if (mask[0] <120){
+      fill(pix);
       rect(xtile,ytile,squarew,squareh);
-    } else{
-      noStroke();
-      var pixmod = sourceImg.get(xtile, ytile);
-      pixmod[0] = pixmod[0]*6;
-      pixmod[1] = pixmod[1]/3;
-      pixmod[2] = pixmod[1]/2;
-      fill(pixmod);
+    } if(mask[0]==120){
+      fill(pix);
+  rect(xtile,ytile,tilewidth,tileheight);
+    }if(mask[0]>120){
+      let pixMod = sourceImg.get(xtile, ytile);
+      pixMod[0]=pixMod[0]*1.2;
+      pixMod[1]=pixMod[1]/4;
+      pixMod[2]=pixMod[2]/4;
+      fill(pixMod);
       rect(xtile,ytile,tilewidth,tileheight);
-      var rand = random(1,6);
+      var rand = random(1,10);
       if(rand<2){
         fill(255);
         circle (xtile,ytile,5);
-      } else{
+      }
     }
-    }
-
-
 }
 }
-
-
-
 
   renderCounter = renderCounter + 1;
   if(renderCounter > 1) {
