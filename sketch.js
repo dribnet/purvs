161bd8198c2af38a27bb4340217b6fc969021047
Phoebe Zeller <phoebe.zeller@hotmail.com>
2020-06-07 21:24:15 +1200

    jitterbug and mask change

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ const x_step = 5;
 const y_step = 5;
 
 function draw() {
-
+  let starcolour = color(200, 100, 100);
   for (let i = 0; i < 3000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -42,40 +42,44 @@ function draw() {
 
     let pointSize = 90;
     let dice = random(1, 6);
+    //console.log(mask[0]);
+    if (mask[0] > 200) {
 
-    if (mask[0] > 128) {
-      line(x, y, x + pointSize, y); // horizonal "windy
-    } else {
-      if (dice > 5) {
-        line(x, y, x + pointSize, y); // horizonal "windy"
-      } else {
-        line(x, y, x, y + pointSize); // vertical "rainy"
-
+      fill(pix);
+      //drawMover(x,y);
+      if (dice < 5) {
+        drawMover(x, y);
       }
+    } else if (mask[0] == 119) {
+
     }
   }
 
-  for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
-  for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
-    let pix = sourceImg.get(x2, y2);
-    let mask = maskImg.get(x2, y2);
-     fill(pix[0],pix[1],pix[2]);
-     noStroke();
-      if (mask[0] > 128) {
-    rect(x2, y2, tileWidth, tileHeight);
-  }
-    //drawStar(x,y,10,pix);
-  }
-}
 
   renderCounter = renderCounter + 1;
-  if (renderCounter > 5) {
+  if (renderCounter > 1) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
+
+function drawMover(x, y) {
+  let curX = x;
+  let curY = y;
+  stroke(0);
+  strokeWeight(0.5);
+
+  for (var i = 0; i < 30; i++) {
+    ellipse(curX, curY, 10, 10);
+
+    curX = curX + random(-15, 10);
+    curY = curY + random(-10, 10);
+  }
+}
+
+
 
 function drawStar(x, y, size, c) {
   stroke(c);
