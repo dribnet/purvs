8b07319b3c795c3c874e09d591757eb8fea98649
Hannah Loma <loma.hannah15@gmail.com>
2020-06-08 02:58:24 +1200

    Final Update

--- sketch.js
+++ sketch.js
@@ -21,11 +21,10 @@ function setup() {
 
   imageMode(CENTER);
   noStroke();
-  background(232, 189, 242); //purple
+  background(232, 189, 242); //light purple
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-
 
 function draw() {
   for (let i = 0; i < 5000; i++) {
@@ -35,12 +34,11 @@ function draw() {
     let mask = maskImg.get(x, y);
 
 
-
     if (mask[0] >= 0 && mask[0] < 201) { //Black mask - background
       let pointSize = 15;
       push();
       strokeWeight(12);
-      stroke(pix[0] + 50, pix[1] + 25, pix[2] + 20, 50);
+      stroke(pix[0] + 50, pix[1] + 25, pix[2] + 20, 10);
       line(x, y, x + pointSize - 10, y + 10);
       pop();
     } else if (mask[0] >= 201 && mask[0] < 255) { //Grey - outline of leaves
@@ -50,22 +48,23 @@ function draw() {
       stroke(pix[0] + 70, pix[1] + 70, pix[2] + 70);
       strokeWeight(3);
       arc(x, y, pointSize, pointSize, HALF_PI, PI);
-      line(x, y, x + pointSize - 10, y + 10);
       pop();
     } else { //white - details inside leaves
       let pointSize = 10;
       push();
       noFill();
-      stroke(pix[0] + 90, pix[1] + 70, pix[2] + 60);
+      stroke(pix[0] + 90, pix[1] + 60, pix[2] + 60);
       strokeWeight(5);
       arc(x, y, pointSize, pointSize, HALF_PI, PI);
       pop();
       push();
-      line(x, y, x + pointSize - 10, y + 10);
+      line(x, y, x + pointSize - 10, y + 10); //adds more colour depth
       pop();
+      drawBlock(x, y, 3, pix); //adds depth and makes leaves look more detailed
     }
   }
 
+  //Layer 2
   //Code of background mask & sub detail(grey mask) mask details
   for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
     for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
@@ -84,22 +83,26 @@ function draw() {
         ellipse(x2, y2, tileWidth, tileHeight); //circle detail
       } else if (mask[0] >= 201 && mask[0] < 250) { //Grey - outline of leaves
         push();
-        var tileHeight = 4;
-        var tileWidth = 16;
-        var x_step = 10;
+        var tileHeight = 5;
+        var tileWidth = 18;
+        var x_step = 9;
         var y_step = 10;
         fill(pix[0] + 100, pix[1] + 100, pix[2] + 250, 190);
         ellipse(x2, y2, tileWidth, tileHeight); //line detail
         pop();
       }
-
     }
   }
 
-
+  function drawBlock(x, y, size, pixelCol) { //For small additional detail to leaves
+    fill(pixelCol);
+    push();
+    rect(x, y, size, 3);
+    pop();
+  }
 
   renderCounter = renderCounter + 1;
-  if (renderCounter > 30) {
+  if (renderCounter > 28) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
