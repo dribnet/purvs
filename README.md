a952948a02fb4781ad0ed57b88009aa569712831
deiiirdre <52726131+deiiirdre@users.noreply.github.com>
2020-06-08 19:15:29 +1200

    08/06
    Try to fix the render page. Done the mask pictures.

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 I choose the theme is Personal and I want to share my unforgettable scenes when I travel. These three photos are my favorite photos. Also, these scenes impressed me so hard to forget. I edited the size of the photos and start to do the masks.
 Try to edit the sketch.js, but meet some problems. Index.html doesn't change when I change the sketch.js. Confusing. I will figure out tomorrow.
 
+08/06/2020
+Try to fix the render page. Done the mask pictures.
+
 The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
--- sketch.js
+++ sketch.js
@@ -17,42 +17,34 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  noStroke();
-  background(255);
+  //noStroke();
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for (let y = 0; y < 800; y+=5) {
-    for (let x = 0; x < 1920; x+=5) {
+  for (let i=0; i<2000; i++) {
+      let x = floor(random(sourceImg.width));
+      let y = floor(random(sourceImg.height));
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
+
       fill(pix);
       stroke(pix);
       console.log(mask);
       let pointSize = 40;
       line(x,y,x, y+pointSize);
-    //  if(mask[0] > 128) {
-    //    noStroke();
-    //    if (pix[0] > 175 && pix[1] > 175 && pix[2] > 175) {
-    //      rect(x+random(-5,5), y+random(-5,5), 15, 15);
-    //    }
-    //    else {
-    //    ellipse(x+random(-5,5), y+random(-5,5), 25, 25);
-    //    }
-    //  }
-    //  else if(mask[0] > 40) {  //mask is grey
-    //    noStroke();
-    //    ellipse(x+random(-1,1), y+random(-1,1), 5, 5);
-    //    ellipse(x+random(-2,2), y+random(-2,2), 5, 5);
-    //  }
-    //  else {
-    //    stroke(pix);
-    //    strokeWeight(random(1,5));
-    //    line(x - random(0, 10), y + random(-10,10), x + random(0, 10), y + random (-10,10));
-    //  }
-    }
+
+      if(mask[0] > 128) {
+         noStroke();
+         ellipse(x, y, pointSize, pointSize);
+       }
+      else {
+         let pointSize = 10;
+         rect(x, y, pointSize, pointSize);
+      }
+
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
@@ -61,7 +53,6 @@ function draw () {
     // uncomment this to save the result
     //saveArtworkImage(outputFile);
   }
-
 }
 
 function keyTyped() {
