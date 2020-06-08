b1acb1c2e2bfe69ec5af5a9a0ad1248df98a7831
Madisyn-0 <61666971+Madisyn-0@users.noreply.github.com>
2020-06-08 14:52:16 +1200

    Refined design from feedback

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Creative Coding 2: Custom Pixel
 
+After receiving feedback, I have tried to focus on making the face and eyes of the cows more prominent rather than the stars. I decreased the size of the stars and got rid of the for loops that were drawing the ellipses on top as I found that this was not working anymore with my design. To make the eyes more prominent, I first created a second shade of grey in the mask to draw rects for the eyes as I wanted to try and have it more detailed. I then changed the colour values to be white because they were still not standing out as the original colours. 
+
 Theme:
-I have chosen farming as my theme for this project as it is something personal to me that I care about. Farming gets a lot of bad media that does not express what farming is really about. Therefore, I took into consideration the location of where the work would be displayed as most people who may view it will have a limited experience with farming. I want to express the beauty of farming and contrast the bad pictures that are presented in the media. 
+I have chosen farming as my theme for this project as it is something personal to me that I care about. Farming gets a lot of bad media that does not express what farming is really about. Therefore, I took into consideration the location of where the work would be displayed as most people who may view it will have a limited experience with farming. I want to express the beauty of farming and contrast the bad pictures that are presented in the media.
 
 The images above are photos from around my farm and the area I live in algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects.
--- sketch.js
+++ sketch.js
@@ -36,12 +36,12 @@ function draw() {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    let r = random(10, 60);
+    let r = random(10, 30);
     fill(pix);
     stroke(pix);
 
     let dice = random(1, 6);
-    if (mask[0] < 120) { //  if mask is black
+    if (mask[0] < 100) { //  if mask is black
       drawStar(x, y, r);
     } else if (mask[0] > 120 && mask[0] < 190) { //   if mask is grey
 
@@ -54,30 +54,34 @@ function draw() {
         strokeWeight(5);
         stroke(pix);
       }
-      
+
       strokeWeight(6);
       if (dice > 3.5) {
         line(x, y, x, y + 12); // horizonal
       } else {
         line(x, y, x + 12, y); // vertical
       }
-    }
 
-  }
-  for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
-    for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
-      let pix = sourceImg.get(x2, y2);
-      let mask = maskImg.get(x2, y2);
-      fill(pix[0], pix[1], pix[2]);
-      noStroke();
-      if (mask[0] > 190) { //mask is white
-        ellipse(x2, y2, tileWidth + 10, tileHeight + 5);
-      }
+    }
+    else if(mask[0] ==110){ //mask is darker grey
+      pix[0] = pix[0]*3;
+      pix[1] = pix[1]*3;
+      pix[2] = pix[2]*3;
+      fill(pix[0], pix[1], pix[2])
+      stroke(pix[0], pix[1], pix[2])
+      rect(x, y, tileWidth/3, tileHeight/3);
+    }
+    if (mask[0] > 190) { //mask is white
+          let pix = sourceImg.get(x, y);
+          let mask = maskImg.get(x, y);
+          fill(pix[0], pix[1], pix[2]);
+          noStroke();
+      ellipse(x, y, tileWidth + 8, tileHeight + 3);
     }
   }
 
   renderCounter = renderCounter + 1;
-  if (renderCounter > 7) {
+  if (renderCounter > 8) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
@@ -88,7 +92,7 @@ function draw() {
 function drawStar(xPos, yPos, size) {
   push();
   translate(xPos, yPos);
-  strokeWeight(3);
+  strokeWeight(2);
   let half = size / 2.5;
   let diagHalf = half * 0.75;
 
