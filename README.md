18f4b1f0322922ec1e9e400e4d60f9487edefadb
doroxv <>
2020-06-08 15:24:28 +1200

    added stains and tweaked photo creases

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel - AGING
 
-I have added my mum's photo to represent an older generation in the theme. Now, I will just tweak the masks and maybe edit the black and white filter. Other than that, I might try and find a way to have a blurry filter but if not I will just  be making the tileWidth and tileHeight size = 1 so that all three photos are clear.
+Now that I'm currently just adding more visual representations of my theme: aging, I focused on making the photos look older in due time. For example, my mums photo should have more wrinkles and creases because it represents the vintage treasury feel to it. I added stains relating to the photo--the first output being of a baby's hand print, and the last output being of a coffee mug stain representing my mum and how she loves her coffee and tea.
 
 (Replace this text with your own which explains the source of your photos, masking, and applied algorithm.)
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "output_3.png";
 
 function preload() {
@@ -28,47 +28,35 @@ const tileHeight = 3;
 
 function draw () {
   colorMode(HSB,250);
-  for(let y = 0; y<height; y= y + tileHeight){
-      for(let x = 0; x<width; x= x + tileHeight){
+  for(let y = 0; y < height; y = y + tileHeight){
+      for(let x = 0; x < width; x = x + tileHeight){
         let pix = sourceImg.get(x, y);
         let mask = maskImg.get(x, y);
 
         // black and white filter
-        let darkest = color(36, 4, 30);
+        let darkest = color(30);
         let lightest = color(40,0,100);
 
-        let lightness = map(pix[1], 0, 200, 0, 1);
+        let brightness = map(pix[1], 0, 200, 0, 1);
 
-        let b_w = lerpColor(darkest, lightest, lightness);
+        let b_w = lerpColor(darkest, lightest, brightness);
 
         fill(b_w);
         if(mask[0] > 128) {
          rect(x, y, tileHeight, tileWidth);
         }
-        else if(mask[0] < 128 && mask[0] > 56){
+        else if(mask[0] < 60 && mask[0] > 0){     // white scratches for that vintage filter
           fill(250);
           rect(x,y, tileHeight,tileHeight);
         }
         else{
-          fill(250);
+          fill(28, 82, 90);
           rect(x,y, tileHeight,tileHeight);
         }
 
       }
+    }
   }
-
- //white scratches to make an "old filter"
-
-  for(let i=0;i<100;i++) {
-     let x = floor(random(sourceImg.width));
-     let y = floor(random(sourceImg.height));
-     let mask = maskImg.get(x, y);
-
-     if(mask[0] > 128) {
-       fill(250);
-       rect(x, y, tileHeight, tileHeight);
-     }
-}
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
     console.log("Done!")
@@ -76,7 +64,7 @@ function draw () {
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
-}
+
 
 function keyTyped() {
   if (key == '!') {
