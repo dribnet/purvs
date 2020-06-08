758270f6323c9c598baa3a46f45eb049dbe16f86
caitlinpiepke <caitlin.piepke@gmail.com>
2020-06-08 13:14:18 +1200

    change

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
+let elementSpacing = 40;
+let squareSize = 40;
+let colorSquareSize = 40;
+
 let sourceImg = null;
 let maskImg = null;
 let renderCounter = 0;
+
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -23,66 +28,42 @@ function setup() {
   maskImg.loadPixels();
 }
 
-const tileWidth = 10;
-const tileHeight = 10;
-
-const xStep = 15;
-const yStep = 15;
+const tileWidth = 30;
+const tileHeight = 20;
 
 
 function draw() {
-
-
-// for(var x = 0; x < sourceImg.width; x = x + xStep){
-//   for(var y = 0; y < sourceImg.height; y = y + yStep){
-//     let pix = sourceImg.get(x, y);
-//     let mask = maskImg.get(x, y);
-//     fill(pix);
-//     stroke(pix);
-    // rect(x, y, tileWidth, tileHeight);
-//   }
-// }
-
-
-
-
-  for (let i = 0; i < 4000; i++) {
+  
+  for(let i=0; i<1000; i++){
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    stroke(pix);
 
-    let pointSize = 20;
-    let dice = random(1, 6);
-
-    if (mask[0] > 120) {
-      line(x, y, x + pointSize, y);
-    } else {
-      if(pix[1] > pix[0]){
-        strokeWeight(3);
-        let pixMod = sourceImg.get(x, y);
-        pixMod[0] = pixMod[0]*3;
-        pixMod[1] = pixMod[1];
-        pixMod[2] = pixMod[2]*1.5;
-
-        stroke(pixMod);
-        //stroke(pix);
-
-      }else{
-        stroke(pix);
-        strokeWeight(1);
-      }
-
-
-      if (dice > 5) {
-        line(x, y, x + pointSize, y); //horizontal windy
-      } else {
-        line(x, y, x, y + pointSize); //vertical rain
-      }
+    if(mask[0] > 128){
+      let pointSize = 20;
+      let pixMod = sourceImg.get(x, y);
+      pixMod[0] = pixMod[0]*2;
+      pixMod[1] = pixMod[1]/3;
+      pixMod[2] = pixMod[2]*3;
+      noStroke();
+      fill(pixMod);
+      ellipse(x, y, pointSize, pointSize);
     }
- }
+
+
+    else{
+          let pointSize = 10;
+          let pixMod = sourceImg.get(x, y);
+          pixMod[0] = pixMod[0]*2;
+          pixMod[1] = pixMod[1];
+          pixMod[2] = pixMod[2]/3;
+          noStroke();
+          fill(pixMod);
+          rect(x, y, tileWidth, tileHeight);
+        }
+      }
 
   //runs 10 different times
   renderCounter = renderCounter + 1;
@@ -97,7 +78,7 @@ function draw() {
 
 function drawStar(x, y, size) {
   push();
-  strokeWeight(4);
+  strokeWeight(1);
   translate(x, y);
   for (var i = 0; i < 10; i++) {
     line(size, 0, -size, 0);
