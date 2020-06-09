c9073dff654fe4e80fb050b17f72a37f3044a7d5
Maxfire2626 <maxfire2626@gmail.com>
2020-06-09 23:46:20 +1200

    first clear theme sorted - friends/fam

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-This version of experimentation involves changing the circles to lines to see what different effects can be created. Still themed as below: preview and thumbnail not updated.
+This version of experimentation involves changing the image into a grid of both square and ellipse pixels dependant on the mask. I've played around with differing sizes and spaces with a few different shape. The current design highlights what I want but more work is needed to better showcase this. There are currently issues with blank space at the boundary which was not the intention.
 
+The theme I am going with at the moment is friends and family as they are an important part of my life and deserve to be captured and showcased. Permission has been granted for use of them in my photographs :)
 
-My initial experiments involve a selection of images from 3 different possible themes I am considering.
-
-The first theme is moments with friends / family.
-The second theme option is interesting architecture / the built environment
-The third option is electronics hardware related.
-
-Currently each image has a custom mask made by me and is processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html)."
-
-The masks for each photo highlight key aspects in each of them.
+Currently each image has a custom mask made by me and is processed with a grid of square pixels for the background and the subject of the photos is shown with ellipses.
--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let renderCounter=0;
 // change these three lines as appropiate
 let sourceFile = "input_3.jpg";
 let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let outputFile = "output_1.png";
 
 function preload() { // what happens before we kick off
   sourceImg = loadImage(sourceFile);
@@ -23,37 +23,88 @@ function setup () { // do not change canvas! - can edit stroke, back colour etc
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<4000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    stroke(pix);
-    if(mask[0] > 128) {
-      let pointSize = 50;
-      let dice = random(1,6);
-      if(dice > 3){
-        line(x, y, x+pointSize, y);
-      }else{
-        line(x, y, x, y+pointSize);
-      }
-      ellipse(x, y, pointSize/10, pointSize/10);
+const tileHeight = 10;
+const tileWidth = 10;
+const tileHeight2 = 50;
+const tileWidth2 = 50;
+const tileHeight3 = 30;
+const tileWidth3 = 30;
 
-    }
-    else {
-      let pointSize = 10;
-      rect(x, y, pointSize, pointSize);
+function draw () {
+
+
+
+  for(var x2 = 0; x2 < sourceImg.width; x2 = x2+ tileWidth2){
+    for(var y2 = 0; y2 < sourceImg.height; y2 = y2+ tileHeight2){
+      let pix2 = sourceImg.get(x2, y2);
+      let mask2 = maskImg.get(x2, y2);
+      fill(pix2[0],pix2[1],pix2[2]);
+      if (mask2[0] > 128){
+        rect(x2,y2, tileWidth2, tileHeight2);
+      }
     }
   }
+
+  for(var x3 = 0; x3 < sourceImg.width; x3 = x3+ tileWidth3){
+    for(var y3 = 0; y3 < sourceImg.height; y3 = y3+ tileHeight3){
+      let pix3 = sourceImg.get(x3, y3);
+      let mask3 = maskImg.get(x3, y3);
+      fill(pix3[0],pix3[1],pix3[2]);
+      if (mask3[0] > 50 & mask3[0] < 180){
+        rect(x3,y3, tileWidth3, tileHeight3);
+
+      }
+    }
+  }
+
+  for(var x = 0; x < sourceImg.width; x = x+ tileWidth){
+    for(var y = 0; y < sourceImg.height; y = y+ tileHeight){
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix[0],pix[1],pix[2]);
+      if (mask[0] < 128){
+        //rect(x,y, tileWidth, tileHeight);
+        ellipse(x,y,tileWidth, tileHeight);
+      }
+    }
+  }
+
+
+  // for(let i=0;i<20;i++) {
+  //   let x2 = floor(random(sourceImg.width));
+  //   let y2 = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x2, y2);
+  //   let mask = maskImg.get(x2, y2);
+  //   fill(pix);
+  //   stroke(pix);
+  //
+  //   let pointSize = 50;
+  //   let dice = random(1,6);
+  //   line(x, y, x+pointSize, y);
+  //   }
+
+    //drawThing(x, y, 10)
+
+
+
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 5) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-     saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
+}
+
+function drawThing(x, y, size){
+  push();
+  translate(x,y);
+  line(size, 0, -size, 0);
+  // for(var i = 0; i < 10; i++){
+  //   line(size, 0, -size, 0);
+  //   rotate(360/i);
+  // }
+  pop();
 }
 
 function keyTyped() {
