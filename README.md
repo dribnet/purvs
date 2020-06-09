8aa090b624e20752c00721a9fe92ac397a6205d8
JamieLum <61666463+JamieLum@users.noreply.github.com>
2020-06-10 11:32:56 +1200

    paint

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Sketch background
+Painting function
 
-The background has been replaced with a sketchy pattern, deliberately full of white gaps to make it look unfinished, as it is considered by the owner of the object to be unimportant. May need to tweak it to be more visually understandable.
-
-Next step is to modify the object of interest to make more like it's been painted, rather than being made of what are obviously ellipses.
+This is a prototype of new function to draw the object in a more painted way. It's going to layer multiple pieces on top of each other top produce a more blotchy, painted look.
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "output_2.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -31,19 +31,15 @@ function draw () {
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
+
     if(mask[0] < 200 && renderCounter < 3) {
-      blur (pix, x, y, pix[1]);
+      pencil (pix, x, y, pix[1]);
+
     }
+
     else if (mask[0] > 200){
-      push();
-      fill(pix);
-      strokeWeight(0);
-      let pointSize = random(5, 15);
-      let pointWidth = random(5, 15);
-      translate (x, y);
-      rotate(random(360));
-      ellipse(0, 0, pointSize, pointWidth);
-      pop();
+      paint (pix, x, y);
+
     }
 
   }
@@ -52,50 +48,51 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+     //saveArtworkImage(outputFile);
   }
 }
 
-function blur (col, posx, posy, pixel) {
-//   for(let i=0;i<3;i++) {
-//   push();
-//   let monoDark = color(0);
-//   let monoLight = color(255);
-//   let brightness = map(pixel, 0, 200, 0, 1);
-//   let monochrome = lerpColor(monoDark, monoLight, brightness);
-//
-//   fill(monochrome);
-//   stroke(monochrome);
-//   strokeWeight(9);
-//   strokeJoin(ROUND);
-//   let pointSize = random(10);
-//   let pointLength = random(10);
-//   translate (posx + random(20), posy + random(20));
-//   rotate(random(360));
-//   rect(0, 0, pointSize, pointLength);
-//   pop();
-// }
 
+
+function pencil (col, posx, posy, pixel) {
 for(let i=0;i<1;i++) {
   push();
     let monoDark = color(0);
-    let monoLight = color(255);
-    let brightness = map(pixel, 0, 255, 0, 1);
+    let monoLight = color(220);
+    let brightness = map(pixel, 0, 220, 0, 1);
     let monochrome = lerpColor(monoDark, monoLight, brightness);
 
     fill(monochrome);
     stroke(monochrome);
     strokeWeight(1);
     strokeCap(ROUND);
-    let xStart = random (-5, 5);
-    let yStart = random (-5, 5);
-    let xDrift = random (-5, 5);
-    let yDrift = random (-5, 5);
+    let xStart = random (-10, 10);
+    let yStart = random (-15, 15);
+    let xDrift = random (-10, 10);
+    let yDrift = random (-15, 15);
     line(posx - 20 + xStart, posy - 5 + yStart, posx + 20 + xDrift, posy + 5 + yDrift);
     line(posx - 20 + xStart, posy + 5 + yStart, posx + 20 + xDrift, posy - 5 + yDrift);
     pop();
 
 }
+
+}
+
+
+
+function paint (col, posx, posy){
+  push();
+  fill(col);
+  strokeWeight(0);
+  let pointSize = random(4, 12);
+  let pointWidth = random(4, 12);
+  translate (posx, posy);
+  rotate(random(360));
+  ellipse(0, 0, pointSize, pointWidth);
+  ellipse(0+4, 0+3, pointSize-2, pointWidth-4);
+  ellipse(0+6, 0-6, pointSize * 0.7, pointWidth);
+  ellipse(0-3, 0-5, pointSize * 0.5, pointWidth * 0.7);
+  pop();
 
 }
 
