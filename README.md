230889893e0a78b541a73fc633e9989cbd904063
Bee Huang <barbarahuang@hotmail.co.uk>
2020-06-09 17:24:14 +1200

    Experimenting with BG Colour

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 ## Creative Coding 2: Custom Pixel
 
-07/06/20 - Random Experiment
+09/06/20 - White Version (Experiment)
 
-The purpose of this commit was supposed to be changing the first image's background to black to match the photos under it (it waspreviously blue) but when I when I was changing the input images and removing the colour code I experimented with this was the result that came up.
-I thought it was worth making a commit because it the result was unexpected. It actually looks really cool because it looks like there's movement in my images, you can't really tell what the original image was supposed to be but that's all right because the same theme of 'fun, excitment and energy' is still there. The last image reminds me of the tie dye.
+While I was coding my final for this project I was interested to see what my pixel images would look like if they were inverted to white as they all have very dark backgrounds. 
 
-I will most likely keep the first image with a darker background because it matches with the other two images and there is more contrast. I believe a dark background and bright imagery will look really good on a billboard as it will stand out and give that neon glow and Wellington city gives.
-
-
+These were the results. They were very bright to look at because I was used to my dark images but I was aa big fan of the first image because it looked like a Wallpaper with some sort of pattern. My images contrasted equally as good on a black or white background so I will take this into consideration when I am handing in my final.
 
 
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,36 +18,45 @@ function setup () {
 
   imageMode(CENTER); //centering image
   noStroke(); //no stroke on the circles
-  background(10); //background colour
+  background(255); //background colour
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
-const tileHeight = 100; //tile hieght
-const tileWidth = 100; //tile width
+const tileHeight = 10; //tile hieght
+const tileWidth = 10; //tile width
 
-const x_step = 40; //circle width
-const y_step = 40; //circle height
+const x_step = 30; //circle width
+const y_step = 30; //circle height
+
+const pointSize = 3;
 
 function draw() {
+
   for(var x = 0; x < sourceImg.width; x = x+ x_step){
     for(var y = 0; y < sourceImg.height; y = y+ y_step){
         let pix = sourceImg.get(x, y);
         let mask = maskImg.get(x, y);
         fill(pix);
         stroke(pix);
-
-        let pointSize = 5;
-
-        if (mask[0] > 128) {
-          // stroke(20, 50)
-          // strokeWeight(10);
-          //  ellipse(x,y,tileWidth,tileHeight); //tile lines
-        }
-        else{
-          ellipse(x, y, x + pointSize, y-350); // horizonal lines
-          }
+         if (mask[0] > 200) {
+           stroke(255);
+           rect(x,y,tileWidth,tileHeight); //tile lines
+  }
+  else{
+    if(mask[0] > 128) {
+      stroke(pix);
+      strokeWeight(2);
+      fill(255); //fills white
+      ellipse(x+pointSize,y+pointSize,pointSize,pointSize); //the circles
+    } else {
+      stroke(255);
+      strokeWeight(5);
+      ellipse(x,y,x_step,y_step); //the circles
 }
+  }
+}
+
 }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
