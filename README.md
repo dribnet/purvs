bcdd1006cb1ff5856686adf7bfc22f6b0c4f474f
daleemil <61666933+daleemil@users.noreply.github.com>
2020-06-09 14:50:34 +1200

    Mask not working

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Experimentation with symbols. I decided I wanted to use symbols to represent what different people in different stages (past present future) have and will teach me.
+I wanted to make my pixels a bit more defined but through doing this it made parts of my mask disappear.
 
-For the past photo (people running up hill) I used the symbol of a dragonfly which represents wisdom, change, transformation. All things in which those people led me to and taught me, which is of high value in my life even if it was not the most positive experience, and tends to be a common experience in most people so is an important stage to highlight in my opinion. For this one I wanted to have the symbol be quite large and make the viewer look across the image. 
-
-For my present photo, taken at the zoo on a disposable camera after adventuring around Wellington to capture moments of the people who have taught me true friendship, love and care. I decided to use the heart symbol and cover the three friends who are the subject of the photo but subtly. 
-
-I am happy with how the symbols are coming along, especially the dragon fly ones, now I just need to make the imagery of the people far more clear and not have the mask be weird which is definitely something I am struggling with. 
+I also added a moon symbol to my second photo. 
--- sketch.js
+++ sketch.js
@@ -1,22 +1,19 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
-
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 // change these three lines as appropiate
-let sourceFile = "input_2.jpg";
-let maskFile   = "mask_2.png";
-let outputFile = "output_2.png";
-
+let sourceFile = "input_3.jpg";
+let maskFile = "mask_3.png";
+let outputFile = "output_3.png";
 //before code is up n running, process of showing the image
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
-
   imageMode(CENTER);
   //noStroke();
   background(0);
@@ -24,11 +21,9 @@ function setup () {
   maskImg.loadPixels();
 }
 
-
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
-
   imageMode(CENTER);
   noStroke();
   background(0);
@@ -37,76 +32,65 @@ function setup () {
 }
 //size for pixels
 const size = 25;
-const tileHeight = 5;
-const tileWidth = 5;
+const tileHeight = 8;
+const tileWidth = 8;
+const x_step = 8;
+const y_step = 8;
 
-const x_step = 5;
-const y_step = 5;
-
-
-function draw () {
-  for(let i=0;i<2000;i++) {
+function draw() {
+  for (let i = 0; i < 3000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
 
 
-//this is the the people
-    if(mask[0] == 255) {
+    //this is the the people
+    if (mask[0] == 255) {
       fill(pix[0], pix[1], pix[2]);
-      ellipse(x, y, size-15, size-15);
-
+      ellipse(x, y, size - 15, size - 15);
     }
     //background
-    else if(mask[0] == 0) {
+    else if (mask[0] == 0) {
       fill(pix[0], pix[1], pix[2], 30);
       rect(x, y, size, size);
-
     }
     //the heart
-    else if(mask[0] == 40) {
-      fill(pix[0]+40, pix[1]+10, pix[2]+10);
-      ellipse(x, y, size-16, size-16);
-
+    else if (mask[0] == 40) {
+      fill(pix[0] + 40, pix[1] + 10, pix[2] + 10);
+      ellipse(x, y, size - 16, size - 16);
     }
-
     //the dragonfly
-    else if(mask[0] == 140) {
-      fill(pix[0]+40, pix[1]+40, pix[2]+50, 100);
-      rect(x, y, size/4, size);
-
+    else if (mask[0] == 140) {
+      fill(pix[0] + 40, pix[1] + 40, pix[2] + 50, 100);
+      rect(x, y, size / 4, size);
     }
     //this is the hill
-    else if(mask[0] == 70 ) {
-      fill(pix[0] + 10, pix[1] +10, pix[2] + 10, 60);
+    else if (mask[0] == 70) {
+      fill(pix[0] + 10, pix[1] + 10, pix[2] + 10, 60);
       rect(x, y, size, size);
-
     }
+  }
 
-      for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
+    for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
       for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
         let pix = sourceImg.get(x2, y2);
         let mask = maskImg.get(x2, y2);
-         fill(pix[0],pix[1],pix[2]);
-         noStroke();
-        if (mask[0] > 255) {
-        rect(x2, y2, tileWidth, tileHeight);
-
-  }
-}
-}
-}
-}
+        fill(pix[0], pix[1], pix[2]);
+        noStroke();
+        if (mask[0] == 255) {
+          rect(x2, y2, tileWidth, tileHeight);
+        }
+      }
+    }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 40) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-   //saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
-
-
+}
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
