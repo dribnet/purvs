fe6e8d4973675d3307b9813fc8c58ad7480daa70
caitlinpiepke <caitlin.piepke@gmail.com>
2020-06-09 14:25:08 +1200

    Ouput 1 - almost done

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Creative Coding 2: Custom Pixel
 
-I was not happy with my previous mask so i wanted to adjust this so that the model (me) was in focus and you could still see, with only the background slightly altered with the effects and colours.
+I wanted to refine the colour scheme further so changed this. I don't like how the orange lines are super pixelated and am trying to adjust this so that it is more of a smooth line.
 
-At the moment there is a bit too much going on in the photos, so I need to switch a few things around so that you can see more of the original image. 
+I also changed the model colour to an almost grey scale which i think looks super effective.
+
+Working on my 2nd mask and output now. 
--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-let elementSpacing = 40;
-let squareSize = 40;
-let colorSquareSize = 40;
-
 let sourceImg = null;
 let maskImg = null;
 let renderCounter = 0;
@@ -31,70 +27,61 @@ function setup() {
 const tileWidth = 5;
 const tileHeight = 5;
 
-const x_step = 7;
-const y_step = 7;
-
 
 function draw() {
 
-  for(let i=0; i<2000; i++){
+  for (let i = 0; i < 1000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
-    // let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    // fill(pix);
-
-    let pointSize = 15;
     let pixMod = sourceImg.get(x, y);
     console.log(mask[0]);
 
-    if(mask[0] > 72 && mask[0] < 76){     // dark grey mask (orange)
-      pixMod[0] = pixMod[0]*2;
-      pixMod[1] = pixMod[1]/3;
-      pixMod[2] = pixMod[2]*3;
+    if (mask[0] > 72 && mask[0] < 76) { //dark grey mask (red)
+      let pixMod = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      pixMod[0] = pixMod[0] * 2;
+      pixMod[1] = pixMod[1] / 2;
+      pixMod[2] = pixMod[2];
       noStroke();
-      fill(230, 214, 78);
-      rect(x, y, pointSize/2, pointSize/2);
-
-    }else if(mask[0]< 130 && mask[0] > 125){     //light grey mask (blue)
-      fill(15, 171, 214);
-      stroke(15, 171, 214);
-      rect(x, y, tileWidth, tileHeight);
+      fill(pixMod);
+      ellipse(x, y, 20, 20);
     }
   }
 
 
+  for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + tileWidth) {
+    for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + tileHeight) {
+      let pixMod = sourceImg.get(x2, y2);
+      let mask = maskImg.get(x2, y2);
+      pixMod[0] = pixMod[2];
+      pixMod[1] = pixMod[0];
+      pixMod[2] = pixMod[1];
+      noStroke();
+      fill(pixMod);
 
-for(var x2 = 0; x2 < sourceImg.width; x2 = x2 + tileWidth){        //white
-  for(var y2 = 0; y2 < sourceImg.height; y2 = y2 + tileHeight){
-    let pixMod = sourceImg.get(x2, y2);
-    let mask = maskImg.get(x2, y2);
-    pixMod[0] = pixMod[0]*2;
-    pixMod[1] = pixMod[1]/2;
-    pixMod[2] = pixMod[2];
-    noStroke();
-    fill(pixMod);
+      if (mask[0] > 220) {   //white mask (model)
+        stroke(pixMod);
+        line(x2, y2, x2, y2 + 200);
 
-    if(mask[0] > 200){
-      rect(x2, y2, tileWidth, tileHeight);
-    }
-    else if (mask[0] < 10){
-      fill(230, 78, 149);
-      stroke(230, 78, 149);
-      rect(x2, y2, tileWidth, tileHeight);
-      fill(88, 153, 135);
-      stroke(88, 153, 135);
-      rectMode(CENTER);
-      rect(x2, y2, tileWidth, tileHeight/3);
+
+      } else if (mask[0] < 130 && mask[0] > 125) { //light grey mask (orange)
+        fill(245, 158, 66);
+        strokeCap(ROUND);
+        stroke(245, 158, 66);
+        rect(x2, y2, tileWidth*3, tileHeight*3);
+
+      } else if (mask[0] < 20) {  //black mask (purple background)
+        let pixMod = sourceImg.get(x2, y2);
+        pixMod[0] = pixMod[0] / 2;
+        pixMod[1] = pixMod[1] / 3;
+        pixMod[2] = pixMod[2];
+        fill(pixMod);
+        stroke(pixMod);
+        ellipse(x2, y2, tileWidth, tileHeight + 10);
+      }
     }
   }
-}
-
-
-
-
-
-
 
   //runs 10 different times
   renderCounter = renderCounter + 1;
@@ -102,20 +89,8 @@ for(var x2 = 0; x2 < sourceImg.width; x2 = x2 + tileWidth){        //white
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
-}
-
-
-function drawStar(x, y, size) {
-  push();
-  strokeWeight(3);
-  translate(x, y);
-  for (var i = 0; i < 10; i++) {
-    line(size, 0, -size, 0);
-    rotate(360 / 5);
-  }
-  pop();
 }
 
 function keyTyped() {
