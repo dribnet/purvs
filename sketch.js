37c09684a64dac6986adde4205686cea7e6c8ac1
James Drown <drownjame1@myvuw.ac.nz>
2020-06-10 10:44:27 +1200

    Signed-off-by: James Drown <drownjame1@myvuw.ac.nz>

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,7 +18,7 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(0);
+  background(100);
   sourceImg.loadPixels();
   maskImg.loadPixels();
   rectMode(CENTER);
@@ -28,28 +28,21 @@ let xstep = 5;
 let ystep = 5;
 function draw() {
   //noLoop();
-  angleMode(DEGREES);
   for (var x = 0; x < sourceImg.width; x = x + xstep) {
     for (var y = 0; y < sourceImg.height; y = y + ystep) {
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
      if (mask[0] > 100) {
         stroke(pix);
-
         let randAngle = floor(random(360));
-        let rand = floor(random(-15, 15));
+        let randLength = floor(random(-15, 15));
         let randWeight = floor(random(3));
         strokeWeight(randWeight);
-        let rand1 = floor(random(-25, 25));
-        let rand2 = floor(random(-25, 25));
         push();
         translate(x,y);
         rotate(randAngle);
-        bezier(0, 0, rand1, rand1, rand2, rand2, rand,rand);
+        line(0, 0, randLength,randLength);
         pop();
-      } else {
-        stroke(0);
-        line(x, y, x-10, y);
       }
     }
   }
