2e4086cbd05c6de5b896cb6293eb026c7ca3e65c
daleemil <61666933+daleemil@users.noreply.github.com>
2020-06-09 17:00:47 +1200

    New pixel

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-I wanted to make my pixels a bit more defined but through doing this it made parts of my mask disappear.
+I was able to get my mask and grid to work to make the pixels look more defined, I am happy with the outcome and look of how this looks. I am pretty happy with how my dragonfly and hearts are looking, after playing around with the colour through code. I need to work on the symbol in the second image because the moons did not look clean enough not matter what I did, same with the butterflies.
 
-I also added a moon symbol to my second photo. 
+I have experimented a lot with the size of the pixels to make sure I have gotten the right look, I also managed to get rid a lot of the black cast that I had previously. So far I am pretty happy with how it is looking, I just need to make some final refinements and work on my second image. 
--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup() {
   main_canvas.parent('canvasContainer');
   imageMode(CENTER);
   noStroke();
-  background(0);
+  background(188, 201, 212);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -38,6 +38,23 @@ const x_step = 8;
 const y_step = 8;
 
 function draw() {
+  for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
+    for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
+      let pix = sourceImg.get(x2, y2);
+      let mask = maskImg.get(x2, y2);
+      fill(pix[0], pix[1], pix[2]);
+      noStroke();
+      if (mask[0] == 255) {
+        rect(x2, y2, tileWidth, tileHeight);
+      }
+      else{
+        if(mask[0]>0 && mask[0] < 255);
+        fill(pix[0], pix[1], pix[2], 30);
+        rect(x2,y2,tileWidth,tileHeight);
+      }
+    }
+  }
+
   for (let i = 0; i < 3000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -55,10 +72,26 @@ function draw() {
       fill(pix[0], pix[1], pix[2], 30);
       rect(x, y, size, size);
     }
+
+    //the faces
+    else if (mask[0] == 20) {
+      fill(pix[0], pix[1], pix[2]);
+      ellipse(x, y, size-20, size-20);
+    }
+    //the moons
+    else if (mask[0] == 130) {
+      fill(pix[0]+100, pix[1]+100, pix[2]+200);
+      ellipse(x, y, size-18, size-18);
+    }
     //the heart
     else if (mask[0] == 40) {
-      fill(pix[0] + 40, pix[1] + 10, pix[2] + 10);
-      ellipse(x, y, size - 16, size - 16);
+      fill(pix[0] + 90, pix[1] + 30, pix[2] + 30);
+      ellipse(x, y, size - 15, size - 15);
+    }
+    //the pictures
+    else if (mask[0] == 219) {
+      fill(pix[0], pix[1], pix[2]);
+      rect(x, y, size/4, size/2);
     }
     //the dragonfly
     else if (mask[0] == 140) {
@@ -70,25 +103,15 @@ function draw() {
       fill(pix[0] + 10, pix[1] + 10, pix[2] + 10, 60);
       rect(x, y, size, size);
     }
-  }
 
-    for (var x2 = 0; x2 < sourceImg.width; x2 = x2 + x_step) {
-      for (var y2 = 0; y2 < sourceImg.height; y2 = y2 + y_step) {
-        let pix = sourceImg.get(x2, y2);
-        let mask = maskImg.get(x2, y2);
-        fill(pix[0], pix[1], pix[2]);
-        noStroke();
-        if (mask[0] == 255) {
-          rect(x2, y2, tileWidth, tileHeight);
-        }
-      }
-    }
+}
+
   renderCounter = renderCounter + 1;
-  if (renderCounter > 10) {
+  if (renderCounter > 30) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    //saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 function keyTyped() {
