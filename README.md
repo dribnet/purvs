455ed7092d4e0217b7caf3211cedc14636a2d1b7
y1ngzhyi <53279099+y1ngzhyi@users.noreply.github.com>
2020-06-09 20:08:44 +1200

    develop

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Creative Coding 2: Custom Pixel
 
 Jun.9th 2020
-I suddenly thought of a new idea when I was going to change the photos back to food photos. I changed the photo to a photo of only the scenery and used the shape of the mask to express the person. The three pictures are connected by wavy lines.
+change the shapes to stars and hearts
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -28,29 +28,63 @@ const tileHeight = 12;
 
 const x_step = 7;
 const y_step = 12;
+const x_step1 = 20;
+const y_step1 = 15;
+
+const starR = 26;
 
 function draw () {
-  for(var x = 0;x< sourceImg.width; x = x+ x_step){
-    for(var y = 0 ; y < sourceImg.height; y = y+ y_step){
-      let pix = sourceImg.get(x,y);
-      let mask = maskImg.get(x,y);
-      if(mask[0]<30){
+
+
+  for(var x2 = 0;x2< sourceImg.width; x2 = x2+ x_step){
+    for(var y2 = 0 ; y2 < sourceImg.height; y2 = y2+ y_step){
+      let pix = sourceImg.get(x2,y2);
+      let mask = maskImg.get(x2,y2);
+
       fill(pix[0],pix[1],pix[2],50);
       noStroke();
-      ellipse(x,y,tileWidth,tileHeight);
-    }else{
-      if(mask[0]>200){
-        strokeWeight(10);
-        stroke(255,249,35);
-        line(x-20,y,x+20,y);
-      }else{
+      ellipse(x2,y2,tileWidth,tileHeight);
+
+  }
+}
+
+for (let i = 0; i<2000;i++){
+  let x1 = floor(random(sourceImg.width));
+  let y1 = floor(random(sourceImg.height));
+  let mask = maskImg.get(x1,y1);
+
+    if(mask[0]>200){
+      // let starX = x1;
+      // let starY = y1;
+      // for(var j = 0; j<25;j++){
+
+      drawStar(x1,y1);
+     // starX = starX +random(-4,6);
+     // starY = starY + random(-3,2);
+    //}
+    }
+}
+
+for(var x3 = 0;x3 < sourceImg.width; x3 = x3+ x_step1){
+  for(var y3 = 0 ; y3 < sourceImg.height; y3 = y3+ y_step1){
+    let pix = sourceImg.get(x3,y3);
+    let mask = maskImg.get(x3,y3);
         if(mask[0]==144){
-          drawcoin(x,y,10)
+          // let coinX = x3;
+          // let coinY = y3;
+          // for(var k = 0; k<2; k++){
+          // drawcoin(coinX,coinY,10)
+          // coinX = coinX + random(-2,3);
+          // coinY = coinY + random(3,-2);
+          drawHeart(x3,y3,26);
+
         }
       }
     }
-  }
-  }
+
+
+
+
 
 
   renderCounter = renderCounter + 1;
@@ -58,27 +92,51 @@ function draw () {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-  //  saveArtworkImage(outputFile);
+       //saveArtworkImage(outputFile);
   }
 }
 
-function drawcoin(x,y,size){
+
+function drawHeart(x,y,size){
   let pix = sourceImg.get(x, y);
   let mask = maskImg.get(x, y);
   fill(pix);
-  noStroke();
   stroke(255);
-  strokeWeight(1.5);
-  beginShape();
-vertex(x, y);
-bezierVertex(x+size,y, x+size,y+size*1.5, x, y+size*1.5);
-bezierVertex(x+size,y, x+size,y+size*1.5, x, y);
-endShape();
+  strokeWeight(3);
+//   beginShape();
+// vertex(x, y);
+// bezierVertex(x+size,y, x+size,y+size*1.5, x, y+size*1.5);
+// bezierVertex(x+size,y, x+size,y+size*1.5, x, y);
+// endShape();
+//
+//   beginShape();
+// vertex(x, y);
+// bezierVertex(x-size,y, x-size,y+size*1.5, x, y+size*1.5);
+// bezierVertex(x-size,y, x-size,y+size*1.5, x, y);
+// endShape();
+beginShape();
+  vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
+  strokeWeight(2.3);
+fill(255,165,190);
+beginShape();
+  vertex(x, y + size / 4.2);
+  bezierVertex(x - size / 3.2, y + size / 4.2 - size / 3.2, x - size / 2.2, y + size / 4.2 + size / 4.2, x, y + size / 4.2 + size / 2.2);
+  bezierVertex(x + size / 2.2, y + size / 4.2 + size / 4.2, x + size / 4.2, y + size / 4.2 - size / 3.2, x, y + size / 4.2);
+  endShape(CLOSE);
+}
 
+function drawStar(x,y){
+  fill(255,249,35);
+  noStroke();
   beginShape();
-vertex(x, y);
-bezierVertex(x-size,y, x-size,y+size*1.5, x, y+size*1.5);
-bezierVertex(x-size,y, x-size,y+size*1.5, x, y);
+vertex(x, y-starR);
+quadraticVertex(x, y, x+starR, y);
+  quadraticVertex(x,y,x,y+starR);
+  quadraticVertex(x,y,x-starR,y);
+  quadraticVertex(x,y,x,y-starR);
 endShape();
 }
 
