5f065c7df32451fc1cbea50a668a9ebff4328ac8
Kayla Palmer <56278807+straycatss@users.noreply.github.com>
2020-06-10 11:50:45 +1200

    changed bg and added grey mast to all 3

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## Creative Coding 2: Custom Pixel
+I added a new grey mask to my images and changed the background to a slightly blurred mess of lines. I did this by giving each line a random x value between 1 and 5, and a random length value between 20 and 60.
 
-This version of my code looks at taking out the dark parts of the background and using a smaller tile size. I am doing this to create a harsh contrast between the foreground and background.
-These colour experiments are heaps of fun, but I think to progress I need to make a layered mask that highlights some of the more intricate background features of this. I also want to try some different patterns in the background/foreground to create more textures.
-This version of the code doesn't look so good on input 2, but it is still an interesting  effect.
+The dark parts which are made of large, slightly see through circles, and tinged slightly red. I did this by accident because I thought that black was close to  255 and not 0, but I like the colour they turned out.
+
+I have decided I am going to style the images based off glitch art, a style of digital art which takes inspiration from digital display glitches (pixels, bright colours, broken lines, etc). This goes  with my theme of graffiti, while still being art, is seen as not functional or fitting into society, and is normally rid of because it is seen as ugly. I want to try blend these ideas and colours (seen in artworks like these https://i.pinimg.com/564x/5d/e7/98/5de798bd10e1d6c92aa0f549075ca78f.jpg) into my pieces.
 
 
 The theme I have chosen for my images is graffiti in the Wellington region. These specific images were taken in the abandoned military bunkers surrounding the city (specifically the Brooklyn windmill bunkers and Fort Ballance on Scorching Bay), where every surface is covered in colourful tags. The council doesn't bother to remove these works, so they layer up ontop of each other to create a chaotic yet beautiful pattern and texture. To me, this represents freedom of expression and art. This graffiti is not on any privately owned property, nor is it in the way of the general public. This is a form of art which is generally not acceptable to the civil eye, as it is disruptive and chaotic.
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -27,36 +27,59 @@ function draw () {
 
   let tilewidth  =6;
   let tileheight = 6;
-  let squarew =4.5;
+  let squarew =6;
   let squareh = 6;
   strokeWeight(1.5);
   for(var xtile = 0; xtile < sourceImg.width; xtile= xtile+tilewidth){
     for(var ytile = 0; ytile < sourceImg.height;  ytile= ytile+tileheight){
     let pix = sourceImg.get(xtile, ytile);
     let mask = maskImg.get(xtile, ytile);
-    if (mask[0] <120){
-      fill(pix);
-      rect(xtile,ytile,squarew,squareh);
-    } if(mask[0]==120){
-      fill(pix);
-  rect(xtile,ytile,tilewidth,tileheight);
+    let pixdark = sourceImg.get(xtile, ytile);
+    if(mask[0]==120){
+      pixdark[0] = pixdark[0]/1.1;
+      pixdark[1]=  pixdark[1]/3.5;
+      pixdark[2]=pixdark[2]/1.5;
+      fill(pixdark[0],pixdark[1],pixdark[2],170);
+      noStroke();
+  ellipse(xtile,ytile,17);
     }if(mask[0]>120){
+      noStroke();
       let pixMod = sourceImg.get(xtile, ytile);
       pixMod[0]=pixMod[0]*1.2;
       pixMod[1]=pixMod[1]/4;
       pixMod[2]=pixMod[2]/4;
       fill(pixMod);
       rect(xtile,ytile,tilewidth,tileheight);
+      //fill(pix[0],pix[1],pix[2],200);
+      //rect(xtile+2,ytile,tilewidth,tileheight);
       var rand = random(1,10);
       if(rand<2){
-        fill(255);
+        noFill();
+        stroke(255);
         circle (xtile,ytile,5);
+        noStroke();
+        fill(pixMod);
       }
     }
 }
 }
 
-  renderCounter = renderCounter + 1;
+for(var xtile1 = 0; xtile1 < sourceImg.width; xtile1= xtile1+3.5){
+  for(var ytile1 = 0; ytile1 < sourceImg.height;  ytile1= ytile1+3.5){
+let mask = maskImg.get(xtile1, ytile1);
+let pix1 = sourceImg.get(xtile1, ytile1);
+    if (mask[0] <120){
+      stroke(pix1);
+      strokeWeight(.7);
+      var lineLength = random(20,60);
+      var xrand =random(1,5);
+      line(xtile1+xrand,ytile1,xtile1+xrand,ytile1+lineLength);
+
+    }
+  }
+}
+
+  renderCounter = renderCounter +1 ;
   if(renderCounter > 1) {
     console.log("Done!")
     noLoop();
