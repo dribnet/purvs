e73317d1de31b9a43799fc173b37fb2a7f67a3a3
James Drown <drownjame1@myvuw.ac.nz>
2020-06-10 16:54:57 +1200

    Final Signed-off-by: James Drown <drownjame1@myvuw.ac.nz>

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -52,7 +52,7 @@ function draw() {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    saveArtworkImage(outputFile);
+    //saveArtworkImage(outputFile);
   }
 }
 
