8608b3b50f36d10a85a12810715d053d58c0ceb6
Craig Springett <springcrai@myvuw.ac.nz>
2020-06-10 17:36:08 +1200

    Add final tweaks and replace images for submission

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -40,7 +40,7 @@ function draw () {
   if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
-    saveArtworkImage(outputFile);
+    // saveArtworkImage(outputFile);
   }
 }
 
@@ -55,7 +55,7 @@ function brushStroke(x, y, c, maskMap, mask, level){ //Recursive function
   var size = map(maskMap, 0, 255, 8, 6);
   var length = map(maskMap, 0, 255, 10, 25);
   
-  if(maskArr < 90){
+  if(maskArr < 90){                             //Darker pixels
     noStroke();
     fill(c);
     
@@ -64,7 +64,7 @@ function brushStroke(x, y, c, maskMap, mask, level){ //Recursive function
       rotate(maskMap);
       ellipse(0, 0, size, length*(size/2));
     pop();
-  }else if(maskArr < 180){
+  }else if(maskArr < 180){                     //Grey pixels
     noStroke();
     fill(c);
 
@@ -73,7 +73,7 @@ function brushStroke(x, y, c, maskMap, mask, level){ //Recursive function
       rotate(maskMap);
       rect(0, 0, length, size, size);
     pop();
-  }else{
+  }else{                                      //Brighter pixels
     noFill();
     stroke(c);
     strokeWeight(size);
@@ -85,13 +85,19 @@ function brushStroke(x, y, c, maskMap, mask, level){ //Recursive function
     pop();
   }
 
-  if(level > 0 && (random(0, 1) > 0.4)){ //adds chance to fail
+  if(level > 0 && (random(0, 1) > 0.4)){ //adds chance to fail entire recursive call
     level-=1;
-    brushStroke(x, y-(length/size), color(red(c), green(c), blue(c)+length), maskMap-size, mask, level);
-    brushStroke(x-(length/size), y, color(red(c), green(c), blue(c)-length), maskMap+size, mask, level);
-    brushStroke(x, y-(length/size), color(red(c), green(c)+length, blue(c)), maskMap-size, mask, level);
-    brushStroke(x-(length/size), y, color(red(c), green(c)-length, blue(c)-length), maskMap+size, mask, level);
-    brushStroke(x, y-(length/size), color(red(c)+length, green(c), blue(c)), maskMap-size, mask, level);
-    brushStroke(x-(length/size), y, color(red(c)-length, green(c), blue(c)), maskMap+size, mask, level);
+    if((random(0, 1) > 0.4)){ //adds chance to fail particular colour variance
+      brushStroke(x, y-(length/size), color(red(c), green(c), blue(c)+length), maskMap-size, mask, level);
+      brushStroke(x-(length/size), y, color(red(c), green(c), blue(c)-length), maskMap+size, mask, level);
+    }
+    if((random(0, 1) > 0.7)){
+      brushStroke(x, y-(length/size), color(red(c), green(c)+length, blue(c)), maskMap-size, mask, level);
+      brushStroke(x-(length/size), y, color(red(c), green(c)-length, blue(c)-length), maskMap+size, mask, level);
+    }
+    if((random(0, 1) > 0.4)){
+      brushStroke(x, y-(length/size), color(red(c)+length, green(c), blue(c)), maskMap-size, mask, level);
+      brushStroke(x-(length/size), y, color(red(c)-length, green(c), blue(c)), maskMap+size, mask, level);
+    }
   }
 }
