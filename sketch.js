5f17daa106a696c942bc4116f55c470322e5cf39
joelle2208 <61666899+joelle2208@users.noreply.github.com>
2020-06-10 19:10:21 +1200

    Final push!
    I polished my code and comments. Fixed my masks and wrote a summary of the assignment.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let maskFile = "mask_3.png";
 let outputFile = "output_3.png";
 
 function preload() {
@@ -12,7 +12,7 @@ function preload() {
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
   imageMode(CENTER);
@@ -22,87 +22,85 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<2000;i++) {
+function draw() {
+  for (let i = 0; i < 2000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
     //console.log(mask[0]);
-    if(mask[0] > 128) { //white
+    if (mask[0] > 128) { //white mask
 
-    if(pix[1] > pix[0]){ //green
+      if (pix[1] > pix[0]) { //green areas
         strokeWeight(1);
-  } else {
-    strokeWeight(0.5);
-  }
-    stroke(pix);
-    drawStar1(x,y,30);
 
-  }else if (mask[0] == (128)) { //grey
-  stroke(pix, 10);
-  strokeWeight(2);
-  drawLine(x,y,35);
+      } else {
+        strokeWeight(0.5);
+      }
+      stroke(pix);
+      drawStar1(x, y, 30);
 
-  }
+    } else if (mask[0] == (128)) { //grey mask
+      stroke(pix, 10);
+      strokeWeight(2);
+      drawLine(x, y, 35);
 
-  else { //black
-    strokeWeight(1);
-    stroke(pix);
-    drawStar2(x,y,25);
+    } else { //black mask
+      strokeWeight(1);
+      stroke(pix);
+      drawStar2(x, y, 25);
     }
   }
 
 
 
-
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
-  //  uncomment this to save the result
-   saveArtworkImage(outputFile);
+    //  uncomment this to save the result
+    saveArtworkImage(outputFile);
   }
 }
 
 
 
+//this draws stars for white mask
 
-function drawStar1(x,y,size) {
-push();
-translate(x,y);
-for(var i = 0; i < 4; i++){
-line(size,0, -size,0);
-rotate(360 / i);
-}
-pop();
-}
-
-
-
-
-
-function drawLine(x,y,size) {
-push();
-translate(x,y);
-for(var i = 0; i < 2; i++){
-line(size,0, -size,0);
-rotate(360 / i);
-}
-pop();
+function drawStar1(x, y, size) {
+  push();
+  translate(x, y);
+  for (var i = 0; i < 4; i++) {
+    line(size, 0, -size, 0);
+    rotate(360 / i);
+  }
+  pop();
 }
 
 
+//This draws the horizontal lines for grey mask
 
-function drawStar2(x,y,size) {
-push();
-translate(x,y);
-for(var i = 0; i < 8; i++){
-line(size,0, -size,0);
-rotate(360 / i);
+function drawLine(x, y, size) {
+  push();
+  translate(x, y);
+  for (var i = 0; i < 2; i++) {
+    line(size, 0, -size, 0);
+    rotate(360 / i);
+  }
+  pop();
 }
-pop();
+
+//this draws stars for black mask
+
+function drawStar2(x, y, size) {
+  push();
+  translate(x, y);
+  for (var i = 0; i < 8; i++) {
+    line(size, 0, -size, 0);
+    rotate(360 / i);
+  }
+  pop();
 }
 
 
