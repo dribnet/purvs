dfd3ed0f9d22882045c9980b2b42b710510c6527
Jtopolegend <52726354+Jtopolegend@users.noreply.github.com>
2020-06-10 18:10:11 +1200

    Final Commit

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 ## Creative Coding 2: Custom Pixel
+Final Commit
 
-Second grid test i decided to focus on making the masks stand out with the
-rectangles to continue breaking it down for helping me understand what to focus
-on overall after doing these tests i am now ready to focus on getting my final
-images done
+Overall for this project the theme i selected was Time and how it is constantly
+changing and evolving, as it keeps moving forward with the things around us that
+are both natural and manmade. I have explored this in this project through my
+final 4 images for example the tides in the oceans change and evolve throughout
+the day from the tides being in or out which are caused by the gravitational
+forces of the moon sun and also rotation of the earth, this occurs on a daily basis
+all throughout time as it continues to move indefinitely. I also looked at the
+evolution of the buildings and landscapes over the years to showcase how time has
+progressed while comparing buildings from old to the new to show how manmade
+buildings have progressed through time and history.
+
+For my final custom pixel I decided to use lines and layers that blend well
+with my images where it can still capture the landscape detail really well
+with the line pixels in the mask and non mask area being used to still capture
+what i took from the original photographs that convey my main theme and idea.
--- sketch.js
+++ sketch.js
@@ -1,98 +1,60 @@
-let sourceImg = null;
-let maskImg = null;
-let renderCounter = 0;
+let sourceImg=null;
+let maskImg=null;
+let renderCounter=0;
 
-// change these three lines as appropiate
-let sourceFile = "input_4.jpg";
-let maskFile = "mask_4.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
 }
 
-function setup() {
+function setup () {
   let main_canvas = createCanvas(1920, 640);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
-  //noStroke();
+  noStroke();
   background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
-// Black Lines Grid
-const tileHeight = 10;
-const tileWidth = 10;
 
-const x_step = 20;
-const y_step = 15;
+function draw () {
+    for (let y = 0; y < 1000; y+=10) {
+    for (let x = 0; x < 1920; x+=10) {
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
 
-function draw() {
-
-  for(var x = 0; x < sourceImg.width; x = x+ x_step){
-    for(var y = 0; y < sourceImg.height; y = y+ y_step){
-        let pix = sourceImg.get(x, y);
-        let mask = maskImg.get(x, y);
-        fill(pix);
+      if(mask[0] > 300) {
+        noStroke();
+        // pixels inside mask
+        if (pix[2] > 200 && pix[2] > 200 && pix[2] > 200) {
+        lines(x+random(-30,5), y+random(-20,1), 5, 5);
+        }
+        // pixels outside mask
+        else {
+        lines(x+random(-30,5), y-random(-20,1), 5, 5);
+        }
+      } // draws the lines around my mask and non mask areas
+      else {
         stroke(pix);
-         if (mask[0] > 128) {
-        rect(x,y,tileWidth,tileHeight);
+        strokeWeight(random(1,7));
+        line(x + random(-30, 10), y - random(-20,20), x + random(-20, 10), y - random (-20,20));
+      }
+    }
   }
-  else{
-    rect(x,y,x_step,y_step);
-  }
-}
-
-}
-
-  //
-  // for (let i = 0; i < 1000; i++) {
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pix = sourceImg.get(x, y);
-  //   let mask = maskImg.get(x, y);
-  //   fill(pix);
-  //   stroke(pix);
-  //
-  //   let pointSize = 20;
-  //   let dice = random(1, 6);
-  //   if (mask[0] > 128) {
-  //     drawEllipse(x,y,15);
-  //   } else {
-  //    if (dice > 5) {
-  //
-  //     } else {
-  //
-  //     }
-  //   }
-  // }
-
-
-
   renderCounter = renderCounter + 1;
-  if (renderCounter > 10) {
+  if(renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     //saveArtworkImage(outputFile);
   }
 }
-
-function drawEllipse(x,y,size){
-  push();
-  //strokeWeight(4);
-  translate(x,y);
-  for(var i = 0; i < 10; i++){
-    line(size,0,-size,0);
-      rotate(360/ i);
-    }
-  pop();
-}
-
-
-
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
