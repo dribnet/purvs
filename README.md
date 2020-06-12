1e5d2ccb9c8231281cfe9a0676aba9390e01465b
Emily <50353742+MLE-G@users.noreply.github.com>
2020-06-12 17:42:29 +1200

    Cross stitch

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## Creative Coding 2: Custom Pixel
 
-Got the cross stitch pattern working. I set the background to a cream colour to mimic the type of fabric used with cross stitch projects and have enlarged the stitches so that the picture can be more easily seen.
-
-I have put it in a very solid grid formation but I previously and unintentionally had it with the random x and y values applied which gave it a very painterly feel to it. I may return to that design as I feel that the cross stitch custom pixel does not work well for bigger projects as the crosses which make up the cross stitch design get lost the smaller they stitch gets and just becomes a normal picture.
-
-My next step is to explore this more painterly feel. 
+My theme for my project I've decided to call "by the way side".
+These are things that are just on the side of the road that are often forgotten or not closely examined. 
+I thought it would be interesting to look at as I find myself more and more these days missing the little things and stopping to smell the roses. 
--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let renderCounter=0;
 let pointSize = 10;
 
 // change these three lines as appropiate
-let sourceFile = "piano.jpg";
-let maskFile   = "mask_piano_black.png";
-let outputFile = "output_piano.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "output_3";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,8 +18,13 @@ function setup () {
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
+  angleMode(DEGREES);
+
   noStroke();
-  background(247,239,215);
+
+  background(30);//grey
+  //background(255);//white
+  //background(245,240,230); //cream
 
   sourceImg.loadPixels();
   maskImg.loadPixels();
@@ -28,25 +33,12 @@ function setup () {
 //1920 x 640
 
 function draw () {
-  for(let y = 0; y < 640; y += pointSize) {
-      for (let x = 0; x < 1920; x += pointSize) {
-      //sets position of pixel
-      let pix = sourceImg.get(x, y);
-      let mask = maskImg.get(x, y);
-      fill(pix);
-      stroke(pix);
-
-      if (mask[0] > 128) {
-        cross(x,y,random(1,4)*5, pix);
-        //cross(x, y,pointSize);
-      }
-      else {
-        embroider(x, y,pointSize);
-      }
-    }
-  }
-  render(1);
-  drawgrid();
+  pointSize = 20;
+  //embroider();
+  pointSize = 10;
+  crossStitch();
+  drawHoles();
+  render(3);
 }
 
 function render(num){
@@ -54,34 +46,99 @@ function render(num){
   if(renderCounter > num) {
     console.log("Done!")
     noLoop();
-    // uncomment this to save the result
-    //saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
-function drawgrid(){
-  for(let i = 0; i < 1920; i+=pointSize) {
-    for (let j = 0; j < 640; j += pointSize) {
-      stroke('black');
-      strokeWeight(pointSize/8);
+function paintedCross(){
+  for(let i=0;i<2000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    fill(pix);
+    stroke(pix);
 
-      point(i,j);
+    let pointSize = 10;
+    cross(x, y, pointSize);
+  }
+  render(30,0);
+}
+
+function crossStitch() {
+  for(let y = 0; y < sourceImg.height; y += pointSize) {
+      for (let x = 0; x < sourceImg.width; x += pointSize) {
+      //sets position of pixel
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+
+      //if (mask[0] < 128) {
+        cross(x,y,pointSize,pix);
+        stroke(0,0,0,80);
+        strokeWeight(2);
+        point(x,y);
+      //}
     }
   }
 }
 
-function cross(x, y, size){
+function cross(x, y, size, pix){
+  strokeWeight(4);
 
-  strokeWeight(3);
-  line(x, y, x + size, y + size);
+  stroke(pix);
+  line(x, y, x + size, y + size); // under most thread
+
+  let shadow = 50;
+  let inc = size*0.45
+  stroke(pix[0] - shadow, pix[1] - shadow, pix[2] - shadow, 50);//shadow
+  line(x + inc, y + inc, x + (size-inc), y + (size-inc));
+  shadow += 20;
+  stroke(pix[0] - shadow, pix[1] - shadow, pix[2] - shadow, 50);//shadow
+  line(x + inc + 5, y + inc + 5, x + (size-inc), y + (size-inc));
+
+  stroke(pix);
   line(x + size, y, x, y + size);
 }
 
-function embroider(x, y, size){
-//random factor in here
-  line (x, y  - (size * random(1,3)), x, y + (size * random(1,3)));
+function drawHoles(){
+  for(let x = 0; x < 1920; x+=pointSize) {
+    for (let y = 0; y < 640; y += pointSize) {
+      let mask = maskImg.get(x,y);
+
+       if (mask[0] < 128) {
+        stroke(0,0,0,80);
+        strokeWeight(2);
+        point(x,y);
+      }
+    }
+}
 }
 
+function embroider(){
+  for(let i=0;i<1500;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+
+    if(mask[0] < 128) {
+      continue;
+    }
+
+    fill(pix);
+    stroke(pix[0], pix[1], pix[2], pix)
+    strokeWeight(5);
+    //if (mask[0] > 128) {
+      push();
+      translate(x,y);
+      let r = random(pointSize*4,pointSize*20);
+      line(0,0-pointSize*(random(3,5)),0,random(pointSize*4,pointSize*20));
+      //ellipse(0,0,pointSize/2,random(pointSize*4,pointSize*20));
+
+      pop();
+    //}
+  }
+}
 
 function keyTyped() {
   if (key == '!') {
