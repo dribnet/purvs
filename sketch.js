42111c82207afd0dcb744d0feaa7767785f7a95e
cindyrrose <61666889+cindyrrose@users.noreply.github.com>
2020-06-13 13:11:06 +1200

    update
    update

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg = null;
 let renderCounter = 0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,13 +18,13 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(237, 62, 231);
+  background(60, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i = 0; i < 4000; i++) {
+  for(let i = 0; i < 5000; i++) {
 
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -35,22 +35,23 @@ function draw () {
     stroke(pix);
 
 
-    if(mask[0] >= 0 && mask[0] < 150) {
+    if(mask[0] >= 0 && mask[0] < 150) {            // black mask
       let pointSize = 12;
       push();
       strokeWeight(5);
       line(x, y, x + pointSize - 30, y + 10);
-      //lineBG(x, y, x + pointSize + 30, y - 10);
       pop();
     }
-    else if(mask[0] >= 201 && mask[0] < 250) {
+    else if(mask[0] >= 201 && mask[0] < 250) {      // grey mask
       let pointSize = 50;
-      line(x, y, x + pointSize, y + 20);          // draws lines for background
+      push();
+      strokeWeight(3);
+      line(x, y, x + pointSize, y + 20);
+      pop();
     }
-    else {
+    else {                                          // white mask
       let pointSize = 10;
-    //  lineBG(x, y, x + pointSize, y + 20);
-      ellipse(x, y, pointSize, pointSize);        // draws circles for jellyfish
+      ellipse(x, y, pointSize, pointSize);
     }
   }
 
