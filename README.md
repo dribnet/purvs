4b7f3403864fdc010a790b0c4d3b363a5244cc78
Sanika Ratanpal <ratansani@myvuw.ac.nz>
2021-03-23 11:15:42 +1300

    First class Working on the sample code. Intro to project 2.

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 ## MDDN 242 2021 Assignment 2
 
-(Replace this README with information about your alphabet. This is an example.)
-
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
-
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+Played around with the sample code. I didnt change too much as i am so confused about all of it. 
+By next class i want to have an idea of where i want to go with this project. 
 
--- sketch.js
+++ sketch.js
@@ -14,27 +14,27 @@ const canvasHeight = 500;
 
 const letterA = {
   "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offsetx": 35,
+  "offsety": 70,
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "size": 10,
+  "offsetx": 140,
+  "offsety": 70
 }
 
 const letterC = {
   "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "offsetx": 50,
+  "offsety": 25
 }
 
 const backgroundColor  = "#e3eded";
 const strokeColor      = "#233f11";
 
 const darkBlue  = "#199cff";
-const lightBlue  = "#59ccff";
+//const lightBlue  = "#59ccff";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -66,14 +66,14 @@ function draw () {
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
   let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let pos2x = posx + letterData["offsetx"];
 
   // draw two circles
   fill(darkBlue);
-  ellipse(posx, posy, 150, 150);
-  fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  rect(posx, posy, 150, 150);
+  fill(backgroundColor);
+  rect(pos2x, pos2y, size2, size2);
 }
 
 function keyTyped() {
