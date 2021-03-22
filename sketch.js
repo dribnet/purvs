6fff1e1cbc462caf6c66758786af942ffafb3558
Daniel Khamis <ihealthdesigner@gmail.com>
2021-03-23 11:22:40 +1300

    First Sketch

--- sketch.js
+++ sketch.js
@@ -13,28 +13,37 @@ const canvasHeight = 500;
  */
 
 const letterA = {
+
   "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offsetx": 60,
+  "offsety": 60,
+  "bool": true,
+  "rotation": 45
+
+
 }
 
 const letterB = {
-  "size": 150,
+  "size": 200,
   "offsetx": 0,
-  "offsety": -145
+  "offsety": -145,
+  "bool": false,
+  "rotation": 88
 }
 
 const letterC = {
   "size": 100,
   "offsetx": 30,
-  "offsety": 0
+  "offsety": 0,
+  "bool": false,
+  "rotation": 170
 }
 
 const backgroundColor  = "#e3eded";
 const strokeColor      = "#233f11";
 
-const darkBlue  = "#199cff";
-const lightBlue  = "#59ccff";
+const darkBlue  = "#fcba03";
+const lightBlue  = "#03cefc";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -44,6 +53,7 @@ function setup () {
   // color/stroke setup
   stroke(strokeColor);
   strokeWeight(4);
+  angleMode(DEGREES);
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
@@ -61,6 +71,10 @@ function draw () {
   drawLetter(center_x - 250, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
   drawLetter(center_x + 250, center_y, letterC);
+
+  // arc(50,50, 100, 100, 0, PI + QUARTER_PI, CHORD);
+
+
 }
 
 function drawLetter(posx, posy, letterData) {
@@ -74,6 +88,24 @@ function drawLetter(posx, posy, letterData) {
   ellipse(posx, posy, 150, 150);
   fill(lightBlue);
   ellipse(pos2x, pos2y, size2, size2);
+  
+  push();
+  rectMode(CENTER);
+  translate(posx, posy);
+  rotate(letterData["rotation"]);
+  rect(0,0, 55, 55);
+  pop();
+
+  if(letterData["bool"]){
+  push();
+  strokeWeight(12.0);
+  strokeCap(ROUND);
+  fill(255,0,0);
+  arc(posx, posy, 80, 80, 50, 275, CHORD);
+  pop();
+  }
+  
+
 }
 
 function keyTyped() {
