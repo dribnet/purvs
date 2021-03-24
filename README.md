4fbae90484c854fbe4a3ee2673515a4acfafb3bf
Vivian Chang <viviuntiy@gmail.com>
2021-03-25 11:23:44 +1300

    Sketch update
    Got a new idea for my sketch

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 :memo:  
 The lighter purple ellipse rotates around the centre based on the feauture of the letter. For example, A could be seen as a triangle / fan, so it has a fan shape in the middle. B has two little half circle on the side, and C is like a complete half circle.  *working on putting triangles* 
 
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
-
+Updating my sketch today, I chose to use rectangles, triangles and ellipses to create my letters.  
--- sketch.js
+++ sketch.js
@@ -13,36 +13,43 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 190,
-  "offsetx": 0,
-  "offsety": 0,
-  "start": 30,
-  "end": 150,
-  "Tri": 180,
+  
+  "offsetx": 50,
+  "offsety": 90,
+  "Rwidth": 30,
+  "Rheight": 50,
+  "size": 30,
+  "CircleX": 65,
+  "CircleY": 140,
 }
 
 const letterB = {
-  "size": 100,
-  "offsetx": 50,
-  "offsety": 10,
-  "start": 0,  
-  "end": 120,
-  "Tri": 470,
+"offsetx": 200,
+  "offsety": 90,
+  "Rwidth": 30,
+  "Rheight": 50,
+  "size": 30,
+  "CircleX": 240,
+  "CircleY": 140,
+
 }
 
 const letterC = {
-  "size": 180,
-  "offsetx": 0,
-  "offsety": 0, 
-  "start": 90, 
-  "end": 270,
-  "Tri": 630,
+"offsetx": 350,
+  "offsety": 90,
+  "Rwidth": 30,
+  "Rheight": 50,
+  "size": 50,
+  "CircleX": 345,
+  "CircleY": 140,
+
 }
 
 const backgroundColor  = "#e3eded";
 const strokeColor      = "#233f11";
 
 function setup () {
+  
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
@@ -64,27 +71,37 @@ angleMode(DEGREES);
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
+  drawLetter(0, 0, letterA);
+  drawLetter(0, 0, letterB);
+  drawLetter(0, 0, letterC);
 }
 
 function drawLetter(posx, posy, letterData) {
-  noStroke();
-  // determine parameters for second circle
-  let size2 = letterData["size"];
+   noStroke();
+  // // determine parameters for second circle
+  let RecX = letterData["Rwidth"];
+  let Recy = letterData["Rheight"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
-  let angleS = letterData["start"];
-  let angleE = letterData["end"];
-  let TriX = letterData["Tri"];
-  // draw two circles
-  fill("#9c4887");
-  ellipse(posx, posy, 150, 150);
+  let CircleX =letterData["CircleX"];
+  let CircleY = letterData["CircleY"];
+  let CircleS = letterData["size"];
+  
+  // // draw two circles
+   fill("#9c4887");
+  // ellipse(posx, posy, 150, 150);
+   
+  // arc(pos2x, pos2y, size2, size2, angleS, angleE);
+
+  rect(pos2x, pos2y, RecX, Recy);
+  ellipse(CircleX, CircleY-50, CircleS);
+  ellipse(CircleX, CircleY, CircleS);
   fill("#ba7599");
-  arc(pos2x, pos2y, size2, size2, angleS, angleE);
-  fill("#fae1f6");
-  triangle(50+TriX, 290, 0+TriX, 250, 90+TriX, 250);
+  beginShape();
+  vertex(65, 95);
+  vertex(35, 130);
+  vertex(95, 130);
+  endShape();
 }
 
 function keyTyped() {
