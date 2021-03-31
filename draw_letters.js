6ed3a40454a2d3a38e6cd602b4cd38cec73e27cf
boylesz <boyleszavi@myvuw.ac.nz>
2021-04-01 11:27:09 +1300

    working exhibition

--- draw_letters.js
+++ draw_letters.js
@@ -4,36 +4,40 @@ var systemLineColor = "#ffffff";
 var systemBoxColor = "#00c800";
 
 /* internal constants */
-const strokeColor  = "#ffffff";
+const strokeColor = "#ffffff";
+let drawWidth = 960;
+let drawHeight = 500;
 
 function drawLetter(letterData) {
   let posx = 50;
   let posy = 150;
   let radius = 50;
 
+  stroke(strokeColor);
+
   // draw points around the circle
   for (let i = 0; i < 8; i++) {
     for (let j = 1; j <= 5; j++) {
-      if (letterData["point"+j] == i) {
+      if (letterData["point" + j] == i) {
         strokeWeight(8);
       } else {
         strokeWeight(random(1, 4));
       }
-      point(radius * cos(i * PI/4) + posx, radius * sin(i * PI/4) + posy);
+      point(radius * cos(i * PI / 4) + posx, radius * sin(i * PI / 4) + posy);
     }
   }
 
-  strokeWeight(2);
+  strokeWeight(1.5);
   for (let i = 1; i <= 5; i++) {
-    let lineStartX = radius * cos(letterData["point"+i] * PI/4) + posx;
-    let lineStartY = radius * sin(letterData["point"+i] * PI/4) + posy;
+    let lineStartX = radius * cos(letterData["point" + i] * PI / 4) + posx;
+    let lineStartY = radius * sin(letterData["point" + i] * PI / 4) + posy;
     let lineEndX, lineEndY;
     if (i == 5) {
       lineEndX = lineStartX;
       lineEndY = lineStartY;
     } else {
-      lineEndX = radius * cos(letterData["point"+(i+1)] * PI/4) + posx;
-      lineEndY = radius * sin(letterData["point"+(i+1)] * PI/4) + posy;
+      lineEndX = radius * cos(letterData["point" + (i + 1)] * PI / 4) + posx;
+      lineEndY = radius * sin(letterData["point" + (i + 1)] * PI / 4) + posy;
     }
     line(lineStartX, lineStartY, lineEndX, lineEndY);
   }
