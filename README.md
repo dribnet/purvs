ed110cba2dbb21db6488ee873141eb55c5d7ee17
Li Yang <yangli1@myvuw.ac.nz>
2021-04-14 18:20:07 +1200

    This is updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## MDDN 242 2021 Assignment 2
 
-(Replace this README with information about your alphabet. This is an example.)
+This is just followed by the lecturer and make some changes.
 
 Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
 
--- sketch.js
+++ sketch.js
@@ -13,21 +13,27 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "size": 70,
+  "offsetx": 80,
+  "offsety": 45,
+  "start":90,
+  "end":270
 }
 
 const letterB = {
   "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "offsetx": -70,
+  "offsety": -100,
+  "start":270,
+  "end":90
 }
 
 const letterC = {
   "size": 100,
   "offsetx": 30,
-  "offsety": 0
+  "offsety": 0,
+  "start":0,
+  "end":180
 }
 
 const backgroundColor  = "#e3eded";
@@ -47,6 +53,7 @@ function setup () {
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
+  angleMode(DEGREES);
 }
 
 function draw () {
@@ -69,11 +76,14 @@ function drawLetter(posx, posy, letterData) {
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
 
+  let startAngle = letterData["start"];
+  let stopAngle = letterData["end"];
+
   // draw two circles
   fill(darkBlue);
   ellipse(posx, posy, 150, 150);
   fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  arc(pos2x, pos2y, size2, size2, startAngle, stopAngle, CHORD);
 }
 
 function keyTyped() {
