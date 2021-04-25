c662ada4c6a3e0903d41db740ca23d3b0133fc57
Laura Garcia <laura.garcia05051998@gmail.com>
2021-04-25 21:05:16 +1200

    I updated the interpolation and the purview.json file

--- README.md
+++ README.md
@@ -1,28 +1,19 @@
-# Feedback
-Tweeked my letters to ressemble the bahaus movement a bit more. To do so, I added a stroke arround each letter as the bauhaus movement usually as a bit of black.
-I also chose to change the background colour from black to white for the overall design to go easy on our eyes.
+## Interpolations
 
-# Interpolations
-I started playing around with interpolations, making the shape sizes go towards the default before moving to the new shape size.
+The size of each element goes back to the default size before going towards the new size. This helps see the change in letter even when you press the same one twice in a row. I also gives a nice bounciness to the overall interpolation of the letter.
+The interpolation of the arc and rectangle's angles are left to default as they did not look nice when I changed them.
 
-# other student's work
-Found that another student has a very similar font to mine!
-We had a talk and her idea is very interesting as well.
-She was trying to recreate the pop art movement in her font whereas mine is inspired by Bauhaus.
+## Inspiration
 
-# Other comments
-My "w" and "M" are slightly out of the boxes
-But it seems fine on the exhibition view.
-
-
-# Inspiration
 This alphabet takes inspiration from [**Wassily Kandinsky**](https://www.wassilykandinsky.net), a Russian painter and pioneer of abstract art (1866-1944).
 The [**Bauhaus art**](https://i.pinimg.com/originals/af/be/fb/afbefbd7da9a1aea6ddf6237a48f7b50.jpg) movement inspires the colour palette because Kandinsky took part in it.
 
-# Letters composition
+## Letters composition
+
 Each of my letters is composed of a circle, an arc and a rectangle. The size and position of each shape vary, the arc start and end, the rectangle rotation, and the rectangle rotation.
 
-# Parameters
+## Parameters
+
 There are twelve parameters per letter:
   * `size` : height of the rectangle
   * `recOffsetx` : x offset of the rectangle
--- draw_letters.js
+++ draw_letters.js
@@ -1,9 +1,9 @@
 
 /***CHANGING THE COLOURS OF THE SYSTEM***/
 
-var systemBackgroundColor = "#fffff9"
-var systemLineColor = "#4ecdc4";
-var systemBoxColor = "#ff8c42";
+var systemBackgroundColor = "#fffff9" //off-white
+var systemLineColor = "#4ecdc4"; //blue
+var systemBoxColor = "#ff8c42"; //orange
 
 
 
@@ -13,44 +13,46 @@ function drawLetter(letterData) {
 
   angleMode(DEGREES); //angle mode to degrees
 
-// determine parameters for the ellipse
-  let size = letterData["size"];
-  let pos2x = letterData["offsetx"];
-  let pos2y = letterData["offsety"];
+  // determine the parameters for the rectangle
+   let size = letterData["size"];
+   let posx = letterData["recOffsetx"];
+   let posy = letterData["recOffsety"];
+   let rectAngle = letterData["rectAngle"];
 
-//determine parameters for the arc
-  let posx = letterData["recOffsetx"];
-  let posy = letterData["recOffsety"];
-  let size1 = letterData["size1"];
-  let size2 = letterData["size2"];
-  let pos3x = letterData["arcOffsetx"];
-  let pos3y = letterData["arcOffsety"];
-  let rectAngle = letterData["rectAngle"];
-  let angleStart = letterData["angleStart"];
-  let angleEnd = letterData["angleEnd"];
+  // determine parameters for the ellipse
+    let size1 = letterData["size1"];
+    let pos2x = letterData["offsetx"];
+    let pos2y = letterData["offsety"];
 
-  noStroke();
+  //determine parameters for the arc
+    let size2 = letterData["size2"];
+    let pos3x = letterData["arcOffsetx"];
+    let pos3y = letterData["arcOffsety"];
+    let angleStart = letterData["angleStart"];
+    let angleEnd = letterData["angleEnd"];
 
-//Draw an ellipse
-  fill('#db1c5f'); //red
-  ellipseMode(CORNER);
-  ellipse(pos2x, pos2y, size1, size1);
+
+  //Draw an ellipse
+    noStroke();
+    fill('#db1c5f'); //red
+    ellipseMode(CORNER);
+    ellipse(pos2x, pos2y, size1, size1);
 
 
   //Draw an arc
-  fill('#4c5bd8');//blue
-  arc(pos3x, pos3y, size2, size2, angleStart, angleEnd);
+    fill('#4c5bd8'); //blue
+    arc(pos3x, pos3y, size2, size2, angleStart, angleEnd);
 
 
   // draw a rectangle with round corners and rotate it
+    fill("#fadf43"); //yellow
+    push();
+    translate(posx,posy);
+    rotate(rectAngle);
+    rect(0, 0, 15, size, 20);
+    pop();
 
-  fill("#fadf43");//yellow
-
-  push();
-  translate(posx,posy);
-  rotate(rectAngle);
-  rect(0, 0, 15, size, 20);
-  pop();
+  //Calling functions
 
   bubbles(posx, posy, pos2x, pos2y, pos3x, pos3y);
 
@@ -92,33 +94,32 @@ function lines(posx, posy, pos2x, pos2y, pos3x, pos3y, size, size1, size2, angle
    stroke('#000000'); // black stroke if the size is other than 0
   }
 
-//Draw an ellipse
-  ellipseMode(CORNER);
-  ellipse(pos2x-5, pos2y-5, size1, size1);
+  //Draw an ellipse
+    ellipseMode(CORNER);
+    ellipse(pos2x-5, pos2y-5, size1, size1);
 
-  if(size2==0){ //if the size of the arc is 0 then no stroke applied to the shape
-    noStroke();
-  } else {
-   stroke('#000000'); // black stroke if the size is other than 0
-  }
+    if(size2==0){ //if the size of the arc is 0 then no stroke applied to the shape
+      noStroke();
+    } else {
+    stroke('#000000'); // black stroke if the size is other than 0
+    }
 
-//Draw an arc
-  arc(pos3x-5, pos3y-5, size2, size2, angleStart, angleEnd);
+  //Draw an arc
+    arc(pos3x-5, pos3y-5, size2, size2, angleStart, angleEnd);
 
+    if(size==0){ //if the size of the rectangle is 0 then no stroke applied to the shape
+      noStroke();
+    }else{
+      stroke('#000000'); // black stroke if the size is other than 0
+    }
 
-  if(size==0){ //if the size of the rectangle is 0 then no stroke applied to the shape
-    noStroke();
-  }else{
-   stroke('#000000'); // black stroke if the size is other than 0
-  }
+  // draw a rectangle with round corners and rotate it
 
-// draw a rectangle with round corners and rotate it
-
-  push();
-  translate(posx,posy);
-  rotate(rectAngle);
-  rect(-5, -5, 15, size, 20);
-  pop();
+    push();
+    translate(posx,posy);
+    rotate(rectAngle);
+    rect(-5, -5, 15, size, 20);
+    pop();
 
 }
 
@@ -129,15 +130,15 @@ function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
   let defaultChar = getObjFromChar("default");
 
-// making the sizes of the rectangle, ellipse and arc go back to the default before interpolating towards the new size
-//thanks to this, you can still see the letter change when you press the same letter
-  if(percent < 50){ 
+  // making the sizes of the rectangle, ellipse and arc go back to the default before interpolating towards the new size
+  //thanks to this, you can still see the letter change when you press the same letter
+    if(percent < 50){ 
 
-     new_letter["size"] = map(percent, 0, 50, oldObj["size"], defaultChar["size"]); 
+       new_letter["size"] = map(percent, 0, 50, oldObj["size"], defaultChar["size"]); 
 
-     new_letter["size1"] = map(percent, 0, 50, oldObj["size1"], defaultChar["size1"]);
+       new_letter["size1"] = map(percent, 0, 50, oldObj["size1"], defaultChar["size1"]);
 
-     new_letter["size2"] = map(percent, 0, 50, oldObj["size2"], defaultChar["size2"]);
+       new_letter["size2"] = map(percent, 0, 50, oldObj["size2"], defaultChar["size2"]);
 
 
   } else {
@@ -149,28 +150,31 @@ function interpolate_letter(percent, oldObj, newObj) {
     new_letter["size2"] = map(percent, 51, 100, defaultChar["size2"], newObj["size2"]);
   }
 
-//rest of the interpolations
+  //rest of the interpolations
 
-  new_letter["recOffsetx"] = map(percent, 30, 100, oldObj["recOffsetx"], newObj["recOffsetx"]);
-  new_letter["recOffsety"] = map(percent, 30, 100, oldObj["recOffsety"], newObj["recOffsety"]);
+    new_letter["recOffsetx"] = map(percent, 30, 100, oldObj["recOffsetx"], newObj["recOffsetx"]);
+    new_letter["recOffsety"] = map(percent, 30, 100, oldObj["recOffsety"], newObj["recOffsety"]);
 
-  new_letter["offsetx"] = map(percent, 30, 100, oldObj["offsetx"], newObj["offsetx"]);
-  new_letter["offsety"] = map(percent, 30, 100, oldObj["offsety"], newObj["offsety"]);
+    new_letter["offsetx"] = map(percent, 30, 100, oldObj["offsetx"], newObj["offsetx"]);
+    new_letter["offsety"] = map(percent, 30, 100, oldObj["offsety"], newObj["offsety"]);
 
 
-  new_letter["arcOffsetx"] = map(percent, 30, 100, oldObj["arcOffsetx"], newObj["arcOffsetx"]);
-  new_letter["arcOffsety"] = map(percent, 30, 100, oldObj["arcOffsety"], newObj["arcOffsety"]);
+    new_letter["arcOffsetx"] = map(percent, 30, 100, oldObj["arcOffsetx"], newObj["arcOffsetx"]);
+    new_letter["arcOffsety"] = map(percent, 30, 100, oldObj["arcOffsety"], newObj["arcOffsety"]);
 
-//smoothly changing the angle from the old to the new arc
-  new_letter["rectAngle"] = map(percent, 0, 100, oldObj["rectAngle"], newObj["rectAngle"]);
-  new_letter["angleStart"] = map(percent, 0, 100, oldObj["angleStart"], newObj["angleStart"]);
-  new_letter["angleEnd"] = map(percent, 0, 100, oldObj["angleEnd"], newObj["angleEnd"]);
+  //smoothly changing the angle from the old to the new arc
+
+    new_letter["rectAngle"] = map(percent, 0, 100, oldObj["rectAngle"], newObj["rectAngle"]);
+    new_letter["angleStart"] = map(percent, 0, 100, oldObj["angleStart"], newObj["angleStart"]);
+    new_letter["angleEnd"] = map(percent, 0, 100, oldObj["angleEnd"], newObj["angleEnd"]);
 
   return new_letter;
 }
 
+/***EXIBITION WORDS***/
+
 var swapWords = [
-  "WASSILYK", //name artist that inspired the font
+  "WASSILYK", //name of the artist that inspired the font
   "BAUHAUS!", //movement that inspired the font
   "BY LAURA" // my name!
 ]
