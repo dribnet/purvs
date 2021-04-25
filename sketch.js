62af949f4e3d67c1a7d8ae8aadfe598a21efde9b
Jierui Zhuge <mszhugejierui@163.com>
2021-04-25 16:44:39 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,67 +13,117 @@ const canvasHeight = 500;
  */
 
 const letterA = {
+
+  "topRectWidth": 130,
+  "middleRectWidth": 130,
+  "bottomRectWidth": 0,
+  "topRectHeight": 30,
+  "middleRectHeight": 30,
+  "bottomRectHeight": 0,
+  "leftRectWidth": 30,
+  "leftRectHeight": 180,
+  "rightRectWidth": 30,
+  "rightRectHeight": 180,
   "size": 80,
   "offsetx": 0,
-  "offsety": 35
+  "offsety": 35,
+  "rotateValue": 90
+
 }
 
 const letterB = {
+
+  "topRectWidth": 130,
+  "middleRectWidth": 130,
+  "bottomRectWidth": 130,
+  "topRectHeight": 30,
+  "middleRectHeight": 30,
+  "bottomRectHeight": 30,
+  "leftRectWidth": 30,
+  "leftRectHeight": 180,
+  "rightRectWidth": 30,
+  "rightRectHeight": 180,
   "size": 150,
   "offsetx": 0,
   "offsety": -145
+
+
+
 }
 
 const letterC = {
+
+  "topRectWidth": 130,
+  "middleRectWidth": 0,
+  "bottomRectWidth": 130,
+  "topRectHeight": 30,
+  "middleRectHeight": 0,
+  "bottomRectHeight": 30,
+  "leftRectWidth": 30,
+  "leftRectHeight": 180,
+  "rightRectWidth": 0,
+  "rightRectHeight": 0,
   "size": 100,
   "offsetx": 30,
   "offsety": 0
+
 }
 
-const backgroundColor  = "#e3eded";
-const strokeColor      = "#233f11";
 
-const darkBlue  = "#199cff";
-const lightBlue  = "#59ccff";
 
 function setup () {
+
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
   // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
+  stroke(25,25,112);
+  angleMode(DEGREES);
+  strokeWeight(15);
 
-  // with no animation, redrawing the screen is not necessary
-  noLoop();
+}
+
+function drawLetter(posx, posy, letterData) {
+  // determine parameters for second circle
+  // let i = letterData["shapeType"];
+  // let size2 = letterData["size"];
+  let pos2x = posx + letterData["offsetx"];
+  let pos2y = posy + letterData["offsety"];
+  let topRectW = letterData["topRectWidth"];
+  let middleRectW = letterData["middleRectWidth"];
+  let botRectW = letterData["bottomRectWidth"];
+  let topRectH = letterData["topRectHeight"];
+  let middleRectH = letterData["middleRectHeight"];
+  let botRectH = letterData["bottomRectHeight"];
+  let leftRectW = letterData["leftRectWidth"];
+  let leftRectH = letterData["leftRectHeight"];
+  let rightRectW = letterData["rightRectWidth"];
+  let rightRectH = letterData["rightRectHeight"];
+
+  fill(255,192,203);
+    rect(posx,posy - 80,leftRectW,leftRectH); //left strip (A,B,C)
+    rect(posx + 100, posy - 80, rightRectW, rightRectH); //right strip (A,B)
+    rect(posx,posy,middleRectW, middleRectH); //middle  strip (A,B)
+    ellipse(posx+65,posy-70,130,40); //top (A,B,C)
+    ellipse(posx+65,posy+100,botRectW, botRectH); //bottom  (B,C)
+    
 }
 
 function draw () {
-  // clear screen
-  background(backgroundColor);
+
+  
+  background(176,196,222);
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;  
   let center_y = canvasHeight / 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, letterA);
+  drawLetter(center_x - 200, center_y, letterA);
   drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
-}
+  drawLetter(center_x + 200, center_y, letterC);
 
-function drawLetter(posx, posy, letterData) {
-  // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
-
-  // draw two circles
-  fill(darkBlue);
-  ellipse(posx, posy, 150, 150);
-  fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
 }
 
 function keyTyped() {
