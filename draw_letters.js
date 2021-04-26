364fc56daf7d754973f545869263293908f38e1d
Xiaoya Zhu <https://github.com/Candice-Zhu>
2021-04-26 21:28:38 +0800

    Update draw_letters.js

--- draw_letters.js
+++ draw_letters.js
@@ -1,7 +1,11 @@
 /* these are optional special variables which will change the system */
-var systemBackgroundColor = "#9FE2BF";
-var systemLineColor = "#FF7F50";
-var systemBoxColor = "#DFFF00";
+const colorFront1  = "#F89880";//center line
+const colorFront2  = "#A7C7E7";//top line
+const colorFront3  = "#96DED1";//bottom line
+const colorFront4  = "#FFC0CB";//shape background
+
+
+const colorStroke  = "#40E0D0";
 
 /*
  * Draw the letter given the letterData
@@ -10,70 +14,80 @@ var systemBoxColor = "#DFFF00";
  * following bounding box guideline:
  * from (0,0) to (100, 200)
  */
- function drawLetter(letterData) {
-   let w = 100;
-   let h = 200;
+function drawLetter(letterData) {
+  // color/stroke setup
+  stroke(colorStroke);
+  strokeWeight(2);
 
-   let curY=0;
-   stroke(0);
-   if(!letterData.structure)return;
-   fill(colorFront1);
+  // determine parameters for second circle
+  let posx1 = 50+letterData["x1"];
+  let posy1 = 100+letterData["y1"];
+  let posw1 = letterData["w1"];
+  let posh1 = letterData["h1"];
 
-   let r=0;
-   let c=0;
-   if(letterData.structure[r][c] === 2){
-     fill(colorFront2);
-   }
+  let posx2 = 50+letterData["x2"];
+  let posy2 = 100+letterData["y2"];
+  let posw2 = letterData["w2"];
+  let posh2 = letterData["h2"];
 
-   for (let r = 0; r < letterData.structure.length; r++) {
-     let curX=0;
-     for (let c = 0; c < letterData.structure[r].length; c++) {
-         if(letterData.structure[r][c] === 1)
-           rect(curX,curY,7,7,2);
-         curX+=7;
-      }
-      curY+=7;
+  let posx3 = 50+letterData["x3"];
+  let posy3 = 100+letterData["y3"];
+  let posw3 = letterData["w3"];
+  let posh3 = letterData["h3"];
 
-   }
 
- }
+  let posx4 = 50+letterData["x4"];
+  let posy4 = 100+letterData["y4"];
+  let posw4 = letterData["w4"];
+  let posh4 = letterData["h4"];
 
- function interpolate_letter(percent, oldObj, newObj) {
-   let new_letter = {};
-   let cur_structure = [
-     [0, 0, 1, 1,1,1,1,1,1,1,1,1,0,0],
-     [0, 1, 1, 1,1,1,1,1,1,1,1,1,1,0],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [1, 1, 1, 1,0,0,1,1,1,1,1,1,1,1],
-     [1, 1, 1, 1,0,0,1,1,1,1,1,1,1,1],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [1, 1, 1, 1,1,1,1,1,0,0,1,1,1,1],
-     [1, 1, 1, 1,1,1,1,1,0,0,1,1,1,1],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [1, 1, 0, 0,1,1,0,0,1,1,0,0,1,1],
-     [0, 1, 1, 1,1,1,1,1,1,1,1,1,1,0],
-     [0, 0, 1, 1,1,1,1,1,1,1,1,1,0,0]];
-   let old_structure = oldObj["structure"];
-   let new_structure = newObj["structure"];
+  // draw two circles
+  push()
+  rectMode(CENTER)
+  fill(colorFront4)
+  rect(50, 100, 100, 200, 50)
 
-     for(let i=0; i<cur_structure.length; i++) {
-       let cur_row = cur_structure[i];
-       let old_row = old_structure[i];
-       let new_row = new_structure[i];
-       for(let j=0; j<cur_row.length; j++) {
-         cur_row[j] = map(percent, 0, 100, old_row[j], new_row[j]);
-       }
-     }
-     new_letter["structure"] = cur_structure;
+  fill(colorFront1);
+  rect(posx1, posy1, posw1, posh1, 15)
 
-   // new_letter["structure"]    = map(percent, 0, 100, oldObj["structure"], newObj["structure"]);
-   // new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
-   // new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
-   return new_letter;
- }
+  fill(colorFront2);
+  rect(posx2, posy2, posw2, posh2, 15)
 
+  fill(colorFront3);
+  rect(posx3, posy3, posw3, posh3, 15)
+  rect(posx4, posy4, posw4, posh4, 15)
+  pop()
+
+  //   fill(colorFront1);
+  // ellipse(50, 150, 75, 75);
+  // fill(colorFront2);
+  // ellipse(pos2x, pos2y, size2, size2);
+}
+
+function interpolate_letter(percent, oldObj, newObj) {
+  let new_letter = {};
+  new_letter["x1"] = map(percent, 0, 100, oldObj["x1"], newObj["x1"]);
+  new_letter["y1"] = map(percent, 0, 100, oldObj["y1"], newObj["y1"]);
+  new_letter["w1"] = map(percent, 0, 100, oldObj["w1"], newObj["w1"]);
+  new_letter["h1"] = map(percent, 0, 100, oldObj["h1"], newObj["h1"]);
+
+  new_letter["x2"] = map(percent, 0, 100, oldObj["x2"], newObj["x2"]);
+  new_letter["y2"] = map(percent, 0, 100, oldObj["y2"], newObj["y2"]);
+  new_letter["w2"] = map(percent, 0, 100, oldObj["w2"], newObj["w2"]);
+  new_letter["h2"] = map(percent, 0, 100, oldObj["h2"], newObj["h2"]);
+
+  new_letter["x3"] = map(percent, 0, 100, oldObj["x3"], newObj["x3"]);
+  new_letter["y3"] = map(percent, 0, 100, oldObj["y3"], newObj["y3"]);
+  new_letter["w3"] = map(percent, 0, 100, oldObj["w3"], newObj["w3"]);
+  new_letter["h3"] = map(percent, 0, 100, oldObj["h3"], newObj["h3"]);
+
+  new_letter["x4"] = map(percent, 0, 100, oldObj["x4"], newObj["x4"]);
+  new_letter["y4"] = map(percent, 0, 100, oldObj["y4"], newObj["y4"]);
+  new_letter["w4"] = map(percent, 0, 100, oldObj["w4"], newObj["w4"]);
+  new_letter["h4"] = map(percent, 0, 100, oldObj["h4"], newObj["h4"]);
+
+  return new_letter;
+}
 
 var swapWords = [
   "ABBAABBA",
