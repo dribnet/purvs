0f711c7b985d3565473084f6cba88e56f846303d
Suarim Ahmed <ahmedsuarim16@gmail.com>
2021-04-27 21:32:04 +1200

    unfortunately have been extremely sick this past week and a half and with a big 40% assignment for another class, I have started this assignment slightly later than I would have liked.
    For my design I chose a colour pallete that was refreshing and colorful. For my A,B and C letters I chose to go wih a simple shape that works with arcs, and almost represents a version of the pacman character.

--- sketch.js
+++ sketch.js
@@ -13,40 +13,44 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
+  "size": 120,
   "offsetx": 0,
-  "offsety": 35
+  "offsety": 0,
+  "start": 140,
+  "mode": 400
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "size": 190,
+  "offsetx": -10,
+  "offsety": 120,
+  "start": 270,
+  "mode": 0
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "size": 120,
+  "offsetx": 0,
+  "offsety": 0,
+  "start": 410,
+  "mode": -50
 }
 
-const backgroundColor  = "#e3eded";
-const strokeColor      = "#233f11";
+const backgroundColor  = "#5BC8AC"; // turquoise
 
-const darkBlue  = "#199cff";
-const lightBlue  = "#59ccff";
+const pink  = "#f18d9e";
+const aqua  = "#98dbc6";
+const yellow = "#e6d72a";
 
 function setup () {
   // create the drawing canvas, save the canvas element
   main_canvas = createCanvas(canvasWidth, canvasHeight);
   main_canvas.parent('canvasContainer');
 
-  // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
-
   // with no animation, redrawing the screen is not necessary
+  noStroke();
   noLoop();
+  angleMode(DEGREES);
 }
 
 function draw () {
@@ -66,14 +70,18 @@ function draw () {
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
   let size2 = letterData["size"];
+
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
 
+  let arcStart = letterData["start"];
+  let arcMode = letterData["mode"];
+
   // draw two circles
-  fill(darkBlue);
+  fill(pink);
   ellipse(posx, posy, 150, 150);
-  fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  fill(aqua);
+  arc(pos2x, pos2y, size2, size2, arcStart, arcMode);
 }
 
 function keyTyped() {
