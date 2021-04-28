b7f97845333bc37c3c67ed0478f1d94f0585c7a1
christopherterrywendt <wendtchri@myvuw.ac.nz>
2021-04-28 19:17:06 +1200

    debugging commit

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## MDDN 242 2021 Assignment 2
 
-PROTOCOS STYLISED
-Welcome to the stylised version of my Protocos font. Here I have flipped the colour scheme completely, and using more custom shape functions in draw_letters.js I have added a line offset to the first two coordinates of the first triangle for added flare and a drop shadow for added pop and dynamism.
-This version of my font exists to provide a palette cleanser from the usual red and black colour palette in all of my other versions of Protocos, and strays away slightly from that original anchor point Adobe Illustrator idea.
+DEBUGGING INTERPOLATION
+This block of my project exists to show my process of debugging my work. In my "Flippy Interpolation" commit you will see in the interaction and exhibition space that the interpolation between letters sees the triangles flip flop, mirror and cross over with each other. This is because I was simply using the parameter points to draw the letterforms rather than plotting each coordinate relative to an overarching rule.
+To solve this issue, I created separate functions in the draw_letters.js file, where I added three separate ellipses to each triangles points. You'll see that the red dot on the red triangle stays on the top left of each letterform, the blue dot on the same triangle stays to the right side, and the green dot stays to the bottom of the letterform. This same concept applies to the black triangle too.
+This approach helped me to switch between the alphabet and interaction ports to make sure the interpolation doesn't have any triangles awkwardly flip and mirror on themselves. It was a pretty successful endeavor into polishing my animation interpolation and I thought it would be worth showcasing how I worked around this.
+For the sake of this commit I have changed the interpolation maps to the vanilla version which was provided when this project was first handed out.
 I hope you enjoy. Christopher.
--- draw_letters.js
+++ draw_letters.js
@@ -1,13 +1,12 @@
 /* these are optional special variables which will change the system */
-var systemBackgroundColor = "#36504f";
+var systemBackgroundColor = "#c9afaf";
 var systemLineColor = "#000090";
 var systemBoxColor = "#00c800";
-var rectSize = 7;
 
 /* internal constants */
-const colourBlue  = "#5ac8c8";
-const colourWhite  = "#e0e0e0";
-const strokeColor  = "#0d0d0d";
+const colourRed  = "#a83636";
+const colourBlack  = "#1f1f1f";
+const strokeColor  = "#f2f2f2";
 /*
  * Draw the letter given the letterData
  * Letters should always be drawn with the
@@ -15,8 +14,14 @@ const strokeColor  = "#0d0d0d";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
+  // color/stroke setup
   stroke(strokeColor);
   strokeWeight(4);
+
+  // determine parameters for second circle
+  let size2 = letterData["size"];
+  let pos2x = 50  + letterData["offsetx"];
+  let pos2y = 150 + letterData["offsety"];
 
   let A1 = letterData["point1A"]; // first X-point start triangle1
   let A2 = letterData["point2A"]; // second X-point start triangle1
@@ -31,84 +36,45 @@ function drawLetter(letterData) {
   let D2 = letterData["point2D"]; // second Y-point end triangle2
   let D3 = letterData["point3D"]; // third Y-point end triangle2
 
-  // draw two customised triangles
-  customTriange1Shade(A1, B1, A2, B2, A3, B3);
-  customTriange2Shade(C1, D1, C2, D2, C3, D3)
-  fill(colourBlue);
-    customTriangle1(A1, B1, A2, B2, A3, B3);
-  fill(colourWhite);
-    customTriangle2(C1, D1, C2, D2, C3, D3);
-
+  // draw two triangles
+  fill(colourRed);
+  customTriangle1(A1, B1, A2, B2, A3, B3);
+  fill(colourBlack);
+  customTriangle2(C1, D1, C2, D2, C3, D3);
 }
-
+// the ellipses in the custom functions below are used for my debug
 function customTriangle1(A1, B1, A2, B2, A3, B3){
-  strokeCap(SQUARE);
   triangle(A1, B1, A2, B2, A3, B3);
-  push(); 
-  rectMode(CENTER);
-    fill('#0d0d0d');
-      rect(A1, B1, rectSize);
-      rect(A2, B2, rectSize);
-      rect(A3, B3, rectSize);
-    stroke(56, 140, 140);
-       line(A1 + 10, B1, A2 + 10, B2); // stylistic flare for an example commit
-  pop(); // used push pop because otherwise rectMode messes up the green boxes when you click in alphabet
-
+  fill('#ff0000');
+  ellipse(A1, B1, 20);
+  fill('#00ff00');
+  ellipse(A2, B2, 20);
+  fill('#0000ff');
+  ellipse(A3, B3, 20);
 }
-
+// the ellipses in the custom functions below are used for my debug
 function customTriangle2(C1, D1, C2, D2, C3, D3){
   triangle(C1, D1, C2, D2, C3, D3);
-  push();
-  rectMode(CENTER);
-    fill('#0d0d0d');
-      rect(C1, D1, rectSize);
-      rect(C2, D2, rectSize);
-      rect(C3, D3, rectSize);
-  pop(); // used push pop because otherwise rectMode messes up the green boxes when you click in alphabet
+  fill('#ff0000');
+  ellipse(C1, D1, 20);
+  fill('#00ff00');
+  ellipse(C2, D2, 20);
+  fill('#0000ff');
+  ellipse(C3, D3, 20);
 }
-
-function customTriange1Shade(A1, B1, A2, B2, A3, B3){ // creates a drop shadow for the first triangle
-  push();
-  noStroke();
-  fill('#002e2e');
-  triangle(A1 + 10, B1 + 10, A2 + 10, B2 + 10, A3 + 10, B3 + 10);
-  pop();
-}
-
-function customTriange2Shade(C1, D1, C2, D2, C3, D3){ // creates a drop shadow for the second triangle
-  push();
-  noStroke();
-  fill('#002e2e');
-  triangle(C1 + 10, D1 + 10, C2 + 10, D2 + 10, C3 + 10, D3 + 10);
-  pop();
-}
-
+// below is the vanilla animation
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  if(percent < 50){
-    new_letter["point1A"] = oldObj["point1A"];
-    new_letter["point1B"] = oldObj["point1B"];
-    }
-  else{
-    new_letter["point1A"] = map(percent, 51, 100, oldObj["point1A"], newObj["point1A"]);
-    new_letter["point1B"] = map(percent, 51, 100, oldObj["point1B"], newObj["point1B"]);
-    } // these maps create a slight delay in the interpolation. See "EXHIBITION" in my write-up for more
-  if(percent < 40){
-    new_letter["point3A"] = oldObj["point3A"];
-    new_letter["point3B"] = oldObj["point3B"];
-    new_letter["point1C"] = oldObj["point1C"];
-    new_letter["point1D"] = oldObj["point1D"];
-    }
-  else{
-    new_letter["point3A"] = map(percent, 41, 100, oldObj["point3A"], newObj["point3A"]);
-    new_letter["point3B"] = map(percent, 41, 100, oldObj["point3B"], newObj["point3B"]);
-    new_letter["point1C"] = map(percent, 41, 100, oldObj["point1C"], newObj["point1C"]);
-    new_letter["point1D"] = map(percent, 41, 100, oldObj["point1D"], newObj["point1D"]);
-    } // these maps create a slight delay in the interpolation. See "EXHIBITION" in my write-up for more
+  new_letter["point1A"] = map(percent, 0, 100, oldObj["point1A"], newObj["point1A"]);
   new_letter["point2A"] = map(percent, 0, 100, oldObj["point2A"], newObj["point2A"]);
+  new_letter["point3A"] = map(percent, 0, 100, oldObj["point3A"], newObj["point3A"]);
+  new_letter["point1B"] = map(percent, 0, 100, oldObj["point1B"], newObj["point1B"]);
   new_letter["point2B"] = map(percent, 0, 100, oldObj["point2B"], newObj["point2B"]);
+  new_letter["point3B"] = map(percent, 0, 100, oldObj["point3B"], newObj["point3B"]);
+  new_letter["point1C"] = map(percent, 0, 100, oldObj["point1C"], newObj["point1C"]);
   new_letter["point2C"] = map(percent, 0, 100, oldObj["point2C"], newObj["point2C"]);
   new_letter["point3C"] = map(percent, 0, 100, oldObj["point3C"], newObj["point3C"]);
+  new_letter["point1D"] = map(percent, 0, 100, oldObj["point1D"], newObj["point1D"]);
   new_letter["point2D"] = map(percent, 0, 100, oldObj["point2D"], newObj["point2D"]);
   new_letter["point3D"] = map(percent, 0, 100, oldObj["point3D"], newObj["point3D"]);
   return new_letter;
