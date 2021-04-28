0678e052e31e3624de35cde59c630fb9da420163
Ethan Sheaf-Morrison <Sheafetha@myvuw.ac.nz>
2021-04-28 22:39:08 +1200

    purview

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## MDDN 242 2021 Assignment 2
 
-My typeface was called "NEONVOLT" as it reflects t
+My typeface was called "NEONVOLT" as it reflects the synthwave inspired neon glow and the sharp and angular geometric shape design.
 
 My typeface consists of a geometric custom shape that morphs and adapts to create unqiue letterforms. The custom shape uses many of the parameters to manipulate the location of each vertices. The remaining parameters are used to translate and maniuplate the overall shape for both the blue and orange. Using two versions of the custom shape i am able to complete complicated letterforms with fewer variables. 
 
 Process
 
-The inspiration 
+The inspiration for the typeface came from a number of places included this identity by SPIN UK and generative design concepts. Originally I wanted to explore 3 dimensional isometric space (examples of these attempts can be found in previous commits). The limitations of the brief pushed me in a two dimensional direction (however a true generative and adaptive 3 dimensional design is something i would be eager to explore). The previously mentioned limitations however led me to consider a stronger symmetrical core shape that evidentally reflected the limitations of the code. This realisation was a turning point for project as embraced the angularity of the letterforms in more abstract ways and I began looking at synthy aesthetic influences that ranged from TRON to Daftpunk
 
 
 Animation
--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,6 @@
 /* these are optional special variables which will change the system */
 var systemBackgroundColor = "#000000";
-var systemLineColor = "#000090";
+var systemLineColor = "#000000";
 var systemBoxColor = "#00c800";
 
 
@@ -15,38 +15,38 @@ function drawLetter(letterData) {
 
     //Nodes - These make up the custom shape used. These can be manipulated with letters.js
 
-    let TopAxis = -75 + letterData["offsetRightTop"]
-    let TopMidAxis = -100 + letterData["offsetMidTop"]
-    let LeftAxis = -50
-    let BottomSideAxis = 75 + letterData["offsetBottom"]
-    let BottomMidAxis = BottomSideAxis + 25 + letterData["offsetBottomMid"]
-    let RightAxis = 50
-    let RightMidAxisY = BottomSideAxis + TopAxis //was 0, this makes it more adpaptive but at the cost of how far it can change. 
-    let RightMidAxisX = RightAxis + letterData["offsetMidRight"]
-    let leftMidNodeX = LeftAxis + letterData["offsetMidLeft"]
-    let leftMidNodeY = TopAxis + BottomSideAxis
+    let TopAxis = -75 + letterData["offsetRightTop"];
+    let TopMidAxis = -100 + letterData["offsetMidTop"];
+    let LeftAxis = -50;
+    let BottomSideAxis = 75 + letterData["offsetBottom"];
+    let BottomMidAxis = BottomSideAxis + 25 + letterData["offsetBottomMid"];
+    let RightAxis = 50;
+    let RightMidAxisY = BottomSideAxis + TopAxis; //was 0, this makes it more adpaptive but at the cost of how far it can change. 
+    let RightMidAxisX = RightAxis + letterData["offsetMidRight"];
+    let leftMidNodeX = LeftAxis + letterData["offsetMidLeft"];
+    let leftMidNodeY = TopAxis + BottomSideAxis;
 
     // Translation Parameters -  Parameters used to adjust and polish letter forms to create more cohesive font family
 
-    let ShapeRotation = letterData["Rotation"]
-    let ShapeTranslationX = letterData["TranslateX"]
-    let ShapeTranslationY = letterData["TranslateY"]
+    let ShapeRotation = letterData["Rotation"];
+    let ShapeTranslationX = letterData["TranslateX"];
+    let ShapeTranslationY = letterData["TranslateY"];
 
     // These variables take the Y information given to the Middle nodes and uses that to 
-    let width = letterData["offsetMidTop"] - letterData["offsetBottomMid"]
-    let WidthScaleChange = map(width, -25, 25, -.15, .05)
+    let width = letterData["offsetMidTop"] - letterData["offsetBottomMid"];
+    let WidthScaleChange = map(width, -25, 25, -.15, .05);
 
 
     //Second Shape - these are the parameters used to adjust and move the second shape that can help to create forms
 
-    let SecondShapeX = 50 + letterData["offsetsecondShapeX"]
-    let SecondShapeY = 100 + letterData["offsetsecondShapeY"]
-    let SecondShapeScale = letterData["secondShapeScale"]
-    let SecondShapeRotation = letterData["SecondShapeRotation"]
+    let SecondShapeX = 50 + letterData["offsetsecondShapeX"];
+    let SecondShapeY = 100 + letterData["offsetsecondShapeY"];
+    let SecondShapeScale = letterData["secondShapeScale"];
+    let SecondShapeRotation = letterData["SecondShapeRotation"];
 
     //Node Variables
-    let NodeSize = 10
-    let NodeTransitionSize = 2
+    let NodeSize = 10;
+    let NodeTransitionSize = 2;
 
     angleMode(DEGREES);
 
@@ -58,14 +58,14 @@ function drawLetter(letterData) {
     lineBright = color(247, 207, 255);
     c = color(215, 66, 245);
 
-    OrangeColour = color(255, 207, 53, 10)
-    GreenColour = color(4, 217, 178, 10)
+    OrangeColour = color(255, 207, 53, 10);
+    GreenColour = color(4, 217, 178, 10);
 
     c.setAlpha(10);
-    let illumination = 2 //illuminations is the strokesize used within the for loop used when making the glowing shape
+    let illumination = 2; //illuminations is the strokesize used within the for loop used when making the glowing shape
 
-    let ShapePosX = 50 + ShapeTranslationX
-    let ShapePosY = 100 + ShapeTranslationY
+    let ShapePosX = 50 + ShapeTranslationX;
+    let ShapePosY = 100 + ShapeTranslationY;
 
 
     GlowShape(); //Draws the letterform
@@ -74,7 +74,7 @@ function drawLetter(letterData) {
     function GlowShape() { //Function uses for loops and layer stacking to give the illusion of a glowing form
 
         for (let i = 0; i < 10; i++) {
-            illumination = illumination + i
+            illumination = illumination + i;
             MainShape(GreenColour, illumination, .75, ShapePosX, ShapePosY, GreenColour, 0);
             MainShape(OrangeColour, illumination, SecondShapeScale, SecondShapeX, SecondShapeY, OrangeColour, SecondShapeRotation);
         }
@@ -93,9 +93,9 @@ function drawLetter(letterData) {
         strokeWeight(NodeSize);
 
         point(LeftAxis, TopAxis);
-        point(RightAxis - 50, TopMidAxis)
+        point(RightAxis - 50, TopMidAxis);
         point(RightAxis, TopAxis);
-        point(RightMidAxisX, RightMidAxisY)
+        point(RightMidAxisX, RightMidAxisY);
         point(RightAxis, BottomSideAxis);
         point(RightAxis - 50, BottomMidAxis);
         point(LeftAxis, BottomSideAxis);
@@ -125,9 +125,9 @@ function drawLetter(letterData) {
 
         beginShape();
         vertex(LeftAxis, TopAxis);
-        vertex(RightAxis - 50, TopMidAxis)
+        vertex(RightAxis - 50, TopMidAxis);
         vertex(RightAxis, TopAxis);
-        vertex(RightMidAxisX, RightMidAxisY)
+        vertex(RightMidAxisX, RightMidAxisY);
         vertex(RightAxis, BottomSideAxis);
         vertex(RightAxis - 50, BottomMidAxis);
         vertex(LeftAxis, BottomSideAxis);
@@ -148,29 +148,25 @@ function interpolate_letter(percent, oldObj, newObj) {
 
     let new_letter = {};
 
-    let NodeSize = 10
-    let NodeTransitionSize = 2
-    let targetScale = oldObj["secondShapeScale"]+.1
-    let illuminationPulse = 5
-    let targetRotation = oldObj["Rotation"] - 1
-    let targetSquish = [-10, 10];
+    let NodeSize = 10;
+    let NodeTransitionSize = 2;
+    let targetScale = oldObj["secondShapeScale"] + .1;
+    let illuminationPulse = 5;
+    let targetRotation = oldObj["Rotation"] - 1;
+    let targetSquish = [-10, 10]; //see i know how arrays work :)
 
     if (percent < 50) {
 
-        new_letter["offsetRightTop"] = oldObj["offsetRightTop"]
-
-        new_letter["offsetMidRight"] = oldObj["offsetMidRight"]
-        new_letter["offsetMidLeft"] = oldObj["offsetMidLeft"]
-        new_letter["offsetBottom"] = oldObj["offsetBottom"]
-
+        new_letter["offsetRightTop"] = oldObj["offsetRightTop"];
+        new_letter["offsetMidRight"] = oldObj["offsetMidRight"];
+        new_letter["offsetMidLeft"] = oldObj["offsetMidLeft"];
+        new_letter["offsetBottom"] = oldObj["offsetBottom"];
 
         //Since i know these two can be manipulated to increase the size of the shape without adding another scale variable i can increase the size by manipulating these variables
         new_letter["offsetMidTop"] = map(percent, 0, 50, oldObj["offsetMidTop"], (oldObj["offsetMidTop"] + targetSquish[0]));
         new_letter["offsetBottomMid"] = map(percent, 0, 50, oldObj["offsetBottomMid"], (oldObj["offsetBottomMid"] + targetSquish[1]));
-
         new_letter["secondShapeScale"] = map(percent, 0, 50, oldObj["secondShapeScale"], targetScale);
 
-        // illumination = map(percent, 0, 100, illumination, illuminationPulse);
 
     } else {
         new_letter["offsetRightTop"] = map(percent, 51, 100, oldObj["offsetRightTop"], newObj["offsetRightTop"]);
@@ -178,22 +174,20 @@ function interpolate_letter(percent, oldObj, newObj) {
         new_letter["offsetMidLeft"] = map(percent, 51, 100, oldObj["offsetMidLeft"], newObj["offsetMidLeft"]);
         new_letter["offsetBottom"] = map(percent, 51, 100, oldObj["offsetBottom"], newObj["offsetBottom"]);
 
+        // Used to scale the shapes
         new_letter["secondShapeScale"] = map(percent, 51, 100, targetScale, newObj["secondShapeScale"]);
-        // illumination = map(percent, 51, 100, illumination, illuminationPulse);
         new_letter["offsetMidTop"] = map(percent, 51, 100, (oldObj["offsetMidTop"] + targetSquish[0]), newObj["offsetMidTop"]);
         new_letter["offsetBottomMid"] = map(percent, 51, 100, (oldObj["offsetBottomMid"] + targetSquish[1]), newObj["offsetBottomMid"]);
 
     }
 
-    new_letter["offsetsecondShapeX"] = map(percent, 0, 100, oldObj["offsetsecondShapeX"], newObj["offsetsecondShapeX"]);
-    new_letter["offsetsecondShapeY"] = map(percent, 0, 100, oldObj["offsetsecondShapeY"], newObj["offsetsecondShapeY"]);
-    new_letter["TranslateX"] = map(percent, 0, 100, oldObj["TranslateX"], newObj["TranslateX"]);
-    new_letter["TranslateY"] = map(percent, 0, 100, oldObj["TranslateY"], newObj["TranslateY"]);
 
+
+    //These manipulate the rotation of the shapes. They were given a slight target rotation that moves slightly from it's original location. This helps to give a "breathing" effect
     if (percent < 20) {
 
         new_letter["Rotation"] = map(percent, 0, 20, oldObj["Rotation"], targetRotation);
-        new_letter["SecondShapeRotation"] = oldObj["SecondShapeRotation"]
+        new_letter["SecondShapeRotation"] = oldObj["SecondShapeRotation"];
 
 
     } else {
@@ -203,7 +197,23 @@ function interpolate_letter(percent, oldObj, newObj) {
     }
 
 
+    //This gives the tranlating parameters
+    if (percent < 10) {
 
+        new_letter["offsetsecondShapeX"] = oldObj["offsetsecondShapeX"];
+        new_letter["offsetsecondShapeY"] = oldObj["offsetsecondShapeY"];
+        new_letter["TranslateX"] = oldObj["TranslateX"];
+        new_letter["TranslateY"] = oldObj["TranslateY"];
+
+
+    } else {
+
+        new_letter["offsetsecondShapeX"] = map(percent, 11, 100, oldObj["offsetsecondShapeX"], newObj["offsetsecondShapeX"]);
+        new_letter["offsetsecondShapeY"] = map(percent, 11, 100, oldObj["offsetsecondShapeY"], newObj["offsetsecondShapeY"]);
+        new_letter["TranslateX"] = map(percent, 11, 100, oldObj["TranslateX"], newObj["TranslateX"]);
+        new_letter["TranslateY"] = map(percent, 11, 100, oldObj["TranslateY"], newObj["TranslateY"]);
+
+    }
 
     return new_letter;
 }
