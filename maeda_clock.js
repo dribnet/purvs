b8846fce0877e2af578b5ba1d9a7671354a02647
adam leach <100628495+MisterBooty@users.noreply.github.com>
2022-03-03 11:22:27 +1300

    I created a maeda clock and updated the iages "thumbnail.jpg' and 'preview.jpg' to a screenshot of it

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,187 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
+
+  background(0); //  beige
+
+  fill(40,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-15,75, width);
+
+  fill(80,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-20,80, width);
+
+  fill(110,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-25,85, width);
+
+  fill(140,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-30,90, width);
+
+  fill(170,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-35,95, width);
+
+  fill(200,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-40,100, width);
+
+  fill(230,0,255);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-45,105, width);
+
+  fill(255,0,210);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-50,110, width);
+
+  fill(255,0,150);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-55,115, width);
+
+  fill(255,0,100);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-60,120, width);
+
+  fill(255,0,60);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-65,125, width);
+
+  fill(255,0,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-70,130, width);
+
+  fill(255,50,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-75,135, width);
+
+  fill(255,80,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-80,140, width);
+
+  fill(255,120,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-85,145, width);
+
+  fill(255,152,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-90,150, width);
+
+  fill(255,184,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-95,155, width);
+
+  fill(255,204,1);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-100,160, width);
+
+  fill(255,224,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-105,165, width);
+
+  fill(255,240,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-110,170, width);
+
+  fill(208,255,1);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-115,175, width);
+
+  fill(147,255,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-120,180, width);
+
+  fill(68,255,0);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-125,185, width);
+
+  fill(0,255,29);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-130,190, width);
+
+  fill(0,255,68);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-135,195, width);
+
+  fill(0,255,100);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-140,200, width);
+
+  fill(0,255,163);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-145,205, width);
+
+  fill(0,255,213);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-150,210, width);
+
+  fill(0,214,214);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-155,215, width);
+
+  fill(0,193,214);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-160,220, width);
+
+  fill(0,160,214);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-165,225, width);
+
+  fill(0,130,214);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-170,230, width);
+
+  fill(0,100,219);
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-175,235, width);
+
+  fill(0,70,219); // //getting lighter
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-180,240, width);
+
+  fill(25,26,219);// dark blue
+  textSize(150);
+  textAlign(RIGHT,CENTER);
+  text("10:11:48",-185,245, width);
+
+
+  fill(500); // dark grey
+  textSize(150);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  text("10:11:48", width/2, height/2);
 }
