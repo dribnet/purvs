e88046f41392dd251eea133040fb5ab3bfd14675
jeremymaih <35henshawjeremy@gmail.com>
2022-03-31 11:48:35 +1300

    Created Sketch Alphabet

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 ## MDDN 242 2022 Assignment 2
 
-Test alphabet
+My Alphabet
+I want to make a square/futuristic font.
+This font uses 5 squares.
+ - 4 squares make up the font
+ - 1 square is the border/container of the font
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by four parameters.
+The twenty parameters per letter:
+  * `Rect1x` : x-position of the first rectangle
+  * `Rect1y` : y-position of the first rectangle
+  * `Rect1h` : height of the first rectangle
+  * `Rect1r` : rotation of the first rectangle
 
-The three parameters per letter:
-  * `size1` : diameter width of the second circle
-  * `size2` : diameter height of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+  * `Rect2x` : x-position of the second rectangle
+  * `Rect2y` : y-position of the second rectangle
+  * `Rect2h` : height of the second rectangle
+  * `Rect2r` : rotation of the second rectangle
+
+  * `Rect3x` : x-position of the third rectangle
+  * `Rect3y` : y-position of the third rectangle
+  * `Rect3h` : height of the third rectangle
+  * `Rect3r` : rotation of the third rectangle
+
+  * `Rect4x` : x-position of the fourth rectangle
+  * `Rect4y` : y-position of the fourth rectangle
+  * `Rect4h` : height of the fourth rectangle
+  * `Rect4r` : rotation of the fourth rectangle
+
+  * `Rect2x` : x-position of the border rectangle
+  * `Rect2y` : y-position of the border rectangle
+  * `Rect2h` : height of the border rectangle
+  * `Rect2w` : width of the border rectangle
--- sketch.js
+++ sketch.js
@@ -13,24 +13,84 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size1": 100,
-  "size2": 120,
-  "offsetx": 0,
-  "offsety": 30
+  "Rect1x": 10,
+  "Rect1y": 0,
+  "Rect1h": 150,
+  "Rect1r": 15,
+
+  "Rect2x": 75,
+  "Rect2y": 0,
+  "Rect2h": 150,
+  "Rect2r": -15,
+
+  "Rect3x": 42,
+  "Rect3y": 20,
+  "Rect3h": 35,
+  "Rect3r": 90,
+
+  "Rect4x": 0,
+  "Rect4y": 0,
+  "Rect4h": 0,
+  "Rect4r": 0,
+
+  "Rect5x": 40,
+  "Rect5y": 0,
+  "Rect5h": 150,
+  "Rect5w": 160
 }
 
 const letterB = {
-  "size1": 100,
-  "size2": 70,
-  "offsetx": 30,
-  "offsety": 30
+  "Rect1x": 0,
+  "Rect1y": 0,
+  "Rect1h": 150,
+  "Rect1r": 0,
+
+  "Rect2x": 50,
+  "Rect2y": 50,
+  "Rect2h": 90,
+  "Rect2r": 90,
+
+  "Rect3x": 90,
+  "Rect3y": 15,
+  "Rect3h": 100,
+  "Rect3r": 0,
+
+  "Rect4x": 50,
+  "Rect4y": -20,
+  "Rect4h": 90,
+  "Rect4r": 90,
+
+  "Rect5x": 40,
+  "Rect5y": 0,
+  "Rect5h": 150,
+  "Rect5w": 160
 }
 
 const letterC = {
-  "size1": 100,
-  "size2": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "Rect1x": -40,
+  "Rect1y": 0,
+  "Rect1h": 150,
+  "Rect1r": 0,
+
+  "Rect2x": 20,
+  "Rect2y": 50,
+  "Rect2h": 100,
+  "Rect2r": 90,
+
+  "Rect3x": 20,
+  "Rect3y": -50,
+  "Rect3h": 100,
+  "Rect3r": 90,
+
+  "Rect4x": 55,
+  "Rect4y": -20,
+  "Rect4h": 30,
+  "Rect4r": 0,
+
+  "Rect5x": 0,
+  "Rect5y": 0,
+  "Rect5h": 150,
+  "Rect5w": 160
 }
 
 const backgroundColor  = "#1B1918";
@@ -46,7 +106,10 @@ function setup () {
 
   // color/stroke setup
   stroke(strokeColor);
-  strokeWeight(6);
+  noStroke();
+  strokeCap(SQUARE);
+  angleMode(DEGREES);
+  rectMode(CENTER);
 
   // with no animation, redrawing the screen is not necessary
   noLoop();
@@ -68,16 +131,60 @@ function draw () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
-  let size1 = letterData["size1"];
-  let size2 = letterData["size2"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  let pos1x = posx + letterData["Rect1x"];
+  let pos1y = posy + letterData["Rect1y"];
+  let pos2x = posx + letterData["Rect2x"];
+  let pos2y = posy + letterData["Rect2y"];
+  let pos3x = posx + letterData["Rect3x"];
+  let pos3y = posy + letterData["Rect3y"];
+  let pos4x = posx + letterData["Rect4x"];
+  let pos4y = posy + letterData["Rect4y"];
+  let pos5x = posx + letterData["Rect5x"];
+  let pos5y = posy + letterData["Rect5y"];
 
-  // draw two circles
+  let height1y = letterData["Rect1h"];
+  let height2y = letterData["Rect2h"];
+  let height3y = letterData["Rect3h"];
+  let height4y = letterData["Rect4h"];
+  let height5y = letterData["Rect5h"];
+  let width5x = letterData["Rect5w"];
+
+  let rot1 = letterData["Rect1r"];
+  let rot2 = letterData["Rect2r"];
+  let rot3 = letterData["Rect3r"];
+  let rot4 = letterData["Rect4r"];
+
+  // square 1
+  noStroke();
   fill(white);
-  ellipse(posx, posy, 150, 150);
-  fill(black);
-  ellipse(pos2x, pos2y, size1, size2);
+  push();
+  translate(pos1x, pos1y);
+  rotate(rot1);
+  rect(0, 0, 30, height1y);
+  pop();
+
+  push();
+  translate(pos2x, pos2y);
+  rotate(rot2);
+  rect(0, 0, 30, height2y);
+  pop();
+
+  push();
+  translate(pos3x, pos3y);
+  rotate(rot3);
+  rect(0, 0, 30, height3y);
+  pop();
+
+  push();
+  translate(pos4x, pos4y);
+  rotate(rot4);
+  rect(0, 0, 30, height4y);
+  pop();
+
+  fill(0, 0, 0, 0);
+  stroke(strokeColor);
+  strokeWeight(20);
+  rect(pos5x,pos5y, width5x, height5y);
 }
 
 function keyTyped() {
