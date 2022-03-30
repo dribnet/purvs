f1969797c6fdff487aec493f027f34fe9143133b
Jakob-Dove <hackerzfuel@gmail.com>
2022-03-30 07:55:48 -0500

    shape changes for the letters

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ## MDDN 242 2022 Assignment 2
 
-(Replace this README with information about your alphabet. This is an example.)
+## March 31th
 
-Each of my letters is composed with two circles. The size and position of the first circle is fixed, but the location and size of the second circle is controlled by three parameters.
-
-The three parameters per letter:
-  * `size` : radius of the second circle
-  * `offsetx` : x offset of the second circle relative to the first one
-  * `offsety` : y offset of the second circle relative to the first one
+I changed some of the letters.  I did not have a plan for the letter "C" unfortunately.
+So, I made the "A" like a house.  For the "b" I was originally going to have a normal circle and a stem.
+I then changed it to have the light pink circle inside the "b" because that's where the light pink circle ended up inside c on accident.
+I don't know what the plan is going forward.  Hopefully I will have something after tomorrow.
--- sketch.js
+++ sketch.js
@@ -14,14 +14,24 @@ const canvasHeight = 500;
 
 const letterA = {
   "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offsetx": 100,
+  "offsety": 240,
+  "coordX1": 30,
+  "coordX2": 58,
+  "coordX3": 86,
+  "coordY1": 75,
+  "coordY2": 20,
+  "coordY3": 75,
+  "rectX1": 0,
+  "rectY1": 0
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "size": 75,
+  "offsetx": 20,
+  "offsety": 0,
+  "rectwidth": 15,
+  "rectheight": 118
 }
 
 const letterC = {
@@ -33,8 +43,8 @@ const letterC = {
 const backgroundColor  = "#caf0f8";
 const strokeColor      = "#03045e";
 
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
+const darkPink  = "#c800A1";
+const lightPink  = "#ff5aac";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -68,11 +78,33 @@ function drawLetter(posx, posy, letterData) {
   let size2 = letterData["size"];
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let pos3x = letterB["offsetx"];//letter b
+  let pos3y = letterB["offsety"];//letter B
+  let rectwidth =  letterB["rectwidth"];
+  let rectheight = letterB["rectheight"];
+
+  let trix1 = letterA["coordX1"];//triangle x coord 1
+  let trix2 = letterA["coordX2"];//triangle x coord 2
+  let trix3 = letterA["coordX3"];//triangle x coord 3
+
+  let triy1 = letterA["offsety"];//triangle y coord 1
+  let triy2 = letterA["offsety"];//triangle y coord 2
+  let triy3 = letterA["offsety"];//triangle y coord 3
+
+  let rectx1 = letterA["rectX1"];
+  let recty1 = letterA["rectY1"];
 
   // draw two circles
-  fill(darkBlue);
-  ellipse(posx, posy, 150, 150);
-  fill(lightBlue);
+  fill(lightPink);
+  stroke(darkPink);
+  rect(pos3x+386,pos3y+50,rectwidth,rectheight+100);
+  rect(rectx1+247, recty1+265, rectwidth, rectheight-80);
+  rect(rectx1+194, recty1+265, rectwidth, rectheight-80);
+  triangle(trix1+165, triy1+25, trix2+170, triy2-15, trix3+175, triy3+25);
+  noFill();
+  stroke(darkPink);
+  ellipse(posx-2.5, posy, 150, 150);
+  stroke(lightPink);
   ellipse(pos2x, pos2y, size2, size2);
 }
 
