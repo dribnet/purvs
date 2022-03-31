ea0cf445aebd79c3656712c0d78be8a9fd5fe171
Jakob-Dove <hackerzfuel@gmail.com>
2022-03-31 16:22:26 -0500

    Letter.js
    Started working in letter.js.  Made a little unown guy.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## MDDN 242 2022 Assignment 2
 
-## March 31th
+## April 1st
 
-I changed some of the letters.  I did not have a plan for the letter "C" unfortunately.
-So, I made the "A" like a house.  For the "b" I was originally going to have a normal circle and a stem.
-I then changed it to have the light pink circle inside the "b" because that's where the light pink circle ended up inside c on accident.
-I don't know what the plan is going forward.  Hopefully I will have something after tomorrow.
+I still don't know what I want to do with this project yet.  I should after today.
+After hearing Phebe mention the Unown alphabet.  I thought that would be pretty cool to see.
+So I just did the letter B.
--- draw_letters.js
+++ draw_letters.js
@@ -4,8 +4,8 @@ var systemLineColor = "#000090";
 var systemBoxColor = "#00c800";
 
 /* internal constants */
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
+const White  = "#FFFFFF";
+const black  = "#000000";
 const strokeColor  = "#03045e";
 
 /*
@@ -16,23 +16,52 @@ const strokeColor  = "#03045e";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
-
+  let posx = 50;
+  let posy = 50;
   // determine parameters for second circle
   let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+  let pos2x = posx + letterData["offsetx"];
+  let pos2y = posy + letterData["offsety"];
+//  let pos3x = letterB["offsetx"];//letter b
+  //let pos3y = letterB["offsety"];//letter B
+  // let rectwidth =  letterB["rectwidth"];
+  // let rectheight = letterB["rectheight"];
+
+  // let trix1 = letterA["coordX1"];//triangle x coord 1
+  // let trix2 = letterA["coordX2"];//triangle x coord 2
+  // let trix3 = letterA["coordX3"];//triangle x coord 3
+  //
+  // let triy1 = letterA["offsety"];//triangle y coord 1
+  // let triy2 = letterA["offsety"];//triangle y coord 2
+  // let triy3 = letterA["offsety"];//triangle y coord 3
+  //
+  // let rectx1 = letterA["rectX1"];
+  // let recty1 = letterA["rectY1"];
 
   // draw two circles
-  fill(darkBlue);
-  ellipse(50, 150, 75, 75);
-  fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  strokeWeight(5);
+  fill(black);
+  rect(posx-5,posy,12,120,0,0,5,5);//main rectangle
+  rect(posx+17,posy+25,25,10,0,5,5,0);//small rect up top right of the top circle
+  rect(posx-42,posy+100,25,10,5,0,0,5);//small rect on the left side
+  // rect(rectx1+194, recty1+265, rectwidth, rectheight-80);
+  // triangle(trix1+165, triy1+25, trix2+170, triy2-15, trix3+175, triy3+25);
+  fill(White);
+  stroke(black);
+  ellipse(posx, posy+36, 60, 60);//The circle on the top of the B
+  ellipse(posx, posy+100, 60, 60);//The circle on the bottom of the B
+
+  fill(black);
+  ellipse(pos2x, pos2y+100, size2-55, size2-55);//Small black circle
+
+  fill(White);
+  noStroke();
+  ellipse(posx-2, posy+26, 6,6);//small white circle
+
+
 }
 
-function interpolate_letter(percent, oldObj, newObj) {
+function interpolate_letter(perc90t, oldObj, newObj) {
   let new_letter = {};
   new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
   new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
