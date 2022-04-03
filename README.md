4b0bc27c57eaa8cd1772f0d4ebafb8ee2964eaeb
walkerleila <100626635+walkerleila@users.noreply.github.com>
2022-04-03 21:04:00 +1200

    sketch updating

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## MDDN 242 2022 Assignment 2
 
-1 / 4 / 22:
-This is my first idea. I'm still working on my sketch and might be change that with new idea.
+3 / 4 / 22:
+
+My final sketch for the alphabet design:
+
+The preview and thumbnail are updated with this sketch today. I try to show a happy face in the form of letters.
+I only use the circles for the whole letter design and used 16 parameters(4 X/Y sizes and 4 X/Y Locations). But all of the parameters are not used for all forms and depend on what I need for that specific letter, might be less or more.
+This is for now. I'll come back with the rest of my work one by one.
--- sketch.js
+++ sketch.js
@@ -13,31 +13,56 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "sizeEll_x": 50,
-  "sizeEll_y":100,
-  "offsetx": 2,
-  "offsety": 25
+  "sizeEll_x1":100,
+  "sizeEll_y1":100,
+  "sizeEll_x2":30,
+  "sizeEll_y2":140,
+  "sizeEll_x3":25,
+  "sizeEll_y3":25,
+  "loc1X": 0,
+  "loc1Y": 0,
+  "loc2X": 60,
+  "loc2Y": 0,
+  "loc3X": -23,
+  "loc3Y": -20,
 }
 
 const letterB = {
-  "sizeEll_x": 100,
-  "sizeEll_y": 100,
-  "offsetx": 0,
-  "offsety": -125
+  "sizeEll_x1":100,
+  "sizeEll_y1":100,
+  "sizeEll_x2":30,
+  "sizeEll_y2":250,
+  "sizeEll_x3":25,
+  "sizeEll_y3":25,
+  "loc1X": 0,
+  "loc1Y": -1,
+  "loc2X": -65,
+  "loc2Y": -50,
+  "loc3X": -23,
+  "loc3Y": -20,
 }
 
 const letterC = {
-  "sizeEll_x": 110,
-  "sizeEll_y": 100,
-  "offsetx": 22,
-  "offsety": 0
+  "sizeEll_x1":100,
+  "sizeEll_y1":100,
+  "sizeEll_x3":25,
+  "sizeEll_y3":25,
+  "sizeEll_x4":100,
+  "sizeEll_y4":100,
+  "loc1X": 0,
+  "loc1Y": 0,
+  "loc3X": -23,
+  "loc3Y": -20,
+  "loc4X": 50,
+  "loc4Y": -1,
 }
 
-const backgroundColor  = "#caf0f8";
-const strokeColor      = "#03045e";
+const backgroundColor  = "#ccecec";//light blue
+const strokeColor      = "#c4c6c0";//gray
 
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
+const peach  = "#f7cabe";// peach
+const lightBlue  = "#ccecec";//"#90e0ef";//light blue
+const darkGreen = "#014d4a"
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -62,22 +87,46 @@ function draw () {
 
   // draw the letters A, B, C from saved data
   drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
+  drawLetter(center_x      , center_y , letterB);
+  drawLetter(center_x + 250, center_y , letterC);
 }
 
 function drawLetter(ell_x, ell_y, letterData) {
   // determine parameters for second circle
-  let sizeEll_x = letterData["sizeEll_x"];
-  let sizeEll_y = letterData["sizeEll_y"];
-  let ell1_x = ell_x + letterData["offsetx"];
-  let ell1_y = ell_y + letterData["offsety"];
+  let sizeEll_x1 = letterData["sizeEll_x1"];
+  let sizeEll_y1 = letterData["sizeEll_y1"];
+  let sizeEll_x2 = letterData["sizeEll_x2"];
+  let sizeEll_y2 = letterData["sizeEll_y2"];
+  let sizeEll_x3 = letterData["sizeEll_x3"];
+  let sizeEll_y3 = letterData["sizeEll_y3"];
+  let sizeEll_x4 = letterData["sizeEll_x4"];
+  let sizeEll_y4 = letterData["sizeEll_y4"];
+  let ell1_x = ell_x + letterData["loc1X"];
+  let ell1_y = ell_y + letterData["loc1Y"];
+  let ell2_x = ell_x + letterData["loc2X"];
+  let ell2_y = ell_y + letterData["loc2Y"];
+  let ell3_x = ell_x + letterData["loc3X"];
+  let ell3_y = ell_y + letterData["loc3Y"];
+  let ell4_x = ell_x + letterData["loc4X"];
+  let ell4_y = ell_y + letterData["loc4Y"];
+
+
 
   // draw two circles
-  fill(darkBlue);
+  noStroke();
+  fill(peach);
+  ellipse(ell2_x, ell2_y, sizeEll_x2, sizeEll_y2);
+  fill(peach);
   ellipse(ell_x, ell_y, 150, 150);
   fill(lightBlue);
-  ellipse(ell1_x, ell1_y, sizeEll_x, sizeEll_y);
+  ellipse(ell1_x, ell1_y, sizeEll_x1, sizeEll_y1);
+  fill(lightBlue);
+  ellipse(ell4_x, ell4_y, sizeEll_x4, sizeEll_y4);
+  fill(darkGreen);
+  ellipse(ell3_x, ell3_y, sizeEll_x3, sizeEll_y3);
+  ellipse(ell3_x + 50, ell3_y, sizeEll_x3, sizeEll_y3);
+
+
 }
 
 
