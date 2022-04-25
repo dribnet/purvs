3da84f52e817ebe96600e877efa7d1d983a126bd
Chloe Akerblom <47228233+chloeakerblom@users.noreply.github.com>
2022-04-26 11:22:04 +1200

    Finishing my letters.

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
 ## MDDN 242 2022 Assignment 2
 
-### 20/04/22 - MAKING MORE LETTERS
+### 20/04/22 - FINISHING MY LETTERS
 
-Today, I continued to make more of my letters and numbers, completing all of them apart from the letters N, W, X and the default, which are a bit harder to figure out. It was quite easy to create the new letters as all of them are made within a similar grid system so it was just a matter of repositioning the squares and rectangles. I have been sketching them as I go so I know where to move each of my squares. The image below shows these sketches, as well as some of my other ideas I had from my last push that I didn't upload then and some notes on variables.
+Today, I created the last few letters of my alphabet - N, W, X and the default value.
 
-**See image alphabet_sketches.jpg for sketch. I couldn't manage to link it in the README file.**
+I am quite happy with the shape of the X as this was one I was struggling to figure out how to do. The way I have my code set up doesn't allow me to have a vertically symmetrical letter (like the K but with the left side separated in the middle). So, I have moved the center of the letter up so that the bottom legs are longer than the top legs. I think this looks good and has a similar balance to the Y shape wise.
 
-The letters I have left are a bit wider and/or might need more squares than I have to complete so I will have to think a bit more on how to do them. You can see this issue with the M as I would have liked to have more space between the side lines and the center square. I could make the middle square thinner to add some white space but that could upset the balance of the letter compared to all the others.
+I have made the M, N and W by repositioning the middle shape and changing its length to have a similar length to the diagonal line that would join the two legs (the N line is longer as it joins the two legs, whereas the M and W middle point is only half the height of the legs). I am still not 100% happy with this so I might get some feedback to see what I could do differently.
+
+The default is just a single red square.
+
+I also changed a few of my variable names to make them more descriptive. I changed all of the rect1 variables to rectYellow as that is the yellow rectangle, and rect2 to rectRed as that is the red rectangle.
 
 The 15 parameters per letter:
   * `numLineSquares` : The number of squares to be drawn in the array
   * `lineSquaresX`: The x position of the square array
   * `lineSquaresY`: The y position of the square array
-  * `rect1X`: The x position of the yellow rectangle
-  * `rect1Y`: The y position of the yellow rectangle
-  * `rect1Width`: The width of the yellow rectangle
-  * `rect1Height`: The height of the yellow rectangle
-  * `rect2X`: The x position of the red rectangle
-  * `rect2Y`: The y position of the red rectangle
-  * `rect2Width`: The width of the red rectangle
-  * `rect2Height`: The height of the red rectangle
+  * `rectYellowX`: The x position of the yellow rectangle
+  * `rectYellowY`: The y position of the yellow rectangle
+  * `rectYellowWidth`: The width of the yellow rectangle
+  * `rectYellowHeight`: The height of the yellow rectangle
+  * `rectRedX`: The x position of the red rectangle
+  * `rectRedY`: The y position of the red rectangle
+  * `rectRedWidth`: The width of the red rectangle
+  * `rectRedHeight`: The height of the red rectangle
   * `square1X`: The x position of one of the first white squares
   * `square1Y`: The y position of the first white square
   * `square2X`: The x position of the second white square
   * `square2Y`: The y position of the second white square
 
-My next steps are to finish the last few letters and adjust some of the others I already have. I will get some feedback to see how I could develop my design further. I also still need to decrease the number of parameters I have to hopefully get it down from 15 to 12.
+My next steps are to further develop my letters and fix the ones I am not quite happy with. I also want to look more at the interpolations of my letters to see if I can make it more interesting. I still need to cut down the number of variables I have, but now that I have all of my letters done this will be easier to do.
--- draw_letters.js
+++ draw_letters.js
@@ -4,9 +4,9 @@ var systemLineColor = "#000090";
 var systemBoxColor = "#00c800";
 
 /* internal constants */
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
-const strokeColor  = "#03045e";
+const darkBlue = "#0077b6";
+const lightBlue = "#90e0ef";
+const strokeColor = "#03045e";
 
 /*
  * Draw the letter given the letterData
@@ -16,57 +16,14 @@ const strokeColor  = "#03045e";
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  const backgroundColor  = "#FFFFF0";
   const strokeColor = "#000000";
   const redColour = "#ff4242";
   const blueColour = "#79d8f2";
   const yellowColour = "#fcfa58";
   const squareSize = 30;
 
-  // push();
-  // rectMode(CENTER);
-  //
   strokeWeight(3);
   stroke(strokeColor);
-  //
-  // let posx = 50;
-  // let posy = 150;
-  //
-  // // determine parameters for second circle
-  // let squareSize = letterData["squareSize"];
-  // let squareX = posx + letterData["squareX"];
-  // let squareY = posy + letterData["squareY"];
-  //
-  // let rectWidth = letterData["rectWidth"];
-  // let rectHeight = letterData["rectHeight"];
-  // let rectX = posx + letterData["rectX"];
-  // let rectY = posy + letterData["rectY"];
-  //
-  // let lineX = posx + letterData["lineX"];
-  // let lineY = posy + letterData["lineY"];
-  // let lineLength = letterData['lineLength'];
-  // let lineHeight = letterData['lineHeight'];
-  //
-  //
-  // let squareArea = squareSize*squareSize;
-  // let rectArea = rectWidth*rectHeight;
-  // let lineArea = lineLength*lineHeight;
-  //
-  // let colour1;
-  // let colour2;
-  // let colour3;
-  //
-  //
-  // fill(redColour);
-  // rect(squareX, squareY, squareSize);
-  // fill(blueColour);
-  // rect(rectX, rectY, rectWidth, rectHeight);
-  // fill(yellowColour);
-  // rect(lineX, lineY, lineLength, lineHeight);
-  //
-  // pop();
-
-  let count = 0;
 
   let posx = -50;
   let posy = 30;
@@ -76,15 +33,15 @@ function drawLetter(letterData) {
   let square2X = posx + letterData["square2X"];
   let square2Y = posy + letterData["square2Y"];
 
-  let rect1Width = letterData["rect1Width"];
-  let rect1Height = letterData["rect1Height"];
-  let rect1X = posx + letterData["rect1X"];
-  let rect1Y = posy + letterData["rect1Y"];
+  let rectYellowWidth = letterData["rectYellowWidth"];
+  let rectYellowHeight = letterData["rectYellowHeight"];
+  let rectYellowX = posx + letterData["rectYellowX"];
+  let rectYellowY = posy + letterData["rectYellowY"];
 
-  let rect2Width = letterData["rect2Width"];
-  let rect2Height = letterData["rect2Height"];
-  let rect2X = posx + letterData["rect2X"];
-  let rect2Y = posy + letterData["rect2Y"];
+  let rectRedWidth = letterData["rectRedWidth"];
+  let rectRedHeight = letterData["rectRedHeight"];
+  let rectRedX = posx + letterData["rectRedX"];
+  let rectRedY = posy + letterData["rectRedY"];
 
   let numLineSquares = letterData["numLineSquares"];
   let lineSquaresX = posx + letterData["lineSquaresX"];
@@ -92,59 +49,28 @@ function drawLetter(letterData) {
   let direction = letterData['direction'];
 
 
-  // if(count == 3){
-  //   fill(blueColour);
-  // }
-  // else if(count == 5){
-  //   fill(yellowColour);
-  // }
-  // else if(count == 7){
-  //   fill(redColour);
-  // }
-  // else{
-  //   fill(255);
-  // }
+  fill(255);
+  rect(square1X, square1Y, squareSize, squareSize);
 
-//  fill(255);
-fill(255);
-rect(square1X, square1Y, squareSize, squareSize);
-count++;
-fill(255);
-rect(square2X, square2Y, squareSize, squareSize);
+  fill(255);
+  rect(square2X, square2Y, squareSize, squareSize);
 
 
-  // if(direction == 0){
-  //   for (i=0; i<numLineSquares; i++){
-  //     if(i == 1){
-  //       fill(blueColour);
-  //     }
-  //     else{
-  //       fill(255);
-  //     }
-  //     rect(lineSquaresX+squareSize*i, lineSquaresY, squareSize, squareSize);
-  //     count++;
-  //   }
-  // }
-  // else{
-    for (i=0; i<numLineSquares; i++){
-      if(i == 1){
-        fill(blueColour);
-      }
-      else{
-        fill(255);
-      }
-      rect(lineSquaresX, lineSquaresY+squareSize*i, squareSize, squareSize);
-      count++;
+  for (i = 0; i < numLineSquares; i++) {
+    if (i == 1) {
+      fill(blueColour);
+    } else {
+      fill(255);
     }
-  // }
+    rect(lineSquaresX, lineSquaresY + squareSize * i, squareSize, squareSize);
+  }
+
 
   fill(yellowColour);
-  rect(rect1X, rect1Y, rect1Width, rect1Height);
-  count++;
-  fill(redColour);
-  rect(rect2X, rect2Y, rect2Width, rect2Height);
-  count++;
+  rect(rectYellowX, rectYellowY, rectYellowWidth, rectYellowHeight);
 
+  fill(redColour);
+  rect(rectRedX, rectRedY, rectRedWidth, rectRedHeight);
 
 
 
@@ -152,40 +78,29 @@ rect(square2X, square2Y, squareSize, squareSize);
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  // new_letter["squareX"]    = map(percent, 0, 100, oldObj["squareX"], newObj["squareX"]);
-  // new_letter["squareY"] = map(percent, 0, 100, oldObj["squareY"], newObj["squareY"]);
-  // new_letter["squareSize"] = map(percent, 0, 100, oldObj["squareSize"], newObj["squareSize"]);
-  // new_letter["rectX"] = map(percent, 0, 100, oldObj["rectX"], newObj["rectX"]);
-  // new_letter["rectY"] = map(percent, 0, 100, oldObj["rectY"], newObj["rectY"]);
-  // new_letter["rectWidth"] = map(percent, 0, 100, oldObj["rectWidth"], newObj["rectWidth"]);
-  // new_letter["rectHeight"] = map(percent, 0, 100, oldObj["rectHeight"], newObj["rectHeight"]);
-  // new_letter["lineX"] = map(percent, 0, 100, oldObj["lineX"], newObj["lineX"]);
-  // new_letter["lineY"] = map(percent, 0, 100, oldObj["lineY"], newObj["lineY"]);
-  // new_letter["lineLength"] = map(percent, 0, 100, oldObj["lineLength"], newObj["lineLength"]);
-  // new_letter["lineHeight"] = map(percent, 0, 100, oldObj["lineHeight"], newObj["lineHeight"]);
 
   new_letter["numLineSquares"] = map(percent, 0, 100, oldObj["numLineSquares"], newObj["numLineSquares"]);
   new_letter["lineSquaresX"] = map(percent, 0, 100, oldObj["lineSquaresX"], newObj["lineSquaresX"]);
   new_letter["lineSquaresY"] = map(percent, 0, 100, oldObj["lineSquaresY"], newObj["lineSquaresY"]);
-  new_letter["rect1X"] = map(percent, 0, 100, oldObj["rect1X"], newObj["rect1X"]);
-  new_letter["rect1Y"] = map(percent, 0, 100, oldObj["rect1Y"], newObj["rect1Y"]);
-  new_letter["rect1Width"] = map(percent, 0, 100, oldObj["rect1Width"], newObj["rect1Width"]);
-  new_letter["rect1Height"] = map(percent, 0, 100, oldObj["rect1Height"], newObj["rect1Height"]);
-  new_letter["rect2X"] = map(percent, 0, 100, oldObj["rect2X"], newObj["rect2X"]);
-  new_letter["rect2Y"] = map(percent, 0, 100, oldObj["rect2Y"], newObj["rect2Y"]);
-  new_letter["rect2Width"] = map(percent, 0, 100, oldObj["rect2Width"], newObj["rect2Width"]);
-  new_letter["rect2Height"] = map(percent, 0, 100, oldObj["rect2Height"], newObj["rect2Height"]);
-  new_letter["square1X"]    = map(percent, 0, 100, oldObj["square1X"], newObj["square1X"]);
+  new_letter["rectYellowX"] = map(percent, 0, 100, oldObj["rectYellowX"], newObj["rectYellowX"]);
+  new_letter["rectYellowY"] = map(percent, 0, 100, oldObj["rectYellowY"], newObj["rectYellowY"]);
+  new_letter["rectYellowWidth"] = map(percent, 0, 100, oldObj["rectYellowWidth"], newObj["rectYellowWidth"]);
+  new_letter["rectYellowHeight"] = map(percent, 0, 100, oldObj["rectYellowHeight"], newObj["rectYellowHeight"]);
+  new_letter["rectRedX"] = map(percent, 0, 100, oldObj["rectRedX"], newObj["rectRedX"]);
+  new_letter["rectRedY"] = map(percent, 0, 100, oldObj["rectRedY"], newObj["rectRedY"]);
+  new_letter["rectRedWidth"] = map(percent, 0, 100, oldObj["rectRedWidth"], newObj["rectRedWidth"]);
+  new_letter["rectRedHeight"] = map(percent, 0, 100, oldObj["rectRedHeight"], newObj["rectRedHeight"]);
+  new_letter["square1X"] = map(percent, 0, 100, oldObj["square1X"], newObj["square1X"]);
   new_letter["square1Y"] = map(percent, 0, 100, oldObj["square1Y"], newObj["square1Y"]);
-  new_letter["square2X"]    = map(percent, 0, 100, oldObj["square2X"], newObj["square2X"]);
+  new_letter["square2X"] = map(percent, 0, 100, oldObj["square2X"], newObj["square2X"]);
   new_letter["square2Y"] = map(percent, 0, 100, oldObj["square2Y"], newObj["square2Y"]);
-//  new_letter["lineHeight"] = map(percent, 0, 100, oldObj["lineHeight"], newObj["lineHeight"]);
+
   return new_letter;
 }
 
 var swapWords = [
   "MONDRIAN",
-  "ABBAABBA",
-  "CAB?CAB?",
-  "BAAAAAAA"
+  "DE STIJL",
+  "ARTWORKS",
+  "PAINTING"
 ]
