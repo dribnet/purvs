bd81622169f25114e8b745cc7814f6825f37a827
ZhuYouhe <100631818+ZhuYouhe@users.noreply.github.com>
2022-05-03 02:19:08 +1200

    animation update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## MDDN 242 2022 Assignment 2
 
-I updated the index page to alphabet.
-Also fixed the interaction animation of some letters ('b','g','j','p','2','3','5','9') by changing the arc begins to negative numbers so that the parameter 'arcBegin' is always smaller than the 'arcend', which will not make the arc disappear when transforming and the animation look smoother, but all the letters' colour pallet are same now. I'll do some colour change to the interaction part.
+I made some changes to the animation but haven't do any colour changing, will do that later.
+Now in the interaction mode, the arc and rects size will change when transforming to make the letters look vivid.
--- draw_letters.js
+++ draw_letters.js
@@ -4,9 +4,11 @@ var systemLineColor = "#734567";
 var systemBoxColor = "#4B50BF";
 
 /* internal constants */
-const darkBlue  = "#0077b6";
-const lightBlue  = "#D2E8E3";
+
 const strokeColor  = "#0F6466";
+const arcOffsetColor  = "#D9967E";
+const rectAcolor  = "#7D8C0B";
+const rectBcolor  = "#D9863D";
 
 /*
  * Draw the letter given the letterData
@@ -39,48 +41,115 @@ function drawLetter(letterData) {
 
 push();
   angleMode(DEGREES);
-  //fill(darkBlue);
   noFill();
   strokeWeight(8);
   arc(arcPosx, arcPosY, arcWidth, arcHeight, arcBegin, arcEnd);
   strokeWeight(4);
-  if (arcBegin <= arcEnd){
-    stroke("#D9967E");
+  stroke(arcOffsetColor);
   arc(arcPosx*1.06, arcPosY*0.96, arcWidth, arcHeight, arcBegin, arcEnd);
-} else {
-  stroke("#AFBF34");
-  arc(arcPosx*0.9, arcPosY*0.92, arcWidth, arcHeight, arcBegin, arcEnd);
-}
+  // stroke("#AFBF34");
+  // arc(arcPosx*0.9, arcPosY*0.92, arcWidth, arcHeight, arcBegin, arcEnd);
+
   stroke(strokeColor);
   strokeWeight(4);
   //noStroke();
-  fill("#7D8C0B");
+  fill(rectAcolor);
   rectMode(CENTER);
   rect(rectAPosX, rectAPosY, rectAWidth, rectAHeight, 14);
   strokeWeight(6);
-  fill("#D9863D");
+  fill(rectBcolor);
   rect(rectBPosX, rectBPosY, rectBWidth, rectBHeight, 14);
 pop();
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
+
   new_letter["arcA_PosX"] = map(percent, 0, 100, oldObj["arcA_PosX"], newObj["arcA_PosX"]);
   new_letter["arcA_PosY"] = map(percent, 0, 100, oldObj["arcA_PosY"], newObj["arcA_PosY"]);
-  new_letter["arcA_SizeX"] = map(percent, 0, 100, oldObj["arcA_SizeX"], newObj["arcA_SizeX"]);
-  new_letter["arcA_SizeY"] = map(percent, 0, 100, oldObj["arcA_SizeY"], newObj["arcA_SizeY"]);
+  // new_letter["arcA_SizeX"] = map(percent, 0, 100, oldObj["arcA_SizeX"], newObj["arcA_SizeX"]);
+  // new_letter["arcA_SizeY"] = map(percent, 0, 100, oldObj["arcA_SizeY"], newObj["arcA_SizeY"]);
   new_letter["arcA_Begin"] = map(percent, 0, 100, oldObj["arcA_Begin"], newObj["arcA_Begin"]);
   new_letter["arcA_End"] = map(percent, 0, 100, oldObj["arcA_End"], newObj["arcA_End"]);
 
+  if(percent < 50){
+    //change arc size
+    new_letter["arcA_SizeX"] = map(percent, 0, 50, oldObj["arcA_SizeX"], 100);
+    new_letter["arcA_SizeY"] = map(percent, 0, 50, oldObj["arcA_SizeY"], 200);
+    //--------------------------------------------------------------------------
+
+    //change rectA size
+    if(oldObj["rectA_SizeX"] <= 30){
+      new_letter["rectA_SizeX"] = map(percent, 0, 50, oldObj["rectA_SizeX"], 70);
+    } else {
+      new_letter["rectA_SizeX"] = map(percent, 0, 50, oldObj["rectA_SizeX"], 20);
+    }
+
+    if(oldObj["rectA_SizeY"] <= 50){
+      new_letter["rectA_SizeY"] = map(percent, 0, 50, oldObj["rectA_SizeY"], 140);
+    } else {
+      new_letter["rectA_SizeY"] = map(percent, 0, 50, oldObj["rectA_SizeY"], 40);
+    }
+    //--------------------------------------------------------------------------
+
+    //change rectB size
+    if(oldObj["rectB_SizeX"] <= 20){
+      new_letter["rectB_SizeX"] = map(percent, 0, 50, oldObj["rectB_SizeX"], 55);
+    } else {
+      new_letter["rectB_SizeX"] = map(percent, 0, 50, oldObj["rectB_SizeX"], 10);
+    }
+
+    if(oldObj["rectB_SizeY"] <= 40){
+      new_letter["rectB_SizeY"] = map(percent, 0, 50, oldObj["rectB_SizeY"], 100);
+    } else {
+      new_letter["rectB_SizeY"] = map(percent, 0, 50, oldObj["rectB_SizeY"], 20);
+    }
+    //--------------------------------------------------------------------------
+  }
+
+  else{
+    //change arc size
+    new_letter["arcA_SizeX"] = map(percent, 51, 100, 100, newObj["arcA_SizeX"]);
+    new_letter["arcA_SizeY"] = map(percent, 51, 100, 200, newObj["arcA_SizeY"]);
+    //--------------------------------------------------------------------------
+
+    //change rectA size
+    if(oldObj["rectA_SizeX"] <= 30){
+      new_letter["rectA_SizeX"] = map(percent, 51, 100, 70, newObj["rectA_SizeX"]);
+    } else {
+      new_letter["rectA_SizeX"] = map(percent, 51, 100, 20, newObj["rectA_SizeX"]);
+    }
+
+    if(oldObj["rectA_SizeY"] <= 50){
+      new_letter["rectA_SizeY"] = map(percent, 51, 100, 140, newObj["rectA_SizeY"]);
+    } else {
+      new_letter["rectA_SizeY"] = map(percent, 51, 100, 40, newObj["rectA_SizeY"]);
+    }
+    //--------------------------------------------------------------------------
+
+    //change rectB size
+    if(oldObj["rectB_SizeX"] <= 20){
+      new_letter["rectB_SizeX"] = map(percent, 51, 100, 55, newObj["rectB_SizeX"]);
+    } else {
+      new_letter["rectB_SizeX"] = map(percent, 51, 100, 10, newObj["rectB_SizeX"]);
+    }
+
+    if(oldObj["rectB_SizeY"] <= 40){
+      new_letter["rectB_SizeY"] = map(percent, 51, 100, 100, newObj["rectB_SizeY"]);
+    } else {
+      new_letter["rectB_SizeY"] = map(percent, 51, 100, 20, newObj["rectB_SizeY"]);
+    }
+
+  }
   new_letter["rectA_PosX"] = map(percent, 0, 100, oldObj["rectA_PosX"], newObj["rectA_PosX"]);
   new_letter["rectA_PosY"] = map(percent, 0, 100, oldObj["rectA_PosY"], newObj["rectA_PosY"]);
-  new_letter["rectA_SizeX"] = map(percent, 0, 100, oldObj["rectA_SizeX"], newObj["rectA_SizeX"]);
-  new_letter["rectA_SizeY"] = map(percent, 0, 100, oldObj["rectA_SizeY"], newObj["rectA_SizeY"]);
+  // new_letter["rectA_SizeX"] = map(percent, 0, 100, oldObj["rectA_SizeX"], newObj["rectA_SizeX"]);
+  // new_letter["rectA_SizeY"] = map(percent, 0, 100, oldObj["rectA_SizeY"], newObj["rectA_SizeY"]);
 
   new_letter["rectB_PosX"] = map(percent, 0, 100, oldObj["rectB_PosX"], newObj["rectB_PosX"]);
   new_letter["rectB_PosY"] = map(percent, 0, 100, oldObj["rectB_PosY"], newObj["rectB_PosY"]);
-  new_letter["rectB_SizeX"] = map(percent, 0, 100, oldObj["rectB_SizeX"], newObj["rectB_SizeX"]);
-  new_letter["rectB_SizeY"] = map(percent, 0, 100, oldObj["rectB_SizeY"], newObj["rectB_SizeY"]);
+  // new_letter["rectB_SizeX"] = map(percent, 0, 100, oldObj["rectB_SizeX"], newObj["rectB_SizeX"]);
+  // new_letter["rectB_SizeY"] = map(percent, 0, 100, oldObj["rectB_SizeY"], newObj["rectB_SizeY"]);
 
   return new_letter;
 }
