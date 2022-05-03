908d2785d4181fa8bb49c924e57e303e9e9dee61
LouisJefferson607 <louisjefferson607@gmail.com>
2022-05-04 08:08:52 +1200

    x`
    ss

--- draw_letters.js
+++ draw_letters.js
@@ -25,12 +25,77 @@ function drawLetter(letterData) {
   let pos2x = 50  + letterData["offsetx"];
   let pos2y = 150 + letterData["offsety"];
 
-  // draw two circles
-  fill(darkBlue);
-  ellipse(50, 150, 75, 75);
-  fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  let posx = 10;
+    let posy = 100;
+    stroke(strokeColor);
+    noStroke();
+    let pos1x = posx + letterData["Rect1x"];
+    let pos1y = posy + letterData["Rect1y"];
+    let pos2x = posx + letterData["Rect2x"];
+    let pos2y = posy + letterData["Rect2y"];
+    let pos3x = posx + letterData["Rect3x"];
+    let pos3y = posy + letterData["Rect3y"];
+    let pos4x = posx + letterData["Rect4x"];
+    let pos4y = posy + letterData["Rect4y"];
+    let pos5x = posx + 40;
+    let pos5y = posy + 0;
+
+    let height1y = letterData["Rect1h"];
+    let height2y = letterData["Rect2h"];
+    let height3y = letterData["Rect3h"];
+    let height4y = letterData["Rect4h"];
+    let height5y = 178;
+    let width5x = 114;
+
+    let rot1 = letterData["Rect1r"];
+    let rot2 = letterData["Rect2r"];
+    let rot3 = letterData["Rect3r"];
+    let rot4 = letterData["Rect4r"];
+    // square 1
+    push();
+    strokeCap(SQUARE);
+    angleMode(DEGREES);
+    rectMode(CENTER);
+    noStroke();
+    fill(white);
+    push();
+    translate(pos1x, pos1y);
+    rotate(rot1);
+    rect(0, 0, 30, height1y);
+    pop();
+
+    push();
+    translate(pos2x, pos2y);
+    rotate(rot2);
+    rect(0, 0, 30, height2y);
+    pop();
+
+    push();
+    translate(pos3x, pos3y);
+    rotate(rot3);
+    rect(0, 0, 30, height3y);
+    pop();
+
+    push();
+    translate(pos4x, pos4y);
+    rotate(rot4);
+    rect(0, 0, 30, height4y);
+    pop();
+
+    push();
+    fill(0, 0, 0, 0);
+    stroke(strokeColor);
+    strokeWeight(20);
+    translate(pos5x, pos5y);
+    rect(0, 0, width5x, height5y);
+    pop();
+    pop();
+  }
 }
+
+
+
+
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
