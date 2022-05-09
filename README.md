e3809c41b2a8b96856428f85eaec57cbd4cf8fa4
Jakob-Dove <hackerzfuel@gmail.com>
2022-05-08 20:57:46 -0500

    Interpolation changes

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## MDDN 242 2022 Assignment 2
 
-## May 2nd part 2
+## May 9th
 
-  Finished J, g, and w.  Finished all the numbers and question mark.
-  They definitely need some refinement.  I have few ideas how to make the 0 and O look different.
-  I have an idea to make the M and A look different as well.
+  Played around with interpolation.  Found that 50% was the best number for interpolation so far.  50% interpolation has the least amount of stuttering.  I will definitely try a different number that is lower than 50%.  I think the color palette looks good.  With the current colors I would probably change the background color.  I will definitely test a few other colors out.  I will also start gathering my exhibition words.
--- draw_letters.js
+++ draw_letters.js
@@ -26,14 +26,13 @@ function drawLetter(letterData) {
   // This code is the foundation of my current project all the credit goes to jeremymaih here is a link to his code https://bl.ocks.org/jeremymaih/99e8644e3b6b4ec8ef263a254e0f017f/a389cc22435a93266aad7e4361f7d655367b045e
   let pos1x = posx + letterData["Rect1x"];
   let pos1y = posy + letterData["Rect1y"];
+
   let pos2x = posx + letterData["Rect2x"];
   let pos2y = posy + letterData["Rect2y"];
+
   let pos3x = posx + letterData["Rect3x"];
   let pos3y = posy + letterData["Rect3y"];
-  // let pos4x = posx + letterData["Rect4x"];
-  // let pos4y = posy + letterData["Rect4y"];
-  // let pos5x = posx + letterData["Rect5x"];
-  // let pos5y = posy + letterData["Rect5y"];
+
 
   let height1y = letterData["Rect1h"];
   let height2y = letterData["Rect2h"];
@@ -43,10 +42,10 @@ function drawLetter(letterData) {
   let rectwidth = letterData["width"];
   let rect2width = letterData["width2"];
   let rect3width = letterData["width3"];
+
   let rot1 = letterData["Rect1r"];
   let rot2 = letterData["Rect2r"];
   let rot3 = letterData["Rect3r"];
-  // let rot4 = letterData["Rect4r"];
 
   let cornerVal = letterData["corners"];
 
@@ -57,21 +56,24 @@ function drawLetter(letterData) {
   rectMode(CENTER);
 
   push();
-  translate(pos1x, pos1y);
+  fill(2,250,216);//turq
+  translate(pos1x, pos1y);//retangle 1
   rotate(rot1);
   rect(0, 0, rectwidth, height1y,cornerVal);
   pop();
 
   push();
-  translate(pos2x, pos2y);
+  fill(200,3,80);//red
+  translate(pos2x, pos2y);//rectangle 2
   rotate(rot2);
   rect(0,0,rect2width,height2y,cornerVal);//left side of the A
   pop();
 
   push();
-  translate(pos3x, pos3y);
+  fill(153,2,214);//purple
+  translate(pos3x, pos3y);//Rectangle 3
   rotate(rot3);
-  rect(0,0,rect3width,height3y,cornerVal);//left side of the A
+  rect(0,0,rect3width,height3y,cornerVal);
   pop();
 
 
@@ -79,11 +81,29 @@ function drawLetter(letterData) {
 
 }
 
-function interpolate_letter(perc90t, oldObj, newObj) {
+function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
-  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
-  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
-  new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
+  new_letter["Rect1x"]  = map(percent, 50, 100, oldObj["Rect1x"], newObj["Rect1x"]);
+  new_letter["Rect1y"] = map(percent, 0, 100, oldObj["Rect1y"], newObj["Rect1y"]);
+  new_letter["Rect2x"] = map(percent, 50, 100, oldObj["Rect2x"], newObj["Rect2x"]);
+  new_letter["Rect2y"] = map(percent, 0, 100, oldObj["Rect2y"], newObj["Rect2y"]);
+  new_letter["Rect3x"] = map(percent, 50, 100, oldObj["Rect3x"], newObj["Rect3x"]);
+  new_letter["Rect3y"] = map(percent, 0, 100, oldObj["Rect3y"], newObj["Rect3y"]);
+  new_letter["Rect1h"] = map(percent, 0, 100, oldObj["Rect1h"], newObj["Rect1h"]);
+  new_letter["Rect2h"] = map(percent, 0, 100, oldObj["Rect2h"], newObj["Rect2h"]);
+  new_letter["Rect3h"] = map(percent, 0, 100, oldObj["Rect3h"], newObj["Rect3h"]);
+  new_letter["Rect4h"] = map(percent, 0, 100, oldObj["Rect4h"], newObj["Rect4h"]);
+  new_letter["width"] = map(percent, 0, 100, oldObj["width"], newObj["width"]);
+  new_letter["width2"] = map(percent, 0, 100, oldObj["width2"], newObj["width2"]);
+  new_letter["width3"] = map(percent, 0, 100, oldObj["width3"], newObj["width3"]);
+  new_letter["Rect1r"] = map(percent, 0, 100, oldObj["Rect1r"], newObj["Rect1r"]);
+  new_letter["Rect2r"] = map(percent, 0, 100, oldObj["Rect2r"], newObj["Rect2r"]);
+  new_letter["Rect3r"] = map(percent, 0, 100, oldObj["Rect3r"], newObj["Rect3r"]);
+  new_letter["corners"] = map(percent, 0, 100, oldObj["corners"], newObj["corners"]);
+
+  // if(percent == 50);{
+  //   new_letter["Rect2x"] = map(percent, 0, 100, oldObj["Rect2x"], newObj["Rect2x"]);
+  // }
   return new_letter;
 }
 
