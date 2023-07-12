7587c8d0d3b01b1bca837109f9ebff7e17960bd5
phoebeoliff <phoebeoliff@gmail.com>
2023-07-13 11:27:13 +1200

    maeda_clock 1
    added hour number and two dots

--- maeda_clock.js
+++ maeda_clock.js
@@ -5,13 +5,82 @@ function draw_clock(obj) {
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  //text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
 
-  fill(200);
+
+  fill(200); //grey/white
+  ellipse(100,140, 30, 30);
+  
+  fill(200); //grey/white
+  ellipse(100,170, 30, 30);
+
+  fill(200); //grey/white
+  ellipse(70,170, 30, 30); //numberflare
+  
+  fill(200); //grey/white
   ellipse(100,200, 30, 30);
 
   fill(200);
   ellipse(100,230, 30, 30);
+
+  fill(200);
+  ellipse(100,260, 30, 30);
+
+  fill(200);
+  ellipse(100,290, 30, 30);
+
+  fill(200);
+  ellipse(100,320, 30, 30);
+
+  fill(200);
+  ellipse(100,350, 30, 30);
+
+  fill(200);
+  ellipse(100,380, 30, 30);
+
+  //second '1'
+
+  fill(200); //grey/white
+  ellipse(300,140, 30, 30);
+  
+  fill(200); //grey/white
+  ellipse(300,170, 30, 30);
+
+  fill(200); //grey/white
+  ellipse(270,170, 30, 30); //numberflare
+  
+  fill(200); //grey/white
+  ellipse(300,200, 30, 30);
+
+  fill(200);
+  ellipse(300,230, 30, 30);
+
+  fill(200);
+  ellipse(300,260, 30, 30);
+
+  fill(200);
+  ellipse(300,290, 30, 30);
+
+  fill(200);
+  ellipse(300,320, 30, 30);
+
+  fill(200);
+  ellipse(300,350, 30, 30);
+
+  fill(200);
+  ellipse(300,380, 30, 30);
+
+//dots
+fill(200);
+ellipse(width/2,170, 30, 30);
+
+fill(200);
+ellipse(width/2,350, 30, 30);
+
+//minutesnumbers
+
+
+
 
 
 }
