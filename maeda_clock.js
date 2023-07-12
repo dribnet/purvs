961ea2d722c2cd487a70a76ee9a456d2928213cf
daniel-gibbens <dr.gibbens@outlook.com>
2023-07-13 11:38:48 +1200

    Created drawBox and drawNum functions

--- maeda_clock.js
+++ maeda_clock.js
@@ -3,7 +3,112 @@ function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(50); //  beige
   fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+
+  //var timeString = obj.hours + ":" + obj.hours + ":" + obj.seconds;
+  //text(timeString, width/2, height/2);
+
+  numberMatrix = [
+
+    [[1,1,1],
+    [1,0,1],
+    [1,0,1],
+    [1,0,1],
+    [1,1,1]],
+
+    [[1,1,0],
+    [0,1,0],
+    [0,1,0],
+    [0,1,0],
+    [1,1,1]],
+
+    [[1,1,1],
+    [0,0,1],
+    [1,1,1],
+    [1,0,0],
+    [1,1,1]],
+
+    [[1,1,1],
+    [0,0,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1]],
+
+    [[1,0,1],
+    [1,0,1],
+    [1,1,1],
+    [0,0,1],
+    [0,0,1]],
+
+    [[1,1,1],
+    [1,0,0],
+    [1,1,1],
+    [0,0,1],
+    [1,1,1]],
+
+    [[1,1,1],
+    [1,0,0],
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]],
+
+    [[1,1,1],
+    [0,0,1],
+    [0,0,1],
+    [0,0,1],
+    [0,0,1]],
+
+    [[1,1,1],
+    [1,0,1],
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]],
+
+    [[1,1,1],
+    [1,0,1],
+    [1,1,1],
+    [0,0,1],
+    [1,1,1]]
+    
+  ];
+
+  console.log(String(obj.seconds))
+  console.log(String(obj.seconds).slice(0, 1))
+
+  
+
+  var firstSecond = String(obj.seconds).slice(0, 1);
+  var secondSecond = String(obj.seconds).slice(-1);
+
+  var firstMat = numberMatrix[firstSecond];
+  var secondMat = numberMatrix[secondSecond];
+
+  drawNum(20, 20, firstMat);
+  drawNum(120, 20, secondMat);
 }
+
+function drawNum(xOffSet, yOffset, numberMat) {
+  const rows = 5;
+  const cols = 3;
+
+
+  for (let col = 0; col < cols; col++) {
+    for (let row = 0; row < rows; row++) {
+      
+      if (numberMat[row][col]) {
+        drawBox(25 * col + xOffSet, 25 * row + yOffset);
+      }
+    }
+  }
+}
+
+
+function drawBox(x, y) {
+  beginShape();
+    vertex(x,y);
+    vertex(x+20,y);
+    vertex(x+20,y+20);
+    vertex(x,y+20);
+  endShape(CLOSE);
+}
+
+// edit the git config file so it doesnt matter when you commit
