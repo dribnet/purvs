940ccb1c249ea57c4fdd16bf73ad1bbbdf3b8012
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-07-13 11:27:50 +1200

    ellipses
    added 2 ellipse

--- maeda_clock.js
+++ maeda_clock.js
@@ -7,6 +7,8 @@ function draw_clock(obj) {
   textAlign(CENTER, CENTER);
   text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
   rect(50,50,50,400);
-  rect(150,50,50,40);
- 
+  rect(150,50,50,400);
+  ellipse(250,150,50,50);
+  ellipse(250,350,50,50);
+  
 }
