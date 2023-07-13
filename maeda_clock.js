5fc21eba997cd73bb75f8b81e1a86819889d2e92
RitaClose <104946160+RitaClose@users.noreply.github.com>
2023-07-13 13:33:59 +1200

    Update maeda_clock.js
    Ticking Clock Done.

--- maeda_clock.js
+++ maeda_clock.js
@@ -3,6 +3,8 @@ let hourX = 12;
 let min = 36;
 let spin = 0;
 let tick = 0;
+let hourSet;
+let morning = "AM";
 
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
@@ -11,16 +13,43 @@ function draw_clock(obj) {
   textSize(30);
   //textAlign(CENTER, CENTER);
   angleMode(DEGREES);
-  tick += 0.1;
-  if (tick % 1 == 0) {
-    spin += 0.3;
+  
+  tick += 1;
+  if (tick == 60) {
+    spin += 6;
+    tick = 0;
   }
-
   translate(width/2, height/2);
-  text("AM", -30, 0);
+  push();
+    rotate(1);
+    text(morning, 0, 0);
+  pop();
   rotate(spin);
-  translate(60, 0);
-  text(hourX + ":" + min, 0, 0);
-  //rect(500, 300, 100, 100);
+  translate(100, 0);
+  textAlign(CENTER);
+  push();
+    rotate(-90);
+    
+    if (hour() > 12) {
+      hourSet = hour() - 12;
+      morning = "PM";
+      if (hourSet < 10) {
+        text("0\n" + hourSet, 0, 0);
+      }
+    } else {
+      morning = "AM";
+      let hourArr = Array/from(String(hourSet), Number);
+      text(hourArr[0] + "\n" + hourArr[1], 0, 0);
+    }
+
+    if (minute() < 10) {
+      hourSet = hour();
+      text("\n..\n0" + minute(), 20, 0);
+    } else {
+      let minSet = minute();
+      let minArr = Array.from(String(minSet), Number);
+      text("\n..\n" + minArr[0] + "\n" + minArr[1], 0, 10);
+    }
+  pop();
 
 }
