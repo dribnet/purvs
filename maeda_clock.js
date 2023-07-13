6ee36a485397c20aecc4374f17d000e3845d3707
daniel-gibbens <dr.gibbens@outlook.com>
2023-07-13 16:02:29 +1200

    Refactored drawDigit function

--- maeda_clock.js
+++ maeda_clock.js
@@ -62,7 +62,6 @@ const numberMatrix = [
   
 ];
 
-
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
@@ -71,9 +70,6 @@ function draw_clock(obj) {
 
   var timeString = obj.hours + ":" + obj.hours + ":" + obj.seconds;
   text(timeString, width/2, height/2);
-
-  //console.log(String(obj.seconds))
-  //console.log(String(obj.seconds).slice(0, 1))
 
   var firstSecond = String(obj.seconds).slice(0, 1);
   var secondSecond = String(obj.seconds).slice(-1);
@@ -85,11 +81,12 @@ function draw_clock(obj) {
   var firstMat = numberMatrix[firstSecond];
   var secondMat = numberMatrix[secondSecond];
 
-  drawNum(20, 20, firstMat);
-  drawNum(120, 20, secondMat);
+  drawDigit(20, 20, firstMat);
+  drawDigit(120, 20, secondMat);
 }
 
-function drawNum(xOffSet, yOffset, numberMat) {
+
+function drawDigit(x, y, numberMat) {
   const rows = 5;
   const cols = 3;
 
@@ -98,7 +95,7 @@ function drawNum(xOffSet, yOffset, numberMat) {
     for (let row = 0; row < rows; row++) {
       
       if (numberMat[row][col]) {
-        drawBox(25 * col + xOffSet, 25 * row + yOffset);
+        drawBox(25 * col + x, 25 * row + y);
       }
     }
   }
