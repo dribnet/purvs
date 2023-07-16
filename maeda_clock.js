be55501f36d307df2d515255c538cf37756927cc
JadeLittlee <jade23.nz@gmail.com>
2023-07-16 18:42:54 +1200

    maeda clock edit

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,28 @@
+
+
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  background(0); //  black
+  fill(255);
+
+  var size = 30;
+  var xposArray = [150,180,210,240,270];
+
+  //the five
+  ellipse(xposArray[0], 100, size);
+  ellipse(xposArray[1], 100, size);
+  ellipse(xposArray[2], 100, size);
+  ellipse(xposArray[3], 100, size);
+  ellipse(xposArray[4], 100, size);
+ 
+  //the two dots
+
+  //the 3
+
+  //the 0
+ 
+
+
+
 }
