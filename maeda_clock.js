0affb50001980e1709fad1da41d9761ce947a386
khaiii <khai.sheak@motuekahigh.school.nz>
2023-07-18 00:37:37 +1200

    Maeda Clock Update 1

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,174 @@
-// Update this function to draw you own maeda clock on a 960x500 canvas
+// let rowAmount = 25;
+// let columnAmount = rowAmount / 2 + 1;
+// let gridSize = 960 / rowAmount;
+
+// let alive = true;
+
+// // Update this function to draw you own maeda clock on a 960x500 canvas
+// function draw_clock(obj) {
+//   // YOUR MAIN CLOCK CODE GOES HERE
+//   background(50); //  beige
+//   fill(200); // dark grey
+
+//   grid();
+//   checkNeighbours(6, 8, gridSize);
+//   checkNeighbours(7, 9, gridSize);
+
+//   checkNeighbours(10, 5, gridSize);
+// }
+
+// //background grid
+// function grid() {
+//   for (row = 0; row < rowAmount; row++) {
+//     for (column = 0; column < columnAmount; column++) {
+
+//       // new activeSquare(row, column, gridSize);
+//       //array added?
+//       // new checkNeighbours(row, column, gridSize);
+
+//       textAlign(CENTER, CENTER);
+//       textSize(rowAmount / 3);
+//       stroke(0);
+//       fill(200);
+//       text(
+//         row + "," + column,
+//         row * gridSize,
+//         column * gridSize,
+//         gridSize,
+//         gridSize
+//       );
+
+//       // if(activeSquare(row) >= activeSquare.checkNeighbours(row)){
+
+//       // }
+//     }
+//   }
+// }
+
+// //main body
+// function activeSquare(bodyX, bodyY, size) {
+//   this.bodyX = bodyX;
+//   this.bodyY = bodyY;
+//   this.size = size;
+
+//   stroke(0);
+//   fill(150);
+//   rect(bodyX * gridSize, bodyY * gridSize, size);
+// }
+
+// //checks neighbour
+// function checkNeighbours(hitX, hitY, size) {
+//   this.hitX = [hitX] + 1;
+//   this.hitY = [hitY] + 1;
+//   this.size = size;
+
+//   if (alive === true) {
+//     stroke(255, 0, 0);
+//     noFill();
+//     rect([hitX] * gridSize, [hitY] * gridSize, gridSize * 3);
+
+//     stroke(0);
+//     fill(150);
+//     new activeSquare([hitX + 1], [hitY + 1], gridSize);
+//   }
+// }
+
+let rowAmount = 25;
+let columnAmount = Math.floor(rowAmount / 2) + 1;
+let gridSize = 960 / rowAmount;
+
+let grid = [];
+let alive = true;
+
+// Update this function to draw your own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
+  background(50); // beige
   fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+
+  drawGrid();
+  // console.log(mouseX);
+
+  // put alive nubmer her
+  one();
+  activeNumbers(10, 10);
 }
+
+// Initialize the grid
+function initializeGrid() {
+  for (let row = 0; row < rowAmount; row++) {
+    grid[row] = [];
+    for (let column = 0; column < columnAmount; column++) {
+      grid[row][column] = {
+        row: row,
+        column: column,
+        size: gridSize,
+      };
+    }
+  }
+}
+
+// Background grid
+function drawGrid() {
+  for (let row = 0; row < rowAmount; row++) {
+    for (let column = 0; column < columnAmount; column++) {
+      drawSquare(grid[row][column]);
+    }
+  }
+}
+
+// Draw square
+function drawSquare(square) {
+  stroke(0);
+  rect(square.row * square.size, square.column * square.size, square.size);
+
+  if (alive === false) {
+    fill(255, 0, 0);
+    rect(square.row * square.size, square.column * square.size, square.size);
+  }
+}
+
+function one(){
+  // if (alive === true) {
+  //   fill(255, 0, 0);
+  //   drawSquare(grid[7][5]);
+  //   drawSquare(grid[8][4]);
+  //   drawSquare(grid[8][5]);
+  //   drawSquare(grid[8][6]);
+  //   drawSquare(grid[8][7]);
+  //   drawSquare(grid[8][8]);
+  //   drawSquare(grid[8][9]);
+  // }
+
+  // if (activeNumbers.noZero[i][j] == 1){
+  //   fill(255, 0, 0);
+  //     drawSquare(grid[row][column]);
+  // }
+
+  activeNumbers();
+}
+
+function activeNumbers(posX, posY){
+
+  for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 7; j++){
+      let noZero = [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+      ]
+
+      if (noZero[j][i] === 1){
+        fill(255, 0, 0);
+          drawSquare(grid[i][j]);
+      }
+    }
+  }
+}
+
+initializeGrid();
+drawGrid();
