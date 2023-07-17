8efd96e3735956152abe9b4a25889c912c2e1d5a
JadeLittlee <jade23.nz@gmail.com>
2023-07-18 09:56:58 +1200

    clock update

--- clock.js
+++ clock.js
@@ -11,17 +11,23 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
+ 
   background(50); //  beige
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+ 
 
+  let Seconds = obj.seconds
+
+  text("Seconds: " + Seconds, width / 2, 200), 
 
   fill(249, 140, 255);// pink
   ellipse(width / 3, 350, 150);
+
   fill(140, 255, 251) // blue
   ellipse(width / 2, 350, 150);
+  
   fill(175, 133, 255); // purple
   ellipse(width / 3 * 2, 350, 150);
 
