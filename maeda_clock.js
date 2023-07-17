8f722370d83f437682353735f2b7b3fe227f63ff
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2023-07-17 21:52:40 +1200

    Got everything working
    The clock is working as intended, a fairly close replica of the Maeda clock I'm trying to recreate. My numbers use a segment display style, where every number is a subset of the active segments in the number 8. This doesn't read nearly as well as the Maeda version. To change this the coordinate values for each number could be changed, without the rest of the code needing any changes.

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,104 +1,93 @@
-originX = 25;
+originX = 150;
 originY = 25;
 
-{ // number shape coords (two arrays for each number, creating pairs of coordinates for X and Y)
+{ // number shape coords map(two arrays for each number, creating pairs of coordinates for X and Y, value + .1 for X, .2 for Y)
 
-numberZeroXCoords = [50, 100, 150, 0, 200, 0, 200, 0, 200, 0, 200, 0, 200, 0, 200, 50, 100, 150];
-numberZeroYCoords = [0, 0, 0, 50, 50, 100, 100, 150, 150, 250, 250, 300, 300, 350, 350, 400, 400, 400];
+var numbersMap = new Map();
+numbersMap.set(0.1, [50, 100, 150, 0, 200, 0, 200, 0, 200, 0, 200, 0, 200, 0, 200, 50, 100, 150]);
+numbersMap.set(0.2, [0, 0, 0, 50, 50, 100, 100, 150, 150, 250, 250, 300, 300, 350, 350, 400, 400, 400]);
 
-numberOneXCoords = [200, 200, 200, 200, 200, 200];
-numberOneYCoords = [50, 100, 150, 250, 300, 350];
+numbersMap.set(1.1, [200, 200, 200, 200, 200, 200]);
+numbersMap.set(1.2, [50, 100, 150, 250, 300, 350]);
 
-numberTwoXCoords = [50, 100, 150, 200, 200, 200, 50, 100, 150, 0, 0, 0, 50, 100, 150];
-numberTwoYCoords = [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400];
+numbersMap.set(2.1, [50, 100, 150, 200, 200, 200, 50, 100, 150, 0, 0, 0, 50, 100, 150]);
+numbersMap.set(2.2, [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400]);
 
-numberThreeXCoords = [50, 100, 150, 200, 200, 200, 50, 100, 150, 200, 200, 200, 50, 100, 150];
-numberThreeYCoords = [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400];
+numbersMap.set(3.1, [50, 100, 150, 200, 200, 200, 50, 100, 150, 200, 200, 200, 50, 100, 150]);
+numbersMap.set(3.2, [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400]);
 
-numberFourXCoords = [0, 200, 0, 200, 0, 200, 50, 100, 150, 200, 200, 200];
-numberFourYCoords = [50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 300, 350];
+numbersMap.set(4.1, [0, 200, 0, 200, 0, 200, 50, 100, 150, 200, 200, 200]);
+numbersMap.set(4.2, [50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 300, 350]);
 
-numberFiveXCoords = [50, 100, 150, 0, 0, 0, 50, 100, 150, 200, 200, 200, 50, 100, 150];
-numberFiveYCoords = [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400];
+numbersMap.set(5.1, [50, 100, 150, 0, 0, 0, 50, 100, 150, 200, 200, 200, 50, 100, 150]);
+numbersMap.set(5.2, [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400]);
 
-numberSixXCoords = [50, 100, 150, 0, 0, 0, 50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150];
-numberSixYCoords = [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 250, 300, 300, 350, 350, 400, 400, 400];
+numbersMap.set(6.1, [50, 100, 150, 0, 0, 0, 50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150]);
+numbersMap.set(6.2, [0, 0, 0, 50, 100, 150, 200, 200, 200, 250, 250, 300, 300, 350, 350, 400, 400, 400]);
 
-numberSevenXCoords = [50, 100, 150, 200, 200, 200, 200, 200, 200];
-numberSevenYCoords = [0, 0, 0, 50, 100, 150, 250, 300, 350];
+numbersMap.set(7.1, [50, 100, 150, 200, 200, 200, 200, 200, 200]);
+numbersMap.set(7.2, [0, 0, 0, 50, 100, 150, 250, 300, 350]);
 
-numberEightXCoords = [50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150];
-numberEightYCoords = [0, 0, 0, 50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 250, 300, 300, 350, 350, 400, 400, 400];
+numbersMap.set(8.1, [50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150]);
+numbersMap.set(8.2, [0, 0, 0, 50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 250, 300, 300, 350, 350, 400, 400, 400]);
 
-numberNineXCoords = [50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150, 200, 200, 200, 50, 100, 150];
-numberNineYCoords = [0, 0, 0, 50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400];
+numbersMap.set(9.1, [50, 100, 150, 0, 200, 0, 200, 0, 200, 50, 100, 150, 200, 200, 200, 50, 100, 150]);
+numbersMap.set(9.2, [0, 0, 0, 50, 50, 100, 100, 150, 150, 200, 200, 200, 250, 300, 350, 400, 400, 400]);
 
 }
 
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
+  background(0); //  beige
   fill(255); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
   
   //text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
-  numberScale = 0.3;
+  numberScale = 1;
   noFill();
-  strokeWeight(5*numberScale);
+
+  // convert time to digits and draw them, checks if the value is one digit, adds a 0 in front if it is
+
   stroke(255, 0, 0);
+  strokeWeight(3*numberScale);
 
-  // draw a number 0
-  for(let i = 0; i < numberZeroXCoords.length; i++) {
-    ellipse(0*numberScale+originX+numberZeroXCoords[i]*numberScale, originY+numberZeroYCoords[i]*numberScale, 40*numberScale);
+  if(obj.hours.toString().length > 1) {
+    draw_number(Number(obj.hours.toString()[0]), 0, 40);
+    draw_number(Number(obj.hours.toString()[1]), 1, 40);
+  }  else {
+    draw_number(0, 0, 40);
+    draw_number(Number(obj.hours.toString()[0]), 0, 40);
   }
 
-  // draw a number 1
-  for(let i = 0; i < numberOneXCoords.length; i++) {
-    ellipse(300*numberScale+originX+numberOneXCoords[i]*numberScale, originY+numberOneYCoords[i]*numberScale, 40*numberScale);
+  stroke(255, 255, 0);
+  strokeWeight(3*numberScale);
+
+  if(obj.minutes.toString().length > 1) {
+    draw_number(Number(obj.minutes.toString()[0]), 0, 30);
+    draw_number(Number(obj.minutes.toString()[1]), 1, 30);
+  }  else {
+    draw_number(0, 0, 30);
+    draw_number(Number(obj.minutes.toString()[0]), 1, 30);
   }
 
-  // draw a number 2
-  for(let i = 0; i < numberTwoXCoords.length; i++) {
-    ellipse(600*numberScale+originX+numberTwoXCoords[i]*numberScale, originY+numberTwoYCoords[i]*numberScale, 40*numberScale);
+  stroke(0, 0, 255);
+  strokeWeight(3*numberScale);
+
+  if(obj.seconds.toString().length > 1) {
+    draw_number(Number(obj.seconds.toString()[0]), 0, 20);
+    draw_number(Number(obj.seconds.toString()[1]), 1, 20);
+  }  else {
+    draw_number(0, 0, 20);
+    draw_number(Number(obj.seconds.toString()[0]), 1, 20);
   }
 
-  // draw a number 3
-  for(let i = 0; i < numberThreeXCoords.length; i++) {
-    ellipse(900*numberScale+originX+numberThreeXCoords[i]*numberScale, originY+numberThreeYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  // draw a number 4
-  for(let i = 0; i < numberFourXCoords.length; i++) {
-    ellipse(1200*numberScale+originX+numberFourXCoords[i]*numberScale, originY+numberFourYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  // draw a number 5
-  for(let i = 0; i < numberFiveXCoords.length; i++) {
-    ellipse(1500*numberScale+originX+numberFiveXCoords[i]*numberScale, originY+numberFiveYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  // draw a number 6
-  for(let i = 0; i < numberSixXCoords.length; i++) {
-    ellipse(1800*numberScale+originX+numberSixXCoords[i]*numberScale, originY+numberSixYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  // draw a number 7
-  for(let i = 0; i < numberSevenXCoords.length; i++) {
-    ellipse(2100*numberScale+originX+numberSevenXCoords[i]*numberScale, originY+numberSevenYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  // draw a number 8
-  for(let i = 0; i < numberEightXCoords.length; i++) {
-    ellipse(2400*numberScale+originX+numberEightXCoords[i]*numberScale, originY+numberEightYCoords[i]*numberScale, 40*numberScale);
-  }
- 
-  // draw a number 9
-  for(let i = 0; i < numberNineXCoords.length; i++) {
-    ellipse(2700*numberScale+originX+numberNineXCoords[i]*numberScale, originY+numberNineYCoords[i]*numberScale, 40*numberScale);
-  }
-
-  
 }
 
+// Draw a given number in a given position, with a given circle size
+function draw_number(number, position, circleSize) {
+  for(let i = 0; i < numbersMap.get(number+0.1).length; i++) {
+    ellipse(position*300*numberScale+originX+numbersMap.get(number+0.1)[i]*numberScale, originY+numbersMap.get(number+0.2)[i]*numberScale, circleSize*numberScale);
+  }
+}
