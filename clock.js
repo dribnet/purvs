b98fed7be3c94259ff70a9dc3c0212227b025e5a
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2023-07-18 11:18:11 +1200

    Attempt at earth orbit DOESN'T WORK

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -12,6 +13,7 @@ function draw_clock(obj) {
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
   background(0); //  black background
+  angleMode(DEGREES);
   fill(200); // dark grey
   textSize(40);
   //textAlign(CENTER, CENTER);
@@ -22,12 +24,25 @@ function draw_clock(obj) {
   let m= map(obj.seconds, 0, 59, 0, height);
 
   fill(249, 174, 10);// yellow sun
-  ellipse(width/2, m, 150);
+  ellipse(width/2, height/2, 150);
   //fill(140, 255, 251) // blue
   //ellipse(width / 2, 350, ellipsewidth);
   //fill(175, 133, 255); // purple
   //ellipse(width / 3 * 2, 350, ellipsewidth);
 
+  //attempt at rotating earth
+
+  let angle = 50;
+  let scalar = 100;
+  let startX = width/2;
+  let startY= height/2;
   
+  let x = startX + scalar * cos(angle);
+  let y = startY + scalar * sin(angle);
+
+  ellipse(x+obj.seconds,y+obj.seconds,80);
+
+  angle++;
+
 
 }
