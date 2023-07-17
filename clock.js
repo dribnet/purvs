6a23df922962da688b1ddcaa4d5c35fbe467cafa
MamieJatthews <jama.a.matthews@gmail.com>
2023-07-18 11:21:49 +1200

    fhgj

--- clock.js
+++ clock.js
@@ -11,18 +11,40 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  
+  let hours = obj.hours;
+  let minutes = obj.minutes;
+  let seconds = obj.seconds;
+  let millis = obj.millis;
+  let alarm = obj.seconds_until_alarm;
 
+    background(50); //  Grey
+    fill(255); // white
+    textSize(20);
+    text("Hour: "   + hours, width*0.2, 22);
+    text("Minute: " + minutes, width*0.4, 22);
+    text("Second: " + seconds, width*0.6, 22);
+    text("Millis: " + millis, width*0.8, 22);
 
+  // fill(200); // dark grey
+  // textSize(40);
+  // textAlign(CENTER, CENTER);
+  // text("Seconds: " + seconds, width / 2, 200);
+
+  let hours_radius = map(obj.hours, 0, 59, 1, 150);
   fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+  ellipse(width / 3, 350, hours_radius);
 
+  let minutes_radius = map(obj.minutes, 0, 59, 1, 150);
+  fill(140, 255, 251) // blue
+  ellipse(width / 2, 350, minutes_radius);
+
+  let seconds_radius = map(seconds, 0, 59, 1, 150);
+  fill(175, 133, 255); // purple
+  ellipse(width / 3 * 2, 350, seconds_radius);
+
+  let rect_height = map(seconds, 0, 59, 1, 150);
+  fill(255);
+  noStroke();
+  rect(width/2, height/2, 40, - rect_height);
 }
--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,31 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  background(0, 0, 0); //  red
+  fill(255); // white
+  
+  //1 
+  ellipse(150, 370, 40);
+  ellipse(150, 330, 40);
+  ellipse(150, 290, 40);
+  ellipse(150, 250, 40);
+  ellipse(150, 210, 40);
+  ellipse(150, 170, 40);
+  ellipse(150, 130, 40);
+  ellipse(110, 170, 40);
+
+  //0
+  ellipse(150, 370, 40);
+  ellipse(150, 330, 40);
+  ellipse(150, 290, 40);
+  ellipse(150, 250, 40);
+  ellipse(150, 210, 40);
+  ellipse(150, 170, 40);
+  ellipse(150, 130, 40);
+  ellipse(110, 170, 40);
+  
+  //middle dots
+  ellipse(960/2, 500/3, 40);
+  ellipse(960/2, 500/1.5, 40);
+
 }
