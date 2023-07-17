f403a16beaf8b2238e2b2aa42b033693a42d4180
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-07-18 09:54:23 +1200

    Update maeda_clock.js
    made comments and adding more rects

--- maeda_clock.js
+++ maeda_clock.js
@@ -6,13 +6,19 @@ function draw_clock(obj) {
   textSize(40);
   textAlign(CENTER, CENTER);
   text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
-  rect(50,50,50,400);
-  rect(150,50,50,400);
-  ellipse(250,150,50,50);
-  ellipse(250,350,50,50);
-  rect(350,50,200,50);
-  rect(500,50,50,400);
-  rect(350,400,200,50);
-  rect(350,215,200,50);
-  Rect(400,50,300,400);
+  rect(50,50,50,400);//one
+  rect(150,50,50,400); //one
+
+  ellipse(250,150,50,50); //middle dot thing
+  ellipse(250,350,50,50); //midle dot thing
+
+  rect(350,50,200,50); //part of the three
+  rect(500,50,50,400); //part of the three
+  rect(350,400,200,50); //part of the three
+  rect(350,215,200,50); //part of the three
+  Rect(400,50,300,400); //part of the three
+
+  rect(600,50,200,50); //part of zero
+  rect()
+
 }
