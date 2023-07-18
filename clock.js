1bc8a0252c9f274586da0d3d7c61d82850cab6ed
Skiidosh <raquidtrei1@myvuw.ac.nz>
2023-07-18 14:03:19 +1200

    Created clock foundation

--- clock.js
+++ clock.js
@@ -11,18 +11,56 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
+  background(3); //  beige
   fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
 
+  SecRing()
+}
 
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+function SecRing() {
+  let millis = obj.millis
+  let seconds = obj.seconds
+  let secFract = seconds + (millis/1000)
 
+  let diameter = 300
+  let secRing1 = map(secFract,0,60,0,PI*2)
+  let secRing2 = map(secFract,0,60,0,PI*8)
+  let secRing3 = map(secFract,0,60,0,PI*30)
+  let secRing4 = map(secFract,0,60,0,PI*60)
+
+  // Define styles of our geometry (circle)
+  stroke('white')
+  strokeWeight(0)
+  noFill()
+    
+  // Create the circle
+  circle(width / 2, height / 2, diameter)
+  
+  // Draw a single point
+  let angle1 = secRing1+(PI*1.5)
+  let angle2 = secRing2+(PI*1.5)
+  let angle3 = secRing3+(PI*1.5)
+  let angle4 = secRing4+(PI*1.5)
+
+  //circle1
+  let circle1x = diameter / 2 * Math.cos(angle1) + width / 2;
+  let circle1y = diameter / 2 * Math.sin(angle1) + height / 2;
+  stroke('white')
+  strokeWeight(12)
+  point(circle1x, circle1y)
+
+  //circle2
+  let circle2x = diameter / 2 * Math.cos(angle2) + width / 2;
+  let circle2y = diameter / 2 * Math.sin(angle2) + height / 2;
+  point(circle2x, circle2y)
+
+  //circle3
+  let circle3x = diameter / 2 * Math.cos(angle3) + width / 2;
+  let circle3y = diameter / 2 * Math.sin(angle3) + height / 2;
+  point(circle3x, circle3y)
+
+  //circle4
+  let circle4x = diameter / 2 * Math.cos(angle4) + width / 2;
+  let circle4y = diameter / 2 * Math.sin(angle4) + height / 2;
+  point(circle4x, circle4y)
 }
