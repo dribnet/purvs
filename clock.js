f50464e9d38b0fd02fd303981f2a36a0f1bbbed3
tommyeryka <tommyeryk@myvuw.ac.nz>
2023-07-18 12:34:14 +1200

    update today

--- clock.js
+++ clock.js
@@ -15,7 +15,7 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
+  background(0); //  beige
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
@@ -28,25 +28,53 @@ function draw_clock(obj) {
 let bounce = map(obj.millis, 0,999,0,TWO_PI);
 let phase1 = sin (bounce) ;
 let y_bounce = map (phase1, -1,1,-75,75);
+
+let bounce2 = map((obj.millis+100), 0, 999, 0, TWO_PI);
+let phase2 = cos(bounce2);
+let y_bounce2 = map(phase2, -1, 1, -75, 75);
   
+//sets hours 
   let hour_radius = map(hours ,0,24,1,150);
   fill(249, 140, 255);// pink
   ellipse(width / 8, 350, +hour_radius);
 
+//sets minutes 
   let min_radius = map(minutes ,0,59,1,150);
   fill(140, 255, 251) // blue
-  ellipse(width -640, 350, +min_radius);
+  ellipse(width -640, 350+y_bounce, +min_radius*2);
   
 
-  let sec_radius = map(seconds ,0,59,1,150);
-  fill(175, 133, 255); // purple
-  ellipse(width -400, 350, + y_bounce);
 
 
 
-  let millis_radius = map(millis ,0,1000,1,150);
-  fill(200, 13, 55); // red
-  ellipse(width -150, 350 +y_bounce, +millis_radius);
  
+//controls the shrink/ restart point or look of the circle
+  let secondsWithFraction   = seconds + (millis / 1000.0);
+ 
+  // text("Seconds with fraction: " + secondsWithFraction, width / 2, 50);
 
+  let seconds_radius = 0;
+  if(secondsWithFraction <= 59)  {
+    seconds_radius = map(secondsWithFraction, 0, 60, 1, 300);
+  }
+  else {
+    seconds_radius = map(millis, 0, 999, 300, 1);
+  }
+
+ // let sec_radius = map(seconds ,0,59,1,150);
+  fill(175, 133, 255); // purple
+  ellipse(width -400, 350, + seconds_radius/2.5);
+
+  let millisWithFraction   = (millis / 100.0);
+
+  let millis_radius = 0;
+  if(millisWithFraction <= 59)  {
+    millis_radius = map(millisWithFraction, 0, 60, 1, 300);
+  }
+  else {
+    millis_radius = map(seconds, 0, 999, 300, 1);
+  }
+  //let millis_radius = map(millis ,0,999,1,150);
+  fill(200, 13, 55); // red
+  ellipse(width -150, 350 +y_bounce2, +millis_radius);
 }
