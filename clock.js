3ccc6385f0e583fc0033742cf3b9f21506fe70fc
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2023-07-20 11:04:48 +1200

    Changing planet colours and rotation distance

--- clock.js
+++ clock.js
@@ -24,28 +24,22 @@ function draw_clock(obj) {
   let m= map(obj.seconds, 0, 59, 0, height);
 
   fill(249, 174, 10);// yellow sun
-  ellipse(width/2, height/2, 150);
-  //fill(140, 255, 251) // blue
-  //ellipse(width / 2, 350, ellipsewidth);
-  //fill(175, 133, 255); // purple
-  //ellipse(width / 3 * 2, 350, ellipsewidth);
+  ellipse(width/2, height/2, 125);
 
-  let rotS = map(Seconds, 0, 59, 0, 360);
-
- // rotate(rotS);
-  //ellipse(width/2,height/2,50);
-
+  let rotS = map(Seconds, 0, 60, 0, 360);
 
  
   push();
   translate(480,250);
   rotate(rotS);
-  ellipse(200, 0, 50);
+  fill(30, 111, 140); //earth blue
+  ellipse(150, 0, 50);
 
   push();
-  translate(200,0);
+  translate(150,0);
   rotate(rotS);
-  ellipse(100,0,30);
+  fill(178, 178, 178); //moon grey
+  ellipse(50,0,30);
   pop();
 
   pop();
