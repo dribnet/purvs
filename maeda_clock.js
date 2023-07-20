756bd0915afe747c0ab6f995a5c5394d35b56d58
khaiscripting <sheakkhai@myvuw.ac.nz>
2023-07-20 18:17:34 +1200

    Maeda Clock final - need to work on the main clock

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,42 +1,51 @@
-function preload() {}
-
 // Update this function to draw your own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(5, 5, 10); // black background
   fill(200, 200, 240); // white text
 
+  //hour
+  timeNumber(obj.hours);
+  //minutes
+  timeNumber(obj.minutes);
+}
+
+//text
+function timeNumber(units){
   textFont("JOKERMAN");
   textAlign(CENTER, CENTER);
 
-  //give this a map then the text size can change depending on seconds
   let fontSize = map(obj.millis, 0, 1000, 20, 150);
-
-  //////////////////////////////////////////////////////////
-
-  if(obj.seconds % 2){
-    fontSize = map(obj.millis, 0, 1000, 20, 150);
-  }
-  else if (obj.seconds % 2 + 1){
-    fontSize = map(obj.millis, 0, 1000, 150, 20);
-  }
-
-  textSize(fontSize);
-
+  
   //hour
-  text(obj.hours, canvasWidth / 4, canvasHeight / 2);
-
-  /////////////////////////////////////////////////////////
-
-  if(obj.seconds % 2){
-    fontSize = map(obj.millis, 0, 1000, 150, 20);
-  }
-  else if (obj.seconds % 2 + 1){
-    fontSize = map(obj.millis, 0, 1000, 20, 150);
-  }
+  if (units == obj.hours){
+    //pulsing
+    if(obj.seconds % 2){
+      fontSize = map(obj.millis, 0, 1000, 20, 150);
+    }
+    else if (obj.seconds % 2 + 1){
+      fontSize = map(obj.millis, 0, 1000, 150, 20);
+    }
 
   textSize(fontSize);
 
-  //minute
-  text(obj.minutes, (canvasWidth / 4) * 3, canvasHeight / 2);
+    text(units, canvasWidth / 4, canvasHeight / 2);
+  };
+
+  //minutes
+  if (units == obj.minutes){
+    //pulsing
+    if(obj.seconds % 2){
+      fontSize = map(obj.millis, 0, 1000, 150, 20);
+    }
+    else if (obj.seconds % 2 + 1){
+      fontSize = map(obj.millis, 0, 1000, 20, 150);
+    }
+
+  textSize(fontSize);
+
+    text(units, (canvasWidth / 4)*3, canvasHeight / 2);
+  };
 }
+
+
