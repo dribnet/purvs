2e8205bf1c14ad9d8270e67c4eb707663caab88f
Skiidosh <raquidtrei1@myvuw.ac.nz>
2023-07-22 16:50:07 +1200

    created second ring for minutes
    Since principle as the seconds ring. After an hour passes, all points should line up at the top of the circle.
    
    I also cleaned up the code a little by adding semi-colons at the end of every line (excluding comments)

--- clock.js
+++ clock.js
@@ -14,105 +14,129 @@ function draw_clock(obj) {
   background(3); //  beige
   fill(200); // dark grey
 
-  MinRing()
-  SecRing()
+  MinRing();
+  SecRing();
 }
 
 function MinRing() {
-  // Controls diameter size for ring
-  let diameter = 150
+  // Controls size and location for ring
+  let diameter = 150;
+  let ringX = width / 2;
+  let ringY = height / 2;
   
   // Declares time varibles revelant to (m) ring
-  let millis = obj.millis
-  let seconds = obj.seconds
-  let mins = obj.minutes
-  let minFract = mins + (seconds/60 + (millis/1000)) //smooths out movement of point
+  let millis = obj.millis;
+  let seconds = obj.seconds;
+  let mins = obj.minutes;
+  let minFract = mins + (seconds/60); //smooths out movement of point
 
-  //Map function allows timed movement around ring
-  let pointLoc1 = map(minFract,0,60,0,PI*120)
+  //Map function allows timed movement around ring (PI*x controls speed)
+  let pointLoc1 = map(minFract,0,60,0,PI*2);
+  let pointLoc2 = map(minFract,0,60,0,PI*4);
+  let pointLoc3 = map(minFract,0,60,0,PI*6);
+  let pointLoc4 = map(minFract,0,60,0,PI*12);
 
   // Define styles of our geometry (circle)
-  stroke('white')
-  strokeWeight(0)
-  noFill()
+  stroke('white');
+  strokeWeight(0);
+  noFill();
     
   // Create the circle
-  circle(width / 2, height / 2, diameter)
+  circle(ringX, ringY, diameter);
   
   // Determines location for each point, the offset of (PI*1.5) places points at top of circle
-  let angle1 = pointLoc1+(PI*1.5)
+  let angle1 = pointLoc1+(PI*1.5);
+  let angle2 = pointLoc2+(PI*1.5);
+  let angle3 = pointLoc3+(PI*1.5);
+  let angle4 = pointLoc4+(PI*1.5);
 
   //point1
-  let point1x = diameter / 2 * Math.cos(angle1) + width / 2;
-  let point1y = diameter / 2 * Math.sin(angle1) + height / 2;
-  stroke('white')
-  strokeWeight(12)
-  point(point1x, point1y)
+  let point1x = diameter / 2 * Math.cos(angle1) + ringX;
+  let point1y = diameter / 2 * Math.sin(angle1) + ringY;
+  stroke('white');
+  strokeWeight(12);
+  point(point1x, point1y);
 
+  //point2
+  let point2x = diameter / 2 * Math.cos(angle2) + ringX;
+  let point2y = diameter / 2 * Math.sin(angle2) + ringY;
+  point(point2x, point2y);
+
+  //point3
+  let point3x = diameter / 2 * Math.cos(angle3) + ringX;
+  let point3y = diameter / 2 * Math.sin(angle3) + ringY;
+  point(point3x, point3y);
+
+  //point4
+  let point4x = diameter / 2 * Math.cos(angle4) + ringX;
+  let point4y = diameter / 2 * Math.sin(angle4) + ringY;
+  point(point4x, point4y);
 }
 
 function SecRing() {
-  // Controls diameter size for ring
-  let diameter = 75
+   // Controls size and location for ring
+  let diameter = 75;
+  let ringX = width / 2;
+  let ringY = height / 2;
   
   // Declares time varibles revelant to (s) ring
-  let millis = obj.millis
-  let seconds = obj.seconds
-  let secFract = seconds + (millis/1000)
+  let millis = obj.millis;
+  let seconds = obj.seconds;
+  let secFract = seconds + (millis/1000);
 
   //Map function allows timed movement around ring
-  let pointLoc1 = map(secFract,0,60,0,PI*30)
-  let pointLoc2 = map(secFract,0,60,0,PI*20)
-  let pointLoc3 = map(secFract,0,60,0,PI*12)
-  let pointLoc4 = map(secFract,0,60,0,PI*10)
-  let pointLoc5 = map(secFract,0,60,0,PI*6)
-  let pointLoc6 = map(secFract,0,60,0,PI*4)
+  let pointLoc1 = map(secFract,0,60,0,PI*30);
+  let pointLoc2 = map(secFract,0,60,0,PI*20);
+  let pointLoc3 = map(secFract,0,60,0,PI*12);
+  let pointLoc4 = map(secFract,0,60,0,PI*10);
+  let pointLoc5 = map(secFract,0,60,0,PI*6);
+  let pointLoc6 = map(secFract,0,60,0,PI*4);
 
   // Define styles of our geometry (circle)
-  stroke('white')
-  strokeWeight(0)
-  noFill()
+  stroke('white');
+  strokeWeight(0);
+  noFill();
     
   // Create the circle
-  circle(width / 2, height / 2, diameter)
+  circle(ringX, ringY, diameter);
   
   // Determines location for each point, the offset of (PI*1.5) places points at top of circle
-  let angle1 = pointLoc1+(PI*1.5)
-  let angle2 = pointLoc2+(PI*1.5)
-  let angle3 = pointLoc3+(PI*1.5)
-  let angle4 = pointLoc4+(PI*1.5)
-  let angle5 = pointLoc5+(PI*1.5)
-  let angle6 = pointLoc6+(PI*1.5)
+  let angle1 = pointLoc1+(PI*1.5);
+  let angle2 = pointLoc2+(PI*1.5);
+  let angle3 = pointLoc3+(PI*1.5);
+  let angle4 = pointLoc4+(PI*1.5);
+  let angle5 = pointLoc5+(PI*1.5);
+  let angle6 = pointLoc6+(PI*1.5);
 
   //point1
-  let point1x = diameter / 2 * Math.cos(angle1) + width / 2;
-  let point1y = diameter / 2 * Math.sin(angle1) + height / 2;
-  stroke('white')
-  strokeWeight(12)
-  point(point1x, point1y)
+  let point1x = diameter / 2 * Math.cos(angle1) + ringX;
+  let point1y = diameter / 2 * Math.sin(angle1) + ringY;
+  stroke('white');
+  strokeWeight(12);
+  point(point1x, point1y);
 
   //point2
-  let point2x = diameter / 2 * Math.cos(angle2) + width / 2;
-  let point2y = diameter / 2 * Math.sin(angle2) + height / 2;
-  point(point2x, point2y)
+  let point2x = diameter / 2 * Math.cos(angle2) + ringX;
+  let point2y = diameter / 2 * Math.sin(angle2) + ringY;
+  point(point2x, point2y);
 
   //point3
-  let point3x = diameter / 2 * Math.cos(angle3) + width / 2;
-  let point3y = diameter / 2 * Math.sin(angle3) + height / 2;
-  point(point3x, point3y)
+  let point3x = diameter / 2 * Math.cos(angle3) + ringX;
+  let point3y = diameter / 2 * Math.sin(angle3) + ringY;
+  point(point3x, point3y);
 
   //point4
-  let point4x = diameter / 2 * Math.cos(angle4) + width / 2;
-  let point4y = diameter / 2 * Math.sin(angle4) + height / 2;
-  point(point4x, point4y)
+  let point4x = diameter / 2 * Math.cos(angle4) + ringX;
+  let point4y = diameter / 2 * Math.sin(angle4) + ringY;
+  point(point4x, point4y);
 
   //point5
-  let point5x = diameter / 2 * Math.cos(angle5) + width / 2;
-  let point5y = diameter / 2 * Math.sin(angle5) + height / 2;
-  point(point5x, point5y)
+  let point5x = diameter / 2 * Math.cos(angle5) + ringX;
+  let point5y = diameter / 2 * Math.sin(angle5) + ringY;
+  point(point5x, point5y);
 
   //point6
-  let point6x = diameter / 2 * Math.cos(angle6) + width / 2;
-  let point6y = diameter / 2 * Math.sin(angle6) + height / 2;
-  point(point6x, point6y)
+  let point6x = diameter / 2 * Math.cos(angle6) + ringX;
+  let point6y = diameter / 2 * Math.sin(angle6) + ringY;
+  point(point6x, point6y);
 }
