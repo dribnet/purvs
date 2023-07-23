737db0123202d09c6010c4aa5603c3877ff322df
kaifohann <kaifohann@myvuw.ac.nz>
2023-07-24 10:16:50 +1200

    updated clock

--- clock.js
+++ clock.js
@@ -11,18 +11,26 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
+  
+  let seconds = obj.seconds;
+  let mins = obj.minutes;
+
   background(50); //  beige
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  text("seconds = " + seconds, width / 2, 200);
 
 
   fill(249, 140, 255);// pink
   ellipse(width / 3, 350, 150);
+
+  let minRad = map(mins,0,59, 0,150)
   fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
+  ellipse(width / 2, 350, minRad);
+
+  let secondsRadius = map(seconds,0,59, 0,150)
   fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+  ellipse(width / 3 * 2, secondsRadius, secondsRadius);
 
 }
