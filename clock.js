a9d01b6d20056adae8ff01385665c9ec0ce7a2f3
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-07-25 11:11:59 +1200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,6 +1,11 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+//let img
+//function preload(){
+//img= loadImage('sketch.jpg')
+//}
+
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -12,23 +17,27 @@ function draw_clock(obj) {
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
   background(210,4,45); //  red/cherry
+  //Image(img,0,0,960,500)
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
   text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+
   
   let millisSize=0
+
+  noStroke()
 
   fill(255,191,0);// Yellow
   ellipse(width / 3, 350, 150);
   
-  fill(255,191,0,100);
+  fill(255,191,0,150);
   
 if(obj.millis<=500){
-  millisSize= map(obj.millis,0,1000,150,300)
+  millisSize= map(obj.millis,0,1000,130,300)
 }
  else if(obj.millis>=500){
-  millisSize= map(obj.millis,0,1000,300,150)
+  millisSize= map(obj.millis,0,1000,300,130)
  }
   ellipse(width/3,350,millisSize)
 
