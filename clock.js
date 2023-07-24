84578cccad245e41c4103a7f949dbce7b62067e0
jonesisla <139181549+jonesisla@users.noreply.github.com>
2023-07-25 09:56:09 +1200

    bounce
    Updated the clock so that as well as the circles getting bigger and smaller based on time, they bounce up and down as well to give the clock more motion :) yay!

--- clock.js
+++ clock.js
@@ -12,6 +12,14 @@ function draw_clock(obj) {
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
 
+
+
+
+
+
+
+
+// First clock made, the circles that get bigger and smaller based on the time, Circles also bounce up and down :)
   let millis = obj.millis;
   let seconds = obj.seconds;
   let minutes = obj.minutes;
@@ -27,26 +35,44 @@ function draw_clock(obj) {
   text("Seconds: " + seconds, width / 2, 200);
   text("Millis: " + millis, width / 2, 240);
 
+  let bounce1 = map(obj.millis, 0, 999, 0, TWO_PI);
+  let phase1 = sin(bounce1);
+  let y_bounce1 = map(phase1, -1, 1, -10, 10);
+
+  let bounce2 = map((obj.millis+100), 0, 999, 0, TWO_PI);
+  let phase2 = sin(bounce2);
+  let y_bounce2 = map(phase2, -1, 1, -10, 10);
+
+  let bounce3 = map(obj.millis, 0, 999, 0, TWO_PI);
+  let phase3 = sin(bounce3);
+  let y_bounce3 = map(phase3, -1, 1, -10, 10);
+
+  let bounce4 = map((obj.millis+100), 0, 999, 0, TWO_PI);
+  let phase4 = sin(bounce4);
+  let y_bounce4 = map(phase4, -1, 1, -10, 10);
+
   
+
   let hours_radius = map(hours, 0, 59, 1, 150);
   fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, hours_radius);
+  ellipse(width / 3, 350 + y_bounce1, hours_radius);
 
   let minutes_radius = map(minutes, 0, 59, 1, 150);
   fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, minutes_radius);
+  ellipse(width / 2, 350 + y_bounce2, minutes_radius);
 
 
   let secondsWithFraction   = seconds + (millis / 1000.0);
 
   let seconds_radius = map(secondsWithFraction, 0, 59, 1, 150);
   fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, seconds_radius);
+  ellipse(width / 3 * 2, 350 + y_bounce3, seconds_radius);
 
 
   let millis_radius = map(millis, 0, 999, 1, 150);
   fill(255, 165, 0); // orange
-  ellipse(width / 3 * 2.5, 350, millis_radius);
-
-
+  ellipse(width / 3 * 2.5, 350 + y_bounce4, millis_radius);
 }
+
+
+
