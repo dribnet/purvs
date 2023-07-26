30c4cf2ea707756427dda325309d69b948c823c0
WillowKin564 <111028696+WillowKin564@users.noreply.github.com>
2023-07-27 11:18:10 +1200

    Lerp Colour
    added pulsing colours and hex code functionality for colour.

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,7 +1,12 @@
+let color1 = "#ffbbcc"
+let color2 = "#8888ee"
+
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
+  let fraction = obj.millis / 1000.0;
+  let bg_color = lerpColor(color(color1), color(color2), fraction);
+  background(bg_color); //  beige
   fill(200); // dark grey
   textSize(40);
   textAlign(CENTER, CENTER);
