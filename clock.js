e161f8160e914106f27f662fbea3aa4677a878fe
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-07-26 13:13:48 +1200

    Revert "Update clock.js"
    This reverts commit c1542437093819895399df9268fc667a8008fe00.

--- clock.js
+++ clock.js
@@ -11,52 +11,74 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  let hours = obj.hours;
-  let minutes = obj.minutes;
-  let seconds = obj.seconds;
-  let millis = obj.millis;
-  let alarm = obj.seconds_until_alarm;
-  
-  background(228,36,38); //  red
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+//   background(50); //  beige
+//   fill(200); // dark grey
+//   textSize(40);
+//   textAlign(CENTER, CENTER);
+//   text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
   
   
-//SUN PULSING
-  fill(252,189,66);// sun orange
-  ellipse(width / 3, 350, 150);
+
+//   fill(249, 1401, 255);// pink
+//   ellipse(width / 3, 350, 150);
   
-  fill(252,189,66,100);
-  let sun_pulse=ellipse(width/3,350,150+millis)
-  let size=map(sun_pulse,150, 600, 150,600 )
-  if (obj.millis>=500){
-    sun_pulse=ellipse(width/3, 350,size-millis)
-  }
+//   fill(249,1401,255,190);
+//   let size=map(ellipse,0, 600, 150,600 )
+//   ellipse(width / 3, 350,150+obj.millis)
+//   if (obj.millis>500){
+//     ellipse(width/3, 350,600-obj.millis)
+//   }
 
 
-  //Purple ellipse
-  let secondsWithFraction   = seconds + (millis / 1000.0);
+//   //Purple ellipse
+//   let secondsWithFraction   = seconds + (millis / 1000.0);
  
-  // text("Seconds with fraction: " + secondsWithFraction, width / 2, 50);
+//   // text("Seconds with fraction: " + secondsWithFraction, width / 2, 50);
 
-  let seconds_radius = 0;
-  if(secondsWithFraction <= 59)  {
-    seconds_radius = map(secondsWithFraction, 0, 60, 1, 300);
-  }
-  else {
-    seconds_radius = map(millis, 0, 999, 300, 1);
-  }
+//   let seconds_radius = 0;
+//   if(secondsWithFraction <= 59)  {
+//     seconds_radius = map(secondsWithFraction, 0, 60, 1, 300);
+//   }
+//   else {
+//     seconds_radius = map(millis, 0, 999, 300, 1);
+//   }
 
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, height/2, seconds_radius);
+//   fill(175, 133, 255); // purple
+//   ellipse(width / 3 * 2, height/2, seconds_radius);
+// }
+ 
+  
+
+//   fill(140, 255, 251) // blue
+//   ellipse(width / 2, 350, 150);
+//   fill(175, 133, 255); // purple
+//   ellipse(width / 3 * 2, 350, 150);
+
+background(210,4,45); //  red/cherry
+//Image(img,0,0,960,500)
+fill(200); // dark grey
+textSize(40);
+textAlign(CENTER, CENTER);
+text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+
+
+let millisSize=0
+
+noStroke()
+
+fill(255,191,0);// Yellow
+ellipse(width / 3, 350, 150);
+
+fill(255,191,0,100);
+fill(255,191,0,150);
+
+if(obj.millis<=500){
+millisSize= map(obj.millis,0,1000,150,300)
+millisSize= map(obj.millis,0,1000,130,300)
 }
- 
-  
-
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
-
+else if(obj.millis>=500){
+millisSize= map(obj.millis,0,1000,300,150)
+millisSize= map(obj.millis,0,1000,300,130)
+}
+ellipse(width/3,350,millisSize)
+}
