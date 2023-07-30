cfe85e8df8bc75fad50ae7f12dff119951e47bd8
RitaClose <104946160+RitaClose@users.noreply.github.com>
2023-07-31 11:40:46 +1200

    Cleaning up Code & Commenting
    Working on a few behind the scenes components like cleaning up the code and adding comments. Also deleted a few parts that doubled up.
    Next will finish the night aesthetic and make sure it's as legible as the rest of the day.

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ I really didn't know what to do for this, I had lots of ideas but not really som
 Working on changing the colour modes so that they work more cohesively in the landscapes and make sense changing colours in the daytime vs. at night.
 I also found a way to use the modulo % to keep the 'hour' numeral a different colour from the rest of the others. This mostly works except at night the background colours are a bit similar, but I think I've found a way around that.
 I have begun adding elements to create the alarm - my idea is that the car will start speeding - maybe with a siren and the landscape will start to blur past while the hour flahses. Getting a seamless transition between the alarm going and not may be difficult though.
+
+31/07
+Over the past few days I have been trying to add in a transition between the alarm beginning and the regualr time. I don't know if it's possible to do a transition after the alarm has ended though. I'm thinking instead of making all the transitions seamless, I could try adding a bright flash covering the screen that would block out the glitching. At the moment it only works if the millis counter is at the right time - I don't know how to get around that.
+
+Maeda Clock:
+I don't know whether this clock needs to be working throughout the day or not - if it doesn't it would be a lot easier to replicate. As it is, it isn't a perfect replica of the original Maeda clock but at least it works - the other issue is there isn't a debugging fuction for that one so making sure it always works has been tricky.
+
+31/07
+I think the next thing I need to work on is commenting my code and cleaning it up so it looks nicer. There are parts that I don't know whether I can condense or not.
--- clock.js
+++ clock.js
@@ -1,19 +1,4 @@
-/*
- * use p5.js to draw a clock on a 960x500 canvas
- */
-
-
-/*
-IDEAS:
-
-- Use log for switch between hours.
-- Dist math function for hour colour change?
-- potentailly use vector for dist function
- - if(alarm > 0 && alarm < 5) - alarm will go off in 5 seconds
- - backgroundColour = map(alarm, 5, 0, 50, 260); etc.
-
-*/
-
+// DRAW CLOCK FUNCTION
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -24,22 +9,29 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
+
+  // Setup Variables
   let carColor = color(0);
   let alarm = obj.seconds_until_alarm;
+  let ringHeight = 600; // Height of Rings
 
+  // Buildings
   function draw_Buildings(x, y, h, w, color) {
     push();
+      //Building Shape
       translate(width / 2, ringHeight);
       if (alarm == 0) {
         rotate(alarmSpin2);
       } else {
-        rotate(scapeDeg + scapePos);
+        rotate(scapeDegSec + scapeDegMin);
       }
       rotate(x + 15);
       fill(color);
       rect(x, y, w, h, 4);
       fill(color - 10);
       rect(x - 1, y + h, w + 2, 5);
+      
+      //Building Windows
       let lightRow = (w / 10) - 1;
       let lightColumn = (h * -1 / 10) - 1;
       if (daySection == night) {
@@ -55,8 +47,9 @@ function draw_clock(obj) {
     pop();
   }
 
+  // Mountains
   function draw_Mountain(rotation, y, h, w, shade, nightshade) {
-    if (nightshade) {
+    if (nightshade) { // Check for Nighttime
       shade -= 100;
     }
     rotate(rotation);
@@ -71,8 +64,9 @@ function draw_clock(obj) {
     triangle(32 + (w / 3.125), y - 100 + h, 0, y - 100 + h, 0, y - 150 + h);
   }
 
+  // Draw Hills
   function draw_Hill(rotation, x, y, h, w, shade, nightshade) {
-    if (nightshade) {
+    if (nightshade) { // Check for Nighttime
       shade += 50;
     }
     rotate(rotation);
@@ -81,14 +75,14 @@ function draw_clock(obj) {
     ellipse(x, y, w, h);
   }
 
+  // Time of day Colour Variables
   colorMode(HSB);
-  let ringHeight = 600;
   let sunrise = [50, 100, 100];
   let dayColor = [210, 100, 100];
   let night = [230, 100, 39];
-  let timeOfDay = sunrise;
   let daySection = sunrise;
 
+  // Check Time of Day
   if (obj.hours >= 5  && obj.hours < 9 || obj.hours >= 17 && obj.hours < 21) {
     daySection = sunrise;
   } else if (obj.hours >= 9 && obj.hours < 17) {
@@ -97,10 +91,15 @@ function draw_clock(obj) {
     daySection = night;
   }
 
-  background(0); //  beige
+  // Set Background Colour
+  if (daySection != night) {
+    background(255);
+  } else {
+    background(0);
+  }
   noStroke();
   
-  // Outer Rings
+  // OUTER RINGS (Inc. Hour Ring)
   fill(daySection[0] - 15, daySection[1], daySection[2]);
   circle(width / 2, ringHeight, width + 500);
   fill(daySection[0] - 10, daySection[1], daySection[2]);
@@ -108,36 +107,37 @@ function draw_clock(obj) {
   fill(daySection[0] - 5, daySection[1], daySection[2]);
   circle(width / 2, ringHeight, width);
   
-  // Inner Ring (Minute Ring)
+  // INNER RING (Minute Ring Background)
   let darkRing = false;
-  if (obj.hours % 2 == 1) {
+  if (obj.hours % 2 == 1) { // Alternate Ring Colour
     darkRing = true;
-  } else {
-    darkRing = false;
-  }
-
-  if (darkRing) {
     fill(daySection[0] - 5, daySection[1], daySection[2] - 5);
   } else {
+    darkRing = false;
     fill(daySection[0] + 5, daySection[1], daySection[2]);
   }
 
   if (daySection == night) {
     fill(235, 100, 30);
   }
+  // Minute Ring Background
   circle(width / 2, ringHeight, width - 30);
   
+  // Time Ring Variables
   angleMode(DEGREES);
-  //Roman Numeral / Hour Ring
-  let hourDeg = map(obj.hours, 0, 23, 0, 690);
+  let hourDeg = map(obj.hours, 0, 23, 0, 690); // Two Rotations in 24 hrs.
   let numeral = ["XII", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI"];
-  let minDeg = map(obj.minutes, 0, 59, -66, 66); //0-126
+  let minDeg = map(obj.minutes, 0, 59, -66, 66);
   let secDeg = map(obj.millis, 0, 999, 0, 2.2);
   let lastSec = map(obj.millis, 0, 999, 0, 30);
   let overlap = map(obj.millis, 0, 999, 0, 15);
   let negOverlap = map(obj.millis, 0, 999, 15, 0);
+  // Time Ring Alarm Variables
+  let alarmFadeIn = map(obj.millis, 0, 999, 0, 200);
+  let alarmFadeOut = map(obj.millis, 0, 999, 200, 0);
+  let alarmSpin = map(obj.millis, 0, 999, 0, 360);
   
-  //Hour Ring
+  //HOUR RING
   push();
     translate(width / 2, ringHeight);
     if (obj.minutes == 59 && obj.seconds == 59) { // Transition to Hour
@@ -150,23 +150,21 @@ function draw_clock(obj) {
         rotate(negOverlap);
       }
     }
-    rotate(hourDeg); // Rotation for top of circle
+    rotate(hourDeg); // Rotation for top of circle ('Current Hour')
     fill(255);
     textSize(100);
     textStyle(BOLD);
     textAlign(CENTER);
-    let alarmFadeIn = map(obj.millis, 0, 999, 0, 200);
-    let alarmFadeOut = map(obj.millis, 0, 999, 200, 0);
 
     for (i = 0; i < 12; i ++) {
-      if (i == (obj.hours % 12)) {
+      if (i == (obj.hours % 12)) { // Numeral Ring Setup
         // fill((daySection == night) ? 255 : 0);
         fill(255);
       } else {
         // fill((daySection == night) ? night : dayColor);
         fill(daySection);
       }
-      if (alarm == 0) {
+      if (alarm == 0) { // Alarm Colour Pulse
         if (obj.millis < 499) {
           fill(daySection[0], alarmFadeIn, 100);
         } else {
@@ -175,26 +173,24 @@ function draw_clock(obj) {
       }
       text(numeral[i], 0, -505);
       rotate(-30);
-      // text(numeral[numeral.length - i], 0, -505);
     }
   pop();
 
-  // Minute Ring
-  let alarmSpin = map(obj.millis, 0, 999, 0, 360);
+  // MINUTE RING
   push();
     translate(width / 2, ringHeight);
     if (alarm == 0) {
       rotate(alarmSpin);
     } else {
       if (obj.seconds == 59){
-        rotate(secDeg);
+        rotate(secDeg); // Smooth Minute Transition
       }
     }
     rotate(minDeg);
     rectMode(CENTER);
     colorMode(HSB);
     
-    for (n = 0; n < obj.minutes; n ++) {
+    for (n = 0; n < obj.minutes; n ++) { // Minute Rectangles
       rotate(-2.5);
       if (darkRing) {
         fill(daySection[0] + 5, daySection[1], daySection[2]);
@@ -207,14 +203,15 @@ function draw_clock(obj) {
 
   //LANDSCAPES
   colorMode(RGB);
-  let scapeDeg = map(obj.seconds, 0, 59, 6, 0);
-  let cityY = -265;
-  let scapePos = map(obj.minutes, 0, 59, 359, 0);
-  let nightShade = false;
+  let scapeDegSec = map(obj.seconds, 0, 59, 6, 0); // Landscape Rotation
+  let scapeDegMin = map(obj.minutes, 0, 59, 359, 0);
+  let cityY = -265; //Y Pos of Buildings
+  let nightShade = false; // Nighttime?
   if (daySection == night) {
     nightShade = true;
   }
 
+  // Minute Alarm Action Variables
   let alarmSpin2 = map(obj.millis, 0, 999, 180, 0);
   if (obj.seconds %2 == 1) {
     alarmSpin2 = map(obj.millis, 0, 999, 360, 180);
@@ -225,10 +222,11 @@ function draw_clock(obj) {
     if (alarm == 0) {
       rotate(alarmSpin2);
     } else {
-      rotate(scapeDeg + scapePos);
+      rotate(scapeDegSec + scapeDegMin);
     }
     rotate(130); // Place around circle
-    // function(rotation, x, y, height, width, shade, night?)
+
+    // Function Format(rotation, x, y, height, width, shade, night?)
     draw_Hill(0, 0, -240, 170, 250, 0, nightShade);
     draw_Mountain(60, -250, 20, 20, -40, nightShade);
     draw_Mountain(-20, -250, 0, 0, 0, nightShade);
@@ -239,7 +237,8 @@ function draw_clock(obj) {
   pop();
   
   rectMode(CORNER);
-  draw_Buildings(-5, cityY, -70, 40, 100);//Negative to reduce Changes || Multiples of 10 Only
+  // Function Format(x, y, -height, width, shade)  //Negative to reduce Changes || Multiples of 10 Only
+  draw_Buildings(-5, cityY, -70, 40, 100);
   draw_Buildings(0, cityY, -140, 40, 160);
   draw_Buildings(2, cityY, -50, 50, 130);
   draw_Buildings(6, cityY, -60, 30, 180);
@@ -253,56 +252,49 @@ function draw_clock(obj) {
   draw_Buildings(27, cityY, -40, 50, 140);
   draw_Buildings(32, cityY, -60, 30, 190);
 
-  // Earth Ring
+  // EARTH RING (Innermost Ring)
   colorMode(HSB);
   fill(daySection[0] - 40, daySection[1], daySection[2] - 70);
   circle(width / 2, ringHeight, width - 400);
 
+  // ROAD SHAPES
   colorMode(RGB);
-  // Road Shapes
   fill(70, 70, 70);
   rect(0, 390, width, 225);
   fill(50, 50, 50);
   rect(0, 400, width, 75);
-
-  //Time Placeholders
-  textSize(30);
-  textAlign(LEFT);
-  fill(255);
-  text(obj.hours + " : " +  obj.minutes + " : " + obj.seconds, 20, 30);
-
-  //CAR
-  let bounce = map(obj.millis, 0, 999, 1, 800);
-  let phase = sin(bounce);
-  let alarmSpeed = map(obj.millis, 0, 999, 0, 250);
-  if (alarm > 0 && alarm <= 1) {
-    draw_Car(50 + alarmSpeed, 230 + phase, phase, carColor);
-  } else if (alarm == 0) {
-    draw_Car(50 + 250, 230 + phase, phase, carColor);
-  } else {
-    carX = 0;
-    draw_Car(50, 230 + phase, phase, carColor);
-  }
 
   //ROAD LINES
   stroke(210);
   strokeWeight(2);
   let linePace = map(obj.millis, 0, 999, width / 5, 0);
   let alarmLinePace = map(obj.millis, 0, 999, width + (width / 5), 0);
-  if (alarm == 0) {
+  if (alarm == 0) { // Alarm (Fast) White Road Lines
     linePace = alarmLinePace;
     for (i = 0; i < 6; i ++) {
       line(linePace + width / 5 * i, 440, linePace + 70 + width / 5 * i, 440);
       line(linePace - width / 5 * i, 440, linePace + 70 - width / 5 * i, 440);
     }
-  } else {
+  } else { // Regualar White Road Lines
     for (i = 0; i < 5; i ++) {
       line(linePace + width / 5 * i, 440, linePace + 70 + width / 5 * i, 440);
       line(linePace - width / 5 * i, 440, linePace + 70 - width / 5 * i, 440);
     }
   }
   stroke(179, 179, 0);
-  line(0, 446, width, 446);
+  line(0, 446, width, 446); // Yellow Center Line
+
+  // CAR SETUP
+  let bounce = map(obj.millis, 0, 999, 1, 800);
+  let phase = sin(bounce);
+  let alarmSpeed = map(obj.millis, 0, 999, 0, 249);
+  if (alarm > 0 && alarm <= 1) {
+    draw_Car(50 + alarmSpeed, 230 + phase, phase, carColor);
+  } else if (alarm == 0) {
+    draw_Car(50 + 250, 230 + phase, phase, carColor);
+  } else {
+    draw_Car(50, 230 + phase, phase, carColor);
+  }
 
   //MARKER POSTS
   let markerPace = map(obj.millis, 0, 999, width / 2, 0);
@@ -314,19 +306,18 @@ function draw_clock(obj) {
   }
   noStroke();
   fill(102, 34, 0);
-  rect(markerWidth + markerPace, 410, 5, 75); //Poles
+  rect(markerWidth + markerPace, 410, 5, 75); // Marker Poles
   rect(markerPace, 410, 5, 75);
   
   angleMode(RADIANS);
   fill(90, 0, 30);
-  arc(markerWidth + markerPace + 4, 396, 38, 64, 0, 2.4 + 0.7, CHORD);
+  arc(markerWidth + markerPace + 4, 396, 38, 64, 0, 2.4 + 0.7, CHORD); // Marker Signs
   arc(markerPace + 4, 396, 38, 64, 0, 2.4 + 0.7, CHORD);
   fill(140, 0, 0);
   arc(markerWidth + markerPace + 2, 396, 36, 64, 0, 2.4 + 0.7, CHORD);
   arc(markerPace + 2, 396, 36, 64, 0, 2.4 + 0.7, CHORD);
   
-  textSize(20);
-  // textStyle(BOLD);
+  textSize(20); // Marker Text Numbers
   textFont('Helvetica');
   textAlign(CENTER);
   fill(255);
@@ -336,15 +327,26 @@ function draw_clock(obj) {
   } else {
     text(obj.seconds - 1, markerPace, 418);
   }
+  
+  // ALARM OVERLAY
+  let opacity = map(obj.millis, 0, 999, 0, 2.55);
+  let flare = sin(opacity);
+  if (alarm > 0 && alarm < 1) {
+    fill(255, 255, 255, flare * 255);
+    rect(0, 0, width, height);
+  }
+
 }
 
+// Draw Car Function
 function draw_Car(x, y, phase, carColor) {
 
+  // Car Shadow
   noStroke();
   fill(0);
   ellipse(x + 135, y + 195 - phase, 280, 20);
 
-  //ALARM
+  // Alarm
   angleMode(RADIANS);
   let sirenSpinSpan = map(obj.millis, 0, 999, 0, 10);
   let sirenSpin = sin(sirenSpinSpan);
@@ -354,16 +356,16 @@ function draw_Car(x, y, phase, carColor) {
   let pulse2 = sin(span2);
   if (obj.seconds_until_alarm == 0) {
     fill(255, 30, 80);
-    rect(x + 150, y + 114, 12, 12, 2);
+    rect(x + 150, y + 114, 12, 12, 2); // Siren Beacon
     fill(255, 220, 220);
     rect(x + 155 + sirenSpin * 3, y + 114, 2, 12, 2);
     fill(255, 30, 80, 100);
-    arc(x + 155, y + 122, pulse * 100, pulse * 100, PI, 0);
+    arc(x + 155, y + 122, pulse * 100, pulse * 100, PI, 0); // Pulsing Lights
     arc(x + 155, y + 122, pulse2 * 100, pulse2 * 100, PI, 0);
   }
 
+  // Window Detailing
   angleMode(DEGREES);
-  //DETAILING
   fill(255, 100);
   quad(x + 80, y + 150, x + 215, y + 150, x + 185, y + 124, x + 100, y + 125);
   noStroke();
@@ -373,7 +375,7 @@ function draw_Car(x, y, phase, carColor) {
   fill(carColor);
   quad(x + 185, y + 123, x + 180, y + 123, x + 210, y + 150, x + 215, y + 150);
 
-  //WHEELS
+  // Wheels
   fill(30);
   circle(x + 62, y + 180 - phase, 35);
   circle(x + 242, y + 180 - phase, 35);
@@ -384,32 +386,34 @@ function draw_Car(x, y, phase, carColor) {
   circle(x + 62, y + 180 - phase, 20);
   circle(x + 242, y + 180 - phase, 20);
 
-  //CAR BODY
+  //Car Body
   fill(carColor);
   stroke(180);
   strokeWeight(1);
   let pointX = [270, 289, 291, 289, 291, 285, 205, 44, -10, -15, -10, 40, 43, 45, 44, 50, 75, 80, 78, 80, 82, 220, 222, 224, 222, 230, 255, 260, 260, 262, 270];
   let pointY = [175, 175, 165, 165, 158, 149, 148, 150, 160, 175, 182, 183, 190, 190, 175, 168, 168, 175, 190, 188, 180, 180, 188, 190, 175, 168, 168, 175, 180, 180, 175];
+  
+  // Lower Body Section
   beginShape();
     for (i = 0; i < pointX.length; i ++) {
       vertex(pointX[i] + x, pointY[i] + y);
     }
   endShape(CLOSE);
 
+  // Top Section
   beginShape();
-    curveVertex(x + 100, y + 150);
+    curveVertex(x + 100, y + 150); // Roof
     curveVertex(x + 90, y + 150);
     curveVertex(x + 120, y + 130);
     vertex(x + 180, y + 125);
     vertex(x + 180, y + 125);
-
-    curveVertex(x + 185, y + 123);//Roof^
+    curveVertex(x + 185, y + 123);
     curveVertex(x + 90, y + 125);
     curveVertex(x + 44, y + 150);//BootArea
     curveVertex(x + 44, y + 150);//BootArea
   endShape();
 
-  //DETAILING
+  //BodyWork Detailing
   fill(220);
   stroke(10);
   quad(x - 14, y + 172, x - 5, y + 172, x - 5, y + 181, x - 10, y + 181);
@@ -422,8 +426,8 @@ function draw_Car(x, y, phase, carColor) {
   rect(x + 90, y + 155, 8, 3, 2);
   rect(x + 155, y + 155, 8, 3, 2);
 
-  //LIGHTS
-  if (obj.hours < 5 || obj.hours > 20) {
+  // Lights
+  if (obj.hours < 5 || obj.hours > 20) { // Nighttime?
     fill(255, 240, 180, 100);
     beginShape();
       curveVertex(x + 284, y + 158);
@@ -435,8 +439,9 @@ function draw_Car(x, y, phase, carColor) {
   }
   fill(200);
   stroke(240);
+  // Front Light Detailing
   beginShape();
-    vertex(x + 289, y + 175);//light
+    vertex(x + 289, y + 175);
     vertex(x + 291, y + 165);
     vertex(x + 289, y + 165);
     vertex(x + 291, y + 158);
