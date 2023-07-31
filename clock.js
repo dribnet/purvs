1fa5dcdfafd66d2b018c90a3abd1eb7309c62b61
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2023-08-01 11:12:39 +1200

    Finished loading all the pngs in and got the rotating on the right time. took out unnecessary bits

--- clock.js
+++ clock.js
@@ -4,8 +4,10 @@
 
 function preload(){
 
-  img1 = loadImage("assets/Earth.png")
-  img2 = loadImage("assets/Moon.png")
+  EarthImg = loadImage("assets/Earth.png")
+  MoonImg = loadImage("assets/Moon.png")
+  SatImg = loadImage("assets/Satellite.png")
+  SunImg = loadImage("assets/Sun.png")
 }
 
 function draw_clock(obj) {
@@ -30,38 +32,37 @@ function draw_clock(obj) {
   let Minutes = obj.minutes;
   let Hours = obj.hours;
   let Milliseconds = obj.millis;
-  text("Seconds: "+Seconds, 700, 100);
-
-
-  fill(249, 174, 10);// yellow sun
-  ellipse(width/2, height/2, 100);
-  image(img1,width/2-25,height/2-25,50,50);
+  
 
   let rotMilli = map(Milliseconds, 0, 1000, 0, 360);
   let rotS = map(Seconds, 0, 60, -90, 270);
   let rotM = map(Minutes, 0, 60, -90, 270);
-  let rotH = map(Hours, 0 , 24, 0, 360);
+  let rotH = map(Hours, 0 , 12, 0, 360);
+
+  push();
+  translate(480,250);
+  rotate(rotH);
+  image(SunImg,-85, -85,170,170);
+  pop();
 
  
   push();
   translate(480,250);
-  rotate(rotS); // rotates on the minute
-  image(img1,125,-25,50);
+  rotate(rotM); // rotates on the minute
+  image(EarthImg,125,-25,50); //Earth
 
   push();
   translate(150,0);
-  rotate(rotMilli); // rotates on the second
-  fill(178, 178, 178); //moon grey
-  //ellipse(50,0,30);
-  image(img2,35,-15,30);
+  rotate(rotMilli); 
+  image(MoonImg,35,-15,30); //Moon
   pop();
   pop();
 
 
   push();
   translate(480,250);
-  rotate(rotM);
-  rect(225,0,15,30);
+  rotate(rotS);
+  image(SatImg,225,0,20,40); //Satellite
   pop();
 
 }
