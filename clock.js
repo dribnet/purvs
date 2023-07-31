9288f38a4d7b827fa021d5afe43b39a538f5a3e3
Danielle <xdholmesx@gmail.com>
2023-07-31 20:23:16 +1200

    Added circle showing seconds

--- clock.js
+++ clock.js
@@ -2,13 +2,34 @@
  * use p5.js to draw a clock on a 960x500 canvas
  */
 var offset = 0;
+var offset2 = 0;
+
+var moonStars = [];
 
 var prevmin = undefined;
 var prevhour = undefined;
 var testing = [];
 
+let font;
+
+let fontPoints = {
+  romanI: [],
+  romanV: [],
+  romanX: []
+};
+
+let fontBounds  = {
+  romanI: [],
+  romanV: [],
+  romanX: []
+};
+
+function preload() {
+  //font = loadFont('HEAVEN MINE.otf');
+  font = loadFont('MartianMono_SemiCondensed-Medium.ttf')
+}
+
 function draw_clock(obj) {
-  angleMode(DEGREES);
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
   //    obj.minutes goes from 0-59
@@ -37,8 +58,43 @@ function draw_clock(obj) {
     colon: [createVector(37,5), createVector(41,15), createVector(35,25), 0, createVector(41,120), createVector(37,105), createVector(35,100)]
   };
 
+  const romanNumerals = [
+    [fontPoints.romanI],
+    [fontPoints.romanI, fontPoints.romanI],
+    [fontPoints.romanI, fontPoints.romanI, fontPoints.romanI],
+    [fontPoints.romanI, fontPoints.romanV],
+    [fontPoints.romanV],
+    [fontPoints.romanV, fontPoints.romanI],
+    [fontPoints.romanV,fontPoints.romanI,fontPoints.romanI],
+    [fontPoints.romanV,fontPoints.romanI,fontPoints.romanI,fontPoints.romanI],
+    [fontPoints.romanI,fontPoints.romanX],
+    [fontPoints.romanX],
+    [fontPoints.romanX,fontPoints.romanI],
+    [fontPoints.romanX,fontPoints.romanI,fontPoints.romanI],
+  ];
+
   const numarray = [numbers.zero, numbers.one, numbers.two, numbers.three, numbers.four, numbers.five, numbers.six, numbers.seven, numbers.eight, numbers.nine];
 
+  if(prevhour == undefined) {
+    fontPoints.romanI = font.textToPoints('I', 0, 0, 12, {
+      sampleFactor: 5,
+      simplifyThreshold: 0
+    });
+    fontBounds.romanI = font.textBounds('I', 0, 0, 12);
+
+    fontPoints.romanV = font.textToPoints('V', 0, 0, 12, {
+      sampleFactor: 5,
+      simplifyThreshold: 0
+    });
+    fontBounds.romanV = font.textBounds('V', 0, 0, 12);
+
+    fontPoints.romanX = font.textToPoints('X', 0, 0, 12, {
+      sampleFactor: 5,
+      simplifyThreshold: 0
+    });
+    fontBounds.romanX = font.textBounds('X', 0, 0, 12);
+
+  }
 
 
 
@@ -48,30 +104,17 @@ function draw_clock(obj) {
     let v1 = createVector(0,0);
     let v2 = createVector(100, 100);
 
-    //testing = [];
-    //testing = new ClockNumber(getNumberLines(numbers.zero), 200, 200, 8, 8);
-    /*testing.push(new ClockNumber(getNumberLines(numbers.zero), 50, 50, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.one), 150, 50, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.two), 250, 50, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.three), 350, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.four), 450, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.five), 550, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.six), 650, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.seven), 750, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.eight), 850, 200, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.nine), 50, 250, 8, 8));
-    testing.push(new ClockNumber(getNumberLines(numbers.colon), 150, 250, 8, 8));*/
     let dig1 = floor(obj.hours/10);
     let dig2 = obj.hours%10;
     let dig3 = floor(obj.minutes/10);
     let dig4 = obj.minutes%10;
 
 
-    if(dig1 != floor(prevhour/10) || prevhour == undefined) testing[0]=(new ClockNumber(getNumberLines(numarray[floor(obj.hours/10)]), -250, -62, 8, 8));
-    if(dig2 != prevhour%10 || prevhour == undefined) testing[1]=(new ClockNumber(getNumberLines(numarray[obj.hours%10]), -150, -62, 8, 8));
-    testing[2]=(new ClockNumber(getNumberLines(numbers.colon), -50, -62, 8, 8));
-    if(dig3 != floor(prevmin/10) || prevmin == undefined) testing[3]=(new ClockNumber(getNumberLines(numarray[floor(obj.minutes/10)]), 50, -62, 8, 8));
-    if(dig4 != prevmin%10 || prevmin == undefined) testing[4]=(new ClockNumber(getNumberLines(numarray[obj.minutes%10]), 150, -62, 8, 8));
+    if(dig1 != floor(prevhour/10) || prevhour == undefined) testing[0]=(new ClockNumber(getNumberLines(numarray[floor(obj.hours/10)]), -220, -62, 8, 8));
+    if(dig2 != prevhour%10 || prevhour == undefined) testing[1]=(new ClockNumber(getNumberLines(numarray[obj.hours%10]), -130, -62, 8, 8));
+    testing[2]=(new ClockNumber(getNumberLines(numbers.colon), -40, -62, 8, 8));
+    if(dig3 != floor(prevmin/10) || prevmin == undefined) testing[3]=(new ClockNumber(getNumberLines(numarray[floor(obj.minutes/10)]), 30, -62, 8, 8));
+    if(dig4 != prevmin%10 || prevmin == undefined) testing[4]=(new ClockNumber(getNumberLines(numarray[obj.minutes%10]), 120, -62, 8, 8));
 
     prevmin = obj.minutes;
     prevhour = obj.hours;
@@ -85,13 +128,14 @@ function draw_clock(obj) {
   
   if(offset > 30) offset = -30;
   offset += 0.5;
+  offset2 += 0.5;
 
   var offs = abs(offset);
 
   noStroke();
   let noiseScale = 0.02
-  let c1 = color(0,5,5,30);
-  let c2 = color(0,120,120,30);
+  let c1 = color(0,5,5,40);
+  let c2 = color(0,120,120,40);
   for(let x = 0; x < width; x+=15) {
     for(let y = 0; y < innerHeight; y+=15) {
       var val = noise(x*noiseScale,y*noiseScale);
@@ -105,13 +149,13 @@ function draw_clock(obj) {
     }
   }
 
-  drawCircle(1.5, width/2, height/2.1, 150+offs);
-  drawCircle(2, width/1.9, height/1.8, 70-offs);
-  drawCircle(1.2, width/2.1, height/1.9, 0-offs);
-  drawCircle(2.3, width/2, height/2, 200-offs);
-  drawCircle(1.9, width/1.9, height/2, 30+offs);
-  drawCircle(1.8, width/2.1, height/2.1, 140-offs);
-  drawCircle(0.9, width/1.95, height/1.9, 90+offs);
+  drawCircle(1.5, width/2, height/2.1, 150+offs, 30);
+  drawCircle(2, width/1.9, height/1.8, 70-offs, 40);
+  drawCircle(1.2, width/2.1, height/1.9, 0-offs, 25);
+  drawCircle(2.3, width/2, height/2, 200-offs, 30);
+  drawCircle(1.9, width/1.9, height/2, 30+offs, 50);
+  drawCircle(1.8, width/2.1, height/2.1, 140-offs, 30);
+  drawCircle(0.9, width/1.95, height/1.9, 90+offs, 25);
 
   generateStars();
 
@@ -126,12 +170,97 @@ function draw_clock(obj) {
 
   push();
   translate(width/2,height/2);
-  scale(0.8);
+  scale(0.75);
   for(let i = 0; i < testing.length; i++) {
     testing[i].drawNumber();
   }
   pop();
 
+  drawCircle(1, width/2, height/2, 150+offset2, 100);
+  noFill();
+  stroke(255);
+  strokeWeight(1);
+  circle(width/2, height/2, 420);
+
+  stroke(255,255,255,2.5);
+  for(let i = 0; i < 10; i++) {
+    strokeWeight(50-(4.5*i));
+    circle(width/2, height/2, 410);
+  }
+
+  
+  push();
+  translate(width/2, height/2);
+  rotate(PI/6);
+  for(let i = 1; i < 13; i++) {
+    if(i%2==0) {
+      fill(255,255,255,100);
+      noStroke();
+    } else {
+      stroke(255);
+      noFill();
+      strokeWeight(0.3);
+    }
+
+    beginShape();
+    vertex(0, 220);
+    vertex(3, 215);
+    vertex(0, 210);
+    vertex(-3, 215);
+    endShape(CLOSE);
+
+    stroke(255);
+    noFill();
+    strokeWeight(0.3);
+
+    for(let n = 0; n < romanNumerals[i-1].length; n++) {
+
+      beginShape();
+      for (let j = 0; j < romanNumerals[i-1][n].length; j++) {
+        let p = romanNumerals[i-1][n][j];
+        vertex(p.x+(fontBounds.romanV.w+1)*(n-(romanNumerals[i-1].length/2)), p.y-230);
+      }
+      endShape();
+    }
+
+    rotate(PI/6);
+  }
+  pop();
+  
+
+  let sec = obj.seconds;
+  let mill = obj.millis;
+
+  let moonAngle = map(sec + mill/1000, 0, 60, 0, 2*PI);
+
+  let moonVec = p5.Vector.fromAngle(moonAngle-PI/2, 208);
+  stroke(255,255,255,7);
+  strokeWeight(5)
+  fill(255);
+  push();
+  translate(width/2+moonVec.x, height/2+moonVec.y);
+  scale(1.3)
+
+  for(let i = 0; i < 5; i++) {
+    strokeWeight(14-i*2.5);
+    beginShape();
+    curveVertex(4, -8);
+    curveVertex(0, -9);
+    curveVertex(-7, -7);
+    curveVertex(-10, 0);
+    curveVertex(-7, 7);
+    curveVertex(0, 9);
+    curveVertex(4, 8);
+    curveVertex(4, 8);
+    curveVertex(-2, 5);
+    curveVertex(-4, 0);
+    curveVertex(-2, -5);
+    curveVertex(4, -8);
+    curveVertex(4, -8);
+    endShape(CLOSE);
+  }
+
+  pop();
 
 }
 
@@ -153,12 +282,12 @@ function getNumberLines(points) {
 
 
 
-function drawCircle(scale, x, y, rot) {
+function drawCircle(scale, x, y, rot, opacity) {
   push();
   translate(x, y);
-  rotate(rot);
+  rotate(rot*0.02);
   noFill();
-  stroke(255, 255, 255, 30);
+  stroke(255, 255, 255, opacity);
   strokeWeight(0.3);
   beginShape();
   var inc = 5;
@@ -275,6 +404,13 @@ class NumLine {
   drawSelf() {
     //stroke(255);
     //line(this.end1.x, this.end1.y, this.end2.x, this.end2.y);
+    stroke(230,230,255,2);
+    strokeWeight(60);
+    for(let i = 0; i < 6; i++) {
+      strokeWeight(50-8*i);
+      line(this.end1.x, this.end1.y, this.end2.x, this.end2.y);
+    }
+    
     this.stars.forEach(element => element.drawSelf());
     this.decorationstars.forEach(element => element.drawSelf());
   }
@@ -283,7 +419,7 @@ class NumLine {
 
 class NumStar {
   constructor(homeLine, x, y) {
-    this.distance = 0;
+    this.lifetime = 20;
     this.x = x;
     this.y = y;
     this.originx = x;
@@ -291,6 +427,7 @@ class NumStar {
   }
 
   drawSelf() {
+    this.lifetime--;
     noStroke();
     fill(255,255,255,10);
     circle(this.x, this.y, 3);
@@ -307,6 +444,10 @@ class NumStar {
 
   pos() {
     return createVector(this.x, this.y);
+  }
+
+  lifetime() {
+    return this.lifetime;
   }
 }
 
