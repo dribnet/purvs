870ffd64bf574fd3288f4ce4fef213bf7b7306ec
Danielle <xdholmesx@gmail.com>
2023-07-31 17:53:29 +1200

    Created constellation numbers

--- clock.js
+++ clock.js
@@ -3,20 +3,9 @@
  */
 var offset = 0;
 
-var numbers = {
-  zero: [],
-  one: [],
-  two: [],
-  three: [],
-  four: [],
-  five: [],
-  six: [],
-  seven: [],
-  eight: [],
-  nine: [],
-};
-
-
+var prevmin = undefined;
+var prevhour = undefined;
+var testing = [];
 
 function draw_clock(obj) {
   angleMode(DEGREES);
@@ -34,13 +23,70 @@ function draw_clock(obj) {
   textSize(40);
   textAlign(CENTER, CENTER);
 
+  const numbers = {
+    zero: [createVector(20,0), createVector(55,0), createVector(75,40), createVector(75,100), createVector(55,125), createVector(20,125), createVector(0,100), createVector(20,0)],
+    one: [createVector(0,50), createVector(50,0), createVector(37,125)],
+    two: [createVector(0,50), createVector(20,0), createVector(55,0), createVector(75,50), createVector(0,125), createVector(75,125)],
+    three: [createVector(0,30), createVector(20,0), createVector(55,0), createVector(75,50), createVector(40,60), createVector(75,90), createVector(60,125), createVector(20,125), createVector(0,100)],
+    four: [createVector(75,75), createVector(75,75), createVector(0,75), createVector(50,0), createVector(50,125), createVector(50,125)],
+    five: [createVector(75,0), createVector(0,0), createVector(0,50), createVector(75,60), createVector(75,100), createVector(50,125), createVector(20,125), createVector(0,100)],
+    six: [createVector(75,30), createVector(50,0), createVector(20,0), createVector(0,60), createVector(0,100), createVector(30,125), createVector(60,125), createVector(75,100), createVector(50,65), createVector(0,75)],
+    seven: [createVector(0,0), createVector(75,0), createVector(40,60), createVector(10,125)],
+    eight: [createVector(55,0), createVector(20,0), createVector(0,50), createVector(37,62), createVector(75,75), createVector(75,105), createVector(55,125), createVector(20,125), createVector(0,105), createVector(0,75), createVector(37,62), createVector(75,50), createVector(75,20), createVector(55,0)],
+    nine: [createVector(55,50), createVector(35,60), createVector(0,40), createVector(0,20), createVector(20,0), createVector(55,0), createVector(55,0), createVector(75,30), createVector(0,125)],
+    colon: [createVector(37,5), createVector(41,15), createVector(35,25), 0, createVector(41,120), createVector(37,105), createVector(35,100)]
+  };
+
+  const numarray = [numbers.zero, numbers.one, numbers.two, numbers.three, numbers.four, numbers.five, numbers.six, numbers.seven, numbers.eight, numbers.nine];
+
+
+
+
+  if(prevmin == undefined || obj.minutes != prevmin) {
+    console.log(prevmin);
+
+    let v1 = createVector(0,0);
+    let v2 = createVector(100, 100);
+
+    //testing = [];
+    //testing = new ClockNumber(getNumberLines(numbers.zero), 200, 200, 8, 8);
+    /*testing.push(new ClockNumber(getNumberLines(numbers.zero), 50, 50, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.one), 150, 50, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.two), 250, 50, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.three), 350, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.four), 450, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.five), 550, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.six), 650, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.seven), 750, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.eight), 850, 200, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.nine), 50, 250, 8, 8));
+    testing.push(new ClockNumber(getNumberLines(numbers.colon), 150, 250, 8, 8));*/
+    let dig1 = floor(obj.hours/10);
+    let dig2 = obj.hours%10;
+    let dig3 = floor(obj.minutes/10);
+    let dig4 = obj.minutes%10;
+
+
+    if(dig1 != floor(prevhour/10) || prevhour == undefined) testing[0]=(new ClockNumber(getNumberLines(numarray[floor(obj.hours/10)]), -250, -62, 8, 8));
+    if(dig2 != prevhour%10 || prevhour == undefined) testing[1]=(new ClockNumber(getNumberLines(numarray[obj.hours%10]), -150, -62, 8, 8));
+    testing[2]=(new ClockNumber(getNumberLines(numbers.colon), -50, -62, 8, 8));
+    if(dig3 != floor(prevmin/10) || prevmin == undefined) testing[3]=(new ClockNumber(getNumberLines(numarray[floor(obj.minutes/10)]), 50, -62, 8, 8));
+    if(dig4 != prevmin%10 || prevmin == undefined) testing[4]=(new ClockNumber(getNumberLines(numarray[obj.minutes%10]), 150, -62, 8, 8));
+
+    prevmin = obj.minutes;
+    prevhour = obj.hours;
+
+    
+  }
+
+  
+
+
   
   if(offset > 30) offset = -30;
   offset += 0.5;
 
   var offs = abs(offset);
-
-  
 
   noStroke();
   let noiseScale = 0.02
@@ -78,10 +124,34 @@ function draw_clock(obj) {
   rect(0,0,width,height);
 
 
-  drawStar(200, 200);
+  push();
+  translate(width/2,height/2);
+  scale(0.8);
+  for(let i = 0; i < testing.length; i++) {
+    testing[i].drawNumber();
+  }
+  pop();
 
 
 }
+
+
+
+function getNumberLines(points) {
+  lines = []
+  for(let i = 1; i < points.length; i++) {
+    if(points[i] == 0 || points[i-1] == 0) continue;
+    lines.push(new NumLine(points[i-1], points[i]));
+  }
+
+  return lines;
+
+}
+
+
+
+
+
 
 function drawCircle(scale, x, y, rot) {
   push();
@@ -131,5 +201,114 @@ function drawStar(x, y, scale) {
   fill(255, 100);
   circle(x, y, off/4+1.5*scale);
 }
+
+function distToLine(linePoint1, linePoint2, point) {
+  
+  d1 = p5.Vector.sub(linePoint2, linePoint1);
+  d2 = p5.Vector.sub(point, linePoint1);
+  l1 = d1.mag();
+  
+  dotp = constrain(d2.dot(d1.normalize()), 0, l1);
+      
+  let vec = p5.Vector.add(linePoint1, d1.mult(dotp));
+  return point.dist(vec);
+  
+}
+
+
+class ClockNumber {
+  constructor(lines, x, y, randomness, density) {
+    this.lines = lines;
+    this.x = x;
+    this.y = y;
+    this.allstars = [];
+    this.lines.forEach(element => element.generate(randomness, this.allstars, density, 0.3));
+
+  }
+
+  drawNumber() {
+    push();
+    translate(this.x, this.y);
+    this.lines.forEach(element => element.drawSelf());
+
+    stroke(255,255,255,200);
+    strokeWeight(0.1);
+    for(let i = 0; i < this.allstars.length-1; i++) {
+      for(let j = 0; j < this.allstars.length-1; j++) {
+        if(i == j) continue;
+        let p1 = this.allstars[i].pos();
+        let p2 = this.allstars[j].pos();
+        if(p1.dist(p2) < 15) {
+          line(p1.x, p1.y, p2.x, p2.y);
+        }
+      }
+    }
+
+    pop();
+  }
+}
+
+
+class NumLine {
+  constructor(end1, end2) {
+    this.end1 = end1;
+    this.end2 = end2;
+    this.units = ceil(end1.dist(end2)/25);
+    this.stars = [];
+    this.decorationstars = [];
+  }
+
+  generate(randomness, starlist, density, decorationDensity) {
+    for(let i = 0; i < density*this.units; i++) {
+      let t = i/(density*this.units);
+      let vec = p5.Vector.lerp(this.end1, this.end2, t);
+      this.stars.push(new NumStar(this, vec.x + random(-randomness,randomness), vec.y + random(-randomness,randomness)));
+
+      if(random() > 1-decorationDensity) this.decorationstars.push(new NumStar(this, vec.x + random(-randomness*5,randomness*5), vec.y + random(-randomness*5,randomness*5)));
+
+    }
+
+    starlist.push.apply(starlist, this.stars);
+
+  }
+
+  drawSelf() {
+    //stroke(255);
+    //line(this.end1.x, this.end1.y, this.end2.x, this.end2.y);
+    this.stars.forEach(element => element.drawSelf());
+    this.decorationstars.forEach(element => element.drawSelf());
+  }
+}
+
+
+class NumStar {
+  constructor(homeLine, x, y) {
+    this.distance = 0;
+    this.x = x;
+    this.y = y;
+    this.originx = x;
+    this.originy = y;
+  }
+
+  drawSelf() {
+    noStroke();
+    fill(255,255,255,10);
+    circle(this.x, this.y, 3);
+    fill(255,255,255,random(100,255));
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+    if(dist(this.x, this.y, this.originx, this.originy) > 6) {
+      let newvec = p5.Vector.lerp(createVector(this.x, this.y), createVector(this.originx, this.originy), 0.1);
+      this.x = newvec.x;
+      this.y = newvec.y;
+    }
+    circle(this.x, this.y, 1);
+  }
+
+  pos() {
+    return createVector(this.x, this.y);
+  }
+}
+
 
 
