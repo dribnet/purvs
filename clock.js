bf8ca1f84b1454bb12645d75d74a63780a85724e
Michael Aphayvong <103137983+robbzombiee@users.noreply.github.com>
2023-08-02 21:49:39 +1200

    big update
    added souls with code to display hours of the day, added AMPM background changes depending on the time.

--- clock.js
+++ clock.js
@@ -4,7 +4,10 @@
 
 let bodyMain;
 let backgroundMain;
+let soulMain;
+let soulVariable = 0;
 let bodyMainValue = 0;
+let backgroundMainValue = 0;
 let _myVariable = 0;
 
 function preload(){
@@ -14,7 +17,37 @@ function preload(){
   ]
 
   backgroundMain = [
-    loadImage("backgroundMain1.jpg")
+    loadImage("background/backgroundMain1.jpg"),
+    loadImage("background/backgroundMain2.png")
+  ]
+
+  soulMain = [
+    loadImage("soul/soul_00.png"),
+    loadImage("soul/soul_01.png"),
+    loadImage("soul/soul_02.png"),
+    loadImage("soul/soul_03.png"),
+    loadImage("soul/soul_04.png"),
+    loadImage("soul/soul_05.png"),
+    loadImage("soul/soul_06.png"),
+    loadImage("soul/soul_07.png"),
+    loadImage("soul/soul_08.png"),
+    loadImage("soul/soul_09.png"),
+    loadImage("soul/soul_10.png"),
+    loadImage("soul/soul_11.png"),
+    loadImage("soul/soul_12.png"),
+    loadImage("soul/soul_13.png"),
+    loadImage("soul/soul_14.png"),
+    loadImage("soul/soul_15.png"),
+    loadImage("soul/soul_16.png"),
+    loadImage("soul/soul_17.png"),
+    loadImage("soul/soul_18.png"),
+    loadImage("soul/soul_19.png"),
+    loadImage("soul/soul_20.png"),
+    loadImage("soul/soul_21.png"),
+    loadImage("soul/soul_22.png"),
+    loadImage("soul/soul_23.png"),
+    loadImage("soul/soul_24.png"),
+
 
   ]
 }
@@ -33,6 +66,9 @@ function draw_clock(obj) {
   let seconds = obj.seconds;
   let minutes = obj.minutes;
   let hours = obj.hours;
+  let milliseconds = obj.millis;
+
+  let soulMovement = map(milliseconds,0,999,5,10);
 
    function setMyVariable() {
     if (_myVariable !== seconds) {
@@ -52,9 +88,32 @@ function draw_clock(obj) {
       textFont('Georgia');
       text(minutes, 456, 182.5);
 }
-image(backgroundMain[0],0,0)
+
+function soulVAR(){
+  soulVariable = hours
+}
+
+function checkAM_PM (){
+  if (hours < 12) {
+    backgroundMainValue = 0;}
+
+    if (hours > 12){
+      backgroundMainValue = 1;
+    }
+
+    if (hours === 12){
+      backgroundMainValue = 1;
+    }
+  }
+
+print(soulVariable);
+image(backgroundMain[backgroundMainValue],0,0);
 image(bodyMain[bodyMainValue], 0, 0);
+image(soulMain[soulVariable],0,soulMovement);
+
+checkAM_PM();
 setMyVariable(); // call lantern function
 minuteNecklaceTimer(); // call minuteNecklaceTimer
+soulVAR();
 
 }
