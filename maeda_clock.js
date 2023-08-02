c6a677e4d5249312ad917fdced46603208bae391
Michael Aphayvong <103137983+robbzombiee@users.noreply.github.com>
2023-08-03 04:57:40 +1200

    update
    fixed some things

--- maeda_clock.js
+++ maeda_clock.js
@@ -3,21 +3,26 @@ function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
   background(50); //  beige
 
+  // 1
   rect(50, 40, 50, 400);
 
+  // 0
   rect(130, 40, 50, 400);
   rect(240, 40, 50, 400);
   rect(130, 40, 160, 50);
   rect(130, 390, 160, 50);
 
+  // COLON
   rect(320, 40, 50, 50);
   rect(320, 390, 50, 50);  
 
+  // 0
   rect(400, 40, 50, 400);
   rect(510, 40, 50, 400);
   rect(400, 40, 160, 50);
   rect(400, 390, 160, 50);
 
+  // 0
   rect(600, 40, 50, 400);
   rect(710, 40, 50, 400);
   rect(600, 40, 160, 50);
