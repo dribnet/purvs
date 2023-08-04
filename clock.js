255e644737bde8438871bca00ab96187ede480ec
phoebeoliff <phoebeoliff@gmail.com>
2023-08-04 15:37:47 +1200

    code clean up

--- clock.js
+++ clock.js
@@ -9,15 +9,7 @@ function preload() {
 }
 
 function draw_clock(obj) {
-  // draw your own clock here based on the values of obj:
-  //    obj.hours goes from 0-23
-  //    obj.minutes goes from 0-59
-  //    obj.seconds goes from 0-59
-  //    obj.millis goes from 0-999
-  //    obj.seconds_until_alarm is:
-  //        < 0 if no alarm is set
-  //        = 0 if the alarm is currently going off
-  //        > 0 --> the number of seconds until alarm should go off
+
   let seconds = obj.seconds; 
   let minutes = obj.minutes;
   let hours = obj.hours; 
@@ -29,7 +21,6 @@ function draw_clock(obj) {
     let text_color = color(107, 161, 121); //green
   
  
-
 
     if(alarm > 0 && alarm < 5) {
       background_color = map(alarm, 5, 0, 50, 200)
@@ -61,25 +52,6 @@ function draw_clock(obj) {
   textAlign(CENTER, CENTER);
   text (); 
  
-
-
-
-//example ellipses from 18/07/23
-  // let millis_radius = map(millis, 0, 999, 1, 150); //ellipse changes size with millis
-  // fill(249, 140, 255);// pink
-  // ellipse(width / 3, 350, millis_radius);
-  
-
-  // let minutes_radius = map(minutes, 0, 59, 1, 150); //ellipse chanages size with minutes 
-  // fill(140, 255, 251) // blue
-  // ellipse(width / 2, 350, minutes_radius);
-  
-
-  // let seconds_radius = map(seconds, 0, 59, 1, 150); //letting ellipse get to size as other ellipses 
-  // fill(175, 133, 255); // purple
-  // ellipse(width / 3 * 2, 350, seconds_radius); //ellipse getting larger with seconds 
-
-
 
 
   let bounce1 = map(obj.millis, 0, 999, 0, TWO_PI);
@@ -144,8 +116,6 @@ ellipse (300,100+y_bounce2, 100, 15); //inside hole UFO front
 
 noStroke();
   fill (255);
-  // triangle(180, 470, 300, 100, 430, 470); //lightbeamFront 
-  // triangle(180+x_shift1, beamY1, 300, beamY2+y_bounce2, 430+x_shift2, beamY3);
   quad(180+x_shift1, 470, 265, 100, 335, 100, 430+x_shift2, 470);
  
   
@@ -159,8 +129,7 @@ noStroke();
 
   fill (255);
   noStroke();
-  // triangle(660, 440, 750, 160, 830, 440); //lightbeamBack
-  // triangle(660+x_shift1, beamY4, 750, beamY5+y_bounce1, 830+x_shift2, beamY6);
+
   quad(660+x_shift1, 440, 730, 164+y_bounce1, 770, 164+y_bounce1, 830+x_shift2, 440); 
   fill (255);
   stroke (200);
@@ -174,7 +143,7 @@ noStroke();
 
 let minutes_lift = map(minutes, 0, 59, 440, 140); //make number move up with hour
 fill (text_color);
-// rect(286, hours_lift, 30, 100); //hoursnumbers
+
 noStroke();
 textSize(80);
 text(hours, 300, minutes_lift);
@@ -187,20 +156,10 @@ let seconds_lift = map(seconds, 0, 59, 425, 175); //make number move up with min
 fill (text_color);
 noStroke();
 textSize(40);
-// text(minutes, 750, seconds_lift);
 text(minutes, 750, seconds_lift);
-// rect(720, minutes_lift, 15, 50); //minutesnumbers 
 
 
-// fill (77, 255, 5);
-// rect(760, minutes_lift, 15, 50); //minutesnumbers
-
-
-image(img3, 0, 0, 960, 500);
-
-
-
-//testing landings
+image(img3, 0, 0, 960, 500); //trees
 
 
 
